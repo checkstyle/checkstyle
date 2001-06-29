3835bf9e4e9194097b8438de0167d758926c4890
oburn <devnull@localhost>
2001-06-29 07:23:11 +0000

    first cut

---
+++ prj.el
@@ -0,0 +1,7 @@
+(jde-set-project-name "chainsaw")
+(jde-set-variables
+ '(jde-db-source-directories (quote ("h:/checkstyle/src/checkstyle/")))
+ '(jde-global-classpath (quote ("h:/checkstyle/classes/checkstyle" "h:/checkstyle/classes/tests" "C:/apps/jakarta-regexp-1.2/jakarta-regexp-1.2.jar")))
+ '(jde-run-application-class "com.puppycrawl.tools.checkstyle.Main")
+ '(jde-run-option-application-args (quote ("VerifierImpl.java")))
+)
