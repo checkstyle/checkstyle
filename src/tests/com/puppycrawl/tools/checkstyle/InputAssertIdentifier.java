aa5eb40574501554885572112508770e72adee4e
lkuehne <devnull@localhost>
2002-03-19 20:19:51 +0000

    added test for grammar fallback in Checker (assert as identifier)

---
+++ src/tests/com/puppycrawl/tools/checkstyle/InputAssertIdentifier.java
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+// Test case file for checkstyle.
+// Created: 2001
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle;
+
+/**
+ * Test case for assert handling fallback.
+ * This file uses "assert" is as an identifier, so the default grammar
+ * fails and the JDK 1.3 grammar has to be used.
+ * @author Lars Kühne
+ **/
+class InputAssertIdentifier
+{
+    /** test method **/
+    void assert()
+    {
+        int assert = 1;
+        int a = assert;
+    }
+}
