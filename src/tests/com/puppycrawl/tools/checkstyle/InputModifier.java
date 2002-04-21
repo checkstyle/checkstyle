206ff56be6d052ebd15d06bffeba31dd14947119
Lars Kühne <lakuehne@gmail.com>
2002-04-21 21:06:42 +0000

    added test for rfe 537107, "public" in interface method
    moved test input for "order of modifiers" to InputModifiers
    to keep InputSimple simple

---
+++ src/tests/com/puppycrawl/tools/checkstyle/InputModifier.java
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+// Test case file for checkstyle.
+// Created: 2001
+////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle;
+
+/**
+ * Test case for Modifier checks:
+ * - order of modifiers
+ * - use of 'public' in interface definition
+ * @author lkuehne
+ */
+strictfp final class InputModifier // illegal order of modifiers for class
+{
+
+    /** Illegal order of modifiers for variables */
+    static private boolean sModifierOrderVar = false;
+
+    /**
+     * Illegal order of modifiers for methods. Make sure that the
+     * first and last modifier from the JLS sequence is used.
+     */
+    strictfp private void doStuff()
+    {
+    }
+
+    /** holder for redundant 'public' modifier check. */
+    public interface InputRedundantPublicModifier
+    {
+        /** redundant 'public' modifier */
+        public void a();
+
+        /** all OK */
+        void b();
+    }
+}
