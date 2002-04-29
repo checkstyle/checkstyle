e5eaf46c6b3ba7cf9bac5e1514b5195d15366fd5
oburn <devnull@localhost>
2002-04-29 04:07:08 +0000

    Fixed Checkstyle errors. Important to make sure that Checkstyle passes with no errors, as I get asked if "Checkstyle Checkstyle's" all the time. :-)

--- src/checkstyle/com/puppycrawl/tools/checkstyle/CheckStyleTask.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/CheckStyleTask.java
@@ -76,18 +76,18 @@ public class CheckStyleTask
     /**
      * Sets a properties file for use instead
      * of individually setting them
-     * @param props the properties File to use
+     * @param aProps the properties File to use
      */
-    public void setProperties(File props)
+    public void setProperties(File aProps)
     {
         Properties mProperties = new Properties();
         try {
-            mProperties.load(new FileInputStream(props));
+            mProperties.load(new FileInputStream(aProps));
             mConfig = new Configuration(mProperties, System.out);
         }
         catch (Exception e) {
             throw new BuildException(
-                "Could not find Properties file '" + props + "'", location);
+                "Could not find Properties file '" + aProps + "'", location);
         }
     }
 
