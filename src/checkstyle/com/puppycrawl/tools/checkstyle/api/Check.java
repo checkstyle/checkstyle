d905adf77e22e301831cd4f68d4462171f5b9b6b
Oliver Burn <oliver@puppycrawl.com>
2002-09-15 11:07:01 +0000

    the start of an interesting adventure

---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/api/Check.java
@@ -0,0 +1,179 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.api;
+
+import java.util.Map;
+
+/**
+ * The base class for checks.
+ *
+ * @author <a href="mailto:checkstyle@puppycrawl.com">Oliver Burn</a>
+ * @version 1.0
+ */
+public abstract class Check
+{
+    /** name to store lines under */
+    private static final String LINES_ATTRIBUTE = "lines";
+    /** name to store filename under */
+    private static final String FILENAME_ATTRIBUTE = "filename";
+
+    /** the global context for the check */
+    private Map mGlobalContext;
+    /** the context for the check across an AST */
+    private Map mTreeContext;
+    /** the context for a check across a token. */
+    private Map mTokenContext;
+
+    /**
+     * Returns the default token a check is interested in. Only used if the
+     * configuration for a check does not define the tokens.
+     * @return the default tokens
+     */
+    public abstract int[] getDefaultTokens();
+
+    /**
+     * Return the global context object for check. This context is valid for
+     * the lifetime of the check.
+     * @return the context object
+     */
+    public Map getGlobalContext()
+    {
+        return mGlobalContext;
+    }
+
+    /**
+     * Set the global context for the check.
+     * @param aGlobalContext the global context
+     */
+    public void setGlobalContext(Map aGlobalContext)
+    {
+        mGlobalContext = aGlobalContext;
+    }
+
+    /**
+     * Return the tree context object for check. This context is valid for
+     * the lifetime of a abstract syntax tree.
+     * @return the context object
+     */
+    public Map getTreeContext()
+    {
+        return mTreeContext;
+    }
+
+    /**
+     * Set the tree context for the check.
+     * @param aContext the context
+     */
+    public void setTreeContext(Map aContext)
+    {
+        mTreeContext = aContext;
+    }
+
+    /**
+     * Return the tree context object for check. This context is valid for
+     * the lifetime of a abstract syntax tree.
+     * @return the context object
+     */
+    public Map getTokenContext()
+    {
+        return mTokenContext;
+    }
+
+    /**
+     * Set the token context for the check.
+     * @param aContext the global context
+     */
+    public void setTokenContext(Map aContext)
+    {
+        mTokenContext = aContext;
+    }
+
+    /**
+     * Initialse the check. This is the time to verify that the check has
+     * everything required to perform it job.
+     */
+    public void init()
+    {
+
+    }
+
+    /**
+     * Destroy the check. It is being retired from service.
+     */
+    public void destroy()
+    {
+    }
+
+    /**
+     * Called before the starting to process a tree. Ideal place to initialise
+     * information that is to be collected whilst processing a tree.
+     */
+    public void beginTree()
+    {
+    }
+
+    /**
+     * Called after finished processing a tree. Ideal place to report on
+     * information collected whilst processing a tree.
+     */
+    public void finishTree()
+    {
+    }
+
+    /**
+     * Called to process a token.
+     * @param aAST the token to process
+     */
+    public void visitToken(DetailAST aAST)
+    {
+    }
+
+    /**
+     * Called after all the child nodes have been process.
+     * @param aAST the token leaving
+     */
+    public void leaveToken(DetailAST aAST)
+    {
+    }
+
+    /**
+     * Set the lines associated with the tree.
+     * @param aLines the file contents
+     */
+    public void setLines(String[] aLines)
+    {
+        getTreeContext().put(LINES_ATTRIBUTE, aLines);
+    }
+
+    /**
+     * Returns the lines associated with the tree.
+     * @return the file contents
+     */
+    public String[] getLines()
+    {
+        return (String[]) getTreeContext().get(LINES_ATTRIBUTE);
+    }
+
+    /** @see needs to be fixed */
+    public void log(int aLine, String aMessage)
+    {
+        final String fname = (String) getTreeContext().get(FILENAME_ATTRIBUTE);
+        System.out.println(fname + ":" + aLine + ": " + aMessage);
+    }
+}
---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/api/DetailAST.java
@@ -0,0 +1,112 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.api;
+
+import antlr.CommonAST;
+import antlr.Token;
+import antlr.collections.AST;
+
+/**
+ * An extension of the CommonAST that records the line and column number.
+ * The idea was taken from http://www.jguru.com/jguru/faq/view.jsp?EID=62654.
+ * @author <a href="mailto:oliver@puppycrawl.com">Oliver Burn</a>
+ * @version 1.0
+ */
+public class DetailAST
+    extends CommonAST
+{
+    /** constant to indicate if not calculated the child count */
+    private static final int NOT_INITIALIZED = Integer.MIN_VALUE;
+
+    /** the line number **/
+    private int mLineNo = 0;
+    /** the column number **/
+    private int mColumnNo = 0;
+
+    /** number of children */
+    private int mChildCount = NOT_INITIALIZED;
+    /** the parent token */
+    private DetailAST mParent;
+
+    /**
+     * Returns the number of child nodes one level below this node. That is is
+     * does not recurse down the tree.
+     * @return the number of child nodes
+     */
+    public int getChildCount()
+    {
+        // lazy init
+        if (mChildCount == NOT_INITIALIZED) {
+            mChildCount = 0;
+            AST child = getFirstChild();
+
+            while (child != null) {
+                mChildCount += 1;
+                child = child.getNextSibling();
+            }
+
+        }
+        return mChildCount;
+    }
+
+    /**
+     * Set the parent token.
+     * @param aParent the parent token
+     */
+    public void setParent(DetailAST aParent)
+    {
+        mParent = aParent;
+    }
+
+    /**
+     * Returns the parent token
+     * @return the parent token
+     */
+    public DetailAST getParent()
+    {
+        return mParent;
+    }
+
+    /** @see antlr.CommonAST **/
+    public void initialize(Token aTok)
+    {
+        super.initialize(aTok);
+        mLineNo = aTok.getLine();
+        mColumnNo = aTok.getColumn() - 1; // expect columns to start @ 0
+    }
+
+    /** @return the line number **/
+    public int getLineNo()
+    {
+        return mLineNo;
+    }
+
+    /** @return the column number **/
+    public int getColumnNo()
+    {
+        return mColumnNo;
+    }
+
+    /** @return a string representation of the object **/
+    public String toString()
+    {
+        return super.toString() + " {line = " + getLineNo() + ", col = "
+            + getColumnNo() + "}";
+    }
+}
