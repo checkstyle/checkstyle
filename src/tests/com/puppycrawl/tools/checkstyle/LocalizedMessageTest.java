1b7d45aed32280e6d30b14f733ad6fe700139d4c
Oliver Burn <oliver@puppycrawl.com>
2002-09-19 08:54:43 +0000

    Got the unit test to pass

--- src/tests/com/puppycrawl/tools/checkstyle/LocalizedMessageTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/LocalizedMessageTest.java
@@ -4,9 +4,12 @@ import junit.framework.TestCase;
 
 import java.util.Locale;
 
+import com.puppycrawl.tools.checkstyle.api.LocalizedMessage;
+
 public class LocalizedMessageTest
     extends TestCase
 {
+    private final String BUNDLE = "com.puppycrawl.tools.checkstyle.messages";
     public LocalizedMessageTest(String name)
     {
         super(name);
@@ -20,15 +23,15 @@ public class LocalizedMessageTest
     public void testMisc()
     {
         LocalizedMessage lm =
-            new LocalizedMessage(0, "DefaultLogger.auditFinished", null);
+            new LocalizedMessage(0, BUNDLE, "DefaultLogger.auditFinished", null);
         assertNotNull(lm);
         assertEquals("Audit done.", lm.getMessage());
-        lm = new LocalizedMessage(0, "DefaultLogger.addException",
+        lm = new LocalizedMessage(0, BUNDLE, "DefaultLogger.addException",
                                   new String[]{"Claira"});
         assertNotNull(lm);
         assertEquals("Error auditing Claira", lm.getMessage());
 
-        lm = new LocalizedMessage(0, "ws.notFollowed", new String[] {"{"});
+        lm = new LocalizedMessage(0, BUNDLE, "ws.notFollowed", new String[] {"{"});
         assertNotNull(lm);
         assertEquals("'{' is not followed by whitespace.", lm.getMessage());
     }
