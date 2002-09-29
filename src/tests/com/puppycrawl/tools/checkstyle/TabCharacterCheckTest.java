acfb247e208ccc855d799bf0f2f6d9520b05fb50
lkuehne <devnull@localhost>
2002-09-29 13:46:35 +0000

    added test for tab character check

---
+++ src/tests/com/puppycrawl/tools/checkstyle/TabCharacterCheckTest.java
@@ -0,0 +1,31 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.TabCharacterCheck;
+
+public class TabCharacterCheckTest
+        extends BaseCheckTestCase
+{
+    public TabCharacterCheckTest(String aName)
+    {
+        super(aName);
+    }
+
+    public void testIt() throws Exception
+    {
+        final CheckConfiguration checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(TabCharacterCheck.class.getName());
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputSimple.java");
+        final String[] expected = {
+            "19:25: Line contains a tab character.",
+            "145:35: Line contains a tab character.",
+            "146:64: Line contains a tab character.",
+            "154:9: Line contains a tab character.",
+            "155:10: Line contains a tab character.",
+            "156:1: Line contains a tab character.",
+            "157:3: Line contains a tab character.",
+            "158:3: Line contains a tab character."
+        };
+        verify(c, fname, expected);
+    }
+}
