1805077ebbb90962671fb66c92782cb0bf2bbb4f
lkuehne <devnull@localhost>
2002-10-06 13:14:16 +0000

    finally managed to tell beanutils what I want, checks now have their 'natural' api

--- src/checkstyle/com/puppycrawl/tools/checkstyle/ConfigurationLoader.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/ConfigurationLoader.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
