d3433c91e0a791b0d1f44bf1975e930e15ed74b8
Lars Kühne <lakuehne@gmail.com>
2002-10-17 00:13:04 +0000

    Added initial GUI.
    Oliver, please check that the file headers do not
    lead to license problems with Sun - maybe we need to remove
    our own header and exclude the gui package from the run.checkstyle
    target?

---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/gui/AbstractCellEditor.java
@@ -0,0 +1,113 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.gui;
+
+import java.util.EventObject;
+import javax.swing.CellEditor;
+import javax.swing.event.CellEditorListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.EventListenerList;
+
+/**
+ * Abstract implementation of a CellEditor.
+ *
+ * @author http://java.sun.com/products/jfc/tsc/articles/treetable2
+ */
+public class AbstractCellEditor implements CellEditor
+{
+    private EventListenerList mListenerList = new EventListenerList();
+
+    /** @see CellEditor */
+    public Object getCellEditorValue()
+    {
+        return null;
+    }
+
+    /** @see CellEditor */
+    public boolean isCellEditable(EventObject e)
+    {
+        return true;
+    }
+
+    /** @see CellEditor */
+    public boolean shouldSelectCell(EventObject anEvent)
+    {
+        return false;
+    }
+
+    /** @see CellEditor */
+    public boolean stopCellEditing()
+    {
+        return true;
+    }
+
+    /** @see CellEditor */
+    public void cancelCellEditing()
+    {
+    }
+
+    /** @see CellEditor */
+    public void addCellEditorListener(CellEditorListener l)
+    {
+        mListenerList.add(CellEditorListener.class, l);
+    }
+
+    /** @see CellEditor */
+    public void removeCellEditorListener(CellEditorListener l)
+    {
+        mListenerList.remove(CellEditorListener.class, l);
+    }
+
+    /*
+     * Notify all listeners that have registered interest for
+     * notification on this event type.
+     * @see EventListenerList
+     */
+    protected void fireEditingStopped()
+    {
+        // Guaranteed to return a non-null array
+        Object[] listeners = mListenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == CellEditorListener.class) {
+                ((CellEditorListener) listeners[i + 1]).editingStopped(new ChangeEvent(this));
+            }
+        }
+    }
+
+    /*
+     * Notify all listeners that have registered interest for
+     * notification on this event type.
+     * @see EventListenerList
+     */
+    protected void fireEditingCanceled()
+    {
+        // Guaranteed to return a non-null array
+        Object[] listeners = mListenerList.getListenerList();
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == CellEditorListener.class) {
+                ((CellEditorListener) listeners[i + 1]).editingCanceled(new ChangeEvent(this));
+            }
+        }
+    }
+}
---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/gui/TreeTableModelAdapter.java
@@ -0,0 +1,168 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+ * @(#)TreeTableModelAdapter.java	1.2 98/10/27
+ *
+ * Copyright 1997, 1998 by Sun Microsystems, Inc.,
+ * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Sun Microsystems, Inc. ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Sun.
+ */
+
+package com.puppycrawl.tools.checkstyle.gui;
+
+import javax.swing.JTree;
+import javax.swing.SwingUtilities;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.tree.TreePath;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeExpansionListener;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
+
+/**
+ * This is a wrapper class takes a TreeTableModel and implements
+ * the table model interface. The implementation is trivial, with
+ * all of the event dispatching support provided by the superclass:
+ * the AbstractTableModel.
+ *
+ * @version 1.2 10/27/98
+ *
+ * @author Philip Milne
+ * @author Scott Violet
+ */
+public class TreeTableModelAdapter extends AbstractTableModel
+{
+    private JTree mTree;
+    private TreeTableModel mTreeTableModel;
+
+    public TreeTableModelAdapter(TreeTableModel aTreeTableModel, JTree aTree)
+    {
+        this.mTree = aTree;
+        this.mTreeTableModel = aTreeTableModel;
+
+        aTree.addTreeExpansionListener(new TreeExpansionListener()
+        {
+            // Don't use fireTableRowsInserted() here; the selection model
+            // would get updated twice.
+            public void treeExpanded(TreeExpansionEvent event)
+            {
+                fireTableDataChanged();
+            }
+
+            public void treeCollapsed(TreeExpansionEvent event)
+            {
+                fireTableDataChanged();
+            }
+        });
+
+        // Install a TreeModelListener that can update the table when
+        // mTree changes. We use delayedFireTableDataChanged as we can
+        // not be guaranteed the mTree will have finished processing
+        // the event before us.
+        aTreeTableModel.addTreeModelListener(new TreeModelListener()
+        {
+            public void treeNodesChanged(TreeModelEvent e)
+            {
+                delayedFireTableDataChanged();
+            }
+
+            public void treeNodesInserted(TreeModelEvent e)
+            {
+                delayedFireTableDataChanged();
+            }
+
+            public void treeNodesRemoved(TreeModelEvent e)
+            {
+                delayedFireTableDataChanged();
+            }
+
+            public void treeStructureChanged(TreeModelEvent e)
+            {
+                delayedFireTableDataChanged();
+            }
+        });
+    }
+
+    // Wrappers, implementing TableModel interface.
+
+    public int getColumnCount()
+    {
+        return mTreeTableModel.getColumnCount();
+    }
+
+    public String getColumnName(int column)
+    {
+        return mTreeTableModel.getColumnName(column);
+    }
+
+    public Class getColumnClass(int column)
+    {
+        return mTreeTableModel.getColumnClass(column);
+    }
+
+    public int getRowCount()
+    {
+        final int rowCount = mTree.getRowCount();
+        return rowCount;
+    }
+
+    protected Object nodeForRow(int row)
+    {
+        TreePath treePath = mTree.getPathForRow(row);
+        return treePath.getLastPathComponent();
+    }
+
+    public Object getValueAt(int row, int column)
+    {
+        return mTreeTableModel.getValueAt(nodeForRow(row), column);
+    }
+
+    public boolean isCellEditable(int row, int column)
+    {
+        return mTreeTableModel.isCellEditable(nodeForRow(row), column);
+    }
+
+    public void setValueAt(Object value, int row, int column)
+    {
+        mTreeTableModel.setValueAt(value, nodeForRow(row), column);
+    }
+
+    /**
+     * Invokes fireTableDataChanged after all the pending events have been
+     * processed. SwingUtilities.invokeLater is used to handle this.
+     */
+    protected void delayedFireTableDataChanged()
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                fireTableDataChanged();
+            }
+        });
+    }
+}
+
