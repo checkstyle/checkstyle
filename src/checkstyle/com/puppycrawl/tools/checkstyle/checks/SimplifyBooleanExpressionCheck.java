f1ef403f5e8fab7f9c68100f5089bb24ecf46bb9
Lars Kühne <lakuehne@gmail.com>
2002-10-21 07:20:19 +0000

    Added SimplifyBooleanExpressionCheck to find code like "if (b == true)". Also fixed SimplyfyBooleanReturnCheck and introduced tests.

---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/SimplifyBooleanExpressionCheck.java
@@ -0,0 +1,54 @@
+package com.puppycrawl.tools.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+
+public class SimplifyBooleanExpressionCheck
+        extends Check
+{
+    /** @see Check */
+    public int[] getDefaultTokens()
+    {
+        return new int[] {TokenTypes.LITERAL_TRUE, TokenTypes.LITERAL_FALSE};
+    }
+
+    /** @see Check */
+    public void visitToken(DetailAST aAST)
+    {
+        final DetailAST parent = aAST.getParent();
+        switch (parent.getType()) {
+            case TokenTypes.NOT_EQUAL:
+            case TokenTypes.EQUAL:
+            case TokenTypes.LNOT:
+            case TokenTypes.LOR:
+            case TokenTypes.LAND:
+                log(parent.getLineNo(), parent.getColumnNo(),
+                        // TODO: i18n
+                        "Expression can be simplified.");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ src/tests/com/puppycrawl/tools/checkstyle/SimplifyBooleanExpressionCheckTest.java
@@ -0,0 +1,31 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.SimplifyBooleanExpressionCheck;
+
+public class SimplifyBooleanExpressionCheckTest
+extends BaseCheckTestCase
+{
+    public SimplifyBooleanExpressionCheckTest(String aName)
+    {
+        super(aName);
+    }
+
+    public void testIt()
+            throws Exception
+    {
+        final CheckConfiguration checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(SimplifyBooleanExpressionCheck.class.getName());
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputSimplifyBoolean.java");
+        final String[] expected = {
+            "20:18: Expression can be simplified.",
+            "41:36: Expression can be simplified.",
+            "42:36: Expression can be simplified.",
+            "43:16: Expression can be simplified.",
+            "43:32: Expression can be simplified.",
+            // TODO: Change Check.log to avoid duplicate messages
+            "43:32: Expression can be simplified.",
+        };
+        verify(c, fname, expected);
+    }
+}
