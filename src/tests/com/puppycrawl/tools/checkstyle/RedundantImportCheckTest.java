57c7ef0c7c100596298b571ba03335416a43b9f9
lkuehne <devnull@localhost>
2002-10-21 05:19:15 +0000

    incorporated patch 626058 from Ville Skytte, Output duplicate/redundant import names

--- src/tests/com/puppycrawl/tools/checkstyle/RedundantImportCheckTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/RedundantImportCheckTest.java
@@ -18,11 +18,11 @@ public class RedundantImportCheckTest
         final Checker c = createChecker(checkConfig);
         final String fname = getPath("InputImport.java");
         final String[] expected = {
-            "7:1: Redundant import from the same package.",
-            "8:38: Redundant import from the same package.",
-            "10:1: Redundant import from the java.lang package.",
-            "11:1: Redundant import from the java.lang package.",
-            "14:1: Duplicate import to line 13.",
+            "7:1: Redundant import from the same package - com.puppycrawl.tools.checkstyle.*.",
+            "8:38: Redundant import from the same package - com.puppycrawl.tools.checkstyle.Configuration.",
+            "10:1: Redundant import from the java.lang package - java.lang.*.",
+            "11:1: Redundant import from the java.lang package - java.lang.String.",
+            "14:1: Duplicate import to line 13 - java.util.List.",
         };
         verify(c, fname, expected);
     }
