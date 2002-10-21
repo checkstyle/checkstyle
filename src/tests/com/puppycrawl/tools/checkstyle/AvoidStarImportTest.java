3fac0af6fc6fcf3bf7f9db913bfb7760ef903800
lkuehne <devnull@localhost>
2002-10-21 05:54:38 +0000

    also added offending import in avoidStar message

--- src/tests/com/puppycrawl/tools/checkstyle/AvoidStarImportTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/AvoidStarImportTest.java
@@ -18,9 +18,9 @@ extends BaseCheckTestCase
         final Checker c = createChecker(checkConfig);
         final String fname = getPath("InputImport.java");
         final String[] expected = {
-            "7: Avoid using the '.*' form of import.",
-            "9: Avoid using the '.*' form of import.",
-            "10: Avoid using the '.*' form of import.",
+            "7: Avoid using the '.*' form of import - com.puppycrawl.tools.checkstyle.*.",
+            "9: Avoid using the '.*' form of import - java.io.*.",
+            "10: Avoid using the '.*' form of import - java.lang.*.",
         };
         verify(c, fname, expected);
     }
