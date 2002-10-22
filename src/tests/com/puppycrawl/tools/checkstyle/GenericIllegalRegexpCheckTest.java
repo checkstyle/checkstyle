7735cda4c0ce3fa34a0035ef20e11acb6a1a7fa0
lkuehne <devnull@localhost>
2002-10-22 08:02:57 +0000

    added generic check for illegal regexps, RFEs #595254 and #621247

---
+++ src/tests/com/puppycrawl/tools/checkstyle/GenericIllegalRegexpCheckTest.java
@@ -0,0 +1,27 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.GenericIllegalRegexpCheck;
+
+public class GenericIllegalRegexpCheckTest
+        extends BaseCheckTestCase
+{
+    public GenericIllegalRegexpCheckTest(String aName)
+    {
+        super(aName);
+    }
+
+    public void testIt()
+            throws Exception
+    {
+        String illegal = "System\\.(out)|(err)\\.print(ln)?\\(";
+        final CheckConfiguration checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(GenericIllegalRegexpCheck.class.getName());
+        checkConfig.addProperty("format", illegal);
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputSemantic.java");
+        final String[] expected = {
+            "68: Line matches the illegal pattern '" + illegal + "'."
+        };
+        verify(c, fname, expected);
+    }
+}
