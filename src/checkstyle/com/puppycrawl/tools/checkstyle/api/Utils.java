40496d0f8c01bf1f9718e6925b76f13db5b2ae3d
Lars Kühne <lakuehne@gmail.com>
2002-10-28 14:47:57 +0000

    import cleanup

--- src/checkstyle/com/puppycrawl/tools/checkstyle/api/Utils.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/api/Utils.java
@@ -16,6 +16,7 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
+
 package com.puppycrawl.tools.checkstyle.api;
 
 import java.util.HashMap;
@@ -31,7 +32,6 @@ import java.io.FileReader;
 import org.apache.regexp.RE;
 import org.apache.regexp.RESyntaxException;
 import org.apache.commons.beanutils.ConversionException;
-import antlr.collections.AST;
 
 /**
  * Contains utility methods.
