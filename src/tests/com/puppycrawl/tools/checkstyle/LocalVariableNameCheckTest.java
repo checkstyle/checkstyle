31de023be8a2085ab9fc470fae030735c94ce031
oburn <devnull@localhost>
2002-10-29 13:17:16 +0000

    Patch from Rick Giles

---
+++ src/tests/com/puppycrawl/tools/checkstyle/LocalVariableNameCheckTest.java
@@ -0,0 +1,28 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.LocalVariableNameCheck;
+
+public class LocalVariableNameCheckTest
+    extends BaseCheckTestCase
+{
+    public LocalVariableNameCheckTest(String aName)
+    {
+        super(aName);
+    }
+
+    public void testDefault()
+        throws Exception
+    {
+        final CheckConfiguration checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(LocalVariableNameCheck.class.getName());;
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputSimple.java");
+        final String[] expected = {
+            "119:13: Name 'ABC' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "130:18: Name 'I' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "132:20: Name 'InnerBlockVariable' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+        };
+        verify(c, fname, expected);
+    }
+}
+
