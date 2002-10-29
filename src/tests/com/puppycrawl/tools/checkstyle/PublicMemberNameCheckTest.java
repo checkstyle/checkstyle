5625c3bfb28c73926e7dfce64b25fc608bebc5c0
oburn <devnull@localhost>
2002-10-29 13:37:20 +0000

    Patch for Rick Giles

---
+++ src/tests/com/puppycrawl/tools/checkstyle/PublicMemberNameCheckTest.java
@@ -0,0 +1,26 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.PublicMemberNameCheck;
+
+public class PublicMemberNameCheckTest
+    extends BaseCheckTestCase
+{
+    public PublicMemberNameCheckTest(String aName)
+    {
+        super(aName);
+    }
+
+    public void testDefault()
+        throws Exception
+    {
+        final CheckConfiguration checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(PublicMemberNameCheck.class.getName());
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputSimple.java");
+        final String[] expected = {
+            "58:16: Name 'mTest2' must match pattern '^f[A-Z][a-zA-Z0-9]*$'.",
+        };
+        verify(c, fname, expected);
+    }
+}
+
