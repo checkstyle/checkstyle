982b3963ca007c6101f69cf50c010f07292dab0e
Rick Giles <rickgiles>
2002-11-03 17:46:03 +0000

    empty block check with options

---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/AbstractOptionCheck.java
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks;
+
+import org.apache.commons.beanutils.ConversionException;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * Abstract class for checks with options.
+ * @author Rick Giles
+ */
+public abstract class AbstractOptionCheck
+    extends Check
+{
+    /** the policy to enforce */
+    protected AbstractOption mOption = null;
+
+    /**
+     * Set the option to enforce.
+     * @param aOption string to decode option from
+     * @throws ConversionException if unable to decode
+     */
+    public void setOption(String aOption)
+    {
+        mOption = AbstractOption.decode(aOption);
+        if (mOption == null) {
+            throw new ConversionException("unable to parse " + aOption);
+        }
+    }
+}
---
+++ src/tests/com/puppycrawl/tools/checkstyle/EmptyBlockCheckTest.java
@@ -0,0 +1,65 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.EmptyBlockCheck;
+import com.puppycrawl.tools.checkstyle.checks.BlockOption;
+
+public class EmptyBlockCheckTest
+    extends BaseCheckTestCase
+{
+    public EmptyBlockCheckTest(String aName)
+    {
+        super(aName);
+    }
+
+    public void testDefault()
+        throws Exception
+    {
+        final CheckConfiguration checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(EmptyBlockCheck.class.getName());
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputSemantic.java");
+        final String[] expected = {
+         };
+        verify(c, fname, expected);
+    }
+
+    public void testText()
+        throws Exception
+    {
+        final CheckConfiguration checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(EmptyBlockCheck.class.getName());
+        checkConfig.addProperty("option", BlockOption.TEXT.toString());
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputSemantic.java");
+        final String[] expected = {
+            "52:65: Empty catch block.",
+            "72:52: Empty catch block.",
+            "73:45: Empty catch block.",
+            "75:13: Empty try block.",
+            "77:17: Empty finally block.",
+        };
+        verify(c, fname, expected);
+    }
+    
+    public void testStatement()
+        throws Exception
+    {
+        final CheckConfiguration checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(EmptyBlockCheck.class.getName());
+        checkConfig.addProperty("option", BlockOption.STMT.toString());
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputSemantic.java");
+        final String[] expected = {
+            "52:65: Must have at least one statement.",
+            "54:41: Must have at least one statement.",
+            "71:38: Must have at least one statement.",
+            "72:52: Must have at least one statement.",
+            "73:45: Must have at least one statement.",
+            "75:13: Must have at least one statement.",
+            "77:17: Must have at least one statement.",
+            "79:13: Must have at least one statement.",
+            "82:17: Must have at least one statement.",
+        };
+        verify(c, fname, expected);
+    }
+}
