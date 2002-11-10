b041e53ae8249dd1d77338d80fa429e2eb427b92
Lars Kühne <lakuehne@gmail.com>
2002-11-10 12:00:31 +0000

    improve test coverage of StringArrayReader

--- src/tests/com/puppycrawl/tools/checkstyle/StringArrayReaderTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/StringArrayReaderTest.java
@@ -27,10 +27,35 @@ public class StringArrayReaderTest
 
     public void testLineBreakSingleChar()
     {
-        final StringArrayReader o = new StringArrayReader(new String[] {"a", "bc"});
+        final StringArrayReader o =
+            new StringArrayReader(new String[] {"a", "bc"});
         try {
             int a = o.read();
             assertEquals('a', a);
+            int nl1 = o.read();
+            assertEquals('\n', nl1);
+            int b = o.read();
+            assertEquals('b', b);
+            int c = o.read();
+            assertEquals('c', c);
+            int nl2 = o.read();
+            assertEquals('\n', nl2);
+            int eof = o.read();
+            assertEquals(-1, eof);
+        }
+        catch (IOException ex) {
+        }
+    }
+
+    public void testLineBreakCharArray()
+    {
+        final StringArrayReader o =
+            new StringArrayReader(new String[] {"a", "bc"});
+        try {
+            char[] a = new char[1];
+            int count = o.read(a, 0, 1);
+            assertEquals(1, count);
+            assertEquals('a', a[0]);
             int nl1 = o.read();
             assertEquals('\n', nl1);
             int b = o.read();
