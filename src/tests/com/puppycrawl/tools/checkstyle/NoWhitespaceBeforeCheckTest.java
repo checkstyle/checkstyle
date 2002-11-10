8afddb6af3aff5ed7e17f35bebdd0718ee50d86b
Rick Giles <rickgiles>
2002-11-10 19:54:31 +0000

    no whitespace checks for unary operators

---
+++ src/tests/com/puppycrawl/tools/checkstyle/NoWhitespaceBeforeCheckTest.java
@@ -0,0 +1,61 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.NoWhitespaceBeforeCheck;
+
+public class NoWhitespaceBeforeCheckTest
+    extends BaseCheckTestCase
+{
+    private CheckConfiguration checkConfig;
+    
+    public NoWhitespaceBeforeCheckTest(String aName)
+    {
+        super(aName);
+    }
+    
+    public void setUp() {
+        checkConfig = new CheckConfiguration();
+        checkConfig.setClassname(NoWhitespaceBeforeCheck.class.getName());
+    }
+        
+    public void testDefault() throws Exception
+    {
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputWhitespace.java");
+        final String[] expected = {
+            "30:14: '++' is preceeded with whitespace.",
+            "30:21: '--' is preceeded with whitespace.",
+        };
+        verify(c, fname, expected);
+    }
+    
+    public void testDot() throws Exception
+    {
+        checkConfig.addTokens("DOT");
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputWhitespace.java");
+        final String[] expected = {
+            "5:12: '.' is preceeded with whitespace.",
+            "6:4: '.' is preceeded with whitespace.",
+            "129:17: '.' is preceeded with whitespace.",
+            "135:12: '.' is preceeded with whitespace.",
+            "136:10: '.' is preceeded with whitespace.",
+        };
+        verify(c, fname, expected);
+    }
+
+    
+    public void testDotAllowLineBreaks() throws Exception
+    {
+        checkConfig.addTokens("DOT");
+        checkConfig.addProperty("allowLineBreaks", "yes");
+        final Checker c = createChecker(checkConfig);
+        final String fname = getPath("InputWhitespace.java");
+        final String[] expected = {
+            "5:12: '.' is preceeded with whitespace.",
+            "129:17: '.' is preceeded with whitespace.",
+            "136:10: '.' is preceeded with whitespace.",
+        };
+        verify(c, fname, expected);
+    }
+    
+}
