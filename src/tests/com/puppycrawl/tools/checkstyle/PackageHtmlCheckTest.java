fd17c6a1c00fed438a7079d47d3fc4a92a59afe4
lkuehne <devnull@localhost>
2002-11-14 18:01:22 +0000

    added scetch of PackageHtmlCheck as the first FileSetCheck

---
+++ src/tests/com/puppycrawl/tools/checkstyle/PackageHtmlCheckTest.java
@@ -0,0 +1,26 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.api.FileSetCheck;
+import com.puppycrawl.tools.checkstyle.checks.PackageHtmlCheck;
+
+public class PackageHtmlCheckTest
+        extends BaseCheckTestCase
+{
+    public PackageHtmlCheckTest(String s) {
+        super(s);
+    }
+
+    public void testPackageHtml()
+         throws Exception
+    {
+        final FileSetCheck fsc = new PackageHtmlCheck();
+        final String packageHtmlPath = getPath("package.html");
+        final String filepath = getPath("InputScopeAnonInner.java");
+        final String[] expected = {
+            packageHtmlPath + ":0: Missing package documentation file.",
+        };
+        // TODO: verify(fsc, filepath, expected);
+    }
+
+
+}
