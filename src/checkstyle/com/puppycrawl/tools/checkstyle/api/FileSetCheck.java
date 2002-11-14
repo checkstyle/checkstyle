efea3a3e56ec1c30897c279712e3c76801a6f536
Lars Kühne <lakuehne@gmail.com>
2002-11-14 17:43:01 +0000

    added FileSetCheck interface

---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/api/FileSetCheck.java
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.api;
+
+import java.io.File;
+
+/**
+ * Interface for Checking a set of files for some criteria.
+ *
+ * @author lkuehne
+ */
+public interface FileSetCheck
+{
+
+    /**
+     * Processes a set of files.
+     * Once this is done, it is highly recommended to call for
+     * the destroy method to close and remove the listeners.
+     * @param aFiles the files to be audited.
+     * @return the total number of errors found
+     * @see #destroy()
+     */
+    int process(File[] aFiles);
+
+    /** Cleans up the object **/
+    void destroy();
+}
