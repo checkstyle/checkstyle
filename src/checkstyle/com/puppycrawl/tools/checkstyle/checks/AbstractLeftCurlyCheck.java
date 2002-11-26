5851470c4d888a4242e5bac829a010107084bcc5
Oliver Burn <oliver@puppycrawl.com>
2002-11-26 06:01:52 +0000

    Added the missing header

--- src/checkstyle/com/puppycrawl/tools/checkstyle/checks/AbstractLeftCurlyCheck.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/AbstractLeftCurlyCheck.java
@@ -1,11 +1,21 @@
-/*
- * Created by IntelliJ IDEA.
- * User: oliver.burn
- * Date: 13/10/2002
- * Time: 20:45:56
- * To change template for new class use
- * Code Style | Class Templates options (Tools | IDE Options).
- */
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
 package com.puppycrawl.tools.checkstyle.checks;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
