a955b402b7db322df8f073a0e8f7a3abd8449f17
Rick Giles <rickgiles>
2002-11-30 17:07:24 +0000

    code for properties expansion copied from ant

--- src/checkstyle/com/puppycrawl/tools/checkstyle/api/Utils.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/api/Utils.java
@@ -23,9 +23,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Vector;
 
 import org.apache.commons.beanutils.ConversionException;
 import org.apache.regexp.RE;
@@ -186,5 +188,135 @@ public final class Utils
                 "Failed to initialise regexp expression " + aPattern, e);
         }
         return retVal;
+    }
+        /**
+     * Replaces <code>${xxx}</code> style constructions in the given value 
+     * with the string value of the corresponding data types.
+     *
+     * @param aValue The string to be scanned for property references.
+     *              May be <code>null</code>, in which case this
+     *              method returns immediately with no effect.
+     * @param aProps  Mapping (String to String) of property names to their 
+     *              values. Must not be <code>null</code>.
+     * 
+     * @throws CheckstyleException if the string contains an opening 
+     *                           <code>${</code> without a closing 
+     *                           <code>}</code>
+     * @return the original string with the properties replaced, or
+     *         <code>null</code> if the original string is <code>null</code>.
+     * 
+     * Code copied from ant - 
+     * http://cvs.apache.org/viewcvs/jakarta-ant/src/main/org/apache/tools/ant/ProjectHelper.java
+     */
+     public static String replaceProperties(String aValue,  Properties aProps)
+     throws CheckstyleException
+     {
+        if (aValue == null) {
+            return null;
+        }
+
+        final Vector fragments = new Vector();
+        final Vector propertyRefs = new Vector();
+        parsePropertyString(aValue, fragments, propertyRefs);
+
+        final StringBuffer sb = new StringBuffer();
+        final Enumeration i = fragments.elements();
+        final Enumeration j = propertyRefs.elements();
+        while (i.hasMoreElements()) {
+            String fragment = (String) i.nextElement();
+            if (fragment == null) {
+                final String propertyName = (String) j.nextElement();
+                if (!aProps.containsKey(propertyName)) {
+                    throw new CheckstyleException(
+                        "Property ${" + propertyName + "} has not been set");
+                }
+                fragment = (aProps.containsKey(propertyName)) 
+                    ? (String) aProps.get(propertyName) 
+                    : "${" + propertyName + "}"; 
+            }
+            sb.append(fragment);
+        }                        
+        
+        return sb.toString();
+    }
+
+    /**
+     * Parses a string containing <code>${xxx}</code> style property
+     * references into two lists. The first list is a collection
+     * of text fragments, while the other is a set of string property names.
+     * <code>null</code> entries in the first list indicate a property 
+     * reference from the second list.
+     * 
+     * @param aValue     Text to parse. Must not be <code>null</code>.
+     * @param aFragments List to add text fragments to. 
+     *                  Must not be <code>null</code>.
+     * @param aPropertyRefs List to add property names to.
+     *                     Must not be <code>null</code>.
+     * 
+     * @throws CheckstyleException if the string contains an opening 
+     *                           <code>${</code> without a closing 
+     *                           <code>}</code>
+     * Code copied from ant - 
+     * http://cvs.apache.org/viewcvs/jakarta-ant/src/main/org/apache/tools/ant/ProjectHelper.java
+     */
+    public static void parsePropertyString(String aValue, Vector aFragments, 
+                                           Vector aPropertyRefs)
+        throws CheckstyleException
+    {
+        int prev = 0;
+        int pos;
+        //search for the next instance of $ from the 'prev' position
+        while ((pos = aValue.indexOf("$", prev)) >= 0) {
+
+            //if there was any text before this, add it as a fragment
+            //TODO, this check could be modified to go if pos>prev;
+            //seems like this current version could stick empty strings
+            //into the list
+            if (pos > 0) {
+                aFragments.addElement(aValue.substring(prev, pos));
+            }
+            //if we are at the end of the string, we tack on a $
+            //then move past it
+            if (pos == (aValue.length() - 1)) {
+                aFragments.addElement("$");
+                prev = pos + 1;
+            }
+            else if (aValue.charAt(pos + 1) != '{') {
+                //peek ahead to see if the next char is a property or not
+                //not a property: insert the char as a literal
+                /*
+                fragments.addElement(value.substring(pos + 1, pos + 2));
+                prev = pos + 2;
+                */
+                if (aValue.charAt(pos + 1) == '$') {
+                    //backwards compatibility two $ map to one mode
+                    aFragments.addElement("$");
+                    prev = pos + 2;
+                }
+                else {
+                    //new behaviour: $X maps to $X for all values of X!='$'
+                    aFragments.addElement(aValue.substring(pos, pos + 2));
+                    prev = pos + 2;
+                }
+                
+            }
+            else {
+                //property found, extract its name or bail on a typo
+                final int endName = aValue.indexOf('}', pos);
+                if (endName < 0) {
+                    throw new CheckstyleException("Syntax error in property: "
+                                                    + aValue);
+                }
+                final String propertyName = aValue.substring(pos + 2, endName);
+                aFragments.addElement(null);
+                aPropertyRefs.addElement(propertyName);
+                prev = endName + 1;
+            }
+        }
+        //no more $ signs found
+        //if there is any tail to the file, append it
+        if (prev < aValue.length()) {
+            aFragments.addElement(aValue.substring(prev));
+        }
     }
 }
--- src/tests/com/puppycrawl/tools/checkstyle/UtilsTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/UtilsTest.java
@@ -1,7 +1,11 @@
 package com.puppycrawl.tools.checkstyle;
 
+import java.util.Properties;
+
 import junit.framework.TestCase;
 import org.apache.regexp.RE;
+
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import com.puppycrawl.tools.checkstyle.api.Utils;
 
 public class UtilsTest
@@ -34,5 +38,75 @@ public class UtilsTest
         final RE r2 = Utils.getRE("a");
         assertEquals(r1, r2);
     }
+    public void testReplacePropertiesNoReplace()
+        throws CheckstyleException
+    {
+        final String[] testValues = {null, "", "a", "$a", "{a",
+                                       "{a}", "a}", "$a}", "$", "a$b"};
+        final Properties props = initProperties();
+        for (int i = 0; i < testValues.length; i++) {
+            final String value = Utils.replaceProperties(testValues[i], props);
+            assertEquals("\"" + testValues[i] + "\"", value, testValues[i]);
+        }
+    }
+    
+    public void testReplacePropertiesSyntaxError()
+    {
+        final Properties props = initProperties();
+        try {
+            final String value = Utils.replaceProperties("${a", props);
+        }
+        catch (CheckstyleException ex) {
+            assertEquals("Syntax error in property: ${a", ex.getMessage());
+            return;
+        }
+        assertTrue(false);
+    }
+    
+    public void testReplacePropertiesMissingProperty()
+    {
+        final Properties props = initProperties();
+        try {
+            final String value = Utils.replaceProperties("${c}", props);
+        }
+        catch (CheckstyleException ex) {
+            assertEquals("Property ${c} has not been set", ex.getMessage());
+            return;
+        }
+        assertTrue(false);
+    }
 
+    public void testReplacePropertiesReplace()
+        throws CheckstyleException
+    {
+        final String[][] testValues = {
+            {"${a}", "A"},
+            {"x${a}", "xA"},
+            {"${a}x", "Ax"},
+            {"${a}${b}", "AB"},
+            {"x${a}${b}", "xAB"},
+            {"${a}x${b}", "AxB"},
+            {"${a}${b}x", "ABx"},
+            {"x${a}y${b}", "xAyB"},
+            {"${a}x${b}y", "AxBy"},
+            {"x${a}${b}y", "xABy"},
+            {"x${a}y${b}z", "xAyBz"},
+            {"$$", "$"},
+            };
+        final Properties props = initProperties();
+        for (int i = 0; i < testValues.length; i++) {
+            final String value =
+                Utils.replaceProperties(testValues[i][0], props);
+            assertEquals("\"" + testValues[i][0] + "\"",
+                testValues[i][1], value);
+        }
+    }
+
+    private Properties initProperties()
+    {
+        final Properties props = new Properties();
+        props.put("a", "A");
+        props.put("b", "B");
+        return props;
+    }
 }
