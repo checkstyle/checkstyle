d15a35c635f938630c0fd50e9f1a9673d37583a7
lkuehne <devnull@localhost>
2002-12-02 06:05:30 +0000

    fixed bundle of ws.notFollowed

--- src/tests/com/puppycrawl/tools/checkstyle/LocalizedMessageTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/LocalizedMessageTest.java
@@ -9,6 +9,9 @@ import com.puppycrawl.tools.checkstyle.api.LocalizedMessage;
 public class LocalizedMessageTest
     extends TestCase
 {
+    private final String CHECKSTYLE_CHECKS_BUNDLE =
+            "com.puppycrawl.tools.checkstyle.checks.messages";
+
     protected void setUp() throws Exception
     {
         LocalizedMessage.setLocale(Locale.ENGLISH);
@@ -27,7 +30,7 @@ public class LocalizedMessageTest
         assertNotNull(lm);
         assertEquals("Error auditing Claira", lm.getMessage());
 
-        lm = new LocalizedMessage(0, Defn.CHECKSTYLE_BUNDLE,
+        lm = new LocalizedMessage(0, CHECKSTYLE_CHECKS_BUNDLE,
                                   "ws.notFollowed", new String[] {"{"});
         assertNotNull(lm);
         assertEquals("'{' is not followed by whitespace.", lm.getMessage());
