f50c47545a5bcdaf1facb2aaf88adeda15594007
Oliver Burn <oliver@puppycrawl.com>
2002-12-19 06:50:19 +0000

    Refactored all the LeftCurly checks into one

--- src/checkstyle/com/puppycrawl/tools/checkstyle/checks/LeftCurlyCheck.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/LeftCurlyCheck.java
@@ -16,95 +16,97 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
-
 package com.puppycrawl.tools.checkstyle.checks;
 
-import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 /**
  * <p>
- * Checks the placement of left curly braces on other blocks apart from
- * methods and types.
+ * Checks the placement of left curly braces on types, methods and other the
+ * other blocks:
+ *  {@link  TokenTypes#LITERAL_CATCH LITERAL_CATCH},  {@link
+ * TokenTypes#LITERAL_DO LITERAL_DO},  {@link TokenTypes#LITERAL_ELSE
+ * LITERAL_ELSE},  {@link TokenTypes#LITERAL_FINALLY LITERAL_FINALLY},  {@link
+ * TokenTypes#LITERAL_FOR LITERAL_FOR},  {@link TokenTypes#LITERAL_IF
+ * LITERAL_IF},  {@link TokenTypes#LITERAL_SWITCH LITERAL_SWITCH},  {@link
+ * TokenTypes#LITERAL_SYNCHRONIZED LITERAL_SYNCHRONIZED},  {@link
+ * TokenTypes#LITERAL_TRY LITERAL_TRY},  {@link TokenTypes#LITERAL_WHILE
+ * LITERAL_WHILE}.
  * </p>
- * <p> By default the check will check the following blocks:
- *  {@link TokenTypes#LITERAL_CATCH LITERAL_CATCH},
- *  {@link TokenTypes#LITERAL_DO LITERAL_DO},
- *  {@link TokenTypes#LITERAL_ELSE LITERAL_ELSE},
- *  {@link TokenTypes#LITERAL_FINALLY LITERAL_FINALLY},
- *  {@link TokenTypes#LITERAL_FOR LITERAL_FOR},
- *  {@link TokenTypes#LITERAL_IF LITERAL_IF},
- *  {@link TokenTypes#LITERAL_SWITCH LITERAL_SWITCH},
- *  {@link TokenTypes#LITERAL_SYNCHRONIZED LITERAL_SYNCHRONIZED},
- *  {@link TokenTypes#LITERAL_TRY LITERAL_TRY},
- *  {@link TokenTypes#LITERAL_WHILE LITERAL_WHILE}.
- * </p>
+ * 
  * <p>
- * The policy to verify is specified using the {@link
- * LeftCurlyOption} class and defaults to {@link LeftCurlyOption#EOL}. Policies
- * {@link LeftCurlyOption#EOL} and {@link LeftCurlyOption#NLOW} take into
- * account property maxLineLength. The default value for maxLineLength is 80. 
+ * The policy to verify is specified using the {@link LeftCurlyOption} class and
+ * defaults to {@link LeftCurlyOption#EOL}. Policies {@link LeftCurlyOption#EOL}
+ * and {@link LeftCurlyOption#NLOW} take into account property maxLineLength.
+ * The default value for maxLineLength is 80.
  * </p>
  * <p>
  * An example of how to configure the check is:
  * </p>
  * <pre>
- * &lt;module name="OtherLeftCurly"/&gt;
+ * &lt;module name="LeftCurly"/&gt;
  * </pre> 
  * <p>
  * An example of how to configure the check with policy
  * {@link LeftCurlyOption#NLOW} and maxLineLength 120 is:
  * </p>
  * <pre>
- * &lt;module name="OtherLeftCurly"&gt;
- *     &lt;property name="option" value="nlow"/&gt;
- *     &lt;property name="maxLineLength" value="120"/&gt;
- * &lt;/module&gt;
- * </pre>
- * <p>
- * An example of how to configure the check with policy
- * {@link LeftCurlyOption#NL} for <code>if</code> and <code>else</code> blocks
- *  is:
- * </p>
- * <pre>
- * &lt;module name="OtherLeftCurly"&gt;
- *     &lt;property name="option" value="nl"/&gt;
- *     &lt;property name="tokens" value="LITERAL_IF, LITERAL_ELSE"/&gt;
- * &lt;/module&gt;
+ * &lt;module name="LeftCurly"&gt;
+ *      &lt;property name="option"
+ * value="nlow"/&gt;     &lt;property name="maxLineLength" value="120"/&gt; &lt;
+ * /module&gt;
  * </pre> 
- *
  * @author <a href="mailto:checkstyle@puppycrawl.com">Oliver Burn</a>
  * @author lkuehne
  * @version 1.0
  */
-public class OtherLeftCurlyCheck
+public class LeftCurlyCheck
     extends AbstractLeftCurlyCheck
 {
     /** @see com.puppycrawl.tools.checkstyle.api.Check */
     public int[] getDefaultTokens()
     {
-        return new int[] {TokenTypes.LITERAL_WHILE,
-                          TokenTypes.LITERAL_TRY,
-                          TokenTypes.LITERAL_CATCH,
-                          TokenTypes.LITERAL_FINALLY,
-                          TokenTypes.LITERAL_SYNCHRONIZED,
-                          TokenTypes.LITERAL_SWITCH,
-                          TokenTypes.LITERAL_DO,
-                          TokenTypes.LITERAL_IF,
-                          TokenTypes.LITERAL_ELSE,
-                          TokenTypes.LITERAL_FOR,
-                          // TODO: need to handle....
-                          //TokenTypes.STATIC_INIT,
+        return new int[] {
+            TokenTypes.INTERFACE_DEF,
+            TokenTypes.CLASS_DEF,
+            TokenTypes.CTOR_DEF,
+            TokenTypes.METHOD_DEF,
+            TokenTypes.LITERAL_WHILE,
+            TokenTypes.LITERAL_TRY,
+            TokenTypes.LITERAL_CATCH,
+            TokenTypes.LITERAL_FINALLY,
+            TokenTypes.LITERAL_SYNCHRONIZED,
+            TokenTypes.LITERAL_SWITCH,
+            TokenTypes.LITERAL_DO,
+            TokenTypes.LITERAL_IF,
+            TokenTypes.LITERAL_ELSE,
+            TokenTypes.LITERAL_FOR,
+            // TODO: need to handle....
+            //TokenTypes.STATIC_INIT,
         };
     }
 
     /** @see com.puppycrawl.tools.checkstyle.api.Check */
     public void visitToken(DetailAST aAST)
     {
-        final DetailAST startToken = aAST;
+        final DetailAST startToken;
         final DetailAST brace;
-
+        
         switch (aAST.getType()) {
+            case TokenTypes.CTOR_DEF :
+            case TokenTypes.METHOD_DEF :
+                startToken = aAST;
+                brace = aAST.findFirstToken(TokenTypes.SLIST);
+                break;
+
+            case TokenTypes.INTERFACE_DEF :
+            case TokenTypes.CLASS_DEF :
+                // TODO: should check for modifiers
+                startToken = (DetailAST) aAST.getFirstChild().getNextSibling();
+                brace = (DetailAST) aAST.getLastChild().getFirstChild();
+                break;
+
             case TokenTypes.LITERAL_WHILE:
             case TokenTypes.LITERAL_CATCH:
             case TokenTypes.LITERAL_SYNCHRONIZED:
@@ -112,27 +114,31 @@ public class OtherLeftCurlyCheck
             case TokenTypes.LITERAL_TRY:
             case TokenTypes.LITERAL_FINALLY:
             case TokenTypes.LITERAL_DO:
-            case TokenTypes.LITERAL_IF:
+            case TokenTypes.LITERAL_IF :
+                startToken = aAST;
                 brace = aAST.findFirstToken(TokenTypes.SLIST);
                 break;
-                
-            case TokenTypes.LITERAL_ELSE:
+
+            case TokenTypes.LITERAL_ELSE :
+                startToken = aAST;
                 final DetailAST candidate = (DetailAST) aAST.getFirstChild();
                 brace =
                     (candidate.getType() == TokenTypes.SLIST)
                         ? candidate
                         : null; // silently ignore
                 break;
-                
+
             case TokenTypes.LITERAL_SWITCH :
+                startToken = aAST;
                 brace = aAST.findFirstToken(TokenTypes.LCURLY);
                 break;
 
-            default:
+            default :
+                startToken = null;
                 brace = null;
         }
-
-        if (brace != null) {
+        
+        if ((brace != null) && (startToken != null)) {
             verifyBrace(brace, startToken);
         }
     }
