50ea97d1321e6b3ca2616720214a8f7092ed9259
lkuehne <devnull@localhost>
2003-03-29 17:34:12 +0000

    added check to detect the double-checked locking idiom, rfe #709333

---
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/DoubleCheckedLockingCheckTest.java
@@ -0,0 +1,22 @@
+package com.puppycrawl.tools.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestCase;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+
+/**
+ * @author lkuehne
+ */
+public class DoubleCheckedLockingCheckTest
+    extends BaseCheckTestCase
+{
+    public void testIt() throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(DoubleCheckedLockingCheck.class);
+        final String[] expected = {
+            "34:17: The double-checked locking idiom is broken and should be avoided.",
+        };
+        verify(checkConfig, getPath("InputDoubleCheckedLocking.java"), expected);
+
+    }
+}
