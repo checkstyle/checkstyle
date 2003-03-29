8f965910b035e732fb541d243ff10b58b7bb7652
Lars Kühne <lakuehne@gmail.com>
2003-03-29 17:34:12 +0000

    added check to detect the double-checked locking idiom, rfe #709333

--- src/tests/com/puppycrawl/tools/checkstyle/AllTests.java
+++ src/tests/com/puppycrawl/tools/checkstyle/AllTests.java
@@ -55,6 +55,7 @@ import com.puppycrawl.tools.checkstyle.checks.UpperEllCheckTest;
 import com.puppycrawl.tools.checkstyle.checks.VisibilityModifierCheckTest;
 import com.puppycrawl.tools.checkstyle.checks.WhitespaceAfterCheckTest;
 import com.puppycrawl.tools.checkstyle.checks.WhitespaceAroundTest;
+import com.puppycrawl.tools.checkstyle.checks.DoubleCheckedLockingCheckTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -78,6 +79,7 @@ public class AllTests {
         suite.addTest(new TestSuite(AvoidStarImportTest.class));
         suite.addTest(new TestSuite(ConfigurationLoaderTest.class));
         suite.addTest(new TestSuite(ConstantNameCheckTest.class));
+        suite.addTest(new TestSuite(DoubleCheckedLockingCheckTest.class));
         suite.addTest(new TestSuite(EmptyBlockCheckTest.class));
         suite.addTest(new TestSuite(EmptyForIteratorPadCheckTest.class));
         suite.addTest(new TestSuite(EqualsHashCodeCheckTest.class));
