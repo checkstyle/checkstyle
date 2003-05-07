7284e8aa425ef5ed01838e99fd228609ce3e1208
Rick Giles <rickgiles>
2003-05-07 16:57:32 +0000

    magic number check. RFE 564206

---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/InputMagicNumber.java
@@ -0,0 +1,58 @@
+package com.puppycrawl.tools.checkstyle;
+
+/**
+ * Describe class InputMagicNumber
+ * @author Rick Giles
+ * @version 6-May-2003
+ */
+public class InputMagicNumber {
+    public void magicMethod() {
+        //constants, ignore
+        final int INT_CONST = 101;
+        final long LONG_CONST1 = 100L;
+        final long LONG_CONST2 = 100l;
+        final float FLOAT_CONST1 = 1.5F;
+        final float FLOAT_CONST2 = 1.5f;
+        final double DOUBLE_CONST1 = 1.5D;
+        final double DOUBLE_CONST2 = 1.5d;
+        final double DOUBLE_CONST3 = 1.5;
+
+        //ignore by default
+        int int_var1 = 1;
+        int int_var2 = (2);
+        long long_var1 = 0L;
+        long long_var2 = 0l;
+        double double_var1 = 0D;
+        double double_var2 = 0d;
+
+        int[] int_array = new int[2];
+
+        int_var1 = 1 + 2;
+        int_var1 += 1;
+        double_var1 = 1.0 + 2.0;
+
+        for (int i = 0; i < 2; i++);
+
+        if (1 < 2);
+        
+        if (1.0 < 2.0);
+
+        //magic numbers
+        int int_magic1 = 3;
+        double double_magic1 = 1.5;
+        int int_magic2 = (3 + 4);
+        
+        int_array = new int[3];
+        
+        int_magic1 += 3;
+        double_magic1 *= 1.5;
+        
+        for (int j = 3; j < 5; j += 3) {
+            int_magic1++;
+        }
+
+        if (int_magic1 < 3) {
+            int_magic1 = int_magic1 + 3;
+        }
+    }
+}
---
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/MagicNumberCheckTest.java
@@ -0,0 +1,115 @@
+package com.puppycrawl.tools.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestCase;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+
+public class MagicNumberCheckTest
+    extends BaseCheckTestCase
+{
+    public void testDefault()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(MagicNumberCheck.class);
+        final String[] expected = {
+            "41:26: '3' is a magic number.",
+            "42:32: '1.5' is a magic number.",
+            "43:27: '3' is a magic number.",
+            "43:31: '4' is a magic number.",
+            "45:29: '3' is a magic number.",
+            "47:23: '3' is a magic number.",
+            "48:26: '1.5' is a magic number.",
+            "50:22: '3' is a magic number.",
+            "50:29: '5' is a magic number.",
+            "50:37: '3' is a magic number.",
+            "54:26: '3' is a magic number.",
+            "55:39: '3' is a magic number.",
+        };
+        verify(checkConfig, getPath("InputMagicNumber.java"), expected);
+    }
+    
+    public void testIgnoreSome()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(MagicNumberCheck.class);
+        checkConfig.addAttribute("ignoreNumbers", "0, 1, 3.0");
+        final String[] expected = {
+            "22:25: '2' is a magic number.",
+            "28:35: '2' is a magic number.",
+            "30:24: '2' is a magic number.",
+            "32:29: '2.0' is a magic number.",
+            "34:29: '2' is a magic number.",
+            "36:17: '2' is a magic number.",
+            "38:19: '2.0' is a magic number.",
+            "42:32: '1.5' is a magic number.",
+            "43:31: '4' is a magic number.",
+            "48:26: '1.5' is a magic number.",
+            "50:29: '5' is a magic number.",
+        };
+        verify(checkConfig, getPath("InputMagicNumber.java"), expected);
+    }
+    
+    public void testIgnoreNone()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(MagicNumberCheck.class);
+        checkConfig.addAttribute("ignoreNumbers", "");
+        final String[] expected = {
+            "21:24: '1' is a magic number.",
+            "22:25: '2' is a magic number.",
+            "23:26: '0L' is a magic number.",
+            "24:26: '0l' is a magic number.",
+            "25:30: '0D' is a magic number.",
+            "26:30: '0d' is a magic number.",
+            "28:35: '2' is a magic number.",
+            "30:20: '1' is a magic number.",
+            "30:24: '2' is a magic number.",
+            "31:21: '1' is a magic number.",
+            "32:23: '1.0' is a magic number.",
+            "32:29: '2.0' is a magic number.",
+            "34:22: '0' is a magic number.",
+            "34:29: '2' is a magic number.",
+            "36:13: '1' is a magic number.",
+            "36:17: '2' is a magic number.",
+            "38:13: '1.0' is a magic number.",
+            "38:19: '2.0' is a magic number.",
+            "41:26: '3' is a magic number.",
+            "42:32: '1.5' is a magic number.",
+            "43:27: '3' is a magic number.",
+            "43:31: '4' is a magic number.",
+            "45:29: '3' is a magic number.",
+            "47:23: '3' is a magic number.",
+            "48:26: '1.5' is a magic number.",
+            "50:22: '3' is a magic number.",
+            "50:29: '5' is a magic number.",
+            "50:37: '3' is a magic number.",
+            "54:26: '3' is a magic number.",
+            "55:39: '3' is a magic number.",
+        };
+        verify(checkConfig, getPath("InputMagicNumber.java"), expected);
+    }
+    
+    public void testIntegersOnly()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(MagicNumberCheck.class);
+        checkConfig.addAttribute("tokens", "NUM_INT, NUM_LONG");
+        final String[] expected = {
+            "41:26: '3' is a magic number.",
+            "43:27: '3' is a magic number.",
+            "43:31: '4' is a magic number.",
+            "45:29: '3' is a magic number.",
+            "47:23: '3' is a magic number.",
+            "50:22: '3' is a magic number.",
+            "50:29: '5' is a magic number.",
+            "50:37: '3' is a magic number.",
+            "54:26: '3' is a magic number.",
+            "55:39: '3' is a magic number.",
+        };
+        verify(checkConfig, getPath("InputMagicNumber.java"), expected);
+    }
+}
+
