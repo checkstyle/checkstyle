190aa27aa72e05d0d079576dba0c371b05b60e19
lkuehne <devnull@localhost>
2003-05-18 07:46:19 +0000

    added tests for InterfaceIsType check

---
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/InterfaceIsTypeCheckTest.java
@@ -0,0 +1,33 @@
+package com.puppycrawl.tools.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestCase;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+
+public class InterfaceIsTypeCheckTest
+    extends BaseCheckTestCase
+{
+    public void testDefault()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(InterfaceIsTypeCheck.class);
+        final String[] expected = {
+            "25: interfaces should describe a type and hence have methods.",
+        };
+        verify(checkConfig, getPath("InputInterfaceIsType.java"), expected);
+    }
+
+    public void testAllowMarker()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(InterfaceIsTypeCheck.class);
+        checkConfig.addAttribute("allowMarkerInterfaces", "false");
+        final String[] expected = {
+            "20: interfaces should describe a type and hence have methods.",
+            "25: interfaces should describe a type and hence have methods.",
+        };
+        verify(checkConfig, getPath("InputInterfaceIsType.java"), expected);
+    }
+
+}
