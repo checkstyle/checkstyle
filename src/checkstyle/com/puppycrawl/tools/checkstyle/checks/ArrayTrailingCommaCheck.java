d6002f86dbdf2196ca585ca3fb9bb74772c45ff4
o_sukhodolsky <devnull@localhost>
2003-05-29 03:30:44 +0000

    Added check for trailing comma in array initialization. (module ArrayTrailingComma, request 696301)

---
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/ArrayTrailingCommaCheck.java
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2002  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+
+/**
+ * <p>
+ * Checks if array initialization contains optional trailing comma.
+ * </p>
+ * <p>
+ * Rationale: Putting this comma in make is easier to change the
+ * order of the elements or add new elements on the end.
+ * </p>
+ * <p>
+ * An example of how to configure the check is:
+ * </p>
+ * <pre>
+ * &lt;module name="ArrayTrailingComma"/&gt;
+ * </pre>
+ * @author o_sukhodolsky
+ */
+public class ArrayTrailingCommaCheck extends Check
+{
+    /** @see Check */
+    public int[] getDefaultTokens()
+    {
+        return new int[] {TokenTypes.ARRAY_INIT};
+    }
+
+    /** @see Check */
+    public void visitToken(DetailAST aArrayInit)
+    {
+        DetailAST rcurly = aArrayInit.findFirstToken(TokenTypes.RCURLY);
+
+        // if curlys are on the same line
+        // or array is empty then check nothing
+        if (aArrayInit.getLineNo() == rcurly.getLineNo()
+            || aArrayInit.getChildCount() == 1)
+        {
+            return;
+        }
+
+        DetailAST prev = rcurly.getPreviousSibling();
+        if (prev.getType() != TokenTypes.COMMA) {
+            log(rcurly.getLineNo(), "array.trailing.comma");
+        }
+    }
+}
---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/InputArrayTrailingComma.java
@@ -0,0 +1,41 @@
+package com.puppycrawl.tools.checkstyle;
+
+public class InputArrayTrailingComma
+{
+    int[] a1 = new int[] 
+    {
+        1,
+        2,
+        3,
+    };
+
+    int[] a2 = new int[] 
+    {
+        1,
+        2,
+        3
+    };
+
+    int[] b1 = new int[] {1, 2, 3,};
+    int[] b2 = new int[] {1, 2, 3};
+
+    int[][] c1 = new int[][] {{1, 2,}, {3, 3,}, {5, 6,},};
+    int[][] c2 = new int[][] {{1, 2}, {3, 3,}, {5, 6,}};
+
+    int[][] d1 = new int[][] 
+    {
+        {1, 2,},
+        {3, 3,},
+        {5, 6,},
+    };
+    int[][] d2 = new int[][] 
+    {
+        {1, 
+         2}, 
+        {3, 3,}, 
+        {5, 6,}
+    };
+
+    int[] e1 = new int[] {
+    };
+}
