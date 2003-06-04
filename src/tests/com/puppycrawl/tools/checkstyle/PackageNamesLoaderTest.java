dfb6c809fb528d2fa0462de3a27166aca07c94ca
oburn <devnull@localhost>
2003-06-04 23:52:04 +0000

    Refactored the Design checks

--- src/tests/com/puppycrawl/tools/checkstyle/PackageNamesLoaderTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/PackageNamesLoaderTest.java
@@ -53,6 +53,7 @@ public class PackageNamesLoaderTest extends TestCase
             "com.puppycrawl.tools.checkstyle.",
             "com.puppycrawl.tools.checkstyle.checks.",
             "com.puppycrawl.tools.checkstyle.checks.blocks.",
+            "com.puppycrawl.tools.checkstyle.checks.design.",
             "com.puppycrawl.tools.checkstyle.checks.imports.",
             "com.puppycrawl.tools.checkstyle.checks.javadoc.",
             "com.puppycrawl.tools.checkstyle.checks.naming.",
