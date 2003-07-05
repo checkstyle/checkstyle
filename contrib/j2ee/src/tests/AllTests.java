a43e43e0e7ed7aca04bb9f8c39674e4ad1dab1db
Rick Giles <rickgiles>
2003-07-05 17:23:25 +0000

    moved to main distribution

--- contrib/j2ee/src/tests/AllTests.java
+++ contrib/j2ee/src/tests/AllTests.java
@@ -1,3 +1,5 @@
+import com.puppycrawl.tools.checkstyle.checks.j2ee.*;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -17,20 +19,13 @@ public class AllTests {
         TestSuite suite = new TestSuite("Test for J2EE checks");
         //$JUnit-BEGIN$
         suite.addTest(new TestSuite(EntityBeanCheckTest.class));
-        suite.addTest(new TestSuite(EntityBeanEjbCreateCheckTest.class));
-        suite.addTest(new TestSuite(EntityBeanEjbHomeCheckTest.class));
-        suite.addTest(new TestSuite(EntityBeanEjbPostCreateCheckTest.class));
-        suite.addTest(new TestSuite(EntityBeanEjbSelectCheckTest.class));
-        suite.addTest(new TestSuite(EntityBeanFindByPrimaryKeyCheckTest.class));
-        suite.addTest(new TestSuite(EntityBeanFinderCheckTest.class));
-        suite.addTest(new TestSuite(EntityBeanMatchEjbCreateCheckTest.class));
-        suite.addTest(new TestSuite(HomeInterfaceCheckTest.class));
+        suite.addTest(new TestSuite(FinalStaticCheckTest.class));
         suite.addTest(new TestSuite(LocalHomeInterfaceCheckTest.class));
+        suite.addTest(new TestSuite(LocalInterfaceCheckTest.class));
         suite.addTest(new TestSuite(MessageBeanCheckTest.class));
+        suite.addTest(new TestSuite(RemoteHomeInterfaceCheckTest.class));
         suite.addTest(new TestSuite(RemoteInterfaceCheckTest.class));
         suite.addTest(new TestSuite(SessionBeanCheckTest.class));
-        suite.addTest(new TestSuite(SessionBeanEjbCreateCheckTest.class));
-        suite.addTest(new TestSuite(FinalStaticCheckTest.class));
         suite.addTest(new TestSuite(ThisParameterCheckTest.class));
         suite.addTest(new TestSuite(ThisReturnCheckTest.class));
         //$JUnit-END$
