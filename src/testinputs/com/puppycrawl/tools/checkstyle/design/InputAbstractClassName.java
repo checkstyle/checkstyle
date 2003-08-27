b84bada438aa577f2d6687b69300f56483987e67
o_sukhodolsky <devnull@localhost>
2003-08-27 21:14:39 +0000

    Added IllegalAbstractClassName check (request 750749).

---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/design/InputAbstractClassName.java
@@ -0,0 +1,23 @@
+package com.puppycrawl.tools.checkstyle.checks.design;
+
+abstract public class InputAbstractClassName {
+}
+
+abstract class NonAbstractClassName {
+}
+
+abstract class FactoryWithBadName {
+}
+
+abstract class AbstractClassName {
+    abstract class NonAbstractInnerClass {
+    }
+}
+
+abstract class ClassFactory {
+    abstract class WellNamedFactory {
+    }
+}
+
+class NonAbstractClass {
+}
