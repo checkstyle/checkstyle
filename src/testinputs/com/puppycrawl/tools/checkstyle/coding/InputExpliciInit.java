03c3efabf3b36fd1b8dd4da30678365c7506370c
Oleg Sukhodolsky <os97673@gmail.com>
2003-10-04 17:01:27 +0000

    Check for variable explicit initialization (request 614387)

---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/coding/InputExpliciInit.java
@@ -0,0 +1,28 @@
+public class InputExplicitInit {
+    private int x = 0;
+    private Bar bar = null;
+    private int y = 1;
+    private long y1 = 1 - 1;
+    private long y3;
+    private long y4 = 0L;
+    private boolean b1 = false;
+    private boolean b2 = true;
+    private boolean b3;
+    private String str = "";
+    java.lang.String str1 = null, str3 = null;
+    int ar1[] = null;
+    int ar2[] = new int[1];
+    int ar3[];
+    float f1 = 0f;
+    double d1 = 0.0;
+
+    static char ch;
+    static char ch1 = 0;
+    static char ch2 = '\0';
+    static char ch3 = '\1';
+
+    void method() {
+        int xx = 0;
+        Strign s = null;
+    }
+}
---
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/coding/ExplicitInitializationCheckTest.java
@@ -0,0 +1,31 @@
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestCase;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+
+import java.io.File;
+
+public class ExplicitInitializationCheckTest extends BaseCheckTestCase
+{
+    public void testDefault() throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(ExplicitInitializationCheck.class);
+        final String[] expected = {
+            "2:17: Variable 'x' explicitly initialized to '0' (default value for its type).",
+            "3:17: Variable 'bar' explicitly initialized to 'null' (default value for its type).",
+            "7:18: Variable 'y4' explicitly initialized to '0' (default value for its type).",
+            "8:21: Variable 'b1' explicitly initialized to 'false' (default value for its type).",
+            "12:22: Variable 'str1' explicitly initialized to 'null' (default value for its type).",
+            "12:35: Variable 'str3' explicitly initialized to 'null' (default value for its type).",
+            "13:9: Variable 'ar1' explicitly initialized to 'null' (default value for its type).",
+            "16:11: Variable 'f1' explicitly initialized to '0' (default value for its type).",
+            "17:12: Variable 'd1' explicitly initialized to '0' (default value for its type).",
+            "20:17: Variable 'ch1' explicitly initialized to '\\0' (default value for its type).",
+            "21:17: Variable 'ch2' explicitly initialized to '\\0' (default value for its type).",
+        };
+        verify(checkConfig, 
+               getPath("coding" + File.separator + "InputExpliciInit.java"),
+               expected);
+    }
+}
