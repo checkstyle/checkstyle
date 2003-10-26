066696714bd522b26177f6be814bac334e57b332
Lars Kühne <lakuehne@gmail.com>
2003-10-26 22:10:00 +0000

    bugfix for #824754, HideUtilityConstructor should not apply to subclasses

---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/design/InputNonUtilityClass.java
@@ -0,0 +1,21 @@
+package com.puppycrawl.tools.checkstyle.checks.design;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+/**
+ * Not a util class because it's not directly derived from java.lang.Object.
+ */
+public class InputNonUtilityClass extends JPanel
+{
+    /** HideUtilityClassConstructorCheck should not report this */
+    public InputNonUtilityClass()
+    {
+	this.setPreferredSize(new Dimension(100, 100));
+    }
+
+    public static void utilMethod()
+    {
+	System.out.println("I'm a utility method");
+    }
+}
