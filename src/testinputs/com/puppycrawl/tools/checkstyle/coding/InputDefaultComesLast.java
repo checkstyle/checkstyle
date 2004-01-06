d559cf9414bb9c7830c48ff6ab5fac887036d63c
Oleg Sukhodolsky <os97673@gmail.com>
2004-01-06 05:50:49 +0000

    DefaultComesLast check (request 754486)

---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/coding/InputDefaultComesLast.java
@@ -0,0 +1,28 @@
+public class InputDefaultComesLast
+{
+    void method(int i) {
+        // switch with last default
+        switch (i) {
+        case 1: break;
+        case 2: break;
+        default: 
+            // do something :)
+        }
+
+        // switch w/o default (not a problem)
+        switch (i) {
+        case 1: break;
+        case 2: break;
+        }
+
+        // VIOLATION!!! default is not the last one.
+        switch (i) {
+        case 1:
+            break;
+        default:
+            break;
+        case 2:
+            break;
+        }
+    }
+}
