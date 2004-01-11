0340dcb26d5db9625e8e1f093f39165b8bdb18bf
Oleg Sukhodolsky <os97673@gmail.com>
2004-01-11 04:36:20 +0000

    Added multiLines property for RegexpHeaderCheck. Added AbstractHeaderCheck - superclass for header checks.

--- src/checkstyle/com/puppycrawl/tools/checkstyle/checks/HeaderCheck.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/HeaderCheck.java
@@ -19,17 +19,9 @@
 
 package com.puppycrawl.tools.checkstyle.checks;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.LineNumberReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 
-import com.puppycrawl.tools.checkstyle.api.Check;
-import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
-
-import org.apache.commons.beanutils.ConversionException;
 
 /**
  * <p>
@@ -71,62 +63,16 @@ import org.apache.commons.beanutils.ConversionException;
  *    &lt;property name="ignoreLines" value="2, 3, 4"/&gt;
  * &lt;/module&gt;
  * </pre>
+ *
  * @author Lars Kühne
  */
-public class HeaderCheck
-    extends Check
+public class HeaderCheck extends AbstractHeaderCheck
 {
-    /** empty array to avoid instantiations */
+    /** empty array to avoid instantiations. */
     private static final int[] EMPTY_INT_ARRAY = new int[0];
 
-    /** the lines of the header file */
-    private String[] mHeaderLines;
-
-    /** the header lines to ignore in the check, sorted */
+    /** the header lines to ignore in the check, sorted. */
     private int[] mIgnoreLines = EMPTY_INT_ARRAY;
-
-    /** @see com.puppycrawl.tools.checkstyle.api.Check */
-    public int[] getDefaultTokens()
-    {
-        return new int[0];
-    }
-
-    /**
-     * Checks that required args were specified.
-     * @see com.puppycrawl.tools.checkstyle.api.AutomaticBean#finishLocalSetup
-     */
-    protected final void finishLocalSetup() throws CheckstyleException
-    {
-        if (mHeaderLines == null) {
-            throw new CheckstyleException(
-                    "property 'headerFile' is missing or invalid in module "
-                    + getConfiguration().getName());
-        }
-    }
-
-    /** @see com.puppycrawl.tools.checkstyle.api.Check */
-    public void beginTree(DetailAST aRootAST)
-    {
-
-        final String[] lines = getLines();
-
-        if (mHeaderLines.length > lines.length) {
-            log(1, "header.missing");
-        }
-        else {
-            for (int i = 0; i < mHeaderLines.length; i++) {
-                // skip lines we are meant to ignore
-                if (isIgnoreLine(i + 1)) {
-                    continue;
-                }
-
-                if (!isMatch(i)) {
-                    log(i + 1, "header.mismatch", mHeaderLines[i]);
-                    break; // stop checking
-                }
-            }
-        }
-    }
 
     /**
      * @param aLineNo a line number
@@ -144,42 +90,10 @@ public class HeaderCheck
      */
     protected boolean isMatch(int aLineNumber)
     {
-        final String[] lines = getLines();
-        return mHeaderLines[aLineNumber].equals(lines[aLineNumber]);
-    }
-
-    /**
-     * Set the header file to check against.
-     * @param aFileName the file that contains the header to check against.
-     * @throws ConversionException if the file cannot be loaded
-     */
-    public void setHeaderFile(String aFileName)
-        throws ConversionException
-    {
-        // Handle empty param
-        if ((aFileName == null) || (aFileName.trim().length() == 0)) {
-            return;
-        }
-
-        // load the file
-        try {
-            final LineNumberReader lnr =
-                    new LineNumberReader(new FileReader(aFileName));
-            final ArrayList lines = new ArrayList();
-            while (true) {
-                final String l = lnr.readLine();
-                if (l == null) {
-                    break;
-                }
-                lines.add(l);
-            }
-            mHeaderLines = (String[]) lines.toArray(new String[0]);
-        }
-        catch (IOException ex) {
-            throw new ConversionException(
-                    "unable to load header file " + aFileName, ex);
-        }
-
+        final String line = getLines()[aLineNumber];
+        // skip lines we are meant to ignore
+        return isIgnoreLine(aLineNumber + 1)
+            || getHeaderLines()[aLineNumber].equals(line);
     }
 
     /**
@@ -198,12 +112,19 @@ public class HeaderCheck
         Arrays.sort(mIgnoreLines);
     }
 
-    /**
-     * Return the header lines to check against.
-     * @return the header lines to check against.
-     */
-    protected String[] getHeaderLines()
+    /** {@inheritDoc} */
+    public void beginTree(DetailAST aRootAST)
     {
-        return mHeaderLines;
+        if (getHeaderLines().length > getLines().length) {
+            log(1, "header.missing");
+        }
+        else {
+            for (int i = 0; i < getHeaderLines().length; i++) {
+                if (!isMatch(i)) {
+                    log(i + 1, "header.mismatch", getHeaderLines()[i]);
+                    break; // stop checking
+                }
+            }
+        }
     }
 }
--- src/checkstyle/com/puppycrawl/tools/checkstyle/checks/RegexpHeaderCheck.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/checks/RegexpHeaderCheck.java
@@ -18,9 +18,13 @@
 ////////////////////////////////////////////////////////////////////////////////
 package com.puppycrawl.tools.checkstyle.checks;
 
+import java.util.Arrays;
+
 import org.apache.regexp.RE;
 import org.apache.regexp.RESyntaxException;
 import org.apache.commons.beanutils.ConversionException;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.Utils;
 
 /**
@@ -40,34 +44,83 @@ import com.puppycrawl.tools.checkstyle.api.Utils;
  * <p>For example, consider the following header file:</p>
  *
  * <pre>
- * line 1: /{71}
- * line 2: // checkstyle:
- * line 3: // Checks Java source code for adherence to a set of rules\.
- * line 4: // Copyright \(C\) \d\d\d\d  Oliver Burn
- * line 5: // Last modification by \$Author.*\$
- * line 6: /{71}
+ * line 1: ^/{71}$
+ * line 2: ^// checkstyle:$
+ * line 3: ^// Checks Java source code for adherence to a set of rules\.$
+ * line 4: ^// Copyright \(C\) \d\d\d\d  Oliver Burn$
+ * line 5: ^// Last modification by \$Author.*\$$
+ * line 6: ^/{71}$
  * </pre>
  *
  * <p>Lines 1 and 6 demonstrate a more compact notation for 71 '/'
  * characters. Line 4 enforces that the copyright notice includes a four digit
  * year. Line 5 is an example how to enforce revision control keywords in a file
- * header.</p>
+ * header. All lines start from ^ (line start symbol) and end with $ (line end)
+ * to force matching regexp with complete line in the source file.</p>
  * <p>An example of how to configure the check to use header file
- * &quot;java.header&quot; and ignore lines 4 and 5 is:
+ * &quot;java.header&quot; is:
  * </p>
  * <pre>
  * &lt;module name="RegexpHeader"&gt;
  *    &lt;property name="headerFile" value="java.header"/&gt;
- *    &lt;property name="ignoreLines" value="4, 5"/&gt;
  * &lt;/module&gt;
  * </pre>
+ *    <p class="body">
+ *    To configure the check to use header file <code
+ *    >&quot;java.header&quot;</code> and <code
+ *    >10</code> and  <code>13</code> muli-lines:
+ *    </p>
+ *    <pre class="body">
+ * &lt;module name=&quot;RegexpHeader&quot;&gt;
+ *   &lt;property name=&quot;headerFile&quot; value=&quot;java.header&quot;/&gt;
+ *   &lt;property name=&quot;multiLines&quot; value=&quot;10, 13&quot;/&gt;
+ *&lt;/module&gt;
+ *     </pre>
+ * <p><u>Note</u>: ignoreLines property was removed you should use ^.*$ regexp
+ * for line to ignore it.
+ * </p>
  *
  * @author Lars Kühne
+ * @author o_sukhodolsky
  */
-public class RegexpHeaderCheck extends HeaderCheck
+public class RegexpHeaderCheck extends AbstractHeaderCheck
 {
+    /** empty array to avoid instantiations. */
+    private static final int[] EMPTY_INT_ARRAY = new int[0];
+
+    /** the header lines to repeat (0 or more) in the check, sorted. */
+    private int[] mMultiLines = EMPTY_INT_ARRAY;
+
     /** the compiled regular expressions */
     private RE[] mHeaderRegexps;
+
+    /** number of current template line. */
+    private int mCurrentLineNo;
+
+    /**
+     * @param aLineNo a line number
+     * @return if <code>aLineNo</code> is one of the repeat header lines.
+     */
+    private boolean isMultiLine(int aLineNo)
+    {
+        return (Arrays.binarySearch(mMultiLines, aLineNo + 1) >= 0);
+    }
+
+    /**
+     * Set the lines numbers to repeat in the header check.
+     * @param aList comma separated list of line numbers to repeat in header.
+     */
+    public void setMultiLines(int[] aList)
+    {
+        if (aList == null || aList.length == 0) {
+            mMultiLines = EMPTY_INT_ARRAY;
+            return;
+        }
+
+        mMultiLines = new int[aList.length];
+        System.arraycopy(aList, 0, mMultiLines, 0, aList.length);
+        Arrays.sort(mMultiLines);
+    }
 
     /**
      * Sets the file that contains the header to check against.
@@ -95,11 +148,56 @@ public class RegexpHeaderCheck extends HeaderCheck
         }
     }
 
-
-    /** @see HeaderCheck */
-    protected boolean isMatch(int aLineNumber)
+    /**
+     * Checks if a code line matches the required header line.
+     * @param aLineNo the linenumber to check against the header
+     * @param aHeaderLineNo the header line number.
+     * @return true if and only if the line matches the required header line.
+     */
+    private boolean isMatch(int aLineNo, int aHeaderLineNo)
     {
-        final String[] lines = getLines();
-        return mHeaderRegexps[aLineNumber].match(lines[aLineNumber]);
+        final String line = getLines()[aLineNo];
+        return mHeaderRegexps[aHeaderLineNo].match(line);
+    }
+
+    /** {@inheritDoc} */
+    public void beginTree(DetailAST aRootAST)
+    {
+        final int headerSize = getHeaderLines().length;
+        final int fileSize = getLines().length;
+
+        if (headerSize - mMultiLines.length > fileSize) {
+            log(1, "header.missing");
+        }
+        else {
+            int headerLineNo = 0;
+            int i;
+            for (i = 0; headerLineNo < headerSize && i < fileSize; i++) {
+                boolean isMatch = isMatch(i, headerLineNo);
+                while (!isMatch && isMultiLine(headerLineNo)) {
+                    headerLineNo++;
+                    isMatch = (headerLineNo == headerSize)
+                        || isMatch(i, headerLineNo);
+                }
+                if (!isMatch) {
+                    log(i + 1, "header.mismatch",
+                        getHeaderLines()[headerLineNo]);
+                    break; // stop checking
+                }
+                if (!isMultiLine(headerLineNo)) {
+                    headerLineNo++;
+                }
+            }
+            if (i == fileSize) {
+                // if file finished, but we have at least one non-multi-line
+                // header isn't completed
+                for (; headerLineNo < headerSize; headerLineNo++) {
+                    if (!isMultiLine(headerLineNo)) {
+                        log(1, "header.missing");
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
