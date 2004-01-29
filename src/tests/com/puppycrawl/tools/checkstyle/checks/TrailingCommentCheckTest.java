cd7d6a6a0e6ed9c0f4f46d17624a67c5ade82385
o_sukhodolsky <devnull@localhost>
2004-01-29 19:30:03 +0000

    TrailingComment check, request 744970

--- src/tests/com/puppycrawl/tools/checkstyle/checks/TrailingCommentCheckTest.java
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/TrailingCommentCheckTest.java
@@ -5,18 +5,17 @@ import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
 
 public class TrailingCommentCheckTest extends BaseCheckTestCase
 {
-    public void testDefaultTokens()
+    public void testDefaults()
             throws Exception
     {
         final DefaultConfiguration checkConfig =
             createCheckConfig(TrailingCommentCheck.class);
-//          checkConfig.addAttribute("tokens", "CTOR_DEF");
         final String[] expected = {
             "2: Don't use trailing comments.",
             "5: Don't use trailing comments.",
             "6: Don't use trailing comments.",
-            "16: Too many comments.",
-            "17: Too many comments.",
+            "16: Don't use trailing comments.",
+            "17: Don't use trailing comments.",
         };
         verify(checkConfig, getPath("InputTrailingComment.java"), expected);
     }
