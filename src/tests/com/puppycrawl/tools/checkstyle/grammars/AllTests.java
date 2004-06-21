6da14c010125ca83d9c8900e0c3cf6f7eda84425
rickgiles <devnull@localhost>
2004-06-21 22:30:52 +0000

    Better information for unexpected char, RFE 666188

---
+++ src/tests/com/puppycrawl/tools/checkstyle/grammars/AllTests.java
@@ -0,0 +1,21 @@
+package com.puppycrawl.tools.checkstyle.grammars;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Describe class AllTests.
+ * @author Rick Giles
+ * @version Jun 21, 2004
+ */
+public class AllTests {
+
+    public static Test suite() {
+        TestSuite suite =
+            new TestSuite("Test for com.puppycrawl.tools.checkstyle.grammars");
+        //$JUnit-BEGIN$
+        suite.addTest(new TestSuite(GeneratedJava14LexerTest.class));
+        //$JUnit-END$
+        return suite;
+    }
+}
---
+++ src/tests/com/puppycrawl/tools/checkstyle/grammars/GeneratedJava14LexerTest.java
@@ -0,0 +1,25 @@
+package com.puppycrawl.tools.checkstyle.grammars;
+
+import java.io.IOException;
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestCase;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.checks.naming.MemberNameCheck;
+
+/**
+ * Tests GeneratedJava14Lexer.
+ * @author Rick Giles
+ */
+public class GeneratedJava14LexerTest
+    extends BaseCheckTestCase
+{
+    public void testUnexpectedChar() throws IOException, Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(MemberNameCheck.class);
+        final String[] expected = {
+            "8:10: Got an exception - unexpected char: 0xA9",
+        };
+        verify(checkConfig, getPath("grammars/InputGrammar.java"), expected);
+    }
+}
