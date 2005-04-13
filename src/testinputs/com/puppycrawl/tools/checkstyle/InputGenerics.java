28c8ed692f83ff95e0814bd101177c4bb7794806
Michael Studman <mstudman@atlassian.com>
2005-04-13 20:00:55 +0000

    no message

---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/InputGenerics.java
@@ -0,0 +1,13 @@
+package com.puppycrawl.tools.checkstyle;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class InputGenerics<A, B extends Collection<?>, C extends D&E>
+{
+}
+
+//No whitespace after commas
+class BadCommas < A,B,C extends Map < A,String > >
+{
+}
