ddd153f70798bcf1e33629e98407db8f9dbe0dcc
Oleg Sukhodolsky <os97673@gmail.com>
2005-07-08 06:26:23 +0000

    Fixed documentation and added one more test for WhitespaceAround

--- src/testinputs/com/puppycrawl/tools/checkstyle/InputGenerics.java
+++ src/testinputs/com/puppycrawl/tools/checkstyle/InputGenerics.java
@@ -10,6 +10,8 @@ public class InputGenerics<A, B extends Collection<?>, C extends D&E>
 //No whitespace after commas
 class BadCommas < A,B,C extends Map < A,String > >
 {
+    private java.util.Hashtable < Integer, D > p =
+        new java.util.Hashtable < Integer, D > ();
 }
 
 // we need these interfaces for generics
