8383617e84264346b60dda7000c19a18c35371a1
Oleg Sukhodolsky <os97673@gmail.com>
2005-08-17 07:04:54 +0000

    It's a good idea to run gump.  Fixed javadoc for new token.

--- src/checkstyle/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/checkstyle/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -1129,7 +1129,7 @@ public final class TokenTypes
      **/
     //public static final int LBRACK = GeneratedJavaTokenTypes.LBRACK;
     /**
-     * <code>[</code>
+     * The <code>[</code> symbol.
      *
      * @see #INDEX_OP
      * @see #ARRAY_DECLARATOR
