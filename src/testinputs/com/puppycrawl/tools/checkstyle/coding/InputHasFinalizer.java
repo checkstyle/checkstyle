f31b6a79623c3f09c95ae558c19e8d574215b1ca
Lars Kühne <lakuehne@gmail.com>
2008-03-02 22:18:36 +0000

    incorporated patch #1892273 (NoFinalizer check) and added better unit tests, one bugfix, docs and i18n

---
+++ src/testinputs/com/puppycrawl/tools/checkstyle/coding/InputHasFinalizer.java
@@ -0,0 +1,27 @@
+package com.puppycrawl.tools.checkstyle.coding;
+
+public class InputHasFinalizer
+{
+    public void finalize()
+    {
+        // It's not enough to check if the METHOD_DEF branch contains a PARAMETER_DEF, as that would
+        // treat this method as having a parameter.
+        Runnable runnable = new Runnable() {
+        
+            public void run() {
+                reallyFinalize("hi");
+            }
+
+            // generates a PARAMETER_DEF AST inside the METHOD_DEF of finalize()
+            private void reallyFinalize(String s)
+            {
+            }
+        };
+        runnable.run();
+    }
+    
+    // should not be reported by NoFinalizer check
+    public void finalize(String x)
+    {
+    }
+}
---
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/coding/NoFinalizerCheckTest.java
@@ -0,0 +1,38 @@
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+
+import com.puppycrawl.tools.checkstyle.BaseCheckTestSupport;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import org.junit.Test;
+
+/**
+ * NoFinalizerCheck test.
+ *
+ * @author smckay@google.com (Steve McKay)
+ */
+public class NoFinalizerCheckTest
+    extends BaseCheckTestSupport
+{
+    @Test
+    public void testHasFinalizer()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(NoFinalizerCheck.class);
+        final String[] expected = {
+            "5: Avoid using finalizer method.",
+        };
+        verify(checkConfig, getPath("coding/InputHasFinalizer.java"), expected);
+    }
+
+    @Test
+    public void testHasNoFinalizer()
+        throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(NoFinalizerCheck.class);
+        final String[] expected = {
+        };
+        verify(checkConfig, getPath("coding/InputIllegalThrowsCheck.java"), expected);
+    }
+}
