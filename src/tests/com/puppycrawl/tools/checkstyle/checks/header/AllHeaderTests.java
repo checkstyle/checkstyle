4a5491810944bed643a9f2acf46ef3b9da8f1be7
Oliver Burn <oliver@puppycrawl.com>
2008-11-16 10:54:31 +0000

    removing CrossLanguageRegexpHeaderCheck.

--- src/tests/com/puppycrawl/tools/checkstyle/checks/header/AllHeaderTests.java
+++ src/tests/com/puppycrawl/tools/checkstyle/checks/header/AllHeaderTests.java
@@ -4,8 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( {CrossLanguageRegexpHeaderCheckTest.class,
-    HeaderCheckTest.class})
+@Suite.SuiteClasses( {HeaderCheckTest.class})
 public class AllHeaderTests
 {
 }
