2992639a3933b662582cac24503de8bfaa10956f
Max <maxvetrenko2241@gmail.com>
2014-06-19 17:21:13 +0400

    Update for existing Check: DeclarationOrder #4

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/DeclarationOrderCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/DeclarationOrderCheckTest.java
@@ -70,6 +70,7 @@ public class DeclarationOrderCheckTest
         checkConfig.addAttribute("ignoreConstructors", "false");
         checkConfig.addAttribute("ignoreMethods", "true");
         checkConfig.addAttribute("ignoreModifiers", "true");
+        checkConfig.addAttribute("groupOverloadMethods", "false");
 
         final String[] expected = {
             "45:9: Static variable definition in wrong order.",
@@ -91,6 +92,7 @@ public class DeclarationOrderCheckTest
         checkConfig.addAttribute("ignoreConstructors", "true");
         checkConfig.addAttribute("ignoreMethods", "true");
         checkConfig.addAttribute("ignoreModifiers", "false");
+        checkConfig.addAttribute("groupOverloadMethods", "false");
 
         final String[] expected = {
             "8:5: Variable access definition in wrong order.",
@@ -117,6 +119,49 @@ public class DeclarationOrderCheckTest
             "143:9: Static variable definition in wrong order.",
             "143:9: Variable access definition in wrong order.",
             "178:5: Instance variable definition in wrong order.",
+        };
+        verify(checkConfig, getPath("coding/InputDeclarationOrder.java"), expected);
+    }
+
+    @Test
+    public void testOverloadMethodsGrouping() throws Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(DeclarationOrderCheck.class);
+        checkConfig.addAttribute("ignoreConstructors", "false");
+        checkConfig.addAttribute("ignoreMethods", "false");
+        checkConfig.addAttribute("ignoreModifiers", "false");
+        checkConfig.addAttribute("groupOverloadMethods", "true");
+
+        final String[] expected = {
+            "8:5: Variable access definition in wrong order.",
+            "13:5: Variable access definition in wrong order.",
+            "18:5: Variable access definition in wrong order.",
+            "21:5: Variable access definition in wrong order.",
+            "27:5: Static variable definition in wrong order.",
+            "27:5: Variable access definition in wrong order.",
+            "34:9: Variable access definition in wrong order.",
+            "45:9: Static variable definition in wrong order.",
+            "45:9: Variable access definition in wrong order.",
+            "54:5: Constructor definition in wrong order.",
+            "80:5: Instance variable definition in wrong order.",
+
+            "92:9: Variable access definition in wrong order.",
+            "100:9: Static variable definition in wrong order.",
+            "100:9: Variable access definition in wrong order.",
+            "106:5: Variable access definition in wrong order.",
+            "111:5: Variable access definition in wrong order.",
+            "116:5: Variable access definition in wrong order.",
+            "119:5: Variable access definition in wrong order.",
+            "125:5: Static variable definition in wrong order.",
+            "125:5: Variable access definition in wrong order.",
+            "132:9: Variable access definition in wrong order.",
+            "143:9: Static variable definition in wrong order.",
+            "143:9: Variable access definition in wrong order.",
+            "152:5: Constructor definition in wrong order.",
+            "178:5: Instance variable definition in wrong order.",
+            "203:9: Overload methods should not be split.",
+            "215:9: Overload methods should not be split.",
         };
         verify(checkConfig, getPath("coding/InputDeclarationOrder.java"), expected);
     }
