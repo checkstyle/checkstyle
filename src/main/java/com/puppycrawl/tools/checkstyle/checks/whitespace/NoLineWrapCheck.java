27442c37bedc44d7c22a73b1a78c8f56ae919d89
Max <maxvetrenko2241@gmail.com>
2014-06-19 17:37:46 +0400

    NoLineWrap #173

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/whitespace/NoLineWrapCheck.java
@@ -0,0 +1,95 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2014  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.checks.whitespace;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * <p>Checks that chosen statements are not line-wrapped.
+ * By default this Check restricts wrapping import and package statements,
+ * but it's possible to check any statement.
+ * </p>
+ * <h4>Examples</h4>
+ * <p class="body">
+ *
+ * Examples of line-wrapped statements (bad case):
+ * <pre><code> package com.puppycrawl.
+ *    tools.checkstyle.checks;
+ *
+ * import com.puppycrawl.tools.
+ *    checkstyle.api.Check;
+ * </code></pre>
+ *
+ * <p>
+ * To configure the check to force no line-wrapping
+ * in package and import statements (default values):
+ * </p>
+ * <pre class="body">
+ * &lt;module name=&quot;NoLineWrap&quot;/&gt;
+ * </pre>
+ *
+ * <p>
+ * To configure the check to force no line-wrapping only
+ * in import statements:
+ * </p>
+ * <pre class="body">
+ * &lt;module name=&quot;NoLineWrap&quot;&gt;
+ *     &lt;property name="tokens" value="IMPORT"&gt;
+ * &lt;/module&gt;
+ * </pre>
+ *
+ * Examples of not line-wrapped statements (good case):
+ * <pre><code> import com.puppycrawl.tools.checkstyle.api.Check;
+ * </code></pre>
+ *
+ * @author maxvetrenko
+ */
+public class NoLineWrapCheck extends Check
+{
+
+    @Override
+    public int[] getDefaultTokens()
+    {
+        return new int[] {TokenTypes.PACKAGE_DEF, TokenTypes.IMPORT};
+    }
+
+    @Override
+    public int[] getAcceptableTokens()
+    {
+        return new int[] {
+            TokenTypes.IMPORT,
+            TokenTypes.PACKAGE_DEF,
+            TokenTypes.CLASS_DEF,
+            TokenTypes.METHOD_DEF,
+            TokenTypes.CTOR_DEF,
+            TokenTypes.ENUM_DEF,
+            TokenTypes.INTERFACE_DEF,
+        };
+    }
+
+    @Override
+    public void visitToken(DetailAST aAST)
+    {
+        if (aAST.getLineNo() != aAST.getLastChild().getLineNo()) {
+            log(aAST.getLineNo(), "no.line.wrap", aAST.getText());
+        }
+    }
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/whitespace/NoLineWrapBadInput.java
@@ -0,0 +1,27 @@
+package com.puppycrawl.tools.
+    checkstyle;
+
+import com.google.common.annotations.Beta;
+
+import javax.accessibility.
+    AccessibleAttributeSequence;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+public class
+    BadLineWrapInput {
+    
+	public void
+	    fooMethod() {
+		final int
+		    foo = 0;
+	}
+}
+
+enum
+    FooFoo {
+}
+
+interface
+    InterFoo {}
+
+
