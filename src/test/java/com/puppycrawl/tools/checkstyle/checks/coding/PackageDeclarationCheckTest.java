ad6c05897a71caba2c00927a8b89a0d5bcf3f625
Ivan Sopov <sopov.ivan@gmail.com>
2014-07-29 11:33:49 +0400

    removing ignoreDirectoryName option from PackageDeclarationCheck
    This option duplicates compilation error and has no sense to be run
    on successfully compiled java sources.

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/PackageDeclarationCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/PackageDeclarationCheckTest.java
@@ -36,30 +36,4 @@ public class PackageDeclarationCheckTest extends BaseCheckTestSupport
 
         verify(checkConfig, getPath("coding" + File.separator + "InputNoPackage.java"), expected);
     }
-
-    @Test
-    public void testDefault1() throws Exception
-    {
-        DefaultConfiguration checkConfig = createCheckConfig(PackageDeclarationCheck.class);
-        final String dname = "com/puppycrawl/tools/checkstyle/checks/coding"
-            .replace('/', File.separatorChar);
-
-        String[] expected = {
-            "1:9: Package declaration does not match directory '" + dname + "'.",
-        };
-
-        verify(checkConfig, getPath("coding" + File.separator + "InputIllegalCatchCheck.java"), expected);
-    }
-
-    @Test
-    public void testQuiet() throws Exception
-    {
-        DefaultConfiguration checkConfig = createCheckConfig(PackageDeclarationCheck.class);
-        checkConfig.addAttribute("ignoreDirectoryName", "true");
-
-        String[] expected = {
-        };
-
-        verify(checkConfig, getPath("coding" + File.separator + "InputIllegalCatchCheck.java"), expected);
-    }
 }
