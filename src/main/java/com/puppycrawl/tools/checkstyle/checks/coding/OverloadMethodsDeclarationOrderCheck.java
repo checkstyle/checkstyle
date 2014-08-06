9f04dbe1ef21dfdee2abe1d3795a82e4443fb93a
Max <maxvetrenko2241@gmail.com>
2014-08-06 15:57:25 +0400

    OverrideMethodsDeclarationOrderCheck #32

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/OverloadMethodsDeclarationOrderCheck.java
@@ -0,0 +1,105 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2014  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * <p>
+ * Checks that overload methods are grouped together. Example:
+ * </p>
+ * <pre>
+ * <code>
+ * public void foo(int i) {}
+ * public void foo(String s) {}
+ * public void notFoo() {} // Have to be after foo(int i, String s)
+ * public void foo(int i, String s) {}
+ * </code>
+ * </pre>
+ * <p>
+ * An example of how to configure the check is:
+ * </p>
+ *
+ * <pre>
+ * &lt;module name="OverrideMethodsDeclaration"/&gt;
+ * </pre>
+ * @author maxvetrenko
+ */
+public class OverloadMethodsDeclarationOrderCheck extends Check
+{
+
+    @Override
+    public int[] getDefaultTokens()
+    {
+        return new int[] {
+            TokenTypes.OBJBLOCK,
+        };
+    }
+
+    @Override
+    public void visitToken(DetailAST aAST)
+    {
+        final int parentType = aAST.getParent().getType();
+        if (parentType == TokenTypes.CLASS_DEF
+                || parentType == TokenTypes.ENUM_DEF
+                || parentType == TokenTypes.INTERFACE_DEF
+                || parentType == TokenTypes.LITERAL_NEW)
+        {
+            checkOverloadMethodsGrouping(aAST);
+        }
+    }
+
+    /**
+     * Checks that if overload methods are grouped together they should not be
+     * separated from each other.
+     * @param aObjectBlock
+     *        is a class, interface or enum object block.
+     */
+    private void checkOverloadMethodsGrouping(DetailAST aObjectBlock)
+    {
+        final int allowedDistance = 1;
+        DetailAST currentToken = aObjectBlock.getFirstChild();
+        final Map<String, Integer> methodIndexMap = new HashMap<String, Integer>();
+        final Map<String, Integer> methodLineNumberMap = new HashMap<String, Integer>();
+        int currentIndex = 0;
+        while (currentToken != null) {
+            if (currentToken.getType() == TokenTypes.METHOD_DEF) {
+                currentIndex++;
+                final String methodName =
+                        currentToken.findFirstToken(TokenTypes.IDENT).getText();
+                if (methodIndexMap.containsKey(methodName)) {
+                    final int priviousIndex = methodIndexMap.get(methodName);
+                    if (currentIndex - priviousIndex > allowedDistance) {
+                        int previousLineWithOverloadMethod = methodLineNumberMap.get(methodName);
+                        log(currentToken.getLineNo(), "overload.methods.declaration",
+                                previousLineWithOverloadMethod);
+                    }
+                }
+                methodIndexMap.put(methodName, currentIndex);
+                methodLineNumberMap.put(methodName, currentToken.getLineNo());
+            }
+            currentToken = currentToken.getNextSibling();
+        }
+    }
+}
--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/MemberNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/MemberNameCheck.java
@@ -76,5 +76,4 @@ public class MemberNameCheck
             && !ScopeUtils.isLocalVariableDef(aAST))
             && shouldCheckInScope(modifiersAST);
     }
-
 }
