b253c1167bed538e4ec5b9d9172da3b30651f262
Max <maxvetrenko2241@gmail.com>
2014-08-11 14:22:37 +0400

    Fix false positive with ANNOTATION_DEF #237

--- src/test/resources/com/puppycrawl/tools/checkstyle/whitespace/InputEmptyTypesAndCycles.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/whitespace/InputEmptyTypesAndCycles.java
@@ -30,3 +30,15 @@ class WithEmptyAnonymous
 		MyClass c = new MyClass() {};
 	}
 }
+
+
+@Retention(RetentionPolicy.CLASS)
+@Target({
+    ElementType.ANNOTATION_TYPE,
+    ElementType.CONSTRUCTOR,
+    ElementType.FIELD,
+    ElementType.METHOD,
+    ElementType.TYPE})
+@Documented
+@GwtCompatible
+public @interface Beta {}
