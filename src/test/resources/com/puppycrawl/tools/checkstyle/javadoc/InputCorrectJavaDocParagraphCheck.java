d9c4e779fbb7f9ab0c5c1d40ecffb001a817dbe1
maxvetrenko <maxvetrenko2241@gmail.com>
2014-10-21 01:20:31 +0400

    JavaDocParagraph #315

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/javadoc/InputCorrectJavaDocParagraphCheck.java
@@ -0,0 +1,85 @@
+package com.puppycrawl.tools.checkstyle.javadoc;
+
+/**
+ * Some Javadoc.
+ *
+ * <p>Some Javadoc.
+ *
+ */
+class InputCorrectJavaDocParagraphCheck {
+
+    /**
+     * Some Javadoc.
+     *
+     * <p>Some Javadoc.
+     *
+     * @since 8.0
+     */
+    public static final byte NUL = 0;
+       
+    /**
+     * Some Javadoc.
+     *
+     * <p>Some Javadoc.
+     *
+     * @see <a href="http://code.google.com/webtoolkit/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
+     *     Documentation about GWT emulated source</a>
+     */
+    boolean emulated() {return false;}
+    
+    /**
+     * Some Javadoc.
+     *
+     * <p>Some Javadoc.
+     *
+     */
+     class InnerInputCorrectJavaDocParagraphCheck {
+
+        /**
+         * Some Javadoc.
+         *
+         * <p>Some Javadoc.
+         *
+         * <p>Some Javadoc.
+         *
+         * @since 8.0
+         */
+        public static final byte NUL = 0;
+           
+        /**
+         * Some Javadoc.
+         *
+         * <p>Some Javadoc.
+         *
+         * @see <a href="http://code.google.com/webtoolkit/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
+         *     Documentation about GWT emulated source</a>
+         */
+        boolean emulated() {return false;}
+    }
+    
+    InnerInputCorrectJavaDocParagraphCheck anon = new InnerInputCorrectJavaDocParagraphCheck() {
+
+    	/**
+         * Some Javadoc.
+         *
+         * <p>Some Javadoc.
+         *
+         * <p>Some Javadoc.
+         *
+         * @since 8.0
+         */
+        public static final byte NUL = 0;
+           
+        /**
+         * Some Javadoc.
+         *
+         * <p>Some Javadoc.
+         *
+         * <p>Some Javadoc.
+         *
+         * @see <a href="http://code.google.com/webtoolkit/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
+         *     Documentation about GWT emulated source</a>
+         */
+        boolean emulated() {return false;}
+    };
+}
