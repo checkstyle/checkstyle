791be6ce008b1a9d146bf5f1b29ad31ce2cbdd7b
Roman Ivanov <ivanov-jr@mail.ru>
2014-11-12 21:44:33 -0800

    extenting of javadoc in PropertyChaceFile

--- src/main/java/com/puppycrawl/tools/checkstyle/PropertyCacheFile.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/PropertyCacheFile.java
@@ -34,8 +34,11 @@ import com.puppycrawl.tools.checkstyle.api.Configuration;
 import com.puppycrawl.tools.checkstyle.api.Utils;
 
 /**
- * This class maintains a persistent store of the files that have
- * checked ok and their associated timestamp. It uses a property file
+ * This class maintains a persistent(on file-system) store of the files
+ * that have checked ok(no validation events) and their associated 
+ * timestamp. It is used to optimize Checkstyle between few launches.
+ * It is mostly useful for plugin and extensions of Checkstyle.  
+ * It uses a property file
  * for storage.  A hashcode of the Configuration is stored in the
  * cache file to ensure the cache is invalidated when the
  * configuration has changed.
