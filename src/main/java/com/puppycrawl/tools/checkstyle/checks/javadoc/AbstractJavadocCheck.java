94a2d77377e8f3664bbf439adbe651592bfba530
Baratali Izmailov <barataliba@gmail.com>
2014-11-24 21:12:48 +0300

    Create ParseErrorMessage if error message is null. Added comments for a few methods.

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/AbstractJavadocCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/AbstractJavadocCheck.java
@@ -55,6 +55,17 @@ import com.puppycrawl.tools.checkstyle.grammars.javadoc.JavadocParser;
 public abstract class AbstractJavadocCheck extends Check
 {
     /**
+     * Error message key for common javadoc errors.
+     */
+    private static final String PARSE_ERROR_MESSAGE_KEY = "javadoc.parse.error";
+
+    /**
+     * Unrecognized error from antlr parser
+     */
+    private static final String UNRECOGNIZED_ANTLR_ERROR_MESSAGE_KEY =
+            "javadoc.unrecognized.antlr.error";
+
+    /**
      * key is "line:column"
      * value is DetailNode tree
      */
@@ -115,24 +126,40 @@ public abstract class AbstractJavadocCheck extends Check
     {
     }
 
+    /**
+     * Defined final to not allow JavadocChecks to change default tokens.
+     * @return default tokens
+     */
     @Override
     public final int[] getDefaultTokens()
     {
         return new int[] {TokenTypes.BLOCK_COMMENT_BEGIN };
     }
 
+    /**
+     * Defined final to not allow JavadocChecks to change acceptable tokens.
+     * @return acceptable tokens
+     */
     @Override
     public final int[] getAcceptableTokens()
     {
         return super.getAcceptableTokens();
     }
 
+    /**
+     * Defined final to not allow JavadocChecks to change required tokens.
+     * @return required tokens
+     */
     @Override
     public final int[] getRequiredTokens()
     {
         return super.getRequiredTokens();
     }
 
+    /**
+     * Defined final because all JavadocChecks require comment nodes.
+     * @return true
+     */
     @Override
     public final boolean isCommentNodesRequired()
     {
@@ -142,6 +169,7 @@ public abstract class AbstractJavadocCheck extends Check
     @Override
     public final void beginTree(DetailAST aRootAST)
     {
+        TREE_CACHE.clear();
     }
 
     @Override
@@ -217,7 +245,7 @@ public abstract class AbstractJavadocCheck extends Check
         catch (IOException e) {
             // Antlr can not initiate its ANTLRInputStream
             parseErrorMessage = new ParseErrorMessage(aJavadocCommentAst.getLineNo(),
-                    "javadoc.parse.error",
+                    PARSE_ERROR_MESSAGE_KEY,
                     aJavadocCommentAst.getColumnNo(), e.getMessage());
         }
         catch (ParseCancellationException e) {
@@ -225,6 +253,13 @@ public abstract class AbstractJavadocCheck extends Check
             // and parser throws this runtime exception to stop parsing.
             // Just stop processing current Javadoc comment.
             parseErrorMessage = mErrorListener.getErrorMessage();
+
+            // There are cases when antlr error listener does not handle syntax error
+            if (parseErrorMessage == null) {
+                parseErrorMessage = new ParseErrorMessage(aJavadocCommentAst.getLineNo(),
+                        UNRECOGNIZED_ANTLR_ERROR_MESSAGE_KEY,
+                        aJavadocCommentAst.getColumnNo(), e.getMessage());
+            }
         }
 
         if (parseErrorMessage == null) {
