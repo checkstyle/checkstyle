05eeceb09dc67d5e30edd22384af1ddb0d0fc4b9
alexkravin <nesterenko-aleksey@list.ru>
2014-12-02 19:28:05 +0400

    Annotation Location Check compilable UT inputs

--- src/test/resources/com/puppycrawl/tools/checkstyle/annotation/InputIncorrectAnnotationLocation.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/annotation/InputIncorrectAnnotationLocation.java
@@ -1,98 +1,106 @@
 package com.puppycrawl.tools.checkstyle.annotation;
 
-import test.MyAnnotation1;
-import test.MyAnnotation2;
+
+
 
 @MyAnnotation2 @MyAnnotation1 //warn
-class InputCorrectAnnotationIndentation
+(value = "")
+class InputIncorrectAnnotationLocation
 {
 
-    @MyAnnotation2 @MyAnnotation1
+    @MyAnnotation2 @MyAnnotation1(value = "")
     public int a;
 
-    @MyAnnotation1 public int b;
+    @MyAnnotation1(value = "") public int b;
 
     @MyAnnotation2
         @MyAnnotation1 //warn
+(value = "")
     public int c;
 
-    @MyAnnotation1
+    @MyAnnotation1(value = "")
     public int d;
 
     @MyAnnotation2
         @MyAnnotation1 //warn
-    public InputCorrectAnnotationIndentation() {}
+(value = "")
+    public InputIncorrectAnnotationLocation() {}
 
     @MyAnnotation1("foo") @MyAnnotation2 void foo1() {} //warn
 
-    @MyAnnotation1
+    @MyAnnotation1(value = "")
        @MyAnnotation2 //warn
     void foo2() {}
 
-    @MyAnnotation1
+    @MyAnnotation1(value = "")
         @MyAnnotation2 //warn
       @MyAnnotation3 //warn
           @MyAnnotation4 //warn
     class InnerClass
     {
         @MyAnnotation2 @MyAnnotation1 //warn
+(value = "")
         public int a;
 
-        @MyAnnotation1 public int b;
+        @MyAnnotation1(value = "") public int b;
         
         @MyAnnotation2
             @MyAnnotation1 //warn
+(value = "")
         public int c;
         
-        @MyAnnotation1
+        @MyAnnotation1(value = "")
         public int d;
         
         @MyAnnotation2
-        @MyAnnotation1 public InnerClass()
+        @MyAnnotation1(value = "") public InnerClass()
         {
             // TODO Auto-generated constructor stub
         }
-        @MyAnnotation1
+        @MyAnnotation1(value = "")
             @MyAnnotation2 //warn
         void foo1() {}
         
-        @MyAnnotation1
+        @MyAnnotation1(value = "")
             @MyAnnotation2 //warn
         void foo2() {}
     }
 
-    @MyAnnotation1
+    @MyAnnotation1(value = "")
        @MyAnnotation2 //warn
     InnerClass anon = new InnerClass()
     {
-        @MyAnnotation2 @MyAnnotation1 public int a;
+        @MyAnnotation2 @MyAnnotation1(value = "") public int a;
 
-        @MyAnnotation1 public int b;
+        @MyAnnotation1(value = "") public int b;
         
         @MyAnnotation2
-        @MyAnnotation1
+        @MyAnnotation1(value = "")
         public int c;
         
-        @MyAnnotation1
+        @MyAnnotation1(value = "")
         public int d;
         
-        @MyAnnotation1
+        @MyAnnotation1(value = "")
            @MyAnnotation2 void foo1() {} //warn
         
-        @MyAnnotation1
+        @MyAnnotation1(value = "")
           @MyAnnotation2 //warn
         void foo2() {}
         
-        @MyAnnotation1 void foo42() {}
+        @MyAnnotation1(value = "") void foo42() {}
     };
     
 }
 
    @MyAnnotation1 //warn
+(value = "")
 @MyAnnotation2
 class Foo {}
 
-@interface MyAnnotation1 {}
+@interface MyAnnotation1 {
+
+	String value();}
 
 @interface MyAnnotation2 {}
 
