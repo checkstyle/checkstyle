57fc8f1ced5eea0a0fa44ee75ee531582e833c47
alexkravin <nesterenko-aleksey@list.ru>
2014-12-09 15:05:40 +0400

    Semicolon Between Imports input moved to resources-noncompilable, created UT for this case as there wasn't one #388

--- src/test/java/com/puppycrawl/tools/checkstyle/grammars/GeneratedJava14LexerTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammars/GeneratedJava14LexerTest.java
@@ -3,7 +3,10 @@ package com.puppycrawl.tools.checkstyle.grammars;
 import com.puppycrawl.tools.checkstyle.BaseCheckTestSupport;
 import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
 import com.puppycrawl.tools.checkstyle.checks.naming.MemberNameCheck;
+
+import java.io.File;
 import java.io.IOException;
+
 import org.junit.Test;
 
 /**
@@ -22,5 +25,17 @@ public class GeneratedJava14LexerTest
             "8:10: Got an exception - Unexpected character 0xa9 in identifier",
         };
         verify(checkConfig, getPath("grammars/InputGrammar.java"), expected);
+    }
+    
+    @Test
+    public void testSemicolonBetweenImports() throws IOException, Exception
+    {
+        final DefaultConfiguration checkConfig =
+            createCheckConfig(MemberNameCheck.class);
+        final String[] expected = {
+
+        };
+        verify(checkConfig, new File("src/test/resources-noncompilable/com/puppycrawl/tools/"
+                + "checkstyle/grammars/SemicolonBetweenImports.java").getCanonicalPath(), expected);
     }
 }
