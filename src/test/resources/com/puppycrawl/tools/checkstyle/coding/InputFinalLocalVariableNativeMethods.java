0ee4ba78e2922d45ff0e64b8b2c807f76fa1513a
alexkravin <nesterenko-aleksey@list.ru>
2014-12-15 14:16:25 +0400

    Final Local Variable Check, fixed false-positive - native method's param should be declared final, issue #158

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/coding/InputFinalLocalVariableNativeMethods.java
@@ -0,0 +1,23 @@
+package com.puppycrawl.tools.checkstyle.coding;
+
+public class InputFinalLocalVariableNativeMethods
+{
+    public native String nativeFoo(int a, int b);    
+    private native double average(int n1, int n2);
+
+     static {
+         System.loadLibrary("foo");
+     }        
+
+     public void print () {
+         String str = nativeFoo(1, 4);
+         System.out.println(str);
+     }
+
+     public static void main(final String[] args) {
+         (new InputFinalLocalVariableNativeMethods()).print();
+         System.out.println("In Java, the average is " +
+             new InputFinalLocalVariableNativeMethods().average(3, 2));
+         return;
+     }
+}
