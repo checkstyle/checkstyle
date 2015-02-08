a4c5c652f6f82eeb5ad0709e57f2306ea0d3b815
Roman Ivanov <ivanov-jr@mail.ru>
2015-02-08 14:06:51 -0800

    extra method getAcceptableTokens was removed

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/EmptyBlockCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/EmptyBlockCheck.java
@@ -113,29 +113,11 @@ public class EmptyBlockCheck
             TokenTypes.INSTANCE_INIT,
             TokenTypes.STATIC_INIT,
             TokenTypes.LITERAL_SWITCH,
-            //TODO: does this handle TokenTypes.LITERAL_SYNCHRONIZED?
-        };
-    }
-
-    @Override
-    public int[] getAcceptableTokens()
-    {
-        return new int[] {
-            TokenTypes.LITERAL_WHILE,
-            TokenTypes.LITERAL_TRY,
-            TokenTypes.LITERAL_CATCH,
-            TokenTypes.LITERAL_FINALLY,
-            TokenTypes.LITERAL_DO,
-            TokenTypes.LITERAL_IF,
-            TokenTypes.LITERAL_ELSE,
-            TokenTypes.LITERAL_FOR,
-            TokenTypes.INSTANCE_INIT,
-            TokenTypes.STATIC_INIT,
-            TokenTypes.LITERAL_SWITCH,
             TokenTypes.LITERAL_CASE,
             TokenTypes.LITERAL_SWITCH,
             TokenTypes.LITERAL_DEFAULT,
             TokenTypes.ARRAY_INIT,
+            //TODO: does this handle TokenTypes.LITERAL_SYNCHRONIZED?
         };
     }
 
