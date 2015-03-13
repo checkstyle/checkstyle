da775081650e4a34d4268541437247ca8f097555
alexkravin <nesterenko-aleksey@list.ru>
2015-03-14 00:53:41 +0400

    Added anchors to sections, issue #586

---
+++ src/site/resources/js/anchors.js
@@ -0,0 +1,23 @@
+'use strict';
+
+window.addEventListener('load', main);
+
+function main() {
+    var url = window.location.href;
+    var anchors = document.getElementsByTagName('h2');
+    var mass = [];
+    for(var i = 0; i < anchors.length; i++) {
+        mass.push(anchors[i].childNodes[0].name);
+    }
+    var links = [];
+    for(i = 0; i < mass.length; i++) {
+        links.push('' + url + '#' + mass[i] + '');
+    }
+    for(i = 0; i < anchors.length; i++) {
+        var anchor = document.createElement('div');
+        anchor.className = 'anchor';
+        anchor.innerHTML += '<a href="' + links[i] + '"><img src="images/anchor.png"></a>';
+        anchors[i].appendChild(anchor);
+    }
+
+}
