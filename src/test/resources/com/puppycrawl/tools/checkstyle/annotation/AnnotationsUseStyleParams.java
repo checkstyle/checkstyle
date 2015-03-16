d44cb066cbd89ce3c8d99c04d2fd28dbae68a6d9
alexkravin <nesterenko-aleksey@list.ru>
2015-03-16 15:00:24 +0400

    Annotation Use Style Check, issue #28

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/annotation/AnnotationsUseStyleParams.java
@@ -0,0 +1,12 @@
+package com.puppycrawl.tools.checkstyle.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+public class AnnotationsUseStyleParams
+{
+    @Target({})
+    public @interface myAnn {
+        
+    }
+}
