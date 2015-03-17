102961a64380b20ccab85822bda5ae7f0b4c8d0b
liscju <piotr.listkiewicz@gmail.com>
2015-03-17 14:57:28 +0100

    Added new DemandTypesInLambdaCheck, fixes issue #142

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/lambda/InputOneGenericParameterLambda.java
@@ -0,0 +1,13 @@
+//Compilable with Java8
+package com.puppycrawl.tools.checkstyle.checks.lambda;
+import java.util.List;
+import java.util.Arrays;
+
+public class InputOneGenericParameterLambda {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        list.forEach(n -> System.out.println(n));
+
+        list.stream().map((x) -> x*x)
+    }
+}
