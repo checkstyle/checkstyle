64edbdc37a897fe41e588de4fa34272b85eb370f
Roman Ivanov <ivanov-jr@mail.ru>
2015-04-13 17:09:12 -0700

    fix for findbugs violation  SIC_INNER_SHOULD_BE_STATIC_NEEDS_THIS. Issue #912

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocMethodCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocMethodCheck.java
@@ -725,8 +725,8 @@ public class JavadocMethodCheck extends AbstractTypeAwareCheck
                         || child.getType() == TokenTypes.DOT)
                 {
                     final FullIdent fi = FullIdent.createFullIdent(child);
-                    final ExceptionInfo ei = new ExceptionInfo(new Token(fi),
-                            getCurrentClassName());
+                    final ExceptionInfo ei = new ExceptionInfo(createClassInfo(new Token(fi),
+                            getCurrentClassName()));
                     retVal.add(ei);
                 }
                 child = child.getNextSibling();
@@ -1076,7 +1076,7 @@ public class JavadocMethodCheck extends AbstractTypeAwareCheck
     }
 
     /** Stores useful information about declared exception. */
-    private class ExceptionInfo
+    private static class ExceptionInfo
     {
         /** does the exception have throws tag associated with. */
         private boolean found;
@@ -1086,12 +1086,11 @@ public class JavadocMethodCheck extends AbstractTypeAwareCheck
         /**
          * Creates new instance for <code>FullIdent</code>.
          *
-         * @param ident the exception
-         * @param currentClass name of current class.
+         * @param classInfo clas info
          */
-        ExceptionInfo(Token ident, String currentClass)
+        ExceptionInfo(ClassInfo classInfo)
         {
-            classInfo = createClassInfo(ident, currentClass);
+            this.classInfo = classInfo;
         }
 
         /** Mark that the exception has associated throws tag */
