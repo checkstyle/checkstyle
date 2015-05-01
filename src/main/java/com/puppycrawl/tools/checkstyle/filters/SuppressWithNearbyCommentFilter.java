41f55782590d7fcfbf91fdec517c1e7a2685e233
Michal Kordas <kontakt@michalkordas.com>
2015-05-01 16:39:32 +0200

    Fix inner class should be static FindBugs violations, issue #928
    All violations of [SIC_INNER_SHOULD_BE_STATIC_NEEDS_THIS](http://findbugs.sourceforge.net/bugDescriptions.html#SIC_INNER_SHOULD_BE_STATIC_NEEDS_THIS) are fixed.

--- src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressWithNearbyCommentFilter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressWithNearbyCommentFilter.java
@@ -79,7 +79,7 @@ public class SuppressWithNearbyCommentFilter
     /**
      * A Tag holds a suppression comment and its location.
      */
-    public class Tag implements Comparable<Tag>
+    public static class Tag implements Comparable<Tag>
     {
         /** The text of the tag. */
         private final String text;
@@ -100,30 +100,31 @@ public class SuppressWithNearbyCommentFilter
          * Constructs a tag.
          * @param text the text of the suppression.
          * @param line the line number.
+         * @param filter the {@code SuppressWithNearbyCommentFilter} with the context
          * @throws ConversionException if unable to parse expanded text.
          * on.
          */
-        public Tag(String text, int line)
+        public Tag(String text, int line, SuppressWithNearbyCommentFilter filter)
             throws ConversionException
         {
             this.text = text;
 
-            tagCheckRegexp = checkRegexp;
+            tagCheckRegexp = filter.checkRegexp;
             //Expand regexp for check and message
             //Does not intern Patterns with Utils.getPattern()
             String format = "";
             try {
-                format = expandFrocomment(text, checkFormat, commentRegexp);
+                format = expandFrocomment(text, filter.checkFormat, filter.commentRegexp);
                 tagCheckRegexp = Pattern.compile(format);
-                if (messageFormat != null) {
+                if (filter.messageFormat != null) {
                     format = expandFrocomment(
-                         text, messageFormat, commentRegexp);
+                         text, filter.messageFormat, filter.commentRegexp);
                     tagMessageRegexp = Pattern.compile(format);
                 }
                 int influence = 0;
-                if (influenceFormat != null) {
+                if (filter.influenceFormat != null) {
                     format = expandFrocomment(
-                        text, influenceFormat, commentRegexp);
+                        text, filter.influenceFormat, filter.commentRegexp);
                     try {
                         if (format.startsWith("+")) {
                             format = format.substring(1);
@@ -133,7 +134,7 @@ public class SuppressWithNearbyCommentFilter
                     catch (final NumberFormatException e) {
                         throw new ConversionException(
                             "unable to parse influence from '" + text
-                                + "' using " + influenceFormat, e);
+                                + "' using " + filter.influenceFormat, e);
                     }
                 }
                 if (influence >= 0) {
@@ -482,7 +483,7 @@ public class SuppressWithNearbyCommentFilter
      */
     private void addTag(String text, int line)
     {
-        final Tag tag = new Tag(text, line);
+        final Tag tag = new Tag(text, line, this);
         tags.add(tag);
     }
 }
--- src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionCommentFilter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionCommentFilter.java
@@ -67,7 +67,7 @@ public class SuppressionCommentFilter
      * whether the supression turns checkstyle reporting on or off.
      * @author Rick Giles
      */
-    public class Tag
+    public static class Tag
         implements Comparable<Tag>
     {
         /** The text of the tag. */
@@ -94,10 +94,11 @@ public class SuppressionCommentFilter
          * @param column the column number.
          * @param text the text of the suppression.
          * @param on <code>true</code> if the tag turns checkstyle reporting.
+         * @param filter the {@code SuppressionCommentFilter} with the context
          * @throws ConversionException if unable to parse expanded text.
          * on.
          */
-        public Tag(int line, int column, String text, boolean on)
+        public Tag(int line, int column, String text, boolean on, SuppressionCommentFilter filter)
             throws ConversionException
         {
             this.line = line;
@@ -105,31 +106,31 @@ public class SuppressionCommentFilter
             this.text = text;
             this.on = on;
 
-            tagCheckRegexp = checkRegexp;
+            tagCheckRegexp = filter.checkRegexp;
             //Expand regexp for check and message
             //Does not intern Patterns with Utils.getPattern()
             String format = "";
             try {
                 if (on) {
                     format =
-                        expandFromCoont(text, checkFormat, onRegexp);
+                        expandFromCoont(text, filter.checkFormat, filter.onRegexp);
                     tagCheckRegexp = Pattern.compile(format);
-                    if (messageFormat != null) {
+                    if (filter.messageFormat != null) {
                         format =
-                            expandFromCoont(text, messageFormat, onRegexp);
+                            expandFromCoont(text, filter.messageFormat, filter.onRegexp);
                         tagMessageRegexp = Pattern.compile(format);
                     }
                 }
                 else {
                     format =
-                        expandFromCoont(text, checkFormat, offRegexp);
+                        expandFromCoont(text, filter.checkFormat, filter.offRegexp);
                     tagCheckRegexp = Pattern.compile(format);
-                    if (messageFormat != null) {
+                    if (filter.messageFormat != null) {
                         format =
                             expandFromCoont(
                                 text,
-                                messageFormat,
-                                offRegexp);
+                                filter.messageFormat,
+                                filter.offRegexp);
                         tagMessageRegexp = Pattern.compile(format);
                     }
                 }
@@ -501,7 +502,7 @@ public class SuppressionCommentFilter
      */
     private void addTag(String text, int line, int column, boolean on)
     {
-        final Tag tag = new Tag(line, column, text, on);
+        final Tag tag = new Tag(line, column, text, on, this);
         tags.add(tag);
     }
 }
