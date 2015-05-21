9dc87c52e507f9ac32ca8e3c85923b616a77c80b
Michal Kordas <kontakt@michalkordas.com>
2015-05-21 23:22:20 +0200

    Add docs for list of allowed tokens in MagicNumber check. #1113

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/MagicNumberCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/MagicNumberCheck.java
@@ -35,6 +35,17 @@ import com.puppycrawl.tools.checkstyle.checks.CheckUtils;
  * By default, -1, 0, 1, and 2 are not considered to be magic numbers.
  * </p>
  * <p>
+ * It is fine to have one constant defining multiple numeric literals within one expression:
+ * <pre>
+ * <code>static final int SECONDS_PER_DAY = 24 * 60 * 60;
+ * static final double SPECIAL_RATIO = 4.0 / 3.0;
+ * static final double SPECIAL_SUM = 1 + Math.E;
+ * static final double SPECIAL_DIFFERENCE = 4 - Math.PI;
+ * static final Border STANDARD_BORDER = BorderFactory.createEmptyBorder(3, 3, 3, 3);
+ * static final Integer ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE = new Integer(42);</code>
+ * </pre>
+ * </p>
+ * <p>
  * Check have following options:
  * ignoreHashCodeMethod - ignore magic numbers in hashCode methods;
  * ignoreAnnotation - ignore magic numbers in annotation declarations;
