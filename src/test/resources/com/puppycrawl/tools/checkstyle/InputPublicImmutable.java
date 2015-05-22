058eb9990a526674c2e28beefc1c035eb7d98001
vlad <vladlis54@gmail.com>
2015-05-22 11:53:30 +0300

    100% test coverage for VisibilityModifierCheck, #1009

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/InputPublicImmutable.java
@@ -0,0 +1,23 @@
+package com.puppycrawl.tools.checkstyle;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.ImmutableSet;
+
+public class InputPublicImmutable {    
+    public final int someIntValue;
+    public final ImmutableSet<String> includes;
+    public final java.lang.String notes;
+    public final BigDecimal value;
+    public final List list;
+    public InputPublicImmutable(Collection<String> includes,
+            BigDecimal value, String notes, int someValue, List l) {
+        this.includes = ImmutableSet.copyOf(includes);
+        this.value = value;
+        this.notes = notes;
+        this.someIntValue = someValue;
+        this.list = l;
+    }
+}
