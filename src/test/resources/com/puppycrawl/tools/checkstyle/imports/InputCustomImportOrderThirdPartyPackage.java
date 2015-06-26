10f70fe1aeea9b2c6985caeb659cc858e08d00e3
Aleksandr Ivanov <ivanov-alex@yandex.ru>
2015-06-26 08:24:14 +0300

    Improved UT coverage for CustomImportOrder #1128

--- src/test/resources/com/puppycrawl/tools/checkstyle/imports/InputCustomImportOrderThirdPartyPackage.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/imports/InputCustomImportOrderThirdPartyPackage.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 
 import static sun.tools.util.CommandLine.parse;
 import static sun.tools.util.ModifierFilter.ALL_ACCESS;
+import static sun.tools.util.ModifierFilter.ALL_ACCESS;
 
 public class InputCustomImportOrderThirdPartyPackage
 {
