93c68168f3b6ef88cfdcd31e059fd22dda7eaeb9
Bhavik Patel <bhavikpatel.aha@gmail.com>
2015-07-04 10:48:17 +0530

    Change default value of property "format" to "^Abstract.*$" in AbstractClassNameCheck #1279

--- src/test/resources/com/puppycrawl/tools/checkstyle/naming/InputAbstractClassName.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/naming/InputAbstractClassName.java
@@ -2,11 +2,8 @@ package com.puppycrawl.tools.checkstyle.naming;
 
 abstract public class InputAbstractClassName {
 }
-// abstract --> ^Abstract.*$|^.*Factory$
+// abstract --> ^Abstract.*$
 abstract class NonAbstractClassName {
-}
-
-abstract class FactoryWithBadName {
 }
 
 abstract class AbstractClassName {
@@ -14,27 +11,14 @@ abstract class AbstractClassName {
     }
 }
 
-abstract class ClassFactory {
-    abstract class WellNamedFactory {
-    }
-}
-
 class NonAbstractClass {
 }
 
-//^Abstract.*$|^.*Factory$ --> abstract
+//^Abstract.*$ --> abstract
 class AbstractClass {
-}
-
-class Class1Factory {
 }
 
 abstract class AbstractClassName2 {
     class AbstractInnerClass {
-    }
-}
-
-abstract class Class2Factory {
-    class WellNamedFactory {
     }
 }
