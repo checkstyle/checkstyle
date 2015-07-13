12a57d940dda3aa2f51494630e4d1b492c7af791
Roman Ivanov <ivanov-jr@mail.ru>
2015-07-12 21:25:25 -0700

    UTs coverage for SuppressionFilter is 100%. #1296

--- src/test/java/com/puppycrawl/tools/checkstyle/filters/SuppressionFilterTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/filters/SuppressionFilterTest.java
@@ -19,8 +19,11 @@
 
 package com.puppycrawl.tools.checkstyle.filters;
 
+import com.puppycrawl.tools.checkstyle.api.AuditEvent;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SuppressionFilterTest {
@@ -32,4 +35,15 @@ public class SuppressionFilterTest {
                 .suppress(Warning.NONFINAL_FIELDS)
                 .verify();
     }
+
+    @Test
+    public void testAccept() throws CheckstyleException {
+        SuppressionFilter filter = new SuppressionFilter();
+        filter.setFile("src/test/resources/com/puppycrawl/tools/checkstyle/suppressions_none.xml");
+
+        final AuditEvent ev = new AuditEvent(this, "ATest.java", null);
+
+        Assert.assertTrue(filter.accept(ev));
+    }
+
 }
