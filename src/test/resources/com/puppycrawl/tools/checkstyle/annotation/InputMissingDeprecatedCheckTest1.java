e1d06c0a92d22352051d90b1e30271578ebc6b84
Ilja Dubinin <iliadubinin91@gmail.com>
2015-07-21 05:41:43 +0100

    Coverage has been increased to 100% in MissingDeprecatedCheck. Issue #908

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/annotation/InputMissingDeprecatedCheckTest1.java
@@ -0,0 +1,15 @@
+package com.puppycrawl.tools.checkstyle.annotation;
+
+import java.lang.annotation.Inherited;
+/**
+ * 
+ * @author idubinin
+ *@deprecated
+ *@deprecated
+ *stuff
+ *stuff
+ */
+public class InputMissingDeprecatedCheckTest1
+{
+    
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/annotation/InputMissingDeprecatedCheckTest2.java
@@ -0,0 +1,14 @@
+package com.puppycrawl.tools.checkstyle.checks.annotation;
+
+public class InputMissingDeprecatedCheckTest2 {
+
+
+    /**
+     * @deprecated
+     * 
+     * @param comment
+     */
+    public void method(){
+    }
+
+}
