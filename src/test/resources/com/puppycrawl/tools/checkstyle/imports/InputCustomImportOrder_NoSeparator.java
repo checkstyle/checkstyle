ed1b18f03e8d8e351f9e9a37b4d088d53e44482b
Aleksandr Ivanov <ivanov-alex@yandex.ru>
2015-07-25 18:45:22 +0300

    100% coverage for CustomImportOrder. #1128

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/imports/InputCustomImportOrder_NoSeparator.java
@@ -0,0 +1,12 @@
+package java.util.concurrent;
+import static java.awt.Button.ABORT;
+import static javax.swing.WindowConstants.*;
+import static java.io.File.createTempFile;
+import java.util.*;
+import java.util.StringTokenizer;
+import com.puppycrawl.tools.*;
+import com.*;
+import org.apache.*;
+
+public class InputCustomImportOrder_NoSeparator {
+}
