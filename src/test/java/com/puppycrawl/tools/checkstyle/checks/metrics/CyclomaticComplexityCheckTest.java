c5d098033865c57c842e7704f77b915de04984ec
Ruslan Diachenko <rd.ryly@gmail.com>
2015-07-26 02:42:01 +0100

    Issue #1287: 'NPathComplexityCheck', 'CyclomaticComplexityCheck', 'AbstractComplexityCheck' refactored, UT coverage improved

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/metrics/CyclomaticComplexityCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/metrics/CyclomaticComplexityCheckTest.java
@@ -21,10 +21,12 @@ package com.puppycrawl.tools.checkstyle.checks.metrics;
 
 import static com.puppycrawl.tools.checkstyle.checks.metrics.CyclomaticComplexityCheck.MSG_KEY;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.puppycrawl.tools.checkstyle.BaseCheckTestSupport;
 import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 public class CyclomaticComplexityCheckTest
     extends BaseCheckTestSupport {
@@ -49,5 +51,28 @@ public class CyclomaticComplexityCheckTest
         };
 
         verify(checkConfig, getPath("ComplexityCheckTestInput.java"), expected);
+    }
+
+    @Test
+    public void testGetAcceptableTokens() {
+        CyclomaticComplexityCheck cyclomaticComplexityCheckObj = new CyclomaticComplexityCheck();
+        int[] actual = cyclomaticComplexityCheckObj.getAcceptableTokens();
+        int[] expected = new int[] {
+            TokenTypes.CTOR_DEF,
+            TokenTypes.METHOD_DEF,
+            TokenTypes.INSTANCE_INIT,
+            TokenTypes.STATIC_INIT,
+            TokenTypes.LITERAL_WHILE,
+            TokenTypes.LITERAL_DO,
+            TokenTypes.LITERAL_FOR,
+            TokenTypes.LITERAL_IF,
+            TokenTypes.LITERAL_CASE,
+            TokenTypes.LITERAL_CATCH,
+            TokenTypes.QUESTION,
+            TokenTypes.LAND,
+            TokenTypes.LOR,
+        };
+        Assert.assertNotNull(actual);
+        Assert.assertArrayEquals(expected, actual);
     }
 }
