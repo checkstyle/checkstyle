16ae5032c5e25ad35475c9a0f2de578ea6e640e5
Baratali Izmailov <barataliba@gmail.com>
2015-08-02 13:09:01 -0700

    Issue #1293: Improved coverage for IllegalInstantiationCheck

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/coding/InputIllegalInstantiationCheckLang.java
@@ -0,0 +1,12 @@
+package java.lang;
+
+class Input {
+    Boolean obj = new Boolean();
+    Integer obj2 = new Integer();
+}
+
+class Boolean{}
+
+class Input2 {
+	String a = new String();
+}
---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/coding/InputIllegalInstantiationCheckNoPackage.java
@@ -0,0 +1,5 @@
+//no package
+class Input {
+    Boolean obj = new Boolean();
+    Integer obj = new Integer();
+}
