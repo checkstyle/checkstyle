8f4c4d832b236b38daf5a2ba1d955f3d074ff780
Ilja Dubinin <iliadubinin91@gmail.com>
2015-08-02 23:31:01 +0100

    100% coverage in OuterType check. Issue #1290

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/InputOuterTypeFilenameCheck1.java
@@ -0,0 +1,9 @@
+// someexamples of 1.5 extensions
+package com.puppycrawl.tools.checkstyle;
+
+public class InputOuterTypeFilenameCheck1 {
+    
+    public class UnicClassName {
+        
+    }
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/InputOuterTypeFilenameCheck2.java
@@ -0,0 +1,7 @@
+// someexamples of 1.5 extensions
+package com.puppycrawl.tools.checkstyle;
+
+public class InputOuterTypeFilenameCheck2 {
+    
+  
+}
