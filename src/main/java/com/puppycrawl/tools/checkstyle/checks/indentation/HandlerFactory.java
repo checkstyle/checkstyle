2224d1e694a4edb75888e43b378d795fe194619d
Pieter Noordhuis <pcnoordhuis@gmail.com>
2015-08-03 00:05:36 -0700

    Issue #1548: Add lambda support to indentation check (see #281)

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/indentation/HandlerFactory.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/indentation/HandlerFactory.java
@@ -75,6 +75,7 @@ public class HandlerFactory {
         register(TokenTypes.LITERAL_NEW, NewHandler.class);
         register(TokenTypes.INDEX_OP, IndexHandler.class);
         register(TokenTypes.LITERAL_SYNCHRONIZED, SynchronizedHandler.class);
+        register(TokenTypes.LAMBDA, LambdaHandler.class);
     }
 
     /**
