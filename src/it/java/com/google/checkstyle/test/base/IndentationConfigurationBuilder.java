c32c7cd3d0a862414cb7f11d0a8e30e31d0b2877
Michal Kordas <kordas.michal@gmail.com>
2015-08-19 20:54:31 +0200

    Remove usage of prohibited exceptions in test code. #1555
    Fixes `BadExceptionThrown` inspection violations in test code.
    
    Description:
    >Reports throw statements which throw inappropriate exceptions. One use of this inspection would be to warn of throw statements which throw overly generic exceptions (e.g. java.lang.Exception or java.io.IOException).

--- src/it/java/com/google/checkstyle/test/base/IndentationConfigurationBuilder.java
+++ src/it/java/com/google/checkstyle/test/base/IndentationConfigurationBuilder.java
@@ -61,7 +61,7 @@ public class IndentationConfigurationBuilder extends ConfigurationBuilder
                     final int actualIndent = getLineStart(line, tabWidth);
 
                     if (actualIndent != indentInComment) {
-                        throw new RuntimeException(String.format(
+                        throw new IllegalStateException(String.format(
                                         "File \"%1$s\" has incorrect indentation in comment."
                                                         + "Line %2$d: comment:%3$d, actual:%4$d.",
                                         aFileName,
@@ -75,14 +75,14 @@ public class IndentationConfigurationBuilder extends ConfigurationBuilder
                     }
 
                     if (!isCommentConsistent(comment)) {
-                        throw new RuntimeException(String.format(
+                        throw new IllegalStateException(String.format(
                                         "File \"%1$s\" has inconsistent comment on line %2$d",
                                         aFileName,
                                         lineNumber));
                     }
                 }
                 else if (NONEMPTY_LINE_REGEX.matcher(line).matches()) {
-                    throw new RuntimeException(String.format(
+                    throw new IllegalStateException(String.format(
                                     "File \"%1$s\" has no indentation comment or its format "
                                                     + "malformed. Error on line: %2$d(%3$s)",
                                     aFileName,
