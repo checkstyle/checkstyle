c4c6bbf688e10f6f2713e5769f4208e981a592f7
Michal Kordas <kordas.michal@gmail.com>
2015-08-20 18:06:22 +0200

    Remove unnecessary this in test code. #1555
    Fixes `UnnecessaryThis` inspection violations in test code.
    
    Description:
    >Reports on any unnecessary uses of this in the code. Using this to disambiguate a code reference may easily become unnecessary via automatic refactorings, and is discouraged by many coding styles.

--- src/test/java/com/puppycrawl/tools/checkstyle/filters/SuppressWithNearbyCommentFilterTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/filters/SuppressWithNearbyCommentFilterTest.java
@@ -264,9 +264,8 @@ public class SuppressWithNearbyCommentFilterTest
     @Test
     public void testAcceptNullFileContents() {
         final LocalizedMessage message =
-            new LocalizedMessage(1, 1,
-                "messages.properties", "key", null, SeverityLevel.ERROR, null,
-                this.getClass(), null);
+            new LocalizedMessage(1, 1, "messages.properties", "key", null, SeverityLevel.ERROR,
+                    null, getClass(), null);
         final AuditEvent auditEvent = new AuditEvent(this, "Test.java", message);
         SuppressWithNearbyCommentFilter filter = new SuppressWithNearbyCommentFilter();
         Assert.assertTrue(filter.accept(auditEvent));
