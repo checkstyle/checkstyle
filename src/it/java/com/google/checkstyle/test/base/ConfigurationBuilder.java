cea7f4acbe4f286cbb031efa816f9e881278fdc3
Michal Kordas <kordas.michal@gmail.com>
2015-08-20 18:34:16 +0200

    Remove redundant throws. #1555
    Fixes `RedundantThrows` inspection violations in test code.
    
    Description:
    >This inspection reports exceptions that are declared in a method's signature but never thrown by the method itself or its implementations/derivatives.

--- src/it/java/com/google/checkstyle/test/base/ConfigurationBuilder.java
+++ src/it/java/com/google/checkstyle/test/base/ConfigurationBuilder.java
@@ -32,14 +32,14 @@ public class ConfigurationBuilder extends BaseCheckTestSupport {
 	final Pattern warnPattern = Utils.createPattern(".*[ ]*//[ ]*warn[ ]*|/[*]warn[*]/");
 
 	public ConfigurationBuilder(File aROOT)
-			throws CheckstyleException, IOException {
+			throws CheckstyleException {
 		root = aROOT;
 		config = getConfigurationFromXML(XML_NAME, System.getProperties());
 		listFiles(files, root, "java");
 	}
 
 	private static Configuration getConfigurationFromXML(String aConfigName,
-			Properties aProps) throws CheckstyleException {
+			Properties aProps) {
 		try {
 			return ConfigurationLoader.loadConfiguration(aConfigName,
 					new PropertiesExpander(aProps));
