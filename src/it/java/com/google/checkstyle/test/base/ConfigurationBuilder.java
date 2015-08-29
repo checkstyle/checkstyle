e849dd65958488fb24899e2913a4b59c03dd0e06
Michal Kordas <kordas.michal@gmail.com>
2015-08-29 23:32:05 +0200

    Issue #1555: Delete unnecessary imports
    Fixes `UnusedImport` inspection violations.
    
    Description:
    >Reports any import statements that are unused. Since IDEA can automatically detect and fix such statements with its "Optimize Imports" command, this inspection is mostly useful for off-line reporting on code bases that you don't intend to change.

--- src/it/java/com/google/checkstyle/test/base/ConfigurationBuilder.java
+++ src/it/java/com/google/checkstyle/test/base/ConfigurationBuilder.java
@@ -3,7 +3,6 @@ package com.google.checkstyle.test.base;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
