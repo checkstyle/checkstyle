cc665162425f76759af79ad2864017ee647f5cde
Vladislav Lisetskiy <vladlis54@gmail.com>
2015-09-02 14:32:03 +0300

    Issue #2074: Correct SummuryJavadoc IT

--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule72thesummaryfragment/InputIncorrectSummaryJavaDocCheck.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule72thesummaryfragment/InputIncorrectSummaryJavaDocCheck.java
@@ -4,14 +4,14 @@ package com.google.checkstyle.test.chapter7javadoc.rule72thesummaryfragment;
  * A {@code Foo. Foo}
  * is a simple Javadoc. Some javadoc.
  */
-class InputCorrectJavaDocParagraphCheck12 {
+class InputIncorrectSummaryJavaDocCheck {
     
     /**
      * As of JDK 1.1, replaced by {@link #setBounds(int,int,int,int)}
      */
     void foo3() {}
     
-    /**
+/*warn*//**
      * @throws Exception if an error occurs
      */
     void foo4() throws Exception {}
@@ -34,7 +34,7 @@ class InputCorrectJavaDocParagraphCheck12 {
      */
      class InnerInputCorrectJavaDocParagraphCheck {
 
-         /**
+/*warn*//**
           * foooo@foooo
           */
         public static final byte NUL = 0;
@@ -44,7 +44,7 @@ class InputCorrectJavaDocParagraphCheck12 {
          */
         public static final byte NUL_2 = 0;
 
-        /**
+ /*warn*//**
          * This method
          * returns some javadoc. Some javadoc.
          */
@@ -55,7 +55,7 @@ class InputCorrectJavaDocParagraphCheck12 {
          */
         void foo2() {}
 
-        /**
+/*warn*//**
          * @return the
          * customer ID some javadoc.
          */
@@ -66,7 +66,7 @@ class InputCorrectJavaDocParagraphCheck12 {
          */
         void foo3() {}
         
-        /**
+/*warn*//**
          * @throws Exception if an error occurs
          */
         void foo4() throws Exception {}
@@ -80,7 +80,7 @@ class InputCorrectJavaDocParagraphCheck12 {
         void foo6() {}
     }
 
-     /**
+/*warn*//**
       * A {@code InnerInputCorrectJavaDocParagraphCheck} is a simple code.
       */
     InnerInputCorrectJavaDocParagraphCheck anon = new InnerInputCorrectJavaDocParagraphCheck() {
@@ -100,7 +100,7 @@ class InputCorrectJavaDocParagraphCheck12 {
          */
         void foo3() {}
         
-        /**
+/*warn*//**
          * @throws Exception if an error occurs
          */
         void foo4() throws Exception {}
