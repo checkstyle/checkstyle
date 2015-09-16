7aa51a035f223ae2e93a42c79760ffa5409ef603
Vladislav Lisetskiy <vladlis54@gmail.com>
2015-09-16 03:53:19 +0300

    Issue #2126: Improve code coverage for TokenTypesDoclet

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/doclets/InputTokenTypesDocletNotConstants.java
@@ -0,0 +1,25 @@
+package com.puppycrawl.tools.checkstyle.doclets;
+
+import com.puppycrawl.tools.checkstyle.grammars.GeneratedJavaTokenTypes;
+
+public final class InputTokenTypesDocletNotConstants {
+
+    /**
+     * The end of file token.
+     **/
+    public final int EOF = GeneratedJavaTokenTypes.EOF; // must be static
+    
+    /**
+     * Modifiers for type, method, and field declarations.
+     **/
+    static final int MODIFIERS = GeneratedJavaTokenTypes.MODIFIERS; // must be public
+
+    /**
+     * An object block.
+     **/
+    public static int OBJBLOCK = GeneratedJavaTokenTypes.OBJBLOCK; // must be final
+    /**
+     * A list of statements.
+     **/
+    public static final double SLIST = GeneratedJavaTokenTypes.SLIST; // must be int
+}
