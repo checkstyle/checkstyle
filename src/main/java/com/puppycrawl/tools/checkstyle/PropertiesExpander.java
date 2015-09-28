8cb51324c5d70cea1c1bc57ba7c2cdab79ec9f2a
Roman Ivanov <ivanov-jr@mail.ru>
2015-09-27 19:21:15 -0700

    Revert "minor: custom tag 'noinspection' cause build problems during release"
    This reverts commit d98c79e9be2ff038a4cfc2def20d04e7237d3991.

--- src/main/java/com/puppycrawl/tools/checkstyle/PropertiesExpander.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/PropertiesExpander.java
@@ -37,6 +37,7 @@ public final class PropertiesExpander
      * @param properties the underlying properties to use for
      *     property resolution.
      * @throws IllegalArgumentException indicates null was passed
+     * @noinspection CollectionDeclaredAsConcreteClass
      */
     public PropertiesExpander(Properties properties) {
         if (properties == null) {
