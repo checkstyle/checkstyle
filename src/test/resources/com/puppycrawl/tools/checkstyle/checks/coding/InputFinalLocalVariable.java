8f5f904957d60ad62b73518ff0fc8a216966f15e
Vladislav Lisetskiy <vladlis54@gmail.com>
2015-10-30 19:28:15 +0300

    Issue #1551: Fix FinalLocalVariable false-negative

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/InputFinalLocalVariable.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/InputFinalLocalVariable.java
@@ -179,3 +179,85 @@ enum Enum1 {
         var = 1;
     }
 }
+
+class class2 {
+    public void method1(){
+        int x;
+        x = 3;
+    }
+    public void method2() {
+        for(int i=0;i<5;i++){
+            int x;
+            x = 3;
+        }
+        int y;
+        for(int i=0;i<5;i++) {
+            y = 3;
+        }
+        for(int i=0;i<5;i++) {
+            int z;
+            for(int j=0;j<5;j++) {
+                z = 3;
+            }
+        }
+    }
+    public void method3() {
+        int m;
+        do {
+           m = 0;
+        } while (false);
+        do {
+            int n;
+           n = 0;
+        } while (true);        
+    }
+
+    private void foo() {
+        int q;
+        int w;
+        int e;
+        q = 1;
+        w = 1;
+        e = 1;
+        e = 2;
+        class Local {
+            void bar() {
+                int q;
+                int w;
+                int e;
+                q = 1;
+                q = 2;
+                w = 1;
+                e = 1;
+            }
+        }
+
+        int i;
+        for (;; i = 1) { }
+    }
+
+    public void method4() {
+        int m;
+        int i = 5;
+        while (i > 1) {
+            m = 0;
+            i++;
+        }
+        while (true) {
+            int n;
+            n = 0;
+        }
+    }
+
+    int[] array = new int[10];
+    public void method5() {
+        int r;
+        for (int a: array) {
+            r = 0;
+        }
+        for (int a: array) {
+            int t;
+            t = 0;
+        }
+    }
+}
