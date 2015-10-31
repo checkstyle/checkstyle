9a39d19a31f06c8614d33fcc9c3f7654ec9cdd9f
rnveach <rveach02@gmail.com>
2015-10-30 23:39:03 -0400

    Issue #2482: Exclude lines with package in LineLength check

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/InputLongPackageStatement.java
@@ -0,0 +1,8 @@
+package com.nameofcompany.nameofdivision.nameofproject.systemtests.parallel.areaoftest.featuretested.flowtested;
+
+public class InputLongImportStatements {
+    @Override
+    public String toString() {
+        return "This is very long line that should be logged because it is not package";
+    }
+}
