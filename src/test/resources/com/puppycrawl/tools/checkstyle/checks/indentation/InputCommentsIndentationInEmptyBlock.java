80c41394852a347afcc04a06adf6261afe2575f1
Andrei Selkin <andreyselkin@gmail.com>
2015-10-31 14:57:33 +0300

    Issue #2078: Update UTs for CommentsIndentationCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/InputCommentsIndentationInEmptyBlock.java
@@ -0,0 +1,86 @@
+package com.puppycrawl.tools.checkstyle.checks.indentation;
+
+public class InputCommentsIndentationInEmptyBlock {
+
+    private void foo1() {
+        int a = 5, b = 3, v = 6;
+        if (a == b
+            && v == b || ( a ==1
+                   /// violation
+                       /* violation
+                        * one fine day ... */
+                               && b == 1)   ) {
+            // Cannot clearly detect user intention of explanation target.
+        }
+    }
+
+    private void foo2() {
+        int a = 5, b = 3, v = 6;
+        if (a == b
+            && v == b || ( a ==1
+            && b == 1)   ) {
+
+
+             // comment
+        }
+    }
+
+    private void foo3() {
+        int a = 5, b = 3, v = 6;
+        if (a == b
+            && v == b || (a == 1
+            && b == 1)) {
+// violation
+        }
+    }
+
+    // Comments here should be ok by Check
+    @SuppressWarnings("unused") // trailing
+    private static void foo4() { // trailing
+        if (true) // trailing comment
+        {
+            // some comment
+        }
+        if (true) { // trailing comment
+
+        }
+        /**
+         *
+         */
+    }
+
+    // Comments here should be ok by Check
+    @SuppressWarnings("unused") // trailing
+    private static void foo5() { // trailing
+        if (true) // trailing comment
+        {
+// violation
+        }
+        if (true) { // trailing comment
+
+        }
+        /**
+         *
+         */
+    }
+
+    public void foo6() {
+        try {
+
+        } catch (Exception e) {
+// violation
+        }
+    }
+
+    public void foo7() {
+        try {
+
+        } catch (Exception e) {
+            // TODO: handle exception here
+        }
+    }
+
+    private static class MyClass extends Object {
+           // no members
+    }
+}
