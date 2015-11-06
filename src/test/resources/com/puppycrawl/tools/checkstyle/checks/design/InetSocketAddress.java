fbb65687c706737608b0abc663accb681d4e4f19
Roman Ivanov <ivanov-jr@mail.ru>
2015-11-06 07:11:37 -0800

    Issue #2161: minor comment in input

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/InetSocketAddress.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/InetSocketAddress.java
@@ -1,5 +1,6 @@
 package com.puppycrawl.tools.checkstyle.checks.design;
-
+// ATTENTION: we need name of this class to be without "Input" prefix to as we
+// need the same name as defined at VisibilityModifierCheck.DEFAULT_IMMUTABLE_TYPES
 public class InetSocketAddress
 {
     class Arrays {
