fe48db9cc02642ce302fb85e931ab96e1b5875bb
Roman Ivanov <ivanov-jr@mail.ru>
2015-11-10 08:27:49 -0800

    Issue #2516: Create test for checkstyle-X.X-all.jar on Travis

--- src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule332nolinewrap/InputNoLineWrapGood.java
+++ src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule332nolinewrap/InputNoLineWrapGood.java
@@ -2,12 +2,12 @@ package com.google.checkstyle.test.chapter3filestructure.rule332nolinewrap; //ok
 
 import com.google.common.annotations.Beta; //ok
  
-import javax.accessibility.AccessibleAttributeSequence; //ok
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater; //ok
+import javax.accessibility.AccessibleAttributeSequence; //ok
 
 public class InputNoLineWrapGood {
     
-    public void fooMethod() {
-        //
-    }
+  public void fooMethod() {
+      //
+  }
 }
