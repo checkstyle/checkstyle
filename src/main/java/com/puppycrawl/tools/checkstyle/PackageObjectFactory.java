0d843780fc3dbe96fa513166a18a192d969dc232
rnveach <rveach02@gmail.com>
2015-12-06 20:33:35 -0500

    Issue #2625: made PackageObjectFactory public

--- src/main/java/com/puppycrawl/tools/checkstyle/PackageObjectFactory.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/PackageObjectFactory.java
@@ -36,7 +36,7 @@ import com.puppycrawl.tools.checkstyle.api.LocalizedMessage;
  * @author Rick Giles
  * @author lkuehne
  */
-class PackageObjectFactory implements ModuleFactory {
+public class PackageObjectFactory implements ModuleFactory {
     /** Logger for PackageObjectFactory. */
     private static final Log LOG = LogFactory.getLog(PackageObjectFactory.class);
 
@@ -59,7 +59,7 @@ class PackageObjectFactory implements ModuleFactory {
      * @param moduleClassLoader class loader used to load Checkstyle
      *          core and custom modules
      */
-    PackageObjectFactory(Set<String> packageNames,
+    public PackageObjectFactory(Set<String> packageNames,
             ClassLoader moduleClassLoader) {
         if (moduleClassLoader == null) {
             throw new IllegalArgumentException(
