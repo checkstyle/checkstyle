af047afee8216a4b0db9027dd013a1aad2be9494
liscju <piotr.listkiewicz@gmail.com>
2015-12-18 12:04:50 +0100

    Issue #2688: Fix OneStatementPerLine lambda validation

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/InputOneStatementPerLine.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/InputOneStatementPerLine.java
@@ -10,9 +10,16 @@ package com.puppycrawl.tools.checkstyle.checks.coding;
  * Two import statements and one 'empty' statement
  * which are not on the same line are legal.
  */
+import java.awt.event.ActionEvent;
 import java.lang.annotation.Annotation;
 ;
 import java.lang.String;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+import javax.swing.JCheckBox;
 
 public class InputOneStatementPerLine {
     /**
@@ -22,4 +29,33 @@ public class InputOneStatementPerLine {
      * and rises violation.
      */
     ;; //warn
+    static {
+        new JCheckBox().addActionListener((final ActionEvent e) -> {good();});
+        List<Integer> ints = new LinkedList<Integer>();
+        ints.stream().map( t -> { return t * 2;} ).filter( t -> { return false;});
+        ints.stream().map( t -> { int m = t * 2; return m; } ); //warn
+        ints.stream().map( t -> { int m = t * 2; return m; } ); int i = 3; //warn
+        ints.stream().map( t -> t * 2); int k = 4; //warn
+        ints.stream().map( t -> t * 2);
+        List<Integer> ints2 = new LinkedList<Integer>();
+        ints.stream().map( t -> { return ints2.stream().map(w -> { return w * 2; });});
+        ints.stream().map( t -> { return ints2.stream().map(w -> { int m = w * 2; return m; });}); //warn
+        ints.stream().map( t -> {
+            return ints2.stream().map(
+                    w -> {
+                        int m = w * 2;
+                        return m;
+                    });
+        });
+        ints.stream().map( t -> {
+            int k = 0;
+            for (int i = 0;i < 10;i++) {
+                k = i + k;
+            }
+            return k;
+        });
+    }
+
+    private static void good() {
+    }
 }
