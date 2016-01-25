d35a8321c2f061bfa1fa58a8826770320f572537
rnveach <rveach02@gmail.com>
2016-01-25 18:46:50 -0500

    Issue #2806: fixed improper javadocs from overridden methods

--- src/main/java/com/puppycrawl/tools/checkstyle/DefaultLogger.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/DefaultLogger.java
@@ -113,6 +113,8 @@ public class DefaultLogger extends AutomaticBean implements AuditListener {
     /**
      * Print an Emacs compliant line on the error stream.
      * If the column number is non zero, then also display it.
+     *
+     * @param event the event details
      * @see AuditListener
      **/
     @Override
