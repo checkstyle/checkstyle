ec20c822a102896281bc9864f76d2677b1f2b2d3
liscju <piotr.listkiewicz@gmail.com>
2016-02-12 12:59:41 +0100

    Issue #2818: Add support for lambda expression in LeftCurlyCheck

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/blocks/InputLeftCurlyNewLineOptionWithLambda.java
@@ -0,0 +1,18 @@
+//Compilable with Java8
+package com.puppycrawl.tools.checkstyle.checks.blocks;
+
+public class InputLeftCurlyNewLineOptionWithLambda
+{
+    static Runnable r1 = () -> {
+        System.out.println("Hello world one!");
+    };
+
+    static Runnable r2 = () -> System.out.println("Hello world two!");
+
+    static Runnable r3 = () -> {System.out.println("Hello world two!");};
+
+    static Runnable r4 = () ->
+    {
+        System.out.println("Hello world one!");
+    };
+}
