cb9218f5a7b62df5ef0ebb3f3487c2913f2ac8be
rnveach <rveach02@gmail.com>
2016-02-15 19:57:18 -0500

    Issue #2904: fixed bad code in InputFinalLocalVariableNameLambda

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/InputFinalLocalVariableNameLambda.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/InputFinalLocalVariableNameLambda.java
@@ -1,18 +1,27 @@
+//Compilable with Java8
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
 import java.math.BigDecimal;
 
 public class InputFinalLocalVariableNameLambda {
+    private interface Lambda {
+        public Object op(AugmentedOrder a, AugmentedOrder b);
+    }
     private static class AugmentedOrder {
-        public void setTotalValueOfOrder(final String plainString) {
+        public BigDecimal reduce(final BigDecimal zero, final Lambda l) {
+            return null;
+        }
+        public Object add(final Object amount) {
+            return null;
+        }
+        public Object getAmount() {
+            return null;
         }
     }
 
     private void addTotalValueOfOrder(final AugmentedOrder order) {
-    final BigDecimal totalValueOfOrder = order.getTransactions().stream()
+    final BigDecimal totalValueOfOrder = order
             .reduce(BigDecimal.ZERO,
-                    (t, u) -> t.add(u.getAmount()),
-                    BigDecimal::add);
-    order.setTotalValueOfOrder(totalValueOfOrder.toPlainString());
+                    (t, u) -> t.add(u.getAmount()));
  }
 }
