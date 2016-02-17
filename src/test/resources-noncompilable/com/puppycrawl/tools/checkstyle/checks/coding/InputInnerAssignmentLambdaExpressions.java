b16231d7b1d2c8c0425fa47c926134e01db951cd
rnveach <rveach02@gmail.com>
2016-02-17 18:59:34 -0500

    Issue #2904: fixed bad code in InputInnerAssignmentLambdaExpressions

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/InputInnerAssignmentLambdaExpressions.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/InputInnerAssignmentLambdaExpressions.java
@@ -1,10 +1,11 @@
+//Compilable with Java8
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
 import java.util.function.Supplier;
 
 public class InputInnerAssignmentLambdaExpressions {
     interface MyLambda {
-        boolean e(Object e);
+        void e(Object e);
     }
     private static class MyButton {
         public void setOnAction(MyLambda my) {
@@ -12,8 +13,6 @@ public class InputInnerAssignmentLambdaExpressions {
     }
     private void setAction() {
         MyButton button = new MyButton();
-        boolean pressed = false;
-        button.setOnAction(e -> pressed = true);  //No violation here
-        button.setOnAction(e -> { pressed = true; });  //No violation here
+        button.setOnAction(e -> { boolean pressed = true; });  //No violation here
     }
 }
