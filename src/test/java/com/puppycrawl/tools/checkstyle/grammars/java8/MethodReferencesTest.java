eace5478d357e95dd7e91ceb28950ca62421fb7a
rnveach <rveach02@gmail.com>
2016-03-21 18:50:26 -0400

    Issue #2968: adding more method references examples

--- src/test/java/com/puppycrawl/tools/checkstyle/grammars/java8/MethodReferencesTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammars/java8/MethodReferencesTest.java
@@ -90,6 +90,15 @@ public class MethodReferencesTest extends BaseCheckTestSupport {
         final DefaultConfiguration checkConfig = createCheckConfig(MemberNameCheck.class);
         final String[] expected = CommonUtils.EMPTY_STRING_ARRAY;
         verify(checkConfig, getNonCompilablePath("InputMethodReferences6.java"), expected);
+    }
+
+    @Test
+    public void testMethodReferences7()
+        throws Exception {
+        final DefaultConfiguration checkConfig =
+                createCheckConfig(MemberNameCheck.class);
+        final String[] expected = CommonUtils.EMPTY_STRING_ARRAY;
+        verify(checkConfig, getNonCompilablePath("InputMethodReferences7.java"), expected);
 
     }
 }
