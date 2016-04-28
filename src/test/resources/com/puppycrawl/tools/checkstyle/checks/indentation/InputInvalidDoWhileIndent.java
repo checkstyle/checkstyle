e2ad52ad596f594968bb08098777551c2bccf8bb
rnveach <rveach02@gmail.com>
2016-04-28 08:37:12 -0400

    Issue #3136: added indentation check of while in do..while (#3139)

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/InputInvalidDoWhileIndent.java
@@ -0,0 +1,25 @@
+package com.puppycrawl.tools.checkstyle.checks.indentation; //indent:0 exp:0
+
+public class InputInvalidDoWhileIndent { //indent:0 exp:0
+    public void method1() { //indent:4 exp:4
+        boolean test = true; //indent:8 exp:8
+
+do System.getProperty("foo"); while (test); //indent:0 exp:8 warn
+do {} while (test); //indent:0 exp:8 warn
+do { //indent:0 exp:8 warn
+} while (test); //indent:0 exp:8 warn
+do {} //indent:0 exp:8 warn
+while (test); //indent:0 exp:8 warn
+do //indent:0 exp:8 warn
+{} while (test); //indent:0 exp:8 warn
+do {} //indent:0 exp:8 warn
+while //indent:0 exp:8 warn
+(test); //indent:0 exp:8 warn
+do {} while //indent:0 exp:8 warn
+(test); //indent:0 exp:8 warn
+do {} while //indent:0 exp:8 warn
+( //indent:0 exp:8 warn
+test //indent:0 exp:8 warn
+); //indent:0 exp:8 warn
+    } //indent:4 exp:4
+} //indent:0 exp:0
