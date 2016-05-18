847a09d68f29a0551c76db66f2fed9cc3e5a57c5
Roman Ivanov <ivanov-jr@mail.ru>
2016-05-18 05:48:26 -0700

    Issue #3164: AnnotationLocation: unclear behavior when annotation is among modifiers

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/annotation/AnnotationLocationCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/annotation/AnnotationLocationCheck.java
@@ -29,6 +29,13 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtils;
  * By default, Check enforce to locate annotations immediately after
  * documentation block and before target element, annotation should be located
  * on separate line from target element.
+ * <p>
+ * Attention: Annotations among modifiers are ignored (looks like false-negative)
+ * as there might be a problem with annotations for return types
+ * <pre>public @Nullable Long getStartTimeOrNull() { ... }</pre>.
+ * Such annotations are better to keep close to type.
+ * Due to limitations Checkstyle can not examin target of annotation.
+ * </p>
  *
  * <p>
  * Example:
