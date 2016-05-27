45b4bc5a00ae347798aa88c8a520629ba763cc9a
Baratali Izmailov <barataliba@gmail.com>
2016-05-27 17:09:42 +0100

    Issue #3219: CLI Javadoc tree printer can print parse errors

--- src/main/java/com/puppycrawl/tools/checkstyle/AstTreeStringPrinter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/AstTreeStringPrinter.java
@@ -114,8 +114,7 @@ public final class AstTreeStringPrinter {
      * @return string javadoc tree
      */
     private static String parseAndPrintJavadocTree(DetailAST node) {
-        final JavadocDetailNodeParser parser = new JavadocDetailNodeParser();
-        final DetailNode tree = parser.parseJavadocAsDetailNode(node).getTree();
+        final DetailNode tree = DetailNodeTreeStringPrinter.parseJavadocAsDetailNode(node);
 
         final String rootPrefix = getIndentation(node);
         final String prefix = rootPrefix.substring(0, rootPrefix.length() - 2) + "   ";
