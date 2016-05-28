92fd7d1e2856c1132877192e8aac73c1f83e5f57
rnveach <rveach02@gmail.com>
2016-05-27 21:43:25 -0400

    Issue #3225: fixed cr lines in TreeWalker

--- src/main/java/com/puppycrawl/tools/checkstyle/TreeWalker.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/TreeWalker.java
@@ -690,14 +690,28 @@ public final class TreeWalker extends AbstractFileSetCheck implements ExternalRe
             String text, int initialLinesCnt, int initialColumnsCnt) {
         int lines = initialLinesCnt;
         int columns = initialColumnsCnt;
+        boolean foundCr = false;
         for (char c : text.toCharArray()) {
             if (c == '\n') {
+                foundCr = false;
                 lines++;
                 columns = 0;
             }
             else {
+                if (foundCr) {
+                    foundCr = false;
+                    lines++;
+                    columns = 0;
+                }
+                if (c == '\r') {
+                    foundCr = true;
+                }
                 columns++;
             }
+        }
+        if (foundCr) {
+            lines++;
+            columns = 0;
         }
         return new SimpleEntry<>(lines, columns);
     }
