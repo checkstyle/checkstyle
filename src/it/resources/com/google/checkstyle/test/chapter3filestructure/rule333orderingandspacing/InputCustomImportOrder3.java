d8a9c32ab5ce42ac4a8bc52ff1f14ab501c47f65
Andrei Selkin <andreyselkin@gmail.com>
2016-10-30 13:32:51 +0300

    Issue #941: Update import ordering in google_checks.xml according to changes in Google Style guide

--- src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule333orderingandspacing/InputCustomImportOrder3.java
+++ src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule333orderingandspacing/InputCustomImportOrder3.java
@@ -1,19 +1,19 @@
 package com.google.checkstyle.test.chapter3filestructure.rule333orderingandspacing;
 
-import static java.io.File.createTempFile;
-import static java.awt.Button.ABORT; //warn
-import static javax.swing.WindowConstants.*;
+import static java.awt.Button.ABORT;
+import java.awt.Dialog; //warn
+import static javax.swing.WindowConstants.*; //warn
 
 
-import java.util.StringTokenizer; //warn
-import java.util.*; //warn
-import java.util.concurrent.AbstractExecutorService; //warn
-import java.util.concurrent.*; //warn
-
-import com.google.checkstyle.test.chapter2filebasic.rule21filename.*;
-import com.sun.xml.internal.xsom.impl.scd.Iterators; //warn
-
+import com.google.checkstyle.test.chapter2filebasic.rule21filename.*; //warn
 import com.google.common.reflect.*; //warn
+import com.sun.xml.internal.xsom.impl.scd.Iterators; //warn
+import java.io.File;
+import static java.io.File.createTempFile; //warn
+import java.util.StringTokenizer;
+import java.util.*; //warn
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.*; //warn
 
 public class InputCustomImportOrder3 {
 }
