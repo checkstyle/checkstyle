7d54099590855acf605130f845557b48affeb032
kazachka <kazachka@mail.ua>
2016-10-30 23:54:55 +0300

    Issue #3445: GUI modified to show javadoc tree as subtree of COMMENT_CONTENT token

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/gui/InputJavadocAttributesAndMethods.java
@@ -0,0 +1,16 @@
+/**
+* class javadoc
+*/
+class InputJavadocAttributesAndMethods {
+
+    /** attribute javadoc*/
+    int attribute;
+
+    /**
+    * method javadoc
+    */
+    public void method() { 
+        /* just comment */
+    }
+
+}
