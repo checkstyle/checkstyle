48b5d092ba01272caeff08ba51bf588f12aef725
Roman Ivanov <ivanov-jr@mail.ru>
2016-12-10 06:09:50 -0800

    Issue #3628: usage of xmlstarlet instead of grep

--- .ci/travis/travis.sh
+++ .ci/travis/travis.sh
@@ -9,7 +9,7 @@ verify)
   ;;
 
 all-sevntu-checks)
-  grep " <module " config/checkstyle_sevntu_checks.xml | cut -d "\"" -f2 | grep -vE "Checker|TreeWalker|Filter|Holder" | sort | uniq | sed "s/Check$//" > file.txt
+  xmlstarlet sel --net --template -m .//module -v "@name" -n checkstyle_sevntu_checks.xml | grep -vE "Checker|TreeWalker|Filter|Holder" | sort | uniq | sed "s/Check$//" > file.txt
   wget -q http://sevntu-checkstyle.github.io/sevntu.checkstyle/apidocs/allclasses-frame.html -O - | html2text | grep -E "Check$" | cut -d " " -f6 | sort | uniq | sed "s/Check$//" > web.txt
   diff web.txt file.txt | cat
   ;;
