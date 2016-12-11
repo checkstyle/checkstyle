b0c6761ac6985db9f79b3811ce0b341d7a06986d
Roman Ivanov <ivanov-jr@mail.ru>
2016-12-11 15:48:43 -0800

    Issue #3632: several Travils items were moved to travis.sh

--- .ci/travis/travis.sh
+++ .ci/travis/travis.sh
@@ -4,6 +4,40 @@ set -e
 
 case "$GOAL" in
 
+nondex)
+  mvn --fail-never clean nondex:nondex
+  cat `grep -RlE 'td class=.x' .nondex/ | cat` < /dev/null > output.txt
+  RESULT=$(cat output.txt | wc -c)
+  cat output.txt
+  echo 'Size of output:'$RESULT
+  if [[ $RESULT != 0 ]]; then false; fi
+  ;;
+
+assembly-run-all-jar)
+  mvn clean package -Passembly
+  CS_POM_VERSION=$(mvn -q -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+  echo version:$CS_POM_VERSION
+  java -jar target/checkstyle-$CS_POM_VERSION-all.jar -c /google_checks.xml \
+        src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule332nolinewrap/InputNoLineWrapGood.java > output.log
+  if grep -vE '(Starting audit)|(warning)|(Audit done.)' output.log ; then exit 1; fi
+  if grep 'warning' output.log ; then exit 1; fi
+  ;;
+
+sonarqube)
+  if [[ $TRAVIS_PULL_REQUEST =~ ^([0-9]*)$ ]]; then exit 0; fi
+  mvn clean package cobertura:cobertura sonar:sonar \
+       -Dsonar.host.url=https://sonarqube.com \
+       -Dsonar.login=$SONAR_TOKEN \
+       -Dcobertura.report.format=xml -Dmaven.test.failure.ignore=true \
+       -Dcheckstyle.skip=true -Dpmd.skip=true -Dcheckstyle.ant.skip=true
+  ;;
+
+release-dry-run)
+  mvn release:prepare -DdryRun=true --batch-mode -Darguments='-DskipTests -DskipITs \
+    -Dcobertura.skip=true -Dpmd.skip=true -Dfindbugs.skip=true  -Dxml.skip=true \
+    -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true -Dgpg.skip=true'
+  ;;
+
 releasenotes-gen)
   .ci/travis/xtr_releasenotes-gen.sh
   ;;
