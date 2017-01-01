071df43c644890e7ba4f2064736e142d46abd02b
MaksimP <maksimp86@mail.ru>
2017-01-01 17:38:02 +0200

    Issue #3683: add to CI test permissions on all files

---
+++ .ci/travis/checkchmod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+CHMOD=$(find -type f -not -path '*/\.git/*' -a -type f -not -name '*.sh' -a \( -type d -not -perm 775 -o -type f -not -perm 664 \))
+if [[ -z $CHMOD ]]; then
+    exit 0;
+else
+    for NAMEFILE in $CHMOD
+    do
+        echo $NAMEFILE;
+    done
+fi
+echo " ";
+echo "To change permissions.";
