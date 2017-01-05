8d533e4fd37bbb6b411f82cf0298fd49ef71ec9c
Roman Ivanov <ivanov-jr@mail.ru>
2017-01-05 12:56:52 -0800

    minor: test

--- .ci/travis/checkchmod.sh
+++ .ci/travis/checkchmod.sh
@@ -15,8 +15,8 @@ set -e
 # stat -c "%a %n" config/checkstyle_checks.xml
 # echo "-----"
 
-
-CHMOD=$(find -type f -not -path '*/\.git/*' -a -type f -not -path '*/\target/*' -a -type f -not -name '*.sh' -exec stat -c "%a %n" {} \; | grep -v "664" | cat)
+#CHMOD=$(find -type f -not -path '*/\.git/*' -a -type f -not -path '*/\target/*' -a -type f -not -name '*.sh' -exec stat -c "%a %n" {} \; | grep -v "664" | cat)
+CHMOD=$(find -type f -not -path '*/\.git/*' -a -type f -not -name '*.sh' -a \( -type d -not -perm 775 -o -type f -not -perm 644 -o -type f -not -perm 664\))
 if [[ ! -z $CHMOD ]]; then
     echo "Expected mode for non '.sh' files is 664.";
     echo "Files that violates this rule:"
