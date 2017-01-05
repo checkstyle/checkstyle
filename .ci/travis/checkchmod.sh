e4a559089090496a0ee7cce2c22c902213054d45
Roman Ivanov <ivanov-jr@mail.ru>
2017-01-05 11:33:36 -0800

    minor: test

--- .ci/travis/checkchmod.sh
+++ .ci/travis/checkchmod.sh
@@ -3,10 +3,13 @@
 set -e
 
 ls -la .ci/
+echo "-----"
 ls -la config/
+echo "-----"
 find -type f -not -path '*/\.git/*' -a -type f -not -perm 664
+echo "-----"
 find -type f -not -perm 664
-
+echo "-----"
 
 CHMOD=$(find -type f -not -path '*/\.git/*' -a -type f -not -name '*.sh' -a \( -type d -not -perm 775 -o -type f -not -perm 664 \))
 if [[ ! -z $CHMOD ]]; then
