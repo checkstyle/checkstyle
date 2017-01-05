54058853b973e93b85677b09a840e136c41e6285
Roman Ivanov <ivanov-jr@mail.ru>
2017-01-05 05:41:51 -0800

    Issue #3683: return exit code if smth is found to fail build

--- .ci/travis/checkchmod.sh
+++ .ci/travis/checkchmod.sh
@@ -3,13 +3,12 @@
 set -e
 
 CHMOD=$(find -type f -not -path '*/\.git/*' -a -type f -not -name '*.sh' -a \( -type d -not -perm 775 -o -type f -not -perm 664 \))
-if [[ -z $CHMOD ]]; then
-    exit 0;
-else
+if [[ ! -z $CHMOD ]]; then
+	echo "Expected mode for non '.sh' files is 664.";
+    echo "Files that violates this rule:"
     for NAMEFILE in $CHMOD
     do
         echo $NAMEFILE;
     done
+    exit 1;
 fi
-echo " ";
-echo "To change permissions.";
