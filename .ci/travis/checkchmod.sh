fa2f05a53390261730f4bfcae15cad067ce7bcb5
Roman Ivanov <ivanov-jr@mail.ru>
2017-01-05 06:00:46 -0800

    minor: fix for intelijidea violation for tabs usage

--- .ci/travis/checkchmod.sh
+++ .ci/travis/checkchmod.sh
@@ -4,7 +4,7 @@ set -e
 
 CHMOD=$(find -type f -not -path '*/\.git/*' -a -type f -not -name '*.sh' -a \( -type d -not -perm 775 -o -type f -not -perm 664 \))
 if [[ ! -z $CHMOD ]]; then
-	echo "Expected mode for non '.sh' files is 664.";
+    echo "Expected mode for non '.sh' files is 664.";
     echo "Files that violates this rule:"
     for NAMEFILE in $CHMOD
     do
