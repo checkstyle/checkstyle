e3686ca4043ac7151512e51cba3e1dcfa44b4352
Andrei Selkin <andreyselkin@gmail.com>
2017-01-22 18:30:32 +0300

    Issue #3741: Fix false negative when import is from java.lang package

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/UnusedImportsCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/UnusedImportsCheckTest.java
@@ -186,4 +186,22 @@ public class UnusedImportsCheckTest extends BaseCheckTestSupport {
             expected);
     }
 
+    @Test
+    public void testImportsFromJavaLang() throws Exception {
+        final DefaultConfiguration checkConfig = createCheckConfig(UnusedImportsCheck.class);
+        final String[] expected = {
+            "3:8: " + getCheckMessage(MSG_KEY, "java.lang.String"),
+            "4:8: " + getCheckMessage(MSG_KEY, "java.lang.Math"),
+            "5:8: " + getCheckMessage(MSG_KEY, "java.lang.Class"),
+            "6:8: " + getCheckMessage(MSG_KEY, "java.lang.Exception"),
+            "7:8: " + getCheckMessage(MSG_KEY, "java.lang.Runnable"),
+            "8:8: " + getCheckMessage(MSG_KEY, "java.lang.RuntimeException"),
+            "9:8: " + getCheckMessage(MSG_KEY, "java.lang.ProcessBuilder"),
+            "10:8: " + getCheckMessage(MSG_KEY, "java.lang.Double"),
+            "11:8: " + getCheckMessage(MSG_KEY, "java.lang.Integer"),
+            "12:8: " + getCheckMessage(MSG_KEY, "java.lang.Float"),
+            "13:8: " + getCheckMessage(MSG_KEY, "java.lang.Short"),
+        };
+        verify(checkConfig, getPath("InputUnusedImportFromJavaLang.java"), expected);
+    }
 }
