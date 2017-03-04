f6425612fad4c3bb6399028143984322990898cb
Vikramaditya Kukreja <kukreja.vikramaditya@gmail.com>
2017-03-05 00:09:59 +0530

    Issue #3437: PackageDeclaration should give option for directory name mismatch

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/InputPackageDiffDirectoryAtSubpackage.java
@@ -0,0 +1,8 @@
+package com.puppycrawl.tools.checkstyle.checks.coding.subpackage;
+
+public class InputPackageDiffDirectoryAtSubpackage {
+    public String value;
+
+    private void get(){
+    }
+}
