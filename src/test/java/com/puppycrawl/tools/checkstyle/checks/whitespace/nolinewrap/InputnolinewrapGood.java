d7fb2598f961f95aff908ebe380ceec6bce6f12e
Divgian <sidhudivgian@gmail.com>
2017-03-07 03:13:22 +0530

    Issue #3933:Modified the NoLineWrapCheck method to take input from files from a subfolder in the directory and also added the input files to that subfolder

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/whitespace/nolinewrap/InputnolinewrapGood.java
@@ -0,0 +1,17 @@
+package com.puppycrawl.tools.checkstyle.checks.whitespace.nolinewrap;
+
+/**
+ *
+ * @author divsidhu-26
+ */
+import com.puppycrawl.tools.checkstyle.TreeWalker;
+
+import javax.accessibility.AccessibleAttributeSequence;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+public class InputnolinewrapGood {
+    
+	public void fooMethod() {
+		//
+	}
+}
