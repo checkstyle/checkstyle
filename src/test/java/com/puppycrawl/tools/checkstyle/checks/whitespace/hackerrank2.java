fd79572c42b9b0421053b8ace9b931db465bce72
shivanshsoni <shivanshsoni23@gmail.com>
2017-03-14 18:53:06 +0530

    Revert "Issue #3976: Split and organised test for input for EmptyForIteratorPad"
    This reverts commit 73d2bd59e7479780f484599386f48f26d0367aec.

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/whitespace/hackerrank2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class hackerrank2
+{
+	public static void main(String[]args)
+	{
+		Scanner scan=new Scanner(System.in);
+		int n,i,j,k;
+		n=scan.nextInt();
+		for(i=0;i<n;i++)
+		{
+			str="min(int,+str)";
+		}
+		System.out.println(str);
+	}
+}
