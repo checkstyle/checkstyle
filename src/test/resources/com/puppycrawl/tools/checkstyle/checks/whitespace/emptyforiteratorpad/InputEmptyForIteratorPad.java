9b7d661fa8bd99591d0471331f77ce451d6907df
shivanshsoni <shivanshsoni23@gmail.com>
2017-03-15 19:43:08 +0530

    Issue #3976: EmptyForIteratorPad input test organise and split

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptyforiteratorpad/InputEmptyForIteratorPad.java
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////////////
+// Test case file for FOR_ITERATION.
+// Created: 2003
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptyforiteratorpad;
+
+class InputEmptyForIteratorPad
+{
+    void method1()
+    {
+        for (int i = 0; i < 1; i++) {
+        }
+        
+        for (int i = 0; i < 1;i++) {
+        }
+
+        for (int i = 0; i < 1;i++ ) {
+        }
+
+        for (int i = 0; i < 1; i++ ) {
+        }
+
+        for (int i = 0; i < 1;) {
+            i++;
+        }
+
+        for (int i = 0; i < 1; ) {
+            i++;
+        }
+        
+        // test eol, there is no space after second SEMI
+        for (int i = 0; i < 1;
+            ) {
+            i++;
+        }
+    }
+
+    void method2()
+    {
+        for ( int i = 0; i < 1; i++ ) {
+        }
+        
+        for ( int i = 0; i < 1; ) {
+            i++;
+        }
+
+        int i = 0;
+        for ( ; i < 1; i++ ) {
+        }
+
+        for (; i < 2; i++ ) {
+        }
+
+        for (
+        ;; ) {
+        }
+    }
+}
