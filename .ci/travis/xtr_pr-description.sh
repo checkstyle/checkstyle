9b72b0f88b6ec67b44faddabbf213fa382e922d9
Vladislav Lisetskii <vladlis54@gmail.com>
2017-03-17 19:33:44 +0300

    doc: fix typos in Travis PR validation messages

--- .ci/travis/xtr_pr-description.sh
+++ .ci/travis/xtr_pr-description.sh
@@ -5,9 +5,9 @@ set -e
 if [[ ! $TRAVIS_PULL_REQUEST =~ ^([0-9]*)$ ]]; then exit 0; fi
 LINK_COMMITS=https://api.github.com/repos/checkstyle/checkstyle/pulls/$TRAVIS_PULL_REQUEST/commits
 COMMITS=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" $LINK_COMMITS | jq '.[0] | .commit.message')
-echo 'Messages from github:'${COMMITS:0:60}...
+echo 'Commit messages from github: '${COMMITS:0:60}...
 ISSUE_NUMBER=$(echo $COMMITS | sed -e 's/^.*Issue //' | sed -e 's/:.*//')
-echo 'Issue number:'$ISSUE_NUMBER && RESULT=0
+echo 'Issue number: '$ISSUE_NUMBER && RESULT=0
 if [[ $ISSUE_NUMBER =~ ^#[0-9]+$ ]]; then
     LINK_PR=https://api.github.com/repos/checkstyle/checkstyle/pulls/$TRAVIS_PULL_REQUEST
     LINK_ISSUE=https://api.github.com/repos/checkstyle/checkstyle/issues/${ISSUE_NUMBER:1}
@@ -15,15 +15,15 @@ if [[ $ISSUE_NUMBER =~ ^#[0-9]+$ ]]; then
     PR_DESC=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" $LINK_PR | jq '.body' | grep -E $REGEXP | cat )
     echo 'PR Description grepped:'${PR_DESC:0:80}
     if [[ -z $PR_DESC ]]; then
-         echo 'Please put a reference of Issue to PR description, this will bind Issue and PR in Github' && RESULT=1;
+         echo 'Please put a reference to an Issue in the PR description, this will bind the Issue to your PR in Github' && RESULT=1;
        fi
     LABEL_APRV=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" $LINK_ISSUE | jq '.labels [] | .name' | grep approved | cat | wc -l )
     if [[ $LABEL_APRV == 0 ]]; then
-         echo 'You provide PR to Issue that is not approved yes, please ask admins to approve your issue first' && RESULT=1;
+         echo 'You are providing a PR for an Issue that is not approved yet, please ask admins to approve your Issue first' && RESULT=1;
     fi
   fi
 if [[ $RESULT == 0 ]]; then
-      echo 'PR validation succeed.';
+      echo 'PR validation succeeded.';
 else
       echo 'PR validation failed.' && false;
 fi
