26d5c57ec01948a96675e971e54e5b92a359fb84
MikhailovOlegM <mikhailov.oleg.m@gmail.com>
2017-03-19 13:30:01 +0200

    Split and Organize Checkstyle inputs by IllegalCatch

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalcatch/InputIllegalCatch2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalcatch/InputIllegalCatch2.java
@@ -1,4 +1,4 @@
-package com.puppycrawl.tools.checkstyle.checks.coding;
+package com.puppycrawl.tools.checkstyle.checks.coding.illegalcatch;
 
 public class InputIllegalCatch2 {
     public void foo() throws OneMoreException {
