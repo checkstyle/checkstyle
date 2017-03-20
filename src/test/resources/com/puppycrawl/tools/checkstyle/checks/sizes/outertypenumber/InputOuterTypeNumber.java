98e21c2e61babc11ba3c101e6b4e805f38092f9d
Ashish Tiwari <ashishtiwari@asu.edu>
2017-03-20 02:58:51 -0700

    4043 - Split and Organize Checkstyle inputs by Test for OuterTypeNumber

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/outertypenumber/InputOuterTypeNumber.java
@@ -0,0 +1,10 @@
+package com.puppycrawl.tools.checkstyle.checks.sizes.outertypenumber;
+
+/**Contains empty inner class for OuterTypeNumberCheckTest*/
+public final class InputOuterTypeNumber {
+
+    /** Just empty inner class*/
+    private class InnerClass {
+        // Do nothing
+    }
+}
