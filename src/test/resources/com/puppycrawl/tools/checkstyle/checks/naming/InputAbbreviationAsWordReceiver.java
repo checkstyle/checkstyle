6dacbc5c7c3d06a66c6fd3b68179525dbdcdce8f
unrealwork <unrealwork@gmail.com>
2017-03-25 02:30:13 +0300

    Issue #4095: Split and Organize Checkstyle inputs by Test for AbbreviationAsWordInName

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/InputAbbreviationAsWordReceiver.java
+++
@@ -1,9 +0,0 @@
-package com.puppycrawl.tools.checkstyle.checks.naming;
-
-public class InputAbbreviationAsWordReceiver {
-    public void foo4(InputAbbreviationAsWordReceiver this) {}
-
-    private class Inner {
-        public Inner(InputAbbreviationAsWordReceiver InputAbbreviationAsWordReceiver.this) {}
-    }
-}
