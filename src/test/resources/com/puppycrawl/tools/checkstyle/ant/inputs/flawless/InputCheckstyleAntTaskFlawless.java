2d32ded7dd58c2a693ccd098c2ce86182fd44f83
Andrew Kuchev <0coming.soon@gmail.com>
2017-04-03 17:42:15 +0500

    Issue #3312: Move inputs into a separate directory

--- src/test/resources/com/puppycrawl/tools/checkstyle/ant/inputs/flawless/InputCheckstyleAntTaskFlawless.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/ant/inputs/flawless/InputCheckstyleAntTaskFlawless.java
@@ -1,4 +1,4 @@
-package com.puppycrawl.tools.checkstyle.ant;
+package com.puppycrawl.tools.checkstyle.ant.inputs.flawless;
 
 public final class InputCheckstyleAntTaskFlawless {
     private String foo = "A short line";
