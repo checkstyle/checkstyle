834fdfc3f34923ec7b93eb0c7329259b19adc2b0
Roman Ivanov <ivanov-jr@mail.ru>
2017-04-08 14:27:50 -0700

    minor: add comment

--- .ci/run-link-check-plugin.sh
+++ .ci/run-link-check-plugin.sh
@@ -7,6 +7,7 @@ mvn --version
 curl -I https://sourceforge.net/projects/checkstyle/
 mvn clean site -Dcheckstyle.ant.skip=true -DskipTests -DskipITs -Dpmd.skip=true -Dfindbugs.skip=true -Dcobertura.skip=true -Dcheckstyle.skip=true
 echo "------------ grep of linkcheck.html--BEGIN"
+# "grep ... | cat" is required command is running in "set -e" mode and grep could return exit code 1 if nothing is matching
 grep externalLink target/site/linkcheck.html | cat
 echo "------------ grep of linkcheck.html--END"
 RESULT=$(grep externalLink target/site/linkcheck.html | grep -v 'Read timed out' | wc -l)
