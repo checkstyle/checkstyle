f19f7c2bcf69fd2136e8467c1866b823e08c61bf
Roman Ivanov <ivanov-jr@mail.ru>
2017-04-08 14:20:19 -0700

    config: introduce shell file to run link-check-plugin

--- .ci/run-link-check-plugin.sh
+++ .ci/run-link-check-plugin.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+<<<<<<< refs/remotes/origin/master
 #This script is used at https://app.codeship.com/projects/124310
 #Run "firefox target/site/linkcheck.html" after completion to review html report
 
+=======
+>>>>>>> config: introduce shell file to run link-check-plugin
 set -e
 
 uname -a
@@ -10,10 +13,17 @@ mvn --version
 curl -I https://sourceforge.net/projects/checkstyle/
 mvn clean site -Dcheckstyle.ant.skip=true -DskipTests -DskipITs -Dpmd.skip=true -Dfindbugs.skip=true -Dcobertura.skip=true -Dcheckstyle.skip=true
 echo "------------ grep of linkcheck.html--BEGIN"
+<<<<<<< refs/remotes/origin/master
 # "grep ... | cat" is required command is running in "set -e" mode and grep could return exit code 1 if nothing is matching
+=======
+>>>>>>> config: introduce shell file to run link-check-plugin
 grep externalLink target/site/linkcheck.html | cat
 echo "------------ grep of linkcheck.html--END"
 RESULT=$(grep externalLink target/site/linkcheck.html | grep -v 'Read timed out' | wc -l)
 echo 'Exit code:'$RESULT
+<<<<<<< refs/remotes/origin/master
 if [[ $RESULT != 0 ]]; then false; fi
 
+=======
+if [[ $RESULT != 0 ]]; then false; fi
+>>>>>>> config: introduce shell file to run link-check-plugin
