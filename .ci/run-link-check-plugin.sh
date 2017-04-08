361b9ac2bbd043ec9af472f9a0333ad7ab1aa996
Roman Ivanov <ivanov-jr@mail.ru>
2017-04-08 14:27:50 -0700

    minor: add comment

--- .ci/run-link-check-plugin.sh
+++ .ci/run-link-check-plugin.sh
@@ -1,4 +1,29 @@
-#!/bin/bash
+
+
+<<<<<<< refs/remotes/origin/master
+#This script is used at https://app.codeship.com/projects/124310
+#Run "firefox target/site/linkcheck.html" after completion to review html report
+
+=======
+>>>>>>> config: introduce shell file to run link-check-plugin
+set -e
+
+uname -a
+mvn --version
+curl -I https://sourceforge.net/projects/checkstyle/
+mvn clean site -Dcheckstyle.ant.skip=true -DskipTests -DskipITs -Dpmd.skip=true -Dfindbugs.skip=true -Dcobertura.skip=true -Dcheckstyle.skip=true
+echo "------------ grep of linkcheck.html--BEGIN"
+grep externalLink target/site/linkcheck.html | cat
+echo "------------ grep of linkcheck.html--END"
+RESULT=$(grep externalLink target/site/linkcheck.html | grep -v 'Read timed out' | wc -l)
+echo 'Exit code:'$RESULT
+<<<<<<< refs/remotes/origin/master
+if [[ $RESULT != 0 ]]; then false; fi
+
+=======
+if [[ $RESULT != 0 ]]; then false; fi
+>>>>>>> config: introduce shell file to run link-check-plugin
+
 
 <<<<<<< refs/remotes/origin/master
 #This script is used at https://app.codeship.com/projects/124310
