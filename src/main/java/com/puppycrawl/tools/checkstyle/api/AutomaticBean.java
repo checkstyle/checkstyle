775da657f81c8eb42bfc980f68a8da143a05a65e
Subbu Dantu <subbudantu@Subbus-MacBook-Pro.local>
2017-04-09 19:57:58 +0530

    Issue #2186: Exception message extended to provide more information on incorrect parent

--- src/main/java/com/puppycrawl/tools/checkstyle/api/AutomaticBean.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/AutomaticBean.java
@@ -271,7 +271,8 @@ public class AutomaticBean
             throws CheckstyleException {
         if (childConf != null) {
             throw new CheckstyleException(childConf.getName() + " is not allowed as a child in "
-                    + getConfiguration().getName());
+                    + getConfiguration().getName() + ". Please review 'Parent Module' section "
+                    + "for this Check in web documentation if Check is standard.");
         }
     }
 
