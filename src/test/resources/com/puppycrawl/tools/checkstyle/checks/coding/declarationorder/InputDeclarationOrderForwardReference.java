cfc58b9fccea9d5449b71c4a2ee9eb23db033069
slava ganyaev <slava.ganyaev@gmail.com>
2017-04-11 10:53:12 +0300

    Issue #4165: Split and Organize Checkstyle inputs by Test for DeclarationOrderCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/declarationorder/InputDeclarationOrderForwardReference.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/declarationorder/InputDeclarationOrderForwardReference.java
@@ -1,4 +1,4 @@
-package com.puppycrawl.tools.checkstyle.checks.coding;
+package com.puppycrawl.tools.checkstyle.checks.coding.declarationorder;
 
 import java.util.ArrayList;
 import java.util.List;
