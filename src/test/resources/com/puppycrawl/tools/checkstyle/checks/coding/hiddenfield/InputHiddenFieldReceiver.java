b7da1c784eea0792c6db695ce91a25782d36685a
slava ganyaev <slava.ganyaev@gmail.com>
2017-04-13 13:22:40 +0300

    Issue #4165: Split and Organize Checkstyle inputs by Test for HiddenFieldCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/hiddenfield/InputHiddenFieldReceiver.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/hiddenfield/InputHiddenFieldReceiver.java
@@ -1,4 +1,4 @@
-package com.puppycrawl.tools.checkstyle.checks.coding;
+package com.puppycrawl.tools.checkstyle.checks.coding.hiddenfield;
 
 public class InputHiddenFieldReceiver {
     public void foo4(InputHiddenFieldReceiver this) {}
