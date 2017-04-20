0c382fd7b1fc9819f38bdd1c2cf9c50c76e0b725
Vladislav Lisetskii <vladislav_lisetskii@epam.com>
2017-04-20 12:56:48 +0300

    Issue #1509: Extra comma is required in multi-line array value

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/ArrayTrailingCommaCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/ArrayTrailingCommaCheck.java
@@ -29,8 +29,56 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * </p>
  * <p>
  * Rationale: Putting this comma in make is easier to change the
- * order of the elements or add new elements on the end.
+ * order of the elements or add new elements on the end. Main benefit of a trailing
+ * comma is that when you add new entry to an array, no surrounding lines are changed.
  * </p>
+ * <p>
+ * The check demands a comma at the end if neither left nor right curly braces
+ * are on the same line as the last element of the array.
+ * </p>
+ * <pre>
+ * return new int[] { 0 };
+ * return new int[] { 0
+ *     };
+ * return new int[] {
+ *     0 };
+ * </pre>
+ * <pre>
+ * {
+ *     100000000000000000000,
+ *     200000000000000000000, // OK
+ * }
+ *
+ * {
+ *     100000000000000000000,
+ *     200000000000000000000,
+ *     300000000000000000000,  // Just this line added, no other changes
+ * }
+ * </pre>
+ * <p>
+ * If closing brace is on the same line as training comma, this benefit is gone
+ * (as the Check does not demand a certain location of curly braces the following
+ * two cases will not produce a violation):
+ * </p>
+ * <pre>
+ * {100000000000000000000,
+ *     200000000000000000000,} // Trailing comma not needed, line needs to be modified anyway
+ *
+ * {100000000000000000000,
+ *     200000000000000000000, // Modified line
+ *     300000000000000000000,} // Added line
+ * </pre>
+ * <p>
+ * If opening brace is on the same line as training comma there's also (more arguable) problem:
+ * </p>
+ * <pre>
+ * {100000000000000000000, // Line cannot be just duplicated to slightly modify entry
+ * }
+ *
+ * {100000000000000000000,
+ *     100000000000000000001, // More work needed to duplicate
+ * }
+ * </pre>
  * <p>
  * An example of how to configure the check is:
  * </p>
@@ -65,15 +113,14 @@ public class ArrayTrailingCommaCheck extends AbstractCheck {
     @Override
     public void visitToken(DetailAST arrayInit) {
         final DetailAST rcurly = arrayInit.findFirstToken(TokenTypes.RCURLY);
+        final DetailAST previousSibling = rcurly.getPreviousSibling();
 
-        // if curlies are on the same line
-        // or array is empty then check nothing
         if (arrayInit.getLineNo() != rcurly.getLineNo()
-            && arrayInit.getChildCount() != 1) {
-            final DetailAST prev = rcurly.getPreviousSibling();
-            if (prev.getType() != TokenTypes.COMMA) {
-                log(rcurly.getLineNo(), MSG_KEY);
-            }
+                && arrayInit.getChildCount() != 1
+                && rcurly.getLineNo() != previousSibling.getLineNo()
+                && arrayInit.getLineNo() != previousSibling.getLineNo()
+                && previousSibling.getType() != TokenTypes.COMMA) {
+            log(rcurly.getLineNo(), MSG_KEY);
         }
     }
 }
