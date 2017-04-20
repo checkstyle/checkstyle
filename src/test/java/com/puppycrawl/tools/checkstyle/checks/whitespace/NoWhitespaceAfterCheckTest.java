d863f040a802d58f6758da32bf939468dca9741a
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-04-20 17:58:31 +0300

    Issue #3950: NoWhitespaceAfter: add support for method reference operator - restart CI

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/whitespace/NoWhitespaceAfterCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/whitespace/NoWhitespaceAfterCheckTest.java
@@ -199,6 +199,16 @@ public class NoWhitespaceAfterCheckTest
     }
 
     @Test
+    public void testMethodReferenceAfter() throws Exception {
+        checkConfig.addAttribute("tokens", "METHOD_REF");
+        final String[] expected = {
+            "17:37: " + getCheckMessage(MSG_KEY, "::"),
+            "18:66: " + getCheckMessage(MSG_KEY, "::"),
+        };
+        verify(checkConfig, getPath("InputNoWhitespaceAfterBadMethodRef.java"), expected);
+    }
+
+    @Test
     public void testVisitTokenSwitchReflection() {
         //unexpected parent for ARRAY_DECLARATOR token
         final DetailAST astImport = mockAST(TokenTypes.IMPORT, "import", "mockfile");
