644dd67b610d6e2ace566811a5dd15d1439c0ce5
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-04-21 21:04:30 +0300

    Issue #4165: Split and Organize Checkstyle inputs by Test for checks in coding package: NestedForDepth

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/NestedForDepthCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/NestedForDepthCheckTest.java
@@ -39,7 +39,9 @@ public class NestedForDepthCheckTest extends BaseCheckTestSupport {
     @Override
     protected String getPath(String filename) throws IOException {
         return super.getPath("checks" + File.separator
-                + "coding" + File.separator + filename);
+                + "coding" + File.separator
+                + "nestedfordepth" + File.separator
+                + filename);
     }
 
     /**
