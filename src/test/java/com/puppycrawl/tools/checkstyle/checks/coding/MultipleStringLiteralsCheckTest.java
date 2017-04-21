9aa7571f42c113880fc6aeaac6b1e5126521aa5b
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-04-21 21:25:49 +0300

    Issue #4165: Split and Organize Checkstyle inputs by Test for checks in coding package: MultipleStringLiterals

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/MultipleStringLiteralsCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/MultipleStringLiteralsCheckTest.java
@@ -34,7 +34,9 @@ public class MultipleStringLiteralsCheckTest extends BaseCheckTestSupport {
     @Override
     protected String getPath(String filename) throws IOException {
         return super.getPath("checks" + File.separator
-                + "coding" + File.separator + filename);
+                + "coding" + File.separator
+                + "multiplestringliterals" + File.separator
+                + filename);
     }
 
     @Test
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplestringliterals/InputMultipleStringLiterals.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplestringliterals/InputMultipleStringLiterals.java
@@ -1,4 +1,4 @@
-package com.puppycrawl.tools.checkstyle.checks.coding;
+package com.puppycrawl.tools.checkstyle.checks.coding.multiplestringliterals;
 
 public class InputMultipleStringLiterals
 {   /*string literals*/
