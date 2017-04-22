0e3bbd099ce621b9a4e0c7c408c369f415ad77ab
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-04-22 13:17:36 +0300

    Issue #4165: Split and Organize Checkstyle inputs by Test for checks in coding package: MagicNumber

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputIntMethodAnnotation.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputIntMethodAnnotation.java
@@ -1,4 +1,4 @@
-package com.puppycrawl.tools.checkstyle.checks.coding;
+package com.puppycrawl.tools.checkstyle.checks.coding.magicnumber;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
