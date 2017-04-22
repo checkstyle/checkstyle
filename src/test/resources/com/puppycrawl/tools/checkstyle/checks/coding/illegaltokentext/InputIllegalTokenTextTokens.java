e22d8f6914bbd2f8fb0c36e947cbbfc681f54a2d
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-04-23 00:11:43 +0300

    Issue #4165: Moved resource class to new package /illegaltokentext

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/InputIllegalTokenTextTokens.java
@@ -0,0 +1,38 @@
+package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
+
+/**
+ * Test for illegal tokens
+ */
+public class InputIllegalTokenTextTokens
+{
+    public void methodWithPreviouslyIllegalTokens()
+    {
+        int i = 0;
+        switch (i)
+        {
+            default:
+                i--;
+                i++;
+                break;
+        }
+    }
+    
+    public native void nativeMethod();
+    
+    public void methodWithLiterals()
+    {
+        final String ref = "<a href=\"";
+        final String refCase = "<A hReF=\"";
+    }
+
+    public void methodWithLabels() {
+        label:
+        {
+            anotherLabel: // some comment href
+            do {
+                continue anotherLabel;
+            } while (false);
+            break label; // some a href
+        }
+    }
+}
