422a9fe24752cf2c3115e15857519efa704a8c56
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-04-22 13:55:11 +0300

    Issue #4165: Split and Organize Checkstyle inputs by Test for checks in coding package: MagicNumber

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/innerassignment/InputInnerAssignment.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/innerassignment/InputInnerAssignment.java
@@ -1,4 +1,4 @@
-package com.puppycrawl.tools.checkstyle.checks.coding;
+package com.puppycrawl.tools.checkstyle.checks.coding.innerassignment;
 
 import java.io.FileInputStream;
 import java.io.IOException;
