0e81cdbdf95651284e61ef663032771c5fcc65e5
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-04-25 11:46:59 +0300

    Issue #4141: Split and Organize Checkstyle inputs by Test for ConstantName

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/constantname/InputConstantNameInner.java
@@ -0,0 +1,79 @@
+////////////////////////////////////////////////////////////////////////////////
+// Test case file for checkstyle.
+// Created: 2001
+////////////////////////////////////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.checks.naming.constantname;
+
+/**
+ * Tests having inner types
+ * @author Oliver Burn
+ **/
+class InputConstantNameInner
+{
+    // Ignore - two errors
+    class InnerInner2
+    {
+        // Ignore
+        public int fData;
+    }
+
+    // Ignore - 2 errors
+    interface InnerInterface2
+    {
+        // Ignore - should be all upper case
+        String data = "zxzc";
+
+        // Ignore
+        class InnerInterfaceInnerClass
+        {
+            // Ignore - need Javadoc and made private
+            public int rData;
+
+            /** needs to be made private unless allowProtected. */
+            protected int protectedVariable;
+
+            /** needs to be made private unless allowPackage. */
+            int packageVariable;
+        }
+    }
+
+    /** demonstrate bug in handling static final **/
+    protected static Object sWeird = new Object();
+    /** demonstrate bug in handling static final **/
+    static Object sWeird2 = new Object();
+
+    /** demonstrate bug in local final variable */
+    public interface Inter
+    {
+    }
+
+     public static void main()
+     {
+        Inter m = new Inter()
+        {
+            private static final int CDS = 1;
+
+            private int ABC;
+        };
+     }
+
+    /** annotation field incorrectly named. */
+    @interface InnerAnnotation
+    {
+        /** Ignore - should be all upper case. */
+        String data = "zxzc";
+    }
+
+    /** enum with public member variable */
+    enum InnerEnum
+    {
+        /** First constant */
+        A,
+
+        /** Second constant */
+        B;
+
+        /** Should be private */
+        public int someValue;
+    }
+}
