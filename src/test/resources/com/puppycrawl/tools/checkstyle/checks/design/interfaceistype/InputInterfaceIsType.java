caac8814caba087becbbe5347983419bca1d0c6b
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-05-03 10:54:03 +0300

    Issue #4314: Split and Organize Checkstyle inputs by Test for checks in design package: InterfaceIsTypeCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/interfaceistype/InputInterfaceIsType.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/interfaceistype/InputInterfaceIsType.java
@@ -2,7 +2,7 @@
 // Test case file for checkstyle.
 // Created: 2001
 ////////////////////////////////////////////////////////////////////////////////
-package com.puppycrawl.tools.checkstyle.checks.design;
+package com.puppycrawl.tools.checkstyle.checks.design.interfaceistype;
 
 /**
  * Test input for InterfaceIsTypeCheck
