f47ddd5078f8f4115260d66757bfb8f394702e59
Nathan Naze <nnaze@google.com>
2017-05-05 13:21:45 -0700

    Issue #2840: UnusedImportsCheck - process javadoc tags that span lines

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/utils/BlockTagUtilsTest.java
@@ -0,0 +1,99 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2017 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.puppycrawl.tools.checkstyle.internal.TestUtils;
+
+/**
+ * Tests BlockTagUtils.
+ */
+public class BlockTagUtilsTest {
+
+    @Test
+    public void testHasPrivateConstructor() throws Exception {
+        TestUtils.assertUtilsClassHasPrivateConstructor(BlockTagUtils.class);
+    }
+
+    @Test
+    public void testExtractBlockTags() {
+        final String[] text = {
+            "/** @foo abc ",
+            " * @bar def  ",
+            "   @baz ghi  ",
+            " * @qux jkl",
+            " */",
+        };
+
+        final List<TagInfo> tags = BlockTagUtils.extractBlockTags(text);
+        assertEquals(4, tags.size());
+
+        final TagInfo tag1 = tags.get(0);
+        assertTagEquals(tag1, "foo", "abc", 1, 4);
+
+        final TagInfo tag2 = tags.get(1);
+        assertTagEquals(tag2, "bar", "def", 2, 3);
+
+        final TagInfo tag3 = tags.get(2);
+        assertTagEquals(tag3, "baz", "ghi", 3, 3);
+
+        final TagInfo tag4 = tags.get(3);
+        assertTagEquals(tag4, "qux", "jkl", 4, 3);
+    }
+
+    @Test
+    public void testVersionStringFormat() {
+        final String[] text = {
+            "/** ",
+            " * @version 1.0",
+            " */",
+        };
+        final List<TagInfo> tags = BlockTagUtils.extractBlockTags(text);
+        assertEquals(1, tags.size());
+        assertEquals("version", tags.get(0).getName());
+        assertEquals("1.0", tags.get(0).getValue());
+    }
+
+    @Test
+    public void testOddVersionString() {
+        final String[] text = {
+            "/**",
+            " * Foo",
+            " * @version 1.0 */"};
+
+        final List<TagInfo> tags = BlockTagUtils.extractBlockTags(text);
+        assertEquals(1, tags.size());
+        assertEquals("version", tags.get(0).getName());
+        assertEquals("1.0", tags.get(0).getValue());
+    }
+
+    private static void assertTagEquals(TagInfo tag, String name, String value,
+            int line, int column) {
+        assertEquals(name, tag.getName());
+        assertEquals(value, tag.getValue());
+        assertEquals(line, tag.getPosition().getLine());
+        assertEquals(column, tag.getPosition().getColumn());
+    }
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsWithNewlinesInsideTags.java
@@ -0,0 +1,28 @@
+package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
+
+import java.net.ResponseCache;
+import java.util.List;
+
+public class InputUnusedImportsWithNewlinesInsideTags {
+
+    /**
+     * This javadoc demonstrates issue #2840.
+     * <p/>
+     * An inline tag references {@link ResponseCache#getDefault
+     * ResponseCache.getDefault} and is split across two lines.
+     */
+    public static void linkClassAndDescriptionSplitAcrossTwoLines() {
+        // do nothing
+    }
+
+    /**
+     * This javadoc demonstrates issue #359.
+     * <p/>
+     * An inline tag references {@link
+     * List} but the class name is on
+     * a different line than the tag name.
+     */
+    public static void linkAndClassnameSplitAcrossTwoLines() {
+        // do nothing
+    }
+}
