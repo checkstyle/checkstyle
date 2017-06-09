35d5d4d9e8cea4b9fec731b67a15f488d61cdf61
rnveach <rveach02@gmail.com>
2017-06-08 20:06:41 -0400

    Issue #4417: fixed failure to load custom root module

--- src/main/java/com/puppycrawl/tools/checkstyle/Main.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/Main.java
@@ -444,7 +444,7 @@ public final class Main {
     private static RootModule getRootModule(String name, ClassLoader moduleClassLoader)
             throws CheckstyleException {
         final ModuleFactory factory = new PackageObjectFactory(
-                Checker.class.getPackage().getName() + ".", moduleClassLoader);
+                Checker.class.getPackage().getName(), moduleClassLoader);
 
         return (RootModule) factory.createModule(name);
     }
