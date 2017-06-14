d6e24a4a3700df62132f315fb7ef36b9e668bcc0
vasilyeva <vasilyeva@rutarget.ru>
2017-06-15 00:07:13 +0300

    Issue #4395: increase coverage of pitest-checkstyle-utils profile to 67%

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/TestUtils.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/TestUtils.java
@@ -19,15 +19,25 @@
 
 package com.puppycrawl.tools.checkstyle.internal;
 
+import static com.puppycrawl.tools.checkstyle.TreeWalker.parseWithComments;
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 
 import org.junit.Assert;
 
+import antlr.ANTLRException;
 import com.puppycrawl.tools.checkstyle.PackageNamesLoader;
 import com.puppycrawl.tools.checkstyle.PackageObjectFactory;
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.FileText;
 
 public final class TestUtils {
 
@@ -55,5 +65,48 @@ public final class TestUtils {
         final ClassLoader cl = TestUtils.class.getClassLoader();
         final Set<String> packageNames = PackageNamesLoader.getPackageNames(cl);
         return new PackageObjectFactory(packageNames, cl);
+    }
+
+    /**
+     * Finds node of specified type among root children, siblings, siblings children
+     * on any deep level.
+     * @param root      DetailAST
+     * @param predicate predicate
+     * @return {@link Optional} of {@link DetailAST} node which matches the predicate.
+     */
+    public static Optional<DetailAST> findTokenInAstByPredicate(DetailAST root,
+                                                                Predicate<DetailAST> predicate) {
+        DetailAST curNode = root;
+        while (!predicate.test(curNode)) {
+            DetailAST toVisit = curNode.getFirstChild();
+            while (curNode != null && toVisit == null) {
+                toVisit = curNode.getNextSibling();
+                if (toVisit == null) {
+                    curNode = curNode.getParent();
+                }
+            }
+
+            if (curNode == toVisit || curNode == root.getParent()) {
+                curNode = null;
+                break;
+            }
+
+            curNode = toVisit;
+        }
+        return Optional.ofNullable(curNode);
+    }
+
+    /**
+     * Parses Java source file. Results in AST which contains comment nodes.
+     * @param file file to parse
+     * @return DetailAST tree
+     * @throws NullPointerException if the text is null
+     * @throws IOException          if the file could not be read
+     * @throws ANTLRException       if parser or lexer failed
+     */
+    public static DetailAST parseFile(File file) throws IOException, ANTLRException {
+        final FileText text = new FileText(file.getAbsoluteFile(), "UTF-8");
+        final FileContents contents = new FileContents(text);
+        return parseWithComments(contents);
     }
 }
