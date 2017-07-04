6418b12d5883dfd524f5ead629f327055f5fcbb5
liscju <piotr.listkiewicz@gmail.com>
2017-07-04 11:47:39 +0200

    Issue #4625: allow not empty line separator between static and plain import

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorNoStaticImportsViolation.java
@@ -0,0 +1,17 @@
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator;
+
+import java.math.BigDecimal.*;
+import java.math.RoundingMode;
+
+import static java.math.BigDecimal.*;
+
+import static java.awt.color.ColorSpace.*;
+
+import java.beans.AppletInitializer;
+
+import static java.io.Console.*;
+import static java.applet.AppletContext.*;
+
+import java.math.MathContext;
+public class InputEmptyLineSeparatorNoStaticImportsViolation {
+}
