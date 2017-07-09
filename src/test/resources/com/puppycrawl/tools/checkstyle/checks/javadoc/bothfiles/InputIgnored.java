a2d68d9687630fd5eff69d4f53e2710c2fd5d25f
BBG <djydewang@gmail.com>
2017-07-09 13:08:16 +0800

    Issue #4586: Moved resources for the JavadocStyle check test

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/bothfiles/InputIgnored.java
+++
@@ -1,5 +0,0 @@
-package com.puppycrawl.tools.checkstyle.checks.javadoc.bothfiles;
-
-class InputIgnored
-{
-}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocstyle/pkginfo/invalidinherit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * {@inheritDoc} Where are we inheriting from
+ */
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocstyle.pkginfo.invalidinherit;
