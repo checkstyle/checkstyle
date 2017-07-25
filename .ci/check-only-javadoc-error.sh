62cae4f319593d6a1065e94354798b5dec245e5f
Vladislav Lisetskii <vladislav_lisetskii@epam.com>
2017-07-25 18:54:09 +0300

    config: add more projects to Distelli CI

--- .ci/check-only-javadoc-error.sh
+++ .ci/check-only-javadoc-error.sh
@@ -11,6 +11,10 @@ mvn clean install -Pno-validations
 git clone https://github.com/checkstyle/contribution && cd contribution/checkstyle-tester
 sed -i.'' 's/^guava/#guava/' projects-to-test-on.properties
 sed -i.'' 's/#spring-framework/spring-framework/' projects-to-test-on.properties
+sed -i.'' 's/#nbia-dcm4che-tools/nbia-dcm4che-tools/' projects-to-test-on.properties
+sed -i.'' 's/#findbugs/findbugs/' projects-to-test-on.properties
+sed -i.'' 's/#pmd/pmd/' projects-to-test-on.properties
+sed -i.'' 's/#apache-ant/apache-ant/' projects-to-test-on.properties
 ./launch.sh -Dcheckstyle.config.location=checks-only-javadoc-error.xml
 if grep "Got an exception" target/site/checkstyle.html; then
   echo "[ERROR] Exceptions detected"
