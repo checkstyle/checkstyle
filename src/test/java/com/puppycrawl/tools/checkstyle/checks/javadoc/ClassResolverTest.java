87f33bfc805f7c5aa2d247f4e5cd434c2548534d
rnveach <rveach02@gmail.com>
2017-08-05 19:34:52 -0400

    Issue #4676: movedAbstractTypeAwareCheck and ClassResolver to javadoc

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/ClassResolverTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/ClassResolverTest.java
@@ -17,7 +17,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
 
-package com.puppycrawl.tools.checkstyle.checks;
+package com.puppycrawl.tools.checkstyle.checks.javadoc;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
