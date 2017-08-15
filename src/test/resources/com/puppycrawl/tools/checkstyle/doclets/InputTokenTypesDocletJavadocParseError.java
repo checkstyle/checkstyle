33533b889da42c63794be432a8154e3ac817f506
vasilyeva <vasilyeva@rutarget.ru>
2017-08-16 00:47:07 +0300

    Issue #4805: fix HtmlTagCanBeJavadocTag idea violation for TokenTypes

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/doclets/InputTokenTypesDocletJavadocParseError.java
@@ -0,0 +1,22 @@
+package com.puppycrawl.tools.checkstyle.doclets;
+
+public final class InputTokenTypesDocletJavadocParseError {
+
+    private InputTokenTypesDocletJavadocParseError() {
+    }
+
+    /**
+     * The <code>+</code> (unary plus) operator.
+     **/
+    public static final int CONSTANT1 = 1;
+
+    /**
+     * The {@code ++} (postfix increment) operator.
+     */
+    public static final int CONSTANT2 = 2;
+
+    /**
+     * Here you can see sentence without html or javadoc tags.
+     **/
+    public static final int CONSTANT3 = 3;
+}
