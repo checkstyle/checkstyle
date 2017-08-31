a6c2330e816ff5a9f09909cf2d8bd15764d8d3e3
vasilyeva <vasilyeva@rutarget.ru>
2017-08-31 22:22:47 +0300

    Issue #2600: remove extra quotes, introduce new variables to ease ecj version management, print output

--- .ci/eclipse-compiler-javac.sh
+++ .ci/eclipse-compiler-javac.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
-set -e
+#!/bin/sh -e
 
-ECJ_VERSION="4.7"
+ECJ_JAR="ecj-4.7.jar"
+ECJ_MAVEN_VERSION="R-4.7-201706120950"
+
+if [ ! -f ~/.m2/repository/$ECJ_MAVEN_VERSION/$ECJ_JAR ]; then
+    wget http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/eclipse/downloads/drops4/$ECJ_MAVEN_VERSION/$ECJ_JAR -P ~/.m2/repository/$ECJ_MAVEN_VERSION
+fi
+
 mkdir -p target/classes
 mkdir -p target/eclipse
 
-if [ ! -e ~/.m2/repository/ecj-$ECJ_VERSION.jar ]; then
-    wget http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/eclipse/downloads/drops4/R-4.7-201706120950/"ecj-$ECJ_VERSION.jar" -P ~/.m2/repository/
-fi
-
-java -jar ~/.m2/repository/ecj-$ECJ_VERSION.jar -target 1.8 -source 1.8 -cp $1 \
+set +e
+java -jar ~/.m2/repository/$ECJ_MAVEN_VERSION/$ECJ_JAR -target 1.8 -source 1.8 -cp $1 \
         -nowarn:[./target/generated-sources/antlr] -d target/eclipse-compile \
         -enableJavadoc src/main/java target/generated-sources/antlr -properties config/org.eclipse.jdt.core.prefs \
-    \
     > target/eclipse/report.txt 2>&1
+set -e
+
+if (( $(grep ERROR target/eclipse/report.txt | cat | wc -l) > 0 )); then
+  cat target/eclipse/report.txt
+  false
+fi
