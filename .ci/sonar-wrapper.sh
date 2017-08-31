89b51acfb8b4737bedbb5c6dbdbcb0ad34378bb4
vasilyeva <vasilyeva@rutarget.ru>
2017-08-31 22:13:09 +0300

    Issue #4912: add initial scripts for running sonar in docker

---
+++ .ci/sonar-wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#This file is for manual execution only
+
+set -e
+
+echo "Building docker image"
+docker pull sonarqube
+echo "Running docker container"
+docker run -dit -p 9000:9000 --name sonar sonarqube:latest
+echo "sleeping 60 sec to let sonar start up"
+sleep "60"
+
+./.ci/sonar.sh
+
+#kill container
+docker stop sonar
+docker rm sonar
---
+++ .ci/sonar.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#set checkstyle sonar profile
+curl -X POST -u admin:admin -F 'backup=@config/default_sonar_profile.xml' -v http://localhost:9000/api/profiles/restore
+
+#execute inspection
+mvn sonar:sonar -P sonar -Dsonar.language=java -Dsonar.profile=checksyle-profile
+
+# Uncomment following to get HTML report.
+# mvn sonar:sonar -Dsonar.analysis.mode=preview -Dsonar.issuesReport.html.enable=true \
+#        -Dsonar.language=java -Dsonar.profile=checksyle-profile
+
+
+#get and parse response from sonar
+#give some time to sonar for report processing
+sleep "60"
+curl -u admin:admin -v http://localhost:9000/api/issues/search?componentRoots=com.puppycrawl.tools:checkstyle \
+        > response.json
+
+OUTPUT="$(cat response.json | jq '.total')"
+
+#print number of found issues
+if [ ! "$OUTPUT" -eq "0" ]; then
+    jq '.' response.json
+    echo "Found issues - $OUTPUT"
+fi
