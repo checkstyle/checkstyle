5d1b4f79deecbce929db25af10ed4485c08bfd73
Roman Ivanov <ivanov-jr@mail.ru>
2017-09-04 12:13:40 -0700

    Issue #4682: remove extra configuration from idea_inspection.sh that does not affect inspection to simplify script

--- .ci/idea_inspection.sh
+++ .ci/idea_inspection.sh
@@ -14,19 +14,15 @@ PROJECT_DIR=$PWD/
 INSPECTIONS_PATH=$PWD/config/intellij-idea-inspections.xml
 RESULTS_DIR=$PWD/target/inspection-results
 NOISE_LVL=v1
-IDEA_LOCATION=
 # we need to export this variable as it is required for idea.sh script
 export IDEA_PROPERTIES=$PWD/config/idea.properties
 
 # Check IDEA_PATH env variable
-
-if [[ ! -z $IDEA_PATH ]]; then
-    IDEA_LOCATION=$IDEA_PATH
-else
+if [[ -z $IDEA_PATH ]]; then
     echo "IDEA_PATH variable not found."
     # Try to search in path
-    IDEA_LOCATION="$(which idea)"
-    if [ -z $IDEA_LOCATION ]; then
+    IDEA_PATH="$(which idea)"
+    if [ -z $IDEA_PATH ]; then
         echo "IntelliJ IDEA was not found in path."
         exit -1
     fi
@@ -34,11 +30,9 @@ fi
 
 mkdir -p $RESULTS_DIR
 rm -rf $RESULTS_DIR/*
-mkdir -p $PWD/.idea/scopes
-cp $PWD/config/intellij-idea-inspection-scope.xml $PWD/.idea/scopes
 
 echo "Validation is about to start ... progress output will be flushed at end. Validation is in progress ..."
-IDEA_OUTPUT=`exec "$IDEA_LOCATION" inspect $PROJECT_DIR $INSPECTIONS_PATH $RESULTS_DIR -$NOISE_LVL`
+IDEA_OUTPUT=`exec "$IDEA_PATH" inspect $PROJECT_DIR $INSPECTIONS_PATH $RESULTS_DIR -$NOISE_LVL`
 echo $IDEA_OUTPUT
 
 echo "Checking results ..."
