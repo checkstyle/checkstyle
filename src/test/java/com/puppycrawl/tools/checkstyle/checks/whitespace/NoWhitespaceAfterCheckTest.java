3be76368423001233ae7d05235527213bf66ee34
Ruslan Iagudin <ruslan_iagudin@epam.com>
2017-09-13 15:31:44 +0300

    Issue #4410: whitespace after @

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/whitespace/NoWhitespaceAfterCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/whitespace/NoWhitespaceAfterCheckTest.java
@@ -62,6 +62,9 @@ public class NoWhitespaceAfterCheckTest
             "132:11: " + getCheckMessage(MSG_KEY, "."),
             "136:12: " + getCheckMessage(MSG_KEY, "."),
             "264:2: " + getCheckMessage(MSG_KEY, "."),
+            "289:6: " + getCheckMessage(MSG_KEY, "@"),
+            "290:6: " + getCheckMessage(MSG_KEY, "@"),
+            "291:6: " + getCheckMessage(MSG_KEY, "@"),
         };
         verify(checkConfig, getPath("InputNoWhitespaceAfter.java"), expected);
     }
