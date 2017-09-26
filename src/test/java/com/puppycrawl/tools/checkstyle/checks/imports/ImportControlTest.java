9d0ecae16ada537b719bd7fbccb19218c7441eb1
rnveach <rveach02@gmail.com>
2017-09-26 19:54:39 -0400

    Issue #5104: merged all ImportControl tests into 1 class

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportControlTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportControlTest.java
@@ -29,6 +29,16 @@ public class ImportControlTest {
     private final ImportControl icRoot = new ImportControl("com.kazgroup.courtlink", false);
     private final ImportControl icCommon = new ImportControl(icRoot, "common", false);
 
+    private final ImportControl icRootRegexpChild = new ImportControl("com.kazgroup.courtlink",
+            false);
+    private final ImportControl icCommonRegexpChild = new ImportControl(icRootRegexpChild,
+            "common", false);
+
+    private final ImportControl icRootRegexpParent = new ImportControl("com\\.[^.]+\\.courtlink",
+            true);
+    private final ImportControl icCommonRegexpParen = new ImportControl(icRootRegexpParent,
+            "com+on", true);
+
     @Before
     public void setUp() {
         icRoot.addChild(icCommon);
@@ -39,8 +49,21 @@ public class ImportControlTest {
         icRoot.addImportRule(
             new PkgImportRule(true, false, "org.apache.commons", false, false));
 
+        icRootRegexpChild.addChild(icCommonRegexpChild);
+        icRootRegexpChild.addImportRule(
+            new PkgImportRule(false, false, ".*\\.(spring|lui)framework", false, true));
+        icRootRegexpChild.addImportRule(
+            new PkgImportRule(false, false, "org\\.hibernate", false, true));
+        icRootRegexpChild.addImportRule(
+            new PkgImportRule(true, false, "org\\.(apache|lui)\\.commons", false, true));
+
         icCommon.addImportRule(
             new PkgImportRule(true, false, "org.hibernate", false, false));
+
+        icCommonRegexpChild.addImportRule(
+            new PkgImportRule(true, false, "org\\.h.*", false, true));
+
+        icRootRegexpParent.addChild(icCommonRegexpParen);
     }
 
     @Test
@@ -80,5 +103,124 @@ public class ImportControlTest {
     @Test
     public void testUnknownPkg() {
         assertNull("Unxpected response", icRoot.locateFinest("net.another"));
+    }
+
+    @Test
+    public void testRegExpChildLocateFinest() {
+        assertEquals("Unxpected response", icRootRegexpChild, icRootRegexpChild
+                .locateFinest("com.kazgroup.courtlink.domain"));
+        assertEquals("Unxpected response", icCommonRegexpChild, icRootRegexpChild
+                .locateFinest("com.kazgroup.courtlink.common.api"));
+        assertNull("Unxpected response", icRootRegexpChild.locateFinest("com"));
+    }
+
+    @Test
+    public void testRegExpChildCheckAccess() {
+        assertEquals("Unxpected access result", AccessResult.DISALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "org.springframework.something"));
+        assertEquals("Unxpected access result", AccessResult.DISALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "org.luiframework.something"));
+        assertEquals("Unxpected access result", AccessResult.DISALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "de.springframework.something"));
+        assertEquals("Unxpected access result", AccessResult.DISALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                "de.luiframework.something"));
+        assertEquals("Unxpected access result", AccessResult.ALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "org.apache.commons.something"));
+        assertEquals("Unxpected access result", AccessResult.ALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "org.lui.commons.something"));
+        assertEquals("Unxpected access result", AccessResult.DISALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "org.apache.commons"));
+        assertEquals("Unxpected access result", AccessResult.DISALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "org.lui.commons"));
+        assertEquals("Unxpected access result", AccessResult.ALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "org.hibernate.something"));
+        assertEquals("Unxpected access result", AccessResult.DISALLOWED,
+                icCommonRegexpChild.checkAccess("com.kazgroup.courtlink.common",
+                        "com.badpackage.something"));
+        assertEquals("Unxpected access result", AccessResult.DISALLOWED,
+                icRootRegexpChild.checkAccess("com.kazgroup.courtlink",
+                        "org.hibernate.something"));
+    }
+
+    @Test
+    public void testRegExpChildUnknownPkg() {
+        assertNull("Unxpected response", icRootRegexpChild.locateFinest("net.another"));
+    }
+
+    @Test
+    public void testRegExpParentInRootIsConsidered() {
+        assertNull("Package should not be null", icRootRegexpParent.locateFinest("com"));
+        assertNull("Package should not be null",
+                icRootRegexpParent.locateFinest("com/hurz/courtlink"));
+        assertNull("Package should not be null",
+                icRootRegexpParent.locateFinest("com.hurz.hurz.courtlink"));
+        assertEquals("Invalid package", icRootRegexpParent,
+                icRootRegexpParent.locateFinest("com.hurz.courtlink.domain"));
+        assertEquals("Invalid package", icRootRegexpParent, icRootRegexpParent
+                .locateFinest("com.kazgroup.courtlink.domain"));
+    }
+
+    @Test
+    public void testRegExpParentInSubpackageIsConsidered() {
+        assertEquals("Invalid package", icCommonRegexpParen, icRootRegexpParent
+                .locateFinest("com.kazgroup.courtlink.common.api"));
+        assertEquals("Invalid package", icCommonRegexpParen, icRootRegexpParent
+                .locateFinest("com.kazgroup.courtlink.comon.api"));
+    }
+
+    @Test
+    public void testRegExpParentEnsureTrailingDot() {
+        assertNull("Invalid package", icRootRegexpParent.locateFinest("com.kazgroup.courtlinkkk"));
+        assertNull("Invalid package",
+                icRootRegexpParent.locateFinest("com.kazgroup.courtlink/common.api"));
+    }
+
+    @Test
+    public void testRegExpParentAlternationInParentIsHandledCorrectly() {
+        // the regular expression has to be adjusted to (com\.foo|com\.bar)
+        final ImportControl root = new ImportControl("com\\.foo|com\\.bar", true);
+        final ImportControl common = new ImportControl(root, "common", false);
+        root.addChild(common);
+        assertEquals("Invalid package", root, root.locateFinest("com.foo"));
+        assertEquals("Invalid package", common, root.locateFinest("com.foo.common"));
+        assertEquals("Invalid package", root, root.locateFinest("com.bar"));
+        assertEquals("Invalid package", common, root.locateFinest("com.bar.common"));
+    }
+
+    @Test
+    public void testRegExpParentAlternationInParentIfUserCaresForIt() {
+        // the regular expression has to be adjusted to (com\.foo|com\.bar)
+        final ImportControl root = new ImportControl("(com\\.foo|com\\.bar)", true);
+        final ImportControl common = new ImportControl(root, "common", false);
+        root.addChild(common);
+        assertEquals("Invalid package", root, root.locateFinest("com.foo"));
+        assertEquals("Invalid package", common, root.locateFinest("com.foo.common"));
+        assertEquals("Invalid package", root, root.locateFinest("com.bar"));
+        assertEquals("Invalid package", common, root.locateFinest("com.bar.common"));
+    }
+
+    @Test
+    public void testRegExpParentAlternationInSubpackageIsHandledCorrectly() {
+        final ImportControl root = new ImportControl("org.somewhere", false);
+        // the regular expression has to be adjusted to (foo|bar)
+        final ImportControl subpackages = new ImportControl(root, "foo|bar", true);
+        root.addChild(subpackages);
+        assertEquals("Invalid package", root, root.locateFinest("org.somewhere"));
+        assertEquals("Invalid package", subpackages, root.locateFinest("org.somewhere.foo"));
+        assertEquals("Invalid package", subpackages, root.locateFinest("org.somewhere.bar"));
+    }
+
+    @Test
+    public void testRegExpParentUnknownPkg() {
+        assertNull("Package should not be null", icRootRegexpParent.locateFinest("net.another"));
     }
 }
