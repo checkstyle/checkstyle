127f729942afa7d645db2e41c1b3ebe4c12be6c4
crude <crude@posteo.de>
2017-10-03 05:10:53 +0200

    Issue #929: Using private final loggers instead of private static final

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/TranslationCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/TranslationCheck.java
@@ -125,9 +125,6 @@ public class TranslationCheck extends AbstractFileSetCheck {
      */
     private static final String WRONG_LANGUAGE_CODE_KEY = "translation.wrongLanguageCode";
 
-    /** Logger for TranslationCheck. */
-    private static final Log LOG = LogFactory.getLog(TranslationCheck.class);
-
     /**
      * Regexp string for default translation files.
      * For example, messages.properties.
@@ -164,6 +161,9 @@ public class TranslationCheck extends AbstractFileSetCheck {
     /** Formatting string to form regexp to validate default translations file names. */
     private static final String REGEXP_FORMAT_TO_CHECK_DEFAULT_TRANSLATIONS = "^%s\\.%s$";
 
+    /** Logger for TranslationCheck. */
+    private final Log log;
+
     /** The files to process. */
     private final Set<File> filesToProcess = new HashSet<>();
 
@@ -181,6 +181,7 @@ public class TranslationCheck extends AbstractFileSetCheck {
     public TranslationCheck() {
         setFileExtensions("properties");
         baseName = CommonUtils.createPattern("^messages.*$");
+        log = LogFactory.getLog(TranslationCheck.class);
     }
 
     /**
@@ -513,7 +514,7 @@ public class TranslationCheck extends AbstractFileSetCheck {
         final SortedSet<LocalizedMessage> messages = new TreeSet<>();
         messages.add(message);
         getMessageDispatcher().fireErrors(file.getPath(), messages);
-        LOG.debug("IOException occurred.", exception);
+        log.debug("IOException occurred.", exception);
     }
 
     /** Class which represents a resource bundle. */
