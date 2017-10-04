747b8ba27b8b15f16b77ef4419491d09850bea6e
rnveach <rveach02@gmail.com>
2017-10-04 15:01:19 -0400

    Issue #5141: added xml verification for tests

--- src/test/java/com/puppycrawl/tools/checkstyle/AbstractPathTestSupport.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/AbstractPathTestSupport.java
@@ -21,8 +21,15 @@ package com.puppycrawl.tools.checkstyle;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public abstract class AbstractPathTestSupport {
+    protected static final String LF_REGEX = "\\\\n";
+
+    protected static final String CRLF_REGEX = "\\\\r\\\\n";
+
     /**
      * Returns the exact location for the package where the file is present.
      * @return path for the package name for the file.
@@ -45,5 +52,16 @@ public abstract class AbstractPathTestSupport {
 
     protected final String getResourcePath(String filename) {
         return "/" + getPackageLocation() + "/" + filename;
+    }
+
+    /** Reads the contents of a file.
+     * @param filename the name of the file whose contents are to be read
+     * @return contents of the file with all {@code \r\n} replaced by {@code \n}
+     * @throws IOException if I/O exception occurs while reading
+     */
+    protected static String readFile(String filename) throws IOException {
+        return new String(Files.readAllBytes(
+                Paths.get(filename)), StandardCharsets.UTF_8)
+                .replaceAll(CRLF_REGEX, LF_REGEX);
     }
 }
