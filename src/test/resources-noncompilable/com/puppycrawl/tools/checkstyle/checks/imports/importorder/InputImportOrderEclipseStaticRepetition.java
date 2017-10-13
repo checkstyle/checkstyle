15a1c52f762615f97e354b1ffc865fa90bdc3c5d
BBG <djydewang@gmail.com>
2017-10-13 21:06:29 +0800

    Issue #5176: adjacent static import groups ones became impossible in 8.3

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrderEclipseStaticRepetition.java
@@ -0,0 +1,8 @@
+package com.puppycrawl.tools.checkstyle.checks.imports;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.DATE;
+import static io.netty.handler.codec.http.HttpHeaders.Names.DATE;
+import static io.netty.handler.codec.http.HttpHeaders.Names.addDate;
+
+public class InputImportOrderEclipseStaticRepetition {
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrderStaticRepetition.java
@@ -0,0 +1,12 @@
+package com.puppycrawl.tools.checkstyle.checks.imports;
+
+import static java.lang.Math.*;
+import static org.antlr.v4.runtime.CommonToken.*;
+import static org.antlr.v4.runtime.CommonToken.*;  //Repetitive static import
+
+import java.util.Set;
+
+import org.junit.Test;
+
+public class InputImportOrderStaticRepetition {
+}
