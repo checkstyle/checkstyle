334068483dcf39cfe0a68e5706887c66bbca73f0
BBG <djydewang@gmail.com>
2017-12-10 21:37:41 +0800

    Issue #5279: ImportOrder: static imports can be separated by local groups when they are at the top/bottom

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrderGroupSeparated.java
@@ -0,0 +1,15 @@
+package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
+
+import static java.lang.Math.abs;
+
+import static java.lang.Math.cos;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import java.awt.color.*;
+import org.junit.Assert.*;
+import org.junit.matchers.*;
+
+public class InputImportOrderGroupSeparated {
+    void method() {
+    }
+}
