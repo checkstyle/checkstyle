1fe23e3391a661608da4a8782b880b65ecae8d2a
Pavel Bludov <pbludov@gmail.com>
2018-03-16 13:25:52 +0800

    config: Test tools with JDK9

--- .ci/eclipse-compiler-javac.sh
+++ .ci/eclipse-compiler-javac.sh
@@ -7,8 +7,8 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-ECJ_JAR="ecj-4.7.jar"
-ECJ_MAVEN_VERSION="R-4.7-201706120950"
+ECJ_JAR="ecj-4.7.2.jar"
+ECJ_MAVEN_VERSION="R-4.7.2-201711300510"
 ECJ_PATH=~/.m2/repository/$ECJ_MAVEN_VERSION/$ECJ_JAR
 
 if [ ! -f $ECJ_PATH ]; then
@@ -23,7 +23,7 @@ mkdir -p target/eclipse
 RESULT_FILE=target/eclipse/report.txt
 
 echo "Executing eclipse compiler, output is redirected to $RESULT_FILE..."
-java -jar $ECJ_PATH -target 1.8 -source 1.8 -cp $1 \
+java -jar $ECJ_PATH -target 9 -source 9 -cp $1 \
         -nowarn:[./target/generated-sources/antlr] -d target/eclipse-compile \
         -enableJavadoc src/main/java src/test/java target/generated-sources/antlr -properties config/org.eclipse.jdt.core.prefs \
     > $RESULT_FILE 2>&1 | true
