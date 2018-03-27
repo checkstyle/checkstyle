d46fa39ce1b6ec5cacdc7c5f0578d19b1e5f4ebb
Taher Ahmed Ghaleb <taher.a.ghaleb@gmail.com>
2018-03-27 10:26:10 -0400

    Adding a comment by Taher
    Just to check the build status

--- src/main/java/com/puppycrawl/tools/checkstyle/Main.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/Main.java
@@ -16,7 +16,7 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
-
+// Just added this to see the current status of the build
 package com.puppycrawl.tools.checkstyle;
 
 import java.io.File;
