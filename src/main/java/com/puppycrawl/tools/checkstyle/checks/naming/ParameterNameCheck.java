e3d4da5b40c251af2623b1b6e8f329edf8c21c00
rnveach <rveach02@gmail.com>
2018-05-27 14:17:13 -0400

    Issue #5750: aligned javadoc/xdoc for ParameterNameCheck

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/ParameterNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/ParameterNameCheck.java
@@ -30,17 +30,34 @@ import com.puppycrawl.tools.checkstyle.utils.ScopeUtils;
 /**
  * <p>
  * Checks that method and {@code catch} parameter names conform to a format specified
- * by the format property. The format is a
- * {@link java.util.regex.Pattern regular expression}
- * and defaults to
- * <strong>^[a-z][a-zA-Z0-9]*$</strong>.
+ * by the format property. By using {@code accessModifiers} property it is possible
+ * to specify different formats for methods at different visibility levels.
  * </p>
- * <p>The check has the following options:</p>
- * <p><b>ignoreOverridden</b> - allows to skip methods with Override annotation from
- * validation. Default values is <b>false</b> .</p>
- * <p><b>accessModifiers</b> - access modifiers of methods which should to be checked.
- * Default value is <b>public, protected, package, private</b> .</p>
+ * <ul>
+ * <li>
+ * Property {@code format} - Specifies valid identifiers. Default value is
+ * {@code "^[a-z][a-zA-Z0-9]*$"}.
+ * </li>
+ * <li>
+ * Property {@code ignoreOverridden} - Allows to skip methods with Override annotation from
+ * validation. For example, the following method's parameter will be skipped from validation,
+ * if ignoreOverridden is true:
+ * <pre>
+ * &#64;Override
+ * public boolean equals(Object o) {
+ *   return super.equals(o);
+ * }
+ * </pre>
+ * Default value is {@code false}.
+ * </li>
+ * <li>
+ * Property {@code accessModifiers} - Access modifiers of methods where parameters are checked.
+ * Default value is {@code public, protected, package, private}.
+ * </li>
+ * </ul>
+ * <p>
  * An example of how to configure the check:
+ * </p>
  * <pre>
  * &lt;module name="ParameterName"/&gt;
  * </pre>
@@ -50,7 +67,7 @@ import com.puppycrawl.tools.checkstyle.utils.ScopeUtils;
  * </p>
  * <pre>
  * &lt;module name="ParameterName"&gt;
- *    &lt;property name="format" value="^[a-z][_a-zA-Z0-9]+$"/&gt;
+ *   &lt;property name="format" value="^[a-z][_a-zA-Z0-9]+$"/&gt;
  * &lt;/module&gt;
  * </pre>
  * <p>
@@ -59,19 +76,54 @@ import com.puppycrawl.tools.checkstyle.utils.ScopeUtils;
  * </p>
  * <pre>
  * &lt;module name="ParameterName"&gt;
- *    &lt;property name="ignoreOverridden" value="true"/&gt;
+ *   &lt;property name="ignoreOverridden" value="true"/&gt;
+ * &lt;/module&gt;
+ * </pre>
+ * <p>
+ * An example of how to configure the check for names that begin with a lower case letter, followed
+ * by letters and digits is:
+ * </p>
+ * <pre>
+ * &lt;module name="ParameterName"&gt;
+ *   &lt;property name="format" value="^[a-z][a-zA-Z0-9]+$"/&gt;
+ * &lt;/module&gt;
+ * </pre>
+ * <p>
+ * The following configuration checks that the parameters always start with two lowercase
+ * characters and, in addition, that public method parameters cannot be one character long:
+ * </p>
+ * <pre>
+ * &lt;module name="ParameterName"&gt;
+ *   &lt;property name="format" value="^[a-z]([a-z0-9][a-zA-Z0-9]*)?$"/&gt;
+ *   &lt;property name="accessModifiers" value="protected, package, private"/&gt;
+ *   &lt;message key="name.invalidPattern"
+ *     value="Parameter name ''{0}'' must match pattern ''{1}''"/&gt;
+ * &lt;/module&gt;
+ * &lt;module name="ParameterName"&gt;
+ *   &lt;property name="format" value="^[a-z][a-z0-9][a-zA-Z0-9]*$"/&gt;
+ *   &lt;property name="accessModifiers" value="public"/&gt;
+ *   &lt;message key="name.invalidPattern"
+ *     value="Parameter name ''{0}'' must match pattern ''{1}''"/&gt;
  * &lt;/module&gt;
  * </pre>
  *
+ * @since 3.0
  */
 public class ParameterNameCheck extends AbstractNameCheck {
 
     /**
-     * Allows to skip methods with Override annotation from validation.
+     * Allows to skip methods with Override annotation from validation. For example, the following
+     * method's parameter will be skipped from validation, if ignoreOverridden is true:
+     * <pre>
+     * &#64;Override
+     * public boolean equals(Object o) {
+     *   return super.equals(o);
+     * }
+     * </pre>
      */
     private boolean ignoreOverridden;
 
-    /** Access modifiers of methods which should be checked. */
+    /** Access modifiers of methods where parameters are checked. */
     private AccessModifier[] accessModifiers = {
         AccessModifier.PUBLIC,
         AccessModifier.PROTECTED,
@@ -87,7 +139,14 @@ public class ParameterNameCheck extends AbstractNameCheck {
     }
 
     /**
-     * Sets whether to skip methods with Override annotation from validation.
+     * Setter to allows to skip methods with Override annotation from validation. For example, the
+     * following method's parameter will be skipped from validation, if ignoreOverridden is true:
+     * <pre>
+     * &#64;Override
+     * public boolean equals(Object o) {
+     *   return super.equals(o);
+     * }
+     * </pre>
      * @param ignoreOverridden Flag for skipping methods with Override annotation.
      */
     public void setIgnoreOverridden(boolean ignoreOverridden) {
@@ -95,7 +154,7 @@ public class ParameterNameCheck extends AbstractNameCheck {
     }
 
     /**
-     * Sets access modifiers of methods which should be checked.
+     * Setter to access modifiers of methods where parameters are checked.
      * @param accessModifiers access modifiers of methods which should be checked.
      */
     public void setAccessModifiers(AccessModifier... accessModifiers) {
