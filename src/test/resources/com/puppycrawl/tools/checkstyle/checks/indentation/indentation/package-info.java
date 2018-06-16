f3e24a6fe41e5f0ac67b72d98b3346f6a25d5f09
Roman Ivanov <ivanov-jr@mail.ru>
2018-06-16 08:02:16 -0700

    minor: resolve eclipse resources compilation problem

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/package-info.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/package-info.java
@@ -1,2 +1,2 @@
  @Deprecated //indent:1 exp:1
- package com.puppycrawl.tools.checkstyle.checks.indentation;//indent:1 exp:0 warn
+ package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;//indent:1 exp:0 warn
