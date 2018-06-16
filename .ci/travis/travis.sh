901584f1e2a93b6a97e670e1d9d3dfdf9f367f7c
Roman Ivanov <ivanov-jr@mail.ru>
2018-06-15 21:15:29 -0700

    Issue #5916: exclude all non-updated files to make process of fix to be continuous

--- .ci/travis/travis.sh
+++ .ci/travis/travis.sh
@@ -69,7 +69,32 @@ javac9)
 
 javac8)
   javac $(grep -REL --include='*.java' \
-            '//non-compiled( with jdk8)?\:' src/test/resources-noncompilable)
+        --exclude='InputSuppressWarningsCompactNonConstant.java' \
+        --exclude='InputSuppressWarningsHolderNonConstant.java' \
+        --exclude='InputIllegalInstantiationNoPackage.java' \
+        --exclude='InputOneStatementPerLine.java' \
+        --exclude='InputPackageDeclarationNoPackage.java' \
+        --exclude='InputFinalClassClassWithPrivateCtorWithNestedExtendingClass.java' \
+        --exclude='InputFinalClassClassWithPrivateCtorWithNestedExtendingClassWithoutPackage.java' \
+        --exclude='InputCustomImportOrderDefaultPackage.java' \
+        --exclude='InputCustomImportOrderNoPackage2.java' \
+        --exclude='InputCustomImportOrderSamePackage.java' \
+        --exclude='InputCustomImportOrderThirdPartyAndSpecial.java' \
+        --exclude='InputCustomImportOrderThirdPartyPackage.java' \
+        --exclude='InputImportOrder_EclipseDefaultNegative.java' \
+        --exclude='InputImportOrder_EclipseDefaultPositive.java' \
+        --exclude='InputImportOrderEclipseStaticCaseSensitive.java' \
+        --exclude='InputImportOrderEclipseStatic.java' \
+        --exclude='InputImportOrderEclipseStaticRepetition.java' \
+        --exclude='InputImportOrderGetGroupNumber.java' \
+        --exclude='InputImportOrder.java' \
+        --exclude='InputImportOrder_MultiplePatternMatches.java' \
+        --exclude='InputModifierOrderTypeAnnotations.java' \
+        --exclude='InputUnusedImportsAnnotations.java' \
+        --exclude='InputIndentationPackageDeclaration2.java' \
+        --exclude='InputSuppressWarningsHolderNonConstant.java' \
+        '//non-compiled( with jdk8)?\:' \
+        src/test/resources-noncompilable)
   ;;
 
 nondex)
