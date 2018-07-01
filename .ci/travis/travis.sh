6c3b76382fa3a2948122001021444968e66accda
Roman Ivanov <ivanov-jr@mail.ru>
2018-06-30 17:57:37 -0700

    config: in travis use version 1.8.1 instead of hash to avoid update in whitelist

--- .ci/travis/travis.sh
+++ .ci/travis/travis.sh
@@ -179,7 +179,7 @@ no-error-test-sbe)
   cd .ci-temp/
   git clone https://github.com/real-logic/simple-binary-encoding.git
   cd simple-binary-encoding
-  git checkout dbf2031ee0a2d081afe306655a5acc5b6f2f020b
+  git checkout 1.8.1
   sed -i'' \
     "s/'com.puppycrawl.tools:checkstyle:.*'/'com.puppycrawl.tools:checkstyle:$CS_POM_VERSION'/" \
     build.gradle
