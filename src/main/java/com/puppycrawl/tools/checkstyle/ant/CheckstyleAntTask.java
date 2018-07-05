0affb3fea901fa1d1fffb4c1b4b38b46e0369481
Roman Ivanov <ivanov-jr@mail.ru>
2018-07-05 00:52:42 -0700

    Issue #6001: print no version if it is null

--- src/main/java/com/puppycrawl/tools/checkstyle/ant/CheckstyleAntTask.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/ant/CheckstyleAntTask.java
@@ -29,6 +29,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
@@ -335,7 +336,9 @@ public class CheckstyleAntTask extends Task {
         log("To locate the files took " + (endTime - startTime) + TIME_SUFFIX,
             Project.MSG_VERBOSE);
 
-        log("Running Checkstyle " + checkstyleVersion + " on " + files.size()
+        log("Running Checkstyle "
+                + Objects.toString(checkstyleVersion, "")
+                + " on " + files.size()
                 + " files", Project.MSG_INFO);
         log("Using configuration " + config, Project.MSG_VERBOSE);
 
