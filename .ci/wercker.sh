d868144fdae6d610e3ed1b06054fe4da6ea7c82d
Roman Ivanov <ivanov-jr@mail.ru>
2018-10-26 07:47:04 -0700

    Issue #6167: Add equalsverifier project to wrecker ci

--- .ci/wercker.sh
+++ .ci/wercker.sh
@@ -77,6 +77,17 @@ no-error-apex-core)
   rm -rf incubator-apex-core
   ;;
 
+no-error-equalsverifier)
+  CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
+                     --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+  echo CS_version: ${CS_POM_VERSION}
+  checkout_from https://github.com/jqno/equalsverifier.git
+  cd .ci-temp/equalsverifier
+  mvn -e compile checkstyle:check -Dcheckstyle.version=${CS_POM_VERSION}
+  cd ../
+  rm -rf equalsverifier
+  ;;
+
 no-error-hibernate-search)
   CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
                      --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
