2debb86e9a52750f3ffdfc2dd58bde70938dd5a2
Pavel Bludov <pbludov@yandex-team.ru>
2018-12-09 12:55:18 +0300

    Issue #5759: Update AbstractParenPadCheck to log DetailAST

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/parenpad/SuppressionXpathRegressionParenPadRightPreceded.java
@@ -0,0 +1,10 @@
+package org.checkstyle.suppressionxpathfilter.parenpad;
+
+public class SuppressionXpathRegressionParenPadRightPreceded {
+    void method() {
+        if (false ) {//warn
+        }
+        if (true) {
+        }
+    }
+}
