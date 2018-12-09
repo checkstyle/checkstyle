4eacbd86af80fd3a7d1f2c13dbce05ba5526ac30
rnveach <rveach02@gmail.com>
2018-12-09 16:47:58 -0500

    Issue #4684: added shell script to find missing pitest files

--- .ci/travis/travis.sh
+++ .ci/travis/travis.sh
@@ -342,6 +342,47 @@ no-exception-test-alot-of-project1)
       --config checks-nonjavadoc-error.xml --checkstyleVersion $CS_POM_VERSION
   ;;
 
+check-missing-pitests)
+  fail=0
+  mkdir -p target
+
+  list=($(cat pom.xml | \
+    xmlstarlet sel --ps -N pom="http://maven.apache.org/POM/4.0.0" \
+    -t -v '//pom:profile[./pom:id[contains(text(),'pitest')]]//pom:targetClasses/pom:param'))
+
+  CMD="find src/main/java -type f ! -name 'package-info.java'"
+
+  for item in ${list[@]}
+  do
+    item=${item//\./\/}
+    if [[ $item == */\*  ]] ; then
+     item=$item
+    else
+      if [[ $item != *\* ]] ; then
+        item="$item.java"
+      else
+        item="${item::-1}.java"
+      fi
+    fi
+
+    CMD="$CMD -and ! -wholename '*/$item'"
+  done
+
+  CMD="$CMD | sort > target/result.txt"
+  eval $CMD
+
+  results=`cat target/result.txt`
+
+  echo "List of missing files in pitest profiles: $results"
+
+  if [[ -n $results ]] ; then
+    fail=1
+  fi
+
+  sleep 5s
+  exit $fail
+  ;;
+
 *)
   echo "Unexpected argument: $1"
   sleep 5s
