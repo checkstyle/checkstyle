39c75642c792e0316601f76e0bb09caa3d2a63bf
rnveach <rveach02@gmail.com>
2018-12-19 19:16:40 -0500

    Issue #6272: added FALSE_RETURNS pitest mutator

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalinstantiation/InputIllegalInstantiationSameClassNameJavaLang.java
@@ -0,0 +1,8 @@
+package com.puppycrawl.tools.checkstyle.checks.coding.illegalinstantiation;
+
+public class InputIllegalInstantiationSameClassNameJavaLang
+{
+    InputTest obj = new InputTest();
+}
+class InputTest {
+}
