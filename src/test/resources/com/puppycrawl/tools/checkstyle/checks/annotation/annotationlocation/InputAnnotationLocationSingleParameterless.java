ead01c2c56b7e04425cd1ff88bca6a340c634285
Björn Kautler <Bjoern@Kautler.net>
2019-03-07 19:16:39 +0100

    Issue #6463: Only handle fields for VARIABLE_DEF in AnnotationLocation check, not local variables

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationlocation/InputAnnotationLocationSingleParameterless.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationlocation/InputAnnotationLocationSingleParameterless.java
@@ -28,9 +28,9 @@ class InputAnnotationLocationSingleParameterless {
 
     void parameterlessSamelineInForEach() {
         for (@Annotation Object o : new Object[0]) break; //ok
-        for (@Annotation @Annotation Object o : new Object[0]) break; //warn
+        for (@Annotation @Annotation Object o : new Object[0]) break; //ok
         for (@Annotation Object o;;) break; // ok
-        for (@Annotation @Annotation Object o;;) break; //warn
+        for (@Annotation @Annotation Object o;;) break; //ok
     }
 
     @Repeatable(Annotations.class)
