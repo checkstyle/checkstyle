6ce4c943528932522255e932ac6d2333f0dc75b1
pbludov <pbludov@gmail.com>
2019-03-15 04:21:57 +0300

    Issue #6586: aligned javadoc/xdoc for ArrayTrailingComma

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/ArrayTrailingCommaCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/ArrayTrailingCommaCheck.java
@@ -26,13 +26,16 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 /**
  * <p>
- * Checks if array initialization contains optional trailing comma.
+ * Checks that array initialization contains a trailing comma.
  * </p>
- * <p>
- * Rationale: Putting this comma in make is easier to change the
- * order of the elements or add new elements on the end. Main benefit of a trailing
- * comma is that when you add new entry to an array, no surrounding lines are changed.
- * </p>
+ * <pre>
+ * int[] a = new int[]
+ * {
+ *   1,
+ *   2,
+ *   3,
+ * };
+ * </pre>
  * <p>
  * The check demands a comma at the end if neither left nor right curly braces
  * are on the same line as the last element of the array.
@@ -40,34 +43,39 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * <pre>
  * return new int[] { 0 };
  * return new int[] { 0
- *     };
+ *   };
  * return new int[] {
- *     0 };
+ *   0 };
  * </pre>
+ * <p>
+ * Rationale: Putting this comma in makes it easier to change the
+ * order of the elements or add new elements on the end. Main benefit of a trailing
+ * comma is that when you add new entry to an array, no surrounding lines are changed.
+ * </p>
  * <pre>
  * {
- *     100000000000000000000,
- *     200000000000000000000, // OK
+ *   100000000000000000000,
+ *   200000000000000000000, // OK
  * }
  *
  * {
- *     100000000000000000000,
- *     200000000000000000000,
- *     300000000000000000000,  // Just this line added, no other changes
+ *   100000000000000000000,
+ *   200000000000000000000,
+ *   300000000000000000000,  // Just this line added, no other changes
  * }
  * </pre>
  * <p>
  * If closing brace is on the same line as training comma, this benefit is gone
- * (as the Check does not demand a certain location of curly braces the following
+ * (as the check does not demand a certain location of curly braces the following
  * two cases will not produce a violation):
  * </p>
  * <pre>
  * {100000000000000000000,
- *     200000000000000000000,} // Trailing comma not needed, line needs to be modified anyway
+ *  200000000000000000000,} // Trailing comma not needed, line needs to be modified anyway
  *
  * {100000000000000000000,
- *     200000000000000000000, // Modified line
- *     300000000000000000000,} // Added line
+ *  200000000000000000000, // Modified line
+ *  300000000000000000000,} // Added line
  * </pre>
  * <p>
  * If opening brace is on the same line as training comma there's also (more arguable) problem:
@@ -77,15 +85,53 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * }
  *
  * {100000000000000000000,
- *     100000000000000000001, // More work needed to duplicate
+ *  100000000000000000001, // More work needed to duplicate
  * }
  * </pre>
  * <p>
- * An example of how to configure the check is:
+ * To configure the check:
  * </p>
  * <pre>
- * &lt;module name="ArrayTrailingComma"/&gt;
+ * &lt;module name=&quot;ArrayTrailingComma&quot;/&gt;
  * </pre>
+ * <p>
+ * Which results in the following violations:
+ * </p>
+ * <pre>
+ * int[] numbers = {1, 2, 3};        //no violation
+ * boolean[] bools = {
+ * true,
+ * true,
+ * false
+ * };        //violation
+ *
+ * String[][] text = {{},{},};        //no violation
+ *
+ * double[][] decimals = {
+ * {0.5, 2.3, 1.1,},        //no violation
+ * {1.7, 1.9, 0.6},
+ * {0.8, 7.4, 6.5}
+ * };        // violation as previous line misses a comma
+ *
+ * char[] chars = {'a', 'b', 'c'
+ *   };        / /no violation
+ *
+ * String[] letters = {
+ *   "a", "b", "c"};        // no violation
+ *
+ * int[] a1 = new int[]{
+ *   1,
+ *   2
+ *   ,
+ * };        // no violation
+ *
+ * int[] a2 = new int[]{
+ *   1,
+ *   2
+ *   ,};        // no violation
+ * </pre>
+ *
+ * @since 3.2
  */
 @StatelessCheck
 public class ArrayTrailingCommaCheck extends AbstractCheck {
