e7a8e2093411007ffe09eaa13eed4e396a325926
pbludov <pbludov@gmail.com>
2019-03-15 05:28:32 +0300

    Issue #6586: aligned javadoc/xdoc for DefaultComesLast

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/DefaultComesLastCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/DefaultComesLastCheck.java
@@ -28,20 +28,56 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 /**
  * <p>
- * Check that the {@code default} is after all the {@code case}s
- * in a {@code switch} statement.
+ * Check that the {@code default} is after all the cases in a {@code switch} statement.
  * </p>
  * <p>
  * Rationale: Java allows {@code default} anywhere within the
- * {@code switch} statement. But if it comes after the last
- * {@code case} then it is more readable.
+ * {@code switch} statement. But it is more readable if it comes after the last {@code case}.
  * </p>
+ * <ul>
+ * <li>
+ * Property {@code skipIfLastAndSharedWithCase} - Control whether to allow {@code default}
+ * along with {@code case} if they are not last.
+ * Default value is {@code false}.
+ * </li>
+ * </ul>
  * <p>
- * An example of how to configure the check is:
+ * To configure the check:
  * </p>
  * <pre>
- * &lt;module name="DefaultComesLast"/&gt;
+ * &lt;module name=&quot;DefaultComesLast&quot;/&gt;
  * </pre>
+ * <p>
+ * To configure the check for skipIfLastAndSharedWithCase:
+ * </p>
+ * <pre>
+ * &lt;module name=&quot;DefaultComesLast&quot;&gt;
+ *   &lt;property name=&quot;skipIfLastAndSharedWithCase&quot; value=&quot;true&quot;/&gt;
+ * &lt;/module&gt;
+ * </pre>
+ * <p>
+ * Example when skipIfLastAndSharedWithCase is set to true.
+ * </p>
+ * <pre>
+ * switch (i) {
+ *   case 1:
+ *     break;
+ *   case 2:
+ *   default: // No violation with the new option is expected
+ *     break;
+ *   case 3:
+ *     break;
+ * }
+ * switch (i) {
+ *   case 1:
+ *     break;
+ *   default: // violation with the new option is expected
+ *   case 2:
+ *     break;
+ * }
+ * </pre>
+ *
+ * @since 3.4
  */
 @StatelessCheck
 public class DefaultComesLastCheck extends AbstractCheck {
@@ -59,7 +95,7 @@ public class DefaultComesLastCheck extends AbstractCheck {
     public static final String MSG_KEY_SKIP_IF_LAST_AND_SHARED_WITH_CASE =
             "default.comes.last.in.casegroup";
 
-    /** Whether to process skipIfLastAndSharedWithCaseInSwitch() invocations. */
+    /** Control whether to allow {@code default} along with {@code case} if they are not last. */
     private boolean skipIfLastAndSharedWithCase;
 
     @Override
@@ -80,7 +116,8 @@ public class DefaultComesLastCheck extends AbstractCheck {
     }
 
     /**
-     * Whether to allow default keyword not in last but surrounded with case.
+     * Setter to control whether to allow {@code default} along with
+     * {@code case} if they are not last.
      * @param newValue whether to ignore checking.
      */
     public void setSkipIfLastAndSharedWithCase(boolean newValue) {
