6a072b25d38e41807cd0388d4b911d46e8a30606
pbludov <pbludov@gmail.com>
2019-05-01 18:26:25 +0300

    Issue #6725: aligned javadoc/xdoc for HideUtilityClassConstructor

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
@@ -96,6 +96,7 @@ public class XdocsJavaDocsTest extends AbstractModuleTestSupport {
         "FinalClass",
         "FinalLocalVariable",
         "HiddenField",
+        "HideUtilityClassConstructor",
         "IllegalCatch",
         "IllegalInstantiation",
         "IllegalThrows",
