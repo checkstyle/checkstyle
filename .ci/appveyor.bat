6111ce5705b5ccfdfc5c078146d0c608d59951bc
ahmadshabib <ahmadshabib94@gmail.com>
2019-05-12 18:58:43 +0300

    Issue #6696: Create appveyor batch file

---
+++ .ci/appveyor.bat
@@ -0,0 +1,60 @@
+@echo off
+
+::----------------------------------------------------------------------
+:: Appveyor bat file to run on local for windows users.
+:: Example of usage
+:: ./.ci/appveyor.bat  verify_without_checkstyle
+::----------------------------------------------------------------------
+
+SET OPTION=%1
+
+if "%OPTION%" == "sevntu" (
+mvn -e verify -DskipTests -DskipITs -Dpmd.skip=true^
+ -Dspotbugs.skip=true -Djacoco.skip=true -Dxml.skip=true
+    goto :END_CASE
+)
+
+if "%OPTION%" ==  "verify_without_checkstyle" (
+mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true
+  goto :END_CASE
+)
+
+if "%OPTION%" ==  "verify_without_checkstyle_JDK11" (
+mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true -Dspotbugs.skip=true
+  goto :END_CASE
+)
+:: Spotbugs check is disabled till https://github.com/spotbugs/spotbugs/issues/259
+:: powermock doesn't support modifying final fields in JDK12
+if "%OPTION%" ==  "verify_without_checkstyle_JDK12" (
+mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true -Dspotbugs.skip=true^
+ -Dtest=!FileContentsTest#testGetJavadocBefore,!FileTextTest#testFindLine*,^
+!MainFrameModelPowerTest#testOpenFileWithUnknownParseMode,^
+!TokenUtilTest#testTokenValueIncorrect2,^
+!ImportControlLoaderPowerTest#testInputStreamThatFailsOnClose^
+ -Djacoco.skip=true
+  goto :END_CASE
+)
+
+if "%OPTION%" ==  "site_without_verify" (
+mvn -e -Pno-validations site
+  goto :END_CASE
+)
+
+:: Spotbugs check is disabled till https://github.com/spotbugs/spotbugs/issues/259
+:: powermock doesn't support modifying final fields in JDK12
+if "%OPTION%" == "site_without_verify_jdk12" (
+mvn -e -Pno-validations site^
+ -Dtest=!FileContentsTest#testGetJavadocBefore,!FileTextTest#testFindLine*,^
+!MainFrameModelPowerTest#testOpenFileWithUnknownParseMode,^
+!TokenUtilTest#testTokenValueIncorrect2,^
+!ImportControlLoaderPowerTest#testInputStreamThatFailsOnClose^
+ -Djacoco.skip=true
+  goto :END_CASE
+)
+
+echo  Unexpected argument %OPTION%
+goto :END_CASE
+
+:END_CASE
+  VER > NUL
+  EXIT
