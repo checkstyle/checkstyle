23cc42b707cd361f5220424f9e29736ae8878314
Andrei Paikin <andreypaykin@gmail.com>
2019-05-21 15:09:17 +0300

    Issue #6774: Add UnnecessarySemicolonInTryWithResourcesCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unnecessarysemicolonintrywithresources/InputUnnecessarySemicolonInTryWithResourcesNoBraceAfterAllowed.java
@@ -0,0 +1,23 @@
+package com.puppycrawl.tools.checkstyle.checks.coding.unnecessarysemicolonintrywithresources;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.Reader;
+
+/**
+ * "allowWhenNoBraceAfterSemicolon" = false
+ */
+public class InputUnnecessarySemicolonInTryWithResourcesNoBraceAfterAllowed {
+
+    void method() throws IOException {
+        try(Reader r1 = new PipedReader();){} // violation
+        try(Reader r2 = new PipedReader();Reader r3 = new PipedReader()){}
+        try(Reader r4 = new PipedReader();Reader r5 = new PipedReader()
+            ;){} // violation
+        try(Reader r6 = new PipedReader();
+            Reader r7
+                = new PipedReader(); // violation
+            ){}
+
+    }
+}
