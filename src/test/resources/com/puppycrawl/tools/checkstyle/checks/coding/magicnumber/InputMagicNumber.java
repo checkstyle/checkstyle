1f30d57b3d6e7691cf05255a3477ea624ca83e13
Andrei Paikin <andreypaykin@gmail.com>
2019-06-20 11:44:10 +0300

    Issue #4383: add ignoreAnnotationElementDefaults property in MagicNumber

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumber.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumber.java
@@ -214,3 +214,15 @@ class Binary {
     int intValue = 0b101;
     long longValue = 0b1010000101000101101000010100010110100001010001011010000101000101L;
 }
+@interface AnnotationWithDefaultValue {
+    int value() default 101;
+    int[] ar() default {102};
+}
+class A {
+    {
+        switch (Blah2.LOW) {
+        default:
+            int b = 122; // violation
+        }
+    }
+}
