7584ddc382de25a2438f35aa27aa4fe1c4fa0616
Roman Ivanov <ivanov-jr@mail.ru>
2019-06-22 20:43:22 -0700

    config: update copy-site-to-sourceforge.sh to avoid conflixt between 'cat' commands

--- .ci/copy-site-to-sourceforge.sh
+++ .ci/copy-site-to-sourceforge.sh
@@ -49,10 +49,10 @@ mv htdocs htdocs-$PREV_RELEASE
 mv checkstyle.github.io htdocs
 
 echo "create .htaccess for dtds redirection"
-cat <<EOF >> htdocs/.htaccess
+cat <<EOA >> htdocs/.htaccess
 Redirect 301 "/dtds" "https://checkstyle.org/dtds"
 RedirectMatch 301 "/version/.*/dtds/(.*)" "https://checkstyle.org/dtds/\$1"
-EOF
+EOA
 chmod o+r htdocs/.htaccess
 
 ln -s /home/project-web/checkstyle/reports htdocs/reports
