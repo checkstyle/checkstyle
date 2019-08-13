5cab933b0f4a932cf21d8d0bec2300eaed108715
pbludov <pbludov@gmail.com>
2019-08-13 12:54:23 +0300

    Issue #6966: aligned javadoc/xdoc for AvoidEscapedUnicodeCharacters

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
@@ -98,7 +98,6 @@ public class XdocsJavaDocsTest extends AbstractModuleTestSupport {
         "JavaNCSS",
         "NPathComplexity",
         // miscellaneous
-        "AvoidEscapedUnicodeCharacters",
         "CommentsIndentation",
         "DescendantToken",
         "Indentation",
