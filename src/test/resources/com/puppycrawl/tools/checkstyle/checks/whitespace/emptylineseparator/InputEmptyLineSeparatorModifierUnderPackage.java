52a20827fd243de91654dd7b7fc190b2f434e486
Andrei Paikin <andreypaykin@gmail.com>
2019-08-19 14:14:27 +0300

    Issue #4763: fix empty statement for comment under package

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorModifierUnderPackage.java
@@ -0,0 +1,13 @@
+/*
+ * Config = default
+ * tokens = PACKAGE_DEF
+ * tokens = IMPORT, STATIC_IMPORT, CLASS_DEF, INTERFACE_DEF, ENUM_DEF, STATIC_INIT,
+ * tokens = INSTANCE_INIT, METHOD_DEF, CTOR_DEF, VARIABLE_DEF
+ * allowNoEmptyLineBetweenFields = false
+ * allowMultipleEmptyLines = true
+ * allowMultipleEmptyLinesInsideClassMembers = true
+ */
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator;
+public class InputEmptyLineSeparatorModifierUnderPackage {
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorSingleCommentUnderPackage.java
@@ -0,0 +1,16 @@
+/*
+ * Config = default
+ * tokens = PACKAGE_DEF
+ * tokens = IMPORT, STATIC_IMPORT, CLASS_DEF, INTERFACE_DEF, ENUM_DEF, STATIC_INIT,
+ * tokens = INSTANCE_INIT, METHOD_DEF, CTOR_DEF, VARIABLE_DEF
+ * allowNoEmptyLineBetweenFields = false
+ * allowMultipleEmptyLines = true
+ * allowMultipleEmptyLinesInsideClassMembers = true
+ */
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator;
+// warn
+
+public class InputEmptyLineSeparatorSingleCommentUnderPackage
+{
+}
