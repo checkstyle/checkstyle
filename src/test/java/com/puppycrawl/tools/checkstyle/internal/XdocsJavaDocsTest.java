41a824153735c2139b1766f576476a8fc717e168
pbludov <pbludov@gmail.com>
2019-08-21 22:55:45 +0300

    Issue #6966: aligned javadoc/xdoc for UniqueProperties

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
@@ -104,7 +104,6 @@ public class XdocsJavaDocsTest extends AbstractModuleTestSupport {
         "TodoComment",
         "TrailingComment",
         "Translation",
-        "UniqueProperties",
         "UpperEll",
         // modifier
         "AnonInnerLength",
