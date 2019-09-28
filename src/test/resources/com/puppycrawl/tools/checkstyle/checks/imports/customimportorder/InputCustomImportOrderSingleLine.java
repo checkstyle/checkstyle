f3b18df006b932fee323063fca2021dac7c4075a
Roman Ivanov <ivanov-jr@mail.ru>
2019-09-28 06:47:19 -0700

    minor: update in UTs for upcomming fix for #6680

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderSingleLine.java
@@ -0,0 +1,15 @@
+package com.puppycrawl.tools.checkstyle.checks.imports.customimportorder; import static java.awt.Button.ABORT; import java.util.Map; import java.util.Map.Entry;
+import com.google.common.annotations.Beta; import com.google.common.collect.HashMultimap;
+
+import org.junit.rules.*;
+import org.junit.runner.*;
+import org.junit.validator.*;
+
+
+import com.puppycrawl.tools.*;
+
+
+
+import antlr.*;
+
+class InputCustomImportOrderSingleLine {}
