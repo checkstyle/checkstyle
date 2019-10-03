7289d9a6b851b55cb60b854424839f747d43ee1c
pbludov <pbludov@gmail.com>
2019-10-03 10:48:55 +0300

    Issue #7144: aligned javadoc/xdoc for AvoidStarImport

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
@@ -84,7 +84,6 @@ public class XdocsJavaDocsTest extends AbstractModuleTestSupport {
         "Header",
         "RegexpHeader",
         // imports
-        "AvoidStarImport",
         "AvoidStaticImport",
         "IllegalImport",
         "ImportControl",
