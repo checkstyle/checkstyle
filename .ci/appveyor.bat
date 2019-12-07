a164e3ce0e2fa5c5aa84a89c9b0d363991822b90
Roman Ivanov <ivanov-jr@mail.ru>
2019-12-06 21:01:01 -0800

    Issue #7330: return exit code from appveyor.bat

--- .ci/appveyor.bat
+++ .ci/appveyor.bat
@@ -9,34 +9,44 @@
 SET OPTION=%1
 
 if "%OPTION%" == "sevntu" (
-mvn -e verify -DskipTests -DskipITs -Dpmd.skip=true^
- -Dspotbugs.skip=true -Djacoco.skip=true -Dxml.skip=true
-    goto :END_CASE
+  call mvn -e verify -DskipTests -DskipITs -Dpmd.skip=true^
+    -Dspotbugs.skip=true -Djacoco.skip=true -Dxml.skip=true^
+    || goto :ERROR
+  goto :END_CASE
 )
 
 if "%OPTION%" ==  "verify_without_checkstyle" (
-mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true
+  call mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true^
+    || goto :ERROR
   goto :END_CASE
 )
 
 if "%OPTION%" ==  "verify_without_checkstyle_JDK11" (
-mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true
+  call mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true^
+    || goto :ERROR
   goto :END_CASE
 )
 
 if "%OPTION%" ==  "verify_without_checkstyle_JDK13" (
-  mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true
+  call mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true^
+    || goto :ERROR
   goto :END_CASE
 )
 
 if "%OPTION%" ==  "site_without_verify" (
-mvn -e -Pno-validations site
+  call mvn -e -Pno-validations site^
+    || goto :ERROR
   goto :END_CASE
 )
 
 echo  Unexpected argument %OPTION%
+SET ERRORLEVEL=-1
 goto :END_CASE
 
 :END_CASE
   VER > NUL
-  EXIT
+  EXIT /b %ERRORLEVEL%
+
+:ERROR
+echo appveyor.bat failed with error code #%ERRORLEVEL%.
+exit /b %ERRORLEVEL%
