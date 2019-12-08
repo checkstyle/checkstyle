65d1084528506fcd917bd26c6f1cb4027bc7c782
pbludov <pbludov@gmail.com>
2019-12-08 20:11:45 +0300

    Issue #6916: migrate tests to junit5 for package grammar

--- src/test/java/com/puppycrawl/tools/checkstyle/grammar/Java7StringSwitchTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammar/Java7StringSwitchTest.java
@@ -19,7 +19,7 @@
 
 package com.puppycrawl.tools.checkstyle.grammar;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
 import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
--- src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocParseTreeTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocParseTreeTest.java
@@ -22,7 +22,7 @@ package com.puppycrawl.tools.checkstyle.grammar.javadoc;
 import java.io.File;
 import java.io.IOException;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractTreeTestSupport;
 
