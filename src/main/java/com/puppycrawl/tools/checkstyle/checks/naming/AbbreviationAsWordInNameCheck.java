ea7d3db9cad0ee9cffcd488ef22348510e0c6e90
pbludov <pbludov@gmail.com>
2019-12-26 23:48:59 +0300

    Issue #5777: Update AbbreviationAsWordInName to log DetailAST

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
@@ -290,7 +290,7 @@ public class AbbreviationAsWordInNameCheck extends AbstractCheck {
 
             final String abbr = getDisallowedAbbreviation(typeName);
             if (abbr != null) {
-                log(nameAst.getLineNo(), MSG_KEY, typeName, allowedAbbreviationLength + 1);
+                log(nameAst, MSG_KEY, typeName, allowedAbbreviationLength + 1);
             }
         }
     }
