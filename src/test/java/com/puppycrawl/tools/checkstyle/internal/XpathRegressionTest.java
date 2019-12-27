44485ef5ce629ad13f6bcecd0cf0298bb0e26439
pbludov <pbludov@gmail.com>
2019-12-27 22:35:32 +0300

    Issue #7405: all new checks should have XpathRegressionTest

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
@@ -19,6 +19,7 @@
 
 package com.puppycrawl.tools.checkstyle.internal;
 
+import static com.google.common.truth.Truth.assertWithMessage;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -28,6 +29,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
@@ -41,12 +43,83 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+import com.puppycrawl.tools.checkstyle.checks.javadoc.AbstractJavadocCheck;
 import com.puppycrawl.tools.checkstyle.internal.utils.CheckUtil;
 
 public class XpathRegressionTest extends AbstractModuleTestSupport {
 
-    // Temporal Checks that allowed to have no XPath IT Regression Testing
-    // https://github.com/checkstyle/checkstyle/issues/6207
+    // Checks that not compatible with SuppressionXpathFilter
+    // till https://github.com/checkstyle/checkstyle/issues/5777
+    public static final Set<String> INCOMPATIBLE_CHECK_NAMES =
+        Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+            "AnnotationLocation",
+            "AnnotationOnSameLine",
+            "AnnotationUseStyle",
+            "ArrayTrailingComma",
+            "AvoidEscapedUnicodeCharacters",
+            "AvoidStarImport",
+            "AvoidStaticImport",
+            "CommentsIndentation",
+            "CustomImportOrder",
+            "EmptyCatchBlock",
+            "EmptyLineSeparator",
+            "FinalClass",
+            "IllegalCatch",
+            "ImportOrder",
+            "Indentation",
+            "InterfaceIsType",
+            "InterfaceMemberImpliedModifier",
+            "InvalidJavadocPosition",
+            "JavadocContentLocation",
+            "JavadocMethod",
+            "JavadocStyle",
+            "JavadocType",
+            "LambdaParameterName",
+            "MethodCount",
+            "MissingCtor",
+            "MissingJavadocMethod",
+            "MissingJavadocPackage",
+            "MissingJavadocType",
+            "MissingOverride",
+            "MissingSwitchDefault",
+            "NeedBraces",
+            "NoClone",
+            "NoFinalizer",
+            "NoLineWrap",
+            "OneTopLevelClass",
+            "OuterTypeFilename",
+            "OverloadMethodsDeclarationOrder",
+            "PackageAnnotation",
+            "PackageDeclaration",
+            "Regexp",
+            "RegexpSinglelineJava",
+            "SuppressWarningsHolder",
+            "TodoComment",
+            "TrailingComment",
+            "UncommentedMain",
+            "UnnecessaryParentheses",
+            "VariableDeclarationUsageDistance",
+            "WriteTag"
+    )));
+
+    // Javadoc checks are not compatible with SuppressionXpathFilter
+    // till https://github.com/checkstyle/checkstyle/issues/5770
+    // then all of them should be added to the list of incompatible checks
+    // and this field should be removed
+    public static final Set<String> INCOMPATIBLE_JAVADOC_CHECK_NAMES =
+            Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+                    "AtclauseOrder",
+                    "JavadocBlockTagLocation",
+                    "JavadocParagraph",
+                    "JavadocTagContinuationIndentation",
+                    "MissingDeprecated",
+                    "NonEmptyAtclauseDescription",
+                    "SingleLineJavadoc",
+                    "SummaryJavadoc"
+    )));
+
+    // Checks that allowed to have no XPath IT Regression Testing
+    // till https://github.com/checkstyle/checkstyle/issues/6207
     private static final Set<String> MISSING_CHECK_NAMES = new HashSet<>(Arrays.asList(
             "BooleanExpressionComplexity",
             "CatchParameterName",
@@ -138,7 +211,20 @@ public class XpathRegressionTest extends AbstractModuleTestSupport {
     }
 
     @Test
+    public void validateIncompatibleJavadocCheckNames() throws IOException {
+        final Set<Class<?>> abstractJavadocCheckNames = CheckUtil.getCheckstyleChecks()
+                .stream()
+                .filter(AbstractJavadocCheck.class::isAssignableFrom)
+                .collect(Collectors.toSet());
+        assertWithMessage("INCOMPATIBLE_JAVADOC_CHECK_NAMES should contains all descendants "
+                    + "of AbstractJavadocCheck")
+            .that(CheckUtil.getSimpleNames(abstractJavadocCheckNames))
+            .isEqualTo(INCOMPATIBLE_JAVADOC_CHECK_NAMES);
+    }
+
+    @Test
     public void validateIntegrationTestClassNames() throws Exception {
+        final Set<String> compatibleChecks = new HashSet<>();
         final Pattern pattern = Pattern.compile("^XpathRegression(.+)Test\\.java$");
         try (DirectoryStream<Path> javaPaths = Files.newDirectoryStream(javaDir)) {
             for (Path path : javaPaths) {
@@ -159,8 +245,24 @@ public class XpathRegressionTest extends AbstractModuleTestSupport {
                 assertFalse(MISSING_CHECK_NAMES.contains(check),
                         "Check '" + check + "' is now tested. Please update the todo list in"
                                 + " XpathRegressionTest.MISSING_CHECK_NAMES");
+                assertFalse(INCOMPATIBLE_CHECK_NAMES.contains(check),
+                        "Check '" + check + "' is now compatible with SuppressionXpathFilter."
+                                + " Please update the todo list in"
+                                + " XpathRegressionTest.INCOMPATIBLE_CHECK_NAMES");
+                compatibleChecks.add(check);
             }
         }
+
+        // Ensure that all lists are up to date
+        final Set<String> allChecks = new HashSet<>(simpleCheckNames);
+        allChecks.removeAll(INCOMPATIBLE_JAVADOC_CHECK_NAMES);
+        allChecks.removeAll(INCOMPATIBLE_CHECK_NAMES);
+        allChecks.removeAll(MISSING_CHECK_NAMES);
+        allChecks.removeAll(compatibleChecks);
+
+        assertTrue(allChecks.isEmpty(), "XpathRegressionTest is missing for ["
+                + String.join(", ", allChecks)
+                + "]. Please add them to src/it/java/org/checkstyle/suppressionxpathfilter");
     }
 
     @Test
