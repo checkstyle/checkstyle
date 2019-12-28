3f3e4a24724a1b3f759b3bba5c343c20666bc09f
Roman Ivanov <ivanov-jr@mail.ru>
2019-12-27 16:38:51 -0800

    config: move all content of 'travis.sh releasenotes-gen' to .ci-temp folder

--- .ci/travis/xtr_releasenotes-gen.sh
+++ .ci/travis/xtr_releasenotes-gen.sh
@@ -13,37 +13,43 @@ if [[ $TRAVIS_PULL_REQUEST =~ ^([0-9]*)$ ]]; then
   exit 0;
 fi
 
-if [ -d contribution ]; then
-  cd contribution/
+mkdir -p .ci-temp
+if [ -d .ci-temp/contribution ]; then
+  cd .ci-temp/contribution/
   git reset --hard origin/master
   git pull origin master
   git fetch --tags
-  cd ../
+  cd ../../
 else
+  cd .ci-temp/
   git clone https://github.com/checkstyle/contribution
+  cd ../
 fi
-cd contribution/releasenotes-builder
+cd .ci-temp/contribution/releasenotes-builder
 mvn -e clean compile package
-cd ../../
+cd ../../../
 
 # we need to do full clone as Travis do "git clone --depth=50"
-if [ -d checkstyle ]; then
-  cd checkstyle/
+if [ -d .ci-temp/checkstyle ]; then
+  cd .ci-temp/checkstyle/
   git reset --hard origin/master
   git pull origin master
   git fetch --tags
-  cd ../
+  cd ../../
 else
+  cd .ci-temp/
   git clone https://github.com/checkstyle/checkstyle
+  cd ../
 fi
-cd checkstyle
+cd .ci-temp/checkstyle
 LATEST_RELEASE_TAG=$(git describe $(git rev-list --tags --max-count=1))
-cd ../
+cd ../../
 
 CS_RELEASE_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
               --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec | sed 's/-SNAPSHOT//' )
 echo LATEST_RELEASE_TAG=$LATEST_RELEASE_TAG
 echo CS_RELEASE_VERSION=$CS_RELEASE_VERSION
+cd .ci-temp
 java -jar contribution/releasenotes-builder/target/releasenotes-builder-1.0-all.jar \
         -localRepoPath checkstyle -remoteRepoPath checkstyle/checkstyle \
         -startRef $LATEST_RELEASE_TAG -releaseNumber $CS_RELEASE_VERSION \
