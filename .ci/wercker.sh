b7eb0e8cd197add5ce6820a0740d34928b20ea7d
Roman Ivanov <ivanov-jr@mail.ru>
2020-01-07 17:06:28 -0800

    Issue #5604: activation of sonar validation in PRs

--- .ci/wercker.sh
+++ .ci/wercker.sh
@@ -20,6 +20,34 @@ function checkout_from {
 
 case $1 in
 
+sonarqube)
+  # token could be generated at https://sonarcloud.io/account/security/
+  # executon on local:
+  # SONAR_TOKEN=xxxxxx PR=xxxxxx WERCKER_GIT_BRANCH=xxxxxx ./.ci/travis/travis.sh sonarqube
+  if [[ $PR && $PR =~ ^([0-9]*)$ ]]; then
+      SONAR_PR_VARIABLES="-Dsonar.pullrequest.key=$PR"
+      SONAR_PR_VARIABLES+=" -Dsonar.pullrequest.branch=$WERCKER_GIT_BRANCH"
+      SONAR_PR_VARIABLES+=" -Dsonar.pullrequest.base=master"
+      echo "SONAR_PR_VARIABLES: "$SONAR_PR_VARIABLES
+  fi
+  if [[ -z $SONAR_TOKEN ]]; then echo "SONAR_TOKEN is not set"; sleep 5s; exit 1; fi
+  export MAVEN_OPTS='-Xmx2000m'
+  mvn -e -Pno-validations clean package sonar:sonar $SONAR_PR_VARIABLES \
+       -Dsonar.host.url=https://sonarcloud.io \
+       -Dsonar.login=$SONAR_TOKEN \
+       -Dsonar.projectKey=org.checkstyle:checkstyle \
+       -Dsonar.organization=checkstyle
+  echo "report-task.txt:"
+  cat target/sonar/report-task.txt
+  echo "Verification of sonar gate status"
+  checkout_from https://github.com/viesure/blog-sonar-build-breaker.git
+  sed -i'' "s|our.sonar.server|sonarcloud.io|" \
+    .ci-temp/blog-sonar-build-breaker/sonar_break_build.sh
+  export SONAR_API_TOKEN=$SONAR_TOKEN
+  .ci-temp/blog-sonar-build-breaker/sonar_break_build.sh
+  ;;
+
+
 no-error-pgjdbc)
   CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
                      --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
