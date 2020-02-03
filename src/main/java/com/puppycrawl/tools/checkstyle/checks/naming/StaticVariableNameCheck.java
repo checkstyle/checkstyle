b8b81efad35a00b7c96896c86ca2e2af7a0fa5c3
0blivious <lyw@415309@gmail.com>
2020-02-03 21:14:02 +0800

    Issue #5832: add javadoc and xdoc Example for StaticVariableName

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/StaticVariableNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/StaticVariableNameCheck.java
@@ -53,16 +53,56 @@ import com.puppycrawl.tools.checkstyle.utils.ScopeUtil;
  * An example of how to configure the check is:
  * </p>
  * <pre>
- * &lt;module name="StaticVariableName"/&gt;
+ * &lt;module name=&quot;StaticVariableName&quot;/&gt;
+ * </pre>
+ * <p>Code Example:</p>
+ * <pre>
+ * class MyClass {
+ *   public static int goodStatic = 2; // OK
+ *   private static int BadStatic = 2; // violation, name 'BadStatic'
+ *                                     // must match pattern '^[a-z][a-zA-Z0-9]*$'
+ * }
+ * </pre>
+ * <p>
+ * An example of how to suppress the check to public and protected types is:
+ * </p>
+ * <pre>
+ * &lt;module name=&quot;StaticVariableName&quot;&gt;
+ *   &lt;property name=&quot;applyToPublic&quot; value=&quot;false&quot;/&gt;
+ *   &lt;property name=&quot;applyToProtected&quot; value=&quot;false&quot;/&gt;
+ * &lt;/module&gt;
+ * </pre>
+ * <p>Code Example:</p>
+ * <pre>
+ * class MyClass {
+ *   public static int GoodStatic1 = 2; // OK
+ *   protected static int GoodStatic2 = 2; //OK
+ *   private static int goodStatic = 2 // OK
+ *   private static int BadStatic = 2; // violation, name 'BadStatic'
+ *                                     // must match pattern '^[a-z][a-zA-Z0-9]*$'
+ * }
  * </pre>
  * <p>
  * An example of how to configure the check for names that begin with
- * a lower case letter, followed by letters, digits, and underscores is:
+ * a lower case letter, followed by letters, digits, and underscores.
+ * Also, suppress the check from being applied to private and package-private types:
  * </p>
  * <pre>
- * &lt;module name="StaticVariableName"&gt;
- *   &lt;property name="format" value="^[a-z](_?[a-zA-Z0-9]+)*$"/&gt;
+ * &lt;module name=&quot;StaticVariableName&quot;&gt;
+ *   &lt;property name=&quot;format&quot; value=&quot;^[a-z](_?[a-zA-Z0-9]+)*$&quot;/&gt;
+ *   &lt;property name=&quot;applyToPrivate&quot; value=&quot;false&quot;/&gt;
+ *   &lt;property name=&quot;applyToPackage&quot; value=&quot;false&quot;/&gt;
  * &lt;/module&gt;
+ * </pre>
+ * <p>Code Example:</p>
+ * <pre>
+ * class MyClass {
+ *   public static int good_static = 2; // OK
+ *   public static int Bad_Static = 2; // violation, name 'Bad_Static'
+ *                                     // must match pattern '^[a-z](_?[a-zA-Z0-9]+)*$'
+ *   private static int Good_Static1 = 2; // OK
+ *   static int Good_Static2 = 2; // OK
+ * }
  * </pre>
  *
  * @since 3.0
