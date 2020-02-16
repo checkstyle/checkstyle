f390b2e1faa75407284dc7315b96a20550f0c7b7
0blivious <lyw@415309@gmail.com>
2020-02-16 18:17:24 +0800

    Issue #5832: add javadoc and xdoc example for LambdaParameterName

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/LambdaParameterNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/LambdaParameterNameCheck.java
@@ -38,6 +38,12 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * <pre>
  * &lt;module name="LambdaParameterName"/&gt;
  * </pre>
+ * <p>Code Example:</p>
+ * <pre>
+ * Function&lt;String, String&gt; function1 = s -&gt; s.toLowerCase(); // OK
+ * Function&lt;String, String&gt; function2 = S -&gt; S.toLowerCase(); // violation, name 'S'
+ *                                                // must match pattern '^[a-z][a-zA-Z0-9]*$'
+ * </pre>
  * <p>
  * An example of how to configure the check for names that begin
  * with a lower case letter, followed by letters is:
@@ -48,21 +54,20 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * &lt;/module&gt;
  * </pre>
  * <p>
- * Example of checking with this config:
+ * Code Example:
  * </p>
  * <pre>
- * public class TestClass {
+ * class MyClass {
+ *   Function&lt;String, String&gt; function1 = str -&gt; str.toUpperCase().trim(); // OK
+ *   Function&lt;String, String&gt; function2 = _s -&gt; _s.trim(); // violation, name '_s'
+ *                                              // must match pattern '^[a-z]([a-zA-Z]+)*$'
  *
- *     Function&lt;String, String&gt; function1 = str -&gt; str.toUpperCase().trim();
- *
- *     Function&lt;String, String&gt; function2 = _s -&gt; _s.trim().toUpperCase(); // violation
- *
- *     public boolean testMethod(String sentence) {
- *         return Stream.of(sentence.split(" "))
- *             .map(word -&gt; word.trim())
- *             .anyMatch(Word -&gt; "in".equals(Word)); // violation
- *     }
- *
+ *   public boolean myMethod(String sentence) {
+ *     return Stream.of(sentence.split(" "))
+ *             .map(word -&gt; word.trim()) // OK
+ *             .anyMatch(Word -&gt; "in".equals(Word)); // violation, name 'Word'
+ *                                                      // must match pattern '^[a-z]([a-zA-Z]+)*$'
+ *   }
  * }
  *
  * </pre>
