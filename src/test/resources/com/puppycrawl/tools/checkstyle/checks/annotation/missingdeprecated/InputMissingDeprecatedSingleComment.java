0446b9cad22a215b69eacc3739abcc0edd325ad1
nmancus1 <nmancus1@dtcc.edu>
2020-03-04 13:11:00 -0500

    Issue #7534: fix NPE in MissingDeprecatedCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/missingdeprecated/InputMissingDeprecatedSingleComment.java
@@ -0,0 +1,13 @@
+/**
+ * Wrong header
+ */
+// test
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.missingdeprecated;
+
+/**
+ * @deprecated do not use
+ */
+@Deprecated
+class InputMissingDeprecatedSingleComment {
+}
