dc335dd731f8c23bc608924eb84f595b7515d2f9
Wilfried Bounsi <wilcoln99@gmail.com>
2020-03-05 17:13:13 +0100

    Issue #7661: Update doc for SuperClone

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/SuperCloneCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/SuperCloneCheck.java
@@ -37,7 +37,32 @@ import com.puppycrawl.tools.checkstyle.StatelessCheck;
  * <pre>
  * &lt;module name=&quot;SuperClone&quot;/&gt;
  * </pre>
+ * <p>Example:</p>
+ * <pre>
+ * class A {
  *
+ *  public Object clone() { // OK
+ *   return super.clone();
+ *  }
+ * }
+ *
+ * class B {
+ * private int b;
+ *
+ *  public B clone() { // violation, does not call super.clone()
+ *   B other = new B();
+ *   other.b = this.b;
+ *   return other;
+ *  }
+ * }
+ *
+ * class C {
+ *
+ *  public C clone() { // OK
+ *   return (C) super.clone();
+ *  }
+ * }
+ * </pre>
  * @since 3.2
  */
 @StatelessCheck
