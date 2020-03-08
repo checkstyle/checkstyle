d0194cc0e42018aa116c811dac9165b5f80ef911
kaustubh <dxtkastb@gmail.com>
2020-03-09 04:11:10 +0530

    Issue #7638: Update doc for EqualsHashCode

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/EqualsHashCodeCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/EqualsHashCodeCheck.java
@@ -49,7 +49,60 @@ import com.puppycrawl.tools.checkstyle.utils.CheckUtil;
  * <pre>
  * &lt;module name=&quot;EqualsHashCode&quot;/&gt;
  * </pre>
- *
+ * <p>Example:</p>
+ * <pre>
+ * public static class Example1 {
+ *     public int hashCode() {
+ *         // code
+ *     }
+ *     public boolean equals(String o) { // violation, overloaded implementation of 'equals'
+ *         // code
+ *     }
+ * }
+ * public static class Example2 {
+ *     public boolean equals(Object o) { // violation, no 'hashCode'
+ *         // code
+ *     }
+ *     public boolean equals(String o) {
+ *         // code
+ *     }
+ * }
+ * public static class Example3 {
+ *     public int hashCode() {
+ *         // code
+ *     }
+ *     public boolean equals(Object o) { // OK
+ *         // code
+ *     }
+ *     public boolean equals(String o) {
+ *         // code
+ *     }
+ * }
+ * public static class Example4 {
+ *     public int hashCode() {
+ *         // code
+ *     }
+ *     public boolean equals(java.lang.Object o) { // OK
+ *         // code
+ *    }
+ * }
+ * public static class Example5 {
+ *     public static int hashCode(int i) {
+ *         // code
+ *     }
+ *     public boolean equals(Object o) { // violation, overloaded implementation of 'hashCode'
+ *         // code
+ *     }
+ * }
+ * public static class Example6 {
+ *     public int hashCode() { // violation, overloaded implementation of 'equals'
+ *         // code
+ *     }
+ *     public static boolean equals(Object o, Object o2) {
+ *         // code
+ *     }
+ * }
+ * </pre>
  * @since 3.0
  */
 @FileStatefulCheck
