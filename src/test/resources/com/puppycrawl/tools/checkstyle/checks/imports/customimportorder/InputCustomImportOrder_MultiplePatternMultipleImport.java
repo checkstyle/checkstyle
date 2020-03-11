131330686ed0658fd8f10fe5e9c073c3b6f8d167
wltan <53135010+wltan@users.noreply.github.com>
2020-03-11 14:42:28 +0800

    Issue #7802: Resolve Pitest Issues - CustomImportOrderCheck (1)

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrder_MultiplePatternMultipleImport.java
@@ -0,0 +1,7 @@
+package com.puppycrawl.tools.checkstyle.checks.imports.customimportorder;
+
+import java.util.Scanner;
+import org.junit.Test;
+
+public class InputCustomImportOrder_MultiplePatternMultipleImport {
+}
