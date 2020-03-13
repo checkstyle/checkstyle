479874ebc8e2022ac7db3dafd99a63afce24d3aa
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-03-13 22:08:10 +0530

    Issue #7749: Update AbstractChecks to log DetailAST - MissingSwitchDefault

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/missingswitchdefault/InputMissingSwitchDefault.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/missingswitchdefault/InputMissingSwitchDefault.java
@@ -19,4 +19,29 @@ class bad_test {
         case 2: i--; break;
         }
     }
+
+    public void nestedSwitch1() {
+        int i = 1;
+        switch (i) {
+            case 1: i++; break;
+            case 2: i--; break;
+            default:
+                switch (i++) { // violation
+                    case 2: i++; break;
+                    case 3: i--; break;
+            }
+        }
+    }
+
+    public void nestedSwitch2() {
+        int i = 1, j = 2;
+        switch(i) {
+            case 1:
+                switch (j) { // violation
+                    case 2: break;
+                    case 3: break;
+            }
+            default: break;
+        }
+    }
 }
