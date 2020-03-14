f2045e56dfbecb23463d9dbe9fa9e654bc49cca1
wltan <53135010+wltan@users.noreply.github.com>
2020-03-14 15:07:12 +0800

    Issue #7740: Update AbstractChecks to log DetailAST - JavadocStyle

---
+++ src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionJavadocStyleTest.java
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2020 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package org.checkstyle.suppressionxpathfilter;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.checks.javadoc.JavadocStyleCheck;
+
+public class XpathRegressionJavadocStyleTest extends AbstractXpathTestSupport {
+
+    private final String checkName = JavadocStyleCheck.class.getSimpleName();
+
+    @Override
+    protected String getCheckName() {
+        return checkName;
+    }
+
+    @Test
+    public void testOne() throws Exception {
+        final File fileToProcess = new File(getPath("SuppressionXpathRegression"
+                + File.separator + "package-info.java"));
+
+        final DefaultConfiguration moduleConfig =
+                createModuleConfig(JavadocStyleCheck.class);
+
+        final String[] expectedViolation = {
+            "1:1: " + getCheckMessage(JavadocStyleCheck.class,
+                JavadocStyleCheck.MSG_JAVADOC_MISSING),
+        };
+
+        final List<String> expectedXpathQueries = Collections.singletonList("/PACKAGE_DEF");
+
+        runVerifications(moduleConfig, fileToProcess, expectedViolation,
+                expectedXpathQueries);
+    }
+}
