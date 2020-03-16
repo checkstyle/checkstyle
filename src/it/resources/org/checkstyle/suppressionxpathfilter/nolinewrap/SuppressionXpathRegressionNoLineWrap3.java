4bf162042c303be7ddd20d7425f10b7a13f460fc
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-03-16 17:45:53 +0530

    Issue #7753: Update AbstractChecks to log DetailAST - NoLineWrap

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/nolinewrap/SuppressionXpathRegressionNoLineWrap3.java
@@ -0,0 +1,8 @@
+package org.checkstyle.suppressionxpathfilter.nolinewrap;
+
+public class SuppressionXpathRegressionNoLineWrap3 {
+    SuppressionXpathRegressionNoLineWrap3 //warn
+            (String input1, String input2) {
+
+    }
+}
