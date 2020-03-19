32954d424ef0170f86b933f556551eeb26a1a173
liach <liach@users.noreply.github.com>
2020-03-19 00:07:23 -0500

    Issue #5969: Respect and expect tab width in column number check for lambda indentations

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/indentation/LambdaHandler.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/indentation/LambdaHandler.java
@@ -66,7 +66,7 @@ public class LambdaHandler extends AbstractExpressionHandler {
         // If the start of the lambda is the first element on the line;
         // assume line wrapping with respect to its parent.
         final DetailAST firstChild = getMainAst().getFirstChild();
-        if (getLineStart(firstChild) == firstChild.getColumnNo()) {
+        if (getLineStart(firstChild) == expandedTabsColumnNo(firstChild)) {
             level = new IndentLevel(level, getIndentCheck().getLineWrappingIndentation());
         }
 
@@ -77,19 +77,19 @@ public class LambdaHandler extends AbstractExpressionHandler {
     public void checkIndentation() {
         // If the argument list is the first element on the line
         final DetailAST firstChild = getMainAst().getFirstChild();
-        if (getLineStart(firstChild) == firstChild.getColumnNo()) {
+        if (getLineStart(firstChild) == expandedTabsColumnNo(firstChild)) {
             final IndentLevel level = getIndent();
-            if (!level.isAcceptable(firstChild.getColumnNo())) {
-                logError(firstChild, "arguments", firstChild.getColumnNo(), level);
+            if (!level.isAcceptable(expandedTabsColumnNo(firstChild))) {
+                logError(firstChild, "arguments", expandedTabsColumnNo(firstChild), level);
             }
         }
 
         // If the "->" is the first element on the line, assume line wrapping.
-        if (getLineStart(getMainAst()) == getMainAst().getColumnNo()) {
+        if (getLineStart(getMainAst()) == expandedTabsColumnNo(getMainAst())) {
             final IndentLevel level =
                 new IndentLevel(getIndent(), getIndentCheck().getLineWrappingIndentation());
-            if (!level.isAcceptable(getMainAst().getColumnNo())) {
-                logError(getMainAst(), "", getMainAst().getColumnNo(), level);
+            if (!level.isAcceptable(expandedTabsColumnNo(getMainAst()))) {
+                logError(getMainAst(), "", expandedTabsColumnNo(getMainAst()), level);
             }
         }
     }
