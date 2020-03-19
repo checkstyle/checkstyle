da3b436b8623833f1cfb38697f277b6e227a88cd
HuGanghui <huganghui18@gmail.com>
2020-03-19 14:22:57 +0800

    Issue #6707: fix false violation report that does not allow spaces in front of ellipsis tokens for Google Style

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputNoWhitespaceBeforeAnnotations.java
@@ -0,0 +1,44 @@
+package com.google.checkstyle.test.chapter4formatting.rule462horizontalwhitespace;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+@interface NonNull {
+
+}
+
+public class InputNoWhitespaceBeforeAnnotations {
+
+    public void foo(final char @NonNull [] param) {}
+
+    @NonNull int @NonNull[] @NonNull[] fiel1; // OK until #8205.
+    @NonNull int @NonNull [] @NonNull [] field2;
+    //@NonNull int @NonNull ... field3; // non-compilable
+    //@NonNull int @NonNull... field4; // non-compilable
+
+
+    public void foo1(final char[] param) { // OK until #8205.
+    }
+
+    public void foo2(final char [] param) {
+    }
+
+    public void foo3(final char @NonNull[] param) { // OK until #8205.
+    }
+
+    public void foo4(final char @NonNull [] param) {
+    }
+
+    void test1(String... param) { // OK until #8205.
+    }
+
+    void test2(String ... param) { // violation on 8.32
+    }
+
+    void test3(String @NonNull... param) { // OK until #8205.
+    }
+
+    void test4(String @NonNull ... param) { // violation on 8.32
+    }
+}
