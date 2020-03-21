938392281b4786f88cff7573860251f86bb984f0
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-03-21 16:17:14 +0530

    Issue #7734: Update AbstractChecks to log DetailAST - Indentation

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
@@ -58,7 +58,6 @@ public class XpathRegressionTest extends AbstractModuleTestSupport {
             "CommentsIndentation",
             "CustomImportOrder",
             "EmptyLineSeparator",
-            "Indentation",
             "InterfaceMemberImpliedModifier",
             "JavadocMethod",
             "JavadocType",
