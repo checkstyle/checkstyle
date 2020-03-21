274740d00db74d9ae4f73b091b1b457a8a974295
AmrDeveloper <amr96@programmer.net>
2020-03-21 03:08:51 +0200

    Issue #7921: Resolve Pitest Issues - UnusedImportsCheck

--- .ci/pitest.sh
+++ .ci/pitest.sh
@@ -80,7 +80,6 @@ pitest-imports)
   "PkgImportControl.java.html:<td class='covered'><pre><span  class='survived'>        if (regex || parent.regex) {</span></pre></td></tr>"
   "PkgImportControl.java.html:<td class='covered'><pre><span  class='survived'>        if (regex) {</span></pre></td></tr>"
   "PkgImportRule.java.html:<td class='covered'><pre><span  class='survived'>        if (isRegExp()) {</span></pre></td></tr>"
-  "UnusedImportsCheck.java.html:<td class='covered'><pre><span  class='survived'>            if (collect) {</span></pre></td></tr>"
   );
   checkPitestReport "${ignoredItems[@]}"
   ;;
@@ -213,6 +212,5 @@ pitest-utils)
   ;;
 
 esac
-
 
 
