e8311771edeccdac28520daac6bf37de9ed44536
yalechen <yalechen@tencent.com>
2020-03-23 17:49:18 +0800

    bug fixed - AbbreviationAsWordInNameCheck - no need to -1

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
@@ -390,7 +390,7 @@ public class AbbreviationAsWordInNameCheck extends AbstractCheck {
             else if (abbrStarted) {
                 abbrStarted = false;
 
-                final int endIndex = index - 1;
+                final int endIndex = index;
                 result = getAbbreviationIfIllegal(str, beginIndex, endIndex);
                 if (result != null) {
                     break;
