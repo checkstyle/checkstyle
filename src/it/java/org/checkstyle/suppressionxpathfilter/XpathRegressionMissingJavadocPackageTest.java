cbc507d237135c903596b7a7a8efc81f4badd5e5
Gaurab Dasgupta <gaurabdasg@gmail.com>
2020-03-23 14:19:04 +0530

    Issue #7746: Update AbstractChecks to log DetailAST- MissingJavadocPackage

---
+++ src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionMissingJavadocPackageTest.java
@@ -0,0 +1,81 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2020 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package org.checkstyle.suppressionxpathfilter;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.checks.javadoc.MissingJavadocPackageCheck;
+
+public class XpathRegressionMissingJavadocPackageTest extends AbstractXpathTestSupport {
+
+    private final String checkName = MissingJavadocPackageCheck.class.getSimpleName();
+
+    @Override
+    protected String getCheckName() {
+        return checkName;
+    }
+
+    @Test
+    public void testBlockComment() throws Exception {
+        final File fileToProcess = new File(getPath(
+                "blockcomment/package-info.java"));
+
+        final DefaultConfiguration moduleConfig =
+                createModuleConfig(MissingJavadocPackageCheck.class);
+
+        final String[] expectedViolation = {
+            "4:1: " + getCheckMessage(MissingJavadocPackageCheck.class,
+                MissingJavadocPackageCheck.MSG_PKG_JAVADOC_MISSING),
+        };
+        final List<String> expectedXpathQueries = Collections.singletonList(
+            "/PACKAGE_DEF"
+        );
+
+        runVerifications(moduleConfig, fileToProcess, expectedViolation,
+                expectedXpathQueries);
+    }
+
+    @Test
+    public void testNoJavadoc() throws Exception {
+        final File fileToProcess = new File(getPath(
+                "nojavadoc/package-info.java"));
+
+        final DefaultConfiguration moduleConfig =
+                createModuleConfig(MissingJavadocPackageCheck.class);
+
+        final String[] expectedViolation = {
+            "1:1: " + getCheckMessage(MissingJavadocPackageCheck.class,
+                MissingJavadocPackageCheck.MSG_PKG_JAVADOC_MISSING),
+        };
+
+        final List<String> expectedXpathQueries = Collections.singletonList(
+            "/PACKAGE_DEF"
+        );
+
+        runVerifications(moduleConfig, fileToProcess, expectedViolation,
+                expectedXpathQueries);
+    }
+
+}
--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/MissingJavadocPackageCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/MissingJavadocPackageCheck.java
@@ -92,7 +92,7 @@ public class MissingJavadocPackageCheck extends AbstractCheck {
     public void visitToken(DetailAST ast) {
         final FileContents contents = getFileContents();
         if (contents.inPackageInfo() && !hasJavadoc(ast)) {
-            log(ast.getLineNo(), MSG_PKG_JAVADOC_MISSING);
+            log(ast, MSG_PKG_JAVADOC_MISSING);
         }
     }
 
