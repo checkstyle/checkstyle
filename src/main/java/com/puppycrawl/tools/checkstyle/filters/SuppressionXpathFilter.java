ab4d7bdcdc8ce90da82afcceb2b7d2e1b2227d07
Wilfried Bounsi <wilcoln99@gmail.com>
2020-03-27 08:36:47 +0100

    Issue #7721: Update AbstractChecks to log DetailAST - AnnotationOnSameLine

--- src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilter.java
@@ -44,9 +44,6 @@ import com.puppycrawl.tools.checkstyle.utils.FilterUtil;
  * </p>
  * <ul id="SuppressionXpathFilter_IncompatibleChecks">
  * <li>
- * AnnotationOnSameLine
- * </li>
- * <li>
  * AnnotationUseStyle
  * </li>
  * <li>
