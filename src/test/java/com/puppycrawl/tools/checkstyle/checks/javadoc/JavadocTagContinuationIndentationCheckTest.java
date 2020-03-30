956fce650952548d62ad750e6a69749b76edac58
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-03-31 04:10:38 +0530

    Issue #2813: JavadocTagContinuationIndentation doesn't report some lines

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTagContinuationIndentationCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTagContinuationIndentationCheckTest.java
@@ -93,4 +93,17 @@ public class JavadocTagContinuationIndentationCheckTest
                 expected);
     }
 
+    @Test
+    public void testCheckWithDescription() throws Exception {
+        final DefaultConfiguration checkConfig =
+                createModuleConfig(JavadocTagContinuationIndentationCheck.class);
+        final String[] expected = {
+            "9: " + getCheckMessage(MSG_KEY, 4),
+            "10: " + getCheckMessage(MSG_KEY, 4),
+            "11: " + getCheckMessage(MSG_KEY, 4),
+        };
+        verify(checkConfig, getPath("InputJavadocTagContinuationIndentationDescription.java"),
+                expected);
+    }
+
 }
