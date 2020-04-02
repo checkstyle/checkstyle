621975e046b5ad7a37da3638020c0ec424ea6088
kaustubh <dxtkastb@gmail.com>
2020-04-03 02:11:40 +0530

    Issue #7758: Update AbstractChecks to log DetailAST - PackageDeclaration

--- src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilter.java
@@ -68,9 +68,6 @@ import com.puppycrawl.tools.checkstyle.utils.FilterUtil;
  * MissingJavadocType
  * </li>
  * <li>
- * PackageDeclaration
- * </li>
- * <li>
  * Regexp
  * </li>
  * <li>
@@ -87,6 +84,15 @@ import com.puppycrawl.tools.checkstyle.utils.FilterUtil;
  * </li>
  * <li>
  * VariableDeclarationUsageDistance
+ * </li>
+ * </ul>
+ * <p>
+ * Certain Checks are partially supported by the filter:
+ * </p>
+ * <ul>
+ * <li>
+ * PackageDeclaration (until
+ * <a href="https://github.com/checkstyle/checkstyle/pull/8110">#8110</a>)
  * </li>
  * </ul>
  * <p>
