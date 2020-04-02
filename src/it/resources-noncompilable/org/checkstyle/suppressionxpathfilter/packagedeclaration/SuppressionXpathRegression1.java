2e03770ac8f24a9b32d40d8511b55ed1b5eaf80b
kaustubh <dxtkastb@gmail.com>
2020-04-03 02:11:40 +0530

    Issue #7758: Update AbstractChecks to log DetailAST - PackageDeclaration

---
+++ src/it/resources-noncompilable/org/checkstyle/suppressionxpathfilter/packagedeclaration/SuppressionXpathRegression1.java
@@ -0,0 +1,7 @@
+// non-compiled with javac: wrong package. Used for Testing purpose.
+// package this.package.mismatch.with.folder
+package org.junit.contrib; // warn
+
+public class SuppressionXpathRegression1 {
+	// code
+}
