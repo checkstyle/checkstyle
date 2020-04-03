961dbaf473459ebe2844d25b5cd699ea3091974a
Gaurab Dasgupta <gaurabdasg@gmail.com>
2020-04-04 03:50:41 +0530

    Issue #7175: INTERFACE_DEF token support in RightCurlyCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlySameBlocksWithSemi.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlySameBlocksWithSemi.java
@@ -56,4 +56,18 @@ public class InputRightCurlySameBlocksWithSemi {
 
     enum TestEnum4{ SOME_VALUE;
     }; //violation
+
+    interface Interface1
+    {
+        int i = 1;
+        public void meth1(); };
+
+    interface Interface2
+    { int i = 1; public void meth1(); };
+
+    interface Interface3 {
+        void display();
+        interface Interface4 {
+            void myMethod();
+        };};
 }
