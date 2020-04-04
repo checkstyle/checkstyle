bcaf35153adaa254b9ad70b628dd3874e454ee16
Akhil singh <akhil1.as42@gmail.com>
2020-04-05 02:26:00 +0530

    Issue #7983: Resolve Pitest Issues - RegexpCheck (2)

--- .ci/pitest.sh
+++ .ci/pitest.sh
@@ -47,9 +47,7 @@ pitest-annotation|pitest-design \
 
 pitest-regexp)
   mvn -e -P$1 clean test org.pitest:pitest-maven:mutationCoverage;
-  declare -a ignoredItems=(
-  "RegexpCheck.java.html:<td class='covered'><pre><span  class='survived'>        return errorCount &#60;= errorLimit - 1</span></pre></td></tr>"
-  );
+  declare -a ignoredItems=();
   checkPitestReport "${ignoredItems[@]}"
   ;;
 
