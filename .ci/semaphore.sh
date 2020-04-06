ee388008d4336397e7669e43520ea0b0067c283d
Pavel Bludov <pbludov@ya.ru>
2020-04-06 12:10:20 +0300

    Issue #8051: set up semaphore CI

---
+++ .ci/semaphore.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+case $1 in
+
+verify)
+  mvn -e clean verify
+  ;;
+
+assembly)
+  mvn -e clean package -Passembly
+  ;;
+
+site)
+  mvn -e clean site -Pno-validations -Dlinkcheck.skip=true
+  ;;
+
+*)
+  echo "Unexpected argument: $1"
+  sleep 5s
+  false
+  ;;
+
+esac
