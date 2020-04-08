e7277f78b334470196bb55ed1affb62f011c6465
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-04-09 04:20:41 +0530

    Issue #3899: Indentation: google-java-format and Checkstyle disagree about switch block indentation

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationStrictCondition.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationStrictCondition.java
@@ -7,8 +7,8 @@ public class InputIndentationStrictCondition { //indent:0 exp:0
         method( //indent:8 exp:8
                 new Thread() { //indent:16 exp:16
                         public void run() { //indent:24 exp:24
-                            } //indent:28 exp:28
+                            } //indent:28 exp:16,20,24 warn
                     } //indent:20 exp:20
         ); //indent:8 exp:8
-        } //indent:8 exp:8
-    } //indent:4 exp:4
+        } //indent:8 exp:4 warn
+    } //indent:4 exp:0 warn
