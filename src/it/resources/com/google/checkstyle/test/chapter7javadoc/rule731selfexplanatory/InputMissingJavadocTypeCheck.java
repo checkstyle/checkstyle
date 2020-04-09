67fc1f7249cc0ee2d4d9ff617bc29174d6d3a514
wltan <53135010+wltan@users.noreply.github.com>
2020-04-09 22:09:15 +0800

    Issue #5148: Add MissingJavadocType to google_checks.xml

---
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule731selfexplanatory/InputMissingJavadocTypeCheck.java
@@ -0,0 +1,9 @@
+package com.google.checkstyle.test.chapter7javadoc.rule731selfexplanatory;
+
+public class InputMissingJavadocTypeCheck { //warn
+
+}
+
+class AdditionalClass { // OK, not public
+
+}
