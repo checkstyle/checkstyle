74ed295ddb49d7b7f97247812c41e44cbdf988b5
Gaurab Dasgupta <gaurabdasg@gmail.com>
2020-04-11 20:33:00 +0530

    Issue #7469: Use ArchUnit library to control source code

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/ArchUnitTest.java
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2020 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.internal;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
+
+import org.junit.jupiter.api.Test;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.domain.JavaModifier;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.ArchRule;
+
+public class ArchUnitTest {
+    /**
+     * The goal is to ensure all classes of a specific name pattern have non-protected methods,
+     * except for those which are annotated with {@code Override}. In the bytecode there is no
+     * trace anymore if this method was annotated with {@code Override} or not (limitation of
+     * Archunit), eventually we need to make checkstyle's Check on this.
+     * Test contains assertions in the callstack, but TeamCity inspection does not see them.
+     *
+     * @noinspection JUnitTestMethodWithNoAssertions
+     */
+    @Test
+    public void nonProtectedCheckMethodsTest() {
+        // This list contains methods which have been overridden and are set to ignore in this test.
+        final String[] methodsWithOverrideAnnotation = {
+            "processFiltered",
+            "getMethodName",
+            "mustCheckName",
+            "postProcessHeaderLines",
+            "getLogMessageId",
+        };
+        final String ignoreMethodList = String.join("|", methodsWithOverrideAnnotation);
+        final JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+                .importPackages("com.puppycrawl.tools.checkstyle.checks");
+
+        final ArchRule checkMethodsShouldNotBeProtectedRule =
+                methods().that()
+                .haveNameNotMatching(".*(" + ignoreMethodList + ")").and()
+                .areDeclaredInClassesThat()
+                .haveSimpleNameEndingWith("Check").and()
+                .areDeclaredInClassesThat()
+                .doNotHaveModifier(JavaModifier.ABSTRACT)
+                .should().notBeProtected();
+
+        checkMethodsShouldNotBeProtectedRule.check(importedClasses);
+    }
+}
