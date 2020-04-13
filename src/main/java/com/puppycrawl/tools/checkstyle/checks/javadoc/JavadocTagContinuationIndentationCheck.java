cb3c1627b967f16b1ccd4afc02a1d484ecf7574d
kaustubh <dxtkastb@gmail.com>
2020-04-13 16:15:29 +0530

    Issue #7600: Update doc for JavadocTagContinuationIndentation

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTagContinuationIndentationCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTagContinuationIndentationCheck.java
@@ -51,12 +51,57 @@ import com.puppycrawl.tools.checkstyle.utils.JavadocUtil;
  * &lt;module name="JavadocTagContinuationIndentation"/&gt;
  * </pre>
  * <p>
+ * Example:
+ * </p>
+ * <pre>
+ * &#47;**
+ *  * @tag comment
+ *  *  Indentation spacing is 1 // violation
+ *  *   Indentation spacing is 2 // violation
+ *  *     Indentation spacing is 4 // OK
+ *  *&#47;
+ * public class Test {
+ * }
+ * </pre>
+ * <p>
  * To configure the check with two spaces indentation:
  * </p>
  * <pre>
  * &lt;module name="JavadocTagContinuationIndentation"&gt;
  *   &lt;property name="offset" value="2"/&gt;
  * &lt;/module&gt;
+ * </pre>
+ * <p>
+ * Example:
+ * </p>
+ * <pre>
+ * &#47;**
+ *  * @tag comment
+ *  * Indentation spacing is 0 // violation
+ *  *   Indentation spacing is 2 // OK
+ *  *  Indentation spacing is 1 // violation
+ *  *&#47;
+ * public class Test {
+ * }
+ * </pre>
+ * <p>
+ * To configure the check to show violations for Tight-HTML Rules:
+ * </p>
+ * <pre>
+ * &lt;module name="JavadocTagContinuationIndentation"&gt;
+ *   &lt;property name="violateExecutionOnNonTightHtml" value="true"/&gt;
+ * &lt;/module&gt;
+ * </pre>
+ * <p>
+ * Example:
+ * </p>
+ * <pre>
+ * &#47;**
+ *  * &lt;p&gt; 'p' tag is unclosed // violation, this html tag needs closing tag.
+ *  * &lt;p&gt; 'p' tag is closed&lt;/p&gt; // OK
+ *  *&#47;
+ * public class Test {
+ * }
  * </pre>
  *
  * @since 6.0
