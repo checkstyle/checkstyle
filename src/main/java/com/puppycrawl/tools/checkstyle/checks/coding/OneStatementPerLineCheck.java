f84ef94623dc657b1d30c1cebf873d6c6efa3cba
Harsh Kukreja <co2015.kukreja.harsh@ves.ac.in>
2020-04-13 20:53:48 +0530

    Issue #6332: Java9: try-with-resources should accept fields

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/OneStatementPerLineCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/OneStatementPerLineCheck.java
@@ -283,10 +283,10 @@ public final class OneStatementPerLineCheck extends AbstractCheck {
     private void checkResourceVariable(DetailAST currentStatement) {
         if (treatTryResourcesAsStatement) {
             final DetailAST nextNode = currentStatement.getNextSibling();
-            if (currentStatement.getPreviousSibling().findFirstToken(TokenTypes.ASSIGN) != null) {
+            if (currentStatement.getPreviousSibling().getFirstChild().findFirstToken(TokenTypes.ASSIGN) != null) {
                 lastVariableResourceStatementEnd = currentStatement.getLineNo();
             }
-            if (nextNode.findFirstToken(TokenTypes.ASSIGN) != null
+            if (nextNode.getFirstChild() != null && nextNode.getFirstChild().findFirstToken(TokenTypes.ASSIGN) != null
                 && nextNode.getLineNo() == lastVariableResourceStatementEnd) {
                 log(currentStatement, MSG_KEY);
             }
