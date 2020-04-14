f20690394cfef7ccc3d1ef843692b4b508f9d5d0
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-04-14 18:05:10 +0530

    Issue #7724: Update AbstractChecks to log DetailAST - AvoidEscapedUnicodeCharacters

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/avoidescapedunicodecharacters/SuppressionXpathRegressionAvoidEscapedUnicodeCharactersDefault.java
@@ -0,0 +1,5 @@
+package org.checkstyle.suppressionxpathfilter.avoidescapedunicodecharacters;
+
+public class SuppressionXpathRegressionAvoidEscapedUnicodeCharactersDefault {
+    private String unitAbbrev2 = "\u03bcs"; /* warn */
+}
