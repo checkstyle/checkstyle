cbe43cfea4eded528266c6dd6cfaf35e04dcafb4
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-04-14 18:05:10 +0530

    Issue #7724: Update AbstractChecks to log DetailAST - AvoidEscapedUnicodeCharacters

--- src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilter.java
@@ -44,9 +44,6 @@ import com.puppycrawl.tools.checkstyle.utils.FilterUtil;
  * </p>
  * <ul id="SuppressionXpathFilter_IncompatibleChecks">
  * <li>
- * AvoidEscapedUnicodeCharacters
- * </li>
- * <li>
  * CommentsIndentation
  * </li>
  * <li>
