eabd121db3bdc18920bd2e1d81d5934da92e5997
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-04-16 06:22:03 +0530

    Issue #6209: add spring-cloud-gcp project to CIs no errror regresson testing

--- .ci/wercker.sh
+++ .ci/wercker.sh
@@ -234,6 +234,18 @@ no-error-spring-integration)
   removeFolderWithProtectedFiles spring-integration
   ;;
 
+no-error-spring-cloud-gcp)
+  set -e
+  CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
+                     --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+  echo CS_version: ${CS_POM_VERSION}
+  checkout_from https://github.com/spring-cloud/spring-cloud-gcp
+  cd .ci-temp/spring-cloud-gcp
+  mvn -e validate -P full-checkstyle -Dpuppycrawl-tools-checkstyle.version=${CS_POM_VERSION}
+  cd ..
+  removeFolderWithProtectedFiles spring-cloud-gcp
+  ;;
+
 no-exception-struts)
   CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
                      --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
