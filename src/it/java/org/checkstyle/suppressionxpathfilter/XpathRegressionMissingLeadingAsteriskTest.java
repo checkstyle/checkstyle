3b1491b6f88401c0ce94e9fae6629cdebc4c49a9
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-04-16 22:24:39 +0530

    Issue #6722: new check MissingLeadingAsteriskCheck

---
+++ src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionMissingLeadingAsteriskTest.java
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2020 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package org.checkstyle.suppressionxpathfilter;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.checks.javadoc.JavadocStyleCheck;
+import com.puppycrawl.tools.checkstyle.checks.javadoc.MissingLeadingAsteriskCheck;
+
+public class XpathRegressionMissingLeadingAsteriskTest extends AbstractXpathTestSupport {
+
+    private final String checkName = MissingLeadingAsteriskCheck.class.getSimpleName();
+
+    @Override
+    protected String getCheckName() {
+        return checkName;
+    }
+
+    @Test
+    public void testOne() throws Exception {
+        final File fileToProcess = new File(
+                getPath("SuppressionXpathRegressionMissingLeadingAsterisk.java"));
+
+        final DefaultConfiguration moduleConfig =
+                createModuleConfig(MissingLeadingAsteriskCheck.class);
+
+        final String[] expectedViolation = {
+            "3:1: " + getCheckMessage(JavadocStyleCheck.class,
+                MissingLeadingAsteriskCheck.MSG_MISSING_ASTERISK),
+        };
+
+        final List<String> expectedXpathQueries = Collections.singletonList(
+                "/CLASS_DEF[./IDENT[@text='SuppressionXpathRegressionMissingLeadingAsterisk']]"
+                + "/MODIFIERS/BLOCK_COMMENT_BEGIN");
+
+        runVerifications(moduleConfig, fileToProcess, expectedViolation,
+                expectedXpathQueries);
+    }
+}
