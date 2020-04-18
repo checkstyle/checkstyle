f06c0cf5cb0666ce2548643d36e408909e004f90
Gaurab Dasgupta <gaurabdasg@gmail.com>
2020-04-19 02:13:46 +0530

    Issue #4219: UnusedImports doesn't recognize imports shadowed by inner interfaces

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsTopLevel.java
@@ -0,0 +1,6 @@
+package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
+
+import com.puppycrawl.tools.checkstyle.checks.imports.unusedimports.InputUnusedImportsTopLevel;
+
+public class InputUnusedImportsTopLevel { // violation
+}
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsWithoutWarnings.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsWithoutWarnings.java
@@ -3,6 +3,7 @@ package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class InputUnusedImportsWithoutWarnings {
     private static final List<String> CONSTANTS = Arrays.asList("a", "b");
 }
