ddf76cd4bc05df7501673f4a2d98cea87d0d78fc
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-04-22 04:12:01 +0530

    Issue #8151: Update XpathRegressionOuterTypeNumber to have atleast 2 tests

--- src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionOuterTypeNumberTest.java
+++ src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionOuterTypeNumberTest.java
@@ -38,6 +38,27 @@ public class XpathRegressionOuterTypeNumberTest extends AbstractXpathTestSupport
     }
 
     @Test
+    public void testDefault() throws Exception {
+        final File fileToProcess =
+            new File(getPath("SuppressionXpathRegressionOuterTypeNumberDefault.java"));
+
+        final DefaultConfiguration moduleConfig =
+            createModuleConfig(OuterTypeNumberCheck.class);
+
+        final String[] expectedViolation = {
+            "1:1: " + getCheckMessage(OuterTypeNumberCheck.class,
+                OuterTypeNumberCheck.MSG_KEY, 2, 1),
+        };
+
+        final List<String> expectedXpathQueries = Collections.singletonList(
+            "/PACKAGE_DEF"
+        );
+
+        runVerifications(moduleConfig, fileToProcess, expectedViolation,
+            expectedXpathQueries);
+    }
+
+    @Test
     public void testOne() throws Exception {
         final File fileToProcess =
                 new File(getPath("SuppressionXpathRegressionOuterTypeNumber.java"));
