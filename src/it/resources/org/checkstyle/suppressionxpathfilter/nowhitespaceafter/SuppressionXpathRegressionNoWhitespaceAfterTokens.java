60041dafe1a9a82d9acac32601a7523615e77d85
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-04-22 02:11:44 +0530

    Issue #8151: Update XpathRegressionNoWhitespaceAfter to have atleast 2 tests

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/nowhitespaceafter/SuppressionXpathRegressionNoWhitespaceAfterTokens.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.nowhitespaceafter;
+
+public class SuppressionXpathRegressionNoWhitespaceAfterTokens {
+    public java. lang.String test() { // warn
+        return "";
+    }
+}
