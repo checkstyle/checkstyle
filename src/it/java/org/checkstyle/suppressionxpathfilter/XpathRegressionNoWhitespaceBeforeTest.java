49b5cfeb234ae24aaf217a8f2840a6adc98cdb7c
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-04-22 02:21:51 +0530

    Issue #8151: Update XpathRegressionNoWhitespaceBefore to have atleast 2 tests

--- src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionNoWhitespaceBeforeTest.java
+++ src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionNoWhitespaceBeforeTest.java
@@ -59,4 +59,53 @@ public class XpathRegressionNoWhitespaceBeforeTest extends AbstractXpathTestSupp
                 expectedXpathQueries);
     }
 
+    @Test
+    public void testTokens() throws Exception {
+        final File fileToProcess =
+            new File(getPath("SuppressionXpathRegressionNoWhitespaceBeforeTokens.java"));
+
+        final DefaultConfiguration moduleConfig =
+            createModuleConfig(NoWhitespaceBeforeCheck.class);
+        moduleConfig.addAttribute("tokens", "DOT");
+
+        final String[] expectedViolation = {
+            "4:17: " + getCheckMessage(NoWhitespaceBeforeCheck.class,
+                NoWhitespaceBeforeCheck.MSG_KEY, "."),
+        };
+
+        final List<String> expectedXpathQueries = Collections.singletonList(
+            "/CLASS_DEF[./IDENT[@text='SuppressionXpathRegressionNoWhitespaceBeforeTokens']]"
+                + "/OBJBLOCK/METHOD_DEF[./IDENT[@text='test']]"
+                + "/TYPE/DOT[./IDENT[@text='String']]"
+                + "/DOT[./IDENT[@text='java']]"
+        );
+
+        runVerifications(moduleConfig, fileToProcess, expectedViolation,
+            expectedXpathQueries);
+    }
+
+    @Test
+    public void testAllowLineBreaks() throws Exception {
+        final File fileToProcess =
+            new File(getPath("SuppressionXpathRegressionNoWhitespaceBeforeLineBreaks.java"));
+
+        final DefaultConfiguration moduleConfig =
+            createModuleConfig(NoWhitespaceBeforeCheck.class);
+        moduleConfig.addAttribute("allowLineBreaks", "false");
+
+        final String[] expectedViolation = {
+            "6:13: " + getCheckMessage(NoWhitespaceBeforeCheck.class,
+                NoWhitespaceBeforeCheck.MSG_KEY, ","),
+        };
+
+        final List<String> expectedXpathQueries = Collections.singletonList(
+            "/CLASS_DEF[./IDENT[@text='SuppressionXpathRegressionNoWhitespaceBeforeLineBreaks']]"
+                + "/OBJBLOCK/METHOD_DEF[./IDENT[@text='test']]"
+                + "/SLIST/VARIABLE_DEF[./IDENT[@text='array']]"
+                + "/ASSIGN/ARRAY_INIT/COMMA"
+        );
+
+        runVerifications(moduleConfig, fileToProcess, expectedViolation,
+            expectedXpathQueries);
+    }
 }
