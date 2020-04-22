2e1f973bae6fa690059c14ff73951a464f16fc60
Shrey Kumar Singh <shrey18266@iiitd.ac.in>
2020-04-22 15:34:27 +0530

    Issue #7727: Updated ITs for CommentsIndentation

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/commentsindentation/SuppressionXpathRegressionCommentsIndentationFive.java
@@ -0,0 +1,8 @@
+package org.checkstyle.suppressionxpathfilter.commentsindentation;
+
+public class SuppressionXpathRegressionCommentsIndentationFive {
+    public void foo() {
+// Single Line
+// block Comment // warn
+    }
+}
