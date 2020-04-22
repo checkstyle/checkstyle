2edfab7a4072b9c6ab62e3c765fb493bae6c6180
Shrey Kumar Singh <shrey18266@iiitd.ac.in>
2020-04-22 15:34:27 +0530

    Issue #7727: Updated ITs for CommentsIndentation

--- src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilter.java
@@ -44,9 +44,6 @@ import com.puppycrawl.tools.checkstyle.utils.FilterUtil;
  * </p>
  * <ul id="SuppressionXpathFilter_IncompatibleChecks">
  * <li>
- * CommentsIndentation
- * </li>
- * <li>
  * CustomImportOrder
  * </li>
  * <li>
