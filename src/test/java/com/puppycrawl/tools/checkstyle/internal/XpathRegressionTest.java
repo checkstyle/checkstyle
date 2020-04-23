ddc5207075b2b5e6572a64fe08fe58e45a7ad4ac
hembhagat <hembhagat99@gmail.com>
2020-04-23 20:57:07 +0530

    Issue #7728: Update AbstractChecks to log DetailAST - CustomImportOrder

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
@@ -55,7 +55,6 @@ public class XpathRegressionTest extends AbstractModuleTestSupport {
     // till https://github.com/checkstyle/checkstyle/issues/5777
     public static final Set<String> INCOMPATIBLE_CHECK_NAMES =
         Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
-            "CustomImportOrder",
             "EmptyLineSeparator",
             "Indentation",
             "JavadocMethod",
