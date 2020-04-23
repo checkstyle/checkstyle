7494531ec5b18ae43ed35f904631d2b3d94e31ea
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-04-23 19:46:59 +0530

    Issue #5313: new check EmptLineWrappingInBlock

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/emptylinewrappinginblock/SuppressionXpathRegressionEmptyLineWrappingInBlockTopSeparator.java
@@ -0,0 +1,8 @@
+package org.checkstyle.suppressionxpathfilter.emptylinewrappinginblock;
+
+public class SuppressionXpathRegressionEmptyLineWrappingInBlockTopSeparator {
+
+    public void test() {}
+    public void test2() { /** warn */
+    }
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/emptylinewrappinginblock/InputEmptyLineWrappingInBlockBottomSeparatorEmptyLine.java
@@ -0,0 +1,49 @@
+package com.puppycrawl.tools.checkstyle.checks.blocks.emptylinewrappinginblock;
+
+/**
+ * Config:
+ *  - bottomSeparator = empty_line
+ *  - tokens = ALL TOKENS
+ */
+public class InputEmptyLineWrappingInBlockBottomSeparatorEmptyLine { // violation
+    public InputEmptyLineWrappingInBlockBottomSeparatorEmptyLine() {} // violation
+    public void test() {}
+
+    public void test2() {} // violation
+    public void test3() {
+        int x = 10;
+        if(x == 1) {
+            // Do something
+        }
+
+        else {
+            // Do something else
+        }
+
+        try {
+            throw new Exception();
+        }
+
+        catch (Exception e) {
+            // Handle exception
+        }
+    }
+
+    public void test4() { // violation
+        int x = 10;
+        if(x == 1) { // violation
+            // Do something
+        }
+        else { // violation
+            // Do something else
+        }
+        try { // violation
+            throw new Exception();
+        }
+        catch (Exception e) {
+            // Handle Exception
+        }
+    }
+}
+interface TestInterface2 { // violation
+}
