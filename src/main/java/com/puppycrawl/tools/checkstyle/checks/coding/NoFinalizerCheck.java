4f948627ca554af73121117a4ed1d878e3401379
Omar Shawky <ipowerfuliii@gmail.com>
2020-04-27 23:16:26 +0200

    Issue #7671: Update doc for NoFinalizer

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/NoFinalizerCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/NoFinalizerCheck.java
@@ -45,6 +45,21 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * <pre>
  * &lt;module name=&quot;NoFinalizer&quot;/&gt;
  * </pre>
+ * <p>Example:</p>
+ * <pre>
+ *  public class Test {
+ *
+ *      protected void finalize() throws Throwable { // violation
+ *          try {
+ *             System.out.println("overriding finalize()");
+ *          } catch (Throwable t) {
+ *             throw t;
+ *          } finally {
+ *             super.finalize();
+ *          }
+ *      }
+ *  }
+ * </pre>
  *
  * @since 5.0
  */
