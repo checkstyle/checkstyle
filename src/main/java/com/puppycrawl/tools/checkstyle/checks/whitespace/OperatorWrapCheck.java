74eb9219b6892f288cd293162666643b811ff5ff
pbludov <pbludov@gmail.com>
2020-05-03 11:12:48 +0300

    Issue #8018: AST based operatorwrap

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/whitespace/OperatorWrapCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/whitespace/OperatorWrapCheck.java
@@ -20,12 +20,14 @@
 package com.puppycrawl.tools.checkstyle.checks.whitespace;
 
 import java.util.Locale;
+import java.util.function.UnaryOperator;
 
 import com.puppycrawl.tools.checkstyle.StatelessCheck;
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
 
 /**
  * <p>
@@ -302,28 +304,168 @@ public class OperatorWrapCheck
 
     @Override
     public void visitToken(DetailAST ast) {
-        final DetailAST parent = ast.getParent();
-        // we do not want to check colon for cases and defaults
-        if (parent.getType() != TokenTypes.LITERAL_DEFAULT
-                && parent.getType() != TokenTypes.LITERAL_CASE) {
-            final String text = ast.getText();
-            final int colNo = ast.getColumnNo();
-            final int lineNo = ast.getLineNo();
-            final String currentLine = getLine(lineNo - 1);
-
-            // Check if rest of line is whitespace, and not just the operator
-            // by itself. This last bit is to handle the operator on a line by
-            // itself.
-            if (option == WrapOption.NL
-                    && !text.equals(currentLine.trim())
-                    && CommonUtil.isBlank(currentLine.substring(colNo + text.length()))) {
-                log(ast, MSG_LINE_NEW, text);
+        if (isTargetNode(ast)) {
+            if (option == WrapOption.NL && isNewLineModeViolation(ast)) {
+                log(ast, MSG_LINE_NEW, ast.getText());
             }
-            else if (option == WrapOption.EOL
-                    && CommonUtil.hasWhitespaceBefore(colNo - 1, currentLine)) {
-                log(ast, MSG_LINE_PREVIOUS, text);
+            else if (option == WrapOption.EOL && isEndOfLineModeViolation(ast)) {
+                log(ast, MSG_LINE_PREVIOUS, ast.getText());
             }
         }
+    }
+
+    /**
+     * Filters some false tokens that this check should ignore.
+     *
+     * @param node the node to check
+     * @return {@code true} for all nodes this check should validate
+     */
+    private static boolean isTargetNode(DetailAST node) {
+        final boolean result;
+        if (node.getType() == TokenTypes.COLON) {
+            result = !isColonFromLabel(node);
+        }
+        else if (node.getType() == TokenTypes.STAR) {
+            // Unlike the import statement, the multiply operator always has children
+            result = node.hasChildren();
+        }
+        else {
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Checks whether operator violates {@link WrapOption#NL} mode.
+     *
+     * @param ast the DetailAst of an operator
+     * @return {@code true} if mode does not match
+     */
+    private static boolean isNewLineModeViolation(DetailAST ast) {
+        return TokenUtil.areOnSameLine(ast, getLeftNode(ast))
+                && !TokenUtil.areOnSameLine(ast, getRightNode(ast));
+    }
+
+    /**
+     * Checks whether operator violates {@link WrapOption#EOL} mode.
+     *
+     * @param ast the DetailAst of an operator
+     * @return {@code true} if mode does not match
+     */
+    private static boolean isEndOfLineModeViolation(DetailAST ast) {
+        return !TokenUtil.areOnSameLine(ast, getLeftNode(ast));
+    }
+
+    /**
+     * Checks if a node is {@link TokenTypes#COLON} from a label, switch case of default.
+     *
+     * @param node the node to check
+     * @return {@code true} if node matches
+     */
+    private static boolean isColonFromLabel(DetailAST node) {
+        return TokenUtil.isOfType(node.getParent(), TokenTypes.LABELED_STAT,
+            TokenTypes.LITERAL_CASE, TokenTypes.LITERAL_DEFAULT);
+    }
+
+    /**
+     * Checks if a node is {@link TokenTypes#ASSIGN} to a variable or resource.
+     *
+     * @param node the node to check
+     * @return {@code true} if node matches
+     */
+    private static boolean isAssignToVariable(DetailAST node) {
+        return TokenUtil.isOfType(node.getParent(), TokenTypes.VARIABLE_DEF, TokenTypes.RESOURCE);
+    }
+
+    /**
+     * Returns the left neighbour of a binary operator. This is the rightmost
+     * grandchild of the left child or sibling. For the assign operator the return value is
+     * the variable name.
+     *
+     * @param node the binary operator
+     * @return nearest node from left
+     */
+    private static DetailAST getLeftNode(DetailAST node) {
+        DetailAST result;
+        if (node.getFirstChild() == null || isAssignToVariable(node)) {
+            result = node.getPreviousSibling();
+        }
+        else {
+            result = adjustParens(node.getFirstChild(), DetailAST::getNextSibling);
+        }
+        while (result.getLastChild() != null) {
+            result = result.getLastChild();
+        }
+        return result;
+    }
+
+    /**
+     * Returns the right neighbour of a binary operator. This is the leftmost
+     * grandchild of the right child or sibling. For the ternary operator this
+     * is the node between {@code ?} and {@code :} .
+     *
+     * @param node the binary operator
+     * @return nearest node from right
+     */
+    private static DetailAST getRightNode(DetailAST node) {
+        DetailAST result;
+        if (node.getLastChild() == null) {
+            result = node.getNextSibling();
+        }
+        else {
+            final DetailAST rightNode;
+            if (node.getType() == TokenTypes.QUESTION) {
+                rightNode = node.findFirstToken(TokenTypes.COLON).getPreviousSibling();
+            }
+            else {
+                rightNode = node.getLastChild();
+            }
+            result = adjustParens(rightNode, DetailAST::getPreviousSibling);
+        }
+
+        // The ARRAY_INIT AST is confusing. It should be
+        // ARRAY_INIT
+        // |--LCURLY (first child)
+        // `--RCURLY (last child)
+        // but there is no LCURLY for ARRAY_INIT
+        if (result.getType() != TokenTypes.ARRAY_INIT) {
+            while (result.getFirstChild() != null) {
+                result = result.getFirstChild();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Finds matching parentheses among siblings. If the given node is not
+     * {@link TokenTypes#LPAREN} nor {@link TokenTypes#RPAREN}, the method adjusts nothing.
+     * This method is for handling case like {@code
+     *   (condition && (condition
+     *     || condition2 || condition3) && condition4
+     *     && condition3)
+     * }
+     *
+     * @param node the node to adjust
+     * @param step the node transformer, should be {@link DetailAST#getPreviousSibling}
+     *             or {@link DetailAST#getNextSibling}
+     * @return adjusted node
+     */
+    private static DetailAST adjustParens(DetailAST node, UnaryOperator<DetailAST> step) {
+        DetailAST result = node;
+        int accumulator = 0;
+        while (true) {
+            if (result.getType() == TokenTypes.LPAREN) {
+                accumulator--;
+            }
+            else if (result.getType() == TokenTypes.RPAREN) {
+                accumulator++;
+            }
+            if (accumulator == 0) {
+                break;
+            }
+            result = step.apply(result);
+        }
+        return result;
     }
 
 }
