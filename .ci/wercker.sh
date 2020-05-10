b9e607787166ba395830156c434a19a8cc019ca3
nmancus1 <nmancus1@dtcc.edu>
2020-05-10 12:30:21 -0400

    Issue #8228: Update CIs to validate empty output of 'git status' after execution

--- .ci/wercker.sh
+++ .ci/wercker.sh
@@ -460,6 +460,14 @@ validate-ci-temp-empty)
   exit $fail
   ;;
 
+git-status)
+  if [ $(git status | grep "Changes not staged for commit" | wc -l) -gt 0 ]; then
+    echo "There are changes in files after clone, recheck .gitattributes file"
+    sleep 5s
+    false
+  fi
+  ;;
+
 *)
   echo "Unexpected argument: $1"
   sleep 5s
