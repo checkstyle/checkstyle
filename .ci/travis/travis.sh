7717c329bd1a7e94bcbdb38ea25eccb667306525
nmancus1 <nmancus1@dtcc.edu>
2020-05-11 01:27:06 -0400

    Issue #8228: Test for travis.yml

--- .ci/travis/travis.sh
+++ .ci/travis/travis.sh
@@ -452,13 +452,13 @@ no-error-pmd)
   CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
                      --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   echo CS_version: ${CS_POM_VERSION}
-  mkdir -p .ci-temp/
-  cd .ci-temp/
+  #mkdir -p .ci-temp/
+  #cd .ci-temp/
   git clone https://github.com/pmd/pmd.git
   cd pmd
   mvn -e install checkstyle:check -Dcheckstyle.version=${CS_POM_VERSION}
   cd ..
-  removeFolderWithProtectedFiles pmd
+  #removeFolderWithProtectedFiles pmd
   ;;
 
 check-missing-pitests)
