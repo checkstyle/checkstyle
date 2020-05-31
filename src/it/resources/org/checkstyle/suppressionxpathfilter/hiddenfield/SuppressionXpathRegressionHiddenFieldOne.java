4b56bb594a58f32dc298d26d02015f387ee40e7f
pbludov <pbludov@gmail.com>
2020-05-31 15:58:36 +0300

    Issue #8283: ignore methods deprecated since Java 9 methods in test inputs

--- src/it/resources/org/checkstyle/suppressionxpathfilter/hiddenfield/SuppressionXpathRegressionHiddenFieldOne.java
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/hiddenfield/SuppressionXpathRegressionHiddenFieldOne.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class SuppressionXpathRegressionHiddenFieldOne {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-    Integer value = new Integer(1);
+    Integer value = Integer.valueOf(1);
     {
         numbers.forEach((Integer value) -> String.valueOf(value)); //warn
     }
