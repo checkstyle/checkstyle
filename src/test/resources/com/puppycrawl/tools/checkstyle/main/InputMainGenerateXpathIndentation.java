3a3db75bd00d1ed1278d4d8107531637f6532023
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-06-06 02:19:12 +0530

    Pull #8038: Add mainTest cases for Xpath generation and modify inputfiles to support PR #8038

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/main/InputMainGenerateXpathIndentation.java
@@ -0,0 +1,16 @@
+package com.puppycrawl.tools.checkstyle.main;
+
+public class InputMainGenerateXpathIndentation {
+    /**
+     * Test for checking generated xpath for indentation.
+     */
+    public void generateIndentationXpath() {
+        int a = 92;
+        for (int i = 0; i < 32; i++) {
+            i += 5;
+        }
+        if (true) {
+            return;
+        }
+    }
+}
