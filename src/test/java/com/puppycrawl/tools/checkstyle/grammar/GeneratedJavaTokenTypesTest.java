25123c9899faee488bb77484a90ace716775f67d
nmancus1 <nmancus1@dtcc.edu>
2020-06-14 09:52:13 -0400

    minor: add text blocks input (#7103)

--- src/test/java/com/puppycrawl/tools/checkstyle/grammar/GeneratedJavaTokenTypesTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammar/GeneratedJavaTokenTypesTest.java
@@ -253,8 +253,13 @@ public class GeneratedJavaTokenTypesTest {
         assertEquals(195, GeneratedJavaTokenTypes.HEX_DOUBLE_LITERAL, message);
         assertEquals(196, GeneratedJavaTokenTypes.SIGNED_INTEGER, message);
         assertEquals(197, GeneratedJavaTokenTypes.BINARY_EXPONENT, message);
+        assertEquals(198, GeneratedJavaTokenTypes.TEXT_BLOCK_LITERAL_BEGIN, message);
+        assertEquals(199, GeneratedJavaTokenTypes.TEXT_BLOCK_CONTENT, message);
+        assertEquals(200, GeneratedJavaTokenTypes.TEXT_BLOCK_LITERAL_END, message);
+
+
         // Read JavaDoc before changing
-        assertEquals(196, GeneratedJavaTokenTypes.class.getDeclaredFields().length,
+        assertEquals(199, GeneratedJavaTokenTypes.class.getDeclaredFields().length,
                 "all tokens must be added to list in"
                         + " 'GeneratedJavaTokenTypesTest' and verified"
                         + " that their old numbering didn't change");
