811237073a88a407b9c5f32d31e1d18c37d92b9a
Gaurab Dasgupta <gaurabdasg@gmail.com>
2020-07-09 21:07:03 +0530

    Issue #8306: Javadoc Modification for Metadata Generation Support - 3

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
@@ -101,7 +101,7 @@ public class XdocsJavaDocsTest extends AbstractModuleTestSupport {
                 "ParameterName",
                 "MemberName",
                 "CatchParameterName",
-                "LambdaParameterName"
+                "LambdaParameterName",
                 "PackageName",
                 "EmptyCatchBlock",
                 "AvoidNestedBlocks",
@@ -332,8 +332,14 @@ public class XdocsJavaDocsTest extends AbstractModuleTestSupport {
             if (MODIFIED_JAVADOC_FILES.contains(checkName)) {
                 String typeText = "int[]";
                 if (!property.get(2).getTextContent().contains("subset of tokens")) {
-                    final String typeName =
-                            property.get(2).getFirstChild().getFirstChild().getTextContent().trim();
+                    final Node typeNode;
+                    if (property.get(2).getFirstChild().getFirstChild() == null) {
+                        typeNode = property.get(2).getFirstChild().getNextSibling();
+                    }
+                    else {
+                        typeNode = property.get(2).getFirstChild().getFirstChild();
+                    }
+                    final String typeName = typeNode.getTextContent().trim();
                     typeText = FULLY_QUALIFIED_CLASS_NAMES.get(typeName).getTypeName();
                 }
                 result.append(" Type is {@code ").append(typeText).append("}.");
