0a2b26cfc20040745b2d221a5b801e5f210d47fc
Nick Mancuso <nmancus1@dtcc.edu>
2020-07-10 08:24:41 -0400

    Issue #6615: Add support for Java 14 switch and yield

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalTokenTextCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalTokenTextCheckTest.java
@@ -142,7 +142,7 @@ public class IllegalTokenTextCheckTest
 
     @Test
     public void testAcceptableTokensMakeSense() {
-        final int expectedTokenTypesTotalNumber = 172;
+        final int expectedTokenTypesTotalNumber = 173;
         assertEquals(expectedTokenTypesTotalNumber, TokenUtil.getTokenTypesTotalNumber(),
                 "Total number of TokenTypes has changed, acceptable tokens in"
                 + " IllegalTokenTextCheck need to be reconsidered.");
