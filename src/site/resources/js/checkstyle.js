8552b59886ec873fb9b4fd82f4e12757afd0ef7e
Roman Ivanov <ivanov-jr@mail.ru>
2020-07-22 07:42:26 -0700

    minor: temporal suppress all jslint and jshint in checkstyle.js till we find a way to make it better

--- src/site/resources/js/checkstyle.js
+++ src/site/resources/js/checkstyle.js
@@ -1,23 +1,25 @@
-/*jslint es6 */
 /*jslint browser: true*/
 /*jshint esversion: 6*/
 /*global window */
 let scrollButton;
 const scrollDistanceToButtonVisibility = 500;
 
+// noinspection JSLint
 window.addEventListener("load", () => {
-    scrollButton = document.querySelector("a[title=\"toTop\"]");
+    scrollButton = document.querySelector("a[title=\"toTop\"]"); // jshint ignore:line
     scrollButton.innerText = "To Top";
     scrollButton.style.display = "none";
 
+    // noinspection JSLint
     scrollButton.addEventListener("click", (event) => {
         event.preventDefault();
         document.documentElement.scrollTop = 0;
     });
 });
 
+// noinspection JSLint
 window.addEventListener("scroll", () => {
-    if (document.documentElement.scrollTop > scrollDistanceToButtonVisibility) {
+    if (document.documentElement.scrollTop > scrollDistanceToButtonVisibility) { // jshint ignore:line
         scrollButton.style.display = "block";
     } else {
         scrollButton.style.display = "none";
