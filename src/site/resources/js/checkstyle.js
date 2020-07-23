00fc3c634bc9cf3d102cc8c51c85ff17d969e6c8
Roman Ivanov <ivanov-jr@mail.ru>
2020-07-23 06:53:34 -0700

    minor: this resolved all violations in IDEA on my local

--- src/site/resources/js/checkstyle.js
+++ src/site/resources/js/checkstyle.js
@@ -1,24 +1,24 @@
+/*jshint esversion: 6 */
 /*jslint browser: true*/
 /*jshint esversion: 6*/
 /*global window */
 let scrollButton;
 const scrollDistanceToButtonVisibility = 500;
 
-// noinspection JSLint
 window.addEventListener("load", function () {
+    "use strict";
     scrollButton = document.querySelector("a[title=\"toTop\"]");
     scrollButton.innerText = "To Top";
     scrollButton.style.display = "none";
 
-    // noinspection JSLint
     scrollButton.addEventListener("click", function (event) {
         event.preventDefault();
         document.documentElement.scrollTop = 0;
     });
 });
 
-// noinspection JSLint
 window.addEventListener("scroll", function () {
+    "use strict";
     if (document.documentElement.scrollTop > scrollDistanceToButtonVisibility) {
         scrollButton.style.display = "block";
     } else {
