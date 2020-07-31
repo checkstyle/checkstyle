08f6d57f3d335e48625838fb8352219f1e183af2
Nick Mancuso <nmancus1@dtcc.edu>
2020-07-31 08:12:15 -0400

    Issue #8561: Records Support check update for OuterTypeFilenameCheck

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/outertypefilename/InputOuterTypeFilenameRecord.java
@@ -0,0 +1,8 @@
+//non-compiled with javac: Compilable with Java14
+package com.puppycrawl.tools.checkstyle.checks.outertypefilename;
+/* Config:
+ * default
+ */
+public record IncorrectName(int x, int y, String str) { // violation
+
+}
