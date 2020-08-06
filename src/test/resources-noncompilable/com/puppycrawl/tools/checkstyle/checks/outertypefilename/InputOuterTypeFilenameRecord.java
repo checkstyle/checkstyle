2057db099eb3ae28e9f066dd9c83672df759e2d2
Nick Mancuso <nmancus1@dtcc.edu>
2020-08-05 22:31:08 -0400

    minor: rename IncorrectName to IncorrectName1 for OuterTypeFilenameCheck for updated UT(#8598)

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/outertypefilename/InputOuterTypeFilenameRecord.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/outertypefilename/InputOuterTypeFilenameRecord.java
@@ -1,8 +1,8 @@
 //non-compiled with javac: contains different class name by demand of test
 package com.puppycrawl.tools.checkstyle.checks.outertypefilename;
+
 /* Config:
  * default
  */
-public record IncorrectName(int x, int y, String str) { // violation
-
+public record IncorrectName1(int x, int y, String str) { // violation
 }
