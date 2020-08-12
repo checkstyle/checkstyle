3ed6a15e8e2b8d2ad0b22286d12df4608872602f
Nick Mancuso <nmancus1@dtcc.edu>
2020-08-12 15:37:33 -0400

    Issue #8748: Create new check RecordComponentNumberCheck

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/recordcomponentnumber/InputRecordComponentNumberTopLevel1.java
@@ -0,0 +1,19 @@
+//non-compiled with javac: Compilable with Java14
+package com.puppycrawl.tools.checkstyle.checks.sizes.recordcomponentnumber;
+
+/* Config:
+ *
+ * maxInPublicRecord = 8
+ * maxInProtectedRecord = 8
+ * maxInPackageRecord = 8
+ * maxInPrivateRecord = 8
+ *
+ */
+public record InputRecordComponentNumberTopLevel1(int x, int y, int z, // violation
+                                                int a, int b, int c,
+                                                int d, int e, int f,
+                                                int g, int h, int i,
+                                                int j, int k,
+                                                 String... myVarargs) {
+
+}
