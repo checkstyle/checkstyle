33f125bf6434ee48d083d995f2f800da619a55a3
Nick Mancuso <nmancus1@dtcc.edu>
2020-08-12 15:37:33 -0400

    Issue #8748: Create new check RecordComponentNumberCheck

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/sizes/RecordComponentNumberCheck.java
@@ -0,0 +1,298 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2020 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.sizes;
+
+import java.util.Arrays;
+
+import com.puppycrawl.tools.checkstyle.FileStatefulCheck;
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.checks.naming.AccessModifierOption;
+import com.puppycrawl.tools.checkstyle.utils.CheckUtil;
+import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
+
+/**
+ * <p>
+ * Checks the number of record components in the header of a record definition.
+ * </p>
+ * <ul>
+ * <li>
+ * Property {@code maxComponents} - Specify the maximum number of components allowed
+ * in record definition headers.
+ * Type is {@code int}.
+ * Default value is {@code 8}.
+ * </li>
+ * <li>
+ * Property {@code accessModifiers} - Access modifiers of record definitions where
+ * the number of record components should be checked.
+ * Type is {@code com.puppycrawl.tools.checkstyle.checks.naming.AccessModifierOption[]}.
+ * Default value is {@code public, protected, package, private}.
+ * </li>
+ * </ul>
+ * <p>
+ * To configure the check:
+ * </p>
+ * <pre>
+ * &lt;module name="RecordComponentNumber"/&gt;
+ * </pre>
+ * <p>
+ * To configure the check to allow 5 record components at all access modifier levels
+ * for record definitions:
+ * </p>
+ * <pre>
+ * &lt;module name="RecordComponentNumber"&gt;
+ *   &lt;property name="maxComponents" value="5"/&gt;
+ * &lt;/module&gt;
+ * </pre>
+ * <p>
+ * Java code example:
+ * </p>
+ * <pre>
+ * public record MyRecord1(int x, int y, String str) { // ok, 3 components
+ *     ...
+ * }
+ *
+ * public record MyRecord2(int x, int y, String str,
+ *                           Node node, Order order, Data data) { // violation, 6 components
+ *     ...
+ * }
+ * </pre>
+ * <p>
+ * To configure the check to allow 10 record components for a public record definition,
+ * but 3 for private record definitions:
+ * </p>
+ * <pre>
+ * &lt;module name="RecordComponentNumber"&gt;
+ *   &lt;property name="maxComponents" value="3"/&gt;
+ *   &lt;property name="accessModifiers" value="private"/&gt;
+ * &lt;/module&gt;
+ * &lt;module name="RecordComponentNumber"&gt;
+ *   &lt;property name="maxComponents" value="10"/&gt;
+ *   &lt;property name="accessModifiers" value="public"/&gt;
+ * &lt;/module&gt;
+ * </pre>
+ * <p>
+ * Java code example:
+ * </p>
+ * <pre>
+ * public record MyRecord1(int x, int y, String str) { // ok, public record definition allowed 10
+ *     ...
+ * }
+ *
+ * private record MyRecord2(int x, int y, String str, Node node) { // violation
+ *     ...                                // private record definition allowed 3 components
+ * }
+ * </pre>
+ * <p>
+ * Parent is {@code com.puppycrawl.tools.checkstyle.TreeWalker}
+ * </p>
+ * <p>
+ * Violation Message Keys:
+ * </p>
+ * <ul>
+ * <li>
+ * {@code too.many.components}
+ * </li>
+ * </ul>
+ *
+ * @since 8.36
+ */
+@FileStatefulCheck
+public class RecordComponentNumberCheck extends AbstractCheck {
+
+    /**
+     * A key is pointing to the warning message text in "messages.properties"
+     * file.
+     */
+    public static final String MSG_KEY = "too.many.components";
+
+    /** Default maximum number of allowed components. */
+    private static final int DEFAULT_MAX_COMPONENTS = 8;
+
+    /** Specify the maximum number of components allowed in record definition headers. */
+    private int maxComponents = DEFAULT_MAX_COMPONENTS;
+
+    /**
+     * Access modifiers of record definitions where the number
+     * of record components should be checked.
+     */
+    private AccessModifierOption[] accessModifiers = {
+        AccessModifierOption.PUBLIC,
+        AccessModifierOption.PROTECTED,
+        AccessModifierOption.PACKAGE,
+        AccessModifierOption.PRIVATE,
+    };
+
+    /**
+     * Setter to specify the maximum number of components allowed in record definition headers.
+     *
+     * @param value the maximum allowed.
+     */
+    public void setMaxComponents(int value) {
+        maxComponents = value;
+    }
+
+    /**
+     * Setter to access modifiers of record definitions where the number of record
+     * components should be checked.
+     *
+     * @param accessModifiers access modifiers of record definitions which should be checked.
+     */
+    public void setAccessModifiers(AccessModifierOption... accessModifiers) {
+        this.accessModifiers =
+                Arrays.copyOf(accessModifiers, accessModifiers.length);
+    }
+
+    @Override
+    public int[] getDefaultTokens() {
+        return getAcceptableTokens();
+    }
+
+    @Override
+    public int[] getAcceptableTokens() {
+        return new int[] {
+            TokenTypes.RECORD_DEF,
+        };
+    }
+
+    @Override
+    public int[] getRequiredTokens() {
+        return new int[] {
+            TokenTypes.RECORD_DEF,
+        };
+    }
+
+    @Override
+    public void visitToken(DetailAST ast) {
+        // Instantiate new component counter
+        final ComponentCounter counter = new ComponentCounter();
+
+        // Count record components in this record definition
+        countComponents(counter, ast);
+
+        // Check counter and report violations
+        checkCounter(counter, ast);
+    }
+
+    /**
+     * Determine the visibility modifier, then iterate through the record component list
+     * and increment the counter.
+     *
+     * @param counter the ComponentCounter instance for this check
+     * @param ast the ast to check
+     */
+    private static void countComponents(ComponentCounter counter, DetailAST ast) {
+        final DetailAST recordComponents =
+                ast.findFirstToken(TokenTypes.RECORD_COMPONENTS);
+
+        counter.setAccessModifierOption(getAccessModifier(ast));
+
+        TokenUtil.forEachChild(recordComponents,
+                TokenTypes.RECORD_COMPONENT_DEF, node -> counter.increment());
+
+    }
+
+    /**
+     * Check the counter and report violations.
+     *
+     * @param counter the component counter to check
+     * @param ast to report violations against.
+     */
+    private void checkCounter(ComponentCounter counter, DetailAST ast) {
+        if (matchAccessModifiers(counter.getAccessModifierOption())
+                && counter.getTotal() > maxComponents) {
+            log(ast, MSG_KEY, counter.getTotal(), maxComponents);
+        }
+    }
+
+    /**
+     * Returns the access modifier of the record definition at the specified AST. If
+     * the record definition is in an interface or annotation block,
+     * the access modifier is assumed to be public.
+     *
+     * @param ast the token of the method/constructor.
+     * @return the access modifier of the method/constructor.
+     */
+    private static AccessModifierOption getAccessModifier(final DetailAST ast) {
+        final DetailAST modsToken = ast.findFirstToken(TokenTypes.MODIFIERS);
+        return CheckUtil.getAccessModifierFromModifiersToken(modsToken);
+    }
+
+    /**
+     * Checks whether a record definition has the correct access modifier to be checked.
+     *
+     * @param accessModifier the access modifier of the record definition.
+     * @return whether the record definition matches the expected access modifier.
+     */
+    private boolean matchAccessModifiers(final AccessModifierOption accessModifier) {
+        return Arrays.stream(accessModifiers)
+                .anyMatch(modifier -> modifier == accessModifier);
+    }
+
+    /**
+     * Counter class used to track the access modifier of the record definition and the number of
+     * components in the record component list.
+     */
+    private static class ComponentCounter {
+
+        /** The access modifier of the record definition. */
+        private AccessModifierOption accessModifierOption;
+
+        /** Tracks the total. */
+        private int total;
+
+        /**
+         * Increments to counter by one.
+         */
+        private void increment() {
+            total++;
+        }
+
+        /**
+         * Fetches total number of components.
+         *
+         * @return the total number of components.
+         */
+        private int getTotal() {
+            return total;
+        }
+
+        /**
+         * Sets the access modifier for the record components
+         * we are counting.
+         *
+         * @param option the AccessModifierOption of the record definition
+         */
+        private void setAccessModifierOption(AccessModifierOption option) {
+            accessModifierOption = option;
+        }
+
+        /**
+         * Gets the access modifier of the record components we are counting.
+         *
+         * @return the accessModifierOption of this ComponentCounter
+         */
+        private AccessModifierOption getAccessModifierOption() {
+            return accessModifierOption;
+        }
+    }
+
+}
