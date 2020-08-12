19cac768b89ed40f44678281a48deb477bba6b39
Nick Mancuso <nmancus1@dtcc.edu>
2020-08-12 15:37:33 -0400

    Issue #8748: Create new check RecordComponentNumberCheck

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/recordcomponentnumber/InputRecordComponentNumberTopLevel2.java
@@ -0,0 +1,10 @@
+//non-compiled with javac: Compilable with Java14
+package com.puppycrawl.tools.checkstyle.checks.sizes.recordcomponentnumber;
+
+/* Config:
+ *
+ * max = 8
+ */
+public record InputRecordComponentNumberTopLevel2<T>() { // ok
+
+}
