00a6f46468340813b1c6c32407ab0e7df05fda94
Gaurab Dasgupta <gaurabdasg@gmail.com>
2020-08-19 23:34:48 +0530

    Issue #8742: Metadata-gen code migration

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/meta/ModulePropertyDetails.java
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2020 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.meta;
+
+public final class ModulePropertyDetails {
+    private String name;
+    private String type;
+    private String defaultValue;
+    /**
+     * This property is java type that plugins can use to validate user input, it is used when
+     * 'type' field is "String". It is used for special cases such as regexp and tokenSet.
+     */
+    private String validationType;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String getValidationType() {
+        return validationType;
+    }
+
+    public void setValidationType(String validationType) {
+        this.validationType = validationType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
