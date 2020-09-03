2d87a9fbcb539eece8d1dafce1d0d8e0f204cdd7
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2020-09-03 14:51:10 +0530

    Issue #7872: fixes Resolve Pitest Issues - ImportOrderCheck (1)

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrderSimilarGroupPattern.java
@@ -0,0 +1,8 @@
+package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
+
+import java.util.java.io.File;
+import java.io.File;
+
+public class InputImportOrderSimilarGroupPattern {
+
+}
