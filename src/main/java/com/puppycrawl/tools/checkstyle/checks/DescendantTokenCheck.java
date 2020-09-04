e91be0668407c3357e6ac09be20d700397d30afa
Gaurab Dasgupta <gaurabdasg@gmail.com>
2020-09-05 03:59:02 +0530

    Issue #8785: Set tokenTypesSet as validationType for non base token properties

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/DescendantTokenCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/DescendantTokenCheck.java
@@ -44,7 +44,7 @@ import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
  * <li>
  * Property {@code limitedTokens} - Specify set of tokens with limited occurrences as descendants.
  * Type is {@code java.lang.String[]}.
- * Validation type is {@code tokenSet}.
+ * Validation type is {@code tokenTypesSet}.
  * Default value is {@code ""}.
  * </li>
  * <li>
