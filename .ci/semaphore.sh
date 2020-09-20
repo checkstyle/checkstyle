5e0815dece61abd8a42b7b36b0405d7b135b7951
pbludov <pbludov@gmail.com>
2020-09-20 16:40:54 +0300

    Issue #8805: move all-sevntu-checks, no-exception-tests, no-error-pmd, no-violation-test-josm to semaphore ci

--- .ci/semaphore.sh
+++ .ci/semaphore.sh
@@ -1,58 +1,63 @@
 #!/bin/bash
 set -e
 
+removeFolderWithProtectedFiles() {
+  find "$1" -delete
+}
+
 case $1 in
 
-assembly)
-  mvn -e clean package -Passembly
+all-sevntu-checks)
+  working_dir=.ci-temp/all-sevntu-checks
+  mkdir -p $working_dir
+  xmlstarlet sel --net --template -m .//module -v "@name" -n config/checkstyle_sevntu_checks.xml \
+    | grep -vE "Checker|TreeWalker|Filter|Holder" | grep -v "^$" \
+    | sed "s/com\.github\.sevntu\.checkstyle\.checks\..*\.//" \
+    | sort | uniq | sed "s/Check$//" > $working_dir/file.txt
+  wget -q http://sevntu-checkstyle.github.io/sevntu.checkstyle/apidocs/allclasses-frame.html -O - \
+    | grep "<li>" | cut -d '>' -f 3 | sed "s/<\/a//" \
+    | grep -E "Check$" \
+    | sort | uniq | sed "s/Check$//" > $working_dir/web.txt
+  # temporal ignore list
+  # sed -i.backup '/Jsr305Annotations/d' web.txt
+  diff -u $working_dir/web.txt $working_dir/file.txt
+  removeFolderWithProtectedFiles $working_dir
   ;;
 
-site)
-  mvn -e clean site -Pno-validations -Dlinkcheck.skip=true
+no-error-pmd)
+  CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
+                     --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+  echo "CS_version: ${CS_POM_VERSION}"
+  mkdir -p .ci-temp/
+  cd .ci-temp/
+  git clone https://github.com/pmd/pmd.git
+  cd pmd
+  mvn -e install checkstyle:check -Dcheckstyle.version=${CS_POM_VERSION}
+  cd ..
+  removeFolderWithProtectedFiles pmd
   ;;
 
-verify)
-  mvn -e clean verify
-  ;;
-
-test-de)
-  mvn -e clean integration-test failsafe:verify \
-    -DargLine='-Duser.language=de -Duser.country=DE -Xms1024m -Xmx2048m'
-  ;;
-
-test-es)
-  mvn -e clean integration-test failsafe:verify \
-    -DargLine='-Duser.language=es -Duser.country=ES -Xms1024m -Xmx2048m'
-  ;;
-
-test-fi)
-  mvn -e clean integration-test failsafe:verify \
-    -DargLine='-Duser.language=fi -Duser.country=FI -Xms1024m -Xmx2048m'
-  ;;
-
-test-fr)
-  mvn -e clean integration-test failsafe:verify \
-    -DargLine='-Duser.language=fr -Duser.country=FR -Xms1024m -Xmx2048m'
-  ;;
-
-test-ja)
-  mvn -e clean integration-test failsafe:verify \
-    -DargLine='-Duser.language=ja -Duser.country=JP -Xms1024m -Xmx2048m'
-  ;;
-
-test-pt)
-  mvn -e clean integration-test failsafe:verify \
-    -DargLine='-Duser.language=pt -Duser.country=PT -Xms1024m -Xmx2048m'
-  ;;
-
-test-tr)
-  mvn -e clean integration-test failsafe:verify \
-    -DargLine='-Duser.language=tr -Duser.country=TR -Xms1024m -Xmx2048m'
-  ;;
-
-test-zh)
-  mvn -e clean integration-test failsafe:verify \
-    -DargLine='-Duser.language=zh -Duser.country=CN -Xms1024m -Xmx2048m'
+no-violation-test-josm)
+  CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
+                     --non-recursive org.codehaus.mojo:exec-maven-plugin:1.6.0:exec)
+  echo "CS_version: ${CS_POM_VERSION}"
+  mkdir -p .ci-temp
+  cd .ci-temp
+  TESTED=$(wget -q -O - https://josm.openstreetmap.de/wiki/TestedVersion?format=txt)
+  echo "JOSM revision: ${TESTED}"
+  svn -q --force export https://josm.openstreetmap.de/svn/trunk/ -r "${TESTED}" --native-eol LF josm
+  cd josm
+  sed -i -E "s/(name=\"checkstyle\" rev=\")([0-9]+\.[0-9]+(-SNAPSHOT)?)/\1${CS_POM_VERSION}/" \
+   tools/ivy.xml
+  ant -v checkstyle
+  grep "<error" checkstyle-josm.xml | cat > errors.log
+  echo "Checkstyle Errors:"
+  RESULT=$(wc -l < errors.log)
+  cat errors.log
+  echo "Size of output: ${RESULT}"
+  cd ..
+  removeFolderWithProtectedFiles josm
+  if [[ ${RESULT} != 0 ]]; then false; fi
   ;;
 
 *)
