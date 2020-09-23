65baee27fdc4e9b5b181bed71402423de98a31f8
pbludov <pbludov@gmail.com>
2020-09-23 09:56:52 +0300

    Issue #8855: set up cirrus ci

---
+++ .ci/validation.cmd
@@ -0,0 +1,40 @@
+@echo off
+
+::----------------------------------------------------------------------
+:: Validation script to run on local for windows users.
+:: Example of usage
+:: ./.ci/validation.cmd  verify_without_checkstyle
+::----------------------------------------------------------------------
+
+SET OPTION=%1
+
+if "%OPTION%" == "sevntu" (
+  call mvn -e verify -DskipTests -DskipITs -Dpmd.skip=true^
+    -Dspotbugs.skip=true -Djacoco.skip=true -Dxml.skip=true^
+    || goto :ERROR
+  goto :END_CASE
+)
+
+if "%OPTION%" ==  "verify_without_checkstyle" (
+  call mvn -e verify -Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true^
+    || goto :ERROR
+  goto :END_CASE
+)
+
+if "%OPTION%" ==  "site_without_verify" (
+  call mvn -e -Pno-validations site^
+    || goto :ERROR
+  goto :END_CASE
+)
+
+echo  Unexpected argument %OPTION%
+SET ERRORLEVEL=-1
+goto :END_CASE
+
+:END_CASE
+  VER > NUL
+  EXIT /b %ERRORLEVEL%
+
+:ERROR
+echo validation failed with error code #%ERRORLEVEL%.
+exit /b %ERRORLEVEL%
