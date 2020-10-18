7db89665c4256bb3286b248b721129c66f649046
Timur Tibeyev <timurtibeyev@gmail.com>
2020-10-19 01:28:43 +0600

    Issue #6481: Generic XPath check

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/genericxpath/SuppressionXpathRegressionGenericXpathOne.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.genericxpath;
+
+public class SuppressionXpathRegressionGenericXpathOne {
+    public void test() { } // warning
+
+    public void correct() { }
+}
