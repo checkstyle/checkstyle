d9379dd5c47eabefd8314283aeda0b239a625045
Vyom-Yadav <jackhammervyom@gmail.com>
2020-11-28 09:01:22 +0530

    Issue #7564: Added example for default config

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/ConstantNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/ConstantNameCheck.java
@@ -61,9 +61,6 @@ import com.puppycrawl.tools.checkstyle.utils.ScopeUtil;
  * <p>
  * To configure the check:
  * </p>
- * <p>
- * The following configuration is applicable by default.
- * </p>
  * <pre>
  * &lt;module name="ConstantName"/&gt;
  * </pre>
@@ -74,7 +71,7 @@ import com.puppycrawl.tools.checkstyle.utils.ScopeUtil;
  *   protected final static int SECOND_CONSTANT2 = 100; // OK
  *   final static int third_Constant3 = 1000; // violation, name 'third_Constant3' must
  *                                           // match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'
- *   private final static int fourth_Constant4 = 50;// violation, name 'fourth_Constant4' must match
+ *   private final static int fourth_Const4 = 50; // violation, name 'fourth_Const4' must match
  *                                                 // pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'
  * }
  * </pre>
