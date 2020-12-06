acc0102b7a404b37106965eb4391382ab254f731
pbludov <pbludov@gmail.com>
2020-12-06 19:28:31 +0300

    Issue #4219: UnusedImports doesn't recognize imports shadowed by inner types

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsShadowed.java
@@ -0,0 +1,69 @@
+package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
+
+import java.util.AbstractMap; // ok
+import java.util.List; // ok
+import java.util.Map; // violation
+import java.util.Set; // violation
+import java.util.concurrent.Callable; // ok
+
+import com.puppycrawl.tools.checkstyle.checks.imports.unusedimports.InputUnusedImportsShadowed; // violation
+import com.puppycrawl.tools.checkstyle.checks.imports.unusedimports.InputUnusedImportsShadowed.Deprecated; // ok
+import com.puppycrawl.tools.checkstyle.checks.imports.unusedimports.InputUnusedImportsShadowed.Nested; // ok
+import com.puppycrawl.tools.checkstyle.checks.imports.unusedimports.InputUnusedImportsShadowed.Nested.List.Map.Entry; // ok
+
+/*
+ * Config: default
+*/
+@Deprecated
+public class InputUnusedImportsShadowed
+        extends AbstractMap.SimpleEntry
+        implements Comparable<Nested.List.Map.Entry>, Callable<Entry> {
+
+    InputUnusedImportsShadowed() {
+        super(null, null);
+        List x = null; // java.util.List
+    }
+
+    @Override
+    public int compareTo(Nested.List.Map.Entry entry) {
+        return 0;
+    }
+
+    @Override
+    public Entry call() throws Exception {
+        return Entry.IGNORED;
+    }
+
+    static class Nested {
+        List.Map field;
+        public Nested() {
+            List x = null;
+        }
+
+        @Set
+        interface List {
+            Map foo();
+            class Map {
+                enum Entry {
+                    BAR, FOO, IGNORED
+                }
+            }
+        }
+    }
+
+    @interface Set {
+    }
+
+    @interface Deprecated {
+    }
+
+    interface Callable<V> {
+        V call();
+    }
+
+    interface AbstractMap {
+        class SimpleEntry {
+        }
+    }
+
+}
