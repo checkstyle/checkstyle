8381cd6a965583e6431ea4b4dd4d60979fc47138
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-12-10 01:57:10 +0530

    Issue #9086: ForceStrictCondition should not affect individual array elements

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationZeroArrayInit.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationZeroArrayInit.java
@@ -19,7 +19,7 @@ public class InputIndentationZeroArrayInit { //indent:0 exp:0
     interface MyInterface { //indent:4 exp:4
         @interface SomeAnnotation { String[] values(); } //indent:8 exp:8
         @SomeAnnotation(values = { //indent:8 exp:8
-           "Hello"//indent:11 exp:11
+           "Hello"//indent:11 exp:8,12,35,37 warn
         }) //indent:8 exp:8
         void works(); //indent:8 exp:8
     } //indent:4 exp:4
