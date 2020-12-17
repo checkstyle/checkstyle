a1c1945309280d05e2b55bb38c0ed27c857b0d77
Andrei Paikin <andreypaykin@gmail.com>
2020-12-17 17:48:26 +0300

    Issue #7875: resolve pitest issue for ImportOrderCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrder_DotPackageName.java
@@ -0,0 +1,12 @@
+package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import javax.swing.JComponent;
+import javax.swing.JTable;
+
+public class InputImportOrder_DotPackageName {
+}
+
