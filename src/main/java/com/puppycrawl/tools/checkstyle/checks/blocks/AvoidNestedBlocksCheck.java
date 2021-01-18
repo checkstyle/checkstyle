c45e564794ccf0bf1d658b2b29857cfdb0139352
Ayushman Sachan <ashusachan01@gmail.com>
2021-01-18 22:25:28 +0530

    Issue #7579: Update doc for AvoidNestedBlocks

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/AvoidNestedBlocksCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/AvoidNestedBlocksCheck.java
@@ -85,17 +85,17 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  *     myInteger = 2;
  *   }
  *   System.out.println("myInteger = " + myInteger);
- * }
  *
- * switch (a) {
- *   case 1:
- *     {                    // violation
- *       System.out.println("Case 1");
+ *   switch (a) {
+ *     case 1:
+ *       {                    // violation
+ *         System.out.println("Case 1");
+ *         break;
+ *       }
+ *     case 2:
+ *       System.out.println("Case 2");     // OK
  *       break;
- *     }
- *   case 2:
- *     System.out.println("Case 2");     // OK
- *     break;
+ *   }
  * }
  * </pre>
  * <p>
