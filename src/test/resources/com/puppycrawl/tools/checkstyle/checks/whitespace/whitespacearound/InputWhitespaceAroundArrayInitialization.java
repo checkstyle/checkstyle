fde0290cb707bd9ab2fff7f0c850fda63774f0f9
Vyom Yadav <73882557+Vyom-Yadav@users.noreply.github.com>
2021-02-05 15:13:31 +0530

    Issue #3203: Adding new property enforceWhitespaceAroundArrayInit

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/InputWhitespaceAroundArrayInitialization.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/InputWhitespaceAroundArrayInitialization.java
@@ -4,27 +4,27 @@ public class InputWhitespaceAroundArrayInitialization {
 
     public void arrayInitTest() {
 
-        final int[] COLORS = new int[]{5 }; // missing WS before "{"
+        final int[] COLORS = new int[]{5 }; // missing WS before and after "{"
 
-        final int[] COLORS1 = new int[] {5 }; // valid
+        final int[] COLORS1 = new int[] {5 }; // missing WS after "{"
 
-        final String[][] COLORS2 = {{"Green"}, {"Red"}}; //missing WS before inner array
+        final String[][] COLORS2 = {{"Green"}, {"Red"}}; //missing WS before and after "{"
 
-        final String[][] COLORS21 = { {"Green", "Red"},{"White"} }; //missing WS between "," and "{"
+        final String[][] COLORS21 = { {"Green", "Red"},{"White"} }; // missing WS between ","
+                                                                   // and "{"
+        final String[][] COLORS22 = { {"White", "Yellow"}, {"Pink"} }; // valid
 
-        final String[][] COLORS22 = { {"White", "Yellow"}, {"Pink"} }; //valid
+        final String[][][] COLORS31 = { { {"Black", "Blue"}, {"Gray", "White"}}, // missing WS
+                                       { {"Green", "Brown", "Magneta"}}, // after "{"
+                                       { {"Red", "Purple", "Violet"}} };
 
-        final String[][][] COLORS31 = { { {"Black", "Blue"}, {"Gray", "White"}},
-                                       { {"Green", "Brown", "Magneta"}},
-                                       { {"Red", "Purple", "Violet"}} }; //valid
+        final String[][][] COLORS32 = { {{"Red", "Green"},{"Pink"}} }; //missing WS
+                                                                      // after and before "{"
+        final String[][][] COLORS33 = {{{"White"}}}; //missing WS before and after "{"
 
-        final String[][][] COLORS32 = { {{"Red", "Green"},{"Pink"}} }; //missing before Red and Pink
-
-        final String[][][] COLORS33 = {{{"White"}}}; //missing WS before "{"
-
-        final String[][][][] COLORS41 = { { { {"Green"}}, { {"Purple"}}}, { { {"Yellow"}}} };//valid
-
-        final String[][][][] COLORS42 = { { {{"White", "Blue"}, //missing WS between "{"
+        final String[][][][] COLORS41 = { { { {"Green"}}, { {"Purple"}}}, { { {"Yellow"}}} };
+                                                            // missing WS before and after "{"
+        final String[][][][] COLORS42 = { { {{"White", "Blue"}, // missing WS before and after "{"
                                              {"Gray", "Black"},},},
                                              { { {"Red"}}}};
 
