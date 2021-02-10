89ddcc3a31040936b3f6c42d44c6e6aa3d343bdd
Himanshu Rijal <himanshurijal@Himanshus-MacBook-Pro.local>
2021-02-10 11:14:38 -0600

    Issue #4052: SingleLineJavadoc update for new requirements

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/SingleLineJavadocCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/SingleLineJavadocCheck.java
@@ -45,12 +45,16 @@ import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
  * Default value is {@code false}.
  * </li>
  * <li>
- * Property {@code ignoredTags} - Specify block tags which are ignored by the check.
+ * Property {@code ignoredTags} - Specify
+ * <a href="http://docs.oracle.com/javase/8/docs/technotes/tools/windows/javadoc.html#CHDBEFIF">
+ * block tags</a> which are ignored by the check.
  * Type is {@code java.lang.String[]}.
  * Default value is {@code ""}.
  * </li>
  * <li>
- * Property {@code ignoreInlineTags} - Control whether inline tags must be ignored.
+ * Property {@code ignoreInlineTags} - Control whether
+ * <a href="http://docs.oracle.com/javase/8/docs/technotes/tools/windows/javadoc.html#CHDBEFIF">
+ * inline tags</a> must be ignored.
  * Type is {@code boolean}.
  * Default value is {@code true}.
  * </li>
