17d6f1f7af9f5f973deeb5f50e6ccebb86bd5177
Andrei Paikin <andreypaykin@gmail.com>
2021-02-11 10:38:57 +0300

    Issue #9256: add job for checking mentioned closed todo issues

---
+++ .ci/no_old_refs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Attention, there is no "-x" to avoid problems on Travis
+set -e
+
+MENTIONED_IDS=.ci-temp/mentioned_ids
+CLOSED_ISSUES=.ci-temp/failed
+API_ISSUE_LINK_PREFIX="https://api.github.com/repos/checkstyle/checkstyle/issues"
+ISSUE_LINK_PREFIX="https://github.com/checkstyle/checkstyle/issues"
+
+grep -Pohr "(after|[Tt]il[l]?) $ISSUE_LINK_PREFIX/\d{1,5}" . \
+  | sed -e 's/.*issues\///' | sort | uniq > $MENTIONED_IDS
+
+while read issue_id; do
+  STATE=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" "$API_ISSUE_LINK_PREFIX/$issue_id" \
+   | jq '.state' | xargs)
+  if [[ "$STATE" == "closed" ]]; then
+    echo "$ISSUE_LINK_PREFIX/$issue_id" >> $CLOSED_ISSUES
+  fi
+done < $MENTIONED_IDS
+
+rm -f $MENTIONED_IDS
+
+if [ -f "$CLOSED_ISSUES" ]; then
+    echo "Following issues are mentioned in code to do something after they are closed:"
+    cat $CLOSED_ISSUES
+    sleep 5
+    rm -f $CLOSED_ISSUES
+    exit 1
+fi
+
