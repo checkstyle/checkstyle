0b5b946ce3710939033a53638bdaafd3e1d0786d
Akash Mondal <akmo3901@gmail.com>
2021-02-16 10:47:36 +0530

    Issue #6893: Generate Xpath Suppresion on escape xml symbols

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/matchxpath/SuppressionXpathRegressionMatchXpathEncodedAposString.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.matchxpath;
+
+public class SuppressionXpathRegressionMatchXpathEncodedAposString {
+
+    String aposChar = "'SingleQuoteOnBothSide'" + // warning
+        "6";
+}
---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/matchxpath/SuppressionXpathRegressionMatchXpathEncodedGreaterString.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.matchxpath;
+
+public class SuppressionXpathRegressionMatchXpathEncodedGreaterString {
+
+    String greaterChar = ">testFour" + // warning
+            "4";
+}
