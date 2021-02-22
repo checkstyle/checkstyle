6ece0255e223140f93cb9387dcf9d859740354bd
AkMo3 <akmo3901@gmail.com>
2021-02-22 13:19:32 +0530

    Issue #9271: Remove detailAstImp from Rcurly

--- .ci/no_old_refs.sh
+++ .ci/no_old_refs.sh
@@ -2,27 +2,28 @@
 # Attention, there is no "-x" to avoid problems on Travis
 set -e
 
-MENTIONED_IDS=/tmp/mentioned_ids
-CLOSED_ISSUES=/tmp/failed
-API_ISSUE_LINK_PREFIX="https://api.github.com/repos/checkstyle/checkstyle/issues"
-ISSUE_LINK_PREFIX="https://github.com/checkstyle/checkstyle/issues"
+MENTIONED_ISSUES=/tmp/mentioned_issues
+CLOSED_ISSUES=/tmp/failed_issues
+API_GITHUB_PREFIX="https://api.github.com/repos"
+GITHUB_HOST="https://github.com"
 
 # collect issues where full link is used
-grep -Pohr "(after|[Tt]il[l]?) $ISSUE_LINK_PREFIX/\d{1,5}" . \
-  | sed -e 's/.*issues\///' >> $MENTIONED_IDS
+grep -Pohr "(after|[Tt]il[l]?) $GITHUB_HOST/[\w.-]+/[\w.-]+/issues/\d{1,5}" . \
+  | sed -e 's/.*github.com\///' >> $MENTIONED_ISSUES
 
-# collect issues where only hash sign is used
-grep -Pohr "[Tt]il[l]? #\d{1,5}" . | sed -e 's/.*#//' >> $MENTIONED_IDS
+# collect checkstyle issues where only hash sign is used
+grep -Pohr "[Tt]il[l]? #\d{1,5}" . \
+  | sed -e 's/.*#/checkstyle\/checkstyle\/issues\//' >> $MENTIONED_ISSUES
 
-for issue_id in $(sort -u $MENTIONED_IDS); do
-  STATE=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" "$API_ISSUE_LINK_PREFIX/$issue_id" \
+for issue in $(sort -u $MENTIONED_ISSUES); do
+  STATE=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" "$API_GITHUB_PREFIX/$issue" \
    | jq '.state' | xargs)
   if [[ "$STATE" == "closed" ]]; then
-    echo "$ISSUE_LINK_PREFIX/$issue_id" >> $CLOSED_ISSUES
+    echo "$GITHUB_HOST/$issue" >> $CLOSED_ISSUES
   fi
 done
 
-rm -f $MENTIONED_IDS
+rm -f $MENTIONED_ISSUES
 
 if [ -f "$CLOSED_ISSUES" ]; then
     echo "Following issues are mentioned in code to do something after they are closed:"
