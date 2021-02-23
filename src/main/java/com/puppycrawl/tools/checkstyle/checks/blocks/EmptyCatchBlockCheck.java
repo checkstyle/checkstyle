11d4e26b6a83cd8f5b6db1199599ed650958de71
Soumyadeep Ganguly <gangulysoumyadeep231@gmail.com>
2021-02-23 17:48:12 +0530

    Issue #7581: Add default config and code examples for EmptyCatchBlock

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/EmptyCatchBlockCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/EmptyCatchBlockCheck.java
@@ -53,6 +53,27 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * </li>
  * </ul>
  * <p>
+ *   To configure the check:
+ * </p>
+ * <pre>
+ * &lt;module name=&quot;EmptyCatchBlock&quot;/&gt;
+ * </pre>
+ * <p>
+ *   Example:
+ * </p>
+ * <pre>
+ * class Hello {
+ *   public static void main(String args[]) {
+ *     try {
+ *
+ *     }
+ *     catch(Exception e) { // violation since catch block is empty
+ *
+ *     }
+ *   }
+ * }
+ * </pre>
+ * <p>
  * To configure the check to suppress empty catch block if exception's variable name is
  * {@code expected} or {@code ignore} or there's any comment inside:
  * </p>
