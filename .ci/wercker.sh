78fe7bcc988ed20b71e3b8d9d5d8bbbdb5045bef
rnveach <rveach02@gmail.com>
2021-02-25 23:44:54 -0500

    minor: revert for CI

--- .ci/wercker.sh
+++ .ci/wercker.sh
@@ -109,9 +109,8 @@ no-error-equalsverifier)
   CS_POM_VERSION=$(mvn -e -q -Dexec.executable='echo' -Dexec.args='${project.version}' \
                      --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   echo CS_version: ${CS_POM_VERSION}
-  checkout_from https://github.com/checkstyle/equalsverifier.git
+  checkout_from https://github.com/jqno/equalsverifier.git
   cd .ci-temp/equalsverifier
-  git checkout checkstyle_7417
   mvn -e compile checkstyle:check -Dcheckstyle.version=${CS_POM_VERSION}
   cd ../
   removeFolderWithProtectedFiles equalsverifier
