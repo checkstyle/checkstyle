e5d081545a2ae4780173b0364edd3ec45c1b90c8
VyomYadav <jackhammervyom@gmail.com>
2021-02-28 09:46:42 +0530

    Issue #5779: fixing no violation on parenthesis in if statement between || and &&

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/UnnecessaryParenthesesCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/UnnecessaryParenthesesCheck.java
@@ -104,7 +104,39 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LAMBDA">
  * LAMBDA</a>,
  * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#TEXT_BLOCK_LITERAL_BEGIN">
- * TEXT_BLOCK_LITERAL_BEGIN</a>.
+ * TEXT_BLOCK_LITERAL_BEGIN</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LAND">
+ * LAND</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LITERAL_INSTANCEOF">
+ * LITERAL_INSTANCEOF</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#GT">
+ * GT</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LT">
+ * LT</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#GE">
+ * GE</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LE">
+ * LE</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#EQUAL">
+ * EQUAL</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#NOT_EQUAL">
+ * NOT_EQUAL</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#UNARY_MINUS">
+ * UNARY_MINUS</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#UNARY_PLUS">
+ * UNARY_PLUS</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#INC">
+ * INC</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#DEC">
+ * DEC</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LNOT">
+ * LNOT</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#BNOT">
+ * BNOT</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#POST_INC">
+ * POST_INC</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#POST_DEC">
+ * POST_DEC</a>.
  * </li>
  * </ul>
  * <p>
@@ -251,6 +283,35 @@ public class UnnecessaryParenthesesCheck extends AbstractCheck {
         TokenTypes.STAR_ASSIGN,
     };
 
+    /** Token types for conditional operators. */
+    private static final int[] CONDITIONALS = {
+        TokenTypes.LOR,
+        TokenTypes.LAND,
+        TokenTypes.LITERAL_INSTANCEOF,
+    };
+
+    /** Token types for relational operators. */
+    private static final int[] RELATIONAL = {
+        TokenTypes.GT,
+        TokenTypes.LT,
+        TokenTypes.GE,
+        TokenTypes.LE,
+        TokenTypes.EQUAL,
+        TokenTypes.NOT_EQUAL,
+    };
+
+    /** Token types for unary and postfix operators. */
+    private static final int[] UNARY_AND_POSTFIX = {
+        TokenTypes.UNARY_MINUS,
+        TokenTypes.UNARY_PLUS,
+        TokenTypes.INC,
+        TokenTypes.DEC,
+        TokenTypes.LNOT,
+        TokenTypes.BNOT,
+        TokenTypes.POST_INC,
+        TokenTypes.POST_DEC,
+    };
+
     /**
      * Used to test if logging a warning in a parent node may be skipped
      * because a warning was already logged on an immediate child node.
@@ -286,6 +347,22 @@ public class UnnecessaryParenthesesCheck extends AbstractCheck {
             TokenTypes.STAR_ASSIGN,
             TokenTypes.LAMBDA,
             TokenTypes.TEXT_BLOCK_LITERAL_BEGIN,
+            TokenTypes.LAND,
+            TokenTypes.LITERAL_INSTANCEOF,
+            TokenTypes.GT,
+            TokenTypes.LT,
+            TokenTypes.GE,
+            TokenTypes.LE,
+            TokenTypes.EQUAL,
+            TokenTypes.NOT_EQUAL,
+            TokenTypes.UNARY_MINUS,
+            TokenTypes.UNARY_PLUS,
+            TokenTypes.INC,
+            TokenTypes.DEC,
+            TokenTypes.LNOT,
+            TokenTypes.BNOT,
+            TokenTypes.POST_INC,
+            TokenTypes.POST_DEC,
         };
     }
 
@@ -316,6 +393,22 @@ public class UnnecessaryParenthesesCheck extends AbstractCheck {
             TokenTypes.STAR_ASSIGN,
             TokenTypes.LAMBDA,
             TokenTypes.TEXT_BLOCK_LITERAL_BEGIN,
+            TokenTypes.LAND,
+            TokenTypes.LITERAL_INSTANCEOF,
+            TokenTypes.GT,
+            TokenTypes.LT,
+            TokenTypes.GE,
+            TokenTypes.LE,
+            TokenTypes.EQUAL,
+            TokenTypes.NOT_EQUAL,
+            TokenTypes.UNARY_MINUS,
+            TokenTypes.UNARY_PLUS,
+            TokenTypes.INC,
+            TokenTypes.DEC,
+            TokenTypes.LNOT,
+            TokenTypes.BNOT,
+            TokenTypes.POST_INC,
+            TokenTypes.POST_DEC,
         };
     }
 
@@ -369,6 +462,14 @@ public class UnnecessaryParenthesesCheck extends AbstractCheck {
                 if (last.getType() == TokenTypes.RPAREN) {
                     log(ast, MSG_ASSIGN);
                 }
+            }
+            else if ((isInTokenList(type, CONDITIONALS)
+                    || isInTokenList(type, RELATIONAL)
+                    || isInTokenList(type, UNARY_AND_POSTFIX))
+                    && (isInTokenList(parent.getType(), CONDITIONALS)
+                    || isInTokenList(parent.getType(), RELATIONAL))
+                    && isSurrounded(ast)) {
+                log(ast.getPreviousSibling(), MSG_EXPR);
             }
         }
     }
