c6205266420ab239e2516bdc6aaa5d47c4d0bf8b
AkMo3 <akmo3901@gmail.com>
2021-03-02 13:14:22 +0530

    Issue #6893: Generate Xpath Suppresion on escape xml symbols

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/xpath/xpathquerygenerator/InputXpathQueryGeneratorTextBlock.java
@@ -0,0 +1,11 @@
+//non-compiled with javac: Compilable with Java14
+package com.puppycrawl.tools.checkstyle.xpath.xpathquerygenerator;
+
+public class InputXpathQueryGeneratorTextBlock {
+
+    String testOne = """               // violation
+        &This is the first line
+        >This is the second line
+        <This is the third line
+        """;
+}
