34f12d91b21a0d8f45270af3599ed2fbf6ec69fa
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-03-07 07:34:23 +0530

    Issue #9348: Extract common bash CI functions into util.sh

---
+++ .ci/util.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script contains common bash CI functions
+set -e
+
+removeFolderWithProtectedFiles() {
+  find $1 -delete
+}
+
+function getCheckstylePomVersion {
+  echo "$(mvn -e --no-transfer-progress -q -Dexec.executable='echo' \
+                      -Dexec.args='${project.version}' \
+                      --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)"
+}
+
+function checkout_from {
+  CLONE_URL=$1
+  PROJECT=$(echo "$CLONE_URL" | sed -nE 's/.*\/(.*).git/\1/p')
+  mkdir -p .ci-temp
+  cd .ci-temp
+  if [ -d "$PROJECT" ]; then
+    echo "Target project $PROJECT is already cloned, latest changes will be fetched"
+    cd $PROJECT
+    git fetch
+    cd ../
+  else
+    for i in 1 2 3 4 5; do git clone $CLONE_URL && break || sleep 15; done
+  fi
+  cd ../
+}
