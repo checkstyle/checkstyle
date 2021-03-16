739d1a4d44261436356ced12518d52df4e21408c
theexplorist <kartiksapra005@gmail.com>
2021-03-16 15:50:59 +0530

    Issue #9414: Added Example of AST for TokenTypes.DEC

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -2488,6 +2488,18 @@ public final class TokenTypes {
     /**
      * The {@code --} (prefix decrement) operator.
      *
+     * <p>For example:</p>
+     * <pre>
+     * --a;
+     * </pre>
+     * <p>parses as:</p>
+     * <pre>
+     * |--EXPR -&gt; EXPR
+     * |   `--DEC -&gt; --
+     * |       `--IDENT -&gt; a
+     * |--SEMI -&gt; ;
+     * </pre>
+     *
      * @see <a
      * href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.15.2">Java
      * Language Specification, &sect;15.15.2</a>
