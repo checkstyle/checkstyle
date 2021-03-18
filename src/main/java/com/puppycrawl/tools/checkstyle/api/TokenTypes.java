d0217d6162583a4912363c3572a28a19de207204
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-03-18 22:49:56 +0530

    Issue #9571: added example of AST for TokenTypes.SR

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -2543,6 +2543,21 @@ public final class TokenTypes {
     /**
      * The {@code >>} (signed shift right) operator.
      *
+     * <p>For example:</p>
+     * <pre>
+     * a = a &gt;&gt; b;
+     * </pre>
+     * <p>parses as:</p>
+     * <pre>
+     * |--EXPR -&gt; EXPR
+     * |   `--ASSIGN -&gt; =
+     * |       |--IDENT -&gt; a
+     * |       `--SR -&gt; &gt;&gt;
+     * |           |--IDENT -&gt; a
+     * |           `--IDENT -&gt; b
+     * |--SEMI -&gt; ;
+     * </pre>
+     *
      * @see <a
      * href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.19">Java
      * Language Specification, &sect;15.19</a>
