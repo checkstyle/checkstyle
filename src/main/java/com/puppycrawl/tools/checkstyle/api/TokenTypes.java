11ac6640858f093d6cf1e5ad2b681535453911c5
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-03-19 13:54:27 +0530

    Issue #9405: Added example of AST for TokenTypes.BSR_ASSIGN

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -2271,6 +2271,19 @@ public final class TokenTypes {
      * The {@code >>>=} (unsigned right shift assignment)
      * operator.
      *
+     * <p>For example:</p>
+     * <pre>
+     * a &gt;&gt;&gt;= b;
+     * </pre>
+     * <p>parses as:</p>
+     * <pre>
+     * |--EXPR -&gt; EXPR
+     * |   `--BSR_ASSIGN -&gt; &gt;&gt;&gt;=
+     * |       |--IDENT -&gt; a
+     * |       `--IDENT -&gt; b
+     * |--SEMI -&gt; ;
+     * </pre>
+     *
      * @see <a
      * href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.26.2">Java
      * Language Specification, &sect;15.26.2</a>
