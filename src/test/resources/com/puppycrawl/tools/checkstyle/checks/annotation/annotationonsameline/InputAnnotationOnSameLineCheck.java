a1558052ce018b6957bdd3e27545d4aee54f647e
Ayushman Sachan <ashusachan01@gmail.com>
2021-03-20 23:15:46 +0530

    Issue #9522: Update AnnotationOnSameLineCheckTest to use unique input files in each test method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationonsameline/InputAnnotationOnSameLineCheck.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationonsameline/InputAnnotationOnSameLineCheck.java
@@ -1,33 +1,33 @@
 package com.puppycrawl.tools.checkstyle.checks.annotation.annotationonsameline;
 
+/* Config:
+ * tokens = "CLASS_DEF, INTERFACE_DEF, ENUM_DEF, METHOD_DEF, CTOR_DEF, VARIABLE_DEF, RECORD_DEF,
+ *           COMPACT_CTOR_DEF"
+ */
 public class InputAnnotationOnSameLineCheck {
 
-    @Annotation int x;
+    @Annotation int x;      // ok
 
     int y;
 
-    @Annotation
-    @SomeClass.Annotation
-    @java.lang.Deprecated
+    @Annotation             // violation
+    @SomeClass.Annotation   // violation
+    @java.lang.Deprecated   // violation
     public int getX() {
         return (int) x;
     }
 
-    @Annotation2 @Annotation
+    @Annotation2 @Annotation    // violation
     public int field;
 
     public
-    @Annotation int field2;
+    @Annotation int field2; // ok
 }
 
 class SomeClass {
 
-    @interface Annotation {
-    }
-
+    @interface Annotation {}
 }
 
-@interface Annotation {
-}
-@interface Annotation2 {
-}
+@interface Annotation {}
+@interface Annotation2 {}
