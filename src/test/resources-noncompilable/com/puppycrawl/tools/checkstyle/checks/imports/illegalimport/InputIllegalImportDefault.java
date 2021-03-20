dc7a7fd55ca5d6d34e09e1a5280e68f897417842
Roman Ivanov <ivanov-jr@mail.ru>
2021-03-20 16:49:06 -0700

    Issue #9678: fix build problem in IDEA with sun package

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/illegalimport/InputIllegalImportDefault.java
@@ -0,0 +1,35 @@
+//non-compiled with javac: contains sun package so IDEA marks it as build problem
+package com.puppycrawl.tools.checkstyle.checks.imports.illegalimport;
+
+import com.puppycrawl.tools.checkstyle.checks.imports.illegalimport.*;
+
+import java.io.*;
+import java.lang.*;
+import java.sql.Connection;
+import java.util.List;
+import java.util.List;
+import java.lang.ArithmeticException;
+import sun.misc.*;
+import java.util.Enumeration;
+import java.util.Arrays;
+
+import javax.swing.JToolBar;
+import javax.swing.JToggleButton;
+import javax.swing.ScrollPaneLayout;
+import javax.swing.BorderFactory;
+import static java.io.File.listRoots;
+
+import static javax.swing.WindowConstants.*;
+import static javax.swing.WindowConstants.*;
+import static java.io.File.createTempFile;
+import sun.reflect.*;
+
+import java.awt.Component;
+import java.awt.Graphics2D;
+import java.awt.HeadlessException;
+import java.awt.Label;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.BitSet;
+
+class InputIllegalImportDefault {}
