2ab25be6b5002d536e8fb52297e87ede719c3f48
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-03-22 21:15:21 +0530

    Issue #8990: New Check to validate method parameter order in javadoc to match parameter order in method

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparamorder/InputJavadocParamOrderIncorrect.java
@@ -0,0 +1,96 @@
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparamorder;
+
+/**
+ * Config: default
+ * Some Javadoc.
+ *
+ * @since something
+ */
+class InputJavadocParamOrderIncorrect {
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc. // ok
+     * @param p2 Parameter 2 desc. // violation
+     * @param p1 Parameter 1 desc.
+     * @param <T> Parameter T desc. // violation
+     * @see Some javadoc. // ok
+     * @param p3 Parameter 3 desc.
+     * @return Some number. // ok
+     */
+    static <T> int test(String p1, String p2, T p3) { // violation
+        return 2021;
+    }
+
+    /**
+     * Description
+     *
+     * @param <i> Parameter i desc. // violation
+     * @param str Parameter str desc.
+     */
+    void test2(int i, String str) { // violation
+    }
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc. // OK
+     * @param <E> Parameter E desc. // violation
+     * @param <E> Parameter E desc.
+     * @param CCC Parameter CCC desc. // violation
+     * @param array Parameter array desc.
+     * @see Some javadoc. // OK
+     * @param e Parameter e desc. // violation
+     * @param str Parameter str desc. // violation
+     * @return Some number. // OK
+     */
+    <T, E, C> int sampleMethod(T[] array, String str, E e ) { // violation
+        return 2021;
+    }
+
+    /**
+     * Description.
+     *
+     * @param str Parameter str desc. // violation
+     * @param i Parameter i desc. // violation
+     */
+    public void add(int i, String str) { // violation
+
+    }
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc.
+     * @param <T Parameter T desc // violation
+     * @param array Parameter array desc.
+     * @see Some javadoc.
+     * @return Some number.
+     */
+    public static <T> int sampleMethod(T[] array) { // violation
+        return 2021;
+    }
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc.
+     * @param <> Parameter T desc. // violation
+     * @param array Parameter array desc.
+     * @see Some javadoc. // OK
+     * @return Some number. // OK
+     */
+    public static <T> int sampleMethod2(T[] array) { // violation
+        return 2021;
+    }
+
+    /**
+     * constructor
+     *
+     * @param <i Parameter i desc. // violation
+     */
+    public InputJavadocParamOrderIncorrect (int i) {
+    }
+
+}
