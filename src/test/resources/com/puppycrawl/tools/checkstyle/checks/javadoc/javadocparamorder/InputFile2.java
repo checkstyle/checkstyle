5ccb11e1d9a10263813fe83349fbe0cb39c19dd6
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-03-22 21:15:21 +0530

    Issue #8990: New Check to validate method parameter order in javadoc to match parameter order in method

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparamorder/InputFile2.java
@@ -0,0 +1,36 @@
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparamorder;
+
+/**
+ * Config: default
+ * Some Javadoc.
+ *
+ * @since 8.36 // ok
+ */
+class InputFile2 {
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc. // OK
+     * @param p2
+     *            Parameter 2 desc. // violation
+     * @param p1
+     *            Parameter 1 desc.
+     * @param <T>
+     *            Parameter T desc. // violation
+     * @see Some javadoc. // OK
+     * @param p3
+     *            Parameter 3 desc.
+     * @return Some number. // OK
+     */
+    public <T> int test(String p1, String p2, T p3) {
+        return 2021;
+    }
+
+    /**
+     * xyz
+     */
+    public void test2() {
+    }
+
+}
