fef7b60bb48921622dd2c646fa526bc887e83eee
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-03-22 21:15:21 +0530

    Issue #8990: New Check to validate method parameter order in javadoc to match parameter order in method

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparamorder/InputJavadocParamOrderRecords.java
@@ -0,0 +1,80 @@
+//non-compiled with javac: Compilable with Java15
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparamorder;
+
+/**
+ * Config: default
+ * Some Javadoc.
+ *
+ * @since something
+ */
+class InputJavadocParamOrderRecords {
+
+    /**
+     * Test case for record.
+     *
+     * @param name
+     *            Parameter name desc.
+     * @param <address>
+     *            Parameter address desc. // violation
+     */
+    public record Person(String name, String address) {
+        /**
+         * Test case for constructor.
+         *
+         * @param name
+         *            Parameter name desc.
+         */
+        public Person(String name) {
+            this(name, "Unknown");
+        }
+    }
+
+    /**
+     * Description.
+     *
+     * @param brand
+     *            Parameter brand desc.
+     * @param licensePlate
+     *            Parameter licensePlate desc.
+     */
+    record Vehicle(String brand, String licensePlate) {
+
+        /**
+         * Description.
+         *
+         * @param <brand
+         *            Parameter brand desc. // violation
+         */
+        public Vehicle(String brand) {
+            this(brand, null);
+        }
+    }
+
+    /**
+     * Description.
+     *
+     * @param errors
+     *            Parameter errors desc.
+     */
+    public record InnerRecord(List<String> errors) {
+        public boolean isSuccess () {
+            return errors.isEmpty();
+        }
+    }
+
+    /**
+     * Defining a Generic record.
+     *
+     * @param <T>
+     *            Parameter node desc.
+     * @param <TT>
+     *            Parameter node desc. // violation
+     * @param node
+     *            Parameter node desc.
+     */
+    public record MyTestRecord3<T>(T contents, Node node) {
+        public MyTestRecord3 {
+            int x = 5;
+        }
+    }
+}
