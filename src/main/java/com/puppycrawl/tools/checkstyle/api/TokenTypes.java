4b301a841a9710e6a5d9e2355106537bd095ce53
suubh <subhdec99@gmail.com>
2021-03-22 11:53:51 +0530

    Added example in Literal_Break"

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -1799,6 +1799,16 @@ public final class TokenTypes {
     /**
      * The {@code break} keyword.  The first child is an optional
      * identifier and the last child is a semicolon.
+     * <p>For example:</p>
+     * <pre>
+     *      break;
+     * </pre>
+     * <p>parses as:</p>
+     * <pre>
+     * +--LITERAL_BREAK -&gt; break
+     *      |
+     *      +--SEMI -&gt; ;
+     * </pre>
      *
      * @see #IDENT
      * @see #SEMI
@@ -1810,16 +1820,7 @@ public final class TokenTypes {
     /**
      * The {@code continue} keyword.  The first child is an
      * optional identifier and the last child is a semicolon.
-     * <p>For example:</p>
-     * <pre>
-     *      break;
-     * </pre>
-     * <p>parses as:</p>
-     * <pre>
-     * +--LITERAL_BREAK -&gt; break 
-     *      |   
-     *      +--SEMI -&gt; ;
-     * </pre>
+     *
      * @see #IDENT
      * @see #SEMI
      * @see #SLIST
