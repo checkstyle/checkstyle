0f5bc54be10d1514478e4f444bf5dfa955a93005
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-03-22 21:15:21 +0530

    Issue #8990: New Check to validate method parameter order in javadoc to match parameter order in method

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparamorder/InputJavadocParamOrderIncorrect.java
@@ -0,0 +1,124 @@
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparamorder;
+
+/**
+ * Config: default
+ * Some Javadoc.
+ *
+ * @since something
+ */
+class InputJavadocParamOrderIncorrect {
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc.
+     * @param p2 Parameter 2 desc.  // violation
+     * @param p1 Parameter 1 desc.  // ok
+     * @param <T> Parameter T desc. // violation
+     * @see Some javadoc.
+     * @param p3 Parameter 3 desc.  // ok
+     * @return Some number. // ok
+     */
+    static <T> int test(String p1, String p2, T p3) {
+        return 2021;
+    }
+
+    /**
+     * Description
+     *
+     * @param <i Parameter i desc.     // ok, param does not exists
+     * @param str Parameter str desc.  // ok
+     */
+    void test2(int i, String str) {
+    }
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc.
+     * @param <E> Parameter E desc.       // ok
+     * @param <E> Parameter E desc.       // ok, duplicate param tag
+     * @param CCC Parameter CCC desc.     // ok, param does not exists
+     * @param array Parameter array desc. // ok
+     * @see Some javadoc.
+     * @param e Parameter e desc.         // violation
+     * @param str Parameter str desc.     // violation
+     * @return Some number.
+     */
+    <T, E, C> int sampleMethod(T[] array, String str, E e ) {
+        return 2021;
+    }
+
+    /**
+     * Description.
+     *
+     * @param str Parameter str desc. // violation
+     * @param i Parameter i desc.     // violation
+     */
+    public void add(int i, String str) {
+
+    }
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc.
+     * @param <T Parameter T desc          // ok, param does not exists
+     * @param array Parameter array desc.  // ok
+     * @see Some javadoc.
+     * @return Some number.
+     */
+    public static <T> int sampleMethod(T[] array) {
+        return 2021;
+    }
+
+    /**
+     * Description.
+     *
+     * @version Some javadoc.
+     * @param <> Parameter T desc.        // ok, param does not exists
+     * @param array Parameter array desc. // ok
+     * @see Some javadoc.
+     * @return Some number.
+     */
+    public static <T> int sampleMethod2(T[] array) {
+        return 2021;
+    }
+
+    /**
+     * constructor
+     *
+     * @param <i Parameter i desc. // ok, param does not exists
+     */
+    public InputJavadocParamOrderIncorrect (int i) {
+    }
+
+    /**
+     * Inner class.
+     *
+     * @param <A> A type param            // ok
+     * @param <B1> Another type param     // ok
+     * @param <T> Parameter T desc        // ok, param does not exists
+     * @param <A> A type param            // violation
+     * @param <C456>> Another type param  // ok
+     * @param <C456>> Another type param  // ok, dublicate param
+     */
+    static class Inner<A,B1,C456 extends Comparable>  {
+
+        /**
+         * Description.
+         *
+         * @version Some javadoc.
+         * @param p2 Parameter 2 desc.         // ok, param does not exists
+         * @param <T> Parameter T desc.        // ok
+         * @param <T> Parameter T desc.        // ok, dublicate param
+         * @param p1 Parameter 1 desc.         // ok
+         * @param <T> Parameter T desc.        // violation
+         * @return Some number.
+         */
+        static <T> int test(String p1) {
+            return 2021;
+        }
+   }
+
+}
