a3cdff8c6f679a5eb67522e40c0a9246bf69d183
Nick Mancuso <nmancus1@dtcc.edu>
2021-03-23 11:08:33 -0400

    Issue #7417: change scope to access modifier in JavadocMethodCheck

--- .ci/travis/travis.sh
+++ .ci/travis/travis.sh
@@ -266,7 +266,7 @@ no-error-xwiki)
   CS_POM_VERSION="$(getCheckstylePomVersion)"
   echo version:$CS_POM_VERSION
   mvn -e --no-transfer-progress clean install -Pno-validations
-  checkout_from https://github.com/xwiki/xwiki-commons.git
+  checkout_from "-b checkstyle_7417 https://github.com/nmancus1/xwiki-commons.git"
   cd .ci-temp/xwiki-commons
   # Build custom Checkstyle rules
   mvn -e --no-transfer-progress -f \
