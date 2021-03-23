01e31b23b4d12b62a64d1befbc42dc88dff80d2f
Nick Mancuso <nmancus1@dtcc.edu>
2021-03-23 11:08:33 -0400

    Issue #7417: change scope to access modifier in JavadocMethodCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodSurroundingAccessModifier.java
@@ -0,0 +1,212 @@
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocmethod;
+
+/* Config:
+ *
+ * accessModifiers = "private"
+ */
+public class InputJavadocMethodSurroundingAccessModifier //comment test
+{
+    public int i1;
+    protected int i2;
+    int i3;
+    private int i4;
+
+    public void foo1() {
+    }
+
+    protected void foo2() {
+    }
+
+    void foo3() {
+    }
+
+    private void foo4() {
+    }
+
+    /**
+     * @return // ok
+     */
+    public void foo5() {
+    }
+
+    /**
+     * @return // ok
+     */
+    protected void foo6() {
+    }
+
+    /**
+     * @return // ok
+     */
+    void foo7() {
+    }
+
+    /**
+     * @return // ok
+     */
+    private void foo8() {
+    }
+
+    protected class ProtectedInner {
+        public int i1;
+        protected int i2;
+        int i3;
+        private int i4;
+
+        public void foo1() {
+        }
+
+        protected void foo2() {
+        }
+
+        void foo3() {
+        }
+
+        private void foo4() {
+        }
+    }
+
+    class PackageInner {
+        public int i1;
+        protected int i2;
+        int i3;
+        private int i4;
+
+        public void foo1() {
+        }
+
+        protected void foo2() {
+        }
+
+        void foo3() {
+        }
+
+        private void foo4() {
+        }
+    }
+
+    private class PrivateInner {
+        public int i1;
+        protected int i2;
+        int i3;
+        private int i4;
+
+        public void foo1() {
+        }
+
+        protected void foo2() {
+        }
+
+        void foo3() {
+        }
+
+        private void foo4() {
+        }
+    }
+}
+
+class PackageClass2 {
+    public int i1;
+    protected int i2;
+    int i3;
+    private int i4;
+
+    public void foo1() {
+    }
+
+    protected void foo2() {
+    }
+
+    void foo3() {
+    }
+
+    private void foo4() {
+    }
+
+    public class PublicInner {
+        public int i1;
+        protected int i2;
+        int i3;
+        private int i4;
+
+        public void foo1() {
+        }
+
+        protected void foo2() {
+        }
+
+        void foo3() {
+        }
+
+        private void foo4() {
+        }
+    }
+
+    protected class ProtectedInner {
+        public int i1;
+        protected int i2;
+        int i3;
+        private int i4;
+
+        public void foo1() {
+        }
+
+        protected void foo2() {
+        }
+
+        void foo3() {
+        }
+
+        private void foo4() {
+        }
+    }
+
+    class PackageInner {
+        public int i1;
+        protected int i2;
+        int i3;
+        private int i4;
+
+        public void foo1() {
+        }
+
+        protected void foo2() {
+        }
+
+        void foo3() {
+        }
+
+        private void foo4() {
+        }
+    }
+
+    private class PrivateInner {
+        public int i1;
+        protected int i2;
+        int i3;
+        private int i4;
+
+        public void foo1() {
+        }
+
+        protected void foo2() {
+        }
+
+        void foo3() {
+        }
+
+        private void foo4() {
+        }
+    }
+
+    class IgnoredName {
+        // ignore by name
+        private int logger;
+        // no warning, 'serialVersionUID' fields do not require Javadoc
+        private static final long serialVersionUID = 0;
+    }
+
+    /**/
+    void methodWithTwoStarComment() {
+    }
+}
