ae604c3d6052ecef0503a9854ee257313508cff7
AkMo3 <akmo3901@gmail.com>
2021-03-23 10:39:00 +0530

    Issue #9529: Update JavadocMethodCheckTest to use unique Input file

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethod_1379666.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethod_1379666.java
@@ -1,13 +1,13 @@
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocmethod;
 
 /**
- * comment.
+ * Config: default
  */
 public class InputJavadocMethod_1379666 {
     /**
      * @throws BadStringFormat some text
      */
-    public void ok() throws BadStringFormat {
+    public void ok() throws BadStringFormat { // ok
     }
 
 
@@ -15,25 +15,25 @@ public class InputJavadocMethod_1379666 {
      * Some comment.
      * @throws java.lang.Exception some text
      */
-    public void error1()
+    public void error1() // ok
         throws java.lang.Exception {
     }
 
     /**
      * @throws InputJavadocMethod_1379666.BadStringFormat some text
      */
-    public void error2() throws InputJavadocMethod_1379666.BadStringFormat {
+    public void error2() throws InputJavadocMethod_1379666.BadStringFormat { // ok
     }
 
     /**
      * Some exception class.
      */
-    public static class BadStringFormat extends Exception {
+    public static class BadStringFormat extends Exception { // ok
         /**
          * Some comment.
          * @param s string.
          */
-        BadStringFormat(String s) {
+        BadStringFormat(String s) { // ok
             super(s);
         }
     }
