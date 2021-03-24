f2565abc41c5476dabfacbe01259a890d3f24be5
theanmolsharma <65589791+theanmolsharma@users.noreply.github.com>
2021-03-24 15:35:47 +0530

    Issue #9490: updated example of AST for TokenTypes.LITERAL_PUBLIC

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -1297,6 +1297,21 @@ public final class TokenTypes {
     /**
      * The {@code public} keyword.
      *
+     * <p>For example:</p>
+     * <pre>
+     * public int x;
+     * </pre>
+     * <p>parses as:</p>
+     * <pre>
+     * VARIABLE_DEF -&gt; VARIABLE_DEF
+     *  |--MODIFIERS -&gt; MODIFIERS
+     *  |   `--LITERAL_PUBLIC -&gt; public
+     *  |--TYPE -&gt; TYPE
+     *  |   `--LITERAL_INT -&gt; int
+     *  |--IDENT -&gt; x
+     *  `--SEMI -&gt; ;
+     * </pre>
+     *
      * @see #MODIFIERS
      **/
     public static final int LITERAL_PUBLIC =
