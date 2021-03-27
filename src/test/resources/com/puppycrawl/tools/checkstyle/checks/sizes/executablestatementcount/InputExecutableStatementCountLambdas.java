2107d12fa0d24e4af128007d1db2ab6e79505a72
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-03-27 23:01:53 +0530

    Issue #8971: ExecutableStatementCount should allow LAMBDA token

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/executablestatementcount/InputExecutableStatementCountLambdas.java
@@ -0,0 +1,34 @@
+package com.puppycrawl.tools.checkstyle.checks.sizes.executablestatementcount;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/* Config:
+ * max = 1
+ * tokens = LAMBDA
+ */
+
+public class InputExecutableStatementCountLambdas {
+
+    Consumer a = (o) -> { // violation
+        o.toString(); // 1
+        o.toString(); // 2
+        o.toString(); // 3
+        o.toString(); // 4
+        o.toString(); // 5
+        o.toString(); // 6
+    };
+
+    Consumer b = (x) -> { // violation
+        Consumer c = (s) -> { // violation
+            String str = s.toString();
+            str = str + "!";
+        };
+        Consumer d = (s) -> { // violation
+            String str = s.toString();
+            Consumer t = a -> a.toString().trim();
+            Function x1 = a -> a;
+            Function y = a -> null;
+        };
+    };
+}
