3f0a6e038699546b79e5b555e1ce8dfd9f0bf45d
theanmolsharma <65589791+theanmolsharma@users.noreply.github.com>
2021-03-29 14:03:28 +0530

    Issue #9477: updated example of AST for TokenTypes.LITERAL_FLOAT

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -1340,6 +1340,21 @@ public final class TokenTypes {
     /**
      * The {@code float} keyword.
      *
+     * <p>For example:</p>
+     * <pre>
+     * public float x;
+     * </pre>
+     * <p>parses as:</p>
+     * <pre>
+     * VARIABLE_DEF -&gt; VARIABLE_DEF
+     *  |--MODIFIERS -&gt; MODIFIERS
+     *  |   `--LITERAL_PUBLIC -&gt; public
+     *  |--TYPE -&gt; TYPE
+     *  |   `--LITERAL_FLOAT -&gt; float
+     *  |--IDENT -&gt; x
+     *  `--SEMI -&gt; ;
+     * </pre>
+     *
      * @see #TYPE
      **/
     public static final int LITERAL_FLOAT =
