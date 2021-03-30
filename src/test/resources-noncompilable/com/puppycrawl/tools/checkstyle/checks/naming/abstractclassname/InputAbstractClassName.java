dc109f64377b475e2235fb3c0f56ff51d753771c
Ritik2604 <raggarwal1_be18@thapar.edu>
2021-03-31 00:36:19 +0530

    Issue #9746: update AbstractClassNameCheckTest

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/naming/abstractclassname/InputAbstractClassName.java
@@ -0,0 +1,24 @@
+package com.puppycrawl.tools.checkstyle.checks.naming.abstractclassname;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+
+import org.w3c.dom.Node;
+
+/* Config:
+ * pattern = "^Abstract.+$"
+ */
+
+
+
+public abstract class InputAbstractClassName { // violation
+}
+
+abstract class NonAbstractClassName { // violation
+}
+
+abstract class NonAbstractInnerClass { // violation
+}
+
+abstract class AbstractClassName{   // ok
+}
