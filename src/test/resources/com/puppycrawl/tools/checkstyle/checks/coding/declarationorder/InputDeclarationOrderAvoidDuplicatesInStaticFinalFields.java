60bd719cc6348bf02f38a6717ec7e9dbe7942547
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2021-03-31 00:48:47 +0200

    Issue #9773: update DeclarationOrderCheckTest.java to use unique Input file in each test method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/declarationorder/InputDeclarationOrderAvoidDuplicatesInStaticFinalFields.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/declarationorder/InputDeclarationOrderAvoidDuplicatesInStaticFinalFields.java
@@ -1,8 +1,10 @@
 package com.puppycrawl.tools.checkstyle.checks.coding.declarationorder;
 
+/*
+ * Config: default
+ */
 public class InputDeclarationOrderAvoidDuplicatesInStaticFinalFields
 {
     private boolean allowInSwitchCase;
-    public static final String MSG_KEY_BLOCK_NESTED = "block.nested"; // static after final
-                                                                      // public after private
+    public static final String MSG_KEY_BLOCK_NESTED = "block.nested"; // violation
 }
