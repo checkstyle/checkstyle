3b89c7f6f28f4349e8c419980329843b36236567
Ritik2604 <raggarwal1_be18@thapar.edu>
2021-04-03 17:18:35 +0530

    Issue #9471: update example of TokenTypes.LITERAL_DEFAULT

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -2274,7 +2274,7 @@ public final class TokenTypes {
      *
      * <p>For example:</p>
      * <pre>
-     * switch (3){
+     * switch (type){
      *   case 1 :
      *     x=1;
      *     break;
@@ -2284,35 +2284,35 @@ public final class TokenTypes {
      * </pre>
      * <p>parses as:</p>
      * <pre>
-     * |--LITERAL_SWITCH -&gt; switch
-     * |   |--LPAREN -&gt; (
-     * |   |--EXPR -&gt; EXPR
-     * |   |   `--NUM_INT -&gt; 3
-     * |   |--RPAREN -&gt; )
-     * |   |--LCURLY -&gt; {
-     * |   |--CASE_GROUP -&gt; CASE_GROUP
-     * |   |   |--LITERAL_CASE -&gt; case
-     * |   |   |   |--EXPR -&gt; EXPR
-     * |   |   |   |   `--NUM_INT -&gt; 1
-     * |   |   |   `--COLON -&gt; :
-     * |   |   `--SLIST -&gt; SLIST
-     * |   |       |--EXPR -&gt; EXPR
-     * |   |       |   `--ASSIGN -&gt; =
-     * |   |       |       |--IDENT -&gt; x
-     * |   |       |       `--NUM_INT -&gt; 1
-     * |   |       |   |       |--SEMI -&gt; ;
-     * |   |       `--LITERAL_BREAK -&gt; break
-     * |   |           `--SEMI -&gt; ;
-     * |   |--CASE_GROUP -&gt; CASE_GROUP
-     * |   |   |--LITERAL_DEFAULT -&gt; default
-     * |   |   |   `--COLON -&gt; :
-     * |   |   `--SLIST -&gt; SLIST
-     * |   |       |--EXPR -&gt; EXPR
-     * |   |       |   `--ASSIGN -&gt; =
-     * |   |       |       |--IDENT -&gt; x
-     * |   |       |       `--NUM_INT -&gt; 3
-     * |   |       `--SEMI -&gt; ;
-     * |   `--RCURLY -&gt; }
+     * LITERAL_SWITCH -&gt; switch
+     *   |--LPAREN -&gt; (
+     *   |--EXPR -&gt; EXPR
+     *   |   `--IDENT -&gt; type
+     *   |--RPAREN -&gt; )
+     *   |--LCURLY -&gt; {
+     *   |--CASE_GROUP -&gt; CASE_GROUP
+     *   |   |--LITERAL_CASE -&gt; case
+     *   |   |   |--EXPR -&gt; EXPR
+     *   |   |   |   `--NUM_INT -&gt; 1
+     *   |   |   `--COLON -&gt; :
+     *   |   `--SLIST -&gt; SLIST
+     *   |       |--EXPR -&gt; EXPR
+     *   |       |   `--ASSIGN -&gt; =
+     *   |       |       |--IDENT -&gt; x
+     *   |       |       `--NUM_INT -&gt; 1
+     *   |       |   |       |--SEMI -&gt; ;
+     *   |       `--LITERAL_BREAK -&gt; break
+     *   |           `--SEMI -&gt; ;
+     *   |--CASE_GROUP -&gt; CASE_GROUP
+     *   |   |--LITERAL_DEFAULT -&gt; default
+     *   |   |   `--COLON -&gt; :
+     *   |   `--SLIST -&gt; SLIST
+     *   |       |--EXPR -&gt; EXPR
+     *   |       |   `--ASSIGN -&gt; =
+     *   |       |       |--IDENT -&gt; x
+     *   |       |       `--NUM_INT -&gt; 3
+     *   |       `--SEMI -&gt; ;
+     *   `--RCURLY -&gt; }
      * </pre>
      *
      * @see #CASE_GROUP
