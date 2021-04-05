17f1d3952fc4628a7e40989d817ec76e841419a0
Ayushman Sachan <ashusachan01@gmail.com>
2021-04-05 23:48:20 +0530

    minor: code refactor in test inputs

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/emptyblock/InputEmptyBlockSemantic2Statement.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/emptyblock/InputEmptyBlockSemantic2Statement.java
@@ -11,29 +11,29 @@ import java.awt.Color;
  */
 class InputEmptyBlockSemantic2Statement
 {
-        public void fooMethod()
-        {
-                int a = 1;
-                if (a == 1) {}  // violation
-                char[] s = {'1', '2'};
-                int index = 2;
-                if (doSideEffect() == 1) {} // violation
-                while ((a = index - 1) != 0) {} // ok
-                for (; index < s.length && s[index] != 'x'; index++) {} // violation
-                if (a == 1) {} else {System.identityHashCode("a");} // violation
-                switch (a) {} //warn
-                switch (a) { //ok
-        case 1:
-            a = 2;
-        case 2:
-            a = 3;
-        default:
-            a = 0;
+    public void fooMethod()
+    {
+        int a = 1;
+        if (a == 1) {}  // violation
+        char[] s = {'1', '2'};
+        int index = 2;
+        if (doSideEffect() == 1) {} // violation
+        while ((a = index - 1) != 0) {} // ok
+        for (; index < s.length && s[index] != 'x'; index++) {} // violation
+        if (a == 1) {} else {System.identityHashCode("a");} // violation
+        switch (a) {}   // violation
+        switch (a) {    // ok
+            case 1:
+                a = 2;
+            case 2:
+                a = 3;
+            default:
+                a = 0;
         }
-        }
+    }
 
-        public int doSideEffect()
-        {
-                return 1;
-        }
+    public int doSideEffect()
+    {
+        return 1;
+    }
 }
