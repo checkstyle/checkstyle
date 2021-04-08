745aa125766809d6288be3c10832567f49ea850c
Saransh <cryptosingh@gmail.com>
2021-04-08 16:56:36 +0530

    Issue #9525: update JavadocStyleCheckTest.java to use unique Input file

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocstyle/InputJavadocStyleNoJavadoc.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocstyle/InputJavadocStyleNoJavadoc.java
@@ -1,120 +1,123 @@
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocstyle;
 
-public class InputJavadocStyleNoJavadoc //comment test
+/*
+ * Config: default
+ */
+public class InputJavadocStyleNoJavadoc // ok
 {
-    public int i1;
-    protected int i2;
-    int i3;
-    private int i4;
+    public int i1; // ok
+    protected int i2; // ok
+    int i3; // ok
+    private int i4; // ok
 
-    public void foo1() {}
-    protected void foo2() {}
-    void foo3() {}
-    private void foo4() {}
+    public void foo1() {} // ok
+    protected void foo2() {} // ok
+    void foo3() {} // ok
+    private void foo4() {} // ok
 
-    protected class ProtectedInner {
-        public int i1;
-        protected int i2;
-        int i3;
-        private int i4;
+    protected class ProtectedInner { // ok
+        public int i1; // ok
+        protected int i2; // ok
+        int i3; // ok
+        private int i4; // ok
 
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
+        public void foo1() {} // ok
+        protected void foo2() {} // ok
+        void foo3() {} // ok
+        private void foo4() {} // ok
     }
 
-    class PackageInner {
-        public int i1;
-        protected int i2;
-        int i3;
-        private int i4;
+    class PackageInner { // ok
+        public int i1; // ok
+        protected int i2; // ok
+        int i3; // ok
+        private int i4; // ok
 
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
+        public void foo1() {} // ok
+        protected void foo2() {} // ok
+        void foo3() {} // ok
+        private void foo4() {} // ok
     }
 
-    private class PrivateInner {
-        public int i1;
-        protected int i2;
-        int i3;
-        private int i4;
+    private class PrivateInner { // ok
+        public int i1; // ok
+        protected int i2; // ok
+        int i3; // ok
+        private int i4; // ok
 
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
+        public void foo1() {} // ok
+        protected void foo2() {} // ok
+        void foo3() {} // ok
+        private void foo4() {} // ok
     }
 }
 
-class PackageClass {
-    public int i1;
-    protected int i2;
-    int i3;
-    private int i4;
+class PackageClass { // ok
+    public int i1; // ok
+    protected int i2; // ok
+    int i3; // ok
+    private int i4; // ok
 
-    public void foo1() {}
-    protected void foo2() {}
-    void foo3() {}
-    private void foo4() {}
+    public void foo1() {} // ok
+    protected void foo2() {} // ok
+    void foo3() {} // ok
+    private void foo4() {} // ok
 
-    public class PublicInner {
-        public int i1;
-        protected int i2;
-        int i3;
-        private int i4;
+    public class PublicInner { // ok
+        public int i1; // ok
+        protected int i2; // ok
+        int i3; // ok
+        private int i4; // ok
 
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
+        public void foo1() {} // ok
+        protected void foo2() {} // ok
+        void foo3() {} // ok
+        private void foo4() {} // ok
     }
 
-    protected class ProtectedInner {
-        public int i1;
-        protected int i2;
-        int i3;
-        private int i4;
+    protected class ProtectedInner { // ok
+        public int i1; // ok
+        protected int i2; // ok
+        int i3; // ok
+        private int i4; // ok
 
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
+        public void foo1() {} // ok
+        protected void foo2() {} // ok
+        void foo3() {} // ok
+        private void foo4() {} // ok
     }
 
-    class PackageInner {
-        public int i1;
-        protected int i2;
-        int i3;
-        private int i4;
+    class PackageInner { // ok
+        public int i1; // ok
+        protected int i2; // ok
+        int i3; // ok
+        private int i4; // ok
 
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
+        public void foo1() {} // ok
+        protected void foo2() {} // ok
+        void foo3() {} // ok
+        private void foo4() {} // ok
     }
 
-    private class PrivateInner {
-        public int i1;
-        protected int i2;
-        int i3;
-        private int i4;
+    private class PrivateInner { // ok
+        public int i1; // ok
+        protected int i2; // ok
+        int i3; // ok
+        private int i4; // ok
 
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
+        public void foo1() {} // ok
+        protected void foo2() {} // ok
+        void foo3() {} // ok
+        private void foo4() {} // ok
     }
 
-    class IgnoredName {
+    class IgnoredName { // ok
         // ignore by name
-        private int logger;
+        private int logger; // ok
         // no warning, 'serialVersionUID' fields do not require Javadoc
-        private static final long serialVersionUID = 0;
+        private static final long serialVersionUID = 0; // ok
     }
 
     /**/
-    void methodWithTwoStarComment() {}
+    void methodWithTwoStarComment() {} // ok
 }
