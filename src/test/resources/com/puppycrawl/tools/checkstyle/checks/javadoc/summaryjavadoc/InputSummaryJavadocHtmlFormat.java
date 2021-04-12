1ff6be278449c7f2fea1527353a600de1ce9d6ca
AkMo3 <akmo3901@gmail.com>
2021-04-12 11:43:41 +0530

    Issue #7202: Add Support for Summary Javadoc tag

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc/InputSummaryJavadocHtmlFormat.java
@@ -0,0 +1,78 @@
+package com.puppycrawl.tools.checkstyle.checks.javadoc.summaryjavadoc;
+
+/**
+ * Config: default.
+ */
+public class InputSummaryJavadocHtmlFormat {
+
+    /**
+     * <p><b>{@summary Normal Javadoc.}</b></p>
+     */
+    private void foo1() {} // ok
+
+    /**
+     * <p><b><i>{@summary Normal Javadoc}</i></b></p>
+     */
+    private void foo2() {} // violation
+
+    /**
+     * <p>{@summary Normal Javadoc {@author my code}.}</p>
+     */
+    private void foo3() {} // violation
+
+    /**
+     * {@summary <p>Normal Javadoc.</p>}
+     */
+    private void foo4() {} // ok
+
+    /**
+     * <p>{@code Code.}</p>
+     */
+    private void foo6() {} // violation
+
+    /**
+     * <p>{@summary Some {@code Code.} javadoc.}</p>
+     */
+    private void foo7() {} // ok
+
+    /**
+     * <p>{@summary Some {@author Code.} javadoc.}</p>
+     */
+    private void foo8() {} // violation
+
+    /**
+     * <p>{@throws Some {@author Code.} javadoc.}</p>
+     */
+    private void foo9() {} // violation
+
+    /**
+     * <p>{@summary Some {@input Input Code.} javadoc.}</p>
+     */
+    private void foo10() {} // ok
+
+    /**
+     * <p>{@summary}</p>
+     */
+    private void foo11() {} // violation
+
+    /**
+     * <b>{@summary Normal Javadoc.}</b>
+     */
+    private void foo12() {} // ok
+
+    /**
+     * <b><p>{@summary Normal Javadoc.}</p></b>
+     */
+    private void foo13() {} // ok
+
+    /**
+     * <i><b><p>{@summary Normal Javadoc.}</p></b></i>
+     */
+    private void foo14() {} // ok
+
+    /**
+     * <b>{@summary Normal Javadoc.}</b>
+     */
+    private void foo15() {} // ok
+}
+
