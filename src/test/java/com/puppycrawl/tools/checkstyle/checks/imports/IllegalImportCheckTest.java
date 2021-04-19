70c7320241d210daeb6f64bd3754be0cc90cc458
Saransh <cryptosingh@gmail.com>
2021-04-19 20:58:50 +0530

    Issue #9896: update IllegalImportCheckTest.java to use unique Input file

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/IllegalImportCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/IllegalImportCheckTest.java
@@ -50,11 +50,11 @@ public class IllegalImportCheckTest extends AbstractModuleTestSupport {
             createModuleConfig(IllegalImportCheck.class);
         checkConfig.addAttribute("illegalPkgs", "java.io");
         final String[] expected = {
-            "9:1: " + getCheckMessage(MSG_KEY, "java.io.*"),
-            "23:1: " + getCheckMessage(MSG_KEY, "java.io.File.listRoots"),
-            "27:1: " + getCheckMessage(MSG_KEY, "java.io.File.createTempFile"),
+            "5:1: " + getCheckMessage(MSG_KEY, "java.io.*"),
+            "19:1: " + getCheckMessage(MSG_KEY, "java.io.File.listRoots"),
+            "23:1: " + getCheckMessage(MSG_KEY, "java.io.File.createTempFile"),
         };
-        verify(checkConfig, getPath("InputIllegalImportDefault.java"), expected);
+        verify(checkConfig, getPath("InputIllegalImportDefault1.java"), expected);
     }
 
     @Test
@@ -63,7 +63,7 @@ public class IllegalImportCheckTest extends AbstractModuleTestSupport {
         final DefaultConfiguration checkConfig =
             createModuleConfig(IllegalImportCheck.class);
         final String[] expected = {};
-        verify(checkConfig, getPath("InputIllegalImportDefault.java"), expected);
+        verify(checkConfig, getPath("InputIllegalImportDefault2.java"), expected);
     }
 
     @Test
@@ -97,11 +97,11 @@ public class IllegalImportCheckTest extends AbstractModuleTestSupport {
         checkConfig.addAttribute("illegalClasses", "java.sql.Connection");
         checkConfig.addAttribute("illegalPkgs", "org.junit.jupiter.api");
         final String[] expected = {
-            "11:1: " + getCheckMessage(MSG_KEY, "java.sql.Connection"),
-            "15:1: " + getCheckMessage(MSG_KEY, "org.junit.jupiter.api.*"),
-            "28:1: " + getCheckMessage(MSG_KEY, "org.junit.jupiter.api.*"),
+            "7:1: " + getCheckMessage(MSG_KEY, "java.sql.Connection"),
+            "11:1: " + getCheckMessage(MSG_KEY, "org.junit.jupiter.api.*"),
+            "24:1: " + getCheckMessage(MSG_KEY, "org.junit.jupiter.api.*"),
         };
-        verify(checkConfig, getPath("InputIllegalImportDefault.java"), expected);
+        verify(checkConfig, getPath("InputIllegalImportDefault3.java"), expected);
     }
 
     @Test
@@ -112,11 +112,11 @@ public class IllegalImportCheckTest extends AbstractModuleTestSupport {
         checkConfig.addAttribute("illegalClasses", "java.io.*");
         checkConfig.addAttribute("illegalPkgs", "org.junit.jupiter.api");
         final String[] expected = {
-            "9:1: " + getCheckMessage(MSG_KEY, "java.io.*"),
-            "15:1: " + getCheckMessage(MSG_KEY, "org.junit.jupiter.api.*"),
-            "28:1: " + getCheckMessage(MSG_KEY, "org.junit.jupiter.api.*"),
+            "5:1: " + getCheckMessage(MSG_KEY, "java.io.*"),
+            "11:1: " + getCheckMessage(MSG_KEY, "org.junit.jupiter.api.*"),
+            "24:1: " + getCheckMessage(MSG_KEY, "org.junit.jupiter.api.*"),
         };
-        verify(checkConfig, getPath("InputIllegalImportDefault.java"), expected);
+        verify(checkConfig, getPath("InputIllegalImportDefault4.java"), expected);
     }
 
     @Test
@@ -127,15 +127,15 @@ public class IllegalImportCheckTest extends AbstractModuleTestSupport {
         checkConfig.addAttribute("illegalPkgs", "java\\.util");
         checkConfig.addAttribute("regexp", "true");
         final String[] expected = {
-            "12:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
-            "13:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
-            "16:1: " + getCheckMessage(MSG_KEY, "java.util.Enumeration"),
-            "17:1: " + getCheckMessage(MSG_KEY, "java.util.Arrays"),
-            "34:1: " + getCheckMessage(MSG_KEY, "java.util.Date"),
-            "35:1: " + getCheckMessage(MSG_KEY, "java.util.Calendar"),
-            "36:1: " + getCheckMessage(MSG_KEY, "java.util.BitSet"),
+            "8:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
+            "9:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
+            "12:1: " + getCheckMessage(MSG_KEY, "java.util.Enumeration"),
+            "13:1: " + getCheckMessage(MSG_KEY, "java.util.Arrays"),
+            "30:1: " + getCheckMessage(MSG_KEY, "java.util.Date"),
+            "31:1: " + getCheckMessage(MSG_KEY, "java.util.Calendar"),
+            "32:1: " + getCheckMessage(MSG_KEY, "java.util.BitSet"),
         };
-        verify(checkConfig, getPath("InputIllegalImportDefault.java"), expected);
+        verify(checkConfig, getPath("InputIllegalImportDefault5.java"), expected);
     }
 
     @Test
@@ -147,11 +147,11 @@ public class IllegalImportCheckTest extends AbstractModuleTestSupport {
         checkConfig.addAttribute("illegalClasses", "^java\\.util\\.(List|Arrays)");
         checkConfig.addAttribute("regexp", "true");
         final String[] expected = {
-            "12:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
-            "13:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
-            "17:1: " + getCheckMessage(MSG_KEY, "java.util.Arrays"),
+            "8:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
+            "9:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
+            "13:1: " + getCheckMessage(MSG_KEY, "java.util.Arrays"),
         };
-        verify(checkConfig, getPath("InputIllegalImportDefault.java"), expected);
+        verify(checkConfig, getPath("InputIllegalImportDefault6.java"), expected);
     }
 
     @Test
@@ -164,19 +164,19 @@ public class IllegalImportCheckTest extends AbstractModuleTestSupport {
                 "^java\\.awt\\..*");
         checkConfig.addAttribute("regexp", "true");
         final String[] expected = {
-            "12:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
-            "13:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
-            "16:1: " + getCheckMessage(MSG_KEY, "java.util.Enumeration"),
-            "17:1: " + getCheckMessage(MSG_KEY, "java.util.Arrays"),
-            "30:1: " + getCheckMessage(MSG_KEY, "java.awt.Component"),
-            "31:1: " + getCheckMessage(MSG_KEY, "java.awt.Graphics2D"),
-            "32:1: " + getCheckMessage(MSG_KEY, "java.awt.HeadlessException"),
-            "33:1: " + getCheckMessage(MSG_KEY, "java.awt.Label"),
-            "34:1: " + getCheckMessage(MSG_KEY, "java.util.Date"),
-            "35:1: " + getCheckMessage(MSG_KEY, "java.util.Calendar"),
-            "36:1: " + getCheckMessage(MSG_KEY, "java.util.BitSet"),
+            "8:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
+            "9:1: " + getCheckMessage(MSG_KEY, "java.util.List"),
+            "12:1: " + getCheckMessage(MSG_KEY, "java.util.Enumeration"),
+            "13:1: " + getCheckMessage(MSG_KEY, "java.util.Arrays"),
+            "26:1: " + getCheckMessage(MSG_KEY, "java.awt.Component"),
+            "27:1: " + getCheckMessage(MSG_KEY, "java.awt.Graphics2D"),
+            "28:1: " + getCheckMessage(MSG_KEY, "java.awt.HeadlessException"),
+            "29:1: " + getCheckMessage(MSG_KEY, "java.awt.Label"),
+            "30:1: " + getCheckMessage(MSG_KEY, "java.util.Date"),
+            "31:1: " + getCheckMessage(MSG_KEY, "java.util.Calendar"),
+            "32:1: " + getCheckMessage(MSG_KEY, "java.util.BitSet"),
         };
-        verify(checkConfig, getPath("InputIllegalImportDefault.java"), expected);
+        verify(checkConfig, getPath("InputIllegalImportDefault7.java"), expected);
     }
 
 }
--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/illegalimport/InputIllegalImportDefault.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/illegalimport/InputIllegalImportDefault.java
@@ -1,12 +1,17 @@
 //non-compiled with javac: contains sun package so IDEA marks it as build problem
 package com.puppycrawl.tools.checkstyle.checks.imports.illegalimport;
 
-import java.io.*;
-import sun.misc.*;
-import javax.swing.BorderFactory;
-import static java.io.File.listRoots;
-import sun.reflect.*;
+import java.io.*; // ok
+import sun.misc.*; // ok
+import javax.swing.BorderFactory; // ok
+import static java.io.File.listRoots; // ok
+import sun.reflect.*; // violation
 
-import java.awt.Component;
+import java.awt.Component; // ok
 
+/*
+ * Config:
+ * regexp = true
+ * illegalPkgs = "sun.reflect"
+ */
 class InputIllegalImportDefault {}
