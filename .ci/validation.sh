adc122fa77dea7cc7b2ac121efff5aac7469cbd2
Roman Ivanov <ivanov-jr@mail.ru>
2021-05-01 22:02:05 -0700

    Issue #9266: remove shippable CI

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -164,6 +164,79 @@ no-violation-test-josm)
   if [[ ${RESULT} != 0 ]]; then false; fi
   ;;
 
+no-exception-openjdk9-lucene-and-others)
+  CS_POM_VERSION="$(getCheckstylePomVersion)"
+  echo 'CS_POM_VERSION='${CS_POM_VERSION}
+  build_checkstyle
+  checkout_from https://github.com/checkstyle/contribution
+  cd .ci-temp/contribution/checkstyle-tester
+  sed -i'' 's/^guava/#guava/' projects-for-circle.properties
+  sed -i'' 's/#infinispan/infinispan/' projects-for-circle.properties
+  sed -i'' 's/#protonpack/protonpack/' projects-for-circle.properties
+  sed -i'' 's/#jOOL/jOOL/' projects-for-circle.properties
+  sed -i'' 's/#lucene-solr/lucene-solr/' projects-for-circle.properties
+  groovy diff.groovy --listOfProjects projects-for-circle.properties \
+    --config checks-nonjavadoc-error.xml --checkstyleVersion ${CS_POM_VERSION} \
+    --allowExcludes -xm "-Dcheckstyle.failsOnError=false"
+  cd ../..
+  removeFolderWithProtectedFiles contribution
+  ;;
+
+no-exception-cassandra-storm-tapestry)
+  CS_POM_VERSION="$(getCheckstylePomVersion)"
+  echo 'CS_POM_VERSION='${CS_POM_VERSION}
+  build_checkstyle
+  checkout_from https://github.com/checkstyle/contribution
+  cd .ci-temp/contribution/checkstyle-tester
+  sed -i'' 's/^guava/#guava/' projects-for-circle.properties
+  sed -i'' 's/#tapestry-5/tapestry-5/' projects-for-circle.properties
+  sed -i'' 's/#storm/storm/' projects-for-circle.properties
+  sed -i'' 's/#cassandra/cassandra/' projects-for-circle.properties
+  groovy diff.groovy --listOfProjects projects-for-circle.properties \
+    --config checks-nonjavadoc-error.xml --checkstyleVersion ${CS_POM_VERSION} \
+    --allowExcludes -xm "-Dcheckstyle.failsOnError=false"
+  cd ../..
+  removeFolderWithProtectedFiles contribution
+  ;;
+
+no-exception-hadoop-apache-groovy-scouter)
+  CS_POM_VERSION="$(getCheckstylePomVersion)"
+  echo 'CS_POM_VERSION='${CS_POM_VERSION}
+  build_checkstyle
+  checkout_from https://github.com/checkstyle/contribution
+  cd .ci-temp/contribution/checkstyle-tester
+  sed -i'' 's/^guava/#guava/' projects-for-circle.properties
+  sed -i'' 's/#apache-commons/apache-commons/' projects-for-circle.properties
+  sed -i'' 's/#hadoop/hadoop/' projects-for-circle.properties
+  sed -i'' 's/#groovy/groovy/' projects-for-circle.properties
+  sed -i'' 's/#scouter/scouter/' projects-for-circle.properties
+  groovy diff.groovy --listOfProjects projects-for-circle.properties \
+    --config checks-nonjavadoc-error.xml --checkstyleVersion ${CS_POM_VERSION} \
+    --allowExcludes -xm "-Dcheckstyle.failsOnError=false"
+  cd ../..
+  removeFolderWithProtectedFiles contribution
+  ;;
+
+no-exception-only-javadoc)
+  CS_POM_VERSION="$(getCheckstylePomVersion)"
+  echo 'CS_POM_VERSION='${CS_POM_VERSION}
+  build_checkstyle
+  checkout_from https://github.com/checkstyle/contribution
+  cd .ci-temp/contribution/checkstyle-tester
+  sed -i.'' 's/^guava/#guava/' projects-to-test-on.properties
+  sed -i.'' 's/#spring-framework/spring-framework/' projects-to-test-on.properties
+  sed -i.'' 's/#nbia-dcm4che-tools/nbia-dcm4che-tools/' projects-to-test-on.properties
+  sed -i.'' 's/#spotbugs/spotbugs/' projects-to-test-on.properties
+  sed -i.'' 's/#pmd/pmd/' projects-to-test-on.properties
+  sed -i.'' 's/#apache-ant/apache-ant/' projects-to-test-on.properties
+  groovy diff.groovy --listOfProjects projects-to-test-on.properties \
+    --config checks-only-javadoc-error.xml --checkstyleVersion ${CS_POM_VERSION} \
+    --allowExcludes -xm "-Dcheckstyle.failsOnError=false"
+  cd ../..
+  removeFolderWithProtectedFiles contribution
+  ;;
+
+
 *)
   echo "Unexpected argument: $1"
   sleep 5s
