c7e81c1dff601912c54d7cc828468048385fc71f
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-05-09 01:03:39 +0530

    Issue #9534: updated AbstractJavadocCheckTest to use unique Input file in each test method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocNumberFormatException.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocNumberFormatException.java
@@ -1,4 +1,8 @@
 package com.puppycrawl.tools.checkstyle.checks.javadoc.abstractjavadoc;
 
-/** <ul><li>a' {@link EntityEntry} (by way of {@link #;}</li></ul> */
+/*
+ * Config: TempCheck
+ */
+
+/** <ul><li>a' {@link EntityEntry} (by way of {@link #;}</li></ul> */ // violation
 class InputAbstractJavadocNumberFormatException{}
