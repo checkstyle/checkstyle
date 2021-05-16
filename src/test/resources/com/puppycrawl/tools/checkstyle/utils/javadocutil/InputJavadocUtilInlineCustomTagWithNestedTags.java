8ddf3b84cad89dcd3fd85c08545c4daaa8d71a76
Vyom-Yadav <jackhammervyom@gmail.com>
2021-05-16 11:15:03 +0530

    Issue #9993: Created util method in JavadocUtil.java to get content of custom tag

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/utils/javadocutil/InputJavadocUtilInlineCustomTagWithNestedTags.java
@@ -0,0 +1,11 @@
+package com.puppycrawl.tools.checkstyle.utils.javadocutil;
+/* Config: Default
+ */
+
+/**
+ * {@customTag This is a custom tag
+ * {@tag A nested inline custom tag {@tag2 another nested tag}}
+ * This method is a test method.}
+ */
+public class InputJavadocUtilInlineCustomTagWithNestedTags { // ok
+}
