9345de38ac4fb3673994eb04ee8d8416185a9ba5
Vyom-Yadav <jackhammervyom@gmail.com>
2021-05-16 11:15:03 +0530

    Issue #9993: Created util method in JavadocUtil.java to get content of custom tag

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/utils/javadocutil/InputJavadocUtilInlineCustomTagWithHtml.java
@@ -0,0 +1,13 @@
+package com.puppycrawl.tools.checkstyle.utils.javadocutil;
+/* Config: Default
+ */
+
+/**
+ * {@summary <!-- This is a HTML comment -->
+ *  <ul>
+ *   <li>Coffee</li>
+ * </ul>
+ * **** some stars}
+ */
+public class InputJavadocUtilInlineCustomTagWithHtml { // ok
+}
