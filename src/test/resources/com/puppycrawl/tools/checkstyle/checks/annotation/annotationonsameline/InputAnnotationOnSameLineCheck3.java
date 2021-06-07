2b4f2b8f84b1356eba07dbf39a4735a73ff76a16
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-06-07 16:00:40 +0530

    Issue #10083: Update inputs for AnnotationOnSameLineCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationonsameline/InputAnnotationOnSameLineCheck3.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationonsameline/InputAnnotationOnSameLineCheck3.java
@@ -1,10 +1,12 @@
+/*
+AnnotationOnSameLine
+tokens = CLASS_DEF, INTERFACE_DEF, ENUM_DEF, METHOD_DEF, CTOR_DEF, VARIABLE_DEF, PARAMETER_DEF, ANNOTATION_DEF, TYPECAST, LITERAL_THROWS, IMPLEMENTS_CLAUSE, TYPE_ARGUMENT, LITERAL_NEW, DOT, ANNOTATION_FIELD_DEF
+
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.annotation.annotationonsameline;
 
-/* Config:
- * tokens = "CLASS_DEF, INTERFACE_DEF, ENUM_DEF, METHOD_DEF, CTOR_DEF, VARIABLE_DEF, PARAMETER_DEF,
- *           ANNOTATION_DEF, TYPECAST, LITERAL_THROWS, IMPLEMENTS_CLAUSE, TYPE_ARGUMENT,
- *           LITERAL_NEW, DOT, ANNOTATION_FIELD_DEF"
- */
 public class InputAnnotationOnSameLineCheck3 {
 
     @Annotation3 int x;     // ok
