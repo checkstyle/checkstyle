fced123056aefdccb62c1e0c1a31d81a37ad1feb
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-06-27 18:44:21 +0530

    Issue #10176: Updated inputs for AtclauseOrderCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/atclauseorder/InputAtclauseOrderWithAnnotationsOutsideJavadoc.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/atclauseorder/InputAtclauseOrderWithAnnotationsOutsideJavadoc.java
@@ -1,7 +1,15 @@
-package com.puppycrawl.tools.checkstyle.checks.javadoc.atclauseorder;
+/*
+AtclauseOrder
+violateExecutionOnNonTightHtml = (default)false
+target = (default)CLASS_DEF, INTERFACE_DEF, ENUM_DEF, METHOD_DEF, \
+         CTOR_DEF, VARIABLE_DEF, RECORD_DEF, COMPACT_CTOR_DEF
+tagOrder = (default)@author, @deprecated, @exception, @param, @return, \
+           @see, @serial, @serialData, @serialField, @since, @throws, @version
 
-/* Config: default
- */
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.atclauseorder;
 
 public class InputAtclauseOrderWithAnnotationsOutsideJavadoc {
     /**
