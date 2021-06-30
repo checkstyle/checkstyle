2ab4374715fca90503a32e504107d7a1633d220e
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-06-30 20:37:19 +0530

    Issue #10213: Update inputs for EqualsHashCodeCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/equalshashcode/InputEqualsHashCode.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/equalshashcode/InputEqualsHashCode.java
@@ -1,15 +1,21 @@
+/*
+EqualsHashCode
+
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.coding.equalshashcode;
 
 public class InputEqualsHashCode {
-    public boolean notEquals() {
+    public boolean notEquals() { // ok
         return true;
     }
 
-    public boolean equals() {
+    public boolean equals() { // ok
         return false;
     }
 
-    public boolean equals(Object o1) {
+    public boolean equals(Object o1) { // ok
         return false;
     }
 
@@ -17,23 +23,23 @@ public class InputEqualsHashCode {
         return false;
     }
 
-    private boolean equals(String s) {
+    private boolean equals(String s) { // ok
         return false;
     }
 
-    protected int notHashCode() {
+    protected int notHashCode() { // ok
         return 1;
     }
 
-    public int hashCode() {
+    public int hashCode() { // ok
         return 1;
     }
 
-    public int hashCode(Object o1) {
+    public int hashCode(Object o1) { // ok
         return 1;
     }
 
-    private int hashCode(Object o1, Object o2) {
+    private int hashCode(Object o1, Object o2) { // ok
         return 1;
     }
 }
