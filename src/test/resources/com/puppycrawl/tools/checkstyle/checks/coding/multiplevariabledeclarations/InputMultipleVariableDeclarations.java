93864bff8336681a6b3263e5ec980b7f01b12746
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-07-06 11:17:24 +0530

    Issue #10237: Update inputs for MultipleVariableDeclarationsCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplevariabledeclarations/InputMultipleVariableDeclarations.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplevariabledeclarations/InputMultipleVariableDeclarations.java
@@ -1,19 +1,26 @@
+/*
+MultipleVariableDeclarations
+
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.coding.multiplevariabledeclarations;
+
 public class InputMultipleVariableDeclarations
 {
-    int i, j;
-    int i1; int j1;
+    int i, j; // violation
+    int i1; int j1; // violation
 
     void method1() {
-        String str, str1;
-        java.lang.Object obj; Object obj1;
+        String str, str1; // violation
+        java.lang.Object obj; Object obj1; // violation
     }
     // second definition is wrapped
     // line of VARIABLE_DEF is not the same as first line of the definition
-    java.lang.String string; java.lang.String
+    java.lang.String string; java.lang.String // violation
         strings[];
     //both definitions is wrapped
-    java.lang.
+    java.lang. // violation
         String string1; java.lang.String
             strings1[];
 
@@ -32,7 +39,7 @@ public class InputMultipleVariableDeclarations
     }
 
     void method3() {
-        java.lang.Object obj; Object obj1; Object obj2; Object obj3;
+        java.lang.Object obj; Object obj1; Object obj2; Object obj3; // violation
         for (String s : new String[] {}) {}
     }
 }
