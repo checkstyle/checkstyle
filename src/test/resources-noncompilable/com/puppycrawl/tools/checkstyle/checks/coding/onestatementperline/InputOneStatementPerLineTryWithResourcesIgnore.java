264182ac418e7e218559671f55100a0c086fd192
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-07-07 12:08:07 +0530

    Issue #10258: Update inputs for OneStatementPerLineCheckTest

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineTryWithResourcesIgnore.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineTryWithResourcesIgnore.java
@@ -1,3 +1,10 @@
+/*
+OneStatementPerLine
+treatTryResourcesAsStatement = (default)false
+
+
+*/
+
 //non-compiled with javac: Compilable with Java9
 package com.puppycrawl.tools.checkstyle.checks.coding.onestatementperline;
 
@@ -5,21 +12,18 @@ import java.io.IOException;
 import java.io.PipedOutputStream;
 import java.io.OutputStream;
 
-/**
- * Config treatTryResourcesAsStatement = false
- */
 public class InputOneStatementPerLineTryWithResourcesIgnore {
 
     void method() throws IOException {
         OutputStream s1 = new PipedOutputStream();
         OutputStream s2 = new PipedOutputStream();
-        try (s1; s2; OutputStream s3 = new PipedOutputStream();) {
+        try (s1; s2; OutputStream s3 = new PipedOutputStream();) { // ok
         }
-        try (s1; OutputStream s4 = new PipedOutputStream(); s2;) {
+        try (s1; OutputStream s4 = new PipedOutputStream(); s2;) { // ok
         }
-        try (s1; s2; OutputStream s5 = new PipedOutputStream()) {
+        try (s1; s2; OutputStream s5 = new PipedOutputStream()) { // ok
         }
-        try (s1; OutputStream s6 = new PipedOutputStream(); s2) {
+        try (s1; OutputStream s6 = new PipedOutputStream(); s2) { // ok
         }
         try (
 OutputStream s7 = new PipedOutputStream();OutputStream s8 = new PipedOutputStream();
@@ -36,7 +40,7 @@ OutputStream s9=new PipedOutputStream();s2;OutputStream s10 = new PipedOutputStr
              ;s2;) {
         }
         try (OutputStream
-             s12 = new PipedOutputStream();s1;OutputStream s3
+             s12 = new PipedOutputStream();s1;OutputStream s3 // ok
                 = new PipedOutputStream()) {}
         try (s1; s2; OutputStream stream3 =
              new PipedOutputStream()) {}
