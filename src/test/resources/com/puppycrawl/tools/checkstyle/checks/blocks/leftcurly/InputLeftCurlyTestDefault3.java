892d5a4ac9e7146abd889fa69c8e20818ebe6e01
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-07-13 12:06:54 +0530

    Issue #10158: Update inputs for LeftCurlyCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/leftcurly/InputLeftCurlyTestDefault3.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/leftcurly/InputLeftCurlyTestDefault3.java
@@ -1,8 +1,18 @@
+/*
+LeftCurly
+option = (default)eol
+ignoreEnums = (default)true
+tokens = (default)ANNOTATION_DEF, CLASS_DEF, CTOR_DEF, ENUM_CONSTANT_DEF, \
+         ENUM_DEF, INTERFACE_DEF, LAMBDA, LITERAL_CASE, LITERAL_CATCH, \
+         LITERAL_DEFAULT, LITERAL_DO, LITERAL_ELSE, LITERAL_FINALLY, LITERAL_FOR, \
+         LITERAL_IF, LITERAL_SWITCH, LITERAL_SYNCHRONIZED, LITERAL_TRY, LITERAL_WHILE, \
+         METHOD_DEF, OBJBLOCK, STATIC_INIT, RECORD_DEF, COMPACT_CTOR_DEF
+
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.blocks.leftcurly;
 
-/*
- * Config: default
- */
 class InputLeftCurlyTestDefault3
 { // violation
     /** @see test method **/
