e17a7a2487d6c8e66b844b035e39f061f6835b68
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-07-20 12:32:51 +0530

    Issue #10386: Update inputs for InnerTypeLastCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/innertypelast/InputInnerTypeLastClassCtorsInitBlocks.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/innertypelast/InputInnerTypeLastClassCtorsInitBlocks.java
@@ -1,6 +1,11 @@
+/*
+InnerTypeLast
+
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.design.innertypelast;
 
-/* Config: default */
 public class InputInnerTypeLastClassCtorsInitBlocks {
     public class Inner { // violation
     }
