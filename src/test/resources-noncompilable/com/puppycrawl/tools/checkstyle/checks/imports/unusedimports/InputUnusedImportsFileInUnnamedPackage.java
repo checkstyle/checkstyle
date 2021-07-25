10b790ef20392e69298195120b6dd45949a84c8f
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-07-26 00:20:15 +0530

    Issue #10412: Update inputs for UnusedImportsCheckTest

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsFileInUnnamedPackage.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsFileInUnnamedPackage.java
@@ -1,9 +1,16 @@
+/*
+UnusedImports
+processJavadoc = (default)true
+
+
+*/
+
 //non-compiled with javac: no package by design of test
 
 import java.util.List;
 import java.util.List;
-import java.util.Arrays;
-import java.lang.String;
+import java.util.Arrays; // violation
+import java.lang.String; // violation
 import static java.lang.Math.PI;
 
 public class InputUnusedImportsFileInUnnamedPackage
