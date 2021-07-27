9d029b4ec2d5fe2310cb5fdc24a6792b385712ca
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-07-28 02:37:07 +0530

    Issue #10429: Update inputs for JavaNCSSCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/metrics/javancss/InputJavaNCSS3.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/metrics/javancss/InputJavaNCSS3.java
@@ -1,34 +1,34 @@
 /*
 JavaNCSS
-methodMaximum = 0
-classMaximum = 1
-fileMaximum = 2
+methodMaximum = (default)50
+classMaximum = (default)1500
+fileMaximum = (default)2000
 recordMaximum = (default)150
 
 
 */
 
 // should give an ncss of 35
-package com.puppycrawl.tools.checkstyle.checks.metrics.javancss; // violation
+package com.puppycrawl.tools.checkstyle.checks.metrics.javancss;
 
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 
 //should give an ncss of 22
-public class InputJavaNCSS { // violation
+public class InputJavaNCSS3 { // ok
 
     private Object mObject;
 
     //should count as 2
-    private void testMethod1() { // violation
+    private void testMethod1() {
 
         //should count as 1
         int x = 1, y = 2;
     }
 
     //should count as 4
-    private void testMethod2() { // violation
+    private void testMethod2() {
 
         int abc = 0;
 
@@ -37,7 +37,7 @@ public class InputJavaNCSS { // violation
     }
 
     //should give an ncss of 12
-    private void testMethod3() { // violation
+    private void testMethod3() {
 
         int a = 0;
         switch (a) {
@@ -49,26 +49,26 @@ public class InputJavaNCSS { // violation
         ItemListener lis = new ItemListener() {
 
             //should give an ncss of 2
-            public void itemStateChanged(ItemEvent e) { // violation
+            public void itemStateChanged(ItemEvent e) {
                 System.identityHashCode("Hello");
             }
         };
     }
 
     //should give an ncss of 2
-    private class TestInnerClass { // violation
+    private class TestInnerClass {
 
         private Object test;
     }
 }
 
 //should give an ncss of 10
-class TestTopLevelNestedClass { // violation
+class TestTopLevelNestedClass3 {
 
     private Object mObject;
 
     //should give an ncss of 8
-    private void testMethod() { // violation
+    private void testMethod() {
 
         for (int i=0; i<10; i++) {
 
@@ -87,8 +87,8 @@ class TestTopLevelNestedClass { // violation
     }
 }
 
-class Input0 { // violation
-    static { } // violation
-    { } // violation
-    public Input0() { } // violation
+class Input03 {
+    static { }
+    { }
+    public Input03() { }
 }
