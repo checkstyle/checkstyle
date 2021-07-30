9168fb3b1b57670127f5a99ea7742f7ca9701120
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-07-30 12:05:15 +0530

    Issue #10452: Update inputs for TypeNameCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/typename/InputTypeName6.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/typename/InputTypeName6.java
@@ -1,25 +1,25 @@
 /*
 TypeName
-format = ^inputHe
+format = (default)^[A-Z][a-zA-Z0-9]*$
 applyToPublic = (default)true
 applyToProtected = (default)true
 applyToPackage = (default)true
 applyToPrivate = (default)true
-tokens = (default)CLASS_DEF, INTERFACE_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
+tokens = ANNOTATION_DEF
 
 
 */
 
 package com.puppycrawl.tools.checkstyle.checks.naming.typename;
 
-class inputHeaderClass {
+class inputHeaderClass6 {
 
     public interface inputHeaderInterface {};
 //comment
     public enum inputHeaderEnum { one, two };
 
-    public @interface inputHeaderAnnotation {};
+    public @interface inputHeaderAnnotation {}; // violation
 
 }
 
-public class InputTypeName {}
+public class InputTypeName6 {}
