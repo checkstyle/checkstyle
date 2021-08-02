35133d3c581b0a8164ccdf6ab0893f6c9ef42e20
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-08-02 21:46:23 +0530

    Issue #10501: Update inputs for IllegalInstantiationCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalinstantiation/InputIllegalInstantiationSameClassNameJavaLang.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalinstantiation/InputIllegalInstantiationSameClassNameJavaLang.java
@@ -1,6 +1,14 @@
+/*
+IllegalInstantiation
+classes = java.lang.InputTest
+tokens = (default)CLASS_DEF
+
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.coding.illegalinstantiation;
 
-public class InputIllegalInstantiationSameClassNameJavaLang
+public class InputIllegalInstantiationSameClassNameJavaLang // ok
 {
     InputTest obj = new InputTest();
 }
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalinstantiation/InputIllegalInstantiationSemantic2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalinstantiation/InputIllegalInstantiationSemantic2.java
@@ -1,6 +1,7 @@
 /*
 IllegalInstantiation
-classes = (default)
+classes = java.lang.Boolean, com.puppycrawl.tools.checkstyle.checks.coding.illegalinstantiation.\
+          InputModifier, java.io.File, java.awt.Color
 tokens = (default)CLASS_DEF
 
 
@@ -8,19 +9,20 @@ tokens = (default)CLASS_DEF
 
 package com.puppycrawl.tools.checkstyle.checks.coding.illegalinstantiation;
 
-import java.io.*; // star import for instantiation tests
-import java.awt.Dimension; // explicit import for instantiation tests
 import java.awt.Color;
+import java.awt.Dimension;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 /**
  * Test case for detecting simple semantic violations.
  * @author Lars Kühne
  **/
-class InputIllegalInstantiationSemantic // ok
+class InputIllegalInstantiationSemantic2
 {
     /* Boolean instantiation in a static initializer */
     static {
-        Boolean x = new Boolean(true);
+        Boolean x = new Boolean(true); // violation
     }
 
     /* Boolean instantiation in a non-static initializer */
@@ -32,7 +34,7 @@ class InputIllegalInstantiationSemantic // ok
     /** fully qualified Boolean instantiation in a method. **/
     Boolean getBoolean()
     {
-        return new java.lang.Boolean(true);
+        return new Boolean(true);
     }
 
     void otherInstantiations()
