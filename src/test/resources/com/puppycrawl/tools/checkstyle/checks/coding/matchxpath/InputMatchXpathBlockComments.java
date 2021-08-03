f1c62c67af1c86cfd53f504d58e98292c729edbd
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-08-03 21:38:08 +0530

    Issue #10507: Update inputs for MatchXpathCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/matchxpath/InputMatchXpathBlockComments.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/matchxpath/InputMatchXpathBlockComments.java
@@ -3,6 +3,7 @@ MatchXpath
 query = //BLOCK_COMMENT_BEGIN[./COMMENT_CONTENT[contains(@text, '{') \
         and not(starts-with(@text, '\nMatchXpath'))]]
 
+
 */
 
 package com.puppycrawl.tools.checkstyle.checks.coding.matchxpath;
