9d0e544235989c8ea9e2682b003957dc5e170459
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-08-04 16:10:55 +0530

    Issue #10540: Update inputs for IllegalIdentifierNameCheckTest

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/naming/illegalidentifiername/InputIllegalIdentifierNameLambda.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/naming/illegalidentifiername/InputIllegalIdentifierNameLambda.java
@@ -1,10 +1,18 @@
+/*
+IllegalIdentifierName
+format = (default)(?i)^(?!(record|yield|var|permits|sealed|_)$).+$
+tokens = (default)CLASS_DEF, INTERFACE_DEF, ENUM_DEF, ANNOTATION_DEF, ANNOTATION_FIELD_DEF, \
+         PARAMETER_DEF, VARIABLE_DEF, METHOD_DEF, ENUM_CONSTANT_DEF, PATTERN_VARIABLE_DEF, \
+         RECORD_DEF, RECORD_COMPONENT_DEF, LAMBDA
+
+
+*/
+
 //non-compiled with javac: Compilable with Java14
 package com.puppycrawl.tools.checkstyle.checks.naming.illegalidentifiername;
 
 import java.util.function.Function;
-/* Config:
- * format = "(?i)^(?!(record|yield|var|permits|sealed|_)$).+$"
- */
+
 public class InputIllegalIdentifierNameLambda {
 
     public static void main(String... args) {
