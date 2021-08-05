dfbc8a6a0e06d87b0e709fa46857d92b2040e1b8
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-08-05 20:26:16 +0530

    Issue #10548: Migrate to Inline Config Parser in AvoidDoubleBraceInitializationCheckTest

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/AvoidDoubleBraceInitializationCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/AvoidDoubleBraceInitializationCheckTest.java
@@ -54,8 +54,10 @@ public class AvoidDoubleBraceInitializationCheckTest extends AbstractModuleTestS
             "76:33: " + getCheckMessage(MSG_KEY),
             "77:33: " + getCheckMessage(MSG_KEY),
         };
-        verify(checkConfig, getPath("InputAvoidDoubleBraceInitialization.java"),
-            expected);
+        System.out.println(getCheckMessage(MSG_KEY));
+        verifyWithInlineConfigParser(checkConfig,
+                getPath("InputAvoidDoubleBraceInitialization.java"),
+                expected);
     }
 
     @Test
