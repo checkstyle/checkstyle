b5efa1bff2d12914297c89e6ab437ec1dc6f1028
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-08-10 20:01:38 +0530

    Issue #10596: Update inputs for ImportControlCheckTest

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/InputImportControl12.java
@@ -0,0 +1,19 @@
+/*
+ImportControl
+file = InputImportControlStrategyOnMismatchOne.xml
+path = (default).*
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.importcontrol;
+
+import java.awt.Image; // violation
+import javax.swing.border.*; // violation
+import java.io.File;
+import static java.awt.Button.ABORT; // violation
+
+public class InputImportControl12
+{
+
+}
