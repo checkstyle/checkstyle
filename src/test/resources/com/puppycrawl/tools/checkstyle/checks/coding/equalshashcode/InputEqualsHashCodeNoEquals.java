77a9f16cb8708caa3306a515213a82dce6d4addc
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-08-11 20:49:28 +0530

    Issue #10548: Migrate to Inline Config Parser in EqualsHashCodeCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/equalshashcode/InputEqualsHashCodeNoEquals.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/equalshashcode/InputEqualsHashCodeNoEquals.java
@@ -7,7 +7,7 @@ EqualsHashCode
 package com.puppycrawl.tools.checkstyle.checks.coding.equalshashcode;
 
 public class InputEqualsHashCodeNoEquals {
-    public int hashCode() { // violation
+    public int hashCode() { // violation 'Definition of 'hashCode()' without corresponding definition of 'equals()'.'
         return 1;
     }
 }
