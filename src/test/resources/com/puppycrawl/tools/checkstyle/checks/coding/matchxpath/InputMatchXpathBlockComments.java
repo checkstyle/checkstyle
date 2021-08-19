0b76ab1c78ee5dd5908b801853705e27c56b2bc6
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-08-19 13:04:17 +0530

    Issue #10548: Migrate to Inline Config Parser in MatchXpathCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/matchxpath/InputMatchXpathBlockComments.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/matchxpath/InputMatchXpathBlockComments.java
@@ -1,7 +1,7 @@
 /*
 MatchXpath
 query = //BLOCK_COMMENT_BEGIN[./COMMENT_CONTENT[contains(@text, '{') \
-        and not(starts-with(@text, '\nMatchXpath'))]]
+        and not(starts-with(@text, '\\nMatchXpath'))]]
 
 
 */
