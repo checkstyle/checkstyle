4ae56b5c0e538f2ecfb1b70817e5c6362bc4b68f
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-08-21 00:13:59 +0530

    Issue #10667: Migrate to Inline Config Parser in Design Check Tests

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/innertypelast/InputInnerTypeLastArray.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/innertypelast/InputInnerTypeLastArray.java
@@ -10,6 +10,6 @@ public class InputInnerTypeLastArray {
     static class P {}
     int[] x; // violation
     int y[]; // violation
-    P p0 = new P(), p1[] = {p0}, p2[][] = {p1}; // violation on first declaration
-    P []p3 = new P[0], p4 = {p0}, p5 = {p0};  // violation on first declaration
+    P p0 = new P(), p1[] = {p0}, p2[][] = {p1}; // violation
+    P []p3 = new P[0], p4 = {p0}, p5 = {p0};  // violation
 }
