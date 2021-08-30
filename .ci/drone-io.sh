2f79fea8b88d32f48fc5464eda5cb6295610b47d
Vadym <sukolenvo@gmail.com>
2021-08-31 06:53:46 +0800

    Issue #10065: cache for Drone ci maven dependencies

---
+++ .ci/drone-io.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+source ./.ci/util.sh
+
+case $1 in
+
+restore-maven-cache)
+  curl -o cache.tar -Ss https://sourceforge.net/projects/checkstyle/files/drone-io/drone-io-m2-repository.tar/download
+  tar -xf cache.tar -C /
+  rm cache.tar
+  ;;
+
+*)
+  echo "Unexpected argument: $1"
+  sleep 5s
+  false
+  ;;
+
+esac
