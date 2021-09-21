7bf008b97bfd194641648c2840f2ad9a521d5adf
nmancus1 <nicholas.mancuso13@gmail.com>
2021-09-21 16:24:04 -0400

    infra: update xwiki commit

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -201,7 +201,7 @@ no-error-xwiki)
   cd ..
   checkout_from https://github.com/xwiki/xwiki-platform.git
   cd .ci-temp/xwiki-platform
-  git checkout "f2a1b708329901""22f79f2fb372e0882d20b66b12"
+  git checkout "ec4cee44da""f56a76915f7445486f""bb502a""da791f"
   # Validate xwiki-platform
   mvn -e --no-transfer-progress checkstyle:check@default -Dcheckstyle.version=${CS_POM_VERSION}
   cd ..
