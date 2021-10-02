ed0db6f07ba81e5bb25d1eb24d749b6ee298f4eb
Andrei Paikin <andreypaykin@gmail.com>
2021-10-02 12:11:59 +0300

    Issue #10144: fix classes count on multicatch

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/metrics/classfanoutcomplexity/InputClassFanOutComplexityImplementsAndNestedCount.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/metrics/classfanoutcomplexity/InputClassFanOutComplexityImplementsAndNestedCount.java
@@ -1,6 +1,6 @@
 /*
 ClassFanOutComplexity
-max = 4
+max = 0
 excludedClasses = (default)ArrayIndexOutOfBoundsException, ArrayList, Boolean, Byte, \
                   Character, Class, Collection, Deprecated, Deque, Double, DoubleStream, \
                   EnumSet, Exception, Float, FunctionalInterface, HashMap, HashSet, \
@@ -20,32 +20,14 @@ excludedPackages = (default)
 
 package com.puppycrawl.tools.checkstyle.checks.metrics.classfanoutcomplexity;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.Serializable;
+import java.util.RandomAccess;
 
-public class InputClassFanOutComplexityRemoveMultiCatchBitwiseOr { // violation
-    public static void main(String[] args) {
-        try {
-            System.out.println(args[7]);
-            File myFile = new File("myfile.txt");
-            InputStream stream = myFile.toURL().openStream();
-            foo1();
-        } catch (ArrayIndexOutOfBoundsException | IOException
-                | SQLException | SecurityException | OneMoreException e) {
+public class InputClassFanOutComplexityImplementsAndNestedCount // violation
+    extends Outer.Nested implements Serializable, RandomAccess {
 
-        }
-    }
+}
 
-    private static void foo1() throws RuntimeException, SQLException, OneMoreException {
-
-    }
-
-    private class SQLException extends Exception {
-
-    }
-
-    private class OneMoreException extends Exception {
-
-    }
+class Outer {
+    static class Nested {}
 }
