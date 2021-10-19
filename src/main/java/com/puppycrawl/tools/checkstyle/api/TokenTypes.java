5a0f05841e874b9671d4de642456d2b13bb59971
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2021-10-19 19:45:09 +0530

    Issue #9462: updated example of AST for TokenTypes.LITERAL_ASSERT

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -4308,14 +4308,14 @@ public final class TokenTypes {
      * </pre>
      * <p>parses as:</p>
      * <pre>
-     * --LITERAL_ASSERT -&gt; assert
-     *    |--EXPR -&gt; EXPR
-     *    |   |--LPAREN -&gt; (
-     *    |   |--EQUAL -&gt; ==
-     *    |   |   |--IDENT -&gt; x
-     *    |   |   `--NUM_INT -&gt; 4
-     *    |   `--RPAREN -&gt; )
-     *    `--SEMI -&gt; ;
+     * LITERAL_ASSERT -&gt; assert
+     *  |--EXPR -&gt; EXPR
+     *  |   |--LPAREN -&gt; (
+     *  |   |--EQUAL -&gt; ==
+     *  |   |   |--IDENT -&gt; x
+     *  |   |   `--NUM_INT -&gt; 4
+     *  |   `--RPAREN -&gt; )
+     *  `--SEMI -&gt; ;
      * </pre>
      **/
     public static final int LITERAL_ASSERT = JavaLanguageLexer.ASSERT;
