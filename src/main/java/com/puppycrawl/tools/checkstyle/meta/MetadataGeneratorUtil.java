b3c9a99306f41d7299e829d38f94a10742cafb1e
Vyom-Yadav <jackhammervyom@gmail.com>
2021-10-25 10:42:33 +0530

    Issue #10747: MetadataGeneratorUtil now reports errors back to the users

--- src/main/java/com/puppycrawl/tools/checkstyle/meta/MetadataGeneratorUtil.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/meta/MetadataGeneratorUtil.java
@@ -32,7 +32,10 @@ import java.util.stream.Stream;
 
 import com.puppycrawl.tools.checkstyle.Checker;
 import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.DefaultLogger;
 import com.puppycrawl.tools.checkstyle.TreeWalker;
+import com.puppycrawl.tools.checkstyle.api.AuditEvent;
+import com.puppycrawl.tools.checkstyle.api.AutomaticBean;
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 
 /** Class which handles all the metadata generation and writing calls. */
@@ -45,12 +48,13 @@ public final class MetadataGeneratorUtil {
     /**
      * Generate metadata from the module source files available in the input argument path.
      *
+     * @param shouldPrintToConsole whether violation messages should be printed to console or not
      * @param path arguments
      * @param moduleFolders folders to check
      * @throws IOException ioException
      * @throws CheckstyleException checkstyleException
      */
-    public static void generate(String path, String... moduleFolders)
+    public static void generate(boolean shouldPrintToConsole, String path, String... moduleFolders)
             throws IOException, CheckstyleException {
         JavadocMetadataScraper.resetModuleDetailsStore();
 
@@ -58,13 +62,29 @@ public final class MetadataGeneratorUtil {
         checker.setModuleClassLoader(Checker.class.getClassLoader());
         final DefaultConfiguration scraperCheckConfig =
                         new DefaultConfiguration(JavadocMetadataScraper.class.getName());
-        final DefaultConfiguration defaultConfiguration = new DefaultConfiguration("configuration");
+        final DefaultConfiguration defaultConfiguration =
+                new DefaultConfiguration("configuration");
         final DefaultConfiguration treeWalkerConfig =
                 new DefaultConfiguration(TreeWalker.class.getName());
         defaultConfiguration.addProperty("charset", StandardCharsets.UTF_8.name());
         defaultConfiguration.addChild(treeWalkerConfig);
         treeWalkerConfig.addChild(scraperCheckConfig);
         checker.configure(defaultConfiguration);
+        if (shouldPrintToConsole) {
+            checker.addListener(new DefaultLogger(System.out,
+                    AutomaticBean.OutputStreamOptions.NONE) {
+                @Override
+                public void auditStarted(AuditEvent event) {
+                    getInfoWriter().flush();
+                }
+
+                @Override
+                public void auditFinished(AuditEvent event) {
+                    getInfoWriter().flush();
+                    getErrorWriter().flush();
+                }
+            });
+        }
         dumpMetadata(checker, path, moduleFolders);
     }
 
@@ -94,7 +114,6 @@ public final class MetadataGeneratorUtil {
                         .collect(Collectors.toList()));
             }
         }
-
         checker.process(validFiles);
     }
 }
