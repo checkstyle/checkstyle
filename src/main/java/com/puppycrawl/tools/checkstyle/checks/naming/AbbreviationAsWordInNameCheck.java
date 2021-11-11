ab3432aa9b6a1abc4deda7f2c19cff4ee473f202
MUzairS15 <muzair.shaikh810@gmail.com>
2021-11-11 12:24:55 +0530

    Issue #10889: Updated documentation about how 'AbbreviationAsWordInName' checks for abbreviation

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
@@ -198,7 +198,7 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  *   &lt;property name="tokens" value="VARIABLE_DEF,CLASS_DEF"/&gt;
  *   &lt;property name="ignoreStatic" value="false"/&gt;
  *   &lt;property name="allowedAbbreviationLength" value="0"/&gt;
- *   &lt;property name="allowedAbbreviations" value="XML,URL"/&gt;
+ *   &lt;property name="allowedAbbreviations" value="XML,URL,O"/&gt;
  * &lt;/module&gt;
  * </pre>
  * <p>Example:</p>
@@ -213,6 +213,15 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  *   String firstURL; // OK, URL abbreviation is allowed
  *   final int TOTAL = 5; // OK, final is ignored
  *   static final int LIMIT = 10; // OK, static final is ignored
+ *   void newOAuth2Client() {} // OK, O abbreviation is allowed
+ *   void OAuth2() {} // OK, O abbreviation is allowed
+ *   void OAUth2() {} // violation, OA abbreviation is not allowed
+ *                    // split occurs as 'OA', 'Uth2'
+ *                    // and not as 'O', 'AUth2'
+ *                    // beginIndex = 0, endIndex = 2,
+ *                    // The char at endIndex is included in the abbreviation if
+ *                    //  it is the last character in the string.
+ *
  * }
  * </pre>
  * <p>
