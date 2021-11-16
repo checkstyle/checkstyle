65ec45d8cd60fa2252faa467cc24a85092557303
Abhinav Rajput <abhinavrajputaes@gmail.com>
2021-11-16 22:43:46 +0530

    Issue #9485: Update example of AST for TokenTypes.LITERAL_NATIVE

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -1877,42 +1877,21 @@ public final class TokenTypes {
      *
      * <p>For example:</p>
      * <pre>
-     * native void equal(int a, int b)
-     * {
-     *   a=b;
-     * }
+     * native void foo(){}
      * </pre>
      * <p>parses as:</p>
      * <pre>
-     * |--LCURLY -&gt; {
-     * |--METHOD_DEF -&gt; METHOD_DEF
-     * |   |--MODIFIERS -&gt; MODIFIERS
-     * |   |   `--LITERAL_NATIVE -&gt; native
-     * |   |--TYPE -&gt; TYPE
-     * |   |   `--LITERAL_VOID -&gt; void
-     * |   |--IDENT -&gt; equal
-     * |   |--LPAREN -&gt; (
-     * |   |--PARAMETERS -&gt; PARAMETERS
-     * |   |   |--PARAMETER_DEF -&gt; PARAMETER_DEF
-     * |   |   |   |--MODIFIERS -&gt; MODIFIERS
-     * |   |   |   |--TYPE -&gt; TYPE
-     * |   |   |   |   `--LITERAL_INT -&gt; int
-     * |   |   |   `--IDENT -&gt; a
-     * |   |   |--COMMA -&gt; ,
-     * |   |   `--PARAMETER_DEF -&gt; PARAMETER_DEF
-     * |   |       |--MODIFIERS -&gt; MODIFIERS
-     * |   |       |--TYPE -&gt; TYPE
-     * |   |       |   `--LITERAL_INT -&gt; int
-     * |   |       `--IDENT -&gt; b
-     * |   |--RPAREN -&gt; )
-     * |   `--SLIST -&gt; {
-     * |       |--EXPR -&gt; EXPR
-     * |       |   `--ASSIGN -&gt; =
-     * |       |       |--IDENT -&gt; a
-     * |       |       `--IDENT -&gt; b
-     * |       |--SEMI -&gt; ;
-     * |       `--RCURLY -&gt; }
-     * `--RCURLY -&gt; }
+     * METHOD_DEF -&gt; METHOD_DEF
+     *  |--MODIFIERS -&gt; MODIFIERS
+     *  |   `--LITERAL_NATIVE -&gt; native
+     *  |--TYPE -&gt; TYPE
+     *  |   `--LITERAL_VOID -&gt; void
+     *  |--IDENT -&gt; foo
+     *  |--LPAREN -&gt; (
+     *  |--PARAMETERS -&gt; PARAMETERS
+     *  |--RPAREN -&gt; )
+     *  `--SLIST -&gt; {
+     *      `--RCURLY -&gt; }
      * </pre>
      *
      * @see #MODIFIERS
