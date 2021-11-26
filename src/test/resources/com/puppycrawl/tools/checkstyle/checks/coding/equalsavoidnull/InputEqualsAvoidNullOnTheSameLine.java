ce398ba3ed477fc0c7249a35ac1c791749cca3b4
Vyom-Yadav <jackhammervyom@gmail.com>
2021-11-26 13:37:09 +0530

    Issue #10943: Message validation in our Inputs should not have trailing and leading '.*'

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/equalsavoidnull/InputEqualsAvoidNullOnTheSameLine.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/equalsavoidnull/InputEqualsAvoidNullOnTheSameLine.java
@@ -11,14 +11,14 @@ public class InputEqualsAvoidNullOnTheSameLine {
 
     static {
         String b = "onion";
-        String a=b;a.equals("ONION"); // violation '.* left .* of .* equals .*.'
+        String a=b;a.equals("ONION"); // violation 'left .* of .* equals'
     }
 
     private String a = "";
     private A b = null;
 
     public void shouldWarn() {
-        a.equals("");A a=b; // violation '.* left .* of .* equals .*.'
+        a.equals("");A a=b; // violation 'left .* of .* equals'
     }
 
     public void shouldNotWarn() {
