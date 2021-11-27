c8349c6a3a41eb446b9eb6d985fb688ceaa6cb72
Abhinav Rajput <abhinavrajputaes@gmail.com>
2021-11-28 00:21:19 +0530

    Issue #9400: updated example of AST for TokenTypes.BLOCK_COMMENT_END

--- src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/TokenTypes.java
@@ -5420,12 +5420,17 @@ public final class TokenTypes {
     /**
      * End of block comment: '&#42;/'.
      *
+     * <p>For example:</p>
      * <pre>
-     * +--BLOCK_COMMENT_BEGIN
-     *         |
-     *         +--COMMENT_CONTENT
-     *         +--BLOCK_COMMENT_END
+     * /&#42;comment&#42;/
      * </pre>
+     * <p>parses as:</p>
+     * <pre>
+     * BLOCK_COMMENT_BEGIN -&gt; /&#42;
+     *  |--COMMENT_CONTENT -&gt; comment
+     *  `--BLOCK_COMMENT_END -&gt; &#42;/
+     * </pre>
+     *
      */
     public static final int BLOCK_COMMENT_END =
             JavaLanguageLexer.BLOCK_COMMENT_END;
