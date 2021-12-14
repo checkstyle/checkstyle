e85bb191449f365f600148a0f08b89f156654861
Roman Ivanov <ivanov-jr@mail.ru>
2021-12-13 17:47:14 -0800

    Issue #11029: move wercker.sh commands to validation.sh

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/redundantthis/InputRedundantThisExtendedMethod.java
@@ -0,0 +1,19 @@
+/*
+RedundantThis
+checkFields = (default)true
+checkMethods = (default)true
+validateOnlyOverlapping = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.redundantthis;
+
+import java.util.logging.Logger;
+
+public class InputRedundantThisExtendedMethod // ok
+{
+    public class Check {
+        private Logger log1 = Logger.getLogger(getClass().getName());
+    }
+}
