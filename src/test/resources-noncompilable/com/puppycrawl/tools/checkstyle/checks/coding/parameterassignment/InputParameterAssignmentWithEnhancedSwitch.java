8b9b27b26ee839e837b7bd5082eb6e316e01a503
Vyom-Yadav <jackhammervyom@gmail.com>
2021-12-15 22:54:44 +0530

    Issue #11038: ParameterAssignment now detects problems for Lambda parameters

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/parameterassignment/InputParameterAssignmentWithEnhancedSwitch.java
@@ -0,0 +1,19 @@
+/*
+ParameterAssignment
+
+
+*/
+
+//non-compiled with javac: Compilable with Java14
+package com.puppycrawl.tools.checkstyle.checks.coding.parameterassignment;
+
+public class InputParameterAssignmentWithEnhancedSwitch {
+
+    int method(int a) {
+        switch (a) {
+            case 1, 2 -> a = 12; // violation
+            default -> throw new IllegalArgumentException("Invalid");
+        }
+        return a;
+    }
+}
