2144a54b40bc2ba4f9babcbb8a11f52b6d340caa
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2021-12-18 23:18:36 +0530

    Issue #7895: add RedundantThisCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/redundantthis/InputRedundantThisFor.java
@@ -0,0 +1,31 @@
+/*
+RedundantThis
+checkFields = (default)true
+checkMethods = (default)true
+validateOnlyOverlapping = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.redundantthis;
+
+public class InputRedundantThisFor {
+    private String name;
+    int bottom;
+    int i;
+
+    public void method1() {
+        for (int i = 0; i < 10; i++) {
+            this.i = i; // no violation
+            this.bottom = i > 0 ? i - 1 : i; // violation
+        }
+    }
+
+    public void method2() {
+        for (String name : new String[]{}) {
+            this.name = name; // no violation
+        }
+
+        String jarPath = new String(this.name + ".jar"); // violation
+    }
+}
