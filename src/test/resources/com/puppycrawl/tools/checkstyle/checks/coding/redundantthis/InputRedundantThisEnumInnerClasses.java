4c680c50bb54f68cc3e0999ca3eea8038826d609
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2021-12-18 23:18:36 +0530

    Issue #7895: add RedundantThisCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/redundantthis/InputRedundantThisEnumInnerClasses.java
@@ -0,0 +1,58 @@
+/*
+RedundantThis
+checkFields = (default)true
+checkMethods = (default)true
+validateOnlyOverlapping = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.redundantthis;
+
+public class InputRedundantThisEnumInnerClasses {
+    int i;
+    int j;
+
+    void method1() {
+        i = 3;
+    }
+
+    void method2(int j) {
+        i++;
+        this.j = j; // ok
+        method1();
+        try {
+            this.method1(); // violation
+        } catch (RuntimeException e) {
+            e.toString();
+        }
+        this.i--; // violation
+
+        Integer.toString(10);
+    }
+
+    void method3() {
+        i = 3;
+    }
+
+    void method4() {
+        this.method3(); // violation
+        method3();
+    }
+}
+
+//  enum
+enum MyEnum {
+    A,
+    B {
+        void doSomething() {
+            this.z = 1; // violation
+        }
+    };
+
+    int z;
+
+    private MyEnum() {
+        z = 0;
+    }
+}
