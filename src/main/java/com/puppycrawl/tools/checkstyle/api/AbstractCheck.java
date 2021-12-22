749b263bd95a2fbe544c3b0e2dc490ba25d9f408
MUzairS15 <muzair.shaikh810@gmail.com>
2021-12-22 11:54:46 +0530

    Issue #10924:  Update UnicodeCharactersCheck to use code points

--- src/main/java/com/puppycrawl/tools/checkstyle/api/AbstractCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/AbstractCheck.java
@@ -295,6 +295,16 @@ public abstract class AbstractCheck extends AbstractViolationReporter {
     }
 
     /**
+     * Returns code point representation of file text from given line number.
+     *
+     * @param index index of the line
+     * @return the array of Unicode code points
+     */
+    public final int[] getLineCodePoints(int index) {
+        return getLine(index).codePoints().toArray();
+    }
+
+    /**
      * The actual context holder.
      */
     private static class FileContext {
