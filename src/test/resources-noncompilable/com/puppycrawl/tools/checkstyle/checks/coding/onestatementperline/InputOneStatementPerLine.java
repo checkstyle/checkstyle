1d969fbf270ec2f33d59e06f14387a314428eb7d
Abhinav Rajput <abhinavrajputaes@gmail.com>
2021-12-28 05:09:55 +0530

    Issue #10625: Fixed three input files and removed them from line length suppression

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLine.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLine.java
@@ -30,40 +30,40 @@ import java.util.stream.Collectors;
 import javax.swing.JCheckBox;
 
 public class InputOneStatementPerLine {
-    /**
-     * According to java language specifications,
-     * statements end with ';'. That is why ';;'
-     * may be considered as two empty statements on the same line
-     * and rises violation.
-     */
-    ;; // violation
-    static {
-        new JCheckBox().addActionListener((final ActionEvent e) -> {good();});
-        List<Integer> ints = new LinkedList<Integer>();
-        ints.stream().map( t -> { return t * 2;} ).filter( t -> { return false;});
-        ints.stream().map( t -> { int m = t * 2; return m; } ); // violation
-        ints.stream().map( t -> { int m = t * 2; return m; } ); int i = 3; // 2 violations
-        ints.stream().map( t -> t * 2); int k = 4; // violation
-        ints.stream().map( t -> t * 2);
-        List<Integer> ints2 = new LinkedList<Integer>();
-        ints.stream().map( t -> { return ints2.stream().map(w -> { return w * 2; });});
-        ints.stream().map( t -> { return ints2.stream().map(w -> { int m=w; return m; });}); // violation
-        ints.stream().map( t -> {
-            return ints2.stream().map(
-                    w -> {
-                        int m = w * 2;
-                        return m;
-                    });
-        });
-        ints.stream().map( t -> {
-            int l = 0;
-            for (int j = 0;j < 10;j++) {
-                l = j + l;
-            }
-            return l;
-        });
-    }
+  /**
+   * According to java language specifications,
+   * statements end with ';'. That is why ';;'
+   * may be considered as two empty statements on the same line
+   * and rises violation.
+   */
+  ;; // violation
+  static {
+    new JCheckBox().addActionListener((final ActionEvent e) -> {good();});
+    List<Integer> ints = new LinkedList<Integer>();
+    ints.stream().map( t -> { return t * 2;} ).filter( t -> { return false;});
+    ints.stream().map( t -> { int m = t * 2; return m; } ); // violation
+    ints.stream().map( t -> { int m = t * 2; return m; } ); int i = 3; // 2 violations
+    ints.stream().map( t -> t * 2); int k = 4; // violation
+    ints.stream().map( t -> t * 2);
+    List<Integer> ints2 = new LinkedList<Integer>();
+    ints.stream().map( t -> { return ints2.stream().map(w -> { return w * 2; });});
+    ints.stream().map( t -> { return ints2.stream().map(w -> { int m=w; return m; });});// violation
+    ints.stream().map( t -> {
+      return ints2.stream().map(
+          w -> {
+            int m = w * 2;
+            return m;
+          });
+    });
+    ints.stream().map( t -> {
+      int l = 0;
+      for (int j = 0;j < 10;j++) {
+        l = j + l;
+      }
+      return l;
+    });
+  }
 
-    private static void good() {
-    }
+  private static void good() {
+  }
 }
