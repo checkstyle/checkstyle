772ed2171b87d8695e9bb25012473855d7d9e617
Abhinav Rajput <abhinavrajputaes@gmail.com>
2021-12-30 02:55:42 +0530

    Issue #10625: Fixed three input files and removed them from line length suppression

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/declarationorder/InputDeclarationOrderAvoidDuplicatesInStaticFinalFields.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/declarationorder/InputDeclarationOrderAvoidDuplicatesInStaticFinalFields.java
@@ -11,6 +11,6 @@ package com.puppycrawl.tools.checkstyle.checks.coding.declarationorder;
 public class InputDeclarationOrderAvoidDuplicatesInStaticFinalFields
 {
     private boolean allowInSwitchCase;
-    public static final String             // violation 'Static variable definition in wrong order.'
+    public static final String // violation 'Static variable definition in wrong order.'
             MSG_KEY_BLOCK_NESTED = "block.nested";
 }
