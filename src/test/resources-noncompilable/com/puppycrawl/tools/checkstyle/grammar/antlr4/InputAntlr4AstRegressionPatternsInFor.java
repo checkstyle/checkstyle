9b493c57f4a7dabe9f8ab9b79ad5a604ca3053ff
nmancus1 <nicholas.mancuso13@gmail.com>
2021-12-29 12:28:32 -0500

    Issue #10848: Support patterns in switch

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/grammar/antlr4/InputAntlr4AstRegressionPatternsInFor.java
@@ -0,0 +1,15 @@
+package com.puppycrawl.tools.checkstyle.grammar.antlr4;
+
+public class InputAntlr4AstRegressionPatternsInFor {
+    void m1(Object o) {
+        for (int i = 0; o instanceof Integer myInt && myInt > 5;) {
+            // type pattern, no `PATTERN_DEF`
+        }
+        for (int i = 0; o instanceof (Integer myInt && myInt > 5);) {
+            // parenthesized pattern, `PATTERN_DEF`
+        }
+        for (int i = 0; o instanceof Integer myInt; ) {
+            // type pattern, no `PATTERN_DEF`
+        }
+    }
+}
