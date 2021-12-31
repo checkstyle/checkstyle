b1c9b2580e628b9bc97ba133e42a8efa05f5d5c0
Abhinav Rajput <abhinavrajputaes@gmail.com>
2021-12-31 16:32:55 +0530

    Issue #10625: Fixed three input files and removed them from line length suppression

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/linelength/InputLineLengthLongPackageStatement.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/linelength/InputLineLengthLongPackageStatement.java
@@ -8,11 +8,12 @@ max = (default)80
 */
 
 //non-compiled with javac: contains different package by demand of test
-package com.nameofcompany.nameofdivision.nameofproject.systemtests.parallel.areaoftest.featuretested.flowtested;
+package com.nameofcompany.nameofdivision.nameofproject.systemtests.
+        parallel.areaoftest.featuretested.flowtested;
 
 public class InputLineLengthLongPackageStatement {
     @Override
     public String toString() {
-        return "This is very long line that should be logged because it is not package"; // violation
+        return "This is very long line that should be logged because it is not package";// violation
     }
 }
