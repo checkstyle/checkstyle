5c6cc98e21cf2a5dc09f9c75d4e9681dfbcad0bd
Vyom-Yadav <jackhammervyom@gmail.com>
2022-01-05 10:40:52 +0530

    testing

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/MetadataGeneratorLogger.java
@@ -0,0 +1,70 @@
+package com.puppycrawl.tools.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.api.AuditEvent;
+import com.puppycrawl.tools.checkstyle.api.AuditListener;
+import com.puppycrawl.tools.checkstyle.api.AutomaticBean;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.SeverityLevel;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Simple logger for metadata generator util.
+ */
+public class MetadataGeneratorLogger extends AutomaticBean implements AuditListener {
+
+    private final PrintWriter errorWriter;
+
+    private final AuditEventFormatter formatter;
+
+    public MetadataGeneratorLogger(OutputStream outputStream) {
+        final Writer errorStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+        this.errorWriter = new PrintWriter(errorStreamWriter);
+        this.formatter = new AuditEventDefaultFormatter();
+    }
+
+    @Override
+    public void auditStarted(AuditEvent event) {
+        errorWriter.flush();
+    }
+
+    @Override
+    public void auditFinished(AuditEvent event) {
+        errorWriter.flush();
+    }
+
+    @Override
+    public void fileStarted(AuditEvent event) {
+
+    }
+
+    @Override
+    public void fileFinished(AuditEvent event) {
+        errorWriter.flush();
+    }
+
+    @Override
+    public void addError(AuditEvent event) {
+        final SeverityLevel severityLevel = event.getSeverityLevel();
+        if (severityLevel != SeverityLevel.IGNORE) {
+            final String errorMessage = formatter.format(event);
+            errorWriter.println(errorMessage);
+        }
+    }
+
+    @Override
+    public void addException(AuditEvent event, Throwable throwable) {
+        synchronized (errorWriter) {
+            throwable.printStackTrace(errorWriter);
+        }
+    }
+
+    @Override
+    protected void finishLocalSetup() throws CheckstyleException {
+        // No code by default.
+    }
+}
