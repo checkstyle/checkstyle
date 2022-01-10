1a4a1b334cbd628cb66fd4b75c3d1d9d56d6b49e
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-01-10 13:33:34 +0530

    Issue #3082: new filter: SuppressByMessageFilter

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressbymessagefilter/InputSuppressByMessageFilter2.java
@@ -0,0 +1,31 @@
+/*
+SuppressByMessageFilter
+checkFormat = ""
+messageFormat = ""
+
+
+com.puppycrawl.tools.checkstyle.checks.coding.RequireThisCheck
+checkFields = true
+checkMethods = true
+validateOnlyOverlapping = false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.filters.suppressbymessagefilter;
+
+public class InputSuppressByMessageFilter2 {
+    private int a;
+    private int b;
+    private int c;
+
+    public InputSuppressByMessageFilter2(int a) {
+        a = a; // violation
+        b = 0; // violation
+        foo(5); // violation
+    }
+
+    public void foo(int c) {
+        c = c; // violation
+    }
+}
