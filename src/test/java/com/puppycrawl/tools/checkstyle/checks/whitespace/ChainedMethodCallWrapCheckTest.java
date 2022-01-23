102f3a0d2897f7dfdce234f1a7b510cca007205b
Vyom-Yadav <jackhammervyom@gmail.com>
2022-01-23 13:13:06 +0530

    Issue #11087: New check ChainedMethodCallWrap

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/whitespace/ChainedMethodCallWrapCheckTest.java
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2022 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static com.puppycrawl.tools.checkstyle.checks.whitespace.ChainedMethodCallWrapCheck.MSG_TOO_MANY_SINGLE_LINE;
+import static com.puppycrawl.tools.checkstyle.checks.whitespace.ChainedMethodCallWrapCheck.MSG_WRAP_METHOD_CALL;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class ChainedMethodCallWrapCheckTest extends AbstractModuleTestSupport {
+
+    @Override
+    protected String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/checks/whitespace/chainedmethodcallwrap";
+    }
+
+    @Test
+    public void testDefaultTokens() {
+        final ChainedMethodCallWrapCheck check = new ChainedMethodCallWrapCheck();
+        final int[] requiredTokens = {
+            TokenTypes.METHOD_CALL,
+        };
+        assertWithMessage("Required tokens are invalid")
+                .that(check.getRequiredTokens())
+                .isEqualTo(requiredTokens);
+    }
+
+    @Test
+    public void testChainedMethodCallWrap() throws Exception {
+        final String[] expected = {
+            "14:36: " + getCheckMessage(MSG_WRAP_METHOD_CALL),
+            "17:13: " + getCheckMessage(MSG_TOO_MANY_SINGLE_LINE, 2, 1),
+            "24:36: " + getCheckMessage(MSG_WRAP_METHOD_CALL),
+            "24:44: " + getCheckMessage(MSG_WRAP_METHOD_CALL),
+            "28:43: " + getCheckMessage(MSG_WRAP_METHOD_CALL),
+        };
+        verifyWithInlineConfigParser(getPath("InputChainedMethodCallWrap.java"), expected);
+    }
+
+    @Test
+    public void testChainedMethodCallWrapMaxSingleLineCalls() throws Exception {
+        final String[] expected = {
+            "19:9: " + getCheckMessage(MSG_TOO_MANY_SINGLE_LINE, 3, 2),
+        };
+        verifyWithInlineConfigParser(
+                getPath("InputChainedMethodCallWrapMaxSingleLineCalls.java"), expected);
+    }
+}
