f87677eb393ad3d58923138c7f651dec0d9ca55d
MUzairS15 <muzair.shaikh810@gmail.com>
2022-01-29 15:00:10 +0530

    minor: fix formatting of expression

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/RightCurlyCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/blocks/RightCurlyCheck.java
@@ -376,8 +376,8 @@ public class RightCurlyCheck extends AbstractCheck {
                                                int... targetSrcLine) {
         return bracePolicy == RightCurlyOption.ALONE
                     && shouldBeAloneOnLineWithAloneOption(details, targetSrcLine)
-                || (bracePolicy == RightCurlyOption.ALONE_OR_SINGLELINE
-                    || details.shouldCheckLastRcurly)
+                    || (bracePolicy == RightCurlyOption.ALONE_OR_SINGLELINE
+                        || details.shouldCheckLastRcurly)
                     && shouldBeAloneOnLineWithNotAloneOption(details, targetSrcLine);
     }
 
