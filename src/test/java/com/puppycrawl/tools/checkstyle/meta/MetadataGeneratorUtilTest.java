4af1900fcf9bd219792868c208d0bdf63989afcd
Vyom-Yadav <jackhammervyom@gmail.com>
2022-02-13 10:58:48 +0530

    Issue #10747: MetadataGeneratorUtil now reports errors back to the users

--- src/test/java/com/puppycrawl/tools/checkstyle/meta/MetadataGeneratorUtilTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/meta/MetadataGeneratorUtilTest.java
@@ -20,6 +20,7 @@
 package com.puppycrawl.tools.checkstyle.meta;
 
 import static com.google.common.truth.Truth.assertWithMessage;
+import static com.puppycrawl.tools.checkstyle.meta.JavadocMetadataScraper.MSG_DESC_MISSING;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,14 +29,21 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.itsallcode.io.Capturable;
+import org.itsallcode.junit.sysextensions.SystemOutGuard;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
 import com.puppycrawl.tools.checkstyle.internal.utils.CheckUtil;
 
-public final class MetadataGeneratorUtilTest {
+@ExtendWith(SystemOutGuard.class)
+public final class MetadataGeneratorUtilTest extends AbstractModuleTestSupport {
 
     private final List<String> modulesContainingNoMetadataFile = Arrays.asList(
             "Checker",
@@ -43,12 +51,29 @@ public final class MetadataGeneratorUtilTest {
             "JavadocMetadataScraper"
     );
 
+    @Override
+    protected String getPackageLocation() {
+        return null;
+    }
+
+    /**
+     * Generates metadata for checkstyle modules and verifies number of
+     * generated metadata modules match the number of checkstyle modules.
+     * Also verifies whether every checkstyle module contains description.
+     *
+     * @param systemOut wrapper for {@code System.out}
+     * @throws Exception if exception occurs during generating metadata or
+     *                   if an I/O error is thrown when accessing the starting file.
+     * @noinspection UseOfSystemOutOrSystemErr
+     */
     @Test
-    public void testMetadataFilesGenerationAllFiles() throws Exception {
+    public void testMetadataFilesGenerationAllFiles(@SystemOutGuard.SysOut Capturable systemOut)
+            throws Exception {
+        systemOut.captureMuted();
 
         MetadataGeneratorUtil.generate(System.getProperty("user.dir")
                         + "/src/main/java/com/puppycrawl/tools/checkstyle",
-                "checks", "filters", "filefilters");
+                System.out, "checks", "filters", "filefilters");
         final Set<String> metaFiles;
 
         try (Stream<Path> fileStream = Files.walk(
@@ -67,8 +92,37 @@ public final class MetadataGeneratorUtilTest {
                 .sorted()
                 .collect(Collectors.toCollection(LinkedHashSet::new));
         checkstyleModules.removeAll(modulesContainingNoMetadataFile);
-        assertWithMessage("Number of generated metadata files dont match with number of checkstyle "
-                + "module").that(metaFiles).isEqualTo(checkstyleModules);
+        assertWithMessage("Number of generated metadata files dont match with "
+                + "number of checkstyle module")
+                .that(metaFiles)
+                .isEqualTo(checkstyleModules);
+
+        final String[] expectedErrorMessages = {
+            "31: " + getCheckMessage(MSG_DESC_MISSING, "AbstractSuper"),
+            "45: " + getCheckMessage(MSG_DESC_MISSING, "AbstractHeader"),
+            "42: " + getCheckMessage(MSG_DESC_MISSING, "AbstractJavadoc"),
+            "45: " + getCheckMessage(MSG_DESC_MISSING, "AbstractClassCoupling"),
+            "26: " + getCheckMessage(MSG_DESC_MISSING, "AbstractAccessControlName"),
+            "30: " + getCheckMessage(MSG_DESC_MISSING, "AbstractName"),
+            "29: " + getCheckMessage(MSG_DESC_MISSING, "AbstractParenPad"),
+        };
+
+        final String[] actualViolations = systemOut.getCapturedData().split("\\n");
+        final Pattern pattern = Pattern.compile("((?<=:)\\d.*:.*(?=\\s\\[))");
+
+        Arrays.setAll(actualViolations, id -> {
+            final Matcher matcher = pattern.matcher(actualViolations[id]);
+            matcher.find();
+            return matcher.group(1);
+        });
+
+        Arrays.sort(actualViolations);
+        Arrays.sort(expectedErrorMessages);
+
+        assertWithMessage("Expected and actual errors do not match")
+                .that(expectedErrorMessages)
+                .asList()
+                .containsExactlyElementsIn(actualViolations);
     }
 
     /**
