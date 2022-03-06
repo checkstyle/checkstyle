4925cbf7dad6df2e43bb7bbda062438f0c955ca5
AmrDeveloper <amr96@programmer.net>
2022-03-06 13:45:53 +0200

    Issue #11214: Specified violation message in MultipleVariableDeclarationsCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplevariabledeclarations/InputMultipleVariableDeclarations.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplevariabledeclarations/InputMultipleVariableDeclarations.java
@@ -8,19 +8,19 @@ package com.puppycrawl.tools.checkstyle.checks.coding.multiplevariabledeclaratio
 
 public class InputMultipleVariableDeclarations
 {
-    int i, j; // violation
-    int i1; int j1; // violation
+    int i, j; // violation 'Each variable declaration must be in its own statement.'
+    int i1; int j1; // violation 'Only one variable definition per line allowed.'
 
     void method1() {
-        String str, str1; // violation
-        java.lang.Object obj; Object obj1; // violation
+        String str, str1; // violation 'Each variable declaration must be in its own statement.'
+        Object obj; Object obj1; // violation 'Only one variable definition per line allowed.'
     }
     // second definition is wrapped
     // line of VARIABLE_DEF is not the same as first line of the definition
-    java.lang.String string; java.lang.String // violation
+    java.lang.String string; String // violation 'Only one variable definition per line allowed.'
         strings[];
-    //both definitions is wrapped
-    java.lang. // violation
+    //both definitions are wrapped
+    java.lang. // violation 'Only one variable definition per line allowed.'
         String string1; java.lang.String
             strings1[];
 
