15a6e83386ae2dd25986ecaf03b10583b45a6a4b
Kevin222004 <kevinpatel222004@gmail.com>
2022-03-11 05:13:36 +0530

    Issue #6207: Add Xpath Regression Test for OperatorWrap

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
@@ -123,7 +123,6 @@ public class XpathRegressionTest extends AbstractModuleTestSupport {
             "ModifierOrder",
             "MultipleStringLiterals",
             "MutableException",
-            "OperatorWrap",
             "PackageName",
             "ParameterAssignment",
             "ParameterName",
