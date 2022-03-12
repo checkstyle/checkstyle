b5028b33082f551f4f0f0d264b56f388fa6f1a44
Vyom-Yadav <jackhammervyom@gmail.com>
2022-03-12 17:38:29 +0530

    Issue #10907: Modify LineLengthCheck to support validating import and package statements

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/sizes/LineLengthCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/sizes/LineLengthCheckTest.java
@@ -103,4 +103,30 @@ public class LineLengthCheckTest extends AbstractModuleTestSupport {
 
     }
 
+    @Test
+    public void testLineLengthIgnoringPackageStatements() throws Exception {
+        final String[] expected = {
+            "13: " + getCheckMessage(MSG_KEY, 75, 76),
+            "22: " + getCheckMessage(MSG_KEY, 75, 86),
+            "26: " + getCheckMessage(MSG_KEY, 75, 76),
+            "34: " + getCheckMessage(MSG_KEY, 75, 77),
+        };
+
+        verifyWithInlineConfigParser(
+            getNonCompilablePath("InputLineLengthIgnoringPackageStatements.java"), expected);
+    }
+
+    @Test
+    public void testLineLengthIgnoringImportStatements() throws Exception {
+        final String[] expected = {
+            "12: " + getCheckMessage(MSG_KEY, 75, 79),
+            "21: " + getCheckMessage(MSG_KEY, 75, 81),
+            "25: " + getCheckMessage(MSG_KEY, 75, 84),
+            "33: " + getCheckMessage(MSG_KEY, 75, 77),
+        };
+
+        verifyWithInlineConfigParser(
+            getNonCompilablePath("InputLineLengthIgnoringImportStatements.java"), expected);
+    }
+
 }
