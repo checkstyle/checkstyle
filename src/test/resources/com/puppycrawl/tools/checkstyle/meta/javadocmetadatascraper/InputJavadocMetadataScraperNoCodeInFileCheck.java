9b0f77a86b6458eb6af0546aafa0ee8a2c40e201
rnveach <rveach02@gmail.com>
2022-03-12 13:09:43 -0500

    Issue #4845: removed usage of DetailAST/Node from inputs

--- src/test/resources/com/puppycrawl/tools/checkstyle/meta/javadocmetadatascraper/InputJavadocMetadataScraperNoCodeInFileCheck.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/meta/javadocmetadatascraper/InputJavadocMetadataScraperNoCodeInFileCheck.java
@@ -9,8 +9,8 @@ package com.puppycrawl.tools.checkstyle.meta.javadocmetadatascraper;
 
 import com.puppycrawl.tools.checkstyle.StatelessCheck;
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
-import com.puppycrawl.tools.checkstyle.api.DetailAST;
-import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+
+
 
 /**
  * <p>
