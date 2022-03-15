19b30953951c029d70857170a9af1e2082fb9ab2
AmrDeveloper <amr96@programmer.net>
2022-03-15 10:45:17 +0200

    Issue #11214: Specified violation message in JavadocContentLocationCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoccontentlocation/InputJavadocContentLocationInterface.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoccontentlocation/InputJavadocContentLocationInterface.java
@@ -8,7 +8,7 @@ location = FIRST_LINE
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoccontentlocation;
 
 /**
- * // violation above
+ * // violation above 'Javadoc content should start from the same line as /\*\*.'
  */
 public interface InputJavadocContentLocationInterface {
 
