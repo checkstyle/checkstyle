81968a4262d597d97b443c4ba6990009c1c5ca84
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-03-18 18:40:46 +0530

    Issue #6207: Add Xpath Regression Test for IllegalToken git commit -m Issue

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
@@ -111,7 +111,6 @@ public class XpathRegressionTest extends AbstractModuleTestSupport {
             "InnerTypeLast",
             "InterfaceTypeParameterName",
             "JavaNCSS",
-            "IllegalImport",
             "LocalFinalVariableName",
             "LocalVariableName",
             "MagicNumber",
