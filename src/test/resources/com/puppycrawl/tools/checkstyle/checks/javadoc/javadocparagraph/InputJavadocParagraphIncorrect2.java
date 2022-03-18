ada4904f53ec8a9631b801248e05cf74e716a69a
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-03-18 16:45:54 +0530

    Issue #11214: Specified violation message in JavadocParagraphCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect2.java
@@ -10,14 +10,16 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparagraph;
 
 /**
  * Some Javadoc.
- * <p> // violation
- * /^ WARN/   Some Javadoc.<p> // violation
+ * <p> // violation 'Javadoc content should start from the next line after /\*\*.'
+ * /^ WARN/   Some Javadoc.<p> // violation 'Javadoc content should start from the next line
+ * after /\*\*.'
  *
  */
 class InputJavadocParagraphIncorrect2 {
 
     /**
-     * Some Javadoc.<P>  // violation
+     * Some Javadoc.<P>  // violation 'Javadoc content should start from the next line after
+     * /\*\*.'
      *
      * <p>  Some Javadoc.
      *
@@ -26,7 +28,8 @@ class InputJavadocParagraphIncorrect2 {
     public static final byte NUL = 0;
 
     /**
-     * Some <p>Javadoc. // violation
+     * Some <p>Javadoc. // violation 'Javadoc content should start from the next line after
+     * /\*\*.'
      *
      * <p>    Some Javadoc.
      *
@@ -36,25 +39,27 @@ class InputJavadocParagraphIncorrect2 {
     boolean emulated() {return false;}
 
     /**<p>Some Javadoc.<p>  // 2 violations
-     * <p>  // violation
-     * <p><p>  // violation
+     * <p>  // violation 'Javadoc content should start from the next line after /\*\*.'
+     * <p><p>  // violation 'Javadoc content should start from the next line after /\*\*.'
      * <p>/^WARN/   Some Javadoc.<p>*/  // violation
      class InnerInputJavadocParagraphIncorrect {
 
         /**
-         * Some Javadoc./WARN/<p>  // violation
+         * Some Javadoc./WARN/<p>  // violation 'Javadoc content should start from the next line
+         * after /\*\*.'
          *
          * @since 8.0
          */
         public static final byte NUL = 0;
 
-        /**<p>  // violation
+        /**<p>  // violation 'Javadoc content should start from the next line after /\*\*.'
          * /^WARN/ Some Javadoc.
          *
          * <P>
          * /^WARN/
          * <p> // violation
-         *  /^WARN/ Some Javadoc.<p> // violation
+         *  /^WARN/ Some Javadoc.<p> // violation 'Javadoc content should start from the next
+         *  line after /\*\*.'
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
          *     Documentation about GWT emulated source</a>
          */
@@ -64,29 +69,35 @@ class InputJavadocParagraphIncorrect2 {
     InnerInputJavadocParagraphIncorrect anon = new InnerInputJavadocParagraphIncorrect() {
 
             /**
-         * <p>Some Javadoc. // violation
+         * <p>Some Javadoc. // violation 'Javadoc content should start from the next line after
+         * /\*\*.'
          *
-         * Some Javadoc. // violation above
+         * Some Javadoc. // violation above 'Javadoc content should start from the next line after
+         * /\*\*.'
          *
          * @since 8.0
          */
         public static final byte NUL = 0;
 
         /**
-         * /WARN/  Some Javadoc.<p> // violation
+         * /WARN/  Some Javadoc.<p> // violation 'Javadoc content should start from the next line
+         * after /\*\*.'
          *
          *  <p>  Some Javadoc.
          *
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
-         *     Documentation about <p> GWT emulated source</a> // violation
+         *     Documentation about <p> GWT emulated source</a> // violation 'Javadoc content
+         *     should start from the next line after /\*\*.'
          */
         boolean emulated() {return false;}
 
         /**
-         * Double newline. // violation below
+         * Double newline. // violation below 'Javadoc content should start from the next line
+         * after /\*\*.'
          *
          *
-         * Some Javadoc. //DOUBLE WARN AT TWO PREVIOUS LINES // violation above
+         * Some Javadoc. //DOUBLE WARN AT TWO PREVIOUS LINES // violation above 'Javadoc content
+         * should start from the next line after /\*\*.'
          */
          void doubleNewline() {}
     };
