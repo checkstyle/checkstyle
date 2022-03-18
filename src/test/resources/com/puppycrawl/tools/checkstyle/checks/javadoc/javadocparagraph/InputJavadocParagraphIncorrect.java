83967c20b9fd7f1737705a6dc3db802c6bc8dcf0
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-03-18 16:45:54 +0530

    Issue #11214: Specified violation message in JavadocParagraphCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect.java
@@ -9,52 +9,66 @@ allowNewlineParagraph = (default)true
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparagraph;
 
 /**
- * Some Javadoc. // 2 violations below
+ * Some Javadoc. // violation '\Q Javadoc content should start from the next line
+ * after /\*\*.\E'
  * <p>
- * /^ WARN/   Some Javadoc.<p> // 2 violations
+ * /^ WARN/   Some Javadoc.<p> // violation '\Q Javadoc content should start from the next line
+ * after /\*\*.\E'
  *
  */
 class InputJavadocParagraphIncorrect {
 
     /**
-     * Some Javadoc.<P> // 2 violations
+     * Some Javadoc.<P> // violation '\Q Javadoc content should start from the next line
+     * after /\*\*.\E'
      *
-     * <p>  Some Javadoc. // violation
+     * <p>  Some Javadoc. // violation '\Q Javadoc content should start from the next line
+     * after /\*\*.\E'
      *
      * @since 8.0
      */
     public static final byte NUL = 0;
 
     /**
-     * Some <p>Javadoc. // violation
+     * Some <p>Javadoc. // violation '\Q Javadoc content should start from the next line
+     * after /\*\*.\E'
      *
-     * <p>    Some Javadoc. // violation
+     * <p> Some Javadoc. // violation '\Q Javadoc content should start from the next line
+    * after /\*\*.\E'
      *
      * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
      *     Documentation about GWT emulated source</a>
      */
     boolean emulated() {return false;}
 
-    /**<p>Some Javadoc.<p>  // 3 violations
-     * <p>  // 2 violations
-     * <p><p>  // 2 violations
-     * <p>/^WARN/   Some Javadoc.<p>*/  // 2 violations
+    /**<p>Some Javadoc.<p>  // violation '\Q Javadoc content should start from the next line
+     * after /\*\*.\E'
+     * <p>  // violation '\Q Javadoc content should start from the next line
+     * after /\*\*.\E'
+     * <p><p>  // violation '\Q Javadoc content should start from the next line
+     * after /\*\*.\E'
+     * <p>/^WARN/   Some Javadoc.<p>*/  // violation
      class InnerInputJavadocParagraphIncorrect {
 
         /**
-         * Some Javadoc./WARN/<p>  // 2 violations
+         * Some Javadoc./WARN/<p>  // violation 'Javadoc content should start from the next
+         * line after /\*\*.'
          *
          * @since 8.0
          */
         public static final byte NUL = 0;
 
         /**<p>
-         * /^WARN/ Some Javadoc. // 2 violations above
+         * /^WARN/ Some Javadoc. // violation above '\Q Javadoc content should start from the next
+         * line after /\*\*.\E'
          *
-         * <P> // violation
+         * <P> // violation '\Q Javadoc content should start from the next line
+         * after /\*\*.\E'
          * /^WARN/
-         * <p> // 2 violations
-         *  /^WARN/ Some Javadoc.<p> // 2 violations
+         * <p> // violation '\Q Javadoc content should start from the next line
+         * after /\*\*.\E'
+         *  /^WARN/ Some Javadoc.<p> // violation '\Q Javadoc content should start from the next
+         *  line after /\*\*.\E'
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
          *     Documentation about GWT emulated source</a>
          */
@@ -64,29 +78,36 @@ class InputJavadocParagraphIncorrect {
     InnerInputJavadocParagraphIncorrect anon = new InnerInputJavadocParagraphIncorrect() {
 
             /**
-         * <p>Some Javadoc. // violation
+         * <p>Some Javadoc. // violation '\Q Javadoc content should start from the next line
+         * after /\*\*.\E'
          *
-         * Some Javadoc. // violation above
+         * Some Javadoc. // violation above '\Q Javadoc content should start from the next line
+         * * after /\*\*.\E'
          *
          * @since 8.0
          */
         public static final byte NUL = 0;
 
         /**
-         * /WARN/  Some Javadoc.<p> // 2 violations
+         * /WARN/  Some Javadoc.<p> // violation '\Q Javadoc content should start from the next line
+         * after /\*\*.\E'
          *
-         *  <p>  Some Javadoc. // violation
+         *  <p>  Some Javadoc. // violation '\Q Javadoc content should start from the next line
+         * after /\*\*.\E'
          *
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
-         *     Documentation about <p> GWT emulated source</a> // 2 violations
+         *     Documentation about <p> GWT emulated source</a> // 2 violations 'Javadoc content
+         *     should start from the next line after /\*\*.'
          */
         boolean emulated() {return false;}
 
         /**
-         * Double newline. // violation below
+         * Double newline. // violation below '\Q Javadoc content should start from the next line
+         * after /\*\*.\E'
          *
          *
-         * Some Javadoc. //DOUBLE WARN AT TWO PREVIOUS LINES // violation above
+         * Some Javadoc. //violation above '\Q Javadoc content should start from the next line
+         * after /\*\*.\E'
          */
          void doubleNewline() {}
     };
