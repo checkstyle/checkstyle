bcf4dff46927c04d04e78d1e3812d4377e22437c
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-03-18 16:45:54 +0530

    Issue #11214: Specified violation message in JavadocParagraphCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect2.java
@@ -10,14 +10,17 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparagraph;
 
 /**
  * Some Javadoc.
- * <p> // violation
- * /^ WARN/   Some Javadoc.<p> // violation
+ * <p> // violation 'tag should be placed immediately before the first word, with no space
+ * after.'
+ * /^ WARN/   Some Javadoc.<p> // violation 'Empty line should be followed by <p> tag on the
+ * next line.'
  *
  */
 class InputJavadocParagraphIncorrect2 {
 
     /**
-     * Some Javadoc.<P>  // violation
+     * Some Javadoc.<P>  // violation 'tag should be placed immediately before the first word,
+     * with no space after.'
      *
      * <p>  Some Javadoc.
      *
@@ -26,7 +29,8 @@ class InputJavadocParagraphIncorrect2 {
     public static final byte NUL = 0;
 
     /**
-     * Some <p>Javadoc. // violation
+     * Some <p>Javadoc. // violation 'Empty line should be followed by <p> tag on the
+     * \ * next line.'
      *
      * <p>    Some Javadoc.
      *
@@ -36,25 +40,28 @@ class InputJavadocParagraphIncorrect2 {
     boolean emulated() {return false;}
 
     /**<p>Some Javadoc.<p>  // 2 violations
-     * <p>  // violation
-     * <p><p>  // violation
+     * <p>  // violation 'Empty line should be followed by <p> tag on the next line.'
+     * <p><p>  // violation 'Empty line should be followed by <p> tag on the next line.'
      * <p>/^WARN/   Some Javadoc.<p>*/  // violation
-     class InnerInputJavadocParagraphIncorrect {
+
+    class InnerInputJavadocParagraphIncorrect {
 
         /**
-         * Some Javadoc./WARN/<p>  // violation
+         * Some Javadoc./WARN/<p>  // violation 'tag should be placed immediately before the
+         * first word, with no space after.'
          *
          * @since 8.0
          */
         public static final byte NUL = 0;
 
-        /**<p>  // violation
+        /**<p>  // violation 'Empty line should be followed by <p> tag on the next line.'
          * /^WARN/ Some Javadoc.
          *
          * <P>
          * /^WARN/
-         * <p> // violation
-         *  /^WARN/ Some Javadoc.<p> // violation
+         * <p> // violation 'Empty line should be followed by <p> tag on the next line.'
+         *  /^WARN/ Some Javadoc.<p> // violation 'tag should be placed immediately before the
+         *  first word, with no space after.'
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
          *     Documentation about GWT emulated source</a>
          */
@@ -63,8 +70,9 @@ class InputJavadocParagraphIncorrect2 {
 
     InnerInputJavadocParagraphIncorrect anon = new InnerInputJavadocParagraphIncorrect() {
 
-            /**
-         * <p>Some Javadoc. // violation
+        /**
+         * <p>Some Javadoc. // violation 'Empty line should be followed by <p> tag on the
+         * next line.'
          *
          * Some Javadoc. // violation above
          *
@@ -73,12 +81,14 @@ class InputJavadocParagraphIncorrect2 {
         public static final byte NUL = 0;
 
         /**
-         * /WARN/  Some Javadoc.<p> // violation
+         * /WARN/  Some Javadoc.<p> // violation 'tag should be placed immediately before
+         * the first word, with no space after.'
          *
          *  <p>  Some Javadoc.
          *
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
-         *     Documentation about <p> GWT emulated source</a> // violation
+         *     Documentation about <p> GWT emulated source</a> // violation 'tag should be
+         * placed immediately before the first word, with no space after.'
          */
         boolean emulated() {return false;}
 
@@ -86,8 +96,8 @@ class InputJavadocParagraphIncorrect2 {
          * Double newline. // violation below
          *
          *
-         * Some Javadoc. //DOUBLE WARN AT TWO PREVIOUS LINES // violation above
+         * Some Javadoc. // violation above //DOUBLE WARN AT TWO PREVIOUS LINES
          */
-         void doubleNewline() {}
+        void doubleNewline() {}
     };
 }
