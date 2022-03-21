ef60a8c9981dcd703554db2fb45fa5cc5c12b6bd
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-03-21 23:17:52 +0530

    Issue #11214: Specified violation message in JavadocParagraphCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect.java
@@ -19,16 +19,17 @@ class InputJavadocParagraphIncorrect {
     /**
      * Some Javadoc.<P> // 2 violations
      *
-     * <p>  Some Javadoc. // violation
+     * <p>  Some Javadoc. // violation 'tag should be placed immediately before the first word'
      *
      * @since 8.0
      */
     public static final byte NUL = 0;
 
+    // violation 2 lines below '<p> tag should be preceded with an empty line.'
     /**
-     * Some <p>Javadoc. // violation
+     * Some <p>Javadoc.
      *
-     * <p>    Some Javadoc. // violation
+     * <p>    Some Javadoc. // violation 'tag should be placed immediately before the first word'
      *
      * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
      *     Documentation about GWT emulated source</a>
@@ -51,7 +52,7 @@ class InputJavadocParagraphIncorrect {
         /**<p>
          * /^WARN/ Some Javadoc. // 2 violations above
          *
-         * <P> // violation
+         * <P> // violation 'tag should be placed immediately before the first word'
          * /^WARN/
          * <p> // 2 violations
          *  /^WARN/ Some Javadoc.<p> // 2 violations
@@ -63,31 +64,35 @@ class InputJavadocParagraphIncorrect {
 
     InnerInputJavadocParagraphIncorrect anon = new InnerInputJavadocParagraphIncorrect() {
 
+        // violation 2 lines below 'Redundant <p> tag.'
             /**
-         * <p>Some Javadoc. // violation
+         * <p>Some Javadoc.
          *
-         * Some Javadoc. // violation above
+         * Some Javadoc.
          *
          * @since 8.0
          */
+        // violation 5 lines above 'Empty line should be followed by <p> tag on the next line.'
         public static final byte NUL = 0;
 
         /**
          * /WARN/  Some Javadoc.<p> // 2 violations
          *
-         *  <p>  Some Javadoc. // violation
+         *  <p>  Some Javadoc. // violation 'tag should be placed immediately before the first word'
          *
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
          *     Documentation about <p> GWT emulated source</a> // 2 violations
          */
         boolean emulated() {return false;}
 
+        // violation 3 lines below 'Empty line should be followed by <p> tag on the next line.'
         /**
-         * Double newline. // violation below
+         * Double newline.
          *
          *
-         * Some Javadoc. //DOUBLE WARN AT TWO PREVIOUS LINES // violation above
+         * Some Javadoc. //DOUBLE WARN AT TWO PREVIOUS LINES
          */
+        // violation 3 lines above 'Empty line should be followed by <p> tag on the next line.'
          void doubleNewline() {}
     };
 }
