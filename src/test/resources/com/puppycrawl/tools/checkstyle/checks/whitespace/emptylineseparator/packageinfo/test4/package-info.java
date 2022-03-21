0a7c240ffb563cedfac084eeebb7d3f8bc45f5a5
Kevin222004 <kevinpatel222004@gmail.com>
2022-03-21 14:58:24 +0530

    Issue #11214: specified violation message in EmptyLineSeparatorCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/packageinfo/test4/package-info.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/packageinfo/test4/package-info.java
@@ -13,7 +13,7 @@ tokens = (default)PACKAGE_DEF, IMPORT, STATIC_IMPORT, CLASS_DEF, INTERFACE_DEF, 
 //for test there's warning when single line comment isn't separated from PACKAGE_DEF by line
 //violation is expected after the next line. Starting * on next comment is to kill mutation
 //*because utility doesnt check javadoc but any comment with starting *.
-package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator.packageinfo.test4; // violation
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator.packageinfo.test4; // violation ''package' should be separated from previous line'
 
 //that import is for doing check of PACKAGE_DEF,
 //because EmptyLineSeparatorCheck doesn't check last token
