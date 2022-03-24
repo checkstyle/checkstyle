56a87add6ee7eeab833e9203c3694c9769a40e34
Vyom-Yadav <jackhammervyom@gmail.com>
2022-03-24 22:17:17 +0530

    Issue #11439: Converted test for SuppressionXpathSingleFilterTest to use inlined config in Input files

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionxpathsinglefilter/InputSuppressionXpathSingleFilterNonMatchingCheck.java
@@ -0,0 +1,20 @@
+/*
+SuppressionXpathSingleFilter
+files = InputSuppressionXpathSingleFilterNonMatchingCheck
+checks = UnusedLocalVariableCheck
+message = (default)(null)
+id = (default)(null)
+query = /COMPILATION_UNIT/CLASS_DEF \
+        [./IDENT[@text='InputSuppressionXpathSingleFilterNonMatchingCheck']]
+
+com.puppycrawl.tools.checkstyle.checks.javadoc.MissingJavadocTypeCheck
+scope = (default)public
+excludeScope = (default)(null)
+skipAnnotations = (default)Generated
+tokens = CLASS_DEF
+
+*/
+package com.puppycrawl.tools.checkstyle.filters.suppressionxpathsinglefilter;
+
+public class InputSuppressionXpathSingleFilterNonMatchingCheck { // violation
+}
