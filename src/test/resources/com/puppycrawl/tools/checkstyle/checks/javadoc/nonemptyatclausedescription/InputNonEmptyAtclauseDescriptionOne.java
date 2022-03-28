69d497a239568660b80cb674365efd8f1a9bd7ff
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-03-28 15:51:58 +0530

    Issue #11163: Enforce file size on InputNonEmptyAtclauseDescription

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/nonemptyatclausedescription/InputNonEmptyAtclauseDescriptionOne.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/nonemptyatclausedescription/InputNonEmptyAtclauseDescriptionOne.java
@@ -9,14 +9,14 @@ javadocTokens = (default)PARAM_LITERAL, RETURN_LITERAL, THROWS_LITERAL, \
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.nonemptyatclausedescription;
 
-class InputNonEmptyAtclauseDescription
+class InputNonEmptyAtclauseDescriptionOne
 {
         /**
          * Some javadoc.
          * @param a Some javadoc.
          * @param b Some javadoc.
          */
-        public InputNonEmptyAtclauseDescription(String a, int b)
+        public InputNonEmptyAtclauseDescriptionOne(String a, int b)
         {
 
         }
@@ -26,7 +26,7 @@ class InputNonEmptyAtclauseDescription
          * @param a Some javadoc. // ok
          * @deprecated Some javadoc.
          */
-        public InputNonEmptyAtclauseDescription(String a)
+        public InputNonEmptyAtclauseDescriptionOne(String a)
         {
 
         }
@@ -39,7 +39,7 @@ class InputNonEmptyAtclauseDescription
          * @param c
          */ // violation 2 lines above
         // violation 2 lines above
-        public InputNonEmptyAtclauseDescription(String a, int b, double c)
+        public InputNonEmptyAtclauseDescriptionOne(String a, int b, double c)
         {
 
         }
@@ -52,7 +52,7 @@ class InputNonEmptyAtclauseDescription
          * @deprecated
          */ // violation 2 lines above
         // violation 2 lines above
-        public InputNonEmptyAtclauseDescription(String a, boolean e)
+        public InputNonEmptyAtclauseDescriptionOne(String a, boolean e)
         {
 
         }
@@ -100,65 +100,6 @@ class InputNonEmptyAtclauseDescription
         // violation 3 lines above
         public int foo3(String a, int b, double c) throws Exception
         {
-                return 1;
-        }
-
-        // violation 4 lines below
-        // violation 4 lines below
-        /**
-         *
-         * @param a
-         * @param b
-         * @param c
-         * @deprecated
-         * @throws Exception
-         */ // violation 3 lines above
-        // violation 3 lines above
-        // violation 3 lines above
-        public int foo4(String a, int b, double c) throws Exception
-        {
-                return 1;
-        }
-
-        /**
-         * Some javadoc
-         * @param a Some javadoc
-         * @param b Some javadoc
-         * @param c Some javadoc
-         * @return Some javadoc
-         * @exception Exception Some javadoc
-         * @deprecated Some javadoc
-         */
-        public int foo5(String a, int b, double c) throws Exception
-        {
-                return 1;
-        }
-
-        /**
-         *
-         * @param a Some javadoc
-         * @param b Some javadoc
-         * @param c Some javadoc
-         * @return Some javadoc
-         * @exception Exception
-         */ // violation above
-        public int foo6(String a, int b, double c) throws Exception
-        {
-                return 1;
-        }
-
-        /**
-         * @param a xxx
-         * @return
-         */ // violation above
-        int foo(int a) {
-                return a;
-        }
-
-        /**
-         * @return {@code 1}
-         */     // ^ ok
-        int bar() {
                 return 1;
         }
 }
