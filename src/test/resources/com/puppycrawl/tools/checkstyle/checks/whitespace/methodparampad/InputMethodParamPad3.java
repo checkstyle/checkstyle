9b8384b7aacd5b20ef62d284a8be9235dc60cbd7
Kevin222004 <kevinpatel222004@gmail.com>
2022-03-29 19:16:38 +0530

    Issue #11214: Specified violation message in MethodParamPadCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/methodparampad/InputMethodParamPad3.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/methodparampad/InputMethodParamPad3.java
@@ -13,9 +13,9 @@ import java.util.Vector;
 /** Test input for MethodDefPadCheck */
 public class InputMethodParamPad3
 {
-    public InputMethodParamPad3() // violation
+    public InputMethodParamPad3() // violation ''(' is not preceded with whitespace'
     {
-        super(); // violation
+        super(); // violation ''(' is not preceded with whitespace'
     }
 
     public InputMethodParamPad3 (int aParam)
@@ -24,13 +24,13 @@ public class InputMethodParamPad3
     }
 
     public InputMethodParamPad3
-        (double aParam) // violation
+        (double aParam) // violation ''(' should be on the previous line'
     {
         super
-            (); // violation
+            (); // violation ''(' should be on the previous line'
     }
 
-    public void method() // violation
+    public void method() // violation ''(' is not preceded with whitespace'
     {
     }
 
@@ -39,45 +39,49 @@ public class InputMethodParamPad3
     }
 
     public void method
-        (double aParam) // violation
+        (double aParam) // violation ''(' should be on the previous line'
     {
         // invoke constructor
-        InputMethodParamPad pad = new InputMethodParamPad(); // violation
+        InputMethodParamPad pad =
+                new InputMethodParamPad(); // violation ''(' is not preceded with whitespace'
         pad = new InputMethodParamPad ();
         pad = new InputMethodParamPad
-            (); // violation
+            (); // violation ''(' should be on the previous line'
 
         // call method
-        method(); // violation
+        method(); // violation ''(' is not preceded with whitespace'
         method ();
         method
-            (); // violation
+            (); // violation ''(' should be on the previous line'
     }
 
-    public void dottedCalls() // violation
+    public void dottedCalls() // violation ''(' is not preceded with whitespace'
     {
-        this.method(); // violation
+        this.method(); // violation ''(' is not preceded with whitespace'
         this.method ();
         this.method
-            (); // violation
+            (); // violation ''(' should be on the previous line'
 
-        InputMethodParamPad p = new InputMethodParamPad(); // violation
-        p.method(); // violation
+        InputMethodParamPad p =
+                new InputMethodParamPad(); // violation ''(' is not preceded with whitespace'
+        p.method(); // violation ''(' is not preceded with whitespace'
         p.method ();
         p.method
-            (); // violation
+            (); // violation ''(' should be on the previous line'
 
-        java.lang.Integer.parseInt("0"); // violation
+        java.lang.Integer.parseInt("0"); // violation ''(' is not preceded with whitespace'
         java.lang.Integer.parseInt ("0");
         java.lang.Integer.parseInt
-            ("0"); // violation
+            ("0"); // violation ''(' should be on the previous line'
     }
 
-    public void newArray() // violation
+    public void newArray() // violation ''(' is not preceded with whitespace'
     {
         int[] a = new int[]{0, 1};
-        java.util.Vector<String> v = new java.util.Vector<String>(); // violation
-        java.util.Vector<String> v1 = new Vector<String>(); // violation
+        java.util.Vector<String> v =
+                new java.util.Vector<String>(); // violation ''(' is not preceded with whitespace'
+        java.util.Vector<String> v1 =
+                new Vector<String>(); // violation ''(' is not preceded with whitespace'
     }
 
     enum TestEnum {
@@ -86,7 +90,7 @@ public class InputMethodParamPad3
             },
 
         SECOND
-            () // violation
+            () // violation ''(' should be on the previous line'
         {
         }
     }
