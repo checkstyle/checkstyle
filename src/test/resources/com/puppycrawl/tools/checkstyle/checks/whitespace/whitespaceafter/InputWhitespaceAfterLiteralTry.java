67002c2835940ec25f25f48f47c3e8e4b618bc3c
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-03-30 21:03:09 +0530

    Issue #11489: Add LITERAL_TRY token support to WhitespaceAfter

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespaceafter/InputWhitespaceAfterLiteralTry.java
@@ -0,0 +1,26 @@
+/*
+WhitespaceAfter
+tokens = LITERAL_TRY
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.whitespaceafter;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputWhitespaceAfterLiteralTry {
+    public static void main(String[] args) throws IOException {
+        try (InputStream ignored = System.in) {} // OK
+
+        try // OK
+            (InputStream ignored = System.in) {}
+
+        try(InputStream ignored = System.in) {} // violation ''try' is not followed by whitespace'
+
+        try {}catch (Exception e){} // OK
+
+        try{}catch (Exception e){} // violation ''try' is not followed by whitespace'
+    }
+}
