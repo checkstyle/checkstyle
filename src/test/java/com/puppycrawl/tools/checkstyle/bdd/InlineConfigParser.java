7036a1e10ce5ed67e5af3ac1a3b7945a21d3939a
Abhinav <abhinavrajputaes@gmail.com>
2022-03-30 18:05:34 +0530

    Issue #11214: Moved OrderedPropertiesCheck from SUPPRESSED_CHECKS to PERMANENT_SUPPRESSED_CHECKS collection

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -106,7 +106,6 @@ public final class InlineConfigParser {
             "com.puppycrawl.tools.checkstyle.checks.coding.ReturnCountCheck",
             "com.puppycrawl.tools.checkstyle.checks.annotation.AnnotationUseStyleCheck",
             "com.puppycrawl.tools.checkstyle.checks.whitespace.EmptyLineSeparatorCheck",
-            "com.puppycrawl.tools.checkstyle.checks.OrderedPropertiesCheck",
             "com.puppycrawl.tools.checkstyle.checks.javadoc.AbstractJavadocCheck",
             "com.puppycrawl.tools.checkstyle.checks.naming.AbstractClassNameCheck",
             "com.puppycrawl.tools.checkstyle.checks.coding.PackageDeclarationCheck",
@@ -132,6 +131,16 @@ public final class InlineConfigParser {
             "com.puppycrawl.tools.checkstyle.checks.imports.CustomImportOrderCheck",
             "com.puppycrawl.tools.checkstyle.checks.coding.VariableDeclarationUsageDistanceCheck",
             "com.puppycrawl.tools.checkstyle.checks.imports.ImportControlCheck"));
+
+    /**
+     *  Inlined configs can not be used in non-java checks, as Inlined config is java style
+     *  multiline comment.
+     *  Such check files needs to be permanently suppressed.
+     */
+    private static final Set<String> PERMANENT_SUPPRESSED_CHECKS = Set.of(
+            // Inlined config is not supported for non java files.
+            "com.puppycrawl.tools.checkstyle.checks.OrderedPropertiesCheck"
+    );
 
     /** Stop instances being created. **/
     private InlineConfigParser() {
@@ -319,6 +328,7 @@ public final class InlineConfigParser {
         final List<ModuleInputConfiguration> moduleLists = inputConfigBuilder.getChildrenModules();
         final boolean specifyViolationMessage = moduleLists.size() == 1
                 && !SUPPRESSED_CHECKS.contains(moduleLists.get(0).getModuleName())
+                && !PERMANENT_SUPPRESSED_CHECKS.contains(moduleLists.get(0).getModuleName())
                 && getNumberOfMessages(moduleLists.get(0).getModuleName()) > 1;
         for (int lineNo = 0; lineNo < lines.size(); lineNo++) {
             setViolations(inputConfigBuilder, lines,
