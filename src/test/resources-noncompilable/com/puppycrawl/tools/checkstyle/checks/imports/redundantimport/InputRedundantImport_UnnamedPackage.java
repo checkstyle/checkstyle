789619ea5a9944ecf58f17398e3fab30a567411e
Binita Kumari <binikumari804408@gmail.com>
2022-04-01 02:57:51 +0530

    Issue #11214: specified violation message in RedundantImport

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/redundantimport/InputRedundantImport_UnnamedPackage.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/redundantimport/InputRedundantImport_UnnamedPackage.java
@@ -7,9 +7,9 @@ RedundantImport
 //non-compiled with javac: no package by design of test
 
 import java.util.List;
-import java.util.List; // violation
+import java.util.List; // violation 'Duplicate import to line 9'
 import java.util.Arrays;
-import java.lang.String; // violation
+import java.lang.String; // violation 'Redundant import from the java.lang package'
 import static java.lang.Math.PI;
 
 public class InputRedundantImport_UnnamedPackage
