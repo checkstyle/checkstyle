2fdeb09ec249666e46b9fb52b2ee276c24b625aa
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-04-03 11:24:30 +0530

    Issue #11446: Update SuppressionCommentFilterTest to use verifyWithInlineConfigParser

--- src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressioncommentfilter/InputSuppressionCommentFilter11.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressioncommentfilter/InputSuppressionCommentFilter11.java
@@ -9,15 +9,6 @@ checkCPP = (default)true
 checkC = (default)true
 
 
-com.puppycrawl.tools.checkstyle.checks.naming.MemberName
-id = ignore
-format = (default)^[a-z][a-zA-Z0-9]*$
-applyToPublic = (default)true
-applyToProtected = (default)true
-applyToPackage = (default)true
-applyToPrivate = (default)true
-
-
 com.puppycrawl.tools.checkstyle.checks.naming.ConstantName
 id = (null)
 format = (default)^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$
@@ -25,11 +16,6 @@ applyToPublic = (default)true
 applyToProtected = (default)true
 applyToPackage = (default)true
 applyToPrivate = (default)true
-
-
-com.puppycrawl.tools.checkstyle.checks.coding.IllegalCatch
-illegalClassNames = (default)Error, Exception, RuntimeException, Throwable, java.lang.Error, \
-                    java.lang.Exception, java.lang.RuntimeException, java.lang.Throwable
 
 
 */
