a5a89597873448739c8a23a197bcd6ce52840dd3
Andrei Paikin <andreypaykin@gmail.com>
2022-04-09 10:41:47 +0300

    minor: replace Collections.unmodifiable* with static factory methods

--- src/main/java/com/puppycrawl/tools/checkstyle/utils/TokenUtil.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/utils/TokenUtil.java
@@ -22,7 +22,6 @@ package com.puppycrawl.tools.checkstyle.utils;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
@@ -93,10 +92,11 @@ public final class TokenUtil {
      * @return unmodifiable name to value map
      */
     public static Map<String, Integer> nameToValueMapFromPublicIntFields(Class<?> cls) {
-        final Map<String, Integer> map = Arrays.stream(cls.getDeclaredFields())
+        return Arrays.stream(cls.getDeclaredFields())
             .filter(fld -> Modifier.isPublic(fld.getModifiers()) && fld.getType() == Integer.TYPE)
-            .collect(Collectors.toMap(Field::getName, fld -> getIntFromField(fld, fld.getName())));
-        return Collections.unmodifiableMap(map);
+            .collect(Collectors.toUnmodifiableMap(
+                Field::getName, fld -> getIntFromField(fld, fld.getName()))
+            );
     }
 
     /**
