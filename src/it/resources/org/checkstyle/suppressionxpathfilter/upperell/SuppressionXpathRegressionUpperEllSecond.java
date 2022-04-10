3b0d054fc002112f6e661b097b012a1c4958fd54
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-04-10 15:58:22 +0530

    Issue #8151: Update XpathRegressionUpperEllTest to have atleast 2 tests

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/upperell/SuppressionXpathRegressionUpperEllSecond.java
@@ -0,0 +1,8 @@
+package org.checkstyle.suppressionxpathfilter.upperell;
+
+public interface SuppressionXpathRegressionUpperEllSecond {
+    public static void test() {
+        long var1 = 508987;
+        long var2 = 508987l; //warn
+    }
+}
