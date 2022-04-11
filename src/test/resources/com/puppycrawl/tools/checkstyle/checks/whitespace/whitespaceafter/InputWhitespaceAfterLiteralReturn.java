783faace74c62d7f2d0614dc0d3b9fcdbf7e1491
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-04-11 23:47:24 +0530

    Issue #11536: Added support for LITERAL_RETURN token in WhitespaceAfter

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespaceafter/InputWhitespaceAfterLiteralReturn.java
@@ -0,0 +1,38 @@
+/*
+WhitespaceAfter
+tokens = LITERAL_RETURN
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.whitespaceafter;
+
+public class InputWhitespaceAfterLiteralReturn {
+
+    interface test {
+        int add (int x);
+    }
+    public static test One() {
+        return(x) -> x; // violation ''return' is not followed by whitespace'
+    }
+
+    public static test Two() {
+        return (x) -> x; // OK
+    }
+
+     void testThree () {
+        return; // OK
+    }
+
+    boolean testFour() {
+        return!true; // violation ''return' is not followed by whitespace'
+    }
+
+    int testFive() {
+        return-1; // violation ''return' is not followed by whitespace'
+    }
+
+    double testSix() {
+        return~1; // violation ''return' is not followed by whitespace'
+    }
+}
