dd99022842c1cac956941e3e8a8d5cb56a80d5df
Kaung Myat Htet <97383493+kaungmyat-htet@users.noreply.github.com>
2022-04-17 23:01:17 +0700

    Issue #11214: Specified violation message in RightCurlyCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlyTestSameAndLiteralDo.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlyTestSameAndLiteralDo.java
@@ -67,12 +67,12 @@ public class InputRightCurlyTestSameAndLiteralDo {
 
     public void foo5() {
         do {
-        } // violation
+        } // violation ''}' at column 9 should be on the same line as .*/while'
         while (true);
     }
 
     public void foo6() {
-        do {} // violation
+        do {} // violation ''}' at column 13 should be on the same line as .*/while'
         while (true);
     }
 
@@ -88,7 +88,7 @@ public class InputRightCurlyTestSameAndLiteralDo {
 
         {
 
-        } // violation
+        } // violation ''}' at column 9 should be on the same line as .*/while'
 
         while
 
