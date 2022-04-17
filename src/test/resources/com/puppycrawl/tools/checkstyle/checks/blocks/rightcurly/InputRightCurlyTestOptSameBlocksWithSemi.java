aaf00cba866326fb462711045dccdc0447e95dbc
Kaung Myat Htet <97383493+kaungmyat-htet@users.noreply.github.com>
2022-04-17 23:01:17 +0700

    Issue #11214: Specified violation message in RightCurlyCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlyTestOptSameBlocksWithSemi.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlyTestOptSameBlocksWithSemi.java
@@ -13,18 +13,18 @@ public class InputRightCurlyTestOptSameBlocksWithSemi {
     public void testMethod() {}; // ok
 
     public void testMethod1() {
-    }; // violation
+    }; // violation ''}' at column 5 should be alone on a line'
 
     public class TestClass {}; // ok
 
     public class TestClass1 {
-    }; // violation
+    }; // violation ''}' at column 5 should be alone on a line'
 
     public class TestClass2 {
         public TestClass2() {}; // ok
 
         public TestClass2(String someValue) {
-        }; // violation
+        }; // violation ''}' at column 9 should be alone on a line'
     }
 
     public void testMethod11() {
@@ -32,37 +32,37 @@ public class InputRightCurlyTestOptSameBlocksWithSemi {
     ;
 
     public @interface TestAnnnotation5 {
-        String someValue(); }; // violation
+        String someValue(); }; // violation ''}' at column 29 should have line break before'
 
     public @interface TestAnnotation6 {}; // ok
 
     public @interface TestAnnotation7 {
         String someValue();
-    }; // violation
+    }; // violation ''}' at column 5 should be alone on a line'
 
     public @interface TestAnnotation8 { String someValue();
-    }; // violation
+    }; // violation ''}' at column 5 should be alone on a line'
 
     public @interface TestAnnotation9 { String someValue(); }; // ok
 
     enum TestEnum{}; // ok
 
     enum TestEnum1{
-        SOME_VALUE;}; // violation
+        SOME_VALUE;}; // violation ''}' at column 20 should have line break before'
 
     enum TestEnum2 { SOME_VALUE; }; // ok
 
     enum TestEnum3{
         SOME_VALUE;
-    }; // violation
+    }; // violation ''}' at column 5 should be alone on a line'
 
     enum TestEnum4{ SOME_VALUE;
-    }; // violation
+    }; // violation ''}' at column 5 should be alone on a line'
 
     interface Interface1
     {
         int i = 1;
-        public void meth1(); }; // violation
+        public void meth1(); }; // violation ''}' at column 30 should have line break before'
 
     interface Interface2
     { int i = 1; public void meth1(); };
@@ -75,5 +75,5 @@ public class InputRightCurlyTestOptSameBlocksWithSemi {
 
     interface InterfaceEndingWithSemiColon2 {
         public void fooEmpty();
-    }; // violation
+    }; // violation ''}' at column 5 should be alone on a line'
 }
