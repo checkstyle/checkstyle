45c8df0a9cc64309be470d1ba31d6cfe6ba9069c
as23415672 <panshuyu0618@gmail.com>
2022-04-23 22:27:43 +0800

    Issue #11460: Fixed the default behavior of SuppressoinXpathSingleFilter

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionxpathsinglefilter/InputSuppressionXpathSingleFilterAllNullConfiguration.java
@@ -0,0 +1,19 @@
+/*
+SuppressionXpathSingleFilter
+files = (default)(null)
+checks = (default)(null)
+message = (default)(null)
+id = (default)(null)
+query = (default)(null)
+
+com.puppycrawl.tools.checkstyle.checks.javadoc.MissingJavadocTypeCheck
+scope = (default)public
+excludeScope = (default)(null)
+skipAnnotations = (default)Generated
+tokens = CLASS_DEF
+
+*/
+package com.puppycrawl.tools.checkstyle.filters.suppressionxpathsinglefilter;
+
+public class InputSuppressionXpathSingleFilterAllNullConfiguration { // violation
+}
