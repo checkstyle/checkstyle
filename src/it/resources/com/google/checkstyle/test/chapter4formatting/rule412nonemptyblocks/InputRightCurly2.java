0c038b85dc8015c87bc5018b4a425a6c275026bb
rnveach <rveach02@gmail.com>
2022-04-30 09:12:00 -0400

    Issue #7546: adds support to it to load multiple configs at once

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule412nonemptyblocks/InputRightCurly2.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule412nonemptyblocks/InputRightCurly2.java
@@ -1,6 +1,6 @@
 package com.google.checkstyle.test.chapter4formatting.rule412nonemptyblocks;
 
-public class InputRightCurlySame {
+public class InputRightCurly2 {
     public static void main(String[] args) {
         boolean after = false;
         try {
