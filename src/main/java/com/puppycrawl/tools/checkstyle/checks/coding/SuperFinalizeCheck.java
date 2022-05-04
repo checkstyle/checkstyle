6a63c82304a3d207e6dbe7b21b4371a87f022d37
Shaman20 <shamansr21@gmail.com>
2022-05-04 20:12:45 +0530

    Issue #7622: Updated code

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/SuperFinalizeCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/SuperFinalizeCheck.java
@@ -43,26 +43,13 @@ import com.puppycrawl.tools.checkstyle.StatelessCheck;
  * <pre>
  * public class A {
  *      protected void finalize() throws Throwable {
- *          try{
- *              System.out.println("Finalize of sub class"); //release resources, perform cleanup
- *          } catch(Throwable t) {
- *              throw t;
- *          } finally { //OK, calls super.finalize
- *              System.out.println("Calling finalize");
- *              super.finalize();
- *          }
+ *         System.out.println("In finalize block");
+ *          super.finalize(); //OK, calls super.finalize()
  *      }
  * }
- * class B {
- *      protected void finalize() throws Throwable {
- *          try{
- *              //Statements;
- *          } catch() {
- *              //body;
- *          }
- *          finally {
- *          //violation does not call super.finalize
- *          }
+ * public class B {
+ *      protected void finalize() throws Throwable { //Violation
+ *          System.out.println("In finalize block");
  *      }
  * }
  * </pre>
