8737844cfc1619674c68b2055971e94d30dd2b01
shubhamxshah <200303124082@paruluniversity.ac.in>
2022-05-10 03:57:14 +0530

    Issue #11214: Specify violation messages for ImportControlCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/InputImportControl_Blacklist.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/InputImportControl_Blacklist.java
@@ -8,11 +8,11 @@ path = (default).*
 
 package com.puppycrawl.tools.checkstyle.checks.imports.importcontrol;
 
-import java.util.stream.Stream; // violation
-import java.util.Date; // violation
+import java.util.stream.Stream; // violation 'Disallowed import - java.util.stream.Stream'
+import java.util.Date; // violation 'Disallowed import - java.util.Date'
 import java.util.List;
-import java.util.stream.Collectors; // violation
-import java.util.stream.IntStream; // violation
+import java.util.stream.Collectors; // violation 'Disallowed import - java.util.stream.Collectors'
+import java.util.stream.IntStream; // violation 'Disallowed import - java.util.stream.IntStream'
 
 public class InputImportControl_Blacklist
 {
