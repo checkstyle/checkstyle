a335b50ed55d0e8710e76f1344e5ffb344b311dc
shubhamxshah <200303124082@paruluniversity.ac.in>
2022-05-10 03:57:14 +0530

    Issue #11214: Specify violation messages for ImportControlCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/InputImportControl2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/InputImportControl2.java
@@ -8,10 +8,10 @@ path = (default).*
 
 package com.puppycrawl.tools.checkstyle.checks.imports.importcontrol;
 
-import java.awt.Image; // violation
-import javax.swing.border.*; // violation
+import java.awt.Image; // violation 'Disallowed import - java.awt.Image'
+import javax.swing.border.*; // violation 'Disallowed import - javax.swing.border.*'
 import java.io.File;
-import static java.awt.Button.ABORT; // violation
+import static java.awt.Button.ABORT; // violation 'Disallowed import - java.awt.Button.ABORT'
 
 public class InputImportControl2
 {
