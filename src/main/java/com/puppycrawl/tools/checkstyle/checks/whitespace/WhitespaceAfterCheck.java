b120505b0ed908715e72200c21019ce2732ce5f2
Binita Kumari <binikumari804408@gmail.com>
2022-05-12 02:17:20 +0530

    Issue #11508: Added LITERAL_YIELD token support to WhitespaceAfter

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/whitespace/WhitespaceAfterCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/whitespace/WhitespaceAfterCheck.java
@@ -58,6 +58,8 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  * LITERAL_FINALLY</a>,
  * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LITERAL_RETURN">
  * LITERAL_RETURN</a>,
+ * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LITERAL_YIELD">
+ * LITERAL_YIELD</a>,
  * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LITERAL_CATCH">
  * LITERAL_CATCH</a>,
  * <a href="https://checkstyle.org/apidocs/com/puppycrawl/tools/checkstyle/api/TokenTypes.html#LITERAL_DO">
@@ -118,6 +120,16 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  *  }
  *  public String testTwo() {
  *      return("a" + "b"); // violation 'return' is not followed by whitespace'
+ *  }
+ *  public static void main(String[] args) {
+ *    int a = switch (args[0]) {
+ *      case "got":
+ *        yield (1); // OK
+ *      case "my":
+ *        yield(3); // violation ''yield' is not followed by whitespace'
+ *      default:
+ *        yield 2;
+ *    };
  *  }
  * </pre>
  * <p>
@@ -191,6 +203,7 @@ public class WhitespaceAfterCheck
             TokenTypes.LITERAL_FOR,
             TokenTypes.LITERAL_FINALLY,
             TokenTypes.LITERAL_RETURN,
+            TokenTypes.LITERAL_YIELD,
             TokenTypes.LITERAL_CATCH,
             TokenTypes.DO_WHILE,
             TokenTypes.ELLIPSIS,
