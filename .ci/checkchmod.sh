3d702d53cc451ca92aec464231291892f2af6f78
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-05-22 19:49:07 +0530

    Issue #11637: Used Shellcheck to resolve SC2236 in Shell Script

--- .ci/checkchmod.sh
+++ .ci/checkchmod.sh
@@ -8,7 +8,7 @@ CHMOD=$(find -type f -not -path '*/\.git/*' \
           -a -type f -not -name '*.sh' \
           -a -type f -not -name '*.pl' \
           -a \( -type d -not -perm 775 -o -type f -executable \))
-if [[ ! -z $CHMOD ]]; then
+if [[ -n $CHMOD ]]; then
     echo "Expected mode for non '.sh' files is 664.";
     echo "Files that violates this rule:"
     for NAMEFILE in $CHMOD
@@ -20,7 +20,7 @@ fi
 
 # On Travis, after clone, all 'sh' files have executable bit
 CHMOD=$(find -type f -not -path '*/\.git/*' -a -type f -name '*.sh' -a -not -executable)
-if [[ ! -z $CHMOD ]]; then
+if [[ -n $CHMOD ]]; then
     echo "Expected mode for '.sh' files is 755.";
     echo "Files that violates this rule:"
     for NAMEFILE in $CHMOD
