fafcc2c0136db4d950e7edffcf23ed02de5a3042
Vyom-Yadav <jackhammervyom@gmail.com>
2022-05-26 23:45:42 +0530

    Issue #11335: Fix false negative in VariableDeclarationDistanceUsage

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/variabledeclarationusagedistance/InputVariableDeclarationUsageDistanceInitializationStatements.java
@@ -0,0 +1,90 @@
+/*
+VariableDeclarationUsageDistance
+allowedDistance = (default)3
+ignoreVariablePattern = (default)
+validateBetweenScopes = (default)false
+ignoreFinal = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.variabledeclarationusagedistance;
+
+public class InputVariableDeclarationUsageDistanceInitializationStatements {
+    class Test {
+        public Test test(Object a) {
+            Test obj = test("abc");
+            int var = 12; // violation
+            obj.test("abc");
+            obj.test("abc");
+            obj.test("abc");
+            int k = var + 3;
+            return this;
+        }
+
+        void test() {
+            Test obj = test("abc");
+            int var = 12; // ok
+            int var2 = 12; // violation
+            obj.test("abc");
+            obj.test("abc");
+            obj.test("abc");
+            obj.test(var);
+            new Test().test(var2);
+        }
+
+        void testChained() {
+            int pc = 0; // ok
+            test("abc").test("abc");
+            test("abc").test("abc");
+            test("abc").test("abc");
+            test("abc").test("abc");
+            test("abc").test("abc");
+            test("abc").test("abc");
+            test(pc).test("abc");
+        }
+
+        void testMixedBranches() {
+            int abc = 12; // violation
+            Test test = new Test();
+            test.test("abc");
+            test("abc").test("abc");
+            test("abc").test("abc");
+            test("abc").test("abc");
+            test.test("abc");
+            test("abc").test(abc);
+        }
+
+        void testMixedBranches2() {
+            int c = 12; // ok
+            Test test = new Test();
+            test.test("abc");
+            test.test(c);
+        }
+
+        void testTypecast() {
+            int c = 12; // ok
+            Test test = new Test();
+            test.test("abc");
+            ((subclass)test).test("abc");
+            test.test("abc");
+            ((subclass)test).test("abc");
+            test.test(c);
+        }
+
+        void testPriorToDeclaration() {
+            Test test2 = new Test();
+            test2.test("abc");
+            test2.test("abc");
+            Test test = new Test();
+            int c = 12;
+            test.test("abc");
+            test.test("abc");
+            test.test("abc");
+            test.test(c);
+        }
+
+        class subclass extends Test {
+        }
+    }
+}
