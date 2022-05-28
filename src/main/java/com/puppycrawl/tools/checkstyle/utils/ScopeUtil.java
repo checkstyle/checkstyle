b50e939b0064cbf1b58e2d231edb0b2ef58a510e
as23415672 <panshuyu0618@gmail.com>
2022-05-28 18:49:35 +0800

    Issue #11569: Fixed the false negative in the rule ClassMemberImpliedModifier

--- src/main/java/com/puppycrawl/tools/checkstyle/utils/ScopeUtil.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/utils/ScopeUtil.java
@@ -182,6 +182,31 @@ public final class ScopeUtil {
     }
 
     /**
+     * Returns whether a node is directly contained within an anonymous object block.
+     *
+     * @param node the node to check if directly contained within an anonymous object block.
+     * @return a {@code boolean} value
+     */
+    public static boolean isInObjBlock(DetailAST node) {
+        boolean returnValue = false;
+
+        // Loop up looking for a containing interface block
+        for (DetailAST token = node.getParent();
+             token != null && !returnValue;
+             token = token.getParent()) {
+            if (token.getType() == TokenTypes.OBJBLOCK
+                && token.getParent().getType() == TokenTypes.LITERAL_NEW) {
+                returnValue = true;
+            }
+            else if (TokenUtil.isOfType(token, TokenTypes.LITERAL_NEW)) {
+                break;
+            }
+        }
+
+        return returnValue;
+    }
+
+    /**
      * Returns whether a node is directly contained within an annotation block.
      *
      * @param node the node to check if directly contained within an annotation block.
