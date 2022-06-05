3454641bb9d844753963f35440e4d71ab6e4e582
Nick Mancuso <nicholas.mancuso13@gmail.com>
2022-06-05 10:43:00 -0400

    Issue #11277: update code base to have javadoc tag to explain noinspection content

--- src/main/java/com/puppycrawl/tools/checkstyle/StatelessCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/StatelessCheck.java
@@ -34,6 +34,7 @@ import java.lang.annotation.Target;
  * This also means, that all files will be processed by the same check instance.
  *
  * @noinspection AnnotationClass, ClassIndependentOfModule
+ * @noinspectionreason we support only Java 11+, we keep this annotation at top level by design
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
