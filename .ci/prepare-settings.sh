f83ced932debc02f33384c9c42c0e47a38a88740
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-06-15 01:10:16 +0530

    Issue #11726: Automate creation of settings.xml file for release

---
+++ .ci/prepare-settings.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+mkdir -p .ci-temp
+cp .ci/release-settings.xml .ci-temp/
+
+checkForVariable() {
+  VAR_NAME=$1
+  if [ -v "${!VAR_NAME}" ]; then
+    echo "Error: Define $1 environment variable"
+    exit 1
+  fi
+}
+
+checkForVariable "SONATYPE_USER"
+checkForVariable "SONATYPE_PWD"
+checkForVariable "GPG_PASSPHRASE"
+checkForVariable "GPG_KEY"
+
+replace() {
+  sed -i "s/$1/$2/g" .ci-temp/release-settings.xml
+}
+
+replace SONATYPE_USER
+replace SONATYPE_PWD
+replace GPG_PASSPHRASE
+replace GPG_KEY
+
+
+TEMP_SETTING="./.ci-temp/release-settings.xml"
+SETTING="~/.m2/settings.xml"
+
+if cmp -s "$TEMP_SETTING" "$SETTING"; then
+  date=$(date + "%y%m%d")
+  mv ~/.m2/settings.xml ~/.m2/settings.xml.backup."${date}"
+fi
+
+cp .ci-temp/release-settings.xml ~/.m2/settings.xml
