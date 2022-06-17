9364971add3320423a7928d8bfc38fa03bb2963c
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-06-17 19:16:37 +0530

    Issue #11729: Automate update of releasenotes.xml by content for new release

---
+++ .ci/releasenotes-gen-auto.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+source ./.ci/util.sh
+
+checkout_from https://github.com/checkstyle/contribution
+
+cd .ci-temp/contribution/releasenotes-builder
+mvn -e --no-transfer-progress clean compile package
+cd ../../../
+
+if [ -d .ci-temp/checkstyle ]; then
+  cd .ci-temp/checkstyle/
+  git reset --hard origin/master
+  git pull origin master
+  git fetch --tags
+  cd ../../
+else
+  cd .ci-temp/
+  git clone https://github.com/checkstyle/checkstyle
+  cd ../
+fi
+
+CS_RELEASE_VERSION="$(getCheckstylePomVersion)"
+echo CS_RELEASE_VERSION="$CS_RELEASE_VERSION"
+
+cd .ci-temp/checkstyle
+LATEST_RELEASE_TAG=$(git describe --tags --abbrev=0)
+echo LATEST_RELEASE_TAG="$LATEST_RELEASE_TAG"
+
+cd ../
+NOTES_DIR="contribution/releasenotes-builder/src/main/resources/com/github/checkstyle/templates/"
+
+java -jar contribution/releasenotes-builder/target/releasenotes-builder-1.0-all.jar \
+        -localRepoPath checkstyle \
+         -remoteRepoPath checkstyle/checkstyle \
+        -startRef "$LATEST_RELEASE_TAG" \
+         -releaseNumber "$CS_RELEASE_VERSION" \
+        -githubAuthToken "$READ_ONLY_TOKEN" \
+         -generateXdoc \
+        -xdocTemplate $NOTES_DIR/xdoc_freemarker.template \
+         -publishXdoc -publishXdocWithPush
+
+echo "releasenotes.xml after commit:"
+head "checkstyle/src/xdocs/releasenotes.xml" -n 100
