aadfa80a7c38e77014c17399f1144bad4f12fe65
Nick Mancuso <nicholas.mancuso13@gmail.com>
2022-06-19 09:25:51 -0400

    Issue #11277: update code base to have javadoc tag to explain noinspection content(JUnitTestMethodWithNoAssertions, ClassIndependentOfModule, unchecked, ProhibitedExceptionThrown)

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/AllTestsTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/AllTestsTest.java
@@ -40,6 +40,8 @@ import org.junit.jupiter.api.Test;
  * AllTestsTest.
  *
  * @noinspection ClassIndependentOfModule
+ * @noinspectionreason ClassIndependentOfModule - architecture of
+ *      test modules requires this structure
  */
 public class AllTestsTest {
 
