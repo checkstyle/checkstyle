3c9f680465dd7c18ddeca01bec521feace50412b
Rahulkhinchi03 <rahulkhinchirk7@gmail.com>
2022-06-27 20:32:16 +0530

    Issue #11783: prepare-settings.sh fails to create settings.xml

--- .ci/prepare-settings.sh
+++ .ci/prepare-settings.sh
@@ -21,18 +21,23 @@ replace() {
   sed -i "s/$1/$2/g" .ci-temp/release-settings.xml
 }
 
-replace SONATYPE_USER
-replace SONATYPE_PWD
-replace GPG_PASSPHRASE
-replace GPG_KEY
+replace SONATYPE_USER "$SONATYPE_USER"
+replace SONATYPE_PWD "$SONATYPE_PWD"
+replace GPG_PASSPHRASE "$GPG_PASSPHRASE"
+replace GPG_KEY "$GPG_KEY"
 
 
-TEMP_SETTING="./.ci-temp/release-settings.xml"
-SETTING="~/.m2/settings.xml"
+TEMP_SETTING=./.ci-temp/release-settings.xml
+SETTING=~/.m2/settings.xml
 
-if cmp -s "$TEMP_SETTING" "$SETTING"; then
-  TODAY=$(date + "%y%m%d")
-  mv $SETTING $SETTING.backup."${TODAY}"
+if [ -f "$SETTING" ]; then
+  echo "file $SETTING already exists"
+  if ! cmp -s "$TEMP_SETTING" "$SETTING"; then
+    NOW=$( date '+%Y%m%d%H%M%S' )
+    echo "backup $SETTING to $SETTING.backup.${NOW}"
+    mv "$SETTING" "$SETTING.backup.${NOW}"
+  else
+    echo "no backup is required as content is same"
+  fi
 fi
-
-cp $TEMP_SETTING $SETTING
+cp "$TEMP_SETTING" "$SETTING"
