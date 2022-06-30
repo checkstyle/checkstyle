e208f0d9c331c8006f0d0bcf0e9e55dedab640ef
Vyom-Yadav <jackhammervyom@gmail.com>
2022-06-30 07:32:01 +0530

    Issue #11720: Kill surviving mutation in RequireThisCheck related to getting block-definition for the code

--- .ci/pitest-survival-check-html.sh
+++ .ci/pitest-survival-check-html.sh
@@ -193,7 +193,6 @@ pitest-coding-2)
 pitest-coding-require-this-check)
   declare -a ignoredItems=(
   "RequireThisCheck.java.html:<td class='covered'><pre><span  class='survived'>                        &#38;&#38; ast.getParent().getType() != TokenTypes.LITERAL_CATCH) {</span></pre></td></tr>"
-  "RequireThisCheck.java.html:<td class='covered'><pre><span  class='survived'>               &#38;&#38; parent.getType() != TokenTypes.CTOR_DEF</span></pre></td></tr>"
   "RequireThisCheck.java.html:<td class='covered'><pre><span  class='survived'>            &#38;&#38; lastChild.getType() == TokenTypes.OBJBLOCK;</span></pre></td></tr>"
   "RequireThisCheck.java.html:<td class='covered'><pre><span  class='survived'>        if (variableDeclarationFrame.getType() == FrameType.CLASS_FRAME) {</span></pre></td></tr>"
   "RequireThisCheck.java.html:<td class='covered'><pre><span  class='survived'>        if (fieldUsageFrame.getType() == FrameType.BLOCK_FRAME) {</span></pre></td></tr>"
