e7c75fdbb2a1e598d6b113aef9f41ae8cf683790
Roman Ivanov <ivanov-jr@mail.ru>
2022-07-02 19:58:19 -0700

    Issue #11784: fix bash scripts to detect not set variable properly

--- .ci/releasenotes-gen-xdoc-push.sh
+++ .ci/releasenotes-gen-xdoc-push.sh
@@ -5,7 +5,7 @@ source ./.ci/util.sh
 
 checkForVariable() {
   VAR_NAME=$1
-  if [ -v "${!VAR_NAME}" ]; then
+  if [ ! -v "$VAR_NAME" ]; then
     echo "Error: Define $1 environment variable"
     exit 1
   fi
