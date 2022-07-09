5178f730e8ffb4f6fb1b5f22b991c132625f3aef
stoyanK7 <stoyank127@gmail.com>
2022-07-09 15:55:17 +0300

    Issue #7064: Add influenceFormat property to SuppressWithPlainTextCommentFilter

--- src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithplaintextcommentfilter/InputSuppressWithPlainTextCommentFilterWithDefaultCfg.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithplaintextcommentfilter/InputSuppressWithPlainTextCommentFilterWithDefaultCfg.java
@@ -1,10 +1,12 @@
 /*
 SuppressWithPlainTextCommentFilter
+areaCommentFormat = (default)SUPPRESS CHECKSTYLE (\w+)
 offCommentFormat = (default)// CHECKSTYLE:OFF
 onCommentFormat = (default)// CHECKSTYLE:ON
 checkFormat = (default).*
 messageFormat = (default)(null)
 idFormat = (default)(null)
+influenceFormat = (default)0
 
 com.puppycrawl.tools.checkstyle.checks.whitespace.FileTabCharacterCheck
 eachLine = true
@@ -27,4 +29,20 @@ public class InputSuppressWithPlainTextCommentFilterWithDefaultCfg {
     // filtered violation below 'Line contains a tab character'
 	private int a; // CHECKSTYLE:OFF
     // CHECKSTYLE:ON
+
+    // filtered violation below 'Line contains a tab character'
+	private int b; // SUPPRESS CHECKSTYLE FileTabCharacterCheck
+
+    // filtered violation below 'Line contains a tab character'
+	private int c;  /* SUPPRESS CHECKSTYLE FileTabCharacterCheck */
+	/* SUPPRESS CHECKSTYLE FileTabCharacterCheck */ private int A3;
+    // filtered violation above 'Line contains a tab character'
+
+    // violation below 'Line contains a tab character'
+	private int d;
+    // SUPPRESS CHECKSTYLE FileTabCharacterCheck
+
+    // SUPPRESS CHECKSTYLE FileTabCharacterCheck
+	private int e;
+    // violation above 'Line contains a tab character'
 }
