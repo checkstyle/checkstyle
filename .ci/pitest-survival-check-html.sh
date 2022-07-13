6b951008c5fab44b17d9bb62c96254d4d9cdd73d
Vyom-Yadav <jackhammervyom@gmail.com>
2022-07-13 17:23:41 +0530

    Issue #11720: Kill surviving mutation in CheckstyleAntTask related to logging

--- .ci/pitest-survival-check-html.sh
+++ .ci/pitest-survival-check-html.sh
@@ -109,7 +109,6 @@ pitest-ant)
   "CheckstyleAntTask.java.html:<td class='covered'><pre><span  class='survived'>            if (toFile == null || !useFile) {</span></pre></td></tr>"
   "CheckstyleAntTask.java.html:<td class='covered'><pre><span  class='survived'>            if (toFile == null || !useFile) {</span></pre></td></tr>"
   "CheckstyleAntTask.java.html:<td class='covered'><pre><span  class='survived'>            log(&#34;To process the files took &#34; + (processingEndTime - processingStartTime)</span></pre></td></tr>"
-  "CheckstyleAntTask.java.html:<td class='covered'><pre><span  class='survived'>            log(&#34;Total execution took &#34; + (endTime - startTime) + TIME_SUFFIX,</span></pre></td></tr>"
   "CheckstyleAntTask.java.html:<td class='covered'><pre><span  class='survived'>        log(&#34;To locate the files took &#34; + (endTime - startTime) + TIME_SUFFIX,</span></pre></td></tr>"
   );
   checkPitestReport ignoredItems
