9ddb7b96804f761055ef19bd82b08924e72b124d
Rahulkhinchi03 <rahulkhinchirk7@gmail.com>
2022-07-26 23:57:37 +0530

    Issue #11900: created Github action that does version bump and update Milestone ID

---
+++ .ci/bump-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+VERSION=$1
+echo VERSION="$VERSION"
+
+echo "bump version in pom.xml"
+mvn -e --no-transfer-progress versions:set -DnewVersion="$VERSION"
+mvn -e --no-transfer-progress versions:commit
