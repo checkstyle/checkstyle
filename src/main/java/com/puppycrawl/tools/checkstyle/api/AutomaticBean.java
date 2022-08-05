74121adc86c229750feaac5a10649c60895b3540
Vyom-Yadav <jackhammervyom@gmail.com>
2022-08-05 11:32:55 +0530

    Issue #12017: Kill surviving mutations in AutomaticBean related to trimming strings

--- src/main/java/com/puppycrawl/tools/checkstyle/api/AutomaticBean.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/AutomaticBean.java
@@ -391,12 +391,12 @@ public abstract class AutomaticBean
         public Object convert(Class type, Object value) {
             // Converts to a String and trims it for the tokenizer.
             final StringTokenizer tokenizer = new StringTokenizer(
-                value.toString().trim(), COMMA_SEPARATOR);
+                value.toString(), COMMA_SEPARATOR);
             final List<AccessModifierOption> result = new ArrayList<>();
 
             while (tokenizer.hasMoreTokens()) {
                 final String token = tokenizer.nextToken();
-                result.add(AccessModifierOption.getInstance(token.trim()));
+                result.add(AccessModifierOption.getInstance(token));
             }
 
             return result.toArray(EMPTY_MODIFIER_ARRAY);
