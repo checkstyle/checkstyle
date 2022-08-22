ab8fe97effed5d12f0097869604df897e0e7edd1
Austin Arbor <aarbor989@gmail.com>
2022-08-21 23:57:10 -0400

    Issue #3770: extending OverloadMethodsDeclarationOrderCheck to support user defined grouping by method modifiers

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/overloadmethodsdeclarationorder/InputOverloadMethodsDeclarationOrder4.java
@@ -0,0 +1,32 @@
+/*
+OverloadMethodsDeclarationOrder
+modifierGroups = static, public|protected|package
+
+
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.overloadmethodsdeclarationorder;
+
+import java.nio.file.Path;
+
+class InputOverloadMethodsDeclarationOrder4 {
+    public void foo() { } // ok
+    protected void foo(String baz) { } // ok
+    void foo(int a) { } // ok
+    void bar() {} // ok
+    static void foo(byte b) { } // ok
+    static void bar(int a) { } // ok
+    private void foo(String s, int i) { } // ok
+}
+
+class InputOverloadMethodsDeclarationOrder4_2 {
+    static private void foo(long b) { } // ok
+    void foo() { } // ok
+    public void foo(String baz) { } // ok
+    private void foo(String s, int i) { } // ok
+    protected void foo(byte b, int i) { } // violation
+    private static void foo(int a) { } // violation
+    protected static void foo(int a, Path p) { } // violation
+    static void foo(long l, int i) { } // violation
+    private void foo(byte b) { } // violation
+}
