1468c1612186f25e4b45e534cefc3c78d9b7beb0
Rahul Khinchi <rahulkhinchirk7@gmail.com>
2022-08-28 22:34:47 +0530

    Issue #12124: Minor changes in in shell scripts

--- .ci/publish-releasenotes-githubpage.sh
+++ .ci/publish-releasenotes-githubpage.sh
@@ -15,10 +15,5 @@ CURRENT_VERSION=$(xmlstarlet sel -N pom=http://maven.apache.org/POM/4.0.0 \
                              -t -m pom:project -v pom:version pom.xml | sed "s/-SNAPSHOT//")
 echo CURRENT_VERSION="$CURRENT_VERSION"
 
-if [ "$TARGET_VERSION" != "$CURRENT_VERSION" ]; then
-  echo "[ERROR] Target Version and Current Version doesn't match."
-  exit 1;
-fi
-
 # Update GitHub Release Page
 ./.ci/update-github-page.sh "$TARGET_VERSION"
