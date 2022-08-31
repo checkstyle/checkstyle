7fccc473c2a30092242d60ed32840e5e653f279a
Drew Mitchell <drew@drewm.com>
2022-08-30 22:31:07 -0400

    Issue #8016: Add allowMissingBuilderJavadoc

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/missingjavadocmethod/InputMissingJavadocMethodBuilder3.java
@@ -0,0 +1,22 @@
+/*
+MissingJavadocMethod
+minLineCount = (default)-1
+allowedAnnotations = (default)Override
+scope = private
+excludeScope = (default)null
+allowMissingPropertyJavadoc = (default)false
+allowMissingBuilderJavadoc = true
+ignoreMethodNamesRegex = (default)null
+tokens = (default)METHOD_DEF, CTOR_DEF, ANNOTATION_FIELD_DEF, COMPACT_CTOR_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.missingjavadocmethod;
+
+public interface InputMissingJavadocMethodBuilder3
+{
+    InputMissingJavadocMethodBuilder3 withField(String field); // violation
+
+    InputMissingJavadocMethodBuilder3 setField(String field); // violation
+}
