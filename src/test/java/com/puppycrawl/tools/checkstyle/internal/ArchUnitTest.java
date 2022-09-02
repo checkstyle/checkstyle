36429fa898f06eb6bdb662d432b0517785d44e1e
Vyom-Yadav <jackhammervyom@gmail.com>
2022-09-02 20:47:00 +0530

    Issue #12099: Add ArchUnit test to ensure classes in api package are not dependent on classes present in utils package

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/ArchUnitTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/ArchUnitTest.java
@@ -19,11 +19,18 @@
 
 package com.puppycrawl.tools.checkstyle.internal;
 
+import static com.tngtech.archunit.base.DescribedPredicate.not;
+import static com.tngtech.archunit.lang.conditions.ArchPredicates.are;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.domain.JavaModifier;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
@@ -31,6 +38,20 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.lang.ArchRule;
 
 public class ArchUnitTest {
+
+    /**
+     * List of classes that don't follow the rule
+     * {@code noClassesInApiShouldDependOnClassesInUtil}.
+     */
+    private static final Set<String> API_CLASSES_DEPENDENT_ON_UTILS = Set.of(
+        "com.puppycrawl.tools.checkstyle.api.FileText",
+        "com.puppycrawl.tools.checkstyle.api.AbstractCheck",
+        "com.puppycrawl.tools.checkstyle.api.AbstractFileSetCheck",
+        "com.puppycrawl.tools.checkstyle.api.AutomaticBean$PatternConverter",
+        "com.puppycrawl.tools.checkstyle.api.AutomaticBean$RelaxedStringArrayConverter",
+        "com.puppycrawl.tools.checkstyle.api.AutomaticBean$UriConverter",
+        "com.puppycrawl.tools.checkstyle.api.FileContents"
+    );
 
     @BeforeAll
     public static void init() {
@@ -73,5 +94,36 @@ public class ArchUnitTest {
                 .should().notBeProtected();
 
         checkMethodsShouldNotBeProtectedRule.check(importedClasses);
+    }
+
+    /**
+     * The goal is to ensure all classes in api package are not dependent on classes in util
+     * packages.
+     *
+     * @noinspection JUnitTestMethodWithNoAssertions
+     * @noinspectionreason JUnitTestMethodWithNoAssertions - asserts in callstack,
+     *     but not in this method
+     */
+    @Test
+    public void testClassesInApiDoNotDependOnClassesInUtil() {
+        final JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.puppycrawl.tools.checkstyle.api");
+
+        final DescribedPredicate<JavaClass> suppressed = new DescribedPredicate<>("suppressed") {
+            @Override
+            public boolean apply(JavaClass input) {
+                return API_CLASSES_DEPENDENT_ON_UTILS.contains(input.getFullName());
+            }
+        };
+
+        final ArchRule noClassesInApiShouldDependOnClassesInUtil = noClasses()
+            .that(are(not(suppressed)))
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("com.puppycrawl.tools.checkstyle.utils",
+                                "com.puppycrawl.tools.checkstyle.checks.javadoc.utils");
+
+        noClassesInApiShouldDependOnClassesInUtil.check(importedClasses);
     }
 }
