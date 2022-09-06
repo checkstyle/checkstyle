212e74e77167f9611b9da6ccadacd252f78bc4d8
Rahul Khinchi <rahulkhinchirk7@gmail.com>
2022-09-06 22:19:42 +0530

    Issue #12168: failure of update github.io to clone checkstyle.github.io

--- .ci/update-github-io.sh
+++ .ci/update-github-io.sh
@@ -18,8 +18,4 @@ git checkout "checkstyle-$TARGET_VERSION"
 echo "Generating web site"
 mvn -e --no-transfer-progress site -Pno-validations -Dmaven.javadoc.skip=false
 
-echo "Clone by ssh only to avoid passwords on push ..."
-checkout_from git@github.com:checkstyle/checkstyle.github.io.git
-cd .ci-temp/checkstyle.github.io
-git rm -rf *
-git checkout HEAD -- CNAME
+mkdir .ci-temp
