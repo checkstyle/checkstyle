4a4862b7595063dcf9ec85387d7ae566f7489793
stoyanK7 <stoyank127@gmail.com>
2022-09-06 22:15:18 +0200

    Issue #11939: New filter SuppressWithNearbyTextFilter

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/InputSuppressWithNearbyTextFilterDefaultConfig.java
@@ -0,0 +1,48 @@
+/*
+SuppressWithNearbyTextFilter
+nearbyTextPattern = (default)SUPPRESS CHECKSTYLE (\\w+)
+checkPattern = (default).*
+messagePattern = (default)(null)
+idPattern = (default)(null)
+lineRange = (default)0
+
+com.puppycrawl.tools.checkstyle.checks.sizes.LineLengthCheck
+fileExtensions = (default)all files
+ignorePattern = (default)^(package|import) .*
+max = 90
+
+com.puppycrawl.tools.checkstyle.checks.naming.ConstantNameCheck
+format = (default)^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$
+applyToPublic = (default)true
+applyToProtected = (default)true
+applyToPackage = (default)true
+applyToPrivate = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.filters.suppresswithnearbytextfilter;
+
+public class InputSuppressWithNearbyTextFilterDefaultConfig {
+
+    // violation below 'Line is longer than 90 characters (found 94).'
+    void someMethodWithReaaaaaaaaaaaalyLoooooooooooooooooongNaaaaaaaaaaaaaaaaaaaaaaaaaaame() {
+        // filtered violation below 'Line is longer than 90 characters (found 97).'
+        for (int longName = 0; longName < 5; longName++) { // SUPPRESS CHECKSTYLE LineLengthCheck
+            System.out.println(longName);
+            if (longName == 4) {
+                break;
+            }
+        }
+    }
+
+    void method() {
+        // filtered violation below 'Line is longer than 90 characters (found 94).'
+        /* SUPPRESS CHECKSTYLE LineLengthCheck */ int ccccccccccccccccccccccccccccccccccc = 3;
+    }
+
+    final static int badConstant = 5; // violation '.*'badConstant' must match pattern.*'
+
+    // filtered violation below '.*'badConstant1' must match pattern.*'
+    final static int badConstant1 = 5; /* SUPPRESS CHECKSTYLE ConstantNameCheck */
+}
