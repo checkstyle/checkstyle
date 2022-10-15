4c8dae495024f62230e0cc69723fc33509afef52
shubh <shubh22.srivastava@gmail.com>
2022-10-15 16:16:21 +0530

    Issue #11446: Update WriteTagCheckTest to use verifyWithInlineConfigParser

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/InputWriteTag.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/InputWriteTag.java
@@ -1,7 +1,7 @@
 /*
 WriteTag
 tag = @author
-tagFormat = \S
+tagFormat = \\S
 tagSeverity = (default)info
 tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
 
@@ -9,10 +9,10 @@ tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
 */
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.writetag;
-
+// violation 3 lines below , 'Javadoc tag .*'
 /**
  * Testing tag writing
- * @author Daniel Grenner // violation
+ * @author Daniel Grenner
  * @incomplete This class needs more code...
  * @doubletag first text
  * @doubletag second text
