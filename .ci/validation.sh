00f78768a8e7b0882ceabeb0d721750caf82b19f
rnveach <rveach02@gmail.com>
2022-10-15 13:51:44 -0400

    minor: test 2

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -330,12 +330,12 @@ verify-no-exception-configs)
     --no-clobber \
     https://raw.githubusercontent.com/checkstyle/contribution/master/checkstyle-tester/checks-only-javadoc-error.xml
   MODULES_WITH_EXTERNAL_FILES="Filter|ImportControl"
-  xmlstarlet fo -D \
+  xmlstarlet -D \
     | xmlstarlet sel --net --template -m .//module -v "@name" \
     -n $working_dir/checks-nonjavadoc-error.xml -n $working_dir/checks-only-javadoc-error.xml \
     | grep -vE $MODULES_WITH_EXTERNAL_FILES | grep -v "^$" \
     | sort | uniq | sed "s/Check$//" > $working_dir/web.txt
-  xmlstarlet fo -D \
+  xmlstarlet -D \
     | xmlstarlet sel --net --template -m .//module -v "@name" -n config/checkstyle_checks.xml \
     | grep -vE $MODULES_WITH_EXTERNAL_FILES | grep -v "^$" \
     | sort | uniq | sed "s/Check$//" > $working_dir/file.txt
