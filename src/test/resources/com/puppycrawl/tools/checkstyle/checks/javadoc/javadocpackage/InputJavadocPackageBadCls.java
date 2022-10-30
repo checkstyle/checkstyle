dd45f7b7eed73db3a465b927327c3d880ac098f3
shubh <shubh22.srivastava@gmail.com>
2022-10-30 12:10:14 +0530

    Issue #11214: Specified violation messages for JavadocPackageCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocpackage/InputJavadocPackageBadCls.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocpackage/InputJavadocPackageBadCls.java
@@ -1,9 +1,12 @@
-package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocpackage;      // violation
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocpackage;
+/*
+allowLegacy = "false"
+fileExtensions = "java"
 
-/* Config:
- * allowLegacy = "false"
- * fileExtensions = "java"
- */
+
+*/
+// violation 7 lines above 'Missing package-info.java file'
+
 class InputJavadocPackageBadCls {
     class X extends Exception {}
     void m() throws X {}
