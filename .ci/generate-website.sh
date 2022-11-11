c34f441b7d2d6d6ac4bf1aa48930993db53bf631
stoyanK7 <stoyank127@gmail.com>
2022-11-11 21:25:19 +0100

    Issue #12168: failure of update github.io to clone checkstyle.github.io

--- .ci/generate-website.sh
+++ .ci/generate-website.sh
@@ -6,7 +6,7 @@ source ./.ci/util.sh
 
 if [[ -z $1 ]]; then
   echo "version is not set"
-  echo "Usage: update-github-io.sh <version>"
+  echo "Usage: generate-website.sh <version>"
   exit 1
 fi
 
@@ -18,8 +18,4 @@ git checkout "checkstyle-$TARGET_VERSION"
 echo "Generating web site"
 mvn -e --no-transfer-progress site -Pno-validations -Dmaven.javadoc.skip=false
 
-echo "Clone by ssh only to avoid passwords on push ..."
-checkout_from git@github.com:checkstyle/checkstyle.github.io.git
-cd .ci-temp/checkstyle.github.io
-git rm -rf *
-git checkout HEAD -- CNAME
+git checkout origin/master
