3f4a51cd59217fe1f9af6d23aaf99d923c6f8e05
stoyanK7 <stoyank127@gmail.com>
2022-11-11 12:26:53 +0100

    Issue #12181: Fail 'Publish GitHub page' when curl fails

--- .ci/update-github-page.sh
+++ .ci/update-github-page.sh
@@ -89,7 +89,7 @@ EOF
 echo "$JSON"
 
 echo "Updating Github tag page"
-curl \
+curl --fail-with-body \
   -X PATCH https://api.github.com/repos/checkstyle/checkstyle/releases/"$RELEASE_ID" \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: token $BUILDER_GITHUB_TOKEN" \
