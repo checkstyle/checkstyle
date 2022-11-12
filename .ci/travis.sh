0fd3a947c50a8de01561ee6adbc87306165d2c82
rnveach <rveach02@gmail.com>
2022-11-12 13:11:46 -0500

    minor: test display

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -12,7 +12,12 @@ init-m2-repo)
   if [[ $RUN_JOB == 1 ]]; then
     MVN_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout);
     echo "Maven Repo Located At: " "$MVN_REPO"
+    echo "Travis Home Located At: " "${TRAVIS_HOME}"
+    mvn -e  --no-transfer-progress -B help:evaluate -X
+    cat /opt/mvn/conf/settings.xml
     MVN_SETTINGS=${TRAVIS_HOME}/.m2/settings.xml
+    cat $MVN_SETTINGS
+    cat /home/travis/.m2/settings.xml
     if [[ -f ${MVN_SETTINGS} ]]; then
       if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         sed -i'' -e "/<mirrors>/,/<\/mirrors>/ d" "$MVN_SETTINGS"
