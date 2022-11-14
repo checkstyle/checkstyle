4b685bbb3cfea2dc5fa66d5cf02ca1d31f8e83e8
Shubh Srivastava <shubh22.srivastava@gmail.com>
2022-11-14 15:15:33 +0530

    Issue #12326: Resolve Pitest suppression for AccessModifierOption

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/accessmodifieroption/InputAccessModifierOption.java
@@ -0,0 +1,32 @@
+/*
+AccessModifierOption
+option = \tSPACE
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.naming.accessmodifieroption;
+
+public class InputAccessModifierOption {
+
+    class classOne {
+
+    }
+
+    class classTwo {
+
+    }
+
+    class classThree {
+
+    }
+
+    public void method1() {
+
+    }
+
+    public void method2() {
+
+    }
+
+}
