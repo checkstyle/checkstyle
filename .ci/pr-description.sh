f1275666a544f4d3c30f1e7b7c3ac72565278da9
Nick Mancuso <nicholas.mancuso13@gmail.com>
2022-11-23 09:15:44 -0500

    infra: remove --fail-with-body from pr-description.sh

--- .ci/pr-description.sh
+++ .ci/pr-description.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ ! $PULL_REQUEST =~ ^([0-9]*)$ ]]; then exit 0; fi
 LINK_COMMITS=https://api.github.com/repos/checkstyle/checkstyle/pulls/$PULL_REQUEST/commits
-COMMITS=$(curl --fail-with-body -s -H "Authorization: token $READ_ONLY_TOKEN" "$LINK_COMMITS" \
+COMMITS=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" "$LINK_COMMITS" \
              | jq '.[0] | .commit.message')
 echo 'Commit messages from github: '"${COMMITS:0:60}"...
 ISSUE_NUMBER=$(echo "$COMMITS" | sed -e 's/^.*Issue //' | sed -e 's/:.*//')
@@ -13,7 +13,7 @@ if [[ $ISSUE_NUMBER =~ ^#[0-9]+$ ]]; then
     LINK_PR=https://api.github.com/repos/checkstyle/checkstyle/pulls/$PULL_REQUEST
     LINK_ISSUE=https://api.github.com/repos/checkstyle/checkstyle/issues/${ISSUE_NUMBER:1}
     REGEXP=($ISSUE_NUMBER\|https://github.com/checkstyle/checkstyle/issues/${ISSUE_NUMBER:1})
-    PR_DESC=$(curl --fail-with-body -s -H "Authorization: token $READ_ONLY_TOKEN" "$LINK_PR" \
+    PR_DESC=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" "$LINK_PR" \
                 | jq '.body' | grep -E "$REGEXP" | cat )
     echo 'PR Description grepped:'"${PR_DESC:0:80}"
     if [[ -z $PR_DESC ]]; then
@@ -21,7 +21,7 @@ if [[ $ISSUE_NUMBER =~ ^#[0-9]+$ ]]; then
          echo 'this will bind the Issue to your PR in Github'
          RESULT=1;
        fi
-    LABEL_APRV=$(curl --fail-with-body -s -H "Authorization: token $READ_ONLY_TOKEN" "$LINK_ISSUE" \
+    LABEL_APRV=$(curl -s -H "Authorization: token $READ_ONLY_TOKEN" "$LINK_ISSUE" \
                    | jq '.labels [] | .name' | grep approved | cat | wc -l )
     if [[ $LABEL_APRV == 0 ]]; then
          echo 'You are providing a PR for an Issue that is not approved yet,'
