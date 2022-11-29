690031b86a95b75e54ef8618ad2ebb70cb454147
Arin Modi <arinmodi2306@gmail.com>
2022-11-29 08:54:48 +0530

    Issue #12409: Inconsistent allowedAbbreviations

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/abbreviationaswordinname/InputAbbreviationAsWordInNameType7.java
@@ -0,0 +1,28 @@
+/*
+AbbreviationAsWordInName
+allowedAbbreviations = ORDER, OBSERVATION, UNDERSCORE, TEST
+
+
+*/
+
+
+package com.puppycrawl.tools.checkstyle.checks.naming.abbreviationaswordinname;
+
+public class InputAbbreviationAsWordInNameType7 {
+    void getTEST() {
+    }
+
+    void getORDER_OBSERVATION() {}
+
+    void getUNDERSCORE() {}
+
+    void getTEST_OBSERVATION() {}
+
+    void getTEST_UNDERSCORE() {}
+
+    void getORDER() {}
+
+    void getOBSERVATION() {}
+
+    void getORDER_UNDERSCORE() {}
+}
