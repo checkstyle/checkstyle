3c98c6d84e874af816ab66561794e2dbafede3a0
Austin Arbor <aarbor989@gmail.com>
2022-11-29 22:28:15 -0500

    Issue #3770: extending OverloadMethodsDeclarationOrderCheck to support user defined grouping by method modifiers

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/overloadmethodsdeclarationorder/InputOverloadMethodsDeclarationOrder2.java
@@ -0,0 +1,99 @@
+/*
+OverloadMethodsDeclarationOrder
+modifierGroups = .*
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.overloadmethodsdeclarationorder;
+
+class InputOverloadMethodsDeclarationOrder2
+{
+    public void overloadMethod(int i) { } // ok
+
+    public void overloadMethod(String s) { } // ok
+
+    public void overloadMethod(boolean b) { } // ok
+
+    public void fooMethod() { } // ok
+
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    public void overloadMethod(String s, Boolean b, int i) {
+
+    }
+
+    InputOverloadMethodsDeclarationOrder anonymous = new InputOverloadMethodsDeclarationOrder()
+    {
+        public void overloadMethod(int i) { } // ok
+
+        public void overloadMethod(String s) { } // ok
+
+        public void overloadMethod(boolean b) { } // ok
+
+        public void fooMethod() { } // ok
+
+        // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+        public void overloadMethod(String s, Boolean b, int i)
+        {
+            //some foo code
+        }
+    };
+}
+
+interface FooableBackwardsCompatible
+{
+    public abstract void foo(int i); // ok
+    public abstract void foo(String s); // ok
+    public abstract void noFoo(); // ok
+    public abstract void foo(String s, Boolean b, int i);
+    // violation above 'All overloaded methods.*Previous overloaded method located at.*'
+}
+
+enum FooTypeBackwardsCompatible {
+    Strategy(""),
+    Shooter(""),
+    RPG("");
+
+    private String description;
+
+    private FooTypeBackwardsCompatible(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() { // ok
+        return description;
+    }
+
+    public void setDescription(String description) { // ok
+        this.description = description;
+    }
+
+    public void overloadMethod(int i) { } // ok
+
+    public void overloadMethod(String s) { } // ok
+
+    public void overloadMethod(boolean b) { } // ok
+
+    public void fooMethod() { } // ok
+
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    public void overloadMethod(String s, Boolean b, int i)
+    {
+        //some foo code
+    }
+}
+
+enum Foo2BackwardsCompatible {
+    VALUE {
+        public void value() {
+            value("");
+        } // ok
+
+        public void middle() { } // ok
+
+        public void value(String s) { } // ok
+    };
+}
+
+@interface ClassPreambleBackwardsCompatible {
+    String author(); // ok
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/overloadmethodsdeclarationorder/InputOverloadMethodsDeclarationOrder6.java
@@ -0,0 +1,105 @@
+/*
+OverloadMethodsDeclarationOrder
+modifierGroups = ^public .*, (protected|package), private, ^static$
+
+
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.overloadmethodsdeclarationorder;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+class InputOverloadMethodsDeclarationOrder6 {
+    public void foo() { } // ok
+    private void bar(String baz) { } // ok
+    private void foo(String baz) { } // ok
+    void bar() { } // ok
+    static protected final void foo(int a) { } // ok
+}
+
+class InputOverloadMethodsDeclarationOrder6_2 {
+    static private void foo(long b) { } // ok
+    void foo() { } // ok
+    void foo(String baz) { } // ok
+    void bar() { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    static void foo(int a) { }
+}
+
+class InputOverloadMethodsDeclarationOrder6_3 {
+    public void foo(long b) { } // ok
+    public void bar(String baz) { } // ok
+    private void foo(String baz) { } // ok
+    public static void foo(byte b) { } // ok
+    void bar() { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    static public void foo(int a) { }
+}
+
+class InputOverloadMethodsDeclarationOrder6_4 {
+    void foo() { } // ok
+    void foo(String baz) { } // ok
+    void bar() { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    static void foo(int a) { }
+}
+
+class InputOverloadMethodsDeclarationOrder6_5 {
+    private void foo() { } // ok
+    void bar() { } // ok
+    public void foo(String baz) { } // ok
+    public void foo(CharSequence baz) { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    private static void foo(int i) { }
+    private final void foo(long i) { } // ok
+}
+
+abstract class InputOverloadMethodsDeclarationOrder6_6 {
+    protected void foo(double d) { } // ok
+    void bar() { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    protected abstract void foo();
+    void foo(int i) { } // ok
+    public final void foo(long b) { } // ok
+}
+
+abstract class InputOverloadMethodsDeclarationOrder6_7 {
+    private void foo(char c) { } // ok
+    void bar() { } // ok
+    static void foo(double d) { } // ok
+    static final int foo() {return  0;} // ok
+    final void foo(String baz) { } // ok
+    final public void foo(UUID baz) { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    private void foo(int i) { }
+    public final void foo(long i) { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    abstract void foo(Error error);
+}
+
+abstract class InputOverloadMethodsDeclarationOrder6_8 {
+    private void foo(char c) { } // ok
+    void bar() { } // ok
+    static void foo(long b) { } // ok
+    static final void foo(float f) { } // ok
+    final void foo(String baz) { } // ok
+    public void foo(Path baz) { } // ok
+    public @Deprecated void foo(byte b) { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    private void foo(int i) { }
+    public @Deprecated final void foo(List<Long> list) { } // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    abstract void foo(Map<Integer, Long> map);
+}
+
+interface InputOverloadMethodsDeclarationOrder6_9 {
+    void foo(); // ok
+    abstract void foo(int i); // ok
+    void bar(); // ok
+    // violation below 'All overloaded methods.*Previous overloaded method located at.*'
+    public abstract void foo(long l);
+    abstract public void foo(String baz); // ok
+}
