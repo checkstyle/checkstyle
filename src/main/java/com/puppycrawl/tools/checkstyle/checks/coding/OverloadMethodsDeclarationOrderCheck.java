5ee9c50b84836bf458f7aca0bab82057c6e4fb38
Austin Arbor <aarbor989@gmail.com>
2022-11-29 22:28:15 -0500

    Issue #3770: extending OverloadMethodsDeclarationOrderCheck to support user defined grouping by method modifiers

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/OverloadMethodsDeclarationOrderCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/OverloadMethodsDeclarationOrderCheck.java
@@ -20,12 +20,20 @@
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.puppycrawl.tools.checkstyle.StatelessCheck;
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.Scope;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+import com.puppycrawl.tools.checkstyle.utils.ScopeUtil;
 import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
 
 /**
@@ -34,31 +42,85 @@ import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
  * name but different signatures where the signature can differ by the number of
  * input parameters or type of input parameters or both.
  * </p>
+ * <ul>
+ * <li>
+ * Property {@code modifierGroups} - control how different types of overloaded methods
+ * can be grouped by their modifiers.
+ * Type is {@code java.util.regex.Pattern[]}.
+ * Default value is {@code ""}.
+ * </li>
+ * </ul>
  * <p>
  * To configure the check:
  * </p>
  * <pre>
- * &lt;module name=&quot;OverloadMethodsDeclarationOrder&quot;/&gt;
- * </pre>
- * <p>
- * Example of correct grouping of overloaded methods:
- * </p>
- * <pre>
- * public void foo(int i) {}
- * public void foo(String s) {}
- * public void foo(String s, int i) {}
- * public void foo(int i, String s) {}
- * public void notFoo() {}
+ * &lt;module name="OverloadMethodsDeclarationOrder"/&gt;
  * </pre>
  * <p>
  * Example of incorrect grouping of overloaded methods:
  * </p>
  * <pre>
+ * public void foo() {} // OK
+ * public final void foo(int i) {} // OK
+ * void notFoo() {} // OK
+ * public static void foo(long l) {} // Violation, should be after 'foo(int i)'
+ * protected void foo(String s) {} // OK
+ * void foo(byte b) {} // OK
+ * private static void foo(String s, int i) { } // OK
+ * public void foo(String a, String b) {} // OK
+ * private void notFoo(int i) {} // Violation, should be after 'notFoo()'
+ * public final void foo(long x, long y) {} // Violation, should be after 'foo(String a, String b)'
+ * </pre>
+ * <p>
+ * Example of correct grouping of overloaded methods:
+ * </p>
+ * <pre>
  * public void foo(int i) {} // OK
- * public void foo(String s) {} // OK
- * public void notFoo() {} // violation. Have to be after foo(String s, int i)
- * public void foo(int i, String s) {}
- * public void foo(String s, int i) {}
+ * void foo(String s) {} // OK
+ * private void foo(String s, int i) {} // OK
+ * public void foo(int i, String s) {} // OK
+ * public void notFoo() {} // OK
+ * private void notFoo(int x) {} // OK
+ * </pre>
+ * <p>
+ * To configure the check to group static and private methods separately within their own
+ * individual groups
+ * </p>
+ * <pre>
+ * &lt;module name="OverloadMethodsDeclarationOrder"&gt;
+ *   &lt;property name="modifierGroups" value=".*static.*, ^public .*, (protected|package),
+ *   private"&gt;&lt;/property&gt;
+ * &lt;/module&gt;
+ * </pre>
+ * <p>
+ * Example of code with violation
+ * </p>
+ * <pre>
+ * public void foo() {} // OK
+ * public final void foo(int i) {} // OK
+ * void notFoo() {} // OK
+ * public static void foo(long l) {} // OK
+ * protected void foo(String s) {} // OK
+ * void foo(byte b) {} // OK
+ * private static void foo(String s, int i) { } // Violation, should be after 'foo(long l)'
+ * public void foo(String a, String b) {} // Violation, should be after 'foo()'
+ * private void notFoo(int i) {} // OK
+ * public final void foo(long x, long y) {} // Violation, should be after 'foo(int i)'
+ * </pre>
+ * <p>
+ * Example of compliant code
+ * </p>
+ * <pre>
+ * public void foo() {} // OK
+ * public void foo(String a, String b) {} // OK
+ * public final void foo(int i) {} // OK
+ * public final void foo(long x, long y) {} // OK
+ * void notFoo() {} // OK
+ * public static void foo(long l) {} // OK
+ * private static void foo(String s, int i) { } // OK
+ * protected void foo(String s) {} // OK
+ * void foo(byte b) {} // OK
+ * private void notFoo(int i) {} // OK
  * </pre>
  * <p>
  * Parent is {@code com.puppycrawl.tools.checkstyle.TreeWalker}
@@ -82,6 +144,27 @@ public class OverloadMethodsDeclarationOrderCheck extends AbstractCheck {
      * file.
      */
     public static final String MSG_KEY = "overload.methods.declaration";
+
+    /**
+     * A match all pattern for the DEFAULT_OVERLOAD_GROUP.
+     */
+    private static final Pattern DEFAULT_MODIFIER_GROUP_PATTERN = Pattern.compile(".*");
+
+    /**
+     * String used as separator between the regex pattern that matches a method's modifiers
+     * and the name of the method.
+     */
+    private static final String SEPARATOR = ":";
+
+    /**
+     * The abstract modifier.
+     */
+    private static final String ABSTRACT_MODIFIER = "abstract";
+
+    /**
+     * Control how different types of overloaded methods can be grouped by their modifiers.
+     */
+    private Pattern[] modifierGroups = new Pattern[0];
 
     @Override
     public int[] getDefaultTokens() {
@@ -118,6 +201,19 @@ public class OverloadMethodsDeclarationOrderCheck extends AbstractCheck {
     }
 
     /**
+     * Setter to control how different types of overloaded methods can be grouped by their
+     * modifiers.
+     *
+     * @param modifierGroupRegex the array of regex patterns
+     */
+    public void setModifierGroups(String... modifierGroupRegex) {
+        modifierGroups = new Pattern[modifierGroupRegex.length];
+        for (int index = 0; index < modifierGroupRegex.length; index++) {
+            modifierGroups[index] = CommonUtil.createPattern(modifierGroupRegex[index]);
+        }
+    }
+
+    /**
      * Checks that if overload methods are grouped together they should not be
      * separated from each other.
      *
@@ -126,27 +222,140 @@ public class OverloadMethodsDeclarationOrderCheck extends AbstractCheck {
      */
     private void checkOverloadMethodsGrouping(DetailAST objectBlock) {
         final int allowedDistance = 1;
+        final Map<String, IndexAndLine> knownMethods = new HashMap<>();
         DetailAST currentToken = objectBlock.getFirstChild();
-        final Map<String, Integer> methodIndexMap = new HashMap<>();
-        final Map<String, Integer> methodLineNumberMap = new HashMap<>();
+
         int currentIndex = 0;
         while (currentToken != null) {
             if (currentToken.getType() == TokenTypes.METHOD_DEF) {
                 currentIndex++;
-                final String methodName =
-                        currentToken.findFirstToken(TokenTypes.IDENT).getText();
-                final Integer previousIndex = methodIndexMap.get(methodName);
-                if (previousIndex != null && currentIndex - previousIndex > allowedDistance) {
-                    final int previousLineWithOverloadMethod =
-                            methodLineNumberMap.get(methodName);
-                    log(currentToken, MSG_KEY,
-                            previousLineWithOverloadMethod);
+                final int modifierGroupIndex = findModifierGroupPatternIndex(currentToken);
+                final String methodKey =
+                    String.format(Locale.ROOT, "%d%s%s", modifierGroupIndex, SEPARATOR,
+                                  currentToken.findFirstToken(TokenTypes.IDENT).getText());
+                final IndexAndLine indexAndLine =
+                    new IndexAndLine(currentIndex, currentToken.getLineNo());
+                final IndexAndLine previousMeta = knownMethods.put(methodKey, indexAndLine);
+                if (previousMeta != null
+                    && currentIndex - previousMeta.getIndex() > allowedDistance) {
+                    final int previousLineWithOverloadMethod = previousMeta.getLineNo();
+                    log(currentToken, MSG_KEY, previousLineWithOverloadMethod);
                 }
-                methodIndexMap.put(methodName, currentIndex);
-                methodLineNumberMap.put(methodName, currentToken.getLineNo());
             }
             currentToken = currentToken.getNextSibling();
         }
     }
 
+    /**
+     * Get the index of the first matching {@code modifierGroup} for the modifiers on the method.
+     *
+     * @param currentToken assumed to be a method
+     * @return index of the first modifier group that matches the modifiers on the method, or -1
+     *     if only the default group matches.
+     */
+    private int findModifierGroupPatternIndex(DetailAST currentToken) {
+        final String methodModifiers = getAllModifiersFrom(currentToken);
+        int modifierIndex = -1;
+        for (int index = 0; index < modifierGroups.length; index++) {
+            final Pattern pattern = modifierGroups[index];
+            final Matcher matcher = pattern.matcher(methodModifiers);
+            if (matcher.find()) {
+                modifierIndex = index;
+                break;
+            }
+        }
+        return modifierIndex;
+    }
+
+    /**
+     * Extract the modifier(s) of a method definition into a string.
+     *
+     * @param token expected to be TokenType.METHOD_DEF .
+     * @return modifier string for {@code token} with 'package' scope added if needed
+     */
+    private static String getAllModifiersFrom(final DetailAST token) {
+        final List<String> collectedModifiers = getExplicitModifiersFrom(token);
+        final String scope = ScopeUtil.getScope(token).getName();
+        if (!collectedModifiers.contains(scope)) {
+            collectedModifiers.add(0, scope);
+        }
+
+        // methods in interfaces can only have public, abstract, default, private,
+        // and static modifiers. a method can be abstract if and only if the only
+        // other modifier present is the public scope. If that is the case, add the implicit
+        // abstract modifier to the list
+        if (ScopeUtil.isInInterfaceBlock(token)
+            && List.of(Scope.PUBLIC.getName()).equals(collectedModifiers)) {
+            collectedModifiers.add(ABSTRACT_MODIFIER);
+        }
+
+        return String.join(" ", collectedModifiers);
+    }
+
+    /**
+     * Get the modifiers associated with a method.
+     *
+     * @param token the method token
+     * @return a list of modifiers found on the method, never empty
+     */
+    private static List<String> getExplicitModifiersFrom(final DetailAST token) {
+        final List<String> collectedModifiers = new LinkedList<>();
+        final DetailAST modifiersAst = token.findFirstToken(TokenTypes.MODIFIERS);
+        DetailAST modifier = modifiersAst.getFirstChild();
+        while (modifier != null) {
+            // skipping annotations
+            if (modifier.getType() != TokenTypes.ANNOTATION) {
+                collectedModifiers.add(modifier.getText());
+            }
+            // advance element in loop
+            modifier = modifier.getNextSibling();
+        }
+        return collectedModifiers;
+    }
+
+    /**
+     * Stores the {@code index} of a method in a class and the {@code lineNo}
+     * on which the method appears.
+     */
+    private static final class IndexAndLine {
+        /**
+         * The index of method within the containing class.
+         */
+        private final int index;
+
+        /**
+         * The line number on which the method appears in the class.
+         */
+        private final int lineNo;
+
+        /**
+         * Create a new composite tuple containing the {@code index} and {@code lineNo}
+         * of a method within a class.
+         *
+         * @param index the index of the method within the class
+         * @param lineNo the line number of the method in the class
+         */
+        private IndexAndLine(int index, int lineNo) {
+            this.index = index;
+            this.lineNo = lineNo;
+        }
+
+        /**
+         * Getter for the index.
+         *
+         * @return the method's index
+         */
+        public int getIndex() {
+            return index;
+        }
+
+        /**
+         * Getter for the line number.
+         *
+         * @return the method's line number
+         */
+        public int getLineNo() {
+            return lineNo;
+        }
+    }
 }
