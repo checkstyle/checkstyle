5d142fc6f5f7e1d0964fa8019ced4abda7ef89e5
Nick Mancuso <nicholas.mancuso13@gmail.com>
2022-12-10 11:27:14 -0500

    Issue #12507: Support record patterns preview

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammar/java19/Java19AstRegressionTest.java
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2022 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.grammar.java19;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractTreeTestSupport;
+
+public class Java19AstRegressionTest extends AbstractTreeTestSupport {
+
+    @Override
+    protected String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/grammar/java19";
+    }
+
+    @Test
+    public void testRecordPatternsWithNestedDecomposition() throws Exception {
+        verifyAst(getPath("ExpectedRecordPatternsPreviewNestedDecomposition.txt"),
+                getNonCompilablePath("InputRecordPatternsPreviewNestedDecomposition.java"));
+    }
+
+    @Test
+    public void testRecordPatternsPreview() throws Exception {
+        verifyAst(getPath("ExpectedRecordPatternsPreview.txt"),
+                getNonCompilablePath("InputRecordPatternsPreview.java"));
+    }
+}
