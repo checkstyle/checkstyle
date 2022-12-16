029cc0c84be2ba508717caf4fb0d2bdd44639f96
Arin Modi <arinmodi2306@gmail.com>
2022-12-16 20:48:04 +0530

    Issue #12409: Inconsistentency In  Allowed Abbreviations

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheckTest.java
@@ -467,4 +467,18 @@ public class AbbreviationAsWordInNameCheckTest extends AbstractModuleTestSupport
                 expected);
     }
 
+    @Test
+    public void testInputAbbreviationAsWordInNameTypeSnakeStyle() throws Exception {
+        final String[] expected = {
+            "13:20: " + getWarningMessage("FLAG_IS_FIRST_RUN", 4),
+            "16:17: " + getWarningMessage("HYBRID_LOCK_PATH", 4),
+            "21:17: " + getWarningMessage("__DEMOS__TESTS_VAR", 4),
+            "54:21: " + getWarningMessage("getIsFIRST_Run", 4),
+            "59:21: " + getWarningMessage("getBoolean_VALUES", 4),
+        };
+
+        verifyWithInlineConfigParser(
+                getPath("InputAbbreviationAsWordInNameTypeSnakeStyle.java"), expected);
+    }
+
 }
