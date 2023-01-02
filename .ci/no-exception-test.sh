acb9141a367456c26994ba2350e53a9c6dc17c4b
rnveach <rveach02@gmail.com>
2023-01-02 11:50:29 -0500

    supplemental: contribution#273 test

--- .ci/no-exception-test.sh
+++ .ci/no-exception-test.sh
@@ -9,7 +9,10 @@ guava-with-google-checks)
   CS_POM_VERSION="$(getCheckstylePomVersion)"
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
   echo CS_version: "$CS_POM_VERSION"
-  checkout_from https://github.com/checkstyle/contribution
+  checkout_from https://github.com/rnveach/contribution
+  cd .ci-temp/contribution
+  git checkout issue_273
+  cd ../..
   cd .ci-temp/contribution/checkstyle-tester
   sed -i.'' 's/^guava/#guava/' projects-to-test-on.properties
   sed -i.'' 's/#guava|/guava|/' projects-to-test-on.properties
@@ -17,10 +20,10 @@ guava-with-google-checks)
   cp src/main/resources/google_checks.xml .ci-temp/google_checks.xml
   sed -i.'' 's/warning/ignore/' .ci-temp/google_checks.xml
   cd .ci-temp/contribution/checkstyle-tester
-  export MAVEN_OPTS="-Xmx2048m"
+  export JDK_JAVA_OPTIONS="-Xmx2048m"
   groovy ./diff.groovy --listOfProjects projects-to-test-on.properties \
       --patchConfig ../../google_checks.xml \
-      --mode single --allowExcludes -xm "-Dcheckstyle.failsOnError=false" \
+      --mode single --allowExcludes --continueOnError \
       -p "$BRANCH" -r ../../..
   cd ../..
   removeFolderWithProtectedFiles contribution
@@ -31,7 +34,10 @@ guava-with-sun-checks)
   CS_POM_VERSION="$(getCheckstylePomVersion)"
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
   echo CS_version: "$CS_POM_VERSION"
-  checkout_from https://github.com/checkstyle/contribution
+  checkout_from https://github.com/rnveach/contribution
+  cd .ci-temp/contribution
+  git checkout issue_273
+  cd ../..
   cd .ci-temp/contribution/checkstyle-tester
   sed -i.'' 's/^guava/#guava/' projects-to-test-on.properties
   sed -i.'' 's/#guava|/guava|/' projects-to-test-on.properties
@@ -39,10 +45,10 @@ guava-with-sun-checks)
   cp src/main/resources/sun_checks.xml .ci-temp/sun_checks.xml
   sed -i.'' 's/value=\"error\"/value=\"ignore\"/' .ci-temp/sun_checks.xml
   cd .ci-temp/contribution/checkstyle-tester
-  export MAVEN_OPTS="-Xmx2048m"
+  export JDK_JAVA_OPTIONS="-Xmx2048m"
   groovy ./diff.groovy --listOfProjects projects-to-test-on.properties \
       --patchConfig ../../sun_checks.xml \
-      --mode single --allowExcludes -xm "-Dcheckstyle.failsOnError=false" \
+      --mode single --allowExcludes --continueOnError \
       -p "$BRANCH" -r ../../..
   cd ../..
   removeFolderWithProtectedFiles contribution
@@ -52,7 +58,10 @@ guava-with-sun-checks)
 openjdk17-with-checks-nonjavadoc-error)
   LOCAL_GIT_REPO=$(pwd)
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  checkout_from https://github.com/checkstyle/contribution
+  checkout_from https://github.com/rnveach/contribution
+  cd .ci-temp/contribution
+  git checkout issue_273
+  cd ../..
   sed -i.'' 's/value=\"error\"/value=\"ignore\"/' \
         .ci-temp/contribution/checkstyle-tester/checks-nonjavadoc-error.xml
   cd .ci-temp/contribution/checkstyle-tester
@@ -60,12 +69,12 @@ openjdk17-with-checks-nonjavadoc-error)
       openjdk-17-projects-to-test-on.config
   sed -i '/  <!-- Filters -->/r ../../../config/projects-to-test/openjdk17-excluded.files' \
       checks-nonjavadoc-error.xml
-  export MAVEN_OPTS="-Xmx2048m"
+  export JDK_JAVA_OPTIONS="-Xmx2048m"
   groovy ./diff.groovy --listOfProjects openjdk-17-projects-to-test-on.config \
       --mode single --allowExcludes \
       --patchConfig checks-nonjavadoc-error.xml \
       --localGitRepo  "$LOCAL_GIT_REPO" \
-      --patchBranch "$BRANCH" -xm "-Dcheckstyle.failsOnError=false"
+      --patchBranch "$BRANCH" --continueOnError
 
   cd ../../
   removeFolderWithProtectedFiles contribution
@@ -74,7 +83,10 @@ openjdk17-with-checks-nonjavadoc-error)
 openjdk19-with-checks-nonjavadoc-error)
   LOCAL_GIT_REPO=$(pwd)
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  checkout_from https://github.com/checkstyle/contribution
+  checkout_from https://github.com/rnveach/contribution
+  cd .ci-temp/contribution
+  git checkout issue_273
+  cd ../..
   sed -i.'' 's/value=\"error\"/value=\"ignore\"/' \
         .ci-temp/contribution/checkstyle-tester/checks-nonjavadoc-error.xml
   cd .ci-temp/contribution/checkstyle-tester
@@ -82,12 +94,12 @@ openjdk19-with-checks-nonjavadoc-error)
       openjdk-19-projects-to-test-on.config
   sed -i '/  <!-- Filters -->/r ../../../config/projects-to-test/openjdk19-excluded.files' \
       checks-nonjavadoc-error.xml
-  export MAVEN_OPTS="-Xmx2048m"
+  export JDK_JAVA_OPTIONS="-Xmx2048m"
   groovy ./diff.groovy --listOfProjects openjdk-19-projects-to-test-on.config \
       --mode single --allowExcludes \
       --patchConfig checks-nonjavadoc-error.xml \
       --localGitRepo  "$LOCAL_GIT_REPO" \
-      --patchBranch "$BRANCH" -xm "-Dcheckstyle.failsOnError=false"
+      --patchBranch "$BRANCH" --continueOnError
 
   cd ../../
   removeFolderWithProtectedFiles contribution
@@ -97,7 +109,10 @@ no-exception-lucene-and-others-javadoc)
   CS_POM_VERSION="$(getCheckstylePomVersion)"
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
   echo 'CS_POM_VERSION='"${CS_POM_VERSION}"
-  checkout_from https://github.com/checkstyle/contribution
+  checkout_from https://github.com/rnveach/contribution
+  cd .ci-temp/contribution
+  git checkout issue_273
+  cd ../..
   cp config/projects-to-test/projects-for-no-exception-javadoc.config \
       .ci-temp/contribution/checkstyle-tester
   cd .ci-temp/contribution/checkstyle-tester
@@ -106,10 +121,10 @@ no-exception-lucene-and-others-javadoc)
   sed -i'' 's/#protonpack/protonpack/' projects-for-no-exception-javadoc.config
   sed -i'' 's/#jOOL/jOOL/' projects-for-no-exception-javadoc.config
   sed -i'' 's/#lucene-solr/lucene-solr/' projects-for-no-exception-javadoc.config
-  export MAVEN_OPTS="-Xmx2048m"
+  export JDK_JAVA_OPTIONS="-Xmx2048m"
   groovy ./diff.groovy --listOfProjects projects-for-no-exception-javadoc.config \
       --patchConfig checks-only-javadoc-error.xml \
-      --mode single --allowExcludes -xm "-Dcheckstyle.failsOnError=false" \
+      --mode single --allowExcludes --continueOnError \
       -p "$BRANCH" -r ../../..
   cd ../..
   removeFolderWithProtectedFiles contribution
@@ -119,7 +134,10 @@ no-exception-cassandra-storm-tapestry-javadoc)
   CS_POM_VERSION="$(getCheckstylePomVersion)"
   echo 'CS_POM_VERSION='"${CS_POM_VERSION}"
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  checkout_from https://github.com/checkstyle/contribution
+  checkout_from https://github.com/rnveach/contribution
+  cd .ci-temp/contribution
+  git checkout issue_273
+  cd ../..
   cp config/projects-to-test/projects-for-no-exception-javadoc.config \
       .ci-temp/contribution/checkstyle-tester
   cd .ci-temp/contribution/checkstyle-tester
@@ -127,10 +145,10 @@ no-exception-cassandra-storm-tapestry-javadoc)
   sed -i'' 's/#tapestry-5/tapestry-5/' projects-for-no-exception-javadoc.config
   sed -i'' 's/#storm/storm/' projects-for-no-exception-javadoc.config
   sed -i'' 's/#cassandra/cassandra/' projects-for-no-exception-javadoc.config
-  export MAVEN_OPTS="-Xmx2048m"
+  export JDK_JAVA_OPTIONS="-Xmx2048m"
   groovy ./diff.groovy --listOfProjects projects-for-no-exception-javadoc.config \
       --patchConfig checks-only-javadoc-error.xml \
-      --mode single --allowExcludes  -xm "-Dcheckstyle.failsOnError=false" \
+      --mode single --allowExcludes  --continueOnError \
       -p "$BRANCH" -r ../../..
   cd ../..
   removeFolderWithProtectedFiles contribution
@@ -140,7 +158,10 @@ no-exception-hadoop-apache-groovy-scouter-javadoc)
   CS_POM_VERSION="$(getCheckstylePomVersion)"
   echo 'CS_POM_VERSION='"${CS_POM_VERSION}"
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  checkout_from https://github.com/checkstyle/contribution
+  checkout_from https://github.com/rnveach/contribution
+  cd .ci-temp/contribution
+  git checkout issue_273
+  cd ../..
   cp config/projects-to-test/projects-for-no-exception-javadoc.config \
       .ci-temp/contribution/checkstyle-tester
   cd .ci-temp/contribution/checkstyle-tester
@@ -149,10 +170,10 @@ no-exception-hadoop-apache-groovy-scouter-javadoc)
   sed -i'' 's/#hadoop/hadoop/' projects-for-no-exception-javadoc.config
   sed -i'' 's/#groovy/groovy/' projects-for-no-exception-javadoc.config
   sed -i'' 's/#scouter/scouter/' projects-for-no-exception-javadoc.config
-  export MAVEN_OPTS="-Xmx2048m"
+  export JDK_JAVA_OPTIONS="-Xmx2048m"
   groovy ./diff.groovy --listOfProjects projects-for-no-exception-javadoc.config \
       --patchConfig checks-only-javadoc-error.xml \
-      --mode single --allowExcludes -xm "-Dcheckstyle.failsOnError=false" \
+      --mode single --allowExcludes --continueOnError \
       -p "$BRANCH" -r ../../..
   cd ../..
   removeFolderWithProtectedFiles contribution
@@ -162,7 +183,10 @@ no-exception-only-javadoc)
   CS_POM_VERSION="$(getCheckstylePomVersion)"
   echo 'CS_POM_VERSION='"${CS_POM_VERSION}"
   BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  checkout_from https://github.com/checkstyle/contribution
+  checkout_from https://github.com/rnveach/contribution
+  cd .ci-temp/contribution
+  git checkout issue_273
+  cd ../..
   cd .ci-temp/contribution/checkstyle-tester
   sed -i.'' 's/^guava/#guava/' projects-to-test-on.properties
   sed -i.'' 's/#spring-framework/spring-framework/' projects-to-test-on.properties
@@ -170,10 +194,10 @@ no-exception-only-javadoc)
   sed -i.'' 's/#spotbugs/spotbugs/' projects-to-test-on.properties
   sed -i.'' 's/#pmd/pmd/' projects-to-test-on.properties
   sed -i.'' 's/#apache-ant/apache-ant/' projects-to-test-on.properties
-  export MAVEN_OPTS="-Xmx2048m"
+  export JDK_JAVA_OPTIONS="-Xmx2048m"
   groovy ./diff.groovy --listOfProjects projects-to-test-on.properties \
       --patchConfig checks-only-javadoc-error.xml --allowExcludes \
-      --mode single -xm "-Dcheckstyle.failsOnError=false" \
+      --mode single --continueOnError \
       -p "$BRANCH" -r ../../..
   cd ../..
   removeFolderWithProtectedFiles contribution
