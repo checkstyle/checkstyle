5af81e506c2e99b419f68e61616d8ac629dada22
Vyom-Yadav <jackhammervyom@gmail.com>
2023-01-13 12:03:22 +0530

    Issue #12613: pitest-survival-check-xml.groovy should automatically update list of suppressions

--- .ci/pitest.sh
+++ .ci/pitest.sh
@@ -19,14 +19,10 @@ case $1 in
 
   if [[ $(echo "$PROFILES" | grep -w -- "${1}" | cat) != "" ]]; then
     mvn -e --no-transfer-progress -P"$1" clean test-compile org.pitest:pitest-maven:mutationCoverage
-    if [[ "$2" != "" ]]; then
-      groovy ./.ci/pitest-survival-check-xml.groovy "$1" "$2"
-    else
       groovy ./.ci/pitest-survival-check-xml.groovy "$1"
-    fi
   else
     echo "Unexpected argument: $*"
-    echo "Usage $0 <profile> <extra argument for pitest-survival-check-xml.groovy>"
+    echo "Usage $0 <profile>"
     echo "To see the full list of supported profiles run '$0 --list'"
     false
   fi
