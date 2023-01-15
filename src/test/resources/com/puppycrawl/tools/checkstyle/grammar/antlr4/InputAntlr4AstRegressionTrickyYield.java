ed1fb9cfb1ee323452187d582c8a65fc0d390616
Kevin222004 <kevinpatel222004@gmail.com>
2023-01-16 02:35:56 +0530

    Issue #9994: LITERAL_SWITCH token support in RightCurlyCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/grammar/antlr4/InputAntlr4AstRegressionTrickyYield.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/grammar/antlr4/InputAntlr4AstRegressionTrickyYield.java
@@ -5,10 +5,10 @@ import java.util.function.Function;
 
 public class InputAntlr4AstRegressionTrickyYield<T1> {
     public Iterator<T1> yield() {
-        return yield(Function.identity());
+        return getYield(Function.identity());
     }
 
-    private static <T, T1> Iterator<T1> yield(Function<T,T> identity) {
+    private static <T, T1> Iterator<T1> getYield(Function<T,T> identity) {
         return null;
     }
 }
