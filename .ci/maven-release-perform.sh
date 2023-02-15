f5b3e7e36b3636ad8488a3dca6bca7244434b83e
stoyanK7 <stoyank127@gmail.com>
2023-02-15 09:24:47 +0100

    Issue #12720: Fix issues with maven-release-perform workflow

--- .ci/maven-release-perform.sh
+++ .ci/maven-release-perform.sh
@@ -17,17 +17,18 @@ CURRENT_VERSION=$(getCheckstylePomVersionWithoutSnapshot)
 echo CURRENT_VERSION="$CURRENT_VERSION"
 
 if [ "$TARGET_VERSION" != "$CURRENT_VERSION" ]; then
-  echo "Target Version and current Version do not match"
+  echo "[ERROR] Target Version and current Version do not match."
   exit 1;
 fi
 
 SKIP_TEST="-DskipTests -DskipITs"
 SKIP_CHECKSTYLE="-Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true"
-SKIP_OTHERS="-Dpmd.skip=true -Dspotbugs.skip=true -Djacoco.skip=true -Dxml.skip=true"
+SKIP_OTHERS="-Dpmd.skip=true -Dspotbugs.skip=true -Djacoco.skip=true -Dxml.skip=true \
+  --no-transfer-progress"
 
 git checkout "checkstyle-$TARGET_VERSION"
 echo "Deploying jars to maven central (release:perform) ..."
 mvn -e --no-transfer-progress -Pgpg release:perform \
-  -DconnectionUrl=scm:git:git@github.com:checkstyle/checkstyle.git \
+  -DconnectionUrl=scm:git:https://github.com/checkstyle/checkstyle.git \
   -Dtag=checkstyle-"$TARGET_VERSION" \
   -Darguments="$SKIP_TEST $SKIP_CHECKSTYLE $SKIP_OTHERS"
