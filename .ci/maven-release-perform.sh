e9d5889e8a653b76d5a2ad29d25a6e562735317e
Roman Ivanov <ivanov-jr@mail.ru>
2023-02-22 04:53:28 -0800

    infra: remove version check from release-perform as maven do this to clone to target folder during execution

--- .ci/maven-release-perform.sh
+++ .ci/maven-release-perform.sh
@@ -13,14 +13,6 @@ fi
 TARGET_VERSION=$1
 echo TARGET_VERSION="$TARGET_VERSION"
 
-CURRENT_VERSION=$(getCheckstylePomVersionWithoutSnapshot)
-echo CURRENT_VERSION="$CURRENT_VERSION"
-
-if [ "$TARGET_VERSION" != "$CURRENT_VERSION" ]; then
-  echo "[ERROR] Target Version and current Version do not match."
-  exit 1;
-fi
-
 SKIP_TEST="-DskipTests -DskipITs"
 SKIP_CHECKSTYLE="-Dcheckstyle.ant.skip=true -Dcheckstyle.skip=true"
 SKIP_OTHERS="-Dpmd.skip=true -Dspotbugs.skip=true -Djacoco.skip=true -Dxml.skip=true \
