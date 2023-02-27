57d33dcb7d3f4841146838b8a6840c63cdf9e6fd
Rick Ossendrijver <rick.ossendrijver@gmail.com>
2023-02-27 09:14:09 +0100

    Add whitespace before initializer

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalCatchCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalCatchCheck.java
@@ -153,7 +153,7 @@ public final class IllegalCatchCheck extends AbstractCheck {
     public static final String MSG_KEY = "illegal.catch";
 
     /** Specify exception class names to reject. */
-    private final Set<String> illegalClassNames = Arrays.stream(new String[]{"Exception", "Error",
+    private final Set<String> illegalClassNames = Arrays.stream(new String[] {"Exception", "Error",
                     "RuntimeException", "Throwable", "java.lang.Error", "java.lang.Exception",
                     "java.lang.RuntimeException", "java.lang.Throwable",})
             .collect(Collectors.toCollection(HashSet::new));
