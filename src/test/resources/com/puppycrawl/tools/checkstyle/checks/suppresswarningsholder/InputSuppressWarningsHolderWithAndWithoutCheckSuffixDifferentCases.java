0097b6bf75f9f50172cb31333359e1889bc14ddf
jxr98 <jqjxxjxr@126.com>
2023-03-01 20:27:37 -0500

    Issue #11446: Updated SuppressWarningsHolderTest to use verifyWithInlineConfigParser method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/suppresswarningsholder/InputSuppressWarningsHolderWithAndWithoutCheckSuffixDifferentCases.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/suppresswarningsholder/InputSuppressWarningsHolderWithAndWithoutCheckSuffixDifferentCases.java
@@ -1,7 +1,19 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.SuppressWarningsHolder
+aliasList = (default)
+
+com.puppycrawl.tools.checkstyle.filters.SuppressWarningsFilter
+
+com.puppycrawl.tools.checkstyle.checks.naming.ConstantNameCheck
+format = (default)^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$
+
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.suppresswarningsholder;
 
 public class InputSuppressWarningsHolderWithAndWithoutCheckSuffixDifferentCases {
-    private static final int a = 0; // violation 'invalid pattern'
+    private static final int a = 0; // violation 'Name 'a' must match pattern'
 
     @SuppressWarnings("checkstyle:constantnamecheck") // filtered violation 'invalid pattern'
     private static final int b = 0;
