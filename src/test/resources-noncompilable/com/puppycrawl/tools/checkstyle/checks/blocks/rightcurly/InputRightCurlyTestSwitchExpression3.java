fa54e9a7b14705ec65f1f7e40676164ca0452fcd
Kevin222004 <kevinpatel222004@gmail.com>
2023-03-04 00:25:24 +0530

    Issue #9994: LITERAL_SWITCH token support in RightCurlyCheck

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlyTestSwitchExpression3.java
@@ -0,0 +1,29 @@
+/*
+RightCurly
+option = ALONE_OR_SINGLELINE
+tokens = LITERAL_SWITCH, LITERAL_IF
+
+
+*/
+
+//non-compiled with javac: Compilable with Java14
+package com.puppycrawl.tools.checkstyle.checks.blocks.rightcurly;
+
+public class InputRightCurlyTestSwitchExpression3 {
+    int foo(int yield) {
+        return switch (yield) {
+            case 1 -> 2;
+            case 2 -> 3;
+            case 3 -> 4;
+            default -> 5;
+        };
+    }
+
+    int foo2(int yield) {
+        return switch (yield) {
+            case 1 -> 2;
+            case 2 -> 3;
+            case 3 -> 4;
+            default -> 5;
+        }; } // ok
+}
