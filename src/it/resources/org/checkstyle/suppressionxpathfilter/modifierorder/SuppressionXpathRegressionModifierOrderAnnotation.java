7d150f349e90930fdbf03ea5ffa2ab4f33d0c045
zyrate <pollery888@gmail.com>
2023-03-14 20:02:47 +0800

    Issue #6207: Add xpath regression test for ModifierOrder

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/modifierorder/SuppressionXpathRegressionModifierOrderAnnotation.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.modifierorder;
+
+public @InterfaceAnnotation @interface SuppressionXpathRegressionModifierOrderAnnotation { //warn
+    int foo();
+}
+
+@interface InterfaceAnnotation {}
