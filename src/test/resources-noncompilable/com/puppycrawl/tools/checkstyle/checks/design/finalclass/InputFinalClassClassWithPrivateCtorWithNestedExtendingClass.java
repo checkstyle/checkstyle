eef7b2b4fe118ff2bf9fc3b70145391c2a315209
Kevin222004 <kevinpatel222004@gmail.com>
2023-03-31 19:51:24 +0530

    Issue #11338: FinalClassCheck should report private classes without constructor

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/design/finalclass/InputFinalClassClassWithPrivateCtorWithNestedExtendingClass.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/design/finalclass/InputFinalClassClassWithPrivateCtorWithNestedExtendingClass.java
@@ -11,16 +11,16 @@ public class InputFinalClassClassWithPrivateCtorWithNestedExtendingClass {
     class A {
         private A() {}
         private class ExtendA extends A {}
-    }
+    } // violation above 'Class ExtendA should be declared as final'
 
     class B {
-        private B() {}
+        private B() {} // violation below 'Class ExtendB should be declared as final'
         private class ExtendB extends com.puppycrawl.tools.checkstyle.checks.design.finalclass.
             InputFinalClassClassWithPrivateCtorWithNestedExtendingClass.B {}
     }
 
-    class C { // violation
+    class C { // violation 'Class C should be declared as final'
         private C() {}
         private class ExtendC extends com.nonexistent.packages.C {}
-    }
+    } // violation above 'Class ExtendC should be declared as final'
 }
