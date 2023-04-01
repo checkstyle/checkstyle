dd4ad61560dc317a15ddc63b7a9b4e5fa7210a80
Sloent <1620491143@qq.com>
2023-04-02 01:41:44 +0800

    Issue #11214: Specified violation message in WriteTagCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/InputWriteTagIncomplete.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/InputWriteTagIncomplete.java
@@ -9,7 +9,7 @@ tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
 */
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.writetag;
-// violation 4 lines below , 'Javadoc tag .*'
+// violation 4 lines below , 'Must contain non-whitespace characters.*'
 /**
  * Testing tag writing
  * @author Daniel Grenner
