6daef687c2fb06d649fe5e756fea3478a1e95c3d
sd9616 <sd96@rice.edu>
2023-04-17 21:11:42 -0500

    Issue #11163: Enforce file size on all InputMagicNumber

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumberWaiverParentToken2.java
@@ -0,0 +1,67 @@
+/*
+MagicNumber
+ignoreNumbers = (default)-1, 0, 1, 2
+ignoreHashCodeMethod = (default)false
+ignoreAnnotation = (default)false
+ignoreFieldDeclaration = (default)false
+ignoreAnnotationElementDefaults = (default)true
+constantWaiverParentToken = ASSIGN, ARRAY_INIT, EXPR, UNARY_PLUS, UNARY_MINUS, TYPECAST, \
+                            ELIST, STAR, DIV, PLUS, MINUS
+tokens = (default)NUM_DOUBLE, NUM_FLOAT, NUM_INT, NUM_LONG
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.magicnumber;
+public class InputMagicNumberWaiverParentToken2 {
+    /**
+     * test long hex
+     */
+    long l = 0xffffffffL; // violation
+
+
+    /**
+     * test signed values
+     */
+    public static final int CONST_PLUS_THREE = +3;
+    public static final int CONST_MINUS_TWO = -2;
+    private int mPlusThree = +3; // violation
+    private int mMinusTwo = -2;  // violation
+    private double mPlusDecimal = +3.5; // violation
+    private double mMinusDecimal = -2.5; // violation
+
+    /**
+     * test octal and hex negative values
+     */
+    private int hexIntMinusOne = 0xffffffff;
+    private long hexLongMinusOne = 0xffffffffffffffffL;
+    private long hexIntMinValue = 0x80000000; // violation
+    private long hexLongMinValue = 0x8000000000000000L; // violation
+    private int octalIntMinusOne = 037777777777;
+    private long octalLongMinusOne = 01777777777777777777777L;
+    private long octalIntMinValue = 020000000000; // violation
+    private long octalLongMinValue = 01000000000000000000000L;  // violation
+    public static final int TESTINTVAL = (byte) 0x80;
+
+    public static final java.util.List MYLIST = new java.util.ArrayList() {
+        public int size() {
+
+            return 378; // violation
+        }
+    };
+
+    public final double SpecialSum = 2 + 1e10, SpecialDifference = 4 - java.lang.Math.PI;
+    public final Integer DefaultInit = new Integer(27); // violation
+    public final int SpecsPerDay = 24 * 60 * 60, SpecialRatio = 4 / 3;
+    public final javax.swing.border.Border StdBorder =
+            javax.swing.BorderFactory.createEmptyBorder(3, 3, 3, 3); // 4 violations
+
+    enum MyEnum2WaiverParentToken2 {
+        A(3),
+        B(54);
+
+        private MyEnum2WaiverParentToken2(int value) {
+
+        }
+    }
+}
