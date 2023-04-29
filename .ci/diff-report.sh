f7b6d02ad07fcd598f0362f959200ae63ebf29fd
Roman Ivanov <ivanov-jr@mail.ru>
2023-04-29 07:47:29 -0700

    infra: diff-report should use files in .ci-temp

--- .ci/diff-report.sh
+++ .ci/diff-report.sh
@@ -36,6 +36,7 @@ case $1 in
 # Downloads all files necessary to generate regression report to the parent directory.
 download-files)
   checkForVariable "GITHUB_TOKEN"
+  mkdir .ci-temp
   echo "Downloading files..."
 
   # check for projects link from PR, if not found use default from contribution repo
@@ -45,27 +46,27 @@ download-files)
   curl --fail-with-body -X GET "${LINK}" \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: token $GITHUB_TOKEN" \
-    -o ../project.properties
+    -o .ci-temp/project.properties
 
   if [ -n "$NEW_MODULE_CONFIG_LINK" ]; then
     curl --fail-with-body -X GET "${NEW_MODULE_CONFIG_LINK}" \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: token $GITHUB_TOKEN" \
-      -o ../new_module_config.xml
+      -o .ci-temp/new_module_config.xml
   fi
 
   if [ -n "$DIFF_CONFIG_LINK" ]; then
     curl --fail-with-body -X GET "${DIFF_CONFIG_LINK}" \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: token $GITHUB_TOKEN" \
-      -o ../diff_config.xml
+      -o .ci-temp/diff_config.xml
   fi
 
   if [ -n "$PATCH_CONFIG_LINK" ]; then
     curl --fail-with-body -X GET "${PATCH_CONFIG_LINK}" \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: token $GITHUB_TOKEN" \
-      -o ../patch_config.xml
+      -o .ci-temp/patch_config.xml
   fi
   ;;
 
