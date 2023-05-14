2e1f7fe82e7814058eaebca5a091e4a0f60b0ecc
Ruslan Diachenko <rd.ryly@gmail.com>
2023-05-14 20:55:24 +0100

    Issue #13063: fix listing pitest profiles on Mac

--- .ci/pitest.sh
+++ .ci/pitest.sh
@@ -4,7 +4,7 @@ set -e
 
 function list_profiles() {
   POM_PATH="$(dirname "${0}")/../pom.xml"
-  cat "$POM_PATH" | sed -n -e 's/^.*<id>\(pitest-[^<]\+\)<\/id>.*$/\1/p' | sort
+  cat "$POM_PATH" | sed -E -n 's/^.*<id>(pitest-[^<]+)<\/id>.*$/\1/p' | sort
 }
 
 case $1 in
