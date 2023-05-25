1978fed4be9c32010b94b187ab206677d223b4e7
shamith2 <shamith@datainsights.ae>
2023-05-25 14:15:06 +0530

    Issue #13086: InnerAssignmentCheck failed for one line code in Java 14 switch expression

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/innerassignment/InputInnerAssignmentWithEnhancedSwitch.java
@@ -0,0 +1,22 @@
+/*
+InnerAssignment
+
+
+*/
+
+//non-compiled with javac: Compilable with Java14
+package com.puppycrawl.tools.checkstyle.checks.coding.innerassignment;
+
+public class InputInnerAssignmentWithEnhancedSwitch {
+
+    void method(final String operation) {
+        boolean flag = false;
+        switch (operation) {
+            case "Y" -> flag = true;  // no violation 
+            case "N" -> {
+                flag = false;
+            }
+            default -> throw new UnsupportedOperationException();
+        }
+    }
+}
