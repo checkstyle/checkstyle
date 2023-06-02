9d6f559157f94224fe02d66d6119085faf1b71d2
stoyanK7 <stoyank127@gmail.com>
2023-06-02 23:27:35 +0200

    Issue #13100: Create tests from VisibilityModifier xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/design/visibilitymodifier/Example11.java
@@ -0,0 +1,35 @@
+/*
+VisibilityModifier
+packageAllowed = (default)false
+protectedAllowed = (default)false
+publicMemberPattern = (default)^serialVersionUID$
+allowPublicFinalFields = (default)false
+allowPublicImmutableFields = true
+immutableClassCanonicalNames = (default)java.io.File, java.lang.Boolean, java.lang.Byte, \
+                               java.lang.Character, java.lang.Double, java.lang.Float, \
+                               java.lang.Integer, java.lang.Long, java.lang.Short, \
+                               java.lang.StackTraceElement, java.lang.String, \
+                               java.math.BigDecimal, java.math.BigInteger, \
+                               java.net.Inet4Address, java.net.Inet6Address, \
+                               java.net.InetSocketAddress, java.net.URI, java.net.URL, \
+                               java.util.Locale, java.util.UUID
+ignoreAnnotationCanonicalNames = (default)com.google.common.annotations.VisibleForTesting, \
+                                 org.junit.ClassRule, org.junit.Rule
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.design.visibilitymodifier;
+
+import com.google.common.collect.ImmutableSet;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+class Example11 {
+  public final int someIntValue = 0; // violation 'must be private'
+  public final ImmutableSet<String> includes = null; // violation 'must be private'
+  public final java.lang.String notes = ""; // violation 'must be private'
+  public final BigDecimal value = null; // violation 'must be private'
+  public final List list = null; // violation 'must be private'
+}
