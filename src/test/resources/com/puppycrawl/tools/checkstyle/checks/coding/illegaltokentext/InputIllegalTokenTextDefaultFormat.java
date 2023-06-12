9534440f2823ffe0fbd88a44ec676ba0cd287d97
Kevin222004 <kevinpatel222004@gmail.com>
2023-06-12 10:58:54 +0530

    Issue #13109: Kill mutation for IllegalTokenTextCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/InputIllegalTokenTextDefaultFormat.java
@@ -0,0 +1,19 @@
+/*
+IllegalTokenText
+format = (default)^$
+ignoreCase = (default)false
+message = (default)
+tokens = STRING_LITERAL
+
+
+*/
+
+
+package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
+
+public class InputIllegalTokenTextDefaultFormat { // ok
+    public void myTest() {
+        String test = "a href";
+        String test2 = "A href";
+    }
+}
