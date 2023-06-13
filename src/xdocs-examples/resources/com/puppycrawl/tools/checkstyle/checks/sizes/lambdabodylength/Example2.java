1faa04cfb6b6eee901dc4b656deec58162149ae3
stoyanK7 <stoyank127@gmail.com>
2023-06-13 18:26:57 +0200

    Issue #13100: Create tests from LambdaBodyLength xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/sizes/lambdabodylength/Example2.java
@@ -0,0 +1,30 @@
+/*
+LambdaBodyLength
+max = 5
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.sizes.lambdabodylength;
+
+class Example2 {
+  Runnable r = () -> { // OK, length is 5
+    System.out.println(2);
+    System.out.println(3);
+    System.out.println(4);
+  };
+  Runnable r2 = () -> { // violation 'length is 6'
+    System.out.println(2); // line 2 of lambda
+    System.out.println(3);
+    System.out.println(4);
+    System.out.println(5);
+  };
+
+  Runnable r3 = () -> // violation 'length is 6'
+    "someString".concat("1")
+                .concat("2")
+                .concat("3")
+                .concat("4")
+                .concat("5")
+                .concat("6");
+}
