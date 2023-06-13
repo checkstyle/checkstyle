05444969f24969f2c9e28bd6cb53ad6a06dac342
stoyanK7 <stoyank127@gmail.com>
2023-06-13 19:00:50 +0200

    Issue #13100: Create tests from EmptyForIteratorPad xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptyforiteratorpad/Example2.java
@@ -0,0 +1,25 @@
+/*
+EmptyForIteratorPad
+option = space
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptyforiteratorpad;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class Example2 {
+  Map<String, String> map = new HashMap<>();
+  void example() {
+    for (Iterator it = map.entrySet().iterator();  it.hasNext(););
+    // violation above '';' is not followed by whitespace.'
+    for (Iterator it = map.entrySet().iterator();  it.hasNext(); );
+
+    for (Iterator foo = map.entrySet().iterator();
+         foo.hasNext();
+         );
+    }
+}
