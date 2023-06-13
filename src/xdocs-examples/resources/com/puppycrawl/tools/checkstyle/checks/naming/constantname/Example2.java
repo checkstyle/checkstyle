469946e43626579caf3d0a2de94b760c2066b62b
stoyanK7 <stoyank127@gmail.com>
2023-06-13 16:21:19 +0200

    Issue #13116: Create separate ant-phase-verify execution for xdocs-examples

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/constantname/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/constantname/Example2.java
@@ -1,10 +1,6 @@
 /*
 ConstantName
 format = ^log(ger)?$|^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$
-applyToPublic = (default)true
-applyToProtected = (default)true
-applyToPackage = (default)true
-applyToPrivate = (default)true
 
 
 */
@@ -12,6 +8,7 @@ applyToPrivate = (default)true
 package com.puppycrawl.tools.checkstyle.checks.naming.constantname;
 
 class Example2 {
+  // xdoc section -- start
   public final static int FIRST_CONSTANT1 = 10;
   protected final static int SECOND_CONSTANT2 = 100;
   final static int third_Constant3 = 1000; // violation 'must match pattern'
@@ -21,4 +18,5 @@ class Example2 {
   final static int loggerMYSELF = 5; // violation 'must match pattern'
   final static int MYSELF = 100;
   protected final static int myselfConstant = 1; // violation 'must match pattern'
+  // xdoc section -- end
 }
