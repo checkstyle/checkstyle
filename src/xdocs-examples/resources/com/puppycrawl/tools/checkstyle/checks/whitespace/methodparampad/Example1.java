65f9de1df7e1d1da99ccd7d51aeab2d7ecc36457
stoyanK7 <stoyank127@gmail.com>
2023-06-13 20:32:36 +0200

    Issue #13100: Create tests from MethodParamPad xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/methodparampad/Example1.java
@@ -0,0 +1,26 @@
+/*
+MethodParamPad
+allowLineBreaks = (default)false
+option = (default)nospace
+tokens = (default)CTOR_DEF, LITERAL_NEW, METHOD_CALL, METHOD_DEF, SUPER_CTOR_CALL, \
+         ENUM_CONSTANT_DEF, RECORD_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.methodparampad;
+
+class Example1 {
+  public Example1() {
+    super();
+  }
+
+  public Example1 (int aParam) { // violation ''(' is preceded with whitespace'
+    super (); // violation ''(' is preceded with whitespace'
+  }
+
+  public void method() {}
+
+  public void methodWithVeryLongName
+    () {} // violation ''(' should be on the previous line.'
+}
