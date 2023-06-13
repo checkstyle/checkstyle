5fcb24e0385b65d3f8bf66ee63d6b4e9f786054d
stoyanK7 <stoyank127@gmail.com>
2023-06-13 18:44:19 +0200

    Issue #13100: Create tests from EmptyForInitializerPad xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptyforinitializerpad/Example2.java
@@ -0,0 +1,20 @@
+/*
+EmptyForInitializerPad
+option = space
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptyforinitializerpad;
+
+class Example2 {
+  int i = 0;
+  void example() {
+    for ( ; i < 2; i++ ) { };
+    for (; i < 2; i++ ) { };    // violation '';' is not preceded with whitespace'
+    for (;i<2;i++) { };         // violation '';' is not preceded with whitespace'
+    for ( ;i<2;i++) { };
+    for (
+          ; i < 2; i++ );
+    }
+}
