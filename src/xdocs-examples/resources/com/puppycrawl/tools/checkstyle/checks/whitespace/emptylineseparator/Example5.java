5ed12e4efebb611726b50989aa74f43630b25505
stoyanK7 <stoyank127@gmail.com>
2023-06-13 19:50:08 +0200

    Issue #13100: Create tests from EmptyLineSeparator xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/Example5.java
@@ -0,0 +1,37 @@
+/*
+EmptyLineSeparator
+allowNoEmptyLineBetweenFields = (default)false
+allowMultipleEmptyLines = (default)true
+allowMultipleEmptyLinesInsideClassMembers = false
+tokens = (default)PACKAGE_DEF, IMPORT, STATIC_IMPORT, CLASS_DEF, INTERFACE_DEF, ENUM_DEF, \
+         STATIC_INIT, INSTANCE_INIT, METHOD_DEF, CTOR_DEF, VARIABLE_DEF, RECORD_DEF, \
+         COMPACT_CTOR_DEF
+
+
+*/
+
+///////////////////////////////////////////////////
+//HEADER
+///////////////////////////////////////////////////
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator;
+import java.io.Serializable;
+// violation 2 lines above ''package' should be separated from previous line'
+// violation 2 lines above ''import' should be separated from previous line'
+
+class Example5 {
+
+  int var1 = 1;
+  int var2 = 2; // violation ''VARIABLE_DEF' should be separated from previous line'
+
+
+  int var3 = 3;
+
+
+  void method1() {}
+  void method2() { // violation ''METHOD_DEF' should be separated from previous line'
+      int var4 = 4; // violation 'There is more than 1 empty line after this line'
+
+
+      int var5 = 5;
+  }
+}
