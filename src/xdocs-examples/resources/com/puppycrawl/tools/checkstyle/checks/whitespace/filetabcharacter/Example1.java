607368774f6e6f96e0f7addc88528f7edc3cd4a1
stoyanK7 <stoyank127@gmail.com>
2023-06-13 20:12:01 +0200

    Issue #13100: Create tests from FileTabCharacter xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/filetabcharacter/Example1.java
@@ -0,0 +1,17 @@
+/*
+FileTabCharacter
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.filetabcharacter;
+
+class Example1 {
+  // xdoc section -- start
+	int a; // violation 'File contains tab characters'
+
+	public void foo (int arg) { // OK, only first occurrence in file reported
+    a = arg; // OK, indented using spaces
+  }
+  // xdoc section -- end
+}
