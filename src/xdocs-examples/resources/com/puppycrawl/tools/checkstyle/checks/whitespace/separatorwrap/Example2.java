a4eec358548de6054d4fe5056dbb35a384241995
stoyanK7 <stoyank127@gmail.com>
2023-06-14 22:55:31 +0200

    Issue #13100: Create tests from SeparatorWrap xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/separatorwrap/Example2.java
@@ -0,0 +1,23 @@
+/*
+SeparatorWrap
+tokens = METHOD_REF
+option = nl
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.separatorwrap;
+
+import java.util.Arrays;
+
+class Example2 {
+  String[] stringArray = {"foo", "bar"};
+
+  void fun() {
+    // violation below ''::' should be on a new line'
+    Arrays.sort(stringArray, String::
+      compareToIgnoreCase);
+    Arrays.sort(stringArray, String
+      ::compareTo); // OK, because it is on a new line
+  }
+}
