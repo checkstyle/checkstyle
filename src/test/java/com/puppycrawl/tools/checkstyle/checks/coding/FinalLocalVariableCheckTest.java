44e3b764d95874d04b6f2cd8fe8e9eb06a3cc9c8
Kevin222004 <kevinpatel222004@gmail.com>
2023-06-14 10:13:34 +0530

    Issue #12671: Kill mutation for finalLocalVariable-3

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/FinalLocalVariableCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/FinalLocalVariableCheckTest.java
@@ -320,4 +320,18 @@ public class FinalLocalVariableCheckTest
             getPath("InputFinalLocalVariableConstructor.java"),
             expected);
     }
+
+    @Test
+    public void test() throws Exception {
+        final String[] expected = {
+            "20:17: " + getCheckMessage(MSG_KEY, "start"),
+            "22:17: " + getCheckMessage(MSG_KEY, "end"),
+            "40:38: " + getCheckMessage(MSG_KEY, "list"),
+            "43:38: " + getCheckMessage(MSG_KEY, "forEach"),
+            "45:38: " + getCheckMessage(MSG_KEY, "body"),
+        };
+        verifyWithInlineConfigParser(
+            getPath("InputFinalLocalVariable3.java"),
+            expected);
+    }
 }
