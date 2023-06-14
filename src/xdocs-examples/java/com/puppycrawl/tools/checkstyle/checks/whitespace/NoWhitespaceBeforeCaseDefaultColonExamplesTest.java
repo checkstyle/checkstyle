cae6e21a66f9dc83762d8518dc20cbabeecb598a
stoyanK7 <stoyank127@gmail.com>
2023-06-14 21:54:08 +0200

    Issue #13100: Create tests from NoWhitespaceBeforeCaseDefaultColon xdoc examples

---
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/whitespace/NoWhitespaceBeforeCaseDefaultColonExamplesTest.java
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2023 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace;
+
+import static com.puppycrawl.tools.checkstyle.checks.whitespace.NoWhitespaceBeforeCaseDefaultColonCheck.MSG_KEY;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+
+public class NoWhitespaceBeforeCaseDefaultColonExamplesTest extends AbstractModuleTestSupport {
+    @Override
+    protected String getResourceLocation() {
+        return "xdocs-examples";
+    }
+
+    @Override
+    protected String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/checks/whitespace/nowhitespacebeforecasedefaultcolon";
+    }
+
+    @Test
+    public void testExample1() throws Exception {
+        final String[] expected = {
+            "11:14: " + getCheckMessage(MSG_KEY, ":"),
+            "15:15: " + getCheckMessage(MSG_KEY, ":"),
+            "23:15: " + getCheckMessage(MSG_KEY, ":"),
+            "27:11: " + getCheckMessage(MSG_KEY, ":"),
+            "34:34: " + getCheckMessage(MSG_KEY, ":"),
+        };
+
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
+    }
+}
---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nowhitespacebeforecasedefaultcolon/Example1.java
@@ -0,0 +1,38 @@
+/*
+NoWhitespaceBeforeCaseDefaultColon
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.nowhitespacebeforecasedefaultcolon;
+
+class Example1 {
+  void example() {
+    switch(1) {
+      case 1 : // violation '':' is preceded with whitespace'
+          break;
+      case 2: 
+          break;
+      default : // violation '':' is preceded with whitespace'
+          break;
+    }
+
+    switch(2) {
+      case 2:
+          break;
+      case 3, 4
+              : break; // violation '':' is preceded with whitespace'
+      case 4,
+          5: break;
+      default
+          : // violation '':' is preceded with whitespace'
+          break;
+    }
+
+    switch(day) {
+      case MONDAY, FRIDAY, SUNDAY: System.out.println("  6"); break;
+      // violation below '':' is preceded with whitespace'
+      case TUESDAY               : System.out.println("  7"); break;
+    }
+  }
+}
