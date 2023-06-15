fcfd0989ac8f938a1ed668903efa27703118844b
Kevin222004 <kevinpatel222004@gmail.com>
2023-06-15 14:13:53 +0530

    Issue #13109: Kill mutation for ImportOrderCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrderTestTrimInOption.java
@@ -0,0 +1,30 @@
+/*
+ImportOrder
+option = above\t
+groups = java, org
+ordered = (default)true
+separated = true
+separatedStaticGroups = (default)false
+caseSensitive = (default)true
+staticGroups = (default)
+sortStaticImportsAlphabetically = (default)false
+useContainerOrderingForStatic = (default)false
+tokens = (default)STATIC_IMPORT
+
+
+*/
+
+
+package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
+
+import static java.lang.Math.*; // ok
+
+import static org.antlr.v4.runtime.CommonToken.*; // ok
+import static org.antlr.v4.runtime.CommonToken.*;  // ok
+
+import java.util.Set; // violation 'Wrong order for 'java.util.Set' import.'
+
+import org.junit.Test; // ok
+
+public class InputImportOrderTestTrimInOption {
+}
