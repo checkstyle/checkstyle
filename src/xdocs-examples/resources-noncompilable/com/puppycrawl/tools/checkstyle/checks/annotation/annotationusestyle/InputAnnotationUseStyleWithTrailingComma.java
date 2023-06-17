c3304270034274ace26c44784d42562150d9b56f
Nick Mancuso <nicholas.mancuso13@gmail.com>
2023-06-17 08:40:08 -0400

    minor: test commit, do not merge

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/annotation/annotationusestyle/InputAnnotationUseStyleWithTrailingComma.java
@@ -0,0 +1,29 @@
+/*
+AnnotationUseStyle
+elementStyle = ignore
+closingParens = ignore
+trailingArrayComma = ALWAYS
+
+
+*/
+
+//non-compiled with eclipse: The value for annotation attribute must be a constant expression
+package com.puppycrawl.tools.checkstyle.checks.annotation.annotationusestyle;
+
+public class InputAnnotationUseStyleWithTrailingComma
+{
+    @Test(value={(false) ? "" : "foo",}, more={(true) ? "" : "bar",}) // ok
+    /**
+
+    */
+    enum P {
+        L,
+        Y;
+    }
+
+}
+
+@interface Test {
+    String[] value();
+    String[] more() default {};
+}
