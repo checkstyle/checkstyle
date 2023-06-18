47ba544385d5fed2a33c120aff5d54e9525b51b2
Kevin222004 <kevinpatel222004@gmail.com>
2023-06-18 22:55:59 +0530

    Issue #13109: Kill mutation for UnusedImportCheck-2

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImports3.java
@@ -0,0 +1,45 @@
+/*
+UnusedImports
+processJavadoc = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
+
+import java.awt.AWTException;
+import java.awt.Rectangle; // violation 'Unused import - java.awt.Rectangle'
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent; // violation 'Unused import - java.awt.event.KeyEvent'
+
+public class InputUnusedImports3 extends java.awt.Robot {
+    private int delay;
+
+    protected InputUnusedImports3() throws AWTException {
+        super();
+    }
+
+    /**
+     * Attention: usage of class by short name KeyEvent is not qualified as usage of type
+     * Qualified usage is by javadoc tag 'link'.
+     *
+     * @param keycode which key to press. For example, KeyEvent
+     */
+    public void hitKey(int keycode) {
+        keyPress(keycode);
+        delay();
+    }
+
+    public void clickMouse(int buttons) {
+        mousePress(buttons);
+        delay();
+    }
+
+    public void clickMouse() {
+        clickMouse(InputEvent.BUTTON1_MASK);
+    }
+
+    public void delay() {
+        delay(delay);
+    }
+}
