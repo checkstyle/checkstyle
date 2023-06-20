e432f49757d219b83c0861ed90d03ee1d8f2b343
stoyanK7 <stoyank127@gmail.com>
2023-06-20 15:15:40 +0200

    Issue #13100: Create tests from SingleSpaceSeparator xdoc examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/singlespaceseparator/Example2.java
@@ -0,0 +1,31 @@
+/*
+SingleSpaceSeparator
+validateComments = true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.singlespaceseparator;
+
+class Example2 {
+  // xdoc section -- start
+  // violation below 'Use a single space'
+  void fun1() {}  // 2 whitespaces before the comment starts
+  // violation below 'Use a single space'
+  void fun2() { return; }  /* 2 whitespaces before the comment starts */
+  // violation below 'Use a single space'
+  /* 2 whitespaces after the comment ends */  int a;
+
+  String s; /* OK, 1 whitespace */
+
+  /**
+   * This is a Javadoc comment
+   */  int b; // 2 whitespaces after the javadoc comment ends
+  // violation above 'Use a single space'
+  float f1; 
+
+  /**
+   * OK, 1 white space after the doc comment ends
+   */ float f2;
+  // xdoc section -- end
+}
