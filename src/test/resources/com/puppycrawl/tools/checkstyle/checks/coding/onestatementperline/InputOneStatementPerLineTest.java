fbb9cb52ed98e26e80be3be58574327c802c7338
Kevin222004 <kevinpatel222004@gmail.com>
2023-06-23 19:31:55 +0530

    Issue #11338: FinalClassCheck should report private classes without constructor

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineTest.java
@@ -0,0 +1,25 @@
+/*
+OneStatementPerLine
+treatTryResourcesAsStatement = (default)false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.onestatementperline;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+
+public class InputOneStatementPerLineTest {
+    void testNestedInLambda() {
+        Runnable r = () -> {
+            try (OutputStream s1 = new PipedOutputStream()) { // ok
+            }
+            catch (IOException e) {
+            }
+        };
+        System.out.println(r);
+    }
+
+}
