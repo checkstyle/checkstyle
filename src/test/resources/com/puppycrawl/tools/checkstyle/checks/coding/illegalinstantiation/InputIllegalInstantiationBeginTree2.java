608599c9a775e468a13b7d85da20a292bcf38509
Kevin222004 <kevinpatel222004@gmail.com>
2023-07-01 13:01:06 +0530

    Issue #13328: Kill mutation for IllegalInstantiationCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalinstantiation/InputIllegalInstantiationBeginTree2.java
@@ -0,0 +1,27 @@
+/*
+IllegalInstantiation
+classes = java.lang.Boolean,com.puppycrawl.tools.checkstyle.checks.coding.illegalinstantiation.\
+          InputIllegalInstantiationBeginTree2.InputModifier
+tokens = (default)CLASS_DEF
+
+
+*/
+
+// ok
+public class InputIllegalInstantiationBeginTree2 {
+
+    void otherInstantiations()
+    {
+        // instantiation of classes in the same package
+        Object o1 = new InputIllegalInstantiationBeginTree2.InputBraces();
+        Object o2 = new InputIllegalInstantiationBeginTree2.InputModifier();
+    }
+
+    private class InputBraces {
+
+    }
+
+    private class InputModifier {
+
+    }
+}
