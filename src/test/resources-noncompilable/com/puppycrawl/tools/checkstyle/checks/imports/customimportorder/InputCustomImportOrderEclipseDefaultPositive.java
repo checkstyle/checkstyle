fa99fb9a7b8bf2ca7b71cb803daafdde0f43c37b
Kevin222004 <kevinpatel222004@gmail.com>
2023-07-09 00:10:19 +0530

    Issue #11214: Specified violation messages for CustomImportOrderCheck

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderEclipseDefaultPositive.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderEclipseDefaultPositive.java
@@ -19,19 +19,19 @@ import static java.io.File.createTempFile;
 import static javax.swing.WindowConstants.*;
 import static org.junit.Assert.assertEquals;
 
-import java.awt.Button; // violation
-import java.awt.Dialog; // violation
-import java.io.InputStream; // violation
+import java.awt.Button; // violation 'Import statement for 'java.awt.Button' is in the wrong order. Should be in the 'STANDARD_JAVA_PACKAGE' group, expecting not assigned imports on this line'
+import java.awt.Dialog; // violation 'Import statement for 'java.awt.Dialog' is in the wrong order. Should be in the 'STANDARD_JAVA_PACKAGE' group, expecting not assigned imports on this line'
+import java.io.InputStream; // violation 'Import statement for 'java.io.InputStream' is in the wrong order. Should be in the 'STANDARD_JAVA_PACKAGE' group, expecting not assigned imports on this line'
 
-import javax.swing.JComponent; // violation
-import javax.swing.JTable; // violation
+import javax.swing.JComponent; // violation 'Import statement for 'javax.swing.JComponent' is in the wrong order. Should be in the 'SPECIAL_IMPORTS' group, expecting not assigned imports on this line'
+import javax.swing.JTable; // violation 'Import statement for 'javax.swing.JTable' is in the wrong order. Should be in the 'SPECIAL_IMPORTS' group, expecting not assigned imports on this line'
 
-import org.junit.Test; // violation
-import org.mockito.Mock; // violation
+import org.junit.Test; // violation 'Import statement for 'org.junit.Test' is in the wrong order. Should be in the 'THIRD_PARTY_PACKAGE' group, expecting not assigned imports on this line'
+import org.mockito.Mock; // violation 'Import statement for 'org.mockito.Mock' is in the wrong order. Should be in the 'THIRD_PARTY_PACKAGE' group, expecting not assigned imports on this line'
 
 import com.some.api.DetailClass;
 
-import sun.tools.java.ArrayType; // violation
+import sun.tools.java.ArrayType; // violation 'Extra separation in import group before 'sun.tools.java.ArrayType''
 
 public class InputCustomImportOrderEclipseDefaultPositive {
 }
--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderNoPackage.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderNoPackage.java
@@ -14,16 +14,16 @@ sortImportsInGroupAlphabetically = true
 import java.io.*;
 
 
-import java.util.*; // violation
+import java.util.*; // violation 'Extra separation in import group before 'java.util.*''
 
-import java.util.HashMap; // violation
+import java.util.HashMap; // violation 'Extra separation in import group before 'java.util.HashMap''
 import java.util.LinkedList;
 // comments between imports
 
-import javax.net.ServerSocketFactory; // violation
+import javax.net.ServerSocketFactory; // violation 'Extra separation in import group before 'javax.net.ServerSocketFactory''
 
 // comments between imports
-import javax.net.SocketFactory; // violation
+import javax.net.SocketFactory; // violation 'Extra separation in import group before 'javax.net.SocketFactory''
 
 class InputCustomImportOrderNoPackage {
 
