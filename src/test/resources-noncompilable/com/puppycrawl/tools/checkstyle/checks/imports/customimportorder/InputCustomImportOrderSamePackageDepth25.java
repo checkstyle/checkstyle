83de97561fac7ad5c2f3382b25173e6088e06ecb
Kumar Kalpadiptya Roy <kalpadiptya.roy@outlook.com>
2023-07-09 11:18:27 +0530

    Issue #11214: Specified violation messages for CustomImportOrderCheck

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderSamePackageDepth25.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderSamePackageDepth25.java
@@ -17,14 +17,14 @@ package java.util.concurrent.locks;
 // SAME_PACKAGE(4) should include only #6
 // SAME_PACKAGE(5) should include no imports because actual package has only 4 domains
 import java.io.File;
-import java.util.*; // violation
-import java.util.List; // violation
-import java.util.StringTokenizer; // violation
-import java.util.concurrent.*; // violation
-import java.util.concurrent.AbstractExecutorService; // violation
-import java.util.concurrent.locks.LockSupport; // violation
-import java.util.regex.Pattern; // violation
-import java.util.regex.Matcher; // violation
+import java.util.*; // violation 'Import statement for 'java.util.*' is in the wrong order. Should be in the .*group, expecting not assigned imports.*'
+import java.util.List; // violation 'Import statement for .*List' is in the wrong order. Should be in the .*group, expecting not assigned imports.*'
+import java.util.StringTokenizer; // violation 'Import statement for .*StringTokenizer' is in the wrong order. Should be in the .*group, expecting not assigned imports on this line.'
+import java.util.concurrent.*; // violation 'Import statement for .*concurrent.*' is in the wrong order. Should be in the .*group, expecting not assigned imports.*'
+import java.util.concurrent.AbstractExecutorService; // violation 'Import statement for .*AbstractExecutorService' is in the wrong order. Should be in the .*group, expecting not assigned imports.*'
+import java.util.concurrent.locks.LockSupport; // violation 'Import statement for .*LockSupport' is in the wrong order. Should be in the .*group, expecting not assigned imports.*'
+import java.util.regex.Pattern; // violation 'Import statement for .*Pattern' is in the wrong order. Should be in the .*group, expecting not assigned imports.*'
+import java.util.regex.Matcher; // violation 'Import statement for .*Matcher' is in the wrong order. Should be in the .*group, expecting not assigned imports.*'
 
 public class InputCustomImportOrderSamePackageDepth25 {
 }
