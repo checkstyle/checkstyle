1b64032f20e20e12e91d7cd78a0bf20a0761705a
Kevin222004 <kevinpatel222004@gmail.com>
2023-07-16 22:27:07 +0530

    Issue #13109: Kill mutation for FileText3

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/api/filetext/InputFileText.java
@@ -0,0 +1,11 @@
+package com.puppycrawl.tools.checkstyle.api.filetext;
+
+public class InputFileText {
+}
+
+/**
+ * Test case for the "design for inheritance" check.
+ * @author Lars Kühne
+ **/
+class Extra {
+}
