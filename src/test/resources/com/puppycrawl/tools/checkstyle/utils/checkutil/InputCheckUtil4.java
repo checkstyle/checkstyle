19f4925489b6d550f0ec414aa654c4e534413ac1
Kevin222004 <kevinpatel222004@gmail.com>
2023-07-25 22:51:33 +0530

    Issue #13321: Kill mutation for CheckUtil-5

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/utils/checkutil/InputCheckUtil4.java
@@ -0,0 +1,21 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.javadoc.JavadocMethodCheck
+accessModifiers = public
+
+*/
+
+package com.puppycrawl.tools.checkstyle.utils.checkutil;
+
+// ok
+public interface InputCheckUtil4 {
+
+    /**
+     *  Description.
+     *
+     * @param it it
+     * @param column coloumn
+     */
+    private InputCheckUtil4 method(int it, int column) {
+        return null;
+    }
+}
