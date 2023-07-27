8e07b4e1da33ab7d76e9eca921ff6857dac3bca7
Stoyan Kostadinov <stoyank127@gmail.com>
2023-07-27 14:19:40 +0300

    Issue #13205: Add links to examples' paragraphs

--- src/site/resources/js/anchors.js
+++ src/site/resources/js/anchors.js
@@ -56,6 +56,25 @@
             anchor.appendChild(a);
             anchorItem.appendChild(anchor);
         });
+
+        const exampleDivs = document.querySelectorAll('p[id^="Example"]');
+        [].forEach.call(exampleDivs, function (exampleDiv) {
+            const name = exampleDiv.id;
+            const link = "" + url + "#" + name + "";
+
+            const a = document.createElement("a");
+            a.setAttribute("href", link);
+
+            const image = document.createElement("img");
+            image.setAttribute("src", `${relativePath}/images/anchor.png`);
+
+            const anchor = document.createElement("div");
+            anchor.className = "anchor";
+
+            a.appendChild(image);
+            anchor.appendChild(a);
+            exampleDiv.appendChild(anchor);
+        });
     });
 }());
 
