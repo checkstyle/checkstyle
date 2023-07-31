7121a897af92098a8b17f025635ae5293c93894f
Kevin222004 <kevinpatel222004@gmail.com>
2023-07-31 17:28:13 +0530

    Issue #13321: Kill mutation for RegexpCheck-3

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/InputRegexpCheckB4.java
@@ -0,0 +1,14 @@
+/*
+Regexp
+format = .*
+errorLimit = 18
+duplicateLimit = 19
+illegalPattern = false
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.regexp.regexp;
+
+public class InputRegexpCheckB4 {
+// ok
+}
