d0eac052968566dda21642bdb061bb7aa665f1f1
Kevin222004 <kevinpatel222004@gmail.com>
2023-08-03 01:23:16 +0530

    Issue #13501: Kill mutation for ImportControlCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/InputImportControlBeginTree2.java
@@ -0,0 +1,15 @@
+/*
+ImportControl
+file = (file)InputImportControlBeginTree.xml
+path = (default).*
+
+
+*/
+
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+// ok
+public class InputImportControlBeginTree2 {
+}
