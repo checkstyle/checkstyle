cd37eaaf68ac6a44d371f9786814644bdafc2b58
Kevin222004 <kevinpatel222004@gmail.com>
2023-08-15 20:02:11 +0530

    Issue #13321: Kill mutation for SummaryJavaDocCheck-11

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/SummaryJavadocCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/SummaryJavadocCheck.java
@@ -636,18 +636,19 @@ public class SummaryJavadocCheck extends AbstractJavadocCheck {
         boolean found = false;
         final DetailNode[] children = root.getChildren();
 
-        for (int i = 0; !found; i++) {
-            final DetailNode child = children[i];
-            if (child.getType() == JavadocTokenTypes.JAVADOC_INLINE_TAG
-                    && child.getChildren()[1].getType() == JavadocTokenTypes.INHERIT_DOC_LITERAL) {
-                found = true;
-            }
-            else if (child.getType() != JavadocTokenTypes.LEADING_ASTERISK
-                    && !CommonUtil.isBlank(child.getText())) {
+        int i = 0;
+        while (i != children.length) {
+            if (found || children[i].getType() == JavadocTokenTypes.TEXT
+                    && !CommonUtil.isBlank(children[i].getText())) {
                 break;
             }
+            if (children[i].getType() == JavadocTokenTypes.JAVADOC_INLINE_TAG
+                    && children[i].getChildren()[1].getType()
+                    == JavadocTokenTypes.INHERIT_DOC_LITERAL) {
+                found = true;
+            }
+            i++;
         }
-
         return found;
     }
 
