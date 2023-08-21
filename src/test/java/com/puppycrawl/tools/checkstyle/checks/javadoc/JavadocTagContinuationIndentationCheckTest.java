86966ea02a6ef131a3dc5101dcad9ad363517ea5
Kevin222004 <kevinpatel222004@gmail.com>
2023-08-21 21:14:09 +0530

    Issue #13501: Kill mutation for JavadocTagContinuationIndentationCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTagContinuationIndentationCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTagContinuationIndentationCheckTest.java
@@ -136,4 +136,13 @@ public class JavadocTagContinuationIndentationCheckTest
                 expected);
     }
 
+    @Test
+    public void testContinuationIndentation() throws Exception {
+        final String[] expected = {
+            "23: " + getCheckMessage(MSG_KEY, 4),
+        };
+        verifyWithInlineConfigParser(
+                getPath("InputJavadocTagContinuationIndentation1.java"),
+                expected);
+    }
 }
