5a6322fbe9448120e088d17cdc3da2e40d948b9b
Kevin222004 <kevinpatel222004@gmail.com>
2023-08-22 23:31:41 +0530

    Issue #13501: Kill mutation for JavadocStyleCheck8

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocstyle/InputJavadocStyleCheck1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocstyle/InputJavadocStyleCheck1.java
@@ -10,6 +10,9 @@ public class InputJavadocStyleCheck1 {
 
     private int field4;
 
+   /** NM Webapp address.**/
+   public static final String NM_WEBAPP_ADDRESS = "webapp.address";
+
     /** // violation 'First sentence should end with a period'
      * <thead> <br> </thead>
      * <tfoot> <AREA ALT="alt" Coordination="100,0,200,50" HREF="/href/"> </tfoot> comment
