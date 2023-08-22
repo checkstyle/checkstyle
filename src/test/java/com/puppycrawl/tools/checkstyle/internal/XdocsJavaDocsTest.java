176f818971be69fb772f0ba06f4549561efa6c8d
Stoyan Kostadinov <stoyank127@gmail.com>
2023-08-22 16:56:03 +0200

    Issue #13100: Create tests from header category checks xdoc examples

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsJavaDocsTest.java
@@ -181,6 +181,8 @@ public class XdocsJavaDocsTest extends AbstractModuleTestSupport {
         "CommentsIndentation",
         "DescendantToken",
         "FinalParameters",
+        "Header",
+        "RegexpHeader",
         "Indentation",
         "NewlineAtEndOfFile",
         "NoCodeInFile",
