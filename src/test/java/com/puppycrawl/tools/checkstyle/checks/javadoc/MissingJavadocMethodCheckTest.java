48ec2e67a31ef2df23152486f04d1900512fcd63
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-08-24 16:25:03 +0530

    Issue #13596: Moved utility methods from CheckUtil to MissingJavadocMethodCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/MissingJavadocMethodCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/MissingJavadocMethodCheckTest.java
@@ -22,10 +22,14 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc;
 import static com.google.common.truth.Truth.assertWithMessage;
 import static com.puppycrawl.tools.checkstyle.checks.javadoc.MissingJavadocMethodCheck.MSG_JAVADOC_MISSING;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.CheckUtilTest;
 import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
 
 public class MissingJavadocMethodCheckTest extends AbstractModuleTestSupport {
@@ -454,5 +458,39 @@ public class MissingJavadocMethodCheckTest extends AbstractModuleTestSupport {
         verifyWithInlineConfigParser(
                 getNonCompilablePath("InputMissingJavadocMethod1.java"),
                 expected);
+    }
+
+    @Test
+    public void testIsGetterMethod() throws Exception {
+        final File testFile =
+                new File(getPath("InputMissingJavadocMethodSetterGetter3.java"));
+        final DetailAST notGetterMethod =
+                CheckUtilTest.getNodeFromSpecificFile(TokenTypes.METHOD_DEF, testFile);
+        final DetailAST getterMethod = notGetterMethod.getNextSibling().getNextSibling();
+
+        assertWithMessage("Invalid result: AST provided is getter method")
+                .that(MissingJavadocMethodCheck.isGetterMethod(getterMethod))
+                .isTrue();
+        assertWithMessage("Invalid result: AST provided is not getter method")
+                .that(MissingJavadocMethodCheck.isGetterMethod(notGetterMethod))
+                .isFalse();
+    }
+
+    @Test
+    public void testIsSetterMethod() throws Exception {
+        final File testFile =
+                new File(getPath("InputMissingJavadocMethodSetterGetter3.java"));
+        final DetailAST firstClassMethod =
+                CheckUtilTest.getNodeFromSpecificFile(TokenTypes.METHOD_DEF, testFile);
+        final DetailAST setterMethod =
+                firstClassMethod.getNextSibling().getNextSibling().getNextSibling();
+        final DetailAST notSetterMethod = setterMethod.getNextSibling();
+
+        assertWithMessage("Invalid result: AST provided is setter method")
+                .that(MissingJavadocMethodCheck.isSetterMethod(setterMethod))
+                .isTrue();
+        assertWithMessage("Invalid result: AST provided is not setter method")
+                .that(MissingJavadocMethodCheck.isSetterMethod(notSetterMethod))
+                .isFalse();
     }
 }
