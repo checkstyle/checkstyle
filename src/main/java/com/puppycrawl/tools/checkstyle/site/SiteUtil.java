41559f21f45905f8c34b3604e2e74a279bc2ed8a
Stoyan Kostadinov <stoyank127@gmail.com>
2023-08-25 14:43:51 +0200

    Issue #13610: Use parent module macro in filters templates

--- src/main/java/com/puppycrawl/tools/checkstyle/site/SiteUtil.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/site/SiteUtil.java
@@ -248,6 +248,17 @@ public final class SiteUtil {
             parentClass = parentClass.getSuperclass();
         }
 
+        // If parent class is not found, check interfaces
+        if (parentModuleName == null || parentModuleName.isEmpty()) {
+            final Class<?>[] interfaces = moduleClass.getInterfaces();
+            for (Class<?> interfaceClass : interfaces) {
+                parentModuleName = CLASS_TO_PARENT_MODULE.get(interfaceClass);
+                if (parentModuleName != null) {
+                    break;
+                }
+            }
+        }
+
         if (parentModuleName == null || parentModuleName.isEmpty()) {
             final String message = String.format(Locale.ROOT,
                     "Failed to find parent module for %s", moduleClass.getSimpleName());
