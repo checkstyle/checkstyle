2cf9a2d5660150b5aba4407e0c436f33dee66bb9
Nick Mancuso <nicholas.mancuso13@gmail.com>
2023-08-26 00:45:55 -0400

    minor: add check for package name

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/design/finalclass/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/design/finalclass/Example1.java
@@ -6,43 +6,53 @@
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.design.finalclass;
+
 // xdoc section -- start
-final class Example1 {
-  private Example1() { }
-}
+public class Example1 { // ok, since it has a public constructor
 
-class A { // violation, 'Class A should be declared as final.'
-  private A() { }
-}
-
-class B { // ok, since it has a public constructor
-  int field1;
-  String field2;
-  private B(int value) {
-    this.field1 = value;
-    this.field2 = " ";
-  }
-  public B(String value) {
-    this.field2 = value;
-    this.field1 = 0;
-  }
-}
-
-class TestAnonymousInnerClasses { // ok, class has an anonymous inner class.
-  public static final TestAnonymousInnerClasses ONE
-          = new TestAnonymousInnerClasses() {};
-
-  private TestAnonymousInnerClasses() {
-  }
-}
-
-class Class1 {
-
-  private class Class2 { // violation, 'Class Class2 should be declared as final'
+  final class A {
+    private A() {
+    }
   }
 
-  public class Class3 {
+  class B { // violation, 'Class B should be declared as final.'
+    private B() {
+    }
   }
 
+  class C { // ok, since it has a public constructor
+    int field1;
+    String field2;
+
+    private C(int value) {
+      this.field1 = value;
+      this.field2 = " ";
+    }
+
+    public C(String value) {
+      this.field2 = value;
+      this.field1 = 0;
+    }
+  }
+
+  class AnonymousInner { // ok, class has an anonymous inner class.
+    public final AnonymousInner ONE
+            = new AnonymousInner() {
+            };
+
+    private AnonymousInner() {
+    }
+  }
+
+  class Class1 {
+
+    private class Class2 { // violation, 'Class Class2 should be declared as final'
+    }
+
+    public class Class3 {
+    }
+
+  }
 }
 // xdoc section -- end
