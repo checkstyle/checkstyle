8b723f8060ee7de9446166b0a78691d28661a0d3
Kevin222004 <kevinpatel222004@gmail.com>
2023-08-27 07:29:30 +0530

    Issue #13501: Kill mutation for DetailAstImpl-3

--- src/main/java/com/puppycrawl/tools/checkstyle/DetailAstImpl.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/DetailAstImpl.java
@@ -147,10 +147,8 @@ public final class DetailAstImpl implements DetailAST {
 
             if (sibling != null) {
                 astImpl.setNextSibling(sibling);
-                sibling.previousSibling = astImpl;
             }
 
-            astImpl.previousSibling = this;
             setNextSibling(astImpl);
         }
     }
