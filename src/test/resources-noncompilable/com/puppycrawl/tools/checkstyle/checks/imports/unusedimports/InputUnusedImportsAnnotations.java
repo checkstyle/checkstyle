8fe8c70b8a11683d304d98f5b6b6b0806735e8a2
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-09-01 22:50:49 +0530

    Issue #13213: Remove '//ok' comments from Input files (RedundantImport and UnusedImport)

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsAnnotations.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsAnnotations.java
@@ -12,4 +12,4 @@ processJavadoc = (default)true
 @MyAnnotation
 package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
 
-import com.puppycrawl.tools.checkstyle.MyAnnotation; // ok
+import com.puppycrawl.tools.checkstyle.MyAnnotation;
