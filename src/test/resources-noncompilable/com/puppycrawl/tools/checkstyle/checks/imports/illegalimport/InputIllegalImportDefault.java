75ea355a337b3466fd8419429071d255e09386be
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-09-04 13:35:54 +0530

    Issue #13213: Remove '//ok' comments from Input files (IllegalImport)

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/illegalimport/InputIllegalImportDefault.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/illegalimport/InputIllegalImportDefault.java
@@ -10,12 +10,12 @@ regexp = true
 //non-compiled with javac: contains sun package so IDEA marks it as build problem
 package com.puppycrawl.tools.checkstyle.checks.imports.illegalimport;
 
-import java.io.*; // ok
-import sun.misc.*; // ok
-import javax.swing.BorderFactory; // ok
-import static java.io.File.listRoots; // ok
+import java.io.*;
+import sun.misc.*;
+import javax.swing.BorderFactory;
+import static java.io.File.listRoots;
 import sun.reflect.*; // violation
 
-import java.awt.Component; // ok
+import java.awt.Component;
 
 class InputIllegalImportDefault {}
