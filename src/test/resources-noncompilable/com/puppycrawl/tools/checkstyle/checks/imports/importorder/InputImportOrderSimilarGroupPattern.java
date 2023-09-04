fa5a26772e2e5cca5acea59ddde7bc4ad9c179e1
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-09-04 17:17:15 +0530

    Issue #13213: Remove '//ok' comments from Input files (ImportOrder)

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrderSimilarGroupPattern.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrderSimilarGroupPattern.java
@@ -17,8 +17,8 @@ tokens = (default)STATIC_IMPORT
 //non-compiled with javac: contains specially crafted set of imports for testing
 package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
 
-import java.util.java.io.File; // ok
-import java.io.File; // ok
+import java.util.java.io.File;
+import java.io.File;
 
 public class InputImportOrderSimilarGroupPattern {
 
