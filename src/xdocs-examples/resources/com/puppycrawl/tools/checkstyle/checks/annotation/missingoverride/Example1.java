f50e2ef1dbfce8ab3414c131933d70b565bc55b9
Guneetsinghtuli <guneetsinghtuli@gmail.com>
2023-09-09 20:10:23 +0530

    Issue #13345: Enable MissingOverrideCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/missingoverride/Example1.java
@@ -0,0 +1,42 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MissingOverride"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.missingoverride;
+
+// xdoc section -- start
+class Example1{
+  public void test1(){}
+
+  public void test2(){}
+
+}
+
+class A extends Example1 {
+
+  /** {@inheritDoc} */
+  @Override
+  public void test1() { // OK
+
+  }
+
+  /** {@inheritDoc} */
+  public void test2() { // violation, 'include @java.lang.Override'
+
+  }
+
+  /** {@inheritDoc} */
+  private void test3() { // violation, 'The Javadoc {@inheritDoc}.'
+
+  }
+
+  /** {@inheritDoc} */
+  public static void test4() { // violation, 'The Javadoc {@inheritDoc}.'
+
+  }
+}
+// xdoc section -- end
