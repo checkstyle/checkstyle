64edb8b4c9428b58b00d5c4e97f075b6cb13531f
Roman Ivanov <ivanov-jr@mail.ru>
2023-09-09 07:01:57 -0700

    Issue #13501: Input based test

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportOrderCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportOrderCheck.java
@@ -686,7 +686,7 @@ public class ImportOrderCheck
      * @param name the import name to find.
      * @return group number for given import name.
      */
-    private static int getGroupNumber(Pattern[] patterns, String name) {
+    public static int getGroupNumber(Pattern[] patterns, String name) {
         int bestIndex = patterns.length;
         int bestEnd = -1;
         int bestPos = Integer.MAX_VALUE;
---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/InputImportOrder6.java
@@ -0,0 +1,39 @@
+/*
+ImportOrder
+option = (default)under
+groups = awt, jar, .util., .jar., .util, jarInputStream
+ordered = (default)true
+separated = (default)false
+separatedStaticGroups = (default)false
+caseSensitive = (default)true
+staticGroups = (default)
+sortStaticImportsAlphabetically = (default)false
+useContainerOrderingForStatic = (default)false
+tokens = (default)STATIC_IMPORT
+
+
+*/
+
+//non-compiled with javac: contains specially crafted set of imports for testing
+package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Dialog; // violation 'Wrong order for 'java.awt.Dialog' import.'
+import java.awt.event.ActionEvent;
+import static java.awt.Button.ABORT
+;
+import javax.swing.JComponent; // violation 'Wrong order for 'javax.swing.JComponent' import.'
+import javax.swing.JTable;
+import java.io.File; // violation 'Wrong order for 'java.io.File' import.'
+import static java.io.File.createTempFile;
+import java.io.IOException; // violation 'Wrong order for 'java.io.IOException' import.'
+import java.io.InputStream;
+import java.io.Reader;
+import static javax.swing.WindowConstants.*;
+
+import static sun.tools.util.ModifierFilter.ALL_ACCESS; // violation 'Extra separation in import group before .*'
+import static sun.tools.util.ModifierFilter.PACKAGE;
+import java.util.jar.JarInputStream; // violation 'Wrong order for 'java.util.jar.JarInputStream' import.'
+
+public class InputImportOrder6 {
+}
