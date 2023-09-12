91e66735876582108057b4b7bedf7e609f972fd1
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-09-12 10:07:45 +0530

    Issue #13345: Enable examples tests for MissingDeprecated

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/missingdeprecated/Example1.java
@@ -0,0 +1,35 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MissingDeprecated"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.missingdeprecated;
+
+// xdoc section -- start
+class Example1 {
+  @Deprecated
+  public static final int MY_CONST = 13; // ok
+
+  /** This javadoc is missing deprecated tag. */
+  // violation below '@deprecated Javadoc tag with description.'
+  @Deprecated
+  public static final int COUNTER = 10;
+
+  /**
+   * @deprecated
+   * <p></p>
+   */
+  @Deprecated
+    public static final int NUM = 123456; // ok
+
+  /**
+   * @deprecated
+   *  <p>
+   */
+  @Deprecated
+  public static final int CONST = 12; // ok
+}
+// xdoc section -- end
