2371af12bec301aefc8a9afd099dd608acb56f16
Kevin222004 <kevinpatel222004@gmail.com>
2023-09-24 22:13:15 +0530

    Issue #13672: Kill mutation for Xpath

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/filters/suppressionxpathfilter/InputSuppressionXpathFilter5.java
@@ -0,0 +1,19 @@
+/*
+com.puppycrawl.tools.checkstyle.filters.SuppressionXpathFilter
+file = (file)InputSuppressionXpathFilter5.xml
+optional = (default)false
+
+com.puppycrawl.tools.checkstyle.checks.coding.IllegalTokenTextCheck
+format = [^a-zA-z0-9]*
+ignoreCase = (default)false
+message = (default)
+tokens = CHAR_LITERAL
+
+*/
+
+package com.puppycrawl.tools.checkstyle.filters.suppressionxpathfilter;
+
+public class InputSuppressionXpathFilter5 {
+
+    char a = '&'; // filtered violation
+}
