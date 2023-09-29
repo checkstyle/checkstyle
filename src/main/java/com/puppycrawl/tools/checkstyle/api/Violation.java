56af76110fd2b6acee4e47db63b7bd4511dc6c15
Kevin222004 <kevinpatel222004@gmail.com>
2023-09-29 22:02:16 +0530

    Issue #13672: Kill mutation using utility class

--- src/main/java/com/puppycrawl/tools/checkstyle/api/Violation.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/Violation.java
@@ -25,6 +25,7 @@ import java.util.Locale;
 import java.util.Objects;
 
 import com.puppycrawl.tools.checkstyle.LocalizedMessage;
+import com.puppycrawl.tools.checkstyle.utils.UnmodifiableCollectionUtil;
 
 /**
  * Represents a violation that can be localised. The translations come from
@@ -111,7 +112,7 @@ public final class Violation
             this.args = null;
         }
         else {
-            this.args = Arrays.copyOf(args, args.length);
+            this.args = UnmodifiableCollectionUtil.copyOfArray(args, args.length);
         }
         this.bundle = bundle;
         this.severityLevel = severityLevel;
