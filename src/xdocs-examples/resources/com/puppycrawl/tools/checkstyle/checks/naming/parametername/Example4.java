b93aacc663fb76a96d2224152bc24d0d6e1174d1
Rohanraj123 <rajrohan88293@gmail.com>
2023-10-01 11:39:38 +0530

    Issue #13345: Enable TestCases for ParameterNameCheckExamplesTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example4.java
@@ -8,10 +8,12 @@
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.naming.parametername;
+
 // xdoc section -- start
-class MyClass {
-  void method1(int v1) {} // OK
-  void method2(int v_2) {} // violation, name 'v_2' must match pattern '^[a-z][a-zA-Z0-9]+$'
-  void method3(int V3) {} // violation, name 'V3' must match pattern '^[a-z][a-zA-Z0-9]+$'
+class Example4 {
+  void method1(int v1) {}
+  void method2(int v_2) {} // violation
+  void method3(int V3) {} // violation
 }
 // xdoc section -- end
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example5.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example5.java
@@ -17,13 +17,13 @@
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.naming.parametername;
+
 // xdoc section -- start
-class MyClass {
-  void method1(int v1) {} // OK
-  protected method2(int V2) {} // violation, Parameter name 'V2'
-                               // must match pattern '^[a-z]([a-z0-9][a-zA-Z0-9]*)?$'
-  private method3(int a) {} // OK
-  public method4(int b) {} // violation, Parameter name 'b'
-                           // must match pattern '^[a-z][a-z0-9][a-zA-Z0-9]*$'
+class Example5 {
+  void method1(int v1) {}
+  private void method2(int V) {} // violation "Parameter name 'V' must match pattern"
+  private void method3(int a) {}
+  public void method4(int b) {} // violation "Parameter name 'b' must match pattern"
 }
 // xdoc section -- end
