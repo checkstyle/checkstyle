607fca5ea231f443fb1c921aa7c640be31581e44
Rohanraj123 <rajrohan88293@gmail.com>
2023-10-01 11:39:38 +0530

    Issue #13345: Enable TestCases for ParameterNameCheckExamplesTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example1.java
@@ -6,9 +6,11 @@
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.naming.parametername;
+
 // xdoc section -- start
-class MyClass {
-  void method1(int v1) {} // OK
-  void method2(int V2) {} // violation, name 'V2' must match pattern '^[a-z][a-zA-Z0-9]*$'
+class Example1 {
+  void method1(int v1) {}
+  void method2(int V2) {} // violation
 }
 // xdoc section -- end
