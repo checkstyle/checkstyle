ba0d373c72d72a5d639de6f5975d2d51116167c5
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-10-03 17:05:09 +0530

    Issue #13775: 'javadoc.unclosedHtml' message not present in documentation of most Javadoc Checks

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/SingleLineJavadocCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/SingleLineJavadocCheck.java
@@ -70,6 +70,9 @@ import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
  * {@code javadoc.parse.rule.error}
  * </li>
  * <li>
+ * {@code javadoc.unclosedHtml}
+ * </li>
+ * <li>
  * {@code javadoc.wrong.singleton.html.tag}
  * </li>
  * <li>
