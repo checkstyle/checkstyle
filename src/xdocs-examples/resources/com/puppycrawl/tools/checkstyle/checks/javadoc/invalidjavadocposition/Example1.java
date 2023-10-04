776c04b84b84f1df9f48071605796f3fc603995b
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-10-04 12:14:26 +0530

    Issue #13345: Enable examples tests for InvalidJavadocPosition

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/invalidjavadocposition/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/invalidjavadocposition/Example1.java
@@ -6,12 +6,16 @@
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.javadoc.invalidjavadocposition;
+
 // xdoc section -- start
 @SuppressWarnings("serial")
+// violation below, 'Javadoc comment is placed in the wrong location'
 /**
  * This comment looks like Javadoc but it is at an invalid location.
- * Therefore, the text will not get into TestClass.html and the check will produce a violation.
+ * Therefore, the text will not get into TestClass.html.
+ * And, the check will produce a violation.
  */
-public class TestClass {
+public class Example1 {
 }
 // xdoc section -- end
