d795480950f399636c1d8f705c2696730de3ea0c
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-10-05 07:49:50 +0530

    Issue #13345: Enable example tests for JavadocContentLocation

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoccontentlocation/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoccontentlocation/Example1.java
@@ -1,17 +1,23 @@
 /*xml
 <module name="Checker">
   <module name="TreeWalker">
-    <module name="JavadocContentLocationCheck"/>
+    <module name="JavadocContentLocation"/>
   </module>
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoccontentlocation;
+
+class Example1 {
+
 // xdoc section -- start
 /** This comment causes a violation because it starts from the first line
-  * and spans several lines.
-  */
+ * and spans several lines.
+ */
 /**
-  * This comment is OK because it starts from the second line.
-  */
+ * This comment is OK because it starts from the second line.
+ */
 /** This comment is OK because it is on the single-line. */
 // xdoc section -- end
+
+}
