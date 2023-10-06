1e5ade4c692452da09e76961204c0bc359076896
Aayush <aaayush@redhat.com>
2023-10-06 11:42:10 +0530

    Issue #13347: False Positive and Negative in SummaryJavaDocCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc/InputSummaryJavadoc1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc/InputSummaryJavadoc1.java
@@ -10,7 +10,6 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc.summaryjavadoc;
 
 public class InputSummaryJavadoc1 {
 
-    // violation 2 lines below 'Forbidden summary fragment'
     /**
      * {@summary Returns the customer ID.
      *  This method returns. }
