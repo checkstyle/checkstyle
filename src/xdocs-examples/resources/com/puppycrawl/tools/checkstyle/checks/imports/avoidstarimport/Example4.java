a117e254292dc39bcc05f4c0c9d3fe01b0ed74c8
Tanay <tdmorakhia26@gmail.com>
2023-10-08 14:06:29 +0530

    Issue #13345: Enable AvoidStarImportCheckExamplesTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport/Example4.java
@@ -3,16 +3,19 @@
   <module name="TreeWalker">
     <module name="AvoidStarImport">
       <property name="allowStaticMemberImports" value="true"/>
-      <property name="excludes" value="java.io,java.net"/>
     </module>
   </module>
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.imports.avoidstarimport;
+
 // xdoc section -- start
-import java.util.Scanner;         // OK
-import java.io.*;                 // OK
-import static java.lang.Math.*;   // OK
+import java.util.Scanner;
+import java.io.*;                 // violation
+import static java.lang.Math.*;
 import java.util.*;               // violation
-import java.net.*;                // OK
+import java.net.*;                // violation
 // xdoc section -- end
+
+class Example4 {}
