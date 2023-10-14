644175d1044eca18c8fb1e6986b2a1e17a0c5c3b
Rohanraj123 <rajrohan88293@gmail.com>
2023-10-14 17:13:44 +0530

    Issue #13345: Enable test cases for avoidescapedunicodecharacter

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/AvoidEscapedUnicodeCharactersCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/AvoidEscapedUnicodeCharactersCheckExamplesTest.java
@@ -19,12 +19,12 @@
 
 package com.puppycrawl.tools.checkstyle.checks;
 
-import org.junit.jupiter.api.Disabled;
+import static com.puppycrawl.tools.checkstyle.checks.AvoidEscapedUnicodeCharactersCheck.MSG_KEY;
+
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
 
-@Disabled("until https://github.com/checkstyle/checkstyle/issues/13345")
 public class AvoidEscapedUnicodeCharactersCheckExamplesTest
         extends AbstractExamplesModuleTestSupport {
     @Override
@@ -35,45 +35,60 @@ public class AvoidEscapedUnicodeCharactersCheckExamplesTest
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-
+            "16:24: " + getCheckMessage(MSG_KEY),
+            "18:24: " + getCheckMessage(MSG_KEY),
+            "20:24: " + getCheckMessage(MSG_KEY),
+            "22:24: " + getCheckMessage(MSG_KEY),
+            "26:12: " + getCheckMessage(MSG_KEY),
         };
 
-        verifyWithInlineConfigParser(getPath("Example1.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
     }
 
     @Test
     public void testExample2() throws Exception {
         final String[] expected = {
-
+            "18:24: " + getCheckMessage(MSG_KEY),
+            "20:24: " + getCheckMessage(MSG_KEY),
+            "22:24: " + getCheckMessage(MSG_KEY),
+            "24:24: " + getCheckMessage(MSG_KEY),
         };
 
-        verifyWithInlineConfigParser(getPath("Example2.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example2.java"), expected);
     }
 
     @Test
     public void testExample3() throws Exception {
         final String[] expected = {
-
+            "18:24: " + getCheckMessage(MSG_KEY),
+            "20:24: " + getCheckMessage(MSG_KEY),
+            "22:24: " + getCheckMessage(MSG_KEY),
+            "28:12: " + getCheckMessage(MSG_KEY),
         };
 
-        verifyWithInlineConfigParser(getPath("Example3.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example3.java"), expected);
     }
 
     @Test
     public void testExample4() throws Exception {
         final String[] expected = {
-
+            "18:24: " + getCheckMessage(MSG_KEY),
+            "22:24: " + getCheckMessage(MSG_KEY),
+            "24:24: " + getCheckMessage(MSG_KEY),
         };
 
-        verifyWithInlineConfigParser(getPath("Example4.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example4.java"), expected);
     }
 
     @Test
     public void testExample5() throws Exception {
         final String[] expected = {
-
+            "18:24: " + getCheckMessage(MSG_KEY, "unitAbbrev1"),
+            "20:24: " + getCheckMessage(MSG_KEY),
+            "22:24: " + getCheckMessage(MSG_KEY),
+            "24:24: " + getCheckMessage(MSG_KEY),
         };
 
-        verifyWithInlineConfigParser(getPath("Example5.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example5.java"), expected);
     }
 }
