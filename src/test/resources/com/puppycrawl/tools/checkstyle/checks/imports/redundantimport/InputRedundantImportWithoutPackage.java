2fe306bc924c5a92431045efb61d6e58810e5602
Suniti <suniti0804@gmail.com>
2023-10-18 18:06:01 +0530

    Issue #4845: Removed checkstyle dependencies from InputReduntantImportWithoutPackage

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/redundantimport/InputRedundantImportWithoutPackage.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/redundantimport/InputRedundantImportWithoutPackage.java
@@ -3,8 +3,8 @@ RedundantImport
 
 
 */
+package com.puppycrawl.tools.checkstyle.checks.imports.redundantimport;
 
-import com.puppycrawl.tools.checkstyle.checks.imports.redundantimport.*;
 import java.io.*;
 import java.lang.*; // violation 'Redundant import from the java.lang package - java.lang.*'
 import java.lang.String; // violation 'Redundant import from the java.lang package - java.lang.String'
