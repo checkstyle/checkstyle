12d9e51799988da3ad2a6b766fc9abb0b97ff9b4
Pubudu Fernando <pubudu.fernando@gmail.com>
2023-10-27 00:08:41 -0400

    Issue #4845: Remove checkstyle dependencies for indentation tests

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationAnonymousClassInMethodCurlyOnNewLine.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationAnonymousClassInMethodCurlyOnNewLine.java
@@ -1,7 +1,5 @@
 package com.puppycrawl.tools.checkstyle.checks.indentation.indentation; //indent:0 exp:0
 
-import com.puppycrawl.tools.checkstyle.checks.indentation.AbstractExpressionHandler; //indent:0 exp:0
-
 import java.util.List; //indent:0 exp:0
 import java.util.Map; //indent:0 exp:0
 import java.util.function.Supplier; //indent:0 exp:0
@@ -22,53 +20,53 @@ public class InputIndentationAnonymousClassInMethodCurlyOnNewLine //indent:0 exp
 { //indent:0 exp:0
     private void aMethod() //indent:4 exp:4
     { //indent:4 exp:4
-        final Supplier<Map<String, List<AbstractExpressionHandler>>> sup1 = //indent:8 exp:8
-                new Supplier<Map<String, List<AbstractExpressionHandler>>>() { //indent:16 exp:>=16
+        final Supplier<Map<String, List<String>>> sup1 = //indent:8 exp:8
+                new Supplier<Map<String, List<String>>>() { //indent:16 exp:>=16
                     @Override //indent:20 exp:20
-                    public Map<String, List<AbstractExpressionHandler>> get() //indent:20 exp:20
+                    public Map<String, List<String>> get() //indent:20 exp:20
                     { //indent:20 exp:20
                         return null; //indent:24 exp:24
                     } //indent:20 exp:20
                 }; //indent:16 exp:16
-        final Supplier<Map<String, List<AbstractExpressionHandler>>> sup2 = //indent:8 exp:8
-                  new Supplier<Map<String, List<AbstractExpressionHandler>>>(){ //indent:18 exp:>=16
+        final Supplier<Map<String, List<String>>> sup2 = //indent:8 exp:8
+                  new Supplier<Map<String, List<String>>>(){ //indent:18 exp:>=16
                     @Override //indent:20 exp:20
-                    public Map<String, List<AbstractExpressionHandler>> get() //indent:20 exp:20
+                    public Map<String, List<String>> get() //indent:20 exp:20
                     { //indent:20 exp:20
                         return null; //indent:24 exp:24
                     } //indent:20 exp:20
                   }; //indent:18 exp:16,20,24 warn
-        final Supplier<Map<String, List<AbstractExpressionHandler>>> sup3 = //indent:8 exp:8
-              new Supplier<Map<String,List<AbstractExpressionHandler>>>(){ //indent:14 exp:>=16 warn
+        final Supplier<Map<String, List<String>>> sup3 = //indent:8 exp:8
+              new Supplier<Map<String,List<String>>>(){ //indent:14 exp:>=16 warn
                     @Override //indent:20 exp:20
-                    public Map<String, List<AbstractExpressionHandler>> get() //indent:20 exp:20
+                    public Map<String, List<String>> get() //indent:20 exp:20
                     { //indent:20 exp:20
                         return null; //indent:24 exp:24
                     } //indent:20 exp:20
               }; //indent:14 exp:16,20,24 warn
-        final Supplier<Map<String, List<AbstractExpressionHandler>>> sup4 = //indent:8 exp:8
-                new Supplier<Map<String, List<AbstractExpressionHandler>>>() //indent:16 exp:>=16
+        final Supplier<Map<String, List<String>>> sup4 = //indent:8 exp:8
+                new Supplier<Map<String, List<String>>>() //indent:16 exp:>=16
                 { //indent:16 exp:16
                     @Override //indent:20 exp:20
-                    public Map<String, List<AbstractExpressionHandler>> get() //indent:20 exp:20
+                    public Map<String, List<String>> get() //indent:20 exp:20
                     { //indent:20 exp:20
                         return null; //indent:24 exp:24
                     } //indent:20 exp:20
                 }; //indent:16 exp:16
-        final Supplier<Map<String, List<AbstractExpressionHandler>>> sup5 = //indent:8 exp:8
-                new Supplier<Map<String, List<AbstractExpressionHandler>>>() //indent:16 exp:>=16
+        final Supplier<Map<String, List<String>>> sup5 = //indent:8 exp:8
+                new Supplier<Map<String, List<String>>>() //indent:16 exp:>=16
                   { //indent:18 exp:16,20,24 warn
                       @Override //indent:22 exp:22
-                      public Map<String, List<AbstractExpressionHandler>> get() //indent:22 exp:22
+                      public Map<String, List<String>> get() //indent:22 exp:22
                       { //indent:22 exp:22
                           return null; //indent:26 exp:26
                       } //indent:22 exp:22
                   }; //indent:18 exp:16,20,24 warn
-        final Supplier<Map<String, List<AbstractExpressionHandler>>> sup6 = //indent:8 exp:8
-                new Supplier<Map<String, List<AbstractExpressionHandler>>>() //indent:16 exp:>=16
+        final Supplier<Map<String, List<String>>> sup6 = //indent:8 exp:8
+                new Supplier<Map<String, List<String>>>() //indent:16 exp:>=16
               { //indent:14 exp:16,20,24 warn
                   @Override //indent:18 exp:18
-                  public Map<String, List<AbstractExpressionHandler>> get() //indent:18 exp:18
+                  public Map<String, List<String>> get() //indent:18 exp:18
                   { //indent:18 exp:18
                       return null; //indent:22 exp:22
                   } //indent:18 exp:18
