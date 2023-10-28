73aec29fb9f14d4bfd90bd1f35abe411be213727
Aayush <kumaraayush9810@gmail.com>
2023-10-28 08:33:14 +0530

    Issue #6207: Added Xpath IT regression test for StaticVariableName

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/staticvariablename/SuppressionXpathRegressionStaticVariableName2.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.staticvariablename;
+
+public class SuppressionXpathRegressionStaticVariableName2 {
+
+     public int num1; // OK
+     protected static int NUM2; // warn
+}
