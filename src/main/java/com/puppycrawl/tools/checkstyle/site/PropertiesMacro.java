468bf1e9c1387ccea858d2eed66ace5c52125965
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-10-30 16:43:43 +0530

    Issue #13930: Migrate magicnumber.xml.template to use properties template

--- src/main/java/com/puppycrawl/tools/checkstyle/site/PropertiesMacro.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/site/PropertiesMacro.java
@@ -22,6 +22,7 @@ package com.puppycrawl.tools.checkstyle.site;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
@@ -87,6 +88,13 @@ public class PropertiesMacro extends AbstractMacro {
 
     /** The file of the current module being processed. */
     private static File currentModuleFile = new File("");
+
+    /** Set of properties not inherited from the base token configuration. */
+    private static final Set<String> NON_BASE_TOKEN_PROP = Collections.unmodifiableSet(
+            Arrays.stream(new String[] {
+                "AtclauseOrder - target",
+                "MagicNumber - constantWaiverParentToken",
+            }).collect(Collectors.toSet()));
 
     @Override
     public void execute(Sink sink, MacroRequest request) throws MacroExecutionException {
@@ -399,7 +407,7 @@ public class PropertiesMacro extends AbstractMacro {
         }
         else {
             sink.rawText(INDENT_LEVEL_18);
-            sink.text(SiteUtil.DOT);
+            sink.rawText(SiteUtil.DOT);
             sink.rawText(INDENT_LEVEL_14);
         }
     }
@@ -466,9 +474,19 @@ public class PropertiesMacro extends AbstractMacro {
         else {
             final String defaultValue = SiteUtil.getDefaultValue(
                     propertyName, field, instance, currentModuleName);
-            sink.rawText(CODE_START);
-            sink.text(defaultValue);
-            sink.rawText(CODE_END);
+
+            final boolean isNonBaseTokenProp = NON_BASE_TOKEN_PROP.stream()
+                    .anyMatch(tokenProp -> tokenProp.endsWith(" - " + propertyName));
+
+            if (isNonBaseTokenProp) {
+                final List<String> defaultValuesList = Arrays.asList(defaultValue.split(", "));
+                writeTokensList(sink, defaultValuesList, SiteUtil.PATH_TO_TOKEN_TYPES);
+            }
+            else {
+                sink.rawText(CODE_START);
+                sink.text(defaultValue);
+                sink.rawText(CODE_END);
+            }
         }
 
         sink.tableCell_();
