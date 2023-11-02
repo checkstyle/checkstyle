761e9518422ac1ae0eedce63fcc706bad9f6a86d
Aayush <kumaraayush9810@gmail.com>
2023-11-02 19:32:05 +0530

    Issue #2827: JavadocMethod doesn't report anonymous class methods

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodValidateAnonymousClass.java
@@ -0,0 +1,26 @@
+/*
+JavadocMethod
+allowedAnnotations = MyAnnotation, Override
+validateThrows = (default)false
+accessModifiers = (default)public, protected, package, private
+allowMissingParamTags = (default)false
+allowMissingReturnTag = (default)false
+validateInAnonymousClasses = true
+tokens = (default)METHOD_DEF, CTOR_DEF, ANNOTATION_FIELD_DEF, COMPACT_CTOR_DEF
+
+
+*/
+
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocmethod;
+
+public class InputJavadocMethodValidateAnonymousClass {
+
+     Thread ob = new Thread(){
+
+     /**
+     *
+     */
+        void print(int a){} //violation 'Expected @param tag for 'a''
+    };
+}
