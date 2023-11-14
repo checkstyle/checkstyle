1409c01e50e15a853539efc020042ddf64fb9d5f
Rohanraj123 <rajrohan88293@gmail.com>
2023-11-14 15:03:44 +0530

    Issue #6207: Added Xpath IT regression test for MagicNumber

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/magicnumber/SuppressionXpathRegressionMagicNumberMethodDef.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.magicnumber;
+
+public class SuppressionXpathRegressionMagicNumberMethodDef {
+    public void methodWithMagicNumber() {
+        int x = 20; // warn
+    }
+}
