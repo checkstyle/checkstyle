15f121273d4bd37f4c7f658c5b5481c817ec6956
Suniti <suniti0804@gmail.com>
2023-11-21 14:24:31 +0530

    Issue #11446: Update AbstractJavadocCheckTest to use execute method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocInvalidAtSeeReference.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocInvalidAtSeeReference.java
@@ -1,13 +1,13 @@
-package com.puppycrawl.tools.checkstyle.checks.javadoc.abstractjavadoc;
-
 /*
- * Config: TempCheck
- */
+com.puppycrawl.tools.checkstyle.checks.javadoc.AbstractJavadocCheckTest$TempCheck
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.abstractjavadoc;
 
 /**
  * @see javax.swing.tree.DefaultTreeCellRenderer.getTreeCellRendererComponent()
  */
-// violation above 'Javadoc comment at column 78 has parse error.
-// Details: mismatched input '(' expecting <EOF> while parsing JAVADOC'
+// violation 2 lines above
 class InputAbstractJavadocInvalidAtSeeReference {
 }
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocJavadocTagsWithoutArgs.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocJavadocTagsWithoutArgs.java
@@ -1,18 +1,19 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.javadoc.AbstractJavadocCheckTest$TempCheck
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.javadoc.abstractjavadoc;
 
 import java.io.Serializable;
 
-/*
- * Config: TempCheck
- */
-
 /**@author*/ // ok
 public class InputAbstractJavadocJavadocTagsWithoutArgs implements Serializable{
+
     /**@serial*/ // ok
     private static final long serialVersionUID = 7556448691029650757L;
 
-    /**@see*/ // violation 'Javadoc comment at column 4 has parse error. Details: no viable
-    // alternative at input '<EOF> while parsing JAVADOC_TAG'
+    /**@see*/ // violation 'Javadoc comment at column 4 has parse error.'
     private static int field2;
 
     /**@since*/ // ok
@@ -24,14 +25,12 @@ public class InputAbstractJavadocJavadocTagsWithoutArgs implements Serializable{
     /**serialField*/ // ok
     private static Object field5;
 
-    /**@exception*/ // violation 'Javadoc comment at column 10 has parse error. Details: no
-    // viable alternative at input '<EOF> while parsing JAVADOC_TAG'
+    /**@exception*/ // violation 'Javadoc comment at column 10 has parse error.'
     public static void method1() {
 
     }
 
-    /**@throws*/ // violation 'Javadoc comment at column 7 has parse error. Details: no viable
-    // alternative at input '<EOF> while parsing JAVADOC_TAG'
+    /**@throws*/ // violation 'Javadoc comment at column 7 has parse error.'
     public static void method2() {
 
     }
@@ -41,8 +40,7 @@ public class InputAbstractJavadocJavadocTagsWithoutArgs implements Serializable{
         return -1;
     }
 
-    /**@param*/ // violation 'Javadoc comment at column 6 has parse error. Details: no viable
-    // alternative at input '<EOF> while parsing JAVADOC_TAG'
+    /**@param*/ // violation 'Javadoc comment at column 6 has parse error.'
     public static void method4(int a) {
 
     }
@@ -65,8 +63,7 @@ public class InputAbstractJavadocJavadocTagsWithoutArgs implements Serializable{
     /**
      * {@link}
      */
-    // violation above 'Javadoc comment at column 13 has parse error. Details: mismatched
-    // input '}' expecting {LEADING_ASTERISK, WS, NEWLINE}" while parsing JAVADOC_INLINE_TAG'
+    // violation 2 lines above 'Javadoc comment at column 13 has parse error.'
     public void method7() {
 
     }
@@ -74,8 +71,7 @@ public class InputAbstractJavadocJavadocTagsWithoutArgs implements Serializable{
     /**
      * {@linkplain }
      */
-    // violation above 'Javadoc comment at column 19 has parse error. Details: no viable
-    // alternative at input '<}> while parsing REFERENCE'
+    // violation 2 lines above 'Javadoc comment at column 19 has parse error.'
     public void method8() {
 
     }
