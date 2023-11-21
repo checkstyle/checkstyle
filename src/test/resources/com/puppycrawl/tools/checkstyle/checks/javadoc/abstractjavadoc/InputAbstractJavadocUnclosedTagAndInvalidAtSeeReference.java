05c24bc8e47ba056d5450cbf0db34d9b12e35bf3
Suniti <suniti0804@gmail.com>
2023-11-21 14:24:31 +0530

    Issue #11446: Update AbstractJavadocCheckTest to use execute method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocUnclosedTagAndInvalidAtSeeReference.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocUnclosedTagAndInvalidAtSeeReference.java
@@ -1,19 +1,22 @@
-package com.puppycrawl.tools.checkstyle.checks.javadoc.abstractjavadoc;
-
 /*
- * Config: TempCheck
- */
+com.puppycrawl.tools.checkstyle.checks.javadoc.AbstractJavadocCheckTest$ParseJavadocOnlyCheck
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.abstractjavadoc;
 
 /**
  * <unclosedTag>
  */
-// violation above 'Unclosed HTML tag found: unclosedTag'
+// violation 2 lines above 'Javadoc comment at column 4 has parse error.'
+// Missed HTML close tag 'unclosedTag'. Sometimes it means that close tag missed for
+// one of previous tags.
 class InputAbstractJavadocUnclosedTagAndInvalidAtSeeReference {
     /**
      * @see javax.swing.tree.DefaultTreeCellRenderer.getTreeCellRendererComponent()
      */
-    // violation above 'Javadoc comment at column 82 has parse error.
-    // Details: mismatched input '(' expecting <EOF> while parsing JAVADOC'
+    // violation 2 lines above 'Javadoc comment at column 82 has parse error.'
+    // Details: mismatched input '(' expecting <EOF> while parsing JAVADOC
     void invalidAtSeeReference() {
     }
 }
