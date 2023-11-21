569df9ea52268b66493e1526f548908e1eec0601
Suniti <suniti0804@gmail.com>
2023-11-21 14:24:31 +0530

    Issue #11446: Update AbstractJavadocCheckTest to use execute method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocLeaveToken.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/abstractjavadoc/InputAbstractJavadocLeaveToken.java
@@ -1,13 +1,14 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.javadoc.AbstractJavadocCheckTest$JavadocVisitLeaveCheck
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.javadoc.abstractjavadoc;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-/*
- * Config: JavadocVisitLeaveCheck
- */
 
 /////////////
 // CLASSES //
