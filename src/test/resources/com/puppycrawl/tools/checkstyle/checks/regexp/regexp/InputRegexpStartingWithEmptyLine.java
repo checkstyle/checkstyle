cacc504a5de5573dcc2ea0e8d7e300baafcb483a
Suniti <suniti0804@gmail.com>
2023-11-21 18:01:22 +0530

    Issue #11446: Update RegexpCheckTest to use verifyWithInlineConfigParser

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/InputRegexpStartingWithEmptyLine.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/InputRegexpStartingWithEmptyLine.java
@@ -1,2 +1,13 @@
+/*
+Regexp
+format = Boolean x = new Boolean
+message = (default)null
+illegalPattern = (default)false
+duplicateLimit = 0
+errorLimit = (default)100
+ignoreComments = (default)false
+
+
+*/
 
 package com.puppycrawl.tools.checkstyle.checks.regexp.regexp; // ok
