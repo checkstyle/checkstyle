584a3ae0e5e66930897c898e9c4f3aa90d422549
piyush bakde <piyush.bakde94@gmail.com>
2023-11-29 19:35:13 +0530

    Issue #4845: Removed Checkstyle Dependency From UnusedImports

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsAnnotations.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsAnnotations.java
@@ -11,5 +11,3 @@ processJavadoc = (default)true
 ///////////////////////////////////////////////////////////////////////////////////////////////
 @MyAnnotation
 package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
-
-import com.puppycrawl.tools.checkstyle.MyAnnotation;
