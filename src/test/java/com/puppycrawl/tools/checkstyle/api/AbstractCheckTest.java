7f50a149922ba81ae867f967e8f146b3fa42097a
Suniti <suniti0804@gmail.com>
2023-12-02 19:26:02 +0530

    Issue #11446: Update AbstractCheckTest to use verifyWithInlineConfigParser

--- src/test/java/com/puppycrawl/tools/checkstyle/api/AbstractCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/api/AbstractCheckTest.java
@@ -177,7 +177,7 @@ public class AbstractCheckTest extends AbstractModuleTestSupport {
             Charset.defaultCharset().name())));
 
         assertWithMessage("Invalid line content")
-                .that(check.getLine(3))
+                .that(check.getLine(9))
                 .isEqualTo(" * I'm a javadoc");
     }
 
@@ -206,7 +206,7 @@ public class AbstractCheckTest extends AbstractModuleTestSupport {
 
         final int[] expectedCodePoints = "    public int getVariable() {".codePoints().toArray();
         assertWithMessage("Invalid line content")
-                .that(check.getLineCodePoints(12))
+                .that(check.getLineCodePoints(18))
                 .isEqualTo(expectedCodePoints);
     }
 
@@ -382,12 +382,10 @@ public class AbstractCheckTest extends AbstractModuleTestSupport {
 
     @Test
     public void testCheck() throws Exception {
-        final DefaultConfiguration checkConfig = createModuleConfig(ViolationAstCheck.class);
-
         final String[] expected = {
-            "1:1: Violation.",
+            "6:1: Violation.",
         };
-        verify(checkConfig, getPath("InputAbstractCheckTestFileContents.java"), expected);
+        verifyWithInlineConfigParser(getPath("InputAbstractCheckTestFileContents.java"), expected);
     }
 
     /**
