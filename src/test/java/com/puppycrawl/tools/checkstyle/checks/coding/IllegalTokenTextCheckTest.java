f0124864ca27c6e54f8a9adc7dd2cdc8b8544ef1
Nick Mancuso <nicholas.mancuso13@gmail.com>
2023-12-09 23:54:27 -0500

    Issue #13830: Add support for Java 21 string templates

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalTokenTextCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalTokenTextCheckTest.java
@@ -144,6 +144,24 @@ public class IllegalTokenTextCheckTest
     }
 
     @Test
+    public void testStringTemplate()
+            throws Exception {
+
+        final String[] expected = {
+            "29:28: " + getCheckMessage(MSG_KEY, "x"),
+            "29:35: " + getCheckMessage(MSG_KEY, "x"),
+            "59:20: " + getCheckMessage(MSG_KEY, "x"),
+            "59:27: " + getCheckMessage(MSG_KEY, "x"),
+            "73:23: " + getCheckMessage(MSG_KEY, "x"),
+            "73:30: " + getCheckMessage(MSG_KEY, "x"),
+        };
+
+        verifyWithInlineConfigParser(
+                getNonCompilablePath(
+                        "InputIllegalTokenTextStringTemplate.java"), expected);
+    }
+
+    @Test
     public void testOrderOfProperties() {
         // pure class must be used as configuration doesn't guarantee order of
         // attributes
@@ -161,7 +179,7 @@ public class IllegalTokenTextCheckTest
 
     @Test
     public void testAcceptableTokensMakeSense() {
-        final int expectedTokenTypesTotalNumber = 188;
+        final int expectedTokenTypesTotalNumber = 194;
         assertWithMessage("Total number of TokenTypes has changed, acceptable tokens in"
                 + " IllegalTokenTextCheck need to be reconsidered.")
             .that(TokenUtil.getTokenTypesTotalNumber())
@@ -178,7 +196,8 @@ public class IllegalTokenTextCheckTest
             TokenTypes.COMMENT_CONTENT,
             TokenTypes.STRING_LITERAL,
             TokenTypes.CHAR_LITERAL,
-            TokenTypes.TEXT_BLOCK_CONTENT
+            TokenTypes.TEXT_BLOCK_CONTENT,
+            TokenTypes.STRING_TEMPLATE_CONTENT
         );
         for (int tokenType : allowedTokens) {
             assertWithMessage(TokenUtil.getTokenName(tokenType) + " should not be allowed"
---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/InputIllegalTokenTextStringTemplate.java
@@ -0,0 +1,76 @@
+/*
+IllegalTokenText
+format = x
+ignoreCase = (default)false
+message = (default)
+tokens = STRING_TEMPLATE_CONTENT
+
+
+*/
+
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
+
+import java.util.List;
+
+public class InputIllegalTokenTextStringTemplate {
+    int x = 10;
+    int y = 20;
+
+    // most basic example, empty string template expression
+    String result1 = STR."";
+
+    String result1_1 = STR."Hello world!";
+
+    // no content, no expression
+    String result1_2 = STR."\{  }";
+
+    // simple single value interpolation
+    String result2 = STR. "x\{ x }x"; // 2 violations
+
+    // simple single value interpolation with a method call
+    String result3 = STR. "\{ y(y("y")) }";
+
+    // bit more complex example, multiple interpolations
+    String result4 = STR. "\{ x }"  + STR. "\{ y }";
+
+    // bit more complex example, multiple interpolations with method calls
+    String result5 = STR. "\{ y(y("y")) }"  + STR. "\{ y(y("y")) }";
+
+    // string template concatenation
+    String result6 = STR. "\{ x }"  + STR. "\{ y }";
+
+    // concatentation in embedded expression
+    String result7 = STR. "\{ x + y }";
+
+    // concatentation in embedded expression with method calls
+    String result8 = STR. "hey \{ y(y("y")) + y(y("y")) } there";
+
+    // multiple lines
+    String result9 = STR. "hey \{
+            y(y("y")) + y(y("y"))
+            } there";
+
+    // multiple lines with many expressions
+    String result10 = STR. "hey \{
+            y(y("y")) + y(y("y"))
+            } there \{
+            y(y("y")) + y(y("y"))
+            + STR."x\{ x }x" // 2 violations
+            }";
+
+    String name = "world";
+    String s = STR."Hello, \{ name }!";
+
+    private static String y(String y) {
+        return "y";
+    }
+
+    // Verify proper behavior of TransType w.r.t. templated Strings
+    void x(List<? extends StringTemplate.Processor<String, RuntimeException>> list) {
+        list.get(0)."";
+        list.get(0)."\{  }";
+        list.get(0). "x\{ x }x"; // 2 violations
+        list.get(0). ".\{ x }.\{ y }.\{ x }.\{}.\{}";
+    }
+}
