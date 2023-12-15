0ebabc81524cfd2be052b8250e7b2ebf181290df
Kumar Kalpadiptya Roy <kalpadiptya.roy@outlook.com>
2023-12-15 23:19:21 +0530

    Issue #13782: Fix false negative for annotation indentation

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/indentation/IndentationCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/indentation/IndentationCheckTest.java
@@ -2998,6 +2998,24 @@ public class IndentationCheckTest extends AbstractModuleTestSupport {
     }
 
     @Test
+    public void testIndentationAnnotationMultiLine() throws Exception {
+        final DefaultConfiguration checkConfig = createModuleConfig(IndentationCheck.class);
+        checkConfig.addProperty("tabWidth", "4");
+        checkConfig.addProperty("basicOffset", "2");
+        checkConfig.addProperty("braceAdjustment", "2");
+        checkConfig.addProperty("caseIndent", "2");
+        checkConfig.addProperty("throwsIndent", "4");
+        checkConfig.addProperty("lineWrappingIndentation", "4");
+        checkConfig.addProperty("arrayInitIndent", "2");
+
+        final String[] expected = {
+            "16:1: " + getCheckMessage(MSG_ERROR, "@", 0, 4),
+        };
+
+        verifyWarns(checkConfig, getPath("InputIndentationAnnotationMultiLine.java"), expected);
+    }
+
+    @Test
     public void testIndentationLongConcatenatedString() throws Exception {
         final DefaultConfiguration checkConfig = createModuleConfig(IndentationCheck.class);
         checkConfig.addProperty("tabWidth", "4");
