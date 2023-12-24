a8ed7e8fc53da460ed3ada0089e710478f108cf3
piyush bakde <piyush.bakde94@gmail.com>
2023-12-24 07:40:00 +0530

    Issue #13345: Enable RightCurlyCheckExamplesTest.java

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/Example3.java
@@ -0,0 +1,39 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="RightCurly">
+      <property name="option" value="alone"/>
+      <property name="tokens" value="LITERAL_SWITCH"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.blocks.rightcurly;
+
+// xdoc section -- start
+class Example3 {
+
+  public void method0() {
+    int mode = 0;
+    switch (mode) {
+      case 1:
+        int x = 1;
+        break;
+      default:
+        x = 0;
+    }
+  }
+
+  public void method01() {
+    int mode = 0;
+    switch (mode) {
+      case 1:
+        int x = 1;
+        break;
+      default:
+        x = 0; } // violation, ''}' at column 16 should be alone on a line.'
+  }
+
+}
+// xdoc section -- end
