0874b3b00390a70b9ae6a917fe5df7ecde4e6006
piyush bakde <piyush.bakde94@gmail.com>
2023-12-24 07:40:00 +0530

    Issue #13345: Enable RightCurlyCheckExampleTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/Example5.java
@@ -0,0 +1,42 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="RightCurly">
+      <property name="option" value="alone_or_singleline"/>
+      <property name="tokens" value="LITERAL_SWITCH"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.blocks.rightcurly;
+
+// xdoc section -- start
+class Example5 {
+
+  public void method0() {
+    int mode = 0;
+    switch (mode) {
+      case 1:
+        int x = 1;
+        break;
+      default:
+        x = 0;
+    }
+  }
+
+  public static void method7() {
+    int mode = 0;
+    int x;
+    switch (mode) { case 1: x = 5; }
+  }
+
+  public void method() {
+    int mode = 0;
+    int x;
+    switch (mode) {
+      case 1:
+        x = 1; } // violation, ''}' at column 16 should be alone on a line.'
+  }
+}
+// xdoc section -- end
