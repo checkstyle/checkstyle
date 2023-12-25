a0f3989012bb2ee93f4e04cfa18aa793112be85d
Mauryan Kansara <mauryankansaramk@gmail.com>
2023-12-25 17:44:22 +0530

    Issue #13345: Enable examples tests for NewlineAtEndOfFile

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/newlineatendoffile/Example4.py
@@ -0,0 +1,9 @@
+/*xml
+<module name="Checker">
+  <module name="NewlineAtEndOfFile">
+    <property name="fileExtensions" value="java, xml, py"/>
+  </module>
+</module>
+*/
+// violation 7 lines above 'File does not end with a newline.'
+print("Hello world!")
