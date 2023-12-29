89340c179f61b727b4284ef733e59f26fd5de950
Roman Ivanov <ivanov-jr@mail.ru>
2023-12-29 07:35:53 -0800

    Issue #13693: fix wording of property fileExtensions

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/regexp/RegexpSinglelineCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/regexp/RegexpSinglelineCheck.java
@@ -38,7 +38,7 @@ import com.puppycrawl.tools.checkstyle.api.FileText;
  * </p>
  * <ul>
  * <li>
- * Property {@code fileExtensions} - Specify the file type extension of files to process.
+ * Property {@code fileExtensions} - Specify the file extensions of the files to process.
  * Type is {@code java.lang.String[]}.
  * Default value is {@code ""}.
  * </li>
