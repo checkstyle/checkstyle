7f9555de9c659c70e081923f700776dfa61cbbe9
Roman Ivanov <ivanov-jr@mail.ru>
2023-12-31 08:58:54 -0800

    Issue #13693: migrate misc modules to property macros

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/UniquePropertiesCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/UniquePropertiesCheck.java
@@ -46,7 +46,7 @@ import com.puppycrawl.tools.checkstyle.api.FileText;
  * </p>
  * <ul>
  * <li>
- * Property {@code fileExtensions} - Specify file type extension of the files to check.
+ * Property {@code fileExtensions} - Specify the file extensions of the files to process.
  * Type is {@code java.lang.String[]}.
  * Default value is {@code .properties}.
  * </li>
