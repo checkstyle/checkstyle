d223403e08fce7dae74b950a085c49b7afee6b01
Charisma Kausar <charisma.kausar@gmail.com>
2024-01-01 23:51:27 +0300

    Issue #13345: Enable RightCurlyCheckExampleTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/Example1.java
@@ -0,0 +1,61 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="RightCurly"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.blocks.rightcurly;
+
+// xdoc section -- start
+public class Example1 {
+
+  public void test() {
+
+    boolean foo = false;
+    if (foo) {
+      bar();
+    } // violation, ''}' at column 5 should be on the same line'
+    // 'as the next part of a multi-block statement (one that directly'
+    // 'contains multiple blocks: if/else-if/else, do/while or try/catch/finally).'
+    else {
+      bar();
+    }
+
+    if (foo) {
+      bar();
+    } else {
+      bar();
+    }
+
+    if (foo) { bar(); } int i = 0;
+    // violation above, ''}' at column 23 should be alone on a line.'
+
+    if (foo) { bar(); }
+    i = 0;
+
+    try {
+      bar();
+    } // violation, ''}' at column 5 should be on the same line'
+    // 'as the next part of a multi-block statement'
+    // '(one that directly contains multiple blocks: if/else-if/else,'
+    // 'do/while or try/catch/finally).'
+    catch (Exception e) {
+      bar();
+    }
+
+    try {
+      bar();
+    } catch (Exception e) {
+      bar();
+    }
+
+  }
+
+  private void bar() {
+  }
+
+  public void testSingleLine() { bar(); } // OK, because singleline is allowed
+}
+// xdoc section -- end
