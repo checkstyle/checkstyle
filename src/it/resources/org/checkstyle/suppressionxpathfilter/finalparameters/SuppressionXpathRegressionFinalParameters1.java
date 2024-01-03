2aa62bb4dd640bdba12088a11fbf1a35cf7a70a9
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-01-04 03:28:18 +0530

    Issue #6207: Add Xpath Regression Test for FinalParameters

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/finalparameters/SuppressionXpathRegressionFinalParameters1.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.finalparameters;
+
+public class SuppressionXpathRegressionFinalParameters1 {
+
+    public void method(int argOne, final int argTwo) { // warn
+    }
+}
