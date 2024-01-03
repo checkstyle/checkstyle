682fb98c169a566939d3b31407ef982100a31b85
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-01-04 03:28:18 +0530

    Issue #6207: Add Xpath Regression Test for FinalParameters

---
+++ src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionFinalParametersTest.java
@@ -0,0 +1,61 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2023 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package org.checkstyle.suppressionxpathfilter;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.checks.FinalParametersCheck;
+
+public class XpathRegressionFinalParametersTest extends AbstractXpathTestSupport {
+
+    private final String checkName = FinalParametersCheck.class.getSimpleName();
+
+    @Override
+    protected String getCheckName() {
+        return checkName;
+    }
+
+    @Test
+    public void testOne() throws Exception {
+        final File fileToProcess =
+                  new File(getPath("SuppressionXpathRegressionFinalParameters1.java"));
+
+        final DefaultConfiguration moduleConfig =
+                createModuleConfig(FinalParametersCheck.class);
+
+        final String[] expectedViolation = {
+            "5:25: " + getCheckMessage(FinalParametersCheck.class, FinalParametersCheck.MSG_KEY, "argOne"),
+        };
+
+        final List<String> expectedXpathQueries = Collections.singletonList(
+                "/COMPILATION_UNIT/CLASS_DEF[./IDENT"
+                    + "[@text='SuppressionXpathRegressionFinalParameters1']]"
+                    + "/OBJBLOCK/METHOD_DEF[./IDENT[@text='method']]"
+                    + "/PARAMETERS/PARAMETER_DEF/IDENT[@text='argOne']"
+        );
+
+        runVerifications(moduleConfig, fileToProcess, expectedViolation, expectedXpathQueries);
+    }
+}
---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/finalparameters/SuppressionXpathRegressionFinalParameters1.java
@@ -0,0 +1,6 @@
+package org.checkstyle.suppressionxpathfilter.finalparameters;
+
+public class SuppressionXpathRegressionFinalParameters1 {
+
+    private void method(int argOne, final int argTwo) { } // warn
+}
