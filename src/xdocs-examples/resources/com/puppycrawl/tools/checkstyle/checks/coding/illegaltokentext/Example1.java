6beaf18c22edbcbadac10985d41fd51d5605af37
Coding-Aliens <kendrickfhtom35@gmail.com>
2024-01-04 20:28:23 +0800

    Issue #13345: Enable IlegalTokenTextCheckExamplesTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example1.java
@@ -1,18 +1,20 @@
+package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
 /*xml
 <module name="Checker">
   <module name="TreeWalker">
     <module name="IllegalTokenText">
       <property name="tokens" value="STRING_LITERAL"/>
       <property name="format" value="a href"/>
-      <property name="ignoreCase" value="true"/>
     </module>
   </module>
 </module>
 */
 
 // xdoc section -- start
-public void myTest() {
-    String test = "a href"; // violation
-    String test2 = "A href"; // violation, case is ignored
+public class Example1 {
+    public void myTest() {
+        String test = "a href"; // violation
+        String test2 = "A href"; // OK, case is sensitive
+    }
 }
 // xdoc section -- end
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example2.java
@@ -1,17 +1,21 @@
+package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
 /*xml
 <module name="Checker">
   <module name="TreeWalker">
     <module name="IllegalTokenText">
       <property name="tokens" value="STRING_LITERAL"/>
       <property name="format" value="a href"/>
+      <property name="ignoreCase" value="true"/>
     </module>
   </module>
 </module>
 */
 
 // xdoc section -- start
-public void myTest() {
-    String test = "a href"; // violation
-    String test2 = "A href"; // OK, case is sensitive
+public class Example2 {
+    public void myTest() {
+        String test = "a href"; // violation
+        String test2 = "A href"; // violation, case is ignored
+    }
 }
 // xdoc section -- end
