6c3f0034a619a5fbf446ffcdeb8686220320c6d4
Rohanraj123 <rajrohan88293@gmail.com>
2024-01-05 21:44:15 +0530

    Issue #11446: Replace verify with execute method in TreeWalker

--- src/test/resources/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalkerSuppressionCommentFilter.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalkerSuppressionCommentFilter.java
@@ -1,11 +1,15 @@
+/*
+com.puppycrawl.tools.checkstyle.TreeWalkerTest$ViolationSuppressionCommentFilterCheck
+
+*/
 package com.puppycrawl.tools.checkstyle.treewalker;
 
 public class InputTreeWalkerSuppressionCommentFilter {
-    private int I;
+    private int I; // violation, 'Name 'I' must match pattern '^[a-z][a-zA-Z0-9]*$''
     /* CHECKSTYLE:OFF */
     private int J;
     /* CHECKSTYLE:ON */
     //CHECKSTYLE:OFF
-    private int P;
+    private int P; // violation, 'Name 'P' must match pattern '^[a-z][a-zA-Z0-9]*$''
     //CHECKSTYLE:ON
 }
