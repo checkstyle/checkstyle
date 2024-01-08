256e63cd2e5ab81f2f3bd18eebd6a27669cfc31f
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-01-08 20:31:07 +0530

    Issue #6207: Add Xpath Regression Test for JavaNCSS

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/javancss/SuppressionXpathRegressionJavaNCSSTwo.java
@@ -0,0 +1,61 @@
+package org.checkstyle.suppressionxpathfilter.javancss;
+
+public class SuppressionXpathRegressionJavaNCSSTwo { // warn
+
+    int a1 = 1;
+    int a2 = 1;
+    int a3 = 1;
+    int a4 = 1;
+    int a5 = 1;
+    int a6 = 1;
+    int a7 = 1;
+    int a8 = 1;
+    int a9 = 1;
+    int a10 = 1;
+    int a11 = 1;
+    int a12 = 1;
+    int a13 = 1;
+    int a14 = 1;
+    int a15 = 1;
+    int a16 = 1;
+    int a17 = 1;
+    int a18 = 1;
+    int a19 = 1;
+    int a20 = 1;
+    int a21 = 1;
+    int a22 = 1;
+    int a23 = 1;
+    int a24 = 1;
+    int a25 = 1;
+    int a26 = 1;
+    int a27 = 1;
+    int a28 = 1;
+    int a29 = 1;
+    int a30 = 1;
+    int a31 = 1;
+    int a32 = 1;
+    int a33 = 1;
+    int a34 = 1;
+    int a35 = 1;
+    int a36 = 1;
+    int a37 = 1;
+    int a38 = 1;
+    int a39 = 1;
+    int a40 = 1;
+    int a41 = 1;
+    int a42 = 1;
+    int a43 = 1;
+    int a44 = 1;
+    int a45 = 1;
+    int a46 = 1;
+    int a47 = 1;
+    int a48 = 1;
+    int a49 = 1;
+    int a50 = 1;
+}
+
+class Test { // ok, class does not exceed the 50 lines limit.
+    int a1 = 1;
+    int a2 = 1;
+    int a3 = 1;
+}
