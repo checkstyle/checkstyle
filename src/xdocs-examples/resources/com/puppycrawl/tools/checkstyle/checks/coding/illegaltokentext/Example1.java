53256a955260df802d9b5a915c1552a9c0ba589d
Coding-Aliens <kendrickfhtom35@gmail.com>
2024-01-11 08:02:51 +0800

    1

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example1.java
@@ -15,7 +15,7 @@ package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
 public class Example1 {
   public void myTest() {
     String test = "a href"; // violation
-    // String test2 = "A href"; // OK, case is sensitive
+    String test2 = "A href"; // OK, case is sensitive
   }
 }
 // xdoc section -- end
