8ac049f7579eb76010dd29fe9b4ebeb0e888b56e
Tanay <tdmorakhia26@gmail.com>
2024-01-12 22:28:13 +0530

    Issue #6207: Add Xpath Regression Test for InnerTypeLastTest

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/innertypelast/SuppressionXpathRegressionInnerTypeLastOne.java
@@ -0,0 +1,11 @@
+package org.checkstyle.suppressionxpathfilter.innertypelast;
+
+public class SuppressionXpathRegressionInnerTypeLastOne {
+
+    public class Inner {
+    }
+
+    public SuppressionXpathRegressionInnerTypeLastOne() { // warn
+    }
+
+}
