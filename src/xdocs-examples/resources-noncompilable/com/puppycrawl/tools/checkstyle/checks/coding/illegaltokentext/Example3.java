422fed44a14c94c5ac4ddda637e2b932a589e8d4
Coding-Aliens <kendrickfhtom35@gmail.com>
2024-01-12 20:09:42 +0800

    minor: placeTheCommentBeforeThePackageLine

--- src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example3.java
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example3.java
@@ -8,8 +8,8 @@
   </module>
 </module>
 */
-//non-compiled with javac: Compilable with Java14
 
+//non-compiled with javac: Compilable with Java14
 package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
 // xdoc section -- start
 public class Example3 {
