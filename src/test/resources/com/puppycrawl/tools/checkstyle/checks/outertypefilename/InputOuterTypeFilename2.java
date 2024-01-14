5f44928a718ce2aee31d98138a8b0607b5e8ec5e
MANISH-K-07 <manish07070707@gmail.com>
2024-01-14 03:41:45 +0000

    Issue #13213: Removed //ok from InputOuterTypeFilename

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/outertypefilename/InputOuterTypeFilename2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/outertypefilename/InputOuterTypeFilename2.java
@@ -7,7 +7,7 @@ OuterTypeFilename
 // someexamples of 1.5 extensions
 package com.puppycrawl.tools.checkstyle.checks.outertypefilename;
 
-public class InputOuterTypeFilename2 { // ok
+public class InputOuterTypeFilename2 {
 
 
 }
