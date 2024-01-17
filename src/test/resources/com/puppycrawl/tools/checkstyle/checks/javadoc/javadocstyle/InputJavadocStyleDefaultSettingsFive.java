2180b923e88ac43120f322200ad35712025f3b76
Omar Bakry <omarbakrey90@gmail.com>
2024-01-17 01:16:09 -0800

    Issue #12600: Modify javadocstyle check to work with inline return

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocstyle/InputJavadocStyleDefaultSettingsFive.java
@@ -0,0 +1,37 @@
+/*
+JavadocStyle
+scope = (default)private
+excludeScope = (default)null
+checkFirstSentence = (default)true
+endOfSentenceFormat = (default)([.?!][ \t\n\r\f<])|([.?!]$)
+checkEmptyJavadoc = (default)false
+checkHtml = (default)true
+tokens = (default)ANNOTATION_DEF, ANNOTATION_FIELD_DEF, CLASS_DEF, CTOR_DEF, \
+         ENUM_CONSTANT_DEF, ENUM_DEF, INTERFACE_DEF, METHOD_DEF, PACKAGE_DEF, \
+         VARIABLE_DEF, RECORD_DEF, COMPACT_CTOR_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocstyle;
+
+public class InputJavadocStyleDefaultSettingsFive
+{
+   /**
+    * {@return a string} // ok, foo one
+    */
+    public String foo() {
+        return "Hello, world";
+    }
+
+   /**
+    *   This is the first sentence with leading and trailing spaces.   Second sentence.
+    */
+    public String foo2() // ok, foo two
+    {
+        return "Hello, world";
+    }
+
+    /**             Set of all class field names.        */
+    public String field;
+}
