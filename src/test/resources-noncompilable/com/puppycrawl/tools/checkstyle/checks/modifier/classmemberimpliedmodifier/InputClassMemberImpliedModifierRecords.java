e93bb8af144194adcc1f08c1a34c8ab39a99adb4
MANISH-K-07 <manish07070707@gmail.com>
2024-01-21 02:28:21 +0000

    Issue #13213: removed //ok noncompilable/modifier

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/modifier/classmemberimpliedmodifier/InputClassMemberImpliedModifierRecords.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/modifier/classmemberimpliedmodifier/InputClassMemberImpliedModifierRecords.java
@@ -11,28 +11,28 @@ violateImpliedStaticOnNestedRecord = (default)true
 package com.puppycrawl.tools.checkstyle.checks.modifier.classmemberimpliedmodifier;
 
 public class InputClassMemberImpliedModifierRecords {
-    public static interface GoodInterface {} // OK
+    public static interface GoodInterface {}
     // Implied modifier 'static' should be explicit. [ClassMemberImpliedModifier]
     public interface BadInterface {} // violation
 
-    public static enum GoodEnum {} // OK
+    public static enum GoodEnum {}
     // Implied modifier 'static' should be explicit. [ClassMemberImpliedModifier]
     public enum BadEnum {} // violation
 
-    public static record GoodRecord() {} // OK
+    public static record GoodRecord() {}
     // Implied modifier 'static' should be explicit. [ClassMemberImpliedModifier]
     public record BadRecord() {} // violation
 
     public static record OuterRecord() {
-        public static record InnerRecord1(){} // ok
+        public static record InnerRecord1(){}
         // Implied modifier 'static' should be explicit. [ClassMemberImpliedModifier]
         public record InnerRecord2(){} // violation
 
-        public static interface InnerInterface1 {} // ok
+        public static interface InnerInterface1 {}
         // Implied modifier 'static' should be explicit. [ClassMemberImpliedModifier]
         public interface InnerInterface2 {} // violation
 
-        public static enum InnerEnum1{} // ok
+        public static enum InnerEnum1{}
         // Implied modifier 'static' should be explicit. [ClassMemberImpliedModifier]
         public enum InnerEnum2{} // violation
     }
@@ -40,6 +40,6 @@ public class InputClassMemberImpliedModifierRecords {
     Object obj = new Object() {
         // Implied modifier 'static' should be explicit. [ClassMemberImpliedModifier]
         public record BadRecord() {} // violation
-        public static record OkRecord() {} // ok
+        public static record OkRecord() {}
     };
 }
