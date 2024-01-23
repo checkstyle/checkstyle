330c99d8ad3f1e8bfa85873e812afe133d7e75bb
MANISH-K-07 <manish07070707@gmail.com>
2024-01-23 08:56:38 +0000

    Issue #13213: Removed //ok from leftout checks

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/commentsindentation/InputCommentsIndentationCommentsAfterMethodCall.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/commentsindentation/InputCommentsIndentationCommentsAfterMethodCall.java
@@ -11,17 +11,14 @@ package com.puppycrawl.tools.checkstyle.checks.indentation.commentsindentation;
 public class InputCommentsIndentationCommentsAfterMethodCall {
     public static void main() {
         InputCommentsIndentationCommentsAfterMethodCall.flatMap(4,5)
-           // ok
            .flatMap()
            .flatMap();
-        // ok
     }
     public static void main1() {
         InputCommentsIndentationCommentsAfterMethodCall.flatMap(4,5)
-                // ok
                 .flatMap()
                 .flatMap();
-               // violation '.* incorrect .* level 15, expected is 8, .* same .* as line 20.'
+               // violation '.* incorrect .* level 15, expected is 8, .* same .* as line 17.'
     }
     public static InputCommentsIndentationCommentsAfterMethodCall flatMap(int i, int h) {
         return new InputCommentsIndentationCommentsAfterMethodCall();
