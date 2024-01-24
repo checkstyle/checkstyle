f727127b7dcc1f3ac8e3b3d6b95eb9a0ae537561
ChristianCecili <chri0407@gmail.com>
2024-01-24 16:01:00 +0100

    Issue #13345: Enable Test Cases for GenericWhitespaceCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/genericwhitespace/Example2.java
@@ -0,0 +1,36 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="GenericWhitespace"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.genericwhitespace;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+// xdoc section -- start
+public class Example2 {
+  List< String> l; // violation, ''<' is followed by whitespace.'
+  Box b = Box. <String>of("foo"); // violation, ''<' is preceded with whitespace.'
+  public<T> void foo() {} // violation, ''<' is not preceded with whitespace.'
+
+  List a = new ArrayList<>  (); // ok, until #14344
+  Map<Integer, String>m; // violation, ''>' is followed by an illegal character.'
+  Pair<Integer, Integer > p; // violation, ''>' is preceded with whitespace.'
+}
+// xdoc section -- end
+
+class Box {
+  public static <T> Box of(T value) {
+    // Implementation details
+    return new Box();
+  }
+}
+
+class Pair<T1, T2> {
+  // Implementation details
+}
