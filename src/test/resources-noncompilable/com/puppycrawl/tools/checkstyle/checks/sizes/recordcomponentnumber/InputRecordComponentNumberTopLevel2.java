5dbb09f11795b931f35dc4250c53737e878b97a7
MANISH-K-07 <manish07070707@gmail.com>
2024-01-25 11:29:20 +0000

    Issue #13213: removed //ok noncompilable/sizes

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/recordcomponentnumber/InputRecordComponentNumberTopLevel2.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/recordcomponentnumber/InputRecordComponentNumberTopLevel2.java
@@ -9,6 +9,6 @@ accessModifiers = (default)public, protected, package, private
 //non-compiled with javac: Compilable with Java14
 package com.puppycrawl.tools.checkstyle.checks.sizes.recordcomponentnumber;
 
-public record InputRecordComponentNumberTopLevel2<T>() { // ok
+public record InputRecordComponentNumberTopLevel2<T>() {
 
 }
