355ddcecf78565fdb507acfdf7fe7c204e4445db
Coding-Aliens <kendrickfhtom35@gmail.com>
2024-01-25 09:36:08 -0600

    Issue #6207: Enable XpathRegressiontest for EqualsHashCode

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/equalshashcode/SuppressionXpathRegressionEqualsHashCode2.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.equalshashcode;
+
+public class SuppressionXpathRegressionEqualsHashCode2 {
+    public int hashCode() { // warn
+        return 0;
+    }
+}
