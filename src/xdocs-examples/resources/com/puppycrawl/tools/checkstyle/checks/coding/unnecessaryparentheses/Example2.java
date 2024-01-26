47b29983424ed8e9ded6ce67617473a2484ad913
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-01-26 22:28:19 +0200

    Issue #13345: EnableUnnecessaryParenthesesCheckExamplesTest
    Issue #13345: EnableUnnecessaryParenthesesCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/unnecessaryparentheses/Example2.java
@@ -0,0 +1,37 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="UnnecessaryParentheses">
+      <property name="tokens" value="BOR, BAND, BXOR" />
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.unnecessaryparentheses;
+
+// xdoc section -- start
+class Example2 {
+
+  void method() {
+    int x = 9, y = 8;
+    if(x>= 0 ^ (x<=8 & y<=11) // violation 'Unnecessary parentheses'
+         ^ y>=8) {
+      return;
+    }
+    if(x>= 0 ^ x<=8 & y<=11 ^ y>=8) {
+      return;
+    }
+    if(x>= 0 || (x<=8 & y<=11) // violation 'Unnecessary parentheses'
+        && y>=8) {
+      return;
+    }
+    if(x>= 0 || x<=8 & y<=11 && y>=8) {
+      return;
+    }
+    if(x>= 0 & (x<=8 ^ y<=11) & y>=8) {
+      return;
+    }
+  }
+
+}
+// xdoc section -- end
