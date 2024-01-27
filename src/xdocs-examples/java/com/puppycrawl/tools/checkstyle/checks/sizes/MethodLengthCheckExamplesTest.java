9c634ee329d50ec3e52111d18fb31987ecbfe343
MANISH-K-07 <manish07070707@gmail.com>
2024-01-27 16:10:03 +0000

    Issue #13345: Enabled MethodLengthCheckExamplesTest

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/sizes/MethodLengthCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/sizes/MethodLengthCheckExamplesTest.java
@@ -19,12 +19,12 @@
 
 package com.puppycrawl.tools.checkstyle.checks.sizes;
 
-import org.junit.jupiter.api.Disabled;
+import static com.puppycrawl.tools.checkstyle.checks.sizes.MethodLengthCheck.MSG_KEY;
+
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
 
-@Disabled("until https://github.com/checkstyle/checkstyle/issues/13345")
 public class MethodLengthCheckExamplesTest extends AbstractExamplesModuleTestSupport {
     @Override
     protected String getPackageLocation() {
@@ -34,27 +34,30 @@ public class MethodLengthCheckExamplesTest extends AbstractExamplesModuleTestSup
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-
+            "1:1: " + getCheckMessage("maxLen.file", 330, 60),
+            "16:3: " + getCheckMessage(MSG_KEY, 151, 150, "Example1"),
+            "177:3: " + getCheckMessage(MSG_KEY, 151, 150, "secondExample"),
         };
 
-        verifyWithInlineConfigParser(getPath("Example1.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
     }
 
     @Test
     public void testExample2() throws Exception {
         final String[] expected = {
-
+            "28:3: " + getCheckMessage(MSG_KEY, 6, 4, "secondMethod"),
+            "36:3: " + getCheckMessage(MSG_KEY, 5, 4, "thirdMethod"),
         };
 
-        verifyWithInlineConfigParser(getPath("Example2.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example2.java"), expected);
     }
 
     @Test
     public void testExample3() throws Exception {
         final String[] expected = {
-
+            "29:3: " + getCheckMessage(MSG_KEY, 6, 4, "secondMethod"),
         };
 
-        verifyWithInlineConfigParser(getPath("Example3.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example3.java"), expected);
     }
 }
