5513413937979bed78614eda6fbf6850aef7fc95
ChristianCecili <chri0407@gmail.com>
2024-01-27 14:41:16 +0100

    Issue #13345: Enable Test Cases GenericWhitespaceCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/genericwhitespace/Example1.java
@@ -0,0 +1,59 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="GenericWhitespace"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.whitespace.genericwhitespace;
+import com.google.common.collect.ImmutableList;
+import java.util.*;
+
+// xdoc section -- start
+public class Example1 {
+
+  @FunctionalInterface
+  interface ImmutableListBuilder<T> {
+      ImmutableList.Builder<T> createBuilder();
+  }
+
+  // Generic methods definitions
+  public static <K, V> boolean isFoo(K key, V value) { return false; }
+  public<T> void foo() {} // violation, ''<' is not preceded with whitespace.'
+
+  // Generic type definition
+  class MyClass<T1, T2> {
+    public MyClass() {}
+    public MyClass(T1 t1, T2 t2) {}
+  }
+
+  public void examples() {
+    // Generic type declaration
+    Map<String, List<Integer>> p;
+    Map<Integer, String>m; // violation, ''>' is followed by an illegal character.'
+    List< String> l; // violation, ''<' is followed by whitespace.'
+
+    // Generic method call
+    boolean same = Example1.<Object, Object>isFoo(null, null);
+    Box b = Box. <String>of("foo"); // violation, ''<' is preceded with whitespace.'
+
+    // Diamond operator
+    MyClass<Integer, String> p1 = new MyClass<Integer, String>(1, "apple");
+    MyClass<Integer, Integer > p2; // violation, ''>' is preceded with whitespace.'
+
+    // Method reference
+    ImmutableListBuilder<String> builderFactory = ImmutableList.Builder<String>::new;
+    Collections.sort(new ArrayList<String>(), Comparable::<String>compareTo);
+
+    // Generic constructor call
+    MyClass<String, String> obj = new MyClass<String, String>();
+    List a = new ArrayList<>  (); // ok, until #14344
+  }
+}
+// xdoc section -- end
+
+class Box {
+  public static <T> Box of(T value) {
+    return new Box();
+  }
+}
