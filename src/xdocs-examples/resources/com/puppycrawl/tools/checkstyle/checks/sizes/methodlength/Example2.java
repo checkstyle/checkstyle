93510d4b9722c5ec4c950321e48ec5d8bcb40ec9
MANISH-K-07 <manish07070707@gmail.com>
2024-01-27 16:10:03 +0000

    Issue #13345: Enable MethodLengthCheckExamplesTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/sizes/methodlength/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/sizes/methodlength/Example2.java
@@ -4,24 +4,25 @@
     <module name="MethodLength">
       <property name="tokens" value="METHOD_DEF"/>
       <property name="max" value="4"/>
-      <property name="countEmpty" value="false"/>
     </module>
   </module>
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.sizes.methodlength;
+
 // xdoc section -- start
-public class MyTest {
-  public MyTest()  {          // constructor (line 1)
+public class Example2 {
+  public Example2()  {          // constructor (line 1)
       int var1 = 2;           // line 2
       int var2 = 4;           // line 3
       int sum = var1 + var2;  // line 4
   } // line 5, OK, constructor is not mentioned in the tokens
 
   public void firstMethod() { // line 1
-      // comment - not counted as line
-      System.out.println("line 2");
-  } // line 3, OK, as it allows at most 4 lines
+      // comment (line 2)
+      System.out.println("line 3");
+  } // line 4, OK, as it allows at most 4 lines
 
   public void secondMethod() { // line 1
       int index = 0;   // line 2
@@ -32,8 +33,8 @@ public class MyTest {
 
   public void thirdMethod() { // line 1
 
-      // comment - not counted as line
-      System.out.println("line 2");
-  } // line 3, OK, as it allows at most 4 lines
+      // comment (line 3)
+      System.out.println("line 4");
+  } // line 5, violation, as it is over 4 lines
 }
 // xdoc section -- end
