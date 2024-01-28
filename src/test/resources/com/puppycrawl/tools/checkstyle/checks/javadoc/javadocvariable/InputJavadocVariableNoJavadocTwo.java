5c8e9f55ef1a24ce455900b9a51319c268c18744
MANISH-K-07 <manish07070707@gmail.com>
2024-01-28 07:45:30 +0000

    Issue #11163: Enforced filesize VariableNoJavadoc

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocvariable/InputJavadocVariableNoJavadocTwo.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocvariable/InputJavadocVariableNoJavadocTwo.java
@@ -10,56 +10,7 @@ tokens = (default)ENUM_CONSTANT_DEF
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocvariable;
 
-public class InputJavadocVariableNoJavadoc //comment test
-{
-    public int i1; // violation
-    protected int i2; // violation
-    int i3; // violation
-    private int i4; // violation
-
-    public void foo1() {}
-    protected void foo2() {}
-    void foo3() {}
-    private void foo4() {}
-
-    protected class ProtectedInner {
-        public int i1; // violation
-        protected int i2; // violation
-        int i3; // violation
-        private int i4; // violation
-
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
-    }
-
-    class PackageInner {
-        public int i1; // violation
-        protected int i2; // violation
-        int i3; // violation
-        private int i4; // violation
-
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
-    }
-
-    private class PrivateInner {
-        public int i1; // violation
-        protected int i2; // violation
-        int i3; // violation
-        private int i4; // violation
-
-        public void foo1() {}
-        protected void foo2() {}
-        void foo3() {}
-        private void foo4() {}
-    }
-}
-
-class PackageClass {
+public class InputJavadocVariableNoJavadocTwo {
     public int i1; // violation
     protected int i2; // violation
     int i3; // violation
