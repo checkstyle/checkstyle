e26e31c3196bd29d76fd9c22323164b1f8a0fb69
Rohanraj123 <rajrohan88293@gmail.com>
2024-01-30 09:08:38 +0530

    Issue #14249: Update AllBlockComments to use input files with violation comments

--- src/test/resources/com/puppycrawl/tools/checkstyle/grammar/comments/InputFullOfBlockComments.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/grammar/comments/InputFullOfBlockComments.java
@@ -1,6 +1,11 @@
+/*
+com.puppycrawl.tools.checkstyle.grammar.comments.AllBlockCommentsTest$ViolationFullOfBlockComments
+
+*/
 /*0*//*1*/package/*2*/ com/*3*/./*4*/puppycrawl/*5*/./*6*/tools/*7*/./*8*/checkstyle.grammar/*9*/./*10*/comments/*11*/;/*12*/
 /*13*/
 /*14*/public/*15*/ class /*16*/InputFullOfBlockComments /*49*/{/*17*/
+    //CHECKSTYLE:OFF
     /*18*/
     /*19*/public/*20*/ static/*
 21
@@ -9,8 +14,10 @@
         /*37*/String/*38*/./*  39  */CASE_INSENSITIVE_ORDER/*40*/./*41*/equals/*42*/(/*43*/line/*44*/)/*45*/;/*46*/
         Integer[] array = null;
         /*50*/for/*51*/ (/*52*/Integer/*53*/ i/*54*/:/*55*/ array/*56*/)/*57*/ {/*58*/
+            // violation, 'Nested block should be indented.'
             /*59*/
         }/*60*/
+        //CHECKSTYLE:ON
         return line;
     }/*47*/
 }/*48*/
