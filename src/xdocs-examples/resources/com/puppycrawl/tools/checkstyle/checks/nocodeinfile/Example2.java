400f6f30d31b1839f315f964f41a3202df43ac0d
MANISH-K-07 <manish07070707@gmail.com>
2024-01-31 10:47:28 +0000

    Issue #13345: Enabled NoCodeInFileCheckExamplesTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/nocodeinfile/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/nocodeinfile/Example2.java
@@ -6,8 +6,13 @@
 </module>
 */
 
+// violation 8 lines above
+
 // xdoc section -- start
-/* // violation
- block comment
+// the violation is on first line of file
+/*
+ public class Example2 {
+ block comment is not code
+ }
 */
 // xdoc section -- end
