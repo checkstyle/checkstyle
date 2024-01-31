a6f1c3c4dd15ef3c2c6b4ff127c1369746831487
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-01-31 04:56:58 +0200

    Issue #11446: update TreeWalkerTest to use verifyInlineConfigParser

--- src/test/resources/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalker.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalker.java
@@ -1,6 +1,11 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.design.OneTopLevelClassCheck
+
+*/
 package com.puppycrawl.tools.checkstyle.treewalker;
 /*comment*/
 public class InputTreeWalker {
 }
+//violation below
 class InputTreeWalkerInner {
 }
