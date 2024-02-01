0615e216d237898cea424c8839f3116361f5d5e3
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-02-01 21:18:48 +0200

    Issue #13345: Enable Example Tests for MethodLengthCheckExamplesTest

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/methodlength/Example2.java
@@ -0,0 +1,60 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MethodLength">
+      <property name="tokens" value="METHOD_DEF"/>
+      <property name="max" value="4"/>
+    </module>
+  </module>
+</module>
+*/
+
+//non-compiled with javac: Compilable with Java14
+package com.puppycrawl.tools.checkstyle.checks.sizes.methodlength;
+
+// xdoc section -- start
+public class Example2 {
+
+  // ok, CTOR_DEF is not in configured tokens
+  public Example2()  {
+    int var1 = 2;
+    int var2 = 4;
+    int sum = var1 + var2;
+  }
+
+  // ok, CTOR_DEF is not in configured tokens
+  public Example2(int a)  {
+    int var1 = 2;
+    int sum = var1 + a;
+  }
+
+  // violation below, 'Method firstMethod length is 6 lines (max allowed is 4)'
+  public void firstMethod() {
+    int index = 0;
+    if (index < 5) {
+      index++;
+    }
+  }
+
+  public void secondMethod() {
+    // comments are counted by default
+    System.out.println("line 3");
+  }
+
+  // violation below, 'Method thirdMethod length is 5 lines (max allowed is 4)'
+  public void thirdMethod() {
+
+    // empty line above is counted by default,just like this comment
+    System.out.println("line 4");
+  }
+
+  record MyBadRecord() {
+    // ok, COMPACT_CTOR_DEF is not in configured tokens
+    public MyBadRecord {
+
+      System.out.println("line3");
+      System.out.println("line4");
+    }
+  }
+}
+// xdoc section -- end
