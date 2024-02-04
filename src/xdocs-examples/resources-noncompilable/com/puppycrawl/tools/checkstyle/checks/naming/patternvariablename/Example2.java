1b296b85e20cb771df91029c6ac52b0e79a6afcd
MANISH-K-07 <manish07070707@gmail.com>
2024-02-04 09:13:55 +0000

    Issue #13345: Enabled PatternVariableNameCheckExamplesTest

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/naming/patternvariablename/Example2.java
@@ -0,0 +1,23 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="PatternVariableName">
+        <property name="format" value="^[a-z](_?[a-zA-Z0-9]+)*$"/>
+    </module>
+  </module>
+</module>
+*/
+
+//non-compiled with javac: Compilable with Java17
+package com.puppycrawl.tools.checkstyle.checks.naming.patternvariablename;
+
+// xdoc section -- start
+class Example2 {
+  Example2(Object o1){
+    if (o1 instanceof String STRING) { // violation
+    }
+    if (o1 instanceof Integer num) {
+    }
+  }
+}
+// xdoc section -- end
