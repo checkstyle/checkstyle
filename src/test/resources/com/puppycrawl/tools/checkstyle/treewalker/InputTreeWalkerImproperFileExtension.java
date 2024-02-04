a56788765442cefc98cfbc0771439c92d19278fa
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-02-04 04:31:52 +0200

    Issue #11446: update TreeWalkerTest to remove verify usage

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalkerImproperFileExtension.java
@@ -0,0 +1,12 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.naming.ConstantNameCheck
+
+*/
+
+package com.puppycrawl.tools.checkstyle.treewalker;
+
+public class InputTreeWalkerImproperFileExtension {
+
+    public static final int k = 5 + 4;
+
+}
