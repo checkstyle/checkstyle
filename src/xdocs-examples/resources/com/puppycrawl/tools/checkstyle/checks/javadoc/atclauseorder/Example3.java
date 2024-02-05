ff285fa41701fe649b006a24a5c95ac0fa9bc759
MANISH-K-07 <manish07070707@gmail.com>
2024-02-05 08:52:32 +0000

    Issue #13345: Enabled AtClauseOrderCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/atclauseorder/Example3.java
@@ -0,0 +1,48 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="AtclauseOrder"/>
+      <property name="tagOrder" 
+                value="@author, @version, @param, @return, @throws, @exception, @see,
+                        @since, @serial, @serialField, @serialData, @deprecated"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.atclauseorder;
+
+import java.io.Serializable;
+
+// xdoc section -- start
+/**
+* Some javadoc.
+*
+* @author Some javadoc.
+* @version Some javadoc.
+* @param Some javadoc.
+* @return Some javadoc.
+* @throws Some javadoc.
+* @exception Some javadoc.
+* @see Some javadoc.
+* @since Some javadoc.
+* @serial Some javadoc.
+* @serialField
+* @serialData
+* @deprecated Some javadoc.
+*/
+
+public class Example3 {}
+
+class Valid3 implements Serializable {}
+
+/**
+* Some javadoc.
+*
+* @since Some javadoc.
+* @version Some javadoc. // violation
+* @deprecated
+* @see Some javadoc. // violation
+* @author Some javadoc. // violation
+*/
+
+class Invalid3 implements Serializable {}
+// xdoc section -- end
