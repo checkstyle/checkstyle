0c285c101140e39ffe761134474f3ae813182b39
MANISH-K-07 <manish07070707@gmail.com>
2024-02-05 05:40:47 +0000

    Issue #13345: Enabled BooleanExpressionComplexityCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example3.java
@@ -0,0 +1,29 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="BooleanExpressionComplexity">
+      <property name="tokens" value="BXOR,LAND,LOR"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.metrics.booleanexpressioncomplexity;
+
+// xdoc section -- start
+public class Example3
+{
+  public static void main(String ... args)
+  {
+    boolean a = true;
+    boolean b = false;
+
+    boolean c = (a & b) | (b ^ a); // OK, 1(^) = 1 (max allowed 3)
+
+    boolean d = (a & b) | (b ^ a) | (a ^ b);
+    // OK above, 1(^) + 1(^) = 2, & and | are ignored here
+
+    boolean e = a ^ (a || b) ^ (b || a) & (a | b); // violation
+    // violation above, 1(^) + 1(||) + 1(^) + 1(||) = 4, & and | are ignored here
+  }
+}
+// xdoc section -- end
