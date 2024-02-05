3e1243e44e34b5adbc29eed665d265f4950639bd
MANISH-K-07 <manish07070707@gmail.com>
2024-02-05 05:40:47 +0000

    Issue #13345: Enabled BooleanExpressionComplexityCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example2.java
@@ -0,0 +1,27 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="BooleanExpressionComplexity">
+      <property name="max" value="5"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.metrics.booleanexpressioncomplexity;
+
+// xdoc section -- start
+public class Example2
+{
+  public static void main(String ... args)
+  {
+    boolean a = true;
+    boolean b = false;
+
+    boolean c = (a & b) | (b ^ a) | (a ^ b);
+    // OK above, 1(&) + 1(|) + 1(^) + 1(|) + 1(^) = 5
+
+    boolean d = (a | b) ^ (a | b) ^ (a || b) & b; // violation
+    // violation above, 1(|) + 1(^) + 1(|) + 1(^) + 1(||) + 1(&) = 6
+  }
+}
+// xdoc section -- end
---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example3.java
@@ -0,0 +1,28 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="BooleanExpressionComplexity">
+      <property name="tokens" value="BXOR,LAND,LOR"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.metrics.booleanexpressioncomplexity;
+
+// xdoc section -- start
+public class Example3
+{
+  public static void main(String ... args)
+  {
+    boolean a = true;
+    boolean b = false;
+
+    boolean c = (!a && b) | (a || !b) ^ a;    
+    // OK above, 1(&&) + 1(||) + 1(^) = 3, | is ignored here
+
+    boolean d = a ^ (a || b) ^ (b || a) & a; // violation
+    // violation above, 1(^) + 1(||) + 1(^) + 1(||) = 4
+    // & is ignored here
+  }
+}
+// xdoc section -- end
