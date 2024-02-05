c24f72875a7dd31183b80fe769cffbb9f5ca63ff
MANISH-K-07 <manish07070707@gmail.com>
2024-02-05 05:40:47 +0000

    Issue #13345: Enabled BooleanExpressionComplexityCheckExamplesTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example2.java
@@ -7,19 +7,20 @@
   </module>
 </module>
 */
+package com.puppycrawl.tools.checkstyle.checks.metrics;
 
 // xdoc section -- start
-public class Test
+public class Example2
 {
   public static void main(String ... args)
   {
     boolean a = true;
     boolean b = false;
 
-    boolean c = (a & b) | (b ^ a) | (a ^ b);   // OK, 1(&) + 1(|) + 1(^) + 1(|) + 1(^) = 5
+    boolean c = (a & b) | (b ^ a) | (a ^ b);   // OK, 1(&)+1(|)+1(^)+1(|)+1(^)=5
 
     boolean d = (a | b) ^ (a | b) ^ (a || b) & b; // violation,
-                                                 // 1(|) + 1(^) + 1(|) + 1(^) + 1(||) + 1(&) = 6
+                                                 // 1(|)+1(^)+1(|)+1(^)+1(||)+1(&)=6
   }
 }
 // xdoc section -- end
