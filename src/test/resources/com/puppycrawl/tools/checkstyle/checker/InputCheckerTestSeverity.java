6d8e4b603109c8ac0998e7e21a30801b85a163eb
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-02-11 11:30:03 +0530

    Issue #14019: Kill mutation for severity in Checker

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checker/InputCheckerTestSeverity.java
@@ -0,0 +1,5 @@
+  package com.puppycrawl.tools.checkstyle.checker;
+
+  public class InputCheckerTestSeverity {
+    String a = "ᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀᾀ";
+  }
