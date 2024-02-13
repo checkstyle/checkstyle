1bb3d569c975eea323094aed9e3fea4edb5a2e08
Tanay <tdmorakhia26@gmail.com>
2024-02-14 01:32:43 +0530

    Issue #13999: Resolve pitest suppression for validateDefaultJavadocTokens method of AbstractJavadocCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/meta/MetadataGeneratorUtilTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/meta/MetadataGeneratorUtilTest.java
@@ -80,7 +80,7 @@ public final class MetadataGeneratorUtilTest extends AbstractModuleTestSupport {
         final String[] expectedErrorMessages = {
             "31: " + getCheckMessage(MSG_DESC_MISSING, "AbstractSuperCheck"),
             "44: " + getCheckMessage(MSG_DESC_MISSING, "AbstractHeaderCheck"),
-            "42: " + getCheckMessage(MSG_DESC_MISSING, "AbstractJavadocCheck"),
+            "43: " + getCheckMessage(MSG_DESC_MISSING, "AbstractJavadocCheck"),
             "45: " + getCheckMessage(MSG_DESC_MISSING, "AbstractClassCouplingCheck"),
             "26: " + getCheckMessage(MSG_DESC_MISSING, "AbstractAccessControlNameCheck"),
             "30: " + getCheckMessage(MSG_DESC_MISSING, "AbstractNameCheck"),
