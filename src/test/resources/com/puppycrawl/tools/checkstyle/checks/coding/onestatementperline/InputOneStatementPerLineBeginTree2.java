99c1e74d6f7e88f04aa865766bfc7ee29edafb45
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2024-02-13 17:58:10 +0600

    Issue #13213: Removed //ok from onestatementperline

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineBeginTree2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineBeginTree2.java
@@ -5,7 +5,6 @@ OneStatementPerLine
 
 package com.puppycrawl.tools.checkstyle.checks.coding.onestatementperline;
 
-// ok
 public class InputOneStatementPerLineBeginTree2 {
     int a;
 }
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineBeginTreeLastVariableResourcesStatementEnd1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineBeginTreeLastVariableResourcesStatementEnd1.java
@@ -9,7 +9,6 @@ package com.puppycrawl.tools.checkstyle.checks.coding.onestatementperline;
 
 import java.io.*;
 
-// ok
 public class InputOneStatementPerLineBeginTreeLastVariableResourcesStatementEnd1 {
     public void resourceListExists() throws IOException {
         try (FileInputStream f1 = new FileInputStream("1"); FileInputStream f2 =
