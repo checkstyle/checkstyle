6edb023844174355197e0ae811b7a8924e9e6b02
ChristianCecili <chri0407@gmail.com>
2024-02-16 13:49:44 +0100

    Issue #11446: Update checker test method testUnmappableCharacters

--- src/test/resources/com/puppycrawl/tools/checkstyle/checker/InputCheckerTestCharset.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checker/InputCheckerTestCharset.java
@@ -1,3 +1,13 @@
+/*xml
+<module name="Checker">
+  <module name="com.puppycrawl.tools.checkstyle.checks.sizes.LineLengthCheck">
+    <property name="max" value="80"/>
+  </module>
+  <property name="charset" value="IBM1098"/>
+</module>
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checker;
 
 public class InputCheckerTestCharset {
