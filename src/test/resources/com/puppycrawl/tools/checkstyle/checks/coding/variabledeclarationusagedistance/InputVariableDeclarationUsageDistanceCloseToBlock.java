ebecf5b10cff7239dd240d98c588e048d46f57ee
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-02-23 03:48:05 +0200

    Issue #3502: update VariableDeclarationUsageDistance to do violation

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/variabledeclarationusagedistance/InputVariableDeclarationUsageDistanceCloseToBlock.java
@@ -0,0 +1,49 @@
+/*
+VariableDeclarationUsageDistance
+allowedDistance = 1
+ignoreVariablePattern = (default)
+validateBetweenScopes = false
+ignoreFinal = false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.variabledeclarationusagedistance;
+
+public class InputVariableDeclarationUsageDistanceCloseToBlock {
+    void method() {
+        int a = 3; // violation 'Distance between .* declaration and its first usage is 13.'
+        int b = 2; // violation 'Distance between .* declaration and its first usage is 13.'
+
+        run();
+        run();
+        run();
+        run();
+        run();
+        run();
+        run();
+        run();
+        run();
+        run();
+        run();
+        run();
+
+        for (int i = 0; i < 10; i++) {
+            a = a + b;
+        }
+    }
+
+    void testMethod() {
+        int wh, k;
+
+        for (int i = 0;i < 10;i++) {
+            if (i > 0) {
+                k = 6;    // distance = 1
+            } else {
+                wh = 6; // distance = 1
+            }
+        }
+    }
+
+    public void run() {};
+}
