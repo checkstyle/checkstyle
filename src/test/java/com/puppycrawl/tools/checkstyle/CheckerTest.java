3aa95cf072a12757629ea99739455a31b4617d01
MANISH-K-07 <manish07070707@gmail.com>
2024-02-26 17:08:11 +0530

    Issue #11446: Updated CheckerTest to use InlineConfigParser

--- src/test/java/com/puppycrawl/tools/checkstyle/CheckerTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/CheckerTest.java
@@ -1014,21 +1014,11 @@ public class CheckerTest extends AbstractModuleTestSupport {
 
     @Test
     public void testClearLazyLoadCacheInDetailAST() throws Exception {
-        final DefaultConfiguration checkConfig1 =
-            createModuleConfig(CheckWhichDoesNotRequireCommentNodes.class);
-        final DefaultConfiguration checkConfig2 =
-            createModuleConfig(CheckWhichRequiresCommentNodes.class);
-
-        final DefaultConfiguration treeWalkerConfig = createModuleConfig(TreeWalker.class);
-        treeWalkerConfig.addChild(checkConfig1);
-        treeWalkerConfig.addChild(checkConfig2);
-
-        final DefaultConfiguration checkerConfig = createRootConfig(treeWalkerConfig);
 
         final String filePath = getPath("InputCheckerClearDetailAstLazyLoadCache.java");
-        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
 
-        verify(checkerConfig, filePath, expected);
+        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+        verifyWithInlineConfigParser(filePath, expected);
     }
 
     @Test
