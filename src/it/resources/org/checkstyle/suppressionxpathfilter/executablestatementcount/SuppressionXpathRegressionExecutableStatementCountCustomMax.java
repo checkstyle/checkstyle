f689c8bec9cafc29063fef91afe09cc782b317d1
MANISH-K-07 <manish07070707@gmail.com>
2024-03-01 19:02:11 +0530

    Issue #6207: Add XPath IT Regression test ExecutableStatementCountCheck

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/executablestatementcount/SuppressionXpathRegressionExecutableStatementCountCustomMax.java
@@ -0,0 +1,14 @@
+package org.checkstyle.suppressionxpathfilter.executablestatementcount;
+
+public class SuppressionXpathRegressionExecutableStatementCountCustomMax {
+    public SuppressionXpathRegressionExecutableStatementCountCustomMax() // warn
+    {
+        int i = 1;
+        if (System.currentTimeMillis() == 0) {
+        } else if (System.currentTimeMillis() == 0) {
+        } else {
+        }
+    }
+    /** Empty constructor */
+    public SuppressionXpathRegressionExecutableStatementCountCustomMax(int i) {}
+}
