54e369203bf4c310530eb23961aa3cdf5148ac9c
pinti0001 <pinti000pk@gmail.com>
2024-03-03 01:54:25 +0530

    Issue #144436: Enforced naming convention for illegaltokentext

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/InputIllegalTokenTextTokensCheckNullCustomMessage.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/InputIllegalTokenTextTokensCheckNullCustomMessage.java
@@ -13,7 +13,7 @@ package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
 /**
  * Test for illegal tokens
  */
-public class InputIllegalTokenTextTokens4
+public class InputIllegalTokenTextTokensCheckNullCustomMessage
 {
     public void methodWithPreviouslyIllegalTokens()
     {
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/InputIllegalTokenTextTokensCheckTestCaseSensitive.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/InputIllegalTokenTextTokensCheckTestCaseSensitive.java
@@ -13,7 +13,7 @@ package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
 /**
  * Test for illegal tokens
  */
-public class InputIllegalTokenTextTokens
+public class InputIllegalTokenTextTokensCheckTestCaseSensitive
 {
     public void methodWithPreviouslyIllegalTokens()
     {
