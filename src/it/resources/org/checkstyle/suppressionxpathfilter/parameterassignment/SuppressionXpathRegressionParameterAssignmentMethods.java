1bf33f4bddaa77d9aed9afddc8975b46dfdfb359
MANISH-K-07 <manish07070707@gmail.com>
2024-03-02 22:30:16 +0530

    Issue #6207: Added Xpath IT Regression test for ParameterAssignmentTest

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/parameterassignment/SuppressionXpathRegressionParameterAssignmentMethods.java
@@ -0,0 +1,16 @@
+package com.puppycrawl.tools.checkstyle.checks.coding.parameterassignment;
+
+public class SuppressionXpathRegressionParameterAssignmentMethods {
+    int field;
+    void Test1(int field) {
+        int i = field;
+        this.field = field;
+        i++;
+        field += 1; // warn
+        this.field++;
+    }
+    // without parameters
+    void Test2() {
+        field = 0;
+    }
+}
