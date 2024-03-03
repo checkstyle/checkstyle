5cacecd941bbed70fb616994d777443bdfaef8fa
MANISH-K-07 <manish07070707@gmail.com>
2024-03-03 12:28:00 +0530

    Issue #14533: Documentation update for atclauseorder

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/atclauseorder/InputAtclauseOrderLotsOfRecords1.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/atclauseorder/InputAtclauseOrderLotsOfRecords1.java
@@ -3,8 +3,8 @@ AtclauseOrder
 violateExecutionOnNonTightHtml = (default)false
 target = (default)CLASS_DEF, INTERFACE_DEF, ENUM_DEF, METHOD_DEF, \
          CTOR_DEF, VARIABLE_DEF, RECORD_DEF, COMPACT_CTOR_DEF
-tagOrder = @author, @version, @param, @return, @throws, @exception, @see, @since, @serial, \
-           @serialField, @serialData, @deprecated
+tagOrder = (default)@author, @version, @param, @return, @throws, @exception, \
+            @see, @since, @serial, @serialField, @serialData, @deprecated
 
 
 */
