bf026891f09468841ad421571d5713b2b075496d
sktpy <145206423+sktpy@users.noreply.github.com>
2024-03-04 16:09:20 +0530

    Issue #13309: False positive error from JavadocType with record

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoctype/InputJavadocTypeParamDescriptionWithAngularTags.java
@@ -0,0 +1,53 @@
+/*
+JavadocType
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoctype;
+
+/**
+ *
+ * @param <T> <stuff>
+ */
+public class InputJavadocTypeParamDescriptionWithAngularTags<T> {}
+
+/**
+ *
+ * @param <T> 🐦‍🔥stuff<stuff>stu🐦‍🔥ff</stuff>stuff🐦‍🔥
+ */
+interface Ronin<T> {}
+
+/**
+ *
+ * @param <T> &lt;stuff&gt;
+ * @param <U> stUff&lt;stuff&gt;sutff&lt;🐦‍🔥&gt;🐦‍🔥
+ */
+class Shogun<T, U> {
+    /**
+     *
+     * @param <T> <//< <stuff></> stuff >>  <stuff>st<u>ff</stuff> >><
+     * @param    <U>     stuff <><><<stuff></></></> stuff
+     * @param <V> /-+`[]:^^<stuff **%%$>##(sutff){stuff}{}()stuff</stuff> @@
+     */
+    interface Mandate<T, U, V> {}
+
+    /**
+     *
+     * @param <V> [(<>{@code stuff<stuff>&lt;stuff&gt;}</>)]{@code {&lt;stuff&gt;}}
+     */
+    class Minowara<V> extends Shogun<T, U> implements Mandate<T, U, V> {}
+}
+
+/**
+ * @param <T>
+ * @param <P> stuff <><><<stuff></></></> stuff // violation, 'Unused @param tag for '<P>'.'
+ */
+interface Regent<T, U> {} // violation, 'Type Javadoc comment is missing @param <U> tag.'
+
+/**
+ *
+ * @param region [(<>{@code stuff<stuff🐦‍🔥@@@>🐦‍🔥&lt;{stuff}&gt;}</>@)]{@code {&lt;stuff&gt;}}
+ * // violation above, 'Unused @param tag for 'region'.'
+ */
+class Fief {}
