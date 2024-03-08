548b8f9c2426b9a884db93691788bdc68f90df16
Minghao Li <lmh1230612306@163.com>
2024-03-07 17:10:12 -0800

    Issue #13213: Remove "// ok" from onestatementperline

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineBeginTreeLastVariableResourcesStatementEnd2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineBeginTreeLastVariableResourcesStatementEnd2.java
@@ -9,7 +9,6 @@ package com.puppycrawl.tools.checkstyle.checks.coding.onestatementperline;
 
 import java.io.*;
 
-// ok
 public class InputOneStatementPerLineBeginTreeLastVariableResourcesStatementEnd2 {
     void m(OutputStream out) throws IOException {
         try (out; InputStream in = new FileInputStream("filename")) {
