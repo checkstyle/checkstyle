17fdfd3d829a282d833059db256604e20a2f9f18
Minghao Li <lmh1230612306@163.com>
2024-03-07 23:25:10 -0800

    Issue #13345: enable test for AvoidNoArgumentSuperConstructorCallCheck

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/AvoidNoArgumentSuperConstructorCallCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/AvoidNoArgumentSuperConstructorCallCheckExamplesTest.java
@@ -19,12 +19,10 @@
 
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
 
-@Disabled("until https://github.com/checkstyle/checkstyle/issues/13345")
 public class AvoidNoArgumentSuperConstructorCallCheckExamplesTest
         extends AbstractExamplesModuleTestSupport {
     @Override
@@ -35,9 +33,9 @@ public class AvoidNoArgumentSuperConstructorCallCheckExamplesTest
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-
+            "20:5: " + getCheckMessage(AvoidNoArgumentSuperConstructorCallCheck.MSG_CTOR),
         };
 
-        verifyWithInlineConfigParser(getPath("Example1.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
     }
 }
