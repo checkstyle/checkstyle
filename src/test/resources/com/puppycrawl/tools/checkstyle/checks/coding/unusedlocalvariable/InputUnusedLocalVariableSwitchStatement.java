15f5e6dbe67d7cf2974a5228ec682d39de9f8d8d
Kevin222004 <kevinpatel222004@gmail.com>
2024-03-19 09:31:23 +0530

    Issue #14506: False positive for UnusedLocalVariable

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedlocalvariable/InputUnusedLocalVariableSwitchStatement.java
@@ -0,0 +1,101 @@
+/*
+UnusedLocalVariable
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.unusedlocalvariable;
+
+public class InputUnusedLocalVariableSwitchStatement {
+    public void testParameters0() {
+
+        int i = 0;
+        switch (i++) {
+            default:
+            case 0:
+                System.out.println("one: ");
+                break;
+            case 1:
+                System.out.println("two: " );
+                break;
+        }
+
+    }
+
+    public void testParameters01() {
+        int i = 0;
+        switch (++i) {
+            default:
+            case 0:
+                System.out.println("one: ");
+                break;
+            case 1:
+                System.out.println("two: " );
+                break;
+        }
+
+    }
+
+    public void testParameters() {
+        String[] tests = { "one", "two" };
+        int i = 0;
+        for (String test : tests) {
+            switch (i++) {
+                default:
+                case 0:
+                    System.out.println("one: " + test);
+                    break;
+                case 1:
+                    System.out.println("two: " + test);
+                    break;
+            }
+        }
+    }
+
+    public void testParameters2() {
+        String[] tests = { "one", "two" };
+        int i = 0;
+        for (String test : tests) {
+            switch (++i) {
+                default:
+                case 0:
+                    System.out.println("one: " + test);
+                    break;
+                case 1:
+                    System.out.println("two: " + test);
+                    break;
+            }
+        }
+    }
+
+    public void testParameters3() {
+        String[] tests = { "one", "two" };
+        int i = 0;
+        for (String test : tests) {
+            switch (--i) {
+                default:
+                case 0:
+                    System.out.println("one: " + test);
+                    break;
+                case 1:
+                    System.out.println("two: " + test);
+                    break;
+            }
+        }
+    }
+
+    public void testParameters4() {
+        String[] tests = { "one", "two" };
+        int i = 0;
+        for (String test : tests) {
+            switch (i--) {
+                default:
+                case 0:
+                    System.out.println("one: " + test);
+                    break;
+                case 1:
+                    System.out.println("two: " + test);
+                    break;
+            }
+        }
+    }
+}
