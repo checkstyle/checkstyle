2a5bceb771f1fbb6daf3e90a217df44fb3d8a7ac
MANISH-K-07 <manish07070707@gmail.com>
2024-03-23 15:43:13 +0530

    doc: updated cmdline docs to be more specific

--- src/main/java/com/puppycrawl/tools/checkstyle/Main.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/Main.java
@@ -723,16 +723,16 @@ public final class Main {
 
         /** Option that controls whether to print the AST of the file. */
         @Option(names = {"-t", "--tree"},
-                description = "Prints Abstract Syntax Tree(AST) of the checked file. The option "
-                        + "cannot be used other options and requires exactly one file to run on "
-                        + "to be specified.")
+                description = "This option is used to display the Abstract Syntax Tree (AST) "
+                        + "of the specified file. It can only be used on a single file and "
+                        + "cannot be combined with other options.")
         private boolean printAst;
 
         /** Option that controls whether to print the AST of the file including comments. */
         @Option(names = {"-T", "--treeWithComments"},
-                description = "Prints Abstract Syntax Tree(AST) with comment nodes "
-                        + "of the checked file. The option cannot be used with other options "
-                        + "and requires exactly one file to run on to be specified.")
+                description = "This option is used to display the Abstract Syntax Tree (AST) "
+                        + "with comment nodes of the specified file. It can only be used on a "
+                        + "single file and cannot be combined with other options.")
         private boolean printAstWithComments;
 
         /** Option that controls whether to print the parse tree of the javadoc comment. */
@@ -740,18 +740,17 @@ public final class Main {
                 description = "Prints Parse Tree of the Javadoc comment. "
                         + "The file have to contain only Javadoc comment content without "
                         + "including '/**' and '*/' at the beginning and at the end respectively. "
-                        + "The option cannot be used other options and requires exactly one file "
-                        + "to run on to be specified.")
+                        + "It can only be used on a single file and cannot be combined "
+                        + "with other options.")
         private boolean printJavadocTree;
 
         /** Option that controls whether to print the full AST of the file. */
         @Option(names = {"-J", "--treeWithJavadoc"},
-                description = "Prints Abstract Syntax Tree(AST) with Javadoc nodes "
-                        + "and comment nodes of the checked file. Attention that line number and "
-                        + "columns will not be the same as it is a file due to the fact that each "
-                        + "javadoc comment is parsed separately from java file. The option cannot "
-                        + "be used with other options and requires exactly one file to run on to "
-                        + "be specified.")
+                description = "This option is used to display the Abstract Syntax Tree (AST) "
+                        + "with Javadoc nodes of the specified file. It can only be used on a "
+                        + "single file and cannot be combined with other options. Attention that "
+                        + "line number and columns will not be the same due to the fact that "
+                        + "each javadoc comment is parsed separately from java file.")
         private boolean printTreeWithJavadoc;
 
         /** Option that controls whether to print debug info. */
