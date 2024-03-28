4725dc5eee1545695c43b4fafa03812d932563fa
Minghao Li <lmh1230612306@163.com>
2024-03-27 22:08:29 -0700

    Issue #14715: Enforce new naming convention on RecordTypeParameterName in IT area

--- src/it/resources-noncompilable/org/checkstyle/suppressionxpathfilter/recordtypeparametername/InputXpathRecordTypeParameterNameTypeDeclared.java
+++ src/it/resources-noncompilable/org/checkstyle/suppressionxpathfilter/recordtypeparametername/InputXpathRecordTypeParameterNameTypeDeclared.java
@@ -4,7 +4,7 @@ package com.puppycrawl.tools.checkstyle.checks.naming.classtypeparametername;
 import java.io.Serializable;
 import java.util.LinkedHashMap;
 
-record Other <foo extends Serializable & Cloneable> // warn
+record InputXpathRecordTypeParameterNameTypeDeclared <foo extends Serializable & Cloneable> // warn
 (LinkedHashMap<String, Node> linkedHashMap) {
 
 }
