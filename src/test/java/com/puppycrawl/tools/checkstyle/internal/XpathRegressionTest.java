645a1db6be422b0aa1c8a673b296058544e5169a
Minghao Li <lmh1230612306@163.com>
2024-03-27 22:08:29 -0700

    Issue #14715: Enforce new naming convention on RecordTypeParameterName in IT area

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XpathRegressionTest.java
@@ -215,8 +215,7 @@ public class XpathRegressionTest extends AbstractModuleTestSupport {
             "PackageDeclaration",
             "PatternVariableName",
             "RecordComponentName",
-            "RecordComponentNumber",
-            "RecordTypeParameterName");
+            "RecordComponentNumber");
 
     private Path javaDir;
     private Path inputDir;
