dc9874de5a1ca980dfce9dc1915ef8ef276f7cac
MANISH-K-07 <manish07070707@gmail.com>
2024-03-29 08:08:25 +0100

    Issue #14715: Enforced new naming convention in IT area Four

--- src/it/resources/org/checkstyle/suppressionxpathfilter/hiddenfield/InputXpathHiddenFieldLambdaExpInMethodCall.java
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/hiddenfield/InputXpathHiddenFieldLambdaExpInMethodCall.java
@@ -3,7 +3,7 @@ package org.checkstyle.suppressionxpathfilter.hiddenfield;
 import java.util.Arrays;
 import java.util.List;
 
-public class SuppressionXpathRegressionHiddenFieldOne {
+public class InputXpathHiddenFieldLambdaExpInMethodCall {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
     Integer value = Integer.valueOf(1);
     {
