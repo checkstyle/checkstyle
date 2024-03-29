b5431ae95b94aff51a340d8a86441caaec5acca2
MANISH-K-07 <manish07070707@gmail.com>
2024-03-29 07:41:54 +0100

    Issue #14715: Enforce new naming convention in IT area Three

--- src/it/resources/org/checkstyle/suppressionxpathfilter/emptyforiteratorpad/InputXpathEmptyForIteratorPadFollowed.java
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/emptyforiteratorpad/InputXpathEmptyForIteratorPadFollowed.java
@@ -1,6 +1,6 @@
 package org.checkstyle.suppressionxpathfilter.emptyforiteratorpad;
 
-public class SuppressionXpathRegressionEmptyForIteratorPadFollowed {
+public class InputXpathEmptyForIteratorPadFollowed {
     void method(int bad, int good) {
         for (bad = 0; ++bad < 1; ) {//warn
         }
