9c35f3e5a7570b6a6f2d25a52f8067dbc9b31916
Minghao Li <lmh1230612306@163.com>
2024-03-30 00:11:35 -0700

    Issue #13758: Add pattern array converter for auto bean

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/metrics/classfanoutcomplexity/InputClassFanOutComplexityLambdaNew.java
@@ -0,0 +1,36 @@
+/*
+ClassFanOutComplexity
+max = 0
+excludedClasses = (default)ArrayIndexOutOfBoundsException, ArrayList, Boolean, Byte, \
+                  Character, Class, Collection, Deprecated, Deque, Double, DoubleStream, \
+                  EnumSet, Exception, Float, FunctionalInterface, HashMap, HashSet, \
+                  IllegalArgumentException, IllegalStateException, IndexOutOfBoundsException, \
+                  IntStream, Integer, LinkedHashMap, LinkedHashSet, LinkedList, List, Long, \
+                  LongStream, Map, NullPointerException, Object, Optional, OptionalDouble, \
+                  OptionalInt, OptionalLong, Override, Queue, RuntimeException, SafeVarargs, \
+                  SecurityException, Set, Short, SortedMap, SortedSet, Stream, String, \
+                  StringBuffer, StringBuilder, SuppressWarnings, Throwable, TreeMap, TreeSet, \
+                  UnsupportedOperationException, Void, boolean, byte, char, double, float, \
+                  int, long, short, var, void
+excludedPackages = (default)
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.metrics.classfanoutcomplexity;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.io.File;
+
+public class InputClassFanOutComplexityLambdaNew {
+    public void testMethod(Map<String, List<String>> entry) {
+        List<File> files = new ArrayList<>();
+        String string = "";
+        entry.values().stream()
+                .flatMap(List::stream)
+                .map(File::new)
+                .forEach(files::add);
+    }
+}
