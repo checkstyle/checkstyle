5734c0e1a7e79bd584dad787bc0c7d082c863862
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-03-31 18:58:41 +0530

    Issue #14726: new check: ConstructorsDeclarationGroupingCheck

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/ConstructorsDeclarationGroupingCheckTest.java
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2024 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+import static com.puppycrawl.tools.checkstyle.checks.coding.ConstructorsDeclarationGroupingCheck.MSG_KEY;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+
+public class ConstructorsDeclarationGroupingCheckTest extends AbstractModuleTestSupport {
+    @Override
+    protected String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/checks/coding/constructorsdeclarationgrouping";
+    }
+
+    @Test
+    public void testDefault() throws Exception {
+        final String[] expected = {
+            "23:5: " + getCheckMessage(MSG_KEY, 19),
+            "27:5: " + getCheckMessage(MSG_KEY, 23),
+            "40:9: " + getCheckMessage(MSG_KEY, 36),
+            "49:13: " + getCheckMessage(MSG_KEY, 43),
+        };
+        verifyWithInlineConfigParser(
+                getPath("InputConstructorsDeclarationGrouping.java"), expected);
+    }
+
+    @Test
+    public void testConstructorsDeclarationGroupingRecords() throws Exception {
+
+        final String[] expected = {
+            "18:9: " + getCheckMessage(MSG_KEY, 12),
+            "34:9: " + getCheckMessage(MSG_KEY, 30),
+        };
+
+        verifyWithInlineConfigParser(
+                getNonCompilablePath("InputConstructorsDeclarationGroupingRecords.java"),
+                expected);
+    }
+
+}
