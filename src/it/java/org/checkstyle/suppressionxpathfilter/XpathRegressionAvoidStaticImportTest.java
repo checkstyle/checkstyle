c4697a253a4bbaaddbb1981a23c9f6f40e636392
MANISH-K-07 <manish07070707@gmail.com>
2024-04-02 14:43:33 +0100

    Issue #14715: Enforced new naming convention in IT area Eight

--- src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionAvoidStaticImportTest.java
+++ src/it/java/org/checkstyle/suppressionxpathfilter/XpathRegressionAvoidStaticImportTest.java
@@ -42,7 +42,7 @@ public class XpathRegressionAvoidStaticImportTest
     @Test
     public void testOne() throws Exception {
         final File fileToProcess = new File(getPath(
-            "SuppressionXpathRegressionAvoidStaticImport1.java"));
+            "InputXpathAvoidStaticImportOne.java"));
 
         final DefaultConfiguration moduleConfig =
                 createModuleConfig(CLASS);
@@ -62,7 +62,7 @@ public class XpathRegressionAvoidStaticImportTest
     @Test
     public void testTwo() throws Exception {
         final File fileToProcess = new File(getPath(
-            "SuppressionXpathRegressionAvoidStaticImport2.java"));
+            "InputXpathAvoidStaticImportTwo.java"));
 
         final DefaultConfiguration moduleConfig =
                 createModuleConfig(CLASS);
