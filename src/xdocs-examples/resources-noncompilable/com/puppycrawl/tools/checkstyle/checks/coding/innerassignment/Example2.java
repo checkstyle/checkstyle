ab49920a39c6a79152878f745d866f123bbbb6c1
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-04-17 00:04:28 +0200

    Issue #13086: fix InnerAssignmentCheck In SwitchRuleAndExpression

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/innerassignment/Example2.java
@@ -0,0 +1,47 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="InnerAssignment"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.innerassignment;
+
+//non-compiled with javac: Compilable with Java14
+
+// xdoc section -- start
+public class Example2 {
+  public void test1(int mode) {
+    int x = 0;
+    x = switch (mode) {
+      case 1 -> x = 1;  // violation
+      case 2 -> {
+        yield x = 2; // violation
+      }
+      default -> x = 0; // violation
+    };
+  }
+  public void test2(int mode) {
+    int x = 0;
+    switch(mode) {
+      case 2 -> {
+        x = 2;
+      }
+      case 1 -> x = 1;
+    }
+  }
+  public void test3(int mode) {
+    int x = 0, y = 0;
+    switch(mode) {
+      case 1:
+      case 2: {
+        x = y = 2; // violation
+      }
+      case 4:
+      case 5:
+        x = 1;
+    }
+  }
+}
+// xdoc section -- end
