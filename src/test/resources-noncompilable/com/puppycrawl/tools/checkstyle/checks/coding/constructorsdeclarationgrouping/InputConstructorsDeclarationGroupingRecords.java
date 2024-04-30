f6ec2d77e977ae831780f56717cbaa778ba56e39
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-04-30 23:06:22 +0530

    Issue #14726: new implementation of the module

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/constructorsdeclarationgrouping/InputConstructorsDeclarationGroupingRecords.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/constructorsdeclarationgrouping/InputConstructorsDeclarationGroupingRecords.java
@@ -15,9 +15,18 @@ public class InputConstructorsDeclarationGroupingRecords {
 
         void foo() {}
 
-        public MyRecord1 {} // violation
+        void foo2() {}
+
+        public MyRecord1 {}
+        // violation above 'Constructors should be grouped together.*'
+
+        public MyRecord1(int a, int b, int c, int d) {
+            // violation above 'Constructors should be grouped together.*'
+            this(a+b, c+d);
+        }
 
         public MyRecord1(int x, int y, int z) {
+            // violation above 'Constructors should be grouped together.*'
             this(x+y, z);
         }
     }
@@ -31,6 +40,41 @@ public class InputConstructorsDeclarationGroupingRecords {
 
         String[] str;
 
-        MyClass(int a) {} // violation
+        String[] str2;
+
+        MyClass(int a) {}
+        // violation above 'Constructors should be grouped together.*'
     }
+
+    public record MyRecord2(double d) {
+        public MyRecord2(double a, double b, double c) {
+            this(a+b+c);
+        }
+
+        public MyRecord2 {}
+
+        public MyRecord2(double a, double b) {
+            this(a+b);
+        }
+    }
+
+    public record MyRecord3(float f) {
+        public MyRecord3(float a, float b, float c) {
+            this(a+b+c);
+        }
+    }
+
+    public record MyRecord4(String str) {
+        public MyRecord4 {}
+    }
+
+    public record MyRecord5(long l) {
+        void test() {}
+
+        void test2() {}
+
+        void test3() {}
+    }
+
+    public record MyRecord6(String str, int x) {}
 }
