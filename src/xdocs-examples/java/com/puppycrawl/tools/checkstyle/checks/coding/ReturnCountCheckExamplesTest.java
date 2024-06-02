e54a32e256dbf89ff9b98e9aad11e0774cb60267
SteLeo1602 <stepanleontev16@gmail.com>
2024-06-02 00:35:37 -0700

    Issue #13345: Enabled ReturnCount example tests

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/ReturnCountCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/ReturnCountCheckExamplesTest.java
@@ -19,12 +19,12 @@
 
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
-import org.junit.jupiter.api.Disabled;
+import static com.puppycrawl.tools.checkstyle.checks.coding.ReturnCountCheck.MSG_KEY;
+import static com.puppycrawl.tools.checkstyle.checks.coding.ReturnCountCheck.MSG_KEY_VOID;
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
 
-@Disabled("until https://github.com/checkstyle/checkstyle/issues/13345")
 public class ReturnCountCheckExamplesTest extends AbstractExamplesModuleTestSupport {
     @Override
     protected String getPackageLocation() {
@@ -34,45 +34,53 @@ public class ReturnCountCheckExamplesTest extends AbstractExamplesModuleTestSupp
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-
+            "53:9: " + getCheckMessage(MSG_KEY, 4, 3)
         };
 
-        verifyWithInlineConfigParser(getPath("Example1.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
     }
 
     @Test
     public void testExample2() throws Exception {
         final String[] expected = {
-
+            "20:9: " + getCheckMessage(MSG_KEY_VOID, 1, 0),
+            "40:9: " + getCheckMessage(MSG_KEY, 3, 2),
+            "51:13: " + getCheckMessage(MSG_KEY, 4, 2),
+            "71:9: " + getCheckMessage(MSG_KEY_VOID, 1, 0)
         };
 
-        verifyWithInlineConfigParser(getPath("Example2.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example2.java"), expected);
     }
 
     @Test
     public void testExample3() throws Exception {
         final String[] expected = {
-
+            "41:9: " + getCheckMessage(MSG_KEY, 3, 2),
+            "52:13: " + getCheckMessage(MSG_KEY, 4, 2)
         };
 
-        verifyWithInlineConfigParser(getPath("Example3.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example3.java"), expected);
     }
 
     @Test
     public void testExample4() throws Exception {
         final String[] expected = {
-
+            "41:9: " + getCheckMessage(MSG_KEY, 4, 3),
+            "52:13: " + getCheckMessage(MSG_KEY, 5, 3)
         };
 
-        verifyWithInlineConfigParser(getPath("Example4.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example4.java"), expected);
     }
 
     @Test
     public void testExample5() throws Exception {
         final String[] expected = {
-
+            "29:9: " + getCheckMessage(MSG_KEY_VOID, 1, 0),
+            "49:9: " + getCheckMessage(MSG_KEY, 3, 2),
+            "60:13: " + getCheckMessage(MSG_KEY, 4, 2),
+            "69:9: " + getCheckMessage(MSG_KEY, 2, 1)
         };
 
-        verifyWithInlineConfigParser(getPath("Example5.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example5.java"), expected);
     }
 }
