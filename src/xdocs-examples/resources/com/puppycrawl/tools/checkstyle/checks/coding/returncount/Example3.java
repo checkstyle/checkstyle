052b2fa6716c819eb376eb54bd395d99022cc3a7
SteLeo1602 <stepanleontev16@gmail.com>
2024-06-02 00:35:37 -0700

    Issue #13345: Enabled ReturnCount example tests

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/returncount/Example3.java
@@ -0,0 +1,44 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="ReturnCount">
+      <property name="max" value="2"/>
+      <property name="maxForVoid" value="1"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.returncount;
+
+// xdoc section -- start
+import java.util.function.Predicate;
+
+public class Example3 {
+    public Example3() {}
+
+    public Example3(int i) { return; }
+
+    public int signA(int x) {
+        if (x < -2) { return -1; }
+        return 0;
+    }
+
+    public int signB(int x) { // violation, 'max allowed for non-void methods/lambdas is 2'
+        if (x < -2) { return -1; }
+        if (x == 0) { return 0; }
+        if (x > 2) { return 2; }
+        return 1;
+    }
+
+    final Predicate<Integer> lambdaA = i -> {
+        if (i > 5) { return true; }
+        return false;
+    };
+
+    final Predicate<Integer> lambdaB = i -> { return i > 5; };
+
+    public void methodA(int x) {}
+
+    public void methodB(int x) { return; }
+}
+// xdoc section -- end
