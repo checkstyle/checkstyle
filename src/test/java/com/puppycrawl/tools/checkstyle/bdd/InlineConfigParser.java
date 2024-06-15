37320d4a4c170e3a70700284241b2993a99392b0
SteLeo1602 <stepanleontev16@gmail.com>
2024-06-14 21:07:30 -0700

    Issue #13345: Enabled SeverityMatch example tests

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -368,6 +368,10 @@ public final class InlineConfigParser {
                 "com.puppycrawl.tools.checkstyle.checks.javadoc.SummaryJavadocCheck");
         moduleMappings.put("LineLength",
                 "com.puppycrawl.tools.checkstyle.checks.sizes.LineLengthCheck");
+        moduleMappings.put("ParameterName",
+                "com.puppycrawl.tools.checkstyle.checks.naming.ParameterNameCheck");
+        moduleMappings.put("MethodName",
+                "com.puppycrawl.tools.checkstyle.checks.naming.MethodNameCheck");
 
         String fullyQualifiedClassName;
         if (moduleMappings.containsKey(moduleName)) {
