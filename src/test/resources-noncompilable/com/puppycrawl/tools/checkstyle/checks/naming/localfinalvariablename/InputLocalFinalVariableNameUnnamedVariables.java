aa27fad0f455662736832d6c78559c05a5eca5ab
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-06-22 02:40:06 +0300

    Issue #15012: Update LocalFinalVariableNameCheck to allow unnnamed variables by default

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/naming/localfinalvariablename/InputLocalFinalVariableNameUnnamedVariables.java
@@ -0,0 +1,44 @@
+/*
+LocalFinalVariableName
+
+*/
+
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.naming.localfinalvariablename;
+
+public class InputLocalFinalVariableNameUnnamedVariables {
+
+    void testTryWithResource(Object obj) {
+        try (var _ = lock()) {
+
+        }
+        catch (final Exception _) {
+        }
+
+        try (var __ = lock()) {   // violation, 'Name '__' must match pattern*.'
+
+        }
+        catch (final Exception __) {  // violation, 'Name '__' must match pattern*.'
+        }
+
+    }
+
+    void testEnhancedForLoop() {
+        for (final var _ : new int[0]) {
+        }
+        for (final var __ : new int[0]) { // violation, 'Name '__' must match pattern*.'
+        }
+        for (final var _BAD : new int[0]) { // violation, 'Name '_BAD' must match pattern*.'
+        }
+    }
+
+    void testLocalVariable(Object obj) {
+        final var _ = obj;
+        final var __ = obj; // violation, 'Name '__' must match pattern*.'
+        final var _BAD = obj; // violation, 'Name '_BAD' must match pattern*.'
+    }
+
+    public AutoCloseable lock() {
+        return null;
+    }
+}
