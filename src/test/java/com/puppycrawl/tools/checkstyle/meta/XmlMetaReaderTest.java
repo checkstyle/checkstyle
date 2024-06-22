d7888b2e7df466aa40c02c3ab3d55366a246467b
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-06-22 04:58:57 +0300

    Issue #14979: New Check MissingNullCaseInSwitch

--- src/test/java/com/puppycrawl/tools/checkstyle/meta/XmlMetaReaderTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/meta/XmlMetaReaderTest.java
@@ -40,20 +40,20 @@ public class XmlMetaReaderTest extends AbstractPathTestSupport {
 
     @Test
     public void test() {
-        assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny()).hasSize(202);
+        assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny()).hasSize(203);
     }
 
     @Test
     public void testDuplicatePackage() {
         assertThat(XmlMetaReader
                     .readAllModulesIncludingThirdPartyIfAny("com.puppycrawl.tools.checkstyle.meta"))
-                .hasSize(202);
+                .hasSize(203);
     }
 
     @Test
     public void testBadPackage() {
         assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny("DOES.NOT.EXIST"))
-                .hasSize(202);
+                .hasSize(203);
     }
 
     @Test
