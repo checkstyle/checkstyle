e2f8234205d893edacf65646162a6583a4699940
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-06-22 17:11:03 +0530

    Issue #14937: Migrated section 3.3.2 No Line-Wrapping to Chapter Wise Testing

---
+++ src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule332nolinewrap/InputNoLineWrapping.java
@@ -0,0 +1,28 @@
+package com.google.checkstyle.test.chapter3filestructure.rule332nolinewrap;
+
+import static java.math.BigInteger.ZERO; // ok
+import com.google.checkstyle.test.chapter3filestructure.toolongpackagetotestcoveragegooglesjavastylerule.*; // ok, long imports are allowed
+
+import com.puppycrawl.tools.checkstyle.checks.design.FinalClassCheck; // ok
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater; // ok
+import javax.accessibility.AccessibleAttributeSequence; // ok
+
+import javax.accessibility. // violation 'import statement should not be line-wrapped.'
+        AccessibleAttributeSequence;
+import static java.math. // violation 'import statement should not be line-wrapped.'
+        BigInteger.ONE;
+import java.util.concurrent.atomic // violation 'import statement should not be line-wrapped.'
+        .AtomicMarkableReference;
+
+public class InputNoLineWrapping {
+
+  public void fooMethod() {
+      //
+  }
+
+  // Long line ----------------------------------------------------------------------------------------
+  // violation above 'Line is longer than 100 characters (found 103).'
+
+  private int[] mInts = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, };
+  // violation above 'Line is longer than 100 characters (found 104).'
+}
