19104af8ea3adcc53b18176ff9f7dc0103c9218f
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-06-24 20:26:08 +0530

    Issue #14937: Migrated section 3.3.3 Ordering And Spacing to use chapter wise testing

---
+++ src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule333orderingandspacing/InputOrderingAndSpacing4.java
@@ -0,0 +1,23 @@
+package com.google.checkstyle.test.chapter3filestructure.rule333orderingandspacing;
+
+import static java.awt.Button.ABORT;
+import static java.io.File.createTempFile;
+
+
+import static javax.swing.WindowConstants.*;
+// violation above 'Extra separation in import group before 'javax.swing.WindowConstants.*''
+
+import com.google.checkstyle.test.chapter2filebasic.rule21filename.*;
+import com.google.checkstyle.test.chapter3filestructure.rule3sourcefile.*;
+import com.google.common.reflect.*;
+import java.util.List;
+
+
+import java.util.StringTokenizer;
+// violation above 'Extra separation in import group before 'java.util.StringTokenizer''
+
+import java.util.concurrent.AbstractExecutorService;
+// violation above 'Extra separation .* before 'java.util.concurrent.AbstractExecutorService''
+
+public class InputOrderingAndSpacing4 {
+}
