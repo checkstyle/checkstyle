9dbfe23f07b8f894a5ee2faaf6a9a0bdc36d66e8
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-06-25 04:47:13 +0300

    Issue #15016: Remove underscore from illegal identifers format

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/naming/illegalidentifiername/InputIllegalIdentifierNameUnnamedVariables.java
@@ -0,0 +1,53 @@
+/*
+IllegalIdentifierName
+format = (default)(?i)^(?!(record|yield|var|permits|sealed)$).+$
+tokens = (default)CLASS_DEF, INTERFACE_DEF, ENUM_DEF, ANNOTATION_DEF, ANNOTATION_FIELD_DEF, \
+         PARAMETER_DEF, VARIABLE_DEF, METHOD_DEF, ENUM_CONSTANT_DEF, PATTERN_VARIABLE_DEF, \
+         RECORD_DEF, RECORD_COMPONENT_DEF, LAMBDA
+
+*/
+
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.naming.illegalidentifiername;
+
+public class InputIllegalIdentifierNameUnnamedVariables {
+    void m(Object o) {
+        int _ = sideEffect();
+        int __ = sideEffect();
+        for (Integer _ : new int[0]) {
+
+        }
+        for (Integer BAD_ : new int[0]) {
+
+        }
+        for (Integer _BAD : new int[0]) {
+
+        }
+        try (var _ = lock()) {
+        }
+        catch (Exception _) {
+        }
+
+        switch(o) {
+            case Integer _ : {}
+            default: {}
+        }
+        if (o instanceof Integer _) {}
+
+        switch (o) {
+            case R(int _ ,int _): {}
+            default : {}
+        }
+        if (o instanceof R(int _ , _)) {}
+    }
+
+    int sideEffect() {
+        return 0;
+    }
+
+    AutoCloseable lock() {
+        return null;
+    }
+
+    record R(int x, int y) {}
+}
