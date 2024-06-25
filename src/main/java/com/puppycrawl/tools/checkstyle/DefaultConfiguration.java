6418d1720535c5b9cab4889f2aeb51d6211a48a9
Rohanraj123 <rajrohan88293@gmail.com>
2024-06-25 13:58:15 +0530

    Issue #14084: Remove checker framework violation for DefaultConfiguration

--- src/main/java/com/puppycrawl/tools/checkstyle/DefaultConfiguration.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/DefaultConfiguration.java
@@ -25,6 +25,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.annotation.Nonnull;
+
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import com.puppycrawl.tools.checkstyle.api.Configuration;
 import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
@@ -102,6 +104,7 @@ public final class DefaultConfiguration implements Configuration {
     }
 
     @Override
+    @Nonnull
     public Configuration[] getChildren() {
         return children.toArray(
                 EMPTY_CONFIGURATION_ARRAY);
