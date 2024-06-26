cad5c4219da397e492aaa7d7eb4d7bbe0d29400e
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-06-26 22:25:16 +0530

    Issue #14937: Migrated section 5.2.5 Non Constant Field Names to chapter wise testing

---
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule525nonconstantfieldnames/InputNonConstantNamesSimple.java
@@ -0,0 +1,141 @@
+package com.google.checkstyle.test.chapter5naming.rule525nonconstantfieldnames;
+
+final class InputNonConstantNamesSimple
+{
+
+    public static final int badConstant = 2;
+
+    public static final int MAX_ROWS = 2;
+
+
+    private int bad$Static = 2; // violation 'Member name .* must match pattern'
+
+    private static int sum_Created = 0;
+
+
+    private int bad_Member = 2; // violation 'Member name 'bad_Member' must match pattern'
+
+    private int m = 0; // violation 'Member name 'm' must match pattern'
+
+    protected int m_M = 0; // violation 'Member name 'm_M' must match pattern'
+
+
+    private int[] m$nts = new int[] {1,2, 3, // violation 'Member name .* must match pattern'
+                                     4};
+
+
+    public static int sTest1;
+
+    protected static int sTest3;
+
+    static int sTest2;
+
+
+    int mTest1; // violation 'Member name 'mTest1' must match pattern'
+
+    public int mTest2; // violation 'Member name 'mTest2' must match pattern'
+
+    public int $mTest2; // violation 'Member name .* must match pattern'
+
+    public int mTes$t2; // violation 'Member name .* must match pattern'
+
+    public int mTest2$; // violation 'Member name .* must match pattern'
+
+    /** test local variables. This Check doesn't verify local variables, only members.*/
+    private void localVariables()
+    {
+        int a;
+        int aA;
+        int a1_a;
+        int A_A;
+        int aa2_a;
+        int _a;
+        int _aa;
+        int aa_;
+        int aaa$aaa;
+        int $aaaaaa;
+        int aaaaaa$;
+
+        int aa;
+        int aaAa1a;
+        int aaAaaAa2a1;
+    }
+
+    interface Foo {
+        public void greet();
+
+    }
+
+    class InnerClass
+    {
+        public static final int badConstant = 2;
+
+        public static final int MAX_ROWS = 2;
+
+
+        private int bad$Static = 2; // violation 'Member name .* must match pattern'
+
+        private  int sum_Created = 0;
+        // violation above 'Member name 'sum_Created' must match pattern'
+
+
+        private int bad_Member = 2; // violation 'Member name 'bad_Member' must match pattern'
+
+        private int m = 0; // violation 'Member name 'm' must match pattern'
+
+        protected int m_M = 0; // violation 'Member name 'm_M' must match pattern'
+
+
+        private int[] m$nts = new int[] {1,2, 3,
+                                         4};
+        // violation 2 lines above 'Member name .* must match pattern'
+
+
+        int mTest1; // violation 'Member name 'mTest1' must match pattern'
+
+        public int mTest2; // violation 'Member name 'mTest2' must match pattern'
+
+        public int $mTest2; // violation 'Member name .* must match pattern'
+
+        public int mTes$t2; // violation 'Member name .* must match pattern'
+
+        public int mTest2$; // violation 'Member name .* must match pattern'
+
+        void fooMethod()
+        {
+            Foo foo = new Foo() {
+
+                    int bad$Static = 2; // violation 'Member name .* must match pattern'
+
+                    int sum_Created = 0;
+                    // violation above 'Member name 'sum_Created' must match pattern'
+
+
+                    int bad_Member = 2;
+                    // violation above 'Member name 'bad_Member' must match pattern'
+
+                    int m = 0; // violation 'Member name 'm' must match pattern'
+
+                    int m_M = 0; // violation 'Member name 'm_M' must match pattern'
+
+
+                    int[] m$nts = new int[] {1,2, 3,
+                                                     4};
+                    // violation 2 lines above 'Member name .* must match pattern'
+
+
+                    int mTest1; // violation 'Member name 'mTest1' must match pattern'
+
+                    int mTest2; // violation 'Member name 'mTest2' must match pattern'
+
+                    int $mTest2; // violation 'Member name .* must match pattern'
+
+                    int mTes$t2; // violation 'Member name .* must match pattern'
+
+                    int mTest2$; // violation 'Member name .* must match pattern'
+
+                    public void greet() {}
+            };
+        }
+    }
+}
--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
@@ -239,7 +239,6 @@ public class XdocsPagesTest {
             "4.8.4.1 Indentation",
             "4.8.5 Annotations",
             "4.8.6.1 Block comment style",
-            "5.2.5 Non-constant field names",
             "5.2.6 Parameter names",
             "5.2.7 Local variable names",
             "5.2.8 Type variable names",
