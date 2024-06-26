06ca527d3cbdc2f6667209492d2131bc64b1f2d8
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-06-26 22:47:16 +0530

    Issue #14937: Migrated section 7.2 The summary fragment to chapter wise testing

---
+++ src/it/java/com/google/checkstyle/test/chapter7javadoc/rule72thesummaryfragment/TheSummaryFragmentTest.java
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2024 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.google.checkstyle.test.chapter7javadoc.rule72thesummaryfragment;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.checkstyle.test.base.AbstractGoogleModuleTestSupport;
+
+public class TheSummaryFragmentTest extends AbstractGoogleModuleTestSupport {
+
+    private static final String[] MODULES = {"SummaryJavadoc"};
+
+    @Override
+    protected String getPackageLocation() {
+        return "com/google/checkstyle/test/chapter7javadoc/rule72thesummaryfragment";
+    }
+
+    @Test
+    public void testCorrect() throws Exception {
+        final String filePath = getPath("InputCorrectSummaryFragment.java");
+        verifyWithConfigParser(MODULES, filePath);
+    }
+
+    @Test
+    public void testIncorrect() throws Exception {
+        final String filePath = getPath("InputIncorrectSummaryFragment.java");
+        verifyWithConfigParser(MODULES, filePath);
+    }
+
+}
