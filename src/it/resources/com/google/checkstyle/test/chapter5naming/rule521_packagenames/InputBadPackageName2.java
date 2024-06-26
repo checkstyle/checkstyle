5df06dfb56ae5da2f4fe71d6fcb553c7ca541bfe
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-06-26 15:10:32 +0530

    Issue #14937: Migrated section 5.2.1 Package names to chapter wise testing

--- src/it/resources/com/google/checkstyle/test/chapter5naming/rule521_packagenames/InputBadPackageName2.java
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule521_packagenames/InputBadPackageName2.java
@@ -1,2 +1,3 @@
-package com.google.checkstyle.test.chapter5naming.rule521_packagenames; //warn
+package com.google.checkstyle.test.chapter5naming.rule521_packagenames;
+// violation above 'Package name .* must match pattern .*'
 final class InputBadPackageName2 {}
--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
@@ -242,7 +242,6 @@ public class XdocsPagesTest {
             "4.8.4.3 Presence of the default label",
             "4.8.5 Annotations",
             "4.8.6.1 Block comment style",
-            "5.2.1 Package names",
             "5.2.5 Non-constant field names",
             "5.2.6 Parameter names",
             "5.2.7 Local variable names",
