fa9fd7cf7ebf95d67c51146848c0dbdf4d52700b
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-06-26 22:25:16 +0530

    Issue #14937: Migrated section 5.2.5 Non Constant Field Names to chapter wise testing

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
@@ -248,7 +248,6 @@ public class XdocsPagesTest {
             "4.8.8 Numeric Literals",
             "5.2.1 Package names",
             "5.2.3 Method names",
-            "5.2.5 Non-constant field names",
             "5.2.6 Parameter names",
             "5.2.7 Local variable names",
             "5.2.8 Type variable names",
