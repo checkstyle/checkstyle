148e78d25899444ba5531ff03f1db5078af7a899
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-06-27 12:21:45 +0530

    minor: Restored formatting for InputRightCurly.java

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule412nonemptyblocks/InputRightCurly.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule412nonemptyblocks/InputRightCurly.java
@@ -4,8 +4,7 @@ public class InputRightCurly {
     public static void main(String[] args) {
         boolean after = false;
         try {
-        } finally {
-            after = true;
-        }
+        } finally { after = true; }
+        // violation above ''{' at column 19 should have line break after.'
     }
 }
