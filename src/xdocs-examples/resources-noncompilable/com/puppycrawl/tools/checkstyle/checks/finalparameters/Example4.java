58f65234355bc969d564b2d3766664f30c33336d
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-06-28 22:06:17 +0300

    Issue #14963: Add new property to skip unnamed parameters in FinalParameterCheck

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/finalparameters/Example4.java
@@ -0,0 +1,46 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="FinalParameters">
+      <property name="tokens" value="FOR_EACH_CLAUSE, LITERAL_CATCH"/>
+      <property name="ignoreUnnamedParameters" value="true"/>
+    </module>
+  </module>
+</module>
+*/
+
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.finalparameters;
+
+// xdoc section -- start
+public class Example4 {
+
+  void testCatchParameters() {
+    try {
+      int x = 1 / 0;
+    }
+    catch (Exception e) { // violation, 'Parameter e should be final'
+      System.out.println(e);
+    }
+    try {
+      int x = 1 / 0;
+    }
+    catch (Exception _) { // ok, unnamed catch parameter, it is implicitly final
+      System.out.println("infinity");
+    }
+  }
+
+  void testForEachParameters() {
+    int[] l = {1, 2, 3};
+    int x = 0;
+    for (int number: l) { // violation, 'Parameter number should be final'
+      System.out.println(number);
+    }
+    // ok, unnamed enhanced for loop variable, it is implicitly final
+    for (int _: l) {
+      x++;
+    }
+  }
+
+}
+// xdoc section -- end
