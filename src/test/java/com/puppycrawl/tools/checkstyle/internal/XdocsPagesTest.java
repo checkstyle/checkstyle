f153c89c4d1b219e11a5c7052138c2113986b7fd
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-01 15:51:30 +0530

    Issue #14937: Migrated section 5.2.8 type variable names to chapter wise testing

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
@@ -240,7 +240,6 @@ public class XdocsPagesTest {
             "4.8.6.1 Block comment style",
             "5.2.6 Parameter names",
             "5.2.7 Local variable names",
-            "5.2.8 Type variable names",
             "7.1.1 General form",
             "7.1.2 Paragraphs",
             "7.1.3 Block tags",
