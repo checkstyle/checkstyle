4026c546f32c1ca0956fd47cd395e96d7ed3fa92
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-02 01:48:42 +0530

    Issue #14937: Migrated section 7.1.2 Paragraphs to chapter wise testing

--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputIncorrectRequireEmptyLineBeforeBlockTagGroupCheck.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputIncorrectRequireEmptyLineBeforeBlockTagGroupCheck.java
@@ -2,13 +2,14 @@ package com.google.checkstyle.test.chapter7javadoc.rule712paragraphs;
 
 /**
  * Some Javadoc.
- * @since 8.36 //warn
+ * @since 8.36 // violation 'Javadoc tag '@since' should be preceded with an empty line.'
  */
 class InputIncorrectRequireEmptyLineBeforeBlockTagGroupCheck {
 
     /**
      * This documents the private method.
-     * @param thisParamTagNeedsNewline this documents the parameter. //warn
+     * @param thisParamTagNeedsNewline this documents the parameter.
+     * // violation above 'Javadoc tag '@param' should be preceded with an empty line.'
      */
     private boolean paramTagNeedsNewline(boolean thisParamTagNeedsNewline) {
         return false;
@@ -16,7 +17,8 @@ class InputIncorrectRequireEmptyLineBeforeBlockTagGroupCheck {
 
     /**
      * This documents the private method.
-     * @param thisParamTagNeedsNewline this documents the parameter. //warn
+     * @param thisParamTagNeedsNewline this documents the parameter.
+     * // violation above 'Javadoc tag '@param' should be preceded with an empty line.'
      * @return this one does not need an empty line, but the tag before this one does.
      */
     private boolean paramMultiTagNeedsNewline(boolean thisParamTagNeedsNewline) {
