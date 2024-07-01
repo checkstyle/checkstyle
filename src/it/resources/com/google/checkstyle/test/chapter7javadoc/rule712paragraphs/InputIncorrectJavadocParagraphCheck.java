b0464ec57c620df634808ed56b071faf17564c35
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-02 01:48:42 +0530

    Issue #14937: Migrated section 7.1.2 Paragraphs to chapter wise testing

--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputIncorrectJavadocParagraphCheck.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputIncorrectJavadocParagraphCheck.java
@@ -1,24 +1,36 @@
 package com.google.checkstyle.test.chapter7javadoc.rule712paragraphs;
 
+// 2 violations 5 lines below:
+//                           '\<p\> tag should be placed immediately before the first word'
+//                           '\<p\> tag should be preceded with an empty line.'
 /**
  * Some Javadoc.
- * <p> //warn
- * /^ WARN/   Some Javadoc.<p> //warn
+ * <p>
+ * /^ WARN/   Some Javadoc.<p>
  *
  */
+// 2 violations 3 lines above:
+//                            '\<p\> tag should be placed immediately before the first word'
+//                            '\<p\> tag should be preceded with an empty line.'
+
 class InputIncorrectJavadocParagraphCheck {
 
+    // 2 violations 4 lines below:
+    //                            '\<p\> tag should be placed immediately before the first word'
+    //                            '\<p\> tag should be preceded with an empty line.'
     /**
-     * Some Javadoc.<P>  //warn
+     * Some Javadoc.<P>
      *
-     * <p>  Some Javadoc. //warn
+     * <p>  Some Javadoc.
      *
      * @since 8.0
      */
+    // violation 4 lines above '\<p\> tag should be placed immediately before the first word'
     public static final byte NUL = 0;
 
+    // violation 2 lines below '\<p\> tag should be preceded with an empty line.'
     /**
-     * Some <p>Javadoc.//warn
+     * Some <p>Javadoc.
      *
      * <p>Some Javadoc.
      *
@@ -27,36 +39,62 @@ class InputIncorrectJavadocParagraphCheck {
      */
     boolean emulated() {return false;}
 
-    /**<p>Some Javadoc. //warn
-     * <p> //warn
-     * <p><p> //warn
-     * <p>/^WARN/   Some Javadoc.<p>*/ //warn
+    // violation 4 lines below 'Redundant \<p\> tag.'
+    // 2 violations 4 lines below:
+    //                            '\<p\> tag should be placed immediately before the first word'
+    //                            '\<p\> tag should be preceded with an empty line.'
+    /**<p>Some Javadoc.
+     * <p>
+     * <p><p>
+     * <p>/^WARN/   Some Javadoc.<p>
+     */
+    // 2 violations 3 lines above:
+    //                            '\<p\> tag should be placed immediately before the first word'
+    //                            '\<p\> tag should be preceded with an empty line.'
+    // 2 violations 5 lines above:
+    //                            '\<p\> tag should be placed immediately before the first word'
+    //                            '\<p\> tag should be preceded with an empty line.'
      class InnerInputCorrectJavaDocParagraphCheck {
 
+        // 2 violations 4 lines below:
+        //                            '\<p\> tag should be placed immediately before the first word'
+        //                            '\<p\> tag should be preceded with an empty line.'
         /**
-         * Some Javadoc.<p> //warn
+         * Some Javadoc.<p>
          *
          * @since 8.0
          */
         public static final byte NUL = 0;
 
-        /**<p> //warn
+        // 2 violations 4 lines below:
+        //                            '\<p\> tag should be placed immediately before the first word'
+        //                            'Redundant \<p\> tag.'
+        // violation 4 lines below '\<p\> tag should be placed immediately before the first word'
+        /**<p>
          * /^WARN/ Some Javadoc.
          *
-         * <P> //warn
+         * <P>
          * /^WARN/
-         * <p> //warn
-         *  /^WARN/ Some Javadoc.<p> //warn
+         * <p>
+         *  /^WARN/ Some Javadoc.<p>
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
          *     Documentation about GWT emulated source</a>
          */
+        // 2 violations 5 lines above:
+        //                            '\<p\> tag should be placed immediately before the first word'
+        //                            '\<p\> tag should be preceded with an empty line.'
+        // 2 violations 7 lines above:
+        //                            '\<p\> tag should be placed immediately before the first word'
+        //                            '\<p\> tag should be preceded with an empty line.'
+        // violation 9 lines above 'Javadoc tag '@see' should be preceded with an empty line.'
         boolean emulated() {return false;}
     }
 
     InnerInputCorrectJavaDocParagraphCheck anon = new InnerInputCorrectJavaDocParagraphCheck() {
 
+        // violation 2 lines below 'Redundant \<p\> tag.'
         /**
-         * <p>Some Javadoc. //warn
+         * <p>Some Javadoc.
          *
          * <p>Some Javadoc.
          *
@@ -64,14 +102,21 @@ class InputIncorrectJavadocParagraphCheck {
          */
         public static final byte NUL = 0;
 
+        // 2 violations 5 lines below:
+        //                            '\<p\> tag should be placed immediately before the first word'
+        //                            '\<p\> tag should be preceded with an empty line.'
+        // violation 4 lines below '\<p\> tag should be placed immediately before the first word'
         /**
-         * /WARN/  Some Javadoc.<p> //warn
+         * /WARN/  Some Javadoc.<p>
          *
-         *  <p>  Some Javadoc. //warn
+         *  <p>  Some Javadoc.
          *
          * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
-         *     Documentation about <p> GWT emulated source</a> //warn
+         *     Documentation about <p> GWT emulated source</a>
          */
+        // 2 violations 2 lines above:
+        //                            '\<p\> tag should be placed immediately before the first word'
+        //                            '\<p\> tag should be preceded with an empty line.'
         boolean emulated() {return false;}
     };
 }
