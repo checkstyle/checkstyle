1e6f7b2979faa3bb132546ffb1270a6522e65d16
SteLeo1602 <stepanleontev16@gmail.com>
2024-07-01 20:21:09 -0700

    Issue #11446: Updated testUnmappableCharacters in CheckerTest

--- src/test/java/com/puppycrawl/tools/checkstyle/CheckerTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/CheckerTest.java
@@ -1656,20 +1656,15 @@ public class CheckerTest extends AbstractModuleTestSupport {
         checker.destroy();
     }
 
-    @SuppressForbidden
     @Test
     public void testUnmappableCharacters() throws Exception {
         final String[] expected = {
-            "4: " + getCheckMessage(LineLengthCheck.class, MSG_KEY, 75, 238),
+            "3: " + getCheckMessage(LineLengthCheck.class, MSG_KEY, 78, 100),
+            "14: " + getCheckMessage(LineLengthCheck.class, MSG_KEY, 75, 100),
         };
 
-        final DefaultConfiguration checkConfig = createModuleConfig(LineLengthCheck.class);
-        checkConfig.addProperty("max", "75");
-
-        final DefaultConfiguration checkerConfig = createRootConfig(checkConfig);
-        checkerConfig.addProperty("charset", "IBM1098");
-
-        verify(checkerConfig, getPath("InputCheckerTestCharset.java"), expected);
+        verifyWithInlineXmlConfig(getPath("InputCheckerTestCharset.java"),
+                expected);
     }
 
     @Test
