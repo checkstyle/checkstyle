7338eda2ac9b906fdd6bd55c6d30c1279af8c310
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-02 21:52:32 +0530

    Issue #14937: Migrated section 5.2.6 parameter names to chapter wise testing

--- src/it/resources/com/google/checkstyle/test/chapter5naming/rule526parameternames/InputLambdaParameterName.java
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule526parameternames/InputLambdaParameterName.java
@@ -6,15 +6,19 @@ import java.util.function.Function;
 public class InputLambdaParameterName {
 
     Function<String, String> badNamedParameterWithoutParenthesis =
-            S -> S.trim().toLowerCase(); // warn
+            S -> S.trim().toLowerCase(); // violation 'Lambda parameter name 'S' must match pattern'
 
     Function<String, String> badNamedParameterWithParenthesis =
-            (sT) -> sT.trim().toLowerCase(); // warn
+            (sT) -> sT.trim().toLowerCase();
+    // violation above 'Lambda parameter name 'sT' must match pattern'
 
-    BiFunction<String, String, String> twoBadNamedParameters = (sT1, sT2) -> sT1 + sT2; // warn
+    BiFunction<String, String, String> twoBadNamedParameters = (sT1, sT2) -> sT1 + sT2;
+    // 2 violations above:
+    //                    'Lambda parameter name 'sT1' must match pattern'
+    //                    'Lambda parameter name 'sT2' must match pattern'
 
     BiFunction<String, String, String> badNamedParameterInBiFunction =
-            (first, _s) -> first + _s; // warn
+            (first, _s) -> first + _s; // violation 'Lambda parameter name '_s' must match pattern'
 
     Function<String, Integer> goodNamedParameterWithoutParenthesis =
             notTrimmedString -> notTrimmedString.trim().length();
