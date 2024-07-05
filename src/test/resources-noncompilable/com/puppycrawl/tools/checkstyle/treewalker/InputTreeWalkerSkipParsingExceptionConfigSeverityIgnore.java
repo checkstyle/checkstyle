29e4bbce1a5f292da9bb077e2895e5bc5e5c1793
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-07-06 02:34:30 +0300

    Issue #12542: new TreeWlaker property to skip java parsing exceptions

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalkerSkipParsingExceptionConfigSeverityIgnore.java
@@ -0,0 +1,12 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <property name="skipFileOnJavaParseException" value="true"/>
+    <property name="javaParseExceptionSeverity" value="ignore"/>
+    <module name="ConstantName"/>
+  </module>
+</module>
+*/
+
+//non-compiled syntax: bad file for testing
+public clazz InputTreeWalkerSkipParsingExceptionConfig {}
