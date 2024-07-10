68d2cf58fdc6630502d9dad64cf148eb029f0c9a
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-10 15:34:41 +0530

    Issue #15214: Added google-java-format as a dependency and migrated section 2.1 File Name to whole config testing

--- src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule21filename/InputFileName1.java
+++ src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule21filename/InputFileName1.java
@@ -1,37 +1,38 @@
 package com.google.checkstyle.test.chapter2filebasic.rule21filename;
 
-@interface MyAnnotation1 { //ok
-    String name();
-    int version();
+// violation below 'Top-level class MyAnnotation1 has to reside in its own source file.'
+@interface MyAnnotation1 { // ok
+  String name();
+
+  int version();
 }
 
+/** Some javadoc. */
 @MyAnnotation1(name = "ABC", version = 1)
-public class InputFileName1 //ok
-{
+public class InputFileName1 {} // ok
 
+// violation below 'Top-level class Enum1 has to reside in its own source file.'
+enum Enum1 {
+  A,
+  B,
+  C;
+
+  Enum1() {}
+
+  public String toString() {
+    return ""; // some custom implementation
+  }
 }
 
-enum Enum1 //ok
-{
-    A, B, C;
-    Enum1() {}
-    public String toString() {
-        return ""; //some custom implementation
-    }
+// violation below 'Top-level class TestRequireThisEnum has to reside in its own source file.'
+interface TestRequireThisEnum { // ok
+  enum DayOfWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+  }
 }
-
-interface TestRequireThisEnum //ok
-{
-    enum DAY_OF_WEEK
-    {
-        SUNDAY,
-        MONDAY,
-        TUESDAY,
-        WEDNESDAY,
-        THURSDAY,
-        FRIDAY,
-        SATURDAY
-    }
-}
-
-
--- src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule21filename/InputFileName3.java
+++ src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule21filename/InputFileName3.java
@@ -1,37 +1,38 @@
 package com.google.checkstyle.test.chapter2filebasic.rule21filename;
 
-@interface MyAnnotation2 { // violation 'The name of the outer type and the file do not match.'
-    String name();
-    int version();
+// violation below 'The name of the outer type and the file do not match.'
+@interface MyAnnotation2 {
+  String name();
+
+  int version();
 }
 
+// violation below 'Top-level class InputFileName3 has to reside in its own source file.'
 @MyAnnotation2(name = "ABC", version = 1)
-class InputFileName3 //ok
-{
+class InputFileName3 {} // ok
 
+// violation below 'Top-level class Enum2 has to reside in its own source file.'
+enum Enum2 { // ok
+  A,
+  B,
+  C;
+
+  Enum2() {}
+
+  public String toString() {
+    return ""; // some custom implementation
+  }
 }
 
-enum Enum2 //ok
-{
-    A, B, C;
-    Enum2() {}
-    public String toString() {
-        return ""; //some custom implementation
-    }
+// violation below 'Top-level class TestRequireThisEnum2 has to reside in its own source file.'
+interface TestRequireThisEnum2 { // ok
+  enum DayOfWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+  }
 }
-
-interface TestRequireThisEnum2 //ok
-{
-    enum DAY_OF_WEEK
-    {
-        SUNDAY,
-        MONDAY,
-        TUESDAY,
-        WEDNESDAY,
-        THURSDAY,
-        FRIDAY,
-        SATURDAY
-    }
-}
-
-
