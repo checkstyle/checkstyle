ac90c9139ca0b13aa10b463d7fd8ff9d626f0ecc
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-12 22:04:00 +0530

    Issue #15218: Formatted the input file for section 4.8.2.1 One variable per line to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4821onevariableperline/InputOneVariablePerDeclaration.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4821onevariableperline/InputOneVariablePerDeclaration.java
@@ -1,100 +1,101 @@
 package com.google.checkstyle.test.chapter4formatting.rule4821onevariableperline;
 
-public class InputOneVariablePerDeclaration
-{
-    int i, j; // violation 'Each variable declaration must be in its own statement.'
+/** Some javadoc. */
+public class InputOneVariablePerDeclaration {
+  int mnp, efg; // violation 'Each variable declaration must be in its own statement.'
+  int i1; int j1; // violation 'Only one variable definition per line allowed.'
+
+  void method1() {
+    String str, str1; // violation 'Each variable declaration must be in its own statement.'
+    java.lang.Object obj; Object obj1;
+    // violation above 'Only one variable definition per line allowed.'
+  }
+
+  // second definition is wrapped
+  // line of VARIABLE_DEF is not the same as first line of the definition
+  // violation below 'Only one variable definition per line allowed.'
+  java.lang.String string; java.lang.String[]
+      strings;
+  // both definitions are wrapped
+  // violation below 'Only one variable definition per line allowed.'
+  java.lang
+              .String string1; java.lang.String[]
+                  strings1;
+
+  void method2() {
+    for (int i = 0, j = 0; i < 10; i++, j--) { // ok
+    }
+
+    for (int i = 0; i < 4; i++) {}
+  }
+
+  class Inner {
+    int xyz, qwe; // violation 'Each variable declaration must be in its own statement.'
     int i1; int j1; // violation 'Only one variable definition per line allowed.'
 
     void method1() {
-        String str, str1; // violation 'Each variable declaration must be in its own statement.'
-        java.lang.Object obj; Object obj1;
-        // violation above 'Only one variable definition per line allowed.'
+      String str, str1;
+      // violation above 'Each variable declaration must be in its own statement.'
+      java.lang.Object obj; Object obj1;
+      // violation above 'Only one variable definition per line allowed.'
     }
+
     // second definition is wrapped
     // line of VARIABLE_DEF is not the same as first line of the definition
-    java.lang.String string; java.lang.String
-    // violation above 'Only one variable definition per line allowed.'
-        strings[];
-    //both definitions are wrapped
-    java.lang. // violation 'Only one variable definition per line allowed.'
-        String string1; java.lang.String
-            strings1[];
+    // violation below 'Only one variable definition per line allowed.'
+    java.lang.String string; java.lang.String[]
+        strings;
+    // both definitions are wrapped
+    // violation below 'Only one variable definition per line allowed.'
+    java.lang
+                .String string1; java.lang.String[]
+                    strings1;
 
     void method2() {
-        for (int i=0, j=0; i < 10; i++, j--) { //ok
-        }
+      for (int i = 0, j = 0; i < 10; i++, j--) { // ok
+      }
 
-        for(int i=0; i<4;i++) {
-
-        }
+      for (int i = 0; i < 4; i++) {}
     }
 
-    class Inner {
-        int i, j; // violation 'Each variable declaration must be in its own statement.'
-        int i1; int j1; // violation 'Only one variable definition per line allowed.'
+    Inner anon =
+            new Inner() {
+              int abc, pqr; // violation 'Each variable declaration must be in its own statement.'
+              int i1; int j1; // violation 'Only one variable definition per line allowed.'
 
-        void method1() {
-            String str, str1;
-            // violation above 'Each variable declaration must be in its own statement.'
-            java.lang.Object obj; Object obj1;
-            // violation above 'Only one variable definition per line allowed.'
-        }
-        // second definition is wrapped
-        // line of VARIABLE_DEF is not the same as first line of the definition
-        java.lang.String string; java.lang.String
-        // violation above 'Only one variable definition per line allowed.'
-            strings[];
-        //both definitions are wrapped
-        java.lang. // violation 'Only one variable definition per line allowed.'
-            String string1; java.lang.String
-                strings1[];
-
-        void method2() {
-            for (int i=0, j=0; i < 10; i++, j--) { //ok
-            }
-
-            for(int i=0; i<4;i++) {
-
-            }
-        }
-        Inner anon = new Inner()
-        {
-            int i, j; // violation 'Each variable declaration must be in its own statement.'
-            int i1; int j1; // violation 'Only one variable definition per line allowed.'
-
-            void method1() {
+              void method1() {
                 String str, str1;
                 // violation above 'Each variable declaration must be in its own statement.'
                 java.lang.Object obj; Object obj1;
                 // violation above 'Only one variable definition per line allowed.'
-            }
-            // second definition is wrapped
-            // line of VARIABLE_DEF is not the same as first line of the definition
-            java.lang.String string; java.lang.String
-            // violation above 'Only one variable definition per line allowed.'
-                strings[];
-            //both definitions are wrapped
-            java.lang. // violation 'Only one variable definition per line allowed.'
-                String string1; java.lang.String
-                    strings1[];
+              }
 
-            void method2() {
-                for (int i=0, j=0; i < 10; i++, j--) { //ok
+              // second definition is wrapped
+              // line of VARIABLE_DEF is not the same as first line of the definition
+              // violation below 'Only one variable definition per line allowed.'
+              java.lang.String string; java.lang.String[]
+                  strings;
+              // both definitions are wrapped
+              // violation below 'Only one variable definition per line allowed.'
+              java.lang
+                  .String string1; java.lang.String[]
+                          strings1;
+
+              void method2() {
+                for (int i = 0, j = 0; i < 10; i++, j--) { // ok
                 }
 
-                for(int i=0; i<4;i++) {
+                for (int i = 0; i < 4; i++) {}
+              }
+            };
+  }
 
-                }
-            }
-        };
-    }
-}
-
-class Suppress {
+  class Suppress {
     @SuppressWarnings("unused")
     // violation above 'Each variable declaration must be in its own statement.'
     long q1, q2, q3;
 
     @SuppressWarnings("unused") long q4, q5, q6;
     // violation above 'Each variable declaration must be in its own statement.'
+  }
 }
