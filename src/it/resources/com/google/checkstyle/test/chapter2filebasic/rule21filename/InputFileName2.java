9b4a8403f509d62ed5de8ff8e2f44fd3c08bfdc7
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-13 01:28:53 +0530

    Issue #15218: Formatted input files for section 2.1 File Name to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule21filename/InputFileName2.java
+++ src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule21filename/InputFileName2.java
@@ -1,27 +1,22 @@
 package com.google.checkstyle.test.chapter2filebasic.rule21filename;
 
-/**
- * Test for illegal tokens
- */
-public class InputFileName2 //ok
-{
-    public void defaultMethod()
-    {
-        int i = 0;
-        switch (i)
-        {
-            default:
-                i--;
-                i++;
-                break;
-        }
+/** Test for illegal tokens. */
+public class InputFileName2 { // ok
+  /** Some javadoc. */
+  public void defaultMethod() {
+    int i = 0;
+    switch (i) {
+      default:
+        i--;
+        i++;
+        break;
     }
+  }
 
-    public native void nativeMethod();
+  public native void nativeMethod();
 
-    public void methodWithLiterals()
-    {
-        final String ref = "<a href=\"";
-        final String refCase = "<A hReF=\"";
-    }
+  public void methodWithLiterals() {
+    final String ref = "<a href=\"";
+    final String refCase = "<A hReF=\"";
+  }
 }
