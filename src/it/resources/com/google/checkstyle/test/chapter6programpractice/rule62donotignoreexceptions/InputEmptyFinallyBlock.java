60c957ad93b36f83f3d64c968c4dbcb07ed49ae0
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-13 16:37:33 +0530

    Issue #15218: Formatted input file of section 6.2 Caught Exceptions: not ignored to follow goolge style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter6programpractice/rule62donotignoreexceptions/InputEmptyFinallyBlock.java
+++ src/it/resources/com/google/checkstyle/test/chapter6programpractice/rule62donotignoreexceptions/InputEmptyFinallyBlock.java
@@ -2,90 +2,108 @@
 // Test case file for checkstyle.
 // Created: 2001
 ///////////////////////////////////////////////////////////////////////////////////////////////
+
 package com.google.checkstyle.test.chapter6programpractice.rule62donotignoreexceptions;
 
-import java.io.*;
-import java.awt.Dimension;
-import java.awt.Color;
-
 class InputEmptyFinallyBlock {
+  boolean flag;
+
+  void doSm() {}
+
+  void foo() {
+    try {
+      if (!flag) {
+        doSm();
+      }
+    } catch (Exception e) {
+      /* ignore */
+    } finally { // ok
+      /* ignore */
+    } // ok
+  }
+
+  void foo2() {
+    try {
+      if (!flag) {
+        doSm();
+      }
+    } catch (Exception e) {
+      // do nothing
+    } finally { } // violation 'Empty finally block.'
+  }
+
+  class Inner {
     boolean flag;
+
     void doSm() {}
+
     void foo() {
-        try {
-           if (!flag) {
-               doSm();
-           }
-        } catch (Exception e) { /* ignore */ } //ok
-        finally {/* ignore */} //ok
+      try {
+        if (!flag) {
+          doSm();
+        }
+      } catch (Exception e) {
+        /* ignore */
+      } finally { // ok
+        /* ignore */
+      } // ok
     }
 
     void foo2() {
-        try {
-           if (!flag) {
-               doSm();
-           }
-        } catch (Exception e) {}
-        finally {} // violation 'Empty finally block.'
-    }
-
-    class Inner {
-        boolean flag;
-        void doSm() {}
-        void foo() {
-            try {
-               if (!flag) {
-                   doSm();
-               }
-            } catch (Exception e) { /* ignore */ } //ok
-            finally {/* ignore */} //ok
+      try {
+        if (!flag) {
+          doSm();
         }
-
-        void foo2() {
-            try {
-               if (!flag) {
-                   doSm();
-               }
-            } catch (Exception e) {}
-            finally {} // violation 'Empty finally block.'
-        }
+      } catch (Exception e) {
+        // do nothing
+      } finally { } // violation 'Empty finally block.'
     }
+  }
 
-    Inner anon = new Inner(){
-        boolean flag;
-        void doSm() {}
-        void foo() {
-            try {
-               if (!flag) {
-                   doSm();
-               }
-            } catch (Exception e) { /* ignore */ } //ok
-            finally {/* ignore */} //ok
-        }
+  Inner anon =
+          new Inner() {
+            boolean flag;
 
-        void foo2() {
-            try {
-               if (!flag) {
-                   doSm();
-               }
-            } catch (Exception e) {}
-            finally {} // violation 'Empty finally block.'
-        }
-    };
+            void doSm() {}
 
-    void bar1() {
-        try {
-           if(!flag) {
-               doSm();
-           }
-        } catch (Exception expected) {}
+            void foo() {
+              try {
+                if (!flag) {
+                  doSm();
+                }
+              } catch (Exception e) {
+                /* ignore */
+              } finally { // ok
+                /* ignore */
+              } // ok
+            }
+
+            void foo2() {
+              try {
+                if (!flag) {
+                  doSm();
+                }
+              } catch (Exception e) {
+                // do nothing
+              } finally { } // violation 'Empty finally block.'
+            }
+          };
+
+  void bar1() {
+    try {
+      if (!flag) {
+        doSm();
+      }
+    } catch (Exception expected) {
     }
+  }
 
-    void bar2() {
-        try {
-           if(!flag) {
-               doSm();
-           }
-        } catch (Exception expected) {}
+  void bar2() {
+    try {
+      if (!flag) {
+        doSm();
+      }
+    } catch (Exception expected) {
     }
+  }
 }
