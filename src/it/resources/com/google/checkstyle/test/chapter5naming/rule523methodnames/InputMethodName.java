289f340c2681ba8cf9f88273fbadca31494339fb
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-13 16:58:41 +0530

    Issue #15218: Formatted input file of section 5.2.3 Method Name to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter5naming/rule523methodnames/InputMethodName.java
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule523methodnames/InputMethodName.java
@@ -1,51 +1,79 @@
 package com.google.checkstyle.test.chapter5naming.rule523methodnames;
 
 /**
- * Test input for MethodNameCheck specifically
- * whether the method name equals the class name.
+ * Test input for MethodNameCheck specifically whether the method name equals the class name.
  *
  * @author Travis Schneeberger
  */
 public class InputMethodName {
+  void foo() {}
+
+  void Foo() {} // violation 'Method name 'Foo' must match pattern'
+
+  void fOo() {} // violation 'Method name 'fOo' must match pattern'
+
+  void f0o() {}
+
+  void f$o() {} // violation 'Method name 'f\$o' must match pattern'
+
+  void f_oo() {} // violation 'Method name 'f_oo' must match pattern'
+
+  void f() {} // violation 'Method name 'f' must match pattern'
+
+  void fO() {} // violation 'Method name 'fO' must match pattern'
+
+  class InnerFoo {
     void foo() {}
+
     void Foo() {} // violation 'Method name 'Foo' must match pattern'
+
     void fOo() {} // violation 'Method name 'fOo' must match pattern'
+
     void f0o() {}
-    void f$o() {} // violation 'Method name '.*' must match pattern'
+
+    void f$o() {} // violation 'Method name 'f\$o' must match pattern'
+
     void f_oo() {} // violation 'Method name 'f_oo' must match pattern'
+
     void f() {} // violation 'Method name 'f' must match pattern'
+
     void fO() {} // violation 'Method name 'fO' must match pattern'
+  }
 
-    class InnerFoo {
-        void foo() {}
-        void Foo() {} // violation 'Method name 'Foo' must match pattern'
-        void fOo() {} // violation 'Method name 'fOo' must match pattern'
-        void f0o() {}
-        void f$o() {} // violation 'Method name '.*' must match pattern'
-        void f_oo() {} // violation 'Method name 'f_oo' must match pattern'
-        void f() {} // violation 'Method name 'f' must match pattern'
-        void fO() {} // violation 'Method name 'fO' must match pattern'
-    }
+  InnerFoo anon =
+          new InnerFoo() {
+            void foo() {}
 
-    InnerFoo anon = new InnerFoo() {
-        void foo() {}
-        void Foo() {} // violation 'Method name 'Foo' must match pattern'
-        void fOo() {} // violation 'Method name 'fOo' must match pattern'
-        void f0o() {}
-        void f$o() {} // violation 'Method name '.*' must match pattern'
-        void f_oo() {} // violation 'Method name 'f_oo' must match pattern'
-        void f() {} // violation 'Method name 'f' must match pattern'
-        void fO() {} // violation 'Method name 'fO' must match pattern'
-    };
-}
+            void Foo() {} // violation 'Method name 'Foo' must match pattern'
 
-interface FooIn {
+            void fOo() {} // violation 'Method name 'fOo' must match pattern'
+
+            void f0o() {}
+
+            void f$o() {} // violation 'Method name 'f\$o' must match pattern'
+
+            void f_oo() {} // violation 'Method name 'f_oo' must match pattern'
+
+            void f() {} // violation 'Method name 'f' must match pattern'
+
+            void fO() {} // violation 'Method name 'fO' must match pattern'
+          };
+
+  interface FooIn {
     void foo();
+
     void Foo(); // violation 'Method name 'Foo' must match pattern'
+
     void fOo(); // violation 'Method name 'fOo' must match pattern'
+
     void f0o();
-    void f$o(); // violation 'Method name '.*' must match pattern'
+
+    void f$o(); // violation 'Method name 'f\$o' must match pattern'
+
     void f_oo(); // violation 'Method name 'f_oo' must match pattern'
+
     void f(); // violation 'Method name 'f' must match pattern'
+
     void fO(); // violation 'Method name 'fO' must match pattern'
+  }
 }
