73eea63641b268674e00b0ed07af22e1c4a95e0d
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-14 15:08:47 +0530

    Issue #15218: Formatted input files of section 5.2.8 Type variable names to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter5naming/rule528typevariablenames/InputClassTypeParameterName.java
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule528typevariablenames/InputClassTypeParameterName.java
@@ -2,46 +2,43 @@ package com.google.checkstyle.test.chapter5naming.rule528typevariablenames;
 
 import java.io.Serializable;
 
-class InputClassTypeParameterName <t> // violation 'Class type name 't' must match pattern'
+class InputClassTypeParameterName<t> // violation 'Class type name 't' must match pattern'
 {
-    public <TT> void foo() { }
+  public <TT> void foo() {}
 
-    <T> void foo(int i) {
-    }
+  <T> void foo(int i) {}
 }
 
-class Other <foo extends Serializable & Cloneable> {
-    // violation above 'Class type name 'foo' must match pattern'
+class Other<foo extends Serializable & Cloneable> {
+  // violation above 'Class type name 'foo' must match pattern'
 
-    foo getOne() {
+  foo getOne() {
     return null;
-    }
+  }
 
-    <T extends foo> T getTwo(T a) {
+  <T extends foo> T getTwo(T a) {
     return null;
-    }
+  }
 
-    <T extends Runnable> foo getShadow() {
+  <T extends Runnable> foo getShadow() {
     return null;
-    }
+  }
 
-    static class Junk <$foo> { // violation 'Class type name .* must match pattern'
-        <T extends $foo> void getMoreFoo() {
-    }
-    }
+  static class Junk<$foo> { // violation 'Class type name .* must match pattern'
+    <T extends $foo> void getMoreFoo() {}
+  }
 }
 
-class MoreOther <T extends Cloneable> {
+class MoreOther<T extends Cloneable> {
 
-    <E extends T> void getMore() {
-        new Other() {
-            <T> void getMoreFoo() {
-        }
+  <E extends T> void getMore() {
+    new Other() {
+      <T> void getMoreFoo() {}
     };
 
-        Other o = new Other() {
-            <T> void getMoreFoo() {
-            }
-        };
-    }
+    Other o =
+            new Other() {
+              <T> void getMoreFoo() {}
+            };
+  }
 }
