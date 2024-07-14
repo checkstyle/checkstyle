1e75808ca183a8bd8dbac09f53adb738cc4a6a62
Rohanraj123 <rajrohan88293@gmail.com>
2024-07-14 12:34:14 +0530

    Issue #13999: Remove the suppression for AbstractJavadocCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalkerJavadocNoViolation.java
@@ -0,0 +1,9 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.javadoc.JavadocParagraphCheck
+
+*/
+package com.puppycrawl.tools.checkstyle.treewalker;
+
+/** Valid Javadoc comment. */ // ok
+public class InputTreeWalkerJavadocNoViolation {
+}
