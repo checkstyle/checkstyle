4851cd044fcb9261b27ad0f51ee7b096446d6b36
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-15 22:46:09 +0530

    Issue #15218: formatted input file for section 4.8.4.3 presence of default label to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4843defaultlabelpresence/InputPresenceOfDefaultLabel.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4843defaultlabelpresence/InputPresenceOfDefaultLabel.java
@@ -1,52 +1,69 @@
 package com.google.checkstyle.test.chapter4formatting.rule4843defaultlabelpresence;
 
 public class InputPresenceOfDefaultLabel {
-    public void foo() {
-        int i = 1;
-        switch (i) {
-        case 1: i++; break;
-        case 2: i--; break;
-        default: return;
-        }
-        switch (i) { // violation 'switch without "default" clause.'
-        }
+  public void foo() {
+    int i = 1;
+    switch (i) {
+      case 1:
+        i++;
+        break;
+      case 2:
+        i--;
+        break;
+      default:
+        return;
     }
+    switch (i) { // violation 'switch without "default" clause.'
+    }
+  }
 }
 
 class bad_test {
-    public void foo() {
-        int i = 1;
-        switch (i) { // violation 'switch without "default" clause.'
-        case 1: i++; break;
-        case 2: i--; break;
-        }
-        switch (i) { // violation 'switch without "default" clause.'
-        }
+  public void foo() {
+    int i = 1;
+    switch (i) { // violation 'switch without "default" clause.'
+      case 1:
+        i++;
+        break;
+      case 2:
+        i--;
+        break;
     }
+    switch (i) { // violation 'switch without "default" clause.'
+    }
+  }
 
-    class inner
-    {
-        public void foo1() {
-            int i = 1;
-            switch (i) { // violation 'switch without "default" clause.'
-            case 1: i++; break;
-            case 2: i--; break;
-        }
-            Foo foo = new Foo() {
+  class inner {
+    public void foo1() {
+      int i = 1;
+      switch (i) { // violation 'switch without "default" clause.'
+        case 1:
+          i++;
+          break;
+        case 2:
+          i--;
+          break;
+      }
+      Foo foo =
+              new Foo() {
                 public void foo() {
-                    int i = 1;
-                    switch (i) { // violation 'switch without "default" clause.'
-                    case 1: i++; break;
-                    case 2: i--; break;
-                    }
-                    switch (i) { // violation 'switch without "default" clause.'
-                    }
+                  int i = 1;
+                  switch (i) { // violation 'switch without "default" clause.'
+                    case 1:
+                      i++;
+                      break;
+                    case 2:
+                      i--;
+                      break;
+                  }
+                  switch (i) { // violation 'switch without "default" clause.'
+                  }
                 }
-            };
-        }
+              };
     }
+  }
 }
 
 interface Foo {
-    public void foo();
+  public void foo();
 }
