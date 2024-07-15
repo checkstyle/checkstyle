ca7a743328ca9a8101235572050219f8bb57d0ac
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-16 00:11:32 +0530

    Issue #15218: formatted input files for section 5.2.1 package names to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter5naming/rule521packagenames/InputPackageNameGood.java
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule521packagenames/InputPackageNameGood.java
@@ -1,2 +1,3 @@
-package com.google.checkstyle.test.chapter5naming.rule521packagenames; //ok
+package com.google.checkstyle.test.chapter5naming.rule521packagenames; // ok
+
 final class InputPackageNameGood {}
