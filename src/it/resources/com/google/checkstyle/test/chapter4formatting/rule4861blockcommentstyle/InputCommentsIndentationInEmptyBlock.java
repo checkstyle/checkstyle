8ef92cdb984a0d32d98ac8495673a82047913ea3
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-17 00:49:37 +0530

    Issue #15218: formatted input files for section 4.8.6.1 block comments style to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4861blockcommentstyle/InputCommentsIndentationInEmptyBlock.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4861blockcommentstyle/InputCommentsIndentationInEmptyBlock.java
@@ -2,91 +2,80 @@ package com.google.checkstyle.test.chapter4formatting.rule4861blockcommentstyle;
 
 public class InputCommentsIndentationInEmptyBlock {
 
-    private void foo1() {
-        int a = 5, b = 3, v = 6;
-        if (a == b
-            && v == b || ( a ==1
-                               // violation 2 lines below '.* should be the same level as line 15.'
-                               // violation 3 lines below '.* should be the same level as line 15.'
-                   /// // odd indentation comment
-                               //
-                       /* // odd indentation comment
-                        * one fine day ... */
-                               && b == 1)   ) {
-            // Cannot clearly detect user intention of explanation target.
-        }
-    }
-
-    private void foo2() {
-        int a = 5, b = 3, v = 6;
-        if (a == b
-            && v == b || ( a ==1
-            && b == 1)   ) {
-
-
-             // comment
-        }
-    }
-
-    private void foo3() {
-        int a = 5, b = 3, v = 6;
-        if (a == b
-            && v == b || (a == 1
+  private void foo1() {
+    int a = 5, b = 3, v = 6;
+    if (a == b && v == b
+            || (a == 1
+            // violation 2 lines below '.* should be the same level as line 15.'
+            // violation 3 lines below '.* should be the same level as line 15.'
+        /// // odd indentation comment
+            //
+              /*   // odd indentation comment
+               * one fine day ... */
             && b == 1)) {
+      // Cannot clearly detect user intention of explanation target.
+    }
+  }
+
+  private void foo2() {
+    int a = 5, b = 3, v = 6;
+    if (a == b && v == b || (a == 1 && b == 1)) {
+
+      // comment
+    }
+  }
+
+  private void foo3() {
+    int a = 5, b = 3, v = 6;
+    if (a == b && v == b || (a == 1 && b == 1)) {
 // odd indentation comment
-        }
-        // violation 2 lines above '.* indentation should be the same level as line 37.'
     }
+    // violation 2 lines above '.* indentation should be the same level as line 32.'
+  }
 
-    // Comments here should be ok by Check
-    @SuppressWarnings("unused") // trailing
-    private static void foo4() { // trailing
-        if (true) // trailing comment
-        {
-            // some comment
-        }
-        if (true) { // trailing comment
-
-        }
-        /**
-         *
-         */
+  // Comments here should be ok by Check
+  @SuppressWarnings("unused") // trailing
+  private static void foo4() { // trailing
+    if (true) // trailing comment
+    {
+      // some comment
     }
+    if (true) { // trailing comment
+    }
+    /** */
+  }
 
-    // Comments here should be ok by Check
-    @SuppressWarnings("unused") // trailing
-    private static void foo5() { // trailing
-        if (true) // trailing comment
-        {
-            // violation below '.* indentation should be the same level as line 63.'
+  // Comments here should be ok by Check
+  @SuppressWarnings("unused") // trailing
+  private static void foo5() { // trailing
+      if (true) // trailing comment
+      {
+        // violation below '.* indentation should be the same level as line 55.'
 // odd indentation comment
-        }
-        if (true) { // trailing comment
+      }
+      if (true) { // trailing comment
+      }
+      /** */
+  }
 
-        }
-        /**
-         *
-         */
-    }
+  public void foo6() {
+    try {
 
-    public void foo6() {
-        try {
-
-        } catch (Exception e) {
+    } catch (Exception e) {
 // odd indentation comment
-        }
-        // violation 2 lines above '.* indentation should be the same level as line 77.'
     }
+    // violation 2 lines above '.* indentation should be the same level as line 66.'
+  }
 
-    public void foo7() {
-        try {
+  public void foo7() {
+    try {
 
-        } catch (Exception e) {
-            // OOOO: handle exception here
-        }
+    } catch (Exception e) {
+      // OOOO: handle exception here
     }
+  }
 
-    private static class MyClass extends Object {
-           // no members
-    }
+  private static class MyClass extends Object {
+    // no members
+  }
 }
--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4861blockcommentstyle/InputCommentsIndentationInSwitchBlock.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4861blockcommentstyle/InputCommentsIndentationInSwitchBlock.java
@@ -2,285 +2,297 @@ package com.google.checkstyle.test.chapter4formatting.rule4861blockcommentstyle;
 
 public class InputCommentsIndentationInSwitchBlock {
 
-    private static void fooSwitch() {
-        switch("") {
-            case "0": //some comment
-            case "1":
-                // my comment
-                foo1();
-                break;
-            case "2":
-                // my comment
-                //comment
-                foo1();
-                // comment
-                break;
-            case "3":
-            /* // odd indentation comment
-            *  */
-                // violation 2 lines above '.* indentation should be the same level as line 22.'
-                foo1();
-                /* com */
-                break;
-            case "5":
-                // violation 2 lines below '.* indentation should be the same level as line 27, 29.'
-                foo1();
-                   // odd indentation comment
-            case "6":
-                int k = 7;
-                // fall through
-            case "7":
-                // violation 2 lines below '.* indentation should be the same level as line 34, 36.'
-                if (true) {}
-                   // odd indentation comment
-            case "8":
-                break;
-            case "9":
-                foo1();
-                // fall through
-            case "10": {
-                if (true) {}
-                // fall through
-            }
-            case "11": {
-            // fall through
-            }
-            case "28": {
-                // fall through
-            }
-            case "12": {
-      // odd indentation comment
-                // violation above '.* indentation should be the same level as line 54.'
-                int i;
-            }
-            case "13": {
-                       // some comment in empty case block
-            }
-            case "14": {
-        // odd indentation comment
-            // violation above '.* indentation should be the same level as line 62.'
-            }
-            case "15": {
-                // violation 2 lines below '.* indentation should be the same level as line 65.'
-                foo1();
-                      // odd indentation comment
-            }
-            case "16": {
-                int a;
-            }
-            // fall through
-            case "17": {
-                int a;
-            }
-                // violation below '.* indentation should be the same level as line 72, 77.'
-          // odd indentation comment
-                case "18": { System.lineSeparator();
-                }   // trailing comment
-            case "19":
-                // comment
-            case "20":
-            // comment
-            case "21":
-            default:
-                // comment
-                break;
-        }
+  private static void fooSwitch() {
+    switch ("") {
+      case "0": // some comment
+      case "1":
+        // my comment
+        foo1();
+        break;
+      case "2":
+        // my comment
+        // comment
+        foo1();
+        // comment
+        break;
+      case "3":
+      /* // odd indentation comment
+         *  */
+        // violation 2 lines above '.* indentation should be the same level as line 22.'
+        foo1();
+        /* com */
+        break;
+      case "5":
+        // violation 2 lines below '.* indentation should be the same level as line 27, 29.'
+        foo1();
+           // odd indentation comment
+      case "6":
+        int k = 7;
+        // fall through
+      case "7":
+        // violation 2 lines below '.* indentation should be the same level as line 34, 36.'
+        if (true) {}
+           // odd indentation comment
+      case "8":
+        break;
+      case "9":
+        foo1();
+        // fall through
+      case "10":
+      {
+        if (true) {}
+        // fall through
+      }
+      case "11":
+      {
+        // fall through
+      }
+      case "28":
+      {
+        // fall through
+      }
+      case "12":
+      {
+  // odd indentation comment
+        // violation above '.* indentation should be the same level as line 58.'
+        int i;
+      }
+      case "13":
+      {
+        // some comment in empty case block
+      }
+      case "14":
+      {
+    // odd indentation comment
+        // violation above '.* indentation should be the same level as line 68.'
+      }
+      case "15":
+      {
+        // violation 2 lines below '.* indentation should be the same level as line 72.'
+        foo1();
+              // odd indentation comment
+      }
+      case "16":
+      {
+        int a;
+      }
+      // fall through
+      case "17":
+      {
+        int a;
+      }
+      // violation below '.* indentation should be the same level as line 80, 86.'
+  // odd indentation comment
+      case "18":
+      {
+        System.lineSeparator();
+      } // trailing comment
+      case "19":
+        // comment
+      case "20":
+        // comment
+      case "21":
+      default:
+        // comment
+        break;
+    }
+  }
+
+  private static void foo1() {
+    if (true) {
+      switch (1) {
+        case 0:
+
+        case 1:
+          // violation below '.* indentation should be the same level as line 109.'
+              // odd indentation comment
+          int b = 10;
+        default:
+          // comment
+      }
+    }
+  }
+
+  public void fooDotInCaseBlock() {
+    int i = 0;
+    String s = "";
+
+    switch (i) {
+      case -2:
+        // what
+        i++;
+        // no break here
+      case 0:
+        // what
+        s.indexOf("ignore");
+        // no break here
+      case -1:
+        // what
+        // violation 2 lines below '.* indentation should be the same .* as line 132, 134.'
+        s.indexOf("no way");
+       // odd indentation comment
+      case 1:
+      case 2:
+        i--;
+        // no break here
+      case 3:
+      {
+      }
+      // fall through
+
     }
 
-    private static void foo1() {
-        if (true) {
-            switch(1) {
-                case 0:
-
-                case 1:
-                    // violation below '.* indentation should be the same level as line 98.'
+    String breaks =
+            ""
+                    // violation below '.* indentation should be the same level as line 149.'
                         // odd indentation comment
-                    int b = 10;
-                default:
-                 // comment
-            }
+                    + "</table>"
+                    // middle
+                    + ""
+            // end
+            ;
+  }
 
-        }
+  public void foo2() {
+    int a = 1;
+    switch (a) {
+      case 1:
+      default:
+  // odd indentation comment
     }
+    // violation 2 lines above'.* indentation should be the same level as line 162.'
+  }
 
-    public void fooDotInCaseBlock() {
-        int i = 0;
-        String s = "";
+  public void foo3() {
+    int a = 1;
+    switch (a) {
+      case 1:
+      default:
 
-        switch (i) {
-            case -2:
-                // what
-                i++;
-                // no break here
-            case 0:
-                // what
-                s.indexOf("ignore");
-                // no break here
-            case -1:
-                 // what
-                 // violation 2 lines below '.* indentation should be the same .* as line 122, 124.'
-                 s.indexOf("no way");
-               // odd indentation comment
-            case 1:
-            case 2:
-                i--;
-                // no break here
-            case 3: { }
-            // fall through
-
-
-        }
-
-        String breaks = ""
-        // violation below '.* indentation should be the same level as line 137.'
-        // odd indentation comment
-            + "</table>"
-            // middle
-            + ""
-        // end
-        ;
+        // comment
     }
+  }
 
-    public void foo2() {
-        int a = 1;
-        switch (a) {
-            case 1:
-            default:
-    // odd indentation comment
-        }
-        // violation 2 lines above'.* indentation should be the same level as line 150.'
+  public void foo4() {
+    int a = 1;
+    switch (a) {
+      case 1:
+        // violation 2 lines below '.* indentation should be the same .* as line 181, 183.'
+        int b;
+          // odd indentation comment
+      default:
     }
+  }
 
-    public void foo3() {
-        int a = 1;
-        switch (a) {
-            case 1:
-            default:
-
-                // comment
-        }
+  public void foo5() {
+    int a = 1;
+    switch (a) {
+      case 1:
+        int b;
+        // comment
+      default:
     }
+  }
 
-    public void foo4() {
-        int a = 1;
-        switch (a) {
-            case 1:
-                // violation 2 lines below '.* indentation should be the same .* as line 169, 171.'
-                int b;
-                  // odd indentation comment
-            default:
-        }
+  public void foo6() {
+    int a = 1;
+    switch (a) {
+      case 1:
+        int b;
+        // comment
+      default:
     }
+  }
 
-    public void foo5() {
-        int a = 1;
-        switch (a) {
-            case 1:
-                int b;
-            // comment
-            default:
-        }
+  public void foo7() {
+    int a = 2;
+    String s = "";
+    switch (a) {
+      // comment
+      // comment
+      // comment
+      case 1:
+      case 2:
+        // comment
+        // comment
+        foo1();
+        // comment
+      case 3:
+        // comment
+        // comment
+        // comment
+        // violation 2 lines below'.* indentation should be the same .* as line 225, 227.'
+      case 4:
+  // odd indentation comment
+      case 5:
+        // violation 4 lines below 'indentation should be the same level as line 230, 235.'
+        // violation 4 lines below 'indentation should be the same level as line 230, 235.'
+        // violation 4 lines below 'indentation should be the same level as line 230, 235.'
+        s.toString().toString().toString();
+              // odd indentation comment
+           // odd indentation comment
+         // odd indentation comment
+      default:
     }
+  }
 
-    public void foo6() {
-        int a = 1;
-        switch (a) {
-            case 1:
-                int b;
-                // comment
-            default:
-        }
+  public void foo8() {
+    int a = 2;
+    String s = "";
+    switch (a) {
+      // comment
+      // comment
+      // comment
+      case 1:
+      case 2:
+        // comment
+        // comment
+        foo1();
+        // comment
+      case 3:
+        // comment
+        // comment
+        s.toString().toString().toString();
+        // comment
+        // violation 2 lines below '.* indentation should be the same level as line 258, 260.'
+      case 4:
+  // odd indentation comment
+      default:
     }
+  }
 
-    public void foo7() {
-        int a = 2;
-        String s = "";
-        switch (a) {
-            // comment
-            // comment
-            // comment
-            case 1:
-            case 2:
-                // comment
-                // comment
-                foo1();
-                // comment
-            case 3:
-                // comment
-                // comment
-                // comment
-                // violation 2 lines below'.* indentation should be the same .* as line 213, 215.'
-            case 4:
-    // odd indentation comment
-            case 5:
-                // violation 4 lines below 'indentation should be the same level as line 218, 223.'
-                // violation 4 lines below 'indentation should be the same level as line 218, 223.'
-                // violation 4 lines below 'indentation should be the same level as line 218, 223.'
-                s.toString().toString().toString();
-                      // odd indentation comment
-                    // odd indentation comment
-                 // odd indentation comment
-            default:
-        }
+  public void foo9() {
+    int a = 5;
+    switch (a) {
+      case 1:
+      case 2:
+        // comment
     }
+  }
 
-    public void foo8() {
-        int a = 2;
-        String s = "";
-        switch (a) {
-            // comment
-            // comment
-            // comment
-            case 1:
-            case 2:
-                // comment
-                // comment
-                foo1();
-                // comment
-            case 3:
-                // comment
-                // comment
-                s.toString().toString().toString();
-                // comment
-            // violation 2 lines below '.* indentation should be the same level as line 246, 248.'
-            case 4:
-      // odd indentation comment
-            default:
-        }
+  public void foo10() {
+    int a = 5;
+    switch (a) {
+      case 1:
+      default:
+        // comment
     }
+  }
 
-    public void foo9() {
-        int a = 5;
-        switch (a) {
-            case 1:
-            case 2:
-                // comment
-        }
+  public void foo11() {
+    int a = 5;
+    switch (a) {
+      case 1:
+      case 2:
+        // comment
     }
+  }
 
-    public void foo10() {
-        int a = 5;
-        switch (a) {
-            case 1:
-            default:
-                // comment
-        }
+  public void foo12() {
+    int a = 5;
+    switch (a) {
+      // comment
+      case 1:
     }
-
-    public void foo11() {
-        int a = 5;
-        switch (a) {
-            case 1:
-            case 2:
-                // comment
-        }
-    }
-
-    public void foo12() {
-        int a = 5;
-        switch (a) {
-            // comment
-            case 1:
-        }
-    }
+  }
 }
