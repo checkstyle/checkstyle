9da5170cdd0d9e82803c83eb2cc23a0c08d370ee
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-16 23:53:22 +0530

    Issue #15218: formatted input files for section 4.1.3 empty blocks may be concise to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule413emptyblocks/InputEmptyBlocksAndCatchBlocksNoViolations.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule413emptyblocks/InputEmptyBlocksAndCatchBlocksNoViolations.java
@@ -5,99 +5,92 @@ package com.google.checkstyle.test.chapter4formatting.rule413emptyblocks;
 
 import java.io.IOException;
 
-public class InputEmptyBlocksAndCatchBlocksNoViolations
-{
-    private void foo6() {
-        try {
-            throw new IOException();
-        } catch (IOException expected) { // This is expected
-            int k = 0;
-        }
+public class InputEmptyBlocksAndCatchBlocksNoViolations {
+  private void foo6() {
+    try {
+      throw new IOException();
+    } catch (IOException expected) { // This is expected
+      int k = 0;
     }
+  }
 
-    public void testTryCatch()
-    {
-        try {
-            int y=0;
-            int u=8;
-            int e=u-y;
-            return;
-        }
-        catch (Exception e) {
-            System.identityHashCode(e);
-            return;
-        }
-        finally
-        {
-            return;
-        }
+  public void testTryCatch() {
+    try {
+      int y = 0;
+      int u = 8;
+      int e = u - y;
+      return;
+    } catch (Exception e) {
+      System.identityHashCode(e);
+      return;
+    } finally {
+      return;
     }
+  }
 
-    public void testTryCatch3()
-    {
-        try {
-            int y=0;
-            int u=8;
-            int e=u-y;
-        }
-        catch (IllegalArgumentException e) {
-            System.identityHashCode(e); //some comment
-            return;
-        }
-        catch (IllegalStateException ex) {
-                System.identityHashCode(ex);
-                return;
-        }
+  public void testTryCatch3() {
+    try {
+      int y = 0;
+      int u = 8;
+      int e = u - y;
+    } catch (IllegalArgumentException e) {
+      System.identityHashCode(e); // some comment
+      return;
+    } catch (IllegalStateException ex) {
+      System.identityHashCode(ex);
+      return;
     }
+  }
 
-    public void testTryCatch4()
-    {
-        int y=0;
-        int u=8;
-        try {
-            int e=u-y;
-        }
-        catch (IllegalArgumentException e) {
-            System.identityHashCode(e);
-            return;
-        }
+  public void testTryCatch4() {
+    int y = 0;
+    int u = 8;
+    try {
+      int e = u - y;
+    } catch (IllegalArgumentException e) {
+      System.identityHashCode(e);
+      return;
     }
-    public void setFormats() {
-        try {
-            int k = 4;
-        } catch (Exception e) {
-            Object k = null;
-            if (k != null)
-                k = "ss";
-            else {
-                return;
-            }
-        }
+  }
+
+  public void setFormats() {
+    try {
+      int k = 4;
+    } catch (Exception e) {
+      Object k = null;
+      if (k != null) k = "ss";
+      else {
+        return;
+      }
     }
-    public void testIfElse() {
-        if (true) {
-            return;
-        } else {
-            return;
-        }
+  }
+
+  public void testIfElse() {
+    if (true) {
+      return;
+    } else {
+      return;
     }
-    public void testIfElseIfLadder() {
-        if (true) {
-            return;
-        } else if (false) {
-            return;
-        } else {
-            return;
-        }
+  }
+
+  public void testIfElseIfLadder() {
+    if (true) {
+      return;
+    } else if (false) {
+      return;
+    } else {
+      return;
     }
-    public void testSwtichCase() {
-        switch (1) {
-            case 1:
-                return;
-            case 2:
-                return;
-            default:
-                return;
-        }
+  }
+
+  public void testSwtichCase() {
+    switch (1) {
+      case 1:
+        return;
+      case 2:
+        return;
+      default:
+        return;
     }
+  }
 }
--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule413emptyblocks/InputEmptyFinallyBlocks.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule413emptyblocks/InputEmptyFinallyBlocks.java
@@ -5,71 +5,97 @@
 package com.google.checkstyle.test.chapter4formatting.rule413emptyblocks;
 
 class InputEmptyFinallyBlocks {
+  boolean flag;
+
+  void doSm() {}
+
+  void foo() {
+    try {
+      if (!flag) {
+        doSm();
+      }
+    } catch (Exception e) {
+      /* ignore */
+    } // ok
+    finally {
+      /* ignore */
+    } // ok
+  }
+
+  void foo2() {
+    try {
+      if (!flag) {
+        doSm();
+      }
+    } catch (Exception e) {
+    } // violation above 'Empty catch block.'
+    finally {
+    } // violation above 'Empty finally block.'
+  }
+
+  class Inner {
     boolean flag;
+
     void doSm() {}
+
     void foo() {
-        try {
-           if (!flag) {
-               doSm();
-           }
-        } catch (Exception e) { /* ignore */ } //ok
-        finally {/* ignore */} //ok
+      try {
+        if (!flag) {
+          doSm();
+        }
+      } catch (Exception e) {
+        /* ignore */
+      } // ok
+      finally {
+        /* ignore */
+      } // ok
     }
 
     void foo2() {
-        try {
-           if (!flag) {
-               doSm();
-           }
-        } catch (Exception e) {} // violation 'Empty catch block.'
-        finally {} // violation 'Empty finally block.'
+      try {
+        if (!flag) {
+          doSm();
+        }
+      } catch (Exception e) {
+      } // violation above 'Empty catch block.'
+      finally {
+      } // violation above 'Empty finally block.'
     }
+  }
 
-    class Inner {
+  Inner anon =
+      new Inner() {
         boolean flag;
+
         void doSm() {}
+
         void foo() {
-            try {
-               if (!flag) {
-                   doSm();
-               }
-            } catch (Exception e) { /* ignore */ } //ok
-            finally {/* ignore */} //ok
+          try {
+            if (!flag) {
+              doSm();
+            }
+          } catch (Exception e) {
+            /* ignore */
+          } // ok
+          finally {
+            /* ignore */
+          } // ok
         }
 
         void foo2() {
-            try {
-               if (!flag) {
-                   doSm();
-               }
-            } catch (Exception e) {} // violation 'Empty catch block.'
-            finally {} // violation 'Empty finally block.'
+          try {
+            if (!flag) {
+              doSm();
+            }
+          } catch (Exception e) {
+          } // violation above 'Empty catch block.'
+          finally {
+          } // violation above 'Empty finally block.'
         }
-    }
-
-    Inner anon = new Inner(){
-        boolean flag;
-        void doSm() {}
-        void foo() {
-            try {
-               if (!flag) {
-                   doSm();
-               }
-            } catch (Exception e) { /* ignore */ } //ok
-            finally {/* ignore */} //ok
-        }
-
-        void foo2() {
-            try {
-               if (!flag) {
-                   doSm();
-               }
-            } catch (Exception e) {} // violation 'Empty catch block.'
-            finally {} // violation 'Empty finally block.'
-        }
-    };
+      };
 }
 
 interface InterfaceEndingWithSemiColon2 {
-    public void fooEmpty();
-}; // ok until #7541
+  public void fooEmpty();
+}
+; // ok until #7541
