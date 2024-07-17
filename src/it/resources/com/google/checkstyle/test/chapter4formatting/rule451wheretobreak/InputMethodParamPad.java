bf78fedd04d72ce9b83838cf02b12f623cb19b8b
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-17 23:34:34 +0530

    Issue #15218: formatted input files for section 4.5.1 Where to break to follow google java style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule451wheretobreak/InputMethodParamPad.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule451wheretobreak/InputMethodParamPad.java
@@ -3,224 +3,195 @@ package com.google.checkstyle.test.chapter4formatting.rule451wheretobreak;
 import java.util.HashMap;
 import java.util.Map;
 
-class InputMethodParamPad
-{
-    class Inner {
-        void testGenerics1
-        () // violation ''(' should be on the previous line.'
-        {
-            Comparable
-                <
-                String
-                >
-                c = new String();
-            Map<String, String> map = new HashMap<String, String>();
-            boolean flag = false;
+class InputMethodParamPad {
+  class Inner {
+    void testGenerics1
+      () { // violation ''(' should be on the previous line.'
+      Comparable
+          <
+          String
+          >
+          c = new String();
+      Map<String, String> map = new HashMap<String, String>();
+      boolean flag = false;
 
-            int init = 9;
-        }
+      int init = 9;
     }
+  }
 
-    Inner anon = new Inner
-            (){  // violation ''(' should be on the previous line.'
+  Inner anon = new Inner
+          () { // violation ''(' should be on the previous line.'
         void testGenerics1
-        () // violation ''(' should be on the previous line.'
-        {
-            Comparable
-                <
-                String
-                >
-                c = new String();
-            Map<String, String> map = new HashMap<String, String>();
-            boolean flag = false;
-            int init = 9;
+        () { // violation ''(' should be on the previous line.'
+          Comparable
+              <
+              String
+              >
+              c = new String();
+          Map<String, String> map = new HashMap<String, String>();
+          boolean flag = false;
+          int init = 9;
         }
-    };
+      };
 }
 
 class AsInput1 {
-    int abc = 0;
-    String string
-        = "string";
-    double PI =
-            3.1415;
+  int abc = 0;
+  String string
+      = "string";
+  double PI =
+          3.1415;
 }
 
 class Ternary2 {
-    void foo() {
-        boolean flag = true;
-        int i2 = flag == true
-                ?
-                1
-                :
-                2;
-        int i3 = flag == true
-                ? 1
-                : 2;
-
-    }
+  void foo() {
+    boolean flag = true;
+    int i2 = flag == true
+            ?
+            1
+            :
+            2;
+    int i3 = flag == true
+            ? 1
+            : 2;
+  }
 }
 
 class AssignClass3 {
+  void foo() {
+    int i = 0;
+    int j = 0;
+    i +=
+            1;
+    j
+         += 2;
+    i -=
+            1;
+    j
+         -= 2;
+    i /=
+            1;
+    j
+         /= 2;
+    i *=
+            1;
+    j
+         *= 2;
+    i %=
+            1;
+    j
+         %= 2;
+    i ^=
+            1;
+    j
+         ^= 2;
+    i |=
+            1;
+    j
+         |= 2;
+    i &=
+            1;
+    j
+         &= 2;
+    i >>=
+            1;
+    j
+        >>= 2;
+    i >>>=
+            1;
+    j
+        >>>= 2;
+    i <<=
+            1;
+    j
+        <<= 2;
+  }
+
+  class InnerClass {
     void foo() {
-        int i = 0;
-        int j = 0;
-        i +=
-                1;
-        j
-             += 2;
-        i -=
-                1;
-        j
-             -= 2;
-        i /=
-                1;
-        j
-             /= 2;
-        i *=
-                1;
-        j
-             *= 2;
-        i %=
-                1;
-        j
-             %= 2;
-        i ^=
-                1;
-        j
-             ^= 2;
-        i |=
-                1;
-        j
-             |= 2;
-        i &=
-                1;
-        j
-             &= 2;
-        i >>=
-                1;
-        j
-            >>= 2;
-        i >>>=
-                1;
-        j
-            >>>= 2;
-        i <<=
-                1;
-        j
-            <<= 2;
+      int i = 0;
+      int j = 0;
+      i += 1;
+      j += 2;
+      i -= 1;
+      j -= 2;
+      i /= 1;
+      j /= 2;
+      i *= 1;
+      j *= 2;
+      i %= 1;
+      j %= 2;
+      i ^= 1;
+      j ^= 2;
+      i |= 1;
+      j |= 2;
+      i &= 1;
+      j &= 2;
+      i >>= 1;
+      j >>= 2;
+      i >>>= 1;
+      j >>>= 2;
+      i <<= 1;
+      j <<= 2;
     }
+  }
 
-    class InnerClass {
+  InnerClass anon =
+      new InnerClass() {
         void foo() {
-            int i = 0;
-            int j = 0;
-            i +=
-                    1;
-            j
-                 += 2;
-            i -=
-                    1;
-            j
-                 -= 2;
-            i /=
-                    1;
-            j
-                 /= 2;
-            i *=
-                    1;
-            j
-                 *= 2;
-            i %=
-                    1;
-            j
-                 %= 2;
-            i ^=
-                    1;
-            j
-                 ^= 2;
-            i |=
-                    1;
-            j
-                 |= 2;
-            i &=
-                    1;
-            j
-                 &= 2;
-            i >>=
-                    1;
-            j
-                >>= 2;
-            i >>>=
-                    1;
-            j
-                >>>= 2;
-            i <<=
-                    1;
-            j
-                <<= 2;
+          int i = 0;
+          int j = 0;
+          i +=
+                  1;
+          j
+               += 2;
+          i -=
+                  1;
+          j
+               -= 2;
+          i /=
+                  1;
+          j
+               /= 2;
+          i *=
+                  1;
+          j
+               *= 2;
+          i %=
+                  1;
+          j
+               %= 2;
+          i ^=
+                  1;
+          j
+               ^= 2;
+          i |=
+                  1;
+          j
+               |= 2;
+          i &=
+                  1;
+          j
+               &= 2;
+          i >>=
+                  1;
+          j
+              >>= 2;
+          i >>>=
+                  1;
+          j
+              >>>= 2;
+          i <<=
+                  1;
+          j
+              <<= 2;
         }
-    }
+      };
 
-    InnerClass anon = new InnerClass() {
-        void foo() {
-            int i = 0;
-            int j = 0;
-            i +=
-                    1;
-            j
-                 += 2;
-            i -=
-                    1;
-            j
-                 -= 2;
-            i /=
-                    1;
-            j
-                 /= 2;
-            i *=
-                    1;
-            j
-                 *= 2;
-            i %=
-                    1;
-            j
-                 %= 2;
-            i ^=
-                    1;
-            j
-                 ^= 2;
-            i |=
-                    1;
-            j
-                 |= 2;
-            i &=
-                    1;
-            j
-                 &= 2;
-            i >>=
-                    1;
-            j
-                >>= 2;
-            i >>>=
-                    1;
-            j
-                >>>= 2;
-            i <<=
-                    1;
-            j
-                <<= 2;
-        }
-    };
+  enum TestEnum {
+    FIRST () {}, // violation ''(' is preceded with whitespace.'
 
-    enum TestEnum {
-        FIRST () // violation ''(' is preceded with whitespace.'
-            {
-            },
-
-        SECOND
-            () // violation ''(' should be on the previous line.'
-            {
-            }
-    }
+    SECOND
+        () {} // violation ''(' should be on the previous line.'
+  }
 }
