e701a6bbf562a274e89675603e7eb9f1223180a5
SteLeo1602 <stepanleontev16@gmail.com>
2024-07-17 21:20:42 -0700

    Issue #15256: Fixed location of violation comments

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/nocodeinfile/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/nocodeinfile/Example2.java
@@ -9,7 +9,7 @@
 // violation 8 lines above
 
 // xdoc section -- start
-// the violation is on first line of file
+/* the violation is on first line of file */
 /*
  public class Example2 {
  block comment is not code
