ff6308ae62fb01d54fc3a2dbbbed82f1eea17ef1
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-18 15:56:27 +0530

    Issue #15218: formatted input files for section 7.3 Where javadoc is used to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule73wherejavadocrequired/InputMissingJavadocTypeCorrect.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule73wherejavadocrequired/InputMissingJavadocTypeCorrect.java
@@ -1,103 +1,53 @@
 package com.google.checkstyle.test.chapter7javadoc.rule73wherejavadocrequired;
 
-/**
- * This is a Javadoc comment.
- */
+/** This is a Javadoc comment. */
 public class InputMissingJavadocTypeCorrect {
   private int myInt;
   int myOtherInt;
 
-  /**
-   * This is a Javadoc comment.
-   */
-  public class InnerPublic implements MyInterfacePublic {
+  /** This is a Javadoc comment. */
+  public class InnerPublic implements MyInterfacePublic {}
 
-  }
+  /** This is a Javadoc comment. */
+  public enum MyEnumPublic {}
 
-  /**
-   * This is a Javadoc comment.
-   */
-  public enum MyEnumPublic {
-
-  }
-
-  /**
-   * This is a Javadoc comment.
-   */
+  /** This is a Javadoc comment. */
   public interface MyInterfacePublic {
 
-    /**
-     * This is a Javadoc comment.
-     */
+    /** This is a Javadoc comment. */
     class MyInterfaceClass {}
   }
 
-  /**
-   * This is a Javadoc comment.
-   */
-  public @interface MyAnnotationPublic {
+  /** This is a Javadoc comment. */
+  public @interface MyAnnotationPublic {}
 
-  }
+  /** This is a Javadoc comment. */
+  protected class InnerProtected {}
 
-  /**
-   * This is a Javadoc comment.
-   */
-  protected class InnerProtected {
+  /** This is a Javadoc comment. */
+  protected enum MyEnumProtected {}
 
-  }
+  /** This is a Javadoc comment. */
+  protected interface MyInterfaceProtected {}
 
-  /**
-   * This is a Javadoc comment.
-   */
-  protected enum MyEnumProtected {
+  /** This is a Javadoc comment. */
+  protected @interface MyAnnotationProtected {}
 
-  }
+  class Inner {}
 
-  /**
-   * This is a Javadoc comment.
-   */
-  protected interface MyInterfaceProtected {
+  enum MyEnum {}
 
-  }
+  interface MyInterface {}
 
-  /**
-   * This is a Javadoc comment.
-   */
-  protected @interface MyAnnotationProtected {
+  @interface MyAnnotation {}
 
-  }
+  private class InnerPrivate {}
 
-  class Inner {
+  private enum MyEnumPrivate {}
 
-  }
+  private interface MyInterfacePrivate {}
 
-  enum MyEnum {
-
-  }
-
-  interface MyInterface {
-
-  }
-
-  @interface MyAnnotation {
-
-  }
-
-  private class InnerPrivate {
-
-  }
-
-  private enum MyEnumPrivate {
-
-  }
-
-  private interface MyInterfacePrivate {
-
-  }
-
-  private @interface MyAnnotationPrivate {
-
-  }
+  private @interface MyAnnotationPrivate {}
 
   public void myMethod() {
     class MyMethodClass {}
--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule73wherejavadocrequired/InputMissingJavadocTypeIncorrect.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule73wherejavadocrequired/InputMissingJavadocTypeIncorrect.java
@@ -2,44 +2,35 @@ package com.google.checkstyle.test.chapter7javadoc.rule73wherejavadocrequired;
 
 public class InputMissingJavadocTypeIncorrect { // violation 'Missing a Javadoc comment.'
 
-    public class Inner { // violation 'Missing a Javadoc comment.'
+  public class Inner { // violation 'Missing a Javadoc comment.'
+  }
 
-    }
+  public enum MyEnum { // violation 'Missing a Javadoc comment.'
+  }
 
-    public enum MyEnum { // violation 'Missing a Javadoc comment.'
+  public interface MyInterface { // violation 'Missing a Javadoc comment.'
+    class MyInterfaceClass {} // violation 'Missing a Javadoc comment.'
+  }
 
-    }
+  public @interface MyAnnotation { // violation 'Missing a Javadoc comment.'
+  }
 
-    public interface MyInterface {  // violation 'Missing a Javadoc comment.'
-        class MyInterfaceClass {}  // violation 'Missing a Javadoc comment.'
-    }
+  protected class InnerProtected { // violation 'Missing a Javadoc comment.'
+  }
 
-    public @interface MyAnnotation { // violation 'Missing a Javadoc comment.'
+  protected enum MyEnumProtected { // violation 'Missing a Javadoc comment.'
+  }
 
-    }
+  protected interface MyInterfaceProtected { // violation 'Missing a Javadoc comment.'
+  }
 
-    protected class InnerProtected { // violation 'Missing a Javadoc comment.'
+  protected @interface MyAnnotationProtected { // violation 'Missing a Javadoc comment.'
+  }
 
-    }
-
-    protected enum MyEnumProtected { // violation 'Missing a Javadoc comment.'
-
-    }
-
-    protected interface MyInterfaceProtected {  // violation 'Missing a Javadoc comment.'
-
-    }
-
-    protected @interface MyAnnotationProtected { // violation 'Missing a Javadoc comment.'
-
-    }
-
-    public void myMethod() {
-        class MyMethodClass {}
-    }
-
+  public void myMethod() {
+    class MyMethodClass {}
+  }
 }
 
 class AdditionalClass { // OK, not public
-
 }
