a17d29abba5020c8c059ca3c7968d88339f28680
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-18 16:12:37 +0530

    Issue #15218: formatted input files for section 7.2 the summary fragment to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule72thesummaryfragment/InputIncorrectSummaryFragment.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule72thesummaryfragment/InputIncorrectSummaryFragment.java
@@ -1,15 +1,57 @@
 package com.google.checkstyle.test.chapter7javadoc.rule72thesummaryfragment;
 
-/**
- * A {@code Foo. Foo}
- * is a simple Javadoc. Some javadoc.
- */
+/** A {@code Foo. Foo} is a simple Javadoc. Some javadoc. */
 class InputIncorrectSummaryFragment {
 
+  // violation below 'First sentence of Javadoc is missing an ending period.'
+  /** As of JDK 1.1, replaced by {@link #setBounds(int,int,int,int)} */
+  void foo3() {}
+
+  // violation below 'Summary javadoc is missing.'
+  /**
+   * @throws Exception if a problem occurs
+   */
+  void foo4() throws Exception {}
+
+  /** An especially short bit of Javadoc. */
+  void foo5() {}
+
+  /** An especially short bit of Javadoc. */
+  void foo6() {}
+
+  /** Some Javadoc. */
+  public static final byte NUL = 0;
+
+  // violation below 'Summary javadoc is missing.'
+  /** <a href="mailto:vlad@htmlbook.ru"/> */
+  class InnerInputCorrectJavaDocParagraphCheck {
+
     // violation below 'First sentence of Javadoc is missing an ending period.'
+    /** foooo@foooo */
+    public static final byte NUL = 0;
+
+    /** Some java@doc. */
+    public static final byte NUL_2 = 0;
+
+    // violation below 'Forbidden summary fragment.'
+    /** This method returns some javadoc. Some javadoc. */
+    boolean emulated() {
+      return false;
+    }
+
+    // violation below 'Summary javadoc is missing.'
+    /** <a href="mailto:vlad@htmlbook.ru"/> */
+    void foo2() {}
+
+    // violation below 'Summary javadoc is missing.'
     /**
-     * As of JDK 1.1, replaced by {@link #setBounds(int,int,int,int)}
+     * @return the customer ID some javadoc.
      */
+    int geId() {
+      return 666;
+    }
+
+    /** As of JDK 1.1, replaced by {@link #setBounds(int,int,int,int)}. */
     void foo3() {}
 
     // violation below 'Summary javadoc is missing.'
@@ -21,92 +63,22 @@ class InputIncorrectSummaryFragment {
     /** An especially short bit of Javadoc. */
     void foo5() {}
 
-    /**
-     * An especially short bit of Javadoc.
-     */
+    /** An especially short bit of Javadoc. */
     void foo6() {}
+  }
 
-    /**
-     * Some Javadoc.
-     */
-    public static final byte NUL = 0;
+  // violation below 'Forbidden summary fragment.'
+  /** A {@code InnerInputCorrectJavaDocParagraphCheck} is a simple code. */
+  InnerInputCorrectJavaDocParagraphCheck anon =
+      new InnerInputCorrectJavaDocParagraphCheck() {
 
-    // violation below 'Summary javadoc is missing.'
-    /**
-     * <a href="mailto:vlad@htmlbook.ru"/>
-     */
-     class InnerInputCorrectJavaDocParagraphCheck {
-
-        // violation below 'First sentence of Javadoc is missing an ending period.'
-        /**
-         * foooo@foooo
-         */
+        /** Some Javadoc. */
         public static final byte NUL = 0;
 
-        /**
-         * Some java@doc.
-         */
-        public static final byte NUL_2 = 0;
-
-        // violation below 'Forbidden summary fragment.'
-        /**
-         * This method
-         * returns some javadoc. Some javadoc.
-         */
-        boolean emulated() {return false;}
-
-        // violation below 'Summary javadoc is missing.'
-        /**
-         * <a href="mailto:vlad@htmlbook.ru"/>
-         */
-        void foo2() {}
-
-        // violation below 'Summary javadoc is missing.'
-        /**
-         * @return the
-         * customer ID some javadoc.
-         */
-        int geId() {return 666;}
-
-        /**
-         * As of JDK 1.1, replaced by {@link #setBounds(int,int,int,int)}.
-         */
-        void foo3() {}
-
-        // violation below 'Summary javadoc is missing.'
-        /**
-         * @throws Exception if a problem occurs
-         */
-        void foo4() throws Exception {}
-
-        /** An especially short bit of Javadoc. */
-        void foo5() {}
-
-        /**
-         * An especially short bit of Javadoc.
-         */
-        void foo6() {}
-    }
-
-    // violation below 'Forbidden summary fragment.'
-    /**
-      * A {@code InnerInputCorrectJavaDocParagraphCheck} is a simple code.
-      */
-    InnerInputCorrectJavaDocParagraphCheck anon = new InnerInputCorrectJavaDocParagraphCheck() {
-
-        /**
-         * Some Javadoc.
-         */
-        public static final byte NUL = 0;
-
-        /**
-         * Some Javadoc.
-         */
+        /** Some Javadoc. */
         void emulated(String s) {}
 
-        /**
-         * As of JDK 1.1, replaced by {@link #setBounds(int,int,int,int)}.
-         */
+        /** As of JDK 1.1, replaced by {@link #setBounds(int,int,int,int)}. */
         void foo3() {}
 
         // violation below 'Summary javadoc is missing.'
@@ -118,9 +90,7 @@ class InputIncorrectSummaryFragment {
         /** An especially short bit of Javadoc. */
         void foo5() {}
 
-        /**
-         * An especially short bit of Javadoc.
-         */
+        /** An especially short bit of Javadoc. */
         void foo6() {}
-    };
+      };
 }
