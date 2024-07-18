ed0dbdcc6d35a5de54ef756ff94653c26a446c54
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-18 16:38:59 +0530

    Issue #15218: formatted input files for section 7.1.2 paragraphs to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputCorrectJavadocParagraph.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputCorrectJavadocParagraph.java
@@ -4,14 +4,51 @@ package com.google.checkstyle.test.chapter7javadoc.rule712paragraphs;
  * Some Javadoc.
  *
  * <p>Some Javadoc.
- *
  */
 class InputCorrectJavadocParagraph {
+
+  /**
+   * Some Javadoc.
+   *
+   * <p>{@code function} will never be invoked with a null value.
+   *
+   * @since 8.0
+   */
+  public static final byte NUL = 0;
+
+  /**
+   * Some Javadoc.
+   *
+   * <p>Some Javadoc.
+   *
+   * <pre>
+   * class Foo {
+   *
+   *   void foo() {}
+   * }
+   * </pre>
+   *
+   * @see <a
+   *     href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
+   *     Documentation about GWT emulated source</a>
+   */
+  boolean emulated() {
+    return false;
+  }
+
+  /**
+   * Some Javadoc.
+   *
+   * <p>Some Javadoc.
+   */
+  class InnerInputCorrectJavaDocParagraphCheck {
 
     /**
      * Some Javadoc.
      *
-     * <p>{@code function} will never be invoked with a null value.
+     * <p>Some Javadoc.
+     *
+     * <p>Some Javadoc.
      *
      * @since 8.0
      */
@@ -22,49 +59,17 @@ class InputCorrectJavadocParagraph {
      *
      * <p>Some Javadoc.
      *
-     * <pre>
-     * class Foo {
-     *
-     *   void foo() {}
-     * }
-     * </pre>
-     *
-     * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
+     * @see <a
+     *     href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
      *     Documentation about GWT emulated source</a>
      */
-    boolean emulated() {return false;}
-
-    /**
-     * Some Javadoc.
-     *
-     * <p>Some Javadoc.
-     *
-     */
-     class InnerInputCorrectJavaDocParagraphCheck {
-
-        /**
-         * Some Javadoc.
-         *
-         * <p>Some Javadoc.
-         *
-         * <p>Some Javadoc.
-         *
-         * @since 8.0
-         */
-        public static final byte NUL = 0;
-
-        /**
-         * Some Javadoc.
-         *
-         * <p>Some Javadoc.
-         *
-         * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
-         *     Documentation about GWT emulated source</a>
-         */
-        boolean emulated() {return false;}
+    boolean emulated() {
+      return false;
     }
+  }
 
-    InnerInputCorrectJavaDocParagraphCheck anon = new InnerInputCorrectJavaDocParagraphCheck() {
+  InnerInputCorrectJavaDocParagraphCheck anon =
+      new InnerInputCorrectJavaDocParagraphCheck() {
 
         /**
          * Some Javadoc.
@@ -84,9 +89,12 @@ class InputCorrectJavadocParagraph {
          *
          * <p>Some Javadoc.
          *
-         * @see <a href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
+         * @see <a
+         *     href="http://www.gwtproject.org/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
          *     Documentation about GWT emulated source</a>
          */
-        boolean emulated() {return false;}
-    };
+        boolean emulated() {
+          return false;
+        }
+      };
 }
