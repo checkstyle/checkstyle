9ee77481f67b40edd41e84e2dccd5ce5ae7cd897
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-18 16:38:59 +0530

    Issue #15218: formatted input files for section 7.1.2 paragraphs to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputIncorrectRequireEmptyLineBeforeBlockTagGroup.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputIncorrectRequireEmptyLineBeforeBlockTagGroup.java
@@ -7,22 +7,22 @@ package com.google.checkstyle.test.chapter7javadoc.rule712paragraphs;
 // violation 2 lines above'Javadoc tag '@since' should be preceded with an empty line.'
 class InputIncorrectRequireEmptyLineBeforeBlockTagGroup {
 
-    /**
-     * This documents the private method.
-     * @param thisParamTagNeedsNewline this documents the parameter.
-     */
-    // violation 2 lines above 'Javadoc tag '@param' should be preceded with an empty line.'
-    private boolean paramTagNeedsNewline(boolean thisParamTagNeedsNewline) {
-        return false;
-    }
+  /**
+   * This documents the private method.
+   * @param thisParamTagNeedsNewline this documents the parameter.
+   */
+  // violation 2 lines above 'Javadoc tag '@param' should be preceded with an empty line.'
+  private boolean paramTagNeedsNewline(boolean thisParamTagNeedsNewline) {
+    return false;
+  }
 
-    /**
-     * This documents the private method.
-     * @param thisParamTagNeedsNewline this documents the parameter.
-     * @return this one does not need an empty line, but the tag before this one does.
-     */
-    // violation 3 lines above 'Javadoc tag '@param' should be preceded with an empty line.'
-    private boolean paramMultiTagNeedsNewline(boolean thisParamTagNeedsNewline) {
-        return false;
-    }
+  /**
+   * This documents the private method.
+   * @param thisParamTagNeedsNewline this documents the parameter.
+   * @return this one does not need an empty line, but the tag before this one does.
+   */
+  // violation 3 lines above 'Javadoc tag '@param' should be preceded with an empty line.'
+  private boolean paramMultiTagNeedsNewline(boolean thisParamTagNeedsNewline) {
+    return false;
+  }
 }
