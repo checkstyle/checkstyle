420677885fd4bbece1139b6f3dbb0fdcb4be31f8
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-19 14:56:42 +0530

    Issue #15218: formatted input file for section 2.3.1 file tab to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule231filetab/InputWhitespaceCharacters.java
+++ src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule231filetab/InputWhitespaceCharacters.java
@@ -1,201 +1,198 @@
 package com.google.checkstyle.test.chapter2filebasic.rule231filetab;
 
-final class InputWhitespaceCharacters
-{
-    // Long line ----------------------------------------------------------------
-    // Contains a tab ->	<- // violation 'Line contains a tab character.'
-    // Contains trailing whitespace ->
+final class InputWhitespaceCharacters {
+  // Long line ----------------------------------------------------------------
+  // Contains a tab ->	<- // violation 'Line contains a tab character.'
+  // Contains trailing whitespace ->
 
-    /** Invalid format **/
-    public static final int badConstant = 2;
-    /** Valid format **/
-    public static final int MAX_ROWS = 2;
+  /** Invalid format * */
+  public static final int badConstant = 2;
 
-    /** Invalid format **/
-    private static int badStatic = 2;
-    /** Valid format **/
-    private static int sNumCreated = 0;
+  /** Valid format * */
+  public static final int MAX_ROWS = 2;
 
-    /** Invalid format **/
-    private int badMember = 2;
-    /** Valid format **/
-    private int mNumCreated1 = 0;
-    /** Valid format **/
-    protected int mNumCreated2 = 0;
+  /** Invalid format * */
+  private static int badStatic = 2;
 
-    /** commas are wrong **/
-    private int[] mInts = new int[] {1,2, 3,
-                                     4};
+  /** Valid format * */
+  private static int sNumCreated = 0;
 
-    public static int sTest1;
-    /** should be private **/
-    protected static int sTest3;
-    /** should be private **/
-    static int sTest2;
+  /** Invalid format * */
+  private int badMember = 2;
 
-    /** should be private **/
-    int mTest1;
-    /** should be private **/
-    public int mTest2;
+  /** Valid format * */
+  private int mNumCreated1 = 0;
 
-    /**
-     * @return hack
-     * @param badFormat1 bad format
-     * @param badFormat2 bad format
-     * @param badFormat3 bad format
-     * @throws java.lang.Exception abc
-     **/
-    int test1(int badFormat1,int badFormat2,
-    			final int badFormat3) // violation 'Line contains a tab character.'
-        throws java.lang.Exception
-    {
-        return 0;
+  /** Valid format * */
+  protected int mNumCreated2 = 0;
+
+  /** commas are wrong * */
+  private int[] mInts = new int[] {1, 2, 3, 4};
+
+  public static int sTest1;
+
+  /** should be private * */
+  protected static int sTest3;
+
+  /** should be private * */
+  static int sTest2;
+
+  /** should be private * */
+  int mTest1;
+
+  /** should be private * */
+  public int mTest2;
+
+  /**
+   * @return hack
+   * @param badFormat1 bad format
+   * @param badFormat2 bad format
+   * @param badFormat3 bad format
+   * @throws java.lang.Exception abc
+   **/
+  int test1(int badFormat1,int badFormat2,
+  		final int badFormat3) // violation 'Line contains a tab character.'
+      throws java.lang.Exception {
+    return 0;
+  }
+
+  /** method that is 20 lines long * */
+  private void longMethod() {
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+  }
+
+  /** constructor that is 10 lines long * */
+  private InputWhitespaceCharacters() {
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+    // a line
+  }
+
+  /** test local variables */
+  private void localVariables() {
+    int abc = 0;
+    int ABC = 0;
+
+    final int cde = 0;
+    final int CDE = 0;
+
+    for (int k = 0; k < 1; k++) {
+      String innerBlockVariable = "";
     }
-
-    /** method that is 20 lines long **/
-    private void longMethod()
-    {
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
+    for (int I = 0; I < 1; I++) {
+      String InnerBlockVariable = "";
     }
+  }
 
-    /** constructor that is 10 lines long **/
-    private InputWhitespaceCharacters()
-    {
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-    }
+  /** test method pattern */
+  void ALL_UPPERCASE_METHOD() {}
 
-    /** test local variables */
-    private void localVariables()
-    {
-        int abc = 0;
-        int ABC = 0;
+  /** test illegal constant * */
+  private static final int BAD__NAME = 3;
 
-        final int cde = 0;
-        final int CDE = 0;
+  // A very, very long line that is OK because it matches the regexp "^.*is OK.*regexp.*$"
+  // long line that has a tab ->	<- and would be OK if tab counted as 1 char
+  // violation above 'Line contains a tab character.'
+  // tabs that count as one char because of their position ->	<-   ->	<-
+  // violation above 'Line contains a tab character.'
 
-        for (int k = 0; k < 1; k++)
-        {
-            String innerBlockVariable = "";
-        }
-        for (int I = 0; I < 1; I++)
-        {
-            String InnerBlockVariable = "";
-        }
-    }
-
-    /** test method pattern */
-    void ALL_UPPERCASE_METHOD()
-    {
-    }
-
-    /** test illegal constant **/
-    private static final int BAD__NAME = 3;
-
-    // A very, very long line that is OK because it matches the regexp "^.*is OK.*regexp.*$"
-    // long line that has a tab ->	<- and would be OK if tab counted as 1 char
-    // violation above 'Line contains a tab character.'
-    // tabs that count as one char because of their position ->	<-   ->	<-
-    // violation above 'Line contains a tab character.'
-
-    /** some lines to test the column after tabs */
-    void violateColumnAfterTabs()
-    {
-        // with tab-width 8 all statements below start at the same column,
-        // with different combinations of ' ' and '\t' before the statement
-                int tab0 =1;
-        	int tab1 =1; // violation 'Line contains a tab character.'
-         	int tab2 =1; // violation 'Line contains a tab character.'
+  /** some lines to test the column after tabs */
+  void violateColumnAfterTabs() {
+    // with tab-width 8 all statements below start at the same column,
+    // with different combinations of ' ' and '\t' before the statement
+    int tab0 =1;
+  	int tab1 =1; // violation 'Line contains a tab character.'
+  	int tab2 =1; // violation 'Line contains a tab character.'
 		int tab3 =1; // violation 'Line contains a tab character.'
-  	  	int tab4 =1; // violation 'Line contains a tab character.'
-  	        int tab5 =1; // violation 'Line contains a tab character.'
-    }
+ 	 	int tab4 =1; // violation 'Line contains a tab character.'
+  	int tab5 =1; // violation 'Line contains a tab character.'
+  }
 
-    // MEMME:
-    /* MEMME: a
-     * MEMME:
-     * OOOO
-     */
-    /* NOTHING */
-    /* YES */ /* MEMME: x */ /* YES!! */
+  // MEMME:
+  /* MEMME: a
+   * MEMME:
+   * OOOO
+   */
+  /* NOTHING */
+  /* YES */
+  /* MEMME: x */
+  /* YES!! */
 
-    /** test long comments **/
-    void veryLong()
-    {
-        /*
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          blah blah blah blah
-          enough talk */
-    }
+  /** test long comments * */
+  void veryLong() {
+    /*
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    blah blah blah blah
+    enough talk */
+  }
 
-    /**
-     * @see to lazy to document all args. Testing excessive # args
-     **/
-    void toManyArgs(int aArg1, int aArg2, int aArg3, int aArg4, int aArg5,
-                    int aArg6, int aArg7, int aArg8, int aArg9)
-    {
-    }
+  /**
+   * @see to lazy to document all args. Testing excessive # args
+   */
+  void toManyArgs(
+      int aArg1,
+      int aArg2,
+      int aArg3,
+      int aArg4,
+      int aArg5,
+      int aArg6,
+      int aArg7,
+      int aArg8,
+      int aArg9) {}
 }
 
 /** Test class for variable naming in for each clause. */
-class InputSimple2
-{
-    /** Some more Javadoc. */
-    public void doSomething()
-    {
-        //"O" should be named "o"
-        for (Object O : new java.util.ArrayList())
-        {
-
-        }
-    }
+class InputSimple2 {
+  /** Some more Javadoc. */
+  public void doSomething() {
+    // "O" should be named "o"
+    for (Object O : new java.util.ArrayList()) {}
+  }
 }
 
 /** Test enum for member naming check */
-enum MyEnum1
-{
-    /** ABC constant */
-    ABC,
+enum MyEnum1 {
+  /** ABC constant */
+  ABC,
 
-    /** XYZ constant */
-    XYZ;
+  /** XYZ constant */
+  XYZ;
 
-    /** Should be mSomeMember */
-    private int someMember;
+  /** Should be mSomeMember */
+  private int someMember;
 }
