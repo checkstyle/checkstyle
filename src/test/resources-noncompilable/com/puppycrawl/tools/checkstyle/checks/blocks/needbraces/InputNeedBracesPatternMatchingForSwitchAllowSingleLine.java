bf62df626d73a51bfb8de416ed50a044831b16c3
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-07-19 14:17:44 +0300

    Issue #15034: Add new input file for pattern matching for switch in NeedsBracesCheck

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/blocks/needbraces/InputNeedBracesPatternMatchingForSwitchAllowSingleLine.java
@@ -0,0 +1,100 @@
+/*
+NeedBraces
+allowSingleLineStatement = true
+allowEmptyLoopBody = (default)false
+tokens = LITERAL_CASE, LITERAL_DEFAULT, LAMBDA
+
+
+*/
+
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.blocks.needbraces;
+
+public class InputNeedBracesPatternMatchingForSwitch2 {
+
+    void testSwitchStatements(Object o) {
+        switch (o) {
+            case Integer i when (i == 0) :  // violation
+                System.out.println("zero");
+                break;
+
+            case String s when (s.equals("a")) :  // violation
+                System.out.println("a");
+                break;
+
+            case Point(int x, int y ) when (x>=0 && y >=0) :  // violation
+                System.out.println("point");
+                break;
+            default :  // violation
+                System.out.println("default");
+        }
+
+         switch (o) {
+            case Integer i when (i == 0) : {
+                System.out.println("zero");
+                break;
+            }
+            case String s when (s.equals("a")) : {
+                System.out.println("a");
+                break;
+            }
+            case Point(int x, int y ) when (x>=0 && y >=0) : {
+                System.out.println("point");
+                break;
+            }
+            default : {
+                System.out.println("default");
+            }
+        }
+    }
+
+    void testSwitchRule(Object o) {
+        switch (o) {
+            case Integer i when (i == 0) -> System.out.println("zero"); // ok, single line is true
+
+            case String s when (s.equals("a")) ->  // violation
+                System.out.println("a");
+
+            case Point(int x, int y ) when (x>=0 && y >=0) ->  // violation
+                System.out.println("point");
+
+            default ->  // violation
+                System.out.println("default");
+        }
+        switch (o) {
+            case Integer i when (i == 0) -> {System.out.println("zero");}
+            case String s when (s.equals("a")) -> {System.out.println("a");}
+            case Point(int x, int y ) when (x>=0 && y >=0) -> {System.out.println("point");}
+            default -> {System.out.println("default");}
+        }
+    }
+
+    void testSwitchExpression(Object o) {
+        int a =  switch (o) {
+            case Integer i when (i == 0) :  // violation
+                System.out.println("zero");
+                yield i;
+
+            case String s when (s.equals("a")) : yield s.length();  // ok, single line is true
+
+            case Point(int x, int y ) when (x>=0 && y >=0) :  // violation
+                yield x + y;
+
+            default :  // violation
+                yield 0;
+        };
+
+        int b =  switch (o) {
+            case Integer i when (i == 0) -> i;
+            case String s when (s.equals("a")) -> s.length();
+            case Point(int x, int y ) when (x>=0 && y >=0) ->  // violation
+                x + y;
+            case Integer i when (i == 9) -> {
+                int n = i;
+                yield n;
+            }
+            default -> 0;
+        };
+    }
+    record Point(int x, int y) {}
+}
