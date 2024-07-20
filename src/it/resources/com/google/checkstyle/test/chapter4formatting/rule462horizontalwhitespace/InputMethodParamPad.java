2057ac975e1c9ad48fdad077afc0f75ae6bb23bc
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-20 23:33:38 +0530

    Issue #15218: formatted input files for section 4.6.2 Horizontal Whitespace to follow google style guide rules

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputMethodParamPad.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputMethodParamPad.java
@@ -1,63 +1,54 @@
 package com.google.checkstyle.test.chapter4formatting.rule462horizontalwhitespace;
+
 import java.util.Vector;
+
 /** Test input for MethodDefPadCheck */
-public class InputMethodParamPad
-{
-    public InputMethodParamPad()
-    {
-        super();
-    }
+public class InputMethodParamPad {
+  public InputMethodParamPad() {
+    super();
+  }
 
-    public InputMethodParamPad (int aParam) // violation ''(' is preceded with whitespace.'
-    {
-        super (); // violation ''(' is preceded with whitespace.'
-    }
+  public InputMethodParamPad (int aParam) { // violation ''(' is preceded with whitespace.'
+    super (); // violation ''(' is preceded with whitespace.'
+  }
 
-    public void method()
-    {
-    }
+  public void method() {}
 
-    public void method (int aParam) // violation ''(' is preceded with whitespace.'
-    {
-    }
+  public void method (int aParam) {} // violation ''(' is preceded with whitespace.'
 
-    public void
-        method(double aParam)
-    {
-        // invoke constructor
-        InputMethodParamPad pad = new InputMethodParamPad();
-        pad = new InputMethodParamPad (); // violation ''(' is preceded with whitespace.'
-        pad = new
+  public void method(double aParam) {
+    // invoke constructor
+    InputMethodParamPad pad = new InputMethodParamPad();
+    pad = new InputMethodParamPad (); // violation ''(' is preceded with whitespace.'
+    pad = new
             InputMethodParamPad();
 
-        // call method
+    // call method
+    method();
+    method (); // violation ''(' is preceded with whitespace.'
+  }
+
+  public void dottedCalls() {
+    this.method();
+    this.method (); // violation ''(' is preceded with whitespace.'
+    this.
         method();
-        method (); // violation ''(' is preceded with whitespace.'
-    }
 
-    public void dottedCalls()
-    {
-        this.method();
-        this.method (); // violation ''(' is preceded with whitespace.'
-        this.
-            method();
+    InputMethodParamPad p = new InputMethodParamPad();
+    p.method();
+    p.method (); // violation ''(' is preceded with whitespace.'
+    p.
+         method();
 
-        InputMethodParamPad p = new InputMethodParamPad();
-        p.method();
-        p.method (); // violation ''(' is preceded with whitespace.'
-        p.
-            method();
+    java.lang.Integer.parseInt("0");
+    java.lang.Integer.parseInt ("0"); // violation ''(' is preceded with whitespace.'
+    java.lang.Integer.
+       parseInt("0");
+  }
 
-        java.lang.Integer.parseInt("0");
-        java.lang.Integer.parseInt ("0"); // violation ''(' is preceded with whitespace.'
-        java.lang.Integer.
-            parseInt("0");
-    }
-
-    public void newArray()
-    {
-        int[] a = new int[]{0, 1};
-        java.util.Vector<String> v = new java.util.Vector<String>();
-        java.util.Vector<String> v1 = new Vector<String>();
-    }
+  public void newArray() {
+    int[] a = new int[]{0, 1};
+    java.util.Vector<String> v = new java.util.Vector<String>();
+    java.util.Vector<String> v1 = new Vector<String>();
+  }
 }
