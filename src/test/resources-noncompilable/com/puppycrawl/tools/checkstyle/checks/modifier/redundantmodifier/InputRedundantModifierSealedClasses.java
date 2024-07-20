1448b26031d08f16c66bb6ca4ce4c402ccb20ddb
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-07-20 04:24:05 +0300

    Issue #15087: Add new input file for sealed classes in RedundantModifierChecK

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/modifier/redundantmodifier/InputRedundantModifierSealedClasses.java
@@ -0,0 +1,55 @@
+/*
+RedundantModifier
+
+
+*/
+
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.modifier.redundantmodifier;
+
+public sealed interface InputRedundantModifierSealedClasses {
+   final public static String s = null;
+   // 3 violations above:
+   //                  'Redundant 'final' modifier'
+   //                  'Redundant 'public' modifier'
+   //                  'Redundant 'static' modifier'
+   abstract public void m();
+   // 2 violations above:
+   //                  'Redundant 'abstract' modifier'
+   //                  'Redundant 'public' modifier'
+   public  static  sealed interface J permits A {
+   // 2 violations above:
+   //                  'Redundant 'public' modifier'
+   //                  'Redundant 'static' modifier'
+        abstract public void m();
+        // 2 violations above:
+        //                  'Redundant 'abstract' modifier'
+        //                  'Redundant 'public' modifier'
+   }
+   public static sealed class B permits D { }
+   // 2 violations above:
+   //                  'Redundant 'public' modifier'
+   //                  'Redundant 'static' modifier'
+   public static non-sealed class D extends B { }
+   // 2 violations above:
+   //                  'Redundant 'public' modifier'
+   //                  'Redundant 'static' modifier'
+}
+
+interface InputRedundantModifierSealedClassesCorrected {
+    String s = null;
+    void m();
+    sealed interface J permits A {
+        void m();
+    }
+    sealed class B permits A, D { }
+    non-sealed class D extends B { }
+}
+
+final class A extends InputRedundantModifierSealedClasses.B
+        implements InputRedundantModifierSealedClasses.J,
+        InputRedundantModifierSealedClassesCorrected.J {
+
+    @Override
+    public void m() {}
+}
