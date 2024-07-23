8ae6819a972dfd8001c4c9d67cb8f0ca5179c0e8
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-07-24 00:11:13 +0530

    Issue #15214: Migrated section 4.3 One statement per line to whole config testing

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule43onestatement/InputOneStatementPerLine.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule43onestatement/InputOneStatementPerLine.java
@@ -1,11 +1,10 @@
 package com.google.checkstyle.test.chapter4formatting.rule43onestatement;
 
-/**
- * Two import statements on the same line are illegal.
- */
-import java.io.EOFException; import java.io.BufferedReader;
+// Two import statements on the same line are illegal.
+import java.io.BufferedReader; import java.io.EOFException;
 // violation above 'Only one statement per line allowed.'
 
+/** Some javadoc. */
 public class InputOneStatementPerLine {
 
   /**
@@ -52,11 +51,19 @@ public class InputOneStatementPerLine {
     if (one == 1) {
       one++; two++; // violation 'Only one statement per line allowed.'
     }
-    if (one != 1) { one++; } else { one--; } // violation 'Only one statement per line allowed.'
+    if (one != 1) { one++; } else { one--; }
+    // 3 violations above:
+    //  ''{' at column 19 should have line break after.'
+    //  ''{' at column 35 should have line break after.'
+    //  'Only one statement per line allowed.'
     int n = 10;
 
     doLegal(); doLegal(); // violation 'Only one statement per line allowed.'
-    while (one == 1) { one++; two--; } // violation 'Only one statement per line allowed.'
+    while (one == 1) { one++; two--; }
+    // 3 violations above:
+    //  ''{' at column 22 should have line break after.'
+    //  'Only one statement per line allowed.'
+    //  ''}' at column 38 should be alone on a line.'
 
   }
 
@@ -96,7 +103,11 @@ public class InputOneStatementPerLine {
       if (one == 1) {
         one++; two++; // violation 'Only one statement per line allowed.'
       }
-      if (one != 1) { one++; } else { one--; } // violation 'Only one statement per line allowed.'
+      if (one != 1) { one++; } else { one--; }
+      // 3 violations above:
+      //  ''{' at column 21 should have line break after.'
+      //  ''{' at column 37 should have line break after.'
+      //  'Only one statement per line allowed.'
       int n = 10;
 
       doLegal(); doLegal(); // violation 'Only one statement per line allowed.'
@@ -105,31 +116,37 @@ public class InputOneStatementPerLine {
       }
 
     }
-    }
+  }
 
   /**
    * Two declaration statements on the same line are illegal.
    */
-  int a; int b; // violation 'Only one statement per line allowed.'
+  int aaaa; int bbb;
+  // 2 violations above:
+  //  'Only one variable definition per line allowed.'
+  //  'Only one statement per line allowed.'
 
   /**
    * Two declaration statements which are not on the same line
    * are legal.
    */
-  int c;
-  int d;
+  int ccc;
+  int dddd;
 
   /**
    * Two assignment (declaration) statements on the same line are illegal.
    */
-  int e = 1; int f = 2; // violation 'Only one statement per line allowed.'
+  int dog = 1; int cow = 2;
+  // 2 violations above:
+  //  'Only one variable definition per line allowed.'
+  //  'Only one statement per line allowed.'
 
   /**
    * Two assignment (declaration) statements on the different lines
    * are legal.
    */
-  int g = 1;
-  int h = 2;
+  int test1 = 1;
+  int test2 = 2;
 
   /**
    * This method contains
@@ -138,45 +155,50 @@ public class InputOneStatementPerLine {
   private void foo() {
     //Two object creation statements on the same line are illegal.
     Object obj1 = new Object(); Object obj2 = new Object();
-    // violation above 'Only one statement per line allowed.'
+    // 2 violations above:
+    //  'Only one variable definition per line allowed.'
+    //  'Only one statement per line allowed.'
   }
 
   /**
    * One multiline  assignment (declaration) statement
    * is legal.
    */
-  int i = 1, j = 2,
-          k = 5;
+  int ijk = 1, jkl = 2, // violation 'Each variable declaration must be in its own statement.'
+          klm = 5;
 
   /**
    * One multiline  assignment (declaration) statement
    * is legal.
    */
-  int l = 1,
-          m = 2,
-          n = 5;
+  int lmn = 1, // violation 'Each variable declaration must be in its own statement.'
+          mnp = 2,
+          npq = 5;
 
   /**
    * One multiline  assignment (declaration) statement
    * is legal.
    */
-  int w = 1,
-          x = 2,
-          y = 5
+  int wxy = 1, // violation 'Each variable declaration must be in its own statement.'
+          xyzz = 2,
+          yys = 5
                   ;
 
   /**
    * Two multiline  assignment (declaration) statements
    * are illegal.
    */
-  int o = 1, p = 2,
-          r = 5; int t; // violation 'Only one statement per line allowed.'
+  // 2 violations 3 lines below:
+  //  'Each variable declaration must be in its own statement.'
+  //  'Only one variable definition per line allowed.'
+  int abc = 1, pqr = 2,
+          xyz = 5; int blah; // violation 'Only one statement per line allowed.'
 
   /**
    * Two assignment (declaration) statement
    * which are not on the same lines and are legal.
    */
-  int four = 1,
+  int four = 1, // violation 'Each variable declaration must be in its own statement.'
           five = 5
                   ;
   int seven = 2;
@@ -186,8 +208,11 @@ public class InputOneStatementPerLine {
    * (they both are distributed over two lines)
    * are illegal.
    */
+  // 2 violations 3 lines below:
+  //  'Each variable declaration must be in its own statement.'
+  //  'Only one variable definition per line allowed.'
   int var1 = 5,
-      var4 = 5; int var2 = 6,
+      var4 = 5; int var2 = 6, // violation 'Each variable declaration must be in its own statement.'
           var3 = 5; // violation 'Only one statement per line allowed.'
 
   /**
@@ -195,6 +220,9 @@ public class InputOneStatementPerLine {
    * (they both are distributed over two lines)
    * are illegal.
    */
+  // 2 violations 3 lines below:
+  //  'Only one variable definition per line allowed.'
+  //  'Each variable declaration must be in its own statement.'
   int var6 = 5; int var7 = 6,
       var8 = 5; // violation 'Only one statement per line allowed.'
 
@@ -206,9 +234,12 @@ public class InputOneStatementPerLine {
   private void foo2() {
     toString(
 
-    ); toString (
+    ); toString(// violation ''method call' .* incorrect indentation level 4, expected .* 6.'
 
-    ); // violation 'Only one statement per line allowed.'
+        );
+    // 2 violations above:
+    // ''method call rparen' has incorrect indentation level 8, expected level should be 4.'
+    // 'Only one statement per line allowed.'
   }
 
 
@@ -216,7 +247,7 @@ public class InputOneStatementPerLine {
    * While theoretically being distributed over two lines, this is a sample
    * of 2 statements on one line.
    */
-  int var9 = 1, var10 = 5
+  int var9 = 1, var10 = 5 // violation 'Each variable declaration must be in its own statement.'
       ; int var11 = 2; // violation 'Only one statement per line allowed.'
 
 
@@ -235,10 +266,11 @@ public class InputOneStatementPerLine {
   /**
    * Two multiline statements (which are not on the same line)
    * are legal.
+   * Violations are not related to "OneStatementPerLine", but to "MultipleVariableDeclarations".
    */
-  int var12,
+  int var12, // violation 'Each variable declaration must be in its own statement.'
       var13 = 12;
-  int var14 = 5,
+  int var14 = 5, // violation 'Each variable declaration must be in its own statement.'
       var15 = 6;
 
   /**
@@ -254,36 +286,43 @@ public class InputOneStatementPerLine {
       var9++;
     } while (var11 < 7); //legal
 
-    /**
+    /*
      * One statement inside for block is legal
      */
-    for (int i = 0; i < 10; i++) one = 5; //legal
+    for (int i = 0; i < 10; i++) { one = 5; } //legal
+    // 2 violations above:
+    //  ''{' at column 34 should have line break after.'
+    //  ''}' at column 45 should be alone on a line.'
 
-    /**
+    /*
      * One statement inside for block where
      * increment expression is empty is legal
      */
-    for (int i = 0; i < 10;) one = 5; //legal
+    for (int i = 0; i < 10;) { one = 5; } //legal
+    // 2 violations above:
+    //  ''{' at column 30 should have line break after.'
+    //  ''}' at column 41 should be alone on a line.'
 
-    /**
-     * One statement inside for block where
-     * increment and conditional expressions are empty
-     * (forever loop) is legal
+    /*
+      One statement inside for block where
+      increment and conditional expressions are empty
+      (forever loop) is legal
      */
-    for (int i = 0;;) one = 5; //legal
+    for (int i = 0;;) { one = 5; } //legal
+    // 2 violations above:
+    //  ''{' at column 23 should have line break after.'
+    //  ''}' at column 34 should be alone on a line.'
   }
 
+  /** Some javadoc. */
   public void foo5() {
-    /**
-     * a "forever" loop.
-     */
+    // a "forever" loop.
     for (;;){} //legal
   }
 
+  /** Some javadoc. */
   public void foo6() {
-    /**
-     * One statement inside for block is legal
-     */
+    // One statement inside for block is legal
     for (;;) {
       one = 5;
     } //legal
