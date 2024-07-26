2b009cb3a179e0e16523547a1086e95adbe6d653
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-07-26 16:12:03 +0300

    Issue #10798: New property to violate strictfp as redundant modifier on java 17 or later

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/modifier/redundantmodifier/InputRedundantModifierStrictfpWithDefaultVersion.java
@@ -0,0 +1,46 @@
+/*
+RedundantModifier
+tokens = (default)METHOD_DEF, VARIABLE_DEF, ANNOTATION_FIELD_DEF, INTERFACE_DEF, \
+         CTOR_DEF, CLASS_DEF, ENUM_DEF, RESOURCE
+
+
+*/
+
+//non-compiled with javac: Compilable with Java17
+package com.puppycrawl.tools.checkstyle.checks.modifier.redundantmodifier;
+
+public class InputRedundantModifierStrictfpWithDefaultVersion {
+
+    public static strictfp class Test { }
+    // violation above, 'Redundant 'strictfp' modifier'
+
+    strictfp interface MyInterface { }
+    // violation above, 'Redundant 'strictfp' modifier'
+
+    strictfp enum MyEnum {}
+    // violation above, 'Redundant 'strictfp' modifier'
+
+    strictfp record MyRecord(int x) {}
+    // violation above, 'Redundant 'strictfp' modifier'
+
+    abstract strictfp class MyAbstractClass { }
+    // violation above, 'Redundant 'strictfp' modifier'
+
+    abstract strictfp interface MyStrictFPInterface {
+        // 2 violations above:
+        //                  'Redundant 'abstract' modifier'
+        //                  'Redundant 'strictfp' modifier'
+        public static strictfp enum MyInnerEnum { }
+        // 3 violations above:
+        //                   'Redundant 'public' modifier'
+        //                   'Redundant 'static' modifier'
+        //                   'Redundant 'strictfp' modifier'
+    }
+
+    final class OtherClass {
+        final strictfp void m1() {}
+        // 2 violations above:
+        //                  'Redundant 'final' modifier'
+        //                  'Redundant 'strictfp' modifier'
+    }
+}
