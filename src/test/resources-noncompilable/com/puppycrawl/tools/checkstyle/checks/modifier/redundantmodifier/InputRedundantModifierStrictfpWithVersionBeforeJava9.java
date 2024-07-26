0ad47100d5e88c02ab1026094b3a58827e4d1e61
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-07-26 16:12:03 +0300

    Issue #10798: New property to violate strictfp as redundant modifier on java 17 or later

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/modifier/redundantmodifier/InputRedundantModifierStrictfpWithVersionBeforeJava9.java
@@ -0,0 +1,37 @@
+/*
+RedundantModifier
+tokens = (default)METHOD_DEF, VARIABLE_DEF, ANNOTATION_FIELD_DEF, INTERFACE_DEF, \
+         CTOR_DEF, CLASS_DEF, ENUM_DEF, RESOURCE
+jdkVersion = 1.8
+
+
+*/
+
+//non-compiled with javac: Compilable with Java17
+package com.puppycrawl.tools.checkstyle.checks.modifier.redundantmodifier;
+
+public class InputRedundantModifierStrictfpWithVersionBeforeJava9 {
+
+    public static strictfp class Test { }
+
+    strictfp interface MyInterface { }
+
+    strictfp enum MyEnum {}
+
+    strictfp record MyRecord(int x) {}
+
+    abstract strictfp class MyAbstractClass { }
+
+    abstract strictfp interface MyStrictFPInterface {
+        // violation above, 'Redundant 'abstract' modifier'
+        public static strictfp enum MyInnerEnum { }
+        // 2 violations above:
+        //                   'Redundant 'public' modifier'
+        //                   'Redundant 'static' modifier'
+    }
+
+    final class OtherClass {
+        final strictfp void m1() {}
+        // violation above, 'Redundant 'final' modifier'
+    }
+}
