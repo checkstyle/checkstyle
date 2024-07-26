6f3a1493c3e425d356c3d1ea8acbe1bf4b7c4921
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-07-26 20:15:07 +0300

    Issue #13345: Enable RedundantModifierCheckExampleTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/modifier/redundantmodifier/Example2.java
@@ -0,0 +1,42 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="RedundantModifier">
+      <property name="tokens" value="METHOD_DEF"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.modifier.redundantmodifier;
+
+// xdoc section -- start
+public class Example2 {
+
+  void test() {
+    try (final var a = lock()) {
+
+    } catch (Exception e) {
+
+    }
+  }
+
+  abstract interface I {
+    public abstract void m();
+    // 2 violations above:
+    //                   'Redundant 'public' modifier'
+    //                   'Redundant 'abstract' modifier'
+    public int x = 0;
+  }
+
+  static enum E {
+        A, B, C
+  }
+
+  public strictfp class Test { }
+
+  AutoCloseable lock() {
+    return null;
+  }
+}
+// xdoc section -- end
