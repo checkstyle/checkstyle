29c0752a8b83b7eb1222fdf0a88958cec1f901bd
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-08-04 22:03:55 +0530

    Issue #15214: migrated section 5.1 to whole config testing and updated it on coverage table, removed unused verifyWithConfigParser method

--- src/it/resources/com/google/checkstyle/test/chapter5naming/rule51identifiernames/InputCatchParameterName.java
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule51identifiernames/InputCatchParameterName.java
@@ -1,65 +1,115 @@
 package com.google.checkstyle.test.chapter5naming.rule51identifiernames;
 
+/** some javadoc. */
 public class InputCatchParameterName {
-    {
-        try {
-        } catch (Exception e) {
-        }
-        try {
-        } catch (Exception ex) {
-        }
-        try {
-        } catch (Error | Exception err) {
-        }
-        try {
-        } catch (Exception exception) {
-        }
-        try {
-        } catch (Exception exception1) {
-        }
-        try {
-        } catch (Exception noWorries) {
-        }
-        try {
-        } catch (Throwable t) {
-        }
-        try {
-            throw new InterruptedException("interruptedException");
-        } catch (InterruptedException ie) {
-        }
-        try {
-        } catch (Exception ok) {
-            // appropriate to take no action here
-        }
-        try {
-        } catch (Exception e1) {
-            try {
-            } catch (Exception e2) {
-            }
-        }
-        try {
-        } catch (Throwable t1) {
-            try {
-            } catch (Throwable t2) {
-            }
-        }
-        try {
-        } catch (Exception iException) { // warn
-        }
-        try {
-        } catch (Exception ex_1) { // warn
-        }
-        try {
-        } catch (Exception eX) { // warn
-        }
-        try {
-        } catch (Exception eXX) { // warn
-        }
-        try {
-        } catch (Exception x_y_z) { // warn
-        }
-        try {
-        } catch (Exception Ex) { // warn
-        }
+  {
+    try {
+      /* foo */
+    } catch (Exception e) {
+      /* foo */
     }
+    try {
+      /* foo */
+    } catch (Exception ex) {
+      /* foo */
+    }
+    try {
+      /* foo */
+    } catch (Error | Exception err) {
+      /* foo */
+    }
+    try {
+      /* foo */
+    } catch (Exception exception) {
+      /* foo */
+    }
+    try {
+      /* foo */
+    } catch (Exception exception1) {
+      /* foo */
+    }
+    try {
+      /* foo */
+    } catch (Exception noWorries) {
+      /* foo */
+    }
+    try {
+      /* foo */
+    } catch (Throwable t) {
+      /* foo */
+    }
+    try {
+      throw new InterruptedException("interruptedException");
+    } catch (InterruptedException ie) {
+      /* foo */
+    }
+    try {
+      /* foo */
+    } catch (Exception ok) {
+      // appropriate to take no action here
+    }
+    try {
+      /* foo */
+    } catch (Exception e1) {
+      try {
+        /* foo */
+      } catch (Exception e2) {
+        /* foo */
+      }
+    }
+    try {
+      /* foo */
+    } catch (Throwable t1) {
+      try {
+        /* foo */
+      } catch (Throwable t2) {
+        /* foo */
+      }
+    }
+    try {
+      /* foo */
+    } catch (Exception iException) {
+      // violation above 'Catch parameter name 'iException' must match pattern'
+    }
+    try {
+      /* foo */
+    } catch (Exception ex_1) { // violation 'Catch parameter name 'ex_1' must match pattern'
+    }
+    try {
+      /* foo */
+    } catch (Exception eX) { // violation 'Catch parameter name 'eX' must match pattern'
+    }
+    try {
+      /* foo */
+    } catch (Exception eXX) {
+      // 2 violations above:
+      //  'Abbreviation in name 'eXX' must contain no more than '1' consecutive capital letters.'
+      //  'Catch parameter name 'eXX' must match pattern'
+      /* foo */
+    }
+    try {
+      /* foo */
+    } catch (Exception x_y_z) { // violation 'Catch parameter name 'x_y_z' must match pattern'
+    }
+    try {
+      /* foo */
+    } catch (Exception Ex) { // violation 'Catch parameter name 'Ex' must match pattern'
+    }
+    try {
+      /* foo */
+    } catch (Exception name_) { // violation 'Catch parameter name 'name_' must match pattern'
+    }
+    try {
+      /* foo */
+    } catch (Exception mName) { // violation 'Catch parameter name 'mName' must match pattern'
+    }
+    try {
+      /* foo */
+    } catch (Exception s_name) { // violation 'Catch parameter name 's_name' must match pattern'
+    }
+    try {
+      /* foo */
+    } catch (Exception kName) { // violation 'Catch parameter name 'kName' must match pattern'
+    }
+  }
 }
