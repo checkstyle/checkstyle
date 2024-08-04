54ef22ffe0239491a3147823e4a32647a1dfdef8
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-08-04 22:03:55 +0530

    Issue #15214: migrated section 5.1 to whole config testing and updated it on coverage table, removed unused verifyWithConfigParser method

--- src/it/java/com/google/checkstyle/test/base/AbstractGoogleModuleTestSupport.java
+++ src/it/java/com/google/checkstyle/test/base/AbstractGoogleModuleTestSupport.java
@@ -20,19 +20,13 @@
 package com.google.checkstyle.test.base;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
 import org.checkstyle.base.AbstractItModuleTestSupport;
 
 import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
-import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
 import com.puppycrawl.tools.checkstyle.PropertiesExpander;
-import com.puppycrawl.tools.checkstyle.TreeWalker;
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import com.puppycrawl.tools.checkstyle.api.Configuration;
 import com.puppycrawl.tools.checkstyle.internal.utils.CheckUtil;
@@ -45,26 +39,6 @@ public abstract class AbstractGoogleModuleTestSupport extends AbstractItModuleTe
     private static final Configuration CONFIGURATION;
 
     private static final Set<Class<?>> CHECKSTYLE_MODULES;
-
-    private static final Set<String> CHECKER_CHILDREN = new HashSet<>(Arrays.asList(
-            "BeforeExecutionExclusionFileFilter",
-            "SeverityMatchFilter",
-            "SuppressionFilter",
-            "SuppressionSingleFilter",
-            "SuppressWarningsFilter",
-            "SuppressWithNearbyTextFilter",
-            "SuppressWithPlainTextCommentFilter",
-            "JavadocPackage",
-            "NewlineAtEndOfFile",
-            "UniqueProperties",
-            "OrderedProperties",
-            "RegexpMultiline",
-            "RegexpSingleline",
-            "RegexpOnFilename",
-            "FileLength",
-            "LineLength",
-            "FileTabCharacter"
-    ));
 
     static {
         try {
@@ -104,42 +78,6 @@ public abstract class AbstractGoogleModuleTestSupport extends AbstractItModuleTe
     }
 
     /**
-     * Returns {@link Configuration} instance for the given module name.
-     * This implementation uses {@link #getModuleConfig(String, String)} method inside.
-     *
-     * @param moduleName module name.
-     * @return {@link Configuration} instance for the given module name.
-     */
-    protected static Configuration getModuleConfig(String moduleName) {
-        return getModuleConfig(moduleName, null);
-    }
-
-    /**
-     * Returns {@link Configuration} instance for the given module name.
-     * This implementation uses {@link #getModuleConfig(String)} method inside.
-     *
-     * @param moduleName module name.
-     * @param moduleId module id.
-     * @return {@link Configuration} instance for the given module name.
-     * @throws IllegalStateException if there is a problem retrieving the module or config.
-     */
-    protected static Configuration getModuleConfig(String moduleName, String moduleId) {
-        return getModuleConfig(CONFIGURATION, moduleName, moduleId);
-    }
-
-    /**
-     * Returns a list of all {@link Configuration} instances for the given module IDs.
-     *
-     * @param moduleIds module IDs.
-     * @return List of {@link Configuration} instances.
-     * @throws CheckstyleException if there is an error with the config.
-     */
-    protected static List<Configuration> getModuleConfigsByIds(String... moduleIds)
-            throws CheckstyleException {
-        return getModuleConfigsByIds(CONFIGURATION, moduleIds);
-    }
-
-    /**
      * Performs verification of the file with the given file path against the whole config.
      *
      * @param filePath file path to verify.
@@ -147,63 +85,5 @@ public abstract class AbstractGoogleModuleTestSupport extends AbstractItModuleTe
      */
     protected void verifyWithWholeConfig(String filePath) throws Exception {
         verifyWithItConfig(CONFIGURATION, filePath);
-    }
-
-    // until https://github.com/checkstyle/checkstyle/issues/15214
-    /**
-     * Performs verification of the file with the given file path against config.
-     * It uses the specified list of modules to load them from config for validation.
-     *
-     * @param listOfModules list of modules to load from config.
-     * @param filePath file path to verify.
-     * @throws Exception if exception occurs during verification process.
-     */
-    protected final void verifyWithConfigParser(String[] listOfModules,
-           String filePath) throws Exception {
-        final DefaultConfiguration rootConfig = new DefaultConfiguration(ROOT_MODULE_NAME);
-        final DefaultConfiguration treeWalkerConfig =
-                new DefaultConfiguration(TreeWalker.class.getSimpleName());
-
-        for (String module : listOfModules) {
-            final List<Configuration> children =
-                    getConfigChildren(module);
-            if (CHECKER_CHILDREN.contains(module)) {
-                for (Configuration child : children) {
-                    rootConfig.addChild(child);
-                }
-            }
-            else {
-                for (Configuration child : children) {
-                    treeWalkerConfig.addChild(child);
-                }
-            }
-        }
-
-        if (treeWalkerConfig.getChildren().length > 0) {
-            rootConfig.addChild(treeWalkerConfig);
-        }
-
-        verifyWithItConfig(rootConfig, filePath);
-    }
-
-    /**
-     * Gets the specified children module(s) from the config.
-     *
-     * @param module the children module to get. It can be either a module name or module id.
-     * @return list of children modules.
-     * @throws CheckstyleException if there is a problem for getting the module.
-     */
-    private static List<Configuration> getConfigChildren(String module)
-            throws CheckstyleException {
-        final List<Configuration> children = new ArrayList<>();
-
-        try {
-            children.add(getModuleConfig(module));
-        }
-        catch (IllegalStateException ex) {
-            children.addAll(getModuleConfigsByIds(module));
-        }
-
-        return children;
     }
 }
