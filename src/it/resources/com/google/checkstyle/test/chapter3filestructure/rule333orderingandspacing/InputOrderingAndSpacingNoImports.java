28af0c88c1597afa6287a7e313a2c7f052dd00e7
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-08-12 17:45:06 +0530

    Issue #15449: partially excluded rule333orderingandspacing from google-java-format.sh

--- src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule333orderingandspacing/InputOrderingAndSpacingNoImports.java
+++ src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule333orderingandspacing/InputOrderingAndSpacingNoImports.java
@@ -1,5 +1,4 @@
 package com.google.checkstyle.test.chapter3filestructure.rule333orderingandspacing;
 
 /** Some javadoc. */
-public class InputOrderingAndSpacingNoImports {
-}
+public class InputOrderingAndSpacingNoImports {}
