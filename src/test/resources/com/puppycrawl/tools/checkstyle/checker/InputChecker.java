20173c77d48ce63936f7bc77f330269a85ac4f29
SteLeo1602 <stepanleontev16@gmail.com>
2024-08-13 12:21:57 -0700

    Issue #11446: Updated testHaltOnExceptionOff in CheckerTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checker/InputChecker.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checker/InputChecker.java
@@ -1,3 +1,13 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="com.puppycrawl.tools.checkstyle.internal.testmodules.CheckWhichThrowsError" />
+  </module>
+  <property name="haltOnException" value="false"/>
+</module>
+*/
+// violation 8 lines above 'java.lang.IndexOutOfBoundsException: test'
+
 package com.puppycrawl.tools.checkstyle.checker;
 /*comment*/
 public class InputChecker {
