8da6f47dc9f45807931e10e454d6e5191127ba52
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-08-23 17:00:16 +0530

    Issue #15340: created InputFormatted file for section 3.3.2 No Line Wrapping

--- src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule332nolinewrap/InputFormattedNoLineWrapping.java
+++ src/it/resources/com/google/checkstyle/test/chapter3filestructure/rule332nolinewrap/InputFormattedNoLineWrapping.java
@@ -1,21 +1,10 @@
 package com.google.checkstyle.test.chapter3filestructure.rule332nolinewrap;
 
-import static java.math. // violation 'import statement should not be line-wrapped.'
-        BigInteger.ONE;
-import static java.math.BigInteger.ZERO; // ok
-
 // ok, long imports are allowed
 import com.google.checkstyle.test.chapter3filestructure.toolongpackagetotestcoveragegooglesjavastylerule.PackageStatementTest;
-import com.puppycrawl.tools.checkstyle.checks.design.FinalClassCheck; // ok
-import java.util.concurrent.atomic.AtomicIntegerFieldUpdater; // ok
-import java.util.concurrent.atomic // violation 'import statement should not be line-wrapped.'
-        .AtomicMarkableReference;
-import javax.accessibility.AccessibleAttributeSequence; // ok
-import javax.accessibility. // violation 'import statement should not be line-wrapped.'
-        AccessibleAttributeSequence;
 
 /** Some javadoc. */
-public class InputNoLineWrapping {
+public class InputFormattedNoLineWrapping {
 
   public void fooMethod() {
     //
