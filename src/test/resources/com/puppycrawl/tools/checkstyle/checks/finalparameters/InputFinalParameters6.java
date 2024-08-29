f660bcf4261513a111c4fa9bb7738d1b80dbd3f4
Roman Ivanov <ivanov-jr@mail.ru>
2024-08-29 05:53:30 -0700

    minor: add Input to PR

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/finalparameters/InputFinalParameters6.java
@@ -0,0 +1,11 @@
+/*
+FinalParameters
+ignorePrimitiveTypes = (default)false
+tokens = FOR_EACH_CLAUSE
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.finalparameters;
+
+class InputFinalParameters6 {}
