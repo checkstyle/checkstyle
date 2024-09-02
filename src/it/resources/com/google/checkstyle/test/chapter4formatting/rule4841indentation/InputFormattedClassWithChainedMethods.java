26fc5ccd8e4bb1c4ca62af67ce125efd04daad05
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-09-02 21:41:54 +0530

    Issue #15340: created InputFormatted file for section 4.8.41 Indentation

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4841indentation/InputFormattedClassWithChainedMethods.java
@@ -0,0 +1,32 @@
+package com.google.checkstyle.test.chapter4formatting.rule4841indentation;
+
+/** some javadoc. */
+public class InputFormattedClassWithChainedMethods {
+
+  public InputFormattedClassWithChainedMethods(Object... params) {}
+
+  /** some javadoc. */
+  public static void main(String[] args) {
+    new InputFormattedClassWithChainedMethods()
+        .getInstance("string_one")
+        .doNothing("string_one".trim(), "string_two");
+
+    String test =
+        new InputFormattedClassWithChainedMethods("param1", "param2")
+            .getInstance()
+            .doNothing("nothing");
+
+    String test2 =
+        new InputFormattedClassWithChainedMethods("param1", "param2")
+            .getInstance()
+            .doNothing("nothing");
+  }
+
+  public String doNothing(String something, String... uselessParams) {
+    return something;
+  }
+
+  public InputFormattedClassWithChainedMethods getInstance(String... uselessParams) {
+    return this;
+  }
+}
