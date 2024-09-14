2b29d20d1cccc0b8adc892d8d66e83e5deafd48d
Chris <chris-carneiro@users.noreply.github.com>
2024-09-14 13:25:20 +0200

    Issue #15456: Specify violation messages for AnnotationOnSameLineCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -200,7 +200,6 @@ public final class InlineConfigParser {
      *  Until <a href="https://github.com/checkstyle/checkstyle/issues/15456">#15456</a>.
      */
     private static final Set<String> SUPPRESSED_CHECKS = Set.of(
-            "com.puppycrawl.tools.checkstyle.checks.annotation.AnnotationOnSameLineCheck",
             "com.puppycrawl.tools.checkstyle.checks.annotation.PackageAnnotationCheck",
             "com.puppycrawl.tools.checkstyle.checks.annotation.SuppressWarningsCheck",
             "com.puppycrawl.tools.checkstyle.checks.ArrayTypeStyleCheck",
