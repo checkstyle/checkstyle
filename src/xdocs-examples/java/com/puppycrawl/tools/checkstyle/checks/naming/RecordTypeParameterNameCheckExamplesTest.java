cd723cc4924ba6652309d2586143f62510ca645a
Rishabh Dhiman <rishabh.dhiman@autorabit.com>
2024-09-18 15:38:40 +0530

    Revert "Issue #13345: Enable RecordTypeParameterNameCheck example tests"
    This reverts commit 54dfada5f85c1df023c54520a3392e3904847e3c.
    
    Revert "Issue #13345: Enable RecordTypeParameterNameCheck example tests"
    
    This reverts commit a0c60422120a7a906e692c6b35949629a0fd04c6.

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/naming/RecordTypeParameterNameCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/naming/RecordTypeParameterNameCheckExamplesTest.java
@@ -24,8 +24,7 @@ import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
 
-import static com.puppycrawl.tools.checkstyle.checks.naming.AbstractNameCheck.MSG_INVALID_PATTERN;
-
+@Disabled("until https://github.com/checkstyle/checkstyle/issues/13345")
 public class RecordTypeParameterNameCheckExamplesTest extends AbstractExamplesModuleTestSupport {
     @Override
     protected String getPackageLocation() {
@@ -35,19 +34,18 @@ public class RecordTypeParameterNameCheckExamplesTest extends AbstractExamplesMo
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-                "14:22: " + getCheckMessage(MSG_INVALID_PATTERN, "t", "^[A-Z]$"),
-                "15:22: " + getCheckMessage(MSG_INVALID_PATTERN, "abc", "^[A-Z]$")
+
         };
 
-        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
+        verifyWithInlineConfigParser(getPath("Example1.txt"), expected);
     }
 
     @Test
     public void testExample2() throws Exception {
         final String[] expected = {
-                "16:22: " + getCheckMessage(MSG_INVALID_PATTERN, "abc", "^[a-zA-Z]$")
+
         };
 
-        verifyWithInlineConfigParser(getPath("Example2.java"), expected);
+        verifyWithInlineConfigParser(getPath("Example2.txt"), expected);
     }
 }
