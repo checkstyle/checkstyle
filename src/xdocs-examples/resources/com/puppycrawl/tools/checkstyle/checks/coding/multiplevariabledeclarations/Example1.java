62c1db3db1a0a630cd2ccbd7e06e389c6319655c
absolutely-sam <sampathvrss@gmail.com>
2024-09-18 16:14:19 +0530

    Issue #13345: Enable MultipleVariableDeclarationsCheck example tests

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplevariabledeclarations/Example1.java
@@ -0,0 +1,24 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MultipleVariableDeclarations"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.multiplevariabledeclarations;
+
+// xdoc section -- start
+public class Example1 {
+  public void myTest() {
+    int mid = 0;
+    int high = 0;
+
+    int lower, higher; // violation, multiple variables declared in same statement
+
+    int value, // violation, multiple variables declared in the same statement
+        index;
+
+    int place = mid, number = high;  // violation, multiple variables declared
+  }
+}
+// xdoc section -- end
