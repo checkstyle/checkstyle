6ef17d902060917e634d1001b9c8ecd80e5cd81d
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-09-19 20:53:02 +0530

    supplemental: tests for #15503

--- src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputCorrectJavadocParagraph.java
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule712paragraphs/InputCorrectJavadocParagraph.java
@@ -97,4 +97,40 @@ class InputCorrectJavadocParagraph {
           return false;
         }
       };
+
+  /**
+   * Some summary.
+   *
+   * <p>testing
+   *
+   * <p>testing
+   */
+  class InnerPrecedingPTag {
+    /**
+     * Some summary.
+     *
+     * <p><b>testing</b>
+     */
+    public void foo() {}
+
+    /**
+     * Some summary.
+     *
+     * <table>
+     *   <thead>
+     *     <p></p>
+     *   </thead>
+     * </table>
+     */
+    public void fooo() {}
+
+    /**
+     * Some summary.
+     *
+     * <p>testing...
+     *
+     * <pre>testing...</pre>
+     */
+    public void foooo() {}
+  }
 }
