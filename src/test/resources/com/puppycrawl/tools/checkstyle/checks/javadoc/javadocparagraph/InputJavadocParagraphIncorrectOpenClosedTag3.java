1042402e60a306559e7a2f263389b8ec7d91ddaa
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-09-22 02:32:06 +0530

    Issue #15685: fixed JavadocParagraph to detect paragraph tag when they have their corresponding closing tag

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrectOpenClosedTag3.java
@@ -0,0 +1,42 @@
+/*
+JavadocParagraph
+violateExecutionOnNonTightHtml = (default)false
+allowNewlineParagraph = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparagraph;
+
+public class InputJavadocParagraphIncorrectOpenClosedTag3 {
+  /**
+   * Some Summary.
+   *
+   * <p><!-- comment is required --> extra space in paragraph after comment is violation</p>
+   */
+  // violation 2 lines above '<p> tag should be placed immediately before the first word'
+  int a;
+
+  /**
+   * Some Summary.
+   *
+   * <p> <!-- comment is required -->extra space in paragraph before comment is violation</p>
+   */
+  // violation 2 lines above '<p> tag should be placed immediately before the first word'
+  int b;
+
+  /**
+   * Some Summary.
+   *
+   * <p> <!-- comment is required --> both cases combined, this is also violation</p>
+   */
+  // violation 2 lines above '<p> tag should be placed immediately before the first word'
+  int c;
+
+  /**
+   * Some Summary.
+   *
+   * <p><!-- comment is required -->no extra spaces, this is okay</p>
+   */
+  int d;
+}
