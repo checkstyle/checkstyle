dedae08765c3b58eb4ee2e3d085096a05957ec91
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-09-22 02:32:06 +0530

    Issue #15685: fixed JavadocParagraph to detect paragraph tag when they have their corresponding closing tag

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphCheck1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphCheck1.java
@@ -23,6 +23,7 @@ public class InputJavadocParagraphCheck1 {
 // violation 4 lines above '<p> tag should be preceded with an empty line'
 class Check {
 
+   // violation 2 lines below 'Redundant <p> tag.'
    /**
     * <p>
     * Checks whether file contains code. Files which are considered to have no code:
@@ -31,4 +32,62 @@ class Check {
     */
    // violation 2 lines above '<p> tag should be preceded with an empty line.'
     void inheritDocWithThrows() {}
+
+    /**
+     * Some summary.
+     *
+     * <p>The following package declaration:</p>
+     * <pre>
+     * package com.puppycrawl.tools.checkstyle;
+     * </pre>
+     */
+    int iamtoolazyyyyyyy = 45;
+
+    // violation 4 lines below '<p> tag should not precede HTML block-tag '<ul>''
+    /**
+     * Some summary.
+     *
+     *<p>
+     *  <ul>
+     *    <p>
+     *      <li>1</li> should NOT give violation as there is not empty line before
+     *    </p>
+     *  </ul>
+     *</p>
+     */
+    public void foo() {}
+
+    // violation 5 lines below '<p> tag should be preceded with an empty line.'
+    /**
+     * Some summary.
+     *
+     * <b>
+     * <p>
+     *  <br/>
+     * </p>
+     * </b>
+     */
+    public void fooo() {}
+
+    // violation 4 lines below '<p> tag should not precede HTML block-tag '<table>''
+    /**
+     *  Some summary.
+     *
+     * <p>
+     *  <table>
+     *  </table>
+     * </p>
+     */
+    public void foooo() {}
+
+    // violation 4 lines below '<p> tag should not precede HTML block-tag '<pre>''
+    /**
+     * Some summary.
+     *
+     * <p>
+     *   <pre>testing...</pre>
+     *   <pre>testing...</pre>
+     * </p>
+     */
+    public void fooooo() {}
 }
