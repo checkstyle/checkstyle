c80a41ccedf6312677b6578b19091f815bb3d1e1
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-09-22 02:32:06 +0530

    Issue #15685: fixed JavadocParagraph to detect paragraph tag when they have their corresponding closing tag

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrectOpenClosedTag2.java
@@ -0,0 +1,94 @@
+/*
+JavadocParagraph
+violateExecutionOnNonTightHtml = (default)false
+allowNewlineParagraph = false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparagraph;
+
+/**
+ * Some summary.
+ *
+ * <p>  Some Javadoc. </p>
+ */
+// violation 2 lines above '<p> tag should be placed immediately before the first word'
+public class InputJavadocParagraphIncorrectOpenClosedTag2 {
+
+  /**
+   * some javadoc.
+   * <p></p>
+   */
+  // violation 2 lines above '<p> tag should be preceded with an empty line.'
+  int a;
+
+  // 2 violations 4 lines below:
+  //  '<p> tag should be placed immediately before the first word'
+  //  '<p> tag should be preceded with an empty line.'
+  /**
+   * Some Javadoc.<P>
+   * Some Javadoc.<P></P>
+   */
+  // violation 2 lines above '<p> tag should be preceded with an empty line.'
+  int b;
+
+  // violation below 'Redundant <p> tag.'
+  /**<p>some javadoc.</p>
+   *
+   * Some <p>Javadoc.</p>
+   *
+   * Some <p>Javadoc.</p>
+   */
+  // violation 5 lines above 'Empty line should be followed by <p> tag on the next line.'
+  // violation 4 lines above 'Empty line should be followed by <p> tag on the next line.'
+  int c;
+
+  /**
+   * Some Summary.
+   *
+   * <p>
+   *   Some Javadoc.
+   * </p>
+   */
+  // violation 4 lines above '<p> tag should be placed immediately before the first word'
+  int d;
+
+  // violation 4 lines below '<p> tag should be placed immediately before the first word'
+  /**
+   * Some Summary.
+   *
+   * <p>
+   *   <ul>
+   *     <li>Item 1</li>
+   *     <li>Item 2</li>
+   *     <li>Item 3</li>
+   *   </ul>
+   * </p>
+   */
+  int e;
+
+  /**
+   * Some Summary.
+   *
+   * <p><b>testing....</b></p>
+   *
+   * <p><h1>testing....</h1></p>
+   */
+  int f;
+
+  // violation 4 lines below '<p> tag should be placed immediately before the first word'
+  /**
+   * Some Summary.
+   *
+   * <p>
+   *     <b>testing....</b>
+   * </p>
+   *
+   * <p>
+   *     <h1>testing....</h1>
+   * </p>
+   */
+  // violation 4 lines above '<p> tag should be placed immediately before the first word'
+  int g;
+}
