730221ad329cb610124b687e2580cc5eb8ee1ce6
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-09-26 16:23:06 +0530

    Issue #15503: Added support for violation of p tag if it precedes a block tag

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect5.java
@@ -0,0 +1,89 @@
+/*
+JavadocParagraph
+violateExecutionOnNonTightHtml = (default)false
+allowNewlineParagraph = false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparagraph;
+
+public class InputJavadocParagraphIncorrect5 {
+    // violation 3 lines below 'tag should be preceded with an empty line.'
+    // violation 4 lines below 'tag should be preceded with an empty line.'
+    /**
+     * <h1><p>Testing....</h1>
+     *
+     * <h6><b><p>Test</b></h6>
+     *
+     * <table>
+     *  <p>
+     *    test
+     *
+     * </table>
+     */
+    // 2 violations 5 lines above:
+    //  '<p> tag should be placed immediately before the first word'
+    //  '<p> tag should be preceded with an empty line.'
+    void fooooo() {}
+
+    // violation 4 lines below 'tag should be preceded with an empty line.'
+    // violation 7 lines below 'tag should be preceded with an empty line.'
+    // violation 12 lines below 'tag should be preceded with an empty line.'
+    /**
+     * <b><p>testtttt.....</b>
+     *
+     * <b>
+     *
+     * <p>testtttt.....
+     *
+     * </b>
+     *
+     * <h1>
+     *
+     * <p>testtttt.....
+     *
+     * </h1>
+     *
+     * <p> test...
+     *
+     * <h1></h1>
+     *
+     * <p>
+     *
+     * <h1></h1>
+     */
+    // violation 8 lines above '<p> tag should be placed immediately before the first word'
+    // 2 violations 5 lines above:
+    //  '<p> tag should be placed immediately before the first word'
+    //  '<p> tag should not precede HTML block-tag '<h1>''
+    void foooooo() {}
+
+    /**
+     * <ul>
+     *  <li>1</li>
+     *  <li>1</li>
+     *  <ul>
+     *    <p>test
+     *  </ul>
+     * </ul>
+     */
+    // violation 4 lines above 'tag should be preceded with an empty line.'
+    void fooooooo() {}
+
+    // 2 violations 6 lines below:
+    //  '<p> tag should be placed immediately before the first word'
+    //  '<p> tag should not precede HTML block-tag '<ul>''
+    /**
+     * Some Summary.
+     *
+     * <p>
+     *  <!-- THIS COMMENT WILL GET IGNORED -->
+     *  <ul>
+     *      <li>Item 1</li>
+     *      <li>Item 2</li>
+     *      <li>Item 3</li>
+     *  </ul>
+     */
+    private static final String NAME = "Heisenberg";
+}
