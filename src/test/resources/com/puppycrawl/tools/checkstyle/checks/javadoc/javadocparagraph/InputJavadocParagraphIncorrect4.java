bcde4e1a201a94f013e7050f6d01c40d6301060a
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-09-26 16:23:06 +0530

    Issue #15503: Added support for violation of p tag if it precedes a block tag

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect4.java
@@ -0,0 +1,60 @@
+/*
+JavadocParagraph
+violateExecutionOnNonTightHtml = (default)false
+allowNewlineParagraph = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparagraph;
+
+/**
+ * Some summary.
+ *
+ * <p><h1>Testing...</h1>
+ */
+// violation 2 lines above '<p> tag should not precede HTML block-tag '<h1>''
+public class InputJavadocParagraphIncorrect4 {
+    // violation 4 lines below '<p> tag should not precede HTML block-tag '<ul>''
+    /**
+     * Some summary.
+     *
+     *<p>
+     *  <ul>
+     *    <p>
+     *      <li>1</li> // should NOT give violation as there is not empty line before
+     *  </ul>
+     *
+     *
+     * <p><b>testing</b> // ok, inline HTML tag. Not a block-level tag
+     */
+    // violation 7 lines above 'tag should be preceded with an empty line.'
+    public void foo() {}
+
+    // violation 4 lines below '<p> tag should not precede HTML block-tag '<table>''
+    /**
+     *  Some summary.
+     *
+     * <p>
+     *  <table>
+     *  </table>
+     *
+     * <p>This is allowed<h1>Testing....</h1>
+     */
+    public void fooo() {}
+
+    // violation 4 lines below '<p> tag should be placed immediately before the first word'
+    /**
+     * <h1>Testing....</h1>
+     *
+     * <p>     Test<h1>test</h1>
+     *
+     * <p>    <ol>test</ol>
+     *
+     * <p><b><h1>Nesting....</h1></b>
+     */
+    // 2 violations 4 lines above:
+    // '<p> tag should be placed immediately before the first word'
+    // '<p> tag should not precede HTML block-tag '<ol>''
+    void foooo() {}
+}
