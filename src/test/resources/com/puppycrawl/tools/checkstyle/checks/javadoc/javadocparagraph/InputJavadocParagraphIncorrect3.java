5b9c6b76736d9b78bae2735cc4846b00be414f38
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-09-28 21:53:30 +0530

    Issue #15713: updated JavadocParagraph's input files based on issue description

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparagraph/InputJavadocParagraphIncorrect3.java
@@ -0,0 +1,52 @@
+/*
+JavadocParagraph
+violateExecutionOnNonTightHtml = (default)false
+allowNewlineParagraph = false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocparagraph;
+
+public class InputJavadocParagraphIncorrect3 {
+    InputJavadocParagraphIncorrect3 anon = new InputJavadocParagraphIncorrect3() {
+
+        // violation 2 lines below 'Redundant <p> tag.'
+        /**
+         * <p>Some Summary.
+         *
+         * Some Paragraph.
+         *
+         * @since 8.0
+         */
+        // violation 5 lines above 'Empty line should be followed by <p> tag on the next line.'
+        public static final byte NUL = 0;
+
+        // 2 violations 5 lines below:
+        //  '<p> tag should be placed immediately before the first word'
+        //  '<p> tag should be preceded with an empty line.'
+        // violation 4 lines below '<p> tag should be placed immediately before the first word'
+        /**
+         *   Some Summary.<p>
+         *
+         *  <p>  Some Paragraph.
+         *
+         * @see <a href="example.com">
+         *     Documentation about <p> GWT emulated source</a>
+         */
+        // 2 violations 2 lines above:
+        //  '<p> tag should be placed immediately before the first word'
+        //  '<p> tag should be preceded with an empty line.'
+        boolean emulated() {return false;}
+
+        // violation 3 lines below 'Empty line should be followed by <p> tag on the next line.'
+        /**
+         * Double newline.
+         *
+         *
+         * Some Paragraph.
+         */
+        // violation 3 lines above 'Empty line should be followed by <p> tag on the next line.'
+        void doubleNewline() {}
+    };
+}
