4eec52651dd46edfb43a29bc788e4ad3f13a4298
Chris <chris-carneiro@users.noreply.github.com>
2024-09-29 08:30:32 +0200

    Issue #6207: add class data abstraction coupling xpath regression tests

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/classdataabstractioncoupling/InputXpathClassDataAbstractionCouplingInterface.java
@@ -0,0 +1,33 @@
+package org.checkstyle.suppressionxpathfilter.classdataabstractioncoupling;
+
+import java.io.CharArrayWriter;
+import java.io.File;
+import java.io.StringWriter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+
+public interface InputXpathClassDataAbstractionCouplingInterface { // warn
+    default Thread create() {
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                CharArrayWriter charArrayWriter = new CharArrayWriter();
+                StringWriter stringWriter = new StringWriter();
+            }
+        });
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            BigInteger bigInteger = new BigInteger("2");
+            MathContext mathContext = new MathContext(2);
+        }
+    };
+
+    BigDecimal bigDecimal = new BigDecimal("0");
+    File file = new File("path");
+}
+
