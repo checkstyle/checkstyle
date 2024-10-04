843bbb754d5d38dc1974a23f63df3bae3c878c6d
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2024-10-04 20:16:47 +0100

    Issue #13345: Enabled UnnecessarySemicolonInTryWithResourcesCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/unnecessarysemicolonintrywithresources/Example1.java
@@ -0,0 +1,27 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="UnnecessarySemicolonInTryWithResources"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.unnecessarysemicolonintrywithresources;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.Reader;
+
+// xdoc section -- start
+class Example1 {
+  void method() throws IOException {
+    try (Reader r1 = new PipedReader();) {} // violation, 'Unnecessary semicolon'
+    try (Reader r4 = new PipedReader(); Reader r5 = new PipedReader()
+         ;) {} // violation, 'Unnecessary semicolon'
+    try (Reader r6 = new PipedReader();
+         Reader r7
+           = new PipedReader(); // ok, closing parenthesis is not on the same line
+    ) {}
+  }
+}
+// xdoc section -- end
