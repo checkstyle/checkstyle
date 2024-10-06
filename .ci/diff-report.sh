341f712ab0565c352cd5eb7e19aa59699c0c3dbd
piyush kumar sadangi <piyushkumarsadangi@MacBook-Air.local>
2024-10-07 01:53:41 +0530

    Issue #15743: report-regresson.yml should use yml files for list of projects

--- .ci/diff-report.sh
+++ .ci/diff-report.sh
@@ -39,14 +39,16 @@ download-files)
   mkdir -p .ci-temp
   echo "Downloading files..."
 
-  # check for projects link from PR, if not found use default from contribution repo
+  # Check for projects link from PR, if not found use default from contribution repo
   LINK="${DIFF_PROJECTS_LINK:-$DEFAULT_PROJECTS_LINK}"
 
-  # get projects file
+  # Get projects file using the original filename
+  PROJECTS_FILENAME=$(basename "$LINK")
+
   curl --fail-with-body -X GET "${LINK}" \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: token $GITHUB_TOKEN" \
-    -o .ci-temp/project.properties
+    -o ".ci-temp/${PROJECTS_FILENAME}"
 
   if [ -n "$NEW_MODULE_CONFIG_LINK" ]; then
     curl --fail-with-body -X GET "${NEW_MODULE_CONFIG_LINK}" \
