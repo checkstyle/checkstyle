8e8df5866b352a34f128983f72d2ccf7539ad8f2
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-10-30 01:44:36 +0530

    Issue #13345: Enable examples tests for CovariantEqualsCheck

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/covariantequals/Example1.java
@@ -0,0 +1,23 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="CovariantEquals"/>
+  </module>
+</module>
+*/
+//non-compiled with javac: Compilable with Java17
+package com.puppycrawl.tools.checkstyle.checks.coding.covariantequals;
+
+// xdoc section -- start
+public class Example1 {
+  public boolean equals(Example1 same) {  // violation
+    return false;
+  }
+
+  record Test(String str) {
+    public boolean equals(Test same) {  // violation
+      return false;
+    }
+  }
+}
+// xdoc section -- end
