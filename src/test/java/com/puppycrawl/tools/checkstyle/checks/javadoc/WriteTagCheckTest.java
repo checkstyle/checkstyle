72d0a9b309b39535ca7ad52d3ddb1754dbfffa92
Mohanad <mohanadkhaled87@gmail.com>
2024-11-02 01:57:12 +0200

    Issue #11584: WriteTag reports violation with confusing message when there is no javadoc

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/WriteTagCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/WriteTagCheckTest.java
@@ -25,19 +25,31 @@ import static com.puppycrawl.tools.checkstyle.checks.javadoc.WriteTagCheck.MSG_T
 import static com.puppycrawl.tools.checkstyle.checks.javadoc.WriteTagCheck.MSG_WRITE_TAG;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
 
+import com.puppycrawl.tools.checkstyle.AbstractAutomaticBean;
 import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
 import com.puppycrawl.tools.checkstyle.Checker;
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.DefaultLogger;
+import com.puppycrawl.tools.checkstyle.PackageObjectFactory;
+import com.puppycrawl.tools.checkstyle.TreeWalker;
+import com.puppycrawl.tools.checkstyle.internal.utils.TestUtil;
 import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+import de.thetaphi.forbiddenapis.SuppressForbidden;
 
 /**
  * Unit test for WriteTagCheck.
@@ -130,6 +142,83 @@ public class WriteTagCheckTest extends AbstractModuleTestSupport {
                 getPath("InputWriteTagSeverity.java"), expected);
     }
 
+    /**
+     * Reason for low level testing:
+     * There is no direct way to fetch severity level directly.
+     * This is an exceptional case in which the logs are fetched indirectly using default
+     * logger listener in order to check for the severity level being reset by logging twice.
+     * First log should be the tag's severity level then it should reset the severity level back to
+     * error which is then checked upon using the log's severity level.
+     * This test needs to use a forbidden api {@code ByteArrayOutputStream#toString()}
+     * to get the logs as a string from the output stream
+     */
+    @Test
+    @SuppressForbidden
+    public void testResetSeverityLevel() throws Exception {
+
+        final Checker checker = new Checker();
+
+        final TreeWalker treeWalker = new TreeWalker();
+        final PackageObjectFactory factory = new PackageObjectFactory(
+                new HashSet<>(), Thread.currentThread().getContextClassLoader());
+
+        treeWalker.setModuleFactory(factory);
+        treeWalker.finishLocalSetup();
+
+        final DefaultConfiguration writeTagConfig = createModuleConfig(WriteTagCheck.class);
+        writeTagConfig.addProperty("tag", "@author");
+        writeTagConfig.addProperty("tagFormat", "Mohanad");
+        writeTagConfig.addProperty("tagSeverity", "warning");
+
+        treeWalker.setupChild(writeTagConfig);
+
+        checker.addFileSetCheck(treeWalker);
+
+        final ByteArrayOutputStream out = TestUtil.getInternalState(this, "stream");
+        final DefaultLogger logger = new DefaultLogger(out,
+                AbstractAutomaticBean.OutputStreamOptions.CLOSE);
+        checker.addListener(logger);
+
+        execute(checker, getPath("InputWriteTagResetSeverity.java"));
+
+        final String output = out.toString();
+
+        // logs severity levels are between square brackets []
+        final Pattern severityPattern = Pattern.compile("\\[(ERROR|WARN|INFO|IGNORE)]");
+
+        final Matcher matcher = severityPattern.matcher(output);
+
+        // First log is just the normal tag one
+        final boolean firstMatchFound = matcher.find();
+        assertWithMessage("Severity level should be wrapped in a square bracket []")
+                .that(firstMatchFound)
+                .isTrue();
+
+        final String tagExpectedSeverityLevel = "warn";
+        final String firstSeverityLevel = matcher.group(1).toLowerCase(Locale.ENGLISH);
+
+        assertWithMessage("First log should have an error severity level")
+                .that(firstSeverityLevel)
+                .isEqualTo(tagExpectedSeverityLevel);
+
+        // Now we check for the second log which should log error  if
+        // the previous log did not have an issue while resetting the original severity level
+        final boolean secondMatchFound = matcher.find();
+        assertWithMessage("Severity level should be wrapped in a square bracket []")
+                .that(secondMatchFound)
+                .isTrue();
+
+        final String expectedSeverityLevelAfterReset = "error";
+
+        final String secondSeverityLevel = matcher.group(1).toLowerCase(Locale.ENGLISH);
+
+        assertWithMessage("Second violation's severity level"
+                + " should have been reset back to default (error)")
+                .that(secondSeverityLevel)
+                .isEqualTo(expectedSeverityLevelAfterReset);
+
+    }
+
     @Test
     public void testIgnoreMissing() throws Exception {
         final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
@@ -170,27 +259,21 @@ public class WriteTagCheckTest extends AbstractModuleTestSupport {
 
     @Test
     public void testNoJavadocs() throws Exception {
-        final String[] expected = {
-            "13: " + getCheckMessage(MSG_MISSING_TAG, "null"),
-        };
+        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+
         verifyWithInlineConfigParserTwice(getPath("InputWriteTagNoJavadoc.java"), expected);
     }
 
     @Test
     public void testWriteTagRecordsAndCompactCtors() throws Exception {
         final String[] expected = {
-            "15: " + getCheckMessage(MSG_MISSING_TAG, "@incomplete"),
             "19: " + getCheckMessage(MSG_TAG_FORMAT, "@incomplete", "\\S"),
             "26: " + getCheckMessage(MSG_WRITE_TAG, "@incomplete",
                     "Failed to recognize 'record' introduced in Java 14."),
-            "33: " + getCheckMessage(MSG_MISSING_TAG, "@incomplete"),
             "37: " + getCheckMessage(MSG_WRITE_TAG, "@incomplete",
                     "Failed to recognize 'record' introduced in Java 14."),
-            "44: " + getCheckMessage(MSG_MISSING_TAG, "@incomplete"),
             "48: " + getCheckMessage(MSG_WRITE_TAG, "@incomplete",
                     "Failed to recognize 'record' introduced in Java 14."),
-            "56: " + getCheckMessage(MSG_MISSING_TAG, "@incomplete"),
-            "58: " + getCheckMessage(MSG_MISSING_TAG, "@incomplete"),
             "62: " + getCheckMessage(MSG_WRITE_TAG, "@incomplete",
                     "Failed to recognize 'record' introduced in Java 14."),
         };
