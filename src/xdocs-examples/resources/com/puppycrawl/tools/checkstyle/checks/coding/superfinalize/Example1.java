0469c1ab4eeb5f8f18177794f8f7ff003dfdd85e
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-05 01:05:40 +0530

    Issue #13345: Enable examples tests for SuperFinalizeCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/superfinalize/Example1.java
@@ -0,0 +1,21 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="SuperFinalize"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.superfinalize;
+
+// xdoc section -- start
+class Example1 {
+  protected void finalize() throws Throwable {
+    super.finalize(); // OK, calls super.finalize()
+  }
+}
+class InvalidExample {
+  // violation below, 'Method 'finalize' should call 'super.finalize''
+  protected void finalize() throws Throwable {
+  }
+}
+// xdoc section -- end
