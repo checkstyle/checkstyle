64a74a37ef6c051547d9482fca4cf29d096226f8
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-07 02:16:47 +0530

    Issue #13345: Enable examples tests for IllegalCatchCheck

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalCatchCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalCatchCheckExamplesTest.java
@@ -19,12 +19,10 @@
 
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
 
-@Disabled("until https://github.com/checkstyle/checkstyle/issues/13345")
 public class IllegalCatchCheckExamplesTest extends AbstractExamplesModuleTestSupport {
     @Override
     protected String getPackageLocation() {
@@ -34,18 +32,22 @@ public class IllegalCatchCheckExamplesTest extends AbstractExamplesModuleTestSup
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-
+            "15:7: " + getCheckMessage(IllegalCatchCheck.MSG_KEY, "Exception"),
+            "24:7: " + getCheckMessage(IllegalCatchCheck.MSG_KEY, "Exception"),
         };
 
-        verifyWithInlineConfigParser(getPath("Example1.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
     }
 
     @Test
     public void testExample2() throws Exception {
         final String[] expected = {
-
+            "25:7: " + getCheckMessage(IllegalCatchCheck.MSG_KEY, "ArithmeticException"),
+            "37:7: " + getCheckMessage(IllegalCatchCheck.MSG_KEY, "OutOfMemoryError"),
+            "45:7: " + getCheckMessage(IllegalCatchCheck.MSG_KEY, "ArithmeticException"),
+            "53:7: " + getCheckMessage(IllegalCatchCheck.MSG_KEY, "OutOfMemoryError"),
         };
 
-        verifyWithInlineConfigParser(getPath("Example2.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example2.java"), expected);
     }
 }
