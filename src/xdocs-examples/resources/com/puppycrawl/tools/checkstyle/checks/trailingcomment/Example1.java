a8268cbee57623137f30ff719f93b8ce21aec8c7
Devarsh Mavani <devarsh@appleute.de>
2024-11-06 20:19:00 +0530

    Issue #13345: Enabled example tests for TrailingCommentCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/trailingcomment/Example1.java
@@ -0,0 +1,27 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="TrailingComment"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.trailingcomment;
+
+// xdoc section -- start
+public class Example1 {
+  public static void main(String[] args) {
+    int x = 10;
+    // OK
+    if (/* OK */ x > 5) {}
+    int a = 5; // violation
+    doSomething(
+            "param1"
+    ); // OK, by default such trailing of method/code-block ending is allowed
+
+  }
+
+  private static void doSomething(String param) {
+  }
+}
+// xdoc section -- end
