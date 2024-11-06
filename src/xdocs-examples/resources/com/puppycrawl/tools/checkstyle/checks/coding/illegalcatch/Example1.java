b6cd923e67bfcff72d09fb97e74169894fc501d1
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-07 02:16:47 +0530

    Issue #13345: Enable examples tests for IllegalCatchCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalcatch/Example1.java
@@ -0,0 +1,52 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="IllegalCatch"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.illegalcatch;
+
+// xdoc section -- start
+class Example1 {
+  void exampleMethod1() {
+    try {
+      // some code here
+    } catch (Exception e) {
+      // violation above, 'Catching 'Exception' is not allowed'
+    }
+  }
+
+  void exampleMethod2() {
+    try {
+      // some code here
+    } catch (ArithmeticException e) {
+    } catch (Exception e) {
+      // violation above, 'Catching 'Exception' is not allowed'
+      //order of catch blocks doesn't matter
+    }
+  }
+
+  void exampleMethod3() {
+    try {
+      // some code here
+    } catch (NullPointerException e) {
+    } catch (OutOfMemoryError e) {
+    }
+  }
+
+  void exampleMethod4() {
+    try {
+      // some code here
+    } catch (ArithmeticException | NullPointerException e) {
+    }
+  }
+
+  void exampleMethod5() {
+    try {
+      // some code here
+    } catch (OutOfMemoryError e) {
+    }
+  }
+}
+// xdoc section -- end
