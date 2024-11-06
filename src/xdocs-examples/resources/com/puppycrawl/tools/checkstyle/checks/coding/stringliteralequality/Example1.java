301521610d7cec2f7a756b974dac247afb3a8643
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-06 22:38:52 +0530

    Issue #13345: Enable examples tests for StringLiteralEqualityCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/stringliteralequality/Example1.java
@@ -0,0 +1,29 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="StringLiteralEquality"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.stringliteralequality;
+
+// xdoc section -- start
+class Example1 {
+  String getName(){
+    return "Y";
+  }
+  void InvalidExample(){
+    String status = "pending";
+    // violation below, 'Literal Strings should be compared using equals(), not '==''
+    if (status == "done") {}
+    // violation below, 'Literal Strings should be compared using equals(), not '!=''
+    while (status != "done") {}
+    // violation below, 'Literal Strings should be compared using equals(), not '==''
+    boolean flag = (status == "done");
+    boolean flag1 = (status.equals("done"));
+    String name = "X";
+    if (name == getName()) {}
+    // OK, limitation that check cannot tell runtime type returned from method call
+  }
+}
+// xdoc section -- end
