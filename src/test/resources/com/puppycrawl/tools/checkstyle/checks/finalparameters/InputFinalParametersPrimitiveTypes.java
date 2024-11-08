d3437f8f7695abf5fa417e9b192d358cfe4b3fbd
Kushagra-sedwal <kushagrasedwal28@gmail.com>
2024-11-08 16:21:45 +0530

    Issue #15456: Specify violation messages for FinalParametersCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/finalparameters/InputFinalParametersPrimitiveTypes.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/finalparameters/InputFinalParametersPrimitiveTypes.java
@@ -11,9 +11,15 @@ package com.puppycrawl.tools.checkstyle.checks.finalparameters;
 public class InputFinalParametersPrimitiveTypes
 {
     void foo(int i) {} //no warning
-    void foo1(int i, String k, float s) {}  // violation
-    void foo2(String s, Object o, long l) {}  // 2 violations
-    void foo3(int[] array) {}  // violation
-    void foo4(int i, float x, int[] s) {}  // violation
-    void foo5(int x, long[] l, String s) {}  // 2 violations
+    void foo1(int i, String k, float s) {}  // violation, 'k' should be final
+    void foo2(String s, Object o, long l) {}
+    // 2 violations above:
+    //    'Parameter s should be final.'
+    //    'Parameter o should be final.'
+    void foo3(int[] array) {}  // violation, 'array' should be final
+    void foo4(int i, float x, int[] s) {}  // violation, 's' should be final
+    void foo5(int x, long[] l, String s) {}
+    // 2 violations above:
+    //    'Parameter l should be final.'
+    //    'Parameter s should be final.'
 }
