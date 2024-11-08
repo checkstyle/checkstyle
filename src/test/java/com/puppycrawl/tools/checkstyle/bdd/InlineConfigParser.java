9643e38b5097d27f0061ef4c6106643eff4cbfd5
Kushagra-sedwal <kushagrasedwal28@gmail.com>
2024-11-08 11:08:29 +0530

    Issue #15456: Specify violation messages for IllegalCatch

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -215,7 +215,6 @@ public final class InlineConfigParser {
             "com.puppycrawl.tools.checkstyle.checks.coding.ExplicitInitializationCheck",
             "com.puppycrawl.tools.checkstyle.checks.coding.FinalLocalVariableCheck",
             "com.puppycrawl.tools.checkstyle.checks.coding.HiddenFieldCheck",
-            "com.puppycrawl.tools.checkstyle.checks.coding.IllegalCatchCheck",
             "com.puppycrawl.tools.checkstyle.checks.coding.IllegalInstantiationCheck",
             "com.puppycrawl.tools.checkstyle.checks.coding.IllegalThrowsCheck",
             "com.puppycrawl.tools.checkstyle.checks.coding.IllegalTokenCheck",
