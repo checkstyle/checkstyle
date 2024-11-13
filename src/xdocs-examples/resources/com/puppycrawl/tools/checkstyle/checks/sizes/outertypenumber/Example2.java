7dd457ce3fc23c8b256aed787138aeac2040f900
Kushagra-sedwal <kushagrasedwal28@gmail.com>
2024-11-14 00:16:28 +0530

    Issue #15905: Java code for OuterTypesNumber

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/sizes/outertypenumber/Example2.java
@@ -0,0 +1,23 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="OuterTypeNumber">
+      <property name="max" value="3"/>
+    </module>
+  </module>
+</module>
+*/
+
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.sizes.outertypenumber;
+
+public class Example2 {
+}
+
+class Examples {
+    void exampleMethod() {}
+}
+
+enum Outertype1 {
+}
+// xdoc section -- end
