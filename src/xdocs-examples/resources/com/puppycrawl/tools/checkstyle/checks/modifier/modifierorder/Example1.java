c273359c5a0d640937eb81df3db1a373728e31f8
Kushagra-Sedwal <143800287+Kushagra-Sedwal@users.noreply.github.com>
2024-11-15 01:53:31 +0530

    Issue #13345: Enable examples tests for ModifierOrderCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/modifier/modifierorder/Example1.java
@@ -0,0 +1,20 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="ModifierOrder"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.modifier.modifierorder;
+
+// xdoc section -- start
+public class Example1 {
+  public static final int MAX_VALUE = 100;
+
+  // violation below "'private' modifier out of order with the JLS suggestions"
+  final private String example = "Example";
+
+  //violation below, 'annotation modifier does not precede non-annotation modifiers'
+  public @Deprecated class Example {}
+}
+// xdoc section -- end
