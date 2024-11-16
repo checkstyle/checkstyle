73e5711654705169093ce087793d8904a1f2cda1
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-16 23:24:37 +0530

    Issue #13345: Enable examples tests for NestedTryDepthCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/nestedtrydepth/Example2.java
@@ -0,0 +1,39 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="NestedTryDepth">
+      <property name="max" value="3"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.nestedtrydepth;
+
+// xdoc section -- start
+public class Example2 {
+  void testMethod() {
+    try {
+      try { // OK, current depth is 1, max allowed depth is 3
+      } catch (Exception e) {}
+    } catch (Exception e) {}
+
+    try {
+      try {
+        try { // OK, current depth is 2, max allowed depth is also 3
+        } catch (Exception e) {}
+      } catch (Exception e) {}
+    } catch (Exception e) {}
+
+    try {
+      try {
+        try { // OK, current depth is 2, max allowed depth is 3
+          try { // OK, current depth is 3, max allowed depth is 3
+            try { // violation, current depth is 4, max allowed depth is 3
+            } catch (Exception e) {}
+          } catch (Exception e) {}
+        } catch (Exception e) {}
+      } catch (Exception e) {}
+    } catch (Exception e) {}
+  }
+}
+// xdoc section -- end
