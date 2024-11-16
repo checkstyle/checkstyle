67c21ed9099a81c0c1c120628609d06615d65e07
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-16 18:21:02 +0530

    Issue #13345: Enable examples tests for MissingSwitchDefaultCheck

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/missingswitchdefault/Example2.java
@@ -0,0 +1,36 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MissingSwitchDefault"/>
+  </module>
+</module>
+*/
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.coding.missingswitchdefault;
+
+// xdoc section -- start
+class Example2 {
+  Example2(int i){
+    switch (i) {
+      case 1:
+        break;
+      case 2:
+        break;
+      default:
+        break;
+    }
+    Object obj = "example";
+    switch (obj) {
+      case String s: // type pattern
+        System.out.println(s);
+        break;
+      case Integer j: // type pattern
+        System.out.println("Integer");
+        break;
+      default:    // will not compile without default label
+        System.out.println("Unknown type");
+        break;
+    }
+  }
+}
+// xdoc section -- end
