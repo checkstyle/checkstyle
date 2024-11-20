25c71b67da5e8516130eae2ceeb937f75f832c8c
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-21 00:03:17 +0530

    Issue #13345: Enable examples tests for WriteTagCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/Example3.java
@@ -0,0 +1,32 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="WriteTag">
+      <property name="tokens"
+                value="INTERFACE_DEF, CLASS_DEF, ENUM_DEF,
+                ANNOTATION_DEF, RECORD_DEF, METHOD_DEF" />
+      <property name="tag" value="@since"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.writetag;
+
+// xdoc section -- start
+/**
+ * Some class
+ */
+public class Example3 {
+  // violation 1 lines above 'Type Javadoc comment is missing @since tag.'
+  // violation 3 lines below 'Javadoc tag @since='
+  /**
+   * some doc
+   * @since
+   */
+  void testMethod1() {}
+
+  /** some doc */
+  public void testMethod2() {}
+
+}
+// xdoc section -- end
