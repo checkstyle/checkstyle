bba423982b80fdd8fc8a41fe0e527d5f56500e1e
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-21 23:30:46 +0530

    Issue #13345: Enable examples tests for JavadocTagContinuationIndentationCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoctagcontinuationindentation/Example3.java
@@ -0,0 +1,35 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="JavadocTagContinuationIndentation">
+      <property name="violateExecutionOnNonTightHtml" value="true"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoctagcontinuationindentation;
+
+// xdoc section -- start
+/**
+ * <p> 'p' tag is unclosed
+ * <p> 'p' tag is closed</p>
+ */
+class Example3 {
+  // violation 4 lines above 'Unclosed HTML tag found: p'
+  /**
+   * @tag comment
+   *     Indentation spacing is 4
+   */
+  public void testMethod1(String input) {
+
+  }
+
+  /**
+   * @tag comment
+   *  Indentation spacing is 1
+   */
+  public void testMethod2(String input) {
+    // violation 3 lines above 'Line continuation have incorrect indentation level'
+  }
+}
+// xdoc section -- end
