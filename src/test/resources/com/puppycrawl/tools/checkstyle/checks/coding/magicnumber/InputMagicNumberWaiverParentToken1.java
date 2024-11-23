344621ae03b4049b4d3685a380c7e0572979918d
mahfouz72 <mohamadmahfouz39@gmail.com>
2024-11-23 02:48:18 +0200

    Issue #15985: Expand Default Value of constantWaiverParentToken in MagicNumberCheck with All Operators

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumberWaiverParentToken1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumberWaiverParentToken1.java
@@ -5,8 +5,10 @@ ignoreHashCodeMethod = (default)false
 ignoreAnnotation = (default)false
 ignoreFieldDeclaration = (default)false
 ignoreAnnotationElementDefaults = (default)true
-constantWaiverParentToken = ASSIGN, ARRAY_INIT, EXPR, UNARY_PLUS, UNARY_MINUS, TYPECAST, \
-                            ELIST, STAR, DIV, PLUS, MINUS
+constantWaiverParentToken = (default)TYPECAST, METHOD_CALL, EXPR, ARRAY_INIT, UNARY_MINUS, \
+                            UNARY_PLUS, ELIST, STAR, ASSIGN, PLUS, MINUS, DIV, LITERAL_NEW, \
+                            SR, BSR, SL, BXOR, BOR, BAND, BNOT, QUESTION, COLON, EQUAL, NOT_EQUAL, \
+                            GE, GT, LE, LT, MOD
 tokens = (default)NUM_DOUBLE, NUM_FLOAT, NUM_INT, NUM_LONG
 
 
