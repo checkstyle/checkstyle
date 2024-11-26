d96138591c0c5fa1a23e191ca2db84df682a8130
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-11-26 15:06:02 +0530

    Issue #13345: Enable examples tests for RegexpMultilineCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexpmultiline/Example4.java
@@ -0,0 +1,45 @@
+/*xml
+<module name="Checker">
+  <module name="RegexpMultiline">
+    <property name="format" value="Test #[0-9]+:[A-Za-z ]+"/>
+    <property name="ignoreCase" value="true"/>
+    <property name="maximum" value="3"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.regexp.regexpmultiline;
+
+// xdoc section -- start
+class Example4 {
+  void testMethod1() {
+
+    System.out.print("Example");
+
+    System.err.println("Example");
+    System
+    .out.print("Example");
+    System
+    .err.println("Example");
+    System.
+    out.print("Example");
+    System.
+    err.println("Example");
+  }
+
+  void testMethod2() {
+
+    System.out.println("Test #1: this is a test string");
+
+    System.out.println("TeSt #2: This is a test string");
+
+    System.out.println("TEST #3: This is a test string");
+    int i = 5;
+
+    System.out.println("Value of i: " + i);
+    // violation below, 'Line matches the illegal pattern'
+    System.out.println("Test #4: This is a test string");
+    // violation below, 'Line matches the illegal pattern'
+    System.out.println("TEst #5: This is a test string");
+  }
+}
+// xdoc section -- end
