599a270740ca192d86c345fad309c6d5e2a769af
Ivan Babushkin <babushkinnavi.ib@gmail.com>
2024-12-01 22:25:27 +0300

    Issue #13345: Enable examples tests for IllegalImportCheckExamplesTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/illegalimport/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/illegalimport/Example3.java
@@ -2,20 +2,23 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="IllegalImport">
-      <property name="regexp" value="true"/>
       <property name="illegalClasses"
-        value="^java\.util\.(List|Arrays), ^java\.sql\.Connection"/>
+        value="java.util.Date, java.sql.Connection"/>
     </module>
   </module>
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.checks.imports.illegalimport;
+
 // xdoc section -- start
 import java.io.*;
 import java.lang.ArithmeticException;
+import java.sql.Connection; // violation
+import java.util.List;
 import java.util.Enumeration;
-import java.util.Date;
-import sun.applet.*;
+import java.util.Arrays;
+import java.util.Date;      // violation
 
-public class InputIllegalImport { }
+public class Example3 {}
 // xdoc section -- end
