f4c693806406ca99c4dddf4c7712ddbaa581c34e
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-12-03 00:00:25 +0530

    Issue #13345: Enable examples tests for MissingjavadocMethodCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/missingjavadocmethod/Example1.java
@@ -0,0 +1,28 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MissingJavadocMethod"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.missingjavadocmethod;
+
+// xdoc section -- start
+public class Example1 {
+  public Example1() {} // violation, missing javadoc for method
+  public void testMethod1() {} // violation, missing javadoc for method
+  /**
+   * Some description here.
+   */
+  public void testMethod2() {}
+
+  @Override
+  public String toString() {
+    return "Some string";
+  }
+
+  private void testMethod3() {}
+  protected void testMethod4() {}
+  void testMethod5() {}
+}
+// xdoc section -- end
