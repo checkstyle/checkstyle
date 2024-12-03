1f6c324bcc70f9d21a76196c801ef039a398a5f5
Wessam100 <125602282+Wessam100@users.noreply.github.com>
2024-12-03 15:03:12 +0200

    Update InputHiddenFieldLambdas

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/hiddenfield/InputHiddenFieldLambdas4.java
@@ -0,0 +1,45 @@
+package com.puppycrawl.tools.checkstyle.checks.coding.hiddenfield;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.puppycrawl.tools.checkstyle.checks.coding.hiddenfield.InputHiddenFieldLambdas3.FunctionWithOneParameter;
+
+public class InputHiddenFieldLambdas4 {
+    
+    /**
+     * Example 14: lambda typed with complex generics
+     */
+    List<Double> justSomeList;
+    Map<String, Object> justSomeMap;
+    {
+        FunctionWithComplexGenerics<List<Double>, Map<String, Object>> someWierdFunc =
+            (List<Double> justSomeList, Map<String, Object> justSomeMap) -> { // 2 violations
+                String.valueOf(justSomeList);
+                String.valueOf(justSomeMap);
+                return new HashMap<>();
+            };
+    }
+
+    /**
+     * Example 15: lambda stored in field (with typed parameter)
+     * hides other field
+     */
+    Object someObject = new Object();
+    FunctionWithOneParameter objectToString = (Object someObject) -> { // violation
+        return someObject.toString();
+    };
+
+    /**
+     * Example 16: lambda stored in field (with untyped parameter)
+     * hides other field
+     */
+    FunctionWithOneParameter otherObjectToString = someObject -> { // violation
+        return someObject.toString();
+    };
+
+    private final String l = "";
+    private interface NestedInterface {
+        void print(String l);
+    }
+}
