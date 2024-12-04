95f0ac6fe1c84fdc6261bd0e619e52ff17c67c20
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-12-04 19:54:18 +0530

    Issue #13345: Enable examples tests for JavadocMethodCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/Example6.java
@@ -0,0 +1,33 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="JavadocMethod">
+        <property name="tokens" value="CTOR_DEF"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocmethod;
+
+// xdoc section -- start
+public class Example6 {
+  /** */
+  Example6(int x) {}
+  // violation above, 'Expected @param tag for 'x''
+  /** */
+  public int testMethod1(int p1) {
+
+    return p1;
+  }
+  /**
+   * @param p1 The first number
+   */
+  @Deprecated
+  private int testMethod2(int p1) {
+    return p1;
+  }
+  /** */
+  void testMethod3(int p1) {}
+
+}
+// xdoc section -- end
