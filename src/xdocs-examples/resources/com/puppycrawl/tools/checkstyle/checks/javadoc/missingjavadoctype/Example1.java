e3a66c71634453b5bf40dfef8a25f4a25c18ca3a
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-12-22 00:14:38 +0530

    Issue #13345: Enable examples tests for MissingjavadocTypeCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/missingjavadoctype/Example1.java
@@ -0,0 +1,17 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MissingJavadocType"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.missingjavadoctype;
+
+// xdoc section -- start
+class Example1 {
+  public class testClass1 {}
+  private class testClass2 {}
+  protected class testClass3 {}
+  class testClass4 {}
+}
+// xdoc section -- end
