a8f7a0dbb1364ad76b1a03f3f75d6efc7267c055
Mostafahassen1 <mostafahass314@gmail.com>
2024-12-24 10:00:44 -0800

    Issue #13345: Enable examples tests for PackageNameCheckExamplesTest

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/packagename/Example2.java
@@ -0,0 +1,18 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="PackageName">
+      <property name="format"
+        value="^[A-Z]+(\.[a-z][a-z0-9]*)*$"/>
+    </module>
+  </module>
+</module>
+*/
+
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.naming.packagename;
+// violation above 'Must match pattern'
+
+public class Example2 {
+}
+// xdoc section -- end
