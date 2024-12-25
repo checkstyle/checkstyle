4d235aa285143a70f6063c928e4e46755948fe29
Anmol <anmol20102005@gmail.com>
2024-12-26 03:34:39 +0530

    Issue #13345: Enable example test for SuppressWarningFilters

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswarningsfilter/Example1.java
@@ -0,0 +1,26 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="SuppressWarningsHolder" />
+    <module name="MemberName" />
+    <module name="NoWhitespaceAfter" />
+  </module>
+  <module name="SuppressWarningsFilter" />
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.filters.suppresswarningsfilter;
+// xdoc section -- start
+public class Example1 {
+  @SuppressWarnings("memberName")
+  int J; // ok only because there is annotation
+  int JJ; // violation 'must match pattern'
+
+  @SuppressWarnings({"MemberName", "NoWhitespaceAfter"})
+  int [] ARRAY; // ok only because there is annotation for both Checks
+  int [] ARRAY2;
+  // 2 violations above:
+  //   ''int' is followed by whitespace'
+  //   'must match pattern'
+}
+// xdoc section -- end
