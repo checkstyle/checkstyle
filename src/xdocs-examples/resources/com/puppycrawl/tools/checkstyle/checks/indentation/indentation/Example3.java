f855fe6863e22d9768a5a581fb70d017a3ad1c54
Anmol <anmol20102005@gmail.com>
2024-12-27 15:19:56 +0530

    Issue #13345: Enable Example Test for IndentationCheck
    Issue #13345: Enable Example Test for IndentationCheck
    
    added xml

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/Example3.java
@@ -0,0 +1,61 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="Indentation">
+      <property name="forceStrictCondition" value="true"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;
+
+// xdoc section -- start
+class Example3 {
+    String field = "example";                  // basicOffset
+    int[] values = {                           // basicOffset
+        10,
+        20,
+        30
+    };
+
+    void processValues() throws Exception {
+        handleValue("Test String", 42);
+    }
+
+    void handleValue(String aFooString,
+                     int aFooInt) {      // violation, "incorrect indentation"
+        boolean cond1 = true;
+        boolean cond2 = false;
+        boolean cond3 = true;
+        boolean cond4 = false;
+        boolean cond5 = true;
+        boolean cond6 = false;
+
+        if (cond1
+            || cond2) {
+            field = field.toUpperCase()
+                .concat(" TASK");
+        }
+
+        if ((cond1 && cond2)
+                || (cond3 && cond4)        // violation, "incorrect indentation"
+                || !(cond5 && cond6)) {    // violation, "incorrect indentation"
+            field.toUpperCase()
+                 .concat(" TASK")           // violation, "incorrect indentation"
+                 .chars().forEach(c -> {    // violation, "incorrect indentation"
+                     System.out.println((char) c);
+                 });
+        }
+    }
+
+    void demonstrateSwitch() throws Exception {
+        switch (field) {
+            case "EXAMPLE": processValues();                            // caseIndent
+            case "COMPLETED": handleValue("Completed Case", 456);       // caseIndent
+        }
+    }
+}
+
+
+// xdoc section -- end
