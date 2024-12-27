d50602c91cdf53e5e4af79ea41772c16e2334d22
Anmol <anmol20102005@gmail.com>
2024-12-27 15:19:56 +0530

    Issue #13345: Enable Test Example for Indentation

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/Example4.java
@@ -0,0 +1,58 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="Indentation"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;
+
+// xdoc section -- start
+class Example4 {
+    String field = "example";   // basicOffset
+    int[] values = {            // basicOffset
+        10,
+        20,
+        30
+    };
+
+    void processValues() throws Exception {
+        handleValue("Test String", 42);
+    }
+
+    void handleValue(String aFooString,
+                     int aFooInt) {  // indent:8 ; expected: > 4; ok, because 8 > 4
+        boolean cond1 = true;
+        boolean cond2 = false;
+        boolean cond3 = true;
+        boolean cond4 = false;
+        boolean cond5 = true;
+        boolean cond6 = false;
+
+        if (cond1
+            || cond2) {
+            field = field.toUpperCase()
+                .concat(" TASK");
+        }
+
+        if ((cond1 && cond2)
+                || (cond3 && cond4)       //ok, lineWrappingIndentation
+                || !(cond5 && cond6)) {   //ok, lineWrappingIndentation
+            field.toUpperCase()
+                 .concat(" TASK")     //ok, lineWrappingIndentation
+                 .chars().forEach(c -> {  //ok, lineWrappingIndentation
+                     System.out.println((char) c);
+                 });
+        }
+    }
+
+    void demonstrateSwitch() throws Exception {
+        switch (field) {
+            case "EXAMPLE": processValues();                                        // caseIndent
+            case "COMPLETED": handleValue("Completed Case", 456);  // caseIndent
+        }
+    }
+
+}
+// xdoc section -- end
