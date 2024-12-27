2d1a546fd6154230e48df4580654aa0321a3007c
Mauryan Kansara <mauryankansaramk@gmail.com>
2024-12-27 22:57:51 +0530

    Issue #8244: Added expandable menu for mobile/tablet devices

--- src/site/resources/js/checkstyle.js
+++ src/site/resources/js/checkstyle.js
@@ -49,6 +49,7 @@ window.addEventListener("load", function () {
     }
 });
 
+let isMenuVisible = false;
 window.addEventListener("scroll", function () {
     "use strict";
     if (document.documentElement.scrollTop > scrollDistanceToButtonVisibility) {
@@ -62,15 +63,75 @@ function setBodyColumnMargin() {
     const leftColumn = document.querySelector('#leftColumn');
     const bodyColumn = document.querySelector('#bodyColumn');
 
+    if (document.getElementById("hamburger")) {
+        resetStyling();
+    }
+
+    console.log("after if condition")
+    console.log("width: ", document.body.clientWidth)
+    console.log("height: ", document.body.clientHeight)
+
+    if (isMenuVisible) {
+        document.getElementById("leftColumn").style.display = "block";
+    }
+
     // If in mobile view use margin as defined in site.css
     if (window.innerWidth < 823) {
         bodyColumn.style.marginLeft = '1.5em';
+        setCollapsableMenuButton();
         return;
     }
 
     // Else calculate margin based on left column width
     const leftColumnWidth = leftColumn.offsetWidth;
     bodyColumn.style.marginLeft = `${leftColumnWidth + 27}px`;
+}
+
+function setCollapsableMenuButton() {
+    const menu = document.getElementById("leftColumn");
+    const breadcrumbs = document.getElementById("breadcrumbs");
+    const hamburger = document.createElement('div');
+    hamburger.id = 'hamburger'
+
+    for (let i = 0; i < 3; i++) {
+        const line = document.createElement('span');
+        hamburger.appendChild(line);
+    }
+
+    const xright = document.getElementsByClassName("xright")[0];
+    xright.appendChild(document.createTextNode(" | "));
+    xright.appendChild(hamburger);
+
+    hamburger.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        menu.style.top = `${breadcrumbs.offsetHeight + 10}px`;
+        menu.style.height = `calc(100vh - ${breadcrumbs.offsetHeight + 10}px)`;
+
+        if (hamburger.classList.contains('openMenu')) {
+            hamburger.classList.remove('openMenu');
+            menu.style.display = "none";
+            isMenuVisible = true;
+        }
+        else {
+            hamburger.classList.add('openMenu');
+            menu.style.display = "block";
+            isMenuVisible = false;
+        }
+    })
+}
+
+function resetStyling() {
+    const hamburger = document.getElementById("hamburger");
+    hamburger.remove()
+
+    const menu = document.getElementById("leftColumn");
+    menu.style.display = "block";
+    menu.style.removeProperty("top");
+    menu.style.removeProperty("height");
+
+    const xright = document.getElementsByClassName("xright")[0];
+    xright.lastChild.remove();
 }
 
 window.addEventListener('load', setBodyColumnMargin);
