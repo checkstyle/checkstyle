4180e6b741315d5af9f3901a2d5218a400c899ce
Aryan Sankar <145859525+ironfist-15@users.noreply.github.com>
2024-12-27 14:06:23 +0530

    Issue #14631: Updated code_literal to new AST format

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
@@ -425,12 +425,12 @@ public final class JavadocTokenTypes {
      * <pre><code>{&#64;code Comparable&lt;E&gt;}</code></pre>
      * <b>Tree:</b>
      * <pre>
-     * <code> |--JAVADOC_INLINE_TAG[3x0] : [{&#64;code Comparable&lt;E&gt;}]
-     *         |--JAVADOC_INLINE_TAG_START[3x0] : [{]
-     *         |--CODE_LITERAL[3x1] : [@code]
-     *         |--WS[3x6] : [ ]
-     *         |--TEXT[3x7] : [Comparable&lt;E&gt;]
-     *         |--JAVADOC_INLINE_TAG_END[3x21] : [}]
+     * <code> |--JAVADOC -> JAVADOC
+     *         |--NEWLINE -> \r\n
+     *         |--LEADING_ASTERISK ->  *
+     *         |--TEXT ->   Comparable&lt;E&gt;
+     *         |--NEWLINE -> \r\n
+     *         |--TEXT ->
      * </code>
      * </pre>
      *
