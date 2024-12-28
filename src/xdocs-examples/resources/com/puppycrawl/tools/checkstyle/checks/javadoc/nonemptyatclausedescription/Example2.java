5e25e01a1c558a24ec981c1eebf91ce9c5979dcc
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-12-29 01:55:07 +0530

    Issue #13345: Enable examples tests for NonEmptyAtClauseDescriptionCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/nonemptyatclausedescription/Example2.java
@@ -0,0 +1,31 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="NonEmptyAtclauseDescription">
+      <property name="javadocTokens" value="PARAM_LITERAL,THROWS_LITERAL"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.nonemptyatclausedescription;
+
+// xdoc section -- start
+class Example2 {
+
+  /**
+   * Some summary.
+   *
+   * @param a Some description
+   * @param b
+   * @deprecated
+   * @throws Exception
+   * @return
+   */
+  public void testMethod(){
+    // violation 6 lines above 'At-clause should have a non-empty description'
+    // @deprecated ignored as not mentioned in javadocTokens
+    // violation 6 lines above 'At-clause should have a non-empty description'
+    // @return ignored as not mentioned in javadocTokens
+  }
+}
+// xdoc section -- end
