8102a31692424f3dd317fc0921c7d0baf866d9bb
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-12-29 15:27:49 +0530

    Issue #13345: Enable examples tests for javadocPackageCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocpackage/legacywithboth/Example3.java
@@ -0,0 +1,15 @@
+/*xml
+<module name="Checker">
+  <module name="JavadocPackage">
+    <property name="allowLegacy" value="true"/>
+  </module>
+</module>
+*/
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocpackage.legacywithboth;
+/* violation on first line 'Legacy package.html file should be removed' */
+public class Example3 {
+  String str="some content";
+}
+// xdoc section -- end
+// violation 14 lines above 'Legacy package.html file should be removed'
---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocpackage/legacywithboth/package-info.java
@@ -0,0 +1,10 @@
+/*xml
+<module name="Checker">
+  <module name="JavadocPackage">
+    <property name="allowLegacy" value="true"/>
+  </module>
+</module>
+*/
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocpackage.legacywithboth;
+// xdoc section -- end
