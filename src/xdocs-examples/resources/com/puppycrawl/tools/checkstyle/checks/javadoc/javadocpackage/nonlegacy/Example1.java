e31e20514c3a27bd7594784894a1cdcf681d798e
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-12-29 15:27:49 +0530

    Issue #13345: Enable examples tests for javadocPackageCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocpackage/nonlegacy/Example1.java
@@ -0,0 +1,11 @@
+/*xml
+<module name="Checker">
+  <module name="JavadocPackage"/>
+</module>
+*/
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocpackage.nonlegacy;
+/* violation on first line 'Missing package-info.java file' */
+public class Example1 { }
+// xdoc section -- end
+// violation 10 lines above 'Missing package-info.java file'
