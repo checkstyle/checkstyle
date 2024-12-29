2e08a5fc7070b1f5a7ed94bbabf6d096989b4505
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-12-29 15:27:49 +0530

    Issue #13345: Enable examples tests for javadocPackageCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocpackage/legacy/Example2.java
@@ -0,0 +1,14 @@
+/*xml
+<module name="Checker">
+  <module name="JavadocPackage">
+    <property name="allowLegacy" value="true"/>
+  </module>
+</module>
+*/
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocpackage.legacy;
+// OK as package.html file is present in directory
+public class Example2 {
+  String str="some content";
+}
+// xdoc section -- end
