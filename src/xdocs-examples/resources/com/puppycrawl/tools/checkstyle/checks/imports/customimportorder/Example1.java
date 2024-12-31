2bcbd83a8931f03fe49e137b597bd5c00309903e
Anmol <anmol20102005@gmail.com>
2024-12-31 19:44:23 +0530

    Issue #13345: Enable Test Example for CustomImport

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/Example1.java
@@ -0,0 +1,18 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="CustomImportOrder"/>
+  </module>
+</module>
+*/
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.imports.customimportorder;
+import org.apache.commons.io.FileUtils;
+import static java.util.Collections.*;
+import java.time.*;
+import static java.io.File.separator;
+import com.puppycrawl.tools.checkstyle.checks.imports.CustomImportOrderCheck;
+import com.puppycrawl.tools.checkstyle.checks.imports.ImportOrderCheck;
+// xdoc section -- end
+public class Example1 {
+}
