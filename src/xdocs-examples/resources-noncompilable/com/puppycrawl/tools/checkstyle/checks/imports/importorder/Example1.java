1c1a734cde1f2e82f5635b8a88c82f20ab5c6a86
Usman Shaikh <shaikhu@gmail.com>
2024-12-31 20:33:21 +0000

    Issue #13345: Enable Example Test for ImportOrder Check

--- src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example1.java
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example1.java
@@ -5,19 +5,23 @@
   </module>
 </module>
 */
+//non-compiled with javac: Compilable with Java11
+package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
 
 // xdoc section -- start
 import java.io.IOException;
 import java.net.URL;
 
-import java.io.IOException; // violation, extra separation before import
-                            // and wrong order, comes before 'java.net.URL'.
+import java.io.IOException;
+// 2 violations above:
+//  'Extra separation in import group before 'java.io.IOException''
+//  'Wrong order for 'java.io.IOException' import.'
 import javax.net.ssl.TrustManager; // violation, extra separation due to above comment
 import javax.swing.JComponent;
-import org.apache.http.conn.ClientConnectionManager; // OK
+import org.apache.http.conn.ClientConnectionManager;
 import java.util.Set; // violation, wrong order, 'java' should not come after 'org' imports
 import com.neurologic.http.HttpClient; // violation, wrong order, 'com' imports comes at top
-import com.neurologic.http.impl.ApacheHttpClient; // OK
-
-public class SomeClass { }
+import com.neurologic.http.impl.ApacheHttpClient;
 // xdoc section -- end
+
+public class Example1 { }
--- src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example8.java
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example8.java
@@ -2,7 +2,7 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="ImportOrder">
-      <property name="useContainerOrderingForStatic" value="false"/>
+      <property name="useContainerOrderingForStatic" value="true"/>
       <property name="ordered" value="true"/>
       <property name="option" value="top"/>
       <property name="caseSensitive" value="false"/>
@@ -11,12 +11,14 @@
   </module>
 </module>
 */
+//non-compiled with javac: Compilable with Java11
+package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
 
 // xdoc section -- start
 import static io.netty.handler.codec.http.HttpConstants.COLON;
 import static io.netty.handler.codec.http.HttpHeaders.addHeader;
 import static io.netty.handler.codec.http.HttpHeaders.setHeader;
-import static io.netty.handler.codec.http.HttpHeaders.Names.DATE; // violation
-
-public class InputEclipseStaticImportsOrder { }
+import static io.netty.handler.codec.http.HttpHeaders.Names.DATE;
 // xdoc section -- end
+
+public class Example8 { }
