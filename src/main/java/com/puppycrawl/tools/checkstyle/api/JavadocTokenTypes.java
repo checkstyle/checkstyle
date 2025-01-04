8872af819517f9e8a9f875b18043572caadf8def
aboro-code <aboro134@gmail.com>
2025-01-04 22:48:58 +0530

    Added escaping for the modified lines

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
@@ -995,17 +995,17 @@ public final class JavadocTokenTypes {
      * <b>Tree:</b>
      * <pre>
      * {@code
-     *   --JAVADOC_TAG -> JAVADOC_TAG
-     *      |--SERIAL_FIELD_LITERAL -> @serialField
-     *      |--WS ->
-     *      |--FIELD_NAME -> counter
-     *      |--WS ->
-     *      |--FIELD_TYPE -> Integer
-     *      |--WS ->
-     *      `--DESCRIPTION -> DESCRIPTION
-     *          |--TEXT -> objects counter
-     *          |--NEWLINE -> \r\n
-     *          `--TEXT ->
+     *   --JAVADOC_TAG -&gt; JAVADOC_TAG
+     *      |--SERIAL_FIELD_LITERAL -&gt; @serialField
+     *      |--WS -&gt;
+     *      |--FIELD_NAME -&gt; counter
+     *      |--WS -&gt;
+     *      |--FIELD_TYPE -&gt; Integer
+     *      |--WS -&gt;
+     *      `--DESCRIPTION -&gt; DESCRIPTION
+     *          |--TEXT -&gt; objects counter
+     *          |--NEWLINE -&gt; \r\n
+     *          `--TEXT -&gt;
      * }</pre>
      *
      * @see
