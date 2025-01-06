ca4e5a8c76ab781e630f2ca7ed0234c0f76f9be1
Usman Shaikh <shaikhu@gmail.com>
2025-01-06 07:14:35 +0000

    Issue #13345: Enable Example Test for SuppressionCommentFilter Check

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/filters/SuppressionCommentFilterExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/filters/SuppressionCommentFilterExamplesTest.java
@@ -19,12 +19,10 @@
 
 package com.puppycrawl.tools.checkstyle.filters;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
 
-@Disabled("until https://github.com/checkstyle/checkstyle/issues/13345")
 public class SuppressionCommentFilterExamplesTest extends AbstractExamplesModuleTestSupport {
     @Override
     protected String getPackageLocation() {
@@ -34,72 +32,97 @@ public class SuppressionCommentFilterExamplesTest extends AbstractExamplesModule
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-
+            "16:7: Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "22:27: Name 'var3' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "32:5: Catching 'Exception' is not allowed.",
         };
 
-        verifyWithInlineConfigParser(getPath("Example1.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
     }
 
     @Test
     public void testExample2() throws Exception {
         final String[] expected = {
-
+            "21:7: Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "24:7: Name 'VAR2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "27:27: Name 'var3' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "37:5: Catching 'Exception' is not allowed.",
+            "42:5: Catching 'Exception' is not allowed.",
+            "43:5: Catching 'Error' is not allowed.",
         };
 
-        verifyWithInlineConfigParser(getPath("Example2.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example2.java"), expected);
     }
 
     @Test
     public void testExample3() throws Exception {
         final String[] expected = {
-
+            "21:7: Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "24:7: Name 'VAR2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "27:27: Name 'var3' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "31:27: Name 'var4' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "38:5: Catching 'Exception' is not allowed.",
+            "44:5: Catching 'Error' is not allowed.",
         };
 
-        verifyWithInlineConfigParser(getPath("Example3.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example3.java"), expected);
     }
 
     @Test
     public void testExample4() throws Exception {
         final String[] expected = {
-
+            "20:7: Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "26:27: Name 'var3' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "36:5: Catching 'Exception' is not allowed.",
         };
 
-        verifyWithInlineConfigParser(getPath("Example4.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example4.java"), expected);
     }
 
     @Test
     public void testExample5() throws Exception {
         final String[] expected = {
-
+            "20:7: Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "26:27: Name 'var3' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "30:27: Name 'var4' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "37:5: Catching 'Exception' is not allowed.",
         };
 
-        verifyWithInlineConfigParser(getPath("Example5.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example5.java"), expected);
     }
 
     @Test
     public void testExample6() throws Exception {
         final String[] expected = {
-
+            "25:7: Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "31:27: Name 'var3' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "41:5: Catching 'Exception' is not allowed.",
         };
 
-        verifyWithInlineConfigParser(getPath("Example6.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example6.java"), expected);
     }
 
     @Test
     public void testExample7() throws Exception {
         final String[] expected = {
-
+            "19:7: Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "25:27: Name 'var3' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
         };
 
-        verifyWithInlineConfigParser(getPath("Example7.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example7.java"), expected);
     }
 
     @Test
     public void testExample8() throws Exception {
         final String[] expected = {
-
+            "18:7: Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "21:7: Name 'VAR2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "24:27: Name 'var3' must match pattern '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'.",
+            "34:5: Catching 'Exception' is not allowed.",
+            "39:5: Catching 'Exception' is not allowed.",
+            "40:5: Catching 'Error' is not allowed.",
         };
 
-        verifyWithInlineConfigParser(getPath("Example8.txt"), expected);
+        verifyWithInlineConfigParser(getPath("Example8.java"), expected);
     }
 }
