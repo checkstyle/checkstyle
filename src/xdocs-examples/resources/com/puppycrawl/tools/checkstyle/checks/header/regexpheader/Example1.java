bbe9cd2639de41ecc8e09a2c82d69e7e553da606
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-01-08 01:09:05 +0530

    Issue #13345: Enable Example Test for RegexpHeaderCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/regexpheader/Example1.java
@@ -0,0 +1,10 @@
+/*xml
+<module name="Checker">
+  <module name="RegexpHeader"/>
+</module>
+*/
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.header.regexpheader;
+// OK, as by default there is not specific header defined to validate it presence
+public class Example1 { }
+// xdoc section -- end
