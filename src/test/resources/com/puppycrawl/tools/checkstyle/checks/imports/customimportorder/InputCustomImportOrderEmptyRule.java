e46208570bc89a7dcd9807dbad8cd539fe053eb8
Anmol <anmol20102005@gmail.com>
2025-01-08 20:19:52 +0530

    Issue #15983: Added validation to default

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderEmptyRule.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderEmptyRule.java
@@ -1,7 +1,7 @@
 /*
 CustomImportOrder
 customImportOrderRules =
-standardPackageRegExp = (default)^(java|javax)\.
+standardPackageRegExp = (default)^(java|javax)\\.
 thirdPartyPackageRegExp = (default).*
 specialImportsRegExp = (default)^$
 separateLineBetweenGroups = (default)true
