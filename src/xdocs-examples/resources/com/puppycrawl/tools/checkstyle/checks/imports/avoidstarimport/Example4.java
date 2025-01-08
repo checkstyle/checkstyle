abedd6f33bb8beac64b1b835cc5ee4584cdf42ae
aboro-code <aboro134@gmail.com>
2025-01-08 22:21:43 +0530

    Issue #15456: Specify violation message for AvoidStarImportCheck

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport/Example4.java
@@ -12,10 +12,10 @@ package com.puppycrawl.tools.checkstyle.checks.imports.avoidstarimport;
 
 // xdoc section -- start
 import java.util.Scanner;
-import java.io.*;                 // violation
+import java.io.*; // violation, 'Using the '.*' form of import should be avoided.'
 import static java.lang.Math.*;
-import java.util.*;               // violation
-import java.net.*;                // violation
+import java.util.*; // violation, 'Using the '.*' form of import should be avoided.'
+import java.net.*; // violation, 'Using the '.*' form of import should be avoided.'
 // xdoc section -- end
 
 class Example4 {}
