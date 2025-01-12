d1185ed192091a2a7672c21907eb5c82c86e7bf3
Aryan Sankar <145859525+ironfist-15@users.noreply.github.com>
2025-01-12 17:18:41 +0530

    Issue #11163: Enforce file size on InputJavadocMethodThrowsDetection

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodThrowsDetection1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodThrowsDetection1.java
@@ -12,7 +12,7 @@ tokens = (default)METHOD_DEF, CTOR_DEF, ANNOTATION_FIELD_DEF, COMPACT_CTOR_DEF
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocmethod;
 
-public class InputJavadocMethodThrowsDetection {
+public class InputJavadocMethodThrowsDetection1 {
 
     void noJavadoc() {
         // no javadoc, no violations
@@ -103,64 +103,6 @@ public class InputJavadocMethodThrowsDetection {
             if (x <= 0) {
                 // violation below 'Expected @throws tag for 'IllegalArgumentException'.'
                 throw new IllegalArgumentException("0");
-            }
-        }
-    }
-
-    /**
-     * Nesting inside 3 layers of if statements.
-     *
-     * @param x some input for control flow statements
-     */
-    void tripleNestedIf(int x) {
-        if (x >= 0)
-            if (x <= 0)
-                if (x == 0)
-                    // violation below 'Expected @throws tag for 'IllegalArgumentException'.'
-                    throw new IllegalArgumentException("0");
-    }
-
-    /**
-     * Throw outside but after if statement.
-     *
-     * @param x some input for control flow statements
-     */
-    void throwAfterIf(int x) {
-        if (x > 0) {
-            x = 0;
-        }
-        // violation below 'Expected @throws tag for 'UnsupportedOperationException'.'
-        throw new UnsupportedOperationException("");
-    }
-
-    /**
-     * Throw in catch but after if statement.
-     *
-     * @param x some input for control flow statements
-     */
-    void tryCatchAfterIf(int x) {
-        if (x == 0) {
-            return;
-        }
-        try {
-            System.out.println("foo");
-        } catch (Exception e) {
-            // violation below 'Expected @throws tag for 'RuntimeException'.'
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Throw in try/catch inside if statement.
-     * @param x some input for control flow statements
-     */
-    void tryCatchWithinIf(int x) {
-        if (x == 0) {
-            try {
-                System.out.println("foo");
-            } catch (Exception e) {
-                // violation below 'Expected @throws tag for 'RuntimeException'.'
-                throw new RuntimeException(e);
             }
         }
     }
