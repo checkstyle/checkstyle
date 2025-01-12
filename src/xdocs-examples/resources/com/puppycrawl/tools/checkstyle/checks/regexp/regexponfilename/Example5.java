d2e982b567dd356a23d9144a9ecb318aaa134483
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-01-12 23:06:38 +0530

    Issue #13345: Enable Example Test for RegexpOnFilenameCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexponfilename/Example5.java
@@ -0,0 +1,21 @@
+/*xml
+<module name="Checker">
+  <module name="RegexpOnFilename">
+    <property name="fileNamePattern" value="^([A-Z][a-z0-9]+\.?)+$"/>
+    <property name="match" value="false"/>
+    <property name="ignoreFileNameExtensions" value="true"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.regexp.regexponfilename;
+/*
+// xdoc section -- start
+checks/regexp/regexponfilename/README.md
+checks/regexp/regexponfilename/TestExample.java
+checks/regexp/regexponfilename/TestExample.md
+checks/regexp/regexponfilename/TestExample.xml
+checks/regexp/regexponfilename/Test Example.xml // violation, 'contains whitespace'
+checks/regexp/regexponfilename/checkstyle.xml // violation, 'not camelcase'
+// xdoc section -- end
+*/
+class Example5 {}
