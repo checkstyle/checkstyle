6bde4bcb002844f597b1b569100361e8877a65ed
Broilzzz <ahilahmed2004@gmail.com>
2025-01-14 15:39:05 +0300

    Issue #11163: Enforce file size on InputJavadocPackageNoJavadoc

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocpackage/InputJavadocPackageClass.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocpackage/InputJavadocPackageClass.java
@@ -7,10 +7,8 @@ fileExtensions = java
 */
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocpackage;
-// violation 9 lines above 'Missing package-info.java file'
 
-public class InputJavadocPackageNoJavadoc //comment test
-{
+public class InputJavadocPackageClass {
     public int i1;
     protected int i2;
     int i3;
@@ -20,6 +18,18 @@ public class InputJavadocPackageNoJavadoc //comment test
     protected void foo2() {}
     void foo3() {}
     private void foo4() {}
+
+    public class PublicInner {
+        public int i1;
+        protected int i2;
+        int i3;
+        private int i4;
+
+        public void foo1() {}
+        protected void foo2() {}
+        void foo3() {}
+        private void foo4() {}
+    }
 
     protected class ProtectedInner {
         public int i1;
@@ -57,4 +67,15 @@ public class InputJavadocPackageNoJavadoc //comment test
         private void foo4() {}
     }
 
+    class IgnoredName {
+        // ignore by name
+        private int logger;
+        // no warning, 'serialVersionUID' fields do not require Javadoc
+        private static final long serialVersionUID = 0;
+    }
+
+    /**/
+    void methodWithTwoStarComment() {}
+
 }
+
