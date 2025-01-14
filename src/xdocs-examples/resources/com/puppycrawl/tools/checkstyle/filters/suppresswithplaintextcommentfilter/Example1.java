cfac7b31b8e0bc839cee820e3cfa0e6b083e9ab9
ElinaZoldnere <elina.zoldnere@gmail.com>
2025-01-14 15:03:31 +0200

    Issue #13345: Enable example tests for SuppressWithPlainTextCommentFilter

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithplaintextcommentfilter/Example1.java
@@ -0,0 +1,16 @@
+/*xml
+<module name="Checker">
+  <property name="fileExtensions" value="properties"/>
+
+  <module name="UniqueProperties"/>
+  <module name="SuppressWithPlainTextCommentFilter"/>
+
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.filters.suppresswithplaintextcommentfilter;
+
+public class Example1 { }
+
+// xdoc section -- start
+// xdoc section -- end
