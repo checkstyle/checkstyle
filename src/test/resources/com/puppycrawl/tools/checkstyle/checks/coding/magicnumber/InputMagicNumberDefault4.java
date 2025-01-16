95575d4247c410a98aa32121a88eeac8a9034394
Anmol <anmol20102005@gmail.com>
2025-01-16 12:05:58 +0530

    Issue #16101: Fixed MagicNumber Check Bug

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumberDefault4.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumberDefault4.java
@@ -4,19 +4,24 @@ ignoreHashCodeMethod = (default)false
 ignoreFieldDeclaration = (default)false
 
 */
-
 package com.puppycrawl.tools.checkstyle.checks.coding.magicnumber;
 
 public class InputMagicNumberDefault4 {
+    // This should not trigger a violation as it's a constant definition
+    private static final int MY_CONSTANT = 42;
 
-    private static final int DEFAULT_RADIUS = 5;
+    // This should not trigger a violation as it uses the constant
+    private final int usingConstant = MY_CONSTANT;
 
-    final int radius;
+    // This should not trigger a violation as it's a direct constant definition
+    private static final int ANOTHER_CONSTANT = 123;
 
-    public InputMagicNumberDefault4(int radius) {
-        // Using a valid constant; no violation here
-        this.radius = DEFAULT_RADIUS;
+    public void method() {
 
-        int area = 42; // violation ''42' is a magic number'
+        int magicNumber = 42; // violation ''42' is a magic number'
+
+        // This should not trigger a violation
+        int usingConstantInMethod = MY_CONSTANT;
     }
 }
+
