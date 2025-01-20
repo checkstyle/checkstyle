b32c54e19aab60e979fd3c583883d9af10d50784
Usman Shaikh <shaikhu@gmail.com>
2025-01-20 20:24:22 +0000

    Issue #13345: Enable example test for SuppressWithNearbyTextFilter

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example3.java
@@ -9,7 +9,10 @@
   </module>
 </module>
 */
-
+package com.puppycrawl.tools.checkstyle.filters.suppresswithnearbytextfilter;
 // xdoc section -- start
-export FOO=BAR # ok, because violation message is matching suppress pattern
+public class Example3 {
+  // ok, because violation message is matching suppress pattern
+  int a_really_long_variable_name = 10;
+}
 // xdoc section -- end
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example6.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example6.java
@@ -1,15 +1,14 @@
 /*xml
 <module name="Checker">
   <module name="SuppressWithNearbyTextFilter">
-    <property name="lineRange" value="2"/>
+    <property name="lineRange" value="-1"/>
   </module>
   <module name="UniqueProperties"/>
 </module>
 */
 
+package com.puppycrawl.tools.checkstyle.filters.suppresswithnearbytextfilter;
+
+public class Example6 {}
 // xdoc section -- start
-key.one=41 # SUPPRESS CHECKSTYLE because I want to
-key.one=42 # ok
-key.one=43 # ok
-key.one=44 # violation
 // xdoc section -- end
