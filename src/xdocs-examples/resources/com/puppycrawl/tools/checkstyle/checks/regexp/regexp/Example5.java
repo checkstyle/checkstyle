46bed0266118283e7295cfbb528cb827c7f32526
Harsh-Agrawal96 <harshagrawal9650@gmail.com>
2025-01-21 21:28:51 +0530

    Issue #13345: enabling tests for RegexpCheckExampleTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/Example5.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/Example5.java
@@ -2,13 +2,17 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="Regexp">
+      <!-- . matches any character, so we need to escape it and use \. to match dots. -->
       <property name="format" value="System\.out\.println"/>
       <property name="illegalPattern" value="true"/>
-      <property name="ignoreComments" value="true"/>
     </module>
   </module>
 </module>
 */
+
+package com.puppycrawl.tools.checkstyle.checks.regexp.regexp;
+
+public class Example5 {}
 
 // xdoc section -- start
 // xdoc section -- end
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/Example8.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/Example8.java
@@ -2,13 +2,14 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="Regexp">
-      <!-- . matches any character, so we need to escape it and use \. to match dots. -->
-      <property name="format" value="System\.out\.println"/>
+      <property name="format" value="(?i)debug"/>
       <property name="illegalPattern" value="true"/>
     </module>
   </module>
 </module>
 */
+
+package com.puppycrawl.tools.checkstyle.checks.regexp.regexp;
 
 // xdoc section -- start
 // xdoc section -- end
