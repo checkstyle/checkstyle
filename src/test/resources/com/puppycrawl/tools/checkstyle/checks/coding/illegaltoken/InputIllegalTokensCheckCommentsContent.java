e3a604f1d7b519873912efe74e273a37699f9892
andrey Ivanov <andreyvorobey.play@gmail.com>
2025-01-26 16:50:06 -0700

    Issue checkstyle#15456: illegal token check

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltoken/InputIllegalTokensCheckCommentsContent.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltoken/InputIllegalTokensCheckCommentsContent.java
@@ -1,16 +1,16 @@
-/* // violation, 'Using ' \/\/ violation\\nIllegalToken\\ntokens = COMMENT_CONTENT\\n\\n\\n' is not allowed'
+/* // violation
 IllegalToken
 tokens = COMMENT_CONTENT
 
 
 */
-
+// violation, 7 lines above 'Using ' \/\/ violation\\nIllegalToken\\ntokens = COMMENT_CONTENT\\n\\n\\n' is not allowed'
 package com.puppycrawl.tools.checkstyle.checks.coding.illegaltoken;
 
-//violation, 'Using '* \/\/ violation\\n * Test for illegal tokens\\n ' is not allowed'
-
-
-
+/** //violation, ''
+ * Test for illegal tokens
+ */
+// violation, 2 lines above 'Using '* \/\/ violation\\n * Test for illegal tokens\\n ' is not allowed'
 public class InputIllegalTokensCheckCommentsContent
 {
     public void methodWithPreviouslyIllegalTokens()
@@ -24,7 +24,6 @@ public class InputIllegalTokensCheckCommentsContent
                 break;
         }
     }
-
     public native void nativeMethod();
 
     public void methodWithLiterals()
@@ -35,11 +34,13 @@ public class InputIllegalTokensCheckCommentsContent
     public void methodWithLabels() {
         label:
         {
-            anotherLabel: // violation, 'Using ' some comment href // violation \\n' is not allowed'
+            anotherLabel: // some comment href // violation, ''
             do {
                 continue anotherLabel;
             } while (false);
-            break label; // violation, 'Using 'some a href \/\/ violation' is not allowed'
+            break label; // some a href // violation, ''
         }
+        // violation, 6 lines above 'Using ' some comment href \/\/ violation \\n' is not allowed'
+        // violation, 3 lines above 'Using 'some a href \/\/ violation\n' is not allowed'
     }
 }
