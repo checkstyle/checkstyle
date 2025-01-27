838bc739fcb594f150595bb0a421adaca7f108e3
andrey Ivanov <andreyvorobey.play@gmail.com>
2025-01-26 17:07:53 -0700

    Issue #15456: Missing Ctor check

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/missingctor/InputMissingCtorEmptyClass.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/missingctor/InputMissingCtorEmptyClass.java
@@ -6,7 +6,7 @@ MissingCtor
 
 package com.puppycrawl.tools.checkstyle.checks.coding.missingctor;
 
-public class InputMissingCtorEmptyClass // violation
+public class InputMissingCtorEmptyClass // violation, 'Class should define a constructor'
 {
 }
 // we shouldn't flag abstract classes
