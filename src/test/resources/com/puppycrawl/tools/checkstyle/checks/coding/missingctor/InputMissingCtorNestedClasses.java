b17228635db42232f7d261bc75971642b8787154
andrey Ivanov <andreyvorobey.play@gmail.com>
2025-01-26 17:07:53 -0700

    Issue #15456: Missing Ctor check

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/missingctor/InputMissingCtorNestedClasses.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/missingctor/InputMissingCtorNestedClasses.java
@@ -6,9 +6,9 @@ MissingCtor
 
 package com.puppycrawl.tools.checkstyle.checks.coding.missingctor;
 
-public class InputMissingCtorNestedClasses { // violation
-    class Inner1 { // violation
-        class Inner2 { // violation
+public class InputMissingCtorNestedClasses { // violation, 'Class should define a constructor'
+    class Inner1 { // violation, 'Class should define a constructor'
+        class Inner2 { // violation, 'Class should define a constructor'
 
         }
     }
