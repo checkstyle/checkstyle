8e7c38185878bcc625b005b454e60d908d2a099f
Broilzzz <ahilahmed2004@gmail.com>
2025-01-28 22:09:53 +0530

    Issue #13345: Enable tests for translation

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/translation/Example3.java
@@ -0,0 +1,18 @@
+/*xml
+<module name="Checker">
+  <module name="Translation">
+    <property name="fileExtensions" value="properties"/>
+    <property name="fileExtensions" value="translations"/>
+    <property name="requiredTranslations" value="fr"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.translation;
+// xdoc section -- start
+/*
+--> Example1
+  |--> messages.properties
+  |--> messages.translations
+*/
+public class Example3 {}
+// xdoc section -- end
