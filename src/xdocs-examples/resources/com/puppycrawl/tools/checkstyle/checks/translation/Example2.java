005645e1b5d093df51c07df015238e6590468aa6
Broilzzz <ahilahmed2004@gmail.com>
2025-01-28 22:09:53 +0530

    Issue #13345: Enable tests for translation

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/translation/Example2.java
@@ -0,0 +1,22 @@
+/*xml
+<module name="Checker">
+  <module name="Translation">
+    <property name="baseName" value="^ButtonLabels.*$"/>
+    <property name="requiredTranslations" value="fr"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.translation;
+/*
+// xdoc section -- start
+#ButtonLabels.properties
+hello=Hello
+cancel=Cancel
+
+#ButtonLabels_fr.properties
+hello=Bonjour
+name=Nom
+// xdoc section -- end
+ */
+public class Example2 {}
---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/translation/Example3.java
@@ -0,0 +1,25 @@
+/*xml
+<module name="Checker">
+  <module name="Translation">
+    <property name="requiredTranslations" value="ja, fr"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.translation;
+/*
+// xdoc section -- start
+#messages.properties
+hello=Hello
+cancel=Cancel
+
+#messages_ja.properties
+greeting=こんにちは
+age=年齢
+
+#messages_fr.properties
+greeting=Bonjour
+name=Nom
+// xdoc section -- end
+ */
+public class Example3 {}
