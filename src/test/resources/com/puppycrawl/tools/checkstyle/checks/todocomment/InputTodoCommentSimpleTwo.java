8dd796468c58d3e2282a97ae5d0eee877a372ce9
ahmedyoussefg <a.goerany@gmail.com>
2025-02-10 01:15:35 +0200

    Issue #11163: Enforce file size on TodoComment

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/todocomment/InputTodoCommentSimpleTwo.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/todocomment/InputTodoCommentSimpleTwo.java
@@ -16,104 +16,8 @@ import java.io.*;
  * - Order of modifiers
  * @author Oliver Burn
  **/
-final class InputTodoCommentSimple
-{
-    // Long line ----------------------------------------------------------------
-    // Contains a tab ->	<-
-    // Contains trailing whitespace ->
 
-    // Name format tests
-    //
-    /** Invalid format **/
-    public static final int badConstant = 2;
-    /** Valid format **/
-    public static final int MAX_ROWS = 2;
-
-    /** Invalid format **/
-    private static int badStatic = 2;
-    /** Valid format **/
-    private static int sNumCreated = 0;
-
-    /** Invalid format **/
-    private int badMember = 2;
-    /** Valid format **/
-    private int mNumCreated1 = 0;
-    /** Valid format **/
-    protected int mNumCreated2 = 0;
-
-    /** commas are wrong **/
-    private int[] mInts = new int[] {1,2, 3,
-                                     4};
-
-    //
-    // Accessor tests
-    //
-    /** should be private **/
-    public static int sTest1;
-    /** should be private **/
-    protected static int sTest3;
-    /** should be private **/
-    static int sTest2;
-
-    /** should be private **/
-    int mTest1;
-    /** should be private **/
-    public int mTest2;
-
-    //
-    // Parameter name format tests
-    //
-
-    /**
-     * @return hack
-     * @param badFormat1 bad format
-     * @param badFormat2 bad format
-     * @param badFormat3 bad format
-     * @throws java.lang.Exception abc
-     **/
-    int test1(int badFormat1,int badFormat2,
-              final int badFormat3)
-        throws java.lang.Exception
-    {
-        return 0;
-    }
-
-    /** method that is 20 lines long **/
-    private void longMethod()
-    {
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-    }
-
-    /** constructor that is 10 lines long **/
-    private InputTodoCommentSimple()
-    {
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-        // a line
-    }
-
+public class InputTodoCommentSimpleTwo {
     /** test local variables */
     private void localVariables()
     {
@@ -214,17 +118,3 @@ class InputTodoCommentSimple2
     }
 }
 
-/** Test enum for member naming check */
-enum MyEnum1
-{
-    /** ABC constant */
-    ABC,
-
-    /** XYZ constant */
-    XYZ;
-
-    /** Should be mSomeMember */
-    private int someMember;
-
-    public final int blah = 5;
-}
