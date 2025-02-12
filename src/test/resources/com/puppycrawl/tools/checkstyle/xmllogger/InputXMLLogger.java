f56bd4c62b98b052e9a895f3641c47d027eb45c3
Ziad Ahmed <zyadahmed2466@gmail.com>
2025-02-12 22:29:17 +0200

    Issue #14019: Resolve pitest suppressions for XMLLogger fileMessages.remove

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/xmllogger/InputXMLLogger.java
@@ -0,0 +1,16 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="com.puppycrawl.tools.checkstyle.checks.imports.AvoidStarImportCheck"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.xmllogger;
+
+// xdoc section -- start
+import java.util.Scanner;
+import java.io.*; // violation, 'Using the '.*' form of import should be avoided.'
+// xdoc section -- end
+
+public class InputXMLLogger {
+}
