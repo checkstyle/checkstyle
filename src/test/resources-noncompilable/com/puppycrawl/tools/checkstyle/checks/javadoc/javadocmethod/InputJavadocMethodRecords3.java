83c0d52f2ba5780149372af948f1b7836b35f884
Anmol <anmol20102005@gmail.com>
2025-02-15 20:53:26 +0530

    Issue #15683: Resolved JavadocMethod Issue

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodRecords3.java
@@ -0,0 +1,38 @@
+/*
+JavadocMethod
+allowedAnnotations = Deprecated
+validateThrows = true
+accessModifiers = (default)public, protected, package, private
+allowMissingParamTags = (default)false
+allowMissingReturnTag = (default)false
+tokens = METHOD_DEF, CTOR_DEF, ANNOTATION_FIELD_DEF, COMPACT_CTOR_DEF
+
+
+*/
+
+//non-compiled with javac: Compilable with Java17
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocmethod;
+
+
+public class InputJavadocMethodRecords3
+{
+    /**
+     *
+     * @param email         the user's email address
+     * @param emailVerified {@code true} if the email address has been verified
+     */
+    @Deprecated
+    public record Inner(String subject, String firstName,
+                        String nickName, String fullName,
+                        String email, boolean emailVerified)
+    {
+        /**
+         * Creates a new UserInfo instance.
+         *
+         */
+        @Deprecated
+        public Inner
+        {
+        }
+    }
+}
