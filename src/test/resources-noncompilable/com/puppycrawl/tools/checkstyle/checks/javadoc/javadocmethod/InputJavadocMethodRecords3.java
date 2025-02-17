bd71cc7c61709ddaa623881615cb88dab4f17697
Anmol <anmol20102005@gmail.com>
2025-02-17 11:42:16 +0530

    changes

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodRecords3.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodRecords3.java
@@ -3,8 +3,8 @@ JavadocMethod
 allowedAnnotations = (default)Override
 validateThrows = true
 accessModifiers = (default)public, protected, package, private
-allowMissingParamTags = true
-allowMissingReturnTag = true
+allowMissingParamTags = (default)false
+allowMissingReturnTag = (default)false
 tokens = METHOD_DEF, CTOR_DEF, ANNOTATION_FIELD_DEF, COMPACT_CTOR_DEF
 
 
@@ -21,6 +21,7 @@ public class InputJavadocMethodRecords3
      * @param email         the user's email address
      * @param emailVerified {@code true} if the email address has been verified
      */
+    @Override
     public record Inner(String subject, String firstName,
                         String nickName, String fullName,
                         String email, boolean emailVerified)
@@ -29,6 +30,7 @@ public class InputJavadocMethodRecords3
          * Creates a new UserInfo instance.
          *
          */
+        @Override
         public Inner
         {
         }
