f29ea03aacb6c960ec7b0cd6e3dafc94e4bd1dfa
Anmol <anmol20102005@gmail.com>
2025-02-19 12:25:25 +0530

    Issue #15161: Resolved False Negative

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationonsameline/InputAnnotationOnSameLine.java
@@ -0,0 +1,26 @@
+/*
+AnnotationOnSameLine
+tokens = CLASS_DEF, INTERFACE_DEF, ENUM_DEF, METHOD_DEF, CTOR_DEF, \
+         VARIABLE_DEF, PARAMETER_DEF, ANNOTATION_DEF, TYPECAST, LITERAL_THROWS, \
+         IMPLEMENTS_CLAUSE, TYPE_ARGUMENT, LITERAL_NEW, DOT, ANNOTATION_FIELD_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.annotationonsameline;
+
+import static java.lang.annotation.ElementType.TYPE_USE;
+
+import java.lang.annotation.Target;
+
+public
+class InputAnnotationOnSameLine {
+  @Ann public   // violation, "Annotation 'Ann' should be on the same line with its target."
+  @Ann2 class E {}
+
+  @Ann private  // violation, "Annotation 'Ann' should be on the same line with its target."
+  @Ann2 class A {}
+
+  @Target({TYPE_USE}) @interface Ann {}
+  @Target({TYPE_USE}) @interface Ann2 {}
+}
