74c53969ab038e054ce719ba3a0c3dd22352904e
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2025-02-21 21:45:36 +0600

    Issue #13213: Remove '//ok' comments for nowhitespaceafter module

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nowhitespaceafter/InputNoWhitespaceAfterSynchronized.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nowhitespaceafter/InputNoWhitespaceAfterSynchronized.java
@@ -16,42 +16,42 @@ public class InputNoWhitespaceAfterSynchronized<T> {
 
     void m1() {
         synchronized (lock) {} // violation
-        synchronized(lock) {} // ok
+        synchronized(lock) {}
     }
 
-    synchronized int m2() { // ok
+    synchronized int m2() {
         return 2;
     }
 
-    synchronized private int[] m3() { // ok
+    synchronized private int[] m3() {
         return new int[]{2};
     }
 
-    synchronized int[] m4() { // ok
+    synchronized int[] m4() {
         return new int[]{2};
     }
 
-    private final synchronized int m5() { // ok
+    private final synchronized int m5() {
         return 2;
     }
 
-    synchronized T m6() { // ok
+    synchronized T m6() {
         return null;
     }
 
-    @SyncAnno synchronized T m7() { // ok
+    @SyncAnno synchronized T m7() {
         return null;
     }
 
-    @SyncAnno private synchronized T m8() { // ok
+    @SyncAnno private synchronized T m8() {
         return null;
     }
 
     // Note that we do not check 'synchronized' as a modifier,
     // convention is to have type parameters of a generic method
     // be surrounded by whitespace.
-    synchronized <T2> void m9() {} // ok
-    synchronized<T2> void m10() {} // ok
+    synchronized <T2> void m9() {}
+    synchronized<T2> void m10() {}
 }
 
 @Target(ElementType.METHOD)
