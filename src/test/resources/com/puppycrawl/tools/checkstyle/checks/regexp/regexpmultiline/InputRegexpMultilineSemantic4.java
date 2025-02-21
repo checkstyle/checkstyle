5857713f570c2b0a47af6187b9474eaba1a44242
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2025-02-21 23:41:54 +0600

    Issue #13213: Remove '//ok' comments for regexpmultiline module

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexpmultiline/InputRegexpMultilineSemantic4.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexpmultiline/InputRegexpMultilineSemantic4.java
@@ -22,7 +22,7 @@ import java.io.File;
  * Test case for detecting simple semantic violations.
  * @author Lars Kühne
  **/
-class InputRegexpMultilineSemantic4 // ok
+class InputRegexpMultilineSemantic4
 {
     /* Boolean instantiation in a static initializer */
     static {
@@ -214,7 +214,7 @@ class InputRegexpMultilineSemantic4 // ok
 
     synchronized void foo() {
         synchronized (this) {} // not OK
-        synchronized (Class.class) { // OK
+        synchronized (Class.class) {
             synchronized (new Object()) {
                 // not OK if checking statements
             }
