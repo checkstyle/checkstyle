49517b4de8f560207093e3ba80a81d6ee33cc83c
Mohit Attry <atrrmohit@gmail.com>
2025-02-24 09:32:26 +0530

    Issue #12817: fix switch-expression when appears on the start of the line

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/indentation/IndentationCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/indentation/IndentationCheckTest.java
@@ -50,6 +50,7 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
 /**
  * Unit test for IndentationCheck.
  */
+// -@cs[JavaNCSS] testing phase.
 public class IndentationCheckTest extends AbstractModuleTestSupport {
 
     private static final Pattern LINE_WITH_COMMENT_REGEX =
@@ -3055,6 +3056,22 @@ public class IndentationCheckTest extends AbstractModuleTestSupport {
         final String fileName = getPath("InputIndentationLineBreakVariableDeclaration.java");
         final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
         verifyWarns(checkConfig, fileName, expected);
+    }
+
+    @Test
+    public void testIndentationSwitchExpressionOnStartOfTheLine() throws Exception {
+        final DefaultConfiguration checkConfig = createModuleConfig(IndentationCheck.class);
+        checkConfig.addProperty("tabWidth", "4");
+        checkConfig.addProperty("basicOffset", "2");
+        checkConfig.addProperty("braceAdjustment", "2");
+        checkConfig.addProperty("caseIndent", "2");
+        checkConfig.addProperty("throwsIndent", "4");
+        checkConfig.addProperty("lineWrappingIndentation", "4");
+
+        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+
+        verifyWarns(checkConfig,
+                getNonCompilablePath("InputIndentationSwitchOnStartOfLine.java"), expected);
     }
 
     @Test
