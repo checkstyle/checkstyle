a6472f91ec2570b814393a29ffef50a587fc2592
Ziad Ahmed <zyadahmed2466@gmail.com>
2025-02-25 15:25:58 +0200

    Issue #14697: Resolved False negative in UnusedImportCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsWithLinkTag.java
@@ -0,0 +1,18 @@
+/*
+UnusedImports
+processJavadoc = (default)true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
+
+import java.util.List; // violation 'Unused import - java.util.List.'
+
+/**
+ * @link List
+ *
+ */
+class InputUnusedImportsWithLinkTag {
+
+}
