86bee50655494441d20bc747f6e02dfdb07f88a2
Vincent Potucek <vincent.potucek@sap.com>
2025-02-26 10:18:46 +0100

    wip

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/UnusedPrivateMethodCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/UnusedPrivateMethodCheckTest.java
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////////////////////////////////////////
 // checkstyle: Checks Java source code and other text files for adherence to a set of rules.
 // Copyright (C) 2001-2025 the original author or authors.
 //
@@ -20,23 +20,9 @@
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
 import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
-import com.puppycrawl.tools.checkstyle.JavaParser;
-import com.puppycrawl.tools.checkstyle.api.DetailAST;
-import com.puppycrawl.tools.checkstyle.api.TokenTypes;
-import com.puppycrawl.tools.checkstyle.internal.utils.TestUtil;
-import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Predicate;
-
-import static com.google.common.truth.Truth.assertWithMessage;
 import static com.puppycrawl.tools.checkstyle.checks.coding.UnusedLocalVariableCheck.MSG_UNUSED_LOCAL_VARIABLE;
-import static com.puppycrawl.tools.checkstyle.checks.coding.UnusedLocalVariableCheck.MSG_UNUSED_NAMED_LOCAL_VARIABLE;
 
 public class UnusedPrivateMethodCheckTest extends AbstractModuleTestSupport {
 
@@ -50,26 +36,33 @@ public class UnusedPrivateMethodCheckTest extends AbstractModuleTestSupport {
         verifyWithInlineConfigParser(getPath("UnusedLocalMethodNoUnused.java"));
     }
 
-//    @Test
-//    public void testUnusedLocalVariable2() throws Exception {
-//        final String[] expected = {
-//            "27:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "sameName"),
-//            "28:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "b"),
-//            "31:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "testInLambdas"),
-//            "33:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "coding"),
-//            "34:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE,
-//                    "InputUnusedLocalVariable"),
-//            "50:13: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "b"),
-//            "54:13: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "c"),
-//            "65:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "b"),
-//            "67:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "c"),
-//            "71:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "p"),
-//            "81:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "f"),
-//            "84:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "foo"),
-//            "91:13: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "a"),
-//        };
-//        verifyWithInlineConfigParser(
-//                getPath("InputUnusedLocalVariable.java"), expected);
-//    }
+    @Test
+    public void UnusedLocalMethodChain() throws Exception {
+        verifyWithInlineConfigParser(getPath("UnusedLocalMethodChain.java"), new String[]{
+                "27:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "sameName"),
+        });
+    }
+
+    @Test
+    public void testUnusedLocalVariable2() throws Exception {
+        final String[] expected = {
+                "27:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "sameName"),
+                "28:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "b"),
+                "31:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "testInLambdas"),
+                "33:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "coding"),
+                "34:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE,
+                        "InputUnusedLocalVariable"),
+                "50:13: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "b"),
+                "54:13: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "c"),
+                "65:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "b"),
+                "67:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "c"),
+                "71:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "p"),
+                "81:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "f"),
+                "84:9: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "foo"),
+                "91:13: " + getCheckMessage(MSG_UNUSED_LOCAL_VARIABLE, "a"),
+        };
+        verifyWithInlineConfigParser(
+                getPath("InputUnusedLocalVariable.java"), expected);
+    }
 
 }
