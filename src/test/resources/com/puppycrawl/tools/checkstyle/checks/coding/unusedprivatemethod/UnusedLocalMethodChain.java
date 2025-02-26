e7abf05fe29576855eace41a9675e59ba30a7198
Vincent Potucek <vincent.potucek@sap.com>
2025-02-26 12:45:45 +0100

    wip

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedprivatemethod/UnusedLocalMethodChain.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedprivatemethod/UnusedLocalMethodChain.java
@@ -6,11 +6,6 @@ package com.puppycrawl.tools.checkstyle.checks.coding.unusedprivatemethod;
 
 public class UnusedLocalMethodChain {
     public UnusedLocalMethodChain() {
-        used();
-    }
-
-    private void used() {
-        used2();
     }
 
     private void used2() {
