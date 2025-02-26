792450095dea1a370e5b17cc3783bd5aadde620b
Vincent Potucek <vincent.potucek@sap.com>
2025-02-26 10:33:57 +0100

    wip

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/UnusedPrivateMethodCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/UnusedPrivateMethodCheckTest.java
@@ -46,9 +46,9 @@ public class UnusedPrivateMethodCheckTest extends AbstractModuleTestSupport {
     @Test
     public void UnusedLocalMethodMulti() throws Exception {
         verifyWithInlineConfigParser(getPath("UnusedLocalMethodMulti.java"), new String[]{
-                "7:18: " + getCheckMessage(MSG_UNUSED_LOCAL_METHOD, "unused1"),
-                "7:18: " + getCheckMessage(MSG_UNUSED_LOCAL_METHOD, "unused2"),
-                "7:18: " + getCheckMessage(MSG_UNUSED_LOCAL_METHOD, "unused3"),
+                "15:18: " + getCheckMessage(MSG_UNUSED_LOCAL_METHOD, "unused1"),
+                "18:18: " + getCheckMessage(MSG_UNUSED_LOCAL_METHOD, "unused2"),
+                "21:18: " + getCheckMessage(MSG_UNUSED_LOCAL_METHOD, "unused3"),
         });
     }
 
