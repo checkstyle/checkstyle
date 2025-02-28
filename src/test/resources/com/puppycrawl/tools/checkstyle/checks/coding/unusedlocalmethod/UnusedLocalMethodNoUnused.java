a6cc4c2cde54146e93a6089fae94e173e9c4286c
Vincent Potucek <vincent.potucek@sap.com>
2025-02-28 10:21:36 +0100

    Issue #16375: (RSPEC-S1144) introduce UnusedLocalMethodCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedlocalmethod/UnusedLocalMethodNoUnused.java
@@ -0,0 +1,14 @@
+/*
+UnusedLocalMethod
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.coding.unusedlocalmethod;
+
+public class UnusedLocalMethodNoUnused {
+    public UnusedLocalMethodNoUnused() {
+        used();
+    }
+
+    private void used() {
+    }
+}
