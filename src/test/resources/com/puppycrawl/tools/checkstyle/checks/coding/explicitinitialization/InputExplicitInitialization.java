f6209bdae2d329f9517bfcdce090d3f53a77285f
Atharv Chavan <atharv55215@gmail.com>
2025-03-03 23:51:55 +0530

    Issue #15456: Define violation messages for all violations

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/explicitinitialization/InputExplicitInitialization.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/explicitinitialization/InputExplicitInitialization.java
@@ -8,26 +8,26 @@ onlyObjectReferences = (default)false
 package com.puppycrawl.tools.checkstyle.checks.coding.explicitinitialization;
 
 public class InputExplicitInitialization {
-    private int x = 0; // violation
-    private Object bar = /* comment test */null; // violation
+    private int x = 0; // violation 'Explicit Initialization'
+    private Object bar = /* comment test */null; // violation 'Explicit Initialization'
     private int y = 1;
     private long y1 = 1 - 1;
     private long y3;
-    private long y4 = 0L; // violation
-    private boolean b1 = false; // violation
+    private long y4 = 0L; // violation 'Explicit Initialization'
+    private boolean b1 = false; // violation 'Explicit Initialization'
     private boolean b2 = true;
     private boolean b3;
     private String str = "";
-    java.lang.String str1 = null, str3 = null; // 2 violations
-    int ar1[] = null; // violation
+    java.lang.String str1 = null, str3 = null; // 2 violations 'Explicit Initialization'
+    int ar1[] = null; // violation 'Explicit Initialization'
     int ar2[] = new int[1];
     int ar3[];
-    float f1 = 0f; // violation
-    double d1 = 0.0; // violation
+    float f1 = 0f; // violation 'Explicit Initialization'
+    double d1 = 0.0; // violation 'Explicit Initialization'
 
     static char ch;
-    static char ch1 = 0; // violation
-    static char ch2 = '\0'; // violation
+    static char ch1 = 0; // violation 'Explicit Initialization'
+    static char ch2 = '\0'; // violation 'Explicit Initialization'
     static char ch3 = '\1';
 
     void method() {
@@ -43,22 +43,22 @@ interface interface1{
 }
 
 class InputExplicitInit2 {
-    private Bar<String> bar = null; // violation
-    private Bar<String>[] barArray = null; // violation
+    private Bar<String> bar = null; // violation 'Explicit Initialization'
+    private Bar<String>[] barArray = null; // violation 'Explicit Initialization'
 }
 
 enum InputExplicitInit3 {
     A,
     B
     {
-        private int x = 0; // violation
-        private Bar<String> bar = null; // violation
-        private Bar<String>[] barArray = null; // violation
+        private int x = 0; // violation 'Explicit Initialization'
+        private Bar<String> bar = null; // violation 'Explicit Initialization'
+        private Bar<String>[] barArray = null; // violation 'Explicit Initialization'
         private int y = 1;
     };
-    private int x = 0; // violation
-    private Bar<String> bar = null; // violation
-    private Bar<String>[] barArray = null; // violation
+    private int x = 0; // violation 'Explicit Initialization'
+    private Bar<String> bar = null; // violation 'Explicit Initialization'
+    private Bar<String>[] barArray = null; // violation 'Explicit Initialization'
     private int y = 1;
     private Boolean booleanAtt = false;
 }
@@ -93,7 +93,7 @@ class Chars {
 class Doubles {
     final double subZero = -0.0;
     final double nan = Double.NaN;
-    private short shortVariable = 0; // violation
-    private byte bite = 0; // violation
-    double d = 0d; // violation
+    private short shortVariable = 0; // violation 'Explicit Initialization'
+    private byte bite = 0; // violation 'Explicit Initialization'
+    double d = 0d; // violation 'Explicit Initialization'
 }
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/explicitinitialization/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/explicitinitialization/Example1.java
@@ -10,23 +10,23 @@ package com.puppycrawl.tools.checkstyle.checks.coding.explicitinitialization;
 
 // xdoc section -- start
 public class Example1 {
-  private int intField1 = 0; // violation
+  private int intField1 = 0; // violation 'Explicit Initialization'
   private int intField2 = 1;
   private int intField3;
 
-  private char charField1 = '\0'; // violation
+  private char charField1 = '\0'; // violation 'Explicit Initialization'
   private char charField2 = 'b';
   private char charField3;
 
-  private boolean boolField1 = false; // violation
+  private boolean boolField1 = false; // violation 'Explicit Initialization'
   private boolean boolField2 = true;
   private boolean boolField3;
 
-  private Object objField1 = null; // violation
+  private Object objField1 = null; // violation 'Explicit Initialization'
   private Object objField2 = new Object();
   private Object objField3;
 
-  private int arrField1[] = null; // violation
+  private int arrField1[] = null; // violation 'Explicit Initialization'
   private int arrField2[] = new int[10];
   private int arrField3[];
 }
