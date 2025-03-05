875c198c41fe1fe4b2859e12b075631c9c323d34
Kushagra-Sedwal <143800287+Kushagra-Sedwal@users.noreply.github.com>
2025-03-05 12:12:29 +0530

    Issue: #16359 Update RegexpHeaderCheckTest to use verifyWithInlineConfigParser

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/header/regexpheader/InputRegexpHeaderNonMatching.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/header/regexpheader/InputRegexpHeaderNonMatching.java
@@ -1,3 +1,13 @@
+/*
+RegexpHeader
+charset = (default)""
+fileExtensions = (default)""
+header = // Nth Line of Header\n// Nth Line of Header\n
+headerFile = (default)(null)
+multiLines = {1}
+
+
+*/
 // First Line of Header
 // Second Line of Header
 // Third Line of Header
