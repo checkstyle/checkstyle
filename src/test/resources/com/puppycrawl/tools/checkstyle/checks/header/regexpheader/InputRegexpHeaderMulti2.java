f59d94795a71a474e13704e9f60dec2c19d34b52
Kushagra-Sedwal <143800287+Kushagra-Sedwal@users.noreply.github.com>
2025-03-05 12:12:29 +0530

    Issue #16359: Update RegexpHeaderCheckTest to use verifyWithInlineConfigParser method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/header/regexpheader/InputRegexpHeaderMulti2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/header/regexpheader/InputRegexpHeaderMulti2.java
@@ -1,3 +1,13 @@
+/*
+RegexpHeader
+charset = (default)""
+fileExtensions = (default)""
+header = (default)(null)
+headerFile = com/puppycrawl/tools/checkstyle/checks/header/regexpheader/InputRegexpHeader2.header
+multiLines = {13, 16}
+
+
+*/
 package com.puppycrawl.tools.checkstyle.checks.header.regexpheader;
 
 import java.awt.*;
