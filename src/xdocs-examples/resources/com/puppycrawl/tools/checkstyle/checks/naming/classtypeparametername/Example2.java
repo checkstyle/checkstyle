05a2f083575a4b34ff408f6a2417947487b729e7
Atharv Chavan <atharv55215@gmail.com>
2025-03-06 07:57:38 +0530

    Issue #15456: Defined violation message for ClassTypeParameterName

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/classtypeparametername/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/classtypeparametername/Example2.java
@@ -12,10 +12,10 @@ package com.puppycrawl.tools.checkstyle.checks.naming.classtypeparametername;
 
 // xdoc section -- start
 class Example2 {
-  class MyClass1<T> {}        // violation
-  class MyClass2<t> {}        // violation
-  class MyClass3<abc> {}      // violation
+  class MyClass1<T> {}        // violation 'Name 'T' must match pattern'
+  class MyClass2<t> {}        // violation 'Name 't' must match pattern'
+  class MyClass3<abc> {}      // violation 'Name 'abc' must match pattern'
   class MyClass4<LISTENER> {}
-  class MyClass5<RequestT> {} // violation
+  class MyClass5<RequestT> {} // violation 'Name 'RequestT' must match pattern'
 }
 // xdoc section -- end
