7f6fd54d81201ddb231fdb7b22cdaf622c5708ee
kornilov-mr <kornilov.mr@gmail.com>
2025-03-07 19:39:06 +0100

    Issue #16358: Update HeaderCheckTest to use verifyWithInlineConfigParser method

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/header/header/InputHeader.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/header/header/InputHeader.java
@@ -1,1 +1,9 @@
-package com.puppycrawl.tools.checkstyle.checks.header.header; class InputHeader {} // One line test
+/*
+Header
+headerFile = (file)InputHeaderjava.header
+ignoreLines = (default)
+
+*/
+// violation 6 lines above 'Missing a header - not enough lines in file'
+
+package com.puppycrawl.tools.checkstyle.checks.header.header; class InputHeader {}
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/header/header/InputHeaderRegexp.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/header/header/InputHeaderRegexp.java
@@ -1,3 +1,8 @@
+/*
+Header
+header = \n    \n
+
+*/
 package com.puppycrawl.tools.checkstyle.checks.header.header;
 
 import java.awt.*;
