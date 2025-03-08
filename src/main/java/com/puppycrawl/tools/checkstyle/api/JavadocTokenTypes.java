f85caae29e7ff50d171b8ac3794e81ffd9a43e42
Sambhav Jain <1501.sambhav@gmail.com>
2025-03-08 15:32:44 +0530

    Issue #14631: Updated P_HTML_TAG_NAME in JavadocTokenTypes.java to new AST format

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
@@ -1128,22 +1128,18 @@ public final class JavadocTokenTypes {
      * <b>Tree:</b>
      * <pre>
      * {@code
-     *  JAVADOC ->; JAVADOC
-     *     |--NEWLINE ->; \r\n
-     *     |--LEADING_ASTERISK ->;      *
-     *     |--TEXT ->;
-     *     |--HTML_ELEMENT ->; HTML_ELEMENT
-     *         `--PARAGRAPH ->; PARAGRAPH
-     *             |--P_TAG_START ->; P_TAG_START
-     *             |   |--START ->; <;
-     *             |   |--P_HTML_TAG_NAME ->; p
-     *             |   `--END ->; >;
-     *             |--TEXT ->; Paragraph Tag.
-     *             `--P_TAG_END ->; P_TAG_END
-     *                 |--START ->; <;
-     *                 |--SLASH ->; /
-     *                 |--P_HTML_TAG_NAME ->; p
-     *                 `--END ->; >;
+     *      `--HTML_ELEMENT -> HTML_ELEMENT
+     *          `--PARAGRAPH -> PARAGRAPH
+     *              |--P_TAG_START -> P_TAG_START
+     *              |   |--START -> <
+     *              |   |--P_HTML_TAG_NAME -> p
+     *              |   `--END -> >
+     *              |--TEXT -> Paragraph Tag.
+     *              `--P_TAG_END -> P_TAG_END
+     *                  |--START -> <
+     *                  |--SLASH -> /
+     *                  |--P_HTML_TAG_NAME -> p
+     *                  `--END -> >
      * }
      * </pre>
      *
@@ -1718,29 +1714,7 @@ public final class JavadocTokenTypes {
     /** End table row tag. */
     public static final int TR_TAG_END = JavadocParser.RULE_trTagEnd + RULE_TYPES_OFFSET;
 
-    /**
-     * Table cell HTML tag.
-     *
-     * <p><b>Example:</b></p>
-     * <pre>{@code <td>Cell Content</td>}</pre>
-     * <b>Tree:</b>
-     * <pre>
-     * {@code
-     * HTML_ELEMENT -> HTML_ELEMENT
-     *    `--TD -> TD
-     *        |--TD_TAG_START -> TD_TAG_START
-     *        |   |--START -> &lt;
-     *        |   |--TD_HTML_TAG_NAME -> td
-     *        |   `--END -> &gt;
-     *        |--TEXT -> Cell Content
-     *        `--TD_TAG_END -> TD_TAG_END
-     *            |--START -> &lt;
-     *            |--SLASH -> /
-     *            |--TD_HTML_TAG_NAME -> td
-     *            `--END -> &gt;
-     * }
-     * </pre>
-     */
+    /** Table cell html tag: {@code <td></td>}. */
     public static final int TD = JavadocParser.RULE_td + RULE_TYPES_OFFSET;
     /** Start table cell tag. */
     public static final int TD_TAG_START = JavadocParser.RULE_tdTagStart + RULE_TYPES_OFFSET;
@@ -1967,34 +1941,32 @@ public final class JavadocTokenTypes {
      * <pre>{@code <table><tbody></tbody></table>}</pre>
      * <b>Tree:</b>
      * <pre>
-     * {@code
-     *    JAVADOC -> JAVADOC
-     *      |--NEWLINE -> \r\n
-     *      |--LEADING_ASTERISK ->  *
-     *      |--TEXT ->
-     *      |--HTML_ELEMENT -> HTML_ELEMENT
-     *      |   `--HTML_TAG -> HTML_TAG
-     *      |       |--HTML_ELEMENT_START -> HTML_ELEMENT_START
-     *      |       |   |--START -> <
-     *      |       |   |--HTML_TAG_NAME -> table
-     *      |       |   `--END -> >
-     *      |       |--HTML_ELEMENT -> HTML_ELEMENT
-     *      |       |   `--TBODY -> TBODY
-     *      |       |       |--TBODY_TAG_START -> TBODY_TAG_START
-     *      |       |       |   |--START -> <
-     *      |       |       |   |--TBODY_HTML_TAG_NAME -> tbody
-     *      |       |       |   `--END -> >
-     *      |       |       `--TBODY_TAG_END -> TBODY_TAG_END
-     *      |       |           |--START -> <
-     *      |       |           |--SLASH -> /
-     *      |       |           |--TBODY_HTML_TAG_NAME -> tbody
-     *      |       |           `--END -> >
-     *      |       `--HTML_ELEMENT_END -> HTML_ELEMENT_END
-     *      |           |--START -> <
-     *      |           |--SLASH -> /
-     *      |           |--HTML_TAG_NAME -> table
-     *      |           `--END -> >
-     *      |--NEWLINE -> \r\n
+     * {JAVADOC -&gt; JAVADOC
+     *      |--NEWLINE -&gt; \r\n
+     *      |--TEXT -&gt; /**
+     *      |--HTML_ELEMENT -&gt; HTML_ELEMENT
+     *      |   `--HTML_TAG -&gt; HTML_TAG
+     *      |       |--HTML_ELEMENT_START -&gt; HTML_ELEMENT_START
+     *      |       |   |--START -&gt; &lt;
+     *      |       |   |--HTML_TAG_NAME -&gt; table
+     *      |       |   `--END -&gt; &gt;
+     *      |       |--HTML_ELEMENT -&gt; HTML_ELEMENT
+     *      |       |   `--TBODY -&gt; TBODY
+     *      |       |       |--TBODY_TAG_START -&gt; TBODY_TAG_START
+     *      |       |       |   |--START -&gt; &lt;
+     *      |       |       |   |--TBODY_HTML_TAG_NAME -&gt; tbody
+     *      |       |       |   `--END -&gt; &gt;
+     *      |       |       `--TBODY_TAG_END -&gt; TBODY_TAG_END
+     *      |       |           |--START -&gt; &lt;
+     *      |       |           |--SLASH -&gt; /
+     *      |       |           |--TBODY_HTML_TAG_NAME -&gt; tbody
+     *      |       |           `--END -&gt; &gt;
+     *      |       `--HTML_ELEMENT_END -&gt; HTML_ELEMENT_END
+     *      |           |--START -&gt; &lt;
+     *      |           |--SLASH -&gt; /
+     *      |           |--HTML_TAG_NAME -&gt; table
+     *      |           `--END -&gt; &gt;
+     *      |--NEWLINE -&gt; \r\n
      * }
      * </pre>
      */
