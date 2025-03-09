716f9ddbc72e88993125d11870bcf9b32b15c595
Vincent Potucek <vincent.potucek@udg.de>
2025-03-09 13:34:28 +0100

    Issue #16375: (RSPEC-S1144) introduce InlineVariableCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/meta/XmlMetaReaderTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/meta/XmlMetaReaderTest.java
@@ -40,20 +40,20 @@ public class XmlMetaReaderTest extends AbstractPathTestSupport {
 
     @Test
     public void test() {
-        assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny()).hasSize(208);
+        assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny()).hasSize(209);
     }
 
     @Test
     public void testDuplicatePackage() {
         assertThat(XmlMetaReader
                     .readAllModulesIncludingThirdPartyIfAny("com.puppycrawl.tools.checkstyle.meta"))
-            .hasSize(208);
+            .hasSize(209);
     }
 
     @Test
     public void testBadPackage() {
         assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny("DOES.NOT.EXIST"))
-            .hasSize(208);
+            .hasSize(209);
     }
 
     @Test
