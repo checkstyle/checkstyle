eaa7bb6bb9281a6c804822f238b3f103602ee5ab
ZaeemSheikh <124173573+SheikhZaeem@users.noreply.github.com>
2025-03-10 11:13:00 +0300

    Issue #14631: Updated DD to new AST format

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
@@ -2414,7 +2414,28 @@ public final class JavadocTokenTypes {
     public static final int COLGROUP_TAG_END = JavadocParser.RULE_colgroupTagEnd
             + RULE_TYPES_OFFSET;
 
-    /** Description of a term html tag: {@code <dd></dd>}. */
+    /**
+     * DD html tag.
+     *
+     * <p><b>Example AST:</b></p>
+     * <pre>{@code <dd>Description content</dd>}</pre>
+     * <pre>
+     * {@code
+     *   --HTML_ELEMENT -> HTML_ELEMENT
+     *      `--DD -> DD
+     *          |--DD_TAG_START -> DD_TAG_START
+     *          |   |--START -> <
+     *          |   |--DD_HTML_TAG_NAME -> dd
+     *          |   `--END -> >
+     *          |--TEXT -> "Description content"
+     *          `--DD_TAG_END -> DD_TAG_END
+     *              |--START -> <
+     *              |--SLASH -> /
+     *              |--DD_HTML_TAG_NAME -> dd
+     *              `--END -> >
+     * }
+     * </pre>
+     */
     public static final int DD = JavadocParser.RULE_dd + RULE_TYPES_OFFSET;
     /** Start description of a term tag. */
     public static final int DD_TAG_START = JavadocParser.RULE_ddTagStart + RULE_TYPES_OFFSET;
