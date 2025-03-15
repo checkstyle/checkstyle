230811109ca03b2593acbc93f4817ca6a8b03fdd
Prarthana-Ramesh <149136492+Prarthana-Ramesh@users.noreply.github.com>
2025-03-15 12:03:48 +0530

    Issue #15456: Specify violation messages for SimplifyBooleanExpressionCheck

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/simplifybooleanexpression/InputSimplifyBooleanExpressionWithWhen.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/simplifybooleanexpression/InputSimplifyBooleanExpressionWithWhen.java
@@ -10,17 +10,17 @@ package com.puppycrawl.tools.checkstyle.checks.coding.simplifybooleanexpression;
 public class InputSimplifyBooleanExpressionWithWhen {
 
     void test(Object o) {
-        if (o instanceof Boolean b && b == true) { } // violation
+        if (o instanceof Boolean b && b == true) { } // violation '"b == true" can be simpified to "b"'
 
         switch (o) {
-            case R(boolean x, _) when x == true -> {} // violation
-            case R(_, boolean y) when y != false -> {} // violation
+            case R(boolean x, _) when x == true -> {} // violation '"x == true" can be simplified to "x"'
+            case R(_, boolean y) when y != false -> {} // violation '"y != false" can be simplified to "y"'
             default -> {}
         }
 
         switch (o) {
-            case R(boolean x, _) when x == false -> {} // violation
-            case R(_, boolean y) when (!(y != true)) -> {} // violation
+            case R(boolean x, _) when x == false -> {} // violation '"x == false" can be simplified to "!x"'
+            case R(_, boolean y) when (!(y != true)) -> {} // violation '"!(y != true)" can be simplified to "y"'
             default -> {}
         }
     }
