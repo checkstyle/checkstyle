430365bedbedc4cd581f654ae3b25074473bdc6d
ZaeemSheikh <124173573+SheikhZaeem@users.noreply.github.com>
2025-03-15 17:26:02 +0300

    Issue #14631: Updated DT to new AST format

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
@@ -2071,7 +2071,32 @@ public final class JavadocTokenTypes {
     /** End description of a term tag. */
     public static final int DD_TAG_END = JavadocParser.RULE_ddTagEnd + RULE_TYPES_OFFSET;
 
-    /** Description term html tag: {@code <dt></dt>}. */
+    /**
+     * DT html tag (definition term in a description list).
+     *
+     * <p><b>Example:</b></p>
+     * <pre>{@code
+     * <dt>Term Name</dt>
+     * }</pre>
+     *
+     * <b>Tree:</b>
+     * <pre>
+     * {@code
+     *   --HTML_ELEMENT -> HTML_ELEMENT
+     *      `--DT -> DT
+     *          |--DT_TAG_START -> DT_TAG_START
+     *          |   |--START -> <
+     *          |   |--DT_HTML_TAG_NAME -> dt
+     *          |   `--END -> >
+     *          |--TEXT -> "Term Name"
+     *          `--DT_TAG_END -> DT_TAG_END
+     *              |--START -> <
+     *              |--SLASH -> /
+     *              |--DT_HTML_TAG_NAME -> dt
+     *              `--END -> >
+     * }
+     * </pre>
+     */
     public static final int DT = JavadocParser.RULE_dt + RULE_TYPES_OFFSET;
     /** Start description term tag. */
     public static final int DT_TAG_START = JavadocParser.RULE_dtTagStart + RULE_TYPES_OFFSET;
