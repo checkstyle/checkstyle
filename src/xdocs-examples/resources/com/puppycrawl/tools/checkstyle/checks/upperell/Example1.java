2d439cb30a69e25fe332114def29a285503dfc9a
Ziad Abd El-Fatah <ziadabdelfattah68@gmail.com>
2025-03-17 07:20:59 +0200

    minor: correct UpperEllCheck violation message

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/upperell/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/upperell/Example1.java
@@ -10,7 +10,7 @@ package com.puppycrawl.tools.checkstyle.checks.upperell;
 // xdoc section -- start
 class Example1 {
   long var1 = 508987; // OK
-  long var2 = 508987l; // violation // violation 'Should use uppercase 'L'.'
+  long var2 = 508987l; // violation 'Should use uppercase 'L'.'
 
   long var3 = 508987L; // OK
 }
