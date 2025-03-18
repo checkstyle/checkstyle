f6f7f2a58f24fc4c3a3e0106268657bd500de30d
Ziad Abd El-Fatah <ziadabdelfattah68@gmail.com>
2025-03-18 03:19:00 +0200

    Issue #11163: Refactored InputUpperEllSemantic.java into smaller files to reduce file size

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/upperell/InputUpperEllEqualsVsHashCode1.java
@@ -0,0 +1,15 @@
+/*
+UpperEll
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.upperell;
+
+public class InputUpperEllEqualsVsHashCode1
+{
+    public boolean equals(int a) // wrong arg type, don't flag
+    {
+        return a == 1;
+    }
+}
