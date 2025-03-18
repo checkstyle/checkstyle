c2036df981e597870cb3fd394d70183f36905664
Vincent Potucek <vincent.potucek@udg.de>
2025-03-18 19:38:59 +0100

    Issue #16543: implement IDE agnostic configuration

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/newlineatendoffile/InputNewlineAtEndOfFileCr.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/newlineatendoffile/InputNewlineAtEndOfFileCr.java
@@ -1,1 +1,20 @@
-/*NewlineAtEndOfFilelineSeparator = CRfileExtensions = (default)""*/package com.puppycrawl.tools.checkstyle.checks.newlineatendoffile;/** * Test case for detection of an existing CR newline at EOF, using the * NewlineAtEndOfFileCheck. * @author Martin Steiger **/public interface InputNewlineAtEndOfFileCr{/** Test with ending with CR newline*/}
+/*
+NewlineAtEndOfFile
+lineSeparator = CR
+fileExtensions = (default)""
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.newlineatendoffile;
+
+/**
+ * Test case for detection of an existing CR newline at EOF, using the
+ * NewlineAtEndOfFileCheck.
+ * @author Martin Steiger
+ **/
+public interface InputNewlineAtEndOfFileCr
+{
+/** Test with ending with CR newline
+*/
+}
