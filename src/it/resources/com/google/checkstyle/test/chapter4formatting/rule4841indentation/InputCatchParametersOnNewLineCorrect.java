501aa8d8be350e4cb27a7fe20d1790d00e797f44
Mohit Attry <atrrmohit@gmail.com>
2025-03-21 13:11:05 +0530

    Issue #14654: fix wrong-indentation for catch parameteres on new line

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4841indentation/InputCatchParametersOnNewLineCorrect.java
@@ -0,0 +1,23 @@
+package com.google.checkstyle.test.chapter4formatting.rule4841indentation;
+
+/** some class. */
+public class InputCatchParametersOnNewLineCorrect {
+
+  /** some test method. */
+  public void test1() {
+    try {
+      System.out.println("try0");
+    } catch (NullPointerException | IllegalArgumentException expected) {
+    }
+  }
+
+  void test2() {
+    try {
+      System.out.println("try");
+    } catch (
+        @SuppressWarnings("PMD.AvoidCatchingGenericException")
+        Exception e) {
+      java.util.logging.Logger.getAnonymousLogger().severe(e.toString());
+    }
+  }
+}
