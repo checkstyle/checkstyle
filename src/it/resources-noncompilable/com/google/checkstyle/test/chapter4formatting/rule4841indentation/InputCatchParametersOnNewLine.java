aeb481c68e906e65a40f242976a3e3c2e694cef2
Mohit Attry <atrrmohit@gmail.com>
2025-03-21 13:11:05 +0530

    Issue #14654: fix wrong-indentation for catch parameters on new line

---
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule4841indentation/InputCatchParametersOnNewLine.java
@@ -0,0 +1,65 @@
+// non-compiled with javac: Compilable with Java17
+
+package com.google.checkstyle.test.chapter4formatting.rule4841indentation;
+
+/** some javadoc. */
+public class InputCatchParametersOnNewLine {
+
+  /** some test Method. */
+  public void test1() {
+    try {
+      System.out.println("try0");
+    } catch (NullPointerException | IllegalArgumentException expected) {
+    }
+  }
+
+  void test2() {
+    try {
+      System.out.println("try");
+    } catch (
+        @SuppressWarnings("PMD.AvoidCatchingGenericException")
+        Exception e) {
+      java.util.logging.Logger.getAnonymousLogger().severe(e.toString());
+    }
+
+    try {
+      System.out.println("try1");
+    } catch (
+        @SuppressWarnings("suppression")
+        Exception e) {
+      java.util.logging.Logger.getAnonymousLogger().severe(e.toString());
+    }
+  }
+
+  void test3() {
+    try {
+      System.out.println("try");
+    } catch (
+        @SuppressWarnings("")
+        Exception e) {
+      java.util.logging.Logger.getAnonymousLogger().severe(e.toString());
+    }
+  }
+
+  void test4() {
+    try {
+      System.out.println("try");
+    } catch (NullPointerException | IllegalArgumentException expected) {
+    }
+  }
+
+  private static String test5() {
+    final String simplePropertyUpdateScript = """s""";
+    return """
+        def newInstance = params.instance;
+        def existingInstance = ctx._source;
+      """ // violation '.* incorrect indentation level 6, expected .* 8.'
+        + simplePropertyUpdateScript;
+  }
+
+  void test2Incorrect(boolean result) {
+    int zk = result
+      ? 0 : // violation '.* incorrect indentation level 6, expected .* 8.'
+      1; // violation '.* incorrect indentation level 6, expected .* 8.'
+  }
+}
