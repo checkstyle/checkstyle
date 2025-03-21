af9860a5927f44abba9c44f2a11b39ce68ca65a4
Harsh-Agrawal96 <harshagrawal9650@gmail.com>
2025-03-21 05:37:35 +0530

    Issue #11641: use of simple name check in aliasList

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/suppresswarningsholder/InputSuppressWarningsHolderAlias3.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/suppresswarningsholder/InputSuppressWarningsHolderAlias3.java
@@ -1,25 +1,44 @@
-/*xml
-<module name="Checker">
-    <module name="SuppressWarningsFilter"/>
-    <module name="TreeWalker">
-        <module name="HiddenField"/>
-        <module name="SuppressWarningsHolder">
-            <property name="aliasList" value="com.puppycrawl.tools.checkstyle.checks.coding.HiddenFieldCheck=LocalVariableHidesMemberVariable"/>
-        </module>
-    </module>
-</module>
+/*
+com.puppycrawl.tools.checkstyle.checks.SuppressWarningsHolder
+aliasList = ParameterNumber=paramnum
+
+com.puppycrawl.tools.checkstyle.filters.SuppressWarningsFilter
+
+com.puppycrawl.tools.checkstyle.checks.sizes.ParameterNumberCheck
+max = (default)7
+
+
 */
 
 package com.puppycrawl.tools.checkstyle.checks.suppresswarningsholder;
 
 public class InputSuppressWarningsHolderAlias3 {
-  private int a;
-  void withOriginal() {
-    @SuppressWarnings({"hiddenfield"})
-    int a = 1;
-  }
-  void withAlias() {
-    @SuppressWarnings("LocalVariableHidesMemberVariable")
-    int a = 1;
-  }
+    public void needsLotsOfParameters0(int a, // violation 'More than 7 parameters (found 8)'
+        int b, int c, int d, int e, int f, int g, int h) {
+        // ...
+    }
+
+    @SuppressWarnings("paramnum")
+    public void needsLotsOfParameters(int a, // filtered violation 'max parameter'
+        int b, int c, int d, int e, int f, int g, int h) {
+        // ...
+    }
+
+    @SuppressWarnings("ParamnumUnknown")
+    public void needsLotsOfParameters2(int a, // violation 'More than 7 parameters (found 8)'
+        int b, int c, int d, int e, int f, int g, int h) {
+        // ...
+    }
+
+    @SuppressWarnings("PAramnuM")
+    public void needsLotsOfParameters3(int a, // filtered violation 'max parameter'
+        int b, int c, int d, int e, int f, int g, int h) {
+        // ...
+    }
+
+    @SuppressWarnings("PARAMNUM")
+    public void needsLotsOfParameters4(int a, // filtered violation 'max parameter'
+        int b, int c, int d, int e, int f, int g, int h) {
+        // ...
+    }
 }
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/suppresswarningsholder/InputSuppressWarningsHolderAlias5.java
@@ -0,0 +1,25 @@
+/*xml
+<module name="Checker">
+    <module name="SuppressWarningsFilter"/>
+    <module name="TreeWalker">
+        <module name="HiddenField"/>
+        <module name="SuppressWarningsHolder">
+            <property name="aliasList" value="com.puppycrawl.tools.checkstyle.checks.coding.HiddenFieldCheck=LocalVariableHidesMemberVariable"/>
+        </module>
+    </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.suppresswarningsholder;
+
+public class InputSuppressWarningsHolderAlias5 {
+    private int a;
+    void withOriginal() {
+      @SuppressWarnings({"hiddenfield"})
+      int a = 1;
+    }
+    void withAlias() {
+      @SuppressWarnings("LocalVariableHidesMemberVariable")
+      int a = 1;
+    }
+}
