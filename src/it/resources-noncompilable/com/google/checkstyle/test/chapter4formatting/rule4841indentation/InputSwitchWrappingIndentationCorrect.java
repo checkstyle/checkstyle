d1c81cbd8d4e664989b2707d969e9dc16752b0c9
MedAzizFrigui <azizfrigui08@gmail.com>
2025-03-24 04:53:18 +0100

    Issue #15098: Indentation of switch block when no braces is now validated

---
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule4841indentation/InputSwitchWrappingIndentationCorrect.java
@@ -0,0 +1,50 @@
+package com.google.checkstyle.test.chapter4formatting.rule42blockindentation;
+
+import java.util.List;
+
+/**some javadoc.*/
+public class InputSwitchWrappingIndentationCorrect {
+  String testMethod1(int i) {
+    String name = "";
+    name =
+        switch (i) {
+          case 1 -> "one";
+          case 2 -> "two";
+          default -> "zero";
+        };
+    return name;
+  }
+
+  String testMethod2(int x, int y) {
+    return switch (x) {
+      case 1 ->
+          switch (y) {
+            case 2 -> "test";
+            default -> "inner default";
+          };
+      default -> "outer default";
+    };
+  }
+
+  String testMethod3(List<Integer> operations) {
+    return operations.stream()
+        .map(
+            op ->
+                switch (op) {
+                  case 1 -> "test";
+                  default -> "TEST";
+                })
+        .findFirst().orElse("defaultValue");
+  }
+
+  String testMethod4(int x, int y) {
+    return switch (x) {
+      case 1 ->
+          switch (y) {
+            case 2 -> "test";
+            default -> "inner default";
+          };
+      default -> "outer default";
+    };
+  }
+}
