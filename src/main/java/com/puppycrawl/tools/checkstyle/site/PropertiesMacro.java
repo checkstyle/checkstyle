0b3174f463c2502b9545c8f2221317ae0d721521
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-04-02 00:37:57 +0530

    Issue #16155: use nio api in PropertiesMacro
    Issue #16880: Update IndentationCheckTest.testAnnotationIncorrect to use verifyWarns
    
    Issue #14631: Update Javadoc for FRAME_HTML_TAG_NAME with example and tree

--- src/main/java/com/puppycrawl/tools/checkstyle/site/PropertiesMacro.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/site/PropertiesMacro.java
@@ -19,8 +19,9 @@
 
 package com.puppycrawl.tools.checkstyle.site;
 
-import java.io.File;
 import java.lang.reflect.Field;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -112,7 +113,7 @@ public class PropertiesMacro extends AbstractMacro {
     private static String currentModuleName = "";
 
     /** The file of the current module being processed. */
-    private static File currentModuleFile = new File("");
+    private static Path currentModulePath = Paths.get("");
 
     @Override
     public void execute(Sink sink, MacroRequest request) throws MacroExecutionException {
@@ -132,11 +133,21 @@ public class PropertiesMacro extends AbstractMacro {
      * Configures the global properties for the current module.
      *
      * @param modulePath the path of the current module processed.
+     * @throws MacroExecutionException if the module path is invalid.
      */
-    private static void configureGlobalProperties(String modulePath) {
-        final File moduleFile = new File(modulePath);
-        currentModuleFile = moduleFile;
-        currentModuleName = CommonUtil.getFileNameWithoutExtension(moduleFile.getName());
+    private static void configureGlobalProperties(String modulePath)
+            throws MacroExecutionException {
+        final Path modulePathObj = Paths.get(modulePath);
+        currentModulePath = modulePathObj;
+        final Path fileNamePath = modulePathObj.getFileName();
+
+        if (fileNamePath == null) {
+            throw new MacroExecutionException(
+                "Invalid modulePath '" + modulePath + "': No file name present.");
+        }
+
+        currentModuleName = CommonUtil.getFileNameWithoutExtension(
+            fileNamePath.toString());
     }
 
     /**
@@ -203,7 +214,7 @@ public class PropertiesMacro extends AbstractMacro {
 
         final Set<String> properties = SiteUtil.getPropertiesForDocumentation(clss, instance);
         final Map<String, DetailNode> propertiesJavadocs = SiteUtil
-                .getPropertiesJavadocs(properties, currentModuleName, currentModuleFile);
+                .getPropertiesJavadocs(properties, currentModuleName, currentModulePath);
 
         final List<String> orderedProperties = orderProperties(properties);
 
