03ae65713ad09b6d7b2a99a9c8b8560334a09fb9
Anmol <anmol20102005@gmail.com>
2025-04-04 03:32:22 +0530

    Issue #16378: Validate that all Inputs mentioned all default properties

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/modifier/redundantmodifier/InputRedundantModifierStaticMethodInInterface.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/modifier/redundantmodifier/InputRedundantModifierStaticMethodInInterface.java
@@ -2,6 +2,7 @@
 RedundantModifier
 tokens = (default)METHOD_DEF, VARIABLE_DEF, ANNOTATION_FIELD_DEF, INTERFACE_DEF, \
          CTOR_DEF, CLASS_DEF, ENUM_DEF, RESOURCE
+jdkVersion = (default)22
 
 
 */
