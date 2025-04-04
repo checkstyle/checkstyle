46705fc4cca6ad0537969a31f2ec8cec9d341cfa
Vincent Potucek <vpotucek@me.com>
2025-04-04 09:53:19 +0200

    findFirstUpperNamedBlock

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/FinalLocalVariableCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/FinalLocalVariableCheck.java
@@ -695,14 +695,12 @@ public class FinalLocalVariableCheck extends AbstractCheck {
      * @param ast Variable for which we want to find the scope in which it is defined
      * @return ast The Class or Constructor or Method in which it is defined.
      */
-    // -@cs[ReturnCount] recursion is a very usefully tool in it
     private static DetailAST findFirstUpperNamedBlock(DetailAST ast) {
-        if (TokenUtil.isOfType(ast, TokenTypes.METHOD_DEF, TokenTypes.CLASS_DEF,
-                TokenTypes.ENUM_DEF, TokenTypes.CTOR_DEF, TokenTypes.COMPACT_CTOR_DEF)
-                || ScopeUtil.isClassFieldDef(ast)) {
-            return ast;
-        }
-        return findFirstUpperNamedBlock(ast.getParent());
+        return TokenUtil.isOfType(ast, TokenTypes.METHOD_DEF, TokenTypes.CLASS_DEF,
+            TokenTypes.ENUM_DEF, TokenTypes.CTOR_DEF, TokenTypes.COMPACT_CTOR_DEF)
+            || ScopeUtil.isClassFieldDef(ast)
+            ? ast
+            : findFirstUpperNamedBlock(ast.getParent());
     }
 
     /**
