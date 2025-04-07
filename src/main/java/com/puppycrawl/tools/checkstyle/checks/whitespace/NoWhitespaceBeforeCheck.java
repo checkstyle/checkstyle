865054f557580876bc51415fd3a080d00b852743
Vincent Potucek <vpotucek@me.com>
2025-04-07 15:25:59 +0200

    tab wip

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/whitespace/NoWhitespaceBeforeCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/whitespace/NoWhitespaceBeforeCheck.java
@@ -45,10 +45,13 @@ public class NoWhitespaceBeforeCheck extends AbstractCheck {
      */
     private static final List<String> COMMON_WHITESPACE_PATTERNS = Arrays.asList(
         " ;",
+		"	;",
         " (",
+		"	(",
         "  = ",
         " =  ",
         ". (",
+		".	(",
         ".  (",
         "\" ."
     );
