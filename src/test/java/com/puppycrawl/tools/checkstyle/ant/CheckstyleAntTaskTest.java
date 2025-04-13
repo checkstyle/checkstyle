64695c04a59ad61569725826e78d61c7585a6e42
atharv <atharv55215@gmail.com>
2025-04-13 22:49:07 +0530

    Issue #14019: Covered pitest survivals with tests for CheckstyleAntTask

--- src/test/java/com/puppycrawl/tools/checkstyle/ant/CheckstyleAntTaskTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/ant/CheckstyleAntTaskTest.java
@@ -978,4 +978,20 @@ public class CheckstyleAntTaskTest extends AbstractPathTestSupport {
         return Long.parseLong(matcher.group(1));
     }
 
+    @Test
+    public void testMaxWarningDefault() throws IOException {
+        final CheckstyleAntTask antTask = getCheckstyleAntTask();
+        final File inputFile = new File(getPath(WARNING_INPUT));
+        final Location fileLocation = new Location("build.xml", 42, 10);
+
+        antTask.setFile(inputFile);
+        antTask.setLocation(fileLocation);
+        try {
+            antTask.execute();
+        }
+        catch (BuildException ex) {
+            assertWithMessage("No exception expected").fail();
+        }
+    }
+
 }
