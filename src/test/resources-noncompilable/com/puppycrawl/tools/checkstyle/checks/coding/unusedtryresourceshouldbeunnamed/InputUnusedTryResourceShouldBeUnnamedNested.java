ac7114cb2c3f13f7d1178687d8df6bb248d3ab74
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2025-04-13 11:05:58 +0100

    Issue #15484: New check UnusedTryResourceShouldBeUnnamed

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/unusedtryresourceshouldbeunnamed/InputUnusedTryResourceShouldBeUnnamedNested.java
@@ -0,0 +1,170 @@
+/*
+UnusedTryResourceShouldBeUnnamed
+
+*/
+
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.coding.unusedtryresourceshouldbeunnamed;
+
+public class InputUnusedTryResourceShouldBeUnnamedNested {
+
+    void testNested() {
+        try (AutoCloseable a = lock()){
+            System.out.println(a);
+            // violation below, 'Unused try resource 'b' should be unnamed'
+            try (AutoCloseable b = lock()){
+
+            } catch (Exception e){
+                System.out.println(e);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void testNested2() {
+        try {
+            try {
+                try {
+                    // violation below, 'Unused try resource 'a' should be unnamed'
+                    try (AutoCloseable a = lock()){
+
+                    } catch (Exception e){
+                        System.out.println(e);
+                    }
+
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            } catch (Exception e){
+                System.out.println(e);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void testNested3() {
+        try (AutoCloseable a = lock()){
+            System.out.println(a);
+            // violation below, 'Unused try resource 'b' should be unnamed'
+            try (AutoCloseable b = lock()){
+                try (AutoCloseable c = lock()){
+                    System.out.println(c);
+                    // violation below, 'Unused try resource 'd' should be unnamed'
+                    try (AutoCloseable d = lock()){
+
+                    } catch (Exception e){
+                        System.out.println(e);
+                    }
+
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            } catch (Exception e){
+                System.out.println(e);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void testNested4() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try (AutoCloseable a = lock()){
+                    System.out.println(a);
+                    // violation below, 'Unused try resource 'b' should be unnamed'
+                    try (AutoCloseable b = lock()){
+                        try (AutoCloseable c = lock()){
+                            System.out.println(c);
+                            // violation below, 'Unused try resource 'd' should be unnamed'
+                            try (AutoCloseable d = lock()){
+
+                            } catch (Exception e){
+                                System.out.println(e);
+                            }
+
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                    } catch (Exception e){
+                        System.out.println(e);
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        };
+    }
+
+    void testNested5() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try (AutoCloseable a = lock()){
+                    System.out.println(a);
+                    // violation below, 'Unused try resource 'b' should be unnamed'
+                    try (AutoCloseable b = lock()){
+                        try (AutoCloseable c = lock()){
+                            System.out.println(c);
+                            Runnable runnable2 = new Runnable() {
+                                @Override
+                                public void run() {
+                                    // violation below, 'Unused try resource 'd' should be unnamed'
+                                    try (AutoCloseable d = lock()){
+
+                                    } catch (Exception e){
+                                        System.out.println(e);
+                                    }
+                                }
+                            };
+
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                    } catch (Exception e){
+                        System.out.println(e);
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        };
+    }
+
+    void testNested6(){
+        try (AutoCloseable a = lock()) {
+            // violation below, 'Unused try resource 'b' should be unnamed'
+            try (AutoCloseable b = a) {
+                try (AutoCloseable c = lock()) {
+                    try (AutoCloseable d = c) {
+                        // violation below, 'Unused try resource 'e' should be unnamed'
+                        try (AutoCloseable e = d) {
+                            // FIXME: Flase Negative here, 'e' try-resource is unused but considered used from catch clause.
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    AutoCloseable lock() {
+        return null;
+    }
+}
