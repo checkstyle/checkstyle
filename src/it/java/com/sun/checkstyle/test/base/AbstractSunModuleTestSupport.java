e123d1262e28655a6da4718245edc0fee704bddc
Vincent Potucek <vpotucek@me.com>
2025-04-13 20:07:43 +0200

    Pull #16855: 'AbstractSunModuleTestSupport' fix format

--- src/it/java/com/sun/checkstyle/test/base/AbstractSunModuleTestSupport.java
+++ src/it/java/com/sun/checkstyle/test/base/AbstractSunModuleTestSupport.java
@@ -42,7 +42,7 @@ public abstract class AbstractSunModuleTestSupport extends AbstractItModuleTestS
     static {
         try {
             CONFIGURATION = ConfigurationLoader.loadConfiguration(XML_NAME,
-                    new PropertiesExpander(System.getProperties()));
+                new PropertiesExpander(System.getProperties()));
         }
         catch (CheckstyleException ex) {
             throw new IllegalStateException(ex);
