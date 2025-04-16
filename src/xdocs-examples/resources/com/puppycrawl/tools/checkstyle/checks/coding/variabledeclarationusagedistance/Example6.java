01a02fef021e44e06b76362e1191a76440d36fb8
MD. Zaid <146553796+Zed69696969@users.noreply.github.com>
2025-04-16 12:21:51 +0530

    Issue #13155: update the docs example for block initialization methods

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/variabledeclarationusagedistance/Example6.java
@@ -0,0 +1,37 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="VariableDeclarationUsageDistance"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.variabledeclarationusagedistance;
+
+import java.util.Calendar;
+// xdoc section -- start
+public class Example6 {
+
+  public void calSet(long timeNow, int hh, int min) {
+    int minutes = min + 5; // Ok, No violation reported
+    Calendar cal = Calendar.getInstance();
+    cal.setTimeInMillis(timeNow);
+    cal.set(Calendar.SECOND, 0);
+    cal.set(Calendar.MILLISECOND, 0);
+    cal.set(Calendar.HOUR_OF_DAY, hh);
+    cal.set(Calendar.MINUTE, minutes);
+  }
+
+  public void calSetAgain(long timeNow, int hh, int min){
+    // violation below, 'variable ''minutes'' declaration and its first usage is 6.'
+    int minutes = min + 5000;
+    Calendar cal = Calendar.getInstance();
+    cal.setTimeInMillis(timeNow);
+    cal.set(Calendar.SECOND, 0);
+    cal.set(Calendar.MILLISECOND, 0);
+    cal.set(Calendar.HOUR_OF_DAY, hh);
+    System.out.println("Hello World");
+    cal.set(Calendar.MINUTE, minutes);
+  }
+}
+// xdoc section -- end
