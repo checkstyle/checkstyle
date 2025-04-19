6d8eca1c6ab4f5dab1efcc78e311f321e27f6f06
Vincent Potucek <vpotucek@me.com>
2025-04-19 21:44:36 +0200

    Pull #16872: api: add new int process(List<Path> files) method to RootModule - add api

--- src/main/java/com/puppycrawl/tools/checkstyle/api/RootModule.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/RootModule.java
@@ -44,7 +44,7 @@ public interface RootModule extends Configurable {
      * @see #destroy()
      * @deprecated Use {@link #process(Collection)}
      */
-    @Deprecated(since = "10.23.0")
+    @Deprecated(since = "10.23.1")
     int process(List<File> files) throws CheckstyleException;
 
     /**
