7094d6958e218ff7855fda39c0c1bc4a1f2f49da
Usman Shaikh <shaikhu@gmail.com>
2025-04-22 09:06:36 +0100

    Issue #16738: Update SuppressionCommentFilterExamplesTest methods to use verifyFilterWithInlineConfigParser

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressioncommentfilter/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressioncommentfilter/Example1.java
@@ -16,14 +16,14 @@ class Example1
   int VAR1; // violation, Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'
 
   //CHECKSTYLE:OFF
-  int VAR2; // suppressed violation
+  int VAR2; // filtered violation 'must match pattern'
   //CHECKSTYLE:ON
 
   public static final int var3 = 1;
   // violation above, 'must match pattern'
 
   //CHECKSTYLE:OFF
-  public static final int var4 = 1; // suppressed violation
+  public static final int var4 = 1; // filtered violation 'must match pattern'
   //CHECKSTYLE:ON
 
   public void method1()
@@ -34,8 +34,10 @@ class Example1
     //CHECKSTYLE:OFF
 
     try {}
-    catch(Exception ex) {} // suppressed violation
-    catch(Error err) {} // suppressed violation
+    catch(Exception ex) {}
+    // filtered violation above 'Catching 'Exception' is not allowed'
+    catch(Error err) {}
+    // filtered violation above 'Catching 'Error' is not allowed'
 
     //CHECKSTYLE:ON
   }
