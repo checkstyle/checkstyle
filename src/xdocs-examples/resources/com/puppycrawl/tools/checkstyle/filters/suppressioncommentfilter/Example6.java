e4d0f53878644183148e5bd09ddb6dce3ec0f468
Usman Shaikh <shaikhu@gmail.com>
2025-04-22 09:06:36 +0100

    Issue #16738: Update SuppressionCommentFilterExamplesTest methods to use verifyFilterWithInlineConfigParser

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressioncommentfilter/Example6.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressioncommentfilter/Example6.java
@@ -25,14 +25,14 @@ class Example6
   int VAR1; // violation, Name 'VAR1' must match pattern '^[a-z][a-zA-Z0-9]*$'
 
   //CSOFF MemberID
-  int VAR2; // suppressed violation
+  int VAR2; // filtered violation 'must match pattern'
   //CSON: MemberID
 
   public static final int var3 = 1;
   // violation above, 'must match pattern'
 
   //CSOFF ConstantID
-  public static final int var4 = 1; // suppressed violation
+  public static final int var4 = 1; // filtered violation 'must match pattern'
   //CSON ConstantID
 
   public void method1()
@@ -43,8 +43,10 @@ class Example6
     //CSOFF IllegalID
 
     try {}
-    catch(Exception ex) {} // suppressed violation
-    catch(Error err) {} // suppressed violation
+    catch(Exception ex) {}
+    // filtered violation above 'Catching 'Exception' is not allowed'
+    catch(Error err) {}
+    // filtered violation above 'Catching 'Error' is not allowed'
 
     //CSON IllegalID
   }
