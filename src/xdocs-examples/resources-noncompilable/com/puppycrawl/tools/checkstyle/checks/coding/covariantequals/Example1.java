e6119e565f3026b7783b354376faaf72d5bc7c8e
Kalash Sharma <kalash.strt@gmail.com>
2025-04-23 20:15:10 +0530

    Issue #15456: Specity violation message for CovariantEqualsCheck

--- src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/covariantequals/Example1.java
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/covariantequals/Example1.java
@@ -10,12 +10,12 @@ package com.puppycrawl.tools.checkstyle.checks.coding.covariantequals;
 
 // xdoc section -- start
 public class Example1 {
-  public boolean equals(Example1 same) {  // violation
+  public boolean equals(Example1 same) {  // violation 'covariant equals'
     return false;
   }
 
   record Test(String str) {
-    public boolean equals(Test same) {  // violation
+    public boolean equals(Test same) {  // violation 'covariant equals'
       return false;
     }
   }
