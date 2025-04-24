b051cbffb0278727573ef12100212bc2020dfe7f
Prarthana-Ramesh <149136492+Prarthana-Ramesh@users.noreply.github.com>
2025-04-24 18:45:59 +0530

    Issue #16880 IndentationCheckTest updated to use verifyWarns

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationAnnotationIncorrect.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationAnnotationIncorrect.java
@@ -8,15 +8,15 @@ class InputIndentationAnnotationIncorrect { //indent:0 exp:0
 
     @MyAnnotation2 //indent:4 exp:4
     @MyAnnotation1 //indent:4 exp:4
-    (value = "") //indent:4 exp:8
+    (value = "") //indent:4 exp:8 warn
     class innerClass { //indent:4 exp:4
         @MyAnnotation2 @MyAnnotation1 //indent:8 exp:8
-        (value = "") //indent:8 exp:12
+        (value = "") //indent:8 exp:12 warn
         public int a; //indent:8 exp:8
     } //indent:4 exp:4
 
     @MyAnnotation2 @MyAnnotation1 //indent:4 exp:4
-    (value = "") //indent:4 exp:8
+    (value = "") //indent:4 exp:8 warn
     class InputIndentationAnnotationInnerClass { //indent:4 exp:4
 
     } //indent:4 exp:4
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationInvalidArrayInitIndent1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationInvalidArrayInitIndent1.java
@@ -16,60 +16,60 @@ package com.puppycrawl.tools.checkstyle.checks.indentation.indentation; //indent
  */                                                                           //indent:1 exp:1
 public class InputIndentationInvalidArrayInitIndent1 { //indent:0 exp:0
 
-    private void func1(int[] arg) {
+    private void func1(int[] arg) {                                      //indent:4 exp:4
 
-    }
+    }                                                                    //indent:4 exp:4
 
 
-    /** Creates a new instance of InputIndentationValidArrayInitIndent */
-    public InputIndentationInvalidArrayInitIndent1() {
+    /** Creates a new instance of InputIndentationValidArrayInitIndent *///indent:4 exp:4
+    public InputIndentationInvalidArrayInitIndent1() {                   //indent:4 exp:4
 
-        func1(new int[] {
-        1, 2, 3
-        });
-    }
+        func1(new int[] {                                                //indent:8 exp:8
+        1, 2, 3                                                          //indent:8 exp:12 warn
+        });                                                              //indent:8 exp:8
+    }                                                                    //indent:4 exp:4
 
-    private static char[] sHexChars = {
-        '0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    private static char[] sHexChars = {                                  //indent:4 exp:4
+        '0', '1', '2', '3', '4', '5', '6', '7',                          //indent:8 exp:8
+        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };                        //indent:8 exp:8
 
-    private void myFunc3()
+    private void myFunc3()                                               //indent:4 exp:4
     { //indent:4 exp:4
-        char[] sHexChars2 = {
-          '0', '1', '2', '3', '4', '5', '6', '7',
-              '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        char[] sHexChars2 = {                                         //indent:8 exp:8
+          '0', '1', '2', '3', '4', '5', '6', '7',                     //indent:10 exp:12,32,70 warn
+              '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };               //indent:14 exp:12,32,70 warn
 
-        char[] sHexChars3 = {
-          '0', '1', '2', '3', '4', '5', '6', '7',
-              '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-      };
+        char[] sHexChars3 = {                                         //indent:8 exp:8
+          '0', '1', '2', '3', '4', '5', '6', '7',                     //indent:10 exp:12,32,70 warn
+              '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'                  //indent:14 exp:12,32,70 warn
+      };                                                              //indent:6 exp:8,12 warn
 
-        char[] sHexChars4 =
-      {
-              '0', '1', '2', '3', '4', '5', '6', '7',
-          '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-      };
+        char[] sHexChars4 =                                           //indent:8 exp:8
+      {                                                               //indent:6 exp:8,12 warn
+              '0', '1', '2', '3', '4', '5', '6', '7',                 //indent:14 exp:10,12,70 warn
+          '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'                      //indent:10 exp:10
+      };                                                              //indent:6 exp:8,12 warn
 
-    }
-    interface SomeInterface1 {
-        @interface SomeAnnotation1 {
-            String[] values();
-            String[] description() default "";
-            String[] description2() default { "hello",
-            "checkstyle"};
-        }
-        @InputIndentationInvalidArrayInitIndent1.SomeInterface1.SomeAnnotation1(values =
-            {
-              "hello",
-            "checkstyle"
-            },
-            description = { "hello",
-              "checkstyle"
-        },
-            description2 = {
-            "hello", "chekstyle"
-            }
+    }                                                                 //indent:4 exp:4
+    interface Interface1 {                                        //indent:4 exp:4
+        @interface Annotation1 {                                  //indent:8 exp:8
+            String[] values();                                        //indent:12 exp:12
+            String[] description() default "";                        //indent:12 exp:12
+            String[] description2() default { "hello",                //indent:12 exp:12
+            "checkstyle"};                                            //indent:12 exp:16,46,48 warn
+        }                                                             //indent:8 exp:8
+        @InputIndentationInvalidArrayInitIndent1.Interface1.Annotation1(values = //indent:8 exp:8
+            {                                                            //indent:12 exp:12
+              "hello",                                               //indent:14 exp:12,16,73 warn
+            "checkstyle"                                             //indent:12 exp:12
+            },                                                       //indent:12 exp:12
+            description = { "hello",                                 //indent:12 exp:12
+              "checkstyle"                                           //indent:14 exp:16,28,30 warn
+        },                                                           //indent:8 exp:12,16 warn
+            description2 = {                                         //indent:12 exp:12
+            "hello", "chekstyle"                                     //indent:12 exp:16,31,73 warn
+            }                                                        //indent:12 exp:12
         ) //indent:8 exp:8
-      void worked();
-    }
+      void worked();                                                     //indent:6 exp:6
+    }                                                                    //indent:4 exp:4
 } //indent:0 exp:0
