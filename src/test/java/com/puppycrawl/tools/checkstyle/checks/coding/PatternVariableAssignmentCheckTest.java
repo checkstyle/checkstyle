89419d9dd1aa1817db07c5e1ec398e327c21f9eb
SteLeo1602 <stepanleontev16@gmail.com>
2025-04-24 20:55:58 -0700

    Issue #14949: Added PatternVariableAssignmentCheck for Java 21 Record Pattern

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/PatternVariableAssignmentCheckTest.java
@@ -0,0 +1,53 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2025 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+import static com.puppycrawl.tools.checkstyle.checks.coding.PatternVariableAssignmentCheck.MSG_KEY;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+
+public class PatternVariableAssignmentCheckTest extends AbstractModuleTestSupport {
+
+    @Override
+    protected String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/checks/coding/patternvariableassignment";
+    }
+
+    @Test
+    public void testUnnecessaryNullCheckWithInstanceof() throws Exception {
+
+        final String[] expected = {
+            "27:13: " + getCheckMessage(MSG_KEY, "s"),
+            "31:13: " + getCheckMessage(MSG_KEY, "x"),
+            "33:13: " + getCheckMessage(MSG_KEY, "y"),
+            "37:13: " + getCheckMessage(MSG_KEY, "c"),
+            "41:13: " + getCheckMessage(MSG_KEY, "c"),
+            "45:13: " + getCheckMessage(MSG_KEY, "c"),
+            "50:21: " + getCheckMessage(MSG_KEY, "d"),
+            "62:14: " + getCheckMessage(MSG_KEY, "f"),
+        };
+
+        verifyWithInlineXmlConfig(getNonCompilablePath(
+            "InputPatternVariableAssignmentCheck1.java"), expected);
+    }
+
+}
