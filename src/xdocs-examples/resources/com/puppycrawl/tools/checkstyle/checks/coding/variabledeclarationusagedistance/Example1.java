f55ca9316fb38731aa46b561fe42d6eaebd53d9f
Praveen Kumar <praveen729483@gmail.com>
2025-04-26 01:00:23 +0530

    Issue #13155: updated the VariableDeclarationUsageDistance check documentation

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/variabledeclarationusagedistance/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/variabledeclarationusagedistance/Example1.java
@@ -13,8 +13,9 @@ package com.puppycrawl.tools.checkstyle.checks.coding.variabledeclarationusagedi
 public class Example1 {
 
   public void foo1() {
-    int num;        // violation, distance = 4
-    final double PI;   // ok, final variables not checked
+    // violation below, 'variable 'num' declaration and its first usage is 4.'
+    int num;
+    final double PI;   // OK, final variables not checked
     System.out.println("Statement 1");
     System.out.println("Statement 2");
     System.out.println("Statement 3");
@@ -23,9 +24,9 @@ public class Example1 {
   }
 
   public void foo2() {
-    int a;          // ok, used in different scope
-    int b;          // ok, used in different scope
-    int count = 0;  // ok, used in different scope
+    int a;          // OK, used in different scope
+    int b;          // OK, used in different scope
+    int count = 0;  // OK, used in different scope
 
     {
       System.out.println("Inside inner scope");
