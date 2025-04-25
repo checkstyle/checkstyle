a304133e9cb90bda39baa1ddb5bd07b432d6d8df
SteLeo1602 <stepanleontev16@gmail.com>
2025-04-24 20:55:58 -0700

    Issue #14949: Added PatternVariableAssignmentCheck for Java 21 Record Pattern

---
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/PatternVariableAssignmentCheckExamplesTest.java
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2025 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+import org.junit.jupiter.api.Test;
+
+import static com.puppycrawl.tools.checkstyle.checks.coding.PatternVariableAssignmentCheck.MSG_KEY;
+
+import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
+
+public class PatternVariableAssignmentCheckExamplesTest extends AbstractExamplesModuleTestSupport {
+    @Override
+    protected String getPackageLocation() {
+      return "com/puppycrawl/tools/checkstyle/checks/coding/patternvariableassignment";
+    }
+
+    @Test
+    public void testUnnecessaryNullCheckWithInstanceof() throws Exception {
+
+        final String[] expected = {
+            "19:15: " + getCheckMessage(MSG_KEY, "s"),
+            "23:15: " + getCheckMessage(MSG_KEY, "x"),
+            "26:15: " + getCheckMessage(MSG_KEY, "c"),
+        };
+
+        verifyWithInlineXmlConfig(getNonCompilablePath(
+            "Example1.java"), expected);
+    }
+}
