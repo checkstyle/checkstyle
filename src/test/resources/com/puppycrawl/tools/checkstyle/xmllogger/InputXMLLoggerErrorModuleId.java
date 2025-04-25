a85ffa9b1fed9f6ab18f815be1cac7991ccafddc
Praveen Kumar <praveen729483@gmail.com>
2025-04-26 01:00:23 +0530

    Issue #16360: Migrate XMLLoggerTest.testErrorModuleId to use inlineConfigParser

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/xmllogger/InputXMLLoggerErrorModuleId.java
@@ -0,0 +1,17 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module
+        name="com.puppycrawl.tools.checkstyle.checks.naming.MethodNameCheck">
+       <property name="id" value="MethodNameCheck"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.xmllogger;
+
+public class InputXMLLoggerErrorModuleId {
+
+    public void Method() {}
+}
