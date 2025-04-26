94da23e0f46c584c9fd68be15e9e0264084195ae
Vincent Potucek <vpotucek@me.com>
2025-04-26 20:43:20 +0200

    Issue #16857: api: add new int process(List<Path> files) method to RootModule - add api

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/testmodules/CheckstyleAntTaskStubFileMock.java
@@ -0,0 +1,143 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2025 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.internal.testmodules;
+
+import java.io.File;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+public class CheckstyleAntTaskStubFileMock implements Path {
+
+    @Override
+    public File toFile() {
+        return new File("mock") {
+            /** A unique serial version identifier. */
+            private static final long serialVersionUID = -2903929010510199407L;
+
+            /** This method is overridden to simulate an exception. */
+            @Override
+            public long lastModified() {
+                throw new SecurityException("mock");
+            }
+        };
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public FileSystem getFileSystem() {
+        return null;
+    }
+
+    @Override
+    public boolean isAbsolute() {
+        return false;
+    }
+
+    @Override
+    public Path getRoot() {
+        return null;
+    }
+
+    @Override
+    public Path getFileName() {
+        return null;
+    }
+
+    @Override
+    public Path getParent() {
+        return null;
+    }
+
+    @Override
+    public int getNameCount() {
+        return 0;
+    }
+
+    @Override
+    public Path getName(int i) {
+        return null;
+    }
+
+    @Override
+    public Path subpath(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public boolean startsWith(Path path) {
+        return false;
+    }
+
+    @Override
+    public boolean endsWith(Path path) {
+        return false;
+    }
+
+    @Override
+    public Path normalize() {
+        return null;
+    }
+
+    @Override
+    public Path resolve(Path path) {
+        return null;
+    }
+
+    @Override
+    public Path relativize(Path path) {
+        return null;
+    }
+
+    @Override
+    public URI toUri() {
+        return null;
+    }
+
+    @Override
+    public Path toAbsolutePath() {
+        return null;
+    }
+
+    @Override
+    public Path toRealPath(LinkOption... linkOptions) {
+        return null;
+    }
+
+    @Override
+    public WatchKey register(WatchService watchService,
+                             WatchEvent.Kind<?>[] kinds,
+                             WatchEvent.Modifier... modifiers) {
+        return null;
+    }
+
+    @Override
+    public int compareTo(Path path) {
+        return 0;
+    }
+}
