7d50f99b0e077a659f71e193d7b0d732f7837360
Vincent Potucek <vpotucek@me.com>
2025-04-28 21:01:16 +0200

    Issue #16857: api: add new int process(List<Path> files) method to RootModule - adapt tests

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/testmodules/PathMock.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/testmodules/PathMock.java
@@ -20,146 +20,22 @@
 package com.puppycrawl.tools.checkstyle.internal.testmodules;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.net.URI;
-import java.nio.file.FileSystem;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
 
-public final class PathMock implements Path, Serializable {
+import org.mockito.Mockito;
 
-    /** A unique serial version identifier. */
-    private static final long serialVersionUID = -7801807253540916684L;
+public final class PathMock {
 
-    private final File file;
-
-    private PathMock(File file) {
-        this.file = file;
+    private PathMock() {
     }
 
     public static Path ofFile(File file) {
-        return new PathMock(file);
+        return whenToFileThenReturn(file, Mockito.mock(Path.class));
     }
 
-    private void readObject(ObjectInputStream str) throws IOException, ClassNotFoundException {
-        str.readObject();
-    }
-
-    private void writeObject(ObjectOutputStream str) throws IOException {
-        str.writeObject("writeObject");
-    }
-
-    @Override
-    public File toFile() {
-        return file;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return 0;
-    }
-
-    @Override
-    public FileSystem getFileSystem() {
-        return null;
-    }
-
-    @Override
-    public boolean isAbsolute() {
-        return false;
-    }
-
-    @Override
-    public Path getRoot() {
-        return null;
-    }
-
-    @Override
-    public Path getFileName() {
-        return null;
-    }
-
-    @Override
-    public Path getParent() {
-        return null;
-    }
-
-    @Override
-    public int getNameCount() {
-        return 0;
-    }
-
-    @Override
-    public Path getName(int i) {
-        return null;
-    }
-
-    @Override
-    public Path subpath(int i, int i1) {
-        return null;
-    }
-
-    @Override
-    public boolean startsWith(Path path) {
-        return false;
-    }
-
-    @Override
-    public boolean endsWith(Path path) {
-        return false;
-    }
-
-    @Override
-    public Path normalize() {
-        return null;
-    }
-
-    @Override
-    public Path resolve(Path path) {
-        return null;
-    }
-
-    @Override
-    public Path relativize(Path path) {
-        return null;
-    }
-
-    @Override
-    public URI toUri() {
-        return null;
-    }
-
-    @Override
-    public Path toAbsolutePath() {
-        return null;
-    }
-
-    @Override
-    public Path toRealPath(LinkOption... linkOptions) {
-        return null;
-    }
-
-    @Override
-    public WatchKey register(WatchService watchService,
-                             WatchEvent.Kind<?>[] kinds,
-                             WatchEvent.Modifier... modifiers) {
-        return null;
-    }
-
-    @Override
-    public int compareTo(Path path) {
-        return 0;
+    private static Path whenToFileThenReturn(File file, Path mock) {
+        Mockito.when(mock.toFile()).thenReturn(file);
+        return mock;
     }
 
 }
