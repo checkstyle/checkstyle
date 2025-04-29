f46b6f210930bdb5ff4c63ef54f544e4be17e442
Vincent Potucek <vpotucek@me.com>
2025-04-29 11:20:35 +0200

    Issue #16857: api: add new int process(List<Path> files) method to RootModule - remove usage

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/WriteTagCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/WriteTagCheckTest.java
@@ -30,13 +30,12 @@ import java.io.File;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -288,10 +287,12 @@ public class WriteTagCheckTest extends AbstractModuleTestSupport {
                           String... expected)
             throws Exception {
         getStream().flush();
-        final List<File> theFiles = new ArrayList<>();
-        Collections.addAll(theFiles, processedFiles);
-        final int errs = checker.process(theFiles);
-
+        final int errs = checker.process(
+                Arrays
+                    .stream(processedFiles)
+                    .map(File::toPath)
+                    .collect(Collectors.toUnmodifiableList())
+        );
         // process each of the lines
         try (ByteArrayInputStream localStream =
                 new ByteArrayInputStream(getStream().toByteArray());
