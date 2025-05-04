38edb110f91da697c0935879555cfaa74e4c39f0
Roman Ivanov <ivanov-jr@mail.ru>
2025-05-04 06:49:04 -0700

    Issue #17015: remove from validation 'apidocs/constant-values.html' to have consistent results in both modes

--- .ci/run-link-check-plugin.sh
+++ .ci/run-link-check-plugin.sh
@@ -27,7 +27,8 @@ else
   echo "$LINKCHECK_ERRORS" | sort > .ci-temp/linkcheck-errors-sorted.txt
 fi
 
-sort config/linkcheck-suppressions.txt > .ci-temp/linkcheck-suppressions-sorted.txt
+sort config/linkcheck-suppressions.txt | grep -v 'apidocs/constant-values.html' \
+  > .ci-temp/linkcheck-suppressions-sorted.txt
 
 # Suppressions exist until https://github.com/checkstyle/checkstyle/issues/11572
 diff -u .ci-temp/linkcheck-suppressions-sorted.txt .ci-temp/linkcheck-errors-sorted.txt || true
