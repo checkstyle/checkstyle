1698ddff9d78f7454721b5f06eb912b69da0fd93
Vincent Potucek <vpotucek@me.com>
2025-05-04 08:37:03 +0200

    Issue #17000: AvoidOutdatedUsageCheck: Use modern Java collections API (toList() instead of collect(Collectors.toList()))

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/avoidoutdatedusage/Example1.java
@@ -0,0 +1,26 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="AvoidOutdatedUsage">
+    </module>
+  </module>
+</module>
+*/
+//non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.coding.avoidoutdatedusage;
+
+// xdoc section -- start
+public class Example1 {
+  public void avoidUsing() {
+    // violation below, Avoid using ''.collect(Collectors.toList())''.
+    Stream.of(FOO).collect(Collectors.toList());
+    Stream.of(FOO).toList(); // ok, as modern
+    List.of(FOO); // ok, as modern
+    new ArrayList<>(List.of(FOO)); // ok, as modern
+    Collector<Object, ?, List<Object>> list = Collectors.toList();
+    // violation below, Avoid using ''.collect(Collectors.toList())''.
+    Collectors.toList();
+    // Stream.of(FOO).toList()); // ok, as successor
+  }
+}
+// xdoc section -- end
