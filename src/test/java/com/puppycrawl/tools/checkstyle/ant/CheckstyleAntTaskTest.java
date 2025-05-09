012cd4fec91c273a1d34a4d5052078ba40a2c739
Vincent Potucek <vpotucek@me.com>
2025-05-09 07:34:05 +0200

    Issue #16802: improve spelling and typo in CheckstyleAntTask

--- src/test/java/com/puppycrawl/tools/checkstyle/ant/CheckstyleAntTaskTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/ant/CheckstyleAntTaskTest.java
@@ -227,7 +227,7 @@ public class CheckstyleAntTaskTest extends AbstractPathTestSupport {
         final List<File> filesToCheck = TestRootModuleChecker.getFilesToCheck();
         assertWithMessage("There are more files to check than expected")
                 .that(filesToCheck)
-                .hasSize(8);
+                .hasSize(9);
         assertWithMessage("The path of file differs from expected")
                 .that(filesToCheck.get(6).getAbsolutePath())
                 .isEqualTo(getPath(FLAWLESS_INPUT));
