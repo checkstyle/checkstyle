4e891d9d2fbeeaf152a25bf7a691fdbb8cc89a72
Brijeshthummar02 <brijeshthummar02@gmail.com>
2025-05-13 13:11:31 +0530

    Issue #14084:Add Nullable annotation to getJavadocBefore method and remove related suppression entry

--- src/main/java/com/puppycrawl/tools/checkstyle/api/FileContents.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/FileContents.java
@@ -25,6 +25,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Nullable;
+
 import java.util.regex.Pattern;
 
 import com.puppycrawl.tools.checkstyle.grammar.CommentListener;
@@ -212,6 +215,7 @@ public final class FileContents implements CommentListener {
      * @param lineNoBefore the line number to check before
      * @return the Javadoc comment, or {@code null} if none
      **/
+    @Nullable
     public TextBlock getJavadocBefore(int lineNoBefore) {
         // Lines start at 1 to the callers perspective, so need to take off 2
         int lineNo = lineNoBefore - 2;
