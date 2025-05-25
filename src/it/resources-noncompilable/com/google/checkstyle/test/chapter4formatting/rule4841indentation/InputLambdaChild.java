6badc863a6d7e1a90c8cee6e0ec805a3f91d8c95
Aziz-755 <azizfrigui08@gmail.com>
2025-05-25 13:35:22 +0200

    Issue #17120: fix false positive for Lambda child in indentation check

---
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule4841indentation/InputLambdaChild.java
@@ -0,0 +1,50 @@
+package com.google.checkstyle.test.chapter4formatting.rule42blockindentation;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**some javadoc.*/
+public class InputLambdaChild {
+  String testMethod1(List<Integer> operations) {
+    return operations.stream()
+        .map(
+                op ->
+                    switch (op) {
+                      case 1 -> "test";
+                      default -> "TEST";
+                    })
+        .findFirst().orElse("defaultValue");
+  }
+
+  String testMethod2(List<Integer> operations) {
+    return operations.stream()
+        .map(
+                op ->
+               switch (op) {
+               // violation above '.* incorrect indentation level 15, expected .* 20.'
+                      case 1 -> "test";
+                      default -> "TEST";
+                    })
+        .findFirst().orElse("defaultValue");
+  }
+
+  void main(String[] args) {
+    group((Function<Integer, Integer>) x -> switch (x) { default: yield x; },
+          (Function<Integer, Integer>) x -> switch (x) { default: yield x; });
+  }
+
+  List<String> getThrowsTrees(Object input) {
+    return getBlockTags(input,
+        kind -> switch (kind) { case "EXCEPTION", "THROWS" -> true; default -> false; },
+        String.class);
+  }
+
+  void group(Function<Integer, Integer> f1, Function<Integer, Integer> f2) {
+    // Dummy method to test syntax/indentation
+  }
+
+  <T> List<T> getBlockTags(Object input, Predicate<String> filter, Class<T> type) {
+    return List.of();
+  }
+}
