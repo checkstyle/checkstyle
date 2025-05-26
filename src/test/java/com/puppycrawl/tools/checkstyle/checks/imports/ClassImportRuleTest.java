3205f427b2df9789dae2be5e4d197b218692b18a
Vincent Potucek <vpotucek@me.com>
2025-05-26 10:42:11 +0200

    Issue #16919: Use Dedicated Keywords like avoid as alternative to disallow - pkg -> package

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/ClassImportRuleTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/ClassImportRuleTest.java
@@ -93,7 +93,7 @@ public class ClassImportRuleTest {
             .that(rule.verifyImport("pkga"))
             .isEqualTo(AccessResult.UNKNOWN);
         assertWithMessage("Invalid access result")
-            .that(rule.verifyImport("package.a"))
+            .that(rule.verifyImport("pkg.a")) // why here no package?
             .isEqualTo(AccessResult.ALLOWED);
         assertWithMessage("Invalid access result")
             .that(rule.verifyImport("pkx.a"))
