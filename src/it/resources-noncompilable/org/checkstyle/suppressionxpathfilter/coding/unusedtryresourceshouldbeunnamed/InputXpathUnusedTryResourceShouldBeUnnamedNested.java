efd18fb5e83f11a0a8619c8e466b17ffc1364fd6
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2025-05-28 08:21:22 +0100

    Issue #15484: New check UnusedTryResourceShouldBeUnnamed

---
+++ src/it/resources-noncompilable/org/checkstyle/suppressionxpathfilter/coding/unusedtryresourceshouldbeunnamed/InputXpathUnusedTryResourceShouldBeUnnamedNested.java
@@ -0,0 +1,29 @@
+// non-compiled with javac: Compilable with Java21
+package org.checkstyle.suppressionxpathfilter.coding.unusedtryresourceshouldbeunnamed;
+
+public class InputXpathUnusedTryResourceShouldBeUnnamedNested {
+    void test() {
+        try (AutoCloseable a = lock()) {
+            System.out.println(a);
+
+            try (AutoCloseable b = lock()) { // warn
+
+            } catch (Exception e) {
+
+            }
+
+        } catch (Exception e) {
+
+        }
+
+        try (AutoCloseable _ = lock()) {
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    AutoCloseable lock() {
+        return null;
+    }
+}
