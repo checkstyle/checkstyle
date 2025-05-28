20aee25c63fab6c06a22cd86f5acaa3d9d17c5fe
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2025-05-28 08:21:22 +0100

    Issue #15484: New check UnusedTryResourceShouldBeUnnamed

--- src/test/java/com/puppycrawl/tools/checkstyle/meta/XmlMetaReaderTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/meta/XmlMetaReaderTest.java
@@ -40,20 +40,20 @@ public class XmlMetaReaderTest extends AbstractPathTestSupport {
 
     @Test
     public void test() {
-        assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny()).hasSize(209);
+        assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny()).hasSize(210);
     }
 
     @Test
     public void testDuplicatePackage() {
         assertThat(XmlMetaReader
                     .readAllModulesIncludingThirdPartyIfAny("com.puppycrawl.tools.checkstyle.meta"))
-                .hasSize(209);
+                .hasSize(210);
     }
 
     @Test
     public void testBadPackage() {
         assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny("DOES.NOT.EXIST"))
-                .hasSize(209);
+                .hasSize(210);
     }
 
     @Test
