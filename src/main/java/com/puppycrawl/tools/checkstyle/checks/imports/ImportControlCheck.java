6027cee754b2eeb13e412d1640d9cc2d85e3804c
Vincent Potucek <vpotucek@me.com>
2025-05-30 21:55:22 +0200

    Issue #16919: Use Dedicated Keywords like forbid as alternative to disallow

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportControlCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportControlCheck.java
@@ -79,8 +79,8 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  * in the XML file, which is from top (first) to bottom (last).
  * </li>
  * <li>
- * If there is matching allow/disallow rule inside the current file/subpackage
- * then the Check returns the first "allowed" or "disallowed" message.
+ * If there is matching allow/forbidden/disallow rule inside the current file/subpackage
+ * then the Check returns the first "allowed" or "forbidden/disallow" message.
  * </li>
  * <li>
  * If there is no matching allow/disallow rule inside the current file/subpackage
@@ -88,14 +88,14 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  * </li>
  * <li>
  * If there is no matching allow/disallow rule in any of the files/subpackages,
- * including the root level (import-control), then the import is disallowed by default.
+ * including the root level (import-control), then the import is forbidden by default.
  * </li>
  * </ul>
  *
  * <p>
  * The DTD for an import control XML document is at
- * <a href="https://checkstyle.org/dtds/import_control_1_4.dtd">
- * https://checkstyle.org/dtds/import_control_1_4.dtd</a>.
+ * <a href="https://checkstyle.org/dtds/import_control_1_5.dtd">
+ * https://checkstyle.org/dtds/import_control_1_5.dtd</a>.
  * It contains documentation on each of the elements and attributes.
  * </p>
  *
@@ -105,8 +105,8 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  * </p>
  * <pre>
  * &lt;!DOCTYPE import-control PUBLIC
- *     "-//Checkstyle//DTD ImportControl Configuration 1.4//EN"
- *     "https://checkstyle.org/dtds/import_control_1_4.dtd"&gt;
+ *     "-//Checkstyle//DTD ImportControl Configuration 1.5//EN"
+ *     "https://checkstyle.org/dtds/import_control_1_5.dtd"&gt;
  * </pre>
  * <ul>
  * <li>
