49d969ba47ffaf0317f3823b4654412139a05c38
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-06-08 20:24:21 +0530

    Issue #17168: Moved DefaultComeLast Input to Compilable Path

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -511,7 +511,8 @@ check-since-version)
 
 compile-test-resources)
   # this task is useful during migration to new JDK to let compile resources on new jdk only
-  ./mvnw -e --no-transfer-progress clean test-compile -Dcheckstyle.skipCompileInputResources=false
+  ./mvnw -e --no-transfer-progress clean test-compile \
+  -Dcheckstyle.skipCompileInputResources=false -Dmaven.compiler.release=17
   ;;
 
 javac11)
