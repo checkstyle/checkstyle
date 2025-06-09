e3c202d2f5bf5f3b65bdf874f6eb14d6db2ffb37
Mohit Attry <atrrmohit@gmail.com>
2025-06-09 09:13:42 +0530

    supplemental: adding inputs for 4.5.1 where to break for record-class name

---
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule451wheretobreak/InputNoWrappingAfterRecordName.java
@@ -0,0 +1,40 @@
+package com.google.checkstyle.test.chapter4formatting.rule451wheretobreak;
+
+/** Some javadoc. */
+public class InputNoWrappingAfterRecordName {
+  record Multi (String string, Record rec) { // violation ''(' is preceded with whitespace'
+    private boolean inRecord (Object obj) { // violation ''(' is preceded with whitespace'
+      int value = 0;
+      if (obj instanceof Integer i) {
+        value = i;
+      }
+      return value > 10;
+    }
+  }
+
+  record Multi2 () { // violation ''(' is preceded with whitespace'
+    Multi2 (String s1, String s2, String s3) { // violation ''(' is preceded with whitespace'
+      this (); // violation ''(' is preceded with whitespace'
+    }
+  }
+
+  record Multi3 (Integer i, Node node) { // violation ''(' is preceded with whitespace'
+    public static void main (String... args) { // violation ''(' is preceded with whitespace'
+      System.out.println("works!");
+    }
+  }
+
+  record Multi4 () { // violation ''(' is preceded with whitespace'
+    void foo (){} // violation ''(' is preceded with whitespace'
+  }
+
+  record Multi5() {
+    static Multi obj =
+        new Multi ("my string", new Mtr4()); // violation ''(' is preceded with whitespace'
+  }
+
+  class MyTestClass {
+    private Multi obj =
+        new Multi ("my string", new Mtr4()); // violation ''(' is preceded with whitespace'
+  }
+}
