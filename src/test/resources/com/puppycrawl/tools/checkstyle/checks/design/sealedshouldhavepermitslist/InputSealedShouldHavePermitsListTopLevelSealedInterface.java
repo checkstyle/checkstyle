f93d2155b8d6278b4723b25b6b68b02b0086f602
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-06-11 17:06:28 +0530

    Issue #17168: Moved CheckOne Module Inputs to Compilable Folder

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/sealedshouldhavepermitslist/InputSealedShouldHavePermitsListTopLevelSealedInterface.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/design/sealedshouldhavepermitslist/InputSealedShouldHavePermitsListTopLevelSealedInterface.java
@@ -3,7 +3,7 @@ SealedShouldHavePermitsList
 
 */
 
-//non-compiled with javac: Compilable with Java17
+// Java17
 package com.puppycrawl.tools.checkstyle.checks.design.sealedshouldhavepermitslist;
 
 public sealed interface InputSealedShouldHavePermitsListTopLevelSealedInterface {
