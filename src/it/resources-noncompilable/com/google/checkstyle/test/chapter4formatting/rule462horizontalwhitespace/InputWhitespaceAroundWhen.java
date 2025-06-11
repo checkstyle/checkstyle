0c1981bf97bf69ae1b55f54c7a5f081c5e33317d
Mohit Attry <atrrmohit@gmail.com>
2025-06-11 08:42:03 +0530

    Issue #17193: Enforce whitespace after the double slashes of the comments

--- src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputWhitespaceAroundWhen.java
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputWhitespaceAroundWhen.java
@@ -1,4 +1,4 @@
-//non-compiled with javac: Compilable with Java21
+// non-compiled with javac: Compilable with Java21
 
 package com.google.checkstyle.test.chapter4formatting.rule462horizontalwhitespace;
 
