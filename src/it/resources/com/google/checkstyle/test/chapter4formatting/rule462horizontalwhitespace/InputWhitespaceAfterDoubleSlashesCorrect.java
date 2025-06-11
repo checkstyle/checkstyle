80c16406d642a197c32d4c7d34c99e0b03174766
Mohit Attry <atrrmohit@gmail.com>
2025-06-11 08:42:03 +0530

    Issue #17193: Enforce whitespace after the double slashes of the comments

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputWhitespaceAfterDoubleSlashesCorrect.java
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+/////////////////////////////////////////////////////////////////////////////////////////
+
+package com.google.checkstyle.test.chapter4formatting.rule462horizontalwhitespace;
+/** some javadoc. */
+public class InputWhitespaceAfterDoubleSlashesCorrect {
+  String googleCheck = "Google"; // google
+
+  /** somejavadoc. */
+  public void foo1() {
+    int pro1 = 0; // the main variable
+
+    int pro2 = 1; // the secondary variable
+
+    int[] pro3 = {1, 2}; // [] should be preceded by datatype
+  }
+}
