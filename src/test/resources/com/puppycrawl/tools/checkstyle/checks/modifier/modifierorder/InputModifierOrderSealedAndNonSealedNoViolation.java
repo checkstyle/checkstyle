63816b9b40dd85325b4a634aaa6986969fbca31b
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-06-13 22:23:33 +0530

    Issue #17168: Moved MicOne Module Inputs to Compilable Folder

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/modifier/modifierorder/InputModifierOrderSealedAndNonSealedNoViolation.java
@@ -0,0 +1,39 @@
+/*
+ModifierOrder
+
+
+*/
+
+//non-compiled with javac: Compilable with Java17
+package com.puppycrawl.tools.checkstyle.checks.modifier.modifierorder;
+
+public sealed class InputModifierOrderSealedAndNonSealedNoViolation
+    permits CircleTwo, SquareTwo, RectangleTwo {
+}
+
+final class CircleTwo extends InputModifierOrderSealedAndNonSealedNoViolation
+        implements SquircleTwo {
+}
+
+sealed class RectangleTwo extends InputModifierOrderSealedAndNonSealedNoViolation
+    implements Cloneable
+    permits TransparentRectangleTwo, FilledRectangleTwo {
+}
+
+final class TransparentRectangleTwo extends RectangleTwo {
+}
+
+sealed class SquareTwo extends InputModifierOrderSealedAndNonSealedNoViolation
+        implements SquircleTwo {
+    private sealed class OtherSquare extends SquareTwo permits OtherSquare2 {
+    }
+
+    private final class OtherSquare2 extends OtherSquare {
+    }
+}
+
+final strictfp class FilledRectangleTwo extends RectangleTwo {
+}
+
+sealed interface SquircleTwo permits CircleTwo, SquareTwo {
+}
