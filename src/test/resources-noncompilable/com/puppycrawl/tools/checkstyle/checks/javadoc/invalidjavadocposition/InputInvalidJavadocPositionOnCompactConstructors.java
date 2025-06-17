16925546f4fbf145908c4961ad9102dc525abef4
Mohit Attry <atrrmohit@gmail.com>
2025-06-17 17:30:36 +0530

    Issue #17158: fix false-positive invalid javadoc position for compact constructor

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/invalidjavadocposition/InputInvalidJavadocPositionOnCompactConstructors.java
@@ -0,0 +1,68 @@
+/*
+InvalidJavadocPosition
+
+
+*/
+
+// non-compiled with javac: Compilable with Java17
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.invalidjavadocposition;
+
+/**
+ * Some description.
+ */
+public class InputInvalidJavadocPositionOnCompactConstructors {
+
+    /**
+     * The configuration of a bind mount.
+     *
+     * @param containerPath a path on the container
+     * @param options       mounting options
+     */
+    record BindMount(String containerPath, String... options) {
+
+        /**
+         * Creates a mount.
+         *
+         * @param containerPath a path on the container
+         * @param options       mounting options
+         * @throws NullPointerException     if any of the arguments are null
+         */
+        BindMount {
+        }
+    }
+
+    /**
+     * The configuration.
+     *
+     * @param text the text
+     */
+    record MyRecord(String text) {
+
+        // violation 2 lines below 'Javadoc comment is placed in the wrong location.'
+        MyRecord {}
+        /** invalid comment. */
+    }
+
+    /**
+     * Maps the ports.
+     *
+     * @param from the from param
+     * @param to   the to param
+     */
+    record Mapping(String from, String to) {
+
+        // violation below 'Javadoc comment is placed in the wrong location.'
+        /** some javadoc. */
+        /**
+         * The constructor for Mapping.
+         *
+         * @param from The source
+         * @param to   The destination
+         */
+        Mapping(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+    }
+}
