3b593527c78a217cc9b3125f4cf60646188c3fc4
SteLeo1602 <stepanleontev16@gmail.com>
2025-06-24 22:27:07 -0700

    Issue #17281: Added new DescriptionMacro

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/site/DescriptionMacro.java
@@ -0,0 +1,180 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2025 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.site;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.apache.maven.doxia.macro.AbstractMacro;
+import org.apache.maven.doxia.macro.Macro;
+import org.apache.maven.doxia.macro.MacroExecutionException;
+import org.apache.maven.doxia.macro.MacroRequest;
+import org.apache.maven.doxia.sink.Sink;
+import org.codehaus.plexus.component.annotations.Component;
+
+import com.puppycrawl.tools.checkstyle.api.DetailNode;
+import com.puppycrawl.tools.checkstyle.api.JavadocTokenTypes;
+import com.puppycrawl.tools.checkstyle.meta.JavadocMetadataScraper;
+import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+import com.puppycrawl.tools.checkstyle.utils.JavadocUtil;
+
+/**
+ * A macro that inserts a description of module from its Javadoc.
+ */
+@Component(role = Macro.class, hint = "description")
+public class DescriptionMacro extends AbstractMacro {
+
+    /** New line escape character. */
+    private static final String NEWLINE = "\n";
+    /** A newline with 8 spaces of indentation. */
+    private static final String INDENT_LEVEL_8 = SiteUtil.getNewlineAndIndentSpaces(8);
+    /** A newline with 10 spaces of indentation. */
+    private static final String INDENT_LEVEL_10 = SiteUtil.getNewlineAndIndentSpaces(10);
+
+    @Override
+    public void execute(Sink sink, MacroRequest request) throws MacroExecutionException {
+        final Path modulePath = Paths.get((String) request.getParameter("modulePath"));
+        final String moduleName = CommonUtil.getFileNameWithoutExtension(modulePath.toString());
+
+        final Set<String> propertyNames = getPropertyNames(moduleName);
+        final Map<String, DetailNode> propertiesJavadocs = SiteUtil.getPropertiesJavadocs(
+            propertyNames, moduleName, modulePath);
+
+        final DetailNode moduleJavadoc = propertiesJavadocs.get(moduleName);
+
+        final int descriptionEndIndex = getDescriptionEndIndex(propertyNames, moduleJavadoc);
+        final String moduleDescription = JavadocMetadataScraper.constructSubTreeText(
+            moduleJavadoc, 0, descriptionEndIndex);
+
+        writeOutDescription(moduleDescription, sink);
+
+    }
+
+    /**
+     * Assigns values to each instance variable.
+     *
+     * @param moduleName name of module.
+     * @return set of property names.
+     * @throws MacroExecutionException if the module could not be retrieved.
+     */
+    private static Set<String> getPropertyNames(String moduleName)
+            throws MacroExecutionException {
+        final Object instance = SiteUtil.getModuleInstance(moduleName);
+        final Class<?> clss = instance.getClass();
+
+        return SiteUtil.getPropertiesForDocumentation(clss, instance);
+    }
+
+    /**
+     * Gets the end index of the description.
+     *
+     * @param propertyNamesSet Set with property names.
+     * @param moduleJavadoc javadoc of module.
+     * @return the end index.
+     */
+    private static int getDescriptionEndIndex(Set<String> propertyNamesSet,
+                                              DetailNode moduleJavadoc) {
+        int descriptionEndIndex = -1;
+
+        if (propertyNamesSet.isEmpty()) {
+            descriptionEndIndex += getParentStartIndex(moduleJavadoc);
+        }
+        else {
+            final Optional<DetailNode> somePropertyModuleNode =
+                SiteUtil.getPropertyJavadocNodeInModule(
+                    propertyNamesSet.iterator().next(), moduleJavadoc);
+
+            if (somePropertyModuleNode.isPresent()) {
+                descriptionEndIndex += JavadocMetadataScraper
+                    .getParentIndexOf(somePropertyModuleNode.get());
+            }
+        }
+
+        return descriptionEndIndex;
+    }
+
+    /**
+     * Gets the start index of the parent subsection in module's JavaDoc.
+     *
+     * @param moduleJavadoc javadoc of module.
+     * @return start index of parent subsection.
+     */
+    private static int getParentStartIndex(DetailNode moduleJavadoc) {
+        int parentStartIndex = 0;
+
+        for (DetailNode node : moduleJavadoc.getChildren()) {
+            if (node.getType() == JavadocTokenTypes.HTML_ELEMENT) {
+                final DetailNode paragraphNode = JavadocUtil.findFirstToken(
+                    node, JavadocTokenTypes.PARAGRAPH);
+                if (paragraphNode != null && JavadocMetadataScraper.isParentText(paragraphNode)) {
+                    parentStartIndex = node.getIndex();
+                    break;
+                }
+            }
+        }
+
+        return parentStartIndex;
+    }
+
+    /**
+     * Writes the description into xdoc.
+     *
+     * @param description description of the module.
+     * @param sink sink of the macro.
+     */
+    private static void writeOutDescription(String description, Sink sink) {
+        final String[] moduleDescriptionLinesSplit = description.split(NEWLINE);
+
+        sink.rawText(moduleDescriptionLinesSplit[0]);
+        String previousProcessedLine = moduleDescriptionLinesSplit[0];
+        for (int index = 1; index < moduleDescriptionLinesSplit.length; index++) {
+            final String currentLine = moduleDescriptionLinesSplit[index].trim();
+            final String processedLine;
+
+            if (currentLine.isEmpty()) {
+                processedLine = NEWLINE;
+            }
+            else if (currentLine.startsWith("<")
+                && !currentLine.startsWith("<code>") && !currentLine.startsWith("<a")) {
+                processedLine = INDENT_LEVEL_8 + currentLine;
+            }
+            else if (index > 1
+                && (previousProcessedLine.contains("<pre")
+                    || !previousProcessedLine.startsWith(INDENT_LEVEL_8))) {
+
+                final String currentLineWithPreservedIndent = moduleDescriptionLinesSplit[index]
+                    .substring(1);
+                processedLine = NEWLINE + currentLineWithPreservedIndent;
+            }
+            else {
+                processedLine = INDENT_LEVEL_10 + currentLine;
+            }
+
+            sink.rawText(processedLine);
+
+            previousProcessedLine = processedLine;
+        }
+
+    }
+
+}
