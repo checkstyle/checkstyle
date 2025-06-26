21dd04f88ea0b708f3a5c29eb62f3b3fcbb8eced
Mohit Attry <atrrmohit@gmail.com>
2025-06-26 18:37:36 +0530

    supplemental: IT for 4.8.9 Text Blocks

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule489textblocks/InputFormattedTextBlocksIndentation.java
@@ -0,0 +1,48 @@
+package com.google.checkstyle.test.chapter4formatting.rule489textblocks;
+
+/** somejavadoc. */
+public class InputFormattedTextBlocksIndentation {
+
+  /** somejavadoc. */
+  public void textIndentation1() {
+    String e1 =
+        """
+        content of the block. e1
+        """;
+
+    String e2 =
+        """
+        content of the block of e3
+        """;
+
+    String e3 =
+        """
+        content of the block. e1
+        """;
+
+    getData(
+        """
+        Indentation of Text-block
+        """,
+        5);
+  }
+
+  /** somejavadoc. */
+  public void textFuncIndenation2() {
+    // violation 2 lines below '.* incorrect indentation level 0, expected .* 8.'
+    String e2 =
+"""
+content of the block e2
+""";
+    // violation above '.* incorrect indentation level 0, expected .* 8.'
+
+    getData(
+        """
+            Indentation of Text-block
+        """,
+        5);
+  }
+
+  /** somejavadoc. */
+  public static void getData(String data, int length) {}
+}
