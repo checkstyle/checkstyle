3585fef94247f719a19cbea0526a87f4cba006fc
Mohit Attry <atrrmohit@gmail.com>
2025-06-28 13:22:03 +0530

    Issue #17285: Add non-compilable resources to google-java-format.sh

--- src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputFormattedWhitespaceAroundArrow.java
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputFormattedWhitespaceAroundArrow.java
@@ -4,17 +4,17 @@ package com.google.checkstyle.test.chapter4formatting.rule462horizontalwhitespac
 public class InputFormattedWhitespaceAroundArrow {
 
   static {
-    // violation below ''->' is not preceded with whitespace.'
-    new JCheckBox().addActionListener((final ActionEvent e)-> {
-      good();
-    });
+    new JCheckBox()
+        .addActionListener(
+            (final ActionEvent e) -> {
+              good();
+            });
   }
 
   void foo1(Object o) {
     switch (o) {
       case String s when (s.equals("a")) -> {}
-      case 'p' -> {
-      }
+      case 'p' -> {}
       default -> {}
     }
   }
@@ -22,79 +22,64 @@ public class InputFormattedWhitespaceAroundArrow {
   /** method. */
   void test(Object o) {
     switch (o) {
-      case String s when (
-          s.equals("a"))-> // violation ''->' is not preceded with whitespace.'
-        {
-        }
+      case String s when (s.equals("a")) -> {}
       case Point(int x, int y) when !(x >= 0 && y >= 0) -> {}
-      default-> // violation ''->' is not preceded with whitespace.'
-        {}
+      default -> {}
     }
 
-    int x = switch (o) {
-      case String s -> {
-        switch (o2) {
-          case Integer newInt-> { // violation ''->' is not preceded with whitespace.'
-            if (y == 0) {
-              System.out.println(0);
+    int x =
+        switch (o) {
+          case String s -> {
+            switch (o2) {
+              case Integer newInt -> {
+                if (y == 0) {
+                  System.out.println(0);
+                }
+              }
+              default -> {}
             }
+            yield 3;
           }
-          default -> {}
-        }
-        yield 3;
-      }
-      default -> 3;
-    };
+          default -> 3;
+        };
   }
 
   int test2() {
-    Predicate predicate = value ->(value != null);
-    // 2 violations above:
-    //     ''->' is not followed by whitespace.'
-    //     'WhitespaceAround: '->' is not followed by whitespace. .*'
+    Predicate predicate = value -> (value != null);
 
-    Object b = ((VoidPredicate) ()->o1 instanceof String s).get();
-    // 3 violations above:
-    //     ''->' is not followed by whitespace.'
-    //     'WhitespaceAround: '->' is not followed by whitespace. .*'
-    //     'WhitespaceAround: '->' is not preceded with whitespace.'
+    Object b = ((VoidPredicate) () -> o1 instanceof String s).get();
 
     List<Integer> ints = new LinkedList<Integer>();
-    // 3 violations 5 lines below:
-    //     ''->' is not followed by whitespace.'
-    //     ''->' is not followed by whitespace. .*'
-    //     ''{' is not preceded with whitespace.'
+
     ints.stream()
-        .map(t ->{
-            return t * 2;
-          }
-        )
-        .filter(t -> {
-          return false;
-        });
+        .map(
+            t -> {
+              return t * 2;
+            })
+        .filter(
+            t -> {
+              return false;
+            });
   }
 
   void test3() {
-    ArrayList<Boolean> boolList
-        = new ArrayList<Boolean>(Arrays.asList(false, true, false, false));
-    // violation 2 lines below 'WhitespaceAround: '->' is not preceded with whitespace.'
-    List<Boolean> filtered = boolList.stream()
-        .filter(statement-> {
-          if (!statement) {
-            return true;
-          } else {
-            return false;
-          }
-        })
-        .collect(Collectors.toList());
+    ArrayList<Boolean> boolList = new ArrayList<Boolean>(Arrays.asList(false, true, false, false));
+    List<Boolean> filtered =
+        boolList.stream()
+            .filter(
+                statement -> {
+                  if (!statement) {
+                    return true;
+                  } else {
+                    return false;
+                  }
+                })
+            .collect(Collectors.toList());
 
-    result = boolList.stream().filter(
-        // violation below 'WhitespaceAround: '->' is not preceded with whitespace.'
-        statement-> someFunction())
-        .findFirst()
-        .orElseThrow(() ->new IllegalStateException("big problem"));
-    // 2 violations above:
-    //     ''->' is not followed by whitespace.'
-    //     'WhitespaceAround: '->' is not followed by whitespace. .*'
+    result =
+        boolList.stream()
+            .filter(statement -> someFunction())
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("big problem"));
   }
 }
--- src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputWhitespaceAroundArrowCorrect.java
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputWhitespaceAroundArrowCorrect.java
@@ -4,16 +4,17 @@ package com.google.checkstyle.test.chapter4formatting.rule462horizontalwhitespac
 public class InputWhitespaceAroundArrowCorrect {
 
   static {
-    new JCheckBox().addActionListener((final ActionEvent e) -> {
-      good();
-    });
+    new JCheckBox()
+        .addActionListener(
+            (final ActionEvent e) -> {
+              good();
+            });
   }
 
   void foo1(Object o) {
     switch (o) {
       case String s when (s.equals("a")) -> {}
-      case 'p' -> {
-      }
+      case 'p' -> {}
       default -> {}
     }
   }
@@ -21,29 +22,26 @@ public class InputWhitespaceAroundArrowCorrect {
   /** method. */
   void test(Object o) {
     switch (o) {
-      case String s when (
-          s.equals("a")) ->
-        {
-        }
+      case String s when (s.equals("a")) -> {}
       case Point(int x, int y) when !(x >= 0 && y >= 0) -> {}
-      default ->
-        {}
+      default -> {}
     }
 
-    int x = switch (o) {
-      case String s -> {
-        switch (o2) {
-          case Integer newInt -> {
-            if (y == 0) {
-              System.out.println(0);
+    int x =
+        switch (o) {
+          case String s -> {
+            switch (o2) {
+              case Integer newInt -> {
+                if (y == 0) {
+                  System.out.println(0);
+                }
+              }
+              default -> {}
             }
+            yield 3;
           }
-          default -> {}
-        }
-        yield 3;
-      }
-      default -> 3;
-    };
+          default -> 3;
+        };
   }
 
   int test2() {
@@ -54,32 +52,35 @@ public class InputWhitespaceAroundArrowCorrect {
     List<Integer> ints = new LinkedList<Integer>();
 
     ints.stream()
-        .map(t -> {
-            return t * 2;
-          }
-        )
-        .filter(t -> {
-          return false;
-        });
+        .map(
+            t -> {
+              return t * 2;
+            })
+        .filter(
+            t -> {
+              return false;
+            });
   }
 
   void test3() {
-    ArrayList<Boolean> boolList
-        = new ArrayList<Boolean>(Arrays.asList(false, true, false, false));
+    ArrayList<Boolean> boolList = new ArrayList<Boolean>(Arrays.asList(false, true, false, false));
 
-    List<Boolean> filtered = boolList.stream()
-        .filter(statement -> {
-          if (!statement) {
-            return true;
-          } else {
-            return false;
-          }
-        })
-        .collect(Collectors.toList());
+    List<Boolean> filtered =
+        boolList.stream()
+            .filter(
+                statement -> {
+                  if (!statement) {
+                    return true;
+                  } else {
+                    return false;
+                  }
+                })
+            .collect(Collectors.toList());
 
-    result = boolList.stream().filter(
-        statement -> someFunction())
-        .findFirst()
-        .orElseThrow(() -> new IllegalStateException("big problem"));
+    result =
+        boolList.stream()
+            .filter(statement -> someFunction())
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("big problem"));
   }
 }
