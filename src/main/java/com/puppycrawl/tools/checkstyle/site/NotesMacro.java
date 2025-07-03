6f07680570842d3c1af34ec5ce04f5a64964ccc2
SteLeo1602 <stepanleontev16@gmail.com>
2025-07-02 20:24:33 -0700

    Issue #17310: Added new Notes Macro

--- src/main/java/com/puppycrawl/tools/checkstyle/site/NotesMacro.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/site/NotesMacro.java
@@ -40,11 +40,17 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
 import com.puppycrawl.tools.checkstyle.utils.JavadocUtil;
 
 /**
- * A macro that inserts a description of module from its Javadoc.
+ * A macro that inserts a notes subsection of module from its Javadoc.
  */
-@Component(role = Macro.class, hint = "description")
-public class DescriptionMacro extends AbstractMacro {
+@Component(role = Macro.class, hint = "notes")
+public class NotesMacro extends AbstractMacro {
 
+    /** "Notes:" javadoc marking. */
+    public static final String NOTES = "Notes:";
+    /** "Notes:" line with new line accounted. */
+    public static final Pattern NOTES_LINE_WITH_NEWLINE = Pattern.compile("\r?\n\\s?" + NOTES);
+    /** "Notes:" line. */
+    public static final Pattern NOTES_LINE = Pattern.compile("\\s*" + NOTES + "$");
     /** New line escape character. */
     private static final String NEWLINE = "\n";
     /** A newline with 8 spaces of indentation. */
@@ -55,10 +61,6 @@ public class DescriptionMacro extends AbstractMacro {
     private static final Set<String> HTML_TEXT_FORMAT_TAGS = Set.of("<code>", "<a", "</a>", "<b>",
         "</b>", "<strong>", "</strong>", "<i>", "</i>", "<em>", "</em>", "<small>", "</small>",
         "<ins>", "<sub>", "<sup>");
-    /** "Notes:" javadoc marking. */
-    private static final String NOTES = "Notes:";
-    /** "Notes:" line. */
-    private static final Pattern NOTES_LINE = Pattern.compile("\\s*" + NOTES + "$");
 
     @Override
     public void execute(Sink sink, MacroRequest request) throws MacroExecutionException {
@@ -66,24 +68,31 @@ public class DescriptionMacro extends AbstractMacro {
         final String moduleName = CommonUtil.getFileNameWithoutExtension(modulePath.toString());
 
         final Set<String> propertyNames = getPropertyNames(moduleName);
-        final Map<String, DetailNode> propertiesJavadocs = SiteUtil.getPropertiesJavadocs(
+        final Map<String, DetailNode> moduleAndPropertiesJavadocs = SiteUtil.getPropertiesJavadocs(
             propertyNames, moduleName, modulePath);
 
-        final DetailNode moduleJavadoc = propertiesJavadocs.get(moduleName);
+        final DetailNode moduleJavadoc = moduleAndPropertiesJavadocs.get(moduleName);
+        if (moduleJavadoc == null) {
+            throw new MacroExecutionException("Javadoc of module " + moduleName + " is not found.");
+        }
 
-        final int descriptionEndIndex = getDescriptionEndIndex(moduleJavadoc, propertyNames);
-        final String moduleDescription = JavadocMetadataScraper.constructSubTreeText(
-            moduleJavadoc, 0, descriptionEndIndex);
+        final int notesStartIndex = getNotesStartIndex(moduleJavadoc);
+        final int notesEndIndex = getNotesEndIndex(moduleJavadoc, propertyNames);
 
-        writeOutDescription(moduleDescription, sink);
+        final String unprocessedModuleNotes = JavadocMetadataScraper.constructSubTreeText(
+            moduleJavadoc, notesStartIndex, notesEndIndex);
+        final String moduleNotes = NOTES_LINE_WITH_NEWLINE.matcher(unprocessedModuleNotes)
+            .replaceAll("");
+
+        writeOutNotes(moduleNotes, sink);
 
     }
 
     /**
-     * Assigns values to each instance variable.
+     * Gets properties of the specified module.
      *
      * @param moduleName name of module.
-     * @return set of property names.
+     * @return set of properties name if present, otherwise null.
      * @throws MacroExecutionException if the module could not be retrieved.
      */
     private static Set<String> getPropertyNames(String moduleName)
@@ -92,39 +101,6 @@ public class DescriptionMacro extends AbstractMacro {
         final Class<?> clss = instance.getClass();
 
         return SiteUtil.getPropertiesForDocumentation(clss, instance);
-    }
-
-    /**
-     * Gets the end index of the description.
-     *
-     * @param moduleJavadoc javadoc of module.
-     * @param propertyNamesSet Set with property names.
-     * @return the end index.
-     */
-    private static int getDescriptionEndIndex(DetailNode moduleJavadoc,
-                                              Set<String> propertyNamesSet) {
-        int descriptionEndIndex = -1;
-
-        if (getNotesStartIndex(moduleJavadoc) > -1) {
-            descriptionEndIndex += getNotesStartIndex(moduleJavadoc);
-        }
-        else if (propertyNamesSet.isEmpty()) {
-            descriptionEndIndex += getParentSectionStartIndex(moduleJavadoc);
-        }
-        else {
-            final String somePropertyName = propertyNamesSet.iterator().next();
-
-            final Optional<DetailNode> somePropertyModuleNode =
-                SiteUtil.getPropertyJavadocNodeInModule(
-                    somePropertyName, moduleJavadoc);
-
-            if (somePropertyModuleNode.isPresent()) {
-                descriptionEndIndex += JavadocMetadataScraper
-                    .getParentIndexOf(somePropertyModuleNode.get());
-            }
-        }
-
-        return descriptionEndIndex;
     }
 
     /**
@@ -153,7 +129,37 @@ public class DescriptionMacro extends AbstractMacro {
     }
 
     /**
-     * Gets the starting index of the "Parent is" paragraph in module's javadoc.
+     * Gets the end index of the Notes.
+     *
+     * @param moduleJavadoc javadoc of module.
+     * @param propertyNamesSet Set with property names.
+     * @return the end index.
+     */
+    private static int getNotesEndIndex(DetailNode moduleJavadoc,
+                                              Set<String> propertyNamesSet) {
+        int notesEndIndex = -1;
+
+        if (propertyNamesSet.isEmpty()) {
+            notesEndIndex += getParentSectionStartIndex(moduleJavadoc);
+        }
+        else {
+            final String somePropertyName = propertyNamesSet.iterator().next();
+
+            final Optional<DetailNode> somePropertyModuleNode =
+                SiteUtil.getPropertyJavadocNodeInModule(
+                    somePropertyName, moduleJavadoc);
+
+            if (somePropertyModuleNode.isPresent()) {
+                notesEndIndex += JavadocMetadataScraper
+                    .getParentIndexOf(somePropertyModuleNode.get());
+            }
+        }
+
+        return notesEndIndex;
+    }
+
+    /**
+     * Gets the start index of the parent subsection in module's JavaDoc.
      *
      * @param moduleJavadoc javadoc of module.
      * @return start index of parent subsection.
@@ -176,19 +182,19 @@ public class DescriptionMacro extends AbstractMacro {
     }
 
     /**
-     * Writes the description into xdoc.
+     * Writes the notes into xdoc.
      *
-     * @param description description of the module.
+     * @param notes notes of the module.
      * @param sink sink of the macro.
      */
-    private static void writeOutDescription(String description, Sink sink) {
-        final String[] moduleDescriptionLinesSplit = description.split(NEWLINE);
+    private static void writeOutNotes(String notes, Sink sink) {
+        final String[] moduleNotesLinesSplit = notes.split(NEWLINE);
 
-        sink.rawText(moduleDescriptionLinesSplit[0]);
-        String lastHtmlTag = moduleDescriptionLinesSplit[0];
+        sink.rawText(moduleNotesLinesSplit[0]);
+        String lastHtmlTag = moduleNotesLinesSplit[0];
 
-        for (int index = 1; index < moduleDescriptionLinesSplit.length; index++) {
-            final String currentLine = moduleDescriptionLinesSplit[index].trim();
+        for (int index = 1; index < moduleNotesLinesSplit.length; index++) {
+            final String currentLine = moduleNotesLinesSplit[index].trim();
             final String processedLine;
 
             if (currentLine.isEmpty()) {
@@ -201,7 +207,7 @@ public class DescriptionMacro extends AbstractMacro {
                 lastHtmlTag = currentLine;
             }
             else if (lastHtmlTag.contains("<pre")) {
-                final String currentLineWithPreservedIndent = moduleDescriptionLinesSplit[index]
+                final String currentLineWithPreservedIndent = moduleNotesLinesSplit[index]
                     .substring(1);
 
                 processedLine = NEWLINE + currentLineWithPreservedIndent;
