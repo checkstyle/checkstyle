e6347223d5c73b9cf8f1c6119aafc309a279132c
Mohit Attry <atrrmohit@gmail.com>
2025-07-04 11:58:53 +0530

    Issue #17223: New Check TextBlockGoogleStyleFormattingCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/textblockgooglestyleformatting/InputTextBlockGoogleStyleFormatting8.java
@@ -0,0 +1,35 @@
+/*
+TextBlockGoogleStyleFormatting
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.textblockgooglestyleformatting;
+
+import java.io.Serial;
+
+public class InputTextBlockGoogleStyleFormatting8 extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 5009606293886748304L;
+
+    public InputTextBlockGoogleStyleFormatting8(
+        final String recordBatchEntry,
+        final int entryLength,
+        final int recordBatchEntriesSize,
+        final int batchSize) {
+        super(
+          """
+              Can't append entry: '%s' with size: %d this would exceed the maximum batch size. \
+              [ currentBatchEntryCount: %d, currentBatchSize: %d]"""
+                  // 2 violations above:
+                  //   'Closing quotes (""") of text-block should not be preceded by non'
+                  //   'Text-block quotes are not vertically aligned'
+                  .formatted(
+                          recordBatchEntry,
+                          entryLength,
+                          batchSize,
+                          recordBatchEntriesSize
+                  ));
+    }
+}
