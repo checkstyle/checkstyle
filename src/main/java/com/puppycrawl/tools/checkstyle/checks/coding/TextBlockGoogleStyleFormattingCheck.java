0d8e0dc2f3f36878fde2146d33c14f6ed7754feb
Mohit Attry <atrrmohit@gmail.com>
2025-07-04 11:58:53 +0530

    Issue #17223: New Check TextBlockGoogleStyleFormattingCheck

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/TextBlockGoogleStyleFormattingCheck.java
@@ -0,0 +1,243 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2025 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.coding;
+
+
+import com.puppycrawl.tools.checkstyle.StatelessCheck;
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
+
+/**
+ * <div>
+ * Checks correct format of
+ * <a href="https://docs.oracle.com/en/java/javase/17/text-blocks/index.html">Java Text Blocks</a>
+ * as specified in
+ * <a href="https://google.github.io/styleguide/javaguide.html#s4.8.9-text-blocks">
+ * Google Java Style Guide</a>.
+ * </div>
+ *
+ * <p>
+ * This Check has two functions: first is to make sure that both opening and closing quotes
+ * ({@code ***}) text block are on new lines(no other item should precede the quotes) and
+ * second is to ensure opening and closing quotes are vertically aligned.
+ * Note: Closing quotes can be followed by further code on the same line.
+ * </p>
+ *
+ * <p>
+ * Parent is {@code com.puppycrawl.tools.checkstyle.TreeWalker}
+ * </p>
+ *
+ * <p>
+ * Violation Message Keys:
+ * </p>
+ * <ul>
+ * <li>
+ * {@code textblock.format.close}
+ * </li>
+ *
+ * <li>
+ * {@code textblock.format.open}
+ * </li>
+ *
+ * <li>
+ * {@code textblock.indentation}
+ * </li>
+ * </ul>
+ *
+ * @since 11.0.0
+ */
+@StatelessCheck
+public class TextBlockGoogleStyleFormattingCheck extends AbstractCheck {
+
+    /**
+     * A key is pointing to the warning message text in "messages.properties"
+     * file.
+     */
+    public static final String MSG_OPEN_QUOTES_ERROR = "textblock.format.open";
+
+    /**
+     * A key is pointing to the warning message text in "messages.properties"
+     * file.
+     */
+    public static final String MSG_CLOSE_QUOTES_ERROR = "textblock.format.close";
+
+    /**
+     * A key is pointing to the warning message text in "messages.properties"
+     * file.
+     */
+    public static final String MSG_INDENTATION_ERROR = "textblock.indentation";
+
+    @Override
+    public int[] getDefaultTokens() {
+        return getRequiredTokens();
+    }
+
+    @Override
+    public int[] getAcceptableTokens() {
+        return getRequiredTokens();
+    }
+
+    @Override
+    public int[] getRequiredTokens() {
+        return new int[] {
+            TokenTypes.TEXT_BLOCK_LITERAL_BEGIN,
+        };
+    }
+
+    @Override
+    public void visitToken(DetailAST ast) {
+        boolean misAlinged = false;
+        if (!areOpeningQuotesOnCorrectPosition(ast)) {
+//            misAlinged = true;
+            log(ast, MSG_OPEN_QUOTES_ERROR);
+        }
+
+        final DetailAST closingQuotes = getClosingQuotes(ast);
+        if (!areClosingQuotesOnCorrectPosition(closingQuotes)) {
+//            misAlinged = true;
+            log(closingQuotes, MSG_CLOSE_QUOTES_ERROR);
+        }
+//        if (misAlinged || !verticallyAligned(ast, closingQuotes)) {
+//            log(ast, MSG_INDENTATION_ERROR);
+//        }
+    }
+
+    /**
+     * Checks if opening and closing quotes are vertically aligned.
+     *
+     * @param openQuotes the ast to check.
+     * @param closeQuotes the ast to check.
+     * @return true if both quotes have same indentation else false.
+     */
+    private boolean verticallyAligned(DetailAST openQuotes, DetailAST closeQuotes) {
+        final String openingline = getLine(openQuotes.getLineNo() - 1);
+        final String closingLine = getLine(closeQuotes.getLineNo() - 1);
+        int tabWidth = getTabWidth();
+
+        return CommonUtil.lengthExpandedTabs(openingline, openQuotes.getColumnNo(), tabWidth)
+            == CommonUtil.lengthExpandedTabs(closingLine, closeQuotes.getColumnNo(), tabWidth);
+    }
+
+    /**
+     * Gets the {@code TEXT_BLOCK_LITERAL_END} of a {@code TEXT_BLOCK_LITERAL_BEGIN}.
+     *
+     * @param ast the ast to check
+     * @return DetailAST {@code TEXT_BLOCK_LITERAL_END}
+     */
+    private static DetailAST getClosingQuotes(DetailAST ast) {
+        return ast.getFirstChild().getNextSibling();
+    }
+
+    /**
+     * Determines if the given expression is at the start of a line.
+     *
+     * @param ast the ast to check
+     * @return true if it is, false otherwise
+     */
+    private boolean isStartOfLine(DetailAST ast) {
+        final int tabWidth = getTabWidth();
+        final String line = getLine(ast.getLineNo() - 1);
+
+        int index = 0;
+        while (Character.isWhitespace(line.charAt(index))) {
+            index++;
+        }
+        return CommonUtil.lengthExpandedTabs(line, index, tabWidth)
+            == CommonUtil.lengthExpandedTabs(line, ast.getColumnNo(), tabWidth);
+    }
+
+    /**
+     * Determines if the given expression is at the start of a line.
+     *
+     * @param ast the ast to check
+     * @return true if it is, false otherwise
+     */
+    private static boolean areOpeningQuotesOnCorrectPosition(DetailAST ast) {
+        DetailAST superParent = ast;
+        while (!TokenUtil.isOfType(superParent.getType(), TokenTypes.LITERAL_RETURN,
+                TokenTypes.ASSIGN, TokenTypes.LITERAL_RETURN, TokenTypes.METHOD_CALL,
+                TokenTypes.PLUS)) {
+            superParent = superParent.getParent();
+        }
+
+        return switch (superParent.getType()) {
+            case TokenTypes.PLUS ->
+                checkPlusExpression(ast, superParent);
+            case TokenTypes.METHOD_CALL ->
+                checkMethodCall(ast, superParent);
+            default ->
+                !TokenUtil.areOnSameLine(ast, superParent);
+        };
+    }
+
+    private static boolean checkPlusExpression(DetailAST ast, DetailAST superParent) {
+        DetailAST ident = superParent.getFirstChild();
+
+//        if (superParent.getParent().getType() == TokenTypes.PLUS) {
+//
+//        }
+
+        return !TokenUtil.areOnSameLine(ast, ident);
+    }
+
+    /** Checks the Method call expression.
+     *
+     * @param ast the quotes
+     * @param superParent the superParent
+     * @return true
+     */
+    private static boolean checkMethodCall(DetailAST ast, DetailAST superParent) {
+        if (superParent.getParent().getType() != TokenTypes.EXPR) {
+            checkPlusExpression(ast, superParent.getParent());
+        }
+
+        DetailAST tmp = superParent.getFirstChild();
+        DetailAST explist = superParent.findFirstToken(TokenTypes.ELIST);
+        if (tmp.getType() == TokenTypes.DOT && tmp.getFirstChild() != ast) {
+            tmp = tmp.getLastChild();
+        }
+        else if (explist.getFirstChild().getFirstChild() != ast) {
+            DetailAST comma = ast.getParent().getPreviousSibling();
+            tmp = explist.findFirstToken(TokenTypes.COMMA);
+        }
+
+        return !TokenUtil.areOnSameLine(ast, tmp);
+    }
+
+    /** Determines if the closing quotes of a text block expression are on a new line.
+     *
+     * @param closingQuotes the closing quotes.
+     * @return true
+     */
+    private static boolean areClosingQuotesOnCorrectPosition(DetailAST closingQuotes) {
+        boolean result = false;
+        final DetailAST content = closingQuotes.getPreviousSibling();
+        final String text = content.getText();
+        int index = text.length() - 1;
+        while (text.charAt(index) == ' ') {
+            index--;
+        }
+        char test = text.charAt(index);
+        return text.charAt(index) == '\n';
+    }
+}
