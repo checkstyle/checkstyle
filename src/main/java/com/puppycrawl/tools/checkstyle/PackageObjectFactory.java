8eca508d22e530b18bbc60c51c0bf4d64f6afd33
SteLeo1602 <stepanleontev16@gmail.com>
2025-07-08 13:20:33 -0700

    Issue #17281: Extended Description Macro to coding modules

--- src/main/java/com/puppycrawl/tools/checkstyle/PackageObjectFactory.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/PackageObjectFactory.java
@@ -595,7 +595,7 @@ public class PackageObjectFactory implements ModuleFactory {
                 BASE_PACKAGE + ".checks.coding.UnnecessarySemicolonInTryWithResourcesCheck");
         NAME_TO_FULL_MODULE_NAME.put("VariableDeclarationUsageDistanceCheck",
                 BASE_PACKAGE + ".checks.coding.VariableDeclarationUsageDistanceCheck");
-        NAME_TO_FULL_MODULE_NAME.put("WhenShouldBeUsed",
+        NAME_TO_FULL_MODULE_NAME.put("WhenShouldBeUsedCheck",
                 BASE_PACKAGE + ".checks.coding.WhenShouldBeUsedCheck");
         NAME_TO_FULL_MODULE_NAME.put("NoArrayTrailingCommaCheck",
                 BASE_PACKAGE + ".checks.coding.NoArrayTrailingCommaCheck");
