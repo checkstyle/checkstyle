7557ec9d359021db44a75a14825e972a87090ae5
ElinaZoldnere <elina.zoldnere@gmail.com>
2025-07-11 16:13:47 +0300

    Issue #16361: Update DefaultLoggerTest.java to verify with InlineConfigParser and logger

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/defaultlogger/InputDefaultLoggerTestSingleError.java
@@ -0,0 +1,14 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="com.puppycrawl.tools.checkstyle.checks.imports.UnusedImportsCheck"/>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.defaultlogger;
+
+import java.util.List; // violation 'Unused import - java.util.List.'
+
+public class InputDefaultLoggerTestSingleError {
+}
