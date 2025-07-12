9328993127dd36b6ed14597fd12173dce6167e80
Praveen Kumar <praveen729483@gmail.com>
2025-07-12 05:34:10 +0530

    Issue #16738: Update SuppressWithNearbyTextFilterExamplesTest to use verifyFilterWithInlineConfigParser

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example1.java
@@ -9,6 +9,7 @@
 package com.puppycrawl.tools.checkstyle.filters.suppresswithnearbytextfilter;
 // xdoc section -- start
 public class Example1 {
+  // filtered violation below ''24' is a magic number'
   int hoursInDay = 24; // SUPPRESS CHECKSTYLE because it is too obvious
   int daysInWeek = 7; // violation, "'7' is a magic number."
 }
