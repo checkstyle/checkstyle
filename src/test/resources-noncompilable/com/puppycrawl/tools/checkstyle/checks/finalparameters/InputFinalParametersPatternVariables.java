ab546c57ce934e83eae68e06f3666bb7929f865e
Jake Potrebic <jake.m.potrebic@gmail.com>
2025-07-11 19:47:40 -0700

    Issue #17366: FinalParameters should check interface methods and pattern variable definitions

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/finalparameters/InputFinalParametersPatternVariables.java
@@ -0,0 +1,45 @@
+/*
+FinalParameters
+ignorePrimitiveTypes = (default)false
+ignoreUnnamedParameters = (default)true
+tokens = PATTERN_VARIABLE_DEF
+
+
+*/
+
+// non-compiled with javac: Compilable with Java21
+package com.puppycrawl.tools.checkstyle.checks.finalparameters;
+
+public class InputFinalParametersPatternVariables {
+    record ARecord(String name, int age) {
+    }
+    static void method(final Object o) {
+        final boolean isString = o instanceof String s; // violation, 's' should be final
+        final boolean isStringCorrect = o instanceof final String correct;
+        if (o instanceof Integer i) { // violation, 'i' should be final
+        }
+        if (o instanceof final Integer i) {
+        }
+
+        if (o instanceof ARecord(String name, final int age)) { // violation, 'name' should be final
+        }
+        if (o instanceof ARecord(final String name, final int age)) {
+        }
+
+        switch (o) {
+            case String s -> { // violation, 's' should be final
+            }
+            case ARecord(String name, final int age) -> { // violation, 'name' should be final
+            }
+            default -> {}
+        }
+        switch (o) {
+            case final String s -> {
+            }
+            case ARecord(final String name, final int age) -> {
+            }
+            default -> {}
+        }
+
+    }
+}
