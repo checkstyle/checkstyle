ba0aa64a08abdd8c1141bcc7d07922d08a04a4bd
Praveen Kumar <praveen729483@gmail.com>
2025-07-12 05:34:10 +0530

    Issue #16738: Update SuppressWithNearbyTextFilterExamplesTest to use verifyFilterWithInlineConfigParser

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example8.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example8.java
@@ -14,10 +14,11 @@
 package com.puppycrawl.tools.checkstyle.filters.suppresswithnearbytextfilter;
 // xdoc section -- start
 public class Example8 {
+  // filtered violation below ''42' is a magic number'
   int a = 42; // @cs-: MagicNumber for +3 lines
-  int b = 43;
-  int c = 44;
-  int d = 45;
+  int b = 43; // filtered violation ''43' is a magic number'
+  int c = 44; // filtered violation ''44' is a magic number'
+  int d = 45; // filtered violation ''45' is a magic number'
   int e = 46; // violation "'46' is a magic number."
 }
 // xdoc section -- end
