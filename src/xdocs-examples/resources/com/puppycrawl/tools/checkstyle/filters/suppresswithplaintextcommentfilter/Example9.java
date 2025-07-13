82ff600253aefa0268d85b6e17958adbfe56def0
Praveen Kumar <praveen729483@gmail.com>
2025-07-14 01:27:37 +0530

    Issue #16738: Update SuppressWithPlainTextCommentFilterExamplesTest to use verifyFilter

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithplaintextcommentfilter/Example9.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithplaintextcommentfilter/Example9.java
@@ -19,12 +19,14 @@ package com.puppycrawl.tools.checkstyle.filters.suppresswithplaintextcommentfilt
 // xdoc section -- start
 public class Example9 {
 
-  // suppressed violation below, opening/closing quotes are 'offCommentFormat' and 'onCommentFormat'
   static final String LOCATION_CSV_SAMPLE = """
           locationId,label,regionId,regionLabel,vendorId,vendorLabel,address,address2,city,stateProvinceCode,zipCode,countryCode,latitude,longitude
           ST001,Station 001,ZONE1,Zone 1,CP1,Competitor 1,123 Street,Unit 2,Houston,TX,77033,US,29.761496813335178,-95.53049214204984
           ST002,Station 002,ZONE2,,CP2,,668 Street,Unit 23,San Jose,CA,95191,US,37.35102477242508,-121.9209934020318
           """;
+  // filtered violation 5 lines above 'Line is longer than 100 characters (found 147).'
+  // filtered violation 5 lines above 'Line is longer than 100 characters (found 133).'
+  // filtered violation 5 lines above 'Line is longer than 100 characters (found 116).'
 
   // violation below, 'Line is longer than 100 characters (found 183).'
   static final String SINGLE_LINE_SAMPLE = "locationId,label,regionId,regionLabel,vendorId,vendorLabel,address,address2,city,stateProvinceCode,zipCode,countryCode,latitude,longitude";
