68c725bb85881d4ba20bc68895e94217c30d8bf3
voidCounter <rohitroy7014@gmail.com>
2025-07-16 20:02:48 +0600

    Issue #17128: Add test for vertically aligned trailing comments

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/IndentationTrailingCommentsVerticalAlignmentTest.java
@@ -0,0 +1,148 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2025 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+
+class IndentationTrailingCommentsVerticalAlignmentTest {
+
+    private static final String INDENTATION_TEST_FILES_PATH =
+        "com/puppycrawl/tools/checkstyle/checks/indentation/indentation";
+
+    private static final int TAB_WIDTH = 4;
+
+    private static final Set<String> ALLOWED_VIOLATION_FILES = Set.of(
+        // reason: IndentationCheckTest tests fail after alignment
+        "InputIndentationAnnArrInit.java",
+        "InputIndentationDifficultAnnotations.java",
+        "InputIndentationZeroArrayInit.java",
+        "InputIndentationAnnArrInitWithEmoji.java",
+        "InputIndentationOddLineWrappingAndArrayInit.java",
+        "InputIndentationArrayInitIndentWithEmoji.java",
+        "InputIndentationInvalidArrayInitIndent.java",
+        "InputIndentationInvalidArrayInitIndentTwoDimensional.java",
+        "InputIndentationAnnArrInit2.java",
+        "InputIndentationInvalidArrayInitIndent1.java",
+
+        // reason: checkstyle check: Line gets longer than 100 characters
+        "InputIndentationAnnotationClosingParenthesisEndsInSameIndentationAsOpening.java",
+        "InputIndentationAnnotationFieldDefinition.java",
+        "InputIndentationAnnotationScopeIndentationCheck.java",
+        "InputIndentationAnonymousClassInMethodCurlyOnNewLine.java",
+        "InputIndentationAnonymousClasses.java",
+        "InputIndentationBraceAdjustment.java",
+        "InputIndentationChainedMethodCalls.java",
+        "InputIndentationCtorCall.java",
+        "InputIndentationCustomAnnotation.java",
+        "InputIndentationFromGuava1.java",
+        "InputIndentationIfAndParameter.java",
+        "InputIndentationInvalidArrayInitIndentWithoutTrailingComments.java",
+        "InputIndentationInvalidClassDefIndent.java",
+        "InputIndentationInvalidClassDefIndent1.java",
+        "InputIndentationInvalidForIndent.java",
+        "InputIndentationInvalidIfIndent.java",
+        "InputIndentationInvalidIfIndent2.java",
+        "InputIndentationInvalidLabelIndent.java",
+        "InputIndentationInvalidMethodIndent2.java",
+        "InputIndentationLambda.java",
+        "InputIndentationLambda1.java",
+        "InputIndentationLambda2.java",
+        "InputIndentationLineWrappedRecordDeclaration.java",
+        "InputIndentationMethodPrecededByAnnotationWithParameterOnSeparateLine.java",
+        "InputIndentationNewChildren.java",
+        "InputIndentationNewWithForceStrictCondition.java",
+        "InputIndentationStrictCondition.java",
+        "InputIndentationTryResourcesNotStrict.java",
+        "InputIndentationTryResourcesNotStrict1.java",
+        "InputIndentationTryWithResourcesStrict.java",
+        "InputIndentationTryWithResourcesStrict1.java",
+        "InputIndentationValidBlockIndent1.java",
+        "InputIndentationValidClassDefIndent.java",
+        "InputIndentationValidClassDefIndent1.java",
+        "InputIndentationCorrectIfAndParameter1.java",
+        "InputIndentationPackageDeclaration3.java"
+    );
+
+    @ParameterizedTest
+    @MethodSource("indentationTestFiles")
+    public void testTrailingCommentsAlignment(Path testFile) throws IOException {
+        final String fileName = testFile.getFileName().toString();
+        if (ALLOWED_VIOLATION_FILES.contains(fileName)) {
+            Assumptions.assumeTrue(false, "Skipping file: " + fileName);
+        }
+
+        final List<String> lines = Files.readAllLines(testFile);
+        int expectedStartIndex = -1;
+
+        for (int idx = 0; idx < lines.size(); idx++) {
+            final String line = lines.get(idx);
+            final int commentStartIndex = line.indexOf("//indent:");
+            if (commentStartIndex > 0) {
+                final String codePart = line.substring(0, commentStartIndex);
+                if (!codePart.isBlank()) {
+                    final int actualStartIndex =
+                        CommonUtil.lengthExpandedTabs(line, commentStartIndex, TAB_WIDTH);
+                    if (expectedStartIndex == -1) {
+                        expectedStartIndex = actualStartIndex;
+                    }
+                    else {
+                        Assertions.assertEquals(expectedStartIndex, actualStartIndex,
+                            "Trailing comment alignment mismatch in file: "
+                                + testFile + " on line " + (idx + 1));
+                    }
+                }
+            }
+        }
+    }
+
+    private static Stream<Path> indentationTestFiles() {
+        final Path resourcesDir = Paths.get("src", "test", "resources");
+        final Path indentationDir = resourcesDir.resolve(INDENTATION_TEST_FILES_PATH);
+
+        Stream<Path> testFiles;
+        try {
+            testFiles = Files.walk(indentationDir)
+                .filter(path -> {
+                        final String fileName = path.getFileName().toString();
+                        return fileName.startsWith("InputIndentation")
+                            && fileName.endsWith(".java");
+                    }
+                );
+        }
+        catch (IOException exception) {
+            Assertions.fail("Failed to find indentation test files", exception);
+            testFiles = Stream.empty();
+        }
+        return testFiles;
+    }
+}
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationValidMethodIndent.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationValidMethodIndent.java
@@ -1,94 +1,94 @@
-package com.puppycrawl.tools.checkstyle.checks.indentation.indentation; //indent:0 exp:0
+package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;        //indent:0 exp:0
 
-import java.util.Arrays; //indent:0 exp:0
+import java.util.Arrays;                                                       //indent:0 exp:0
 
-/**                                                                           //indent:0 exp:0
- * This test-input is intended to be checked using following configuration:   //indent:1 exp:1
- *                                                                            //indent:1 exp:1
- * arrayInitIndent = 4                                                        //indent:1 exp:1
- * basicOffset = 4                                                            //indent:1 exp:1
- * braceAdjustment = 0                                                        //indent:1 exp:1
- * caseIndent = 4                                                             //indent:1 exp:1
- * forceStrictCondition = false                                               //indent:1 exp:1
- * lineWrappingIndentation = 4                                                //indent:1 exp:1
- * tabWidth = 4                                                               //indent:1 exp:1
- * throwsIndent = 4                                                           //indent:1 exp:1
- *                                                                            //indent:1 exp:1
- * @author  jrichard                                                          //indent:1 exp:1
- */                                                                           //indent:1 exp:1
-public class InputIndentationValidMethodIndent extends Object { //indent:0 exp:0
+/**                                                                            //indent:0 exp:0
+ * This test-input is intended to be checked using following configuration:    //indent:1 exp:1
+ *                                                                             //indent:1 exp:1
+ * arrayInitIndent = 4                                                         //indent:1 exp:1
+ * basicOffset = 4                                                             //indent:1 exp:1
+ * braceAdjustment = 0                                                         //indent:1 exp:1
+ * caseIndent = 4                                                              //indent:1 exp:1
+ * forceStrictCondition = false                                                //indent:1 exp:1
+ * lineWrappingIndentation = 4                                                 //indent:1 exp:1
+ * tabWidth = 4                                                                //indent:1 exp:1
+ * throwsIndent = 4                                                            //indent:1 exp:1
+ *                                                                             //indent:1 exp:1
+ * @author  jrichard                                                           //indent:1 exp:1
+ */                                                                            //indent:1 exp:1
+public class InputIndentationValidMethodIndent extends Object {                //indent:0 exp:0
 
-    // ctor with rcurly on same line //indent:4 exp:4
-    public InputIndentationValidMethodIndent() { //indent:4 exp:4
-    } //indent:4 exp:4
+    // ctor with rcurly on same line                                           //indent:4 exp:4
+    public InputIndentationValidMethodIndent() {                               //indent:4 exp:4
+    }                                                                          //indent:4 exp:4
 
-    private InputIndentationValidMethodIndent(boolean test) { //indent:4 exp:4
-        boolean test2 = true; //indent:8 exp:>=8
+    private InputIndentationValidMethodIndent(boolean test) {                  //indent:4 exp:4
+        boolean test2 = true;                                                  //indent:8 exp:>=8
 
-        int i = 4 +  //indent:8 exp:8
-            4; //indent:12 exp:>=12
-    } //indent:4 exp:4
-
-
-    private InputIndentationValidMethodIndent(boolean test, //indent:4 exp:4
-        boolean test2) { //indent:8 exp:>=8
-        boolean test3 = true; //indent:8 exp:>=8
-
-        int i = 4 +  //indent:8 exp:8
-            4; //indent:12 exp:>=12
-    } //indent:4 exp:4
+        int i = 4 +                                                            //indent:8 exp:8
+            4;                                                                 //indent:12 exp:>=12
+    }                                                                          //indent:4 exp:4
 
 
-    private InputIndentationValidMethodIndent(boolean test, //indent:4 exp:4
-        boolean test2, boolean test3)  //indent:8 exp:>=8
-    { //indent:4 exp:4
-        boolean test4 = true; //indent:8 exp:8
+    private InputIndentationValidMethodIndent(boolean test,                    //indent:4 exp:4
+        boolean test2) {                                                       //indent:8 exp:>=8
+        boolean test3 = true;                                                  //indent:8 exp:>=8
 
-        int i = 4 +  //indent:8 exp:8
-            4; //indent:12 exp:>=12
-    } //indent:4 exp:4
+        int i = 4 +                                                            //indent:8 exp:8
+            4;                                                                 //indent:12 exp:>=12
+    }                                                                          //indent:4 exp:4
 
-    // ctor with rcurly on next line //indent:4 exp:4
-    public InputIndentationValidMethodIndent(int dummy) //indent:4 exp:4
-    { //indent:4 exp:4
-    } //indent:4 exp:4
 
-    // method with rcurly on next line //indent:4 exp:4
-    public void method2() //indent:4 exp:4
-    { //indent:4 exp:4
-    } //indent:4 exp:4
+    private InputIndentationValidMethodIndent(boolean test,                    //indent:4 exp:4
+        boolean test2, boolean test3)                                          //indent:8 exp:>=8
+    {                                                                          //indent:4 exp:4
+        boolean test4 = true;                                                  //indent:8 exp:8
 
-    // params on multiple lines //indent:4 exp:4
-    public void method2(int x, int y, int w, int h, //indent:4 exp:4
-        int x1, int y1, int w1, int h1) //indent:8 exp:>=8
-    { //indent:4 exp:4
-    } //indent:4 exp:4
+        int i = 4 +                                                            //indent:8 exp:8
+            4;                                                                 //indent:12 exp:>=12
+    }                                                                          //indent:4 exp:4
 
-    // params on multiple lines //indent:4 exp:4
-    public void method3(int x, int y, int w, int h, //indent:4 exp:4
-        int x1, int y1, int w1, int h1) //indent:8 exp:>=8
-    { //indent:4 exp:4
-        System.getProperty("foo"); //indent:8 exp:8
-    } //indent:4 exp:4
+    // ctor with rcurly on next line                                           //indent:4 exp:4
+    public InputIndentationValidMethodIndent(int dummy)                        //indent:4 exp:4
+    {                                                                          //indent:4 exp:4
+    }                                                                          //indent:4 exp:4
 
-    // strange IMHO, but I suppose this should be allowed //indent:4 exp:4
-    public //indent:4 exp:4
-    void //indent:4 exp:8 warn
-    method5() { //indent:4 exp:8 warn
-    } //indent:4 exp:4
+    // method with rcurly on next line                                         //indent:4 exp:4
+    public void method2()                                                      //indent:4 exp:4
+    {                                                                          //indent:4 exp:4
+    }                                                                          //indent:4 exp:4
 
-    private int[] getArray() { //indent:4 exp:4
-        return new int[] {1}; //indent:8 exp:8
-    } //indent:4 exp:4
+    // params on multiple lines                                                //indent:4 exp:4
+    public void method2(int x, int y, int w, int h,                            //indent:4 exp:4
+        int x1, int y1, int w1, int h1)                                        //indent:8 exp:>=8
+    {                                                                          //indent:4 exp:4
+    }                                                                          //indent:4 exp:4
 
-    private void indexTest() { //indent:4 exp:4
-        getArray()[0] = 2; //indent:8 exp:8
-    } //indent:4 exp:4
+    // params on multiple lines                                                //indent:4 exp:4
+    public void method3(int x, int y, int w, int h,                            //indent:4 exp:4
+        int x1, int y1, int w1, int h1)                                        //indent:8 exp:>=8
+    {                                                                          //indent:4 exp:4
+        System.getProperty("foo");                                             //indent:8 exp:8
+    }                                                                          //indent:4 exp:4
 
-    // the following lines have tabs //indent:4 exp:4
-	@SuppressWarnings( //indent:4 exp:4
-		value="" //indent:8 exp:8
-	) //indent:4 exp:4
-	public void testStartOfSequence() { //indent:4 exp:4
-	} //indent:4 exp:4
-} //indent:0 exp:0
+    // strange IMHO, but I suppose this should be allowed                      //indent:4 exp:4
+    public                                                                     //indent:4 exp:4
+    void                                                                       //indent:4 exp:8 warn
+    method5() {                                                                //indent:4 exp:8 warn
+    }                                                                          //indent:4 exp:4
+
+    private int[] getArray() {                                                 //indent:4 exp:4
+        return new int[] {1};                                                  //indent:8 exp:8
+    }                                                                          //indent:4 exp:4
+
+    private void indexTest() {                                                 //indent:4 exp:4
+        getArray()[0] = 2;                                                     //indent:8 exp:8
+    }                                                                          //indent:4 exp:4
+
+    // the following lines have tabs                                           //indent:4 exp:4
+	@SuppressWarnings(                                                         //indent:4 exp:4
+		value=""                                                               //indent:8 exp:8
+	)                                                                          //indent:4 exp:4
+	public void testStartOfSequence() {                                        //indent:4 exp:4
+	}                                                                          //indent:4 exp:4
+}                                                                              //indent:0 exp:0
