5c696faec6893b458f0bea2a71018042a754240a
Mohit Attry <atrrmohit@gmail.com>
2025-07-17 16:32:52 +0530

    Issue #14291: Add TEXT_BLOCK_CONTENT token in IllegalTokenText in google config

---
+++ src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule232specialescape/InputSpecialEscapeSequencesInTextBlock2.java
@@ -0,0 +1,219 @@
+package com.google.checkstyle.test.chapter2filebasic.rule232specialescape;
+
+/** Some javadoc. */
+public class InputSpecialEscapeSequencesInTextBlock2 {
+
+  class Inner {
+    public String wrongEscapeSequences() {
+      final String r1 =
+          """
+          \u0008
+          """;
+      final String r2 = // violation below 'Consider using special escape sequence .*'
+          """
+          \u0009
+          """;
+      final String r3 = // violation below 'Consider using special escape sequence .*'
+          """
+          \u000csssdfsd
+          """;
+
+      final char r5 = // violation below 'Consider using special escape sequence .*'
+          """
+          '\012'
+          """;
+      final char r6 = // violation below 'Consider using special escape sequence .*'
+          """
+          '\\u0022'
+          """;
+    }
+
+    public void specialCharsWithoutWarn() {
+      String r1 =
+           """
+           \b
+           """;
+      String r2 =
+           """
+           \t
+           """;
+      String r3 =
+           """
+           \n
+           """;
+      String r4 =
+           """
+           \f
+           """;
+      String r5 =
+           """
+           \r
+           """;
+      String r6 =
+           """
+           \"
+           """;
+      String r7 =
+           """
+           \'
+           """;
+      String r8 =
+           """
+           \\
+           """;
+    }
+
+    public void specialCharsWithWarn() {
+      String r1 =
+          """
+          \\u0008
+          """;
+      String r2 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\u0009
+          """;
+      String r3 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\u000a
+          """;
+      String r4 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\u000c
+          """;
+      String r5 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\u000d
+          """;
+    }
+
+    public void specialCharsWithWarn2() {
+      String r1 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\010
+          """;
+      String r2 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\011
+          """;
+      String r3 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\012
+          """;
+      String r4 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\014
+          """;
+      String r5 = // violation below 'Consider using special escape sequence .*'
+          """
+          \\015
+          """;
+    }
+
+    Inner anoInner =
+        new Inner() {
+          public String wrongEscapeSequences() {
+            final String r1 =
+                """
+                \u0008
+                """;
+            final String r2 = // violation below 'Consider using special escape sequence .*'
+                """
+                \u0009
+                """;
+            final String r3 = // violation below 'Consider using special escape sequence .*'
+                """
+                \u000csssdfsd
+                """;
+            final char r5 = // violation below 'Consider using special escape sequence .*'
+                """
+                '\012'
+                """;
+            final char r6 = // violation below 'Consider using special escape sequence .*'
+                """
+                '\\u0022'
+                """;
+            final char r7 =
+                """
+                '\b'
+                """;
+            return // violation below 'Consider using special escape sequence .*'
+                """
+                \u000csssdfsd
+                """;
+          }
+
+          public void specialCharsWithoutWarn() {
+            String r1 =
+                """
+                \b
+                """;
+            String r2 =
+                """
+                \t
+                """;
+            String r3 =
+                """
+                \n
+                """;
+            String r4 =
+                """
+                \f
+                """;
+            String r5 =
+                """
+                \r
+                """;
+            String r6 =
+                """
+                \"
+                """;
+            String r7 =
+                """
+                \'
+                """;
+            String r8 =
+                """
+                \\
+                """;
+          }
+
+          public void specialCharsWithWarn() {
+            String r1 =
+                """
+                \\u0008
+                """;
+            String r2 = // violation below 'Consider using special escape sequence .*'
+                """
+                \\u0009
+                """;
+            String r3 = // violation below 'Consider using special escape sequence .*'
+                """
+                \\u000a
+                """;
+            String r4 = // violation below 'Consider using special escape sequence .*'
+                """
+                \\u000c
+                """;
+          }
+
+          public void specialCharsWithWarn2() {
+            String r1 = // violation below 'Consider using special escape sequence .*'
+                """
+                \\010
+                """;
+            String r2 = // violation below 'Consider using special escape sequence .*'
+                """
+                \\011
+                """;
+            String r3 = // violation below 'Consider using special escape sequence .*'
+                """
+                \\012
+                """;
+            String r4 = // violation below 'Consider using special escape sequence .*'
+                """
+                \\014
+                """;
+          }
+        };
+  }
+}
