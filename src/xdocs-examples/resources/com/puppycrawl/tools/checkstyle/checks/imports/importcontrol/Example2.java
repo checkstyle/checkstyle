7aa104c1cd188b56d79db102c973e2e7693b0c6e
SteLeo1602 <stepanleontev16@gmail.com>
2025-07-17 23:06:27 -0700

    Issue #17400: Enabled examples macro for Example 3, 4 in importcontrol.xml.template

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/Example2.java
@@ -2,18 +2,18 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="ImportControl">
-      <property name="file" value="${config.folder}/import-control1.xml"/>
+      <property name="file" value="${config.folder}/import-control2.xml"/>
+      <property name="path" value="${config.folder}[\\/]api[\\/].*$"/>
     </module>
   </module>
 </module>
 */
 
-// non-compiled with javac: Compilable with Java17
 // xdoc section -- start
 package com.puppycrawl.tools.checkstyle.checks.imports.importcontrol;
 
-import java.io.File; // violation, 'Disallowed import - java.io.File'
+import java.io.File; // ok, import control pkg attribute does not package this class
 import java.io.FileReader;
 
-public class Example1 {}
+public class Example2 {}
 // xdoc section -- end
