d592a1fdbaf98b89445a85f54e6c7a61def65435
SteLeo1602 <stepanleontev16@gmail.com>
2025-07-17 23:06:27 -0700

    Issue #17400: Enabled examples macro for Example 3, 4 in importcontrol.xml.template

---
+++ src/xdocs-examples/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/Example5.java
@@ -0,0 +1,21 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="ImportControl">
+      <property name="file" value="${config.folder}/import-control5.xml"/>
+    </module>
+  </module>
+</module>
+*/
+
+// non-compiled with javac: Compilable with Java17
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.imports.importcontrol;
+
+import java.awt.Image; // violation, 'Disallowed import - java.awt.Image'
+import java.io.File; // violation, 'Disallowed import - java.io.File'
+
+import java.util.Scanner; // ok, allowed on mismatch by default
+
+public class Example5 {}
+// xdoc section -- end
