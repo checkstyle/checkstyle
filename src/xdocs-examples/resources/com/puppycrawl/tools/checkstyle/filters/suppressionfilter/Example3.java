0a77b61524abd8ecd7d5b949ce3918cb768c23ea
Praveen Kumar <praveen729483@gmail.com>
2025-07-20 17:13:44 +0530

    Issue #17401: Added more macro examples to the suppressionfilter.xml.template

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressionfilter/Example3.java
@@ -0,0 +1,19 @@
+/*xml
+<module name="Checker">
+  <property name="fileExtensions" value="properties"/>
+
+  <module name="SuppressionFilter">
+    <property name="file" value="suppressionexample3.xml"/>
+  </module>
+
+  <module name="OrderedProperties"/>
+  <module name="UniqueProperties"/>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.filters.suppressionfilter;
+
+public class Example3 {}
+
+// xdoc section -- start
+// xdoc section -- end
