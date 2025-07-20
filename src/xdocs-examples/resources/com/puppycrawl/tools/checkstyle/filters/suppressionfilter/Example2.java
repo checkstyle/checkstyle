d3bfe27dd81b391eea3f55c91e9187f4db621924
Praveen Kumar <praveen729483@gmail.com>
2025-07-20 17:13:44 +0530

    Issue #17401: Added more macro examples to the suppressionfilter.xml.template

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressionfilter/Example2.java
@@ -0,0 +1,38 @@
+/*xml
+<module name="Checker">
+  <module name="SuppressionFilter">
+    <property name="file" value="suppressionexample2.xml"/>
+    <property name="optional" value="false"/>
+  </module>
+  <module name="TreeWalker">
+    <module name="EqualsAvoidNull">
+      <property name="id" value="stringEqual"/>
+    </module>
+    <module name="LineLength">
+      <property name="id" value="lineLength"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.filters.suppressionfilter;
+// xdoc section -- start
+public class Example2 {
+
+  // violation below, 'Line is longer than 80 characters'
+  String line = "This line is long and exceeds the default limit of 80 characters.";
+
+  public void foo() {
+
+    String nullString = null;
+
+    // filtered violation below 'expressions should be on the left side'
+    nullString.equals("My_Sweet_String");
+    "My_Sweet_String".equals(nullString);
+
+    // filtered violation below 'expressions should be on the left side'
+    nullString.equalsIgnoreCase("My_Sweet_String");
+    "My_Sweet_String".equalsIgnoreCase(nullString);
+  }
+}
+// xdoc section -- end
