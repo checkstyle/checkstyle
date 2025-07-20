57ad3a47e7ddc79b50f97347064ed433bf477cc0
SteLeo1602 <stepanleontev16@gmail.com>
2025-07-19 21:31:07 -0700

    Issue #17400: Enabled examples macro for Example 8 in importcontrol.xml.template

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/gui/Example8View.java
@@ -0,0 +1,20 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="ImportControl">
+      <property name="file" value="${config.folder}/import-control8.xml"/>
+    </module>
+  </module>
+</module>
+*/
+
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.imports.importcontrol.gui;
+
+import java.sql.Blob; // violation, 'Disallowed import - java.sql.Blob'
+import java.io.File;
+
+import javax.swing.Renderer; // ok, does not match a file name for disallow rule.
+
+public class Example8View {}
+// xdoc section -- end
