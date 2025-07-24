c6c4cbe7c8cb48943f39239bd6f8131c2a0ec3f6
KishorMore <kishormoresuresh123@gmail.com>
2025-07-24 18:52:28 +0530

    Issue #14631: Updated JavadocTokenTypes.java to new format of AST print

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
@@ -1040,6 +1040,23 @@ public final class JavadocTokenTypes {
 
     /**
      * Start html tag component: {@code '<'}.
+     *
+     * <p><b>Tree:</b></p>
+     * <pre>{@code
+     * JAVADOC -> JAVADOC
+     *  |--NEWLINE -> \r\n
+     *  |--LEADING_ASTERISK ->  *
+     *  |--TEXT ->  Start html tag component:
+     *  |--JAVADOC_INLINE_TAG -> JAVADOC_INLINE_TAG
+     *  |   |--JAVADOC_INLINE_TAG_START -> {
+     *  |   |--CODE_LITERAL -> @code
+     *  |   |--WS ->
+     *  |   |--TEXT -> '<'
+     *  |   `--JAVADOC_INLINE_TAG_END -> }
+     *  |--TEXT -> .
+     *  |--NEWLINE -> \r\n
+     *  |--TEXT ->
+     *  }</pre>
      */
     public static final int START = JavadocParser.START;
 
