89a37b49396a1d4d485414eab01e1f049b09c03b
Vincent Potucek <vpotucek@me.com>
2025-07-29 16:29:59 +0200

    Issue #17487: Add `rewrite` support for `errorprone.refasterrules`

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -428,6 +428,10 @@ spotbugs-and-pmd)
   exit "$RESULT"
 ;;
 
+rewrite)
+  ./mvnw -e --no-transfer-progress clean rewrite:dryRun
+  ;;
+
 site)
   ./mvnw -e --no-transfer-progress clean site -Pno-validations
   ;;
