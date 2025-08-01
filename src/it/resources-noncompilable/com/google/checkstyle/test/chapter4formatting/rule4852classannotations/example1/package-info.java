73ab4c0aa0465226c73996cfe1dfedd24c99d203
Mohit Attry <atrrmohit@gmail.com>
2025-08-01 18:12:03 +0530

    supplemental: IT for Annotations on class and package

---
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule4852classannotations/example1/package-info.java
@@ -0,0 +1,3 @@
+/** This is a package. */
+@Deprecated @CheckReturnValue // false-negative until #17540
+package com.google.checkstyle.test.chapter4formatting.rule4852classannotations;
---
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule4852classannotations/package-info.java
@@ -0,0 +1,4 @@
+/** This is a package. */
+@Deprecated
+@CheckReturnValue
+package com.google.checkstyle.test.chapter4formatting.rule4852classannotations;
