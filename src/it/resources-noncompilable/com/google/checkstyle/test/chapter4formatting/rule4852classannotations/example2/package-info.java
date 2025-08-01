5ce20bc52cfff475ec9ceec2e2b1a9d2aae82816
Mohit Attry <atrrmohit@gmail.com>
2025-08-01 18:12:03 +0530

    supplemental: IT for Annotations on class and package

---
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule4852classannotations/example2/package-info.java
@@ -0,0 +1,7 @@
+// non-compiled with javac: Used for Testing purpose.
+
+/** This is a package. */
+@Deprecated
+// testing
+@SuppressWarnings("...")
+package com.google.checkstyle.test.chapter4formatting.rule4852classannotations;
