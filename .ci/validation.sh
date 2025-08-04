2f76db240f672621ff5a54e6eb5f09d672b86445
Vincent Potucek <vpotucek@me.com>
2025-08-04 10:15:01 +0200

    Issue #17487: Add `rewrite` support for `errorprone.refasterrules`

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -429,7 +429,9 @@ spotbugs-and-pmd)
 ;;
 
 rewrite)
-  export MAVEN_OPTS='-Xmx4g'
+  # https://docs.openrewrite.org/reference/faq#im-getting-javalangoutofmemoryerror-java-heap-space-when-running-openrewrite
+  # export MAVEN_OPTS='-Xmx6g' check performance diff
+  # export MAVEN_OPTS='-Xmx8g'
   ./mvnw -e --no-transfer-progress clean rewrite:dryRun
   ;;
 
