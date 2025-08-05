df5a9ca230effc4aa03c2990695da501e8f8adc1
Mohit Attry <atrrmohit@gmail.com>
2025-08-05 12:54:42 +0530

    Issue #17563: Remove exceptionVariableName property of EmptyCatchBlock Check

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule413emptyblocks/InputEmptyCatchEmptyComment.java
@@ -0,0 +1,81 @@
+package com.google.checkstyle.test.chapter4formatting.rule413emptyblocks;
+
+import java.io.IOException;
+
+/** some javadoc. */
+public class InputEmptyCatchEmptyComment {
+  private void foo() {
+    try {
+      throw new RuntimeException();
+      // violation below 'Empty catch block'
+    } catch (Exception expected) {
+      //
+    }
+  }
+
+  private void foo1() {
+    try {
+      throw new RuntimeException();
+      // violation below 'Empty catch block'
+    } catch (Exception e) {
+      //
+    }
+  }
+
+  private void foo2() {
+    try {
+      throw new IOException();
+    } catch (IOException | NullPointerException | ArithmeticException ignore) {
+      // ignore
+    }
+  }
+
+  private void foo3() { // comment
+    try {
+      throw new IOException();
+      // violation below 'Empty catch block'
+    } catch (IOException | NullPointerException | ArithmeticException e) {
+      /**/
+    }
+  }
+
+  private void foo4() {
+    try {
+      throw new IOException();
+      // violation below 'Empty catch block'
+    } catch (IOException | NullPointerException | ArithmeticException e) {
+      /* */
+    }
+  }
+
+  private void foo5() {
+    try {
+      throw new IOException();
+    } catch (IOException | NullPointerException | ArithmeticException e) { //
+    } // violation above 'Empty catch block'
+  }
+
+  private void foo6() {
+    try {
+      throw new IOException();
+    } catch (IOException e) { //
+    } // violation above 'Empty catch block'
+  }
+
+  private void foo7() {
+    try {
+      throw new IOException();
+      // violation below 'Empty catch block'
+    } catch (IOException e) {
+      /**/
+    }
+  }
+
+  private void foo8() {
+    try {
+      throw new IOException();
+    } catch (IOException e) {
+      /* no violation. */
+    }
+  }
+}
