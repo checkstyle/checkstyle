21262a63b38ee4b957869dee0a1c8394e04abeeb
Vincent Potucek <vpotucek@me.com>
2025-08-05 18:17:13 +0200

    Issue #17487: Add `rewrite` support

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -428,6 +428,10 @@ spotbugs-and-pmd)
   exit "$RESULT"
 ;;
 
+rewriteDryRun)
+  ./mvnw -e --no-transfer-progress clean rewrite:dryRun
+  ;;
+
 site)
   ./mvnw -e --no-transfer-progress clean site -Pno-validations
   ;;
