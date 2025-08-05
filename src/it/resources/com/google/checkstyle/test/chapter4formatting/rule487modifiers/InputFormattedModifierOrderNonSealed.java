1db6d23168428712426bd236eb2b87f6a72be12c
Mohit Attry <atrrmohit@gmail.com>
2025-08-05 16:33:14 +0530

    supplemental: IT for '4.8.7 Modifiers'

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule487modifiers/InputFormattedModifierOrderNonSealed.java
@@ -0,0 +1,39 @@
+package com.google.checkstyle.test.chapter4formatting.rule487modifiers;
+
+/** some javadoc. */
+public sealed class InputFormattedModifierOrderNonSealed
+    permits InputFormattedModifierOrderNonSealed.CircleOne,
+        InputFormattedModifierOrderNonSealed.RectangleOne,
+        InputFormattedModifierOrderNonSealed.SquareOne {
+
+  final class CircleOne extends InputFormattedModifierOrderNonSealed implements SquircleOne {}
+
+  sealed class RectangleOne extends InputFormattedModifierOrderNonSealed
+      permits TransparentRectangleOne, FilledRectangleOne {}
+
+  final class TransparentRectangleOne extends RectangleOne {}
+
+  sealed class SquareOne extends InputFormattedModifierOrderNonSealed implements SquircleOne {
+
+    private sealed class OtherSquare extends SquareOne permits OtherSquare2 {}
+
+    private final class OtherSquare2 extends OtherSquare {}
+
+    static non-sealed class StaticClass implements SquircleOne {}
+  }
+
+  final class FilledRectangleOne extends RectangleOne {}
+
+  sealed interface SquircleOne permits CircleOne, SquareOne, Ellipse, SquareOne.StaticClass {}
+
+  sealed interface Rhombus permits Parallelogram, Parallelogram.Trapezoid {}
+
+  record Parallelogram(int x, int y, double z) implements Rhombus {
+
+    public record Trapezoid(int x, int y, double z) implements Rhombus {}
+  }
+
+  non-sealed interface Ellipse extends SquircleOne {}
+
+  class Oval implements Ellipse {}
+}
---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule487modifiers/InputModifierOrderNonSealed.java
@@ -0,0 +1,46 @@
+package com.google.checkstyle.test.chapter4formatting.rule487modifiers;
+
+// violation 2 lines below ''public' modifier out of order with the JLS suggestions.'
+/** some javadoc. */
+sealed public class InputModifierOrderNonSealed
+    permits InputModifierOrderNonSealed.CircleOne1, InputModifierOrderNonSealed.RectangleOne1,
+        InputModifierOrderNonSealed.SquareOne1 {
+
+  final class CircleOne1 extends InputModifierOrderNonSealed
+      implements SquircleOne1 {
+  }
+
+  sealed class RectangleOne1 extends InputModifierOrderNonSealed
+          permits TransparentRectangleOne1, FilledRectangleOne1 {
+  }
+
+  final class TransparentRectangleOne1 extends RectangleOne1 {
+  }
+
+  sealed class SquareOne1 extends InputModifierOrderNonSealed implements SquircleOne1 {
+
+    sealed private class OtherSquare extends SquareOne1 permits OtherSquare2 {
+    } // violation above ''private' modifier out of order with the JLS suggestions.'
+
+    private final class OtherSquare2 extends OtherSquare {}
+
+    static non-sealed class StaticClass1 implements SquircleOne1 {}
+  }
+
+  final class FilledRectangleOne1 extends RectangleOne1 {}
+
+  sealed interface SquircleOne1 permits CircleOne1, SquareOne1, Ellipse1, SquareOne1.StaticClass1 {
+  }
+
+  sealed interface Rhombus1 permits Parallelogram1,
+          Parallelogram1.Trapezoid1 {}
+
+  record Parallelogram1(int x, int y, double z) implements Rhombus1 {
+
+    public record Trapezoid1(int x, int y, double z) implements Rhombus1 {}
+  }
+
+  non-sealed interface Ellipse1 extends SquircleOne1 {}
+
+  class Oval1 implements Ellipse1 {}
+}
