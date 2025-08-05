39b290f0a9c01f4db51bedbeec554ba4d7f7f006
Mohit Attry <atrrmohit@gmail.com>
2025-08-05 16:33:14 +0530

    supplemental: IT for '4.8.7 Modifiers'

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule487modifiers/InputModifierOrderSealed.java
@@ -0,0 +1,28 @@
+package com.google.checkstyle.test.chapter4formatting.rule487modifiers;
+
+// violation 2 lines below ''public' modifier out of order with the JLS suggestions'
+/** some javadoc. */
+sealed public class InputModifierOrderSealed permits
+        InputModifierOrderSealed.Four,
+        InputModifierOrderSealed.Three,
+        InputModifierOrderSealed.Two {
+
+  /** some javadoc. */
+  public final class Two extends InputModifierOrderSealed implements Five {}
+
+  sealed class Four extends InputModifierOrderSealed {}
+
+  sealed class Three extends InputModifierOrderSealed implements Five {
+
+    // violation below ''private' modifier out of order with the JLS suggestions'
+    sealed private class OtherSquare extends Three {}
+
+    private final class OtherSquare2 extends OtherSquare {}
+  }
+
+  /** some javadoc. */
+  public final class FilledFour extends Four {}
+
+  // violation below ''abstract' modifier out of order with the JLS suggestions'
+  sealed abstract interface Five permits Three, Two {}
+}
