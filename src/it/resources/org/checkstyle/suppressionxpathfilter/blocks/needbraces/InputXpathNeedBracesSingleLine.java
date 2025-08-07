08855b400cc62ce39b9f5f9dd053983051b5dd90
Zbynek Konecny <zbynek@geogebra.org>
2025-08-07 18:27:26 +0200

    Issue #17554: Report UnneccessaryParentheses for dot, 'new' and array access

--- src/it/resources/org/checkstyle/suppressionxpathfilter/blocks/needbraces/InputXpathNeedBracesSingleLine.java
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/blocks/needbraces/InputXpathNeedBracesSingleLine.java
@@ -1,7 +1,5 @@
 package org.checkstyle.suppressionxpathfilter.blocks.needbraces;
 
-import com.puppycrawl.tools.checkstyle.checks.blocks.needbraces.InputNeedBracesSingleLineStatements;
-
 public class InputXpathNeedBracesSingleLine {
     private static class SomeClass {
         boolean flag = true;
