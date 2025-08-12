81ddb6575df80a0b740878197839512fa067b41f
Mohit Attry <atrrmohit@gmail.com>
2025-08-12 10:48:14 +0530

    Issue #17551: Violation on octal value of '\s'

--- src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule232specialescape/InputSpecialEscapeSequencesInTextBlockForOctalValues.java
+++ src/it/resources/com/google/checkstyle/test/chapter2filebasic/rule232specialescape/InputSpecialEscapeSequencesInTextBlockForOctalValues.java
@@ -125,6 +125,10 @@ public class InputSpecialEscapeSequencesInTextBlockForOctalValues {
         """
         \\134
         """;
+    String r9 = // violation below 'Consider using special escape sequence'
+        """
+        \\040
+        """;
   }
 
   private static void specialCharsWithWarn3() {
@@ -150,13 +154,17 @@ public class InputSpecialEscapeSequencesInTextBlockForOctalValues {
         """;
     String r6 = // violation below 'Consider using special escape sequence'
         """
-        \\042
+        \\040
         """;
     String r7 = // violation below 'Consider using special escape sequence'
         """
-        \\047
+        \\042
         """;
     String r8 = // violation below 'Consider using special escape sequence'
+        """
+        \\047
+        """;
+    String r9 = // violation below 'Consider using special escape sequence'
         """
         \\134
         """;
@@ -226,13 +234,17 @@ public class InputSpecialEscapeSequencesInTextBlockForOctalValues {
               """;
           String r6 = // violation below 'Consider using special escape sequence'
               """
-              \\042
+              \\040
               """;
           String r7 = // violation below 'Consider using special escape sequence'
               """
-              \\047
+              \\042
               """;
           String r8 = // violation below 'Consider using special escape sequence'
+              """
+              \\047
+              """;
+          String r9 = // violation below 'Consider using special escape sequence'
               """
               \\134
               """;
