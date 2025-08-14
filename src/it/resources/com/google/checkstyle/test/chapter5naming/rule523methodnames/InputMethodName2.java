f1084a78d9f40519a4859130b32fb43192b47b45
Mohit Attry <atrrmohit@gmail.com>
2025-08-14 11:54:04 +0530

    supplemental: IT covering test methods names containing underscores

---
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule523methodnames/InputMethodName2.java
@@ -0,0 +1,37 @@
+package com.google.checkstyle.test.chapter5naming.rule523methodnames;
+
+import org.junit.jupiter.api.Test;
+
+/** some data. */
+public class InputMethodName2 {
+
+  @Test
+  public void transferMoney_deductsFromSource() {}
+
+  @Test // violation below 'Method name 'transferMoney_DeductsFromSource' must match pattern'
+  public void transferMoney_DeductsFromSource() {}
+
+  @Test
+  public void solve6x6_returnsTrue() {}
+
+  @Test // violation below 'Method name 'solve6x6_ReturnsTrue' must match pattern'
+  public void solve6x6_ReturnsTrue() {}
+
+  @Test
+  public void solve6x6_noSolution_returnsFalse() {}
+
+  @Test // violation below ''solve6x6_NoSolution_ReturnsFalse' must match pattern'
+  public void solve6x6_NoSolution_ReturnsFalse() {}
+
+  @Test
+  public void openMenu_deletePreviousView() {}
+
+  @Test // violation below 'Method name 'openMenu_DeletePreviousView' must match pattern'
+  public void openMenu_DeletePreviousView() {}
+
+  @Test
+  public void test_general_logic() {}
+
+  @Test
+  public void test_General_Logic() {} // violation ''test_General_Logic' must match pattern'
+}
