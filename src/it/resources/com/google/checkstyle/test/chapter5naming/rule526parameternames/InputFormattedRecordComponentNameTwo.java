2073613bcf9a3cb7c6a7b397a54dfe3acb38903d
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-08-15 21:13:59 +0530

    Issue #17429: Relocate inputs to compilable path for IT module

---
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule526parameternames/InputFormattedRecordComponentNameTwo.java
@@ -0,0 +1,13 @@
+// Java17
+
+package com.google.checkstyle.test.chapter5naming.rule526parameternames;
+
+/** some javadoc. Config: format = "^[a-z]([a-z0-9][a-zA-Z0-9]*)?$" */
+public record InputFormattedRecordComponentNameTwo<E>(int _componentName, String componentName2) {}
+
+// violation 2 lines above 'Record component name '_componentName' must match pattern'
+
+record InputRecordComponentName<E>(int Capital) {}
+// 2 violations above:
+//  'Top-level class InputRecordComponentName has to reside in its own source file.'
+//  'Record component name 'Capital' must match pattern'
