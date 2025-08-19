bf8fdee8ccb04a7fbacadcb0f8148fa835651334
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-08-20 00:20:24 +0530

    Issue #17429: Relocate inputs to compilable path for Xpath Module 2

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example12.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example12.java
@@ -8,14 +8,14 @@
   </module>
 </module>
 */
-// non-compiled with javac: Compilable with Java17
+// Java17
 package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
 
 // xdoc section -- start
 import static java.io.File.listRoots;
-import static javax.swing.WindowConstants.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 import static java.io.File.createTempFile; // violation, should be before javax
-import static com.puppycrawl.tools.checkstyle;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 // xdoc section -- end
 
 public class Example12 { }
---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example3.java
@@ -0,0 +1,35 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="ImportOrder">
+      <property name="groups" value="/^java\./,javax,org,com"/>
+      <property name="ordered" value="true"/>
+      <property name="separated" value="true"/>
+      <property name="option" value="above"/>
+      <property name="sortStaticImportsAlphabetically" value="true"/>
+    </module>
+  </module>
+</module>
+*/
+// Java17
+package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
+
+// xdoc section -- start
+import static java.io.File.createTempFile;
+import static java.lang.Math.abs; // ok, alphabetical within java.*
+import static java.lang.Math.sqrt;
+
+import java.io.File; // violation, should come before java.lang.Math (alphabetical order)
+import java.io.IOException;
+
+import org.w3c.dom.Document;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE; // violation, wrong order, 'javax' must come before 'org'
+import javax.swing.JComponent;
+import org.w3c.dom.Element; // violation, must be separated from previous 'javax' import
+
+import com.sun.security.auth.UserPrincipal;
+import com.sun.source.tree.Tree;
+// xdoc section -- end
+
+public class Example3 { }
