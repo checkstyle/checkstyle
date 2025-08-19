b50b3f6ebbd99231c49792356a8351b8c8822e3b
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-08-20 00:20:24 +0530

    Issue #17429: Relocate inputs to compilable path for Xpath Module 2

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example12.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importorder/Example12.java
@@ -2,20 +2,20 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="ImportOrder">
-      <property name="staticGroups" value=""/>
+      <property name="staticGroups" value="java, javax"/>
       <property name="option" value="top"/>
     </module>
   </module>
 </module>
 */
-// non-compiled with javac: Compilable with Java17
+
 package com.puppycrawl.tools.checkstyle.checks.imports.importorder;
 
 // xdoc section -- start
 import static java.io.File.listRoots;
-import static javax.swing.WindowConstants.*;
-import static java.io.File.createTempFile;
-import static com.puppycrawl.tools.checkstyle;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import static java.io.File.createTempFile; // violation, should be before javax
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 // xdoc section -- end
 
-public class Example11 { }
+public class Example12 { }
