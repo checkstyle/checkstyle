0ac71f3f06cfc936d97c103ee4c22cb3f84adb5d
Mohit Attry <atrrmohit@gmail.com>
2025-08-20 12:51:02 +0530

    Issue #17540: Enforce Annotation location rule in package-info.java

---
+++ src/it/resources-noncompilable/com/google/checkstyle/test/chapter4formatting/rule4852classannotations/example3/package-info.java
@@ -0,0 +1,24 @@
+// non-compiled with javac: Used for Testing purpose.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
+@ParentPackage("person") // violation below 'Annotation 'Namespace' should be alone on line'
+@Namespace("/person") package org.apache.struts2.showcase.person;
+
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
