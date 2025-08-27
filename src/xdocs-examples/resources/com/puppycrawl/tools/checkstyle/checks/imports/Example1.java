a444701ac8c95fc23158fe884c957474bf772574
Balamurugan315 <119878232+Balamurugan315@users.noreply.github.com>
2025-08-27 09:35:06 +0530

    Update Example1.java

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/Example1.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Enumeration;
 import java.util.Arrays;
 import java.util.Date;
-import sun.applet.*; // violation, 'Illegal import'
+//import sun.applet.*; // violation, 'Illegal import'
 
 public class Example1 {}
 // xdoc section -- end
