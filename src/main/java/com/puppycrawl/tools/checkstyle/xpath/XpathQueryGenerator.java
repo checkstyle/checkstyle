5f6033d7fed22da2dc2cc5d41b3ff484c76947bb
Vincent Potucek <vpotucek@me.com>
2025-09-03 12:03:03 +0200

    Issue #17734: Introduce `Rewrite:UpgradeToJava17`

--- src/main/java/com/puppycrawl/tools/checkstyle/xpath/XpathQueryGenerator.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/xpath/XpathQueryGenerator.java
@@ -22,7 +22,7 @@ package com.puppycrawl.tools.checkstyle.xpath;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.Nullable;
+import jakarta.annotation.Nullable;
 
 import com.puppycrawl.tools.checkstyle.TreeWalkerAuditEvent;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
