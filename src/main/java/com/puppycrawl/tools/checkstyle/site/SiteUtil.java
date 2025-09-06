c209cdba74be9b1ef3c985ea0060507a64d0671d
Vincent Potucek <vpotucek@me.com>
2025-09-06 11:45:31 +0200

    Issue #17734: Introduce `Rewrite:UpgradeToJava17`

--- src/main/java/com/puppycrawl/tools/checkstyle/site/SiteUtil.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/site/SiteUtil.java
@@ -52,8 +52,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import javax.annotation.Nullable;
-
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.maven.doxia.macro.MacroExecutionException;
 
@@ -82,6 +80,7 @@ import com.puppycrawl.tools.checkstyle.checks.regexp.RegexpSinglelineJavaCheck;
 import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
 import com.puppycrawl.tools.checkstyle.utils.JavadocUtil;
 import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
+import jakarta.annotation.Nullable;
 
 /**
  * Utility class for site generation.
@@ -1126,12 +1125,12 @@ public final class SiteUtil {
      */
     private static IntStream getIntStream(Object value) {
         final IntStream stream;
-        if (value instanceof Collection<?> collection) {
-            stream = collection.stream()
+        if (value instanceof Collection<?> values) {
+            stream = values.stream()
                     .mapToInt(int.class::cast);
         }
-        else if (value instanceof BitSet) {
-            stream = ((BitSet) value).stream();
+        else if (value instanceof BitSet set) {
+            stream = set.stream();
         }
         else {
             stream = Arrays.stream((int[]) value);
