3f87a9ba51af61657617e38f01c51bab78c2d32e
Vinisha Wagh <vinisha.wagh21@gmail.com>
2025-09-07 15:16:21 +0530

    Issue #14631: Update Javadoc for SLASH token

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocTokenTypes.java
@@ -1033,7 +1033,23 @@ public final class JavadocTokenTypes {
     public static final int START = JavadocParser.START;
 
     /**
-     * Slash html tag component: {@code '/'}.
+     * Slash HTML tag component.
+     *
+     * <p><b>Example:</b></p>
+     * 
+     * <pre>
+     * <p>This is the first paragraph.</p>
+     * <p>This is the second paragraph.</p>
+     * </pre>
+     * 
+     * <p><b>Tree:</b></p>
+     * <pre>{@code
+     * HTML_ELEMENT_END -> HTML_ELEMENT_END
+     * |--START -> <
+     * |--SLASH -> /
+     * |--HTML_TAG_NAME -> p
+     * `--END -> >
+     * }</pre>
      */
     public static final int SLASH = JavadocParser.SLASH;
 
