755f0b1a8e08835c64d17dae983246e16f42837d
SteLeo1602 <stepanleontev16@gmail.com>
2025-09-08 16:30:15 -0700

    Issue #13626: Simplified modifier and regexp modules javadocs

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/regexp/RegexpMultilineCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/regexp/RegexpMultilineCheck.java
@@ -36,67 +36,6 @@ import com.puppycrawl.tools.checkstyle.api.FileText;
  * <p>
  * Rationale: This check can be used to when the regular expression can be span multiple lines.
  * </p>
- * <ul>
- * <li>
- * Property {@code fileExtensions} - Specify the file extensions of the files to process.
- * Type is {@code java.lang.String[]}.
- * Default value is {@code ""}.
- * </li>
- * <li>
- * Property {@code format} - Specify the format of the regular expression to match.
- * Type is {@code java.util.regex.Pattern}.
- * Default value is {@code "$."}.
- * </li>
- * <li>
- * Property {@code ignoreCase} - Control whether to ignore case when searching.
- * Type is {@code boolean}.
- * Default value is {@code false}.
- * </li>
- * <li>
- * Property {@code matchAcrossLines} - Control whether to match expressions
- * across multiple lines.
- * Type is {@code boolean}.
- * Default value is {@code false}.
- * </li>
- * <li>
- * Property {@code maximum} - Specify the maximum number of matches required in each file.
- * Type is {@code int}.
- * Default value is {@code 0}.
- * </li>
- * <li>
- * Property {@code message} - Specify the message which is used to notify about
- * violations, if empty then default (hard-coded) message is used.
- * Type is {@code java.lang.String}.
- * Default value is {@code null}.
- * </li>
- * <li>
- * Property {@code minimum} - Specify the minimum number of matches required in each file.
- * Type is {@code int}.
- * Default value is {@code 0}.
- * </li>
- * </ul>
- *
- * <p>
- * Parent is {@code com.puppycrawl.tools.checkstyle.Checker}
- * </p>
- *
- * <p>
- * Violation Message Keys:
- * </p>
- * <ul>
- * <li>
- * {@code regexp.StackOverflowError}
- * </li>
- * <li>
- * {@code regexp.empty}
- * </li>
- * <li>
- * {@code regexp.exceeded}
- * </li>
- * <li>
- * {@code regexp.minimum}
- * </li>
- * </ul>
  *
  * @since 5.0
  */
