522a6935adb73226afb04a740ec0308ba20edc03
Mattias Reichel <mattias.reichel@gmail.com>
2025-09-12 10:56:50 +0200

    Issue #17393: new check TypeBodyPadding

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/typebodypadding/Example3.java
@@ -0,0 +1,44 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="TypeBodyPadding">
+        <property name="starting" value="false"/>
+        <property name="ending" value="true"/>
+    </module>
+  </module>
+</module>
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.typebodypadding;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// xdoc section -- start
+public class Example3 {
+  int a = 0;
+
+  static class Inner {
+    int b = 0;
+  }
+
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.TYPE)
+  public @interface MyAnnotation {}
+
+  public void myMethod() {
+    class LocalClass {
+      int c = 0;
+    }
+    interface LocalInterface {
+      void foo();
+    }
+    enum LocalEnum { A, B }
+    record LocalRecord(int d) {}
+  }
+} // violation 'A blank line is required before the closing brace'
+// xdoc section -- end
