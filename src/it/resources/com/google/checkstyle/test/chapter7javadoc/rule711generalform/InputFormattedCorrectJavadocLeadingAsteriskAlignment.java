92bd194b53dc085f140d202abc9821841b58ed81
Mauryan Kansara <mauryankansaramk@gmail.com>
2025-09-20 09:32:48 +0530

    Issue #17778: Added JavadocLeadingAsteriskAlign Check in google_checks.xml for Rule: 7.1.1 General Form

---
+++ src/it/resources/com/google/checkstyle/test/chapter7javadoc/rule711generalform/InputFormattedCorrectJavadocLeadingAsteriskAlignment.java
@@ -0,0 +1,88 @@
+package com.google.checkstyle.test.chapter7javadoc.rule711generalform;
+
+/**
+ * This file contains violations, still it is named "Correct" because it doesn't contain any
+ * violations regarding Javadoc's Leading Asterisk Alignments. Other violations are kept to ensure
+ * that we cover all the edge cases.
+ */
+public class InputFormattedCorrectJavadocLeadingAsteriskAlignment {
+
+  /** javadoc for instance variable. */
+  private int int1;
+
+  /** */
+  // violation above "Summary javadoc is missing."
+  private int int2;
+
+  /***/
+  // violation above "Summary javadoc is missing."
+  private String str;
+
+  /** */
+  // violation above "Summary javadoc is missing."
+  private String str1;
+
+  /** No leading asterisk present. False negative until #17778, Subproblem 2. */
+  public InputFormattedCorrectJavadocLeadingAsteriskAlignment() {}
+
+  // violation 2 lines below "Summary javadoc is missing."
+  // violation 2 lines below "Javadoc tag '@param' should be preceded with an empty line."
+  /***
+   * @param a testing....
+   */
+  public InputFormattedCorrectJavadocLeadingAsteriskAlignment(int a) {}
+
+  /*************************************************
+   *** @param str testing.....
+   **********************************/
+  // False negative for above javadoc, until #17778, Subproblem 1 & 3.
+  public InputFormattedCorrectJavadocLeadingAsteriskAlignment(String str) {}
+
+  /** * */
+  // violation above "Summary javadoc is missing."
+  private String str2;
+
+  /****/
+  // violation above "Summary javadoc is missing."
+  private String str3;
+
+  /** This method does nothing. */
+  private void foo() {}
+
+  /**
+   * Opening tag should be alone on line. // False negative until #17778, Subproblem 1. This method
+   * does nothing.
+   */
+  private void foo2() {}
+
+  /** Javadoc for foo3. Closing tag should be alone on line. */
+  // False negative until #17778, Subproblem 3.
+  private void foo3() {
+    // foo2 code goes here
+  }
+
+  /** Javadoc for enum. */
+  // False negative until #17778, Subproblem 3.
+  private enum CorrectJavadocEnum {
+    // violation 2 lines below "Summary javadoc is missing."
+    // False negative until #17778, Subproblem 2.
+    /** */
+    ONE,
+
+    // False negative until #17778, Subproblem 2.
+    /** Not allowed. No leading asterisk present. */
+    TWO,
+
+    // False negative until #17778, Subproblem 1 & 2.
+    /** Not allowed. Opening javadoc tag should be alone on line. */
+    THREE,
+
+    // False negative until #17778, Subproblem 3.
+    /** Not allowed. Closing javadoc tag should be alone on line. */
+    FOUR,
+
+    // violation below "Summary javadoc is missing."
+    /** */
+    FIVE
+  }
+}
