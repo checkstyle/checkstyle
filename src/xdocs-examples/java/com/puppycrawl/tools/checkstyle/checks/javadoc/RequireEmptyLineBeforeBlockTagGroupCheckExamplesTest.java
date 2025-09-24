26aa90be826ffaf2366c13925bb2d8f21ee9da8a
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2025-09-24 14:24:19 +0300

    Enable example tests and remove JavadocParseTreeTest (#86)

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/javadoc/RequireEmptyLineBeforeBlockTagGroupCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/javadoc/RequireEmptyLineBeforeBlockTagGroupCheckExamplesTest.java
@@ -19,12 +19,10 @@
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
 
-@Disabled
 public class RequireEmptyLineBeforeBlockTagGroupCheckExamplesTest
         extends AbstractExamplesModuleTestSupport {
     @Override
