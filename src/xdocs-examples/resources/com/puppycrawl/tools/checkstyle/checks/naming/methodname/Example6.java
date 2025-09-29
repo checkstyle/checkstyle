850ef3370566b98ce1638cf645bad689d179c1f6
Javawired <hariomkr9128@gmail.com>
2025-09-30 00:05:17 +0530

    Issue #17449: Removed comments

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/methodname/Example6.java
@@ -0,0 +1,21 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MethodName">
+       <property name="format" value="^[a-z](_?[a-zA-Z0-9]+)*$"/>
+       <property name="applyToPrivate" value="false"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.naming.methodname;
+
+// xdoc section -- start
+class Example6 {
+  public void Method1() {} // violation 'Name 'Method1' must match pattern'
+  protected void Method2() {} // violation 'Name 'Method2' must match pattern'
+  private void Method3() {}
+  void Method4() {} // violation 'Name 'Method4' must match pattern'
+}
+// xdoc section -- end
---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/methodname/Example7.java
@@ -0,0 +1,21 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MethodName">
+       <property name="format" value="^[a-z](_?[a-zA-Z0-9]+)*$"/>
+       <property name="applyToPackage" value="false"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.naming.methodname;
+
+// xdoc section -- start
+class Example7 {
+  public void Method1() {} // violation 'Name 'Method1' must match pattern'
+  protected void Method2() {} // violation 'Name 'Method2' must match pattern'
+  private void Method3() {} // violation 'Name 'Method3' must match pattern'
+  void Method4() {}
+}
+// xdoc section -- end
