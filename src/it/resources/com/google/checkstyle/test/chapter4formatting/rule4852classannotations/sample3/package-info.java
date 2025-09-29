182d120064412a590dd502648c9b616e2e12ee27
Mohit Attry <atrrmohit@gmail.com>
2025-09-29 09:22:39 +0530

    minor: Rename ExampleX to SampleX in IT

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4852classannotations/sample3/package-info.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4852classannotations/sample3/package-info.java
@@ -4,7 +4,7 @@
 */
 
 @ParentPackage("person") // violation below 'Annotation 'Name' should be alone on line'
-@Name("/") package com.google.checkstyle.test.chapter4formatting.rule4852classannotations.example3;
+@Name("/") package com.google.checkstyle.test.chapter4formatting.rule4852classannotations.sample3;
 
 import com.google.checkstyle.test.chapter4formatting.rule4852classannotations.Name;
 import com.google.checkstyle.test.chapter4formatting.rule4852classannotations.ParentPackage;
--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4852classannotations/sample5/package-info.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule4852classannotations/sample5/package-info.java
@@ -5,7 +5,7 @@
 @ParentPackage("val")
 @Name("1")
 @SizePackage("2")
-package com.google.checkstyle.test.chapter4formatting.rule4852classannotations.example5;
+package com.google.checkstyle.test.chapter4formatting.rule4852classannotations.sample5;
 
 import com.google.checkstyle.test.chapter4formatting.rule4852classannotations.Name;
 import com.google.checkstyle.test.chapter4formatting.rule4852classannotations.ParentPackage;
