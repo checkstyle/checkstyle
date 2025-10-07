0a247afb4309caeb603011eab4280c4d016b8bcd
Hariom <hariomkr9128@gmail.com>
2025-10-07 12:50:37 +0530

    Issue #16360: Update testFileStarted to use verifyWithInlineConfigParserAndXmlLogger

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/xmllogger/InputXMLLoggerFileStarted.java
@@ -0,0 +1,14 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="com.puppycrawl.tools.checkstyle.checks.naming.MemberNameCheck">
+      <property name="format" value="^[a-z][a-zA-Z0-9]*$"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.xmllogger;
+
+public class InputXMLLoggerFileStarted {
+    private int validName;
+}
