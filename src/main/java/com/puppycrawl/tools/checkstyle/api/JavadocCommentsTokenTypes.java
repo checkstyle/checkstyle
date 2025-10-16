5c07d82acdc2b8a4c2a37f5eb72b7dba0b9d8ea5
daksh <dakshjain737@gmail.com>
2025-10-16 17:18:49 +0530

    Issue #17882 Update LINK_INLINE_TAG to new format of AST print

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocCommentsTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocCommentsTokenTypes.java
@@ -427,12 +427,35 @@ public final class JavadocCommentsTokenTypes {
     public static final int CODE_INLINE_TAG = JavadocCommentsLexer.CODE_INLINE_TAG;
 
     /**
-     * {@code {@link}} inline tag.
+     * {@code {@link}} Javadoc inline tag.
+     *
+     * <p>Such Javadoc tag can have two children:</p>
+     * <ol>
+     *   <li>{@link #REFERENCE}</li>
+     *   <li>{@link #IDENTIFIER}</li>
+     * </ol>
+     * <p><b>Example:</b></p>
+     * <pre>{@code * {@link String}}</pre>
+     * <b>Tree:</b>
+     * <pre>{@code
+     * JAVADOC_INLINE_TAG -> JAVADOC_INLINE_TAG
+     * `--LINK_INLINE_TAG -> LINK_INLINE_TAG
+     *     |--JAVADOC_INLINE_TAG_START -> {@
+     *     |--TAG_NAME -> link
+     *     |--TEXT ->
+     *     |--REFERENCE -> REFERENCE
+     *     |   `--IDENTIFIER -> String
+     *     `--JAVADOC_INLINE_TAG_END -> }
+     * }</pre>
+     *
+     * @see #JAVADOC_BLOCK_TAG
      */
     public static final int LINK_INLINE_TAG = JavadocCommentsLexer.LINK_INLINE_TAG;
 
     /**
      * {@code {@linkplain}} inline tag.
+     *
+     *
      */
     public static final int LINKPLAIN_INLINE_TAG = JavadocCommentsLexer.LINKPLAIN_INLINE_TAG;
 
