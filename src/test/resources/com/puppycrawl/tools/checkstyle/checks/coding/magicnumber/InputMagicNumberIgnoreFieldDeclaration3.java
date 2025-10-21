8ec5034114cb471ed004efe882e26e57c5891dc7
ivanmalutin <ivmaly1@yandex.ru>
2025-10-21 20:17:25 +0300

    Issue #15456: Define violation messages for MagicNumberCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumberIgnoreFieldDeclaration3.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/InputMagicNumberIgnoreFieldDeclaration3.java
@@ -19,44 +19,44 @@ package com.puppycrawl.tools.checkstyle.checks.coding.magicnumber;
 class InputMagicNumberIgnoreFieldDeclaration3 {
 
     public int hashCode() {
-        return 31; // violation
+        return 31; // violation ''31' is a magic number'
     }
 
 
     public int hashCode(int val) {
-        return 42; // violation
+        return 42; // violation ''42' is a magic number'
     }
 
 
     public int hashcode() {
-        return 13; // violation
+        return 13; // violation ''13' is a magic number'
     }
 
     static {
-        int x=21; // violation
+        int x=21; // violation ''21' is a magic number'
     }
 
     {
-        int y=37; // violation
+        int y=37; // violation ''37' is a magic number'
     }
 
     public InputMagicNumberIgnoreFieldDeclaration3() {
-        int z=101; // violation
+        int z=101; // violation ''101' is a magic number'
     }
 
-    @InputMagicNumberIntMethodAnnotation(42) // violation
+    @InputMagicNumberIntMethodAnnotation(42) // violation ''42' is a magic number'
     public void another() {
     }
 
-    @InputMagicNumberIntMethodAnnotation(value=43) // violation
+    @InputMagicNumberIntMethodAnnotation(value=43) // violation ''43' is a magic number'
     public void another2() {
     }
 
-    @InputMagicNumberIntMethodAnnotation(-44) // violation
+    @InputMagicNumberIntMethodAnnotation(-44) // violation ''-44' is a magic number'
     public void anotherNegative() {
     }
 
-    @InputMagicNumberIntMethodAnnotation(value=-45) // violation
+    @InputMagicNumberIntMethodAnnotation(value=-45) // violation ''-45' is a magic number'
     public void anotherNegative2() {
     }
 }
@@ -85,7 +85,7 @@ class AIgnoreFieldDeclaration3 {
     {
         switch (Blah2IgnoreFieldDeclaration1.LOW) {
         default:
-            int b = 122; // violation
+            int b = 122; // violation ''122' is a magic number'
         }
     }
 }
