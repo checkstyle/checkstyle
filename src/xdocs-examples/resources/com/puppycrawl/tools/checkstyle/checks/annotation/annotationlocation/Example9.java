05b987249c3d21922e4b230ddfebd8cca5769d3a
Hariom <hariomkr9128@gmail.com>
2025-10-22 12:32:47 +0530

    Issue #3730: Add type annotation tokens to AnnotationLocation check

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationlocation/Example9.java
@@ -0,0 +1,27 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="AnnotationLocation">
+      <property name="tokens" value="IMPLEMENTS_CLAUSE"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.annotationlocation;
+
+import java.io.Serializable;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+@interface ImplementsAnnotation1 {}
+
+@Target(ElementType.TYPE_USE)
+@interface ImplementsAnnotation2 {}
+
+// xdoc section -- start
+class Example9 implements @ImplementsAnnotation1 Serializable { } // ok
+class Example9a implements @ImplementsAnnotation1 @ImplementsAnnotation2 Serializable { }
+// violation above, 'Annotation 'ImplementsAnnotation2' should be alone on line.'
+// xdoc section -- end
