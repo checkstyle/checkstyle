4ce348b052fabdc6d861ee1186c7020e53929d52
daksh <dakshjain737@gmail.com>
2025-10-30 21:41:42 +0530

    Issue #16360: Migrate XMLLoggerTest.testNoCloseStream to use inlineConfigParser

--- src/test/java/com/puppycrawl/tools/checkstyle/XMLLoggerTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/XMLLoggerTest.java
@@ -140,6 +140,12 @@ public class XMLLoggerTest extends AbstractXmlTestSupport {
         verifyXml(getPath("ExpectedXMLLoggerEmpty.xml"), outStream);
     }
 
+    /**
+     * Cannot use verifyWithInlineConfigParserAndXmlLogger because it requires
+     * a custom stream to count close() calls.
+     *
+     * @throws Exception throws exception
+     */
     @Test
     public void testNoCloseStream()
             throws Exception {
