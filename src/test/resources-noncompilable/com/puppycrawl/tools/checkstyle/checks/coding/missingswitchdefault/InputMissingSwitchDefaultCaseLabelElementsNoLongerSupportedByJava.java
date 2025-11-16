5ef47fa95dba40f5d0a771a480174463a49e0351
stoyanK7 <stoyank127@gmail.com>
2025-11-16 18:03:53 +0100

    Issue #18097: Split and update unsupported Java syntax test files

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/missingswitchdefault/InputMissingSwitchDefaultCaseLabelElementsNoLongerSupportedByJava.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/missingswitchdefault/InputMissingSwitchDefaultCaseLabelElementsNoLongerSupportedByJava.java
@@ -4,32 +4,21 @@ MissingSwitchDefault
 
 */
 
-// non-compiled with javac: until https://github.com/checkstyle/checkstyle/issues/18097
+// non-compiled with javac: Compilable with Java17
+// until https://github.com/checkstyle/checkstyle/issues/18104
 package com.puppycrawl.tools.checkstyle.checks.coding.missingswitchdefault;
 
-public class InputMissingSwitchDefaultCaseLabelElements {
-    static void m(Object o) {
+class InputMissingSwitchDefaultCaseLabelElementsNoLongerSupportedByJava {
+    void m(Object o) {
         switch (o) {
             case null, String s -> System.out.println("String, including null");
             default -> System.out.println("something else");
         }
-
         switch (o) {
             case null, String s: System.out.println("String, including null"); break;
             default: System.out.println("something else");
         }
-
         switch(o) {
-            case null: default:
-                System.out.println("The rest (including null)");
-        }
-
-        switch(o) { // ok, pattern totality enforced by compiler
-            case Object o1:
-                System.out.println("object");
-        }
-
-        switch(o) { // ok, pattern totality enforced by compiler
             case Object o1 && o1.toString().length() > 2:
                 System.out.println("object");
                 break;
@@ -39,51 +28,27 @@ public class InputMissingSwitchDefaultCaseLabelElements {
             case null, Object o1:
                 break;
         }
-
         switch(o) {
             case String str, null ->
                 System.out.println("null");
             default -> System.out.println("default");
         }
-
-        switch(o) {
-            case null, default ->
-                System.out.println("The rest (including null)");
-        }
-
         switch(o) {
             case String s && s.length() > 2 ->
                 System.out.println("The string longer than 2 chars");
             default -> System.out.println("default!");
         }
-
-        switch(o) {
-            case default, null ->
-                System.out.println("The rest (including null)");
-        }
-
         switch(o) {
             case default, null:
                 System.out.println("The rest (including null)");
         }
-
-        switch(o) {
-            case null, default:
-                System.out.println("The rest (including null)");
-        }
-
-        switch(o) {
-            case null, default:
-                throw new UnsupportedOperationException("not supported!");
-        }
     }
-
-    void m2(String s) {
-        switch (s) {
+    void m1(String s) {
+        switch(s) {
             case "a": throw new AssertionError("Wrong branch.");
             case default: break;
         }
-        switch (s) {
+        switch(s) {
             case "a" -> throw new AssertionError("Wrong branch.");
             case default -> {}
         }
--- src/test/resources/com/puppycrawl/tools/checkstyle/grammar/antlr4/InputAntlr4AstRegressionPatternsInWhile.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/grammar/antlr4/InputAntlr4AstRegressionPatternsInWhile.java
@@ -1,11 +1,7 @@
-// non-compiled with javac: until https://github.com/checkstyle/checkstyle/issues/18097
 package com.puppycrawl.tools.checkstyle.grammar.antlr4;
 
 public class InputAntlr4AstRegressionPatternsInWhile {
     void m1(Object o) {
-        while (o instanceof (String s && s.length() > 6)) { // parenthesized pattern, `PATTERN_DEF`
-
-        }
         while (o instanceof String s && s.length() > 4) { // type pattern, no `PATTERN_DEF`
 
         }
@@ -13,9 +9,6 @@ public class InputAntlr4AstRegressionPatternsInWhile {
 
         }
 
-        do {
-            // parenthesized pattern, `PATTERN_DEF`
-        } while (o instanceof (String s && s.length() > 6));
         do {
             // type pattern, no `PATTERN_DEF`
         } while (o instanceof String s && s.length() > 4);
