91e850e74d9b9ae99639daa925ef5a5df1943df0
vivek-0509 <viveksolanki0509@gmail.com>
2025-11-20 14:42:29 +0530

    Issue #15456: Define violation messages for LineLengthCheck

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/linelength/InputLineLengthLongPackageStatement.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/linelength/InputLineLengthLongPackageStatement.java
@@ -14,6 +14,6 @@ package com.nameofcompany.nameofdivision.nameofproject.systemtests.
 public class InputLineLengthLongPackageStatement {
     @Override
     public String toString() {
-        return "This is very long line that should be logged because it is not package";// violation
+        return "This is very long line that should be logged because it is not package";// violation, 'Line is longer than 80 characters (found 150).'
     }
 }
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/linelength/InputLineLengthUnicodeChars.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/linelength/InputLineLengthUnicodeChars.java
@@ -12,6 +12,6 @@ package com.puppycrawl.tools.checkstyle.checks.sizes.linelength;
 public class InputLineLengthUnicodeChars {
     String aaaaaaa = "1234567890123456789012345678901234567890" + "1234567890123456789012345" + "_";
     String aaaaaab = "1234567890123456789012345678901234567890" + "1234567890123456789012345" + "💩";
-    String aaaaaac = "This line is too long, and will be reported by checkstyle.  This line is 137 characters long, excluding unicode."; // violation
-    String aaaaaad = "This line is too long, and will be reported by checkstyle.💩💩This line is 137 characters long, including unicode."; // violation
+    String aaaaaac = "This line is too long, and will be reported by checkstyle.  This line is 137 characters long, excluding unicode."; // violation, 'Line is longer than 100 characters (found 200).'
+    String aaaaaad = "This line is too long, and will be reported by checkstyle.💩💩This line is 137 characters long, including unicode."; // violation, 'Line is longer than 100 characters (found 200).'
 }
