92572a11abcadc3321683068c85c046d16f55b0a
ivanmalutin <ivmaly1@yandex.ru>
2025-11-21 18:12:34 +0300

    Issue #18088: Add check name alongside id

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/xmllogger/InputXMLLoggerErrorEmptyModuleId.java
@@ -0,0 +1,12 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.naming.MethodNameCheck
+id =
+
+*/
+
+package com.puppycrawl.tools.checkstyle.xmllogger;
+
+public class InputXMLLoggerErrorEmptyModuleId {
+
+    public void Method() {}
+}
