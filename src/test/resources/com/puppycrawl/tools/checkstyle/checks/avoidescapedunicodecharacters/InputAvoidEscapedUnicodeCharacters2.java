1bc20d748e4988b07396d735d9f448e43ead95d1
vivek-0509 <viveksolanki0509@gmail.com>
2025-11-23 02:13:49 +0530

    Issue #15456: Define violation messages for AvoidEscapedUnicodeCharactersCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/avoidescapedunicodecharacters/InputAvoidEscapedUnicodeCharacters2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/avoidescapedunicodecharacters/InputAvoidEscapedUnicodeCharacters2.java
@@ -13,16 +13,18 @@ package com.puppycrawl.tools.checkstyle.checks.avoidescapedunicodecharacters;
 import java.util.concurrent.TimeUnit;
 
 public class InputAvoidEscapedUnicodeCharacters2 {
-         // violation below
+
         private String unitAbbrev2 = "\u03bcs";
+                // violation above, 'Unicode escape(s) usage should be avoided.'
 
         private String unitAbbrev3 = "\u03bcs"; // Greek letter mu
 
         private String unitAbbrev4 = "\u03bcs"; // Greek letter mu
 
         public Object fooString() {
-                String unitAbbrev = "Î¼s"; // violation below
+                String unitAbbrev = "Î¼s";
                 String unitAbbrev2 = "\u03bcs";
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                 String unitAbbrev3 = "\u03bcs"; // Greek letter mu, "s"
                 String fakeUnicode = "asd\tsasd";
                 String fakeUnicode2 = "\\u23\\u123i\\u";
@@ -30,19 +32,23 @@ public class InputAvoidEscapedUnicodeCharacters2 {
                 return "\ufeff" + content; // byte order mark
         }
 
-        public Object fooChar() { // violation below
+        public Object fooChar() {
                 char unitAbbrev2 = '\u03bc';
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                 char unitAbbrev3 = '\u03bc'; // Greek letter mu, "s"
                 char content = 0;
                 return '\ufeff' + content; // byte order mark
         }
 
-        public void multiplyString() { // violation below
+        public void multiplyString() {
                 String unitAbbrev2 = "asd\u03bcsasd";
-                String unitAbbrev3 = "aBc\u03bcssdf\u03bc"; /* mu, "s" */ // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
+                String unitAbbrev3 = "aBc\u03bcssdf\u03bc"; /* mu, "s" */
                 String unitAbbrev4 = "\u03bcaBc\u03bcssdf\u03bc";
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                 String allCharactersEscaped = "\u03bc\u03bc";
-        } // violation above
+                // violation above, 'Unicode escape(s) usage should be avoided.'
+        }
 
         private static String abbreviate(TimeUnit unit) {
                 switch (unit) {
@@ -65,14 +71,15 @@ public class InputAvoidEscapedUnicodeCharacters2 {
                 }
         }
 
-                static final String WHITESPACE_TABLE = "" // violation below
+                static final String WHITESPACE_TABLE = ""
                                 + "\u2002\u3000\r\u0085\u200A\u2005\u2000\u3000\\"
-                         // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                                 + "\u2029\u000B\u3000\u2008\u2003\u205F\u3000\u1680"
-                         // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                                 + "\u0009\u0020\u2006\u2001\u202F\u00A0\u000C\u2009"
-                         // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                                 + "\u3000\u2004\u3000\u3000\u2028\n\u2007\u3000";
+                // violation above, 'Unicode escape(s) usage should be avoided.'
 
               public boolean matches(char c) {
                 switch (c) {
@@ -82,61 +89,68 @@ public class InputAvoidEscapedUnicodeCharacters2 {
                   case '\f':
                   case '\r':
                   case ' ':
-                  case '\u0085': // some comment // violation below
+                  case '\u0085': // some comment
                   case '\u1680':
-                       // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                   case '\u2028':
-                       // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                   case '\u2029':
-                       // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                   case '\u205f':
-                       // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                   case '\u3000':
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                     return true;
-                     // violation below
                   case '\u2007':
+                // violation above, 'Unicode escape(s) usage should be avoided.'
                     return false;
                   default:
-                       // 2 violations below
                   return c >= '\u2000' && c <= '\u200a';
+                // 2 violations above:
+                //                 'Unicode escape(s) usage should be avoided.'
+                //                 'Unicode escape(s) usage should be avoided.'
               }
          }
 
-        private String unitAbbrev5 = "\u03bcs";         // comment is separated by space + tab
-        private String unitAbbrev6 = "\u03bcs";        // comment is separated by tab
-        private String unitAbbrev7 = "\u03bcs";        /* comment is separated by tab */
-        private String unitAbbrev8 = "\u03bcs"; /* comment
+        private String unitAbbrev5 = "\u03bcs";  // comment is separated by space + tab
+        private String unitAbbrev6 = "\u03bcs";  // comment is separated by tab
+        private String unitAbbrev7 = "\u03bcs";  /* comment is separated by tab */
+        private String unitAbbrev8 = "\u03bcs";  /* comment
                                                    has 2 lines */
         void foo() {
                 for (char c = '\u0000'; c < '\uffff'; c++) {
-                        if (c == '\u001b' ||     // 2 violations above
+                        if (c == '\u001b' ||// 2 violations above:
+                                   //                 'Unicode escape(s) usage should be avoided.'
+                                   //                 'Unicode escape(s) usage should be avoided.'
+
                                         c == '\u2014')   // Em-Dash?
                                 continue;
                 }
-        } // violation below
+        }
         private String unitAbbrev9 = "\u03bcs"; /* comment */ int i;
-
+                // violation above, 'Unicode escape(s) usage should be avoided.'
         private String notAUnicodeEscaped1 = "\\u1234";
 
         private String notAUnicodeEscaped2 = "\\\\u1234";
-        // violation below
+
         private String onlyEscaped = "\\\u1234";
-         // violation below
+                // violation above, 'Unicode escape(s) usage should be avoided.'
         private String sumilarToEscapedByB = "b\u1234";
+                // violation above, 'Unicode escape(s) usage should be avoided.'
         private String sumilarToEscapedCommentedByB = "b\u1234"; // comment
-         // violation below
+                // violation below, 'Unicode escape(s) usage should be avoided.'
         private String sumilarToEscapedByF = "f\u1234";
         private String sumilarToEscapedCommentedByF = "f\u1234"; // comment
-         // violation below
+                // violation below, 'Unicode escape(s) usage should be avoided.'
         private String sumilarToEscapedByR = "r\u1234";
         private String sumilarToEscapedCommentedByR = "r\u1234"; // comment
-         // violation below
+                // violation below, 'Unicode escape(s) usage should be avoided.'
         private String sumilarToEscapedByN = "n\u1234";
         private String sumilarToEscapedCommentedByN = "n\u1234"; // comment
-         // violation below
+                // violation below, 'Unicode escape(s) usage should be avoided.'
         private String sumilarToEscapedByT = "t\u1234";
         private String sumilarToEscapedCommentedByT = "t\u1234"; // comment
-        // violation below
+                // violation below, 'Unicode escape(s) usage should be avoided.'
         private String validEscapeWithManyUs = "t\uuuuuuuuu1234";
         private String validEscapeWithManyUsCommented = "t\uuuuuuuuu1234"; // comment
 }
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/avoidescapedunicodecharacters/InputAvoidEscapedUnicodeCharacters5.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/avoidescapedunicodecharacters/InputAvoidEscapedUnicodeCharacters5.java
@@ -11,14 +11,15 @@ allowNonPrintableEscapes = (default)false
 package com.puppycrawl.tools.checkstyle.checks.avoidescapedunicodecharacters;
 
 public class InputAvoidEscapedUnicodeCharacters5 {
-    private String a = "🎄"; // violation below
+    private String a = "🎄";// violation below, 'Unicode escape(s) usage should be avoided.'
     private String b = "\uD83E\uDD73abs😀";
     private String c = "\uD83C\uDF84😀\uD83C\uDF84"; // ok, allowed by trailing comment
-    // violation below
+    // violation below, 'Unicode escape(s) usage should be avoided.'
     private String d = "\uD83C\uDF84😀\uD83C\uDF84asdas\uD83C\uDF84abcd";
 
     public Object fooEmoji() {
-        String unitAbbrev = "Î¼sð😀"; // violation below
+        String unitAbbrev = "Î¼sð😀";
+        // violation below, 'Unicode escape(s) usage should be avoided.'
         String unitAbbrev2 = "\u03bc😀";
         String unitAbbrev3 = "😀\u03bcs"; // Greek letter mu, "s"
         String fakeUnicode2 = "\\u23\\u123i\\u😀";
@@ -28,15 +29,15 @@ public class InputAvoidEscapedUnicodeCharacters5 {
 
     public boolean matches(String c) {
         switch (c) {
-            // violation below
+        // violation below, 'Unicode escape(s) usage should be avoided.'
             case "\u03bc🎄":
             case "🎄\u03bc": /* OK, allowed by trailing comment */
-                // violation below
+        // violation below, 'Unicode escape(s) usage should be avoided.'
             case "\t\u2028":
-                // violation below
+        // violation below, 'Unicode escape(s) usage should be avoided.'
             case "\n😂\u3000":
                 return true;
-                // violation below
+        // violation below, 'Unicode escape(s) usage should be avoided.'
             case "😂\uD83D\uDE02":
                 return false;
             default:
