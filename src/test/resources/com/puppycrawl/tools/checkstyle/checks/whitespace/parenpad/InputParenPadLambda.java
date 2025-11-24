af552fa59ccc8c1a2fd7fabd4ad73239f5579b51
vivek-0509 <viveksolanki0509@gmail.com>
2025-11-24 18:09:10 +0530

    Issue #15456: Define violation messages for ParenPadCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/parenpad/InputParenPadLambda.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/parenpad/InputParenPadLambda.java
@@ -17,19 +17,36 @@ class InputParenPadLambda {
 
         java.util.function.Consumer b = o -> { o.toString(); };
 
-        java.util.function.Consumer c = ( o ) -> { o.toString(); }; // 2 violations
+        java.util.function.Consumer c = ( o ) -> { o.toString(); };
+        // 2 violations above:
+        //           ''(' is followed by whitespace.'
+        //           '')' is preceded with whitespace.'
 
-        java.util.function.Consumer d = (o ) -> { o.toString(); }; // violation
+        java.util.function.Consumer d = (o ) -> { o.toString(); };
+        // violation above, '')' is preceded with whitespace.'
 
-        java.util.function.Consumer e = ( o) -> { o.toString(); }; // violation
+        java.util.function.Consumer e = ( o) -> { o.toString(); };
+        // violation above, ''(' is followed by whitespace.'
 
-        java.util.stream.Stream.of().forEach(( o ) -> o.toString()); // 2 violations
+        java.util.stream.Stream.of().forEach(( o ) -> o.toString());
+        // 2 violations above:
+        //           ''(' is followed by whitespace.'
+        //           '')' is preceded with whitespace.'
 
-        java.util.stream.Stream.of().forEach(( Object o ) -> o.toString()); // 2 violations
+        java.util.stream.Stream.of().forEach(( Object o ) -> o.toString());
+        // 2 violations above:
+        //           ''(' is followed by whitespace.'
+        //           '')' is preceded with whitespace.'
 
-        java.util.stream.Stream.of().forEach(o -> o.toString( )); // 2 violations
+        java.util.stream.Stream.of().forEach(o -> o.toString( ));
+        // 2 violations above:
+        //           ''(' is followed by whitespace.'
+        //           '')' is preceded with whitespace.'
     }
 
-    void someMethod( String param ) { // 2 violations
+    void someMethod( String param ) {
+        // 2 violations above:
+        //           ''(' is followed by whitespace.'
+        //           '')' is preceded with whitespace.'
     }
 }
