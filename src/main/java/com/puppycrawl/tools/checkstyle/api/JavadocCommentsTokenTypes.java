f27a1fe330334ec7fd2dd480e5361803fa65ba81
vishalup29 <vishalupadhyay977@gmail.com>
2025-11-26 13:57:23 -0600

    Issue #17882: Update TAG_OPEN of JavadocCommentsTokenTypes to new AST format

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocCommentsTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocCommentsTokenTypes.java
@@ -1096,24 +1096,29 @@ public final class JavadocCommentsTokenTypes {
     public static final int HTML_TAG_END = JavadocCommentsLexer.HTML_TAG_END;
 
     /**
-     * {@code <} Opening tag delimiter.
+     * {@code TAG_OPEN} represents the opening {@code "&lt;"} symbol of an HTML start tag.
      *
-     * <p>Part of an HTML start tag.</p>
+     * <p>Appears in Javadoc comments when documenting HTML elements.</p>
      *
      * <p><b>Example:</b></p>
-     * <pre>{@code * <html>}</pre>
+     * <pre>{@code
+     * public class Sample {
+     *     /**
+     *      * <div class="container" lang="en"></div>
+     *      *\/
+     *     void demo() { }
+     * }
+     * }</pre>
      *
      * <b>Tree:</b>
      * <pre>{@code
      * HTML_TAG_START -> HTML_TAG_START
      * |--TAG_OPEN -> <
-     * |--TAG_NAME -> html
-     * `--HTML_ATTRIBUTES -> HTML_ATTRIBUTES
+     * ...
      * }</pre>
-     *
-     * @see #HTML_ELEMENT
      */
     public static final int TAG_OPEN = JavadocCommentsLexer.TAG_OPEN;
+
 
     /**
      * HTML tag name.
