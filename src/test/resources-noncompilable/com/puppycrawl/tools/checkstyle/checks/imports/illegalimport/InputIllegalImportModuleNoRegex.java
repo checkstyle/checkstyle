d0b346cf535799574e4183302a650bd0709ea4ba
stoyanK7 <stoyank127@gmail.com>
2025-11-30 18:31:07 +0100

    Issue #18207: IllegalImport: new property illegalModules to cover module imports

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/illegalimport/InputIllegalImportModuleNoRegex.java
@@ -0,0 +1,25 @@
+/*
+IllegalImport
+illegalPkgs = (default)sun
+illegalClasses = java.sql.Connection
+illegalModules = java.base,java.logging
+regexp = (default)false
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+package com.puppycrawl.tools.checkstyle.checks.imports.illegalimport;
+
+import module java.base; // violation 'Illegal import - java.base'
+import module java.xml;
+import module java.sql;
+import module java.logging; // violation 'Illegal import - java.logging'
+import module java.se;
+
+import java.sql.Connection; // violation 'Illegal import - java.sql.Connection'
+import java.sql.Driver;
+import java.util.List;
+import java.lang.*;
+
+class InputIllegalImportModuleNoRegex {}
