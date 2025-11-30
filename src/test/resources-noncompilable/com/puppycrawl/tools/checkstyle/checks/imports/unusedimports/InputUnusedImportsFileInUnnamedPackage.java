3bedae4435ad520f5d45de05858c9eaf30c20d9a
gianmarco <gianmarco.schifone02@gmail.com>
2025-11-30 02:08:33 +0100

    Issue #11166: Remove getFileContents() usage from UnusedImportsCheck

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsFileInUnnamedPackage.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsFileInUnnamedPackage.java
@@ -1,10 +1,10 @@
 /*
 UnusedImports
 processJavadoc = (default)true
-
+violateExecutionOnNonTightHtml = (default)false
+javadocTokens = (default)REFERENCE, PARAMETER_TYPE, THROWS_BLOCK_TAG
 
 */
-
 // non-compiled with javac: no package by design of test
 
 import java.util.List;
