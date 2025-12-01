c8961cd51c768b7f017096efa831b45b01c03c8e
Pratyksh Gupta <pratykshgupta9999@gmail.com>
2025-12-01 14:18:49 +0530

    Issue #18024: Resolve Pitest Suppressions - common

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocStyleCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocStyleCheckTest.java
@@ -834,4 +834,18 @@ public class JavadocStyleCheckTest
                 getPath("InputJavadocStyleAboveComments.java"),
                 expected);
     }
+
+    @Test
+    public void testJavadocStyleUnclosedTags() throws Exception {
+        // Test covers mutations in JavadocCommentsLexerUtil through realistic Javadoc inputs
+        final String[] expected = {
+            "25:11: " + getCheckMessage(MSG_UNCLOSED_HTML, "<span>"),
+            "33:11: " + getCheckMessage(MSG_UNCLOSED_HTML, "<code>"),
+            "33:23: " + getCheckMessage(MSG_EXTRA_HTML, "</pre>"),
+        };
+
+        verifyWithInlineConfigParser(
+                getPath("InputJavadocStyleUnclosedTags.java"),
+                expected);
+    }
 }
