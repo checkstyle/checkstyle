5512682bed7f764230a8431d916d22147333c754
Pratyksh Gupta <pratykshgupta9999@gmail.com>
2025-12-02 00:32:27 +0530

    Issue #18218: Add since macro to extract version from @since javadoc tag

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/site/SinceMacro.java
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2025 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.site;
+
+import java.nio.file.Path;
+
+import org.apache.maven.doxia.macro.AbstractMacro;
+import org.apache.maven.doxia.macro.Macro;
+import org.apache.maven.doxia.macro.MacroExecutionException;
+import org.apache.maven.doxia.macro.MacroRequest;
+import org.apache.maven.doxia.sink.Sink;
+import org.codehaus.plexus.component.annotations.Component;
+
+import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+
+/**
+ * A macro that inserts the since version of module from its Javadoc.
+ */
+@Component(role = Macro.class, hint = "since")
+public class SinceMacro extends AbstractMacro {
+
+    @Override
+    public void execute(Sink sink, MacroRequest request) throws MacroExecutionException {
+        final Path modulePath = Path.of((String) request.getParameter("modulePath"));
+        final String moduleName = CommonUtil.getFileNameWithoutExtension(modulePath.toString());
+
+        final String sinceVersion = SiteUtil.getModuleSinceVersion(moduleName, modulePath);
+
+        sink.paragraph();
+        sink.text("Since Checkstyle " + sinceVersion);
+        sink.paragraph_();
+
+    }
+
+}
