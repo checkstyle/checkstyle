5d59abf9966f28b69e15780c9c17919ec32f6c9d
Saad <mohd.saadhaider@gmail.com>
2025-12-03 22:54:45 +0530

    Issue #18064: added ArrayBracketNoWhitespace check

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/whitespace/arraybracketnowhitespace/InputXpathArrayBracketNoWhitespaceFollowed.java
@@ -0,0 +1,5 @@
+package org.checkstyle.suppressionxpathfilter.whitespace.arraybracketnowhitespace;
+
+public class InputXpathArrayBracketNoWhitespaceFollowed {
+    int[] offsets = new int[ 5][10]; // violation: whitespace after '['
+}
