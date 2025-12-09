08d325197513cb10c849bccaebabb9fa634f1572
vishalup29 <vishalupadhyay977@gmail.com>
2025-12-09 01:28:16 -0600

    Issue #17449: Add XDocs example for MethodCountCheck max* properties

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
@@ -67,7 +67,6 @@ public class XdocsExampleFileTest {
                     "arrayInitIndent",
                     "braceAdjustment"
             )),
-            Map.entry("MethodCountCheck", Set.of("maxPrivate", "maxPackage", "maxProtected")),
             Map.entry("ClassMemberImpliedModifierCheck", Set.of(
                     "violateImpliedStaticOnNestedEnum",
                     "violateImpliedStaticOnNestedRecord",
