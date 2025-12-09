ee43ec485b06243f78b18c9bb998edd4f802d575
vivek-0509 <viveksolanki0509@gmail.com>
2025-12-10 03:27:09 +0530

    Issue #17561: Add missing @Override detection for record accessor methods

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/missingoverride/InputMissingOverrideRecordAccessorJava5.java
@@ -0,0 +1,20 @@
+/*
+MissingOverride
+javaFiveCompatibility = true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.missingoverride;
+
+/** some javadoc. */
+public record InputMissingOverrideRecordAccessorJava5(String name) {
+    /**
+     * {@inheritDoc}
+     */
+    public String name() {
+    // violation above, 'Must include @java.lang.Override annotation when'
+    // '@inheritDoc' Javadoc tag exists.'
+        return name;
+    }
+}
---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/missingoverride/Example3.java
@@ -0,0 +1,23 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MissingOverride"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.annotation.missingoverride;
+
+// xdoc section -- start
+record Example3(String name, int age) {
+
+  @Override
+  public String name() { // ok
+    return name;
+  }
+
+  public int age() {
+    // violation above, 'record accessor and must include @java.lang.Override'
+    return age;
+  }
+}
+// xdoc section -- end
