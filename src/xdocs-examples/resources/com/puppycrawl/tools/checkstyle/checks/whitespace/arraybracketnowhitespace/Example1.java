b009c3327c8ae33cc15fae05cf01894ad8e1bc80
Saad <mohd.saadhaider@gmail.com>
2025-12-11 00:00:19 +0530

    Issue #18064: added ArrayBracketNoWhitespace check

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/arraybracketnowhitespace/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/arraybracketnowhitespace/Example1.java
@@ -10,19 +10,20 @@ package com.puppycrawl.tools.checkstyle.checks.whitespace.arraybracketnowhitespa
 
 // xdoc section -- start
 class Example1 {
-    int array1[] ; // violation, space before ;
-    int array2[ ] = new int[ 5 ]; // 3 violations, space in [] and before ;
-    int[] array3 = new int[5 ]; // violation, space before ]
-    int array4 [] = new int [ 5 ] ; // 5 violations, space after variable name, in all []
-    int array5
-      [ ]
-      =
-      new int
-      [ 1 ] ; // violations, space in []
-    void method1(int param[ ]) { // violation, space in []
-    }
-    void method2() {
-        int arr [  ] = { 1, 2, 3 }; // violations, spaces in []
-    }
+  int array1[] ; // violation, space before ;
+  int array2[ ] = new int[ 5 ]; // 3 violations, space in [] and before ;
+  int[] array3 = new int[5 ]; // violation, space before ]
+  int array4 [] = new int [ 5 ] ;
+  // 5 violations, space after variable name, in all []
+  int array5
+    [ ]
+    =
+    new int
+    [ 1 ] ; // violations, space in []
+  void method1(int param[ ]) { // violation, space in []
+  }
+  void method2() {
+    int arr [  ] = { 1, 2, 3 }; // violations, spaces in []
+  }
 }
 // xdoc section -- end
