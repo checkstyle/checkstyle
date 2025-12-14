c641c3f7b236b17277b785f38345dd3acc285671
atharv <atharv55215@gmail.com>
2025-12-15 01:28:06 +0530

    Issue #18109: Adding new check LineEnding

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/lineending/InputLineEndingCrlfExpected1.java
@@ -0,0 +1,15 @@
+/*
+LineEnding
+lineEnding = crlf
+fileExtensions = (default)""
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.lineending;
+
+public class InputLineEndingCrlfExpected1 {
+    public void method() {
+        int a = 1;
+    }
+}
