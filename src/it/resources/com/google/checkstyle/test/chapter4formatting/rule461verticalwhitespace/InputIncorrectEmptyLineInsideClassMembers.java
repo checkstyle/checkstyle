d4a479e368dbf82cd527a9120311df403c88e54a
Praveen Kumar <praveen729483@gmail.com>
2025-12-16 03:33:55 +0530

    Issue #18265: Added allowMultipleEmptyLinesInsideClassMembers property inside EmptyLineSeparator check in google_checks.xml

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule461verticalwhitespace/InputIncorrectEmptyLineInsideClassMembers.java
@@ -0,0 +1,99 @@
+package com.google.checkstyle.test.chapter4formatting.rule461verticalwhitespace;
+
+/**
+ * This file contains incorrect vertical whitespace inside class
+ * members and also between class members.
+ */
+public class InputIncorrectEmptyLineInsideClassMembers {
+
+
+  static int count; // violation ''VARIABLE_DEF' has more than 1 empty lines before.'
+
+
+  // violation below ''VARIABLE_DEF' has more than 1 empty lines before.'
+  String str;
+
+
+  // violation below ''VARIABLE_DEF' has more than 1 empty lines before.'
+  int value;
+
+
+  { // violation ''INSTANCE_INIT' has more than 1 empty lines before.'
+    str = "Hello"; // violation 'There is more than 1 empty line after this line.'
+
+
+  }
+
+
+  { // violation ''INSTANCE_INIT' has more than 1 empty lines before.'
+    value = 10; // violation 'There is more than 1 empty line after this line.'
+
+
+  }
+
+
+  InputIncorrectEmptyLineInsideClassMembers() {
+    // violation above ''CTOR_DEF' has more than 1 empty lines before.'
+    int a; // violation 'There is more than 1 empty line after this line.'
+
+
+    int b; // violation 'There is more than 1 empty line after this line.'
+
+
+  }
+
+
+  static { // violation ''STATIC_INIT' has more than 1 empty lines before.'
+    // violation above 'There is more than 1 empty line after this line.'
+
+
+    count = 5; // violation 'There is more than 1 empty line after this line.'
+
+
+  }
+
+
+  static { // violation ''STATIC_INIT' has more than 1 empty lines before.'
+    // violation above 'There is more than 1 empty line after this line.'
+
+
+    count++; // violation 'There is more than 1 empty line after this line.'
+
+
+  }
+
+  /** Method. */
+  public void foo() {
+
+    int x = 10;
+    // violation above 'There is more than 1 empty line after this line.'
+
+
+    // violation below 'There is more than 1 empty line after this line.'
+    int y = 20;
+
+
+  }
+
+  /** Another method. */
+  public void bar() {
+    // violation above 'There is more than 1 empty line after this line.'
+
+
+    // violation below 'There is more than 1 empty line after this line.'
+    int z = 30;
+
+
+  }
+
+  /** Main method. */
+  public static void main(String[] args) {
+    // violation above 'There is more than 1 empty line after this line.'
+
+
+    // violation below 'There is more than 1 empty line after this line.'
+    new InputIncorrectEmptyLineInsideClassMembers();
+
+
+  }
+}
