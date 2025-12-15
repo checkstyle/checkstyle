73a00888b0b4daafca7a1b478d6c415a82888bea
Praveen Kumar <praveen729483@gmail.com>
2025-12-16 03:33:55 +0530

    Issue #18265: Added allowMultipleEmptyLinesInsideClassMembers property inside EmptyLineSeparator check in google_checks.xml

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule461verticalwhitespace/InputCorrectEmptyLineInsideClassMembers.java
@@ -0,0 +1,38 @@
+package com.google.checkstyle.test.chapter4formatting.rule461verticalwhitespace;
+
+/**
+ * This file contains correct vertical whitespace inside class
+ * members.
+ */
+public class InputCorrectEmptyLineInsideClassMembers {
+
+  static int count;
+  String str;
+
+  {
+    str = "Hello";
+  }
+
+  /** Constructor. */
+  InputCorrectEmptyLineInsideClassMembers() {
+    int a;
+
+    int b;
+  }
+
+  static {
+    count = 5;
+  }
+
+  /** Method. */
+  public void foo() {
+    int x = 10;
+
+    int y = 20;
+  }
+
+  /** Main method. */
+  public static void main(String[] args) {
+    new InputCorrectEmptyLineInsideClassMembers();
+  }
+}
