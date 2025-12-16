8bb25c4d574c06cb03c3b5296b46c347b6d210e1
Saad <mohd.saadhaider@gmail.com>
2025-12-16 19:27:19 +0530

    Issue #18064: add ArrayBracketNoWhitespaceCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/arraybracketnowhitespace/Example2.java
@@ -0,0 +1,24 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="ArrayBracketNoWhitespace"/>
+  </module>
+</module>
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.arraybracketnowhitespace;
+
+// xdoc section -- start
+class Example2 {
+  int[] array1;
+  int[] array2 = new int[5];
+  int[] array3 = new int[]{1, 2, 3};
+  void method1(int[] param) {
+  }
+  void method2() {
+    int[] arr = { 1, 2, 3 };
+  }
+}
+// xdoc section -- end
