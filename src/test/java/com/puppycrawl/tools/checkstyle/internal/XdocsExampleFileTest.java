246e982e18b5854aae0e2d8c29156e15fcf70387
Aradhya Sharma <aradhyasharmaa96@gmail.com>
2025-12-17 04:47:37 +0530

    Issue #18317: update doc example to show method without inheritDoc

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
@@ -155,7 +155,7 @@ public class XdocsExampleFileTest {
         missing.removeAll(referencedExamples);
 
         assertWithMessage(
-                "Some xdocs examples are not referenced in *ExamplesTest.java:\n"
+                "Some xdocs examples are not referenced in any *ExamplesTest.java:\n"
                         + String.join("\n", missing))
                 .that(missing)
                 .isEmpty();
