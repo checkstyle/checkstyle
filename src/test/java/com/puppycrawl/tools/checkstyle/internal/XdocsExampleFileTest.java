744a8eab6c7a222b6ceb829ced5803f56b608e51
Aradhya Sharma <aradhyasharmaa96@gmail.com>
2025-12-17 17:05:31 +0530

    Issue #18303 Add regression test to ensure all xdoc example files are referenced in Example tests

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
@@ -156,7 +156,7 @@ public class XdocsExampleFileTest {
         missing.removeAll(referencedExamples);
 
         assertWithMessage(
-                "Some xdocs examples are not referenced in any *ExamplesTest.java:\n %s",
+                "Some xdocs examples are not referenced in *ExamplesTest.java:\n %s",
                         String.join("\n", missing))
                 .that(missing)
                 .isEmpty();
