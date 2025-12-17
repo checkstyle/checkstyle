efb6f0ac30e6692d632eae944f2152f18e2cde15
Aradhya Sharma <aradhyasharmaa96@gmail.com>
2025-12-17 18:12:12 +0530

    Issue #18303: XdocsExampleFileTest should catch fact that ExampleX.java is not referenced in respected Test class

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
@@ -28,7 +28,6 @@ import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
