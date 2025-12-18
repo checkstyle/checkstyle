6abba9204548c60c40ef4bf8a7eb2b9900ca8216
Roman Ivanov <ivanov-jr@mail.ru>
2025-12-18 05:52:03 -0800

    Pull #18322: auto refactoring by org.openrewrite.java.migrate.util.ListFirstAndLast

--- src/main/java/com/puppycrawl/tools/checkstyle/Main.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/Main.java
@@ -298,33 +298,34 @@ public final class Main {
         // create config helper object
         if (options.printAst) {
             // print AST
-            final File file = filesToProcess.get(0);
+            final File file = filesToProcess.getFirst();
             final String stringAst = AstTreeStringPrinter.printFileAst(file,
                     JavaParser.Options.WITHOUT_COMMENTS);
             System.out.print(stringAst);
         }
         else if (Objects.nonNull(options.xpath)) {
-            final String branch = XpathUtil.printXpathBranch(options.xpath, filesToProcess.get(0));
+            final String branch =
+                    XpathUtil.printXpathBranch(options.xpath, filesToProcess.getFirst());
             System.out.print(branch);
         }
         else if (options.printAstWithComments) {
-            final File file = filesToProcess.get(0);
+            final File file = filesToProcess.getFirst();
             final String stringAst = AstTreeStringPrinter.printFileAst(file,
                     JavaParser.Options.WITH_COMMENTS);
             System.out.print(stringAst);
         }
         else if (options.printJavadocTree) {
-            final File file = filesToProcess.get(0);
+            final File file = filesToProcess.getFirst();
             final String stringAst = DetailNodeTreeStringPrinter.printFileAst(file);
             System.out.print(stringAst);
         }
         else if (options.printTreeWithJavadoc) {
-            final File file = filesToProcess.get(0);
+            final File file = filesToProcess.getFirst();
             final String stringAst = AstTreeStringPrinter.printJavaAndJavadocTree(file);
             System.out.print(stringAst);
         }
         else if (hasSuppressionLineColumnNumber) {
-            final File file = filesToProcess.get(0);
+            final File file = filesToProcess.getFirst();
             final String stringSuppressions =
                     SuppressionsStringPrinter.printSuppressions(file,
                             options.suppressionLineColumnNumber, options.tabWidth);
