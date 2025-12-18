b20f154ff40053089adffecd1352d42698bacf3e
Aradhya Sharma <aradhyasharmaa96@gmail.com>
2025-12-18 14:30:45 +0530

    Issue #18317: update doc example to show method without inheritDoc

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/missingoverride/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/missingoverride/Example3.java
@@ -9,33 +9,37 @@
 package com.puppycrawl.tools.checkstyle.checks.annotation.missingoverride;
 
 class ParentClass3 {
-    public void test1() {}
-    public void test2() {}
-    public void test3() {}
-    public void test4() {}
+  public void test1() {}
+  public void test2() {}
+  public void test3() {}
+  public void test4() {}
 }
 
 // xdoc section -- start
 class Example3 extends ParentClass3 {
 
-    // No javadoc, no @Override needed
-    public void test1() {
-    }
+  // No javadoc, no @Override needed
+  public void test1() {
 
-    // Javadoc present but no {@inheritDoc}, no @Override needed
-    /**
-     * This is a sample javadoc.
-     */
-    public void test2() {
-    }
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public void test3() {
-    }
+  // Javadoc present but no {@inheritDoc}, no @Override needed
+  /**
+  * This is a sample javadoc.
+  */
+  public void test2() {
 
-    /** {@inheritDoc} */
-    public void test4() { // violation, '@inheritDoc' tag requires @Override annotation
-    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void test3() {
+
+  }
+
+  /** {@inheritDoc} */
+  public void test4() { // violation, '@inheritDoc' tag requires @Override annotation
+
+  }
 }
 // xdoc section -- end
