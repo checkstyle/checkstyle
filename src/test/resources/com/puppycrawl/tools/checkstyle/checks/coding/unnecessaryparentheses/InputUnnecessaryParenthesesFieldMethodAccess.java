15f62e59f5661ffa517cb589dcf77d6623e7cbdd
Zbynek Konecny <zbynek@geogebra.org>
2025-12-18 23:16:19 +0100

    Issue #17554: add tests

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unnecessaryparentheses/InputUnnecessaryParenthesesFieldMethodAccess.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unnecessaryparentheses/InputUnnecessaryParenthesesFieldMethodAccess.java
@@ -20,6 +20,7 @@ public class InputUnnecessaryParenthesesFieldMethodAccess {
 
     private static class Main {
         private String name;
+        private int size;
 
         private boolean check() {
             return false;
@@ -27,22 +28,69 @@ public class InputUnnecessaryParenthesesFieldMethodAccess {
     }
 
     public static void main(String[] args) {
+        foo = args[0].length();
         foo = (args[0]).length();
         // violation above 'Unnecessary parentheses around expression.'
+        foo = String.valueOf(foo).length();
         foo = (String.valueOf(foo)).length();
         // violation above 'Unnecessary parentheses around expression.'
+        foo = INSTANCE.name.length();
         foo = (INSTANCE.name).length();
         // violation above 'Unnecessary parentheses around expression.'
+        foo = new StringBuilder().length();
         foo = (new StringBuilder()).length();
         // violation above 'Unnecessary parentheses around expression.'
         boolean b = INSTANCE.check() ? (INSTANCE.check()) : true;
         // violation above 'Unnecessary parentheses around expression.'
-        foo = args[0].length();
-        foo = String.valueOf(foo).length();
-        foo = INSTANCE.name.length();
-        foo = new StringBuilder().length();
         foo = (INSTANCE.check()) ? 1 : 2;
+        foo = ((String) INSTANCE.name).length();
         System.out.println(foo);
+    }
+
+    public static void plus() {
+        int[] a = {1};
+        int b;
+        Main m = new Main();
+        b = a[0] + 1;
+        b = (a[0]) + 1;
+        // violation above 'Unnecessary parentheses around expression.'
+        b = m.size + 1;
+        b = (m.size) + 1;
+        // violation above 'Unnecessary parentheses around expression.'
+        String s;
+        s = m.check() + "";
+        s = (m.check()) + "";
+        // violation above 'Unnecessary parentheses around expression.'
+        s = new Main() + "";
+        s = (new Main()) + "";
+        // violation above 'Unnecessary parentheses around expression.'
+    }
+
+    public void checks() {
+        Main m = new Main();
+        if (!m.check()) {
+            throw new RuntimeException("fail");
+        }
+        if (!(m.check())) {
+            // violation above 'Unnecessary parentheses around expression.'
+            throw new RuntimeException("fail");
+        }
+
+        boolean b1 = ((Main) m.clone()).check();
+        boolean b2 = ((Main) (m.clone())).check();
+        // violation above 'Unnecessary parentheses around expression.'
+        Class c = m.getClass();
+        if (c == null || !Main.class.isAssignableFrom(c)) {
+            return null;
+        }
+        if (c == null || !(Main.class.isAssignableFrom(c))) {
+            // violation above 'Unnecessary parentheses around expression.'
+            return null;
+        }
+        java.util.List<String> nolinks = java.util.List.of("s");
+        String[] newfiles = (String[]) noLinks.toArray(new String[noLinks.size()]);
+        String[] newfiles = (String[]) (noLinks.toArray(new String[noLinks.size()]));
+        // violation above 'Unnecessary parentheses around expression.'
     }
 
 }
