04a7efdf18eae43d3710e073fcec440e18e473c2
vishalup29 <vishalupadhyay977@gmail.com>
2025-12-19 00:19:53 -0600

    Issue #17449: Add XDocs example for FinalLocalVariableCheck validateUnnamedVariables property

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/finallocalvariable/Example5.java
@@ -0,0 +1,34 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="FinalLocalVariable">
+      <property name="tokens" value="VARIABLE_DEF,PARAMETER_DEF"/>
+      <property name="validateEnhancedForLoopVariable" value="false"/>
+      <property name="validateUnnamedVariables" value="true"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.finallocalvariable;
+
+// xdoc section -- start
+class Example5
+{
+    static int foo(int x, int y) {
+        // 2 violations above:
+        //  'Variable 'x' should be declared final'
+        //  'Variable 'y' should be declared final'
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        // violation above, 'Variable 'args' should be declared final'
+        // ok below, because validateEnhancedForLoopVariable is false
+        for (String i : args) {
+            System.out.println(i);
+        }
+        int result = foo(1, 2); // violation, 'Variable 'result' should be declared final'
+    }
+}
+// xdoc section -- end
