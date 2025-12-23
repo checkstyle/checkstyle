f34224489ce0171174707af64fa13ec96a3b0dbe
vivek-0509 <viveksolanki0509@gmail.com>
2025-12-23 12:32:35 +0530

    Issue #18494: Add GoogleMemberNameCheck for Google Java Style compliance

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/naming/googlemembername/InputXpathGoogleMemberName.java
@@ -0,0 +1,7 @@
+package org.checkstyle.suppressionxpathfilter.naming.googlemembername;
+
+public class InputXpathGoogleMemberName {
+
+    int Foo; // warn 'Member name 'Foo' must start with a lowercase letter, min 2 chars'
+
+}
