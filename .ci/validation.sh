c06f69d6d6bc80c7948cc2b36b3bdb12497ff4af
Vincent Potucek <vpotucek@me.com>
2025-12-26 13:56:08 +0100

    Issue #17487: Add `sanity` CI job #18157

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -1365,30 +1365,26 @@ spotless)
   ./mvnw -e --no-transfer-progress clean spotless:check -P spotless-autofix
   ;;
 
-openrewrite-recipes)
-  echo "Cloning and building OpenRewrite recipes..."
-  PROJECT_ROOT="$(pwd)"
+sanity)
   export MAVEN_OPTS="-Xmx4g -Xms2g"
-
+  echo "Sanity Check 🦢"
+  echo "Cloning and building OpenRewrite recipes 📥"
   cd /tmp
   git clone https://github.com/checkstyle/checkstyle-openrewrite-recipes.git
   cd checkstyle-openrewrite-recipes
   mvn -e --no-transfer-progress clean install -DskipTests
-
-  cd "$PROJECT_ROOT"
-
-  echo "Running Checkstyle validation to get report for openrewrite..."
+  cd "$(pwd)"
+  echo "Run Checkstyle validation to get report for openrewrite ☑️"
   set +e
   ./mvnw -e --no-transfer-progress clean compile antrun:run@ant-phase-verify
   set -e
-  echo "Running OpenRewrite recipes..."
+  echo "Run OpenRewrite recipes ☑️"
   ./mvnw -e --no-transfer-progress -Drewrite.recipeChangeLogLevel=INFO \
-    rewrite:run -P checkstyle-autofix
-
-  echo "Checking for uncommitted changes..."
+    clean rewrite:run -P checkstyle-autofix
+  echo "Checking for uncommitted changes 📋"
   ./.ci/print-diff-as-patch.sh target/rewrite.patch
-
   rm -rf /tmp/checkstyle-openrewrite-recipes
+  echo "Complete ☑️"
   ;;
 
 *)
