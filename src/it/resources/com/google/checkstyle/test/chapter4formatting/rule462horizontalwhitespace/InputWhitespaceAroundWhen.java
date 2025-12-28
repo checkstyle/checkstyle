5d34b81f1c31bd684607aef7a341d6bed9fae760
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-12-28 10:58:45 +0530

    Issue #17565: Enforce line break after '{' in LeftCurlyCheck

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputWhitespaceAroundWhen.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputWhitespaceAroundWhen.java
@@ -7,41 +7,50 @@ class InputWhitespaceAroundWhen {
   /** Method. */
   void test(Object o) {
     switch (o) {
-      case Integer i when(i == 0) -> // violation ''when' is not followed by whitespace.'
+      // violation below ''when' is not followed by whitespace.'
+      case Integer i when(i == 0) ->
         {
         }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
       // violation below, ''when' is not followed by whitespace.'
       case String s when(
               s.equals("a")) ->
         {
         }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
 
       // violation below, ''when' is not followed by whitespace.'
       case Point(int x, int y) when!(x >= 0 && y >= 0) ->
         {
         }
-
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
+      // violation 2 lines below ''{' at column 9 should be on the previous line.'
       default ->
         {
         }
     }
 
     switch (o) {
+      // violation below ''when' is not preceded with whitespace.'
       case Point(int x, int y)when (x < 9 && y >= 0) ->
         {
-        }  // violation 2 lines above ''when' is not preceded with whitespace.'
+        }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
+      // 2 violations 3 lines below:
+      //              ''when' is not followed by whitespace.'
+      //              ''when' is not preceded with whitespace.'
       case Point(int x, int y)when(x >= 0 && y >= 0) ->
         {
         }
-      // 2 violations 3 lines above:
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
+      // 2 violations 3 lines below:
       //              ''when' is not followed by whitespace.'
       //              ''when' is not preceded with whitespace.'
       case Point(int x, int y)when!(x >= 0 && y >= 0) ->
         {
         }
-      // 2 violations 3 lines above:
-      //              ''when' is not followed by whitespace.'
-      //              ''when' is not preceded with whitespace.'
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
+      // violation 2 lines below ''{' at column 9 should be on the previous line.'
       default ->
         {
         }
@@ -55,12 +64,16 @@ class InputWhitespaceAroundWhen {
       case Integer i when (i == 0) ->
         {
         }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
       case String s when (s.equals("a")) ->
         {
         }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
       case Point(int x, int y) when (x >= 0 && y >= 0) ->
         {
         }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
+      // violation 2 lines below ''{' at column 9 should be on the previous line.'
       default ->
         {
         }
@@ -70,12 +83,16 @@ class InputWhitespaceAroundWhen {
       case Integer i when i == 0 ->
         {
         }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
       case String s when s.equals("a") ->
         {
         }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
       case Point(int x, int y) when x >= 0 && y >= 0 ->
         {
         }
+      // violation 2 lines above ''{' at column 9 should be on the previous line.'
+      // violation 2 lines below ''{' at column 9 should be on the previous line.'
       default ->
         {
         }
@@ -107,6 +124,7 @@ class InputWhitespaceAroundWhen {
       case 'p' -> {
         System.out.println("o");
       }
+      // violation 2 lines below ''{' at column 9 should be on the previous line.'
       default ->
         {
           System.out.println("default");
