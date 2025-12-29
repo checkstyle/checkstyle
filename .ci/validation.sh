5632f906b80d6ab2d32393f276563c602d1eed6a
Vincent Potucek <vpotucek@me.com>
2025-12-29 11:33:43 +0100

    Issue #17487: Finalize rewrite cleanup

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -1368,25 +1368,19 @@ spotless)
 openrewrite-recipes)
   echo "Cloning and building OpenRewrite recipes..."
   PROJECT_ROOT="$(pwd)"
-  # export MAVEN_OPTS="-Xmx4g -Xms2g"
-
   cd /tmp
   git clone https://github.com/checkstyle/checkstyle-openrewrite-recipes.git
   cd checkstyle-openrewrite-recipes
   mvn -e --no-transfer-progress clean install -DskipTests
-
   cd "$PROJECT_ROOT"
-
   echo "Running Checkstyle validation to get report for openrewrite..."
   set +e
   ./mvnw -e --no-transfer-progress clean compile antrun:run@ant-phase-verify
   set -e
   echo "Running OpenRewrite recipes..."
   ./mvnw -e --no-transfer-progress rewrite:run -Drewrite.recipeChangeLogLevel=INFO
-
   echo "Checking for uncommitted changes..."
   ./.ci/print-diff-as-patch.sh target/rewrite.patch
-
   rm -rf /tmp/checkstyle-openrewrite-recipes
   ;;
 
