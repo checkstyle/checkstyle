ba32605c8770bb859edaca042a4afdc7e46a6580
Vincent Potucek <vpotucek@me.com>
2026-01-01 10:43:31 +0100

    Issue #18470: Cleanup CI config #18440

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -1341,7 +1341,7 @@ sevntu)
 spotbugs)
   # https://github.com/spotbugs/spotbugs-maven-plugin/issues/806 explains
   # why we need execution of spotbugs without any other maven plugins which can change binaries
-  ./mvnw -e --no-transfer-progress clean test-compile spotbugs:check
+  ./mvnw -e --no-transfer-progress test-compile spotbugs:check
   ;;
 
 spotless)
@@ -1349,8 +1349,8 @@ spotless)
   ;;
 
 error-prone)
-  ./mvnw -e --no-transfer-progress clean verify -P error-prone-compile
-  ./mvnw -e --no-transfer-progress clean verify -P error-prone-test-compile
+  ./mvnw -e --no-transfer-progress verify -P error-prone-compile
+  ./mvnw -e --no-transfer-progress verify -P error-prone-test-compile
   ;;
 
 pmd)
