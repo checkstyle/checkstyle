eee2f23e3f14bba699db32cac65f65c305b40233
0xChaitanya <chaitanyasatsangi26@gmail.com>
2026-01-07 14:20:23 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test nowhitespacebefore

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nowhitespacebefore/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nowhitespacebefore/Example4.java
@@ -18,13 +18,12 @@ import com.google.common.collect.Lists;
 // xdoc section -- start
 class Example4 {
   void example() {
-    Lists .charactersOf("foo") // violation ''.' is preceded with whitespace'
-          .listIterator()
-          .forEachRemaining(System.out ::print);
-    // violation above ''::' is preceded with whitespace'
-    Lists.charactersOf("foo")
-         .listIterator()
-         .forEachRemaining(System.out::print);
+      Lists.charactersOf("foo").listIterator()
+              .forEachRemaining(System.out::print);
+      // violation above ''.' is preceded with whitespace'
+      Lists.charactersOf("foo").listIterator().forEachRemaining(System.out ::print);
+      // violation above ''::' is preceded with whitespace'
+      Lists.charactersOf("foo").listIterator().forEachRemaining(System.out::print);
   }
 }
 // xdoc section -- end
