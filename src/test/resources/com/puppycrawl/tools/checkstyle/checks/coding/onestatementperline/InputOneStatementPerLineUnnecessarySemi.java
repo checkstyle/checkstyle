ac0cfba82bdaa6b4d589e676c5e92bb765306205
atharv <atharv55215@gmail.com>
2026-01-09 12:32:03 +0530

    Issue #17662: Rework OneStatementPerLineCheck implementation logic

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineUnnecessarySemi.java
@@ -0,0 +1,32 @@
+/*
+OneStatementPerLine
+treatTryResourcesAsStatement = (default)false
+
+
+*/
+
+
+package com.puppycrawl.tools.checkstyle.checks.coding.onestatementperline;
+
+public class InputOneStatementPerLineUnnecessarySemi {
+
+    class Inner {};
+
+    class Inner2 {};
+
+    interface Inner3 {
+    };
+
+    record Inner4() {
+    };
+
+    enum Inner5 {
+    };
+
+    void method() {
+        int j = 1;
+
+        class LocalClass {};
+    };
+
+}
