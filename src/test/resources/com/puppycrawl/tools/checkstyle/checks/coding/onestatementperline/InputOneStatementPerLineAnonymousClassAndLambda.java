9a123a24d8793834cc714431214d6a9c44782691
atharv <atharv55215@gmail.com>
2026-01-09 12:32:03 +0530

    Issue #17662: Rework OneStatementPerLineCheck implementation logic

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLineAnonymousClassAndLambda.java
@@ -0,0 +1,55 @@
+/*
+OneStatementPerLine
+treatTryResourcesAsStatement = (default)false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.onestatementperline;
+
+import java.util.function.Consumer;
+
+public class InputOneStatementPerLineAnonymousClassAndLambda {
+    void method() {
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() { int a =
+                1; Consumer<String> c = s -> { System.out.println(s); };
+            } // 2 violations above:
+        };    // 'Only one statement per line allowed.'
+              // 'Only one statement per line allowed.'
+
+        Consumer<String> c2 = (str) -> {
+            Runnable r2 = new Runnable() {
+                @Override public void run() { int x = 10;; }
+            }; // violation above 'Only one statement per line allowed.'
+        };; // violation 'Only one statement per line allowed.'
+
+        Runnable r3 = new Runnable() { public void run() {} };
+        int[] b =
+                new int[2]; Runnable r4 = () -> {};
+        // violation above 'Only one statement per line allowed.'
+
+        Object obj = new Object();
+
+        Runnable r5 = () -> {
+            new Thread(new Runnable() {
+                public void run() {
+                    Runnable r6 = () -> { int y = 20; };
+                }
+            }).start();
+        };
+    }
+    void method2() {
+        Object l1 = new Object() {
+            Consumer<Object> l2 = (obj) -> {
+                Object l3 = new Object() {
+                    int a = 2; Consumer<String> l4 =
+                            s -> { foo();};
+                }; // violation above 'Only one statement per line allowed.'
+            int b = 1; };
+        };
+        }
+        void foo() {}
+
+int a = 2;}; // violation 'Only one statement per line allowed.'
