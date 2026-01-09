99df288aaec02918860336c7a3a2a4a9df6b825e
MonuChaudhary14 <monuchaudharypoonia@gmail.com>
2026-01-09 18:51:54 +0530

    Issue #17541: Enforced emptyLineSeperator on package-info.java

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/packageinfo/Example11/package-info.java
@@ -0,0 +1,19 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="EmptyLineSeparator"/>
+  </module>
+</module>
+*/
+
+// xdoc section -- start
+///////////////////////////////////////////////////
+//HEADER
+///////////////////////////////////////////////////
+
+// single-line comment before package
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator.packageinfo.Example11;
+// violation above ''package' should be separated from previous line'
+
+import java.lang.System;
+// xdoc section -- end
