13c054a53cab86d729eb0405363d8a6f2daf79ed
atharv <atharv55215@gmail.com>
2026-01-09 12:32:03 +0530

    Issue #17662: Rework OneStatementPerLineCheck implementation logic

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLine2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/onestatementperline/InputOneStatementPerLine2.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PipedOutputStream;
 
-public class InputOneStatementPerLineTest {
+public class InputOneStatementPerLine2 {
     void testNestedInLambda() {
         Runnable r = () -> {
             try (OutputStream s1 = new PipedOutputStream()) {
@@ -20,6 +20,17 @@ public class InputOneStatementPerLineTest {
             }
         };
         System.out.println(r);
+    }
+
+    void testAssert(int x) {
+        assert x > 0; assert x < 100;
+        // violation above 'Only one statement per line allowed.'
+
+        assert x != 50
+                : "x should not be 50";
+
+        int y = 5; assert y > 0 : "y should be positive";
+        // violation above 'Only one statement per line allowed.'
     }
 
 }
