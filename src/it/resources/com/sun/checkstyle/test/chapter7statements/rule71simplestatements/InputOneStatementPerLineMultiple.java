9214ec42635902e7b901c24ace62969e2368d633
vivek-0509 <viveksolanki0509@gmail.com>
2026-01-09 10:08:21 +0530

    Issue #18559: Add missing checks to sun_checks.xml

---
+++ src/it/resources/com/sun/checkstyle/test/chapter7statements/rule71simplestatements/InputOneStatementPerLineMultiple.java
@@ -0,0 +1,46 @@
+package com.sun.checkstyle.test.chapter7statements.rule71simplestatements;
+
+/**
+ * Test input for multiple statements per line.
+ */
+public final class InputOneStatementPerLineMultiple {
+    /** Dummy variable. */
+    private int one = 0;
+
+    /** Dummy variable. */
+    private int two = 0;
+
+    /**
+     * Method with multiple violations.
+     */
+    public void method() {
+        int num = 0;
+        int count = 1; count++; // violation 'Only one statement'
+        num++; count++; // violation 'Only one statement per line allowed.'
+    }
+
+    /**
+     * Method with while loop violation.
+     */
+    public void whileMethod() {
+        while (one < 2) {
+            one++; two--; // violation 'Only one statement per line allowed.'
+        }
+    }
+
+    /**
+     * Method with if statement violation.
+     */
+    public void ifMethod() {
+        if (one == 1) {
+            one++; two++; // violation 'Only one statement per line allowed.'
+        }
+    }
+
+    /**
+     * Method with method call violations.
+     */
+    public void callMethod() {
+        toString(); toString(); // violation 'Only one statement'
+    }
+}
