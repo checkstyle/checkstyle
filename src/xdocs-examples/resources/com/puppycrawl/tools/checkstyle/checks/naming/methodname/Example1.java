d209717adad1ac6534d1df4a4b1006862a6dbc30
EKansh Mittal <ekanshmittal04@gmail.com>
2026-01-09 11:30:04 +0530

    Issue #18435: Fix AST consistency across MethodNameCheck xdocs examples

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/methodname/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/methodname/Example1.java
@@ -6,13 +6,18 @@
 </module>
 */
 
+
+
+
+
 package com.puppycrawl.tools.checkstyle.checks.naming.methodname;
 
 // xdoc section -- start
 class Example1 {
   public void method1() {}
-  protected void method2() {}
+  protected void Method2() {} // violation 'Name 'Method2' must match pattern'
   private void Method3() {} // violation 'Name 'Method3' must match pattern'
   public void Method4() {} // violation 'Name 'Method4' must match pattern'
+  void Method5() {} // violation 'Name 'Method5' must match pattern'
 }
 // xdoc section -- end
