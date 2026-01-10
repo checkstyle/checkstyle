ec518904028082823de6fb32368b1c1947fbf343
Anushree Bondia <anushreebondia@gmail.com>
2026-01-10 13:22:05 +0530

    fix:reduced repetition

--- src/site/resources/js/anchors.js
+++ src/site/resources/js/anchors.js
@@ -2,6 +2,46 @@
 /*global window */
 (function () {
     "use strict";
+
+    function showCopiedFeedback(element) {
+        var feedback = document.createElement("span");
+        feedback.textContent = "copied";
+        feedback.className = "anchor-feedback";
+
+        element.appendChild(feedback);
+
+        setTimeout(function () {
+            feedback.remove();
+        }, 1200);
+    }
+
+    function createAnchor(parentElement, link, name, relativePath, feedbackTarget) {
+        var a = document.createElement("a");
+        a.setAttribute("href", link);
+
+        a.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(link);
+                showCopiedFeedback(feedbackTarget);
+            }
+
+            // Update URL without page jump
+            history.replaceState(null, "", "#" + name);
+        });
+
+        var image = document.createElement("img");
+        image.setAttribute("src", relativePath + "/images/anchor.png");
+
+        var anchor = document.createElement("div");
+        anchor.className = "anchor";
+
+        a.appendChild(image);
+        anchor.appendChild(a);
+        parentElement.appendChild(anchor);
+    }
+
     window.addEventListener("load", function () {
         var url = window.location.href;
         var position = url.indexOf("#");
@@ -10,10 +50,11 @@
             url = url.substring(0, position);
         }
 
-        const scriptElement = document.querySelector('script[src*="anchors.js"]');
-        const scriptElementSrc = scriptElement.attributes.src.textContent;
-        const relativePath = scriptElementSrc.replace(/\/js\/anchors.js/, '');
+        var scriptElement = document.querySelector('script[src*="anchors.js"]');
+        var scriptElementSrc = scriptElement.attributes.src.textContent;
+        var relativePath = scriptElementSrc.replace(/\/js\/anchors.js/, "");
 
+        // h1 / h2
         var anchors = document.querySelectorAll("h1, h2");
         [].forEach.call(anchors, function (anchorItem) {
 
@@ -22,34 +63,12 @@
             }
 
             var name = anchorItem.previousSibling.previousElementSibling.id;
-            var link = "" + url + "#" + name + "";
+            var link = url + "#" + name;
 
-            var a = document.createElement("a");
-            a.setAttribute("href", link);
-
-            a.addEventListener("click", function (event) {
-                event.preventDefault();
-
-                if (navigator.clipboard) {
-                    navigator.clipboard.writeText(link);
-                    showCopiedFeedback(anchorItem);
-                }
-
-                // Update URL without page jump
-                history.replaceState(null, "", "#" + name);
-            });
-
-            var image = document.createElement("img");
-            image.setAttribute("src", `${relativePath}/images/anchor.png`);
-
-            var anchor = document.createElement("div");
-            anchor.className = "anchor";
-
-            a.appendChild(image);
-            anchor.appendChild(a);
-            anchorItem.appendChild(anchor);
+            createAnchor(anchorItem, link, name, relativePath, anchorItem);
         });
 
+        // h3
         var anchorsSubSection = document.getElementsByTagName("h3");
         [].forEach.call(anchorsSubSection, function (anchorItem) {
             var name;
@@ -58,78 +77,19 @@
             } else {
                 name = anchorItem.childNodes[0].name;
             }
-            var link = "" + url + "#" + name + "";
 
-            var a = document.createElement("a");
-            a.setAttribute("href", link);
-
-            a.addEventListener("click", function (event) {
-                event.preventDefault();
-
-                if (navigator.clipboard) {
-                    navigator.clipboard.writeText(link);
-                    showCopiedFeedback(anchorItem);
-                }
-
-                // Update URL without page jump
-                history.replaceState(null, "", "#" + name);
-            });
-
-            var image = document.createElement("img");
-            image.setAttribute("src", `${relativePath}/images/anchor.png`);
-
-            var anchor = document.createElement("div");
-            anchor.className = "anchor";
-
-            a.appendChild(image);
-            anchor.appendChild(a);
-            anchorItem.appendChild(anchor);
+            var link = url + "#" + name;
+            createAnchor(anchorItem, link, name, relativePath, anchorItem);
         });
 
-        const exampleDivs = document.querySelectorAll('p[id^="Example"]');
+        // Example sections
+        var exampleDivs = document.querySelectorAll('p[id^="Example"]');
         [].forEach.call(exampleDivs, function (exampleDiv) {
-            const name = exampleDiv.id;
-            const link = "" + url + "#" + name + "";
+            var name = exampleDiv.id;
+            var link = url + "#" + name;
 
-            const a = document.createElement("a");
-            a.setAttribute("href", link);
-
-            a.addEventListener("click", function (event) {
-                event.preventDefault();
-
-                if (navigator.clipboard) {
-                    navigator.clipboard.writeText(link);
-                    showCopiedFeedback(anchorItem);
-                }
-
-                // Update URL without page jump
-                history.replaceState(null, "", "#" + name);
-            });
-
-            const image = document.createElement("img");
-            image.setAttribute("src", `${relativePath}/images/anchor.png`);
-
-            const anchor = document.createElement("div");
-            anchor.className = "anchor";
-
-            a.appendChild(image);
-            anchor.appendChild(a);
-            exampleDiv.appendChild(anchor);
+            createAnchor(exampleDiv, link, name, relativePath, exampleDiv);
         });
     });
 
-}(),
-function showCopiedFeedback(element) {
-    var feedback = document.createElement("span");
-    feedback.textContent = "Link copied";
-    feedback.className = "anchor-feedback";
-
-    element.appendChild(feedback);
-
-    setTimeout(function () {
-        feedback.remove();
-    }, 1200);
-}
-
-
-);
+}());
