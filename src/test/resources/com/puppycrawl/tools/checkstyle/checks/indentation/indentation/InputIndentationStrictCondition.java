31c25b43c42b0910f62a53a8ac36f0a709b4d05d
Anushree Bondia <anushreebondia@gmail.com>
2026-01-10 13:56:45 +0530

    Issue #17128: Fix trailing comments vertical alignment in indentation test files

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationStrictCondition.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationStrictCondition.java
@@ -3,12 +3,12 @@ package com.puppycrawl.tools.checkstyle.checks.indentation.indentation; //indent
 import java.lang.Thread; //indent:0 exp:0
 
 public class InputIndentationStrictCondition { //indent:0 exp:0
-    void method(Thread foo) { //indent:4 exp:4
-        method( //indent:8 exp:8
-                new Thread() { //indent:16 exp:16
-                        public void run() { //indent:24 exp:24
-                            } //indent:28 exp:16,20,24 warn
-                    } //indent:20 exp:20
-        ); //indent:8 exp:8
-        } //indent:8 exp:4 warn
-    } //indent:4 exp:0 warn
+    void method(Thread foo) {                  //indent:4 exp:4
+        method(                                //indent:8 exp:8
+                new Thread() {                 //indent:16 exp:16
+                        public void run() {    //indent:24 exp:24
+                            }                  //indent:28 exp:16,20,24 warn
+                    }                          //indent:20 exp:20
+        );                                     //indent:8 exp:8
+        }                                      //indent:8 exp:4 warn
+    }                                          //indent:4 exp:0 warn
