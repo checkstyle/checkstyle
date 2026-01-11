91fb7b9f852dfdacdec8e415aa5f4765e46741e1
abhayrajjais01 <abhayraj916146@gmail.com>
2026-01-11 16:13:06 +0530

    fixed issue #18599

--- src/main/java/com/puppycrawl/tools/checkstyle/ConfigurationLoader.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/ConfigurationLoader.java
@@ -371,8 +371,8 @@ public final class ConfigurationLoader {
          *
          * @return the original string with the properties replaced.
          * @throws CheckstyleException if the string contains an opening
-         *                           {@code ${} without a closing
-         *                           {@code }}
+         *                           {@code $&#123;} without a closing
+         *                           {@code &#125;}
          */
         private String replaceProperties(
                 String value, String defaultValue)
@@ -424,8 +424,8 @@ public final class ConfigurationLoader {
          *                     Must not be {@code null}.
          *
          * @throws CheckstyleException if the string contains an opening
-         *                           {@code ${} without a closing
-         *                           {@code }}
+         *                           {@code $&#123;} without a closing
+         *                           {@code &#125;}
          */
         private static void parsePropertyString(String value,
                                                Collection<String> fragments,
--- src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionCommentFilter.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/filters/SuppressionCommentFilter.java
@@ -60,7 +60,7 @@ import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
  *
  * <p>
  * Attention: This filter may only be specified within the TreeWalker module
- * ({@code &lt;module name="TreeWalker"/&gt;}) and only applies to checks which are also
+ * ({@code <module name="TreeWalker"/>}) and only applies to checks which are also
  * defined within this module. To filter non-TreeWalker checks like {@code RegexpSingleline}, a
  * <a href="https://checkstyle.org/filters/suppresswithplaintextcommentfilter.html">
  * SuppressWithPlainTextCommentFilter</a> or similar filter must be used.
@@ -111,7 +111,7 @@ public class SuppressionCommentFilter
     /** Tagged comments. */
     private final List<Tag> tags = new ArrayList<>();
 
-    /** Control whether to check C style comments ({@code &#47;* ... *&#47;}). */
+    /** Control whether to check C style comments ({@code /* ... */}). */
     private boolean checkC = true;
 
     /** Control whether to check C++ style comments ({@code //}). */
@@ -217,17 +217,17 @@ public class SuppressionCommentFilter
     /**
      * Setter to control whether to check C++ style comments ({@code //}).
      *
-     * @param checkCpp {@code true} if C++ comments are checked.
+     * @param checkCPP {@code true} if C++ comments are checked.
      * @since 3.5
      */
     // -@cs[AbbreviationAsWordInName] We can not change it as,
     // check's property is a part of API (used in configurations).
-    public void setCheckCPP(boolean checkCpp) {
-        checkCPP = checkCpp;
+    public void setCheckCPP(boolean checkCPP) {
+        this.checkCPP = checkCPP;
     }
 
     /**
-     * Setter to control whether to check C style comments ({@code &#47;* ... *&#47;}).
+     * Setter to control whether to check C style comments ({@code /* ... */}).
      *
      * @param checkC {@code true} if C comments are checked.
      * @since 3.5
@@ -505,8 +505,8 @@ public class SuppressionCommentFilter
                 return false;
             }
             final Tag tag = (Tag) other;
-            return Objects.equals(line, tag.line)
-                    && Objects.equals(column, tag.column)
+            return line == tag.line
+                    && column == tag.column
                     && Objects.equals(tagType, tag.tagType)
                     && Objects.equals(text, tag.text)
                     && Objects.equals(tagCheckRegexp, tag.tagCheckRegexp)
