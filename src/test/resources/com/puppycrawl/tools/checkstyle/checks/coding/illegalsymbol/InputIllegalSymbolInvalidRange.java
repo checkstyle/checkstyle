8c5cfb0ce5ba848f5bb9ede46b44735acdc47d6f
smita_1078 <smita.prajapati082@gmail.com>
2026-01-11 23:45:27 +0530

    Issue #18065: Forbid Emoji in Code

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalsymbol/InputIllegalSymbolInvalidRange.java
@@ -0,0 +1,13 @@
+/*
+IllegalSymbol
+symbolCodes = 0x2705-
+tokens = (default)COMMENT_CONTENT
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.illegalsymbol;
+
+public class InputIllegalSymbolInvalidRange {
+    int x = 1;
+}
