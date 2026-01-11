97295c39cbfad8253592fbec0bf643f966aa55e5
smita_1078 <smita.prajapati082@gmail.com>
2026-01-11 23:45:27 +0530

    Issue #18065: Forbid Emoji in Code

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalsymbol/InputIllegalSymbolDefaultStrong.java
@@ -0,0 +1,15 @@
+/*
+IllegalSymbol
+symbolCodes = (default)0x2190-0x27BF, 0x1F600-0x1F64F, 0x1F680-0x1F6FF, 0x1F700-0x1FFFFF
+tokens = (default)COMMENT_CONTENT
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.illegalsymbol;
+
+public class InputIllegalSymbolDefaultStrong {
+    // 😀 First emoji from 0x1F600 range // violation 'Illegal symbol detected'
+    // 🚀 Second emoji from 0x1F680 range // violation 'Illegal symbol detected'
+    int x = 1;
+}
