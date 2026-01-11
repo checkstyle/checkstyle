6fba69150314f2973cc6453b69d940328c34736d
smita_1078 <smita.prajapati082@gmail.com>
2026-01-11 23:45:27 +0530

    Issue #18065: Forbid Emoji in Code

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalsymbol/InputIllegalSymbolSpacedEntries.java
@@ -0,0 +1,14 @@
+/*
+IllegalSymbol
+symbolCodes = 0x2705
+tokens = (default)COMMENT_CONTENT
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.illegalsymbol;
+
+public class InputIllegalSymbolSpacedEntries {
+               // ✅ spaces in config // violation 'Illegal symbol detected: '✅''
+    int x = 1;
+}
