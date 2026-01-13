5120f947f139a8c2f684693bb3485015d8a50a7f
vivek-0509 <viveksolanki0509@gmail.com>
2026-01-13 14:14:07 +0530

    Issue #18621: Add OpenJDK Java Style Guidelines coverage infrastructure

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapter3formatting/rule38wrappinglines/InputOneStatementPerLineForLoop.java
@@ -0,0 +1,51 @@
+package com.openjdk.checkstyle.test.chapter3formatting.rule38wrappinglines;
+
+/**
+ * Test input for one statement per line in for loop.
+ */
+public final class InputOneStatementPerLineForLoop {
+
+    /**
+     * Method with for loop - multiple statements in header are allowed.
+     */
+    public void method() {
+        int sum = 0;
+        for (int i = 0, j = 0; i < 2; i++, j++) {
+            sum += i + j;
+        }
+    }
+
+    /**
+     * Multiline for loop is legal.
+     */
+    public void multilineFor() {
+        int sum = 0;
+        for (int i = 0,
+                j = 1; i < 2; i++, j--) {
+            sum += i;
+        }
+    }
+
+    /**
+     * Forever loop is legal.
+     */
+    public void foreverLoop() {
+        int count = 0;
+        for (;;) {
+            count++;
+            if (count > 1) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * One statement inside for block is legal.
+     */
+    public void singleStatementFor() {
+        int one = 0;
+        for (int i = 0; i < 2; i++) {
+            one = i;
+        }
+    }
+}
---
+++ src/it/resources/com/openjdk/checkstyle/test/chapter3formatting/rule38wrappinglines/InputOneStatementPerLineValid.java
@@ -0,0 +1,61 @@
+package com.openjdk.checkstyle.test.chapter3formatting.rule38wrappinglines;
+
+/**
+ * Test input for valid one statement per line.
+ */
+public final class InputOneStatementPerLineValid {
+
+    /** Dummy variable. */
+    private int one = 0;
+
+    /** Dummy variable. */
+    private int two = 0;
+
+    /**
+     * Method with proper one statement per line.
+     */
+    public void method() {
+        int num = 0;
+        int count = 1;
+        num++;
+        count++;
+    }
+
+    /**
+     * Legal method with separate lines.
+     */
+    public void doLegal() {
+        one = 1;
+        two = 2;
+    }
+
+    /**
+     * String with format inside is legal.
+     */
+    public void doLegalString() {
+        one = 1;
+        two = 2;
+        System.identityHashCode("one = 1; two = 2");
+    }
+
+    /**
+     * Break and continue statements.
+     */
+    public void loopMethod() {
+        int sum = 0;
+        for (int i = 0; i < 2; i++) {
+            sum += i;
+            if (sum > 1) {
+                break;
+            }
+        }
+
+        while (one < 2) {
+            one++;
+        }
+
+        do {
+            two++;
+        } while (two < 2);
+    }
+}
