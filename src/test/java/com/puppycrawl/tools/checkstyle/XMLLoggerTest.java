4468c25a11805dd73cd592bd071c15b09296a71a
ayushactiveat <253532972+ayushactiveat@users.noreply.github.com>
2026-01-14 22:59:50 +0000

    Issue #16360: Migrate XMLLoggerTest.testAddException to use inlineConfigParser

--- src/test/java/com/puppycrawl/tools/checkstyle/XMLLoggerTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/XMLLoggerTest.java
@@ -247,20 +247,9 @@ public class XMLLoggerTest extends AbstractXmlTestSupport {
     }
 
     @Test
-    public void testAddException()
-            throws Exception {
-        final XMLLogger logger = new XMLLogger(outStream, OutputStreamOptions.CLOSE);
-        logger.auditStarted(null);
-        final Violation violation =
-            new Violation(1, 1,
-                "messages.properties", null, null, null, getClass(), null);
-        final AuditEvent ev = new AuditEvent(this, "Test.java", violation);
-        logger.addException(ev, new TestException("msg", new RuntimeException("msg")));
-        logger.auditFinished(null);
-        verifyXml(getPath("ExpectedXMLLoggerException.xml"), outStream);
-        assertWithMessage("Invalid close count")
-            .that(outStream.getCloseCount())
-            .isEqualTo(1);
+    public void testAddException() throws Exception {
+        verifyWithInlineConfigParserAndXmlLogger("InputXMLLoggerException.java",
+                "ExpectedXMLLoggerException.xml");
     }
 
     /**
