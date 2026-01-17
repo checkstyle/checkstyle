fe06cbfc199a4d70264f9333dd68e557af260396
atharv <atharv55215@gmail.com>
2026-01-17 23:28:37 +0530

    Issue #18420: Add GoogleMethodNameCheck to enforce Google Java Style Guide method naming

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/googlemethodname/InputGoogleMethodNameInsideRecord.java
@@ -0,0 +1,27 @@
+/*
+GoogleMethodName
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.naming.googlemethodname;
+
+import org.junit.jupiter.api.Test;
+
+/** Test methods inside a record. */
+public record InputGoogleMethodNameInsideRecord(String name, int age) {
+
+    void fooBar() {}
+    void parseXml() {}
+    void guava33_4_5() {}
+
+    void Foo() {}
+    // violation above 'Method name 'Foo' .* start lowercase'
+
+    void mName() {}
+    // violation above 'Method name 'mName' .* have a single lowercase followed by uppercase.'
+
+    @Test
+    void testLogin_failsGracefully() {}
+
+    @Test
+    void login_fails() {}
+}
