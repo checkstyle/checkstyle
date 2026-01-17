a6d7e3e405a6010224e28b34ee92a09050d5c727
atharv <atharv55215@gmail.com>
2026-01-17 23:28:37 +0530

    Issue #18420: Add GoogleMethodNameCheck to enforce Google Java Style Guide method naming

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/googlemethodname/InputGoogleMethodNameInvalidTest.java
@@ -0,0 +1,63 @@
+/*
+GoogleMethodName
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.naming.googlemethodname;
+
+import org.junit.jupiter.api.Test;
+
+/** Test invalid test method names. */
+public class InputGoogleMethodNameInvalidTest {
+    @Test
+    void transferMoney_DeductsFromSource() {}
+    // violation above, 'Test method name .* segment must be more than a character, start lowercase'
+
+    @Test
+    void Testing_Foo() {}
+    // violation above, 'Test method name .* segment must be more than a character, start lowercase'
+
+    @Test
+    void testing__foo() {}
+    // violation above, 'Test method name 'testing__foo' has invalid underscore usage'
+
+    @Test
+    void testing_foo_() {}
+    // violation above, 'Test method name 'testing_foo_' has invalid underscore usage'
+
+    @Test
+    void _testing() {}
+    // violation above, 'Test method name '_testing' has invalid underscore usage'
+
+    @Test
+    void TestingFooBad() {}
+    // violation above, 'Test method name 'TestingFooBad' segment must .* start lowercase'
+
+    @Test
+    void test_1value() {}
+    // violation above, 'Test method name 'test_1value' has invalid underscore usage'
+
+    @Test
+    void test_FOO_bar() {}
+    // violation above, 'Test method name 'test_FOO_bar' segment must .* start lowercase'
+
+    @Test
+    void testing_a() {}
+    // violation above, 'Test method name 'testing_a' segment must be more than a character'
+
+    @Test
+    void test_fO_bar() {}
+    // violation above, 'Test method name 'test_fO_bar' .* single lowercase followed by uppercase.'
+
+    @Test
+    void solve6x6_returnsTrue() {}
+    // violation above, ''solve6x6_returnsTrue' has invalid underscore usage'
+
+    @Test
+    void solve6x6_noSolution_returnsFalse() {}
+    // violation above, ''solve6x6_noSolution_returnsFalse' has invalid underscore usage'
+
+    @Test
+    void foo$bar() {}
+    // violation above, ''foo\$bar' must only have letters, digits and underscores'
+
+}
