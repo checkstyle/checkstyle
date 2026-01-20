fcf15d09f1b10db9b2ba801971171e05e2e2d659
Anushree Bondia <anushreebondia@gmail.com>
2026-01-20 16:54:44 +0530

    Issue #18556: Add MultiFileRegexpHeader documentation with examples and template

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/Example3.java
@@ -0,0 +1,13 @@
+/*xml
+<module name="Checker">
+  <module name="MultiFileRegexpHeader">
+    <property
+      name="headerFiles"
+      value="${config.folder}/java.header, ${config.folder}/apache.header"/>
+  </module>
+</module>
+*/
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.header.multifileregexpheader;
+public class Example3 { }
+// xdoc section -- end
