1671482c33f54fc0fdbbbd0c47bf0f657c74da07
Anushree Bondia <anushreebondia@gmail.com>
2026-01-20 16:54:44 +0530

    Issue #18556: Updated MultiFileRegexpHeader documentation to use inline examples

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/Example1.java
@@ -0,0 +1,10 @@
+/*xml
+<module name="Checker">
+  <module name="MultiFileRegexpHeader"/>
+</module>
+*/
+// xdoc section -- start
+package com.puppycrawl.tools.checkstyle.checks.header.multifileregexpheader;
+// ok, as by default there is not specific header defined to validate it presence
+public class Example1 { }
+// xdoc section -- end
