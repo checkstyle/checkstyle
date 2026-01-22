60c0bec3590022e62dcbc2bf7523fea2fa16ec1b
Junaid-Ashraf-56 <ashrafjunaid848@gmail.com>
2026-01-22 14:28:14 +0500

    Issue #18556: Convert MultiFileRegexpHeader examples to JUnit executed templates

---
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/header/MultiFileRegexpHeaderCheckExamplesTest.java
@@ -0,0 +1,79 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2025 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.header;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
+import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+
+public class MultiFileRegexpHeaderCheckExamplesTest extends AbstractExamplesModuleTestSupport {
+
+    @Override
+    protected String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/java";
+    }
+
+    @Test
+    public void testExample1() throws Exception {
+        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
+    }
+
+    @Test
+    public void testExample2() throws Exception {
+        // Construct the expected path that the check will report in the message
+        final String headerPath = "src/xdocs-examples/resources/"
+                + getPackageLocation() + "/java.header";
+
+        final String[] expected = {
+                // Pass the path as an argument to fill the {0} placeholder
+                "1: " + getCheckMessage(MultiFileRegexpHeaderCheck.MSG_HEADER_MISSING, headerPath),
+        };
+
+        System.setProperty("config.folder",
+                "src/xdocs-examples/resources/" + getPackageLocation());
+        verifyWithInlineConfigParser(getPath("Example2.java"), expected);
+    }
+
+    @Test
+    public void testExample3() throws Exception {
+        final String headerPath = "src/xdocs-examples/resources/"
+                + getPackageLocation() + "/java.header";
+
+        final String[] expected = {
+                "1: " + getCheckMessage(MultiFileRegexpHeaderCheck.MSG_HEADER_MISSING, headerPath),
+        };
+
+        System.setProperty("config.folder",
+                "src/xdocs-examples/resources/" + getPackageLocation());
+        verifyWithInlineConfigParser(getPath("Example3.java"), expected);
+    }
+
+    @Test
+    public void testExample4() throws Exception {
+        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+
+        System.setProperty("config.folder",
+                "src/xdocs-examples/resources/" + getPackageLocation());
+        verifyWithInlineConfigParser(getPath("Example4.java"), expected);
+    }
+}
