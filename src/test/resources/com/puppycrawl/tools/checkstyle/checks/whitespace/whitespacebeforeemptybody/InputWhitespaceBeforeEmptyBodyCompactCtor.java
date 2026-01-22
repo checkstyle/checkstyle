be85a6e8398a6c6b21f852e92d7f520e08eb6fe8
vivek-0509 <viveksolanki0509@gmail.com>
2026-01-22 22:56:02 +0530

    Issue #17715: Add new WhitespaceBeforeEmptyBody check

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacebeforeemptybody/InputWhitespaceBeforeEmptyBodyCompactCtor.java
@@ -0,0 +1,37 @@
+/*
+WhitespaceBeforeEmptyBody
+tokens = (default)METHOD_DEF, CTOR_DEF, COMPACT_CTOR_DEF, \
+         CLASS_DEF, INTERFACE_DEF, ENUM_DEF, RECORD_DEF, ANNOTATION_DEF, \
+         LITERAL_WHILE, LITERAL_FOR, LITERAL_DO, \
+         STATIC_INIT, \
+         LITERAL_TRY, LITERAL_CATCH, LITERAL_FINALLY, LITERAL_SYNCHRONIZED, LITERAL_SWITCH, \
+         LAMBDA, LITERAL_NEW
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.whitespacebeforeemptybody;
+
+public class InputWhitespaceBeforeEmptyBodyCompactCtor {
+
+    record MyRecord(String name) {
+        // violation below 'Whitespace is not present before the empty body of 'MyRecord''
+        public MyRecord{}
+    }
+
+    record ValidRecord(String name) {
+        public ValidRecord {}
+    }
+
+    record NonEmptyRecord(String name) {
+        public NonEmptyRecord{
+            name = name.trim();
+        }
+    }
+
+    record RecordWithComment(String name){
+        public RecordWithComment{
+            // comment inside
+        }
+    }
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacebeforeemptybody/InputWhitespaceBeforeEmptyBodyConstructor.java
@@ -0,0 +1,29 @@
+/*
+WhitespaceBeforeEmptyBody
+tokens = (default)METHOD_DEF, CTOR_DEF, COMPACT_CTOR_DEF, \
+         CLASS_DEF, INTERFACE_DEF, ENUM_DEF, RECORD_DEF, ANNOTATION_DEF, \
+         LITERAL_WHILE, LITERAL_FOR, LITERAL_DO, \
+         STATIC_INIT, \
+         LITERAL_TRY, LITERAL_CATCH, LITERAL_FINALLY, LITERAL_SYNCHRONIZED, LITERAL_SWITCH, \
+         LAMBDA, LITERAL_NEW
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.whitespacebeforeemptybody;
+
+public class InputWhitespaceBeforeEmptyBodyConstructor {
+    // violation below 'Whitespace is not present before the empty body'
+    InputWhitespaceBeforeEmptyBodyConstructor(){}
+
+    // violation below 'Whitespace is not present before the empty body'
+    public InputWhitespaceBeforeEmptyBodyConstructor(int x){}
+
+    public InputWhitespaceBeforeEmptyBodyConstructor(String s) {}
+
+    protected InputWhitespaceBeforeEmptyBodyConstructor(long l) {}
+
+    InputWhitespaceBeforeEmptyBodyConstructor(int x, int y){
+        // comment inside
+    }
+}
