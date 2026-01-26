8772e86eca0f83abf7869dd58c82c0ccb0327bf1
Krish-876 <nikhilradhakrishna876@gmail.com>
2026-01-26 12:36:04 +0530

    Issue #17725: add regression test for whitespace around reserved words

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/InputWhitespaceAroundReservedWords.java
@@ -0,0 +1,35 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="WhitespaceAround">
+      <property name="tokens"
+        value="LITERAL_IF, LITERAL_ELSE, LITERAL_FOR, LITERAL_WHILE"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.whitespace.whitespacearound;
+
+public class InputWhitespaceAroundReservedWords {
+
+    void testIf(int x) {
+        if (x > 0) {
+            x++;
+        }
+        else {
+            x--;
+        }
+    }
+
+    void testFor() {
+        for (int i = 0; i < 1; i++) {
+            System.out.println(i);
+        }
+    }
+
+    void testWhile() {
+        while (true) {
+            break;
+        }
+    }
+}
