692d1eb826dc8db0505612589cb67a0e9d26fd51
Anushree Bondia <anushreebondia@gmail.com>
2026-01-26 15:19:54 +0530

    Issue #18556: added changes

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/Example2.java
@@ -9,5 +9,6 @@
 */
 // xdoc section -- start
 package com.puppycrawl.tools.checkstyle.checks.header.multifileregexpheader;
+/* violation on first line 'Header mismatch, expected line content was...' */
 public class Example2 { }
 // xdoc section -- end
