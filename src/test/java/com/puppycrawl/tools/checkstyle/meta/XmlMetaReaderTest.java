f17ae09315ad311b56aa0c754dc92492a8f296ce
Saad <mohd.saadhaider@gmail.com>
2026-01-30 17:48:18 +0530

    Issue #18064: add ArrayBracketNoWhitespace check

--- src/test/java/com/puppycrawl/tools/checkstyle/meta/XmlMetaReaderTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/meta/XmlMetaReaderTest.java
@@ -40,20 +40,20 @@ public class XmlMetaReaderTest extends AbstractPathTestSupport {
 
     @Test
     public void test() {
-        assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny()).hasSize(213);
+        assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny()).hasSize(214);
     }
 
     @Test
     public void testDuplicatePackage() {
         assertThat(XmlMetaReader
                     .readAllModulesIncludingThirdPartyIfAny("com.puppycrawl.tools.checkstyle.meta"))
-                .hasSize(213);
+                .hasSize(214);
     }
 
     @Test
     public void testBadPackage() {
         assertThat(XmlMetaReader.readAllModulesIncludingThirdPartyIfAny("DOES.NOT.EXIST"))
-                .hasSize(213);
+                .hasSize(214);
     }
 
     @Test
