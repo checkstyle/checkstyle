d84bc45c9c18ca495eaa6f2e3aa90b3d1fad5e5a
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-01-31 05:46:26 +0500

    Issue #18556: updating multifileregexpheader and adding external xml in check

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/Example3.java
@@ -1,15 +1,8 @@
 /*xml
-<module name="Checker">
-  <module name="MultiFileRegexpHeader">
-    <property
-      name="headerFiles" value="${config.folder}/java.header,
-      ${config.folder}/apache.header"/>
-  </module>
-</module>
 */
 // xdoc section -- start
 package com.puppycrawl.tools.checkstyle.checks.header.multifileregexpheader;
 /* violation on first line 'Header mismatch, expected line content was' */
 public class Example3 { }
 // xdoc section -- end
-// violation 14 lines above 'Header mismatch, expected line content was'
+// violation 7 lines above 'Header mismatch, expected line content was'
