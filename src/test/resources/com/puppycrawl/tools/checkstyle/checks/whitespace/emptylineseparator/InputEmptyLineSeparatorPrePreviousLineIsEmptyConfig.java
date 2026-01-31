dde21c62bffc40003130cdc20e4648546392acb7
Praveen Kumar <praveen729483@gmail.com>
2026-02-01 02:02:28 +0530

    Issue #18437: Added 'ENUM_CONSTANT_DEF' in EmptyLineSeparator Check

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorPrePreviousLineIsEmptyConfig.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorPrePreviousLineIsEmptyConfig.java
@@ -4,8 +4,8 @@ allowNoEmptyLineBetweenFields = (default)false
 allowMultipleEmptyLines = false
 allowMultipleEmptyLinesInsideClassMembers = (default)true
 tokens = (default)PACKAGE_DEF, IMPORT, STATIC_IMPORT, MODULE_IMPORT, CLASS_DEF, \
-         INTERFACE_DEF, ENUM_DEF, STATIC_INIT, INSTANCE_INIT, METHOD_DEF, \
-         CTOR_DEF, VARIABLE_DEF, RECORD_DEF, COMPACT_CTOR_DEF
+         INTERFACE_DEF, ENUM_DEF, ENUM_CONSTANT_DEF, STATIC_INIT, INSTANCE_INIT, \
+         METHOD_DEF, CTOR_DEF, VARIABLE_DEF, RECORD_DEF, COMPACT_CTOR_DEF
 
 
 */
