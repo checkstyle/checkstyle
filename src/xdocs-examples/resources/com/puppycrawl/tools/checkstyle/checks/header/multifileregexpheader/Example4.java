cbdb4ca9917ff357a5d7b49a10cf258a283f0bb4
Anushree Bondia <anushreebondia@gmail.com>
2026-02-01 10:24:58 +0530

    Issue #18809: removed xdocs section markers

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/multifileregexpheader/Example4.java
@@ -1,7 +1,3 @@
-/*xml
-*/
-// xdoc section -- start
+// Any header works here
 package com.puppycrawl.tools.checkstyle.checks.header.multifileregexpheader;
-// ok, as regex value matches all lines
 public class Example4 { }
-// xdoc section -- end
