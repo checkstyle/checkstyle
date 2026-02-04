7950e3aafe40f08f50f3343a7e00854a6357e005
roshan <rmahananda51@gmail.com>
2026-02-04 09:34:23 +0530

    Issue #18660: fix crash in EmptyLineSeparatorCheck on files with few lines

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorIssue18660b.java
@@ -0,0 +1,7 @@
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator;
+
+public class InputEmptyLineSeparatorIssue18660b {
+    int x = 1;
+
+    void method1() { } void display() { }
+}
