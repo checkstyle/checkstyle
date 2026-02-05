092ba9545cc21cde2dafd8dc11172f177db39057
Kairav Mittal <kairavmittal@gmail.com>
2026-02-06 03:53:05 +0530

    Issue #16100: adding basic md support

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/javaparser/InputJavaParserMarkdownIndented.java
@@ -0,0 +1,10 @@
+package com.puppycrawl.tools.checkstyle.javaparser;
+
+class InputJavaParserMarkdownIndented {
+    /// Indented markdown comment
+    void method1() {}
+
+    /// More indented comment
+        /// another line
+    void method2() {}
+}
