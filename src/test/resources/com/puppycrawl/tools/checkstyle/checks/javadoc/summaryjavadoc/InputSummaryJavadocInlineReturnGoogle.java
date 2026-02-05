98a67abe2849135da00aec8cfcc0167f6e2eeb1b
Anushree Bondia <anushreebondia@gmail.com>
2026-02-05 20:37:23 +0530

    Issue #18842: corrected google_checks.xml violation

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc/InputSummaryJavadocInlineReturnGoogle.java
@@ -0,0 +1,36 @@
+/*
+SummaryJavadoc
+violateExecutionOnNonTightHtml = (default)false
+forbiddenSummaryFragments = ^@return the *|^This method returns |^A \
+                            [{]@code [a-zA-Z0-9]+[}]( is a )|^[a-z]
+period = (default).
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.summaryjavadoc;
+
+public class InputSummaryJavadocInlineReturnGoogle {
+    /** {@return the customer ID} */
+    int customerId() {
+        return 0;
+    }
+
+    /** {@return the nothing} */
+    int returnNothing() {
+        return 0;
+    }
+
+    /** {@return This method returns something} */ // violation 'Forbidden summary fragment.'
+    int returnSomething() {
+        return 0;
+    }
+
+    /** {@return a customer object} */
+    Object getCustomer() {
+        return null;
+    }
+
+    /** some javadoc. */ // violation 'Forbidden summary fragment.'
+    void someMethod() {
+    }
+}
