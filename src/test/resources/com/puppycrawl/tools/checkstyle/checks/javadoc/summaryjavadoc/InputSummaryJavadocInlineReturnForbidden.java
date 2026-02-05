727bd34814eb1ad4400c45c3110b909e6a3d78eb
Anushree Bondia <anushreebondia@gmail.com>
2026-02-05 20:37:23 +0530

    Issue #18842: corrected google_checks.xml violation

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc/InputSummaryJavadocInlineReturnForbidden.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc/InputSummaryJavadocInlineReturnForbidden.java
@@ -10,21 +10,18 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc.summaryjavadoc;
 
 public class InputSummaryJavadocInlineReturnForbidden {
     /**
-     // violation below 'Forbidden summary fragment.'
      * {@return the nothing}
      */
     int returnNothing() {
         return 0;
     }
     /**
-     // violation below 'Forbidden summary fragment.'
      * {@return This method returns something}
      */
     int returnSomething() {
         return 0;
     }
     /**
-     // violation below 'Forbidden summary fragment.'
      * {@return This method returns something.}
      */
     int returnSomethingElse() {
