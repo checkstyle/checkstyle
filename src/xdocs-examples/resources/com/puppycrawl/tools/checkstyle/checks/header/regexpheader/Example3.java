52d4e387f28398889f0338823dbb2fb6989347d1
Anushree Bondia <anushreebondia@gmail.com>
2026-02-06 10:32:26 +0530

    Issue #18868: added header for regexpheader check

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/regexpheader/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/regexpheader/Example3.java
@@ -1,16 +1,7 @@
-/*xml
-<module name="Checker">
-  <module name="RegexpHeader">
-    <property
-      name="header"
-      value="^// Copyright \(C\) (\d\d\d\d -)? 2004 MyCompany$
-      \n^// All rights reserved$"/>
-  </module>
-</module>
-*/
-// xdoc section -- start
-package com.puppycrawl.tools.checkstyle.checks.header.regexpheader;
 /* violation on first line 'Line does not match expected header line' */
-public class Example3 { }
-// xdoc section -- end
-// violation 15 lines above 'Line does not match expected header line'
+// because the file header format is incorrect
+package com.puppycrawl.tools.checkstyle.checks.header.regexpheader;
+
+public class Example3 {
+    // Some code
+}
