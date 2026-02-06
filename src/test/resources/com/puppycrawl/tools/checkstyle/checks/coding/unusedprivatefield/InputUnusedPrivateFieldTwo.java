156ea7886658535b658ceff367b1fb708bf6a978
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-02-06 12:28:05 +0500

    Issue #12596: Add new module unusedprivatefield

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedprivatefield/InputUnusedPrivateFieldTwo.java
@@ -0,0 +1,16 @@
+/*
+UnusedPrivateField
+checkUnusedPrivateField = (default)true
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.unusedprivatefield;
+
+public class InputUnusedPrivateFieldTwo {
+
+    private int c;
+
+    public int getC() {
+        return c;
+    }
+}
