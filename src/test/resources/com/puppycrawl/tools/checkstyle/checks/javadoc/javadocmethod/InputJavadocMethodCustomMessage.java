1f9a3d14bc7d3279828033ad56181297dab4b583
Adam Gamil <adammgamil35@gmail.com>
2026-02-06 15:13:12 +0200

    Issue #16807: update default properties for JavadocMethod

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodCustomMessage.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodCustomMessage.java
@@ -5,6 +5,7 @@ validateThrows = (default)false
 accessModifiers = (default)public, protected, package, private
 allowMissingParamTags = (default)false
 allowMissingReturnTag = (default)false
+allowInlineReturn = (default)false
 tokens = (default)METHOD_DEF, CTOR_DEF, ANNOTATION_FIELD_DEF, COMPACT_CTOR_DEF
 message.javadoc.return.expected = @return tag should be present and have description :)
 message.javadoc.expectedTag = Expected {0} tag for ''{1}'' :)
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/Example8.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/Example8.java
@@ -3,6 +3,16 @@
   <module name="TreeWalker">
     <module name="JavadocMethod">
         <property name="allowInlineReturn" value="true"/>
+        <property name="validateThrows" value="false" default="true"/>
+        <property name="allowedAnnotations" value="Override" default="true"/>
+        <property name="accessModifiers"
+                value="public, protected, package, private"
+                default="true"/>
+        <property name="allowMissingParamTags" value="false" default="true"/>
+        <property name="allowMissingReturnTag" value="false" default="true"/>
+        <property name="tokens"
+                value="METHOD_DEF, CTOR_DEF, ANNOTATION_FIELD_DEF, COMPACT_CTOR_DEF"
+                default="true"/>
     </module>
   </module>
 </module>
