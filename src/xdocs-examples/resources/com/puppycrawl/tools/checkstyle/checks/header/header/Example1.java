7d9a43d8ef5f6ea6b4b36b4543c135a555e9a8ca
Anushree Bondia <anushreebondia@gmail.com>
2026-02-06 13:10:17 +0530

    Issue #18868: added headers for Header

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/header/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/header/Example1.java
@@ -1,10 +1,4 @@
-/*xml
-<module name="Checker">
-  <module name="Header"/>
-</module>
-*/
-// xdoc section -- start
+// Any header works here
 package com.puppycrawl.tools.checkstyle.checks.header.header;
 // ok, as by default there is not specific header defined to validate it presence
 public class Example1 { }
-// xdoc section -- end
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/header/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/header/header/Example3.java
@@ -1,14 +1,3 @@
-/*xml
-<module name="Checker">
-  <module name="Header">
-    <property name="header"
-      value="// Copyright (C) 2004 MyCompany\n// All rights reserved"/>
-  </module>
-</module>
-*/
-// xdoc section -- start
 package com.puppycrawl.tools.checkstyle.checks.header.header;
-/* violation on first line 'Line does not match expected header' */
+// violation on first line 'Line does not match expected header'
 public class Example3 { }
-// xdoc section -- end
-// violation 13 lines above 'Line does not match expected header'
