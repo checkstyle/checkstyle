a8e4dd80d06caa5c489ebdbe121b16f730e0c14a
Anushree Bondia <anushreebondia@gmail.com>
2026-02-10 11:45:54 +0530

    Issue #18590: EmptyStatement check

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/emptystatement/InputEmptyStatementInterfaceBody.java
@@ -0,0 +1,11 @@
+/*
+EmptyStatement
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.emptystatement;
+
+public class InputEmptyStatementInterfaceBody {
+    ; // violation 'Empty statement'
+}
