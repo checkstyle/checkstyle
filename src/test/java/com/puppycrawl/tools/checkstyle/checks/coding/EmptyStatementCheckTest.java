c208198c14a69f38e7d81dd2b02ab7862e6c6c99
Anushree Bondia <anushreebondia@gmail.com>
2026-02-10 11:45:54 +0530

    Issue #18590: EmptyStatement check

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/EmptyStatementCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/EmptyStatementCheckTest.java
@@ -22,9 +22,16 @@ package com.puppycrawl.tools.checkstyle.checks.coding;
 import static com.google.common.truth.Truth.assertWithMessage;
 import static com.puppycrawl.tools.checkstyle.checks.coding.EmptyStatementCheck.MSG_KEY;
 
+import java.io.File;
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
 
 import com.puppycrawl.tools.checkstyle.AbstractModuleTestSupport;
+import com.puppycrawl.tools.checkstyle.DetailAstImpl;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 public class EmptyStatementCheckTest
     extends AbstractModuleTestSupport {
@@ -35,29 +42,193 @@ public class EmptyStatementCheckTest
     }
 
     @Test
-    public void testEmptyStatements()
-            throws Exception {
+    public void testEmptyStatements() throws Exception {
         final String[] expected = {
-            "18:7: " + getCheckMessage(MSG_KEY),
-            "23:7: " + getCheckMessage(MSG_KEY),
-            "28:19: " + getCheckMessage(MSG_KEY),
-            "32:10: " + getCheckMessage(MSG_KEY),
-            "35:16: " + getCheckMessage(MSG_KEY),
-            "39:10: " + getCheckMessage(MSG_KEY),
-            "49:10: " + getCheckMessage(MSG_KEY),
-            "55:13: " + getCheckMessage(MSG_KEY),
-            "57:13: " + getCheckMessage(MSG_KEY),
-            "60:19: " + getCheckMessage(MSG_KEY),
-            "64:10: " + getCheckMessage(MSG_KEY),
-            "67:9: " + getCheckMessage(MSG_KEY),
-            "72:10: " + getCheckMessage(MSG_KEY),
-            "78:10: " + getCheckMessage(MSG_KEY),
-            "82:10: " + getCheckMessage(MSG_KEY),
-            "86:10: " + getCheckMessage(MSG_KEY),
+            "16:4: " + getCheckMessage(MSG_KEY),
+            "17:4: " + getCheckMessage(MSG_KEY),
+            "21:7: " + getCheckMessage(MSG_KEY),
+            "28:7: " + getCheckMessage(MSG_KEY),
+            "33:19: " + getCheckMessage(MSG_KEY),
+            "37:10: " + getCheckMessage(MSG_KEY),
+            "40:16: " + getCheckMessage(MSG_KEY),
+            "44:10: " + getCheckMessage(MSG_KEY),
+            "54:10: " + getCheckMessage(MSG_KEY),
+            "60:13: " + getCheckMessage(MSG_KEY),
+            "62:13: " + getCheckMessage(MSG_KEY),
+            "65:19: " + getCheckMessage(MSG_KEY),
+            "69:10: " + getCheckMessage(MSG_KEY),
+            "72:9: " + getCheckMessage(MSG_KEY),
+            "77:10: " + getCheckMessage(MSG_KEY),
+            "83:10: " + getCheckMessage(MSG_KEY),
+            "87:10: " + getCheckMessage(MSG_KEY),
+            "91:10: " + getCheckMessage(MSG_KEY),
+        };
+        verifyWithInlineConfigParser(
+            getPath("InputEmptyStatement.java"), expected);
+    }
+
+    @Test
+    public void testEnumSemicolonIsNotEmptyStatement() throws Exception {
+        final String[] expected = {};
+
+        verifyWithInlineConfigParser(
+            getPath("InputEmptyStatementEnumSemicolon.java"), expected);
+    }
+
+    @Test
+    public void testNonTypeSemicolonIsNotEmptyStatement() throws Exception {
+        final String[] expected = {};
+
+        verifyWithInlineConfigParser(
+            getPath("InputEmptyStatementNonTypeSemicolon.java"), expected);
+    }
+
+    @Test
+    public void testCompilationUnitSemicolonIsEmptyStatement() throws Exception {
+        final String[] expected = {
+            "9:1: " + getCheckMessage(MSG_KEY),
         };
 
         verifyWithInlineConfigParser(
-                getPath("InputEmptyStatement.java"), expected);
+            getPath("InputEmptyStatementBeforeClass.java"), expected);
+    }
+
+    @Test
+    public void testSemicolonAfterTypeMemberIsNotEmptyStatement() throws Exception {
+        final String[] expected = {};
+
+        verifyWithInlineConfigParser(
+            getPath("InputEmptyStatementAfterTypeMember.java"), expected);
+    }
+
+    @Test
+    public void testSemicolonAfterDefaultMethodIsNotEmptyStatement() throws Exception {
+        final String[] expected = {};
+
+        verifyWithInlineConfigParser(
+            getPath("InputEmptyStatementAfterDefaultMethod.java"), expected);
+    }
+
+    @Test
+    public void testSemicolonAfterInnerTypeIsNotEmptyStatement() throws Exception {
+        final String[] expected = {};
+
+        verifyWithInlineConfigParser(
+            getPath("InputEmptyStatementAfterInnerType.java"), expected);
+    }
+
+    @Test
+    public void testSemicolonInCompilationUnitVisitToken() throws Exception {
+        final EmptyStatementCheck check = newCheckWithFileContents();
+        final DetailAstImpl compilationUnit = ast(TokenTypes.COMPILATION_UNIT);
+        final DetailAstImpl semicolon = ast(TokenTypes.SEMI);
+        compilationUnit.addChild(semicolon);
+
+        assertWithMessage("Expected SEMI token")
+            .that(semicolon.getType())
+            .isEqualTo(TokenTypes.SEMI);
+        check.visitToken(semicolon);
+    }
+
+    @Test
+    public void testSemicolonOutsideCompilationUnitVisitToken() throws Exception {
+        final EmptyStatementCheck check = newCheckWithFileContents();
+        final DetailAstImpl methodDef = ast(TokenTypes.METHOD_DEF);
+        final DetailAstImpl semicolon = ast(TokenTypes.SEMI);
+        methodDef.addChild(semicolon);
+
+        assertWithMessage("Expected method to be parent")
+            .that(semicolon.getParent().getType())
+            .isEqualTo(TokenTypes.METHOD_DEF);
+        check.visitToken(semicolon);
+    }
+
+    @Test
+    public void testStandaloneSemicolonInTypeBodyVisitToken() throws Exception {
+        final EmptyStatementCheck check = newCheckWithFileContents();
+        final DetailAstImpl classDef = ast(TokenTypes.CLASS_DEF);
+        final DetailAstImpl objBlock = ast(TokenTypes.OBJBLOCK);
+        classDef.addChild(objBlock);
+        final DetailAstImpl semicolon = ast(TokenTypes.SEMI);
+        objBlock.addChild(semicolon);
+
+        assertWithMessage("Expected SEMI in class body")
+            .that(semicolon.getParent().getType())
+            .isEqualTo(TokenTypes.OBJBLOCK);
+        check.visitToken(semicolon);
+    }
+
+    @Test
+    public void testEnumSemicolonInTypeBodyVisitToken() throws Exception {
+        final EmptyStatementCheck check = newCheckWithFileContents();
+        final DetailAstImpl enumDef = ast(TokenTypes.ENUM_DEF);
+        final DetailAstImpl objBlock = ast(TokenTypes.OBJBLOCK);
+        enumDef.addChild(objBlock);
+        final DetailAstImpl semicolon = ast(TokenTypes.SEMI);
+        objBlock.addChild(semicolon);
+
+        assertWithMessage("Expected SEMI in enum body")
+            .that(semicolon.getParent().getType())
+            .isEqualTo(TokenTypes.OBJBLOCK);
+        check.visitToken(semicolon);
+    }
+
+    @Test
+    public void testSemicolonAfterTypeMemberVisitToken() throws Exception {
+        final EmptyStatementCheck check = newCheckWithFileContents();
+        final DetailAstImpl objBlock = ast(TokenTypes.OBJBLOCK);
+        final DetailAstImpl methodDef = ast(TokenTypes.METHOD_DEF);
+        objBlock.addChild(methodDef);
+        final DetailAstImpl semicolon = ast(TokenTypes.SEMI);
+        objBlock.addChild(semicolon);
+
+        assertWithMessage("Expected previous sibling to be method")
+            .that(semicolon.getPreviousSibling().getType())
+            .isEqualTo(TokenTypes.METHOD_DEF);
+        check.visitToken(semicolon);
+    }
+
+    @Test
+    public void testEmptyStatUnderMethodDefVisitToken() throws Exception {
+        final EmptyStatementCheck check = newCheckWithFileContents();
+        final DetailAstImpl methodDef = ast(TokenTypes.METHOD_DEF);
+        final DetailAstImpl emptyStat = ast(TokenTypes.EMPTY_STAT);
+        methodDef.addChild(emptyStat);
+
+        assertWithMessage("Expected EMPTY_STAT token")
+            .that(emptyStat.getType())
+            .isEqualTo(TokenTypes.EMPTY_STAT);
+        check.visitToken(emptyStat);
+    }
+
+    @Test
+    public void testEmptyStatUnderStatementListVisitToken() throws Exception {
+        final EmptyStatementCheck check = newCheckWithFileContents();
+        final DetailAstImpl slist = ast(TokenTypes.SLIST);
+        final DetailAstImpl emptyStat = ast(TokenTypes.EMPTY_STAT);
+        slist.addChild(emptyStat);
+
+        assertWithMessage("Expected SLIST parent")
+            .that(emptyStat.getParent().getType())
+            .isEqualTo(TokenTypes.SLIST);
+        check.visitToken(emptyStat);
+    }
+
+    private static DetailAstImpl ast(int tokenType) {
+        final DetailAstImpl ast = new DetailAstImpl();
+        ast.setType(tokenType);
+        ast.setLineNo(1);
+        ast.setColumnNo(0);
+        return ast;
+    }
+
+    private static EmptyStatementCheck newCheckWithFileContents() throws Exception {
+        final EmptyStatementCheck check = new EmptyStatementCheck();
+        check.configure(createModuleConfig(EmptyStatementCheck.class));
+        final FileText text = new FileText(new File("EmptyStatementCheckTest"),
+            Collections.singletonList(";"));
+        check.setFileContents(new FileContents(text));
+        return check;
     }
 
     @Test
