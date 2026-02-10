0e8fa15460d1354287e883e17af2e9a23fb0c78a
Anushree Bondia <anushreebondia@gmail.com>
2026-02-10 11:45:54 +0530

    Issue #18590: EmptyStatement check

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/EmptyStatementCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/coding/EmptyStatementCheck.java
@@ -23,6 +23,7 @@ import com.puppycrawl.tools.checkstyle.StatelessCheck;
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.ScopeUtil;
 
 /**
  * <div>
@@ -53,12 +54,22 @@ public class EmptyStatementCheck extends AbstractCheck {
 
     @Override
     public int[] getRequiredTokens() {
-        return new int[] {TokenTypes.EMPTY_STAT};
+        return new int[] {TokenTypes.EMPTY_STAT, TokenTypes.SEMI};
     }
 
     @Override
     public void visitToken(DetailAST ast) {
-        log(ast, MSG_KEY);
+        if (ast.getType() == TokenTypes.EMPTY_STAT) {
+            log(ast, MSG_KEY);
+        }
+        else if (isTypeBodySemicolon(ast)) {
+            log(ast, MSG_KEY);
+        }
+    }
+
+    private static boolean isTypeBodySemicolon(DetailAST ast) {
+        return ast.getParent().getType() == TokenTypes.OBJBLOCK
+            && !ScopeUtil.isInEnumBlock(ast);
     }
 
 }
