6e55d969e9c713a03bc9f271cc1fa45406212a7c
atharv <atharv55215@gmail.com>
2026-02-11 22:45:57 +0530

    infra: disable Qodana PR mode and exclude specific inspections

--- src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocCommentsTokenTypes.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/api/JavadocCommentsTokenTypes.java
@@ -27,6 +27,8 @@ import com.puppycrawl.tools.checkstyle.grammar.javadoc.JavadocCommentsLexer;
  *
  * @see <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/unix/javadoc.html">
  *     javadoc - The Java API Documentation Generator</a>
+ * @noinspection JavadocDeclaration
+ * @noinspectionreason JavadocDeclaration - Javadoc is intentional
  */
 @SuppressWarnings({"InvalidInlineTag", "UnrecognisedJavadocTag"})
 public final class JavadocCommentsTokenTypes {
