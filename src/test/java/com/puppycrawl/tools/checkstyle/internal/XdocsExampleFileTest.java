48bd7f85129606ae5c113df523de5b74bc2385d2
Krish-876 <nikhilradhakrishna876@gmail.com>
2026-02-13 17:08:09 +0530

    Issue #17449: update javadocstyle endOfSentenceFormat documentation and examples

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
@@ -60,6 +60,8 @@ public class XdocsExampleFileTest {
     private static final Map<String, Set<String>> SUPPRESSED_PROPERTIES_BY_CHECK = Map.ofEntries(
             Map.entry("MissingJavadocTypeCheck", Set.of("skipAnnotations")),
             Map.entry("JavadocStyleCheck", Set.of("endOfSentenceFormat")),
+            Map.entry("ConstantNameCheck", Set.of("applyToPackage", "applyToPrivate")),
+            Map.entry("WhitespaceAroundCheck", Set.of("allowEmptySwitchBlockStatements")),
             Map.entry("SuppressWarningsHolder", Set.of("aliasList")),
             Map.entry("IndentationCheck", Set.of(
                     "basicOffset",
