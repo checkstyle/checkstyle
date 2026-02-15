e778def8db557aade71af5875b32523901809f32
MostafaAbdulazziz <es-moustafa.abdelazziz2027@alexu.edu.eg>
2026-02-15 22:34:18 +0200

    Issue #15456: Define violation messages for SuperFinalizeCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -237,7 +237,6 @@ public final class InlineConfigParser {
      */
     private static final Set<String> SUPPRESSED_CHECKS = Set.of(
             "com.puppycrawl.tools.checkstyle.checks.coding.MultipleStringLiteralsCheck",
-            "com.puppycrawl.tools.checkstyle.checks.coding.SuperFinalizeCheck",
             "com.puppycrawl.tools.checkstyle.checks.coding"
                     + ".UnnecessarySemicolonAfterTypeMemberDeclarationCheck",
             "com.puppycrawl.tools.checkstyle.checks.design.DesignForExtensionCheck",
