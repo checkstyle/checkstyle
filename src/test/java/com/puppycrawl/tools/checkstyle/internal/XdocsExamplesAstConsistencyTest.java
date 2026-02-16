c5e11f2466ddcc5c6dc947ad400b1094943e7187
Adam Gamil <adammgamil35@gmail.com>
2026-02-16 20:53:45 +0200

    Issue #17449: Add XDocs example for WhitespaceAroundCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
@@ -266,6 +266,7 @@ public class XdocsExamplesAstConsistencyTest {
             "checks/whitespace/whitespacearound/Example7",
             "checks/whitespace/whitespacearound/Example8",
             "checks/whitespace/whitespacearound/Example9",
+            "checks/whitespace/whitespacearound/Example11",
             "filters/suppressionfilter/Example2",
             "filters/suppressionfilter/Example3",
             "filters/suppressionfilter/Example4",
