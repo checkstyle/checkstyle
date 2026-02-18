53408ae87a659945afc5d2ba487474bc2c163757
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-02-18 15:15:23 +0500

    Issue #18227: Adding TextBlockGoogleStyleFormattingCheck inside string

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/textblockgooglestyleformatting/InputTextBlockGoogleStyleFormatting.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/textblockgooglestyleformatting/InputTextBlockGoogleStyleFormatting.java
@@ -11,7 +11,7 @@ public class InputTextBlockGoogleStyleFormatting {
     public static String textFun() {
         // violation below 'Opening quotes (""") of text-block must be on the new line'
         final String simpleScript = """
-            s
+                                    s
         """; // violation 'Text-block quotes are not vertically aligned'
 
         final String simpleScript1 =
@@ -20,7 +20,7 @@ public class InputTextBlockGoogleStyleFormatting {
             """;
 
         // violation 2 lines below 'Opening quotes (""") of text-block must be on the new line'
-        final String simpleScript2 = simpleScript +
+        final String simpleScript2 = simpleScript + // violation 2 lines below 'Each line of text in the text block must be indented at least as much as the opening and closing quotes'
                 simpleScript1 + """
                 this is simple script
                 """; // violation 'Text-block quotes are not vertically aligned'
@@ -32,22 +32,22 @@ public class InputTextBlockGoogleStyleFormatting {
             """;
 
         // violation 2 lines below 'Opening quotes (""") of text-block must be on the new line'
-        final String simpleScript4 = simpleScript +
+        final String simpleScript4 = simpleScript + // violation 2 lines below 'Each line of text in the text block must be indented at least as much as the opening and closing quotes'
             simpleScript3.endsWith("""
                 this is simple
                 """); // violation 'Text-block quotes are not vertically aligned'
 
         // violation below 'Opening quotes (""") of text-block must be on the new line'
         getData("""
-            Hello,
-            This is a multi-line message.
+                Hello,
+                This is a multi-line message.
             """); // violation 'Text-block quotes are not vertically aligned'
 
         // violation below 'Opening quotes (""") of text-block must be on the new line'
         return """
             this is sample text
-            """; // violation 'Text-block quotes are not vertically aligned'
-    }
+            """; // violation above 'Each line of text in the text block must be indented at least as much as the opening and closing quotes'
+    }  // violation above 'Text-block quotes are not vertically aligned'
 
     public void textFun2() {
         final String simpleScript2 =
@@ -95,7 +95,7 @@ public class InputTextBlockGoogleStyleFormatting {
         // violation 2 lines below 'Opening quotes (""") of text-block must be on the new line'
         // violation 3 lines below 'Text-block quotes are not vertically aligned'
         getData("""
-            first string
+                      first string
             """ + """
             some String
             """,
@@ -103,10 +103,15 @@ public class InputTextBlockGoogleStyleFormatting {
             second string
             """
             // violation 6 lines above'Opening quotes (""") of text-block must be on the new line'
-            // violation 5 lines above 'Text-block quotes are not vertically aligned'
+            // violation 6 lines above 'Each line of text in the text block must be indented at least as much as the opening and closing quotes'
+            // violation 6 lines above 'Text-block quotes are not vertically aligned'
         );
+        final String coverageCase =
+    """
+    
+    some text
+    """;
     }
-
     public static void getData(String data) {}
     public static void getData(String data, String data2) {}
     public static void getData(String data, int length) {}
