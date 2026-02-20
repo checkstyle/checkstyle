2e3dfa9a0ab3f48d47e6d38795778fa2af57294d
avadhutmali <mali.avadhut7@gmail.com>
2026-02-21 03:37:54 +0530

    Issue #18435: Fix RightCurly example AST consistency

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/Example4.java
@@ -13,34 +13,43 @@ package com.puppycrawl.tools.checkstyle.checks.blocks.rightcurly;
 
 // xdoc section -- start
 public class Example4 {
-
   public void test() {
-
-    boolean foo = false;
-    if (foo) {
+    boolean f = false;
+    if (f) {
+      bar();
+    } // ok, 'should be on the same line'
+    else {
+      bar();
+    }
+    if (f) {
       bar();
     } else { // violation, 'should be alone on a line.'
       bar();
     }
-
-    if (foo) {
-      bar();
-    }
-    else {
-      bar();
-    }
-
+    if (f) { bar(); } int i = 0; // violation, 'should be alone on a line.'
     try {
       bar();
-    } catch (Exception e) { // ok because config did not set token LITERAL_TRY
+    } // ok, 'should be on the same line'
+    catch (Exception e) {
       bar();
     }
-
   }
-
   private void bar() {
   }
-
-  public void violate() { bar(); } // ok , because singleline
+  public void testSingleLine() { bar(); } // ok, 'should be alone on a line.'
+  public void violate() { Object b = "b"; } // ok, 'should be alone on a line.'
+  public void method0() {
+    int mode = 0;
+    int x;
+    switch (mode) {
+      case 1: int y = 1; break;
+      case 2: {x = 1;} // ok, 'should be alone on a line.'
+      case 3: int z = 0; {break;}
+      default: x = 0;
+    }
+    switch (mode) {
+      case 1: x = 1; break;
+      default: x = 0; } // ok, 'should be alone on a line.'
+  }
 }
 // xdoc section -- end
