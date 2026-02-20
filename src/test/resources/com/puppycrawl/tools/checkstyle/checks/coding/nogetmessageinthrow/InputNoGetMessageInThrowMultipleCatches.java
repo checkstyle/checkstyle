d25188ad08e2e941aa569b35a56604dc7ab0b344
Youssef Alaa <youssef.alaa.ahmed.haredy@gmail.com>
2026-02-20 21:47:41 +0200

    Issue #18971: New check to not put caught exception message to exception that we are going to throw

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/nogetmessageinthrow/InputNoGetMessageInThrowMultipleCatches.java
@@ -0,0 +1,43 @@
+/*
+NoGetMessageInThrow
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.nogetmessageinthrow;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+// xdoc section -- start
+class InputNoGetMessageInThrowMultipleCatches {
+
+    void method1() throws IOException {
+        // Multiple catch blocks
+        try {
+            throw new IOException();
+        } catch (IOException ex) {
+            throw new IOException("IO: " + ex.getMessage());
+                // violation above 'Avoid using '.getMessage()'
+                // in throw statement.
+        } catch (RuntimeException ex) {
+            // Not a violation - different catch block
+            System.out.println(ex);
+        }
+    }
+
+    void method2() throws Exception {
+        // Multiple catch blocks with multi-catch
+        try {
+            if (Math.random() > 0.5) {
+                throw new IOException();
+            }
+            throw new SQLException();
+        } catch (IOException | SQLException ex) {
+            throw new IOException("Error: " + ex.getMessage());
+                // violation above 'Avoid using '.getMessage()'
+                // in throw statement.
+        }
+    }
+}
+// xdoc section -- end
