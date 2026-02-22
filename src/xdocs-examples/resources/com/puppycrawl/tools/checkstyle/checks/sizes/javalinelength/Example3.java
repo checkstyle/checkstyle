1cb8843036de9a09a1289dd062086592a9586bda
daksh <dakshjain737@gmail.com>
2026-02-22 21:50:59 +0530

    Issue #17707: Add new check JavaLineLengthCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/sizes/javalinelength/Example3.java
@@ -0,0 +1,36 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="JavaLineLength">
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.sizes.javalinelength;
+
+// xdoc section - start
+public class Example3 {
+
+  String s1 = """
+            This string is a really long word that exceeds default line limit but will not raise violation.
+            """;
+
+  String s2 = "This string is a really long word that exceeds default line limit but will raise violation";
+  // violation above 'Line is longer than 100 characters (found 107).'
+
+  // violation below 'Line is longer than 100 characters (found 113).'
+  /**
+   * This javadoc is really long long long long long long long long long long long long long long long long long.
+   *
+   * @param s1 string 1
+   * @param s2 string 2
+   * @param s3 string 3
+   */
+  public void methodWhoseCharactersExceed80Characters (String s1, String s2, String s3, String s5, String s6) {
+    // violation above 'Line is longer than 100 characters (found 111).'
+    String s4 = """
+                This string is greater than 80 characters but no violations will be raised.
+                """;
+  }
+}
+// xdoc section - end
