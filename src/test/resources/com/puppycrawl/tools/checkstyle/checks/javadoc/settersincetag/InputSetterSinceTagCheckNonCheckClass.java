cd003a395594edb360497e1311f704b4e031eafd
daksh <dakshjain737@gmail.com>
2026-02-22 12:29:39 +0530

    Issue #13491: Simplify setterHaveSinceTag

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/settersincetag/InputSetterSinceTagCheckNonCheckClass.java
@@ -0,0 +1,21 @@
+/*
+SetterSinceTag
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.settersincetag;
+
+public class InputSetterSinceTagCheckNonCheckClass {
+
+    private boolean enabled;
+
+    /**
+     * Sets the enabled flag.
+     *
+     * @param enabled the value to set
+     */
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
