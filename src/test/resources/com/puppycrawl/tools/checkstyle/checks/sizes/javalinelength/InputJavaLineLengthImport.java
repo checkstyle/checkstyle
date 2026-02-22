2947010334db34e99c8cb19263c2bfa1330bb13b
daksh <dakshjain737@gmail.com>
2026-02-22 21:50:59 +0530

    Issue #17707: Add new check JavaLineLengthCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/javalinelength/InputJavaLineLengthImport.java
@@ -0,0 +1,16 @@
+/*
+JavaLineLength
+max = (default)100
+tokens = IMPORT
+
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.sizes.javalinelength;
+
+import      java.                util.                                                           ArrayList;
+import java.util.                                                                                   List;
+
+public class InputJavaLineLengthImport {
+
+    List<Integer> list = new ArrayList<>();
+}
