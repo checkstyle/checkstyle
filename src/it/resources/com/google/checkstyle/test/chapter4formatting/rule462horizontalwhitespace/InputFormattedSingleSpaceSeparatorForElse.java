284cb71a7fed884b9516ee978199333445fe716c
Youssef Alaa <youssef.alaa.ahmed.haredy@gmail.com>
2026-02-25 06:23:57 +0200

    Issue #17725: Detected multiple spaces around reserved words not detected

---
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule462horizontalwhitespace/InputFormattedSingleSpaceSeparatorForElse.java
@@ -0,0 +1,18 @@
+package com.google.checkstyle.test.chapter4formatting.rule462horizontalwhitespace;
+
+/** Test input for SingleSpaceSeparator check. */
+public class InputFormattedSingleSpaceSeparatorForElse {
+  void testIf(int x) {
+    if (x > 0) {
+      System.out.println("Positive");
+    } else {
+      return;
+    }
+  }
+
+  void testFor() {
+    for (int cur = 0; cur < 5; cur++) {
+      System.out.println(cur);
+    }
+  }
+}
