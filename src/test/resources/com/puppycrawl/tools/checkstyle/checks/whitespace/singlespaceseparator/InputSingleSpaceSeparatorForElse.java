c979a74293b2773424c4df6652e7b67a0621f7d2
Youssef Alaa <youssef.alaa.ahmed.haredy@gmail.com>
2026-02-25 06:23:57 +0200

    Issue #17725: Detected multiple spaces around reserved words not detected

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/singlespaceseparator/InputSingleSpaceSeparatorForElse.java
@@ -0,0 +1,28 @@
+/*
+SingleSpaceSeparator
+validateComments = true
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.singlespaceseparator;
+
+public class InputSingleSpaceSeparatorForElse {
+      void testIf(int x) {
+        // violation below 'Use a single space to separate non-whitespace characters'
+        if   (x > 0) {
+          System.out.println("Positive");
+          // violation 2 lines below 'Use a single space to separate non-whitespace characters'
+          // violation below 'Use a single space to separate non-whitespace characters'
+        }       else       {
+          return;
+        }
+      }
+
+      void testFor() {
+        // violation below 'Use a single space to separate non-whitespace characters'
+        for    (int i = 0; i < 5; i++) {
+          System.out.println(i);
+        }
+      }
+}
