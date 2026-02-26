6dd029e52b6816381eb9689a51d1aa4a1dd6afdf
Praveen Kumar <praveen729483@gmail.com>
2026-02-27 00:20:29 +0530

    Issue #18571: New Check BlockCommentEndPositionCheck

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/javadoc/blockcommentendposition/InputXpathBlockCommentEndPositionTwo.java
@@ -0,0 +1,8 @@
+package org.checkstyle.suppressionxpathfilter.javadoc.blockcommentendposition;
+
+public class InputXpathBlockCommentEndPositionTwo {
+
+    /** Text. */ // warn
+    int a = 10;
+
+}
