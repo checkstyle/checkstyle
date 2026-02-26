945a1ea3e0682b7518355c63c3572652c1dba7c6
vivek-0509 <viveksolanki0509@gmail.com>
2026-02-26 15:42:05 +0530

    Issue #14782: Add LITERAL_DEFAULT token support in RightCurlyCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlyDefaultBlocksInSwitchStatementSame.java
@@ -0,0 +1,114 @@
+/*
+RightCurly
+option = SAME
+tokens = LITERAL_DEFAULT
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.blocks.rightcurly;
+
+public class InputRightCurlyDefaultBlocksInSwitchStatementSame {
+    public static void test0() {
+        int mode = 0;
+        switch (mode) {
+            case 1: {
+                int x = 1;
+                break;
+            }
+            default: {
+                int x = 0;
+                break;
+            }
+        }
+    }
+
+    public static void test1() {
+        int mode = 0;
+        switch (mode) {
+            case 1:{
+                int x
+                    = 1;
+                break;
+            }
+            default:{
+                int x
+
+            =0;} case 2 :          // violation '}' at column 16 should have line break before'
+                int x = 0;
+        }
+    }
+
+    public static void test2() {
+        int k = 0;
+        switch (k) {
+            default:{
+                 int x = 1;
+                 break;
+            } case 1:        // violation '}' at column 13 should be alone on a line'
+                 int x = 2;
+                 break;
+        }
+    }
+
+    public static void test3() {
+         int k = 0;
+         switch (k) {
+            default:{
+                 int x = 1;
+                 break;
+            }
+            case 1:
+                 int x = 2;
+                 break;
+         }
+    }
+
+    public static void test4() {
+         int k = 0;
+         switch (k) {
+            case 1:{
+                 int x = 1;
+                 break;
+            }
+            default:{
+                    int x = 2;
+                    break;
+            } }      // violation '}' at column 13 should be alone on a line'
+    }
+
+    public static void test5() {
+        int mode = 0;
+        switch (mode) { case 0: int x = 1; break; default: {x = 5; break;} }
+        // violation above '}' at column 74 should be alone on a line'
+    }
+
+    public static void test6() {
+        int mode = 0;
+        switch (mode) { default: int x = 1;}
+    }
+
+    public static void test7() {
+        int mode = 0;
+        switch (mode) {
+            default: {int x = 1; break;}
+            case 0 : break;
+        }
+    }
+
+    public static void test8() {
+        int mode = 0;
+        switch (mode) {default:{int x = 1;}
+            break; case 0 : break; }
+    }
+
+    public static void test9() {
+        int mode = 0;
+        int x = 0;
+        switch (mode) {
+            case 0:{
+            int y = 1;}
+            default: {x = 1;
+            } case 1:  // violation '}' at column 13 should be alone on a line'
+                x = 5;
+        }
+    }
+}
