a4466fa9f1b52f7ec4298a3d859223994aa04e07
Krish-876 <nikhilradhakrishna876@gmail.com>
2026-02-27 09:43:18 +0530

    Issue #16361: Add testAddException to SarifLoggerTest

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/sariflogger/InputSarifLoggerSingleException.java
@@ -0,0 +1,16 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="RegexpSingleline">
+      <property name="format" value="SINGLE_EXCEPTION_TRIGGER"/>
+      <property name="message" value="found an error"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.sariflogger;
+// violation SINGLE_EXCEPTION_TRIGGER
+public class InputSarifLoggerSingleException {
+
+}
