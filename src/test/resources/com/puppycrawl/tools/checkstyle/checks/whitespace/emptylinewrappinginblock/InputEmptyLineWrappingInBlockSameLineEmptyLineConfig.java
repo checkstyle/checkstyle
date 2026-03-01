f41769276a29ea5ece03db529f299b9013fe18c4
Daniel Gao <danielgao1029@outlook.com>
2026-03-01 19:20:13 +1100

    Issue #5313: Add new check EmptyLineWrappingInBlock

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylinewrappinginblock/InputEmptyLineWrappingInBlockSameLineEmptyLineConfig.java
@@ -0,0 +1,11 @@
+/*
+EmptyLineWrappingInBlock
+tokens = CLASS_DEF
+topSeparator = no_empty_line
+bottomSeparator = (default)empty_line
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylinewrappinginblock;
+
+public class InputEmptyLineWrappingInBlockSameLineEmptyLineConfig { } // violation ''}' must have exactly one empty line before'
