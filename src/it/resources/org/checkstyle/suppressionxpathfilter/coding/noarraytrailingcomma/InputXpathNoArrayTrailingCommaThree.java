b8c1cde156909ec38b95cbd8f29d408999453634
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-03-02 16:20:05 +0500

    Issue #19064: Adding third test in XpathRegressionNoArrayTrailingCommaTest

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/coding/noarraytrailingcomma/InputXpathNoArrayTrailingCommaThree.java
@@ -0,0 +1,6 @@
+package org.checkstyle.suppressionxpathfilter.coding.noarraytrailingcomma;
+
+public class InputXpathNoArrayTrailingCommaThree {
+
+    int[][] t6 = new int[][] {{1, 2}, {3, 3}, {5, 6},}; //warn
+}
