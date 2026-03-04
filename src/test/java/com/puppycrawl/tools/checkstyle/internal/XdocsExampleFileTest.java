4ba23751598a97ec50cb1efc7880d58cd00a1bd2
Ahmed Khairi <ahmedkhairi1122@gmail.com>
2026-03-04 02:15:17 +0200

    Issue #17449: add XDocs example for SuppressWarningsHolder aliasList

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
@@ -58,8 +58,8 @@ public class XdocsExampleFileTest {
     // This list is temporarily suppressed.
     // Until: https://github.com/checkstyle/checkstyle/issues/17449
     private static final Map<String, Set<String>> SUPPRESSED_PROPERTIES_BY_CHECK = Map.ofEntries(
-            Map.entry("JavadocStyleCheck", Set.of("endOfSentenceFormat")),
             Map.entry("SuppressWarningsHolder", Set.of("aliasList")),
+            Map.entry("JavadocStyleCheck", Set.of("endOfSentenceFormat")),
             Map.entry("IndentationCheck", Set.of(
                     "basicOffset",
                     "lineWrappingIndentation",
