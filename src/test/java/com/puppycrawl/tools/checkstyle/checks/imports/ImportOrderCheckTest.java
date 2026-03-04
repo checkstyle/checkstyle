e9711c7f20c0c1232e4a880ac34ce23502f6e51c
vivek-0509 <viveksolanki0509@gmail.com>
2026-03-04 16:50:04 +0530

    Issue #5460: Fix false positive in ImportOrder for separator between static and non-static imports with option under/above

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportOrderCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportOrderCheckTest.java
@@ -896,4 +896,26 @@ public class ImportOrderCheckTest extends AbstractModuleTestSupport {
                 .isTrue();
 
     }
+
+    @Test
+    public void testUnderSeparatedStaticNonStaticBlankLine() throws Exception {
+        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+
+        verifyWithInlineConfigParser(
+                getPath("InputImportOrderUnderSeparatedStaticNonStaticBlankLine.java"), expected);
+    }
+
+    @Test
+    public void testUnderSeparatedSameTypeBlankLine() throws Exception {
+        final String[] expected = {
+            "21:1: " + getCheckMessage(MSG_SEPARATED_IN_GROUP,
+                    "java.util.List"),
+            "25:1: " + getCheckMessage(MSG_SEPARATED_IN_GROUP,
+                    "java.util.Collections.sort"),
+        };
+
+        verifyWithInlineConfigParser(
+                getPath("InputImportOrderUnderSeparatedSameTypeBlankLine.java"), expected);
+    }
+
 }
