c791764cad745823a9429fde4dd91f04c8958df2
vivek-0509 <viveksolanki0509@gmail.com>
2026-03-04 16:50:04 +0530

    Issue #5460: Fix false positive in ImportOrder for separator between static and non-static imports with option under/above

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportOrderCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportOrderCheck.java
@@ -139,9 +139,11 @@ public class ImportOrderCheck
 
     /**
      * Control whether static import groups should be separated by, at least, one blank
-     * line or comment and aren't separated internally. This property has effect only when the
-     * property {@code option} is set to {@code top} or {@code bottom} and when property
-     * {@code staticGroups} is enabled.
+     * line or comment and aren't separated internally. When the property {@code option}
+     * is set to {@code top} or {@code bottom}, this property requires {@code staticGroups}
+     * to be enabled. When the property {@code option} is set to {@code under} or
+     * {@code above}, this property allows blank lines between static and non-static
+     * imports within the same group.
      */
     private boolean separatedStaticGroups;
 
@@ -279,9 +281,11 @@ public class ImportOrderCheck
     /**
      * Setter to control whether static import groups should be separated by, at least,
      * one blank line or comment and aren't separated internally.
-     * This property has effect only when the property
-     * {@code option} is set to {@code top} or {@code bottom} and when property {@code staticGroups}
-     * is enabled.
+     * When the property {@code option} is set to {@code top} or {@code bottom},
+     * this property requires {@code staticGroups} to be enabled.
+     * When the property {@code option} is set to {@code under} or {@code above},
+     * this property allows blank lines between static and non-static imports
+     * within the same group.
      *
      * @param separatedStaticGroups
      *            whether groups should be separated by one blank line or comment.
@@ -489,7 +493,15 @@ public class ImportOrderCheck
      */
     private boolean isSeparatorInGroup(int groupIdx, boolean isStatic, int line) {
         final boolean inSameGroup = groupIdx == lastGroup;
-        return (inSameGroup || !needSeparator(isStatic)) && isSeparatorBeforeImport(line);
+        final boolean result;
+        if (inSameGroup) {
+            result = (isStatic == lastImportStatic || !separatedStaticGroups)
+                    && isSeparatorBeforeImport(line);
+        }
+        else {
+            result = !needSeparator(isStatic) && isSeparatorBeforeImport(line);
+        }
+        return result;
     }
 
     /**
