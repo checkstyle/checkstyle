88d0e966e7f6584ea930218eea36e595ef8ee497
gianmarco <gianmarco.schifone02@gmail.com>
2026-03-10 01:17:44 +0100

    Issue #7176: Add AnnotatedMethodVisibilityModifierCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/modifier/annotatedmethodvisibilitymodifier/Example1.java
@@ -0,0 +1,50 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="AnnotatedMethodVisibilityModifier"/>
+  </module>
+</module>
+
+
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.modifier.annotatedmethodvisibilitymodifier;
+
+// xdoc section -- start
+import com.google.common.annotations.VisibleForTesting;
+
+public class Example1 {
+
+  // ok, default visibility includes protected
+  @VisibleForTesting
+  protected void protectedMethod() {}
+
+  // ok, default visibility includes package-private
+  @VisibleForTesting
+  void packagePrivateMethod() {}
+
+  // ok, default visibility includes protected
+  @VisibleForTesting
+  protected int protectedField;
+
+  // violation 2 lines below 'Annotated element has disallowed visibility 'public'.'
+  @VisibleForTesting
+  public void publicMethod() {}
+
+  // violation 2 lines below 'Annotated element has disallowed visibility 'private'.'
+  @VisibleForTesting
+  private void privateMethod() {}
+
+  // violation 3 lines below 'Annotated element has disallowed visibility 'public'.'
+  @VisibleForTesting
+  @Deprecated
+  public int publicField;
+
+  // violation 2 lines below 'Annotated element has disallowed visibility 'public'.'
+  @VisibleForTesting
+  public Example1() {}
+
+}
+// xdoc section -- end
