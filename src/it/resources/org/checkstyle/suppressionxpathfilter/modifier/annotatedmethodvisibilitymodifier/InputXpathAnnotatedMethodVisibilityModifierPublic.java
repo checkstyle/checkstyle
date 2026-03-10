4f3a4e1c32672e548505ceb2b34799c71891df78
gianmarco <gianmarco.schifone02@gmail.com>
2026-03-10 01:17:44 +0100

    Issue #7176: Add AnnotatedMethodVisibilityModifierCheck

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/modifier/annotatedmethodvisibilitymodifier/InputXpathAnnotatedMethodVisibilityModifierPublic.java
@@ -0,0 +1,8 @@
+package org.checkstyle.suppressionxpathfilter.modifier.annotatedmethodvisibilitymodifier;
+
+public class InputXpathAnnotatedMethodVisibilityModifierPublic {
+
+    @com.google.common.annotations.VisibleForTesting
+    public void violationMethod() {} // warn
+
+}
