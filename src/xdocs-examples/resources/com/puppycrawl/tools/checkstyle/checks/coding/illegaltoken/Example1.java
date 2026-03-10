b8619144bdf6c86ebec24a6026c1890155c0dc9f
Samhitha Savalam <samhithasavalam@gmail.com>
2026-03-10 22:53:47 +0530

    Issue #18435: Fix AST consistency in IllegalToken Example1

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltoken/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltoken/Example1.java
@@ -9,10 +9,17 @@ package com.puppycrawl.tools.checkstyle.checks.coding.illegaltoken;
 
 // xdoc section -- start
 class Example1 {
+<<<<<<< HEAD
+
+  void InvalidExample() {
+    // violation, 'Using 'outer:' is not allowed'
+    outer:
+=======
   native void InvalidExample();
 
   void anotherMethod() {
     outer: // violation, 'Using 'outer:' is not allowed'
+>>>>>>> upstream/master
     for (int i = 0; i < 5; i++) {
       if (i == 1) {
         break outer;
