53e6c1fee37a13ff9f13d22634c28e9c3b873d6f
gianmarco <gianmarco.schifone02@gmail.com>
2026-03-10 01:17:44 +0100

    Issue #7176: Add AnnotatedDeclarationVisibilityCheck

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/modifier/annotateddeclarationvisibility/Example2.java
@@ -0,0 +1,50 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="AnnotatedDeclarationVisibility">
+      <property name="tokens" value="METHOD_DEF"/>
+    </module>
+  </module>
+</module>
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.modifier.annotateddeclarationvisibility;
+
+// xdoc section -- start
+import com.google.common.annotations.VisibleForTesting;
+
+public class Example2 {
+
+  // ok, default visibility includes protected
+  @VisibleForTesting
+  protected void protectedMethod() {}
+
+  // ok, default visibility includes package-private
+  @VisibleForTesting
+  void packagePrivateMethod() {}
+
+  // ok, default visibility includes protected
+  @VisibleForTesting
+  protected int protectedField;
+
+  // violation below 'Annotated element has disallowed visibility 'public'.'
+  @VisibleForTesting
+  public void publicMethod() {}
+
+  // violation below 'Annotated element has disallowed visibility 'private'.'
+  @VisibleForTesting
+  private void privateMethod() {}
+
+  // ok, fields are not checked when tokens only include METHOD_DEF
+  @VisibleForTesting
+  @Deprecated
+  public int publicField;
+
+  // ok, constructors are not checked when tokens only include METHOD_DEF
+  @VisibleForTesting
+  public Example2() {}
+
+}
+// xdoc section -- end
