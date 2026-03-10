f5dc96b3353bb77d510ec5f0fc7ed4e069fb5846
gianmarco <gianmarco.schifone02@gmail.com>
2026-03-10 01:17:44 +0100

    Issue #7176: Add AnnotatedMethodVisibilityModifierCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/modifier/annotatedmethodvisibilitymodifier/InputAnnotatedMethodVisibilityModifierClearedImport1.java
@@ -0,0 +1,27 @@
+/*
+AnnotatedMethodVisibilityModifier
+annotations = org.jspecify.annotations.Nullable, org.junit.jupiter.api.Disabled
+visibility = (default)protected,package-private
+tokens = (default)PACKAGE_DEF, IMPORT, CLASS_DEF, \
+         INTERFACE_DEF, ENUM_DEF, RECORD_DEF, \
+         METHOD_DEF, CTOR_DEF, VARIABLE_DEF, ANNOTATION_DEF,
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.modifier.annotatedmethodvisibilitymodifier;
+
+import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.*;
+
+public class InputAnnotatedMethodVisibilityModifierClearedImport1 {
+
+    @Disabled
+    private void violationMethod() {}
+    // violation above 'Annotated element has disallowed visibility 'private'.'
+
+    @Nullable
+    public int violationField;
+    // violation above 'Annotated element has disallowed visibility 'public'.'
+
+}
