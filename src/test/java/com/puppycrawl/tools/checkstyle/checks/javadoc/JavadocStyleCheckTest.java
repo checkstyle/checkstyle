2d0b9362b051392b0b98354e75598200160faacf
Brijeshthummar02 <brijeshthummar02@gmail.com>
2026-03-11 00:07:36 +0530

    Issue #19154: Add format property to JavadocStyle to control allowed Javadoc comment formats

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocStyleCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocStyleCheckTest.java
@@ -849,4 +849,20 @@ public class JavadocStyleCheckTest
                 getPath("InputJavadocStyleUnclosedTags.java"),
                 expected);
     }
+
+    @Test
+    public void testJavadocStyleFormatTraditional() throws Exception {
+        final String[] expected = {
+            "26: " + getCheckMessage(MSG_NO_PERIOD),
+        };
+        verifyWithInlineConfigParser(
+                getPath("InputJavadocStyleFormatTraditional.java"), expected);
+    }
+
+    @Test
+    public void testJavadocStyleFormatMarkdown() throws Exception {
+        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+        verifyWithInlineConfigParser(
+                getPath("InputJavadocStyleFormatMarkdown.java"), expected);
+    }
 }
