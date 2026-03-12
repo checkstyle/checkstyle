0bcbe0ce20dc8d273b17b16064c9be27e4aa1753
lithops-zty <1220708797@qq.com>
2026-03-12 21:55:41 +0800

    Issue #19150: Update WriteTagCheck to use AST of Javadoc

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/InputWriteTagTypeCustom.java
@@ -0,0 +1,22 @@
+/*
+WriteTag
+tag = @customBlock
+tagFormat = \\S
+tagSeverity = (default)info
+tokens = INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF, METHOD_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.writetag;
+
+class InputWriteTagTypeCustom {
+    // violation 2 lines below 'Javadoc tag @customBlock={@customInline {@nested <br>}}'
+    /**
+     * @customBlock {@customInline {@nested <br>}}
+     * @return value of type {@code String}
+     */
+    private String method() {
+        return "";
+    }
+}
