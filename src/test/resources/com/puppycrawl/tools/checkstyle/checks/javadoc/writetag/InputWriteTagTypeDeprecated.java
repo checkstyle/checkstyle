37e37df493c677a63840e22898f9dceb77eb3807
lithops-zty <1220708797@qq.com>
2026-03-12 21:55:41 +0800

    Issue #19150: Update WriteTagCheck to use AST of Javadoc

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/InputWriteTagTypeDeprecated.java
@@ -0,0 +1,19 @@
+/*
+WriteTag
+tag = @deprecated
+tagFormat = \\S
+tagSeverity = (default)info
+tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.writetag;
+
+// violation 2 lines below 'Javadoc tag @deprecated={@systemProperty x}'
+/**
+ * @deprecated {@systemProperty x}
+ */
+@Deprecated
+class InputWriteTagTypeDeprecated {
+}
