b879d3d8f112e7c12067545c518f12d090f27c1f
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-03-16 19:11:44 +0500

    Issue #19149: MissingJavadocTypeCheck Check to use AST

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/missingjavadoctype/InputMissingJavadocTypeAboveComments.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/missingjavadoctype/InputMissingJavadocTypeAboveComments.java
@@ -13,16 +13,45 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc.missingjavadoctype;
 public class InputMissingJavadocTypeAboveComments { // violation, 'Missing a Javadoc comment.'
 
     /**
-     *
+     * java doc comment
      */
     /* */
-    public class InputMissingJavadocTypeAboveComments2 {
-    }
+    //  comment contant
+    public class InputMissingJavadocTypeAboveComments2 { }
 
     /**
      *
      */
+
     public class Myclass { } /* My class */
 
-    public class Myclass2 { } // violation, 'Missing a Javadoc comment.'
+    /**
+     *
+     */
+
+    // singline comment missing javadoc
+    public class InputMissingJavadocTypeAboveComments3 { }
+
+
+    /**
+     *
+     */
+
+    /* singline comment missing javadoc */
+    public class InputMissingJavadocTypeAboveComments4 { }
+
+    /**
+     *
+     */ public class Myclass2 { }
+
+  /**javadoc*/ public class Myclass5 { }
+
+  /** Test class for variable naming in for each clause.
+   *
+   */
+  /*
+     Input class for JavadocType
+  */
+  public class InputJavadocTypeAboveComments { }
+
 }
