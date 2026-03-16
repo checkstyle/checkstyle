40adf02f929fbf0694f072df482ab647201684e4
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-03-16 19:11:44 +0500

    Issue #19149: MissingJavadocTypeCheck Check to use AST

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/missingjavadoctype/InputMissingJavadocTypeNonJavadocOnly.java
@@ -0,0 +1,17 @@
+/*
+MissingJavadocType
+scope = (default)public
+excludeScope = (default)null
+skipAnnotations = (default)Generated
+tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
+
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.missingjavadoctype;
+
+public class InputMissingJavadocTypeNonJavadocOnly { // violation, 'Missing a Javadoc comment.'
+
+    /* This is NOT a Javadoc comment */
+    public class InnerClass { // violation, 'Missing a Javadoc comment.'
+    }
+}
