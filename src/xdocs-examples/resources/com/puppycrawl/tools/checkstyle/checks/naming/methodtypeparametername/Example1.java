bfcfa53f316ecc10d1de21808e85d112f22aac5c
avadhutmali <mali.avadhut7@gmail.com>
2026-03-16 22:03:53 +0530

    Issue #16807: update input files for MethodTypeParameterNameCheck to specify violation messages

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/methodtypeparametername/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/methodtypeparametername/Example1.java
@@ -11,8 +11,8 @@ package com.puppycrawl.tools.checkstyle.checks.naming.methodtypeparametername;
 // xdoc section -- start
 class Example1 {
   public <T> void method1() {}
-  public <a> void method2() {} // violation
+  public <a> void method2() {} // violation 'Name 'a' must match pattern'
   public <K, V> void method3() {}
-  public <k, V> void method4() {} // violation
+  public <k, V> void method4() {} // violation 'Name 'k' must match pattern'
 }
 // xdoc section -- end
