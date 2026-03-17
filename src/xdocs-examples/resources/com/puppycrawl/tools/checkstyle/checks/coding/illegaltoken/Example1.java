f44af9c32919c8c248e4bf0e88029b8a5891fa52
Samhitha Savalam <samhithasavalam@gmail.com>
2026-03-17 08:20:03 +0530

    Issue #18435: Fix syntax in

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltoken/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltoken/Example1.java
@@ -8,18 +8,15 @@
 package com.puppycrawl.tools.checkstyle.checks.coding.illegaltoken;
 
 // xdoc section -- start
-class Example1 {
-<<<<<<< HEAD
-
+class Example1{
   void InvalidExample() {
-    // violation, 'Using 'outer:' is not allowed'
+      // violation, 'Using 'outer:' is not allowed'
     outer:
-=======
+  }
   native void InvalidExample();
-
+  
   void anotherMethod() {
     outer: // violation, 'Using 'outer:' is not allowed'
->>>>>>> upstream/master
     for (int i = 0; i < 5; i++) {
       if (i == 1) {
         break outer;
