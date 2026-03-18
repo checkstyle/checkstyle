d9e5423b3ededed45e72cc3d8d6267be1e02cd1b
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-03-18 00:11:35 +0000

    Issue #19064: Add third test to XpathRegressionUnusedLambdaParameterShouldBeUnnamedTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/localvariablename/InputLocalVariableNameUnnamedVariables.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/localvariablename/InputLocalVariableNameUnnamedVariables.java
@@ -1,5 +1,9 @@
 /*
 LocalVariableName
+allowOneCharVarInForLoop = (default)false
+format = (default)^([a-z][a-zA-Z0-9]*|_)$
+tokens = (default)VARIABLE_DEF
+
 
 */
 
@@ -13,8 +17,8 @@ public class InputLocalVariableNameUnnamedVariables {
 
     public void testLocalVariables() {
         Queue<Integer> q = new PriorityQueue<>();
-        int __ = 0;   // violation
-        int _result = 0; // violation
+        int __ = 0;   // violation, 'Name '__' must match pattern*'
+        int _result = 0; // violation, 'Name '_result' must match pattern*'
         q.add(1);
         q.add(2);
         for (Integer element : q) {
@@ -30,7 +34,7 @@ public class InputLocalVariableNameUnnamedVariables {
             var x1 = q.poll();
             var x2 = q.poll();
         }
-        for (Integer __ : q) {  // violation
+        for (Integer __ : q) {  // violation, 'Name '__' must match pattern*'
             var x1 = q.poll();
             var x2 = q.poll();
         }
