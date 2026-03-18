f526f506e3e95aaf7df86668b67ebe8f82b3e0ef
Adam Mohamed Gamil <adammgamil35@gmail.com>
2026-03-19 00:19:59 +0200

    Issue #17393: completing the new check TypeBodyPadding

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/typebodypadding/InputTypeBodyPaddingEmptyNotAllowed2.java
@@ -0,0 +1,15 @@
+/*
+TypeBodyPadding
+starting = (default)true
+ending = true
+allowEmpty = false
+skipInner = (default)true
+skipLocal = (default)true
+tokens = (default)CLASS_DEF, INTERFACE_DEF, ENUM_DEF, RECORD_DEF, ANNOTATION_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.typebodypadding;
+
+class InputTypeBodyPaddingEmptyNotAllowed2 {} // 2 violations
