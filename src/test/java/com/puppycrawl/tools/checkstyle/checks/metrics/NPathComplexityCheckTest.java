f245f17606cba2b9e28f625833b8109007d43fc1
Brijeshthummar02 <brijeshthummar02@gmail.com>
2026-03-21 22:20:32 +0530

    Issue #19188: Move Intputs to non-compiled folders to let Idea to run test without compilation error

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/metrics/NPathComplexityCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/metrics/NPathComplexityCheckTest.java
@@ -266,7 +266,7 @@ public class NPathComplexityCheckTest extends AbstractModuleTestSupport {
         };
 
         verifyWithInlineConfigParser(
-                getPath("InputNPathComplexityPatternMatchingForSwitch.java"),
+                getNonCompilablePath("InputNPathComplexityPatternMatchingForSwitch.java"),
             expected);
 
     }
@@ -463,3 +463,4 @@ public class NPathComplexityCheckTest extends AbstractModuleTestSupport {
     }
 
 }
+
