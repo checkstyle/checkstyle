0c2be67af1b78985cdd8b264a4c04ac6ad85a225
surajgojanur <surajgojanur@gmail.com>
2026-03-22 22:03:39 +0530

    Issue #16807: update ParameterNameCheck to fully specify inline config properties

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example4.java
@@ -13,7 +13,7 @@ package com.puppycrawl.tools.checkstyle.checks.naming.parametername;
 // xdoc section -- start
 class Example4 {
   void method1(int v1) {}
-  void method2(int v_2) {} // violation
-  void method3(int V3) {} // violation
+  void method2(int v_2) {} // violation 'Name 'v_2' must match pattern'
+  void method3(int V3) {} // violation 'Name 'V3' must match pattern'
 }
 // xdoc section -- end
