af64d22ca2b8a7f2292d86de1c3d409a16a90568
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-03-23 21:00:54 +0100

    Issue #11163: Enforce file size on MultipleStringLiterals input files

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplestringliterals/InputMultipleStringLiteralsTextBlocks1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/multiplestringliterals/InputMultipleStringLiteralsTextBlocks1.java
@@ -10,7 +10,7 @@ ignoreOccurrenceContext = (default)ANNOTATION
 // Java17
 package com.puppycrawl.tools.checkstyle.checks.coding.multiplestringliterals;
 
-public class InputMultipleStringLiteralsTextBlocks {
+public class InputMultipleStringLiteralsTextBlocks1 {
     String string1 = "string"; // occurance #1 // violation
     String string2a = "string"; // violation #1
     String string2b = """
@@ -68,54 +68,4 @@ public class InputMultipleStringLiteralsTextBlocks {
             foo"""; // violation #2
     String str2b = """
             foo"""; // violation #3
-
-
-    String str3 = "another test"; // occurrence #1 // violation
-    String str4 = """
-            another test"""; // violation #1
-
-    String str5a = ""; // occurrence #1 // violation
-    String str5b = """
-            """; // violation #1
-    String str6 = """
-                        """; // violation #2
-    String str7 = """
-"""; // violation #3
-
-    String str8 = """
-                             """; // violation #4
-     // violation below
-    String str8b = """
-        .
-         .
-.
-        """;
-
-    String str8c = """
-        .
-         .
-.
-        """;
-
-    String str9a = """
-            test    """; // trailing whitespace removed per javac result
-    String str9b = "test    "; // trailing whitespace not removed, strings are not equal!
-     // violation below
-    String str10a = """
-             foo
-
-
-        bar""";
-    String str10b = """
-             foo
-
-
-        bar""";
-    String emptyWithLotsOfLines = """
-
-
-
-
-""";
-
 }
