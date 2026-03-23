bb43d2d2725981b626530e80e269ea387def2dc1
HamzehAdawi <ronham12@yahoo.com>
2026-03-23 13:53:56 -0500

    Issue #16807: Update input files for RegexpCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/InputRegexpSemantic5.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/regexp/regexp/InputRegexpSemantic5.java
@@ -3,7 +3,7 @@ Regexp
 format = Boolean x = new Boolean
 message = (default)null
 illegalPattern = (default)false
-duplicateLimit = 0
+duplicateLimit = 1
 errorLimit = (default)100
 ignoreComments = (default)false
 
@@ -24,10 +24,10 @@ class InputRegexpSemantic5
 {
     /* Boolean instantiation in a static initializer */
     static {
-        Boolean x = new Boolean(true); // violation 'Found duplicate pattern'
+        Boolean x = new Boolean(true);
     }
 
-    /* Boolean instantiation in a non-static initializer */
+    /* Boolean instantiation in a static initializer */
     {
         Boolean x = new Boolean(true); // violation 'Found duplicate pattern'
         Boolean[] y = new Boolean[]{Boolean.TRUE, Boolean.FALSE};
