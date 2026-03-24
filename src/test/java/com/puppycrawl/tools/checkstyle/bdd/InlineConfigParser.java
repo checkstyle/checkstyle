112850aa9a9a82617cd1eff0bfec4d1a4d192b16
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-03-24 11:41:08 +0500

    Issue #16807: Updating properties in Input files for module SummaryJavadocCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -307,7 +307,6 @@ public final class InlineConfigParser {
             "com.puppycrawl.tools.checkstyle.checks.javadoc.JavadocVariableCheck",
             "com.puppycrawl.tools.checkstyle.checks.javadoc.MissingJavadocPackageCheck",
             "com.puppycrawl.tools.checkstyle.checks.javadoc.MissingJavadocTypeCheck",
-            "com.puppycrawl.tools.checkstyle.checks.javadoc.SummaryJavadocCheck",
             "com.puppycrawl.tools.checkstyle.checks.modifier.RedundantModifierCheck",
             "com.puppycrawl.tools.checkstyle.checks.naming.LocalFinalVariableNameCheck",
 
