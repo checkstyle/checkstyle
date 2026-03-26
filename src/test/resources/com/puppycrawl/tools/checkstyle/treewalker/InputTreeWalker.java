d2e19c87a4aa791ab396e544b5908faa0a706b4b
Samhitha Savalam <samhithasavalam@gmail.com>
2026-03-26 16:36:09 +0530

    Issue #16807 Update input file to mention default property for OneTopLevelClassCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalker.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/treewalker/InputTreeWalker.java
@@ -1,8 +1,19 @@
 /*
-com.puppycrawl.tools.checkstyle.checks.design.OneTopLevelClassCheck
-max = 1
+<!DOCTYPE module PUBLIC
+"-//Checkstyle//DTD Checkstyle Configuration 1.3//EN"
+"https://checkstyle.org/dtds/configuration_1_3.dtd">
+
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="com.puppycrawl.tools.checkstyle.checks.design.OneTopLevelClassCheck">
+      <property name="max" value="1" default="true"/>
+    </module>
+  </module>
+</module>
 */
+
 package com.puppycrawl.tools.checkstyle.treewalker;
+
 /*comment*/
 public class InputTreeWalker {
 }
