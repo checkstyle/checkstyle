c361edf3529a02219a15628166f16c879a614d50
achyuth8055 <achyuthkumark2001@gmail.com>
2026-03-29 12:56:41 -0500

    Issue #16807: mention defaults in coding package input files

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltype/InputIllegalTypeAbstractClassNameFormat.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltype/InputIllegalTypeAbstractClassNameFormat.java
@@ -1,7 +1,16 @@
 /*
 IllegalType
 validateAbstractClassNames = true
+illegalClassNames = (default)HashMap, HashSet, LinkedHashMap, LinkedHashSet, TreeMap, TreeSet, \
+                    java.util.HashMap, java.util.HashSet, java.util.LinkedHashMap, \
+                    java.util.LinkedHashSet, java.util.TreeMap, java.util.TreeSet
+legalAbstractClassNames = (default)
+ignoredMethodNames = (default)getEnvironment, getInitialContext
 illegalAbstractClassNameFormat = Gitt
+memberModifiers = (default)
+tokens = (default)ANNOTATION_FIELD_DEF, CLASS_DEF, INTERFACE_DEF, METHOD_CALL, METHOD_DEF, \
+         METHOD_REF, PARAMETER_DEF, VARIABLE_DEF, PATTERN_VARIABLE_DEF, RECORD_DEF, \
+         RECORD_COMPONENT_DEF
 
 
 */
