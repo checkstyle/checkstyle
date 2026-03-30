d852f1e3319341c60e1833c491a3dd214b2e6b3c
MonuChaudhary14 <monuchaudharypoonia@gmail.com>
2026-03-31 00:46:02 +0530

    Issue #18064: New check ArrayBracketWhitespace

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/whitespace/arraybracketwhitespace/InputXpathArrayBracketWhitespaceFollowed.java
@@ -0,0 +1,6 @@
+package org.checkstyle.suppressionxpathfilter.whitespace.arraybracketwhitespace;
+
+public class InputXpathArrayBracketWhitespaceFollowed {
+    int[] arr = new int[10];
+    int x = arr[ 0]; //warn
+}
