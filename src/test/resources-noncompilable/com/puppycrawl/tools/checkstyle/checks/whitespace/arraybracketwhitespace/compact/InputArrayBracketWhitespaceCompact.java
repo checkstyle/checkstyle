a2e3a5151eca1fe00acc1954b75b2c7063e2e63b
MonuChaudhary14 <monuchaudharypoonia@gmail.com>
2026-03-31 00:46:02 +0530

    Issue #18064: New check ArrayBracketWhitespace

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/whitespace/arraybracketwhitespace/compact/InputArrayBracketWhitespaceCompact.java
@@ -0,0 +1,14 @@
+/*
+ArrayBracketWhitespace
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+void main() {
+    int [] arr1 = new int [10];
+    // 2 violations above:
+    //  ''\[' is preceded with whitespace.'
+    //  ''\[' is preceded with whitespace.'
+}
