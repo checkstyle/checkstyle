f4c565f4b3da00be89148aa717cadf5ed94442b0
achyuth8055 <achyuthkumark2001@gmail.com>
2026-03-30 21:55:52 -0500

    Issue #15456: Define violation messages for NoLineWrapCheck

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -271,7 +271,6 @@ public final class InlineConfigParser {
             "com.puppycrawl.tools.checkstyle.checks.sizes.RecordComponentNumberCheck",
             "com.puppycrawl.tools.checkstyle.checks.TodoCommentCheck",
             "com.puppycrawl.tools.checkstyle.checks.TrailingCommentCheck",
-            "com.puppycrawl.tools.checkstyle.checks.whitespace.NoLineWrapCheck",
             "com.puppycrawl.tools.checkstyle.checks.whitespace."
                     + "NoWhitespaceBeforeCaseDefaultColonCheck",
             "com.puppycrawl.tools.checkstyle.checks.whitespace.NoWhitespaceBeforeCheck",
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nolinewrap/InputNoLineWrapBad2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nolinewrap/InputNoLineWrapBad2.java
@@ -10,24 +10,24 @@ package com.puppycrawl.tools.
 
 import java.util.Calendar;
 
-import javax.accessibility. // violation
+import javax.accessibility. // violation 'import statement should not be line-wrapped.'
     AccessibleAttributeSequence;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
-import static java.math. // violation
+import static java.math. // violation 'import statement should not be line-wrapped.'
 		BigInteger.ZERO;
 
-public class // violation
+public class // violation 'CLASS_DEF statement should not be line-wrapped.'
     InputNoLineWrapBad2 {
 
-	public void // violation
+	public void // violation 'METHOD_DEF statement should not be line-wrapped.'
 	    fooMethod() {
 		final int
 		    foo = 0;
 	}
 }
 
-enum // violation
+enum // violation 'ENUM_DEF statement should not be line-wrapped.'
     FooFoo2 {
 }
 
