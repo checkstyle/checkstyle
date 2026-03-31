d86ad95f65bd26e1a2489ed1187152903927e4cc
achyuth8055 <achyuthkumark2001@gmail.com>
2026-03-30 21:55:52 -0500

    Issue #15456: Define violation messages for NoLineWrapCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nolinewrap/InputNoLineWrapBad.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nolinewrap/InputNoLineWrapBad.java
@@ -5,16 +5,16 @@ tokens = (default)PACKAGE_DEF, IMPORT, STATIC_IMPORT
 
 */
 
-package com.puppycrawl.tools. // violation
+package com.puppycrawl.tools. // violation 'package statement should not be line-wrapped.'
     checkstyle.checks.whitespace.nolinewrap;
 
 import java.util.Calendar;
 
-import javax.accessibility. // violation
+import javax.accessibility. // violation 'import statement should not be line-wrapped.'
     AccessibleAttributeSequence;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
-import static java.math. // violation
+import static java.math. // violation 'import statement should not be line-wrapped.'
 		BigInteger.ZERO;
 
 public class
