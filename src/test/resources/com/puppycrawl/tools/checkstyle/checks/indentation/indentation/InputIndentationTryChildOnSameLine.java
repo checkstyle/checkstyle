97737ee8eee6939de8039929830e17107458ef70
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-04-02 01:47:32 +0100

    Issue #5685: Fix false positive indentation for try/catch with body on same line

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationTryChildOnSameLine.java
@@ -0,0 +1,33 @@
+package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;         //indent:0 exp:0
+/**                                                                             //indent:0 exp:0
+ * This test-input is intended to be checked using following configuration:     //indent:1 exp:1
+ *                                                                              //indent:1 exp:1
+ * arrayInitIndent = 4                                                          //indent:1 exp:1
+ * basicOffset = 4                                                              //indent:1 exp:1
+ * braceAdjustment = 0                                                          //indent:1 exp:1
+ * caseIndent = 4                                                               //indent:1 exp:1
+ * forceStrictCondition = false                                                 //indent:1 exp:1
+ * lineWrappingIndentation = 4                                                  //indent:1 exp:1
+ * tabWidth = 4                                                                 //indent:1 exp:1
+ * throwsIndent = 4                                                             //indent:1 exp:1
+ *                                                                              //indent:1 exp:1
+ */                                                                             //indent:1 exp:1
+public class InputIndentationTryChildOnSameLine {                               //indent:0 exp:0
+    void method() {                                                             //indent:4 exp:4
+        try { return;                                                           //indent:8 exp:8
+        }                                                                       //indent:8 exp:8
+        catch (Exception e) { return;                                           //indent:8 exp:8
+        }                                                                       //indent:8 exp:8
+    }                                                                           //indent:4 exp:4
+    void method2() {                                                            //indent:4 exp:4
+        try { doSomething();                                                    //indent:8 exp:8
+        }                                                                       //indent:8 exp:8
+        catch (RuntimeException e) { handle(e);                                 //indent:8 exp:8
+        }                                                                       //indent:8 exp:8
+        finally { cleanup();                                                    //indent:8 exp:8
+        }                                                                       //indent:8 exp:8
+    }                                                                           //indent:4 exp:4
+    private void doSomething() {}                                               //indent:4 exp:4
+    private void handle(Exception e) {}                                         //indent:4 exp:4
+    private void cleanup() {}                                                   //indent:4 exp:4
+}                                                                               //indent:0 exp:0
