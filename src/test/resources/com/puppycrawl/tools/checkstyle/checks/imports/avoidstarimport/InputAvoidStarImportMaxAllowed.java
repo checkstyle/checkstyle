59faa852e3c339f5bda861da47615fd5e7f6a4a8
Aman Baliyan <baliyanaman200@gmail.com>
2026-04-03 18:07:51 +0530

    Issue #19496: Add maxAllowed property in AvoidStarImport

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport/InputAvoidStarImportMaxAllowed.java
@@ -0,0 +1,16 @@
+/*
+AvoidStarImport
+excludes = (default)
+allowClassImports = (default)false
+allowStaticMemberImports = (default)false
+maxAllowedStarImports = 1
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.avoidstarimport;
+
+import java.util.*;
+
+public class InputAvoidStarImportMaxAllowed {
+
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport/InputAvoidStarImportMaxAllowed2.java
@@ -0,0 +1,16 @@
+/*
+AvoidStarImport
+excludes = (default)
+allowClassImports = (default)false
+allowStaticMemberImports = (default)false
+maxAllowedStarImports = 1
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.avoidstarimport;
+
+import static javax.swing.WindowConstants.*;
+
+public class InputAvoidStarImportMaxAllowed2 {
+
+}
