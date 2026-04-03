428ebdd0fa350555d4cad830dad60b6d5a897d4d
Aman Baliyan <baliyanaman200@gmail.com>
2026-04-03 18:07:51 +0530

    Issue #19496: Add maxAllowed property in AvoidStarImport

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/avoidstarimport/Example7.java
@@ -0,0 +1,21 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="AvoidStarImport">
+      <property name="maxAllowedStarImports" value="1"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.avoidstarimport;
+
+// xdoc section -- start
+import java.util.Scanner;
+import java.io.*;
+import static java.lang.Math.*; // violation, 'Only '1' star import is allowed per file.'
+import java.util.*; // violation, 'Only '1' star import is allowed per file.'
+import java.net.*; // violation, 'Only '1' star import is allowed per file.'
+// xdoc section -- end
+
+public class Example7 {}
