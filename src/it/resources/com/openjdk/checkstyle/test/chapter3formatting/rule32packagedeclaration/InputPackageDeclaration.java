ab4cf2c3dfce7edd3f1ea2ae848f1767cb9178bd
Anushree Bondia <anushreebondia@gmail.com>
2026-04-06 21:37:28 +0530

    Issue #19622: Added checks for OpenJDK Style §3.2 - Package declaration

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapter3formatting/rule32packagedeclaration/InputPackageDeclaration.java
@@ -0,0 +1,9 @@
+package // violation 'package statement should not be line-wrapped.'
+    com.openjdk.checkstyle.test.chapter3formatting.rule32packagedeclaration;
+
+/**
+ * Test input for the package declaration rule.
+ */
+public final class InputPackageDeclaration {
+}
+
