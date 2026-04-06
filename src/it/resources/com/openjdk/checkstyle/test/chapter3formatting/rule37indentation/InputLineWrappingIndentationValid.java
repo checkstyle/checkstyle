63a946af3c4aa6157b2cf1f3b8f54421b32b3866
Anushree Bondia <anushreebondia@gmail.com>
2026-04-06 22:14:09 +0530

    Issue #19619: Added checks for OpenJDK Style §3.7 - Indentation

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapter3formatting/rule37indentation/InputLineWrappingIndentationValid.java
@@ -0,0 +1,9 @@
+package com.openjdk.checkstyle.test.chapter3formatting.rule37indentation;
+
+public class InputLineWrappingIndentationValid {
+
+    void method(int a,
+            int b) {
+        int sum = a + b;
+    }
+}
