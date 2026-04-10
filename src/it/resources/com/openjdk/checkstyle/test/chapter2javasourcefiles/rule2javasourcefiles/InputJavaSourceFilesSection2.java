a93f41e3236135e82463ca85a2db90722c867b94
Anushree Bondia <anushreebondia@gmail.com>
2026-04-10 19:25:18 +0530

    Issue #19617: Added checks for OpenJDK Style §2 - Java Source Files

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapter2javasourcefiles/rule2javasourcefiles/InputJavaSourceFilesSection2.java
@@ -0,0 +1,9 @@
+package com.openjdk.checkstyle.test.chapter2javasourcefiles.rule2javasourcefiles;
+
+class InputJavaSourceFilesSection2 {
+    private final int value;
+
+    InputJavaSourceFilesSection2() {
+        value = 1;
+    }
+}
