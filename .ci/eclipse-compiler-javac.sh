a31ae4221a9cb744db45657e179b9e9faaa1ce48
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-04-10 08:50:09 +0100

    Issue #13040: convert all wget to curls

--- .ci/eclipse-compiler-javac.sh
+++ .ci/eclipse-compiler-javac.sh
@@ -18,13 +18,13 @@ ECJ_MAVEN_VERSION="R-4.37-202509050730"
 
 echo "Using pinned eclipse release: $ECJ_MAVEN_VERSION"
 
-ECJ_JAR=$(wget --quiet -O- "$ECLIPSE_URL/$ECJ_MAVEN_VERSION/" | grep -o "ecj-[^\"]*" | head -n1)
+ECJ_JAR=$(curl -s "$ECLIPSE_URL/$ECJ_MAVEN_VERSION/" | grep -o "ecj-[^\"]*" | head -n1)
 ECJ_PATH=~/.m2/repository/$ECJ_MAVEN_VERSION/$ECJ_JAR
 
 if [ ! -f "$ECJ_PATH" ]; then
     echo "$ECJ_PATH is not found, downloading ..."
     cd target
-    wget $ECLIPSE_URL/"$ECJ_MAVEN_VERSION"/"$ECJ_JAR"
+    curl -O "$ECLIPSE_URL/$ECJ_MAVEN_VERSION/$ECJ_JAR"
     echo "test jar after download:"
     jar -tvf "$ECJ_JAR" > /dev/null
     echo "check compiler options"
