4c76306aa3da9a97f3f9d7ef49f969325d99450b
Anushree Bondia <anushreebondia@gmail.com>
2026-04-18 16:02:47 +0530

    Issue #19617: added Checks to cover special characters

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapter2javasourcefiles/rule21specialcharacters/InputSpecialCharactersValid.java
@@ -0,0 +1,14 @@
+package com.openjdk.checkstyle.test.chapter2javasourcefiles.rule21specialcharacters;
+
+public class InputSpecialCharactersValid {
+
+    private final char apostrophe = '\'';
+    private final char quote = '"';
+    private final char slash = '\\';
+    private final char backspace = '\b';
+    private final char tab = '\t';
+    private final char formFeed = '\f';
+    private final char carriageReturn = '\r';
+    private final char newLine = '\n';
+
+}
