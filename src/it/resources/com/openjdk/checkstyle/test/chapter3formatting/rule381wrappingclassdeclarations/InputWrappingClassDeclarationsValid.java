d671174fb2378897bf10673e5a376940a9000007
Anushree Bondia <anushreebondia@gmail.com>
2026-04-23 13:24:11 +0530

    Issue #19743: added checks for OpenJDK Style §3.8.1 - Wrapping Class Declarations

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapter3formatting/rule381wrappingclassdeclarations/InputWrappingClassDeclarationsValid.java
@@ -0,0 +1,14 @@
+package com.openjdk.checkstyle.test.chapter3formatting.rule381wrappingclassdeclarations;
+
+import java.util.HashMap;
+
+public class InputWrappingClassDeclarationsValid<T, S>
+        extends HashMap<T, S>
+        implements Comparable<T> {
+}
+
+class AnotherValidClass<
+        K,
+        R,
+        V> {
+}
