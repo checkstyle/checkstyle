4fe0987586c1a1abd8a6b509038dbcc6f6f1ad05
Gusti Maulana <mgustimz@gmail.com>
2026-05-11 11:05:37 +0700

    Issue #19770: fix JavadocTypeCheck record component @param tag prefix match

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoctype/InputJavadocTypeRecordComponentPrefixMatch.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoctype/InputJavadocTypeRecordComponentPrefixMatch.java
@@ -14,7 +14,7 @@ tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoctype;
 
-// violation 4 lines below 'Unused @param tag for 'valueExtra'.'
+// violation above 'Unused @param tag for 'valueExtra'.'
 /**
  * Example case with prefix match bug.
  *
