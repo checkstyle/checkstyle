ef94ad363f8d439bdc2d27f3d3940962e2f68237
Gusti Maulana <mgustimz@gmail.com>
2026-05-11 15:07:09 +0700

    Issue checkstyle#19770: Add test for JavadocTypeCheck to validate parameter tag mismatches in record components

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoctype/InputJavadocTypeRecordComponentNameMismatch.java
@@ -0,0 +1,25 @@
+/*
+JavadocType
+scope = protected
+excludeScope = (default)null
+authorFormat = (default)null
+versionFormat = (default)null
+allowMissingParamTags = (default)false
+allowUnknownTags = (default)false
+allowedAnnotations = (default)Generated
+tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
+
+
+*/
+
+// Java17
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoctype;
+
+/**
+ * Example case
+ *
+ * @param valueExtra wrong tag for a different component
+ */
+// violation 2 lines above 'Unused @param tag for 'valueExtra'.'
+public record InputJavadocTypeRecordComponentNameMismatch(String value) { // violation 'Type Javadoc comment is missing @param value tag.'
+}
