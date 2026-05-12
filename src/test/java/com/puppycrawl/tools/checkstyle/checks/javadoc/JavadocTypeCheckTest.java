7b477cfde0bbdc8cd3f6d202dde295185d013489
Gusti Maulana <mgustimz@gmail.com>
2026-05-12 11:36:48 +0700

    Issue #19770: Add test for bare @param tag to cover null arg branch

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTypeCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTypeCheckTest.java
@@ -371,12 +371,19 @@ public class JavadocTypeCheckTest extends AbstractModuleTestSupport {
 
     @Test
     public void testJavadocTypeRecordComponentNameMismatch() throws Exception {
-        final String[] expected = {
+        final String[] expected1 = {
             "20:4: " + getCheckMessage(MSG_UNUSED_TAG, "@param", "valueExtra"),
             "23:1: " + getCheckMessage(MSG_MISSING_TAG, "@param value"),
         };
         verifyWithInlineConfigParser(
-                getPath("InputJavadocTypeRecordComponentNameMismatch.java"), expected);
+                getPath("InputJavadocTypeRecordComponentNameMismatch.java"), expected1);
+
+        final String[] expected2 = {
+            "23:1: " + getCheckMessage(MSG_MISSING_TAG, "@param value"),
+            "21:4: " + getCheckMessage(MSG_UNUSED_TAG_GENERAL),
+        };
+        verifyWithInlineConfigParser(
+                getPath("InputJavadocTypeRecordComponentNameMismatch2.java"), expected2);
     }
 
     @Test
