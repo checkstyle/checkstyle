942072aae573a000af0f1646109f4427d8fed9c6
mgustimz <mgustimz@gmail.com>
2026-05-13 21:44:05 +0700

    Issue #19770: Improve JavadocTypeCheck to handle record component @param tags more robustly

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoctype/InputJavadocTypeRecordComponentNameMismatch.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoctype/InputJavadocTypeRecordComponentNameMismatch.java
@@ -14,12 +14,12 @@ tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoctype;
 
+// violation 4 lines below 'Unused @param tag for 'valueExtra'.'
 /**
  * Example case
  *
  * @param valueExtra wrong tag for a different component
  */
-// violation 2 lines above 'Unused @param tag for 'valueExtra'.'
 public record InputJavadocTypeRecordComponentNameMismatch(String value) {
     // violation above 'Type Javadoc comment is missing @param value tag.'
 }
