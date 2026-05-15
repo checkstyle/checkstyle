60d20194bcf9d2893f197ed558f9de3803985cb7
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-05-15 08:34:55 +0500

    Issue #12596: Add new module unusedprivatefield

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedprivatefield/InputUnusedPrivateField4.java
@@ -0,0 +1,30 @@
+/*
+UnusedPrivateField
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.unusedprivatefield;
+
+public class InputUnusedPrivateField4 {
+
+    private int copyfield;
+
+    private int usedField;
+
+    void setCopyfield() {
+        int copy = copyfield;
+    }
+
+    void useField() {
+        System.out.println(usedField);
+    }
+
+
+    class Inner {
+        Inner(InputUnusedPrivateField4 InputUnusedPrivateField4.this) {
+        }
+    }
+
+    void m(InputUnusedPrivateField4 this) { }
+
+}
