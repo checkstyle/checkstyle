485efb51405df17fd61f53ca44677774b97bdd7b
gianmarco <gianmarco.schifone02@gmail.com>
2026-05-17 00:38:02 +0200

    Issue #19827: Remove chapter numbers from Doc Comments Coverage Page

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsPagesTest.java
@@ -1915,12 +1915,14 @@ public class XdocsPagesTest {
         for (Path path : XdocUtil.getXdocsStyleFilePaths(XdocUtil.getXdocsFilePaths())) {
             final String fileName = path.getFileName().toString();
             final String styleName = fileName.substring(0, fileName.lastIndexOf('_'));
+            if ("doc_comments".equals(styleName)) {
+                continue;
+            }
             final NodeList sources = getTagSourcesNode(path, "tr");
 
             final Set<String> styleChecks = switch (styleName) {
                 case "google" -> new HashSet<>(GOOGLE_MODULES);
                 case "openjdk" -> new HashSet<>(OPENJDK_MODULES);
-                case "doc_comments" -> new HashSet<>(DOC_COMMENTS_MODULES);
                 case "sun" -> {
                     final Set<String> checks = new HashSet<>(SUN_MODULES);
                     checks.removeAll(IGNORED_SUN_MODULES);
@@ -1966,19 +1968,7 @@ public class XdocsPagesTest {
                 lastRuleName = ruleName;
             }
 
-            // these modules aren't documented, but are added to the config
-            styleChecks.remove("BeforeExecutionExclusionFileFilter");
-            styleChecks.remove("SuppressionFilter");
-            styleChecks.remove("SuppressionXpathFilter");
-            styleChecks.remove("SuppressionXpathSingleFilter");
-            styleChecks.remove("TreeWalker");
-            styleChecks.remove("Checker");
-            styleChecks.remove("SuppressWithNearbyCommentFilter");
-            styleChecks.remove("SuppressionCommentFilter");
-            styleChecks.remove("SuppressWarningsFilter");
-            styleChecks.remove("SuppressWarningsHolder");
-            styleChecks.remove("SuppressWithNearbyTextFilter");
-            styleChecks.remove("SuppressWithPlainTextCommentFilter");
+            removeCommonUndocumentedModules(styleChecks);
             assertWithMessage(
                     "%s requires the following check(s) to appear: %s", fileName, styleChecks)
                 .that(styleChecks)
@@ -2088,9 +2078,8 @@ public class XdocsPagesTest {
         final Iterator<Node> itrConfigs = configs.iterator();
         final boolean isGoogleDocumentation = "google".equals(styleName);
         final boolean isOpenJdkDocumentation = "openjdk".equals(styleName);
-        final boolean isDocCommentsDocumentation = "doc_comments".equals(styleName);
 
-        if (isGoogleDocumentation || isOpenJdkDocumentation || isDocCommentsDocumentation) {
+        if (isGoogleDocumentation || isOpenJdkDocumentation) {
             validateChapterWiseTesting(itrChecks, itrConfigs, styleChecks, styleName, ruleName);
         }
         else {
@@ -2251,13 +2240,11 @@ public class XdocsPagesTest {
             final String extractedChapterNumber = getExtractedChapterNumber(ruleName);
             final String extractedSectionNumber = getExtractedSectionNumber(ruleName);
 
-            final String stylePackageName = getStylePackageName(styleName);
-
             assertWithMessage("%s_style.xml rule '%s' rule '' should have matching sample url",
                 styleName, ruleName)
                     .that(inputFolderUrl)
                     .startsWith("https://github.com/checkstyle/checkstyle/"
-                        + "tree/master/src/it/resources/com/" + stylePackageName
+                        + "tree/master/src/it/resources/com/" + styleName
                         + "/checkstyle/test/");
 
             assertWithMessage("%s_style.xml rule '%s' should have matching sample url",
@@ -2286,14 +2273,6 @@ public class XdocsPagesTest {
         }
     }
 
-    private static String getStylePackageName(String styleName) {
-        String result = styleName;
-        if ("doc_comments".equals(styleName)) {
-            result = "doccomments";
-        }
-        return result;
-    }
-
     private static String getExpectedStyleGuideUrl(String styleGuideName) {
         return "https://github.com/search?q=path%3Asrc%2Fmain%2Fresources%20path%3A**%2F"
             + styleGuideName
@@ -2312,6 +2291,152 @@ public class XdocsPagesTest {
         final Matcher matcher = pattern.matcher(ruleName);
         matcher.find();
         return matcher.group().replaceAll("\\.", "");
+    }
+
+    @Test
+    public void testDocCommentsStyleRules() throws Exception {
+        final Path path = Path.of("src/site/xdoc/doc_comments_style.xml");
+        final NodeList sources = getTagSourcesNode(path, "tr");
+        final Set<String> styleChecks = new HashSet<>(DOC_COMMENTS_MODULES);
+
+        for (int position = 0; position < sources.getLength(); position++) {
+            final Node row = sources.item(position);
+            final List<Node> columns = new ArrayList<>(
+                    XmlUtil.findChildElementsByTag(row, "td"));
+
+            if (columns.isEmpty()) {
+                continue;
+            }
+
+            final String ruleName = columns.get(1).getTextContent().trim();
+
+            validateDocCommentsStyleModules(XmlUtil.findChildElementsByTag(columns.get(2), "a"),
+                    XmlUtil.findChildElementsByTag(columns.get(3), "a"), styleChecks, ruleName);
+        }
+
+        removeCommonUndocumentedModules(styleChecks);
+        assertWithMessage(
+                "doc_comments_style.xml requires the following check(s) to appear: %s",
+                styleChecks)
+            .that(styleChecks)
+            .isEmpty();
+    }
+
+    private static void validateDocCommentsStyleModules(Set<Node> checks, Set<Node> samples,
+            Set<String> styleChecks, String ruleName) {
+        final Iterator<Node> itrChecks = checks.iterator();
+        boolean hasChecks = false;
+        final Set<String> usedModules = new HashSet<>();
+
+        while (itrChecks.hasNext()) {
+            final Node module = itrChecks.next();
+            final String moduleName = module.getTextContent().trim();
+            final String href = module.getAttributes().getNamedItem("href").getTextContent();
+            final boolean moduleIsCheck = href.startsWith("checks/");
+
+            final String partialConfigUrl = getExpectedStyleGuideUrl("doc_comments_checks.xml");
+
+            if (!moduleIsCheck) {
+                if (href.startsWith(partialConfigUrl)) {
+                    assertWithMessage(
+                        "doc_comments_style.xml rule '%s' module '%s' has too many config links",
+                        ruleName, moduleName).fail();
+                }
+                continue;
+            }
+
+            hasChecks = true;
+
+            assertWithMessage(
+                "The module '%s' in the rule '%s' of the style guide 'doc_comments_style.xml'"
+                    + " should not appear more than once in the section.",
+                moduleName, ruleName)
+                .that(usedModules)
+                .doesNotContain(moduleName);
+
+            usedModules.add(moduleName);
+
+            assertWithMessage("doc_comments_style.xml rule '%s' module '%s' shouldn't end"
+                    + " with 'Check'", ruleName, moduleName)
+                .that(moduleName.endsWith("Check"))
+                .isFalse();
+
+            styleChecks.remove(moduleName);
+
+            if (itrChecks.hasNext()) {
+                final Node config = itrChecks.next();
+
+                final String configUrl = config.getAttributes()
+                                       .getNamedItem("href").getTextContent();
+
+                final String expectedUrl = partialConfigUrl + moduleName;
+
+                assertWithMessage(
+                    "doc_comments_style.xml rule '%s' module '%s' should have matching config url",
+                    ruleName, moduleName)
+                    .that(configUrl)
+                    .isEqualTo(expectedUrl);
+            }
+            else {
+                assertWithMessage("doc_comments_style.xml rule '%s' module '%s' is missing the"
+                        + " config link", ruleName, moduleName).fail();
+            }
+        }
+
+        validateDocCommentsStyleSamples(samples.iterator(), hasChecks, ruleName);
+    }
+
+    private static void validateDocCommentsStyleSamples(Iterator<Node> itrSample,
+            boolean hasChecks, String ruleName) {
+        if (itrSample.hasNext()) {
+            assertWithMessage("doc_comments_style.xml rule '%s' should have checks if it has"
+                    + " sample links", ruleName)
+                    .that(hasChecks)
+                    .isTrue();
+
+            final Node sample = itrSample.next();
+            final String inputFolderUrl = sample.getAttributes().getNamedItem("href")
+                    .getTextContent();
+
+            assertWithMessage("doc_comments_style.xml rule '%s' should have matching sample url",
+                ruleName)
+                    .that(inputFolderUrl)
+                    .startsWith("https://github.com/checkstyle/checkstyle/"
+                        + "tree/master/src/it/resources/com/doccomments/checkstyle/test/");
+
+            assertWithMessage(
+                "doc_comments_style.xml rule '%s' should have a inputs test folder that exists",
+                ruleName)
+                    .that(new File(inputFolderUrl.substring(53).replace('/',
+                            File.separatorChar)).exists())
+                    .isTrue();
+
+            assertWithMessage("doc_comments_style.xml rule '%s' has too many samples link",
+                ruleName)
+                    .that(itrSample.hasNext())
+                    .isFalse();
+        }
+        else {
+            assertWithMessage("doc_comments_style.xml rule '%s' is missing sample link", ruleName)
+                .that(hasChecks)
+                .isFalse();
+        }
+    }
+
+    private static void removeCommonUndocumentedModules(Set<String> styleChecks) {
+        // these modules aren't documented, but are added to the config
+        styleChecks.remove("BeforeExecutionExclusionFileFilter");
+        styleChecks.remove("SuppressionFilter");
+        styleChecks.remove("SuppressionXpathFilter");
+        styleChecks.remove("SuppressionXpathSingleFilter");
+        styleChecks.remove("TreeWalker");
+        styleChecks.remove("Checker");
+        styleChecks.remove("SuppressWithNearbyCommentFilter");
+        styleChecks.remove("SuppressionCommentFilter");
+        styleChecks.remove("SuppressWarningsFilter");
+        styleChecks.remove("SuppressWarningsHolder");
+        styleChecks.remove("SuppressWithNearbyTextFilter");
+        styleChecks.remove("SuppressWithPlainTextCommentFilter");
     }
 
     @Test
