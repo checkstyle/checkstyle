f4c1cd5d41c52cca74cf88d78a06833c40821e3c
Aman Baliyan <baliyanaman200@gmail.com>
2026-05-17 17:55:42 +0530

    supplemental: fix Constructor order violations

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTag.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocTag.java
@@ -39,13 +39,9 @@ public class JavadocTag {
      * @param line the line number of the tag
      * @param column the column number of the tag
      * @param tag the tag string
-     * @param firstArg the tag argument
      **/
-    public JavadocTag(int line, int column, String tag, String firstArg) {
-        lineNo = line;
-        columnNo = column;
-        this.firstArg = firstArg;
-        tagInfo = JavadocTagInfo.fromName(tag);
+    public JavadocTag(int line, int column, String tag) {
+        this(line, column, tag, null);
     }
 
     /**
@@ -54,9 +50,13 @@ public class JavadocTag {
      * @param line the line number of the tag
      * @param column the column number of the tag
      * @param tag the tag string
+     * @param firstArg the tag argument
      **/
-    public JavadocTag(int line, int column, String tag) {
-        this(line, column, tag, null);
+    public JavadocTag(int line, int column, String tag, String firstArg) {
+        lineNo = line;
+        columnNo = column;
+        this.firstArg = firstArg;
+        tagInfo = JavadocTagInfo.fromName(tag);
     }
 
     /**
