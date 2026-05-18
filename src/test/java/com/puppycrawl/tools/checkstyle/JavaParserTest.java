cd3d94600153baf4d898f16efeb562ee9683a211
vivek-0509 <viveksolanki0509@gmail.com>
2026-05-19 02:14:17 +0530

    Issue #17810: Add grammar support for JDK 25 compact source files

--- src/test/java/com/puppycrawl/tools/checkstyle/JavaParserTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/JavaParserTest.java
@@ -211,7 +211,7 @@ public class JavaParserTest extends AbstractModuleTestSupport {
             assertWithMessage("Invalid exception message")
                 .that(exc.getCause().toString())
                 .isEqualTo(IllegalStateException.class.getName()
-                            + ": 2:0: no viable alternative at input 'classD'");
+                            + ": 2:9: no viable alternative at input 'classDa{'");
         }
     }
 
