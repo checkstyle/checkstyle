a76d8320b3494d5c4d3f9161f4c4c87d10db9b47
smita_1078 <smita.prajapati082@gmail.com>
2026-05-21 23:17:32 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - visibilitymodifier

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/design/visibilitymodifier/Example12.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/design/visibilitymodifier/Example12.java
@@ -12,18 +12,52 @@
 
 package com.puppycrawl.tools.checkstyle.checks.design.visibilitymodifier;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 // xdoc section -- start
 class Example12 {
+  private int myPrivateField1;
+
+  int field1; // violation, 'must be private'
+
+  protected String field2; // violation, 'must be private'
+
+  // violation below, 'must be private'
+  public int field3 = 42;
+
+  public long serialVersionUID = 1L;
+
+  public static final int field4 = 42;
+
+  public final int field5 = 42;
+
+  public final java.lang.String notes = null;
+
+  public final Set<String> mySet1 = new HashSet<>();
+
+  public final ImmutableSet<String> mySet2 = null;
+
+  public final ImmutableMap<String, Object> objects1 = null;
+
+  @java.lang.Deprecated
+  String annotatedString; // violation, 'must be private'
+
+  @Deprecated
+  // violation below, 'must be private'
+  String shortCustomAnnotated;
+
+  @com.google.common.annotations.VisibleForTesting
+  public String testString = "";
+
   public final int someIntValue = 0;
 
   public final ImmutableSet<String> includes = null;
-
-  public final java.lang.String notes = "";
 
   public final BigDecimal value = null;
 
