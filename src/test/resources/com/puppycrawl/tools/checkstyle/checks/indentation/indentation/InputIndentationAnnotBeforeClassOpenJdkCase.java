000014b26c66eec9d5ba770d3ae05dfdfc5e0caf
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-05-21 04:09:04 +0100

    Issue #14716: fix false positives when class declaration is wrapped after public and before static

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationAnnotBeforeClassOpenJdkCase.java
@@ -0,0 +1,6 @@
+package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;
+
+public class InputIndentationAnnotBeforeClassOpenJdkCase {
+   @SuppressWarnings("deprecation")                                        //indent:3 exp:4 warn
+   private static final class TlsMasterSecretKey {}                        //indent:3 exp:4 warn
+}
