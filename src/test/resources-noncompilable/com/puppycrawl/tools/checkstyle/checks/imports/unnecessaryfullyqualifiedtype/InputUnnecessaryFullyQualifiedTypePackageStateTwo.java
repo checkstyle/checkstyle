9e7b0d0187aee4bd169340daf3ef0221a00f962c
Alex Bertram <alex@bedatadriven.com>
2026-05-23 21:01:16 +0200

    Issue #19885: New Check: UnnecessaryFullyQualifiedTypeCheck

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/unnecessaryfullyqualifiedtype/InputUnnecessaryFullyQualifiedTypePackageStateTwo.java
@@ -0,0 +1,11 @@
+/*
+UnnecessaryFullyQualifiedType
+
+
+*/
+// non-compiled with javac: Compilable with Java25
+// non-compiled with javac: missing package. Used for Testing purpose.
+private com.puppycrawl.tools.checkstyle.checks.imports.unnecessaryfullyqualifiedtype.Baz baz;
+private Baz baz2;
+
+
