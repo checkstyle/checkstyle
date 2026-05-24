3a785e7090e21ec539baba0a0fa311286ff598ec
smita_1078 <smita.prajapati082@gmail.com>
2026-05-25 00:36:29 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - indentation

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/Example2.java
@@ -12,15 +12,48 @@ package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;
 
 // xdoc section -- start
 class Example2 {
-    void method(int x) {
-        switch (x) {
-        case 1:             // ok, caseIndent = 0 (same level as switch body)
-            break;
-        case 2:             // ok
-            break;
-            case 3:         // violation, 'level 12, expected level should be 8'
-            break;
+    int a;
+    boolean x, y;
+    String field = "example";
+    int[] values = {
+        1, 2, 3
+    };
+    int[] values2 = {
+  10 // violation, 'level 2, expected level should be 8'
+    };
+
+    void method2()
+        throws Exception {
+        switch (a) {
+            case 1:             // violation, 'level 12, expected level should be 8'
+                break;          // violation, 'level 16, expected level should be 12'
+            case 2:             // violation, 'level 12, expected level should be 8'
+                break;          // violation, 'level 16, expected level should be 12'
         }
+    }
+    void method3(int a,
+                 int b) {
+        if (x
+                && y) {
+            method3(a, b);
+        }
+    }
+
+    void handleValue(String aFooString,
+                     int aFooInt) {
+
+        boolean cond1,cond2,cond3,cond4,cond5,cond6;
+
+        cond1=cond2=cond3=cond4=cond5=cond6=false;
+        if (cond1
+                || cond2) {
+            field = field.toUpperCase()
+                    .concat(" TASK");
+        }
+    }
+
+    void methodBrace()
+    {
     }
 }
 // xdoc section -- end
