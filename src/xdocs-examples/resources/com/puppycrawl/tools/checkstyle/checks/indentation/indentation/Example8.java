b531d564d4b8296da3fef2ade719d0f202ad1dae
smita_1078 <smita.prajapati082@gmail.com>
2026-05-25 00:36:29 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - indentation

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/Example8.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/Example8.java
@@ -12,14 +12,48 @@ package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;
 
 // xdoc section -- start
 class Example8 {
-    int[] values1 = {
-      10,                 // ok, arrayInitIndent = 2 (col 6 = array col 4 + 2)
-      20,
-      30
+    int a;
+    boolean x, y;
+    String field = "example";
+    int[] values = {
+        1, 2, 3
+    };
+    int[] values2 = {
+  10 // violation, 'level 2, expected level should be one of'
     };
 
-    int[] values2 = {
-  10                      // violation, 'level 2, expected level should be one of'
-    };
+    void method2()
+        throws Exception {
+        switch (a) {
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+    void method3(int a,
+                 int b) {
+        if (x
+                && y) {
+            method3(a, b);
+        }
+    }
+
+    void handleValue(String aFooString,
+                     int aFooInt) {
+
+        boolean cond1,cond2,cond3,cond4,cond5,cond6;
+
+        cond1=cond2=cond3=cond4=cond5=cond6=false;
+        if (cond1
+                || cond2) {
+            field = field.toUpperCase()
+                    .concat(" TASK");
+        }
+    }
+
+    void methodBrace()
+    {
+    }
 }
 // xdoc section -- end
