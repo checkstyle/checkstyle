59f9be0d6fb08e249c08babb4d2cfa34523d94a5
smita_1078 <smita.prajapati082@gmail.com>
2026-05-25 21:19:11 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - whitespacearound

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/Example4.java
@@ -14,8 +14,34 @@ package com.puppycrawl.tools.checkstyle.checks.whitespace.whitespacearound;
 
 // xdoc section -- start
 class Example4 {
-  public void muFunction() {}
-  int a=4; // 2 violations
-  // no space before and after '='
+  public Example4(){} // 3 violations
+  // no space after ')' and '{', no space before '}'
+  int y = 0;
+  int a = 4;
+
+  void example() {
+    Runnable noop = () ->{}; // 4 violations
+    // no space after '->' and '{', no space before '{' and '}'
+    try { }
+    catch (Exception e){} // 3 violations
+    // no space after ')' and '{', no space before '}'
+    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+    for (char item: vowels) { }
+    for (int i = 100;i > 10; i--){} // 3 violations
+    // no space before '{', no space after '{', no space before '}'
+    do {} while (y == 1); // 2 violations
+    // no space after '{', no space before '}'
+    int i = 0;
+    switch (i) {
+      case 1: {} // 2 violations
+      // no space after '{', no space before '}'
+    }
+    int a=4; // 2 violations
+    // no space before '=', no space after '='
+  }
+
+  void myFunction() {}
+
+  void myFunction2() { }
 }
 // xdoc section -- end
