a7f5ba95d12e2bb34cb4182ebccc0564ac10e1ed
lukman48 <lukman_uki@yahoo.co.id>
2026-05-26 08:32:36 +0700

    supplemental: add test and input file for switch expression wrapped case labels (issue #17270)

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentation17270b.java
@@ -0,0 +1,17 @@
+package com.puppycrawl.tools.checkstyle.checks.indentation.indentation; //indent:0 exp:0
+
+public class InputIndentation17270b {                                   //indent:0 exp:0
+    private void test() {                                               //indent:4 exp:4
+        int v = 0;                                                      //indent:8 exp:8
+        System.out.println(switch (v) {                                 //indent:8 exp:8
+            case 0 -> "a";                                              //indent:12 exp:12
+            case 1, 2,                                                  //indent:12 exp:12
+              3, 4 -> "under by 2";                                     //indent:14 exp:16 warn
+            case 5, 6,                                                  //indent:12 exp:12
+                7, 8 -> "correct";                                      //indent:16 exp:16
+            case 9, 10,                                                 //indent:12 exp:12
+               11, 12 -> "under by 1";                                  //indent:15 exp:16 warn
+            default -> "d";                                             //indent:12 exp:12
+        });                                                             //indent:8 exp:8
+    }                                                                   //indent:4 exp:4
+}                                                                       //indent:0 exp:0
