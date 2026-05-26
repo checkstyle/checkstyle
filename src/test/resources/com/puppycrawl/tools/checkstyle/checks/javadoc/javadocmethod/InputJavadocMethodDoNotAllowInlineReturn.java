900429aca8b9773524c7b2370bf7263ab31c47ec
gianmarco <gianmarco.schifone02@gmail.com>
2026-05-26 10:52:53 +0200

    Issue #19144: update JavadocMethod to use AST of javadoc

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodDoNotAllowInlineReturn.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethodDoNotAllowInlineReturn.java
@@ -6,8 +6,8 @@ validateThrows = (default)false
 accessModifiers = (default)public, protected, package, private
 allowMissingParamTags = (default)false
 allowMissingReturnTag = (default)false
+violateExecutionOnNonTightHtml = (default)false
 tokens = (default)METHOD_DEF, CTOR_DEF, ANNOTATION_FIELD_DEF, COMPACT_CTOR_DEF
-
 
 */
 
