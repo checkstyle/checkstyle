73091b8c589dfc29b6d47ff31d32691a1cfd8fe3
Tomas Viveiros Nunes <tomas.viveiros.nunes@tecnico.ulisboa.pt>
2026-05-28 23:53:38 +0100

    Issue #8312: Added control flow tokens to NoWhiteSpaceAfterCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nowhitespaceafter/InputNoWhitespaceAfterLiteralIf.java
@@ -0,0 +1,21 @@
+/*
+NoWhitespaceAfter
+allowLineBreaks = (default)true
+tokens = LITERAL_IF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.nowhitespaceafter;
+
+public class InputNoWhitespaceAfterLiteralIf {
+    void test(boolean condition) {
+        if(condition) {
+        }
+        if (condition) { // violation, ''if' is followed by whitespace.'
+        }
+        if
+            (condition) {
+        }
+    }
+}
