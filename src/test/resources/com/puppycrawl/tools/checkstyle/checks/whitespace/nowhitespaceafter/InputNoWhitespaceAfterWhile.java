9b2d917f302aea2693f94584560506d502f1e64f
Tomas Viveiros Nunes <tomas.viveiros.nunes@tecnico.ulisboa.pt>
2026-05-28 23:53:38 +0100

    Issue #8312: Added control flow tokens to NoWhiteSpaceAfterCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/nowhitespaceafter/InputNoWhitespaceAfterWhile.java
@@ -0,0 +1,22 @@
+/*
+NoWhitespaceAfter
+allowLineBreaks = (default)true
+tokens = LITERAL_WHILE
+
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.whitespace.nowhitespaceafter;
+
+public class InputNoWhitespaceAfterWhile {
+
+    boolean condition = false;
+
+    void test() {
+
+        while (condition) { // violation, ''while' is followed by whitespace.'
+        }
+
+        while(condition) {
+        }
+    }
+}
