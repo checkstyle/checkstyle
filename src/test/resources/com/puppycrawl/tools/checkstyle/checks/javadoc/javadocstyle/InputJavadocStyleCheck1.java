0ed57f3ebab8343bc16da850fe14d74f3e3f7a09
adityaaries23 <adiditya52@gmail.com>
2026-05-29 23:29:33 +0700

    Issue #19764: Move violation comment out of Javadoc

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocstyle/InputJavadocStyleCheck1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocstyle/InputJavadocStyleCheck1.java
@@ -21,12 +21,13 @@ public class InputJavadocStyleCheck1 {
    /** NM Webapp address.**/
    public static final String NM_WEBAPP_ADDRESS = "webapp.address";
 
-    /** // violation 'First sentence should end with a period'
+    // violation 2 lines below 'First sentence should end with a period'
+    // violation 3 lines below 'Unclosed HTML tag found: .*'
+    /**
      * <thead> <br> </thead>
      * <tfoot> <AREA ALT="alt" Coordination="100,0,200,50" HREF="/href/"> </tfoot> comment
      * <p> </p>
      * @param field4 value to which {@link #field4} is to be set to
      */
-    // violation 4 lines above 'Unclosed HTML tag found: .*'
     private void setField4(int field4) {this.field4 = field4;}
 }
