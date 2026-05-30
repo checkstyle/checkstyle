8e049bbd135e1cecb5f8ebaa43f431fd7a9a6aa9
Aman-Baliyan <baliyanaman200@gmail.com>
2026-05-30 15:32:01 +0530

    Issue #19623: Adding remaining modules

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterformatting/ruleimportstatements/importnolinewrap/InputImportNoLineWrap.java
@@ -0,0 +1,19 @@
+package com.openjdk.checkstyle.test.chapterformatting.ruleimportstatements.importnolinewrap;
+
+// violation below 'should not be line-wrapped'
+import java.util.
+        HashMap;
+import java.util.Map;
+import java.util.
+        Set;
+// violation 2 lines above 'should not be line-wrapped'
+
+/**
+ * Test input for the import no line wrap rule.
+ */
+public class InputImportNoLineWrap {
+
+    Map<Integer, Integer> map = new HashMap<>();
+    Set<Integer> set;
+
+}
