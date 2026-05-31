ec529f1ce693f3d64eb35511f3a6831370472784
wuyangfan <1102042793@qq.com>
2026-06-01 02:50:29 +0800

    Issue #19764: move violation comments out of Javadoc in InputIllegalTokensCheckCommentsContent
    Part of #19764.

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalTokenCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/coding/IllegalTokenCheckTest.java
@@ -71,8 +71,7 @@ public class IllegalTokenCheckTest
             "1:3: " + getCheckMessage(MSG_KEY,
                         "\\nIllegalToken\\ntokens = COMMENT_CONTENT\\n\\n\\n"),
             "10:3: " + getCheckMessage(MSG_KEY,
-                        "*\\n * // violation 10"
-                            + " lines above 'is not allowed'\\n"
+                        "\\n * // violation 10 lines above 'is not allowed'\\n"
                             + " * // violation 2 lines above 'is not allowed'\\n"
                             + " * Test for illegal tokens\\n "),
             "40:29: " + getCheckMessage(MSG_KEY,
--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltoken/InputIllegalTokensCheckCommentsContent.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltoken/InputIllegalTokensCheckCommentsContent.java
@@ -7,7 +7,7 @@ tokens = COMMENT_CONTENT
 
 package com.puppycrawl.tools.checkstyle.checks.coding.illegaltoken;
 
-/**
+/*
  * // violation 10 lines above 'is not allowed'
  * // violation 2 lines above 'is not allowed'
  * Test for illegal tokens
