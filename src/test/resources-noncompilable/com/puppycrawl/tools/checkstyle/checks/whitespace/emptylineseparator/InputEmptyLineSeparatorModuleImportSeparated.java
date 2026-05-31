cfc66e14d716e23ecfdea19ff33ded1a9ea3b99a
vivek-0509 <viveksolanki0509@gmail.com>
2026-05-31 21:37:28 +0530

    Issue #20111: Add MODULE_IMPORT support to EmptyLineSeparator

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorModuleImportSeparated.java
@@ -0,0 +1,22 @@
+/*
+EmptyLineSeparator
+allowNoEmptyLineBetweenFields = (default)false
+allowMultipleEmptyLines = (default)true
+allowMultipleEmptyLinesInsideClassMembers = (default)true
+tokens = (default)PACKAGE_DEF, IMPORT, STATIC_IMPORT, MODULE_IMPORT, CLASS_DEF, \
+         INTERFACE_DEF, ENUM_DEF, STATIC_INIT, INSTANCE_INIT, METHOD_DEF, CTOR_DEF, \
+         VARIABLE_DEF, RECORD_DEF, COMPACT_CTOR_DEF
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator;
+
+import module java.base;
+import java.util.List;
+import static java.lang.Math.PI;
+
+public class InputEmptyLineSeparatorModuleImportSeparated {
+}
