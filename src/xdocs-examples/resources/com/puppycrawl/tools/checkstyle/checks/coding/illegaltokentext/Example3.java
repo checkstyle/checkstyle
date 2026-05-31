283d105825ec0d35638f1e847297584abdacfe24
smita_1078 <smita.prajapati082@gmail.com>
2026-05-31 20:19:41 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - illegaltoekntext

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example3.java
@@ -11,11 +11,23 @@
 
 // Java17
 package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
+
 // xdoc section -- start
 public class Example3 {
   public void myTest() {
+
+    String test  = "a href";
+
+    String test2 = "A href";
+    String link = "href";
     final String quote = """
             \""""; // violation above 'Token text matches the illegal pattern '"'.'
+    int num1 = 0;
+    int num2 = 0x111;
+    int num3 = 0X111;
+    int num4 = 010;
+    long num5 = 0L;
+    long num6 = 010L;
   }
 }
 // xdoc section -- end
