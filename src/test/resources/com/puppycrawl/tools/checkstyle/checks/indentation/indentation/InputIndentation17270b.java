8d8bc05ae4833ff1357ad4faf344768d336cb4b5
lukman48 <lukman_uki@yahoo.co.id>
2026-05-31 13:41:31 +0700

    supplemental: add wrapped case label test for indent 15 (issue #17270)

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentation17270b.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentation17270b.java
@@ -11,6 +11,8 @@ public class InputIndentation17270b {                                   //indent
                 7, 8 -> "correct";                                      //indent:16 exp:16
             case 9, 10,                                                 //indent:12 exp:12
                11, 12 -> "under by 1";                                  //indent:15 exp:16 warn
+            case 13, 14,                                                 //indent:12 exp:12
+               15, 16 -> "under by 1";                                  //indent:15 exp:16 warn
             default -> "d";                                             //indent:12 exp:12
         });                                                             //indent:8 exp:8
     }                                                                   //indent:4 exp:4
