ea5e593b8bbc5b6d8fd511fe873cf30c6df42ce7
mahfouz72 <mohamadmahfouz39@gmail.com>
2026-06-02 16:24:44 +0300

    Issue #20206: Add support for URI fragments in javadoc references

--- src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocCommentsAstRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocCommentsAstRegressionTest.java
@@ -259,4 +259,10 @@ public class JavadocCommentsAstRegressionTest extends AbstractTreeTestSupport {
         verifyJavadocTree(getHtmlTagsPath("ExpectedHtmlComment.txt"),
                 getHtmlTagsPath("InputHtmlComment.javadoc"));
     }
+    
+    @Test
+    public void testReferencesToURIFragments() throws Exception {
+        verifyJavadocTree(getPath("ExpectedReferencesToURIFragments.txt"),
+                getPath("InputReferencesToURIFragments.javadoc"));
+    }
 }
