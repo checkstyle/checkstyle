cccafededf712542348bc0c8cf10970b64287ce1
Zhu Tianyi <lithopszty@outlook.com>
2026-06-02 23:46:20 +0800

    Issue #19150: Update WriteTagCheck to use AST of Javadoc

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/InputWriteTagSeverity.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/writetag/InputWriteTagSeverity.java
@@ -4,6 +4,7 @@ tag = @author
 tagFormat = \\S
 tagSeverity = error
 tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
+violateExecutionOnNonTightHtml = (default)false
 
 
 */
