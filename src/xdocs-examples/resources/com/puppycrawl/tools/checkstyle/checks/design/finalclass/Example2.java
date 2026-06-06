78c17b32376e0555ab3fa50b31631b6cdf9f1178
Benjamín-Serrano <167205322+BenjaminGaelSerrano@users.noreply.github.com>
2026-06-06 01:09:36 -0300

    Issue #15562: FinalClass: add ignoreAnnotatedBy property

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/design/finalclass/Example2.java
@@ -0,0 +1,36 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="FinalClass">
+      <property name="ignoreAnnotatedBy"
+        value="Configuration, java.lang.Deprecated" />
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.design.finalclass;
+
+// xdoc section -- start
+@java.lang.Deprecated // ok, skipped by annotation
+class Example2 {
+
+  private Example2() {
+  }
+}
+
+class Example2A { // violation, 'Class Example2A should be declared as final.'
+
+  private Example2A() {
+  }
+}
+
+@Configuration // ok, skipped by annotation
+class Example2B {
+
+  private Example2B() {
+  }
+}
+// xdoc section -- end
+
+@interface Configuration {}
