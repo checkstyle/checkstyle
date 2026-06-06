f48bac1834b1ac014f46094a3af69f31016c56d9
smita_1078 <smita.prajapati082@gmail.com>
2026-06-06 21:37:42 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - localfinalvariablename

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/localfinalvariablename/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/localfinalvariablename/Example1.java
@@ -10,13 +10,18 @@
 
 package com.puppycrawl.tools.checkstyle.checks.naming.localfinalvariablename;
 
+import java.util.Scanner;
+
 // xdoc section -- start
-class Example1{
+class Example1 {
   void MyMethod() {
-    try {
-      final int VAR1 = 5; // violation 'Name 'VAR1' must match pattern'
+    try (Scanner scanner = new Scanner(System.in)) {
+
+      final int VAR1 = 5;  // violation 'Name 'VAR1' must match pattern'
       final int var1 = 10;
-    } catch (Exception ex) {
+    }
+    catch (final Exception ex) {
+
       final int VAR2 = 15; // violation 'Name 'VAR2' must match pattern'
       final int var2 = 20;
     }
