c104a68debcdf9f2720a6e4e4c0cbbafb23bb17e
smita_1078 <smita.prajapati082@gmail.com>
2026-06-06 12:02:57 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - lambdabodylength

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/sizes/lambdabodylength/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/sizes/lambdabodylength/Example2.java
@@ -6,33 +6,42 @@
     </module>
   </module>
 </module>
-
-
 */
 
 package com.puppycrawl.tools.checkstyle.checks.sizes.lambdabodylength;
 
 // xdoc section -- start
 class Example2 {
-  Runnable r = () -> { // ok, length is 5
-    System.out.println(2);
+  // violation below 'Lambda body length is 10 lines (max allowed is 5).'
+  Runnable r = () -> {
+    System.out.println(2); // line 2 of lambda
     System.out.println(3);
     System.out.println(4);
-  };
-  // violation below 'Lambda body length is 6 lines (max allowed is 5).'
+    System.out.println(5);
+    System.out.println(6);
+    System.out.println(7);
+    System.out.println(8);
+    System.out.println(9);
+  }; // line 10
+  // violation below 'Lambda body length is 11 lines (max allowed is 5.'
   Runnable r2 = () -> {
     System.out.println(2); // line 2 of lambda
     System.out.println(3);
     System.out.println(4);
     System.out.println(5);
-  };
+    System.out.println(6);
+    System.out.println(7);
+    System.out.println(8);
+    System.out.println(9);
+    System.out.println(10);
+  }; // line 11
   // violation below 'Lambda body length is 6 lines (max allowed is 5).'
   Runnable r3 = () ->
-    "someString".concat("1")
-                .concat("2")
-                .concat("3")
-                .concat("4")
-                .concat("5")
-                .concat("6");
+          "someString".concat("1")
+                  .concat("2")
+                  .concat("3")
+                  .concat("4")
+                  .concat("5")
+                  .concat("6");
 }
 // xdoc section -- end
