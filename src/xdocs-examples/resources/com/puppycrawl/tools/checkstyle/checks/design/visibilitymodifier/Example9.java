794291517180ef7bbe1afbc72e129fcb7cce794e
vivek-0509 <viveksolanki0509@gmail.com>
2026-06-07 18:56:35 +0530

    Issue #20269: Fix UnnecessarySemicolonAfterOuterTypeDeclaration false positive on JEP 512 compact source files

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/design/visibilitymodifier/Example9.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/design/visibilitymodifier/Example9.java
@@ -48,9 +48,8 @@ class Example9 {
   String annotatedString; // violation, annotation not configured 'must be private'
 
   @Deprecated
-  // violation below, annotation not configured 'must be private'
   String shortCustomAnnotated;
-
+  // violation above, annotation not configured 'must be private'
   @com.google.common.annotations.VisibleForTesting
   public String testString = "";
 }
