503cc00a03488a769d68310ef12d1c482acc7cdb
smita_1078 <smita.prajapati082@gmail.com>
2026-06-07 17:45:35 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - jacadocmethod

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocMethodCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/javadoc/JavadocMethodCheckExamplesTest.java
@@ -36,11 +36,12 @@ public class JavadocMethodCheckExamplesTest extends AbstractExamplesModuleTestSu
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-            "14:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
-            "17: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
-            "17:21: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
-            "25: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
-            "30:15: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "16:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
+            "19: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "19:21: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "30: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "36:15: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "41: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
         };
 
         verifyWithInlineConfigParser(getPath("Example1.java"), expected);
@@ -49,7 +50,8 @@ public class JavadocMethodCheckExamplesTest extends AbstractExamplesModuleTestSu
     @Test
     public void testExample2() throws Exception {
         final String[] expected = {
-            "20: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "22: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "44: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
         };
 
         verifyWithInlineConfigParser(getPath("Example2.java"), expected);
@@ -64,9 +66,9 @@ public class JavadocMethodCheckExamplesTest extends AbstractExamplesModuleTestSu
     @Test
     public void testExample4() throws Exception {
         final String[] expected = {
-            "16:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
-            "19:21: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
-            "32:15: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "18:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
+            "21:21: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "38:15: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
         };
 
         verifyWithInlineConfigParser(getPath("Example4.java"), expected);
@@ -75,10 +77,11 @@ public class JavadocMethodCheckExamplesTest extends AbstractExamplesModuleTestSu
     @Test
     public void testExample5() throws Exception {
         final String[] expected = {
-            "16:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
-            "19: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
-            "19:21: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
-            "32:15: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "18:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
+            "21: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "21:21: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "38:15: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "43: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
         };
 
         verifyWithInlineConfigParser(getPath("Example5.java"), expected);
@@ -87,7 +90,7 @@ public class JavadocMethodCheckExamplesTest extends AbstractExamplesModuleTestSu
     @Test
     public void testExample6() throws Exception {
         final String[] expected = {
-            "16:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
+            "18:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
         };
 
         verifyWithInlineConfigParser(getPath("Example6.java"), expected);
@@ -96,10 +99,13 @@ public class JavadocMethodCheckExamplesTest extends AbstractExamplesModuleTestSu
     @Test
     public void testExample7() throws Exception {
         final String[] expected = {
-            "31:17: " + getCheckMessage(MSG_EXPECTED_TAG, "@throws", "FileNotFoundException"),
-            "64:17: " + getCheckMessage(MSG_EXPECTED_TAG, "@throws", "IllegalArgumentException"),
-            "67:17: " + getCheckMessage(MSG_EXPECTED_TAG, "@throws", "IllegalStateException"),
-            "80:19: " + getCheckMessage(MSG_EXPECTED_TAG, "@throws", "IllegalStateException"),
+            "18:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
+            "21: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "21:21: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "21:32: " + getCheckMessage(MSG_EXPECTED_TAG, "@throws", "IOException"),
+            "32: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "38:15: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "43: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
         };
 
         verifyWithInlineConfigParser(getPath("Example7.java"), expected);
@@ -107,7 +113,13 @@ public class JavadocMethodCheckExamplesTest extends AbstractExamplesModuleTestSu
 
     @Test
     public void testExample8() throws Exception {
-        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+        final String[] expected = {
+            "18:16: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "x"),
+            "21: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "21:21: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+            "32: " + getCheckMessage(MSG_RETURN_EXPECTED, "@return"),
+            "38:15: " + getCheckMessage(MSG_EXPECTED_TAG, "@param", "p1"),
+        };
         verifyWithInlineConfigParser(getPath("Example8.java"), expected);
     }
 }
