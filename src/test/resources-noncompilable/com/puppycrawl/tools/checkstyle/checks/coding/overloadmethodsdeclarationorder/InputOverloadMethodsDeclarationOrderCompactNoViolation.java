7851e41e7de98903e40ce790439af7de94329f23
Ved Darji <veddarji2708@gmail.com>
2026-06-08 23:21:04 +0530

    Issue #20228: Add check support for Java 25 compact source files

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/overloadmethodsdeclarationorder/InputOverloadMethodsDeclarationOrderCompactNoViolation.java
@@ -0,0 +1,10 @@
+/*
+OverloadMethodsDeclarationOrder
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.coding.overloadmethodsdeclarationorder;
+
+void process(int x) { }
+void process(String s) { }
+void main() { }
