30343486b676c52f8dbb72a7ee5f4ee30a12cd90
smita_1078 <smita.prajapati082@gmail.com>
2026-06-08 15:21:35 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - npathcomplexityCheck

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/npathcomplexity/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/npathcomplexity/Example1.java
@@ -9,52 +9,48 @@ package com.puppycrawl.tools.checkstyle.checks.metrics.npathcomplexity;
 
 // xdoc section -- start
 public abstract class Example1 {
-  final int a = 0;
   int b = 0;
+  public void foo() {
+    int a,b,t,m,n;
+    a=b=t=m=n = 0;
 
-  public void foo() { // ok, NPath complexity is less than default threshold
-    // function consists of one if-else block with an NPath Complexity of 3
-    if (a > 10) {
-      if (a > b) { // nested if-else decision tree adds 2 to the complexity count
-        buzz();
-      } else {
-        fizz();
-      }
-    } else { // last possible outcome of the main if-else block, adds 1 to complexity
-      buzz();
-    }
+    print(a > b ? bar() : baz()); // 7 ternary statements multiply complexity by 2^7
+    print(t > 1 ? bar() : baz());
+    print(t > 2 ? bar() : baz());
+    print(t > 3 ? bar() : baz());
+    print(t > 4 ? bar() : baz());
+    print(t > 5 ? bar() : baz());
+    print(m > n ? baz() : bar());
   }
 
   public void boo() { // violation, NPath complexity is 217 (max allowed is 200)
     // looping through 3 switch statements produces 6^3 + 1 (217) possible outcomes
     for(int i = 0; i < b; i++) { // for statement adds 1 to final complexity
       switch(i) { // each independent switch statement multiplies complexity by 6
-        case a:
-          // ternary with && adds 3 to switch's complexity
-          print(f(i) && g(i) ? fizz() : buzz());
-        default:
-          // ternary with || adds 3 to switch's complexity
-          print(f(i) || g(i) ? fizz() : buzz());
+        case 1:    // ternary with && adds 3 to switch's complexity
+          print(f(i) && g(i) ? bar() : baz());
+        default:   // ternary with || adds 3 to switch's complexity
+          print(f(i) || g(i) ? bar() : baz());
       }
       switch(i - 1) { // multiplies complexity by 6
-        case a:
-          print(f(i) && g(i) ? fizz() : buzz());
+        case 1:
+          print(f(i) && g(i) ? bar() : baz());
         default:
-          print(f(i) || g(i) ? fizz() : buzz());
+          print(f(i) || g(i) ? bar() : baz());
       }
       switch(i + 1) { // multiplies complexity by 6
-        case a:
-          print(f(i) && g(i) ? fizz() : buzz());
+        case 1:
+          print(f(i) && g(i) ? bar() : baz());
         default:
-          print(f(i) || g(i) ? fizz() : buzz());
+          print(f(i) || g(i) ? bar() : baz());
       }
     }
   }
 
   public abstract boolean f(int x);
   public abstract boolean g(int x);
-  public abstract String fizz();
-  public abstract String buzz();
+  public abstract String bar();
+  public abstract String baz();
   public abstract void print(String str);
 }
 // xdoc section -- end
