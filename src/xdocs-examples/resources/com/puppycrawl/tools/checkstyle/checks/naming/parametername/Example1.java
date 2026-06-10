e63933cddbeff2ecc64ccd02d0fa6545cae871cb
smita_1078 <smita.prajapati082@gmail.com>
2026-06-10 07:45:54 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - parametername

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/parametername/Example1.java
@@ -10,10 +10,11 @@ package com.puppycrawl.tools.checkstyle.checks.naming.parametername;
 
 // xdoc section -- start
 class Example1 {
+  void method0(int v) {}
   void method1(int v1) {}
-  void method2(int V2) {} // violation 'Name 'V2' must match pattern'
+  void method2(int V2) {}            // violation 'Name 'V2' must match pattern'
   @Override
-  public boolean equals(Object v3) {
+  public boolean equals(Object V3) { // violation 'Name 'V3' must match pattern'
     return true;
   }
 }
