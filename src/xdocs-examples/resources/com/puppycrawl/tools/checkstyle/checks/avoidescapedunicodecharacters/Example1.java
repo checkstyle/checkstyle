7c7f5e50ce0f6d768fededb1b275a6e881cf9856
smita_1078 <smita.prajapati082@gmail.com>
2026-06-11 23:29:22 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - avoidescapeunicodecharacters

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/avoidescapedunicodecharacters/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/avoidescapedunicodecharacters/Example1.java
@@ -17,7 +17,7 @@ public class Example1 {
   // violation below, 'Unicode escape(s) usage should be avoided.'
   String unitAbbrev2 = "\u03bc\u03bc\u03bc";
   // violation below, 'Unicode escape(s) usage should be avoided.'
-  String unitAbbrev3 = "\u03bcs"; // it is  μs
+  String unitAbbrev3 = "\u03bcs"; // it is μs
   // violation below, 'Unicode escape(s) usage should be avoided.'
   String unitAbbrev4 = "\u03bc\u03bcs";
   public static int content() {
