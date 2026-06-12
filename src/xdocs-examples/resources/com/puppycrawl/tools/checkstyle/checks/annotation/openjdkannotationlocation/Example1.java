aedc9846f76544a126801531adfee9639c744ff7
Aman-Baliyan <baliyanaman200@gmail.com>
2026-06-12 15:13:25 +0530

    Issue #20209: Added new check OpenjdkAnnotationLocation

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/openjdkannotationlocation/Example1.java
@@ -0,0 +1,55 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="OpenjdkAnnotationLocation"/>
+  </module>
+</module>
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.openjdkannotationlocation;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+// xdoc section -- start
+class Example1 {
+  @Nonnull @Deprecated
+  public void foo1() {
+  }
+
+  @Deprecated public void test() {}
+
+  @Nonnull
+  @Deprecated @SafeVarargs
+  public final void badMethod(String... str) {
+  // violation above """Annotations on 'badMethod' must be all on one line or
+  // all on separate lines."""
+  }
+
+  @Nonnull
+  @Deprecated
+  @SafeVarargs
+  public final void goodMethodOne(String... str) {
+  }
+
+  @Nonnull @Deprecated @SafeVarargs
+  public final void goodMethodTwo(String... str) {
+  }
+
+  @Nonnull @Deprecated public void fooBad() {
+  // violation above 'Annotations must be on a separate line from 'fooBad'.'
+  }
+
+  @Deprecated @Nonnull public void fooGood() {}
+  @Deprecated int test;
+
+  // ok for Nullable and Deprecated due to checkstyle limitations
+  public @Nullable @Deprecated List<String> getItemsOrNull(List<String> items) {
+    return items;
+  }
+
+}
+// xdoc section -- end
