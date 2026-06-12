d7ae89dd0948f4ba901e9958ca35a28b29b39dda
Aman-Baliyan <baliyanaman200@gmail.com>
2026-06-12 15:13:25 +0530

    Issue #20209: Added new check OpenjdkAnnotationLocation

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/openjdkannotationlocation/InputOpenjdkAnnotationLocation4.java
@@ -0,0 +1,42 @@
+/*
+OpenjdkAnnotationLocation
+tokens = (default)CLASS_DEF, INTERFACE_DEF, PACKAGE_DEF, ENUM_CONSTANT_DEF, \
+         ENUM_DEF, METHOD_DEF, CTOR_DEF, VARIABLE_DEF, RECORD_DEF, COMPACT_CTOR_DEF
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.openjdkannotationlocation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Target;
+
+public class InputOpenjdkAnnotationLocation4 {
+
+    @Annotation void exampleGood() {}
+
+    // violation below, 'Annotation 'Annotation' must be on a separate line from target.'
+    @Annotation public
+            String method() {
+                 return "";
+            }
+
+    void parameterlessSamelineInForEach() {
+        for (@Annotation Object o : new Object[0]) break;
+        for (@Annotation @Annotation Object o : new Object[0]) break;
+        for (@Annotation Object o;;) break;
+        for (@Annotation @Annotation Object o;;) break;
+    }
+
+    @Repeatable(Annotations.class)
+    @Target({ElementType.METHOD, ElementType.LOCAL_VARIABLE})
+    @interface Annotation {
+        String value() default "";
+    }
+
+    @Target({ElementType.METHOD, ElementType.LOCAL_VARIABLE})
+    @interface Annotations {
+        Annotation[] value();
+    }
+
+}
