59683ad620d84fb0aa1b3465b0556c55f1480583
Aman-Baliyan <baliyanaman200@gmail.com>
2026-06-12 15:13:25 +0530

    Issue #20209: Added new check OpenjdkAnnotationLocation

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/annotation/openjdkannotationlocation/InputOpenjdkAnnotationLocation.java
@@ -0,0 +1,43 @@
+/*
+OpenjdkAnnotationLocation
+tokens = ANNOTATION_DEF, ANNOTATION_FIELD_DEF
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.annotation.openjdkannotationlocation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Target;
+
+@Annotation2(value = "foo") @Annotation2
+// violation below, 'Annotation 'Annotation2' must be alone on a line, or all on one line.'
+@Annotation2("bar") @interface InputOpenjdkAnnotationLocation {
+// violation above, 'Annotation 'Annotation2' must be on a separate line from target.'
+
+    @Annotation2(value = "foo")
+    @Annotation2
+    @Annotation2("bar") String value();
+    // violation above, 'Annotation 'Annotation2' must be on a separate line from target.'
+
+    // violation below, 'Annotation 'MyAnnotation2' must be on a separate line from target.'
+    @MyAnnotation2 @MyAnnotation2("bar") String val();
+    // violation above, 'Annotation 'MyAnnotation2' must be on a separate line from target.'
+
+    @MyAnnotation2 @MyAnnotation2("bar")
+    String values();
+
+}
+
+@Repeatable(Annotation1.class)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@interface Annotation2  {
+    String value() default "";
+}
+
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@interface Annotation1 {
+
+    Annotation2[] value();
+
+}
