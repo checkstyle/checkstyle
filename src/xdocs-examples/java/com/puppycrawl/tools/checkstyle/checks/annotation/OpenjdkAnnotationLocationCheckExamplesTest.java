ef73fc5c96a6053b02ba5b273b714482adadebfb
Aman-Baliyan <baliyanaman200@gmail.com>
2026-06-12 15:13:25 +0530

    Issue #20209: Added new check OpenjdkAnnotationLocation

---
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/annotation/OpenjdkAnnotationLocationCheckExamplesTest.java
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2026 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.annotation;
+
+import static com.puppycrawl.tools.checkstyle.checks.annotation.OpenjdkAnnotationLocationCheck.MSG_KEY_ANNOTATION_ALONE_OR_SAME;
+import static com.puppycrawl.tools.checkstyle.checks.annotation.OpenjdkAnnotationLocationCheck.MSG_KEY_ANNOTATION_ON_TARGET_LINE;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
+
+public class OpenjdkAnnotationLocationCheckExamplesTest extends AbstractExamplesModuleTestSupport {
+
+    @Override
+    public String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/checks/annotation/openjdkannotationlocation";
+    }
+
+    @Test
+    public void testExample1() throws Exception {
+        final String[] expected = {
+            "34:3: " + getCheckMessage(MSG_KEY_ANNOTATION_ALONE_OR_SAME, "Deprecated"),
+            "34:15: " + getCheckMessage(MSG_KEY_ANNOTATION_ALONE_OR_SAME, "SafeVarargs"),
+            "42:3: " + getCheckMessage(MSG_KEY_ANNOTATION_ON_TARGET_LINE, "Nonnull"),
+            "42:12: " + getCheckMessage(MSG_KEY_ANNOTATION_ON_TARGET_LINE, "Deprecated"),
+            "53:3: " + getCheckMessage(MSG_KEY_ANNOTATION_ON_TARGET_LINE, "Deprecated"),
+        };
+
+        verifyWithInlineConfigParser(getPath("Example1.java"), expected);
+    }
+
+}
