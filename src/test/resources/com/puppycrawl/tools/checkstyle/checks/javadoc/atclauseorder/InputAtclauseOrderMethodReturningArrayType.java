2f1af325d0e265e18dbc57790b61226a89cf0eff
surajgojanur <surajgojanur@gmail.com>
2026-06-12 16:19:45 +0530

    Issue #19764: Move violation comments out of Javadoc in atclauseorder

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/atclauseorder/InputAtclauseOrderMethodReturningArrayType.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/atclauseorder/InputAtclauseOrderMethodReturningArrayType.java
@@ -6,7 +6,6 @@ target = (default)CLASS_DEF, INTERFACE_DEF, ENUM_DEF, METHOD_DEF, \
 tagOrder = (default)@author, @deprecated, @exception, @param, @return, \
            @see, @serial, @serialData, @serialField, @since, @throws, @version
 
-
 */
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.atclauseorder;
@@ -14,24 +13,27 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc.atclauseorder;
 import java.sql.SQLException;
 
 public interface InputAtclauseOrderMethodReturningArrayType {
+    // violation 5 lines below 'Block tags have to appear in the order'
     /**
      * @return int array
      * @exception SQLException
+     *
      * @throws java.sql.SQLTimeoutException
-     * @see #addBatch // violation above
+     * @see #addBatch
      * @see DatabaseMetaData#supportsBatchUpdates
      * @since 1.2
      */
     int[] executeBatch() throws SQLException;
 
+    // violation 7 lines below 'Block tags have to appear in the order'
+    // violation 7 lines below 'Block tags have to appear in the order'
     /**
       * {@code selected} is an array that will contain
       * the indices of items that have been selected.
       *
       * @serial
-      * @see #getSelectedIndexes() // violation
-      * @see #getSelectedIndex() // violation
+      * @see #getSelectedIndexes()
+      * @see #getSelectedIndex()
       */
     int selected[] = new int[0];
 }
-
