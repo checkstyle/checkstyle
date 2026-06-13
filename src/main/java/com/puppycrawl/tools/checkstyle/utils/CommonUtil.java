ab56a7e4d1805337d39e4af7f775d00adea4fb89
Aizal Khan <aizumusheer2@gmail.com>
2026-06-13 23:20:30 +0530

    Pull #20336: use quoteReplacement in fillTemplateWithStringsByRegexp

--- src/main/java/com/puppycrawl/tools/checkstyle/utils/CommonUtil.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/utils/CommonUtil.java
@@ -469,7 +469,7 @@ public final class CommonUtil {
         if (matcher.find()) {
             for (int i = 0; i <= matcher.groupCount(); i++) {
                 // $n expands comment match like in Pattern.subst().
-                result = result.replaceAll("\\$" + i, matcher.group(i));
+                result = result.replaceAll("\\$" + i, Matcher.quoteReplacement(matcher.group(i)));
             }
         }
         return result;
