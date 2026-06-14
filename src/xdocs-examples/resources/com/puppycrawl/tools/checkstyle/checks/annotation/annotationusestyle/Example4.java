6bc0d1c61e78e818f41ca58157e0486152de61c1
smita_1078 <smita.prajapati082@gmail.com>
2026-06-14 23:58:58 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - annotationusestyle

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationusestyle/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/annotationusestyle/Example4.java
@@ -14,17 +14,15 @@ package com.puppycrawl.tools.checkstyle.checks.annotation.annotationusestyle;
 
 // xdoc section -- start
 @SuppressWarnings("unchecked") // ok as element style set to 'ignore'
-@Deprecated // ok as 'closingParens' is set to 'ignore
+@Deprecated // ok as 'closingParens' is set to 'ignore'
 // violation below 'Annotation array values must contain trailing comma'
 @SomeArrays({"unchecked","unused"})
-public class Example4
-{
+public class Example4 {
 
 }
-
 // violation below 'Annotation array values must contain trailing comma'
 @SuppressWarnings(value={"unchecked"})
-@Deprecated() // ok as 'closingParens' is set to 'ignore
+@Deprecated() // ok as 'closingParens' is set to 'ignore'
 // ok below as it has a trailing array comma
 @SomeArrays(value={"unchecked","unused",})
 class TestStyle4 {
