b94c38c4d0d1088fefced04305814d0c86772b02
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-06-16 21:33:12 +0100

    Issue #20222: fix parameter shadowing detection in compact source files

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/hiddenfield/InputHiddenFieldCompactSourceFileStatic.java
@@ -0,0 +1,21 @@
+/*
+HiddenField
+ignoreFormat = (default)null
+ignoreConstructorParameter = (default)false
+ignoreSetter = (default)false
+setterCanReturnItsClass = (default)false
+ignoreAbstractMethods = (default)false
+tokens = VARIABLE_DEF, PARAMETER_DEF
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+static String name = "default";
+
+void process(String name) { // violation, ''name' hides a field'
+    System.out.println(name);
+}
+
+void main() { process("test"); }
