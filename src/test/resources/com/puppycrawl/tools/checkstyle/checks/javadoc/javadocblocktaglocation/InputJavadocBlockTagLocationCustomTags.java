4713932eccab33b51a3b2e85a881b4925d079e36
surajgojanur <surajgojanur@gmail.com>
2026-06-18 15:59:35 +0530

    Issue #19764: move violation comments out of Javadoc

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocblocktaglocation/InputJavadocBlockTagLocationCustomTags.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocblocktaglocation/InputJavadocBlockTagLocationCustomTags.java
@@ -10,12 +10,18 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocblocktaglocation;
 
 public class InputJavadocBlockTagLocationCustomTags {
 
+    // violation 7 lines below
+    // violation 6 lines below
+    // violation 5 lines below
+    // violation 6 lines below
+    // violation 6 lines below
+    // violation 6 lines below
     /**
-     * Text. @apiNote note @implNote note @implSpec spec // 3 violations
+     * Text. @apiNote note @implNote note @implSpec spec
      * {@code @apiNote}
-     * text @apiNote note1 // violation
-     * text text @implNote note2  // violation
-     * text @implSpec  // violation
+     * text @apiNote note1
+     * text text @implNote note2
+     * text @implSpec
      * text @since 1.0
      */
     public void method(int param) {
