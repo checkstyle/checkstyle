1dbbeec93b8c98eff179b80d54520d170f5ed52d
Aman-Baliyan <baliyanaman200@gmail.com>
2026-06-18 16:21:29 +0530

    Issue #18570: Added new check PreferJavadocInlineTags

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/preferjavadocinlinetags/InputPreferJavadocInlineTagsMultipleEntities.java
@@ -0,0 +1,19 @@
+/*
+PreferJavadocInlineTags
+violateExecutionOnNonTightHtml = (default)false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.preferjavadocinlinetags;
+
+// 4 violations 6 lines below:
+//  'Prefer Javadoc inline tag '{@literal <}' over '&lt;'.'
+//  'Prefer Javadoc inline tag '{@literal >}' over '&gt;'.'
+//  'Prefer Javadoc inline tag '{@literal <}' over '&lt;'.'
+//  'Prefer Javadoc inline tag '{@literal >}' over '&gt;'.'
+/**
+ * Multiple: &lt;T&gt; and &lt;E&gt; generics.
+ */
+public class InputPreferJavadocInlineTagsMultipleEntities {
+}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/preferjavadocinlinetags/InputPreferJavadocInlineTagsSnippetTag.java
@@ -0,0 +1,24 @@
+/*
+PreferJavadocInlineTags
+violateExecutionOnNonTightHtml = (default)false
+
+
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.preferjavadocinlinetags;
+
+/**
+ * Test that HTML patterns inside inline tags are not flagged.
+ */
+public class InputPreferJavadocInlineTagsSnippetTag {
+
+    /**
+     * Nested HTML inside snippet:
+     * {@snippet :
+     * <a href="#method">link</a>
+     * <pre>Hello</pre>
+     * }
+     */
+    public void nestedInsideSnippet() {
+    }
+
+}
