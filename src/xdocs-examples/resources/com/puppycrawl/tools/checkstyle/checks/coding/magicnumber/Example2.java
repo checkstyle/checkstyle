a482e5f76564faad003277b074b771808d202710
smita_1078 <smita.prajapati082@gmail.com>
2026-06-20 00:36:59 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - magicnumber

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/magicnumber/Example2.java
@@ -14,7 +14,7 @@
 package com.puppycrawl.tools.checkstyle.checks.coding.magicnumber;
 
 // xdoc section -- start
-@Example2.Annotation(6)
+@Annotation(6)
 public class Example2 {
   private int field = 7;
 
@@ -24,15 +24,19 @@ public class Example2 {
   }
   public void method2() {
     final TestClass testObject = new TestClass(62);
+
     final int a = 3;
     final int[] b = {4, 5};
     final int c = -3;
     final int d = +4;
     final int e = method3(10, 20);
+
     final int f = 3 * 4;
+
     final int g = 3 / 4;
     final int h = 3 + 4;
     final int i = 3 - 4;
+
     final int j = (int) 3.4;
   }
   private int method3(int a, int b) {
