65da7bf0de9247c1bdb75e6007be5dfeaa88666e
smita_1078 <smita.prajapati082@gmail.com>
2026-06-21 11:39:33 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - suppresswarningholder

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/suppresswarningsholder/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/annotation/suppresswarningsholder/Example2.java
@@ -15,17 +15,41 @@ package com.puppycrawl.tools.checkstyle.checks.annotation.suppresswarningsholder
 
 // xdoc section -- start
 public class Example2 {
+
+  private int K;
+
+  @SuppressWarnings({"membername"})
+  private int J;
+
+  private static final int i = 0;
+
+  @SuppressWarnings("checkstyle:constantname")
+  private static final int m = 0;
+
   // violation below, 'More than 7 parameters (found 8)'
-  public void needsLotsOfParameters (int a,
-    int b, int c, int d, int e, int f, int g, int h) {
-    // ...
+  public void needsLotsOfParameters(
+          int a, int b, int c, int d,
+          int e, int f, int g, int h) {
+
+  }
+  // violation 2 lines below 'More than 7 parameters (found 8)'
+  @SuppressWarnings("ParamNumberId")
+  public void needsLotsOfParameters1(
+          int a, int b, int c, int d,
+          int e, int f, int g, int h) {
+
   }
 
   @SuppressWarnings("paramnum")
-  public void needsLotsOfParameters1 (int a, // violation suppressed
-    int b, int c, int d, int e, int f, int g, int h) {
-    // ...
+  public void needsLotsOfParameters2(
+          int a, int b, int c, int d,
+          int e, int f, int g, int h) { // violation suppressed
+
   }
 
+  private int [] ARR;
+
+  @SuppressWarnings("all")
+  private int [] ARRAY;
 }
 // xdoc section -- end
