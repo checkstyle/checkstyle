db7325efdd1bb85ee904b358cf34db91656fa993
vivek-0509 <viveksolanki0509@gmail.com>
2026-06-21 18:25:43 +0530

    Issue #20225: Fix MethodCount false negative on compact source files

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/methodcount/InputMethodCountCompactSourceFileValid.java
@@ -0,0 +1,21 @@
+/*
+MethodCount
+maxTotal = 5
+maxPrivate = (default)100
+maxPackage = (default)100
+maxProtected = (default)100
+maxPublic = (default)100
+tokens = (default)CLASS_DEF, ENUM_CONSTANT_DEF, ENUM_DEF, INTERFACE_DEF, ANNOTATION_DEF, \
+         METHOD_DEF, RECORD_DEF, COMPACT_COMPILATION_UNIT
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+int counter = 3;
+
+void a() {}
+
+void b() {}
+
+void main() {}
