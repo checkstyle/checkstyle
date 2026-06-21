2f6bc289ad62220f39da5f47dff7d92e4b643307
vivek-0509 <viveksolanki0509@gmail.com>
2026-06-21 18:25:43 +0530

    Issue #20225: Fix MethodCount false negative on compact source files

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/sizes/methodcount/InputMethodCountCompactSourceFileFieldInitializer.java
@@ -0,0 +1,21 @@
+/*
+MethodCount
+maxTotal = 1
+maxPrivate = (default)100
+maxPackage = (default)100
+maxProtected = (default)100
+maxPublic = (default)100
+tokens = (default)CLASS_DEF, ENUM_CONSTANT_DEF, ENUM_DEF, INTERFACE_DEF, ANNOTATION_DEF, RECORD_DEF
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+// violation below 'Total number of methods is 2 (max allowed is 1).'
+Runnable r = new Runnable() {
+    public void run() {} // NOT counted towards the compact source file
+};
+
+void helper() {}
+
+void main() {}
---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/methodcount/InputMethodCountEmpty.java
@@ -0,0 +1,12 @@
+/*
+MethodCount
+maxTotal = (default)100
+maxPrivate = (default)100
+maxPackage = (default)100
+maxProtected = (default)100
+maxPublic = (default)100
+tokens = (default)CLASS_DEF, ENUM_CONSTANT_DEF, ENUM_DEF, INTERFACE_DEF, ANNOTATION_DEF, RECORD_DEF
+
+*/
+
+/* Comment only, so the parsed tree root is null. */
