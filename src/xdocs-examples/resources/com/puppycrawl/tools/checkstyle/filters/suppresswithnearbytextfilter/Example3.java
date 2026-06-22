5ac305610a325f54f43da439f1642f166474756a
smita_1078 <smita.prajapati082@gmail.com>
2026-06-22 22:04:43 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - suppresswithnearbytextfilter

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example3.java
@@ -12,8 +12,21 @@
 package com.puppycrawl.tools.checkstyle.filters.suppresswithnearbytextfilter;
 // xdoc section -- start
 public class Example3 {
+
+  int a = 42; // SUPPRESS CHECKSTYLE because I want to
+  int b = 43;
+
   // ok, because violation message is matching suppress pattern
   String a_really_long_variable_name = "A sentence greater than 70 chars";
   // filtered violation above 'Line is longer ...'
+  String longText =
+          "This is a very very very very very very very very very very long string";
+  // filtered violation above 'Line is longer ...'
+  /**
+   * Flag description.
+   * Disabled until <a href="www.github.com/owner/repo/issue/9#comment">
+   * // filtered violation above 'Line is longer ...'
+   */
+  public static final boolean SOME_FLAG = false;
 }
 // xdoc section -- end
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example5.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppresswithnearbytextfilter/Example5.java
@@ -8,7 +8,23 @@
 */
 
 package com.puppycrawl.tools.checkstyle.filters.suppresswithnearbytextfilter;
-
-public class Example5 {}
 // xdoc section -- start
+public class Example5 {
+
+  int a = 42; // SUPPRESS CHECKSTYLE because I want to
+  int b = 43;
+
+  // ok, because violation message is matching suppress pattern
+  String a_really_long_variable_name = "A sentence greater than 70 chars";
+
+  String longText =
+          "This is a very very very very very very very very very very long string";
+
+  /**
+   * Flag description.
+   * Disabled until <a href="www.github.com/owner/repo/issue/9#comment">
+   *
+   */
+  public static final boolean SOME_FLAG = false;
+}
 // xdoc section -- end
