b35ad56d11fb789182ac38de66cab4a79bb6c0bb
Aman-Baliyan <baliyanaman200@gmail.com>
2026-06-23 16:58:17 +0530

    Issue #19805: Complete coverage for section-3.4.1

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterformatting/ruleorderofconstructorsandoverloadedmethods/InputOrderOfConstructorsAndOverloadedMethodsOne.java
@@ -0,0 +1,45 @@
+package com.openjdk.checkstyle.test.chapterformatting.ruleorderofconstructorsandoverloadedmethods;
+
+public class InputOrderOfConstructorsAndOverloadedMethodsOne {
+    int x;
+
+    Example2() {}
+
+    Example2(String s, int x, int y) {}
+
+    // comments between constructors are allowed.
+    Example2(int x) {}
+    // violation above 'Constructors should be ordered by increasing parameter count.'
+
+    int a = 0;
+
+    // violation 2 lines below """Constructors should be grouped together.
+    // The last grouped constructor is declared at line '11'."""
+    Example2(String s, int x) {}
+    // violation above 'Constructors should be ordered by increasing parameter count.'
+
+    private enum ExampleEnum {
+
+        ONE, TWO, THREE;
+
+        ExampleEnum() {}
+
+        void foo() {}
+
+        // violation 2 lines below """Constructors should be grouped together.
+        // The last grouped constructor is declared at line '25'."""
+        ExampleEnum(int x, int y) {}
+
+        // violation 2 lines below """Constructors should be grouped together.
+        // The last grouped constructor is declared at line '25'."""
+        ExampleEnum(String s, int x) {}
+
+    }
+
+    class InputWithOrderedCtors {
+        InputWithOrderedCtors() {}
+        InputWithOrderedCtors(String s) {}
+        InputWithOrderedCtors(int x) {}
+        InputWithOrderedCtors(String s, int x) {}
+    }
+}
