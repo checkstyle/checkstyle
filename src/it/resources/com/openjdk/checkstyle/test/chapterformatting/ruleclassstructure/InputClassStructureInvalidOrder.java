a8d77b5d1d56140c4a3f3dc61b5f900285026c15
Aman-Baliyan <baliyanaman200@gmail.com>
2026-06-24 15:49:26 +0530

    Issue #20428: Added check for class structure

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterformatting/ruleclassstructure/InputClassStructureInvalidOrder.java
@@ -0,0 +1,23 @@
+package com.openjdk.checkstyle.test.chapterformatting.ruleclassstructure;
+
+public class InputClassStructureInvalidOrder {
+
+    public int b;
+    static int a = 1;
+    private int x;
+
+    public InputClassStructureInvalidOrder() {
+    }
+
+    public int c; // violation, Field declaration is in wrong order
+    static Long s; // violation, Field declaration is in wrong order
+
+    void foo() {}
+
+    public InputClassStructureInvalidOrder(int x) {
+    } // violation above, 'Constructor definition in wrong order.'
+
+    void foo1() {}
+
+    void bar() {}
+}
