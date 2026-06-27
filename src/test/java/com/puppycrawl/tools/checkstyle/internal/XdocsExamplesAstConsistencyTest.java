89b0cbd13fe415834034005afef92bf3a8f92d6b
smita_1078 <smita.prajapati082@gmail.com>
2026-06-27 18:57:56 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - interfacememberimpliedmodifier

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
@@ -139,9 +139,6 @@ public class XdocsExamplesAstConsistencyTest {
             "checks/metrics/classdataabstractioncoupling/ignore/deeper/Example6",
             "checks/metrics/cyclomaticcomplexity/Example2",
             "checks/metrics/cyclomaticcomplexity/Example3",
-            "checks/modifier/interfacememberimpliedmodifier/Example2",
-            "checks/modifier/interfacememberimpliedmodifier/Example3",
-            "checks/modifier/interfacememberimpliedmodifier/Example4",
             "checks/modifier/redundantmodifier/Example2",
             "checks/naming/abstractclassname/Example3",
             "checks/naming/abstractclassname/Example4",
@@ -916,6 +913,7 @@ public class XdocsExamplesAstConsistencyTest {
         return parent != null
                 && ast.getType() == TokenTypes.IDENT
                 && (parent.getType() == TokenTypes.CLASS_DEF
+                    || parent.getType() == TokenTypes.INTERFACE_DEF
                     || parent.getType() == TokenTypes.CTOR_DEF);
     }
 
