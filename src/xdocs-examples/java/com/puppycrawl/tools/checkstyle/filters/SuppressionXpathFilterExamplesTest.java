cc14df676981a91b7b625b8849624d62e49a8e1d
smita_1078 <smita.prajapati082@gmail.com>
2026-06-28 14:05:14 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - suppressxpath

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilterExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/filters/SuppressionXpathFilterExamplesTest.java
@@ -31,13 +31,31 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     }
 
     @Test
-    public void testExample1() throws Exception {
+    public void testExample0() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "21:3: Cyclomatic Complexity is 4 (max allowed is 3).",
+            "41:3: Cyclomatic Complexity is 4 (max allowed is 3).",
         };
 
         final String[] expectedWithFilter = {};
+
+        System.setProperty("config.folder", "src/xdocs-examples/resources/"
+                + getPackageLocation());
+        verifyFilterWithInlineConfigParser(getPath("Example0.java"),
+                expectedWithoutFilter,
+                expectedWithFilter);
+    }
+
+    @Test
+    public void testExample1() throws Exception {
+
+        final String[] expectedWithoutFilter = {
+            "41:3: Cyclomatic Complexity is 4 (max allowed is 3).",
+        };
+
+        final String[] expectedWithFilter = {
+            "41:3: Cyclomatic Complexity is 4 (max allowed is 3).",
+        };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
             + getPackageLocation());
@@ -50,12 +68,16 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample2() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "14:1: 'package' should be separated from previous line.",
-            "15:1: 'CLASS_DEF' should be separated from previous line.",
+            "13:1: 'package' should be separated from previous line.",
+            "21:3: 'VARIABLE_DEF' should be separated from previous line.",
+            "22:3: 'METHOD_DEF' should be separated from previous line.",
+            "23:3: 'METHOD_DEF' should be separated from previous line.",
         };
 
         final String[] expectedWithFilter = {
-            "15:1: 'CLASS_DEF' should be separated from previous line.",
+            "21:3: 'VARIABLE_DEF' should be separated from previous line.",
+            "22:3: 'METHOD_DEF' should be separated from previous line.",
+            "23:3: 'METHOD_DEF' should be separated from previous line.",
         };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
@@ -69,12 +91,22 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample3() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "21:31: '{' at column 31 should be on a new line.",
-            "26:31: '{' at column 31 should be on a new line.",
+            "19:23: '{' at column 23 should be on a new line.",
+            "27:36: '{' at column 36 should be on a new line.",
+            "31:27: '{' at column 27 should be on a new line.",
+            "35:28: '{' at column 28 should be on a new line.",
+            "40:31: '{' at column 31 should be on a new line.",
+            "41:35: '{' at column 35 should be on a new line.",
+            "44:23: '{' at column 23 should be on a new line.",
         };
 
         final String[] expectedWithFilter = {
-            "26:31: '{' at column 31 should be on a new line.",
+            "27:36: '{' at column 36 should be on a new line.",
+            "31:27: '{' at column 27 should be on a new line.",
+            "35:28: '{' at column 28 should be on a new line.",
+            "40:31: '{' at column 31 should be on a new line.",
+            "41:35: '{' at column 35 should be on a new line.",
+            "44:23: '{' at column 23 should be on a new line.",
         };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
@@ -88,12 +120,13 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample4() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "17:3: 'VARIABLE_DEF' should be separated from previous line.",
-            "18:3: 'METHOD_DEF' should be separated from previous line.",
+            "20:3: 'VARIABLE_DEF' should be separated from previous line.",
+            "21:3: 'METHOD_DEF' should be separated from previous line.",
+            "22:3: 'METHOD_DEF' should be separated from previous line.",
         };
 
         final String[] expectedWithFilter = {
-            "17:3: 'VARIABLE_DEF' should be separated from previous line.",
+            "20:3: 'VARIABLE_DEF' should be separated from previous line.",
         };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
@@ -107,12 +140,16 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample5() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "17:15: Name 'SetSomeVar' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
-            "19:15: Name 'TestMethod' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "20:15: Name 'SetSomeVar' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "21:15: Name 'DoMATH' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "48:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "51:15: Name 'Test2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
         };
 
         final String[] expectedWithFilter = {
-            "19:15: Name 'TestMethod' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "21:15: Name 'DoMATH' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "48:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "51:15: Name 'Test2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
         };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
@@ -126,12 +163,12 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample6() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "19:9: Name 'TestVariable' must match pattern '^([a-z][a-zA-Z0-9]*|_)$'.",
-            "21:9: Name 'WeirdName' must match pattern '^([a-z][a-zA-Z0-9]*|_)$'.",
+            "34:9: Name 'TestVariable' must match pattern '^([a-z][a-zA-Z0-9]*|_)$'.",
+            "35:9: Name 'WeirdName' must match pattern '^([a-z][a-zA-Z0-9]*|_)$'.",
         };
 
         final String[] expectedWithFilter = {
-            "21:9: Name 'WeirdName' must match pattern '^([a-z][a-zA-Z0-9]*|_)$'.",
+            "35:9: Name 'WeirdName' must match pattern '^([a-z][a-zA-Z0-9]*|_)$'.",
         };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
@@ -145,15 +182,25 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample7() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "17:15: Name 'DoMATH' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
-            "18:15: Name 'DoEng' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
-            "22:19: '11' is a magic number.",
-            "23:8: Name 'FOO' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "19:13: '23' is a magic number.",
+            "20:27: '11' is a magic number.",
+            "21:15: Name 'SetSomeVar' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "22:15: Name 'DoMATH' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "31:11: '24' is a magic number.",
+            "49:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "52:15: Name 'Test2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "56:19: '11' is a magic number.",
+            "57:8: Name 'FOO' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
         };
 
         final String[] expectedWithFilter = {
-            "18:15: Name 'DoEng' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
-            "22:19: '11' is a magic number.",
+            "19:13: '23' is a magic number.",
+            "20:27: '11' is a magic number.",
+            "21:15: Name 'SetSomeVar' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "31:11: '24' is a magic number.",
+            "49:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "52:15: Name 'Test2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "56:19: '11' is a magic number.",
         };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
@@ -167,12 +214,16 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample8() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "22:5: Reference to instance variable 'age' needs \"this.\".",
-            "26:12: Reference to instance variable 'age' needs \"this.\".",
+            "32:5: Reference to instance variable 'age' needs \"this.\".",
+            "41:9: Reference to instance variable 'age' needs \"this.\".",
+            "41:20: Reference to instance variable 'wordCount' needs \"this.\".",
+            "44:14: Reference to instance variable 'age' needs \"this.\".",
         };
 
         final String[] expectedWithFilter = {
-            "26:12: Reference to instance variable 'age' needs \"this.\".",
+            "41:9: Reference to instance variable 'age' needs \"this.\".",
+            "41:20: Reference to instance variable 'wordCount' needs \"this.\".",
+            "44:14: Reference to instance variable 'age' needs \"this.\".",
         };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
@@ -186,13 +237,10 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample9() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "17:37: Throwing 'RuntimeException' is not allowed.",
-            "21:37: Throwing 'RuntimeException' is not allowed.",
+            "23:37: Throwing 'RuntimeException' is not allowed.",
         };
 
-        final String[] expectedWithFilter = {
-            "21:37: Throwing 'RuntimeException' is not allowed.",
-        };
+        final String[] expectedWithFilter = {};
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
             + getPackageLocation());
@@ -205,14 +253,11 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample10() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "17:9: 'public' modifier out of order with the JLS suggestions.",
-            "18:14: 'abstract' modifier out of order with the JLS suggestions.",
-            "23:14: 'abstract' modifier out of order with the JLS suggestions.",
+            "25:9: 'public' modifier out of order with the JLS suggestions.",
+            "26:14: 'abstract' modifier out of order with the JLS suggestions.",
         };
 
-        final String[] expectedWithFilter = {
-            "23:14: 'abstract' modifier out of order with the JLS suggestions.",
-        };
+        final String[] expectedWithFilter = {};
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
             + getPackageLocation());
@@ -223,12 +268,16 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
 
     @Test
     public void testExample11() throws Exception {
-
         final String[] expectedWithoutFilter = {
-            "16:27: '11' is a magic number.",
+            "18:13: '23' is a magic number.",
+            "19:27: '11' is a magic number.",
+            "30:11: '24' is a magic number.",
         };
 
-        final String[] expectedWithFilter = {};
+        final String[] expectedWithFilter = {
+            "18:13: '23' is a magic number.",
+            "30:11: '24' is a magic number.",
+        };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
             + getPackageLocation());
@@ -241,10 +290,15 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample12() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "16:27: '11' is a magic number.",
+            "18:13: '23' is a magic number.",
+            "19:27: '11' is a magic number.",
+            "30:11: '24' is a magic number.",
         };
 
-        final String[] expectedWithFilter = {};
+        final String[] expectedWithFilter = {
+            "18:13: '23' is a magic number.",
+            "30:11: '24' is a magic number.",
+        };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
             + getPackageLocation());
@@ -257,11 +311,16 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample13() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "18:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
-            "22:15: Name 'Test2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "19:15: Name 'SetSomeVar' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "20:15: Name 'DoMATH' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "47:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "50:15: Name 'Test2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
         };
 
-        final String[] expectedWithFilter = {};
+        final String[] expectedWithFilter = {
+            "19:15: Name 'SetSomeVar' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "20:15: Name 'DoMATH' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+        };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
             + getPackageLocation());
@@ -274,12 +333,16 @@ public class SuppressionXpathFilterExamplesTest extends AbstractExamplesModuleTe
     public void testExample14() throws Exception {
 
         final String[] expectedWithoutFilter = {
-            "18:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
-            "22:15: Name 'Test2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "19:15: Name 'SetSomeVar' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "20:15: Name 'DoMATH' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "47:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "50:15: Name 'Test2' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
         };
 
         final String[] expectedWithFilter = {
-            "18:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "19:15: Name 'SetSomeVar' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "20:15: Name 'DoMATH' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
+            "47:15: Name 'Test1' must match pattern '^[a-z][a-zA-Z0-9]*$'.",
         };
 
         System.setProperty("config.folder", "src/xdocs-examples/resources/"
