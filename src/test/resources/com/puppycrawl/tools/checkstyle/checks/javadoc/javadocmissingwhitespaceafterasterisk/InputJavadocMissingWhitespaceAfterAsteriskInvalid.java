edd033cdd762909cfb0b35aab6b74457008bd2f1
surajgojanur <surajgojanur@gmail.com>
2026-06-28 17:44:47 +0530

    Issue #19764: Move violation comments out of Javadoc for JavadocMissingWhitespaceAfterAsterisk

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmissingwhitespaceafterasterisk/InputJavadocMissingWhitespaceAfterAsteriskInvalid.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmissingwhitespaceafterasterisk/InputJavadocMissingWhitespaceAfterAsteriskInvalid.java
@@ -7,24 +7,26 @@ violateExecutionOnNonTightHtml = (default)false
 
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocmissingwhitespaceafterasterisk;
 
+// violation below 'Missing a whitespace after the leading asterisk.'
 /**Some Javadoc with violation.
- * // violation above 'Missing a whitespace after the leading asterisk.'
  */
 class InputJavadocMissingWhitespaceAfterAsteriskInvalid
 {
-    /** // violation below 'Missing a whitespace after the leading asterisk.'
+    // violation 2 lines below 'Missing a whitespace after the leading asterisk.'
+    /**
      *<p>Some Javadoc with violation.
      */
     class InnerInputJavadocMissingWhitespaceAfterAsteriskInValid {
 
-        /** // violation below 'Missing a whitespace after the leading asterisk.'
+        // violation 2 lines below 'Missing a whitespace after the leading asterisk.'
+        /**
          *Some Javadoc with violation.
          */
         public static final byte NUL = 0;
 
+        // violation 3 lines below 'Missing a whitespace after the leading asterisk.'
         /**
          * Some Javadoc.
-         * // violation below 'Missing a whitespace after the leading asterisk.'
          *<p>Some Javadoc with violation.
          */
         void bar1() {}
@@ -34,25 +36,30 @@ class InputJavadocMissingWhitespaceAfterAsteriskInvalid
         void bar2() {}
     }
 
+    // violation 3 lines below 'Missing a whitespace after the leading asterisk.'
     /****
-     * Some Javadoc. // violation below 'Missing a whitespace after the leading asterisk.'
+     * Some Javadoc.
      *@see Something with violation
      ****/
     void foo1() {}
 
-    /** // violation below 'Missing a whitespace after the leading asterisk.'
+    // violation 2 lines below 'Missing a whitespace after the leading asterisk.'
+    /**
      *Some Javadoc with violation.
      **/
     public static final int foo2 = 0;
 
-    /** // violation below 'Missing a whitespace after the leading asterisk.'
+    // violation 2 lines below 'Missing a whitespace after the leading asterisk.'
+    /**
      *Some Javadoc with violation.
      */
     enum Foo3 {}
 
-    /** // violation below 'Missing a whitespace after the leading asterisk.'
+    // violation 3 lines below 'Missing a whitespace after the leading asterisk.'
+    // violation 4 lines below 'Missing a whitespace after the leading asterisk.'
+    /**
      *<pre>Some Javadoc with violation.
-     *  Some Javadoc. // violation below 'Missing a whitespace after the leading asterisk.'
+     *  Some Javadoc.
      *</pre>Some Javadoc with violation.
      **/
     interface Foo4 {}
