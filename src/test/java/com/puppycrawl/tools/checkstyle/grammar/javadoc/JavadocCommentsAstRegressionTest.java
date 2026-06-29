7eb0d4a278e03e86dd8a91c554820b5957185f6e
gianmarco <gianmarco.schifone02@gmail.com>
2026-06-30 01:28:20 +0200

    Issue #20392: Fix Javadoc parse error while parsing reference

--- src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocCommentsAstRegressionTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/grammar/javadoc/JavadocCommentsAstRegressionTest.java
@@ -289,4 +289,10 @@ public class JavadocCommentsAstRegressionTest extends AbstractTreeTestSupport {
         verifyJavadocTree(getPath("ExpectedReferencesToUriFragments2.txt"),
                 getPath("InputReferencesToUriFragments2.javadoc"));
     }
+
+    @Test
+    public void testMethodReferencesWithoutHash() throws Exception {
+        verifyJavadocTree(getPath("ExpectedMethodReferencesWithoutHash.txt"),
+                getPath("InputMethodReferencesWithoutHash.javadoc"));
+    }
 }
