ba13bd569acabc3e425bab2950dc8b17c535e6d3
smita_1078 <smita.prajapati082@gmail.com>
2026-07-01 21:07:17 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - indentation

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
@@ -103,7 +103,6 @@ public class XdocsExamplesAstConsistencyTest {
     private static final Set<String> SUPPRESSED_EXAMPLES = Set.of(
             "checks/coding/unnecessarysemicolonafteroutertypedeclaration/Example2",
             "checks/imports/importcontrol/filters/Example9",
-            "checks/indentation/indentation/Example7",
             "checks/javadoc/javadoccontentlocation/Example2",
             "checks/javadoc/javadocleadingasteriskalign/Example2",
             "checks/javadoc/javadocleadingasteriskalign/Example3",
