13a718bbea455ec22736985b29c6eec368746412
smita_1078 <smita.prajapati082@gmail.com>
2026-07-01 16:22:55 +0530

    Issue #18435: Fix xdocs Examples AST Consistency Test - packagedeclaration

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/PackageDeclarationCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/coding/PackageDeclarationCheckExamplesTest.java
@@ -19,7 +19,7 @@
 
 package com.puppycrawl.tools.checkstyle.checks.coding;
 
-import static com.puppycrawl.tools.checkstyle.checks.coding.PackageDeclarationCheck.MSG_KEY_MISSING;
+import static com.puppycrawl.tools.checkstyle.checks.coding.PackageDeclarationCheck.MSG_KEY_MISMATCH;
 
 import org.junit.jupiter.api.Test;
 
@@ -35,7 +35,7 @@ public class PackageDeclarationCheckExamplesTest extends AbstractExamplesModuleT
     @Test
     public void testExample1() throws Exception {
         final String[] expected = {
-            "10:1: " + getCheckMessage(MSG_KEY_MISSING),
+            "10:1: " + getCheckMessage(MSG_KEY_MISMATCH),
         };
 
         verifyWithInlineConfigParser(getPath("Example1.java"), expected);
