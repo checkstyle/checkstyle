df3f91a177aa3f7dae861432463613c9fc4dca43
Aman-Baliyan <baliyanaman200@gmail.com>
2026-07-02 18:16:16 +0530

    Issue #20526: Added checks for first rule

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterformatting/rulehorizontalwhitespace/InputHorizontalWhiteSpaceInvalidFour.java
@@ -0,0 +1,75 @@
+package com.openjdk.checkstyle.test.chapterformatting.rulehorizontalwhitespace;
+
+import java.io.IOException;
+
+/**
+ * Dummy input
+ */
+public class InputHorizontalWhiteSpaceInvalidFour {
+
+    int x;
+
+    /**
+     * Dummy input
+     */
+    public InputHorizontalWhiteSpaceInvalidFour(int n) {
+    }
+
+    /**
+     * Dummy input
+     */
+    public void fun() {
+
+        boolean a = true;
+        if ( a ) { // 2 violations
+        // ''(' is followed by whitespace'
+        // '')' is preceded with whitespace'
+        }
+
+        try {
+            throw new IOException();
+        } catch ( IOException e) { // violation 'is followed by whitespace'
+        } catch (Exception e ) {}  // violation 'is preceded with whitespace'
+
+        for ( int i = 0; i < x; i++ ) { // 2 violations
+        // ''(' is followed by whitespace'
+        // '')' is preceded with whitespace'
+        }
+    }
+
+    /**
+     * Dummy input
+     */
+    public void fun2() {
+        switch ( x) { // violation 'is followed by whitespace'
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Dummy input
+     */
+    class Bar extends InputHorizontalWhiteSpaceInvalidFour {
+
+        /**
+         * Dummy input
+         */
+        Bar() {
+            super(1 ); // violation '')' is preceded with whitespace'
+        }
+
+        /**
+         * Dummy input
+         */
+        Bar(int k) {
+            super( k ); // 2 violations
+            // ''(' is followed by whitespace'
+            // '')' is preceded with whitespace'
+            for ( int i = 0; i < k; i++) { // violation ''(' is followed by whitespace'
+            }
+        }
+    }
+}
