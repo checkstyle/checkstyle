7d80efc72401d9e2ea24fdd19211a09e97ad9d42
vivek-0509 <viveksolanki0509@gmail.com>
2026-07-04 01:06:45 +0530

    Issue #20524: Fix AbbreviationAsWordInName NPE on compact source files

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/naming/AbbreviationAsWordInNameCheck.java
@@ -289,11 +289,14 @@ public class AbbreviationAsWordInNameCheck extends AbstractCheck {
     private static boolean isInterfaceDeclaration(DetailAST variableDefAst) {
         boolean result = false;
         final DetailAST astBlock = variableDefAst.getParent();
-        final DetailAST astParent2 = astBlock.getParent();
 
-        if (astParent2.getType() == TokenTypes.INTERFACE_DEF
-                || astParent2.getType() == TokenTypes.ANNOTATION_DEF) {
-            result = true;
+        if (astBlock.getType() != TokenTypes.COMPACT_COMPILATION_UNIT) {
+            final DetailAST astParent2 = astBlock.getParent();
+
+            if (astParent2.getType() == TokenTypes.INTERFACE_DEF
+                    || astParent2.getType() == TokenTypes.ANNOTATION_DEF) {
+                result = true;
+            }
         }
         return result;
     }
