7423feab4c6ac782d46da5434482180b2bb5d5c3
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-07-05 08:55:15 +0100

    Issue #20547: fix imported types inside generics

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unusedimports/InputUnusedImportsWithLinkAndGenericMethodParameters4.java
@@ -0,0 +1,27 @@
+/*
+UnusedImports
+processJavadoc = (default)true
+violateExecutionOnNonTightHtml = (default)false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.unusedimports;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * {@link #method(List<BigDecimal>, List<RoundingMode>)}
+ * {@link #method(Set<List<BigDecimal>>, List<NumberFormat>)}
+ * {@link #method(List<BigDecimal>, List<RoundingMode>, List<NumberFormat>)}
+ */
+public class InputUnusedImportsWithLinkAndGenericMethodParameters4 {
+
+    public int calculate() {
+        return 0;
+    }
+}
