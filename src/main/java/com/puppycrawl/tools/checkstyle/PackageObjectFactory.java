ed19485c1a4a6a9fc85aa5463cf5778d402b3ca2
gianmarco <gianmarco.schifone02@gmail.com>
2026-07-05 18:13:15 +0200

    Issue #19145: remove JavadocStyleCheck

--- src/main/java/com/puppycrawl/tools/checkstyle/PackageObjectFactory.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/PackageObjectFactory.java
@@ -715,8 +715,6 @@ public class PackageObjectFactory implements ModuleFactory {
                 BASE_PACKAGE + ".checks.javadoc.JavadocPackageCheck");
         NAME_TO_FULL_MODULE_NAME.put("JavadocParagraphCheck",
                 BASE_PACKAGE + ".checks.javadoc.JavadocParagraphCheck");
-        NAME_TO_FULL_MODULE_NAME.put("JavadocStyleCheck",
-                BASE_PACKAGE + ".checks.javadoc.JavadocStyleCheck");
         NAME_TO_FULL_MODULE_NAME.put("JavadocTagContinuationIndentationCheck",
                 BASE_PACKAGE + ".checks.javadoc.JavadocTagContinuationIndentationCheck");
         NAME_TO_FULL_MODULE_NAME.put("JavadocTypeCheck",
