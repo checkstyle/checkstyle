6a983a8b0a566c14ca872ae18f0cc4d168d09041
gianmarco <gianmarco.schifone02@gmail.com>
2026-07-05 18:13:15 +0200

    Issue #20610: remove JavadocStyleCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/utils/checkutil/InputCheckUtilJavadocTypeNonGenericClass.java
@@ -0,0 +1,21 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.javadoc.JavadocTypeCheck
+scope = (default)private
+excludeScope = (default)null
+authorFormat = (default)null
+versionFormat = (default)null
+allowMissingParamTags = (default)false
+allowUnknownTags = (default)false
+allowedAnnotations = (default)Generated
+tokens = (default)INTERFACE_DEF, CLASS_DEF, ENUM_DEF, ANNOTATION_DEF, RECORD_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.utils.checkutil;
+
+/**
+ * Non generic class.
+ */
+public class InputCheckUtilJavadocTypeNonGenericClass {
+}
