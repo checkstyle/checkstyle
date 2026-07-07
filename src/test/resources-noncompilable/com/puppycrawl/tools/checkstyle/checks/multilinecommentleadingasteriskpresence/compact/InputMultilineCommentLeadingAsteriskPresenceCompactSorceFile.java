383b98e0718fcb08677621293a8be99f8a3d5688
atharv <atharv55215@gmail.com>
2026-07-07 16:12:19 +0530

    Issue #17695: Adding new check MultilineCommentLeadingAsteriskPresence

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/multilinecommentleadingasteriskpresence/compact/InputMultilineCommentLeadingAsteriskPresenceCompactSorceFile.java
@@ -0,0 +1,15 @@
+/*
+MultilineCommentLeadingAsteriskPresence
+
+*/
+// violation 3 lines above 'Multiline comment should start with leading asterisk'
+// violation 3 lines above 'Multiline comment should start with leading asterisk'
+
+// non-compiled with javac: Compilable with Java25
+
+/*
+ * Has leading asterisk.
+ */
+void main() {
+    System.out.println("hello");
+}
