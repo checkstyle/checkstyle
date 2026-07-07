071089a6e15c5f9ece4298e49eeaa125491a9ad3
atharv <atharv55215@gmail.com>
2026-07-07 16:12:19 +0530

    Issue #17695: Adding new check MultilineCommentLeadingAsteriskPresence

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/multilinecommentleadingasteriskpresence/Example1.java
@@ -0,0 +1,33 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MultilineCommentLeadingAsteriskPresence"/>
+  </module>
+</module>
+*/
+
+// violation 8 lines above """
+// Multiline comment line(s) 2, 3, 4, 5, 6 should start with leading asterisk"""
+
+package com.puppycrawl.tools.checkstyle.checks.multilinecommentleadingasteriskpresence;
+
+// xdoc section -- start
+public class Example1 {
+
+  /*
+   * Line has leading asterisk.
+   */
+  void foo() {}
+
+  // violation 2 lines below """Multiline comment line(s) 25
+  // should start with leading asterisk"""
+  /* Line with leading asterisk
+     Line with leading asterisk
+  */
+  void bar() {}
+
+  /* Single line comment */
+  void method() {}
+
+}
+// xdoc section -- end
