f445d445d44a35c25469daa88e0d3b9316138d4e
Aman-Baliyan <baliyanaman200@gmail.com>
2026-07-07 17:02:08 +0530

    Issue #20597: Added check for formatting

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterformatting/ruleformatting/InputFormattingTwo.java
@@ -0,0 +1,10 @@
+package com.openjdk.checkstyle.test.chapterformatting.ruleformatting;
+
+public interface InputFormattingTwo {
+}
+
+class Temp1 { // violation, 'Top-level class Temp1 has to reside in its own source file.'
+}
+
+class Temp2 { // violation, 'Top-level class Temp2 has to reside in its own source file.'
+}
