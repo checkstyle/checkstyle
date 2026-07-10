ab7f31320774c7e1b7719a0514bf3b01083e6ebf
Aman-Baliyan <baliyanaman200@gmail.com>
2026-07-10 21:09:01 +0530

    Issue #20641: Add checks for programming section

--- src/it/resources/com/openjdk/checkstyle/test/chapterformatting/ruleindentation/InputIndentationValid.java
+++ src/it/resources/com/openjdk/checkstyle/test/chapterformatting/ruleindentation/InputIndentationValid.java
@@ -3,7 +3,8 @@ package com.openjdk.checkstyle.test.chapterformatting.ruleindentation;
 /** Valid indentation examples for OpenJDK style section 3.7. */
 public class InputIndentationValid {
 
-    private void method(int value) {
+    private void method() {
+        int value = 0;
         switch (value) {
             case 1: // violation ''case' construct must use '{}'s.'
                 value++;
---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterprogrammingpractices/ruleprogrammingpractices/InputProgrammingPracticesOverrideTwo.java
@@ -0,0 +1,25 @@
+package com.openjdk.checkstyle.test.chapterprogrammingpractices.ruleprogrammingpractices;
+
+public class InputProgrammingPracticesOverrideTwo {
+
+    record Person(String name, int age) {
+
+        // violation below, 'method must include @java.lang.Override annotation.'
+        public String name() {
+            return name.toUpperCase();
+        }
+
+        public String name(String value) {
+            return value;
+        }
+
+        @Override
+        public int age() {
+            return age;
+        }
+
+        public int age(int value) {
+            return value;
+        }
+    }
+}
