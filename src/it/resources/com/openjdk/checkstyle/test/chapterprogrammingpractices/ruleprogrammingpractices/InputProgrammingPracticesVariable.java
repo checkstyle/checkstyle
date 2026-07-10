a431b96299d2cad639b5267bdcd49e4ac95464ea
Aman-Baliyan <baliyanaman200@gmail.com>
2026-07-10 21:09:01 +0530

    Issue #20641: Add checks for programming section

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterprogrammingpractices/ruleprogrammingpractices/InputProgrammingPracticesVariable.java
@@ -0,0 +1,15 @@
+package com.openjdk.checkstyle.test.chapterprogrammingpractices.ruleprogrammingpractices;
+
+public class InputProgrammingPracticesVariable {
+
+    public static final int CONSTANT = 0;
+
+    public static int a = 0; // violation 'Public static variable must be final.'
+
+    private static int c = 0;
+
+    class Temp {
+        public static int cons = 10; // violation 'Public static variable must be final.'
+    }
+}
+
