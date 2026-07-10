f06eeb5178dd1ad756d6871a504a9482a6d37f68
smita.prajapati082@gmail.com <smita.prajapati082@gmail.com>
2026-07-10 06:47:24 +0530

    Issue #20615: Adding .xml.template files

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/descendanttoken/UseCase1.java
@@ -0,0 +1,53 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="DescendantToken">
+      <property name="tokens" value="FOR_INIT"/>
+      <property name="limitedTokens" value="EXPR"/>
+      <property name="minimumNumber" value="1"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.descendanttoken;
+
+// xdoc section -- start
+class UseCase1 {
+  private int field1;
+  private int field2;
+
+  int testMethod(int x, String str)
+          throws ArithmeticException, IllegalArgumentException {
+
+    switch (x) {
+      case 1:
+        break;
+      case 2:
+        break;
+    }
+
+    try { }
+    catch (Exception e) {
+      try { }
+      catch (Exception ex) { }
+      return -1;
+    }
+    finally {
+      try { }
+      catch (Exception ex) { }
+    }
+
+    for (;;) {  // violation, 'Count of 0 for 'FOR_INIT' descendant'
+      break;
+    }
+    int a = 1;
+    int b = 2;
+    if (this == null || str == "abc") {
+      return 0;
+    }
+    assert a++ == 0;
+    ;
+    return 2;
+  }
+}
+// xdoc section -- end
