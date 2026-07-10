e203d8210256b208978efbdde82f51ad3d992b7a
Aman-Baliyan <baliyanaman200@gmail.com>
2026-07-10 21:09:01 +0530

    Issue #20641: Add checks for programming section

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterprogrammingpractices/ruleprogrammingpractices/InputProgrammingPracticesOverrideOne.java
@@ -0,0 +1,45 @@
+package com.openjdk.checkstyle.test.chapterprogrammingpractices.ruleprogrammingpractices;
+
+public class InputProgrammingPracticesOverrideOne {
+
+    class Child extends Parent {
+        /** {@inheritDoc} */
+        @Override
+        public void test1() {}
+
+        /** {@inheritDoc} */
+        public void test2() {}
+        // violation above """Must include @java.lang.Override annotation when
+        //  '@inheritDoc' Javadoc tag exists."""
+
+        /** {@inheritDoc} */
+        private void test3() {}
+        // violation above '{@inheritDoc} tag is not valid at this location.'
+
+        /** {@inheritDoc} */
+        public void test4() {}
+        // violation above """Must include @java.lang.Override annotation when
+        //  '@inheritDoc' Javadoc tag exists."""
+
+        /** {@inheritDoc} */
+        public boolean equals(Object o) {
+            // violation above """Must include @java.lang.Override annotation when
+            //  '@inheritDoc' Javadoc tag exists."""
+            return o == this;
+        }
+    }
+
+    class Parent {
+        public void test1() {
+        }
+
+        public void test2() {
+        }
+
+        private void test3() {
+        }
+
+        public void test4() {
+        }
+    }
+}
