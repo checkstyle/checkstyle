665767dae05393a987d3a5f4f6c94195dfb9fbeb
Dhanush Pothulapati <pothulapati5434@gmail.com>
2026-07-11 13:50:56 +0530

    Issue #15456: specify violation messages for ExecutableStatementCountCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/executablestatementcount/InputExecutableStatementCountStaticInit.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/executablestatementcount/InputExecutableStatementCountStaticInit.java
@@ -10,7 +10,8 @@ package com.puppycrawl.tools.checkstyle.checks.sizes.executablestatementcount;
 
 public class InputExecutableStatementCountStaticInit {
     // STATIC_INIT
-    static { // violation
+    // violation below 'Executable statement count is 2'
+    static {
         int i = 1;
         if (System.currentTimeMillis() == 0) {
         } else if (System.currentTimeMillis() == 0) {
