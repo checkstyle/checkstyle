0e1eba3a817c28125abd7f066486e572d1cd7834
Aman-Baliyan <baliyanaman200@gmail.com>
2026-07-11 16:39:24 +0530

    Issue #20660: Added checks for CopyRight

---
+++ src/it/resources/com/openjdk/checkstyle/test/chapterformatting/rulecopyrightnotice/InputCopyRightNoticeTwo.java
@@ -0,0 +1,5 @@
+package com.openjdk.checkstyle.test.chapterformatting.rulecopyrightnotice;
+
+// No header present in this file, so it will violate the CopyRightHeader check.
+public class InputCopyRightNoticeTwo {
+}
