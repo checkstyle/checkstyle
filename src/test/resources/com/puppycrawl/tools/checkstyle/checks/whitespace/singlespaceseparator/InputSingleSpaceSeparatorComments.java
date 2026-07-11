d6cf2e73ff880a15dc5255fb8a7dc351b92d9438
Dhanush Pothulapati <pothulapati5434@gmail.com>
2026-07-11 15:29:01 +0530

    Issue #15456: specify violation messages for SingleSpaceSeparatorCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/singlespaceseparator/InputSingleSpaceSeparatorComments.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/singlespaceseparator/InputSingleSpaceSeparatorComments.java
@@ -9,14 +9,21 @@ package com.puppycrawl.tools.checkstyle.checks.whitespace.singlespaceseparator;
 
 public class InputSingleSpaceSeparatorComments {
     /* always correct */ int i = 0;
-    int   /* wrong if X is enabled */     j = 0; // 2 violations
-    int k;   // violation
+    int   /* wrong if X is enabled */     j = 0;
+    // 2 violations above:
+    //     'Use a single space to separate non-whitespace characters.'
+    //     'Use a single space to separate non-whitespace characters.'
+    int k;   // violation 'Use a single space to separate non-whitespace'
 
     /**
      * Always correct
      */
     void foo() {
         /* Always correct */
-        int  a = 0;	// <- a tab // 2 violations
-    }  // Wrong if X is enabled // violation
+        int  a = 0;	// <- a tab
+        // 2 violations above:
+        //     'Use a single space to separate non-whitespace characters.'
+        //     'Use a single space to separate non-whitespace characters.'
+    // violation below 'Use a single space to separate non-whitespace'
+    }  // Wrong if X is enabled
 }
