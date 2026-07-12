0a472dee8289f8fa682dee0d7501a0ce17f8cddf
smita1078 <smita.prajapati082@gmail.com>
2026-07-12 17:55:33 +0530

    Issue #20624: Adding Example for missing property - missingjavadoc

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/missingjavadocmethod/Example8.java
@@ -0,0 +1,36 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MissingJavadocMethod">
+      <property name="tokens" value="METHOD_DEF"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.missingjavadocmethod;
+
+// xdoc section -- start
+public class Example8 {
+  public Example8() {}
+  public void testMethod1() {}   // violation, 'Missing a Javadoc comment'
+  /**
+   * Some description here.
+   */
+  public void testMethod2() {}
+
+  @Override
+  public String toString() {
+    return "Some string";
+  }
+
+  private void testMethod3() {}
+  protected void testMethod4() {}
+  void testMethod5() {}
+
+  public void testMethod6() {    // violation, 'Missing a Javadoc comment'
+    System.out.println("line 1");
+    System.out.println("line 2");
+    System.out.println("line 3");
+  }
+}
+// xdoc section -- end
