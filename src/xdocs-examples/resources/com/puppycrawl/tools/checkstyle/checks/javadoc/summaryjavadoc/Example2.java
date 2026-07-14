97232d9eb2225b39e7ff7e0bc685ea7bd6686994
smita1078 <smita.prajapati082@gmail.com>
2026-07-14 22:52:37 +0530

    Issue #20711: Fix comment mismatches in xdocs examples currently suppressed in XdocsExamplesAstConsistencyTest

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/summaryjavadoc/Example2.java
@@ -12,49 +12,35 @@ package com.puppycrawl.tools.checkstyle.checks.javadoc.summaryjavadoc;
 
 // xdoc section -- start
 class Example2 {
-
-  /**
-   * {@inheritDoc}
-   */
-  public String m1(){ return ""; }
   // violation below, 'Summary javadoc is missing'
   /** */
-  public String m2(){ return ""; }
+  public String withoutJavadoc() { return ""; }
 
   /**
    * {@summary  }
    */
-  public String m3(){ return ""; }
+  public String withJavadoc() { return ""; }
   // violation 3 lines above 'Summary javadoc is missing'
-  /**
-   * {@summary <p> <p/>}
-   */
-  public String m4() { return ""; }
-  // violation 3 lines above 'Summary javadoc is missing'
-  /**
-   * {@summary <p>This is a javadoc with period.<p/>}
-   */
-  public void m5() {}
 
+  /**
+   * {@summary This is a java doc with dot period.}
+   */
+  public void withPeriod() {}
+
+  /**
+   * {@summary This is a java doc with ideographic period。}
+   */
+  public void withoutPeriod() {}
+  // violation 3 lines above 'Summary of Javadoc is missing an ending period'
   /**
    * This method returns nothing.
    */
-  void m6() {}
+  void withDotPeriod() {}
   // violation 4 lines above 'Forbidden summary fragment'
   /**
-  * {@summary This is a java doc with period symbol。}
-  */
-  public void m7() {}
-  // violation 3 lines above 'Summary of Javadoc is missing an ending period'
-  /**
-  * {@summary First sentence is normally the summary.
-  * Use of html tags:
-  * <ul>
-  * <li>Item one.</li>
-  * <li>Item two.</li>
-  * </ul>}
-  */
-  public void m8() {}
-
+   * This method returns nothing。
+   */
+  void withoutDotPeriod() {}
+  // violation 4 lines above 'First sentence of Javadoc is missing an ending period'
 }
 // xdoc section -- end
