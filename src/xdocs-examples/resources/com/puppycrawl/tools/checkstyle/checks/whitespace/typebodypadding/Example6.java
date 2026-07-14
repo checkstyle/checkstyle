1920bc4e1ad284574eafb3350275209a43ae27a4
Saad Haider <mohd.saadhaider@gmail.com>
2026-07-14 10:54:44 +0530

    Issue #17393: add new check TypeBodyPadding

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/typebodypadding/Example6.java
@@ -0,0 +1,23 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="TypeBodyPadding">
+      <property name="skipInner" value="false"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.whitespace.typebodypadding;
+
+// xdoc section -- start
+class Example6 {
+
+  // violation 2 lines below 'Blank line required after the opening brace of type
+  //  definition.'
+  class Inner {
+    int y;
+  } // violation 'Blank line required before the closing brace of type definition.'
+
+}
+// xdoc section -- end
