54e4cb27cdd4a7445427fd4c985b14570fa28526
gianmarco <gianmarco.schifone02@gmail.com>
2026-07-17 12:08:59 +0200

    Issue #19927: add coverage for exceptions to document

---
+++ src/it/java/com/doccomments/checkstyle/test/writingdoccomments/documentingexceptionswiththrowstag/exceptionstodocument/ExceptionsToDocumentTest.java
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2026 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.doccomments.checkstyle.test.writingdoccomments.documentingexceptionswiththrowstag
+        .exceptionstodocument;
+
+import org.junit.jupiter.api.Test;
+
+import com.doccomments.checkstyle.test.base.AbstractDocCommentsModuleTestSupport;
+
+public class ExceptionsToDocumentTest extends AbstractDocCommentsModuleTestSupport {
+
+    @Override
+    public String getPackageLocation() {
+        return "com/doccomments/checkstyle/test/writingdoccomments/"
+                + "documentingexceptionswiththrowstag/"
+                + "exceptionstodocument";
+    }
+
+    @Test
+    public void testCorrect() throws Exception {
+        verifyWithWholeConfig(getPath("InputCorrectExceptionsToDocument.java"));
+    }
+
+    @Test
+    public void testIncorrect() throws Exception {
+        verifyWithWholeConfig(getPath("InputIncorrectExceptionsToDocument.java"));
+    }
+
+}
