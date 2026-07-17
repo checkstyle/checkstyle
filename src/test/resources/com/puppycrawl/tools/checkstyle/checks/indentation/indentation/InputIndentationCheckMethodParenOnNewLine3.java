c6e298b0ee5284616089b8c3bdb4cbff1c4f6682
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-07-17 18:36:10 +0100

    Issue #10015: fix false positive for method definition parenthesis on new lines

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationCheckMethodParenOnNewLine3.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/InputIndentationCheckMethodParenOnNewLine3.java
@@ -1,4 +1,4 @@
-package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;  //indent:0 exp:0
+package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;    //indent:0 exp:0
 
 /* Config:                                                                 //indent:0 exp:0
  * basicOffset = 4                                                         //indent:1 exp:1
@@ -11,19 +11,11 @@ package com.puppycrawl.tools.checkstyle.checks.indentation.indentation;  //inden
  * forceStrictCondition = false                                            //indent:1 exp:1
  */                                                                        //indent:1 exp:1
 
-public class InputIndentationCheckMethodParenOnNewLine1 {                  //indent:0 exp:0
+public class InputIndentationCheckMethodParenOnNewLine3 {                  //indent:0 exp:0
     void                                                                   //indent:4 exp:4
-        method (                                                           //indent:8 exp:8
-    )                                                                      //indent:4 exp:4
+        method                                                             //indent:8 exp:8
+            (                                                              //indent:12 exp:4 warn
+            int a)                                                         //indent:12 exp:12
     {                                                                      //indent:4 exp:4
-        int b =                                                            //indent:8 exp:8
-         2                                                                 //indent:9 exp:12 warn
-            *                                                              //indent:12 exp:12
-            3;                                                             //indent:12 exp:12
     }                                                                      //indent:4 exp:4
-    void                                                                   //indent:4 exp:4
-        methodTest (                                                       //indent:8 exp:8
-       int a                                                               //indent:7 exp:8 warn
-        )                                                                  //indent:8 exp:4 warn
-    {}                                                                     //indent:4 exp:4
 }                                                                          //indent:0 exp:0
