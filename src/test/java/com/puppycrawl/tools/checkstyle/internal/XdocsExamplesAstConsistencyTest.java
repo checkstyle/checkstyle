4fd74e2d84ae1341e55b1782d3efbe3ea0819c06
smita1078 <smita.prajapati082@gmail.com>
2026-07-18 23:03:10 +0530

    Issue #20625: Added example for EXAMPLE_COUNT_SUPPRESSED_MODULES : typename

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
@@ -170,7 +170,6 @@ public class XdocsExamplesAstConsistencyTest {
             "checks/naming/membername",
             "checks/naming/methodname",
             "checks/naming/staticvariablename",
-            "checks/naming/typename",
             "checks/regexp/regexp",
             "checks/regexp/regexpmultiline",
             "checks/regexp/regexponfilename",
