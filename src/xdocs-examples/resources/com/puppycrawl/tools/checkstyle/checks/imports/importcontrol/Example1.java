670c8fc74c505300e4ec583b5561b26aee66733d
smita1078 <smita.prajapati082@gmail.com>
2026-07-18 11:59:44 +0530

    Issue #20819: XdocsExamplesAstConsistencyTest.testEveryPropertyHasAnExample misses examples nested in subdirectories

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/imports/importcontrol/Example1.java
@@ -8,10 +8,11 @@
 </module>
 */
 
-// xdoc section -- start
 package com.puppycrawl.tools.checkstyle.checks.imports.importcontrol;
-
+// xdoc section -- start
 import java.awt.Image;     // violation, 'Disallowed import - java.awt.Image'
+import java.lang.ref.SoftReference;
+// violation above 'Disallowed import - java.lang.ref.SoftReference'
 import java.io.File;       // violation, 'Disallowed import - java.io.File'
 import java.io.FileReader;
 import java.util.Date;     // violation, 'Disallowed import - java.util.Date'
