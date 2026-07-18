bba2839b71c01d8b06271dc7639873ea79ac54f8
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-07-18 18:59:35 +0100

    Issue #20552: fix CovariantEqualsCheck to detect covariant equals in compact source files

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/covariantequals/InputCovariantEqualsCompactSourceFileNoViolation.java
@@ -0,0 +1,21 @@
+/*
+CovariantEquals
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+public boolean equals(Object obj) {
+    return true;
+}
+
+public boolean equals(String other) {
+    return false;
+}
+
+public boolean equals(Integer other) {
+    return false;
+}
+
+void main() { }
