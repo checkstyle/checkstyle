dfa14659e8f21705edda7aab049036f94215e229
arturict <arturict@users.noreply.github.com>
2026-07-19 23:47:10 +0200

    Fix Java 25 compact-source fixture

--- src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/nofinalizer/compact/InputNoFinalizerCompactSourceFile.java
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/nofinalizer/compact/InputNoFinalizerCompactSourceFile.java
@@ -7,7 +7,7 @@ NoFinalizer
 // non-compiled with javac: Compilable with Java25
 
 // violation below, 'Avoid using finalizer method.'
-void finalize() {
+protected void finalize() {
 }
 
 void finalize(String value) {
