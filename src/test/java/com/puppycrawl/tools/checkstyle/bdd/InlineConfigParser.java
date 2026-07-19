630f722aad7e6fa86ae88c7fcac8f31adf9d5d1a
Solaris-star <820622658@qq.com>
2026-07-20 03:36:11 +0800

    Issue #16807: remove MissingJavadocMethod inputs from default-property suppressions

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -630,7 +630,6 @@ public final class InlineConfigParser {
         "checks/javadoc/javadocvariable/InputJavadocVariableTagsMethods1.java",
         "checks/javadoc/javadocvariable/InputJavadocVariableTagsMethods2.java",
         "checks/javadoc/javadocvariable/InputJavadocVariableTagsMethods3.java",
-        "checks/javadoc/missingjavadocmethod/InputMissingJavadocMethodBasic.java",
         "checks/javadoc/nonemptyatclausedescription/InputNonEmptyAtclauseDescriptionOne.java",
         "checks/metrics/booleanexpressioncomplexity/InputBooleanExpressionComplexity.java",
         "checks/metrics/booleanexpressioncomplexity/InputBooleanExpressionComplexityLeaves.java",
