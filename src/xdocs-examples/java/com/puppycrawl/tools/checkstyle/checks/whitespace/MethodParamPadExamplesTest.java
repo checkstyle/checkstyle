f343ff44ca579998a4200095bde1e5f9162b7118
smita1078 <smita.prajapati082@gmail.com>
2026-07-20 15:33:39 +0530

    Issue #20625: Added example for EXAMPLE_COUNT_SUPPRESSED_MODULES : methodparam + nowhitespace + operatorwrap

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/whitespace/MethodParamPadExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/whitespace/MethodParamPadExamplesTest.java
@@ -54,4 +54,26 @@ public class MethodParamPadExamplesTest extends AbstractExamplesModuleTestSuppor
         verifyWithInlineConfigParser(getPath("Example2.java"), expected);
     }
 
+    @Test
+    public void testExample3() throws Exception {
+        final String[] expected = {
+            "15:18: " + getCheckMessage(MSG_WS_NOT_PRECEDED, "("),
+            "16:10: " + getCheckMessage(MSG_WS_NOT_PRECEDED, "("),
+            "23:21: " + getCheckMessage(MSG_WS_NOT_PRECEDED, "("),
+            "26:3: " + getCheckMessage(MSG_LINE_PREVIOUS, "("),
+        };
+
+        verifyWithInlineConfigParser(getPath("Example3.java"), expected);
+    }
+
+    @Test
+    public void testExample4() throws Exception {
+        final String[] expected = {
+            "19:19: " + getCheckMessage(MSG_WS_PRECEDED, "("),
+            "20:11: " + getCheckMessage(MSG_WS_PRECEDED, "("),
+        };
+
+        verifyWithInlineConfigParser(getPath("Example4.java"), expected);
+    }
+
 }
