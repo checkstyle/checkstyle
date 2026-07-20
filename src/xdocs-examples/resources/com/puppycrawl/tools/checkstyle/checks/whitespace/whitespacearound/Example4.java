af55975954d36971b04ca360ad14e65347492c0a
Priyanshu <pv254424@gmail.com>
2026-07-20 22:07:30 +0530

    Issue #20794: make examples of WhitespaceAround compact

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/Example4.java
@@ -8,49 +8,10 @@
 </module>
 */
 package com.puppycrawl.tools.checkstyle.checks.whitespace.whitespacearound;
+
 // xdoc section -- start
 class Example4 {
-  interface Empty {}
-  // 2 violations above:
-  //  ''{' is not followed by whitespace'
-  //  ''}' is not preceded with whitespace'
-  public Example4(){}
-  // violation above ''{' is not preceded with whitespace.
-
-  // ok, allowEmptyConstructors is true
-
-  int y = 0;
-  void example() {
-    Runnable noop = () ->{};
-    // 4 violations above:
-    //  ''->' is not followed by whitespace'
-    //  ''{' is not followed by whitespace'
-    //  ''{' is not preceded with whitespace'
-    //  ''}' is not preceded with whitespace'
-    try { }
-    catch (Exception e){}
-    // 3 violations above:
-    //  ''{' is not followed by whitespace'
-    //  ''{' is not preceded with whitespace'
-    //  ''}' is not preceded with whitespace'
-    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-    for (char item: vowels) { }
-    for (int i = 0; i < 10; i++){}
-    // 3 violations above:
-    //  ''{' is not followed by whitespace'
-    //  ''{' is not preceded with whitespace'
-    //  ''}' is not preceded with whitespace'
-    do {} while (y == 1);
-    // 2 violations above:
-    //  ''{' is not followed by whitespace'
-    //  ''}' is not preceded with whitespace'
-    switch (y) {
-      case 1: {}
-      // 2 violations above:
-      //  ''{' is not followed by whitespace'
-      //  ''}' is not preceded with whitespace'
-    }
-  }
+  public Example4(){} // ok, empty constructor body allowed
   void myFunction() {}
   // 2 violations above:
   //  ''{' is not followed by whitespace'
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/Example6.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/Example6.java
@@ -8,48 +8,13 @@
 </module>
 */
 package com.puppycrawl.tools.checkstyle.checks.whitespace.whitespacearound;
+
 // xdoc section -- start
 class Example6 {
-  interface Empty {}
-  // 2 violations above:
-  //  ''{' is not followed by whitespace'
-  //  ''}' is not preceded with whitespace'
-  public Example6(){}
-  // 3 violations above:
-  //  ''{' is not followed by whitespace'
-  //  ''{' is not preceded with whitespace'
-  //  ''}' is not preceded with whitespace'
   int y = 0;
   void example() {
-    Runnable noop = () ->{};
-    // 4 violations above:
-    //  ''->' is not followed by whitespace'
-    //  ''{' is not followed by whitespace'
-    //  ''{' is not preceded with whitespace'
-    //  ''}' is not preceded with whitespace'
-    try { }
-    catch (Exception e){}
-    // 3 violations above:
-    //  ''{' is not followed by whitespace'
-    //  ''{' is not preceded with whitespace'
-    //  ''}' is not preceded with whitespace'
-    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-    for (char item: vowels) { }
-    for (int i = 0; i < 10; i++){}
-
-    // ok, allowEmptyLoops
-    // is true above
-
-    do {} while (y == 1);
-
-    // ok, allowEmptyLoops is true
-
-    switch (y) {
-      case 1: {}
-      // 2 violations above:
-      //  ''{' is not followed by whitespace'
-      //  ''}' is not preceded with whitespace'
-    }
+    for (int i = 0; i < 10; i++){} // ok, empty loop body allowed
+    do {} while (y == 1); // ok, empty loop body allowed
   }
   void myFunction() {}
   // 2 violations above:
