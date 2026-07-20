76b471678495a26d1ce724f8daa0d7d63b03afde
Sahitya0805 <sahityasingh200805@gmail.com>
2026-07-20 17:39:27 +0530

    Issue #20690: add dedicated tests for InvalidJavadocTag and TagInfo to satisfy pitest line coverage

---
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/javadoc/utils/TagInfoTest.java
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2026 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.utils;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.api.LineColumn;
+
+public class TagInfoTest {
+
+    @Test
+    public void testTagInfo() {
+        final LineColumn position = new LineColumn(1, 2);
+        final TagInfo tagInfo = new TagInfo("name", "value", position);
+
+        assertWithMessage("Invalid name")
+                .that(tagInfo.name())
+                .isEqualTo("name");
+        assertWithMessage("Invalid value")
+                .that(tagInfo.value())
+                .isEqualTo("value");
+        assertWithMessage("Invalid position")
+                .that(tagInfo.position())
+                .isEqualTo(position);
+    }
+
+}
