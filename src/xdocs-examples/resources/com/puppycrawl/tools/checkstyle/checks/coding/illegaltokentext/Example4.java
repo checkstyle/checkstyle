ba783b864f5d2b8627dd2ef82182b45fa057a36e
smita1078 <smita.prajapati082@gmail.com>
2026-07-21 21:54:49 +0530

    Issue #20625: Added example for EXAMPLE_COUNT_SUPPRESSED_MODULE

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltokentext/Example4.java
@@ -1,11 +1,7 @@
 /*xml
 <module name="Checker">
   <module name="TreeWalker">
-    <module name="IllegalTokenText">
-      <property name="tokens" value="STRING_LITERAL"/>
-      <property name="format" value="a href"/>
-      <property name="ignoreCase" value="true"/>
-    </module>
+    <module name="IllegalTokenText"/>
   </module>
 </module>
 */
@@ -13,11 +9,11 @@
 package com.puppycrawl.tools.checkstyle.checks.coding.illegaltokentext;
 // xdoc section -- start
 
-public class Example2 {
+public class Example4 {
   public void myTest() {
-    // violation below 'Token text matches the illegal pattern 'a href'.'
+
     String test  = "a href";
-    // violation below 'Token text matches the illegal pattern 'a href'.'
+
     String test2 = "A href";
     String link = "href";
     final String quote = """
