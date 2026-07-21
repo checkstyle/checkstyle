a1f61c86078e7891745070f5c96df1568271ee76
Mohamed Hamed <mdhamed.dev@gmail.com>
2026-07-22 01:51:51 +0200

    Issue #20812: Add MultiFileRegexpHeader to cover Sun style rule 3.1.1

---
+++ src/it/resources/com/sun/checkstyle/test/chapter3fileorganization/rule311beginningcomments/InputBeginningCommentsValid.java
@@ -0,0 +1,17 @@
+/*
+ * InputBeginningCommentsValid
+ *
+ * Version 1.0
+ *
+ * 20/07/2026
+ *
+ * Copyright (c) 2026 Some Company. All rights reserved.
+ */
+
+package com.sun.checkstyle.test.chapter3fileorganization.rule311beginningcomments;
+
+/**
+ * Test input with a valid beginning comment.
+ */
+public class InputBeginningCommentsValid {
+}
