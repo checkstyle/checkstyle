d2dac3bef013447c21391ec86e548b18690126be
unknown <khushivishwas8@gmail.com>
2026-07-22 02:49:09 +0530

    minor: update rule43onestatement inputs

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule43onestatement/InputFormattedOneStatementPerLine2.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule43onestatement/InputFormattedOneStatementPerLine2.java
@@ -1,110 +1,40 @@
-// non-compiled with eclipse: extra semicolumn in imports
-// non-compiled with javac: contains specially crafted set of imports that requires classpath
-
-package com.puppycrawl.tools.checkstyle.checks.coding.onestatementperline;
+package com.google.checkstyle.test.chapter4formatting.rule43onestatement;
 
 /*
- * This file contains test inputs for InputFormattedOneStatementPerLine2
- * which cause compilation problem in Eclipse 4.2.2 but still must be tested.
+ * This file contains test inputs for InputOneStatementPerLine2.
  */
 
-/*
- * Two import statements and one 'empty' statement
- * which are not on the same line are legal.
- */
 import java.awt.event.ActionEvent;
+import java.lang.Integer;
+import java.lang.String;
+import java.lang.annotation.Annotation;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 import javax.swing.JCheckBox;
 
 /** Some javadoc. */
-public class InputFormattedOneStatementPerLine2 {
+public class InputOneStatementPerLine2 {
   /*
    * According to java language specifications,
    * statements end with ';'. That is why ';;'
    * may be considered as two empty statements on the same line
    * and rises violation.
    */
-  ;
-
+  ;; // violation 'Only one statement per line allowed.'
   static {
-    new JCheckBox()
-        .addActionListener(
-            (final ActionEvent e) -> {
-              good();
-            });
+    new JCheckBox().addActionListener((final ActionEvent e) -> { good(); });
+    // violation above ''{' at column 64 should have line break after.'
     List<Integer> ints = new LinkedList<Integer>();
-    ints.stream()
-        .map(
-            t -> {
-              return t * 2;
-            })
-        .filter(
-            t -> {
-              return false;
-            });
-
-    ints.stream()
-        .map(
-            t -> {
-              int m = t * 2;
-              return m;
-            });
-
-    ints.stream()
-        .map(
-            t -> {
-              int m = t * 2;
-              return m;
-            });
-    int i = 3;
-
-    ints.stream().map(t -> t * 2);
-    int k = 4;
-
-    ints.stream().map(t -> t * 2);
-    List<Integer> ints2 = new LinkedList<Integer>();
-
-    ints.stream()
-        .map(
-            t -> {
-              return ints2.stream()
-                  .map(
-                      w -> {
-                        return w * 2;
-                      });
-            });
-
-    ints.stream()
-        .map(
-            t -> {
-              return ints2.stream()
-                  .map(
-                      w -> {
-                        int m = w;
-                        return m;
-                      });
-            });
-
-    ints.stream()
-        .map(
-            t -> {
-              return ints2.stream()
-                  .map(
-                      w -> {
-                        int m = w * 2;
-                        return m;
-                      });
-            });
-    ints.stream()
-        .map(
-            t -> {
-              int l = 0;
-              for (int j = 0; j < 10; j++) {
-                l = j + l;
-              }
-              return l;
-            });
+    ints.stream().map(t -> { return t * 2; }).filter(t -> { return false; });
+    // 2 violations above:
+    //  ''{' at column 28 should have line break after.'
+    //  ''{' at column 59 should have line break after.'
+    ints.stream().map(t -> { int m = t * 2; return m; });
+    // 2 violations above:
+    //  ''{' at column 28 should have line break after.'
+    //  'Only one statement per line allowed.'
   }
 
   private static void good() {}
