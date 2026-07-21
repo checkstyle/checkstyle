456b0f5302893b7ce97b362ebf05d99b9b9ebfc2
arturict <arturict@users.noreply.github.com>
2026-07-21 19:12:31 +0200

    Issue #20590: Add NoFinalizer compact source coverage

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/coding/nofinalizer/compact/InputNoFinalizerCompactSourceFile.java
@@ -0,0 +1,17 @@
+/*
+NoFinalizer
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+// violation below, 'Avoid using finalizer method.'
+protected void finalize() {
+}
+
+void finalize(String value) {
+}
+
+void main() {
+}
