55626a3a5fe163b9fd783128ceebb20afa950e1c
Mohamed Hamed <mdhamed.dev@gmail.com>
2026-07-22 00:32:19 +0200

    Issue #20877: compile input resources by default to match IDE source roots

--- .ci/validation.sh
+++ .ci/validation.sh
@@ -613,6 +613,7 @@ release-dry-run)
     ./mvnw -e --no-transfer-progress release:prepare -DdryRun=true --batch-mode \
     -Darguments='-DskipTests -DskipITs -Djacoco.skip=true -Dpmd.skip=true \
       -Dspotbugs.skip=true -Dxml.skip=true -Dcheckstyle.ant.skip=true \
+      -Dcheckstyle.skipCompileInputResources=true \
       -Dcheckstyle.skip=true -Dgpg.skip=true --no-transfer-progress'
     ./mvnw -e --no-transfer-progress release:clean
   fi
