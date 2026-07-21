9a49f1c93d92c8204865aa748395b3a237e6dd17
daksh <dakshjain737@gmail.com>
2026-07-21 11:36:18 +0530

    Issue #20906: Add checks for Sun Style 3.1.2 - Package And Import Names

---
+++ src/it/resources/com/sun/checkstyle/test/chapter3fileorganization/rule312packageandimportstatements/InputPackageNameBad.java
@@ -0,0 +1,11 @@
+package Com.sun.checkstyle.test.chapter3fileorganization.rule312PackageAndImportstatements;
+
+// violation first line 'Header is missing'
+// violation 3 lines above 'Name
+// 'Com.sun.checkstyle.test.chapter3fileorganization.rule312PackageAndImportstatements'
+// must match pattern '^(com|edu|gov|mil|net|org)(\.[a-zA-Z_]\w*)*$'.'
+
+/**
+ * Input package name with uppercase letter in the middle of the name.
+ */
+final class InputPackageNameBad {}
