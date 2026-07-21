d22d86e01c8ab6198a72471583f69e3aa2e252a9
smita1078 <smita.prajapati082@gmail.com>
2026-07-21 22:52:59 +0530

    Issue #20625: Added example for EXAMPLE_COUNT_SUPPRESSED_MODULE

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/modifier/interfacememberimpliedmodifier/Example7.java
@@ -0,0 +1,40 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="InterfaceMemberImpliedModifier">
+      <property name="violateImpliedPublicField" value="false"/>
+    </module>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.modifier.interfacememberimpliedmodifier;
+
+import java.util.List;
+
+// xdoc section -- start
+public interface Example7 {
+
+  public static final String UNKNOWN = "Unknown";
+  String OTHER = "Other";
+  // 2 violations above:
+  //    'Implied modifier 'final' should be explicit'
+  //    'Implied modifier 'static' should be explicit'
+  // ok, because 'violateImpliedPublicField' is false
+  public static Address instance() {return null;}
+
+  public abstract Address createAddress(String addressLine, String city);
+  List<Address> findAddresses(String city);
+  // 2 violations above:
+  //    'Implied modifier 'abstract' should be explicit'
+  //    'Implied modifier 'public' should be explicit'
+  interface Address {
+    // 2 violations above:
+    //    'Implied modifier 'public' should be explicit'
+    //    'Implied modifier 'static' should be explicit'
+    String getCity();
+    // 2 violations above:
+    //    'Implied modifier 'abstract' should be explicit'
+    //    'Implied modifier 'public' should be explicit'
+  }
+}
+// xdoc section -- end
---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressionsinglefilter/Example6.java
@@ -0,0 +1,30 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="MagicNumber"/>
+  </module>
+  <module name="SuppressionSingleFilter">
+    <property name="lines" value="1,5-100"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.filters.suppressionsinglefilter;
+// xdoc section -- start
+public class Example6 {
+  // filtered violation below ''5' is a magic number'
+  private int MyVariable = 5;
+
+  public void exampleMethod(int a, int b) {
+    int value = 100; // filtered violation ''100' is a magic number'
+
+    Integer. parseInt("3");
+  }
+
+  public void printExample() {
+    int [] x;
+    System.out.println(
+            "example"
+    );
+  }
+}
+// xdoc section -- end
