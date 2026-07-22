34068d1888b7e807e22e6a3ce9e13681bdfbffd0
arturict <arturict@users.noreply.github.com>
2026-07-22 17:41:18 +0200

    Issue #20270: Document compact source exclusion

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/AllChecksCompactSourceCoverageTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/AllChecksCompactSourceCoverageTest.java
@@ -262,7 +262,6 @@ public class AllChecksCompactSourceCoverageTest {
         "UpperEllCheck",
         "UseEnhancedSwitchCheck",
         "VariableDeclarationUsageDistanceCheck",
-        "VisibilityModifierCheck",
         "WhenShouldBeUsedCheck",
         "WhitespaceAfterCheck",
         "WhitespaceAroundCheck",
