d788c0b56d76b9b816f84d70f6d0dba0d0e91cee
vivek-0509 <viveksolanki0509@gmail.com>
2026-07-22 08:55:48 +0530

    Issue #19967: add new check ModuleImportOrder

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/moduleimportorder/InputModuleImportOrderInvalidOption.java
@@ -0,0 +1,12 @@
+/*
+ModuleImportOrder
+option = invalid_option
+separated = (default)false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.moduleimportorder;
+
+public class InputModuleImportOrderInvalidOption {
+}
