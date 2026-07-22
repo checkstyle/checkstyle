c6561efbe8c7daa09db173f1094fb5c52b0a8037
vivek-0509 <viveksolanki0509@gmail.com>
2026-07-22 08:55:48 +0530

    Issue #19967: add new check ModuleImportOrder

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/imports/moduleimportorder/InputModuleImportOrderDefault.java
@@ -0,0 +1,19 @@
+/*
+ModuleImportOrder
+option = (default)top
+separated = (default)false
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+package com.puppycrawl.tools.checkstyle.checks.imports.moduleimportorder;
+
+import module java.desktop;
+import module java.sql;
+import java.util.List;
+
+public class InputModuleImportOrderDefault {
+    List<String> list;
+}
