6f95b4de8d724f1e62e9ddc00559b82dcc3c98bf
smita1078 <smita.prajapati082@gmail.com>
2026-07-25 16:53:51 +0530

    Issue #20625: Added example to remove suppression for : regexponfilename/translation

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/translation/Example4.java
@@ -0,0 +1,15 @@
+/*xml
+<module name="Checker">
+  <module name="Translation"/>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.translation;
+
+/*
+// xdoc section -- start
+messages_de.properties
+// violation above 'Properties file 'messages.properties' missing.'
+// xdoc section -- end
+*/
+public class Example4 {}
