b71da6618ed9d7686d583cac3a44fc5130b14e56
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-07-26 19:56:41 +0100

    Issue #19937: create new JavadocLinkFirstOccurrenceCheck

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/javadoc/javadoclinkfirstoccurrence/InputXpathJavadocLinkFirstOccurrenceTwo.java
@@ -0,0 +1,11 @@
+package org.checkstyle.suppressionxpathfilter.javadoc.javadoclinkfirstoccurrence;
+
+public class InputXpathJavadocLinkFirstOccurrenceTwo {
+
+    /**
+     * Uses a {@link java.util.StringJoiner}.
+     * The {@link String} is returned.
+     */
+    public String invalid() { return ""; }
+
+}
