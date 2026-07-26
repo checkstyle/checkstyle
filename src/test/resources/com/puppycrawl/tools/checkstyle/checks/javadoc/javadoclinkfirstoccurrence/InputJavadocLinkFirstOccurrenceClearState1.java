f1e999a182f46029977e80e7b6d34bd5e62282b1
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-07-26 19:56:41 +0100

    Issue #19937: create new JavadocLinkFirstOccurrenceCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoclinkfirstoccurrence/InputJavadocLinkFirstOccurrenceClearState1.java
@@ -0,0 +1,9 @@
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoclinkfirstoccurrence;
+
+class InputJavadocLinkFirstOccurrenceClearState1 {
+
+    /**
+     * Uses a {@link String}.
+     */
+    public void first() { }
+}
