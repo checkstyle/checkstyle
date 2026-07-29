640ad551bcf9abd5e110cddeb12a5884c7c7a1d7
Vedanth <vedanth1101@gmail.com>
2026-07-29 15:46:03 +0530

    Issue #20954: Quote violation messages in NestedTryDepth examples

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -365,8 +365,6 @@ public final class InlineConfigParser {
             "checks/coding/illegaltype/InputIllegalTypeTestStaticImports.java",
             "checks/coding/nestedifdepth/Example1.java",
             "checks/coding/nestedifdepth/Example2.java",
-            "checks/coding/nestedtrydepth/Example1.java",
-            "checks/coding/nestedtrydepth/Example2.java",
             "checks/coding/noclone/Example1.java",
             "checks/coding/simplifybooleanreturn/Example1.java",
             "checks/coding/superfinalize/InputSuperFinalizeVariations.java",
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/nestedtrydepth/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/nestedtrydepth/Example1.java
@@ -17,16 +17,16 @@ public class Example1 {
 
     try {
       try {
-        try { // violation, current depth is 2, default max allowed depth is 1
+        try { // violation 'Nested try depth is 2 (max allowed is 1).'
         } catch (Exception e) {}
       } catch (Exception e) {}
     } catch (Exception e) {}
 
     try {
       try {
-        try { // violation, current depth is 2, default max allowed depth is 1
-          try { // violation, current depth is 3, default max allowed depth is 1
-            try { // violation, current depth is 4, default max allowed depth is 1
+        try { // violation 'Nested try depth is 2 (max allowed is 1).'
+          try { // violation 'Nested try depth is 3 (max allowed is 1).'
+            try { // violation 'Nested try depth is 4 (max allowed is 1).'
             } catch (Exception e) {}
           } catch (Exception e) {}
         } catch (Exception e) {}
