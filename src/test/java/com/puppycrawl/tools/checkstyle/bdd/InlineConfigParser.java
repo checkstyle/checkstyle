845ce9dd512f6c38add8d5205540a80581e7611a
Noor Ahmadi <noorahmadi014@gmail.com>
2026-07-30 17:37:50 -0400

    Issue #20954: Quote NPathComplexity example violation messages

--- src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/bdd/InlineConfigParser.java
@@ -388,8 +388,6 @@ public final class InlineConfigParser {
             "checks/coding/unusedlocalvariable/InputUnusedLocalVariableUnnamedTryCatch.java",
             "checks/imports/avoidstarimport/InputAvoidStarImportExcludes.java",
             "checks/imports/importorder/Example10.java",
-            "checks/metrics/npathcomplexity/Example1.java",
-            "checks/metrics/npathcomplexity/Example2.java",
             "checks/naming/recordtypeparametername/Example1.java",
             "checks/naming/recordtypeparametername/Example2.java",
             "checks/regexp/regexpmultiline/InputRegexpMultilineSemantic8.java",
