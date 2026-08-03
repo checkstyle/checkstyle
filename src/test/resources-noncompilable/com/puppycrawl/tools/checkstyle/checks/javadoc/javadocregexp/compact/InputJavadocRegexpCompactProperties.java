111431373f603a4b7f8a1fbdb4aedd1388bf1c4d
gianmarco <gianmarco.schifone02@gmail.com>
2026-08-03 18:42:34 +0200

    Issue #20987: New check JavadocRegexp

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocregexp/compact/InputJavadocRegexpCompactProperties.java
@@ -0,0 +1,18 @@
+/*
+JavadocRegexp
+format = AKA
+ignoreCase = true
+ignoreMarkup = true
+violateExecutionOnNonTightHtml = (default)false
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+// violation below 'Javadoc content matches the illegal pattern'
+/**
+ * Lowercase aka is matched.
+ */
+void main() {
+}
