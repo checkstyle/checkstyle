cf4ccf382e5d9e2cc4a31f18bf92be0e22df8b48
gianmarco <gianmarco.schifone02@gmail.com>
2026-08-03 18:42:34 +0200

    Issue #20987: New check JavadocRegexp

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocregexp/Example1.java
@@ -0,0 +1,30 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="JavadocRegexp">
+      <property name="format" value="(^|\W)(AKA)(\W|$)"/>
+      <property name="ignoreCase" value="false"/>
+      <property name="ignoreMarkup" value="true"/>
+    </module>
+  </module>
+</module>
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.javadoc.javadocregexp;
+
+// xdoc section - start
+class Example1 {
+
+  // ok, rendered text has no AKA
+  /**
+   * See <a href="https://example.com/aka">documentation</a>.
+   */
+  void first() {}
+
+  // violation below 'Javadoc content matches the illegal pattern'
+  /**
+   * Creates a user, AKA an account owner.
+   */
+  void second() {}
+}
+// xdoc section - end
