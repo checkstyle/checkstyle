f7e270752d3281d6eab1f89f1c1c61e92bf4bb6e
Ali Akbar <akbar01@ads.uni-passau.de>
2026-08-05 08:32:46 +0200

    Issue #20794: Compact WhitespaceAround examples with spacing on all empty bodies

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/Example7.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/whitespacearound/Example7.java
@@ -14,46 +14,29 @@ class Example7 {
   // 2 violations above:
   //  ''{' is not followed by whitespace'
   //  ''}' is not preceded with whitespace'
-  public Example7(){}
-  // 3 violations above:
-  //  ''{' is not followed by whitespace'
-  //  ''{' is not preceded with whitespace'
-  //  ''}' is not preceded with whitespace'
+  public Example7(){ }
+  // violation above ''{' is not preceded with whitespace.'
   int y = 0;
   void example() {
-    Runnable noop = () ->{};
+    Runnable noop = () ->{ };
     // violation above '->' is not followed by whitespace.
-
-    // ok, allowEmptyLambdas
-    // is true above
+    // ok, allowEmptyLambdas is true
 
     try { }
-    catch (Exception e){}
-    // 3 violations above:
-    //  ''{' is not followed by whitespace'
-    //  ''{' is not preceded with whitespace'
-    //  ''}' is not preceded with whitespace'
+    catch (Exception e){ }
+    // violation above ''{' is not preceded with whitespace.'
     char[] vowels = {'a', 'e', 'i', 'o', 'u'};
     for (char item: vowels) { }
-    for (int i = 0; i < 10; i++){}
-    // 3 violations above:
-    //  ''{' is not followed by whitespace'
-    //  ''{' is not preceded with whitespace'
-    //  ''}' is not preceded with whitespace'
-    do {} while (y == 1);
+    do{ } while (y == 1);
     // 2 violations above:
-    //  ''{' is not followed by whitespace'
-    //  ''}' is not preceded with whitespace'
+    //  ''do' is not followed by whitespace'
+    //  ''{' is not preceded with whitespace'
     switch (y) {
-      case 1: {}
-      // 2 violations above:
-      //  ''{' is not followed by whitespace'
-      //  ''}' is not preceded with whitespace'
+      case 1:{ }
+      // violation above ''{' is not preceded with whitespace.'
     }
   }
-  void myFunction() {}
-  // 2 violations above:
-  //  ''{' is not followed by whitespace'
-  //  ''}' is not preceded with whitespace'
+  void myFunction(){ }
+  // violation above ''{' is not preceded with whitespace.'
 }
 // xdoc section - end
