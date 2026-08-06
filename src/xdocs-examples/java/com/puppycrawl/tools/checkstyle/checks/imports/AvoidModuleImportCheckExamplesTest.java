ab36b4897ac37cf0ad19ce3f6314afebd53180ab
atharv <atharv55215@gmail.com>
2026-08-06 18:57:57 +0530

    Issue #19968: Add check AvoidModuleImport

---
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/imports/AvoidModuleImportCheckExamplesTest.java
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2026 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.imports;
+
+import static com.puppycrawl.tools.checkstyle.checks.imports.AvoidModuleImportCheck.MSG_COUNT;
+import static com.puppycrawl.tools.checkstyle.checks.imports.AvoidModuleImportCheck.MSG_KEY;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.AbstractExamplesModuleTestSupport;
+import com.puppycrawl.tools.checkstyle.utils.CommonUtil;
+
+public class AvoidModuleImportCheckExamplesTest extends AbstractExamplesModuleTestSupport {
+
+    @Override
+    public String getPackageLocation() {
+        return "com/puppycrawl/tools/checkstyle/checks/imports/avoidmoduleimport";
+    }
+
+    @Test
+    public void testExample1() throws Exception {
+        final String[] expected = {
+            "14:1: " + getCheckMessage(MSG_KEY, "java.base"),
+            "16:1: " + getCheckMessage(MSG_KEY, "java.sql"),
+        };
+
+        verifyWithInlineConfigParser(
+                getNonCompilablePath("Example1.java"), expected);
+    }
+
+    @Test
+    public void testExample2() throws Exception {
+        final String[] expected = CommonUtil.EMPTY_STRING_ARRAY;
+
+        verifyWithInlineConfigParser(
+                getNonCompilablePath("Example2.java"), expected);
+    }
+
+    @Test
+    public void testExample3() throws Exception {
+        final String[] expected = {
+            "18:1: " + getCheckMessage(MSG_COUNT, 1),
+        };
+
+        verifyWithInlineConfigParser(
+                getNonCompilablePath("Example3.java"), expected);
+    }
+
+}
