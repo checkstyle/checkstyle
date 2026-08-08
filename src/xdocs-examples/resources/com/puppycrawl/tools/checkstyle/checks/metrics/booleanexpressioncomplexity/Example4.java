0313a9d7bfffdcfdf510155e87bf9717a02e0335
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-08-08 20:27:09 +0500

    Issue #1525: Add new property in BooleanExpressionComplexityCheck

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example4.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example4.java
@@ -1,32 +1,34 @@
 /*xml
 <module name="Checker">
   <module name="TreeWalker">
-    <module name="BooleanExpressionComplexity"/>
+    <module name="BooleanExpressionComplexity">
+      <property name="treatUniformExpressionsAsOne" value="false"/>
+    </module>
   </module>
 </module>
 */
 package com.puppycrawl.tools.checkstyle.checks.metrics.booleanexpressioncomplexity;
 
 // xdoc section - start
-public class Example1
+public class Example4
 {
   public static void main(String ... args)
   {
     boolean a = true;
     boolean b = false;
 
-    boolean c = (a & b) | (b ^ a); // ok, 1(&) + 1(|) + 1(^) = 3 (max allowed 3)
+    boolean c = (a & b) | (b ^ a); // ok, 1(^) = 1 (max allowed 3)
 
     boolean d = (a & b) | (b ^ a) | (a ^ b);
     // violation above 'Boolean expression complexity is 5 (max allowed is 3)'
 
     boolean e = a ^ (a || b) ^ (b || a) & (a | b);
     // violation above 'Boolean expression complexity is 6 (max allowed is 3)'
-    // 1(^) + 1(||) + 1(^) + 1(||) + 1(&) + 1(|) = 6
+    // 1(^) + 1(||) + 1(^) + 1(||) = 4, & and | are ignored here
   }
 
   public static boolean isType(int type)
-  {
+  {// violation below 'Boolean expression complexity is 4 (max allowed is 3)'
     return type == 1
             || type == 2
             || type == 3
