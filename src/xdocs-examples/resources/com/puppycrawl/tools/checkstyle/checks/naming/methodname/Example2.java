130eefbe85acaec9a7dfbcd03aab79b8d54aec7d
smita1078 <smita.prajapati082@gmail.com>
2026-08-08 22:01:07 +0530

    Issue #21119: Fix duplicate violation behavior in examples: methodname

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/methodname/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/naming/methodname/Example2.java
@@ -2,7 +2,7 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="MethodName">
-       <property name="format" value="^[a-z](_?[a-zA-Z0-9]+)*$"/>
+       <property name="format" value="^[a-z][a-zA-Z0-9]{7,}$"/>
     </module>
   </module>
 </module>
@@ -14,10 +14,10 @@ package com.puppycrawl.tools.checkstyle.checks.naming.methodname;
 
 // xdoc section - start
 class Example2 {
-  public void method1() {}
+  public void method1() {}    // violation 'Name 'method1' must match pattern'
   protected void Method2() {} // violation 'Name 'Method2' must match pattern'
-  private void Method3() {} // violation 'Name 'Method3' must match pattern'
-  public void Example3() {} // violation 'Name 'Example3' must match pattern'
+  private void Method3() {}   // violation 'Name 'Method3' must match pattern'
+  public void Example3() {}   // violation 'Name 'Example3' must match pattern'
   void Method5() {} // violation 'Name 'Method5' must match pattern'
 }
 // xdoc section - end
