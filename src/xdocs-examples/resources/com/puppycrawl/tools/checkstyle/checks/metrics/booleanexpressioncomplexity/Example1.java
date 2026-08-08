008946b12d4eb85d7e765ea4dda19ab35f865800
Zaheer Ahmad <129817147+ZaheerAhmadDev@users.noreply.github.com>
2026-08-08 20:27:09 +0500

    Issue #1525: Add new property in BooleanExpressionComplexityCheck

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/metrics/booleanexpressioncomplexity/Example1.java
@@ -24,5 +24,27 @@ public class Example1
     // violation above 'Boolean expression complexity is 6 (max allowed is 3)'
     // 1(^) + 1(||) + 1(^) + 1(||) + 1(&) + 1(|) = 6
   }
+
+  public static boolean isType(int type)
+  {
+    return type == 1
+            || type == 2
+            || type == 3
+            || type == 4
+            || type == 5;
+  }
+
+  public static boolean isValid(boolean a, boolean b, boolean c, boolean d)
+  {
+    return a && b && c && d;
+  }
+
+  public static boolean isExpressionAsOne(boolean a, boolean b, boolean c,
+           boolean d, Object leftType, Object rightType)
+  {
+    return a && b && c &&
+            d && leftType != null && rightType != null;
+  }
+
 }
 // xdoc section - end
