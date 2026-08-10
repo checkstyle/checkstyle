bff17b3cb595c18a04a4bade10924cbb448b177e
smita1078 <smita.prajapati082@gmail.com>
2026-08-10 18:16:57 +0530

    Issue #21137: Added default config Example : matchxpath

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/matchxpath/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/matchxpath/Example1.java
@@ -2,11 +2,6 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="MatchXpath">
-      <property name="query"
-           value="//METHOD_DEF[.//LITERAL_PRIVATE
-                  and following-sibling::METHOD_DEF[.//LITERAL_PUBLIC]]"/>
-      <message key="matchxpath.match"
-           value="Private methods must appear after public methods"/>
     </module>
   </module>
 </module>
@@ -16,10 +11,10 @@ package com.puppycrawl.tools.checkstyle.checks.coding.matchxpath;
 // xdoc section - start
 public class Example1 {
   public void method1() { }
-  // violation below 'Private methods must appear after public methods'
+
   private void method2() { }
   public void method3() { }
-  // violation below 'Private methods must appear after public methods'
+
   private void method4() { }
   public void method5() { }
   private void method6() { }
