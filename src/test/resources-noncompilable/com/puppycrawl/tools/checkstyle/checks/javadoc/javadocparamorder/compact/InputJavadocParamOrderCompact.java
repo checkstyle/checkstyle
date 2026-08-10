eed9866e342073c7c23f39a432f7d221520848ca
gianmarco <gianmarco.schifone02@gmail.com>
2026-08-10 11:16:08 +0200

    Issue #8990: New check JavadocParamOrder

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocparamorder/compact/InputJavadocParamOrderCompact.java
@@ -0,0 +1,17 @@
+/*
+JavadocParamOrder
+violateExecutionOnNonTightHtml = (default)false
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+// violation 5 lines below '@param tag for .* should be in declaration order.'
+/**
+ * Compact source method.
+ *
+ * @param second second parameter
+ * @param first first parameter
+ */
+void compactSourceMethod(String first, String second) {
+}
