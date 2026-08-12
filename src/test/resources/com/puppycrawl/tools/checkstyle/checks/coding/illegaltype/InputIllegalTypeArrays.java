9fb7575c9042a5705d26a169504e70872dce262c
Tejas H <tejashtejuhtj@gmail.com>
2026-08-12 21:06:20 +0530

    Issue #21177: Support bracket auto-escaping in BDD expected messages

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltype/InputIllegalTypeArrays.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegaltype/InputIllegalTypeArrays.java
@@ -17,22 +17,22 @@ package com.puppycrawl.tools.checkstyle.checks.coding.illegaltype;
 
 public class InputIllegalTypeArrays {
 
-    public Boolean[] array; // violation "Usage of type 'Boolean\[\]' is not allowed."
+    public Boolean[] array; // violation "Usage of type 'Boolean[]' is not allowed."
 
-    public Boolean[][] matrix; // violation "Usage of type 'Boolean\[\]\[\]' is not allowed."
+    public Boolean[][] matrix; // violation "Usage of type 'Boolean[][]' is not allowed."
 
-    public Boolean[] getArray() { // violation "Usage of type 'Boolean\[\]' is not allowed."
+    public Boolean[] getArray() { // violation "Usage of type 'Boolean[]' is not allowed."
         Boolean[] value = array != null ? array : new Boolean[0];
-        // violation above "Usage of type 'Boolean\[\]' is not allowed."
+        // violation above "Usage of type 'Boolean[]' is not allowed."
 
         return value;
     }
 
     public Boolean[][] getMatrix() {
-        // violation above "Usage of type 'Boolean\[\]\[\]' is not allowed."
+        // violation above "Usage of type 'Boolean[][]' is not allowed."
 
         Boolean[][] value = matrix != null ? matrix : new Boolean[0][0];
-        // violation above "Usage of type 'Boolean\[\]\[\]' is not allowed."
+        // violation above "Usage of type 'Boolean[][]' is not allowed."
 
         return value;
     }
