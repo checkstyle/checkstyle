59c81fda95b8321a3c6812c16becd4b8e5b362c0
Burak KALAYCI <kalayciburak1996@gmail.com>
2026-08-12 21:15:38 +0300

    Issue #21119: Fix duplicate violation behavior in examples: localfinalvariablename

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExampleFileTest.java
@@ -103,7 +103,6 @@ public class XdocsExampleFileTest {
         "checks/javadoc/javadocvariable/",
         "checks/javadoc/missingjavadoctype/",
         "checks/naming/illegalidentifiername/",
-        "checks/naming/localfinalvariablename/",
         "checks/naming/patternvariablename/",
         "checks/outertypefilename/",
         "checks/regexp/regexpmultiline/",
