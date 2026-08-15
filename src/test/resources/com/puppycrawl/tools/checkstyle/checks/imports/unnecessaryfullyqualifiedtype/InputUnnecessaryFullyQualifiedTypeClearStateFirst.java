ac8c083813fa8c80cfd326e0b63ecaa88eb7fea2
ZaheerAhmadDev <zk7386048@gmail.com>
2026-08-15 11:19:51 +0500

    Issue #19885: New Check: UnnecessaryFullyQualifiedTypeCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/unnecessaryfullyqualifiedtype/InputUnnecessaryFullyQualifiedTypeClearStateFirst.java
@@ -0,0 +1,15 @@
+/*
+UnnecessaryFullyQualifiedType
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.unnecessaryfullyqualifiedtype;
+
+import java.awt.List;
+
+public class InputUnnecessaryFullyQualifiedTypeClearStateFirst {
+
+    private List list;
+
+}
