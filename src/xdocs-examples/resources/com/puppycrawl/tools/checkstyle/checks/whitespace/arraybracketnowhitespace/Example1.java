6e5717c7096d60ef13670a3e1e587031e1b03983
Tejas H <tejashtejuhtj@gmail.com>
2026-08-15 17:39:14 +0530

    Issue #21177: Support bracket auto-escaping in BDD expected messages

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/arraybracketnowhitespace/Example1.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/arraybracketnowhitespace/Example1.java
@@ -13,48 +13,48 @@ import java.util.function.IntFunction;
 // xdoc section - start
 class Example1 {
   int[] numbersGood = new int[10];
-  int[] numbersWarn = new int[10 ]; // violation ''\]' is preceded with whitespace'
+  int[] numbersWarn = new int[10 ]; // violation '']' is preceded with whitespace'
 
   String[] dataGood = {"a", "b"};
-  String [] dataWarn = {"a", "b"}; // violation ''\[' is preceded with whitespace'
+  String [] dataWarn = {"a", "b"}; // violation ''[' is preceded with whitespace'
 
   byte bufferGood[];
   byte bufferWarn[ ];
   // 2 violations above:
-  // ''\[' is followed by whitespace'
-  // ''\]' is preceded with whitespace'
+  // ''[' is followed by whitespace'
+  // '']' is preceded with whitespace'
 
   int[] matrixGood[];
-  int[] matrixWarn[] ; // violation ''\]' is followed by whitespace'
+  int[] matrixWarn[] ; // violation '']' is followed by whitespace'
 
   void methodGood(int[] arr) {}
-  void methodWarn(int[]arr) {} // violation ''\]' is not followed by whitespace'
+  void methodWarn(int[]arr) {} // violation '']' is not followed by whitespace'
 
   void processArray(int[] arr) {
     int[] goodArr = {arr[0], arr[1]};
-    int[] warnArr = {arr[0], arr[1] };// violation ''\]' is followed by whitespace'
+    int[] warnArr = {arr[0], arr[1] };// violation '']' is followed by whitespace'
 
     int valueGood = arr[0] * 2;
-    int valueWarn = arr[0]* 2; // violation ''\]' is not followed by whitespace'
+    int valueWarn = arr[0]* 2; // violation '']' is not followed by whitespace'
 
     int good = arr[0] >> 2;
-    int warn = arr[0]>>2; // violation ''\]' is not followed by whitespace'
+    int warn = arr[0]>>2; // violation '']' is not followed by whitespace'
 
     arr[0]++;
-    arr[0] ++; // violation ''\]' is followed by whitespace'
+    arr[0] ++; // violation '']' is followed by whitespace'
 
     Class<?> goodClass = int[].class;
-    Class<?> warnClass = int[] .class; // violation ''\]' is followed by whitespace.'
+    Class<?> warnClass = int[] .class; // violation '']' is followed by whitespace.'
 
     IntFunction<int[]> goodMethodRef = int[]::new;
     IntFunction<int[]> warnMethodRef = int[] ::new;
-    // violation above ''\]' is followed by whitespace.'
+    // violation above '']' is followed by whitespace.'
     IntFunction<int[] > warnAngleBracket = int[]::new;
-    // violation above ''\]' is followed by whitespace.'
+    // violation above '']' is followed by whitespace.'
   }
 
   void goodEllipsis(String[]... params) {}
   void warnEllipsis(String[] ... params) {}
-  // violation above ''\]' is followed by whitespace'
+  // violation above '']' is followed by whitespace'
 }
 // xdoc section - end
