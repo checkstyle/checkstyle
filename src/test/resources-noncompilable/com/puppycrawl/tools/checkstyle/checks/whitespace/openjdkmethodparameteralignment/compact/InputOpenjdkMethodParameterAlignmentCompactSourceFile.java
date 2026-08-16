c44a6d46fb8af8e956a53685041373f192b5f8da
Mohamed Hamed <mdhamed.dev@gmail.com>
2026-08-16 12:37:17 +0200

    Issue #20638: Add new check OpenjdkMethodParameterAlignment

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/whitespace/openjdkmethodparameteralignment/compact/InputOpenjdkMethodParameterAlignmentCompactSourceFile.java
@@ -0,0 +1,30 @@
+/*
+OpenjdkMethodParameterAlignment
+
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+void main() {
+    int a = 1;
+}
+
+void listedVertically(int a,
+                      int b,
+                      int c) {
+}
+
+void twoOnOneLine(int a,
+                  int b, int c) {
+    // violation 2 lines above 'Only one parameter is allowed per line in a vertical list.'
+}
+
+void twoOnFirstLine(int a, int b,
+                    int c) {
+    // violation 2 lines above 'Only one parameter is allowed per line in a vertical list.'
+}
+
+void wrappedByEightSpaces(int a, int b,
+        int c) {
+}
