54a1f0e38f33a1d4d97b5d235c7fcfc6c51c0d74
sahitya0xsingh <sahitya0xsingh@users.noreply.github.com>
2026-08-20 11:33:16 +0530

    Issue #21160: Add indentation support for module declarations

---
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/indentation/ModuleDirectiveHandler.java
@@ -0,0 +1,61 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code and other text files for adherence to a set of rules.
+// Copyright (C) 2001-2026 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.puppycrawl.tools.checkstyle.checks.indentation;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * Handler for module directives.
+ *
+ */
+public class ModuleDirectiveHandler extends AbstractExpressionHandler {
+
+    /**
+     * Construct an instance of this handler with the given indentation check,
+     * abstract syntax tree, and parent handler.
+     *
+     * @param indentCheck   the indentation check
+     * @param ast           the abstract syntax tree
+     * @param parent        the parent handler
+     */
+    public ModuleDirectiveHandler(IndentationCheck indentCheck,
+        DetailAST ast, AbstractExpressionHandler parent) {
+        super(indentCheck, "module directive", ast, parent);
+    }
+
+    @Override
+    public void checkIndentation() {
+        final DetailAST mainAst = getMainAst();
+
+        if (mainAst != null) {
+            final int columnNo = expandedTabsColumnNo(mainAst);
+
+            if (!getIndent().isAcceptable(columnNo) && isOnStartOfLine(mainAst)) {
+                logError(mainAst, "", columnNo);
+            }
+
+            final DetailAST semi = mainAst.findFirstToken(TokenTypes.SEMI);
+
+            checkWrappingIndentation(mainAst, semi);
+        }
+    }
+
+}
