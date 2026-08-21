d843101967413683a1a8571063bab6e10ccae893
Tejas H <tejashtejuhtj@gmail.com>
2026-08-21 14:52:13 +0530

    Issue #21177: Support bracket auto-escaping in BDD expected messages

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/arraybracketnowhitespace/InputArrayBracketNoWhitespaceMisc.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/arraybracketnowhitespace/InputArrayBracketNoWhitespaceMisc.java
@@ -7,12 +7,12 @@ package com.puppycrawl.tools.checkstyle.checks.whitespace.arraybracketnowhitespa
 public class InputArrayBracketNoWhitespaceMisc {
 
     void testVar(int... nums) {
-        int b = nums [0]; // violation ''\[' is preceded with whitespace.'
-        int[] arr3 = new int [] {1,2,3}; // violation ''\[' is preceded with whitespace.'
+        int b = nums [0]; // violation ''[' is preceded with whitespace.'
+        int[] arr3 = new int [] {1,2,3}; // violation ''[' is preceded with whitespace.'
     }
 
     void testInstanceof(Object o) {
-        if (o instanceof int []) {} // violation ''\[' is preceded with whitespace.'
+        if (o instanceof int []) {} // violation ''[' is preceded with whitespace.'
     }
 
     void testMultilineAncestor() {
