c7427dedb469eb436cbe441f966b73b0128a11ec
smita1078 <smita.prajapati082@gmail.com>
2026-08-22 20:15:10 +0530

    Issue #21305: Optimize lychee external link check to avoid multi-hour runs

--- .ci/run-link-check-plugin.sh
+++ .ci/run-link-check-plugin.sh
@@ -24,6 +24,7 @@ if [[ $OPTION == "--skip-external" ]]; then
 else
   echo "Checking internal (checkstyle website) and external links."
   curl --fail-with-body -I https://sourceforge.net/projects/checkstyle/
+  export GITHUB_TOKEN="${GITHUB_TOKEN:-}"
   ./mvnw -e --no-transfer-progress clean post-site -Pno-validations \
     -Dmaven.javadoc.skip=false -Dlychee.skip=false
 fi
