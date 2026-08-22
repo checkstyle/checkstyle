d7a172e22cbd331e1be03c001eea77d491eb23aa
Abdullah Bohra <abdullahbohra790@gmail.com>
2026-08-23 02:12:22 +0530

    Issue #21119: Remove outertypefilename from SUPPRESSED_UNIQUENESS_CHECK_MODULES

--- src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/OuterTypeFilenameCheckExamplesTest.java
+++ src/xdocs-examples/java/com/puppycrawl/tools/checkstyle/checks/OuterTypeFilenameCheckExamplesTest.java
@@ -51,30 +51,21 @@ public class OuterTypeFilenameCheckExamplesTest extends AbstractExamplesModuleTe
     }
 
     @Test
-    public void testExample3() throws Exception {
+    public void testUseCase1() throws Exception {
         final String[] expected = {
             "12:1: " + getCheckMessage(MSG_KEY),
         };
 
-        verifyWithInlineConfigParser(getPath("Example3.java"), expected);
+        verifyWithInlineConfigParser(getPath("UseCase1.java"), expected);
     }
 
     @Test
-    public void testExample4() throws Exception {
+    public void testUseCase2() throws Exception {
         final String[] expected = {
             "12:1: " + getCheckMessage(MSG_KEY),
         };
 
-        verifyWithInlineConfigParser(getPath("Example4.java"), expected);
-    }
-
-    @Test
-    public void testExample5() throws Exception {
-        final String[] expected = {
-            "12:1: " + getCheckMessage(MSG_KEY),
-        };
-
-        verifyWithInlineConfigParser(getPath("Example5.java"), expected);
+        verifyWithInlineConfigParser(getPath("UseCase2.java"), expected);
     }
 
 }
