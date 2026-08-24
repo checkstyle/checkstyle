e1bbcd5e4aca518ec849f1f407acf3d75c89403f
Saad Haider <mohd.saadhaider@gmail.com>
2026-08-24 20:30:54 +0530

    Issue #21063: add new check InappropriateJavadocBlockTagsOnPackage

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/utils/javadocutil/annotationnojavadoc/package-info.java
@@ -0,0 +1,7 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.javadoc.MissingJavadocPackageCheck
+
+*/
+
+package com.puppycrawl.tools.checkstyle.utils.javadocutil.annotationnojavadoc;
+// violation above 'Missing javadoc for package-info.java file'
