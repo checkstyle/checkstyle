fb35e1ad82623bedca608a8ae7bf5741bb485c86
Saad Haider <mohd.saadhaider@gmail.com>
2026-08-24 20:30:54 +0530

    Issue #21063: add new check InappropriateJavadocBlockTagsOnPackage

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/utils/javadocutil/InputJavadocUtilPackageNoComment.java
@@ -0,0 +1,7 @@
+/*
+com.puppycrawl.tools.checkstyle.checks.javadoc.InappropriateJavadocBlockTagsOnPackageCheck
+violateExecutionOnNonTightHtml = (default)false
+
+*/
+
+package com.puppycrawl.tools.checkstyle.utils.javadocutil;
