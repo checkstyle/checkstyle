dd3334d4f7007276dee6f5c6cd2a8cef11f8dbe5
Saad Haider <mohd.saadhaider@gmail.com>
2026-08-24 20:30:54 +0530

    Issue #21063: add new check InappropriateJavadocBlockTagsOnPackage

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/javadoc/inappropriatejavadocblocktagsonpackage/compact/InputInappropriateJavadocBlockTagsOnPackageCompactSourceFile.java
@@ -0,0 +1,15 @@
+/*
+InappropriateJavadocBlockTagsOnPackage
+violateExecutionOnNonTightHtml = (default)false
+
+*/
+
+// non-compiled with javac: Compilable with Java25
+
+/**
+ * Invalid Javadoc with inappropriate tags.
+ * @return Something invalid.
+ * @throws Exception This is also invalid.
+ */
+package com.puppycrawl.tools.checkstyle.checks.javadoc.inappropriatejavadocblocktagsonpackage.compact;
+
