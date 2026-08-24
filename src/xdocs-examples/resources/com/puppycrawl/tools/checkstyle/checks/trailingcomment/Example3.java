a9c68006fb8a116f735c4c665f27892f285f90d0
ryikaio <hello.ryuzaki@gmail.com>
2026-08-24 14:01:20 +0530

    Issue #21330: Move violation hints from trailing comments

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/trailingcomment/Example3.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/trailingcomment/Example3.java
@@ -16,15 +16,18 @@ public class Example3 {
   int b;
   int c;
   int d; // ok, SUPPRESS CHECKSTYLE
+  // ok above, matches legalComment pattern
 
   public static void main(String[] args) {
     int x = 10;
 
     if (/* OK */ x > 5) {}
-    int a = 5; // violation 'Don't use trailing comments.'
+    int a = 5; // trailing comment
+    // violation above 'Don't use trailing comments.'
     doSomething(
             "param1"
-    ); // ok, by default such trailing of method/code-block ending is allowed
+    ); // trailing comment
+    // ok above, trailing comment after ');' is allowed by default
 
   }
 
