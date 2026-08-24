4cecf64259150d09e583586b1d0a7a699712369f
sahitya0xsingh <sahitya0xsingh@users.noreply.github.com>
2026-08-24 17:49:53 +0530

    Issue #21062: Add InappropriateJavadocBlockTagsOnField check

---
+++ src/it/resources/org/checkstyle/suppressionxpathfilter/javadoc/inappropriatejavadocblocktagsonfield/InputXpathInappropriateJavadocBlockTagsOnFieldEnum.java
@@ -0,0 +1,10 @@
+package org.checkstyle.suppressionxpathfilter.javadoc.inappropriatejavadocblocktagsonfield;
+
+public enum InputXpathInappropriateJavadocBlockTagsOnFieldEnum {
+    VALUE;
+
+    /**
+     * @return value
+     */
+    private int field; // warn
+}
