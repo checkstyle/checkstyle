923fab66d19c283a8d61cfe2b35938bf3a0ae2ba
sahitya0xsingh <sahitya0xsingh@users.noreply.github.com>
2026-08-24 15:06:28 +0530

    Issue #21062: Add InappropriateJavadocBlockTagsOnField check

---
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/inappropriatejavadocblocktagsonfield/Example1.java
@@ -0,0 +1,48 @@
+/*xml
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="InappropriateJavadocBlockTagsOnField"/>
+  </module>
+</module>
+*/
+package com.puppycrawl.tools.checkstyle.checks.javadoc.inappropriatejavadocblocktagsonfield;
+
+// xdoc section - start
+class Example1 {
+  /**
+   * Valid Javadoc on field with appropriate tags.
+   *
+   * @since 1.0
+   * @deprecated for testing
+   * @see Object
+   * @serial field description
+   */
+  private String field1;
+
+  /**
+   * Inappropriate tags on class field.
+   *
+   * @return Invalid tag for field.
+   * @param BAD Invalid tag for field.
+   * @throws Exception Invalid tag for field.
+   * @exception Exception Invalid tag for field.
+   */
+  private String field2;
+  // violation 6 lines above 'Invalid '@return' tag for 'field2'.'
+  // violation 6 lines above 'Invalid '@param' tag for 'field2'.'
+  // violation 6 lines above 'Invalid '@throws' tag for 'field2'.'
+  // violation 6 lines above 'Invalid '@exception' tag for 'field2'.'
+
+  interface MyInterface {
+    /**
+     * Inappropriate tags on interface field.
+     *
+     * @return Invalid tag for field.
+     * @param BAD Invalid tag for field.
+     */
+    int INT_FIELD = 0;
+    // violation 4 lines above 'Invalid '@return' tag for 'INT_FIELD'.'
+    // violation 4 lines above 'Invalid '@param' tag for 'INT_FIELD'.'
+  }
+}
+// xdoc section - end
