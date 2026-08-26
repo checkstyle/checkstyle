578439c5176cb5333726625650160643ffeb6d78
Md Tanvir Alam <tanviralamshuvo@gmail.com>
2026-08-26 19:02:35 +0000

    Issue #20954: Quote UnusedLocalVariable NestedClasses input messages

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedlocalvariable/InputUnusedLocalVariableNestedClasses7.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedlocalvariable/InputUnusedLocalVariableNestedClasses7.java
@@ -8,25 +8,25 @@ package com.puppycrawl.tools.checkstyle.checks.coding.unusedlocalvariable;
 
 public class InputUnusedLocalVariableNestedClasses7 {
   void foo() {
-    int a = 12; // violation, unused variable 'a'
-    int ab = 12; // violation, unused variable 'ab'
+    int a = 12; // violation 'Unused local variable 'a'.'
+    int ab = 12; // violation 'Unused local variable 'ab'.'
 
     class Ghi {
       void foo() {
         int a = 12;
-        int ab = 12; // violation, unused variable 'ab'
+        int ab = 12; // violation 'Unused local variable 'ab'.'
         System.out.println(a);
       }
     }
 
     class Def {
       void foo() {
-        int a = 12; // violation, unused variable 'a'
-        int ab = 12; // violation, unused variable 'ab'
+        int a = 12; // violation 'Unused local variable 'a'.'
+        int ab = 12; // violation 'Unused local variable 'ab'.'
 
         class InnerDef {
           void foo() {
-            int a = 12; // violation, unused variable 'a'
+            int a = 12; // violation 'Unused local variable 'a'.'
             int ab = 12;
             System.out.println(ab);
           }
