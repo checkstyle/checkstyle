902a1fb874a2812d4230bfe1fd3dbcea38152de7
sahitya0xsingh <sahitya0xsingh@users.noreply.github.com>
2026-08-27 13:55:47 +0530

    Issue #21164: MissingDeprecated: no support for module declarations

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/annotation/missingdeprecated/InputMissingDeprecatedModule.java
@@ -0,0 +1,14 @@
+/*
+MissingDeprecated
+violateExecutionOnNonTightHtml = (default)false
+
+*/
+// non-compiled with javac: Compilable with Java9
+
+/**
+ * Javadoc without the deprecated tag.
+ */
+// violation below 'Must include.*@java.lang.Deprecated.*@deprecated.*description.'
+@Deprecated
+module com.example.app {
+}
