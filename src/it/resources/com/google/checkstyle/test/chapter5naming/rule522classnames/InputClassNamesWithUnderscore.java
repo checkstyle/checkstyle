92b0b7b18dda90c1e595fa77f5a76e50a47c0fbd
Nikhil <nikk4645@gmail.com>
2026-08-28 03:16:18 +0530

    Issue #20954: Quote ClassNamesWithUnderscore violation messages

--- src/it/resources/com/google/checkstyle/test/chapter5naming/rule522classnames/InputClassNamesWithUnderscore.java
+++ src/it/resources/com/google/checkstyle/test/chapter5naming/rule522classnames/InputClassNamesWithUnderscore.java
@@ -2,15 +2,15 @@ package com.google.checkstyle.test.chapter5naming.rule522classnames;
 
 class InputClassNamesWithUnderscore {
 
-  class ConvertToKotlinVersion_ {} // violation 'Type name '.*' must match pattern'
+  class ConvertToKotlinVersion_ {} // violation "Type name '.*' must match pattern"
 
-  class ConvertToKotlinVersion_1_9_24 {} // violation 'Type name '.*' must match pattern'
+  class ConvertToKotlinVersion_1_9_24 {} // violation "Type name '.*' must match pattern"
 
   class ConvertToKotlinVersion1_9_24 {}
 
-  class ConvertToKotlinVersion1_9_24_ {} // violation 'Type name '.*' must match pattern'
+  class ConvertToKotlinVersion1_9_24_ {} // violation "Type name '.*' must match pattern"
 
   class ConvertToKotlinVersion10_0 {}
 
-  class ConvertToKotlinVersion__ {} // violation 'Type name '.*' must match pattern'
+  class ConvertToKotlinVersion__ {} // violation "Type name '.*' must match pattern"
 }
