f4199d80ad562a46e3d1fca0f4e0bc931838eef1
Md Tanvir Alam <tanviralamshuvo@gmail.com>
2026-08-29 01:07:53 +0000

    Issue #20954: Quote UnusedLocalVariable remaining input messages

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedlocalvariable/InputUnusedLocalVariableAllowNamedPatternVariables.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/unusedlocalvariable/InputUnusedLocalVariableAllowNamedPatternVariables.java
@@ -18,27 +18,27 @@ public class InputUnusedLocalVariableAllowNamedPatternVariables {
 
     String whatClass(Object object) {
         return switch (object) {
-            case String ignored -> "A String"; // violation 'Unused local variable'
-            case Integer ignored2 -> "An Integer"; // violation 'Unused local variable'
+            case String ignored -> "A String"; // violation 'Unused local variable 'ignored'.'
+            case Integer ignored2 -> "An Integer"; // violation 'Unused local variable 'ignored2'.'
             default -> "Something Else";
         };
     }
 
     void method(Object object) {
-        int x = 10; // violation 'Unused local variable'
-        if (object instanceof String ignored) { // violation 'Unused local variable'
+        int x = 10; // violation 'Unused local variable 'x'.'
+        if (object instanceof String ignored) { // violation 'Unused local variable 'ignored'.'
             System.out.println("string");
         }
     }
 
     String withrecord(Object object) {
-        return switch (object) { // violation below, unused local variable 'y'
-            case Ignored(int y, int z) -> "record switch"; // violation, unused local variable 'z'
+        return switch (object) { // violation below 'Unused local variable 'y'.'
+            case Ignored(int y, int z) -> "record switch"; // violation 'Unused local variable 'z'.'
             default -> "other";
         };
     }
 
-    String nameWithoutIgnored(Customer customer) { // violation below 'Unused local variable'
+    String nameWithoutIgnored(Customer customer) { // violation below 'ignoredAge'
         if (customer instanceof Person(String name, int ignoredAge)) {
             return name;
         } else if (customer instanceof Company(String companyName)) {
@@ -55,7 +55,7 @@ public class InputUnusedLocalVariableAllowNamedPatternVariables {
     }
 
     boolean isNested(Maybe<?> maybe) {
-        if (maybe instanceof Some(Some<?> inner)) { // violation 'Unused local variable'
+        if (maybe instanceof Some(Some<?> inner)) { // violation 'Unused local variable 'inner'.'
             return true;
         }
         return false;
