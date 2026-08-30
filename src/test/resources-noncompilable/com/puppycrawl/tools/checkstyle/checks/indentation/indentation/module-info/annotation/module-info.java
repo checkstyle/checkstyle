ff9fb9b5b100c70cb7ee86ce773ba58a7610acd7
sahitya0xsingh <sahitya0xsingh@users.noreply.github.com>
2026-08-30 16:29:49 +0530

    Issue #21160: Add indentation support for module declarations

---
+++ src/test/resources-noncompilable/com/puppycrawl/tools/checkstyle/checks/indentation/indentation/module-info/annotation/module-info.java
@@ -0,0 +1,5 @@
+@Deprecated //indent:0 exp:0
+  @SuppressWarnings("all") //indent:2 exp:2
+    module com.example.app { //indent:4 exp:0 warn
+    requires java.base; //indent:4 exp:4
+  } //indent:2 exp:0 warn
