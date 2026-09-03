dba0061bf2fbba8dcee1c91a3d6fe4bf0547cc01
dewa3355 <134440439+dewa3355@users.noreply.github.com>
2026-09-03 10:10:53 +0000

    Issue #21137: add default config example for SuppressionSingleFilter
    Co-authored-by: dewa3355 <dewa3355@users.noreply.github.com>

--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressionsinglefilter/Example2.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/filters/suppressionsinglefilter/Example2.java
@@ -2,28 +2,32 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="NoWhitespaceAfter"/>
+    <module name="MagicNumber"/>
   </module>
   <module name="SuppressionSingleFilter">
+    <property name="checks" value="NoWhitespaceAfter|MagicNumber"/>
     <property name="files" value="Example2.java"/>
-    <property name="checks" value="NoWhitespaceAfter"/>
-    <property name="columns" value="12"/>
+    <property name="lines" value="1,5-100"/>
+  </module>
+  <module name="SuppressionSingleFilter">
+    <property name="message" value="Missing a Javadoc comment for"/>
   </module>
 </module>
 */
 package com.puppycrawl.tools.checkstyle.filters.suppressionsinglefilter;
 // xdoc section - start
 public class Example2 {
-
+  // filtered violation below ''5' is a magic number'
   private int MyVariable = 5;
 
   public void exampleMethod(int a, int b) {
-    int value = 100;
-    // filtered violation below ''.' is followed by whitespace'
-    Integer. parseInt("3");
+    int value = 100; // filtered violation ''100' is a magic number'
+
+    Integer. parseInt("3"); // filtered violation ''.' is followed by whitespace'
   }
 
   public void printExample() {
-    int [] x; // violation ''int' is followed by whitespace'
+    int [] x; // filtered violation ''int' is followed by whitespace'
     System.out.println(
             "example"
     );
