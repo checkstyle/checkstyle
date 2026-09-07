03e7e068979f725835451860d502e182fc94a98c
Rupesh Kumar <rupesh.kumar.92.a@gmail.com>
2026-09-07 20:24:03 +0530

    Issue #20590: add OneStatementPerLine compact source input coverage

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/AllChecksCompactSourceCoverageTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/AllChecksCompactSourceCoverageTest.java
@@ -201,7 +201,6 @@ public class AllChecksCompactSourceCoverageTest {
         "NoWhitespaceBeforeCheck",
         "NonEmptyAtclauseDescriptionCheck",
         "NumericalPrefixesInfixesSuffixesCharacterCaseCheck",
-        "OneStatementPerLineCheck",
         "OneTopLevelClassCheck",
         "OperatorWrapCheck",
         "OuterTypeNumberCheck",
