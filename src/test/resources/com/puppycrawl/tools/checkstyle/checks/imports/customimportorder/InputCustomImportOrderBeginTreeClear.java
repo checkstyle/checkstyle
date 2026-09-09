00e4b2bbd4c722da6d01c755141fbaaec5c79de1
shreyo2005 <diya.laik2005@gmail.com>
2026-09-09 11:04:02 +0530

    Issue #21480: Convert CustomImportOrderCheckTest#testBeginTreeClear to embedded config

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/imports/customimportorder/InputCustomImportOrderBeginTreeClear.java
@@ -0,0 +1,19 @@
+/*
+CustomImportOrder
+customImportOrderRules = STANDARD_JAVA_PACKAGE###SPECIAL_IMPORTS
+standardPackageRegExp = (default)^(java|javax)\\.
+thirdPartyPackageRegExp = (default).*
+specialImportsRegExp = com
+separateLineBetweenGroups = false
+sortImportsInGroupAlphabetically = (default)false
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.imports.customimportorder;
+
+import java.io.File;
+import javax.lang.model.SourceVersion;
+import com.google.errorprone.annotations.*;
+
+public class InputCustomImportOrderBeginTreeClear {}
