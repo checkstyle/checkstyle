3e1b9028365f3636c1efe20712937a9a8065c10f
smita1078 <smita.prajapati082@gmail.com>
2026-09-09 22:23:56 +0530

    Issue #21137: Fix modules suppressed from the default-config example test

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XdocsExamplesAstConsistencyTest.java
@@ -149,7 +149,6 @@ public class XdocsExamplesAstConsistencyTest {
      * Until: <a href="https://github.com/checkstyle/checkstyle/issues/21137">...</a>
      */
     private static final Set<String> EXAMPLE_DEFAULT_CONFIG_SUPPRESSED_MODULES = Set.of(
-            "checks/coding/returncount",
             "checks/descendanttoken",
             "checks/imports/importcontrol",
             "filters/severitymatchfilter",
--- src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/returncount/Example5.java
+++ src/xdocs-examples/resources/com/puppycrawl/tools/checkstyle/checks/coding/returncount/Example5.java
@@ -2,16 +2,7 @@
 <module name="Checker">
   <module name="TreeWalker">
     <module name="ReturnCount">
-      <property name="maxForVoid" value="0"/>
-      <property name="tokens" value="CTOR_DEF"/>
-    </module>
-    <module name="ReturnCount">
-      <property name="max" value="1"/>
-      <property name="tokens" value="LAMBDA"/>
-    </module>
-    <module name="ReturnCount">
-      <property name="max" value="2"/>
-      <property name="tokens" value="METHOD_DEF"/>
+      <property name="max" value="3"/>
     </module>
   </module>
 </module>
@@ -23,21 +14,21 @@ import java.util.function.Predicate;
 // xdoc section - start
 public class Example5 {
     public Example5() {}
-    // violation below 'max allowed for void methods/constructors/lambdas is 0'
+    // ok below, because default void restriction is 1
     public Example5(int i) { return; }
 
     public int signA(int x) {
         if (x < -2) { return -1; }
         return 0;
     }
-    // violation below 'max allowed for non-void methods/lambdas is 2'
+    // violation below 'max allowed for non-void methods/lambdas is 3'
     public int signB(int x) {
         if (x < -2) { return -1; }
         if (x == 0) { return 0; }
         if (x > 2) { return 2; }
         return 1;
     }
-    // violation below 'max allowed for non-void methods/lambdas is 1'
+    // ok below, because non-void restriction is 3
     final Predicate<Integer> lambdaA = i -> {
         if (i > 5) { return true; }
         return false;
