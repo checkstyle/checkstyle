e6e9423fd770513230007532904350f6ecf47822
Amit Kumar Deohoria <kamit5741@gmail.com>
2026-09-14 12:50:54 +0530

    Issue #21480: Convert JavadocLinkFirstOccurrenceCheckTest#testClearStateBetweenFiles to embedded config

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoclinkfirstoccurrence/InputJavadocLinkFirstOccurrenceClearState2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoclinkfirstoccurrence/InputJavadocLinkFirstOccurrenceClearState2.java
@@ -1,7 +1,13 @@
+/*
+JavadocLinkFirstOccurrence
+violateExecutionOnNonTightHtml = (default)false
+
+*/
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoclinkfirstoccurrence;
 
 class InputJavadocLinkFirstOccurrenceClearState2 {
 
+    // violation 2 lines below 'String'
     /**
      * Uses a {@link String} and a {@link String}.
      */
