65a9d632db43ffc41542bf7c7db080b71660d7df
Amit Kumar Deohoria <kamit5741@gmail.com>
2026-09-14 12:50:54 +0530

    Issue #21480: Convert JavadocLinkFirstOccurrenceCheckTest#testClearStateBetweenFiles to embedded config

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoclinkfirstoccurrence/InputJavadocLinkFirstOccurrenceClearState1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadoclinkfirstoccurrence/InputJavadocLinkFirstOccurrenceClearState1.java
@@ -1,7 +1,13 @@
+/*
+JavadocLinkFirstOccurrence
+violateExecutionOnNonTightHtml = (default)false
+
+*/
 package com.puppycrawl.tools.checkstyle.checks.javadoc.javadoclinkfirstoccurrence;
 
 class InputJavadocLinkFirstOccurrenceClearState1 {
 
+    // ok, javadoc aligned with ClearState2 line to exercise parent-cache clearing across files
     /**
      * Uses a {@link String}.
      */
