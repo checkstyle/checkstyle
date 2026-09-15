38904b02eeb6d6bc4c6166c723f19bc60755f164
Amit Kumar Deohoria <kamit5741@gmail.com>
2026-09-15 23:28:01 +0530

    Issue #21480: Convert IllegalInstantiationCheckTest#testStateIsClearedOnBeginTreePackageName to embedded config

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalinstantiation/InputIllegalInstantiationBeginTree1.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/illegalinstantiation/InputIllegalInstantiationBeginTree1.java
@@ -1,3 +1,12 @@
+/*
+IllegalInstantiation
+classes = java.lang.Boolean,com.puppycrawl.tools.checkstyle.checks.coding.illegalinstantiation.\
+          InputIllegalInstantiationBeginTree2.InputModifier
+tokens = (default)IMPORT, LITERAL_NEW, PACKAGE_DEF, CLASS_DEF, RECORD_DEF
+
+
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.coding.illegalinstantiation;
 
 public class InputIllegalInstantiationBeginTree1 {
