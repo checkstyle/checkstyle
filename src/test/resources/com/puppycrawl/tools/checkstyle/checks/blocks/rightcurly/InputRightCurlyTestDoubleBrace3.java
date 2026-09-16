eecd8851ad200c2bf6149270a40d5524c3df2f7d
atharv <atharv55215@gmail.com>
2026-09-16 20:01:13 +0530

    Issue #21609: Fixed NPE in RightCurlyCheck

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/blocks/rightcurly/InputRightCurlyTestDoubleBrace3.java
@@ -0,0 +1,22 @@
+/*
+RightCurly
+option = ALONE
+tokens = CLASS_DEF, METHOD_DEF, CTOR_DEF, LITERAL_FOR, LITERAL_WHILE, STATIC_INIT, \
+         INSTANCE_INIT, ANNOTATION_DEF, ENUM_DEF, INTERFACE_DEF, RECORD_DEF, COMPACT_CTOR_DEF
+
+
+*/
+
+package com.puppycrawl.tools.checkstyle.checks.blocks.rightcurly;
+
+public class InputRightCurlyTestDoubleBrace3 {{
+}}
+// 2 violations above:
+// ''}' at column 1 should be alone on a line.'
+// ''}' at column 2 should be alone on a line.'
+
+class Inner {{
+}};
+// 2 violations above:
+// ''}' at column 1 should be alone on a line.'
+// ''}' at column 2 should be alone on a line.'
