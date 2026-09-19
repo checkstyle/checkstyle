cd2187dea35527f4eea07974abc89336e0f774b5
Amit Kumar Deoghoria <kamit5741@gmail.com>
2026-09-19 09:46:10 +0530

    Issue #21480: Convert NonEmptyAtclauseDescriptionCheckTest#testDecoderOnMalformedInput to embedded config

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/nonemptyatclausedescription/InputNonEmptyAtclauseDescriptionDifferentCharset.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/nonemptyatclausedescription/InputNonEmptyAtclauseDescriptionDifferentCharset.java
@@ -1,3 +1,12 @@
+/*xml
+<module name="Checker">
+  <property name="charset" value="US-ASCII"/>
+  <module name="TreeWalker">
+    <module name="NonEmptyAtclauseDescription"/>
+  </module>
+</module>
+*/
+
 package com.puppycrawl.tools.checkstyle.checks.javadoc.nonemptyatclausedescription;
 
 public class InputNonEmptyAtclauseDescriptionDifferentCharset {
