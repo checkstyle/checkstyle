0442fca8c7017a35dbffc851c39103699979ea9b
Amit Kumar Deoghoria <kamit5741@gmail.com>
2026-09-21 00:47:23 +0530

    Issue #21480: Convert LineEndingCheckTest#testInputLineEndingCrExpected3 to embedded config

--- src/test/java/com/puppycrawl/tools/checkstyle/checks/LineEndingCheckTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/checks/LineEndingCheckTest.java
@@ -169,11 +169,8 @@ public class LineEndingCheckTest extends AbstractModuleTestSupport {
             "6: " + getCheckMessage(MSG_KEY_WRONG_ENDING, CR, CRLF),
             "7: " + getCheckMessage(MSG_KEY_WRONG_ENDING, CR, CRLF),
         };
-
-        final DefaultConfiguration checkConfig = createModuleConfig(LineEndingCheck.class);
-        checkConfig.addProperty("lineEnding", "cr");
-
-        verify(checkConfig,
+        verifyWithInlineConfigParserSeparateConfigAndTarget(
+                getPath("InputLineEndingCrExpected3Config.java"),
                 getPath("InputLineEndingCrExpected3.java"),
                 expected);
     }
