e37508cb123b854209b1a381e5757e23b78b9dd4
Rafael <roliveira1111@gmail.com>
2026-09-22 20:18:26 +0100

    Issue #15456: Define violation messages for MethodCountCheck

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/methodcount/InputMethodCount2.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/sizes/methodcount/InputMethodCount2.java
@@ -18,7 +18,10 @@ public enum InputMethodCount2 {
         @Override void something() {};
     },
 
-    BLUE { // 2 violations
+    BLUE {
+        // 2 violations above:
+        //   'Number of private methods is 1 (max allowed is 0).'
+        //   'Total number of methods is 3 (max allowed is 2).'
         @Override void something() {};
         protected void other1() {};
         private void other2() {};
