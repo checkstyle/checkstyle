9884ca77fd4f9addb9360b31a12f54e677eb6a94
SidheshwarSarangal <163771854+SidheshwarSarangal@users.noreply.github.com>
2026-09-24 17:47:28 +0530

    Issue #6450: Detect redundant imports covered by wildcard imports

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/RedundantImportCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/RedundantImportCheck.java
@@ -19,7 +19,9 @@
 
 package com.puppycrawl.tools.checkstyle.checks.imports;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import com.puppycrawl.tools.checkstyle.FileStatefulCheck;
@@ -40,6 +42,8 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  *   package, e.g. importing {@code java.lang.String}.</li>
  *   <li>The class non-statically imported is from the same package as the
  *   current package.</li>
+ *   <li>A non-static import is covered by a wildcard import from the same
+ *   package or type.</li>
  * </ul>
  *
  * @since 3.0
@@ -66,8 +70,17 @@ public class RedundantImportCheck
      */
     public static final String MSG_DUPLICATE = "import.duplicate";
 
-    /** Set of the imports. */
-    private final Set<FullIdent> imports = new HashSet<>();
+    /**
+     * A key is pointing to the warning message text in "messages.properties"
+     * file.
+     */
+    public static final String MSG_REDUNDANT_WILDCARD = "import.redundantWildcard";
+
+    /** Name of the package implicitly imported by Java. */
+    private static final String JAVA_LANG_PACKAGE = "java.lang";
+
+    /** Map of the imports to their AST nodes. */
+    private final Map<FullIdent, DetailAST> imports = new HashMap<>();
     /** Set of static and module imports. */
     private final Set<FullIdent> staticAndModuleImports = new HashSet<>();
 
@@ -117,7 +130,7 @@ public class RedundantImportCheck
         else if (ast.getType() == TokenTypes.IMPORT) {
             final FullIdent imp = FullIdent.createFullIdentBelow(ast);
             final String importText = imp.getText();
-            if (isFromPackage(importText, "java.lang")) {
+            if (isFromPackage(importText, JAVA_LANG_PACKAGE)) {
                 log(ast, MSG_LANG, importText);
             }
             // imports from unnamed package are not allowed,
@@ -126,10 +139,10 @@ public class RedundantImportCheck
                 log(ast, MSG_SAME, importText);
             }
             // Check for a duplicate import
-            imports.stream().filter(full -> importText.equals(full.getText()))
+            imports.keySet().stream().filter(full -> importText.equals(full.getText()))
                 .forEach(full -> log(ast, MSG_DUPLICATE, full.getLineNo(), importText));
 
-            imports.add(imp);
+            imports.put(imp, ast);
         }
         else {
             // Check for a duplicate static or module import
@@ -148,6 +161,32 @@ public class RedundantImportCheck
         }
     }
 
+    @Override
+    public void finishTree(DetailAST rootAst) {
+        final Map<String, FullIdent> wildcardImports = new HashMap<>();
+        for (FullIdent importStatement : imports.keySet()) {
+            final String importText = importStatement.getText();
+            if (isWildcardImport(importText)) {
+                wildcardImports.putIfAbsent(getImportQualifier(importText), importStatement);
+            }
+        }
+
+        for (Map.Entry<FullIdent, DetailAST> importEntry : imports.entrySet()) {
+            final FullIdent importStatement = importEntry.getKey();
+            final String importText = importStatement.getText();
+            if (!isWildcardImport(importText)
+                    && !isFromPackage(importText, JAVA_LANG_PACKAGE)
+                    && (pkgName == null || !isFromPackage(importText, pkgName))) {
+                final FullIdent wildcardImport =
+                    wildcardImports.get(getImportQualifier(importText));
+                if (wildcardImport != null) {
+                    log(importEntry.getValue(), MSG_REDUNDANT_WILDCARD,
+                        importText, wildcardImport.getText());
+                }
+            }
+        }
+    }
+
     /**
      * Determines if an import statement is for types from a specified package.
      *
@@ -159,9 +198,28 @@ public class RedundantImportCheck
         // imports from unnamed package are not allowed:
         // https://docs.oracle.com/javase/specs/jls/se7/html/jls-7.html#jls-7.5
         // So '.' must be present in member name and we are not checking for it
+        return pkg.equals(getImportQualifier(importName));
+    }
+
+    /**
+     * Determines whether an import uses the wildcard form.
+     *
+     * @param importName the import name
+     * @return whether the import uses the wildcard form
+     */
+    private static boolean isWildcardImport(String importName) {
+        return importName.endsWith(".*");
+    }
+
+    /**
+     * Returns the qualifier of an import name.
+     *
+     * @param importName the import name
+     * @return the import qualifier
+     */
+    private static String getImportQualifier(String importName) {
         final int index = importName.lastIndexOf('.');
-        final String front = importName.substring(0, index);
-        return pkg.equals(front);
+        return importName.substring(0, index);
     }
 
 }
